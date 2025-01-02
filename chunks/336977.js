9d5baadb21959e673579e769a2b470adad7191b1
DiscrapperManager <admin@minerpl.xyz>
2025-01-02 19:24:53 +0000

    canaryRelease: 356158
    Main scripts: 3
    Lazy Loaded Scripts: 923
    Chunks:
     * Main Files: 11938
     * Scripts: 10336
    Strings: 0
    Routes: 687
    Experiments Count: 373

--- chunks/336977.js
+++ chunks/336977.js
@@ -1,14 +1,14 @@
 e.r(n),
     e.d(n, {
         VibingWumpus: function () {
-            return b;
+            return x;
         }
     }),
     e(47120);
 var i = e(200651),
-    o = e(192379),
-    r = e(120356),
-    a = e.n(r),
+    r = e(192379),
+    o = e(120356),
+    a = e.n(o),
     c = e(399606),
     s = e(481060),
     u = e(607070),
@@ -16,32 +16,31 @@ var i = e(200651),
     d = e(832239),
     _ = e(407383),
     I = e(981631),
-    p = e(388032),
+    N = e(388032),
     f = e(724263),
-    C = e(461247),
-    N = e(617921);
-function b(t) {
-    let { onClose: n, source: r = _.I.INAPPROPRIATE_CONVERSATION } = t,
-        b = (0, c.e7)([u.Z], () => u.Z.useReducedMotion),
-        [x, h] = o.useState(!1);
-    o.useEffect(() => {
+    p = e(617921);
+function x(t) {
+    let { onClose: n, source: o = _.I.INAPPROPRIATE_CONVERSATION } = t,
+        x = (0, c.e7)([u.Z], () => u.Z.useReducedMotion),
+        [C, A] = r.useState(!1);
+    r.useEffect(() => {
         let t = new Date();
         return (
-            l.default.track(I.rMx.VIBING_WUMPUS_VIEWED, { source: r }),
+            l.default.track(I.rMx.VIBING_WUMPUS_VIEWED, { source: o }),
             (0, d.JO)(),
             () => {
                 l.default.track(I.rMx.VIBING_WUMPUS_CLOSED, {
                     duration_open_ms: new Date().getTime() - t.getTime(),
-                    source: r
+                    source: o
                 }),
                     (0, d.zu)();
             }
         );
-    }, [r]);
-    let A = () => {
-        x ? ((0, d.JO)(), l.default.track(I.rMx.VIBING_WUMPUS_ACTION, { action: _.G.PLAY })) : ((0, d.hW)(), l.default.track(I.rMx.VIBING_WUMPUS_ACTION, { action: _.G.PAUSE })), h(!x);
+    }, [o]);
+    let h = () => {
+        C ? ((0, d.JO)(), l.default.track(I.rMx.VIBING_WUMPUS_ACTION, { action: _.G.PLAY })) : ((0, d.hW)(), l.default.track(I.rMx.VIBING_WUMPUS_ACTION, { action: _.G.PAUSE })), A(!C);
     };
-    async function m() {
+    async function S() {
         let { default: t } = await e.e('29641').then(e.t.bind(e, 937359, 19));
         return t;
     }
@@ -50,27 +49,27 @@ function b(t) {
             (0, i.jsx)('div', {
                 className: f.ringContainer,
                 children: (0, i.jsx)('img', {
-                    src: N,
+                    src: p,
                     alt: '',
                     className: f.__invalid_ringArt
                 })
             }),
             (0, i.jsx)(s.LottieAnimation, {
-                importData: m,
-                shouldAnimate: !x && !b,
+                importData: S,
+                shouldAnimate: !C && !x,
                 className: f.wumpus,
-                pauseAtFrame: b ? 200 : void 0
+                pauseAtFrame: x ? 200 : void 0
             }),
             (0, i.jsx)(s.Tooltip, {
-                text: x ? p.intl.string(p.t.RscU7O) : p.intl.string(p.t.ZcgDJS),
+                text: C ? N.intl.string(N.t.RscU7O) : N.intl.string(N.t.ZcgDJS),
                 position: 'top',
-                'aria-label': x ? p.intl.string(p.t.RscU7O) : p.intl.string(p.t.ZcgDJS),
+                'aria-label': C ? N.intl.string(N.t.RscU7O) : N.intl.string(N.t.ZcgDJS),
                 children: (t) =>
                     (0, i.jsx)(s.Clickable, {
                         ...t,
-                        onClick: A,
+                        onClick: h,
                         className: f.iconButton,
-                        children: x
+                        children: C
                             ? (0, i.jsx)(s.PlayIcon, {
                                   size: 'xs',
                                   color: 'currentColor',
@@ -83,37 +82,37 @@ function b(t) {
                               })
                     })
             }),
-            (0, i.jsxs)(s.ModalContent, {
-                className: a()(C.body, f.body),
-                children: [
-                    (0, i.jsxs)('div', {
-                        className: a()(C.textContainer, f.textContainer),
-                        children: [
-                            (0, i.jsx)(s.Heading, {
-                                variant: 'heading-xl/semibold',
-                                color: 'header-primary',
-                                children: p.intl.string(p.t.L4ifkZ)
-                            }),
-                            (0, i.jsx)(s.Text, {
-                                variant: 'text-md/normal',
-                                color: 'header-secondary',
-                                children: p.intl.string(p.t.R8LCMT)
-                            })
-                        ]
-                    }),
-                    (0, i.jsx)('div', {
-                        className: f.buttonContainer,
-                        children: (0, i.jsx)(s.Button, {
-                            fullWidth: !0,
-                            color: s.Button.Colors.BRAND,
-                            size: s.Button.Sizes.MEDIUM,
-                            onClick: () => {
-                                n(), l.default.track(I.rMx.VIBING_WUMPUS_ACTION, { action: _.G.BACK_TO_CONVERSATION });
-                            },
-                            children: p.intl.string(p.t['8eKkaW'])
+            (0, i.jsx)(s.ModalContent, {
+                className: a()(f.body),
+                children: (0, i.jsxs)('div', {
+                    className: a()(f.textContainer),
+                    children: [
+                        (0, i.jsx)(s.Heading, {
+                            variant: 'heading-xl/semibold',
+                            color: 'header-primary',
+                            children: N.intl.string(N.t.L4ifkZ)
+                        }),
+                        (0, i.jsx)(s.Text, {
+                            variant: 'text-md/normal',
+                            color: 'header-secondary',
+                            children: N.intl.string(N.t.R8LCMT)
                         })
+                    ]
+                })
+            }),
+            (0, i.jsx)(s.ModalFooter, {
+                children: (0, i.jsx)('div', {
+                    className: f.buttonContainer,
+                    children: (0, i.jsx)(s.Button, {
+                        fullWidth: !0,
+                        color: s.Button.Colors.BRAND,
+                        size: s.Button.Sizes.MEDIUM,
+                        onClick: () => {
+                            n(), l.default.track(I.rMx.VIBING_WUMPUS_ACTION, { action: _.G.BACK_TO_CONVERSATION });
+                        },
+                        children: N.intl.string(N.t['8eKkaW'])
                     })
-                ]
+                })
             })
         ]
     });
@@ -122,6 +121,6 @@ n.default = (t) => {
     let { onClose: n, transitionState: e } = t;
     return (0, i.jsx)(s.ModalRoot, {
         transitionState: e,
-        children: (0, i.jsx)(b, { onClose: n })
+        children: (0, i.jsx)(x, { onClose: n })
     });
 };
