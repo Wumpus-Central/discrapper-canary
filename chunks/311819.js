1ebfaec5c5c4804481cea3e6314ac300f43a8c0d
DiscrapperManager <admin@minerpl.xyz>
2025-01-06 19:52:14 +0000

    canaryRelease: 356736
    Main scripts: 3
    Lazy Loaded Scripts: 926
    Chunks:
     * Main Files: 11939
     * Scripts: 10349
    Strings: 0
    Routes: 687
    Experiments Count: 375

--- chunks/311819.js
+++ chunks/311819.js
@@ -1,25 +1,31 @@
 r.d(n, {
+    H: function () {
+        return c;
+    },
     J: function () {
-        return s;
+        return d;
     }
 });
-var i = r(47120);
-var a = r(981631);
-function s(e) {
-    let { id: n, customInstallUrl: r, installParams: i, integrationTypesConfig: s } = e;
+var i = r(610138);
+var a = r(216116);
+var s = r(78328);
+var o = r(815648);
+var l = r(47120);
+var u = r(981631);
+function c(e) {
+    let { applicationId: n, customId: r, referrerId: i } = e,
+        a = new URLSearchParams();
+    return null != i && a.set('referrer_id', i), null != r && a.set('custom_id', r), ''.concat(location.protocol, '//').concat(location.host).concat(u.Z5c.ACTIVITY_DETAILS(n), '?').concat(a.toString());
+}
+function d(e) {
+    let { id: n, customInstallUrl: r, installParams: i, integrationTypesConfig: a } = e;
     if (null != r) return r;
-    let o = {};
-    o.client_id = n;
-    let l = null != s && Object.values(s).some((e) => (null == e ? void 0 : e.oauth2_install_params) != null || (null == e ? void 0 : e.oauth2InstallParams) != null);
-    if (null != i && !l) {
+    let s = new URLSearchParams();
+    s.set('client_id', n);
+    let o = null != a && Object.values(a).some((e) => (null == e ? void 0 : e.oauth2_install_params) != null || (null == e ? void 0 : e.oauth2InstallParams) != null);
+    if (null != i && !o) {
         let { permissions: e, scopes: n } = i;
-        null != e && (o.permissions = e), null != n && (o.scope = n.join(' '));
+        null != e && s.set('permissions', e), null != n && s.set('scope', n.join(' '));
     }
-    let u = Object.entries(o)
-        .map((e) => {
-            let [n, r] = e;
-            return ''.concat(n, '=').concat(encodeURIComponent(r));
-        })
-        .join('&');
-    return ''.concat(location.protocol, '//').concat(location.host).concat(a.Z5c.OAUTH2_AUTHORIZE, '?').concat(u);
+    return ''.concat(location.protocol, '//').concat(location.host).concat(u.Z5c.OAUTH2_AUTHORIZE, '?').concat(s.toString());
 }
--- chunks/488977.js
+++ chunks/488977.js
@@ -1,8 +1,9 @@
 t.d(n, {
     Z: function () {
-        return I;
+        return j;
     }
 }),
+    t(789020),
     t(47120);
 var i = t(200651),
     l = t(120356),
@@ -16,52 +17,61 @@ var i = t(200651),
     m = t(726721),
     p = t(726521),
     h = t(973616),
-    f = t(626135),
-    C = t(572004),
-    v = t(135431),
-    x = t(541099),
-    _ = t(783097),
-    N = t(570949),
-    g = t(314734),
-    A = t(981631),
-    b = t(388032),
-    E = t(472033);
-function I(e) {
-    let { application: n, channel: t, className: l, sectionName: I } = e,
-        P = (0, o.e7)([x.Z], () => x.Z.entrypoint()),
-        j = (0, _.L1)(n),
-        y = (0, v.Eb)(j),
-        S = {
+    f = t(594174),
+    C = t(626135),
+    v = t(572004),
+    x = t(630388),
+    _ = t(135431),
+    N = t(541099),
+    g = t(783097),
+    A = t(570949),
+    E = t(314734),
+    b = t(981631),
+    I = t(388032),
+    P = t(472033);
+function j(e) {
+    var n;
+    let { application: t, channel: l, className: j, sectionName: y } = e,
+        S = (0, o.e7)([N.Z], () => N.Z.entrypoint()),
+        T = (0, g.L1)(t),
+        L = (0, _.Eb)(T),
+        R = {
             location: c.Vh.APP_LAUNCHER_APPLICATION_VIEW_MORE_MENU,
-            application_id: n.id,
-            section_name: I,
-            source: x.Z.lastShownEntrypoint()
+            application_id: t.id,
+            section_name: y,
+            source: N.Z.lastShownEntrypoint()
         },
-        T = m.Z.useExperiment({ location: c.Vh.APP_LAUNCHER_APPLICATION_VIEW_MORE_MENU }, { autoTrackExposure: !0 }).enabled,
-        L = (0, u.Z)({
-            id: n.id,
-            label: b.intl.string(b.t['+NP/b2'])
+        M = f.default.getCurrentUser(),
+        Z = m.Z.useExperiment({ location: c.Vh.APP_LAUNCHER_APPLICATION_VIEW_MORE_MENU }, { autoTrackExposure: !0 }).enabled,
+        k = (0, u.Z)({
+            id: t.id,
+            label: I.intl.string(I.t['+NP/b2'])
         }),
-        R = (0, N.P)({ application: n });
+        O = (0, A.P)({ application: t }),
+        U = (0, x.yE)(null !== (n = t.flags) && void 0 !== n ? n : 0, b.udG.EMBEDDED);
     return (0, i.jsxs)('div', {
-        className: E.container,
+        className: P.container,
         children: [
             (0, i.jsx)(r.Clickable, {
                 onClick: () => {
-                    f.default.track(A.rMx.APP_LAUNCHER_APPLICATION_LINK_COPIED, {
-                        application_id: n.id,
-                        source: P
-                    }),
-                        (0, C.JG)(
-                            (0, d.J)({
-                                id: n.id,
-                                ...j
-                            })
-                        ),
-                        (0, r.showToast)((0, r.createToast)(b.intl.string(b.t['L/PwZW']), r.ToastType.SUCCESS));
+                    let e = U
+                        ? (0, d.H)({
+                              applicationId: t.id,
+                              referrerId: null == M ? void 0 : M.id
+                          })
+                        : (0, d.J)({
+                              id: t.id,
+                              ...T
+                          });
+                    (0, v.JG)(e),
+                        C.default.track(b.rMx.APP_LAUNCHER_APPLICATION_LINK_COPIED, {
+                            application_id: t.id,
+                            source: S
+                        }),
+                        (0, r.showToast)((0, r.createToast)(I.intl.string(I.t['L/PwZW']), r.ToastType.SUCCESS));
                 },
-                className: a()(E.clickable, l),
-                'aria-label': b.intl.string(b.t.WqhZsr),
+                className: a()(P.clickable, j),
+                'aria-label': I.intl.string(I.t.WqhZsr),
                 children: (0, i.jsx)(r.LinkIcon, {
                     size: 'sm',
                     color: r.tokens.colors.INTERACTIVE_ACTIVE
@@ -69,53 +79,53 @@ function I(e) {
             }),
             (0, i.jsx)(r.Popout, {
                 renderPopout: (e) => {
-                    let { closePopout: l } = e;
+                    let { closePopout: n } = e;
                     return (0, i.jsxs)(r.Menu, {
-                        className: g.NN,
+                        className: E.NN,
                         navId: 'app-details-more-menu',
-                        onClose: l,
-                        'aria-label': b.intl.string(b.t.AXIHpa),
+                        onClose: n,
+                        'aria-label': I.intl.string(I.t.AXIHpa),
                         onSelect: void 0,
                         children: [
                             (0, i.jsxs)(r.MenuGroup, {
                                 children: [
-                                    y
+                                    L
                                         ? (0, i.jsx)(r.MenuItem, {
                                               id: 'add-app',
-                                              label: b.intl.string(b.t.NgXl3N),
+                                              label: I.intl.string(I.t.NgXl3N),
                                               action: () => {
-                                                  null == j.customInstallUrl && (0, s.yw)(A.rMx.APP_LAUNCHER_OAUTH2_AUTHORIZE_OPENED, S),
-                                                      (0, v.LO)({
-                                                          ...j,
+                                                  null == T.customInstallUrl && (0, s.yw)(b.rMx.APP_LAUNCHER_OAUTH2_AUTHORIZE_OPENED, R),
+                                                      (0, _.LO)({
+                                                          ...T,
                                                           oauth2Callback: (e) => {
                                                               let { location: n } = e;
-                                                              null != n && (0, s.yw)(A.rMx.APP_LAUNCHER_OAUTH2_AUTHORIZE_SUCCEEDED, S);
+                                                              null != n && (0, s.yw)(b.rMx.APP_LAUNCHER_OAUTH2_AUTHORIZE_SUCCEEDED, R);
                                                           },
                                                           source: 'app_launcher_app_details'
                                                       });
                                               }
                                           })
                                         : null,
-                                    T && n instanceof h.ZP
+                                    Z && t instanceof h.ZP
                                         ? (0, i.jsx)(r.MenuItem, {
                                               id: 'report-app',
                                               color: 'danger',
-                                              label: b.intl.string(b.t.jhJze3),
+                                              label: I.intl.string(I.t.jhJze3),
                                               action: () => {
                                                   var e;
                                                   (0, p.uu)({
-                                                      application: n,
+                                                      application: t,
                                                       entrypoint: 'app_launcher',
-                                                      contextualGuildId: null !== (e = t.getGuildId()) && void 0 !== e ? e : void 0,
-                                                      contextualChannelId: t.id
+                                                      contextualGuildId: null !== (e = l.getGuildId()) && void 0 !== e ? e : void 0,
+                                                      contextualChannelId: l.id
                                                   });
                                               }
                                           })
                                         : null
                                 ]
                             }),
-                            null != R && (0, i.jsxs)(r.MenuGroup, { children: [...R] }),
-                            (0, i.jsx)(r.MenuGroup, { children: L })
+                            null != O && (0, i.jsxs)(r.MenuGroup, { children: [...O] }),
+                            (0, i.jsx)(r.MenuGroup, { children: k })
                         ]
                     });
                 },
@@ -124,8 +134,8 @@ function I(e) {
                 children: (e) =>
                     (0, i.jsx)(r.Clickable, {
                         onClick: e.onClick,
-                        className: a()(E.clickable, l),
-                        'aria-label': b.intl.string(b.t.UKOtz8),
+                        className: a()(P.clickable, j),
+                        'aria-label': I.intl.string(I.t.UKOtz8),
                         children: (0, i.jsx)(r.MoreHorizontalIcon, {
                             size: 'sm',
                             color: r.tokens.colors.INTERACTIVE_ACTIVE
