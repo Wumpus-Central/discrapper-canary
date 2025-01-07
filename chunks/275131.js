17bee23e8f43008c9f7868bc64d5e44207563780
DiscrapperManager <admin@minerpl.xyz>
2025-01-07 16:18:51 +0000

    canaryRelease: 356920
    Main scripts: 3
    Lazy Loaded Scripts: 926
    Chunks:
     * Main Files: 11939
     * Scripts: 10341
    Strings: 0
    Routes: 687
    Experiments Count: 374

--- chunks/275131.js
+++ chunks/275131.js
@@ -1,14 +1,15 @@
 var i = r(47120);
 var a = r(664751),
-    s = r(544891),
-    o = r(570140),
-    l = r(147913),
-    u = r(900849),
-    c = r(356164),
-    d = r(726115),
-    f = r(128449),
-    _ = r(981631);
-function h(e, n, r) {
+    s = r(975641),
+    o = r(544891),
+    l = r(570140),
+    u = r(147913),
+    c = r(900849),
+    d = r(356164),
+    f = r(726115),
+    _ = r(128449),
+    h = r(981631);
+function p(e, n, r) {
     return (
         n in e
             ? Object.defineProperty(e, n, {
@@ -21,98 +22,98 @@ function h(e, n, r) {
         e
     );
 }
-class p extends l.Z {
+class m extends u.Z {
     constructor(...e) {
         super(...e),
-            h(this, 'actions', { POST_CONNECTION_OPEN: () => this.handleConnectionOpen() }),
-            h(this, 'queue', new Set()),
-            h(this, 'isFetchEnabled', !1),
-            h(this, 'handleConnectionOpen', () => {
+            p(this, 'actions', { POST_CONNECTION_OPEN: () => this.handleConnectionOpen() }),
+            p(this, 'queue', new Set()),
+            p(this, 'isFetchEnabled', !1),
+            p(this, 'handleConnectionOpen', () => {
                 (this.isFetchEnabled = !0),
                     this.queue.forEach((e) => {
-                        e === f.Hk ? this.fetchFeaturedGuilds() : this.fetchCategoryFeaturedGuilds({ categoryId: e });
+                        e === _.Hk ? this.fetchFeaturedGuilds() : this.fetchCategoryFeaturedGuilds({ categoryId: e });
                     });
             }),
-            h(this, 'fetchFeaturedGuilds', async (e) => {
+            p(this, 'fetchFeaturedGuilds', async (e) => {
                 var n;
                 if (!this.isFetchEnabled) {
-                    this.queue.add(f.Hk);
+                    this.queue.add(_.Hk);
                     return;
                 }
                 let r = null !== (n = null == e ? void 0 : e.forceRefresh) && void 0 !== n && n,
-                    i = c.Z.getLastFetchTimestamp({ categoryId: f.Hk });
-                if (r || (0, d.Ew)(i)) {
-                    o.Z.dispatch({
+                    i = d.Z.getLastFetchTimestamp({ categoryId: _.Hk });
+                if (r || (0, f.Ew)(i)) {
+                    l.Z.dispatch({
                         type: 'GLOBAL_DISCOVERY_SERVERS_SEARCH_START',
-                        categoryId: f.Hk,
+                        categoryId: _.Hk,
                         reset: !0
                     });
                     try {
-                        let e = await s.tn.get({
-                                url: _.ANM.GUILD_DISCOVERY,
+                        let e = await o.tn.get({
+                                url: h.ANM.GUILD_DISCOVERY,
                                 query: a.stringify({
                                     offset: 0,
-                                    limit: 30
+                                    limit: s.g.FEATURED_DEFAULT_LIMIT
                                 }),
                                 oldFormErrors: !0,
                                 rejectWithError: !1
                             }),
                             n = e.body.total,
-                            r = e.body.guilds.map(d.Uv);
-                        o.Z.dispatch({
+                            r = e.body.guilds.map(f.Uv);
+                        l.Z.dispatch({
                             type: 'GLOBAL_DISCOVERY_SERVERS_SEARCH_SUCCESS',
-                            categoryId: f.Hk,
+                            categoryId: _.Hk,
                             guilds: r,
                             total: n
                         });
                     } catch (e) {
-                        o.Z.dispatch({
+                        l.Z.dispatch({
                             type: 'GLOBAL_DISCOVERY_SERVERS_SEARCH_FAILURE',
-                            categoryId: f.Hk,
+                            categoryId: _.Hk,
                             error: e
                         }),
-                            u.rC({ categoryId: f.Hk });
+                            c.rC({ categoryId: _.Hk });
                     }
                 }
             }),
-            h(this, 'fetchCategoryFeaturedGuilds', async (e) => {
+            p(this, 'fetchCategoryFeaturedGuilds', async (e) => {
                 let { categoryId: n, forceRefresh: r = !1 } = e;
                 if (!this.isFetchEnabled) {
                     this.queue.add(n);
                     return;
                 }
-                let i = c.Z.getLastFetchTimestamp({ categoryId: n });
-                if (r || (0, d.Ew)(i)) {
-                    o.Z.dispatch({
+                let i = d.Z.getLastFetchTimestamp({ categoryId: n });
+                if (r || (0, f.Ew)(i)) {
+                    l.Z.dispatch({
                         type: 'GLOBAL_DISCOVERY_SERVERS_SEARCH_START',
                         categoryId: n,
                         reset: !0
                     });
                     try {
-                        let e = await s.tn.get({
-                                url: _.ANM.GUILD_DISCOVERY,
+                        let e = await o.tn.get({
+                                url: h.ANM.GUILD_DISCOVERY,
                                 query: a.stringify({ categories: [n] }),
                                 oldFormErrors: !0,
                                 rejectWithError: !1
                             }),
                             r = e.body.total,
-                            i = e.body.guilds.map(d.Uv);
-                        o.Z.dispatch({
+                            i = e.body.guilds.map(f.Uv);
+                        l.Z.dispatch({
                             type: 'GLOBAL_DISCOVERY_SERVERS_SEARCH_SUCCESS',
                             categoryId: n,
                             guilds: i,
                             total: r
                         });
                     } catch (e) {
-                        o.Z.dispatch({
+                        l.Z.dispatch({
                             type: 'GLOBAL_DISCOVERY_SERVERS_SEARCH_FAILURE',
                             categoryId: n,
                             error: e
                         }),
-                            u.rC({ categoryId: n });
+                            c.rC({ categoryId: n });
                     }
                 }
             });
     }
 }
-n.Z = new p();
+n.Z = new m();
---
+++ chunks/975641.js
@@ -0,0 +1,9 @@
+var i;
+r.d(n, {
+    g: function () {
+        return i;
+    }
+}),
+    !(function (e) {
+        (e[(e.FEATURED_DEFAULT_LIMIT = 30)] = 'FEATURED_DEFAULT_LIMIT'), (e[(e.SEARCH_RESULTS_DEFAULT_LIMIT = 24)] = 'SEARCH_RESULTS_DEFAULT_LIMIT');
+    })(i || (i = {}));
