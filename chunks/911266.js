7e21f47905b6edfa6b6433d14939f42c0918afba
DiscrapperManager <admin@minerpl.xyz>
2025-01-10 21:27:29 +0000

    canaryRelease: 358197
    Main scripts: 3
    Lazy Loaded Scripts: 932
    Chunks:
     * Main Files: 11946
     * Scripts: 10378
    Strings: 0
    Routes: 688
    Experiments Count: 375

--- chunks/911266.js
+++ chunks/911266.js
@@ -3,30 +3,34 @@ var i = n(200651),
     r = n(120356),
     a = n.n(r),
     s = n(768581),
-    o = n(111810),
-    c = n(235894),
-    d = n(61750);
+    o = n(176354),
+    c = n(111810),
+    d = n(235894),
+    u = n(61750);
 t.Z = (e) => {
     let { message: t } = e,
         n = l.useRef(5 * Math.random() - 2.5).current;
-    if (!(0, o.p7)('Message Confetti Peak') || !(0, c.U)(t)) return null;
-    let r = (0, c.E)(t);
+    if (!(0, c.p7)('Message Confetti Peak') || !(0, d.U)(t)) return null;
+    let r = (0, d.E)(t);
     if (null == r) return null;
-    let u = r.emoji[0],
-        h = s.ZP.getEmojiURL({
-            id: u.id,
-            animated: !1,
-            size: 32,
-            forcePNG: !0
-        });
+    let h = r.emoji[0],
+        p =
+            null == h.id
+                ? o.ZP.getURL(h.name)
+                : s.ZP.getEmojiURL({
+                      id: h.id,
+                      animated: !1,
+                      size: 32,
+                      forcePNG: !0
+                  });
     return (0, i.jsxs)('div', {
-        className: d.emojiWrapper,
+        className: u.emojiWrapper,
         style: { right: ''.concat(13 + n, '%') },
         children: [
             (0, i.jsx)('img', {
                 alt: '',
-                src: h,
-                className: a()([d.emoji, d.left]),
+                src: p,
+                className: a()([u.emoji, u.left]),
                 height: 28,
                 width: 28,
                 style: {
@@ -36,8 +40,8 @@ t.Z = (e) => {
             }),
             (0, i.jsx)('img', {
                 alt: '',
-                src: h,
-                className: a()([d.emoji, d.right]),
+                src: p,
+                className: a()([u.emoji, u.right]),
                 height: 26,
                 width: 26,
                 style: {
@@ -47,8 +51,8 @@ t.Z = (e) => {
             }),
             (0, i.jsx)('img', {
                 alt: '',
-                src: h,
-                className: a()([d.emoji, d.center]),
+                src: p,
+                className: a()([u.emoji, u.center]),
                 height: 32,
                 width: 32,
                 style: {
