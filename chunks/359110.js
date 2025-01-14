2e2b2bfb03e527d20c06326066951a20d38b6bf9
DiscrapperManager <admin@minerpl.xyz>
2025-01-14 02:57:31 +0000

    canaryRelease: 358743
    Main scripts: 3
    Lazy Loaded Scripts: 932
    Chunks:
     * Main Files: 11946
     * Scripts: 10395
    Strings: 0
    Routes: 692
    Experiments Count: 375

--- chunks/359110.js
+++ chunks/359110.js
@@ -1,37 +1,39 @@
 r.d(n, {
     Kh: function () {
-        return d;
+        return f;
     },
     ad: function () {
-        return f;
+        return _;
     }
 });
 var i = r(512722),
     a = r.n(i),
     s = r(493683),
-    o = r(727429),
-    l = r(592125),
-    u = r(703656),
-    c = r(981631);
-function d(e, n) {
-    let r = l.Z.getChannel(e);
+    o = r(475179),
+    l = r(727429),
+    u = r(592125),
+    c = r(703656),
+    d = r(981631);
+function f(e, n) {
+    let r = u.Z.getChannel(e);
     if (null == r) return;
-    let i = (0, o.e)(r);
+    let i = (0, l.e)(r);
     s.Z.preload(r.guild_id, r.id),
-        (0, u.uL)(c.Z5c.CHANNEL(i, r.id), {
+        (0, c.uL)(d.Z5c.CHANNEL(i, r.id), {
             openChannel: !0,
             ...n
-        });
+        }),
+        (null == n ? void 0 : n.openTextInVoiceIfVoiceChannel) && r.isGuildVocal() && o.Z.updateChatOpen(r.id, !0);
 }
-function f(e, n) {
+function _(e, n) {
     a()(null != e.parent_id, 'Thread must have a parent ID.');
-    let r = (0, o.e)(e);
-    (0, u.uL)(c.Z5c.CHANNEL(r, e.id), {
+    let r = (0, l.e)(e);
+    (0, c.uL)(d.Z5c.CHANNEL(r, e.id), {
         openChannel: !0,
         ...n
     });
 }
-function _(e, n, r) {
+function h(e, n, r) {
     invariant(null != e.parent_id, 'Thread must have a parent ID.');
     let i = getGuildIdForGenericRedirect(e);
     transitionTo(Routes.CHANNEL(i, e.id, n), {
