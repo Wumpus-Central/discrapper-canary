2e3544eb31af2b6b1b9ae05b9f7e8a47802e8846
DiscrapperManager <admin@minerpl.xyz>
2025-01-16 19:02:57 +0000

    canaryRelease: 359851
    Main scripts: 3
    Lazy Loaded Scripts: 931
    Chunks:
     * Main Files: 11967
     * Scripts: 10426
    Strings: 0
    Routes: 693
    Experiments Count: 376

--- chunks/287941.js
+++ chunks/287941.js
@@ -1,37 +1,65 @@
+r.d(n, {
+    I: function () {
+        return c;
+    }
+});
 var i = r(570140),
     a = r(607070),
     s = r(823961),
     o = r(111810),
     l = r(477931);
 let u = function (e) {
-    var n;
-    let u =
-            arguments.length > 1 && void 0 !== arguments[1]
-                ? arguments[1]
-                : {
-                      x: 0,
-                      y: 0,
-                      w: 0,
-                      h: 0
-                  },
-        c = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
-        d = o.HZ.getCurrentConfig({ location: 'TriggerConfetti' }, { autoTrackExposure: !1 }).canSeeConfetti,
-        f = !0;
-    if (!!(f = r(451478).Z.isFocused()))
-        (s.Z.confettiMode || c) &&
-            d &&
-            !a.Z.useReducedMotion &&
-            (null === (n = e.potions) ||
-                void 0 === n ||
-                n.forEach((e) => {
-                    e.type === l.B.CONFETTI &&
-                        e.emoji.forEach((e) => {
-                            i.Z.dispatch({
-                                type: 'POTIONS_TRIGGER_MESSAGE_CONFETTI',
-                                emoji: e,
-                                boundingRect: u
+        var n;
+        let u =
+                arguments.length > 1 && void 0 !== arguments[1]
+                    ? arguments[1]
+                    : {
+                          x: 0,
+                          y: 0,
+                          w: 0,
+                          h: 0
+                      },
+            c = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
+            d = o.HZ.getCurrentConfig({ location: 'TriggerConfetti' }, { autoTrackExposure: !1 }).canSeeConfetti,
+            f = !0;
+        if (!!(f = r(451478).Z.isFocused()))
+            (s.Z.confettiMode || c) &&
+                d &&
+                !a.Z.useReducedMotion &&
+                (null === (n = e.potions) ||
+                    void 0 === n ||
+                    n.forEach((e) => {
+                        e.type === l.B.CONFETTI &&
+                            e.emoji.forEach((e) => {
+                                i.Z.dispatch({
+                                    type: 'POTIONS_TRIGGER_MESSAGE_CONFETTI',
+                                    emoji: e,
+                                    boundingRect: u
+                                });
                             });
-                        });
-                }));
-};
+                    }));
+    },
+    c = function (e) {
+        let n =
+                arguments.length > 1 && void 0 !== arguments[1]
+                    ? arguments[1]
+                    : {
+                          x: 0,
+                          y: 0,
+                          w: 0,
+                          h: 0
+                      },
+            l = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
+            u = o.HZ.getCurrentConfig({ location: 'TriggerConfetti' }, { autoTrackExposure: !1 }).canSeeConfetti,
+            c = !0;
+        if (!!(c = r(451478).Z.isFocused()))
+            (s.Z.confettiMode || l) &&
+                u &&
+                !a.Z.useReducedMotion &&
+                i.Z.dispatch({
+                    type: 'POTIONS_TRIGGER_MESSAGE_CONFETTI',
+                    emoji: e,
+                    boundingRect: n
+                });
+    };
 n.Z = u;
--- chunks/980463.js
+++ chunks/980463.js
@@ -1,21 +1,21 @@
 n.d(t, {
     Gq: function () {
-        return s;
-    },
-    SN: function () {
-        return _;
-    },
-    Zu: function () {
-        return E;
-    },
-    gA: function () {
         return d;
     },
-    qc: function () {
+    SN: function () {
+        return m;
+    },
+    Zu: function () {
         return f;
     },
+    gA: function () {
+        return E;
+    },
+    qc: function () {
+        return p;
+    },
     x8: function () {
-        return m;
+        return S;
     }
 }),
     n(411104);
@@ -24,19 +24,20 @@ var r = n(544891),
     u = n(570140),
     l = n(959546),
     o = n(317951),
-    a = n(981631);
-let c = {
-        [o.D1]: a.ANM.FETCH_MESSAGE_CONFETTI_ENTITLEMENT,
-        [o.FX]: a.ANM.FETCH_HD_STREAMING_ENTITLEMENT
+    a = n(287941),
+    c = n(981631);
+let s = {
+        [o.D1]: c.ANM.FETCH_MESSAGE_CONFETTI_ENTITLEMENT,
+        [o.FX]: c.ANM.FETCH_HD_STREAMING_ENTITLEMENT
     },
-    s = async (e) => {
+    d = async (e) => {
         u.Z.dispatch({
             type: 'CONSUMABLES_PRICE_FETCH_STARTED',
             skuId: e
         });
         try {
             let t = await r.tn.get({
-                url: a.ANM.CONSUMABLE_FETCH_PRICE(e),
+                url: c.ANM.CONSUMABLE_FETCH_PRICE(e),
                 rejectWithError: !1
             });
             if (null == t.body.price)
@@ -70,13 +71,13 @@ let c = {
             );
         }
     },
-    d = async (e) => {
+    E = async (e) => {
         if (
             (u.Z.dispatch({
                 type: 'CONSUMABLES_ENTITLEMENT_FETCH_STARTED',
                 skuId: e
             }),
-            null == c[e])
+            null == s[e])
         )
             throw (
                 (u.Z.dispatch({
@@ -87,7 +88,7 @@ let c = {
             );
         try {
             let t = await r.tn.get({
-                    url: c[e],
+                    url: s[e],
                     rejectWithError: !1
                 }),
                 n = null != t.body.entitlement ? l.Z.createFromServer(t.body.entitlement) : null;
@@ -109,10 +110,10 @@ let c = {
             );
         }
     },
-    E = async (e, t) => {
+    f = async (e, t) => {
         try {
             await r.tn.post({
-                url: a.ANM.CONSUME_HD_STREAMING_POTION,
+                url: c.ANM.CONSUME_HD_STREAMING_POTION,
                 body: { channel_id: e },
                 rejectWithError: !1
             }),
@@ -124,14 +125,14 @@ let c = {
             throw new i.Hx(e);
         }
     },
-    f = async (e, t, n, l) => {
+    p = async (e, t, n, l) => {
         try {
             await r.tn.post({
-                url: a.ANM.CONSUME_MESSAGE_CONFETTI_POTION,
+                url: c.ANM.CONSUME_MESSAGE_CONFETTI_POTION,
                 body: {
                     channel_id: e,
                     message_id: t,
-                    emoji_name: p(n)
+                    emoji_name: _(n)
                 },
                 rejectWithError: !1
             }),
@@ -142,15 +143,23 @@ let c = {
         } catch (e) {
             throw new i.Hx(e);
         }
+        (0, a.I)(
+            {
+                name: n.name,
+                id: n.id
+            },
+            void 0,
+            !0
+        );
     },
-    p = (e) => (null != e.id ? ''.concat(e.name, ':').concat(e.id) : e.optionallyDiverseSequence),
-    _ = (e) => {
+    _ = (e) => (null != e.id ? ''.concat(e.name, ':').concat(e.id) : e.optionallyDiverseSequence),
+    m = (e) => {
         u.Z.dispatch({
             type: 'CONSUMABLES_CLEAR_ERROR',
             skuId: e
         });
     },
-    m = (e) => {
+    S = (e) => {
         u.Z.dispatch({
             type: 'SET_PREVIOUS_GO_LIVE_SETTINGS',
             previousGoLiveSettings: e
