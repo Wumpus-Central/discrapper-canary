5e9a62cd440e3b39c7fdde08041b4aa1076c039f
DiscrapperManager <admin@minerpl.xyz>
2025-01-17 01:20:29 +0000

    canaryRelease: 360134
    Main scripts: 3
    Lazy Loaded Scripts: 931
    Chunks:
     * Main Files: 11973
     * Scripts: 10430
    Strings: 0
    Routes: 693
    Experiments Count: 375

--- chunks/455199.js
+++ chunks/455199.js
@@ -47,15 +47,14 @@ let R = 'recentMentionFilterSettings',
     }),
     M = !1,
     k = 0,
-    U = !1,
-    B = !1;
-function G(e) {
+    U = !1;
+function B(e) {
     (D = {}),
         e.forEach((e) => {
             null == D[e.getChannelId()] && (D[e.getChannelId()] = 0), D[e.getChannelId()]++;
         });
 }
-function F(e) {
+function G(e) {
     let { addedMessages: n, deletedMessages: r } = e;
     null != n &&
         n.forEach((e) => {
@@ -66,25 +65,19 @@ function F(e) {
                 null != D[e.getChannelId()] && (D[e.getChannelId()] = Math.max(0, D[e.getChannelId()] - 1));
             });
 }
-function Z() {
-    U = !0;
-}
-function V() {
-    U = !1;
-}
-function j(e) {
+function F(e) {
     let { guildId: n } = e;
-    (x = !0), null == n && P.guildFilter === N.NgX.THIS_SERVER && $({ guildFilter: N.NgX.ALL_SERVERS });
+    (x = !0), null == n && P.guildFilter === N.NgX.THIS_SERVER && Q({ guildFilter: N.NgX.ALL_SERVERS });
 }
-function H(e) {
+function Z(e) {
     if (e instanceof g.ZP) return e;
     let n = I.Z.getMessage(e.channel_id, e.id);
     return null != n ? n : (0, _.e5)(e);
 }
-function Y(e) {
+function V(e) {
     let { hasMoreAfter: n, messages: r, isAfter: i } = e,
-        a = l().map(r, H);
-    F({ addedMessages: a }),
+        a = l().map(r, Z);
+    G({ addedMessages: a }),
         i ? (O = O.concat(a)) : ((O = a), (L = {})),
         l().forEach(a, (e) => {
             L[e.id] = !0;
@@ -94,10 +87,10 @@ function Y(e) {
         (k = (0, d.zO)()),
         (M = !0);
 }
-function W() {
+function j() {
     x = !1;
 }
-function K(e) {
+function H(e) {
     let n = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null;
     if ((0, p.Z)(e) && !N.V$x.SELF_MENTIONABLE_SYSTEM.has(e.type)) return null;
     null == n && (n = e.channel_id);
@@ -105,7 +98,7 @@ function K(e) {
     if (null == r || r.type === N.d4z.DM || (P.guildFilter === N.NgX.THIS_SERVER && r.getGuildId() !== y.Z.getGuildId())) return null;
     let i = E.default.getId();
     if (b.Z.isBlockedOrIgnoredForMessage(e) || (0, m.Z)(e, i)) return null;
-    e = H(e);
+    e = Z(e);
     let a = !P.everyoneFilter,
         s = !P.roleFilter;
     return (0, h.ZP)({
@@ -114,7 +107,7 @@ function K(e) {
         suppressEveryone: a,
         suppressRoles: s
     })
-        ? (B &&
+        ? (U &&
               T.ZP.ackMessageId(r.id) !== e.id &&
               (0, h.ZP)({
                   message: e,
@@ -122,11 +115,11 @@ function K(e) {
                   suppressEveryone: S.ZP.isSuppressEveryoneEnabled(r.getGuildId()),
                   suppressRoles: S.ZP.isSuppressRolesEnabled(r.getGuildId())
               }) &&
-              (B = !1),
+              (U = !1),
           e)
         : null;
 }
-function z(e) {
+function Y(e) {
     let { channelId: n, message: r } = e,
         i = A.default.getCurrentUser();
     if (
@@ -139,11 +132,11 @@ function z(e) {
         })
     )
         return !1;
-    let a = K(r, n);
+    let a = H(r, n);
     if (null == a) return !1;
-    (O = O.slice()).unshift(a), (L[a.id] = !0), F({ addedMessages: [a] });
+    (O = O.slice()).unshift(a), (L[a.id] = !0), G({ addedMessages: [a] });
 }
-function q(e) {
+function W(e) {
     let n = e.message.id;
     if (null == L[String(n)]) return !1;
     let r = l().findIndex(O, (e) => {
@@ -153,10 +146,10 @@ function q(e) {
         i = (O = O.slice())[r];
     null != i && (O[r] = (0, _.wi)(i, e.message));
 }
-function Q(e) {
+function K(e) {
     if (null == L[e]) return !1;
     delete L[e],
-        F({
+        G({
             deletedMessages: l().filter(O, (n) => {
                 let { id: r } = n;
                 return r === e;
@@ -167,15 +160,15 @@ function Q(e) {
             return r !== e;
         }));
 }
-function X(e) {
+function z(e) {
     let { id: n } = e;
-    return Q(n);
+    return K(n);
 }
-function J(e) {
+function q(e) {
     let { ids: n } = e;
-    l().forEach(n, Q);
+    l().forEach(n, K);
 }
-function $(e) {
+function Q(e) {
     let n = { ...P };
     (P = l().defaults(l().pick(e, ['guildFilter', 'roleFilter', 'everyoneFilter']), P)), c.K.set(R, P);
     let r = (e, r) => n[e] !== P[e] && P[e] === r,
@@ -184,55 +177,52 @@ function $(e) {
     let a = [];
     i &&
         O.forEach((e) => {
-            let n = K(e);
+            let n = H(e);
             null != n && (a.push(n), (L[n.id] = !0));
         }),
-        G((O = a)),
+        B((O = a)),
         0 === O.length && (M = !1);
 }
-function ee() {
+function X() {
     if (P.guildFilter !== N.NgX.THIS_SERVER) return !1;
     M = !1;
 }
-function et() {
-    (O = []), (L = {}), (M = !1), (B = !1), (D = {});
+function J() {
+    (O = []), (L = {}), (M = !1), (U = !1), (D = {});
 }
-function en(e) {
+function $(e) {
     let { guild: n } = e,
         r = [];
     (O = l().filter(O, (e) => {
         let i = v.Z.getChannel(e.channel_id);
         return (null != i && i.getGuildId() !== n.id) || (delete L[e.id], r.push(e), !1);
     })),
-        F({ deletedMessages: r });
+        G({ deletedMessages: r });
 }
-function er() {
-    F({ deletedMessages: l().filter(O, (e) => b.Z.isBlockedOrIgnoredForMessage(e)) }), (O = O.filter((e) => !b.Z.isBlockedOrIgnoredForMessage(e)));
+function ee() {
+    G({ deletedMessages: l().filter(O, (e) => b.Z.isBlockedOrIgnoredForMessage(e)) }), (O = O.filter((e) => !b.Z.isBlockedOrIgnoredForMessage(e)));
 }
-function ei(e) {
+function et(e) {
     let { channel: n } = e,
         r = [];
-    (O = l().filter(O, (e) => e.channel_id !== n.id || (delete L[e.id], r.push(e), !1))), F({ deletedMessages: r });
+    (O = l().filter(O, (e) => e.channel_id !== n.id || (delete L[e.id], r.push(e), !1))), G({ deletedMessages: r });
 }
-function ea(e) {
-    et();
+function en(e) {
+    J();
 }
-function es(e) {
+function er(e) {
     let { size: n } = e;
-    F({ deletedMessages: O.slice(n) });
+    G({ deletedMessages: O.slice(n) });
     for (let e = n; e < O.length; ++e) delete L[O[e].id];
     let r = O.length;
     r > (O = O.slice(0, n)).length && (w = !0);
 }
-function eo(e) {
-    B = !0;
+function ei(e) {
+    U = !0;
 }
-class el extends (i = u.ZP.Store) {
+class ea extends (i = u.ZP.Store) {
     initialize() {
         this.waitFor(A.default, v.Z, I.Z, T.ZP);
-    }
-    isOpen() {
-        return U;
     }
     get hasLoadedEver() {
         return M;
@@ -262,7 +252,7 @@ class el extends (i = u.ZP.Store) {
         return P.roleFilter;
     }
     get mentionsAreStale() {
-        return B;
+        return U;
     }
     get mentionCountByChannel() {
         return D;
@@ -272,28 +262,26 @@ class el extends (i = u.ZP.Store) {
         return null !== (n = D[e]) && void 0 !== n ? n : 0;
     }
 }
-C(el, 'displayName', 'RecentMentionsStore'),
-    (n.Z = new el(f.Z, {
-        LOAD_RECENT_MENTIONS: j,
-        LOAD_RECENT_MENTIONS_SUCCESS: Y,
-        LOAD_RECENT_MENTIONS_FAILURE: W,
-        SET_RECENT_MENTIONS_FILTER: $,
-        CLEAR_MENTIONS: ea,
-        TRUNCATE_MENTIONS: es,
-        CHANNEL_SELECT: ee,
-        CONNECTION_OPEN: et,
-        GUILD_DELETE: en,
-        MESSAGE_CREATE: z,
-        MESSAGE_UPDATE: q,
-        MESSAGE_DELETE: X,
-        RECENT_MENTION_DELETE: X,
-        MESSAGE_DELETE_BULK: J,
-        CHANNEL_DELETE: ei,
-        THREAD_DELETE: ei,
-        RELATIONSHIP_ADD: er,
-        RELATIONSHIP_REMOVE: er,
-        RELATIONSHIP_UPDATE: er,
-        MENTION_MODAL_OPEN: Z,
-        MENTION_MODAL_CLOSE: V,
-        SET_RECENT_MENTIONS_STALE: eo
+C(ea, 'displayName', 'RecentMentionsStore'),
+    (n.Z = new ea(f.Z, {
+        LOAD_RECENT_MENTIONS: F,
+        LOAD_RECENT_MENTIONS_SUCCESS: V,
+        LOAD_RECENT_MENTIONS_FAILURE: j,
+        SET_RECENT_MENTIONS_FILTER: Q,
+        CLEAR_MENTIONS: en,
+        TRUNCATE_MENTIONS: er,
+        CHANNEL_SELECT: X,
+        CONNECTION_OPEN: J,
+        GUILD_DELETE: $,
+        MESSAGE_CREATE: Y,
+        MESSAGE_UPDATE: W,
+        MESSAGE_DELETE: z,
+        RECENT_MENTION_DELETE: z,
+        MESSAGE_DELETE_BULK: q,
+        CHANNEL_DELETE: et,
+        THREAD_DELETE: et,
+        RELATIONSHIP_ADD: ee,
+        RELATIONSHIP_REMOVE: ee,
+        RELATIONSHIP_UPDATE: ee,
+        SET_RECENT_MENTIONS_STALE: ei
     }));
