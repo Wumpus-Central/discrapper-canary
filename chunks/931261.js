888e83b29678e59295caed9fa0fd2a51dbe4381c
DiscrapperManager <admin@minerpl.xyz>
2025-01-17 13:43:57 +0000

    canaryRelease: 360221
    Main scripts: 3
    Lazy Loaded Scripts: 931
    Chunks:
     * Main Files: 12019
     * Scripts: 10430
    Strings: 0
    Routes: 693
    Experiments Count: 375

--- chunks/931261.js
+++ chunks/931261.js
@@ -1,53 +1,50 @@
 r.d(n, {
     g: function () {
-        return E;
+        return m;
     },
     s: function () {
-        return v;
+        return g;
     }
 });
-var i = r(47120);
-var a = r(392711),
-    s = r.n(a),
-    o = r(399606),
-    l = r(637853),
-    u = r(160404),
-    c = r(592125),
-    d = r(430824),
-    f = r(563534),
-    _ = r(199689),
-    h = r(460347),
-    p = r(981631),
-    m = r(176505);
-function g(e) {
+var i = r(399606),
+    a = r(637853),
+    s = r(160404),
+    o = r(592125),
+    l = r(430824),
+    u = r(563534),
+    c = r(199689),
+    d = r(460347),
+    f = r(981631),
+    _ = r(176505);
+function h(e) {
     var n;
     let r = e.id,
-        i = (null !== (n = f.Z.getNewMemberActions(r)) && void 0 !== n ? n : []).length > 0,
-        a = f.Z.getEnabled(r);
-    return i && e.hasFeature(p.oNc.COMMUNITY) && !(e.hasFeature(p.oNc.GUILD_ONBOARDING) && !a);
+        i = (null !== (n = u.Z.getNewMemberActions(r)) && void 0 !== n ? n : []).length > 0,
+        a = u.Z.getEnabled(r);
+    return i && e.hasFeature(f.oNc.COMMUNITY) && !(e.hasFeature(f.oNc.GUILD_ONBOARDING) && !a);
 }
-function E(e) {
-    let n = (0, h.Z)(e),
-        r = c.Z.getMutableGuildChannelsForGuild(e);
-    return (0, o.e7)(
-        [d.Z, u.Z],
+function p(e, n) {
+    let r = n.getMutableGuildChannelsForGuild(e);
+    for (let e in r) if (r[e].hasFlag(_.zZ.IS_GUILD_RESOURCE_CHANNEL)) return !0;
+    return !1;
+}
+function m(e) {
+    let n = (0, d.Z)(e);
+    return (0, i.e7)(
+        [o.Z, l.Z, s.Z],
         () => {
-            let i = d.Z.getGuild(e);
-            if (__OVERLAY__ || e === p.ME || e === p.I_8 || null == i) return !1;
-            if (u.Z.isFullServerPreview(e)) return g(i);
-            let a = (0, _.Z)(i),
-                o = (0, l.wC)(e) && i.hasFeature(p.oNc.GUILD_ONBOARDING) && i.hasFeature(p.oNc.GUILD_SERVER_GUIDE),
-                c = s().some(s().values(r), (e) => e.hasFlag(m.zZ.IS_GUILD_RESOURCE_CHANNEL));
-            return !(!n && !c) && ((a && i.hasFeature(p.oNc.GUILD_ONBOARDING) && i.hasFeature(p.oNc.GUILD_SERVER_GUIDE)) || o) && i.hasFeature(p.oNc.COMMUNITY);
+            if (__OVERLAY__ || e === f.ME || e === f.I_8) return !1;
+            let r = l.Z.getGuild(e);
+            return (null == r ? !!void 0 : !!r.hasFeature(f.oNc.COMMUNITY)) && (s.Z.isFullServerPreview(e) ? h(r) : (0, a.wC)(e) ? n || p(e, o.Z) : !!(0, c.Z)(r) && (n || p(e, o.Z)));
         },
-        [e, n, r]
+        [e, n]
     );
 }
-function v(e) {
-    let n = d.Z.getGuild(e),
-        r = u.Z.isFullServerPreview(e);
-    if (null == n || __OVERLAY__ || e === p.ME || e === p.I_8) return !1;
-    if (r) return g(n);
-    let i = (0, l.wC)(e) && n.hasFeature(p.oNc.GUILD_ONBOARDING) && n.hasFeature(p.oNc.GUILD_SERVER_GUIDE);
-    return ((n.hasFeature(p.oNc.GUILD_ONBOARDING) && n.hasFeature(p.oNc.GUILD_SERVER_GUIDE)) || i) && n.hasFeature(p.oNc.COMMUNITY);
+function g(e) {
+    let n = l.Z.getGuild(e),
+        r = s.Z.isFullServerPreview(e);
+    if (null == n || __OVERLAY__ || e === f.ME || e === f.I_8) return !1;
+    if (r) return h(n);
+    let i = (0, a.wC)(e) && n.hasFeature(f.oNc.GUILD_ONBOARDING) && n.hasFeature(f.oNc.GUILD_SERVER_GUIDE);
+    return ((n.hasFeature(f.oNc.GUILD_ONBOARDING) && n.hasFeature(f.oNc.GUILD_SERVER_GUIDE)) || i) && n.hasFeature(f.oNc.COMMUNITY);
 }
