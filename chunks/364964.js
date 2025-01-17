0c315f2a4bc155503d757daa2a8be3a76098f049
DiscrapperManager <admin@minerpl.xyz>
2025-01-17 22:57:03 +0000

    canaryRelease: 360524
    Main scripts: 3
    Lazy Loaded Scripts: 930
    Chunks:
     * Main Files: 12041
     * Scripts: 10411
    Strings: 0
    Routes: 693
    Experiments Count: 378

--- chunks/364964.js
+++ chunks/364964.js
@@ -1,28 +1,39 @@
 r.r(n);
 var i = r(47120);
-var a = r(147519),
-    s = r(392711),
-    o = r.n(s),
-    l = r(462176);
-a.Z.registerLanguage('ansi', l.Z),
-    (n.default = {
-        highlight(e, n, r) {
-            if (!c(n)) return null;
-            let i = Date.now(),
-                s = a.Z.highlight(e, n, r);
-            return Date.now(), s;
-        },
-        hasLanguage: (e) => null != a.Z.getLanguage(e)
-    });
-let u = o().repeat('/', 15);
-function c(e) {
-    if (e.indexOf(u) >= 0) return !1;
-    let n = 0;
-    for (let r of e.split('\n')) {
-        if (r.length > 1000) return !1;
-        null == r.match(/^\s*[/][/]/) && null != r.match(/[^/]/) && (n = 0);
-        let e = r.match(/[/]/g);
-        if (null != e && ((n += e.length), n > 30)) return !1;
+var a = r(757143);
+var s = r(147519),
+    o = r(392711),
+    l = r.n(o),
+    u = r(462176);
+s.Z.registerLanguage('ansi', u.Z);
+let c = /<[^>]*>[^>]*(html\\`)/g,
+    d = new Set(['html', 'xml']);
+n.default = {
+    highlight(e, n, r) {
+        if (!_(n, e)) return null;
+        let i = Date.now(),
+            a = s.Z.highlight(n, {
+                ignoreIllegals: r,
+                language: e
+            });
+        return Date.now(), a;
+    },
+    hasLanguage: (e) => null != s.Z.getLanguage(e)
+};
+let f = l().repeat('/', 15);
+function _(e, n) {
+    if (e.indexOf(f) >= 0) return !1;
+    let r = 0;
+    for (let n of e.split('\n')) {
+        if (n.length > 1000) return !1;
+        null == n.match(/^\s*[/][/]/) && null != n.match(/[^/]/) && (r = 0);
+        let e = n.match(/[/]/g);
+        if (null != e && ((r += e.length), r > 30)) return !1;
+    }
+    if (d.has(n)) {
+        var i;
+        let n = e.replace(/\s*/g, '').match(c);
+        if ((null !== (i = null == n ? void 0 : n.length) && void 0 !== i ? i : 0) > 10) return !1;
     }
     return !0;
 }
