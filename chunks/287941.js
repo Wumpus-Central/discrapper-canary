4e1f2d77ef22a2d830c9fa44316e743b115233c6
DiscrapperManager <admin@minerpl.xyz>
2025-01-22 15:28:33 +0000

    canaryRelease: 361203
    Main scripts: 3
    Lazy Loaded Scripts: 917
    Chunks:
     * Main Files: 13040
     * Scripts: 9448
    Strings: 0
    Routes: 694
    Experiments Count: 380

--- chunks/287941.js
+++ chunks/287941.js
@@ -1,6 +1,6 @@
 r.d(n, {
     I: function () {
-        return c;
+        return d;
     }
 });
 var i = r(570140),
@@ -8,9 +8,13 @@ var i = r(570140),
     o = r(823961),
     s = r(111810),
     l = r(477931);
-let u = function (e) {
+let u = () => {
+        let e = !0;
+        return (e = r(451478).Z.isFocused());
+    },
+    c = function (e) {
         var n;
-        let u =
+        let r =
                 arguments.length > 1 && void 0 !== arguments[1]
                     ? arguments[1]
                     : {
@@ -19,27 +23,26 @@ let u = function (e) {
                           w: 0,
                           h: 0
                       },
-            c = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
-            d = s.HZ.getCurrentConfig({ location: 'TriggerConfetti' }, { autoTrackExposure: !1 }).canSeeConfetti,
-            f = !0;
-        if (!!(f = r(451478).Z.isFocused()))
-            (o.Z.confettiMode || c) &&
-                d &&
-                !a.Z.useReducedMotion &&
-                (null === (n = e.potions) ||
-                    void 0 === n ||
-                    n.forEach((e) => {
-                        e.type === l.B.CONFETTI &&
-                            e.emoji.forEach((e) => {
-                                i.Z.dispatch({
-                                    type: 'POTIONS_TRIGGER_MESSAGE_CONFETTI',
-                                    emoji: e,
-                                    boundingRect: u
-                                });
+            c = arguments.length > 2 && void 0 !== arguments[2] && arguments[2];
+        if (!u()) return;
+        let d = s.HZ.getCurrentConfig({ location: 'TriggerConfetti' }, { autoTrackExposure: !1 }).canSeeConfetti;
+        (o.Z.confettiMode || c) &&
+            d &&
+            !a.Z.useReducedMotion &&
+            (null === (n = e.potions) ||
+                void 0 === n ||
+                n.forEach((e) => {
+                    e.type === l.B.CONFETTI &&
+                        e.emoji.forEach((e) => {
+                            i.Z.dispatch({
+                                type: 'POTIONS_TRIGGER_MESSAGE_CONFETTI',
+                                emoji: e,
+                                boundingRect: r
                             });
-                    }));
+                        });
+                }));
     },
-    c = function (e) {
+    d = function (e) {
         let n =
                 arguments.length > 1 && void 0 !== arguments[1]
                     ? arguments[1]
@@ -49,17 +52,16 @@ let u = function (e) {
                           w: 0,
                           h: 0
                       },
-            l = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
-            u = s.HZ.getCurrentConfig({ location: 'TriggerConfetti' }, { autoTrackExposure: !1 }).canSeeConfetti,
-            c = !0;
-        if (!!(c = r(451478).Z.isFocused()))
-            (o.Z.confettiMode || l) &&
-                u &&
-                !a.Z.useReducedMotion &&
-                i.Z.dispatch({
-                    type: 'POTIONS_TRIGGER_MESSAGE_CONFETTI',
-                    emoji: e,
-                    boundingRect: n
-                });
+            r = arguments.length > 2 && void 0 !== arguments[2] && arguments[2];
+        if (!u()) return;
+        let l = s.HZ.getCurrentConfig({ location: 'TriggerConfetti' }, { autoTrackExposure: !1 }).canSeeConfetti;
+        (o.Z.confettiMode || r) &&
+            l &&
+            !a.Z.useReducedMotion &&
+            i.Z.dispatch({
+                type: 'POTIONS_TRIGGER_MESSAGE_CONFETTI',
+                emoji: e,
+                boundingRect: n
+            });
     };
-n.Z = u;
+n.Z = c;
