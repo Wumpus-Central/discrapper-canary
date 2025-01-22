19440d626f0b5ab0c26e453896ae0061ed58cac4
DiscrapperManager <admin@minerpl.xyz>
2025-01-22 23:13:13 +0000

    canaryRelease: 361562
    Main scripts: 3
    Lazy Loaded Scripts: 905
    Chunks:
     * Main Files: 13047
     * Scripts: 9353
    Strings: 0
    Routes: 694
    Experiments Count: 380

--- chunks/362352.js
+++ chunks/362352.js
@@ -1,32 +1,41 @@
 r.d(n, {
-    l: function () {
-        return a;
+    Yd: function () {
+        return l;
+    },
+    o0: function () {
+        return s;
     }
 });
 var i = r(818083);
 let a = (0, i.B)({
-    kind: 'user',
-    id: '2025-01_default_activity_status',
-    label: 'Guild activity status defaults',
-    defaultConfig: { enabled: !1 },
-    treatments: [
-        {
-            id: 1,
-            label: 'Enable new defaults',
-            config: { enabled: !0 }
-        }
-    ]
-});
-(0, i.B)({
-    kind: 'user',
-    id: '2025-01_202501defaultactivitystatusnewusers',
-    label: 'Guild activity status defaults',
-    defaultConfig: { enabled: !1 },
-    treatments: [
-        {
-            id: 1,
-            label: 'Enable new defaults',
-            config: { enabled: !0 }
-        }
-    ]
-});
+        kind: 'user',
+        id: '2025-01_default_activity_status',
+        label: 'Guild activity status defaults',
+        defaultConfig: { enabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'Enable new defaults',
+                config: { enabled: !0 }
+            }
+        ]
+    }),
+    o = (0, i.B)({
+        kind: 'user',
+        id: '2025-01_default_activity_status_new_users',
+        label: 'Guild activity status defaults',
+        defaultConfig: { enabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'Enable new defaults',
+                config: { enabled: !0 }
+            }
+        ]
+    }),
+    s = (e) => a.getCurrentConfig({ location: e }, { autoTrackExposure: !1 }).enabled || o.getCurrentConfig({ location: e }, { autoTrackExposure: !1 }).enabled,
+    l = (e) => {
+        let n = a.useExperiment({ location: e }, { autoTrackExposure: !1 }).enabled,
+            r = o.useExperiment({ location: e }, { autoTrackExposure: !1 }).enabled;
+        return n || r;
+    };
