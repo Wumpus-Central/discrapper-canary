e54d74189b460c1db7effad894ff2fab5c47d906
DiscrapperManager <admin@minerpl.xyz>
2025-01-22 18:38:43 +0000

    canaryRelease: 361326
    Main scripts: 3
    Lazy Loaded Scripts: 917
    Chunks:
     * Main Files: 13043
     * Scripts: 9446
    Strings: 0
    Routes: 694
    Experiments Count: 381

--- chunks/497060.js
+++ chunks/497060.js
@@ -1,32 +1,69 @@
 r.d(n, {
-    r: function () {
-        return i;
+    k6: function () {
+        return s;
+    },
+    kH: function () {
+        return l;
     }
 });
-let i = (0, r(818083).B)({
-    kind: 'user',
-    id: '2025-01_friend_requests_alerts',
-    label: 'Friend Request Setting Defaults',
-    defaultConfig: {
-        enabled: !1,
-        bucket: 0
-    },
-    treatments: [
-        {
-            id: 1,
-            label: 'warning',
-            config: {
-                enabled: !0,
-                bucket: 1
-            }
+var i = r(818083);
+let a = (0, i.B)({
+        kind: 'user',
+        id: '2025-01_friend_requests_alerts',
+        label: 'Friend Request Setting Defaults',
+        defaultConfig: {
+            enabled: !1,
+            bucket: 0
         },
-        {
-            id: 2,
-            label: 'defaults',
-            config: {
-                enabled: !0,
-                bucket: 2
+        treatments: [
+            {
+                id: 1,
+                label: 'warning',
+                config: {
+                    enabled: !0,
+                    bucket: 1
+                }
+            },
+            {
+                id: 2,
+                label: 'defaults',
+                config: {
+                    enabled: !0,
+                    bucket: 2
+                }
             }
-        }
-    ]
-});
+        ]
+    }),
+    o = (0, i.B)({
+        kind: 'user',
+        id: '2025-01_friend_requests_alerts_new_users',
+        label: 'Friend Request Setting Defaults',
+        defaultConfig: {
+            enabled: !1,
+            bucket: 0
+        },
+        treatments: [
+            {
+                id: 1,
+                label: 'warning',
+                config: {
+                    enabled: !0,
+                    bucket: 1
+                }
+            },
+            {
+                id: 2,
+                label: 'defaults',
+                config: {
+                    enabled: !0,
+                    bucket: 2
+                }
+            }
+        ]
+    }),
+    s = (e) => 2 === a.getCurrentConfig({ location: e }, { autoTrackExposure: !1 }).bucket || 2 === o.getCurrentConfig({ location: e }, { autoTrackExposure: !1 }).bucket,
+    l = (e) => {
+        let n = a.useExperiment({ location: e }, { autoTrackExposure: !1 }).bucket,
+            r = o.useExperiment({ location: e }, { autoTrackExposure: !1 }).bucket;
+        return 2 === n || 2 === r;
+    };
