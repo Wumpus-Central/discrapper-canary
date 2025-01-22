ea2413c6528c2a11342a0c0a6844f1c1aac4c60e
DiscrapperManager <admin@minerpl.xyz>
2025-01-22 17:49:22 +0000

    canaryRelease: 361278
    Main scripts: 3
    Lazy Loaded Scripts: 917
    Chunks:
     * Main Files: 13044
     * Scripts: 9445
    Strings: 0
    Routes: 694
    Experiments Count: 380

--- chunks/680415.js
+++ chunks/680415.js
@@ -2,12 +2,20 @@ e(312677);
 var r = e(511004);
 e(149182);
 var u = e(486324);
-async function o(n, t, e, o, i) {
+async function o(n) {
+    let { gif: t, x: e, y: o, width: i, height: c, rotationDegrees: f = 0 } = n;
     try {
-        let c = await (0, r.t)(n, t, e, o, i);
+        let n = await (0, r.Q)({
+            gif: t,
+            x: e,
+            y: o,
+            width: i,
+            height: c,
+            rotationDegrees: f
+        });
         self.postMessage({
             type: u.u.CROP_GIF_COMPLETE,
-            result: c
+            result: n
         });
     } catch (n) {
         self.postMessage({ type: u.u.CROP_GIF_ERROR });
@@ -16,7 +24,14 @@ async function o(n, t, e, o, i) {
 self.addEventListener('message', (n) => {
     let { data: t } = n;
     if (t.type === u.u.CROP_GIF_START) {
-        let { gif: n, x: e, y: r, width: u, height: i } = t;
-        o(n, e, r, u, i);
+        let { gif: n, x: e, y: r, width: u, height: i, rotationDegrees: c } = t;
+        o({
+            gif: n,
+            x: e,
+            y: r,
+            width: u,
+            height: i,
+            rotationDegrees: c
+        });
     }
 });
