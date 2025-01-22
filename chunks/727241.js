a9584493e6fd9a002669f1a5000bdfb49175580a
DiscrapperManager <admin@minerpl.xyz>
2025-01-22 23:47:05 +0000

    canaryRelease: 361583
    Main scripts: 3
    Lazy Loaded Scripts: 905
    Chunks:
     * Main Files: 13046
     * Scripts: 9353
    Strings: 0
    Routes: 694
    Experiments Count: 379

--- chunks/727241.js
+++ chunks/727241.js
@@ -5,4 +5,5 @@ t.d(n, {
     }
 }),
     ((l = i || (i = {}))[(l.PROMOTED = 1)] = 'PROMOTED'),
+    (l[(l.SKIPS_APPLICATION_DISCOVERABILITY_VALIDATION = 2)] = 'SKIPS_APPLICATION_DISCOVERABILITY_VALIDATION'),
     (l[(l.DEFAULT = 0)] = 'DEFAULT');
