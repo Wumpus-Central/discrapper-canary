97143cf0dacf5926debac1a815e991c0fd57195a
DiscrapperManager <admin@minerpl.xyz>
2025-01-23 18:10:39 +0000

    canaryRelease: 361811
    Main scripts: 3
    Lazy Loaded Scripts: 905
    Chunks:
     * Main Files: 13047
     * Scripts: 9353
    Strings: 0
    Routes: 694
    Experiments Count: 380

--- chunks/260775.js
+++ chunks/260775.js
@@ -1,1 +1,5 @@
-t.exports = { art: 'art_f2f3e8' };
+t.exports = {
+    art: 'art_f2f3e8',
+    modalHeader: 'modalHeader_f2f3e8',
+    modalInnerCloseButton: 'modalInnerCloseButton_f2f3e8'
+};
--- chunks/340140.js
+++ chunks/340140.js
@@ -1,86 +1,88 @@
-l.r(n), l(47120);
-var e = l(200651),
-    i = l(192379),
-    r = l(481060),
-    s = l(600164),
-    a = l(538211),
-    o = l(698066),
-    u = l(648159),
-    c = l(388032),
-    d = l(260775),
-    h = l(434204);
-function g(t, n, l) {
+e.r(n), e(47120);
+var l = e(200651),
+    i = e(192379),
+    a = e(481060),
+    r = e(600164),
+    s = e(538211),
+    o = e(698066),
+    u = e(648159),
+    c = e(388032),
+    d = e(260775),
+    m = e(434204);
+function h(t, n, e) {
     return (
         n in t
             ? Object.defineProperty(t, n, {
-                  value: l,
+                  value: e,
                   enumerable: !0,
                   configurable: !0,
                   writable: !0
               })
-            : (t[n] = l),
+            : (t[n] = e),
         t
     );
 }
-class m extends i.PureComponent {
+class g extends i.PureComponent {
     render() {
-        let { isStreamer: t, transitionState: n, onClose: l } = this.props,
+        let { isStreamer: t, transitionState: n, onClose: e } = this.props,
             { problem: i } = this.state;
-        return (0, e.jsxs)(r.ModalRoot, {
+        return (0, l.jsxs)(a.ModalRoot, {
             transitionState: n,
-            size: r.ModalSize.SMALL,
+            size: a.ModalSize.SMALL,
             'aria-label': c.intl.string(c.t.qnJ9W1),
             children: [
-                (0, e.jsx)(u.Z, {}),
-                (0, e.jsxs)(r.ModalHeader, {
+                (0, l.jsx)(u.Z, {}),
+                (0, l.jsxs)(a.ModalHeader, {
                     separator: !1,
+                    className: d.modalHeader,
                     children: [
-                        (0, e.jsx)(r.ModalCloseButton, {
-                            className: h.closeButton,
-                            onClick: l
-                        }),
-                        (0, e.jsx)(r.FormTitle, {
-                            tag: 'h2',
-                            className: h.title,
+                        (0, l.jsx)(a.Heading, {
+                            variant: 'text-md/semibold',
+                            className: m.title,
                             children: c.intl.string(c.t.qnJ9W1)
+                        }),
+                        (0, l.jsx)(a.ModalCloseButton, {
+                            className: m.closeButton,
+                            innerClassName: d.modalInnerCloseButton,
+                            onClick: e
                         })
                     ]
                 }),
-                (0, e.jsxs)(r.ModalContent, {
-                    className: h.content,
+                (0, l.jsxs)(a.ModalContent, {
+                    className: m.content,
                     children: [
-                        (0, e.jsx)(r.Text, {
+                        (0, l.jsx)(a.Text, {
                             variant: 'text-sm/normal',
                             children: c.intl.string(c.t['7vw0h4'])
                         }),
-                        (0, e.jsx)(r.FormItem, {
+                        (0, l.jsx)(a.FormItem, {
                             title: c.intl.string(c.t['6Y1t5O']),
-                            children: (0, e.jsx)(r.SingleSelect, {
+                            children: (0, l.jsx)(a.SingleSelect, {
                                 placeholder: c.intl.string(c.t.U0kGk5),
-                                options: (0, a.Z)(t, !1),
+                                options: (0, s.Z)(t, !1),
                                 onChange: this.handleChanged,
                                 value: i,
                                 maxVisibleItems: 4
                             })
                         }),
-                        (0, e.jsx)('div', { className: d.art })
+                        (0, l.jsx)('div', { className: d.art })
                     ]
                 }),
-                (0, e.jsxs)(r.ModalFooter, {
-                    className: h.__invalid_footer,
+                (0, l.jsxs)(a.ModalFooter, {
+                    className: m.__invalid_footer,
                     children: [
-                        (0, e.jsx)(r.Button, {
-                            color: r.Button.Colors.BRAND,
+                        (0, l.jsx)(a.Button, {
+                            color: a.Button.Colors.BRAND,
                             disabled: null == i,
                             onClick: this.handleSubmit,
                             children: c.intl.string(c.t.E48BIS)
                         }),
-                        (0, e.jsx)(s.Z, {
-                            children: (0, e.jsx)(r.Button, {
-                                look: r.Button.Looks.LINK,
-                                color: r.Button.Colors.PRIMARY,
-                                size: r.Button.Sizes.NONE,
-                                onClick: l,
+                        (0, l.jsx)(r.Z, {
+                            children: (0, l.jsx)(a.Button, {
+                                look: a.Button.Looks.LINK,
+                                color: a.Button.Colors.PRIMARY,
+                                size: a.Button.Sizes.NONE,
+                                onClick: e,
                                 children: c.intl.string(c.t['ETE/oK'])
                             })
                         })
@@ -91,26 +93,26 @@ class m extends i.PureComponent {
     }
     constructor(...t) {
         super(...t),
-            g(this, 'state', { problem: null }),
-            g(this, 'handleChanged', (t) => {
+            h(this, 'state', { problem: null }),
+            h(this, 'handleChanged', (t) => {
                 this.setState({ problem: null != t ? t : null });
             }),
-            g(this, 'handleSubmit', () => {
-                let { stream: t, streamApplication: n, analyticsData: i, onClose: s } = this.props,
-                    { problem: a } = this.state;
+            h(this, 'handleSubmit', () => {
+                let { stream: t, streamApplication: n, analyticsData: i, onClose: r } = this.props,
+                    { problem: s } = this.state;
                 (0, o.Z)({
-                    problem: a,
+                    problem: s,
                     stream: t,
                     feedback: '',
                     streamApplication: n,
                     analyticsData: i,
                     location: 'Stream'
                 }),
-                    s(),
-                    (0, r.openModalLazy)(async () => {
-                        let { default: t } = await l.e('14466').then(l.bind(l, 729328));
+                    r(),
+                    (0, a.openModalLazy)(async () => {
+                        let { default: t } = await e.e('14466').then(e.bind(e, 729328));
                         return (n) =>
-                            (0, e.jsx)(t, {
+                            (0, l.jsx)(t, {
                                 body: c.intl.string(c.t.mMTVnp),
                                 ...n
                             });
@@ -118,4 +120,4 @@ class m extends i.PureComponent {
             });
     }
 }
-n.default = m;
+n.default = g;
