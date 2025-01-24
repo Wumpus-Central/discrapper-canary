e02ab34a4b74ebec53c9e46a989778cf7899153d
DiscrapperManager <admin@minerpl.xyz>
2025-01-24 00:03:46 +0000

    canaryRelease: 362056
    Main scripts: 3
    Lazy Loaded Scripts: 906
    Chunks:
     * Main Files: 13049
     * Scripts: 9362
    Strings: 0
    Routes: 694
    Experiments Count: 381

--- chunks/163612.js
+++ chunks/163612.js
@@ -1,8 +1,11 @@
-let i = (0, r(818083).B)({
+var i = r(818083),
+    a = r(987338);
+let o = (0, i.B)({
     kind: 'guild',
     id: '2024-12_guild_voice_channel_ringing',
     label: 'Guild Voice Ringing',
     defaultConfig: { enabled: !1 },
+    commonTriggerPoint: a.$P.VOICE_CALL,
     treatments: [
         {
             id: 1,
@@ -11,4 +14,4 @@ let i = (0, r(818083).B)({
         }
     ]
 });
-n.Z = i;
+n.Z = o;
--- chunks/268004.js
+++ chunks/268004.js
@@ -1,12 +1,13 @@
 r.d(n, {
     M: function () {
-        return c;
+        return d;
     }
 });
 var i = r(424602),
     a = r(942766),
     o = r(861254),
-    s = r(91641),
-    l = r(895536),
-    u = r(987338);
-let c = new s.E([l.NR, l.ZI, l.Ob, l.uc, l.m1, i.aq, i.xN, o.kX, a.O], u.$P.VOICE_CALL, { location: 'voice call initiated' });
+    s = r(163612),
+    l = r(91641),
+    u = r(895536),
+    c = r(987338);
+let d = new l.E([u.NR, u.ZI, u.Ob, u.uc, u.m1, i.aq, i.xN, s.Z, o.kX, a.O], c.$P.VOICE_CALL, { location: 'voice call initiated' });
