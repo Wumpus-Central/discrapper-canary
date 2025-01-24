3ec66cf13065234da662cd763c5bbb966965ba8d
DiscrapperManager <admin@minerpl.xyz>
2025-01-24 19:10:51 +0000

    canaryRelease: 362260
    Main scripts: 3
    Lazy Loaded Scripts: 907
    Chunks:
     * Main Files: 13050
     * Scripts: 9373
    Strings: 0
    Routes: 694
    Experiments Count: 384

--- chunks/516887.js
+++ chunks/516887.js
@@ -4,30 +4,35 @@ var i = n(200651),
     a = n.n(r),
     s = n(481060),
     o = n(826298),
-    c = n(625553);
+    c = n(540059),
+    d = n(625553);
 t.Z = l.memo(function (e) {
     var t;
     let { className: n, channel: l, section: r } = e,
-        d = null != r ? (0, o.ky)(r) : null;
-    return (0, i.jsx)('div', {
-        className: a()(n, c.wrapper),
-        children: (0, i.jsx)('div', {
-            className: c.icon,
-            children:
-                null != d
-                    ? (0, i.jsx)(s.Tooltip, {
-                          text: null !== (t = null == r ? void 0 : r.name) && void 0 !== t ? t : '',
-                          position: 'top',
-                          children: (e) =>
-                              (0, i.jsx)(d, {
-                                  ...e,
-                                  channel: l,
-                                  section: r,
-                                  width: 24,
-                                  height: 24
-                              })
-                      })
-                    : null
-        })
-    });
+        u = (0, c.R6)('ChannelApplicationIcon'),
+        h = null != r ? (0, o.ky)(r) : null,
+        p =
+            null != h
+                ? (0, i.jsx)(s.Tooltip, {
+                      text: null !== (t = null == r ? void 0 : r.name) && void 0 !== t ? t : '',
+                      position: 'top',
+                      children: (e) =>
+                          (0, i.jsx)(h, {
+                              ...e,
+                              channel: l,
+                              section: r,
+                              width: 24,
+                              height: 24
+                          })
+                  })
+                : null;
+    return u
+        ? p
+        : (0, i.jsx)('div', {
+              className: a()(n, d.wrapper),
+              children: (0, i.jsx)('div', {
+                  className: d.icon,
+                  children: p
+              })
+          });
 });
