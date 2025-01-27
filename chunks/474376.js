06e54478fdd8877447f51264db975f71e8d04de5
DiscrapperManager <admin@minerpl.xyz>
2025-01-27 23:58:39 +0000

    canaryRelease: 362917
    Main scripts: 3
    Lazy Loaded Scripts: 908
    Chunks:
     * Main Files: 13068
     * Scripts: 9389
    Strings: 0
    Routes: 694
    Experiments Count: 384

--- chunks/474376.js
+++ chunks/474376.js
@@ -1,111 +1,116 @@
 n.d(t, {
     g: function () {
-        return I;
+        return N;
     }
 }),
     n(653041);
 var i = n(200651);
 n(192379);
 var r = n(442837),
-    l = n(481060),
-    a = n(44315),
-    s = n(598077),
-    o = n(246946),
-    c = n(594174),
-    d = n(626135),
-    u = n(51144),
-    h = n(480387),
-    m = n(726745),
-    p = n(251423),
-    g = n(383832),
-    f = n(981631),
-    _ = n(388032),
-    E = n(543395);
-function I(e) {
-    let t = (0, r.e7)([c.default], () => c.default.getCurrentUser()),
-        n = (0, r.e7)([o.Z], () => o.Z.hidePersonalInformation),
-        { multiAccountUsers: I } = (0, p.L)(),
-        C = (e) => {
-            if (e !== (null == t ? void 0 : t.id)) d.default.track(f.rMx.MULTI_ACCOUNT_SWITCH_ATTEMPT, { location: { section: f.jXE.USER_PROFILE } }), h.yD(e);
+    l = n(780384),
+    a = n(481060),
+    s = n(44315),
+    o = n(540059),
+    c = n(210887),
+    d = n(598077),
+    u = n(246946),
+    h = n(594174),
+    m = n(626135),
+    p = n(51144),
+    g = n(480387),
+    f = n(726745),
+    _ = n(251423),
+    E = n(383832),
+    I = n(981631),
+    C = n(388032),
+    v = n(543395);
+function N(e) {
+    let t = (0, r.e7)([h.default], () => h.default.getCurrentUser()),
+        n = (0, r.e7)([u.Z], () => u.Z.hidePersonalInformation),
+        N = (0, r.e7)([c.Z], () => (0, l.wj)(c.Z.theme)),
+        T = (0, o.R6)('useMultiAccountMenuItems'),
+        { multiAccountUsers: S } = (0, _.L)(),
+        b = (e) => {
+            if (e !== (null == t ? void 0 : t.id)) m.default.track(I.rMx.MULTI_ACCOUNT_SWITCH_ATTEMPT, { location: { section: I.jXE.USER_PROFILE } }), g.yD(e);
         },
-        v = I.map((r) => {
-            let o = new s.Z(r),
-                c = o.id === (null == t ? void 0 : t.id),
-                d = r.tokenStatus === m.q.INVALID,
-                h = n ? null : '#'.concat(o.discriminator);
+        A = S.map((r) => {
+            let l = new d.Z(r),
+                o = l.id === (null == t ? void 0 : t.id),
+                c = r.tokenStatus === f.q.INVALID,
+                u = n ? null : '#'.concat(l.discriminator);
             return (0, i.jsx)(
-                l.MenuItem,
+                a.MenuItem,
                 {
-                    id: o.id,
-                    focusedClassName: E.focused,
+                    id: l.id,
+                    focusedClassName: v.focused,
                     label: (e) => {
                         let { isFocused: t } = e;
                         return (0, i.jsxs)('div', {
-                            className: E.userMenuItem,
+                            className: v.userMenuItem,
                             children: [
-                                (0, i.jsx)(l.Avatar, {
-                                    src: o.getAvatarURL(void 0, 40),
-                                    size: l.AvatarSizes.SIZE_24,
+                                (0, i.jsx)(a.Avatar, {
+                                    src: l.getAvatarURL(void 0, 40),
+                                    size: a.AvatarSizes.SIZE_24,
                                     'aria-label': r.username
                                 }),
                                 (0, i.jsxs)('div', {
-                                    className: E.userMenuUsername,
+                                    className: v.userMenuUsername,
                                     children: [
-                                        (0, i.jsx)(l.Text, {
-                                            className: E.userMenuText,
+                                        (0, i.jsx)(a.Text, {
+                                            className: v.userMenuText,
                                             variant: 'text-sm/normal',
-                                            children: u.ZP.getUserTag(o, {
+                                            children: p.ZP.getUserTag(l, {
                                                 mode: 'username',
                                                 identifiable: n ? 'never' : 'always'
                                             })
                                         }),
-                                        !o.isPomelo() &&
-                                            (0, i.jsx)(l.Text, {
-                                                className: E.userMenuDiscriminator,
+                                        !l.isPomelo() &&
+                                            (0, i.jsx)(a.Text, {
+                                                className: v.userMenuDiscriminator,
                                                 variant: 'text-sm/normal',
-                                                children: h
+                                                children: u
                                             })
                                     ]
                                 }),
-                                c &&
-                                    (0, i.jsx)(l.CircleCheckIcon, {
+                                o &&
+                                    (0, i.jsx)(a.CircleCheckIcon, {
                                         size: 'sm',
-                                        color: (0, a.Lq)(t ? f.Ilk.WHITE_500 : f.Ilk.BRAND_500),
-                                        secondaryColor: (0, a.Lq)(t ? f.Ilk.BRAND_500 : f.Ilk.WHITE_500),
-                                        className: E.activeIcon
+                                        color: (0, s.Lq)(t ? I.Ilk.WHITE_500 : I.Ilk.BRAND_500),
+                                        secondaryColor: (0, s.Lq)((t && N) || (t && !T && !N) ? I.Ilk.BRAND_500 : I.Ilk.WHITE_500),
+                                        className: v.activeIcon
                                     }),
-                                d &&
-                                    (0, i.jsx)(l.CircleWarningIcon, {
-                                        color: (0, a.Lq)(f.Ilk.RED_400),
-                                        secondaryColor: (0, a.Lq)(f.Ilk.WHITE_500),
+                                c &&
+                                    (0, i.jsx)(a.CircleWarningIcon, {
+                                        color: (0, s.Lq)(I.Ilk.RED_400),
+                                        secondaryColor: (0, s.Lq)((t && N) || (t && !T && !N) ? I.Ilk.BRAND_500 : I.Ilk.WHITE_500),
                                         size: 'xs',
-                                        className: E.activeIcon
+                                        className: v.activeIcon
                                     })
                             ]
                         });
                     },
                     action: () => {
-                        null == e || e(), d ? (0, g.Z)() : C(o.id);
+                        null == e || e(), c ? (0, E.Z)() : b(l.id);
                     }
                 },
-                o.id
+                l.id
             );
         });
     return (
-        v.push(
+        A.push(
             (0, i.jsxs)(i.Fragment, {
                 children: [
-                    (0, i.jsx)(l.MenuSeparator, {}),
-                    (0, i.jsx)(l.MenuItem, {
+                    (0, i.jsx)(a.MenuSeparator, {}),
+                    (0, i.jsx)(a.MenuItem, {
                         id: 'manage-accounts',
-                        label: _.intl.string(_.t.WbFpq6),
+                        label: C.intl.string(C.t.WbFpq6),
                         action: () => {
-                            null == e || e(), (0, g.Z)();
+                            null == e || e(), (0, E.Z)();
                         }
                     })
                 ]
             })
         ),
-        v
+        A
     );
 }
