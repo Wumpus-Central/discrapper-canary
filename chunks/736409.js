de8d01b862781e507192946dc291f87ac06c0633
DiscrapperManager <admin@minerpl.xyz>
2025-01-27 17:48:48 +0000

    canaryRelease: 362625
    Main scripts: 3
    Lazy Loaded Scripts: 907
    Chunks:
     * Main Files: 13063
     * Scripts: 9371
    Strings: 0
    Routes: 694
    Experiments Count: 383

--- chunks/736409.js
+++ chunks/736409.js
@@ -1,6 +1,6 @@
 n.d(t, {
     j: function () {
-        return x;
+        return _;
     }
 });
 var i = n(200651),
@@ -9,48 +9,50 @@ var i = n(200651),
     a = n(704215),
     o = n(780384),
     s = n(481060),
-    c = n(410030),
-    u = n(243778),
-    d = n(695346),
-    h = n(316496),
-    p = n(921944),
-    m = n(388032),
-    f = n(61260),
-    g = n(204794);
-let v = {
+    c = n(230711),
+    u = n(410030),
+    d = n(243778),
+    h = n(695346),
+    p = n(316496),
+    m = n(981631),
+    f = n(921944),
+    g = n(388032),
+    v = n(61260),
+    C = n(204794);
+let x = {
         topLeftDark: 'rgba(62, 22, 137, 0.8)',
         topRightDark: 'rgba(160, 86, 242, 0.4)',
         topLeftLight: 'rgba(159, 86, 242, 0.4)',
         topRightLight: 'rgba(222, 194, 252, 0.5)'
     },
-    C = (e) => {
+    I = (e) => {
         let { markAsDismissed: t, setIsForceShowSharingPopout: n } = e,
-            a = (0, c.ZP)(),
-            u = d.G6.useSetting();
+            a = (0, u.ZP)(),
+            d = h.G6.useSetting();
         return (
             (0, l.useEffect)(
                 () => () => {
-                    t(p.L.INDIRECT_ACTION), n(!1);
+                    t(f.L.INDIRECT_ACTION), n(!1);
                 },
                 [t, n]
             ),
             (0, i.jsxs)('div', {
-                className: f.wrapper,
+                className: v.wrapper,
                 children: [
                     (0, i.jsx)('div', {
-                        className: f.gradient,
-                        style: { background: 'radial-gradient(75% 75% at 0% 0%, '.concat((0, o.wj)(a) ? v.topLeftDark : v.topLeftLight, ' 0%, transparent 100%),\n    radial-gradient(70% 50% at 100% 0%, ').concat((0, o.wj)(a) ? v.topRightDark : v.topRightLight, ' 0%, transparent 100%)') }
+                        className: v.gradient,
+                        style: { background: 'radial-gradient(75% 75% at 0% 0%, '.concat((0, o.wj)(a) ? x.topLeftDark : x.topLeftLight, ' 0%, transparent 100%),\n    radial-gradient(70% 50% at 100% 0%, ').concat((0, o.wj)(a) ? x.topRightDark : x.topRightLight, ' 0%, transparent 100%)') }
                     }),
                     (0, i.jsxs)('div', {
-                        className: f.content,
+                        className: v.content,
                         children: [
                             (0, i.jsx)(s.Button, {
-                                className: f.closeButton,
-                                'aria-label': m.intl.string(m.t.cpT0Cg),
+                                className: v.closeButton,
+                                'aria-label': g.intl.string(g.t.cpT0Cg),
                                 look: s.Button.Looks.BLANK,
                                 size: s.Button.Sizes.NONE,
                                 onClick: () => {
-                                    t(p.L.USER_DISMISS), n(!1);
+                                    t(f.L.USER_DISMISS), n(!1);
                                 },
                                 children: (0, i.jsx)(s.XSmallIcon, {
                                     size: 'xs',
@@ -59,40 +61,41 @@ let v = {
                             }),
                             (0, i.jsx)('img', {
                                 alt: '',
-                                src: g.Z,
-                                className: f.controllerImage
+                                src: C.Z,
+                                className: v.controllerImage
                             }),
                             (0, i.jsx)(r.X6, {
                                 variant: 'heading-md/bold',
-                                className: f.title,
-                                children: m.intl.string(m.t.ObD1w8)
+                                className: v.title,
+                                children: g.intl.string(g.t.ObD1w8)
                             }),
                             (0, i.jsxs)('div', {
-                                className: f.contentContainer,
+                                className: v.contentContainer,
                                 children: [
                                     (0, i.jsxs)('div', {
-                                        className: f.toggleContainer,
+                                        className: v.toggleContainer,
                                         children: [
                                             (0, i.jsx)(s.Text, {
                                                 variant: 'text-sm/medium',
-                                                children: m.intl.string(m.t.drIYTU)
+                                                children: g.intl.string(g.t.drIYTU)
                                             }),
                                             (0, i.jsx)(s.Switch, {
-                                                onChange: (e) => d.G6.updateSetting(e),
-                                                checked: u
+                                                onChange: (e) => h.G6.updateSetting(e),
+                                                checked: d
                                             })
                                         ]
                                     }),
                                     (0, i.jsx)(s.Text, {
                                         variant: 'text-xs/normal',
-                                        children: m.intl.string(m.t.GnhKNz)
+                                        children: g.intl.string(g.t.GnhKNz)
                                     })
                                 ]
                             }),
                             (0, i.jsx)(s.Button, {
                                 look: s.Button.Looks.BLANK,
-                                className: f.viewSettingsButton,
-                                children: m.intl.string(m.t.g8usuL)
+                                className: v.viewSettingsButton,
+                                onClick: () => c.Z.open(m.oAB.ACTIVITY_PRIVACY),
+                                children: g.intl.string(g.t.g8usuL)
                             })
                         ]
                     })
@@ -100,16 +103,16 @@ let v = {
             })
         );
     },
-    x = (e) => {
+    _ = (e) => {
         let { isForceShowSharingPopout: t, setIsForceShowSharingPopout: n } = e,
-            { rtcPopoutEnabled: l } = (0, h.S)('SharingPrivacyPopout');
+            { rtcPopoutEnabled: l } = (0, p.S)('SharingPrivacyPopout');
         return l
-            ? (0, i.jsx)(u.ZP, {
+            ? (0, i.jsx)(d.ZP, {
                   contentTypes: [a.z.SHARE_ACTIVITY_COACHMARK],
                   children: (e) => {
                       let { visibleContent: l, markAsDismissed: r } = e;
                       if (l === a.z.SHARE_ACTIVITY_COACHMARK || t)
-                          return (0, i.jsx)(C, {
+                          return (0, i.jsx)(I, {
                               markAsDismissed: r,
                               setIsForceShowSharingPopout: n
                           });
