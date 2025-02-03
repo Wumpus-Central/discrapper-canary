8f94c63ca8d73dc526d2e1134b046b57fcfe197e
DiscrapperManager <admin@minerpl.xyz>
2025-02-03 19:22:59 +0000

    canaryRelease: 364809
    Main scripts: 3
    Lazy Loaded Scripts: 895
    Chunks:
     * Main Files: 12662
     * Scripts: 9291
    Strings: 0
    Routes: 694
    Experiments Count: 386

--- chunks/999224.js
+++ chunks/999224.js
@@ -19,97 +19,109 @@ let E = {
         name: 'Skye',
         iconURL: _.Z,
         iconBgColors: ['rgb(53, 237, 126)', 'rgb(242, 96, 195)'],
-        splashGradient: 'radial-gradient(circle at 0% 100%, rgba(242, 96, 195, 0.40) 0%, rgba(242, 96, 195, 0.00) 100%)',
-        baseColor: '#390528'
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(242, 96, 195, 0.50) 0%, rgba(242, 96, 195, 0.00) 100%), linear-gradient(90deg, rgba(53, 236, 125, 0.25) 0%, rgba(53, 236, 125, 0.00) 15%)',
+        baseColor: '#380527'
     },
     quinn: {
         name: 'Quinn',
         iconURL: c.Z,
         iconBgColors: ['rgb(53, 237, 126)', 'rgb(47, 94, 67)'],
-        splashGradient: 'radial-gradient(circle at 0% 100%, rgba(255, 138, 0, 0.40) 0%, rgba(255, 138, 0, 0.00) 100%)',
-        baseColor: '#3D2100'
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(47, 94, 67, 0.50) 0%, rgba(47, 94, 67, 0.00) 100%), linear-gradient(90deg, rgba(53, 236, 126, 0.25) 0%, rgba(53, 236, 126, 0.00) 15%)',
+        baseColor: '#14291D'
     },
     axel: {
         name: 'Axel',
         iconURL: i.Z,
         iconBgColors: ['rgb(104, 189, 255)', 'rgb(40, 84, 54)'],
-        splashGradient: 'radial-gradient(circle at 0% 100%, rgba(44, 126, 167, 0.40) 0%, rgba(44, 126, 167, 0.00) 100%)',
-        baseColor: '#0D2530'
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(40, 84, 54, 0.50) 0%, rgba(40, 84, 54, 0.00) 100%), linear-gradient(90deg, rgba(104, 189, 255, 0.25) 0%, rgba(104, 189, 255, 0.00) 15%)',
+        baseColor: '#14291B'
     },
     sebastien: {
         name: 'Sébastien',
         iconURL: f.Z,
         iconBgColors: ['rgb(104, 188, 254)', 'rgb(92, 119, 31)'],
-        splashGradient: 'radial-gradient(circle at 0% 100%, rgba(92, 119, 31, 0.40) 0%, rgba(92, 119, 31, 0.00) 100%)',
+        splashGradient: 'radial-gradient(9% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(100% 555.56% at 0% 100%, rgba(92, 119, 31, 0.50) 0%, rgba(92, 119, 31, 0.00) 100%), linear-gradient(90deg, rgba(104, 188, 254, 0.25) 0%, rgba(104, 188, 254, 0.00) 15%)',
         baseColor: '#25300D'
     },
     megaphone: {
         name: 'Megaphone',
         iconURL: u.Z,
         iconBgColors: ['rgb(127, 233, 134)', 'rgb(230, 90, 205)'],
-        splashGradient: 'radial-gradient(circle at 0% 100%, rgba(127, 234, 135, 0.40) 0%, rgba(127, 234, 135, 0.00) 100%)',
-        baseColor: '#09350C'
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(230, 90, 205, 0.50) 0%, rgba(230, 90, 205, 0.00) 100%), linear-gradient(90deg, rgba(127, 233, 134, 0.25) 0%, rgba(127, 233, 134, 0.00) 15%)',
+        baseColor: '#35082D'
     },
     robot: {
-        name: '\uD83D\uDEA7 Robot',
+        name: '\uD83D\uDEA7 DR01D',
         iconURL: d.Z,
         iconBgColors: ['rgb(53, 236, 125)', 'rgb(61, 72, 79)'],
         underDevelopment: !0,
-        splashGradient: 'radial-gradient(circle at 0% 100%, rgba(195, 187, 172, 0.40) 0%, rgba(195, 187, 172, 0.00) 100%)',
-        baseColor: '#24201A'
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(61, 72, 79, 0.50) 0%, rgba(61, 72, 79, 0.00) 100%), linear-gradient(90deg, rgba(118, 233, 136, 0.25) 0%, rgba(118, 233, 136, 0.00) 15%)',
+        baseColor: '#1B1F23'
     },
     tunes: {
-        name: '\uD83D\uDEA7 Tunes',
+        name: '\uD83D\uDEA7 Tunetron',
         iconURL: m.Z,
         iconBgColors: ['rgb(255, 76, 210)', 'rgb(111, 134, 253)'],
         underDevelopment: !0,
-        splashGradient: 'radial-gradient(circle at 0% 100%, rgba(223, 48, 215, 0.40) 0%, rgba(223, 48, 215, 0.00) 100%)',
-        baseColor: '#350833'
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(100% 555.56% at 0% 100%, rgba(111, 134, 253, 0.50) 0%, rgba(111, 134, 253, 0.00) 100%), linear-gradient(90deg, rgba(254, 76, 209, 0.25) 0%, rgba(254, 76, 209, 0.00) 15%)',
+        baseColor: '#010A3C'
     },
     ghost: {
         name: '\uD83D\uDEA7 Ghost',
         iconURL: s.Z,
         iconBgColors: ['rgb(255, 76, 210)', 'rgb(110, 73, 156)'],
-        underDevelopment: !0
+        underDevelopment: !0,
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(110, 73, 156, 0.50) 0%, rgba(110, 73, 156, 0.00) 100%), linear-gradient(90deg, rgba(255, 76, 210, 0.25) 0%, rgba(255, 76, 210, 0.00) 15%)',
+        baseColor: '#1D142A'
     },
     space_bunny: {
         name: 'Space Bunny',
         iconURL: h.Z,
         iconBgColors: ['rgb(118, 234, 137)', 'rgb(110, 60, 223)'],
-        splashGradient: 'radial-gradient(circle at 0% 100%, rgba(110, 60, 223, 0.40) 0%, rgba(110, 60, 223, 0.00) 100%)',
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(100% 555.56% at 0% 100%, rgba(110, 60, 223, 0.40) 0%, rgba(110, 60, 223, 0.00) 100%), linear-gradient(90deg, rgba(118, 233, 136, 0.25) 0%, rgba(118, 233, 136, 0.00) 15%)',
         baseColor: '#160935'
     },
     justus: {
         name: 'Justus',
         iconURL: l.Z,
-        iconBgColors: ['rgb(104, 189, 255)', 'rgb(5, 86, 248)']
+        iconBgColors: ['rgb(104, 189, 255)', 'rgb(5, 86, 248)'],
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(100% 555.56% at 0% 100%, rgba(5, 86, 248, 0.50) 0%, rgba(5, 86, 248, 0.00) 100%), linear-gradient(90deg, rgba(104, 188, 254, 0.25) 0%, rgba(104, 188, 254, 0.00) 15%)',
+        baseColor: '#01153C'
     },
     harper: {
         name: 'Harper',
         iconURL: o.Z,
-        iconBgColors: ['rgb(255, 76, 210)', 'rgb(125, 87, 179)']
+        iconBgColors: ['rgb(255, 76, 210)', 'rgb(125, 87, 179)'],
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(125, 87, 179, 0.50) 0%, rgba(125, 87, 179, 0.00) 100%), linear-gradient(90deg, rgba(254, 76, 209, 0.25) 0%, rgba(254, 76, 209, 0.00) 15%)',
+        baseColor: '#1D132A'
     },
     villain: {
         name: 'Villain',
         iconURL: g.Z,
-        iconBgColors: ['rgb(53, 237, 126)', 'rgb(219, 2, 34)']
+        iconBgColors: ['rgb(53, 237, 126)', 'rgb(219, 2, 34)'],
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(100% 555.56% at 0% 100%, rgba(219, 2, 34, 0.50) 0%, rgba(219, 2, 34, 0.00) 100%), linear-gradient(90deg, rgba(53, 236, 125, 0.25) 0%, rgba(53, 236, 125, 0.00) 15%)',
+        baseColor: '#3D0109'
     },
     solara: {
         name: 'Solara',
         iconURL: p.Z,
         iconBgColors: ['rgb(255, 76, 210)', 'rgb(214, 155, 56)'],
-        splashGradient: 'radial-gradient(circle at 0% 100%, rgba(255, 209, 60, 0.40) 0%, rgba(255, 209, 60, 0.00) 100%)',
-        baseColor: '#332700'
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(214, 155, 56, 0.50) 0%, rgba(214, 155, 56, 0.00) 100%), linear-gradient(90deg, rgba(254, 76, 209, 0.25) 0%, rgba(254, 76, 209, 0.00) 15%)',
+        baseColor: '#33240A'
     },
     cave: {
         name: 'Cave',
         iconURL: r.Z,
-        iconBgColors: ['rgb(124, 188, 249)', 'rgb(207, 122, 124)']
+        iconBgColors: ['rgb(124, 188, 249)', 'rgb(207, 122, 124)'],
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(207, 122, 124, 0.50) 0%, rgba(207, 122, 124, 0.00) 100%), linear-gradient(90deg, rgba(124, 188, 249, 0.25) 0%, rgba(124, 188, 249, 0.00) 15%)',
+        baseColor: '#2D1011'
     },
     deepfried: {
         name: 'Deep Fried',
         iconURL: a.Z,
-        iconBgColors: ['rgb(127, 233, 135)', 'rgb(253, 177, 45)']
+        iconBgColors: ['rgb(127, 233, 135)', 'rgb(253, 177, 45)'],
+        splashGradient: 'radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.10) 0%, rgba(255, 255, 255, 0.00) 25%), radial-gradient(89% 494.44% at 11% 100%, rgba(255, 255, 255, 0.17) 0%, rgba(255, 255, 255, 0.10) 15%, rgba(255, 255, 255, 0.05) 30%, rgba(255, 255, 255, 0.03) 45%, rgba(255, 255, 255, 0.01) 60%, rgba(255, 255, 255, 0.00) 75%), radial-gradient(84.38% 468.75% at 15.63% 100%, rgba(253, 177, 45, 0.50) 0%, rgba(253, 177, 45, 0.00) 100%), linear-gradient(90deg, rgba(127, 233, 135, 0.25) 0%, rgba(127, 233, 135, 0.00) 15%)',
+        baseColor: '#3D2701'
     }
 };
 (E.rachel = E.skye), (E.alex = E.quinn), (E.elliot = E.axel), (E.sebastian = E.sebastien), (E.justin = E.justus), (E.taylor = E.harper), (E.monique = E.solara);
