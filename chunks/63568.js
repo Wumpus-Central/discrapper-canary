0be889aadc59b7c420aa46b17b2c306ee18470e8
DiscrapperManager <admin@minerpl.xyz>
2025-02-03 18:41:14 +0000

    canaryRelease: 364783
    Main scripts: 3
    Lazy Loaded Scripts: 895
    Chunks:
     * Main Files: 12661
     * Scripts: 9274
    Strings: 0
    Routes: 694
    Experiments Count: 385

--- chunks/63568.js
+++ chunks/63568.js
@@ -1,9 +1,14 @@
 n.d(t, {
-    K2: () => a,
-    jS: () => r,
-    tW: () => i
+    K2: () => c,
+    jS: () => u,
+    tW: () => l
 });
-let i = (0, n(818083).B)({
+var i = n(442837),
+    r = n(818083),
+    a = n(944163),
+    s = n(937111),
+    o = n(981631);
+let l = (0, r.B)({
     kind: 'guild',
     id: '2024-11_member_verification_rollout',
     label: 'Member verification rollout',
@@ -16,21 +21,36 @@ let i = (0, n(818083).B)({
         }
     ]
 });
-function r(e, t) {
-    return i.useExperiment(
-        {
-            guildId: e,
-            location: t
-        },
-        { autoTrackExposure: !0 }
-    ).enabled;
+function u(e, t) {
+    let n = (0, i.e7)([s.Z, a.Z], () => {
+        var t, n;
+        let i = null != e ? s.Z.getJoinRequestGuild(e) : null,
+            r = null === (t = a.Z.get(e)) || void 0 === t ? void 0 : t.guild;
+        return (null == i ? void 0 : i.hasFeature(o.oNc.MEMBER_VERIFICATION_ROLLOUT_TEST)) || (null == r ? void 0 : null === (n = r.features) || void 0 === n ? void 0 : n.includes(o.oNc.MEMBER_VERIFICATION_ROLLOUT_TEST));
+    });
+    return !!(
+        l.useExperiment(
+            {
+                guildId: e,
+                location: t
+            },
+            { autoTrackExposure: !0 }
+        ).enabled || n
+    );
 }
-function a(e, t) {
-    return i.getCurrentConfig(
-        {
-            guildId: e,
-            location: t
-        },
-        { autoTrackExposure: !0 }
-    ).enabled;
+function c(e, t) {
+    var n, i;
+    let r = null != e ? s.Z.getJoinRequestGuild(e) : null,
+        u = null === (n = a.Z.get(e)) || void 0 === n ? void 0 : n.guild;
+    return !!(
+        (null == r ? void 0 : r.hasFeature(o.oNc.MEMBER_VERIFICATION_ROLLOUT_TEST)) ||
+        (null == u ? void 0 : null === (i = u.features) || void 0 === i ? void 0 : i.includes(o.oNc.MEMBER_VERIFICATION_ROLLOUT_TEST)) ||
+        l.getCurrentConfig(
+            {
+                guildId: e,
+                location: t
+            },
+            { autoTrackExposure: !0 }
+        ).enabled
+    );
 }
--- chunks/944163.js
+++ chunks/944163.js
@@ -1,77 +1,84 @@
 n.d(t, {
-    Z: () => A,
+    Z: () => g,
     t: () => c
 });
-var r,
-    l,
-    i,
-    a = n(392711),
-    s = n.n(a),
-    o = n(442837),
-    E = n(570140),
-    u = n(246364);
+var i,
+    r = n(392711),
+    a = n.n(r),
+    s = n(442837),
+    o = n(570140),
+    l = n(246364);
+function u(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
 let c = {
         version: '',
         description: '',
         formFields: []
     },
-    _ = {};
-class d extends (i = o.ZP.Store) {
+    d = {};
+function f(e) {
+    let { form: t, guildId: n } = e;
+    if (null == t) d[n] = c;
+    else {
+        var i;
+        let e = null !== (i = d[n]) && void 0 !== i ? i : c;
+        d[n] = {
+            ...e,
+            ...t
+        };
+    }
+}
+function _(e) {
+    var t;
+    let { guildId: n } = e;
+    d[n] = null !== (t = d[n]) && void 0 !== t ? t : c;
+}
+function p(e) {
+    let { invite: t } = e,
+        { member_verification_form: n } = t,
+        { guild: i } = t;
+    if (null != i && null != n) {
+        var r;
+        return (
+            (d[i.id] = {
+                version: n.version,
+                description: null !== (r = n.description) && void 0 !== r ? r : '',
+                formFields: n.form_fields,
+                guild: i
+            }),
+            !0
+        );
+    }
+    return !1;
+}
+function h(e) {
+    let { guild: t } = e;
+    delete d[null == t ? void 0 : t.id];
+}
+class m extends (i = s.ZP.Store) {
     get(e) {
-        if (null != e) return _[e];
+        if (null != e) return d[e];
     }
     getRulesPrompt(e) {
         var t;
-        return s().find(null === (t = _[e]) || void 0 === t ? void 0 : t.formFields, u.J);
+        return a().find(null === (t = d[e]) || void 0 === t ? void 0 : t.formFields, l.J);
     }
 }
-(l = 'MemberVerificationFormStore'),
-    (r = 'displayName') in d
-        ? Object.defineProperty(d, r, {
-              value: l,
-              enumerable: !0,
-              configurable: !0,
-              writable: !0
-          })
-        : (d[r] = l);
-let A = new d(E.Z, {
-    INVITE_ACCEPT_SUCCESS: function (e) {
-        let { invite: t } = e,
-            { member_verification_form: n } = t,
-            { guild: r } = t;
-        if (null != r && null != n) {
-            var l;
-            return (
-                (_[r.id] = {
-                    version: n.version,
-                    description: null !== (l = n.description) && void 0 !== l ? l : '',
-                    formFields: n.form_fields,
-                    guild: r
-                }),
-                !0
-            );
-        }
-        return !1;
-    },
-    MEMBER_VERIFICATION_FORM_UPDATE: function (e) {
-        let { form: t, guildId: n } = e;
-        if (null == t) _[n] = c;
-        else {
-            var r;
-            let e = null !== (r = _[n]) && void 0 !== r ? r : c;
-            _[n] = {
-                ...e,
-                ...t
-            };
-        }
-    },
-    MEMBER_VERIFICATION_FORM_FETCH_FAIL: function (e) {
-        var t;
-        let { guildId: n } = e;
-        _[n] = null !== (t = _[n]) && void 0 !== t ? t : c;
-    },
-    GUILD_DELETE: function (e) {
-        let { guild: t } = e;
-        delete _[null == t ? void 0 : t.id];
-    }
+u(m, 'displayName', 'MemberVerificationFormStore');
+let g = new m(o.Z, {
+    INVITE_ACCEPT_SUCCESS: p,
+    MEMBER_VERIFICATION_FORM_UPDATE: f,
+    MEMBER_VERIFICATION_FORM_FETCH_FAIL: _,
+    GUILD_DELETE: h
 });
