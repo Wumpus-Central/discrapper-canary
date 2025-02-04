e080882f54181fa037fa7ef1b64a7ebd841e7645
DiscrapperManager <admin@minerpl.xyz>
2025-02-04 23:43:54 +0000

    canaryRelease: 365429
    Main scripts: 3
    Lazy Loaded Scripts: 897
    Chunks:
     * Main Files: 12670
     * Scripts: 9291
    Strings: 0
    Routes: 694
    Experiments Count: 378

--- chunks/187565.js
+++ chunks/187565.js
@@ -1,43 +1,44 @@
 n.d(t, {
-    KJ: () => g,
-    N0: () => C,
-    k3: () => T,
-    lk: () => p,
-    ng: () => v
+    KJ: () => v,
+    N0: () => T,
+    k3: () => p,
+    lk: () => N,
+    ng: () => C
 }),
     n(47120);
 var i,
     l = n(192379),
     r = n(442837),
-    s = n(658666),
-    o = n(384632),
-    a = n(601964),
-    u = n(430824),
-    c = n(863249),
-    d = n(944163),
-    m = n(63568),
-    x = n(246364),
-    f = n(983736),
-    h = n(266395),
+    s = n(999382),
+    o = n(658666),
+    a = n(384632),
+    u = n(601964),
+    c = n(430824),
+    d = n(863249),
+    m = n(944163),
+    x = n(63568),
+    f = n(246364),
+    h = n(983736),
+    g = n(266395),
     j = n(388032),
-    g = (((i = {})[(i.CLAIM_ACCOUNT = 0)] = 'CLAIM_ACCOUNT'), (i[(i.EMAIL_CONFIRMATION = 1)] = 'EMAIL_CONFIRMATION'), (i[(i.VERIFICATION_FORM = 2)] = 'VERIFICATION_FORM'), i);
-function v(e, t, n) {
-    let i = (0, m.jS)(e, 'MemberVerificationScreens'),
-        a = (0, r.e7)([d.Z], () => d.Z.get(e)),
-        u = (0, r.e7)([s.Z], () => s.Z.pendingState);
+    v = (((i = {})[(i.CLAIM_ACCOUNT = 0)] = 'CLAIM_ACCOUNT'), (i[(i.EMAIL_CONFIRMATION = 1)] = 'EMAIL_CONFIRMATION'), (i[(i.VERIFICATION_FORM = 2)] = 'VERIFICATION_FORM'), i);
+function C(e, t, n) {
+    let i = (0, x.jS)(e, 'MemberVerificationScreens'),
+        s = (0, r.e7)([m.Z], () => m.Z.get(e)),
+        u = (0, r.e7)([o.Z], () => o.Z.pendingState);
     l.useEffect(() => {
-        !n && c.ZP.fetchVerificationForm(e, t);
+        !n && d.ZP.fetchVerificationForm(e, t);
     }, [e, t, n]);
-    let f = l.useMemo(() => {
+    let c = l.useMemo(() => {
         var e, t;
         let l;
         if (!n || !i || null == u) return null;
-        if (u.joinType === o.A.APPLY) l = null != u.pendingVerificationFields ? [...u.pendingVerificationFields] : void 0;
+        if (u.joinType === a.A.APPLY) l = null != u.pendingVerificationFields ? [...u.pendingVerificationFields] : void 0;
         else if (null != u.termRules) {
             let e = u.termRules.map((e) => e.value.trim()).filter((e) => '' !== e);
             l = [
                 {
-                    field_type: x.QJ.TERMS,
+                    field_type: f.QJ.TERMS,
                     label: j.intl.string(j.t['9suSIC']),
                     values: e,
                     required: !0
@@ -47,19 +48,20 @@ function v(e, t, n) {
         return null == l
             ? null
             : {
-                  version: null !== (e = null == a ? void 0 : a.version) && void 0 !== e ? e : '',
-                  description: null !== (t = null == a ? void 0 : a.description) && void 0 !== t ? t : '',
+                  version: null !== (e = null == s ? void 0 : s.version) && void 0 !== e ? e : '',
+                  description: null !== (t = null == s ? void 0 : s.description) && void 0 !== t ? t : '',
                   formFields: l,
-                  guild: null == a ? void 0 : a.guild
+                  guild: null == s ? void 0 : s.guild
               };
-    }, [n, i, u, a]);
-    return null != f ? f : a;
+    }, [n, i, u, s]);
+    return null != c ? c : s;
 }
-function C(e, t) {
-    let n = (0, r.e7)([u.Z], () => u.Z.getGuild(e));
-    return l.useMemo(() => (null != n ? n : null != t ? new a.ZP(t) : null), [n, t]);
+function T(e, t, n) {
+    let i = (0, r.e7)([c.Z], () => c.Z.getGuild(e)),
+        o = (0, r.e7)([s.Z], () => s.Z.getGuild());
+    return l.useMemo(() => (n && (null == o ? void 0 : o.id) === e ? o : null != i ? i : null != t ? new u.ZP(t) : null), [n, o, e, i, t]);
 }
-function T(e, t) {
+function p(e, t) {
     let n = !(null == e ? void 0 : e.isClaimed()),
         i = null == e ? void 0 : e.verified,
         [r, s] = l.useState(!t && n ? 0 : 2);
@@ -76,19 +78,19 @@ function T(e, t) {
         }
     );
 }
-function p(e) {
-    let t = (0, h.rb)((e) => e.hasUnsubmittedChanges);
+function N(e) {
+    let t = (0, g.rb)((e) => e.hasUnsubmittedChanges);
     l.useEffect(
         () => (
-            (0, h.a5)(!1),
+            (0, g.a5)(!1),
             () => {
-                (0, h.a5)(!1);
+                (0, g.a5)(!1);
             }
         ),
         []
     ),
         l.useEffect(() => {
-            let n = e.some((e) => e.field_type !== x.QJ.TERMS && (0, f.OA)(e));
-            n && !t ? (0, h.a5)(!0) : !n && t && (0, h.a5)(!1);
+            let n = e.some((e) => e.field_type !== f.QJ.TERMS && (0, h.OA)(e));
+            n && !t ? (0, g.a5)(!0) : !n && t && (0, g.a5)(!1);
         }, [e, t]);
 }
