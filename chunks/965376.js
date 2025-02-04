f18e9799bbbbc8d16f5dd9c39aacfa49f5b63151
DiscrapperManager <admin@minerpl.xyz>
2025-02-04 02:34:52 +0000

    canaryRelease: 365064
    Main scripts: 3
    Lazy Loaded Scripts: 895
    Chunks:
     * Main Files: 12667
     * Scripts: 9286
    Strings: 0
    Routes: 694
    Experiments Count: 383

--- chunks/965376.js
+++ chunks/965376.js
@@ -1,22 +1,28 @@
-n.d(t, { D: () => h }), n(47120), n(653041);
+n.d(t, { D: () => f }), n(47120), n(653041);
 var i = n(192379),
     l = n(392711),
     a = n.n(l),
     r = n(442837),
-    s = n(430824),
-    o = n(159299),
-    c = n(231338),
-    d = n(388032);
-let u = [];
-function h(e, t) {
-    let n = (0, r.e7)([s.Z], () => (null != t ? s.Z.getRoles(t.id) : void 0)),
-        { version: l, members: h } = (0, r.cj)([o.Z], () => ({
-            version: o.Z.getMemberListVersion(e),
-            members: o.Z.getMemberListSections(e)
+    s = n(941028),
+    o = n(493773),
+    c = n(509848),
+    d = n(430824),
+    u = n(159299),
+    h = n(231338),
+    p = n(388032);
+let m = [];
+function f(e, t) {
+    (0, o.Z)(() => {
+        (null == t ? void 0 : t.id) != null && (0, s.b8)(t.id, e, c.KV);
+    });
+    let n = (0, r.e7)([d.Z], () => (null != t ? d.Z.getRoles(t.id) : void 0)),
+        { version: l, members: f } = (0, r.cj)([u.Z], () => ({
+            version: u.Z.getMemberListVersion(e),
+            members: u.Z.getMemberListSections(e)
         })),
-        p = null == t,
-        m = i.useMemo(() => {
-            if (p) return u;
+        g = null == t,
+        _ = i.useMemo(() => {
+            if (g) return m;
             let e = a()(n)
                 .values()
                 .filter((e) => e.hoist)
@@ -25,17 +31,17 @@ function h(e, t) {
                 .map((e) => e.id)
                 .value();
             return (
-                e.push(c.Sk.ONLINE, c.Sk.OFFLINE),
+                e.push(h.Sk.ONLINE, h.Sk.OFFLINE),
                 e.map((e) => {
                     var t, i, l, a;
                     return {
-                        label: e === c.Sk.ONLINE ? d.intl.string(d.t.WbGtnJ) : e === c.Sk.OFFLINE ? d.intl.string(d.t.Vv0abG) : null !== (l = null == n ? void 0 : null === (t = n[e]) || void 0 === t ? void 0 : t.name) && void 0 !== l ? l : '',
-                        userIds: null !== (a = null == h ? void 0 : null === (i = h[e]) || void 0 === i ? void 0 : i.userIds) && void 0 !== a ? a : [],
+                        label: e === h.Sk.ONLINE ? p.intl.string(p.t.WbGtnJ) : e === h.Sk.OFFLINE ? p.intl.string(p.t.Vv0abG) : null !== (l = null == n ? void 0 : null === (t = n[e]) || void 0 === t ? void 0 : t.name) && void 0 !== l ? l : '',
+                        userIds: null !== (a = null == f ? void 0 : null === (i = f[e]) || void 0 === i ? void 0 : i.userIds) && void 0 !== a ? a : [],
                         id: e,
                         roleId: e
                     };
                 })
             );
-        }, [n, h, l, p]);
-    return null != h ? m : u;
+        }, [n, f, l, g]);
+    return null != f ? _ : m;
 }
