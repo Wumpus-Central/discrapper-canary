dbc6cc8409f9d6ebd8d1194b8ea263ee65f32ba2
DiscrapperManager <admin@minerpl.xyz>
2025-02-04 00:20:49 +0000

    canaryRelease: 365000
    Main scripts: 3
    Lazy Loaded Scripts: 895
    Chunks:
     * Main Files: 12667
     * Scripts: 9290
    Strings: 0
    Routes: 694
    Experiments Count: 384

---
+++ chunks/479892.js
@@ -0,0 +1,19 @@
+n.d(t, { ZP: () => r }), n(442837);
+var i = n(818083);
+n(496675);
+var l = n(987338);
+n(231338);
+let r = (0, i.B)({
+    kind: 'guild',
+    id: '2025-02_skill_trees',
+    label: 'Skill Trees',
+    commonTriggerPoint: l.$P.CONNECTION_OPEN,
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
