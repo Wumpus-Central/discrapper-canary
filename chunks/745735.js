0f69d98f3378258575f6e7088952026562da52b9
DiscrapperManager <admin@minerpl.xyz>
2025-02-04 21:12:30 +0000

    canaryRelease: 365290
    Main scripts: 3
    Lazy Loaded Scripts: 895
    Chunks:
     * Main Files: 12668
     * Scripts: 9288
    Strings: 0
    Routes: 694
    Experiments Count: 380

--- chunks/745735.js
+++ chunks/745735.js
@@ -3,7 +3,8 @@ var i = n(192379),
     r = n(512722),
     a = n.n(r);
 function s(e, t) {
-    let n = (0, i.useRef)(e);
+    let n = (0, i.useRef)(e),
+        r = (0, i.useRef)(null);
     (0, i.useEffect)(() => {
         n.current = e;
     }, [e]),
@@ -11,8 +12,10 @@ function s(e, t) {
             function e() {
                 a()(null != n.current, 'Missing callback'), n.current();
             }
-            if (null === t) return;
-            let i = setInterval(e, t);
-            return () => clearInterval(i);
+            if (null === t) {
+                null !== r.current && (clearInterval(r.current), (r.current = null));
+                return;
+            }
+            return (r.current = setInterval(e, t)), () => clearInterval(r.current);
         }, [t]);
 }
