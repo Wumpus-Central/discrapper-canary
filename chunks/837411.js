8ce70b279e30c96a78042f198f09520b2341df3e
DiscrapperManager <admin@minerpl.xyz>
2025-02-04 23:23:50 +0000

    canaryRelease: 365417
    Main scripts: 3
    Lazy Loaded Scripts: 897
    Chunks:
     * Main Files: 12670
     * Scripts: 9289
    Strings: 0
    Routes: 694
    Experiments Count: 378

--- chunks/837411.js
+++ chunks/837411.js
@@ -1,13 +1,14 @@
-n.d(t, { Z: () => u });
+n.d(t, { Z: () => c });
 var i = n(442837),
-    r = n(41776),
-    a = n(695346),
-    s = n(314897),
-    o = n(271383),
-    l = n(699516);
-function u(e, t) {
-    let n = (0, i.e7)([s.default], () => s.default.getId() === e),
-        u = (0, i.e7)([r.Z], () => null != t && r.Z.isLurking(t)),
-        c = a.h2.useSetting();
-    return (0, i.e7)([l.Z, o.ZP], () => !n && !u && (l.Z.isFriend(e) || null != o.ZP.memberOf(e).find((e) => !c.includes(e))));
+    r = n(91896),
+    a = n(41776),
+    s = n(695346),
+    o = n(314897),
+    l = n(271383),
+    u = n(699516);
+function c(e, t) {
+    let n = (0, i.e7)([o.default], () => o.default.getId() === e),
+        c = (0, i.e7)([a.Z], () => null != t && a.Z.isLurking(t)),
+        d = s.h2.useSetting();
+    return (0, i.e7)([u.Z, l.ZP, r.Z], () => (!n && !c && (u.Z.isFriend(e) || null != l.ZP.memberOf(e).find((e) => !d.includes(e)))) || (r.Z.getGameFriendsForUser(e).length > 0 && s._j.getSetting()));
 }
