2d95a0a21025b83e6b9e879db85347c118342dd2
DiscrapperManager <admin@minerpl.xyz>
2025-02-04 20:34:21 +0000

    canaryRelease: 365254
    Main scripts: 3
    Lazy Loaded Scripts: 895
    Chunks:
     * Main Files: 12668
     * Scripts: 9288
    Strings: 0
    Routes: 694
    Experiments Count: 380

--- chunks/766186.js
+++ chunks/766186.js
@@ -1,27 +1,34 @@
-n.d(t, { s: () => s });
+n.d(t, { s: () => l });
 var i = n(280551),
     r = n(706454),
     a = n(388032);
-function s(e) {
-    let t;
-    let n = r.default.locale;
+function s(e, t) {
     try {
-        t = Intl.DateTimeFormat(n, e).format;
-    } catch (n) {
-        t = Intl.DateTimeFormat(void 0, e).format;
+        return Intl.DateTimeFormat(e, t).format;
+    } catch (e) {
+        return Intl.DateTimeFormat(void 0, t).format;
     }
-    if (null != i.s)
-        try {
-            let r = n === a.initialLocale,
-                s = null != n && n.split('-')[0] === (null === a.systemLocale || void 0 === a.systemLocale ? void 0 : a.systemLocale.split('-')[0]),
-                o = (0, i.s)(r && s ? void 0 : n, e);
-            if (null != o)
-                return function (e) {
-                    try {
-                        return o(e);
-                    } catch (e) {}
-                    return t(e);
-                };
-        } catch (e) {}
-    return t;
+}
+function o(e, t) {
+    if (null == i.s) return null;
+    try {
+        let n = e === a.initialLocale,
+            r = null != e && e.split('-')[0] === (null === a.systemLocale || void 0 === a.systemLocale ? void 0 : a.systemLocale.split('-')[0]),
+            o = (0, i.s)(n && r ? void 0 : e, t);
+        if (null == o) return null;
+        let l = null;
+        return function (n) {
+            try {
+                return o(n);
+            } catch (e) {}
+            return null == l && (l = s(e, t)), l(n);
+        };
+    } catch (e) {
+        return null;
+    }
+}
+function l(e) {
+    let t = r.default.locale,
+        n = o(t, e);
+    return null != n ? n : s(t, e);
 }
