ef7b72830f6680950f7a98950ac8e424ca692cfc
DiscrapperManager <admin@minerpl.xyz>
2025-02-04 22:26:01 +0000

    canaryRelease: 365363
    Main scripts: 3
    Lazy Loaded Scripts: 895
    Chunks:
     * Main Files: 12668
     * Scripts: 9286
    Strings: 0
    Routes: 694
    Experiments Count: 378

--- chunks/655678.js
+++ chunks/655678.js
@@ -1,60 +1,64 @@
-n.d(t, { Z: () => b }), n(47120);
+n.d(t, { Z: () => S }), n(47120);
 var i = n(200651),
     r = n(192379),
     a = n(120356),
     s = n.n(a),
-    o = n(704215),
-    l = n(481060),
-    u = n(540059),
-    c = n(243778),
-    d = n(328908),
-    f = n(992970),
-    _ = n(13132),
-    p = n(576645),
-    h = n(317951),
-    m = n(443603),
-    g = n(981631),
-    E = n(957825),
-    v = n(388032),
-    y = n(553796);
-function I(e, t) {
-    let { disabled: n, channelId: a } = e,
-        I = (0, u.Q3)('ChannelMessageConfettiButton'),
-        b = (0, d.LN)(a),
-        { entitlement: T } = (0, p.t6)(h.D1),
-        S = r.useCallback(() => {
-            (0, d.Rg)(a, f.MG.MessageComposerButtons);
-        }, [a]),
-        A = (null == T ? void 0 : T.type) === g.qc2.DEVELOPER_GIFT ? [o.z.CONFETTI_POTION_UPSELL] : [],
-        [N, C] = (0, c.US)(A);
+    o = n(442837),
+    l = n(704215),
+    u = n(481060),
+    c = n(540059),
+    d = n(243778),
+    f = n(328908),
+    _ = n(992970),
+    p = n(13132),
+    h = n(576645),
+    m = n(317951),
+    g = n(819640),
+    E = n(443603),
+    v = n(981631),
+    y = n(957825),
+    I = n(388032),
+    b = n(553796);
+function T(e, t) {
+    let { disabled: n, channel: a } = e,
+        T = (0, c.Q3)('ChannelMessageConfettiButton'),
+        S = (0, f.LN)(a.id),
+        { entitlement: A } = (0, h.t6)(m.D1),
+        N = (0, o.e7)([g.Z], () => g.Z.hasLayers()),
+        C = r.useCallback(() => {
+            (0, f.Rg)(a.id, _.MG.MessageComposerButtons);
+        }, [a.id]),
+        R = a.isThread(),
+        O = r.useMemo(() => !R && !N, [R, N]) && (null == A ? void 0 : A.type) === v.qc2.DEVELOPER_GIFT ? [l.z.CONFETTI_POTION_UPSELL] : [],
+        [D, x] = (0, d.US)(O);
     return n
         ? null
-        : (0, i.jsx)(l.yRy, {
+        : (0, i.jsx)(u.yRy, {
               renderPopout: () =>
-                  (0, i.jsx)(_.Z, {
-                      channelId: a,
-                      dismissContent: C
+                  (0, i.jsx)(p.Z, {
+                      channelId: a.id,
+                      dismissContent: x
                   }),
               align: 'center',
               position: 'top',
-              shouldShow: N === o.z.CONFETTI_POTION_UPSELL,
+              shouldShow: D === l.z.CONFETTI_POTION_UPSELL,
               children: () =>
                   (0, i.jsx)('div', {
-                      className: s()(E.CT, y.buttonContainer),
+                      className: s()(y.CT, b.buttonContainer),
                       ref: t,
-                      children: (0, i.jsx)(m.Z, {
-                          innerClassName: s()(y.button, y.messageConfettiButton),
-                          childClassName: s()(y.messageConfettiIcon, { [y.active]: b }),
-                          onClick: S,
-                          isActive: b,
-                          'aria-label': v.intl.string(v.t.yvN8bG),
+                      children: (0, i.jsx)(E.Z, {
+                          innerClassName: s()(b.button, b.messageConfettiButton),
+                          childClassName: s()(b.messageConfettiIcon, { [b.active]: S }),
+                          onClick: C,
+                          isActive: S,
+                          'aria-label': I.intl.string(I.t.yvN8bG),
                           'aria-haspopup': 'false',
-                          children: (0, i.jsx)(l.l22, {
-                              size: I ? 'refresh_sm' : void 0,
+                          children: (0, i.jsx)(u.l22, {
+                              size: T ? 'refresh_sm' : void 0,
                               color: 'currentColor'
                           })
                       })
                   })
           });
 }
-let b = r.memo(r.forwardRef(I));
+let S = r.memo(r.forwardRef(T));
