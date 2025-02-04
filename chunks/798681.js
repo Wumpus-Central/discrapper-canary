c2849a161a3b8affbaa1a3cf02b889db893e005c
DiscrapperManager <admin@minerpl.xyz>
2025-02-04 18:16:39 +0000

    canaryRelease: 365162
    Main scripts: 3
    Lazy Loaded Scripts: 895
    Chunks:
     * Main Files: 12667
     * Scripts: 9284
    Strings: 0
    Routes: 694
    Experiments Count: 380

--- chunks/798681.js
+++ chunks/798681.js
@@ -1,16 +1,15 @@
 n.d(t, {
-    e: () => f,
-    w: () => p
+    e: () => d,
+    w: () => _
 });
 var i = n(47770),
     r = n(846519),
     a = n(570140),
     s = n(710845),
     o = n(646047),
-    l = n(425497),
-    u = n(70956),
-    c = n(27414);
-function d(e, t, n) {
+    l = n(70956),
+    u = n(27414);
+function c(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -23,22 +22,12 @@ function d(e, t, n) {
         e
     );
 }
-var f = (function (e) {
+var d = (function (e) {
     return (e.WindowVisibilityChanged = 'window-visibility-changed'), (e.IncomingVideoEnabledChanged = 'incoming-video-enabled-changed'), e;
 })({});
-class _ extends i.Z {
+class f extends i.Z {
     isIncomingVideoEnabled() {
         return this.incomingVideoEnabled;
-    }
-    update() {
-        this.discordVisible !== (0, c.Z)() &&
-            ((this.discordVisible = (0, c.Z)()),
-            this.emit('window-visibility-changed', this.discordVisible),
-            this.discordVisible
-                ? (this.disableVideoTimer.stop(), this.setIncomingVideoEnabled(!0))
-                : this.disableVideoTimer.start(this.HIDDEN_WINDOW_DISABLE_VIDEO_DURATION_MS, () => {
-                      this.setIncomingVideoEnabled(!1);
-                  }));
     }
     setIncomingVideoEnabled(e) {
         let t = this.incomingVideoEnabled !== e;
@@ -46,23 +35,24 @@ class _ extends i.Z {
     }
     constructor() {
         super(),
-            d(this, 'disableVideoTimer', new r.V7()),
-            d(this, 'discordVisible', !0),
-            d(this, 'incomingVideoEnabled', !0),
-            d(this, 'logger', new s.Z('WindowVisibilityVideoManager')),
-            d(this, 'HIDDEN_WINDOW_DISABLE_VIDEO_DURATION_MS', 30 * u.Z.Millis.SECOND),
-            d(this, 'handleWindowVisibilityChange', () => {
-                this.update();
+            c(this, 'disableVideoTimer', new r.V7()),
+            c(this, 'discordVisible', !0),
+            c(this, 'incomingVideoEnabled', !0),
+            c(this, 'logger', new s.Z('WindowVisibilityVideoManager')),
+            c(this, 'HIDDEN_WINDOW_DISABLE_VIDEO_DURATION_MS', 30 * l.Z.Millis.SECOND),
+            c(this, 'update', () => {
+                this.discordVisible !== (0, u.Z)() &&
+                    ((this.discordVisible = (0, u.Z)()),
+                    this.emit('window-visibility-changed', this.discordVisible),
+                    this.discordVisible
+                        ? (this.disableVideoTimer.stop(), this.setIncomingVideoEnabled(!0))
+                        : this.disableVideoTimer.start(this.HIDDEN_WINDOW_DISABLE_VIDEO_DURATION_MS, () => {
+                              this.setIncomingVideoEnabled(!1);
+                          }));
             }),
-            d(this, 'handleAppStateUpdate', () => {
-                l.Z.stopVideoWhenHidden() && this.update();
-            }),
-            d(this, 'handlePipModeChanged', () => {
-                l.Z.stopVideoWhenHidden() && this.update();
-            }),
-            a.Z.subscribe('WINDOW_VISIBILITY_CHANGE', this.handleWindowVisibilityChange),
-            a.Z.subscribe('APP_STATE_UPDATE', this.handleAppStateUpdate),
-            o.Z.addOnPipModeChangedListener(this.handlePipModeChanged);
+            a.Z.subscribe('WINDOW_VISIBILITY_CHANGE', this.update),
+            a.Z.subscribe('APP_STATE_UPDATE', this.update),
+            o.Z.addOnPipModeChangedListener(this.update);
     }
 }
-let p = new _();
+let _ = new f();
