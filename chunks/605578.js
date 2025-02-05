5ac1d425c26bdbc42a1278b5d671bfd9d3a5dc5a
DiscrapperManager <admin@minerpl.xyz>
2025-02-05 00:08:54 +0000

    canaryRelease: 365447
    Main scripts: 3
    Lazy Loaded Scripts: 897
    Chunks:
     * Main Files: 12670
     * Scripts: 9291
    Strings: 0
    Routes: 694
    Experiments Count: 378

--- chunks/605578.js
+++ chunks/605578.js
@@ -3,36 +3,39 @@ var i = n(200651),
     r = n(192379),
     a = n(1561),
     s = n(104611);
-function o(e) {
-    let { tag: t = 'div', children: n, className: o, style: l, focusProps: u, onClick: c, onDoubleClick: d, onContextMenu: f, onKeyDown: _, onFocus: p, onBlur: h, onMouseOver: m, onMouseDown: g, onMouseLeave: E, buttonProps: v, 'aria-label': y } = e,
-        I = r.useRef(null);
-    return (0, i.jsxs)(t, {
-        ref: I,
-        className: o,
-        style: l,
-        onClick: c,
-        onMouseOver: m,
-        onMouseLeave: E,
-        onDoubleClick: d,
-        onContextMenu: f,
-        children: [
-            (0, i.jsx)(a.P, {
-                ...v,
-                focusProps: {
-                    ringTarget: I,
-                    ...u
-                },
-                className: s.focusTarget,
-                'aria-label': y,
-                onClick: (e) => {
-                    e.stopPropagation(), null == c || c(e);
-                },
-                onKeyDown: _,
-                onFocus: p,
-                onBlur: h,
-                onMouseDown: g
-            }),
-            n
-        ]
-    });
-}
+let o = r.forwardRef(function (e, t) {
+    let { tag: n = 'div', children: o, className: l, style: u, focusProps: c, onClick: d, onDoubleClick: f, onContextMenu: _, onKeyDown: p, onFocus: h, onBlur: m, onMouseOver: g, onMouseDown: E, onMouseLeave: v, buttonProps: y, 'aria-label': I } = e,
+        b = r.useRef(null);
+    return (
+        r.useImperativeHandle(t, () => b.current),
+        (0, i.jsxs)(n, {
+            ref: b,
+            className: l,
+            style: u,
+            onClick: d,
+            onMouseOver: g,
+            onMouseLeave: v,
+            onDoubleClick: f,
+            onContextMenu: _,
+            children: [
+                (0, i.jsx)(a.P, {
+                    ...y,
+                    focusProps: {
+                        ringTarget: b,
+                        ...c
+                    },
+                    className: s.focusTarget,
+                    'aria-label': I,
+                    onClick: (e) => {
+                        e.stopPropagation(), null == d || d(e);
+                    },
+                    onKeyDown: p,
+                    onFocus: h,
+                    onBlur: m,
+                    onMouseDown: E
+                }),
+                o
+            ]
+        })
+    );
+});
