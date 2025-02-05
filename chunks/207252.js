05c2c5bd16b642af5b6d0145d75ae21c4d5c65b0
DiscrapperManager <admin@minerpl.xyz>
2025-02-05 18:06:20 +0000

    canaryRelease: 365645
    Main scripts: 3
    Lazy Loaded Scripts: 897
    Chunks:
     * Main Files: 12670
     * Scripts: 9292
    Strings: 0
    Routes: 694
    Experiments Count: 379

--- chunks/207252.js
+++ chunks/207252.js
@@ -1,46 +1,46 @@
-i.d(t, { default: () => k });
-var n = i(200651),
-    c = i(192379),
-    s = i(442837),
-    l = i(780384),
-    a = i(481060),
-    r = i(313201),
-    d = i(210887),
-    o = i(450269),
-    h = i(388032),
-    x = i(42129),
-    u = i(112847),
-    m = i(516954);
+c.d(t, { default: () => k });
+var i = c(200651),
+    n = c(192379),
+    l = c(442837),
+    s = c(780384),
+    r = c(481060),
+    o = c(313201),
+    a = c(210887),
+    d = c(450269),
+    h = c(388032),
+    x = c(42129),
+    u = c(112847),
+    m = c(516954);
 function k(e) {
-    let { guildId: t, transitionState: i, onClose: k, onConfirm: _ } = e,
-        C = (0, r.Dt)(),
-        b = (0, s.e7)([d.Z], () => d.Z.theme),
-        g = (0, l.wj)(b) ? u : m,
-        { rules: j, rulesAccepted: p } = (0, o.V)(t),
-        y = c.useCallback(() => {
+    let { guildId: t, transitionState: c, onClose: k, onConfirm: _ } = e,
+        C = (0, o.Dt)(),
+        b = (0, l.e7)([a.Z], () => a.Z.theme),
+        g = (0, s.wj)(b) ? u : m,
+        { rules: j, rulesAccepted: p } = (0, d.V)(t),
+        y = n.useCallback(() => {
             k(), _();
         }, [k, _]);
-    return (0, n.jsxs)(a.Y0X, {
-        transitionState: i,
+    return (0, i.jsxs)(r.Y0X, {
+        transitionState: c,
         'aria-labelledby': C,
         className: x.modal,
         children: [
-            (0, n.jsx)(a.olH, {
+            (0, i.jsx)(r.olH, {
                 onClick: k,
                 className: x.close
             }),
-            (0, n.jsxs)(a.hzk, {
+            (0, i.jsxs)(r.hzk, {
                 className: x.content,
                 children: [
-                    (0, n.jsxs)('div', {
+                    (0, i.jsxs)('div', {
                         className: x.header,
                         children: [
-                            (0, n.jsx)('img', {
+                            (0, i.jsx)('img', {
                                 alt: '',
                                 src: g,
                                 width: 80
                             }),
-                            (0, n.jsx)(a.X6q, {
+                            (0, i.jsx)(r.X6q, {
                                 variant: 'heading-xxl/semibold',
                                 color: 'header-primary',
                                 id: C,
@@ -48,30 +48,30 @@ function k(e) {
                             })
                         ]
                     }),
-                    (0, n.jsx)(a.Text, {
+                    (0, i.jsx)(r.Text, {
                         variant: 'text-md/medium',
                         color: 'header-secondary',
                         className: x.centeredText,
                         children: h.intl.string(h.t['WT+829'])
                     }),
-                    (0, n.jsx)('div', {
+                    (0, i.jsx)('div', {
                         className: x.checkboxContainer,
                         children: j.map((e) =>
-                            (0, n.jsxs)(
-                                a.XZJ,
+                            (0, i.jsxs)(
+                                r.XZJ,
                                 {
                                     className: x.checkbox,
                                     reverse: !0,
                                     value: e.checked,
-                                    align: a.XZJ.Aligns.CENTER,
+                                    align: r.XZJ.Aligns.CENTER,
                                     onChange: e.onCheck,
                                     children: [
-                                        (0, n.jsx)(a.Text, {
+                                        (0, i.jsx)(r.Text, {
                                             variant: 'text-md/semibold',
                                             color: 'header-primary',
                                             children: e.title
                                         }),
-                                        (0, n.jsx)(a.Text, {
+                                        (0, i.jsx)(r.Text, {
                                             variant: 'text-sm/medium',
                                             color: 'text-secondary',
                                             children: e.body
@@ -84,15 +84,16 @@ function k(e) {
                     })
                 ]
             }),
-            (0, n.jsx)(a.mzw, {
-                children: (0, n.jsx)(a.zxk, {
+            (0, i.jsx)(r.mzw, {
+                children: (0, i.jsx)(r.zxk, {
                     fullWidth: !0,
                     disabled: !p,
-                    color: a.zxk.Colors.BRAND,
+                    color: r.zxk.Colors.BRAND,
                     onClick: y,
-                    size: a.zxk.Sizes.LARGE,
-                    children: (0, n.jsx)(a.Text, {
+                    size: r.zxk.Sizes.LARGE,
+                    children: (0, i.jsx)(r.Text, {
                         variant: 'text-sm/semibold',
+                        color: 'currentColor',
                         children: h.intl.string(h.t['qjtt/v'])
                     })
                 })
