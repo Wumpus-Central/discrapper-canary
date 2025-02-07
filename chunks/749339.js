9424c9a03527dd5a5c6edef113fd2d6721e83d53
DiscrapperManager <admin@minerpl.xyz>
2025-02-07 23:39:43 +0000

    canaryRelease: 366862
    Main scripts: 3
    Lazy Loaded Scripts: 904
    Chunks:
     * Main Files: 12673
     * Scripts: 9318
    Strings: 0
    Routes: 697
    Experiments Count: 387

--- chunks/749339.js
+++ chunks/749339.js
@@ -1,21 +1,19 @@
-n.d(t, { Z: () => u }), n(789020);
+n.d(t, { Z: () => d });
 var i = n(200651);
 n(192379);
 var l = n(481060),
     a = n(39154),
     s = n(292419),
     r = n(572004),
-    o = n(630388),
-    d = n(981631),
-    c = n(388032);
-function u(e) {
+    o = n(388032);
+function d(e) {
     return r.wS
         ? (0, i.jsx)(l.sNh, {
               id: 'copy-text',
-              label: c.intl.string(c.t.JrGD7O),
+              label: o.intl.string(o.t.JrGD7O),
               icon: l.TIy,
               action: () => {
-                  if (((e = (0, a.Z)(e)), (0, o.yE)(e.flags, d.iLy.IS_UIKIT_COMPONENTS))) {
+                  if ((e = (0, a.Z)(e)).isComponentsV2()) {
                       let t = (0, s.np)(e.components);
                       null != t && (0, r.JG)(t);
                   } else (0, r.JG)(e.content);
