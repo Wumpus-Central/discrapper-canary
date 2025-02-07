972f33c4576398c1f81e9c4a1b712463995872f8
DiscrapperManager <admin@minerpl.xyz>
2025-02-07 19:59:53 +0000

    canaryRelease: 366666
    Main scripts: 3
    Lazy Loaded Scripts: 902
    Chunks:
     * Main Files: 12670
     * Scripts: 9302
    Strings: 0
    Routes: 695
    Experiments Count: 383

---
+++ chunks/664344.js
@@ -0,0 +1,17 @@
+n.d(t, { Z: () => r });
+var i = n(987170),
+    l = n(987338);
+let r = (0, i.Z)({
+    kind: 'user',
+    id: '2025-02_test_user_auth_apply_filter_badges',
+    label: 'Test User Auth Apply Filter Badges',
+    defaultConfig: { enabled: !1 },
+    commonTriggerPoint: l.$P.CONNECTION_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: 'Variation with treatment enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
