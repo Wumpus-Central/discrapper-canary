cff843c0f9bcaaddf390a3c2e36244e467f944a6
DiscrapperManager <admin@minerpl.xyz>
2025-02-07 21:27:09 +0000

    canaryRelease: 366727
    Main scripts: 3
    Lazy Loaded Scripts: 903
    Chunks:
     * Main Files: 12670
     * Scripts: 9306
    Strings: 0
    Routes: 695
    Experiments Count: 386

---
+++ chunks/260263.js
@@ -0,0 +1,17 @@
+n.d(t, { G5: () => r });
+var i = n(818083),
+    l = n(987338);
+let r = (0, i.B)({
+    kind: 'user',
+    id: '2025-01_quick_launcher_mobile',
+    label: 'Quick Launcher - Mobile',
+    defaultConfig: { enabled: !1 },
+    commonTriggerPoint: l.$P.CONNECTION_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: 'Enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
