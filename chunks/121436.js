bc7e90d365b5adfedeb84e3b427dadecc195d176
DiscrapperManager <admin@minerpl.xyz>
2025-02-07 21:37:44 +0000

    canaryRelease: 366740
    Main scripts: 3
    Lazy Loaded Scripts: 903
    Chunks:
     * Main Files: 12670
     * Scripts: 9306
    Strings: 0
    Routes: 696
    Experiments Count: 386

--- chunks/121436.js
+++ chunks/121436.js
@@ -1,66 +1,67 @@
-s.d(a, { default: () => h });
-var t = s(200651);
-s(192379);
-var d = s(120356),
-    n = s.n(d),
-    i = s(793030),
-    r = s(481060),
-    c = s(100527),
-    l = s(906732),
-    _ = s(313201),
-    E = s(703656),
-    N = s(594174),
-    m = s(111361),
-    T = s(197115),
-    M = s(943180),
-    o = s(553896),
-    g = s(848572),
-    u = s(787528),
-    x = s(474936),
-    R = s(981631),
-    b = s(388032),
-    U = s(837005);
-let h = (e) => {
-    let { transitionState: a, onClose: s } = e,
+t.d(a, { default: () => I });
+var s = t(200651);
+t(192379);
+var d = t(120356),
+    n = t.n(d),
+    i = t(793030),
+    r = t(481060),
+    c = t(100527),
+    l = t(906732),
+    _ = t(313201),
+    E = t(703656),
+    N = t(594174),
+    m = t(63063),
+    T = t(111361),
+    M = t(197115),
+    o = t(943180),
+    g = t(553896),
+    R = t(848572),
+    u = t(787528),
+    x = t(474936),
+    b = t(981631),
+    U = t(388032),
+    h = t(837005);
+let I = (e) => {
+    let { transitionState: a, onClose: t } = e,
         { analyticsLocations: d } = (0, l.ZP)(c.Z.TIERED_TENURE_BADGES_MODAL),
-        h = (0, _.Dt)(),
-        I = (0, g.j_)(),
-        v = (0, g.Rw)(),
-        j = N.default.getCurrentUser(),
-        O = (0, m.yd)(null == j ? void 0 : j.premiumType, x.p9.TIER_2),
-        { version: P } = (0, M.D)(),
-        H = I.map((e) => {
-            let { id: a, badgeVersion: s, nameUnformatted: d, tenureReqNumMonths: c } = e,
-                l = b.intl.string(d),
+        I = (0, _.Dt)(),
+        v = (0, R.j_)(),
+        j = (0, R.Rw)(),
+        O = N.default.getCurrentUser(),
+        P = (0, T.yd)(null == O ? void 0 : O.premiumType, x.p9.TIER_2),
+        { version: H } = (0, o.D)(),
+        p = v.map((e) => {
+            let { id: a, badgeVersion: t, nameUnformatted: d, tenureReqNumMonths: c } = e,
+                l = U.intl.string(d),
                 _ = (0, u.J)(a),
-                E = (0, o.q)(a, c),
-                N = (null == v ? void 0 : v.id) === a && 2 === s;
-            return (0, t.jsxs)(
+                E = (0, g.q)(a, c),
+                N = (null == j ? void 0 : j.id) === a && 2 === t;
+            return (0, s.jsxs)(
                 'div',
                 {
-                    className: n()(U.badge, {
-                        [U.badgeV2]: 2 === s,
-                        [U.earnedBadge]: N
+                    className: n()(h.badge, {
+                        [h.badgeV2]: 2 === t,
+                        [h.earnedBadge]: N
                     }),
                     children: [
-                        (0, t.jsx)('img', {
+                        (0, s.jsx)('img', {
                             src: _,
                             alt: l,
-                            className: U.badgeImage
+                            className: h.badgeImage
                         }),
-                        (0, t.jsxs)('div', {
-                            className: n()(U.badgeText, { [U.badgeTextV2]: 2 === s }),
+                        (0, s.jsxs)('div', {
+                            className: n()(h.badgeText, { [h.badgeTextV2]: 2 === t }),
                             children: [
-                                (0, t.jsx)(i.xv, {
+                                (0, s.jsx)(i.xv, {
                                     variant: 'text-md/semibold',
-                                    className: U.badgeName,
+                                    className: h.badgeName,
                                     children: l
                                 }),
-                                (0, t.jsx)(i.xv, {
+                                (0, s.jsx)(i.xv, {
                                     variant: 'text-xs/normal',
                                     children: E
                                 }),
-                                N && (0, t.jsx)(r.kmB, {})
+                                N && (0, s.jsx)(r.kmB, {})
                             ]
                         })
                     ]
@@ -68,88 +69,89 @@ let h = (e) => {
                 a
             );
         }),
-        p = (0, t.jsxs)(t.Fragment, {
+        B = (0, s.jsxs)(s.Fragment, {
             children: [
-                (0, t.jsx)(i.xv, {
+                (0, s.jsx)(i.xv, {
                     variant: 'display-md',
-                    className: U.header,
-                    children: b.intl.string(b.t['rmUN7+'])
+                    className: h.header,
+                    children: U.intl.string(U.t['rmUN7+'])
                 }),
-                (0, t.jsx)(i.xv, {
+                (0, s.jsx)(i.xv, {
                     variant: 'text-md/medium',
-                    className: U.subHeader,
-                    children: b.intl.string(b.t.OI50am)
+                    className: h.subHeader,
+                    children: U.intl.string(U.t.OI50am)
                 }),
-                (0, t.jsx)('div', {
-                    className: U.badgeList,
-                    children: H
+                (0, s.jsx)('div', {
+                    className: h.badgeList,
+                    children: p
                 })
             ]
         }),
-        f = O
-            ? (0, t.jsxs)(t.Fragment, {
+        f = P
+            ? (0, s.jsxs)(s.Fragment, {
                   children: [
-                      (0, t.jsx)(i.xv, {
+                      (0, s.jsx)(i.xv, {
                           variant: 'display-md',
-                          className: U.header,
-                          children: b.intl.string(b.t.Og62j4)
+                          className: h.header,
+                          children: U.intl.string(U.t.Og62j4)
                       }),
-                      (0, t.jsx)(i.xv, {
+                      (0, s.jsx)(i.xv, {
                           variant: 'text-md/medium',
-                          className: U.subHeader,
-                          children: b.intl.string(b.t['IdAP9/'])
+                          className: h.subHeader,
+                          children: U.intl.string(U.t['IdAP9/'])
                       }),
-                      (0, t.jsx)('div', {
-                          className: U.badgeList,
-                          children: H
+                      (0, s.jsx)('div', {
+                          className: h.badgeList,
+                          children: p
                       })
                   ]
               })
-            : (0, t.jsxs)(t.Fragment, {
+            : (0, s.jsxs)(s.Fragment, {
                   children: [
-                      (0, t.jsx)(i.xv, {
+                      (0, s.jsx)(i.xv, {
                           variant: 'display-md',
-                          className: U.header,
-                          children: b.intl.string(b.t.RtGeFR)
+                          className: h.header,
+                          children: U.intl.string(U.t.RtGeFR)
                       }),
-                      (0, t.jsx)(i.xv, {
+                      (0, s.jsx)(i.xv, {
                           variant: 'text-md/medium',
-                          className: U.subHeader,
-                          children: b.intl.format(b.t.ffatnZ, {
+                          className: h.subHeader,
+                          children: U.intl.format(U.t.ffatnZ, {
                               onClick: () => {
-                                  (0, E.uL)(R.Z5c.APPLICATION_STORE, {
+                                  (0, E.uL)(b.Z5c.APPLICATION_STORE, {
                                       source: c.Z.TIERED_TENURE_BADGES_MODAL,
                                       sourceLocationStack: d
                                   }),
-                                      null == s || s();
-                              }
+                                      null == t || t();
+                              },
+                              helpdeskArticle: m.Z.getArticleURL(b.BhN.TIERED_TENURE_BADGES)
                           })
                       }),
-                      (0, t.jsx)('div', {
-                          className: U.badgeList,
-                          children: H
+                      (0, s.jsx)('div', {
+                          className: h.badgeList,
+                          children: p
                       }),
-                      (0, t.jsx)(T.Z, {
-                          className: U.subscribeButton,
-                          premiumModalAnalyticsLocation: { page: R.ZY5.TIERED_TENURE_BADGES_MODAL },
-                          buttonText: b.intl.string(b.t.JST6jo),
+                      (0, s.jsx)(M.Z, {
+                          className: h.subscribeButton,
+                          premiumModalAnalyticsLocation: { page: b.ZY5.TIERED_TENURE_BADGES_MODAL },
+                          buttonText: U.intl.string(U.t.JST6jo),
                           subscriptionTier: x.Si.TIER_2,
                           showGradient: !0
                       })
                   ]
               });
-    return (0, t.jsxs)(r.Y0X, {
+    return (0, s.jsxs)(r.Y0X, {
         transitionState: a,
-        'aria-labelledby': h,
-        className: n()(U.container, 'theme-dark'),
+        'aria-labelledby': I,
+        className: n()(h.container, 'theme-dark'),
         children: [
-            (0, t.jsx)(r.olH, {
-                onClick: s,
-                className: U.closeButton
+            (0, s.jsx)(r.olH, {
+                onClick: t,
+                className: h.closeButton
             }),
-            (0, t.jsx)(r.hzk, {
-                className: U.content,
-                children: 2 === P ? f : p
+            (0, s.jsx)(r.hzk, {
+                className: h.content,
+                children: 2 === H ? f : B
             })
         ]
     });
