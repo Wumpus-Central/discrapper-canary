4991ae0228ce7a7c65ac584e2c08eda27233bcd6
DiscrapperManager <admin@minerpl.xyz>
2025-02-10 21:15:05 +0000

    canaryRelease: 367267
    Main scripts: 3
    Lazy Loaded Scripts: 921
    Chunks:
     * Main Files: 12141
     * Scripts: 9865
    Strings: 0
    Routes: 698
    Experiments Count: 391

--- chunks/162461.js
+++ chunks/162461.js
@@ -1,19 +1,12 @@
 n.d(t, {
-    DG: () => l,
-    m6: () => p,
-    mN: () => _,
-    nP: () => f,
-    sA: () => u,
-    wm: () => c
+    DG: () => a,
+    nP: () => u,
+    sA: () => s,
+    wm: () => o
 });
 var i = n(818083),
-    r = n(109911),
-    a = n(70956),
-    s = n(987338),
-    o = (function (e) {
-        return (e[(e.THREE_DAYS = 3 * a.Z.Millis.DAY)] = 'THREE_DAYS'), (e[(e.SIXTEEN_HOURS = 16 * a.Z.Millis.HOUR)] = 'SIXTEEN_HOURS'), e;
-    })(o || {});
-let l = (0, i.B)({
+    r = n(109911);
+let a = (0, i.B)({
     kind: 'user',
     id: '2024-03_content_inventory_memberlist_and_ranker',
     label: 'Enables the memberlist content feed',
@@ -34,16 +27,16 @@ let l = (0, i.B)({
         }
     ]
 });
-function u(e) {
-    let { enabled: t } = l.getCurrentConfig({ location: e }, { autoTrackExposure: !0 }),
+function s(e) {
+    let { enabled: t } = a.getCurrentConfig({ location: e }, { autoTrackExposure: !0 }),
         n = (0, r.Ji)(e, !1);
     return t || n;
 }
-function c(e) {
-    let { enabled: t, impressionCappingEnabled: n } = l.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
+function o(e) {
+    let { enabled: t, impressionCappingEnabled: n } = a.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
     return t && !0 === n;
 }
-let d = (0, i.B)({
+let l = (0, i.B)({
     kind: 'user',
     id: '2024-08_content_inventory_analytics_sampling',
     label: 'Content Inventory Analytics Sampling',
@@ -56,28 +49,6 @@ let d = (0, i.B)({
         }
     ]
 });
-function f(e) {
-    return d.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
-}
-let _ = (0, i.B)({
-    kind: 'user',
-    id: '2024-12_hotwheels_activity_feed_inventory_thresholds',
-    label: 'Hotwheels Activity Feed (Inventory Options)',
-    commonTriggerPoint: s.$P.CONNECTION_OPEN,
-    defaultConfig: { impressionAge: o.SIXTEEN_HOURS },
-    treatments: [
-        {
-            id: 11,
-            label: 'Relaxed impression capping. (16 hours -> 3 days)',
-            config: { impressionAge: o.THREE_DAYS }
-        },
-        {
-            id: 12,
-            label: 'All previous treatments combined.',
-            config: { impressionAge: o.THREE_DAYS }
-        }
-    ]
-});
-function p(e) {
-    return _.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
+function u(e) {
+    return l.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
 }
