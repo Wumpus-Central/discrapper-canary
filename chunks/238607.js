8425d56597a0814d0b6b95be8898a9dcb1a8852c
DiscrapperManager <admin@minerpl.xyz>
2025-02-10 17:42:54 +0000

    canaryRelease: 367092
    Main scripts: 3
    Lazy Loaded Scripts: 921
    Chunks:
     * Main Files: 12137
     * Scripts: 9861
    Strings: 0
    Routes: 698
    Experiments Count: 387

--- chunks/238607.js
+++ chunks/238607.js
@@ -1,47 +1,85 @@
-n.d(t, { Z: () => o });
+n.d(t, { Z: () => v });
 var i = n(200651);
 n(192379);
-var r = n(481060),
-    a = n(103450),
-    s = n(475413);
-function o(e) {
-    let { user: t, activity: n, onAction: o, onClose: l } = e,
-        u = (0, a.e)({
+var r = n(442837),
+    a = n(481060),
+    s = n(40851),
+    o = n(841784),
+    l = n(906732),
+    u = n(499254),
+    c = n(827498),
+    d = n(397698),
+    f = n(541716),
+    _ = n(103450),
+    p = n(592125),
+    h = n(944486),
+    m = n(475413),
+    g = n(981631),
+    E = n(388032);
+function v(e) {
+    let { user: t, activity: n, onAction: v, onClose: y } = e,
+        { newestAnalyticsLocation: I } = (0, l.ZP)(),
+        T = (0, s.bp)() === g.IlC.POPOUT,
+        b = (0, r.e7)([p.Z, h.Z], () => p.Z.getChannel(h.Z.getChannelId())),
+        S = (0, _.e)({
             activity: null != n ? n : void 0,
             embeddedActivity: void 0,
             user: t,
-            onClose: l
+            onClose: y
         });
-    if (null == u) return null;
-    let { isJoining: c, handleJoinRequest: d, buttonCTA: f, tooltip: _, isEnabled: p, isEmbedded: h } = u;
-    return h
-        ? (0, i.jsx)(r.ua7, {
-              text: _,
+    if (null == S && null != n && (0, o.Z)(n))
+        return (0, i.jsx)(m.tG, {
+            icon: a.jje,
+            text: E.intl.string(E.t.RscU7O),
+            color: a.Ttl.BRAND,
+            themeColor: 'none',
+            fullWidth: !0,
+            onClick: (e) => {
+                e.stopPropagation(),
+                    null == y || y(),
+                    null == v || v({ action: 'PRESS_PLAY_BUTTON' }),
+                    (null == b ? void 0 : b.isVocal())
+                        ? (0, d.Z)({
+                              context: {
+                                  type: 'channel',
+                                  channel: b
+                              },
+                              analyticsLocation: I,
+                              openInPopout: T
+                          })
+                        : (0, u.__)(c._b.TEXT, f.Ie.NORMAL, { applicationId: n.application_id });
+            }
+        });
+    if (null == S) return null;
+    let { isJoining: A, handleJoinRequest: N, buttonCTA: C, tooltip: R, isEnabled: O, isEmbedded: D } = S;
+    return D
+        ? (0, i.jsx)(a.ua7, {
+              text: R,
               children: (e) =>
-                  (0, i.jsx)(s.tG, {
+                  (0, i.jsx)(m.tG, {
                       ...e,
-                      icon: r.YVR,
-                      text: f,
-                      disabled: !p,
-                      submitting: c,
+                      icon: a.YVR,
+                      text: C,
+                      disabled: !O,
+                      submitting: A,
                       fullWidth: !0,
                       onClick: (e) => {
-                          e.stopPropagation(), null == o || o({ action: 'PRESS_JOIN_BUTTON' }), d();
+                          e.stopPropagation(), null == v || v({ action: 'PRESS_JOIN_BUTTON' }), N();
                       }
                   })
           })
-        : (0, i.jsx)(r.ua7, {
-              text: _,
+        : (0, i.jsx)(a.ua7, {
+              text: R,
               children: (e) =>
-                  (0, i.jsx)(s.tG, {
+                  (0, i.jsx)(m.tG, {
                       ...e,
-                      icon: r.iWm,
-                      text: f,
-                      disabled: !p,
-                      submitting: c,
+                      icon: a.iWm,
+                      text: C,
+                      disabled: !O,
+                      submitting: A,
                       fullWidth: !0,
                       onClick: (e) => {
-                          e.stopPropagation(), null == o || o({ action: 'PRESS_ASK_TO_JOIN_BUTTON' }), d();
+                          e.stopPropagation(), null == v || v({ action: 'PRESS_ASK_TO_JOIN_BUTTON' }), N();
                       }
                   })
           });
