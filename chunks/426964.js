73b966033e449786e7a346b487a386ff164fec56
DiscrapperManager <admin@minerpl.xyz>
2025-02-10 18:31:23 +0000

    canaryRelease: 367136
    Main scripts: 3
    Lazy Loaded Scripts: 921
    Chunks:
     * Main Files: 12137
     * Scripts: 9861
    Strings: 0
    Routes: 698
    Experiments Count: 386

--- chunks/426964.js
+++ chunks/426964.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => D }), n(47120);
+n.d(t, { Z: () => C });
 var i = n(200651),
     r = n(192379),
     a = n(481060),
@@ -9,196 +9,133 @@ var i = n(200651),
     c = n(937111),
     d = n(741606),
     f = n(703656),
-    _ = n(698305),
-    p = n(314897),
-    h = n(271383),
-    m = n(594174),
-    g = n(51144),
-    E = n(970606),
-    v = n(645896),
-    y = n(905362),
-    I = n(353093),
-    T = n(574410),
-    b = n(900681),
-    S = n(284019),
-    A = n(308083),
-    N = n(981631),
-    C = n(388032),
-    R = n(652134);
-function O(e) {
+    _ = n(314897),
+    p = n(271383),
+    h = n(594174),
+    m = n(970606),
+    g = n(645896),
+    E = n(905362),
+    v = n(353093),
+    y = n(900681),
+    I = n(284019),
+    T = n(308083),
+    b = n(981631),
+    S = n(388032),
+    A = n(652134);
+function N(e) {
     e.stopPropagation();
 }
-let D = r.memo(function (e) {
-    var t, n;
-    let { clan: D, isLoading: L, onClose: x, profileViewedAnalytics: P, userId: w } = e,
-        M = null == D ? void 0 : D.identityGuildId,
-        k = (0, v.Cc)(null != M ? M : null),
-        U = null == D ? void 0 : D.tag,
-        { enabled: G } = T.w.useExperiment({ location: 'ClanGuildProfile' }, { autoTrackExposure: !1 }),
-        [B, Z] = r.useState(!1),
-        [F, V] = r.useState(''),
-        j = (0, s.e7)([m.default], () => m.default.getUser(w)),
-        { newestAnalyticsLocation: H } = (0, l.ZP)(),
-        Y = r.useMemo(
+let C = r.memo(function (e) {
+    var t;
+    let { clan: n, isLoading: C, onClose: R, profileViewedAnalytics: O, userId: D } = e,
+        L = null == n ? void 0 : n.identityGuildId,
+        x = (0, g.Cc)(null != L ? L : null),
+        P = null == n ? void 0 : n.tag,
+        { newestAnalyticsLocation: w } = (0, l.ZP)(),
+        M = r.useMemo(
             () => ({
-                tagUserId: w,
-                source: H,
-                ...P
+                tagUserId: D,
+                source: w,
+                ...O
             }),
-            [w, H, P]
+            [D, w, O]
         ),
-        W = (0, s.e7)([p.default], () => p.default.getId()),
-        K = (0, s.e7)([h.ZP], () => (null != M ? h.ZP.getMember(M, W) : null), [M, W]),
-        z = (0, s.e7)([c.Z], () => {
+        k = (0, s.e7)([_.default], () => _.default.getId()),
+        U = (0, s.e7)([p.ZP], () => (null != L ? p.ZP.getMember(L, k) : null), [L, k]),
+        G = (0, s.e7)([c.Z], () => {
             var e;
-            return (null == k ? void 0 : k.id) != null && (null === (e = c.Z.getRequest(k.id)) || void 0 === e ? void 0 : e.applicationStatus) === u.wB.SUBMITTED;
+            return (null == x ? void 0 : x.id) != null && (null === (e = c.Z.getRequest(x.id)) || void 0 === e ? void 0 : e.applicationStatus) === u.wB.SUBMITTED;
         }),
-        q = null != k && k.memberCount >= A.Du,
-        Q = (0, s.e7)([m.default], () => m.default.getUser(W), [W]),
-        X = (null == K ? void 0 : K.joinedAt) != null,
-        J = (0, I.Z4)(M, null == Q ? void 0 : Q.primaryGuild),
-        $ = null != (0, d.Z)(M);
+        B = null != x && x.memberCount >= T.Du,
+        Z = (0, s.e7)([h.default], () => h.default.getUser(k), [k]),
+        F = (null == U ? void 0 : U.joinedAt) != null,
+        V = (0, v.Z4)(L, null == Z ? void 0 : Z.primaryGuild),
+        j = null != (0, d.Z)(L);
     r.useEffect(() => {
-        null != M &&
-            (0, E.u0)({
-                guildId: M,
-                hasJoinRequest: $,
-                ...Y,
+        null != L &&
+            (0, m.u0)({
+                guildId: L,
+                hasJoinRequest: j,
+                ...M,
                 location: o.Z.CLAN_GUILD_PROFILE
             });
-    }, [M, $, Y]);
-    let ee = r.useCallback(() => {
-            (0, E.hM)(M, E.UE.ASK_TO_JOIN), Z(!B);
-        }, [M, B]),
-        et = r.useCallback(async () => {
-            if (null != j)
-                try {
-                    await (0, _.Z)({
-                        userId: j.id,
-                        content: F,
-                        openChannel: !0,
-                        location: 'guildProfilePopout'
-                    });
-                } catch (e) {}
-        }, [j, F]),
-        en = r.useCallback(
+    }, [L, j, M]);
+    let H = r.useCallback(
             (e) => {
-                null != M && (e.stopPropagation(), e.preventDefault(), null == x || x(), (0, E.hM)(M, E.UE.TAG_ADOPTION), (0, y.SJ)(M));
+                null != L && (e.stopPropagation(), e.preventDefault(), null == R || R(), (0, m.hM)(L, m.UE.TAG_ADOPTION), (0, E.SJ)(L));
             },
-            [M, x]
+            [L, R]
         ),
-        ei = r.useCallback(() => {
-            null != M && ((0, E.hM)(M, E.UE.APPLICATION), (0, f.uL)(N.Z5c.GUILD_MEMBER_VERIFICATION(M)));
-        }, [M]),
-        er = r.useCallback(() => {
-            null != M && ((0, E.hM)(M, E.UE.GUILD), (0, f.XU)(M));
-        }, [M]),
-        ea = r.useMemo(() => {
+        Y = r.useCallback(() => {
+            null != L && ((0, m.hM)(L, m.UE.APPLICATION), (0, f.uL)(b.Z5c.GUILD_MEMBER_VERIFICATION(L)));
+        }, [L]),
+        W = r.useCallback(() => {
+            null != L && ((0, m.hM)(L, m.UE.GUILD), (0, f.XU)(L));
+        }, [L]),
+        K = r.useMemo(() => {
             var e, t;
             return {
                 color: a.zxk.Colors.BRAND,
                 size: a.zxk.Sizes.SMALL,
                 fullWidth: !0,
-                className: R.button,
-                themeColor: null !== (t = null == k ? void 0 : null === (e = k.branding) || void 0 === e ? void 0 : e.primaryColor) && void 0 !== t ? t : null
+                className: A.button,
+                themeColor: null !== (t = null == x ? void 0 : null === (e = x.branding) || void 0 === e ? void 0 : e.primaryColor) && void 0 !== t ? t : null
             };
-        }, [null == k ? void 0 : null === (t = k.branding) || void 0 === t ? void 0 : t.primaryColor]),
-        es = r.useMemo(() => {
+        }, [null == x ? void 0 : null === (t = x.branding) || void 0 === t ? void 0 : t.primaryColor]),
+        z = r.useMemo(() => {
             var e;
             switch (!0) {
-                case !X && $:
-                    return (0, i.jsx)(S.Z, {
-                        onClick: ei,
-                        ...ea,
-                        children: C.intl.string(C.t.dXvKgY)
+                case !F && j:
+                    return (0, i.jsx)(I.Z, {
+                        onClick: Y,
+                        ...K,
+                        children: S.intl.string(S.t.dXvKgY)
                     });
-                case !X && (null == k ? void 0 : null === (e = k.discoveryProfileFeatures) || void 0 === e ? void 0 : e.has(N.oNc.CLAN_DISCOVERY_DISABLED)):
+                case !F && (null == x ? void 0 : null === (e = x.discoveryProfileFeatures) || void 0 === e ? void 0 : e.has(b.oNc.CLAN_DISCOVERY_DISABLED)):
                     return null;
-                case X && !J:
-                    return (0, i.jsx)(S.Z, {
-                        onClick: en,
-                        ...ea,
-                        children: C.intl.string(C.t['1keBy8'])
+                case F && !V:
+                    return (0, i.jsx)(I.Z, {
+                        onClick: H,
+                        ...K,
+                        children: S.intl.string(S.t['1keBy8'])
                     });
-                case X && J:
-                    return (0, i.jsx)(S.Z, {
-                        onClick: er,
-                        ...ea,
-                        children: C.intl.string(C.t.muzrLS)
+                case F && V:
+                    return (0, i.jsx)(I.Z, {
+                        onClick: W,
+                        ...K,
+                        children: S.intl.string(S.t.muzrLS)
                     });
                 default:
                     return null;
             }
-        }, [X, $, null == k ? void 0 : k.discoveryProfileFeatures, J, ei, ea, en, er]),
-        eo = r.useMemo(
+        }, [F, j, null == x ? void 0 : x.discoveryProfileFeatures, V, Y, K, H, W]),
+        q = r.useMemo(
             () => ({
-                className: R.container,
-                onClick: O
+                className: A.container,
+                onClick: N
             }),
             []
         );
-    if (null == U || null == k || null == M || L)
+    if (null == P || null == x || null == L || C)
         return (0, i.jsx)('div', {
-            ...eo,
+            ...q,
             children: (0, i.jsx)(a.$jN, {})
         });
-    let el = !X && null != j && !$ && G && !B,
-        eu = null != es || el,
-        ec = null !== (n = g.ZP.getName(j)) && void 0 !== n ? n : '';
-    return (0, i.jsxs)(i.Fragment, {
+    let Q = null != z;
+    return (0, i.jsxs)('div', {
+        ...q,
         children: [
-            (0, i.jsxs)('div', {
-                ...eo,
-                children: [
-                    (0, i.jsx)(b.x, {
-                        clan: k,
-                        className: R.cardContainer,
-                        isMember: X,
-                        hasPendingJoinRequest: z,
-                        atMaxMemberCapacity: q
-                    }),
-                    eu &&
-                        (0, i.jsxs)('div', {
-                            className: R.buttonGroupContainer,
-                            children: [
-                                null != es && es,
-                                el &&
-                                    (0, i.jsx)(S.Z, {
-                                        onClick: ee,
-                                        ...ea,
-                                        children: C.intl.string(C.t.HzLAb2)
-                                    })
-                            ]
-                        })
-                ]
+            (0, i.jsx)(y.x, {
+                clan: x,
+                className: A.cardContainer,
+                isMember: F,
+                hasPendingJoinRequest: G,
+                atMaxMemberCapacity: B
             }),
-            B &&
-                (0, i.jsxs)(a.VqE, {
-                    className: R.askToJoinInputWrapper,
-                    children: [
-                        (0, i.jsx)(a.Text, {
-                            color: 'interactive-normal',
-                            variant: 'text-xs/normal',
-                            children: C.intl.format(C.t.k5d4Wl, { username: ec })
-                        }),
-                        (0, i.jsxs)('div', {
-                            className: R.inputRow,
-                            children: [
-                                (0, i.jsx)(a.oil, {
-                                    inputClassName: R.input,
-                                    value: F,
-                                    onChange: V,
-                                    placeholder: C.intl.formatToPlainString(C.t['4c+CAw'], { channel: ec })
-                                }),
-                                (0, i.jsx)(a.zxk, {
-                                    className: R.sendButton,
-                                    color: a.zxk.Colors.BRAND,
-                                    onClick: et,
-                                    children: C.intl.string(C.t.TXNS7e)
-                                })
-                            ]
-                        })
-                    ]
+            Q &&
+                (0, i.jsx)('div', {
+                    className: A.buttonGroupContainer,
+                    children: null != z && z
                 })
         ]
     });
--- chunks/652134.js
+++ chunks/652134.js
@@ -2,10 +2,6 @@ var i = {
     container: 'container__88e24',
     buttonGroupContainer: 'buttonGroupContainer__88e24',
     cardContainer: 'cardContainer__88e24',
-    button: 'button__88e24',
-    askToJoinInputWrapper: 'askToJoinInputWrapper__88e24',
-    inputRow: 'inputRow__88e24',
-    sendButton: 'sendButton__88e24',
-    input: 'input__88e24'
+    button: 'button__88e24'
 };
 e.exports = i;
