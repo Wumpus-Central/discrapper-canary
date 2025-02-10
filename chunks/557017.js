10055d55130da95575897a574fff93568ffc121f
DiscrapperManager <admin@minerpl.xyz>
2025-02-10 21:55:05 +0000

    canaryRelease: 367297
    Main scripts: 3
    Lazy Loaded Scripts: 921
    Chunks:
     * Main Files: 12141
     * Scripts: 9867
    Strings: 0
    Routes: 698
    Experiments Count: 393

---
+++ chunks/557017.js
@@ -0,0 +1,17 @@
+n.d(t, { t: () => r });
+var i = n(818083),
+    l = n(987338);
+let r = (0, i.B)({
+    kind: 'user',
+    id: '2025-01_q1_marketing_messaging',
+    label: '2025 Q1 Collectible Deco Marketing Moment',
+    defaultConfig: { enabled: !1 },
+    commonTriggerPoint: l.$P.CONNECTION_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: 'Users will see the 2025 Q1 Collectible Deco Marketing Moment',
+            config: { enabled: !0 }
+        }
+    ]
+});
