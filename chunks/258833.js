0e8533f8f8dfb3ed031c2c46dddd01607bc48d5e
DiscrapperManager <admin@minerpl.xyz>
2025-02-11 00:07:49 +0000

    canaryRelease: 367422
    Main scripts: 3
    Lazy Loaded Scripts: 924
    Chunks:
     * Main Files: 12141
     * Scripts: 9874
    Strings: 0
    Routes: 698
    Experiments Count: 393

--- chunks/258833.js
+++ chunks/258833.js
@@ -1,31 +1,44 @@
-n.d(t, { Z: () => I }), n(47120);
+n.d(t, { Z: () => A }), n(47120);
 var i = n(147913),
     r = n(460181),
     a = n(474873),
     s = n(523746),
     o = n(592125),
     l = n(292959),
-    u = n(246946),
-    c = n(979651),
-    d = n(938475);
-function f(e, t) {
+    u = n(944486),
+    c = n(246946),
+    d = n(979651),
+    f = n(938475),
+    _ = n(358221);
+function p(e, t) {
     if (t.has(e)) throw TypeError('Cannot initialize the same private elements twice on an object');
 }
-function _(e, t) {
+function h(e, t) {
     return t.get ? t.get.call(e) : t.value;
 }
-function p(e, t, n) {
+function m(e, t, n) {
+    if (t.set) t.set.call(e, n);
+    else {
+        if (!t.writable) throw TypeError('attempted to set read only private field');
+        t.value = n;
+    }
+}
+function g(e, t, n) {
     if (!t.has(e)) throw TypeError('attempted to ' + n + ' private field on non-instance');
     return t.get(e);
 }
-function h(e, t) {
-    var n = p(e, t, 'get');
-    return _(e, n);
+function E(e, t) {
+    var n = g(e, t, 'get');
+    return h(e, n);
 }
-function m(e, t, n) {
-    f(e, t), t.set(e, n);
+function v(e, t, n) {
+    p(e, t), t.set(e, n);
 }
-function g(e, t, n) {
+function y(e, t, n) {
+    var i = g(e, t, 'set');
+    return m(e, i, n), n;
+}
+function I(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -38,49 +51,69 @@ function g(e, t, n) {
         e
     );
 }
-let E = (0, r.uk)('call_calling', a.Z.getSoundpack());
-var v = new WeakMap();
-class y extends i.Z {
+let T = (0, r.uk)('call_calling', a.Z.getSoundpack());
+var b = new WeakMap();
+class S extends i.Z {
     _initialize() {
-        this.stores = new Map().set(s.Z, this.handleRingUpdate).set(l.Z, this.handleRingUpdate).set(u.Z, this.handleRingUpdate).set(c.Z, this.handleRingUpdate).set(a.Z, this.handleSoundpackUpdate);
+        this.stores = new Map().set(s.Z, this.handleRingUpdate).set(l.Z, this.handleRingUpdate).set(c.Z, this.handleRingUpdate).set(d.Z, this.handleRingUpdate).set(_.Z, this.handleChannelRTCStoreChange).set(a.Z, this.handleSoundpackUpdate);
     }
     constructor(...e) {
         super(...e),
-            m(this, v, {
+            v(this, b, {
                 writable: !0,
                 value: new Set()
             }),
-            g(this, 'actions', {
+            I(this, 'actions', {
                 GUILD_LOCAL_RING_START: (e) => this.handleGuildRingStart(e),
                 GUILD_RING_STOP: (e) => this.handleGuildRingStop(e)
             }),
-            g(this, '_handleRing', (e, t) => {
-                let n = c.Z.getCurrentClientVoiceChannelId(t),
+            I(this, '_handleRing', (e, t) => {
+                let n = d.Z.getCurrentClientVoiceChannelId(t),
                     i = o.Z.getChannel(n),
-                    r = null != n && (null == i ? void 0 : i.guild_id) == null && d.ZP.countVoiceStatesForChannel(n) >= 2;
-                null == n || r || !e || l.Z.isSoundDisabled('call_calling') || u.Z.disableSounds ? E.stop() : E.loop();
+                    r = null != n && (null == i ? void 0 : i.guild_id) == null && f.ZP.countVoiceStatesForChannel(n) >= 2;
+                null == n || r || !e || l.Z.isSoundDisabled('call_calling') || c.Z.disableSounds ? T.stop() : T.loop();
             }),
-            g(this, 'handleSoundpackUpdate', () => {
-                E.stop(), (E = (0, r.uk)('call_calling', a.Z.getSoundpack()));
+            I(this, 'handleSoundpackUpdate', () => {
+                T.stop(), (T = (0, r.uk)('call_calling', a.Z.getSoundpack()));
             }),
-            g(this, 'handleRingUpdate', () => {
-                let e = s.Z.getCalls().some((e) => e.ringing.length > 0 && c.Z.getCurrentClientVoiceChannelId(null) === e.channelId);
-                this._handleRing(e, null);
+            I(this, 'handleRingUpdate', () => {
+                var e, t;
+                let n = u.Z.getVoiceChannelId(),
+                    i = null !== (t = null === (e = o.Z.getChannel(n)) || void 0 === e ? void 0 : e.guild_id) && void 0 !== t ? t : null,
+                    r = s.Z.getCalls().some((e) => e.ringing.length > 0 && d.Z.getCurrentClientVoiceChannelId(null) === e.channelId);
+                this._handleRing(r || E(this, b).size > 0, i);
             }),
-            g(this, 'handleGuildRingStart', (e) => {
+            I(this, 'handleGuildRingStart', (e) => {
                 let { ringing: t, guildId: n } = e;
                 t.forEach((e) => {
-                    h(this, v).add(e);
+                    E(this, b).add(e);
                 }),
-                    this._handleRing(h(this, v).size > 0, n);
+                    this._handleRing(E(this, b).size > 0, n);
             }),
-            g(this, 'handleGuildRingStop', (e) => {
+            I(this, 'handleGuildRingStop', (e) => {
                 let { ringing: t, guildId: n } = e;
                 t.forEach((e) => {
-                    h(this, v).delete(e);
+                    E(this, b).delete(e);
                 }),
-                    this._handleRing(h(this, v).size > 0, n);
+                    this._handleRing(E(this, b).size > 0, n);
+            }),
+            I(this, 'handleChannelRTCStoreChange', () => {
+                let e = u.Z.getVoiceChannelId(),
+                    t = E(this, b).size > 0;
+                if (!t) return;
+                if (null == e && t) {
+                    y(this, b, new Set()), this._handleRing(E(this, b).size > 0, null);
+                    return;
+                }
+                if (null == e) return;
+                let n = _.Z.getGuildRingingUsers(e),
+                    i = E(this, b).difference(n);
+                i.size > 0 &&
+                    (i.forEach((e) => {
+                        E(this, b).delete(e);
+                    }),
+                    this._handleRing(E(this, b).size > 0, null));
             });
     }
 }
-let I = new y();
+let A = new S();
--- chunks/340895.js
+++ chunks/340895.js
@@ -1,18 +1,19 @@
 let i;
-n.d(t, { Z: () => w }), n(47120);
+n.d(t, { Z: () => k }), n(47120);
 var r,
     a = n(442837),
     s = n(433517),
     o = n(570140),
-    l = n(695346),
-    u = n(581883),
-    c = n(163612),
-    d = n(314897),
-    f = n(592125),
-    _ = n(885110),
-    p = n(451478),
-    h = n(981631);
-function m(e, t, n) {
+    l = n(358221),
+    u = n(695346),
+    c = n(581883),
+    d = n(163612),
+    f = n(314897),
+    _ = n(592125),
+    p = n(885110),
+    h = n(451478),
+    m = n(981631);
+function g(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -25,117 +26,124 @@ function m(e, t, n) {
         e
     );
 }
-let g = 'IncomingCallStore',
-    E = {
+let E = 'IncomingCallStore',
+    v = {
         width: 232,
         height: 315
     },
-    v = 10,
-    y = new Set(),
-    I = [],
-    T = new Map(),
-    b = new Set(),
-    S = !1;
-function A() {
-    let e = p.Z.windowSize();
-    return null != i && i.x + E.width < e.width && i.y + E.height < e.height
+    y = 10,
+    I = new Set(),
+    T = [],
+    b = new Map(),
+    S = new Set(),
+    A = !1;
+function N() {
+    let e = h.Z.windowSize();
+    return null != i && i.x + v.width < e.width && i.y + v.height < e.height
         ? i
         : {
-              x: e.width / 2 - E.width / 2,
-              y: e.height / 2 - E.height / 2
+              x: e.width / 2 - v.width / 2,
+              y: e.height / 2 - v.height / 2
           };
 }
-function N(e) {
-    if (null == e || null == T.get(e)) return !1;
-    T.delete(e), (b = new Set(b)).delete(e);
-}
 function C(e) {
+    if (null == e || null == b.get(e)) return !1;
+    b.delete(e), (S = new Set(S)).delete(e);
+}
+function R(e) {
     let { channelId: t, ringing: n } = e;
     if (
         'GUILD_RING_START' === e.type &&
-        !c.Z.getCurrentConfig({
+        !d.Z.getCurrentConfig({
             guildId: e.guildId,
             location: 'IncomingCallCreate'
         }).enabled
     )
         return !1;
-    let i = n.includes(d.default.getId());
-    if (!b.has(t) && i) {
-        let e = f.Z.getChannel(t);
+    let i = n.includes(f.default.getId());
+    if (!S.has(t) && i) {
+        let e = _.Z.getChannel(t);
         if (null == e) return !1;
-        let n = v * b.size,
-            { x: i, y: r } = A();
+        let n = y * S.size,
+            { x: i, y: r } = N();
         return (
-            T.set(t, {
+            b.set(t, {
                 channel: e,
                 x: i + n,
                 y: r + n
             }),
-            void (b = new Set(b)).add(t)
+            void (S = new Set(S)).add(t)
         );
     }
-    return !!b.has(t) && !i && N(t);
+    return !!S.has(t) && !i && C(t);
 }
-function R(e) {
+function O(e) {
     let { channelId: t } = e;
     return (
         !!(
             'GUILD_RING_STOP' !== e.type ||
-            (c.Z.getCurrentConfig({
+            (d.Z.getCurrentConfig({
                 guildId: e.guildId,
                 location: 'IncomingCallDelete'
             }).enabled &&
-                e.ringing.includes(d.default.getId()))
-        ) && N(t)
+                e.ringing.includes(f.default.getId()))
+        ) && C(t)
     );
 }
-function O(e) {
-    let { channelId: t } = e;
-    return N(t);
-}
 function D(e) {
+    let { channelId: t } = e;
+    return C(t);
+}
+function L(e) {
     let { x: t, y: n } = e;
     return (
         (i = {
             x: t,
             y: n
         }),
-        s.K.set(g, i),
+        s.K.set(E, i),
         !1
     );
 }
-function L(e) {
+function x(e) {
     let { channel: t } = e;
-    return N(t.id);
+    return C(t.id);
 }
-function x() {
-    S = _.Z.getStatus() === h.Skl.DND || l.QZ.getSetting();
+function P() {
+    A = p.Z.getStatus() === m.Skl.DND || u.QZ.getSetting();
 }
-class P extends (r = a.ZP.Store) {
+function w() {
+    let e = f.default.getId();
+    S.forEach((t) => {
+        let n = t;
+        l.Z.getGuildRingingUsers(n).has(e) || C(n);
+    });
+}
+class M extends (r = a.ZP.Store) {
     initialize() {
-        this.waitFor(f.Z, _.Z), this.syncWith([_.Z], x), this.syncWith([u.Z], x);
+        this.waitFor(_.Z, p.Z), this.syncWith([p.Z], P), this.syncWith([c.Z], P), this.syncWith([l.Z], w);
     }
     getIncomingCalls() {
-        return S ? I : Array.from(T.values());
+        return A ? T : Array.from(b.values());
     }
     getIncomingCallChannelIds() {
-        return S ? y : b;
+        return A ? I : S;
     }
     getFirstIncomingCallId() {
-        return S ? null : b.values().next().value;
+        return A ? null : S.values().next().value;
     }
     hasIncomingCalls() {
-        return !S && b.size > 0;
+        return !A && S.size > 0;
     }
 }
-m(P, 'displayName', 'IncomingCallStore');
-let w = new P(o.Z, {
-    CALL_CREATE: C,
-    CALL_UPDATE: C,
-    CALL_DELETE: R,
-    GUILD_RING_START: C,
-    GUILD_RING_STOP: R,
-    VOICE_CHANNEL_SELECT: O,
-    INCOMING_CALL_MOVE: D,
-    CHANNEL_DELETE: L
+g(M, 'displayName', 'IncomingCallStore');
+let k = new M(o.Z, {
+    CALL_CREATE: R,
+    CALL_UPDATE: R,
+    CALL_DELETE: O,
+    GUILD_RING_START: R,
+    GUILD_RING_STOP: O,
+    VOICE_CHANNEL_SELECT: D,
+    INCOMING_CALL_MOVE: L,
+    CHANNEL_DELETE: x
 });
