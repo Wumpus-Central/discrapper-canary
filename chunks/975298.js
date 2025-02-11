de6f9b908949a67a59388bf76227bfb95580fd0f
DiscrapperManager <admin@minerpl.xyz>
2025-02-11 16:15:21 +0000

    canaryRelease: 367565
    Main scripts: 3
    Lazy Loaded Scripts: 924
    Chunks:
     * Main Files: 12147
     * Scripts: 9888
    Strings: 0
    Routes: 698
    Experiments Count: 394

--- chunks/975298.js
+++ chunks/975298.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => v }), n(47120), n(411104);
+n.d(t, { Z: () => E }), n(47120), n(411104);
 var i = n(192379),
     r = n(512722),
     a = n.n(r),
@@ -11,18 +11,17 @@ var i = n(192379),
     f = n(78839),
     _ = n(580130),
     p = n(122289),
-    h = n(111361),
-    m = n(74538),
-    g = n(981631),
-    E = n(474936);
-function v() {
+    h = n(74538),
+    m = n(981631),
+    g = n(474936);
+function E() {
     let { forceFetch: e } = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : { forceFetch: !1 },
         t = (0, l.e7)([d.default], () => d.default.getCurrentUser()),
         n = (0, l.Wu)([_.Z], () => _.Z.getFractionalPremium(!1)),
         r = (0, l.Wu)([_.Z], () => _.Z.getUnactivatedFractionalPremiumUnits()),
         s = (0, l.e7)([f.ZP], () => f.ZP.getPremiumTypeSubscription()),
-        [v, y] = i.useState({
-            fractionalState: E.a$.NONE,
+        [E, v] = i.useState({
+            fractionalState: g.a$.NONE,
             startsAt: o()(0),
             endsAt: o()(0),
             currentEntitlementId: '',
@@ -30,15 +29,15 @@ function v() {
         });
     return (
         (0, c.ZP)(() => {
-            ((null != t && (0, h.I5)(t)) || e) && (_.Z.fetchingAllEntitlements || (0, u.p0)({ entitlementType: g.qc2.FRACTIONAL_REDEMPTION }));
+            (null != t || e) && (_.Z.fetchingAllEntitlements || (0, u.p0)({ entitlementType: m.qc2.FRACTIONAL_REDEMPTION }));
         }),
         i.useEffect(() => {
-            if (null == t || !(0, h.I5)(t) || 0 === n.length) {
-                y((e) =>
-                    e.endsAt.isSame(o()(0)) && e.fractionalState === E.a$.NONE
+            if (null == t || (0 === n.length && 0 === r.length)) {
+                v((e) =>
+                    e.endsAt.isSame(o()(0)) && e.fractionalState === g.a$.NONE
                         ? e
                         : {
-                              fractionalState: E.a$.NONE,
+                              fractionalState: g.a$.NONE,
                               startsAt: o()(0),
                               endsAt: o()(0),
                               currentEntitlementId: '',
@@ -48,19 +47,22 @@ function v() {
                 return;
             }
             let e = n.filter((e) => null != e.endsAt && null != e.startsAt).sort((e, t) => ((a()(null != e.endsAt && null != t.endsAt, 'endsAt should not be null'), e.endsAt < t.endsAt) ? -1 : e.endsAt > t.endsAt ? 1 : 0));
-            if ((e.reverse(), e.length !== n.length || null == e[0].startsAt || null == e[0].endsAt)) {
+            if ((e.reverse(), e.length > 0 && (e.length !== n.length || null == e[0].startsAt || null == e[0].endsAt))) {
                 let e = Array.from(n.values()).map((e) => e.id),
                     t = 'fractional redemption entitlements should have startsAt/endsAt';
                 throw ((0, p.g9)(t, { extra: { entitlementIds: e } }), Error(t));
             }
-            y({
-                fractionalState: null == s || g.JwP.INACTIVE.has(s.status) || s.isBoostOnly ? E.a$.FP_ONLY : E.a$.FP_SUB_PAUSED,
-                startsAt: o()(e[0].startsAt),
-                endsAt: o()((0, m.N1)(e[0].endsAt, r)),
-                currentEntitlementId: e.length > 0 ? e[0].id : '',
-                unactivatedUnits: r
-            });
+            let i = e.length > 0,
+                l = i ? g.a$.FP_ONLY : g.a$.NONE;
+            null != s && s.status === m.O0b.PAUSED && (l = g.a$.FP_SUB_PAUSED),
+                v({
+                    fractionalState: l,
+                    startsAt: i ? o()(e[0].startsAt) : o()(0),
+                    endsAt: i ? o()((0, h.N1)(e[0].endsAt, r)) : o()(0),
+                    currentEntitlementId: e.length > 0 ? e[0].id : '',
+                    unactivatedUnits: r
+                });
         }, [t, n, s, r]),
-        v
+        E
     );
 }
