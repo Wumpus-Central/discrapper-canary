8c9c464db6d8d1f61ae4bd65b5714d097d60d4d4
DiscrapperManager <admin@minerpl.xyz>
2025-02-11 19:55:38 +0000

    canaryRelease: 367688
    Main scripts: 3
    Lazy Loaded Scripts: 924
    Chunks:
     * Main Files: 12151
     * Scripts: 9891
    Strings: 0
    Routes: 698
    Experiments Count: 395

--- chunks/591974.js
+++ chunks/591974.js
@@ -1,36 +1,28 @@
-n.d(t, { Z: () => m });
+n.d(t, { Z: () => h });
 var i = n(200651),
     l = n(442837),
     r = n(481060),
-    a = n(8454),
-    s = n(626562),
-    o = n(885110),
-    c = n(51144),
-    d = n(231338),
-    u = n(388032),
-    h = n(395568);
-function m(e) {
-    var t;
-    let { user: n } = e,
-        { activities: m, status: p } = (0, l.cj)([o.Z], () => ({
-            activities: o.Z.getActivities(),
-            status: o.Z.getStatus()
-        })),
-        g = null !== (t = (0, c.u5)(p)) && void 0 !== t ? t : '',
-        _ = m.length > 0,
-        f = (0, a.Z)(n.id),
-        E = p === d.Sk.INVISIBLE || p === d.Sk.OFFLINE,
-        I = (0, s.Vm)({ location: 'AccountProfilePopoutMenuItemStatusLabel' });
+    a = n(626562),
+    s = n(885110),
+    o = n(51144),
+    c = n(231338),
+    d = n(388032),
+    u = n(395568);
+function h() {
+    var e;
+    let t = (0, l.e7)([s.Z], () => s.Z.getStatus()),
+        n = null !== (e = (0, o.u5)(t)) && void 0 !== e ? e : '',
+        h = t === c.Sk.INVISIBLE || t === c.Sk.OFFLINE,
+        m = (0, a.Vm)({ location: 'AccountProfilePopoutMenuItemStatusLabel' });
     return (0, i.jsxs)('div', {
-        className: h.container,
+        className: u.container,
         children: [
-            g,
-            g.length > 0 &&
-                I &&
-                E &&
-                (_ || null != f) &&
+            n,
+            n.length > 0 &&
+                m &&
+                h &&
                 (0, i.jsx)(r.ua7, {
-                    text: u.intl.string(u.t.L99HQk),
+                    text: d.intl.string(d.t.L99HQk),
                     children: (e) =>
                         (0, i.jsx)(r.P4T, {
                             size: 'xs',
