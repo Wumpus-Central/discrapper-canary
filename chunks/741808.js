a87a74d5f89166dab3d17cad262cf78b99da6243
DiscrapperManager <admin@minerpl.xyz>
2025-02-12 21:00:04 +0000

    canaryRelease: 368083
    Main scripts: 3
    Lazy Loaded Scripts: 924
    Chunks:
     * Main Files: 12166
     * Scripts: 9897
    Strings: 0
    Routes: 699
    Experiments Count: 400

--- chunks/741808.js
+++ chunks/741808.js
@@ -1,18 +1,21 @@
-n.d(t, { X: () => c }), n(47120);
-var i = n(200651),
-    a = n(192379),
-    o = n(392711);
-let r = (e, t) => (void 0 !== t && t > e ? t : e),
-    l = {
-        position: 'absolute',
-        pointerEvents: 'all'
-    };
-function c(e) {
-    let { children: t, className: n, key: c, ...s } = e,
-        { baseStyles: _, offsets: d } = (function (e) {
-            let { key: t, backgroundElementRef: n, getOffsetsRelativeToElement: i, fallbackAbsoluteOffsets: c, onGetBoundingRect: s = () => {}, debounceTime: _ = 60, minimumOffsets: d } = e,
-                [u, f] = a.useState(null),
-                p = a.useCallback(() => {
+n.d(t, { X: () => m }), n(47120);
+var a = n(200651),
+    r = n(192379),
+    l = n(120356),
+    i = n.n(l),
+    o = n(392711),
+    s = n(442837),
+    c = n(110924),
+    d = n(819640),
+    u = n(357597);
+let h = (e, t) => (void 0 !== t && t > e ? t : e);
+function m(e) {
+    let { children: t, className: n, key: l, ...m } = e,
+        { offsets: x } = (function (e) {
+            let { key: t, backgroundElementRef: n, getOffsetsRelativeToElement: a, fallbackAbsoluteOffsets: l, onGetBoundingRect: i = () => {}, debounceTime: u = 60, minimumOffsets: m } = e,
+                { hasLayers: x } = (0, s.cj)([d.Z], () => ({ hasLayers: d.Z.hasLayers() })),
+                [_, p] = r.useState(null),
+                f = r.useCallback(() => {
                     let e = n.current;
                     return null === e
                         ? {
@@ -24,55 +27,66 @@ function c(e) {
                               width: e.offsetWidth
                           };
                 }, [n]),
-                g = a.useCallback(() => {
+                b = r.useCallback(() => {
                     let e = n.current;
-                    if (null === e) return c;
-                    let t = { ...i(p()) },
-                        a = e.getBoundingClientRect();
-                    return void 0 !== t.top && (t.top = r(a.top + t.top, null == d ? void 0 : d.top)), void 0 !== t.left && (t.left = r(a.left + t.left, null == d ? void 0 : d.left)), void 0 !== t.right && (t.right = r(window.innerWidth - a.right + t.right, null == d ? void 0 : d.right)), void 0 !== t.bottom && (t.bottom = r(window.innerHeight - a.bottom + t.bottom, null == d ? void 0 : d.bottom)), (0, o.isEqual)(t, u) || f(t), s(a), t;
-                }, [i, c, u, p, d, n, s]);
-            a.useEffect(() => {
-                g();
-            }, [t, g]);
-            let b = a.useMemo(
+                    if (null === e) return l;
+                    let t = { ...a(f()) },
+                        r = e.getBoundingClientRect();
+                    return void 0 !== t.top && (t.top = h(r.top + t.top, null == m ? void 0 : m.top)), void 0 !== t.left && (t.left = h(r.left + t.left, null == m ? void 0 : m.left)), void 0 !== t.right && (t.right = h(window.innerWidth - r.right + t.right, null == m ? void 0 : m.right)), void 0 !== t.bottom && (t.bottom = h(window.innerHeight - r.bottom + t.bottom, null == m ? void 0 : m.bottom)), (0, o.isEqual)(t, _) || p(t), i(r), t;
+                }, [a, l, _, f, m, n, i]);
+            r.useEffect(() => {
+                b();
+            }, [t, b]);
+            let g = (0, c.Z)(x),
+                v = r.useRef(null),
+                j = r.useRef(0);
+            r.useEffect(() => {
+                g &&
+                    !x &&
+                    (null === v.current || j.current >= 3) &&
+                    (v.current = setInterval(() => {
+                        if (j.current >= 5) {
+                            clearInterval(v.current), (v.current = null), (j.current = 0);
+                            return;
+                        }
+                        b(), j.current++;
+                    }, 200));
+            }, [b, x, g]);
+            let C = r.useMemo(
                 () =>
                     (0, o.debounce)(() => {
-                        g();
-                    }, _),
-                [g, _]
+                        b();
+                    }, u),
+                [b, u]
             );
             return (
-                a.useEffect(
+                r.useEffect(
                     () => (
-                        window.addEventListener('resize', b),
+                        window.addEventListener('resize', C),
                         () => {
-                            window.removeEventListener('resize', b);
+                            window.removeEventListener('resize', C);
                         }
                     ),
-                    [b]
+                    [C]
                 ),
                 {
-                    offsets: u,
-                    baseStyles: l,
-                    getElementDimensions: p,
-                    updateElementOffsets: g
+                    offsets: _,
+                    getElementDimensions: f,
+                    updateElementOffsets: b
                 }
             );
-        })({ ...s }),
-        u = {
-            ..._,
-            ...d
-        };
-    return null === d
+        })({ ...m }),
+        _ = { ...x };
+    return null === x
         ? null
-        : (0, i.jsx)(
+        : (0, a.jsx)(
               'div',
               {
-                  style: { ...u },
-                  className: n,
+                  style: _,
+                  className: i()(u.wrapper, n),
                   children: t
               },
-              c
+              l
           );
 }
-c.displayName = 'ElementOffsetModalContentWrapper';
+m.displayName = 'ElementOffsetModalContentWrapper';
