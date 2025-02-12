d00c2832f2d2f0f7e18ca22606482e32e0070d99
DiscrapperManager <admin@minerpl.xyz>
2025-02-12 01:29:16 +0000

    canaryRelease: 367900
    Main scripts: 3
    Lazy Loaded Scripts: 924
    Chunks:
     * Main Files: 12165
     * Scripts: 9892
    Strings: 0
    Routes: 698
    Experiments Count: 399

--- chunks/932374.js
+++ chunks/932374.js
@@ -1,130 +1,134 @@
-i.d(n, { default: () => o });
-var e = i(200651);
-i(192379);
-var s = i(481060),
-    l = i(230711),
-    r = i(695346),
-    a = i(981631),
-    c = i(388032),
-    d = i(827085);
-let o = (t) => {
-    let { transitionState: n, onClose: i } = t,
-        o = [
-            {
-                title: c.intl.string(c.t['wnt8/P']),
-                description: c.intl.string(c.t.VupM1t)
-            },
-            {
-                title: c.intl.string(c.t.Arohq6),
-                description: c.intl.string(c.t.nYLud3)
-            },
-            {
-                title: c.intl.string(c.t.MDXRKi),
-                description: c.intl.string(c.t.EG31Wl)
-            }
-        ];
-    return (0, e.jsx)(s.Y0X, {
-        transitionState: n,
+n.d(i, { default: () => h });
+var e = n(200651),
+    s = n(192379),
+    l = n(481060),
+    r = n(230711),
+    a = n(695346),
+    c = n(626135),
+    d = n(981631),
+    o = n(388032),
+    x = n(827085);
+let h = (t) => {
+    let { transitionState: i, onClose: n } = t;
+    s.useEffect(() => {
+        c.default.track(d.rMx.OPEN_MODAL, { type: 'Confirm Disable Activity Sharing' });
+    }, []);
+    let h = [
+        {
+            title: o.intl.string(o.t['wnt8/P']),
+            description: o.intl.string(o.t.VupM1t)
+        },
+        {
+            title: o.intl.string(o.t.Arohq6),
+            description: o.intl.string(o.t.nYLud3)
+        },
+        {
+            title: o.intl.string(o.t.MDXRKi),
+            description: o.intl.string(o.t.EG31Wl)
+        }
+    ];
+    return (0, e.jsx)(l.Y0X, {
+        transitionState: i,
         'aria-label': '',
-        children: (0, e.jsxs)(s.hzk, {
-            className: d.content,
+        children: (0, e.jsxs)(l.hzk, {
+            className: x.content,
             children: [
                 (0, e.jsxs)('div', {
-                    className: d.header,
+                    className: x.header,
                     children: [
-                        (0, e.jsx)(s.X6q, {
+                        (0, e.jsx)(l.X6q, {
                             variant: 'heading-xl/bold',
-                            children: c.intl.string(c.t['3k4hTU'])
+                            children: o.intl.string(o.t['3k4hTU'])
                         }),
-                        (0, e.jsx)(s.Text, {
+                        (0, e.jsx)(l.Text, {
                             variant: 'text-md/normal',
-                            children: c.intl.string(c.t.IR0Bf3)
+                            children: o.intl.string(o.t.IR0Bf3)
                         })
                     ]
                 }),
                 (0, e.jsx)('div', {
-                    className: d.notes,
-                    children: o.map((t, n) => {
-                        let { title: i, description: l } = t;
+                    className: x.notes,
+                    children: h.map((t, i) => {
+                        let { title: n, description: s } = t;
                         return (0, e.jsxs)(e.Fragment, {
                             children: [
                                 (0, e.jsxs)(
                                     'div',
                                     {
-                                        className: d.note,
+                                        className: x.note,
                                         children: [
-                                            (0, e.jsx)(s.k$p, {}),
+                                            (0, e.jsx)(l.k$p, {}),
                                             (0, e.jsxs)('div', {
                                                 children: [
-                                                    (0, e.jsx)(s.X6q, {
+                                                    (0, e.jsx)(l.X6q, {
                                                         variant: 'heading-md/semibold',
-                                                        children: i
+                                                        children: n
                                                     }),
-                                                    (0, e.jsx)(s.Text, {
+                                                    (0, e.jsx)(l.Text, {
                                                         variant: 'text-xs/normal',
-                                                        children: l
+                                                        children: s
                                                     })
                                                 ]
                                             })
                                         ]
                                     },
-                                    n
+                                    i
                                 ),
-                                n !== o.length - 1 && (0, e.jsx)(s.$i$, { className: d.noteDivider })
+                                i !== h.length - 1 && (0, e.jsx)(l.$i$, { className: x.noteDivider })
                             ]
                         });
                     })
                 }),
-                (0, e.jsx)(s.Text, {
+                (0, e.jsx)(l.Text, {
                     variant: 'text-md/normal',
-                    children: c.intl.string(c.t.cEjHRk)
+                    children: o.intl.string(o.t.cEjHRk)
                 }),
                 (0, e.jsxs)('div', {
-                    className: d.linksContainer,
+                    className: x.linksContainer,
                     children: [
-                        (0, e.jsxs)(s.P3F, {
+                        (0, e.jsxs)(l.P3F, {
                             onClick: () => {
-                                i(), l.Z.open(a.oAB.REGISTERED_GAMES);
+                                n(), r.Z.open(d.oAB.REGISTERED_GAMES);
                             },
-                            className: d.link,
+                            className: x.link,
                             children: [
-                                (0, e.jsx)(s.Text, {
+                                (0, e.jsx)(l.Text, {
                                     variant: 'text-md/medium',
-                                    children: c.intl.string(c.t['8uoLf3'])
+                                    children: o.intl.string(o.t['8uoLf3'])
                                 }),
-                                (0, e.jsx)(s.LJT, { size: 'md' })
+                                (0, e.jsx)(l.LJT, { size: 'md' })
                             ]
                         }),
-                        (0, e.jsx)(s.$i$, { className: d.linkDivider }),
-                        (0, e.jsxs)(s.P3F, {
+                        (0, e.jsx)(l.$i$, { className: x.linkDivider }),
+                        (0, e.jsxs)(l.P3F, {
                             onClick: () => {
-                                i(), l.Z.open(a.oAB.CONNECTIONS);
+                                n(), r.Z.open(d.oAB.CONNECTIONS);
                             },
-                            className: d.link,
+                            className: x.link,
                             children: [
-                                (0, e.jsx)(s.Text, {
+                                (0, e.jsx)(l.Text, {
                                     variant: 'text-md/medium',
-                                    children: c.intl.string(c.t.Q42xe3)
+                                    children: o.intl.string(o.t.Q42xe3)
                                 }),
-                                (0, e.jsx)(s.LJT, { size: 'md' })
+                                (0, e.jsx)(l.LJT, { size: 'md' })
                             ]
                         })
                     ]
                 }),
                 (0, e.jsxs)('div', {
-                    className: d.buttonsContainer,
+                    className: x.buttonsContainer,
                     children: [
-                        (0, e.jsx)(s.zxk, {
-                            onClick: i,
-                            color: s.Ttl.PRIMARY,
-                            children: c.intl.string(c.t['ETE/oK'])
+                        (0, e.jsx)(l.zxk, {
+                            onClick: n,
+                            color: l.Ttl.PRIMARY,
+                            children: o.intl.string(o.t['ETE/oK'])
                         }),
-                        (0, e.jsx)(s.zxk, {
+                        (0, e.jsx)(l.zxk, {
                             onClick: () => {
-                                r.G6.updateSetting(!1), i();
+                                a.G6.updateSetting(!1), n();
                             },
-                            color: s.Ttl.RED,
-                            children: c.intl.string(c.t.R9GHyc)
+                            color: l.Ttl.RED,
+                            children: o.intl.string(o.t.R9GHyc)
                         })
                     ]
                 })
