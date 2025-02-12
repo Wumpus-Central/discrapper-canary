1e7982600ed3629249426db65368c7bce056b8de
DiscrapperManager <admin@minerpl.xyz>
2025-02-12 20:34:10 +0000

    canaryRelease: 368071
    Main scripts: 3
    Lazy Loaded Scripts: 924
    Chunks:
     * Main Files: 12166
     * Scripts: 9896
    Strings: 0
    Routes: 699
    Experiments Count: 400

--- chunks/454991.js
+++ chunks/454991.js
@@ -31,38 +31,43 @@ class o {
             legacyEnabled: this.legacyEnabled,
             oopEnabled: this.oopEnabled
         };
-        i.K.set('OverlayStore5', e);
+        i.K.set('OverlayStore6', e);
     }
     static load() {
         return null == o._loaded && (o._loaded = o.loadInternal()), o._loaded;
     }
     static loadInternal() {
-        var e, t, n, s;
-        let l = (0, r.NW)('OverlayStoredSettings', !1),
-            u = i.K.get('OverlayStore');
-        if (null != u) {
-            let e = new o('boolean' == typeof u.enabled ? u.enabled : a.iP, l);
+        var e, t, n, s, l;
+        let u = (0, r.NW)('OverlayStoredSettings', !1),
+            c = i.K.get('OverlayStore');
+        if (null != c) {
+            let e = new o('boolean' == typeof c.enabled ? c.enabled : a.iP, u);
             return e.save(), i.K.remove('OverlayStore'), e;
         }
-        let c = i.K.get('overlayEnabled');
-        if (null != c) {
-            let e = new o('boolean' == typeof c ? c : a.iP, l);
+        let d = i.K.get('overlayEnabled');
+        if (null != d) {
+            let e = new o('boolean' == typeof d ? d : a.iP, u);
             return e.save(), i.K.remove('overlayEnabled'), e;
         }
-        let d = i.K.get('OverlayStore3');
-        if (null != d) {
-            let t = new o(null !== (e = d.enabled || d.legacyEnabled) && void 0 !== e ? e : a.iP, a.iP && l);
+        let f = i.K.get('OverlayStore3');
+        if (null != f) {
+            let t = new o(null !== (e = f.enabled || f.legacyEnabled) && void 0 !== e ? e : a.iP, a.iP && u);
             return t.save(), i.K.remove('OverlayStore3'), t;
         }
-        let f = i.K.get('OverlayStore4');
-        if (null != f) {
-            let e = new o(f.legacyEnabled || a.iP, null !== (t = f.oopEnabled) && void 0 !== t ? t : a.iP && l);
+        let _ = i.K.get('OverlayStore4');
+        if (null != _) {
+            let e = new o(_.legacyEnabled || a.iP, null !== (t = _.oopEnabled) && void 0 !== t ? t : a.iP && u);
             return e.save(), i.K.remove('OverlayStore4'), e;
         }
-        let _ = i.K.get('OverlayStore5');
-        if (null != _) return new o(null !== (n = _.legacyEnabled) && void 0 !== n ? n : a.iP, null !== (s = _.oopEnabled) && void 0 !== s ? s : a.iP && l);
-        let p = new o(a.iP, l);
-        return p.save(), p;
+        let p = i.K.get('OverlayStore5');
+        if (null != p) {
+            let e = new o(null !== (n = p.legacyEnabled) && void 0 !== n ? n : a.iP, p.oopEnabled || a.iP);
+            return e.save(), i.K.remove('OverlayStore5'), e;
+        }
+        let h = i.K.get('OverlayStore6');
+        if (null != h) return new o(null !== (s = h.legacyEnabled) && void 0 !== s ? s : a.iP, null !== (l = h.oopEnabled) && void 0 !== l ? l : a.iP);
+        let m = new o(a.iP, u);
+        return m.save(), m;
     }
     constructor(e, t) {
         s(this, 'legacyEnabled', void 0), s(this, 'oopEnabled', void 0), (this.legacyEnabled = e), (this.oopEnabled = t);
