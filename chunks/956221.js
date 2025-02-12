12a3d962d4ff506897860919353bf76c72bf5be0
DiscrapperManager <admin@minerpl.xyz>
2025-02-12 23:04:35 +0000

    canaryRelease: 368198
    Main scripts: 3
    Lazy Loaded Scripts: 924
    Chunks:
     * Main Files: 12174
     * Scripts: 9901
    Strings: 0
    Routes: 699
    Experiments Count: 400

--- chunks/956221.js
+++ chunks/956221.js
@@ -1,15 +1,20 @@
-n.d(t, { Z: () => s });
+n.d(t, { Z: () => l });
 var i = n(442837),
     r = n(592125),
-    a = n(979651);
-function s(e) {
+    a = n(496675),
+    s = n(979651),
+    o = n(231338);
+function l(e) {
     let { userId: t, guildId: n } = e,
-        s = (0, i.e7)([a.Z], () => (null != n && null != t ? a.Z.getVoiceState(n, t) : null != t ? a.Z.getVoiceStateForUser(t) : void 0)),
-        o = null == s ? void 0 : s.channelId;
-    return {
-        voiceState: s,
-        voiceChannel: (0, i.e7)([r.Z], () => {
-            if (null != o) return r.Z.getChannel(o);
-        })
-    };
+        l = (0, i.e7)([s.Z], () => (null != n && null != t ? s.Z.getVoiceState(n, t) : null != t ? s.Z.getVoiceStateForUser(t) : void 0)),
+        u = null == l ? void 0 : l.channelId,
+        c = (0, i.e7)([r.Z], () => {
+            if (null != u) return r.Z.getChannel(u);
+        });
+    return (0, i.e7)([a.Z], () => (null == c ? void 0 : c.isPrivate()) || a.Z.can(o.Pl.VIEW_CHANNEL, c))
+        ? {
+              voiceState: l,
+              voiceChannel: c
+          }
+        : {};
 }
