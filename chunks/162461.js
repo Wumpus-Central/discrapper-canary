ac3a3b886aeaa85fab5890f60249f6103d8de7aa
DiscrapperManager <admin@minerpl.xyz>
2025-02-13 17:17:37 +0000

    canaryRelease: 368382
    Main scripts: 3
    Lazy Loaded Scripts: 925
    Chunks:
     * Main Files: 12174
     * Scripts: 9906
    Strings: 0
    Routes: 699
    Experiments Count: 401

--- chunks/162461.js
+++ chunks/162461.js
@@ -1,42 +1,63 @@
 n.d(t, {
-    DG: () => a,
-    nP: () => u,
-    sA: () => s,
-    wm: () => o
+    DG: () => s,
+    MY: () => o,
+    nP: () => d,
+    sA: () => l,
+    wm: () => u
 });
 var i = n(818083),
-    r = n(109911);
-let a = (0, i.B)({
-    kind: 'user',
-    id: '2024-03_content_inventory_memberlist_and_ranker',
-    label: 'Enables the memberlist content feed',
-    defaultConfig: {
-        enabled: !0,
-        impressionCappingEnabled: !0
-    },
-    treatments: [
-        {
-            id: -1,
-            label: 'Not in experiment',
-            config: { enabled: !1 }
+    r = n(109911),
+    a = n(987338);
+let s = (0, i.B)({
+        kind: 'user',
+        id: '2024-03_content_inventory_memberlist_and_ranker',
+        label: 'Enables the memberlist content feed',
+        defaultConfig: {
+            enabled: !0,
+            impressionCappingEnabled: !0
         },
-        {
-            id: 0,
-            label: 'Holdout',
-            config: { enabled: !1 }
-        }
-    ]
-});
-function s(e) {
-    let { enabled: t } = a.getCurrentConfig({ location: e }, { autoTrackExposure: !0 }),
+        treatments: [
+            {
+                id: -1,
+                label: 'Not in experiment',
+                config: { enabled: !1 }
+            },
+            {
+                id: 0,
+                label: 'Holdout',
+                config: { enabled: !1 }
+            }
+        ]
+    }),
+    o = (0, i.B)({
+        kind: 'user',
+        id: '2025-02_hotwheels_v2_model',
+        label: 'Next iteration of the activity feed ranking model.',
+        commonTriggerPoint: a.$P.CONNECTION_OPEN,
+        defaultConfig: {},
+        treatments: [
+            {
+                id: 5,
+                label: 'ML model V1',
+                config: {}
+            },
+            {
+                id: 13,
+                label: 'ML model V2',
+                config: {}
+            }
+        ]
+    });
+function l(e) {
+    let { enabled: t } = s.getCurrentConfig({ location: e }, { autoTrackExposure: !0 }),
         n = (0, r.Ji)(e, !1);
     return t || n;
 }
-function o(e) {
-    let { enabled: t, impressionCappingEnabled: n } = a.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
+function u(e) {
+    let { enabled: t, impressionCappingEnabled: n } = s.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
     return t && !0 === n;
 }
-let l = (0, i.B)({
+let c = (0, i.B)({
     kind: 'user',
     id: '2024-08_content_inventory_analytics_sampling',
     label: 'Content Inventory Analytics Sampling',
@@ -49,6 +70,6 @@ let l = (0, i.B)({
         }
     ]
 });
-function u(e) {
-    return l.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
+function d(e) {
+    return c.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
 }
