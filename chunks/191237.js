f3a9501b639312d39dceba73573d9662669fcffa
DiscrapperManager <admin@minerpl.xyz>
2025-02-13 18:51:20 +0000

    canaryRelease: 368443
    Main scripts: 3
    Lazy Loaded Scripts: 925
    Chunks:
     * Main Files: 12174
     * Scripts: 9905
    Strings: 0
    Routes: 699
    Experiments Count: 400

--- chunks/191237.js
+++ chunks/191237.js
@@ -1,11 +1,10 @@
-n.d(e, { Z: () => l });
+n.d(e, { Z: () => r });
 var i = n(200651);
 n(192379);
-var r = n(481060),
-    o = n(838645);
-let l = {
+var o = n(481060);
+let r = {
     showAgeVerification: (t) => {
-        (0, r.ZDy)(async () => {
+        (0, o.ZDy)(async () => {
             let { default: e } = await n.e('12014').then(n.bind(n, 879367));
             return (n) =>
                 (0, i.jsx)(e, {
@@ -13,7 +12,5 @@ let l = {
                     ...n
                 });
         });
-    },
-    showTiggerPawtect: () => (0, o.H1)({ location: 'age-verify-utils' }),
-    useShouldShowTiggerPawtect: () => (0, o.s8)({ location: 'age-verify-utils' })
+    }
 };
