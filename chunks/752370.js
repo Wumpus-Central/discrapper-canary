05ac4918bd8e6d3d033239a8ca228cd2dcf6c020
DiscrapperManager <admin@minerpl.xyz>
2025-02-14 19:25:25 +0000

    canaryRelease: 368875
    Main scripts: 3
    Lazy Loaded Scripts: 923
    Chunks:
     * Main Files: 12176
     * Scripts: 9921
    Strings: 0
    Routes: 699
    Experiments Count: 400

--- chunks/752370.js
+++ chunks/752370.js
@@ -1,20 +1,21 @@
-n.d(t, { Z: () => S }), n(610138), n(216116), n(78328), n(815648), n(47120);
+n.d(t, { Z: () => A }), n(610138), n(216116), n(78328), n(815648), n(47120);
 var i = n(904245),
     r = n(147913),
     a = n(710845),
     s = n(957730),
     o = n(592125),
-    l = n(131951),
-    u = n(496675),
-    c = n(944486),
-    d = n(594174),
-    f = n(70956),
-    _ = n(358085),
-    p = n(875527),
-    h = n(709706),
-    m = n(981631),
-    g = n(231338);
-function E(e, t, n) {
+    l = n(430824),
+    u = n(131951),
+    c = n(496675),
+    d = n(944486),
+    f = n(594174),
+    _ = n(70956),
+    p = n(358085),
+    h = n(875527),
+    m = n(709706),
+    g = n(981631),
+    E = n(231338);
+function v(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -27,75 +28,78 @@ function E(e, t, n) {
         e
     );
 }
-let v = 30 * f.Z.Millis.MINUTE,
-    y = 10 * f.Z.Millis.SECOND,
-    I = new a.Z('VoiceFilterFeedbackManager');
-function T() {
-    return (0, p.wt)({
+let y = 30 * _.Z.Millis.MINUTE,
+    I = 10 * _.Z.Millis.SECOND,
+    T = new a.Z('VoiceFilterFeedbackManager');
+function b() {
+    return (0, h.wt)({
         location: 'VoiceFilterFeedbackManager',
         autoTrackExposure: !1
     });
 }
-class b extends r.Z {
+class S extends r.Z {
     handleMediaEngineStoreChange() {
-        T() && this.maybeQueueFeedbackMessage();
+        b() && this.maybeQueueFeedbackMessage();
     }
     maybeQueueFeedbackMessage() {
-        if (!T()) return;
+        if (!b()) return;
         clearTimeout(this.queuedSendTimeout);
-        let e = d.default.getCurrentUser();
-        !((null == e ? void 0 : e.isStaff()) !== !0 || null == l.Z.getActiveVoiceFilter() || l.Z.isSelfMute() || l.Z.isSelfMutedTemporarily()) &&
+        let e = f.default.getCurrentUser();
+        !((null == e ? void 0 : e.isStaff()) !== !0 || null == u.Z.getActiveVoiceFilter() || u.Z.isSelfMute() || u.Z.isSelfMutedTemporarily()) &&
             (this.queuedSendTimeout = setTimeout(() => {
                 this._maybeSendFeedbackMessage();
-            }, y));
+            }, I));
     }
     _maybeSendFeedbackMessage() {
         var e, t, n;
-        let r = l.Z.getActiveVoiceFilter();
+        let r = u.Z.getActiveVoiceFilter();
         if (null == r) return;
-        let a = o.Z.getChannel(c.Z.getVoiceChannelId());
-        if (null == a || a.type === m.d4z.GUILD_STAGE_VOICE) return;
-        if (!u.Z.can(g.Pl.SEND_MESSAGES, a)) {
-            I.info('Skipping vf feedback message due to no send permission');
+        let a = o.Z.getChannel(d.Z.getVoiceChannelId());
+        if (null == a) return;
+        let f = a.getGuildId(),
+            _ = l.Z.getGuild(f);
+        if (null == _ || !_.hasFeature(g.oNc.INTERNAL_EMPLOYEE_ONLY) || a.type === g.d4z.GUILD_STAGE_VOICE) return;
+        if (!c.Z.can(E.Pl.SEND_MESSAGES, a)) {
+            T.info('Skipping vf feedback message due to no send permission');
             return;
         }
-        let d = ''.concat(a.id, ':').concat(r),
-            f = this.lastSentTimestamp.get(d);
-        if (null != f) {
-            let e = Date.now() - f;
-            if (e - f < v) {
-                I.info('Skipping vf feedback message due to cooldown', e);
+        let h = ''.concat(a.id, ':').concat(r),
+            v = this.lastSentTimestamp.get(h);
+        if (null != v) {
+            let e = Date.now() - v;
+            if (e - v < y) {
+                T.info('Skipping vf feedback message due to cooldown', e);
                 return;
             }
         }
-        let p = h.Z.getVoiceFilter(r),
-            E = null !== (n = null == p ? void 0 : p.name) && void 0 !== n ? n : "Don't Know",
-            y = l.Z.getInputDeviceId(),
-            T = null === (e = l.Z.getInputDevices()[y]) || void 0 === e ? void 0 : e.name,
-            b = l.Z.getOutputDeviceId(),
-            S = new URLSearchParams({
+        let I = m.Z.getVoiceFilter(r),
+            b = null !== (n = null == I ? void 0 : I.name) && void 0 !== n ? n : "Don't Know",
+            S = u.Z.getInputDeviceId(),
+            A = null === (e = u.Z.getInputDevices()[S]) || void 0 === e ? void 0 : e.name,
+            N = u.Z.getOutputDeviceId(),
+            C = new URLSearchParams({
                 usp: 'pp_url',
-                'entry.1110868176': E,
-                'entry.51879725': T,
-                'entry.45023460': null === (t = l.Z.getOutputDevices()[b]) || void 0 === t ? void 0 : t.name,
-                'entry.1264180151': (0, _.isWindows)() ? 'Windows' : (0, _.isMac)() ? 'Mac' : (0, _.getPlatformName)()
+                'entry.1110868176': b,
+                'entry.51879725': A,
+                'entry.45023460': null === (t = u.Z.getOutputDevices()[N]) || void 0 === t ? void 0 : t.name,
+                'entry.1264180151': (0, p.isWindows)() ? 'Windows' : (0, p.isMac)() ? 'Mac' : (0, p.getPlatformName)()
             }),
-            A = 'https://docs.google.com/forms/d/e/1FAIpQLSfiEURI77Kv0pp4DR8XAaZ0eou7fmdJ6tuU2lWkNyD_aiknsg/viewform?'.concat(S);
-        i.Z.sendMessage(a.id, s.ZP.parse(a, "-# STAFF TESTING\n<:voicefilter:1336871150252458015> I'm using the **".concat(E, '** voice filter.\nHow does it sound? [Please share feedback](').concat(A, ').'))), this.lastSentTimestamp.set(d, Date.now());
+            R = 'https://docs.google.com/forms/d/e/1FAIpQLSfiEURI77Kv0pp4DR8XAaZ0eou7fmdJ6tuU2lWkNyD_aiknsg/viewform?'.concat(C);
+        i.Z.sendMessage(a.id, s.ZP.parse(a, "-# STAFF TESTING\n<:voicefilter:1336871150252458015> I'm using the **".concat(b, '** voice filter.\nHow does it sound? [Please share feedback](').concat(R, ').'))), this.lastSentTimestamp.set(h, Date.now());
     }
     constructor(...e) {
         super(...e),
-            E(this, 'actions', {
+            v(this, 'actions', {
                 VOICE_FILTER_APPLIED: () => this.maybeQueueFeedbackMessage(),
                 VOICE_CHANNEL_SELECT: () => this.maybeQueueFeedbackMessage()
             }),
-            E(
+            v(
                 this,
                 'stores',
-                new Map().set(l.Z, () => this.handleMediaEngineStoreChange())
+                new Map().set(u.Z, () => this.handleMediaEngineStoreChange())
             ),
-            E(this, 'lastSentTimestamp', new Map()),
-            E(this, 'queuedSendTimeout', void 0);
+            v(this, 'lastSentTimestamp', new Map()),
+            v(this, 'queuedSendTimeout', void 0);
     }
 }
-let S = new b();
+let A = new S();
