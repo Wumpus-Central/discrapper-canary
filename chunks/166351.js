c7196987d7a40d62f9ace4098d355c6533238b38
DiscrapperManager <admin@minerpl.xyz>
2025-02-14 00:03:48 +0000

    canaryRelease: 368651
    Main scripts: 3
    Lazy Loaded Scripts: 920
    Chunks:
     * Main Files: 12176
     * Scripts: 9906
    Strings: 0
    Routes: 699
    Experiments Count: 400

--- chunks/166351.js
+++ chunks/166351.js
@@ -1,32 +1,37 @@
-i.d(e, { Z: () => p });
+i.d(e, { Z: () => m });
 var t = i(200651),
-    r = i(192379),
-    a = i(120356),
-    s = i.n(a),
-    o = i(793030),
+    a = i(192379),
+    r = i(120356),
+    o = i.n(r),
+    s = i(793030),
     c = i(481060),
     l = i(413335),
     d = i(773),
     u = i(394512),
     x = i(306685),
-    m = i(388032),
+    p = i(388032),
     v = i(14573);
-function p(n) {
+function m(n) {
     let { powerup: e } = n,
-        a = (0, u.Z)(e),
-        p = r.useCallback(() => {
-            (0, c.ZDy)(async () => {
-                let { default: n } = await i.e('78718').then(i.bind(i, 640139));
-                return (i) =>
-                    (0, t.jsx)(n, {
-                        powerup: e,
-                        ...i
+        { activatePowerup: r, deactivatePowerup: m } = (0, u.Z)(e),
+        j = a.useCallback(
+            (n) => {
+                n.stopPropagation(),
+                    (0, c.ZDy)(async () => {
+                        let { default: n } = await i.e('78718').then(i.bind(i, 640139));
+                        return (i) =>
+                            (0, t.jsx)(n, {
+                                powerup: e,
+                                ...i
+                            });
                     });
-            });
-        }, [e]);
-    return (0, t.jsxs)(c.P3F, {
-        onClick: p,
-        className: s()(v.container, { [v.active]: e.active }),
+            },
+            [e]
+        );
+    return (0, t.jsxs)(c.kL8, {
+        'aria-label': e.title,
+        onClick: j,
+        className: o()(v.container, { [v.active]: e.active }),
         children: [
             (0, t.jsx)('div', {
                 className: v.imageContainer,
@@ -38,19 +43,19 @@ function p(n) {
                     (0, t.jsxs)('div', {
                         className: v.headerContainer,
                         children: [
-                            (0, t.jsx)(o.X6, {
+                            (0, t.jsx)(s.X6, {
                                 variant: 'heading-md/bold',
                                 children: e.title
                             }),
                             e.bundle &&
-                                (0, t.jsx)(o.xv, {
-                                    className: s()(v.bundle, { [v.active]: e.active }),
+                                (0, t.jsx)(s.xv, {
+                                    className: o()(v.bundle, { [v.active]: e.active }),
                                     variant: 'text-xs/semibold',
-                                    children: m.intl.string(x.Z.j2jUjY)
+                                    children: p.intl.string(x.Z.j2jUjY)
                                 })
                         ]
                     }),
-                    (0, t.jsx)(o.xv, {
+                    (0, t.jsx)(s.xv, {
                         className: v.description,
                         variant: 'text-sm/medium',
                         children: e.description
@@ -62,7 +67,7 @@ function p(n) {
                                 className: v.inline,
                                 children: [
                                     (0, t.jsx)(l.Z, {}),
-                                    (0, t.jsx)(o.xv, {
+                                    (0, t.jsx)(s.xv, {
                                         tag: 'div',
                                         variant: 'heading-md/bold',
                                         children: e.cost
@@ -77,10 +82,10 @@ function p(n) {
                                             size: 'xs',
                                             color: c.TVs.colors.TEXT_POSITIVE
                                         }),
-                                        (0, t.jsx)(o.xv, {
+                                        (0, t.jsx)(s.xv, {
                                             color: 'text-positive',
                                             variant: 'text-sm/bold',
-                                            children: m.intl.string(x.Z.FFLkm5)
+                                            children: p.intl.string(x.Z.FFLkm5)
                                         })
                                     ]
                                 })
@@ -93,7 +98,8 @@ function p(n) {
                 children: (0, t.jsx)(d.Z, {
                     skuId: e.skuId,
                     active: e.active,
-                    onPrimaryClick: a
+                    onPrimaryClick: r,
+                    onSecondaryClick: m
                 })
             })
         ]
--- chunks/394512.js
+++ chunks/394512.js
@@ -1,16 +1,25 @@
-i.d(e, { Z: () => s });
+i.d(e, { Z: () => o });
 var t = i(200651),
-    r = i(192379),
-    a = i(481060);
-function s(n) {
-    return r.useCallback(() => {
-        (0, a.ZDy)(async () => {
-            let { default: e } = await i.e('13965').then(i.bind(i, 666083));
-            return (i) =>
-                (0, t.jsx)(e, {
-                    powerup: n,
-                    ...i
-                });
-        });
-    }, [n]);
+    a = i(192379),
+    r = i(481060);
+function o(n) {
+    return {
+        activatePowerup: a.useCallback(
+            (e) => {
+                e.stopPropagation(),
+                    (0, r.ZDy)(async () => {
+                        let { default: e } = await i.e('13965').then(i.bind(i, 666083));
+                        return (i) =>
+                            (0, t.jsx)(e, {
+                                powerup: n,
+                                ...i
+                            });
+                    });
+            },
+            [n]
+        ),
+        deactivatePowerup: a.useCallback((n) => {
+            n.stopPropagation();
+        }, [])
+    };
 }
