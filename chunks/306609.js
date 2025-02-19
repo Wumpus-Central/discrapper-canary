a2dc116803bd3cdb0d2782f76365cdf7daf7ed3f
DiscrapperManager <admin@minerpl.xyz>
2025-02-19 18:02:50 +0000

    canaryRelease: 369781
    Main scripts: 3
    Lazy Loaded Scripts: 932
    Chunks:
     * Main Files: 12175
     * Scripts: 9957
    Strings: 0
    Routes: 699
    Experiments Count: 403

--- chunks/306609.js
+++ chunks/306609.js
@@ -1,4 +1,4 @@
-n.d(t, { l: () => Z }), n(47120);
+n.d(t, { l: () => R }), n(47120);
 var r = n(200651),
     i = n(192379),
     a = n(120356),
@@ -19,8 +19,8 @@ var r = n(200651),
     y = n(74538),
     x = n(709706),
     O = n(378441),
-    E = n(799143),
-    j = n(358820),
+    j = n(799143),
+    E = n(358820),
     N = n(448697),
     C = n(365831),
     I = n(639539),
@@ -30,46 +30,110 @@ var r = n(200651),
     A = n(388032),
     w = n(848980);
 function Z(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        'function' == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                })
+            )),
+            r.forEach(function (t) {
+                var r;
+                (r = n[t]),
+                    t in e
+                        ? Object.defineProperty(e, t, {
+                              value: r,
+                              enumerable: !0,
+                              configurable: !0,
+                              writable: !0
+                          })
+                        : (e[t] = r);
+            });
+    }
+    return e;
+}
+function k(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : (function (e, t) {
+                  var n = Object.keys(e);
+                  if (Object.getOwnPropertySymbols) {
+                      var r = Object.getOwnPropertySymbols(e);
+                      n.push.apply(n, r);
+                  }
+                  return n;
+              })(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+function R(e) {
     let { onSettingsButtonClick: t, wide: n, showOutputDevices: a = !1 } = e,
-        [Z, k] = i.useState(''),
-        { analyticsLocations: R } = (0, m.ZP)(p.Z.VOICE_FILTER_POPOUT),
-        L = i.useRef(null),
-        D = (0, u.Z)({
+        [R, L] = i.useState(''),
+        { analyticsLocations: D } = (0, m.ZP)(p.Z.VOICE_FILTER_POPOUT),
+        M = i.useRef(null),
+        W = (0, u.Z)({
             minDimension: 400,
             maxDimension: 600,
-            resizableDomNodeRef: L,
+            resizableDomNodeRef: M,
             onElementResize: (e, t) => {},
             onElementResizeEnd: (e) => {},
             orientation: u.y.VERTICAL_TOP
         }),
-        M = (0, s.e7)([_.default], () => (0, v.I5)(_.default.getCurrentUser())),
-        W = (0, s.e7)([x.Z], () => x.Z.getCatalogUpdateTime()),
-        [F, U] = i.useState(!1),
+        F = (0, s.e7)([_.default], () => (0, v.I5)(_.default.getCurrentUser())),
+        U = (0, s.e7)([x.Z], () => x.Z.getCatalogUpdateTime()),
         [B, G] = i.useState(!1),
-        { activeVoice: H } = (0, O.o)(),
-        V = (0, c.q_F)({
-            transform: F ? 'translateY(-'.concat(8, 'px) scale(').concat(0.96, ')') : 'translateY(0px) scale(1)',
-            config: { duration: 200 }
+        [H, V] = i.useState(!1),
+        { activeVoice: z } = (0, O.o)(),
+        K = (0, c.q_F)({
+            transform: B ? 'translateY(-$8px) scale('.concat(0.96, ')') : 'translateY(0px) scale(1)',
+            config: {
+                duration: 150,
+                mass: 1,
+                stiffness: 711.1,
+                damping: 40
+            }
         }),
-        z = (0, c.q_F)({
-            transform: F ? 'translateY(0px)' : 'translateY('.concat(50, 'px)'),
-            config: { duration: 200 }
+        Y = (0, c.q_F)({
+            transform: B ? 'translateY(0px)' : 'translateY(50px)',
+            config: {
+                duration: 150,
+                mass: 1,
+                stiffness: 711.1,
+                damping: 40
+            }
         }),
-        K = i.useRef(null);
+        X = (0, c.q_F)({
+            to: {
+                bottom: '' !== R ? 'translateY(75px)' : 'translateY(50px)',
+                opacity: +('' === R)
+            },
+            config: {
+                tension: 280,
+                friction: 24,
+                duration: 200
+            }
+        }),
+        q = i.useRef(null);
     return (
         (0, d.ZP)(() => {
-            (0, j.r5)(), b.default.track(P.rMx.VOICE_FILTER_PICKER_OPENED, { active_voice_filter_id: null != H ? H : null });
+            (0, E.r5)(), b.default.track(P.rMx.VOICE_FILTER_PICKER_OPENED, { active_voice_filter_id: null != z ? z : null });
         }),
         (0, r.jsx)(m.Gt, {
-            value: R,
+            value: D,
             children: (0, r.jsxs)('div', {
-                ref: L,
+                ref: M,
                 className: o()(w.voiceFiltersPopout, { [w.wide]: n }),
                 style: { height: 500 },
                 children: [
                     (0, r.jsx)('div', {
                         className: w.resizeHandle,
-                        onMouseDown: D,
+                        onMouseDown: W,
                         children: (0, r.jsx)('div', { className: w.resizePill })
                     }),
                     (0, r.jsx)(c.E1j, {
@@ -77,44 +141,45 @@ function Z(e) {
                         placeholder: A.NW.string(A.t.hHCZJS),
                         className: w.voiceFiltersHeader,
                         autoFocus: !0,
-                        onChange: k,
-                        query: Z,
-                        onClear: () => k('')
+                        onChange: L,
+                        query: R,
+                        onClear: () => L('')
                     }),
                     (0, r.jsx)(S.w, {
-                        query: Z,
+                        query: R,
                         columns: n ? 5 : 4,
                         handleScroll: (e) => {
-                            let t = K.current;
-                            if (null == t || (U(e > 0), B)) return;
+                            let t = q.current;
+                            if (null == t || (G(e > 0), H)) return;
                             let { height: n, totalHeight: r } = t.getListDimensions();
-                            e < r - n || (G(!0), b.default.track(P.rMx.VOICE_FILTER_PICKER_SCROLLED));
+                            e < r - n || (V(!0), b.default.track(P.rMx.VOICE_FILTER_PICKER_SCROLLED));
                         },
-                        voiceListRef: K
+                        voiceListRef: q
                     }),
-                    !M &&
-                        (0, r.jsxs)('div', {
+                    !F &&
+                        (0, r.jsxs)(l.animated.div, {
+                            style: k(Z({}, X), { display: X.opacity.to((e) => (0 === e ? 'none' : 'block')) }),
                             className: w.upsellCountdownContainer,
                             children: [
-                                null != W &&
+                                null != U &&
                                     (0, r.jsx)(l.animated.div, {
-                                        style: V,
+                                        style: K,
                                         children: (0, r.jsx)(I.J, {
-                                            catalogUpdateTime: W,
-                                            isScrolled: F
+                                            catalogUpdateTime: U,
+                                            isScrolled: B
                                         })
                                     }),
                                 (0, r.jsx)('div', {
                                     className: w.upsellContainer,
                                     children: (0, r.jsx)(l.animated.div, {
-                                        style: z,
+                                        style: Y,
                                         children: (0, r.jsx)(h.p, {
                                             showUpsell: !0,
                                             text: A.NW.format(A.t['XMDm8/'], {
                                                 nitroTierName: (0, y.Px)(T.p9.TIER_2),
                                                 onClick: () => (0, g.i)()
                                             }),
-                                            button: A.NW.string(A.t.YScQSE),
+                                            button: A.NW.string(A.t.cRCCJy),
                                             buttonAnalyticsObject: { section: P.jXE.VOICE_FILTERS_FLOATING_UPSELL },
                                             showShadow: !1,
                                             position: 'inline',
@@ -130,10 +195,10 @@ function Z(e) {
                     (0, r.jsx)(N.R, {}),
                     (0, r.jsx)(C.y, {}),
                     (0, r.jsxs)('div', {
-                        className: o()(w.voiceFiltersFooter, { [w.hasActiveVoice]: null != H }),
+                        className: o()(w.voiceFiltersFooter, { [w.hasActiveVoice]: null != z }),
                         children: [
-                            (0, r.jsx)(E.Y, {}),
-                            a && (0, r.jsx)(E.a, {}),
+                            (0, r.jsx)(j.Y, {}),
+                            a && (0, r.jsx)(j.a, {}),
                             null != t &&
                                 (0, r.jsx)(c.yRy, {
                                     position: 'top',
@@ -143,67 +208,26 @@ function Z(e) {
                                             renderInputVolume: !0,
                                             onClose: t
                                         }),
-                                    children: (e) => {
-                                        var t, n;
-                                        return (0, r.jsx)(
+                                    children: (e) =>
+                                        (0, r.jsx)(
                                             c.zxk,
-                                            ((t = (function (e) {
-                                                for (var t = 1; t < arguments.length; t++) {
-                                                    var n = null != arguments[t] ? arguments[t] : {},
-                                                        r = Object.keys(n);
-                                                    'function' == typeof Object.getOwnPropertySymbols &&
-                                                        (r = r.concat(
-                                                            Object.getOwnPropertySymbols(n).filter(function (e) {
-                                                                return Object.getOwnPropertyDescriptor(n, e).enumerable;
-                                                            })
-                                                        )),
-                                                        r.forEach(function (t) {
-                                                            var r;
-                                                            (r = n[t]),
-                                                                t in e
-                                                                    ? Object.defineProperty(e, t, {
-                                                                          value: r,
-                                                                          enumerable: !0,
-                                                                          configurable: !0,
-                                                                          writable: !0
-                                                                      })
-                                                                    : (e[t] = r);
-                                                        });
-                                                }
-                                                return e;
-                                            })({}, e)),
-                                            (n = n =
-                                                {
-                                                    size: c.zxk.Sizes.MIN,
-                                                    look: c.zxk.Looks.BLANK,
-                                                    grow: !1,
-                                                    className: w.voiceFiltersFooterButton,
-                                                    innerClassName: w.voiceFiltersFooterButtonInner,
-                                                    onClick: (t) => {
-                                                        var n;
-                                                        b.default.track(P.rMx.VOICE_FILTER_VOICE_SETTINGS_CLICKED, { active_voice_filter_id: null != H ? H : null }), null === (n = e.onClick) || void 0 === n || n.call(e, t);
-                                                    },
-                                                    children: (0, r.jsx)(c.Huf, {
-                                                        size: 'custom',
-                                                        width: 20,
-                                                        height: 20
-                                                    })
-                                                }),
-                                            Object.getOwnPropertyDescriptors
-                                                ? Object.defineProperties(t, Object.getOwnPropertyDescriptors(n))
-                                                : (function (e, t) {
-                                                      var n = Object.keys(e);
-                                                      if (Object.getOwnPropertySymbols) {
-                                                          var r = Object.getOwnPropertySymbols(e);
-                                                          n.push.apply(n, r);
-                                                      }
-                                                      return n;
-                                                  })(Object(n)).forEach(function (e) {
-                                                      Object.defineProperty(t, e, Object.getOwnPropertyDescriptor(n, e));
-                                                  }),
-                                            t)
-                                        );
-                                    }
+                                            k(Z({}, e), {
+                                                size: c.zxk.Sizes.MIN,
+                                                look: c.zxk.Looks.BLANK,
+                                                grow: !1,
+                                                className: w.voiceFiltersFooterButton,
+                                                innerClassName: w.voiceFiltersFooterButtonInner,
+                                                onClick: (t) => {
+                                                    var n;
+                                                    b.default.track(P.rMx.VOICE_FILTER_VOICE_SETTINGS_CLICKED, { active_voice_filter_id: null != z ? z : null }), null === (n = e.onClick) || void 0 === n || n.call(e, t);
+                                                },
+                                                children: (0, r.jsx)(c.Huf, {
+                                                    size: 'custom',
+                                                    width: 20,
+                                                    height: 20
+                                                })
+                                            })
+                                        )
                                 })
                         ]
                     })
