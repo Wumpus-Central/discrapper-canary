8e0558657b33bf160e297a03e40e642740a198f0
DiscrapperManager <admin@minerpl.xyz>
2025-02-19 20:26:04 +0000

    canaryRelease: 369871
    Main scripts: 3
    Lazy Loaded Scripts: 932
    Chunks:
     * Main Files: 12186
     * Scripts: 9957
    Strings: 0
    Routes: 699
    Experiments Count: 404

--- chunks/40614.js
+++ chunks/40614.js
@@ -1,36 +1,41 @@
 n.d(t, { V: () => g });
-var i = n(200651),
-    l = n(481060),
-    r = n(230711),
-    a = n(100527),
-    o = n(906732),
+var r = n(200651),
+    i = n(481060),
+    l = n(230711),
+    o = n(100527),
+    a = n(906732),
     s = n(667105),
     c = n(695346),
-    d = n(316496),
-    u = n(981631),
-    h = n(46140),
-    p = n(388032),
-    m = n(868282);
+    u = n(316496),
+    d = n(981631),
+    p = n(46140),
+    h = n(388032),
+    f = n(675277);
 let g = (e) => {
     let { onClick: t } = e,
-        { analyticsLocations: n } = (0, o.ZP)(a.Z.NOT_SHARING_LINK),
-        { rtcPopoutEnabled: g } = (0, d.S)('not sharing link'),
-        f = c.G6.useSetting(),
-        _ = (0, s.Ws)({ location: h.dr.CONFLICT_CHECKS });
-    return !g || f
+        { analyticsLocations: n } = (0, a.ZP)(o.Z.NOT_SHARING_LINK),
+        { rtcPopoutEnabled: g } = (0, u.S)('not sharing link'),
+        m = c.G6.useSetting(),
+        b = (0, s.Ws)({ location: p.dr.CONFLICT_CHECKS });
+    return !g || m
         ? null
-        : (0, i.jsx)(l.P3F, {
+        : (0, r.jsx)(i.P3F, {
               onClick: () => {
-                  _ ? t() : r.Z.open(u.oAB.ACTIVITY_PRIVACY, null, { analyticsLocations: n });
+                  b ? t() : l.Z.open(d.oAB.ACTIVITY_PRIVACY, null, { analyticsLocations: n });
               },
-              className: m.container,
-              children: (0, i.jsxs)(l.Text, {
+              className: f.container,
+              children: (0, r.jsxs)(i.Text, {
                   variant: 'text-xs/medium',
-                  className: m.text,
+                  className: f.text,
                   color: 'header-secondary',
                   children: [
-                      p.intl.string(p.t.jfrLLS),
-                      (0, i.jsx)(l.Fbu, {
+                      (0, r.jsx)(i.kZF, {
+                          size: 'xxs',
+                          color: 'currentColor',
+                          className: f.iconEye
+                      }),
+                      h.NW.string(h.t.jfrLLS),
+                      (0, r.jsx)(i.Fbu, {
                           size: 'xxs',
                           color: 'currentColor'
                       })
