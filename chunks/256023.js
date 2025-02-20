3c0ab78e9912f5bfea34e9f1b5a6d84830ec02de
DiscrapperManager <admin@minerpl.xyz>
2025-02-20 16:00:41 +0000

    canaryRelease: 370164
    Main scripts: 3
    Lazy Loaded Scripts: 931
    Chunks:
     * Main Files: 12213
     * Scripts: 9953
    Strings: 0
    Routes: 699
    Experiments Count: 406

---
+++ chunks/256023.js
@@ -0,0 +1,22 @@
+n.d(t, { p: () => l });
+var r = n(987170),
+    i = n(987338);
+let l = (0, r.Z)({
+    kind: 'user',
+    id: '2025-02_test_first_bucket_allocation',
+    label: 'Test First Bucket Allocation Experiment',
+    defaultConfig: { enabled: !1 },
+    commonTriggerPoint: i.$P.CONNECTION_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: 'Variation with first treatment enabled',
+            config: { enabled: !0 }
+        },
+        {
+            id: 2,
+            label: 'Variation with second treatment enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
