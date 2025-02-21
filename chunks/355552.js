7cf80dc9aed8d61748efcaae3173a32b2c7eee49
DiscrapperManager <admin@minerpl.xyz>
2025-02-21 03:28:46 +0000

    canaryRelease: 370523
    Main scripts: 3
    Lazy Loaded Scripts: 934
    Chunks:
     * Main Files: 12212
     * Scripts: 9965
    Strings: 0
    Routes: 699
    Experiments Count: 406

--- chunks/355552.js
+++ chunks/355552.js
@@ -1,5 +1,7 @@
-n.d(t, { s: () => r });
-let r = (0, n(818083).B)({
+n.d(t, { D: () => a });
+var r = n(818083),
+    i = n(984134);
+let o = (0, r.B)({
     kind: 'user',
     id: '2025-01_audio_nack',
     label: 'Audio NACK ',
@@ -12,3 +14,14 @@ let r = (0, n(818083).B)({
         }
     ]
 });
+function a(e) {
+    let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
+        { isInHoldout: n } = i.h.getCurrentConfig({ location: e }, { autoTrackExposure: t });
+    return o.getCurrentConfig(
+        { location: e },
+        {
+            disable: n,
+            autoTrackExposure: t
+        }
+    );
+}
