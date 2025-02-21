91a6c9cc708dea067eefe4354a696eeb9e47ad31
DiscrapperManager <admin@minerpl.xyz>
2025-02-21 00:50:36 +0000

    canaryRelease: 370462
    Main scripts: 3
    Lazy Loaded Scripts: 934
    Chunks:
     * Main Files: 12211
     * Scripts: 9965
    Strings: 0
    Routes: 699
    Experiments Count: 406

--- chunks/200100.js
+++ chunks/200100.js
@@ -1,13 +1,11 @@
-var r = {
-    settingsColumn: 'settingsColumn_fa2640 ' + n(229083).customColumn,
-    settingsContainer: 'settingsContainer_fa2640 ' + n(229083).customContainer,
-    settingsScroller: 'settingsScroller_fa2640 ' + n(229083).customScroller + ' ' + n(229083).contentRegionScroller,
+e.exports = {
+    override: 'override_fa2640',
+    settingsColumn: 'settingsColumn_fa2640',
     settingsHorizontalScroller: 'settingsHorizontalScroller_fa2640',
-    settingsContent: 'settingsContent_fa2640 ' + n(229083).contentColumnDefault + ' settingsColumn_fa2640 ' + n(229083).customColumn,
+    settingsContent: 'settingsContent_fa2640',
     header: 'header_fa2640',
     tableContainer: 'tableContainer_fa2640',
     mainTableContainer: 'mainTableContainer_fa2640',
     movedTable: 'movedTable_fa2640',
     noResultsContainer: 'noResultsContainer_fa2640'
 };
-e.exports = r;
--- chunks/313364.js
+++ chunks/313364.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => E });
+n.d(t, { Z: () => S });
 var r = n(200651),
     i = n(192379),
     s = n(120356),
@@ -22,14 +22,15 @@ var r = n(200651),
     O = n(918192),
     y = n(981631),
     C = n(388032),
-    I = n(200100);
-function E(e) {
+    I = n(200100),
+    E = n(229083);
+function S(e) {
     let { guildId: t } = e,
         n = (0, l.e7)([p.Z], () => p.Z.getGuild(t)),
         { analyticsLocations: s } = (0, d.ZP)(c.Z.GUILD_SETTINGS_MEMBERS_PAGE),
-        E = !1 == !!(null == n ? void 0 : n.hasFeature(y.oNc.ENABLED_MODERATION_EXPERIENCE_FOR_NON_COMMUNITY)),
-        S = (0, l.e7)([g.Z], () => g.Z.getEstimatedMemberSearchCountByGuildId(t), [t]),
-        T = i.useCallback(
+        S = !1 == !!(null == n ? void 0 : n.hasFeature(y.oNc.ENABLED_MODERATION_EXPERIENCE_FOR_NON_COMMUNITY)),
+        T = (0, l.e7)([g.Z], () => g.Z.getEstimatedMemberSearchCountByGuildId(t), [t]),
+        P = i.useCallback(
             (e) => {
                 null != e &&
                     (0, m.openUserProfileModal)({
@@ -41,32 +42,32 @@ function E(e) {
             },
             [s]
         ),
-        P = i.useRef(null),
-        w = i.useCallback(() => {
+        w = i.useRef(null),
+        R = i.useCallback(() => {
             var e;
-            null === (e = P.current) || void 0 === e || e.resetSearchText();
+            null === (e = w.current) || void 0 === e || e.resetSearchText();
         }, []);
     if (null == n) return null;
-    let R = (0, j.xb)(!1, !1, S);
+    let D = (0, j.xb)(!1, !1, T);
     return (0, r.jsx)(d.Gt, {
         value: s,
         children: (0, r.jsx)('div', {
-            className: I.settingsColumn,
+            className: a()(E.customColumn, I.override, I.settingsColumn),
             children: (0, r.jsx)('div', {
-                className: I.settingsContainer,
+                className: E.customContainer,
                 children: (0, r.jsx)(o.w0Z, {
-                    className: I.settingsScroller,
+                    className: E.customScroller,
                     orientation: 'auto',
                     children: (0, r.jsx)(o.w0Z, {
                         className: I.settingsHorizontalScroller,
                         orientation: 'auto',
                         children: (0, r.jsxs)('main', {
-                            className: I.settingsContent,
+                            className: a()(E.customColumn, E.contentColumnDefault, I.override, I.settingsColumn, I.settingsContent),
                             children: [
                                 (0, r.jsx)(
                                     o.hjN,
                                     {
-                                        className: a()(I.header),
+                                        className: I.header,
                                         children: (0, r.jsx)(o.vwX, {
                                             tag: o.RB0.H1,
                                             children: C.NW.string(C.t['S40K6+'])
@@ -78,37 +79,34 @@ function E(e) {
                                     o.hjN,
                                     {
                                         children: (0, r.jsxs)('div', {
-                                            className: a()(I.tableContainer),
+                                            className: I.tableContainer,
                                             children: [
                                                 (0, r.jsx)(b.Z, { guild: n }),
                                                 (0, r.jsx)(f.Z, { guild: n }),
-                                                E
+                                                S
                                                     ? (0, r.jsxs)('div', {
-                                                          className: a()(I.mainTableContainer),
+                                                          className: I.mainTableContainer,
                                                           children: [
                                                               (0, r.jsx)(_.Z, {
                                                                   guild: n,
-                                                                  ref: P
+                                                                  ref: w
                                                               }),
                                                               (0, r.jsx)(N.Z, {
                                                                   guild: n,
-                                                                  searchState: R,
+                                                                  searchState: D,
                                                                   compact: !0,
-                                                                  onSelectRow: T,
-                                                                  onResetForNewMembers: w
+                                                                  onSelectRow: P,
+                                                                  onResetForNewMembers: R
                                                               }),
-                                                              R !== j.po.SUCCESS_STILL_INDEXING && (0, r.jsx)(v.Z, { guildId: n.id })
+                                                              D !== j.po.SUCCESS_STILL_INDEXING && (0, r.jsx)(v.Z, { guildId: n.id })
                                                           ]
                                                       })
                                                     : (0, r.jsx)('div', {
-                                                          className: a()(I.movedTable),
+                                                          className: I.movedTable,
                                                           children: (0, r.jsxs)('div', {
-                                                              className: a()(I.noResultsContainer),
+                                                              className: I.noResultsContainer,
                                                               children: [
-                                                                  (0, r.jsx)('div', {
-                                                                      className: a()(I.__invalid_noResultsIconContainer),
-                                                                      children: (0, r.jsx)(O.Z, {})
-                                                                  }),
+                                                                  (0, r.jsx)('div', { children: (0, r.jsx)(O.Z, {}) }),
                                                                   (0, r.jsx)(o.Text, {
                                                                       variant: 'text-md/normal',
                                                                       color: 'text-muted',
