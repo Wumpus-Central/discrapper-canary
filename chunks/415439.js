ddc8b7600ef0909ccb316e61c3ff5f3ceb4266a8
DiscrapperManager <admin@minerpl.xyz>
2025-02-22 01:12:21 +0000

    canaryRelease: 370902
    Main scripts: 3
    Lazy Loaded Scripts: 936
    Chunks:
     * Main Files: 12212
     * Scripts: 9977
    Strings: 0
    Routes: 699
    Experiments Count: 407

--- chunks/415439.js
+++ chunks/415439.js
@@ -1,102 +1,87 @@
 n.d(t, {
-    FW: () => v,
-    Kt: () => N,
-    ZC: () => y
-}),
-    n(230036),
-    n(47120);
+    FW: () => O,
+    Kt: () => E,
+    ZC: () => N
+});
 var r = n(442837),
     i = n(493683),
     l = n(636449),
     o = n(122613),
-    a = n(983695),
-    s = n(100527),
-    c = n(87005),
-    u = n(895924),
-    d = n(812236),
-    p = n(740492),
-    h = n(973616),
-    f = n(592125),
-    g = n(626135),
-    m = n(823379),
-    b = n(728345),
-    _ = n(812206),
-    E = n(835473),
-    O = n(981631);
-async function N(e, t, n) {
+    a = n(100527),
+    s = n(87005),
+    c = n(895924),
+    u = n(812236),
+    d = n(740492),
+    p = n(973616),
+    h = n(592125),
+    f = n(626135),
+    g = n(823379),
+    m = n(728345),
+    b = n(812206),
+    _ = n(981631);
+async function E(e, t, n) {
     t(e.id);
     let r = e.bot;
     if (null == r)
         try {
-            var a, c, p;
-            r = null !== (p = null === (a = _.Z.getApplication(e.id)) || void 0 === a ? void 0 : a.bot) && void 0 !== p ? p : null === (c = h.ZP.createFromServer(await (0, b.UM)(e.id))) || void 0 === c ? void 0 : c.bot;
+            var s, d, g;
+            r = null !== (g = null === (s = b.Z.getApplication(e.id)) || void 0 === s ? void 0 : s.bot) && void 0 !== g ? g : null === (d = p.ZP.createFromServer(await (0, m.UM)(e.id))) || void 0 === d ? void 0 : d.bot;
         } catch (e) {
             t(void 0);
             return;
         }
     if (null != r) {
-        let a;
-        let c = null == f.Z.getDMFromUserId(r.id);
+        let s;
+        let d = null == h.Z.getDMFromUserId(r.id);
         try {
-            a = (0, l.R)() ? await i.Z.getOrEnsurePrivateChannel(r.id) : await i.Z.openPrivateChannel([r.id], !1, !1, s.Z.APP_DMS_QUICK_LAUNCHER);
+            s = (0, l.R)() ? await i.Z.getOrEnsurePrivateChannel(r.id) : await i.Z.openPrivateChannel([r.id], !1, !1, a.Z.APP_DMS_QUICK_LAUNCHER);
         } catch (e) {
             t(void 0);
         }
         if (
-            (g.default.track(O.rMx.APP_DMS_QUICK_LAUNCHER_CLICKED, {
+            (f.default.track(_.rMx.APP_DMS_QUICK_LAUNCHER_CLICKED, {
                 application_id: e.id,
-                is_new_dm: c,
-                channel_id: a
+                is_new_dm: d,
+                channel_id: s
             }),
-            null == a)
+            null == s)
         ) {
             t(void 0);
             return;
         }
         try {
-            await (0, d.ZP)(a, e.id);
+            await (0, u.ZP)(s, e.id);
         } catch (e) {
-            if (e.message === d.sV) {
+            if (e.message === u.sV) {
                 t(void 0);
                 return;
             }
         }
         await (0, o.Z)({
             targetApplicationId: e.id,
-            channelId: a,
+            channelId: s,
             analyticsLocations: n,
-            commandOrigin: u.bB.APP_DMS_ENTRY_POINT_COMMAND_BUTTON,
+            commandOrigin: c.bB.APP_DMS_ENTRY_POINT_COMMAND_BUTTON,
             onExecutedCallback() {
                 t(void 0);
             }
         });
     } else t(void 0);
 }
-function v() {
-    let e = (0, r.e7)([p.ZP], () => p.ZP.showPlayAgain),
-        { frecentApps: t } = y();
+function O() {
+    let e = (0, r.e7)([d.ZP], () => d.ZP.showPlayAgain),
+        { frecentApps: t } = N();
     return e && t.length > 0;
 }
-function y() {
-    let { frecentApps: e, loading: t } = (0, c.f)({
-            context: { type: 'contextless' },
-            onlyActivityApps: !0,
-            allowCommandFetch: !0,
-            includeAuthorizedAppsAndFetch: !1
-        }),
-        n = e.map((e) => e.id).sort(),
-        r = new Map(
-            (0, E.Z)(n)
-                .filter(m.lm)
-                .map((e) => [e.id, e])
-        );
+function N() {
+    let { frecentApps: e, loading: t } = (0, s.f)({
+        context: { type: 'contextless' },
+        onlyActivityApps: !0,
+        allowCommandFetch: !0,
+        includeAuthorizedAppsAndFetch: !1
+    });
     return {
-        frecentApps: e
-            .map((e) => r.get(e.id))
-            .filter((e) => {
-                var t;
-                return (0, a.Z)(null == e ? void 0 : null === (t = e.embeddedActivityConfig) || void 0 === t ? void 0 : t.supported_platforms);
-            }),
+        frecentApps: e.map((e) => e.application).filter(g.lm),
         loading: t
     };
 }
