967079e279521e46cd86779d1594d387a8874041
DiscrapperManager <admin@minerpl.xyz>
2025-02-24 10:26:30 +0000

    canaryRelease: 370982
    Main scripts: 3
    Lazy Loaded Scripts: 937
    Chunks:
     * Main Files: 12211
     * Scripts: 9980
    Strings: 0
    Routes: 699
    Experiments Count: 407

--- chunks/800869.js
+++ chunks/800869.js
@@ -1,34 +1,43 @@
-t.d(n, { Z: () => d });
+t.d(n, { Z: () => v }), t(47120);
 var i = t(200651),
     r = t(793030),
-    a = t(481060),
-    o = t(783684),
-    s = t(388032),
-    c = t(503388),
-    l = t(719119);
-function d() {
-    return (0, i.jsxs)('div', {
-        className: c.container,
-        children: [
-            (0, i.jsx)('img', {
-                alt: '',
-                className: c.image,
-                src: l
-            }),
-            (0, i.jsx)(r.xv, {
-                color: 'always-white',
-                variant: 'text-md/semibold',
-                children: s.NW.string(o.Z.diMhWV)
-            }),
-            (0, i.jsx)(r.xv, {
-                color: 'always-white',
-                variant: 'text-sm/medium',
-                children: s.NW.string(o.Z.pycxTk)
-            }),
-            (0, i.jsx)(a.olH, {
-                className: c.close,
-                innerClassName: c.innerClose
-            })
-        ]
-    });
+    a = t(704215),
+    o = t(481060),
+    s = t(243778),
+    c = t(921944),
+    l = t(783684),
+    d = t(388032),
+    u = t(503388),
+    x = t(719119);
+function v() {
+    let [e, n] = (0, s.US)([a.z.GUILD_POWERUPS_OVERVIEW_SIDEBAR_COACHMARK], void 0, !1);
+    return null == e || e !== a.z.GUILD_POWERUPS_OVERVIEW_SIDEBAR_COACHMARK
+        ? null
+        : (0, i.jsxs)('div', {
+              className: u.container,
+              children: [
+                  (0, i.jsx)('img', {
+                      alt: '',
+                      className: u.image,
+                      src: x
+                  }),
+                  (0, i.jsx)(r.xv, {
+                      color: 'always-white',
+                      variant: 'text-md/semibold',
+                      children: d.NW.string(l.Z.diMhWV)
+                  }),
+                  (0, i.jsx)(r.xv, {
+                      color: 'always-white',
+                      variant: 'text-sm/medium',
+                      children: d.NW.string(l.Z.pycxTk)
+                  }),
+                  (0, i.jsx)(o.olH, {
+                      className: u.close,
+                      innerClassName: u.innerClose,
+                      onClick: () => {
+                          n(c.L.USER_DISMISS);
+                      }
+                  })
+              ]
+          });
 }
