b8147ed75aa76a961589012f96e16559a1e3aa1f
DiscrapperManager <admin@minerpl.xyz>
2025-02-24 15:27:03 +0000

    canaryRelease: 370993
    Main scripts: 3
    Lazy Loaded Scripts: 937
    Chunks:
     * Main Files: 12211
     * Scripts: 9980
    Strings: 0
    Routes: 699
    Experiments Count: 407

--- chunks/40614.js
+++ chunks/40614.js
@@ -1,40 +1,30 @@
-n.d(t, { V: () => g });
+n.d(t, { V: () => c });
 var r = n(200651),
     i = n(481060),
-    l = n(230711),
-    o = n(100527),
-    a = n(906732),
-    s = n(667105),
-    c = n(695346),
-    u = n(316496),
-    d = n(981631),
-    p = n(46140),
-    h = n(388032),
-    f = n(675277);
-let g = (e) => {
+    l = n(695346),
+    o = n(316496),
+    a = n(388032),
+    s = n(675277);
+let c = (e) => {
     let { onClick: t } = e,
-        { analyticsLocations: n } = (0, a.ZP)(o.Z.NOT_SHARING_LINK),
-        { rtcPopoutEnabled: g } = (0, u.S)('not sharing link'),
-        m = c.G6.useSetting(),
-        b = (0, s.Ws)({ location: p.dr.CONFLICT_CHECKS });
-    return !g || m
+        { rtcPopoutEnabled: n } = (0, o.S)('not sharing link'),
+        c = l.G6.useSetting();
+    return !n || c
         ? null
         : (0, r.jsx)(i.P3F, {
-              onClick: () => {
-                  b ? t() : l.Z.open(d.oAB.ACTIVITY_PRIVACY, null, { analyticsLocations: n });
-              },
-              className: f.container,
+              onClick: t,
+              className: s.container,
               children: (0, r.jsxs)(i.Text, {
                   variant: 'text-xs/medium',
-                  className: f.text,
+                  className: s.text,
                   color: 'header-secondary',
                   children: [
                       (0, r.jsx)(i.kZF, {
                           size: 'xxs',
                           color: 'currentColor',
-                          className: f.iconEye
+                          className: s.iconEye
                       }),
-                      h.NW.string(h.t.jfrLLS),
+                      a.NW.string(a.t.jfrLLS),
                       (0, r.jsx)(i.Fbu, {
                           size: 'xxs',
                           color: 'currentColor'
