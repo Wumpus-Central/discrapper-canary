a735812dfa05189f75236e3174fe2d123a49eab1
DiscrapperManager <admin@minerpl.xyz>
2025-02-26 19:13:55 +0000

    canaryRelease: 372322
    Main scripts: 3
    Lazy Loaded Scripts: 945
    Chunks:
     * Main Files: 11173
     * Scripts: 10954
    Strings: 0
    Routes: 701
    Experiments Count: 404

--- chunks/231541.js
+++ chunks/231541.js
@@ -1,30 +1,27 @@
-n.d(t, { M: () => f });
-var r = n(355467),
-    i = n(594174),
-    l = n(78839),
-    o = n(431),
-    a = n(111361),
-    s = n(74538),
-    c = n(937579),
-    u = n(557017),
-    d = n(474936);
-let p = (e) => {
+n.d(t, { M: () => p });
+var r = n(594174),
+    i = n(78839),
+    l = n(431),
+    o = n(111361),
+    a = n(74538),
+    s = n(557017),
+    c = n(474936);
+let u = (e) => {
         let t = new Date();
         return t.setDate(t.getDate() - 10), e.currentPeriodEnd >= t;
     },
-    h = (e) => {
+    d = (e) => {
         var t;
-        let n = null === (t = (0, s.Af)(e)) || void 0 === t ? void 0 : t.planId;
-        return void 0 === n || s.ZP.getPremiumType(n) !== d.p9.TIER_2;
+        let n = null === (t = (0, a.Af)(e)) || void 0 === t ? void 0 : t.planId;
+        return void 0 === n || a.ZP.getPremiumType(n) !== c.p9.TIER_2;
     };
-async function f(e) {
+function p(e) {
     let { location: t } = e;
-    if (!1 === u.t.getCurrentConfig({ location: t }).enabled) return !1;
-    l.ZP.hasFetchedMostRecentPremiumTypeSubscription() || (await (0, r.ou)()), o.Z.shouldFetchOffer() && (await (0, c.T)('useCollectibleDecoIsEligible'));
-    let n = l.ZP.getMostRecentPremiumTypeSubscription();
+    if (!1 === s.t.getCurrentConfig({ location: t }).enabled) return !1;
+    let n = i.ZP.getMostRecentPremiumTypeSubscription();
     if (null === n) {
-        let e = i.default.getCurrentUser();
-        return !(0, a.I5)(e);
+        let e = r.default.getCurrentUser();
+        return !(0, o.I5)(e);
     }
-    return !!n.isPaused || (n.isEnded ? !(o.Z.hasAnyUnexpiredOffer() || o.Z.hasAnyUnexpiredDiscountOffer() || p(n)) : !!n.isActive && h(n));
+    return !!n.isPaused || (n.isEnded ? !(l.Z.hasAnyUnexpiredOffer() || l.Z.hasAnyUnexpiredDiscountOffer() || u(n)) : !!n.isActive && d(n));
 }
--- chunks/351367.js
+++ chunks/351367.js
@@ -1,55 +1,45 @@
-r.d(t, { m: () => m }), r(47120);
+r.d(t, { m: () => b }), r(47120);
 var n = r(192379),
     s = r(442837),
-    i = r(355467),
-    o = r(493773),
-    l = r(594174),
-    a = r(78839),
-    c = r(431),
-    u = r(111361),
-    d = r(74538),
-    p = r(937579),
-    f = r(557017),
-    b = r(474936);
-let g = (e) => {
+    i = r(594174),
+    o = r(78839),
+    l = r(431),
+    a = r(111361),
+    c = r(74538),
+    u = r(557017),
+    d = r(474936);
+let p = (e) => {
         let t = new Date();
         return t.setDate(t.getDate() - 10), e.currentPeriodEnd >= t;
     },
-    O = (e) => {
+    f = (e) => {
         var t;
-        let r = null === (t = (0, d.Af)(e)) || void 0 === t ? void 0 : t.planId;
-        return void 0 === r || d.ZP.getPremiumType(r) !== b.p9.TIER_2;
+        let r = null === (t = (0, c.Af)(e)) || void 0 === t ? void 0 : t.planId;
+        return void 0 === r || c.ZP.getPremiumType(r) !== d.p9.TIER_2;
     };
-function m(e) {
+function b(e) {
     let { location: t } = e,
-        [r, d] = n.useState(null),
-        b = f.t.useExperiment({ location: t }),
-        m = (0, s.e7)([a.ZP], () => a.ZP.getMostRecentPremiumTypeSubscription()),
-        h = (0, s.e7)([c.Z], () => !c.Z.shouldFetchOffer()),
-        x = (0, s.e7)([a.ZP], () => a.ZP.hasFetchedMostRecentPremiumTypeSubscription()),
-        j = (0, s.e7)([l.default], () => l.default.getCurrentUser()),
-        y = (0, u.I5)(j);
+        [r, c] = n.useState(null),
+        d = u.t.useExperiment({ location: t }),
+        b = (0, s.e7)([o.ZP], () => o.ZP.getMostRecentPremiumTypeSubscription()),
+        g = (0, s.e7)([l.Z], () => !l.Z.shouldFetchOffer()),
+        O = (0, s.e7)([o.ZP], () => o.ZP.hasFetchedMostRecentPremiumTypeSubscription()),
+        m = (0, s.e7)([i.default], () => i.default.getCurrentUser()),
+        h = (0, a.I5)(m);
     return (
-        (0, o.ZP)(() => {
-            if (!1 === b.enabled) {
-                d(!1);
-                return;
-            }
-            x || (0, i.ou)(), c.Z.shouldFetchOffer() && (0, p.T)('useCollectibleDecoIsEligible');
-        }),
         n.useEffect(() => {
-            if (!1 === b.enabled) {
-                d(!1);
+            if (!1 === d.enabled) {
+                c(!1);
                 return;
             }
-            if (x && h) {
-                if (null === m) {
-                    y ? d(!1) : d(!0);
+            if (O && g) {
+                if (null === b) {
+                    h ? c(!1) : c(!0);
                     return;
                 }
-                m.isPaused ? d(!0) : m.isEnded ? (c.Z.hasAnyUnexpiredOffer() || c.Z.hasAnyUnexpiredDiscountOffer() || g(m) ? d(!1) : d(!0)) : m.isActive ? d(O(m)) : d(!1);
+                b.isPaused ? c(!0) : b.isEnded ? (l.Z.hasAnyUnexpiredOffer() || l.Z.hasAnyUnexpiredDiscountOffer() || p(b) ? c(!1) : c(!0)) : b.isActive ? c(f(b)) : c(!1);
             }
-        }, [b.enabled, x, m, h, y]),
+        }, [d.enabled, O, b, g, h]),
         r
     );
 }
