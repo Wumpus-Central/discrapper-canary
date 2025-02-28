1b324dbbe4ca287b235feb37739c1a742569f71d
DiscrapperManager <admin@minerpl.xyz>
2025-02-28 23:07:50 +0000

    canaryRelease: 373632
    Main scripts: 3
    Lazy Loaded Scripts: 947
    Chunks:
     * Main Files: 11196
     * Scripts: 10974
    Strings: 0
    Routes: 703
    Experiments Count: 404

--- chunks/43542.js
+++ chunks/43542.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => v });
+n.d(t, { Z: () => f });
 var r = n(200651),
     l = n(192379),
     i = n(442837),
@@ -9,42 +9,91 @@ var r = n(200651),
     u = n(709054),
     d = n(997431),
     m = n(388032),
-    x = n(62552);
-function v(e) {
+    v = n(597089);
+function f(e) {
     let { profile: t } = e,
         n = (0, i.e7)([c.default], () => c.default.locale),
-        v = (0, d.M)(u.default.extractTimestamp(t.id), n),
-        f = l.useMemo(() => {
+        f = (0, d.M)(u.default.extractTimestamp(t.id), n),
+        j = l.useMemo(() => {
             let e = new s.ZP(t);
             return (0, r.jsx)(o.Z, {
                 guild: e,
-                className: x.guildBadge,
+                className: v.guildBadge,
                 tooltipColor: a.ua7.Colors.NESTED,
                 disableBoostClick: !0
             });
         }, [t]);
     return (0, r.jsxs)('div', {
-        className: x.header,
+        className: v.header,
         children: [
             (0, r.jsxs)('div', {
-                className: x.nameContainer,
+                className: v.nameContainer,
                 children: [
-                    (0, r.jsx)(a.X6q, {
-                        className: x.guildName,
-                        variant: 'heading-lg/medium',
-                        color: 'header-primary',
-                        children: t.name
+                    (0, r.jsx)(a.ua7, {
+                        text: t.name,
+                        overflowOnly: !0,
+                        children: (e) => {
+                            var n, l;
+                            return (0, r.jsx)(
+                                a.X6q,
+                                ((n = (function (e) {
+                                    for (var t = 1; t < arguments.length; t++) {
+                                        var n = null != arguments[t] ? arguments[t] : {},
+                                            r = Object.keys(n);
+                                        'function' == typeof Object.getOwnPropertySymbols &&
+                                            (r = r.concat(
+                                                Object.getOwnPropertySymbols(n).filter(function (e) {
+                                                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                                                })
+                                            )),
+                                            r.forEach(function (t) {
+                                                var r;
+                                                (r = n[t]),
+                                                    t in e
+                                                        ? Object.defineProperty(e, t, {
+                                                              value: r,
+                                                              enumerable: !0,
+                                                              configurable: !0,
+                                                              writable: !0
+                                                          })
+                                                        : (e[t] = r);
+                                            });
+                                    }
+                                    return e;
+                                })({}, e)),
+                                (l = l =
+                                    {
+                                        className: v.guildName,
+                                        variant: 'heading-lg/medium',
+                                        color: 'header-primary',
+                                        children: t.name
+                                    }),
+                                Object.getOwnPropertyDescriptors
+                                    ? Object.defineProperties(n, Object.getOwnPropertyDescriptors(l))
+                                    : (function (e, t) {
+                                          var n = Object.keys(e);
+                                          if (Object.getOwnPropertySymbols) {
+                                              var r = Object.getOwnPropertySymbols(e);
+                                              n.push.apply(n, r);
+                                          }
+                                          return n;
+                                      })(Object(l)).forEach(function (e) {
+                                          Object.defineProperty(n, e, Object.getOwnPropertyDescriptor(l, e));
+                                      }),
+                                n)
+                            );
+                        }
                     }),
-                    f
+                    j
                 ]
             }),
             (0, r.jsxs)('div', {
-                className: x.members,
+                className: v.members,
                 children: [
                     (0, r.jsxs)('div', {
-                        className: x.memberCount,
+                        className: v.memberCount,
                         children: [
-                            (0, r.jsx)('div', { className: x.dot }),
+                            (0, r.jsx)('div', { className: v.dot }),
                             (0, r.jsx)(a.Text, {
                                 variant: 'text-sm/normal',
                                 color: 'text-secondary',
@@ -53,9 +102,9 @@ function v(e) {
                         ]
                     }),
                     (0, r.jsxs)('div', {
-                        className: x.memberCount,
+                        className: v.memberCount,
                         children: [
-                            (0, r.jsx)('div', { className: x.dotOnline }),
+                            (0, r.jsx)('div', { className: v.dotOnline }),
                             (0, r.jsx)(a.Text, {
                                 variant: 'text-sm/normal',
                                 color: 'text-secondary',
@@ -66,11 +115,11 @@ function v(e) {
                 ]
             }),
             (0, r.jsx)('div', {
-                className: x.established,
+                className: v.established,
                 children: (0, r.jsx)(a.Text, {
                     variant: 'text-sm/normal',
                     color: 'text-secondary',
-                    children: m.NW.format(m.t['zb2Q5+'], { createdAtDate: v })
+                    children: m.NW.format(m.t['zb2Q5+'], { createdAtDate: f })
                 })
             })
         ]
