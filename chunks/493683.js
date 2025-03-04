e8866ff3681648fcdf3455e62567aac5d6a60534
DiscrapperManager <admin@minerpl.xyz>
2025-03-04 02:56:27 +0000

    canaryRelease: 374236
    Main scripts: 3
    Lazy Loaded Scripts: 942
    Chunks:
     * Main Files: 11202
     * Scripts: 10977
    Strings: 0
    Routes: 704
    Experiments Count: 405

--- chunks/493683.js
+++ chunks/493683.js
@@ -9,11 +9,11 @@ var r = n(544891),
     u = n(895886),
     d = n(703656),
     f = n(359110),
-    p = n(131704),
-    _ = n(592125),
+    _ = n(131704),
+    p = n(592125),
     h = n(306680),
-    m = n(626135),
-    g = n(73346),
+    g = n(626135),
+    m = n(73346),
     E = n(26151),
     v = n(287734),
     b = n(981631),
@@ -51,8 +51,8 @@ let O = {
         }
     },
     _openCachedDMChannel(e, t) {
-        let n = _.Z.getDMFromUserId(e),
-            r = null != n ? _.Z.getChannel(n) : null;
+        let n = p.Z.getDMFromUserId(e),
+            r = null != n ? p.Z.getChannel(n) : null;
         return null == r ? null : (null == t || t(), null != (0, c.D)() ? (0, f.Kh)(r.id, { navigationReplace: !0 }) : v.default.selectPrivateChannel(r.id), r);
     },
     async ensurePrivateChannel(e) {
@@ -63,7 +63,7 @@ let O = {
                 oldFormErrors: !0,
                 rejectWithError: !1
             }),
-            i = (0, p.q_)(n.body);
+            i = (0, _.q_)(n.body);
         return (
             o.Z.dispatch({
                 type: 'CHANNEL_CREATE',
@@ -73,7 +73,7 @@ let O = {
         );
     },
     async getOrEnsurePrivateChannel(e) {
-        let t = _.Z.getDMFromUserId(e);
+        let t = p.Z.getDMFromUserId(e);
         return null != t ? t : await this.ensurePrivateChannel(e);
     },
     async getDMChannel(e) {
@@ -81,7 +81,7 @@ let O = {
                 url: b.ANM.DM_CHANNEL(e),
                 rejectWithError: !0
             }),
-            n = (0, p.q_)(t.body);
+            n = (0, _.q_)(t.body);
         return (
             o.Z.dispatch({
                 type: 'CHANNEL_CREATE',
@@ -92,7 +92,7 @@ let O = {
     },
     _getRecipients: (e) => (null != e ? (Array.isArray(e) ? e : [e]) : []),
     _openPrivateChannel(e) {
-        let t = (0, p.q_)(e);
+        let t = (0, _.q_)(e);
         return (
             o.Z.dispatch({
                 type: 'CHANNEL_CREATE',
@@ -107,7 +107,7 @@ let O = {
             n = arguments.length > 2 && void 0 !== arguments[2] && arguments[2];
         return (
             (0, s.Z)(e) &&
-                m.default.track(b.rMx.CHANGE_LOG_DM_REMOVED, {
+                g.default.track(b.rMx.CHANGE_LOG_DM_REMOVED, {
                     last_changelog_id: a.Z.latestChangelogId(),
                     unread_count: h.ZP.getUnreadCount(e)
                 }),
@@ -195,7 +195,7 @@ let O = {
             rejectWithError: !0
         }),
     async setName(e, t) {
-        let n = _.Z.getChannel(e),
+        let n = p.Z.getChannel(e),
             i = await r.tn.patch({
                 url: b.ANM.CHANNEL(e),
                 body: { name: t },
@@ -205,19 +205,17 @@ let O = {
             o = null == n ? void 0 : n.getGuildId();
         return null == o || (null == n ? void 0 : n.isThread()) || l.Z.checkGuildTemplateDirty(o), i;
     },
-    setIcon(e, t) {
-        let n = _.Z.getChannel(e);
-        r.tn
-            .patch({
+    async setIcon(e, t) {
+        let n = p.Z.getChannel(e),
+            i = await r.tn.patch({
                 url: b.ANM.CHANNEL(e),
                 body: { icon: t },
                 oldFormErrors: !0,
-                rejectWithError: !0
-            })
-            .then(() => {
-                let e = null == n ? void 0 : n.getGuildId();
-                null == e || (null == n ? void 0 : n.isThread()) || l.Z.checkGuildTemplateDirty(e);
-            });
+                rejectWithError: !0,
+                failImmediatelyWhenRateLimited: !0
+            }),
+            o = null == n ? void 0 : n.getGuildId();
+        return null == o || (null == n ? void 0 : n.isThread()) || l.Z.checkGuildTemplateDirty(o), i;
     },
     convertToGuild: (e) =>
         r.tn.post({
@@ -235,7 +233,7 @@ let O = {
     },
     fetchChannelStoreListing(e, t) {
         let n = null != t ? b.ANM.CHANNEL_STORE_LISTING_SKU(e, t) : b.ANM.CHANNEL_STORE_LISTING(e);
-        return (0, g.Kb)(n).then((t) => {
+        return (0, m.Kb)(n).then((t) => {
             o.Z.dispatch({
                 type: 'STORE_LISTING_FETCH_SUCCESS',
                 channelId: e,
