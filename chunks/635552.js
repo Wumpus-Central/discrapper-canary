97909d49a2e93355069e08442018684be0dca613
DiscrapperManager <admin@minerpl.xyz>
2025-03-12 18:04:12 +0000

    canaryRelease: 376964
    Main scripts: 3
    Lazy Loaded Scripts: 976
    Chunks:
     * Main Files: 11243
     * Scripts: 11029
    Strings: 0
    Routes: 706
    Experiments Count: 418

--- chunks/635552.js
+++ chunks/635552.js
@@ -9,12 +9,12 @@ var r = n(192379),
 let u = 6000,
     d = (e) => {
         let { product: t, onSuccess: d, onError: f } = e,
-            [p, _] = r.useState(!1),
+            [_, p] = r.useState(!1),
             { firstAvatarDecoration: h, firstProfileEffect: m } = (0, l.R)(t),
-            g = (0, s.x6)(t) ? c.NW.string(c.t.tf1ZZ2) : t.type === i.Z.AVATAR_DECORATION ? c.NW.string(c.t.zOA4a2) : c.NW.string(c.t.SWm2am);
+            g = (0, s.x6)(t) ? c.NW.string(c.t.tf1ZZ2) : t.type === i.Z.AVATAR_DECORATION ? c.NW.string(c.t.zOA4a2) : t.type === i.Z.NAMEPLATE ? c.NW.string(c.t.gOzMv7) : c.NW.string(c.t.SWm2am);
         return {
             handleUseNow: r.useCallback(async () => {
-                _(!0);
+                p(!0);
                 try {
                     if (null != h) {
                         let e = { avatarDecoration: h };
@@ -23,6 +23,10 @@ let u = 6000,
                     if (null != m) {
                         let e = { profile_effect_id: m.id };
                         await (0, a.Z)(e);
+                    }
+                    if (t.type === i.Z.NAMEPLATE) {
+                        let e = { nameplate: t.items[0] };
+                        await (0, o.Mn)(e);
                     }
                     {
                         let { ToastPosition: e, ToastType: t, createToast: r, popToast: i, showToast: o } = await Promise.resolve().then(n.bind(n, 481060));
@@ -38,9 +42,9 @@ let u = 6000,
                 } catch (e) {
                     null == f || f(e);
                 } finally {
-                    _(!1);
+                    p(!1);
                 }
-            }, [h, m, d, g, f]),
-            isApplying: p
+            }, [h, m, t, d, g, f]),
+            isApplying: _
         };
     };
