fc11e6c19b5851b92b4d32e6af2c4771005b1c66
DiscrapperManager <admin@minerpl.xyz>
2025-03-12 14:10:32 +0000

    canaryRelease: 376851
    Main scripts: 3
    Lazy Loaded Scripts: 976
    Chunks:
     * Main Files: 11243
     * Scripts: 11016
    Strings: 0
    Routes: 706
    Experiments Count: 417

--- chunks/230385.js
+++ chunks/230385.js
@@ -122,10 +122,11 @@ class b extends i.Z {
     }
     handleVoiceFilterDownloadFailed(e) {
         var t;
-        let { modelId: n, voiceFilterId: r } = e;
+        let { modelId: n, voiceFilterId: r, error: i } = e;
         l.default.track(_.rMx.VOICE_FILTER_DOWNLOAD_ATTEMPTED, {
             active_voice_filter_id: null !== (t = s.Z.getActiveVoiceFilter()) && void 0 !== t ? t : null,
             success: !1,
+            canceled: null != i && 'USER_CANCELED_DOWNLOAD' in i,
             voice_filter_id: r,
             model_id: n
         });
