1762c6a77adf2d4d2696695dbe81a65f943597ac
DiscrapperManager <admin@minerpl.xyz>
2025-03-12 18:19:30 +0000

    canaryRelease: 376977
    Main scripts: 3
    Lazy Loaded Scripts: 976
    Chunks:
     * Main Files: 11243
     * Scripts: 11029
    Strings: 0
    Routes: 706
    Experiments Count: 418

--- chunks/379839.js
+++ chunks/379839.js
@@ -1,19 +1,24 @@
 n.d(t, {
-    A: () => s,
-    p: () => l
+    A: () => f,
+    p: () => _
 }),
     n(47120);
 var r = n(192379),
     i = n(410030),
     o = n(4242),
     a = n(231338);
-function s(e) {
+let s = '1A',
+    l = '33',
+    c = '4D',
+    u = '66',
+    d = '80';
+function f(e) {
     let t = (0, i.ZP)(),
         [n, o] = (0, r.useState)({});
     return (
         (0, r.useEffect)(() => {
             if (null == e) return;
-            let n = (t === a.BR.LIGHT ? e.palette.lightBackground : e.palette.darkBackground) + (t === a.BR.LIGHT ? '33' : '80');
+            let n = (t === a.BR.LIGHT ? e.palette.lightBackground : e.palette.darkBackground) + l;
             o({
                 '--background-modifier-selected': n,
                 '--button-secondary-background-hover': n,
@@ -25,20 +30,20 @@ function s(e) {
         n
     );
 }
-function l(e, t, n, o, a) {
+function _(e, t, n, o, a) {
     let s = (0, i.ZP)(),
-        [l, u] = (0, r.useState)({});
+        [l, c] = (0, r.useState)({});
     return (
         (0, r.useEffect)(() => {
             if (null == e) return;
-            let r = c(e.palette, s, t, n, a);
+            let r = p(e.palette, s, t, n, a);
             if (null == o) {
-                u({ background: r });
+                c({ background: r });
                 return;
             }
             let i = new ResizeObserver((e) => {
                 let t = e[0].contentRect.width;
-                u({
+                c({
                     background: r,
                     maskImage: 'linear-gradient(to right, rgba(0, 0, 0, .2) '.concat(0.9 * t, 'px, rgba(0, 0, 0, 1) ').concat(1.5 * t, 'px)')
                 });
@@ -48,12 +53,12 @@ function l(e, t, n, o, a) {
         l
     );
 }
-function c(e, t, n, r, i) {
+function p(e, t, n, r, i) {
     if (!(0, o.ic)(e)) return;
-    let s = t === a.BR.LIGHT ? e.lightBackground : e.darkBackground,
-        l = n || r;
-    if (i) return t === a.BR.LIGHT ? 'linear-gradient(90deg, '.concat(s, '1A 0%, ').concat(s, '4D 100%)') : 'linear-gradient(90deg, '.concat(s, '1A 0%, ').concat(s, '66 100%)');
-    let c = n ? 'var(--background-modifier-hover)' : r ? 'var(--background-modifier-selected)' : ''.concat(s, '00'),
-        u = ''.concat(s).concat(l ? '80' : '33');
-    return 'linear-gradient(90deg, '.concat(c, ' 0%, ').concat(u, ' 100%)');
+    let l = t === a.BR.LIGHT ? e.lightBackground : e.darkBackground,
+        f = n || r;
+    if (i) return t === a.BR.LIGHT ? 'linear-gradient(90deg, '.concat(l).concat(s, ' 0%, ').concat(l).concat(c, ' 100%)') : 'linear-gradient(90deg, '.concat(l).concat(s, ' 0%, ').concat(l).concat(u, ' 100%)');
+    let _ = n ? 'var(--background-modifier-hover)' : r ? 'var(--background-modifier-selected)' : ''.concat(l, '00'),
+        p = ''.concat(l).concat(f ? (t !== a.BR.LIGHT ? d : c) : s);
+    return 'linear-gradient(90deg, '.concat(_, ' 0%, ').concat(p, ' 100%)');
 }
