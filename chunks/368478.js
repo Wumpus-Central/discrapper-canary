c6b1eb0bdee5601a6fca88f0c8a17c74c587665a
DiscrapperManager <admin@minerpl.xyz>
2025-03-13 20:24:51 +0000

    canaryRelease: 377669
    Main scripts: 3
    Lazy Loaded Scripts: 978
    Chunks:
     * Main Files: 11248
     * Scripts: 11038
    Strings: 0
    Routes: 706
    Experiments Count: 419

--- chunks/368478.js
+++ chunks/368478.js
@@ -5,8 +5,8 @@ n.d(t, {
     n(47120),
     n(789020);
 var r = n(192379),
-    i = n(442837),
-    l = n(774226),
+    i = n(252258),
+    l = n(442837),
     o = n(317381),
     a = n(122613),
     s = n(16609),
@@ -22,13 +22,12 @@ var r = n(192379),
 function _(e) {
     let { context: t, primaryEntryPointCommand: n, application: a, showAppLauncherPopup: c = !1 } = e,
         [h, _] = r.useState(!1),
-        C = (0, i.e7)([o.ZP], () => o.ZP.getCurrentEmbeddedActivity()),
-        v = (0, l.GK)(),
-        y = c ? b.t.cpT0Cg : b.t['0hKkS0'],
-        x = r.useMemo(() => (null != n ? (0, p.XZ)(n.displayName) : b.NW.string(b.t['3xjX0d'])), [n]),
-        j = void 0 !== a.flags && (0, m.yE)(a.flags, g.udG.EMBEDDED),
-        O = null != C && C.applicationId === a.id && (v || (0, s.pY)(C.location) === t.channel.id),
-        { analyticsLocations: N } = (0, d.ZP)(u.Z.APP_DMS_ENTRY_POINT_COMMAND_BUTTON),
+        C = (0, l.e7)([o.ZP], () => o.ZP.getCurrentEmbeddedActivity()),
+        v = c ? b.t.cpT0Cg : b.t['0hKkS0'],
+        y = r.useMemo(() => (null != n ? (0, p.XZ)(n.displayName) : b.NW.string(b.t['3xjX0d'])), [n]),
+        x = void 0 !== a.flags && (0, m.yE)(a.flags, g.udG.EMBEDDED),
+        j = null != C && C.applicationId === a.id && (C.location.kind === i.E.CONTEXTLESS || (0, s.pY)(C.location) === t.channel.id),
+        { analyticsLocations: O } = (0, d.ZP)(u.Z.APP_DMS_ENTRY_POINT_COMMAND_BUTTON),
         E = null != t.channel.recipients ? t.channel.recipients[0] : void 0;
     return {
         isExecutingLaunchInteraction: h,
@@ -37,19 +36,19 @@ function _(e) {
                 context: t,
                 applicationId: a.id,
                 botUserId: E
-            }) && j
-                ? O
+            }) && x
+                ? j
                     ? b.NW.string(b.t['Hi1/aW'])
-                    : null != x
-                      ? x
+                    : null != y
+                      ? y
                       : b.NW.string(b.t.zKX8Nj)
-                : b.NW.string(y),
-        hasActiveMatchingEmbeddedActivity: O,
-        isEmbeddedApp: j,
+                : b.NW.string(v),
+        hasActiveMatchingEmbeddedActivity: j,
+        isEmbeddedApp: x,
         currentEmbeddedActivity: C,
         channelRecipientUserId: E,
         setIsExecutingLaunchInteraction: _,
-        analyticsLocations: N
+        analyticsLocations: O
     };
 }
 function C(e) {
