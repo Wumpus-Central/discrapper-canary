938e1df09260f7d09b3ce783242ac0909cb335e2
DiscrapperManager <admin@minerpl.xyz>
2025-03-14 21:16:33 +0000

    canaryRelease: 378364
    Main scripts: 3
    Lazy Loaded Scripts: 979
    Chunks:
     * Main Files: 11265
     * Scripts: 11047
    Strings: 0
    Routes: 706
    Experiments Count: 423

--- chunks/339227.js
+++ chunks/339227.js
@@ -12,8 +12,8 @@ var r = n(512722),
     _ = n(364458),
     p = n(70956),
     h = n(40786),
-    g = n(362092);
-function m(e, t, n) {
+    m = n(362092);
+function g(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -37,7 +37,7 @@ function E(e) {
                 })
             )),
             r.forEach(function (t) {
-                m(e, t, n[t]);
+                g(e, t, n[t]);
             });
     }
     return e;
@@ -175,12 +175,12 @@ let y = {
         }
     },
     O = new Set(['*', '_', '\\']),
-    S = {},
-    I = {};
+    I = {},
+    S = {};
 for (let e in d.Z.RULES) {
     if (!(e in y)) throw Error('Slate: Unknown markdown rule: '.concat(e, '.  If you have just added a new markdown rule ') + 'then you probably need to add it to this file so that the rich chat box understands it.');
     let t = y[e];
-    'skip' !== t.type && (S[e] = T(d.Z.RULES[e])), 'skip' !== t.type && 'inlineObject' !== t.type && (I[e] = T('text' === e ? f.ZP : d.Z.RULES[e]));
+    'skip' !== t.type && (I[e] = T(d.Z.RULES[e])), 'skip' !== t.type && 'inlineObject' !== t.type && (S[e] = T('text' === e ? f.ZP : d.Z.RULES[e]));
 }
 function T(e) {
     i()(null != e.parse, 'Slate: rule must have a parse function');
@@ -225,7 +225,7 @@ let N = {
             order: s.defaultRules.inlineCode.order - 0.1,
             match: (e) => /^(```)([a-z0-9_+\-.#]+$)?/.exec(e),
             parse: (e) =>
-                null != e[2] && '' !== e[2] && g.i6(e[2])
+                null != e[2] && '' !== e[2] && m.i6(e[2])
                     ? [
                           {
                               type: 'codeBlockSyntax',
@@ -246,26 +246,25 @@ let N = {
         }
     },
     A = /(-# +)/,
-    C = (0, _.Z)([S, N]),
-    R = (0, _.Z)([I, N]),
+    C = (0, _.Z)([I, N]),
+    R = (0, _.Z)([S, N]),
     P = l._p(C),
-    D = l._p(R),
-    w = {
+    w = l._p(R),
+    D = {
         max: 1 / 0,
         maxAge: +p.Z.Millis.MINUTE,
         updateAgeOnGet: !0
     },
-    L = new (a())(w),
-    x = new (a())(w);
+    L = new (a())(D),
+    x = new (a())(D);
 function M(e, t, n) {
     let r = [],
         i = {
             returnMentionIds: !0,
             disableAutoBlockNewlines: !0,
-            guildId: t,
-            isSlate: !0
+            guildId: t
         },
-        o = n ? D : P,
+        o = n ? w : P,
         a = n ? x : L,
         s = a.get(e);
     if (null != s) return s;
@@ -289,7 +288,7 @@ function M(e, t, n) {
 function k(e, t) {
     let n = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
         r = arguments.length > 3 && void 0 !== arguments[3] && arguments[3],
-        i = g.rN(e);
+        i = m.rN(e);
     if ((i.push(e.length), 1 === i.length && n)) return [];
     let o = 0,
         a = n,
@@ -304,7 +303,7 @@ function k(e, t) {
             });
         else {
             let a = n === i[i.length - 2] ? e.substring(n + 3) : '';
-            n += 3 + (null != a.match(g.Q2) ? a : '').length;
+            n += 3 + (null != a.match(m.Q2) ? a : '').length;
             let l = e.substring(o, n);
             '' !== l &&
                 M(l, t, r).forEach((e) => {
--- chunks/840498.js
+++ chunks/840498.js
@@ -11,51 +11,40 @@ function i(e, t) {
     e.push(t);
 }
 function o(e) {
-    let t = null;
-    for (let n = 0; n < e.length; n++) {
-        let r = e[n];
-        if (null == t || 'text' != t.type || t.type != r.type) {
-            t = r;
-            continue;
-        }
-        (t.content += r.content), e.splice(n, 1), n--;
+    let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null;
+    if (Array.isArray(e)) {
+        let n = e.length,
+            r = [];
+        for (let a = 0; a < n; a++) i(r, o(e[a], t));
+        return r;
     }
-}
-function a(e, t) {
-    let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : null;
-    if (Array.isArray(t)) {
-        let r = t.length,
-            s = [];
-        for (let o = 0; o < r; o++) i(s, a(e, t[o], n));
-        return e.isSlate || o(s), s;
-    }
-    return (null != t.content && (t.content = a(e, t.content, t)), 'inlineCode' === t.type && delete t.validationChildContent, 'list' === t.type && (t.items = t.items.map((t) => (Array.isArray(t) ? a(e, t, null) : t))), null != n && t.type === n.type) ? t.content : t;
+    return (null != e.content && (e.content = o(e.content, e)), 'inlineCode' === e.type && delete e.validationChildContent, 'list' === e.type && (e.items = e.items.map((e) => (Array.isArray(e) ? o(e, null) : e))), null != t && e.type === t.type) ? e.content : e;
 }
 n.d(t, {
-    RA: () => l,
-    Rp: () => c,
-    ge: () => a
+    RA: () => s,
+    Rp: () => l,
+    ge: () => o
 }),
     n(653041);
-let s = {};
-function l(e) {
+let a = {};
+function s(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : { limit: 200 };
     if (Array.isArray(e)) {
         let n = e.length;
         for (let r = 0; r < n; r++) {
-            let n = l(e[r], t);
-            if (n === s) {
+            let n = s(e[r], t);
+            if (n === a) {
                 e.length = r;
                 break;
             }
             e[r] = n;
         }
     } else if ('text' !== e.type) {
-        if (((t.limit -= 1), t.limit <= 0)) return s;
-        Array.isArray(e.content) && (e.content = l(e.content, t)), 'list' === e.type && (e.items = e.items.map((e) => l(e, t)));
+        if (((t.limit -= 1), t.limit <= 0)) return a;
+        Array.isArray(e.content) && (e.content = s(e.content, t)), 'list' === e.type && (e.items = e.items.map((e) => s(e, t)));
     }
     return e;
 }
-function c(e) {
+function l(e) {
     return r(e).join('');
 }
