2065dafa5a3dbb545abae770dba8b8d63c22b37d
DiscrapperManager <admin@minerpl.xyz>
2025-03-17 16:25:54 +0000

    canaryRelease: 378726
    Main scripts: 3
    Lazy Loaded Scripts: 977
    Chunks:
     * Main Files: 11262
     * Scripts: 11068
    Strings: 0
    Routes: 706
    Experiments Count: 423

--- chunks/196033.js
+++ chunks/196033.js
@@ -1,31 +1,44 @@
-n.d(t, { W: () => p });
+n.d(t, { W: () => f });
 var r = n(192379),
     i = n(115434),
     s = n(264181),
     a = n(442837),
-    l = n(874703),
-    o = n(619899),
-    c = n(281598),
-    d = n(388032),
-    u = n(786792),
-    m = n(161594);
-let p = () => {
-    let e = (0, o.z)(c.Fr.UPSELL_BANNER),
-        t = (0, o.z)(c.Fr.UPSELL_BANNER_POPOUT),
-        n = (0, a.e7)([l.Z], () => l.Z.getMarketingBySurface(i.K.EDIT_PROFILE_SETTINGS)),
-        p = r.useMemo(
+    l = n(309351),
+    o = n(874703),
+    c = n(619899),
+    d = n(281598),
+    u = n(388032),
+    m = n(513767),
+    g = n(78230),
+    p = n(786792),
+    h = n(161594);
+let f = () => {
+    let e = (0, c.z)(d.Fr.UPSELL_BANNER),
+        t = (0, c.z)(d.Fr.UPSELL_BANNER_POPOUT),
+        n = (0, l.H)('upsell_banner'),
+        f = (0, a.e7)([o.Z], () => o.Z.getMarketingBySurface(i.K.EDIT_PROFILE_SETTINGS)),
+        x = r.useMemo(
             () =>
-                null != n
-                    ? n
-                    : {
-                          asset: u,
+                n
+                    ? {
+                          asset: g,
                           popoutAsset: m,
-                          title: d.NW.string(d.t['4Emkur']),
-                          body: d.NW.string(d.t.gn7SZW),
+                          title: u.NW.string(u.t.QZVVBg),
+                          body: u.NW.string(u.t.sajmAg),
                           version: 0,
                           revertTextColor: !1
-                      },
-            [n]
+                      }
+                    : null != f
+                      ? f
+                      : {
+                            asset: p,
+                            popoutAsset: h,
+                            title: u.NW.string(u.t['4Emkur']),
+                            body: u.NW.string(u.t.gn7SZW),
+                            version: 0,
+                            revertTextColor: !1
+                        },
+            [f, n]
         );
     return r.useMemo(() => {
         var n, r;
@@ -54,12 +67,12 @@ let p = () => {
                         });
                 }
                 return e;
-            })({}, p)),
+            })({}, x)),
             (r = r =
                 {
                     type: s.Z.BANNER,
-                    asset: null != e ? e : p.asset,
-                    popoutAsset: null != t ? t : p.popoutAsset
+                    asset: null != e ? e : x.asset,
+                    popoutAsset: null != t ? t : x.popoutAsset
                 }),
             Object.getOwnPropertyDescriptors
                 ? Object.defineProperties(n, Object.getOwnPropertyDescriptors(r))
@@ -75,5 +88,5 @@ let p = () => {
                   }),
             n
         );
-    }, [e, t, p]);
+    }, [e, t, x]);
 };
