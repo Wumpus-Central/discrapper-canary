118bff64f5d50d88f3877746608fdbc2dbab11c6
DiscrapperManager <admin@minerpl.xyz>
2025-03-17 23:27:34 +0000

    canaryRelease: 379040
    Main scripts: 3
    Lazy Loaded Scripts: 978
    Chunks:
     * Main Files: 11264
     * Scripts: 11071
    Strings: 0
    Routes: 706
    Experiments Count: 423

--- chunks/254925.js
+++ chunks/254925.js
@@ -1,7 +1,7 @@
 n.d(t, {
-    QV: () => x,
-    ZP: () => N,
-    go: () => j
+    QV: () => j,
+    ZP: () => v,
+    go: () => N
 }),
     n(266796);
 var r = n(200651),
@@ -15,12 +15,13 @@ var r = n(200651),
     u = n(835887),
     m = n(388661),
     g = n(500230),
-    p = n(116175),
-    h = n(502936),
-    f = n(388032),
-    b = n(935921);
-let x = [p.ZD.LEAF, p.ZD.SWORD, p.ZD.HEART, p.ZD.FIRE, p.ZD.WATER_DROP, p.ZD.SKULL, p.ZD.MOON, p.ZD.LIGHTNING, p.ZD.COMPASS, p.ZD.TOADSTOOL],
-    j = [
+    p = n(981631),
+    h = n(116175),
+    f = n(502936),
+    b = n(388032),
+    x = n(935921);
+let j = [h.ZD.LEAF, h.ZD.SWORD, h.ZD.HEART, h.ZD.FIRE, h.ZD.WATER_DROP, h.ZD.SKULL, h.ZD.MOON, h.ZD.LIGHTNING, h.ZD.COMPASS, h.ZD.TOADSTOOL],
+    N = [
         {
             primary: '#ff1c90',
             secondary: '#ff7fc0'
@@ -74,79 +75,79 @@ let x = [p.ZD.LEAF, p.ZD.SWORD, p.ZD.HEART, p.ZD.FIRE, p.ZD.WATER_DROP, p.ZD.SKU
             secondary: '#cccccc'
         }
     ],
-    N = function () {
-        (0, f.j1)(h.M);
+    v = function () {
+        (0, b.j1)(f.M);
         let e = (0, s.e7)([d.Z], () => d.Z.getGuild()),
             t = (0, s.e7)([d.Z], () => d.Z.getGuildProfile()),
-            { fetchGuildProfile: n, fetchStatus: p } = (0, o.u)(null == e ? void 0 : e.id);
+            { fetchGuildProfile: n, fetchStatus: h } = (0, o.u)(null == e ? void 0 : e.id);
         if (
             (i.useEffect(() => {
-                p === l.a.NOT_FETCHED && n();
-            }, [n, p]),
-            p !== l.a.FETCHED)
+                h === l.a.NOT_FETCHED && n();
+            }, [n, h]),
+            h !== l.a.FETCHED)
         )
             return (0, r.jsx)('div', {
-                className: b.spinnerContainer,
+                className: x.spinnerContainer,
                 children: (0, r.jsx)(a.$jN, {})
             });
         if (null == e || null == t) return null;
-        let N = (null == t ? void 0 : t.tag) == null,
-            v = (0, r.jsxs)(r.Fragment, {
+        let v = (null == t ? void 0 : t.tag) == null,
+            _ = (0, r.jsxs)(r.Fragment, {
                 children: [
                     (0, r.jsx)(m.Z, {
-                        className: b.section,
+                        className: x.section,
                         guildId: e.id,
                         guildProfile: t,
-                        isDisabled: N
+                        isDisabled: v
                     }),
                     (0, r.jsx)(u.Z, {
-                        className: b.section,
+                        className: x.section,
                         guildId: e.id,
                         guildProfile: t
                     }),
                     (0, r.jsx)(g.Z, {
-                        className: b.section,
+                        className: x.section,
                         guildId: e.id,
                         guildProfile: t
                     })
                 ]
             });
         return (0, r.jsxs)('div', {
-            className: b.leftContent,
+            className: x.leftContent,
             children: [
                 (0, r.jsx)(a.X6q, {
-                    className: b.title,
+                    className: x.title,
                     variant: 'heading-lg/semibold',
-                    children: f.NW.string(h.Z.mf2OwM)
+                    children: b.NW.string(f.Z.mf2OwM)
                 }),
                 (0, r.jsx)(a.Text, {
                     variant: 'text-sm/medium',
                     color: 'text-secondary',
-                    className: b.description,
-                    children: f.NW.string(h.Z['655Un5'])
+                    className: x.description,
+                    children: b.NW.string(f.Z['655Un5'])
                 }),
                 (0, r.jsxs)('div', {
-                    className: b.enableSwitchContainer,
+                    className: x.enableSwitchContainer,
                     children: [
                         (0, r.jsx)('label', {
                             htmlFor: 'enable-server-tag',
                             children: (0, r.jsx)(a.Text, {
                                 variant: 'text-md/semibold',
                                 color: 'header-primary',
-                                children: f.NW.string(h.Z['1zams7'])
+                                children: b.NW.string(f.Z['1zams7'])
                             })
                         }),
                         (0, r.jsx)(a.rsf, {
                             id: 'enable-server-tag',
-                            checked: !N,
+                            checked: !v,
                             onChange: () => {
-                                if (N) {
+                                if (v) {
                                     var n, r, i, s;
                                     c.Z.updateGuildProfile(e.id, {
                                         tag: null !== (n = null == t ? void 0 : t.tag) && void 0 !== n ? n : '',
-                                        badge: null !== (r = null == t ? void 0 : t.badge) && void 0 !== r ? r : x[0],
-                                        badgeColorPrimary: null !== (i = null == t ? void 0 : t.badgeColorPrimary) && void 0 !== i ? i : j[0].primary,
-                                        badgeColorSecondary: null !== (s = null == t ? void 0 : t.badgeColorSecondary) && void 0 !== s ? s : j[0].secondary
+                                        badge: null !== (r = null == t ? void 0 : t.badge) && void 0 !== r ? r : j[0],
+                                        badgeColorPrimary: null !== (i = null == t ? void 0 : t.badgeColorPrimary) && void 0 !== i ? i : N[0].primary,
+                                        badgeColorSecondary: null !== (s = null == t ? void 0 : t.badgeColorSecondary) && void 0 !== s ? s : N[0].secondary
                                     });
                                 } else c.Z.updateGuildProfile(e.id, { tag: null });
                             }
@@ -156,19 +157,21 @@ let x = [p.ZD.LEAF, p.ZD.SWORD, p.ZD.HEART, p.ZD.FIRE, p.ZD.WATER_DROP, p.ZD.SKU
                 (0, r.jsx)(a.Text, {
                     variant: 'text-xs/medium',
                     color: 'text-muted',
-                    className: b.memberApplicationsInfo,
-                    children: f.NW.format(h.Z.WUACVF, {
-                        onClick: () => {}
+                    className: x.memberApplicationsInfo,
+                    children: b.NW.format(f.Z.WUACVF, {
+                        onClick: () => {
+                            c.Z.setSection(p.pNK.ACCESS);
+                        }
                     })
                 }),
-                N
+                v
                     ? (0, r.jsx)(a.nuw, {
                           children: (0, r.jsx)('div', {
-                              className: b.disabledForm,
-                              children: v
+                              className: x.disabledForm,
+                              children: _
                           })
                       })
-                    : v
+                    : _
             ]
         });
     };
