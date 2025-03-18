646581de8ee39fe2f0e1fab4126358a29a28cc51
DiscrapperManager <admin@minerpl.xyz>
2025-03-18 20:27:32 +0000

    canaryRelease: 379373
    Main scripts: 3
    Lazy Loaded Scripts: 975
    Chunks:
     * Main Files: 11271
     * Scripts: 11013
    Strings: 0
    Routes: 706
    Experiments Count: 423

--- chunks/986035.js
+++ chunks/986035.js
@@ -1,61 +1,57 @@
-n.d(t, { Z: () => p }), n(266796);
+n.d(t, { Z: () => d }), n(266796);
 var r = n(200651),
     i = n(192379),
-    o = n(120356),
-    c = n.n(o),
+    l = n(120356),
+    o = n.n(l),
     a = n(481060),
     s = n(480916),
-    l = n(600164),
-    u = n(258083),
-    g = n(802138);
-let p = function (e) {
-    let { title: t, description: n, agreement: o, disagreement: p, onAgree: f, onDisagree: d, imageClassName: b, agreementButtonColor: m = a.zxk.Colors.RED, modalType: O, channelId: y, guildId: j } = e;
+    c = n(258083),
+    u = n(802138);
+let d = function (e) {
+    let { title: t, description: n, agreement: l, disagreement: d, onAgree: p, onDisagree: h, imageClassName: f, agreementButtonColor: m = a.zxk.Colors.RED, modalType: g, channelId: b, guildId: _ } = e;
     i.useEffect(() => {
-        (0, s.YT)(O, y, j);
-    }, [O, y, j]);
-    let E = i.useCallback(() => {
-            (0, s.qd)(s.iP.NSFW_CHANNEL_DISAGREE_CTA, O, y, j), null == d || d();
-        }, [d, O, y, j]),
-        h = i.useCallback(() => {
-            (0, s.qd)(s.iP.NSFW_CHANNEL_AGREE_CTA, O, y, j), null == f || f();
-        }, [f, O, y, j]);
-    return (0, r.jsxs)(l.Z, {
-        className: u.gatedContent,
-        justify: l.Z.Justify.CENTER,
-        align: l.Z.Align.CENTER,
-        direction: l.Z.Direction.VERTICAL,
+        (0, s.YT)(g, b, _);
+    }, [g, b, _]);
+    let C = i.useCallback(() => {
+            (0, s.qd)(s.iP.NSFW_CHANNEL_DISAGREE_CTA, g, b, _), null == h || h();
+        }, [h, g, b, _]),
+        v = i.useCallback(() => {
+            (0, s.qd)(s.iP.NSFW_CHANNEL_AGREE_CTA, g, b, _), null == p || p();
+        }, [p, g, b, _]);
+    return (0, r.jsxs)(a.Kqy, {
+        className: c.gatedContent,
+        justify: 'center',
+        align: 'center',
+        direction: 'vertical',
         children: [
-            (0, r.jsx)('div', { className: c()(u.image, b) }),
+            (0, r.jsx)('div', { className: o()(c.image, f) }),
             (0, r.jsx)('div', {
-                className: c()(u.title, g.marginBottom8),
+                className: o()(c.title, u.marginBottom8),
                 children: t
             }),
             (0, r.jsx)(a.Text, {
                 variant: 'text-md/medium',
-                className: c()(u.description, g.marginBottom20),
+                className: o()(c.description, u.marginBottom20),
                 children: n
             }),
-            (0, r.jsxs)(l.Z, {
-                justify: l.Z.Justify.CENTER,
-                align: l.Z.Align.CENTER,
-                grow: 0,
+            (0, r.jsxs)(a.Kqy, {
+                direction: 'horizontal',
+                justify: 'center',
+                align: 'center',
+                gap: 16,
                 children: [
-                    null != p && null != d
+                    null != d && null != h
                         ? (0, r.jsx)(a.zxk, {
-                              className: u.action,
-                              size: a.PhG.LARGE,
                               color: a.Ttl.PRIMARY,
-                              onClick: E,
-                              children: p
+                              onClick: C,
+                              children: d
                           })
                         : null,
-                    null != o && null != f
+                    null != l && null != p
                         ? (0, r.jsx)(a.zxk, {
-                              className: u.action,
                               color: m,
-                              size: a.PhG.LARGE,
-                              onClick: h,
-                              children: o
+                              onClick: v,
+                              children: l
                           })
                         : null
                 ]
