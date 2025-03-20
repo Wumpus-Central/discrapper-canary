8743e2df9b5205fb6bb12e9ae0d13984fffaa99f
DiscrapperManager <admin@minerpl.xyz>
2025-03-20 23:38:02 +0000

    canaryRelease: 380611
    Main scripts: 3
    Lazy Loaded Scripts: 979
    Chunks:
     * Main Files: 11285
     * Scripts: 11013
    Strings: 0
    Routes: 706
    Experiments Count: 422

--- chunks/460608.js
+++ chunks/460608.js
@@ -1,36 +1,42 @@
-t.d(n, { Z: () => x }), t(978209);
-var r = t(200651),
-    i = t(192379),
+t.d(n, { Z: () => m }), t(230036);
+var i = t(200651),
+    r = t(192379),
     o = t(793030),
     s = t(442837),
     a = t(179360),
     l = t(899667),
-    c = t(659679),
-    d = t(388032),
-    u = t(549564);
-function x(e) {
+    c = t(430824),
+    d = t(659679),
+    u = t(388032),
+    x = t(549564);
+function m(e) {
     let { guildId: n } = e,
-        t = (0, s.e7)(
+        t = (0, s.Wu)(
             [l.Z],
             () => {
                 var e;
-                return null === (e = l.Z.getAppliedGuildBoostsForGuild(n)) || void 0 === e ? void 0 : e.reverse();
+                return null !== (e = l.Z.getAppliedGuildBoostsForGuild(n)) && void 0 !== e ? e : [];
             },
             [n]
-        );
-    return (i.useEffect(() => {
+        ),
+        m = r.useMemo(() => t.sort((e, n) => (e.id < n.id ? 1 : -1)), [t]),
+        v = (0, s.e7)([c.Z], () => {
+            var e;
+            return null === (e = c.Z.getGuild(n)) || void 0 === e ? void 0 : e.premiumSubscriberCount;
+        });
+    return (r.useEffect(() => {
         (0, a.C0)(n);
-    }, [n]),
-    null == t)
+    }, [n, v]),
+    0 === m.length)
         ? null
-        : (0, r.jsxs)('div', {
-              className: u.container,
+        : (0, i.jsxs)('div', {
+              className: x.container,
               children: [
-                  (0, r.jsx)(o.X6, {
+                  (0, i.jsx)(o.X6, {
                       variant: 'eyebrow',
-                      children: d.NW.string(d.t.yM9Krq)
+                      children: u.NW.string(u.t.yM9Krq)
                   }),
-                  t.map((e, n) => (0, r.jsx)(c.Z, { boost: e }, 'boost-activity-'.concat(n)))
+                  m.map((e, n) => (0, i.jsx)(d.Z, { boost: e }, 'boost-activity-'.concat(n)))
               ]
           });
 }
--- chunks/639660.js
+++ chunks/639660.js
@@ -1,13 +1,20 @@
 t.d(n, { Z: () => c }), t(47120);
-var r = t(200651),
-    i = t(704215),
+var i = t(200651),
+    r = t(704215),
     o = t(243778),
     s = t(618460),
     a = t(800869),
     l = t(309945);
 function c(e) {
     let { guildId: n } = e,
-        [t, c] = (0, o.US)([i.z.GUILD_POWERUPS_OVERVIEW_SIDEBAR_COACHMARK], void 0, !1),
+        [t, c] = (0, o.US)([r.z.GUILD_POWERUPS_OVERVIEW_SIDEBAR_COACHMARK], void 0, !1),
         d = (0, s.Z)(n);
-    return d.length > 0 ? (0, r.jsx)(l.Z, { powerup: d[0] }) : null != t && t === i.z.GUILD_POWERUPS_OVERVIEW_SIDEBAR_COACHMARK ? (0, r.jsx)(a.Z, { markAsDismissed: c }) : null;
+    return d.length > 0
+        ? (0, i.jsx)(l.Z, {
+              guildId: n,
+              powerups: d
+          })
+        : null != t && t === r.z.GUILD_POWERUPS_OVERVIEW_SIDEBAR_COACHMARK
+          ? (0, i.jsx)(a.Z, { markAsDismissed: c })
+          : null;
 }
