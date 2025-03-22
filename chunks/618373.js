d83c99ec64ed4b16ef9b12a5a11824ee63c0e83f
DiscrapperManager <admin@minerpl.xyz>
2025-03-22 22:58:45 +0000

    canaryRelease: 381150
    Main scripts: 3
    Lazy Loaded Scripts: 986
    Chunks:
     * Main Files: 11283
     * Scripts: 11023
    Strings: 0
    Routes: 705
    Experiments Count: 422

--- chunks/618373.js
+++ chunks/618373.js
@@ -1,22 +1,38 @@
-n.d(t, { Q: () => a });
+n.d(t, { Q: () => u });
 var i = n(200651);
 n(192379);
-var r = n(809357),
-    o = n(44359),
-    l = n(388032);
-function a(e, t, n) {
-    return (0, i.jsx)(s, {
+var r = n(442837),
+    l = n(481060),
+    o = n(594190),
+    a = n(809357),
+    s = n(44359),
+    c = n(388032);
+function u(e, t, n, r) {
+    return (0, i.jsx)(d, {
         keybind: e,
         message: t,
-        messageWithoutKeybind: n
+        messageWithoutKeybind: n,
+        highlightAdminWarningIfElevated: r
     });
 }
-function s(e) {
-    let { keybind: t, message: n, messageWithoutKeybind: a } = e;
-    return (0, r.o)()
-        ? l.NW.format(n, {
-              keybind: t,
-              keybindHook: (e) => (0, i.jsx)(o.Z, { keybind: t }, t.join('+'))
+function d(e) {
+    let { keybind: t, message: n, messageWithoutKeybind: u, highlightAdminWarningIfElevated: d = !1 } = e,
+        p = (0, a.o)();
+    return (0, r.e7)([o.ZP], () => {
+        let e = o.ZP.getVisibleGame();
+        return null != e && e.elevated && o.ZP.canShowAdminWarning;
+    })
+        ? (0, i.jsx)(l.Text, {
+              variant: 'text-xxs/semibold',
+              color: d ? 'text-warning' : 'text-muted',
+              children: c.NW.string(c.t.PIeSHB)
           })
-        : a;
+        : p
+          ? (0, i.jsx)(i.Fragment, {
+                children: c.NW.format(n, {
+                    keybind: t,
+                    keybindHook: (e) => (0, i.jsx)(s.Z, { keybind: t }, t.join('+'))
+                })
+            })
+          : u;
 }
