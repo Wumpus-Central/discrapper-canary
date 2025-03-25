6d8949e43b0552ec8c729749f36564908bc543f6
DiscrapperManager <admin@minerpl.xyz>
2025-03-25 04:33:32 +0000

    canaryRelease: 381681
    Main scripts: 3
    Lazy Loaded Scripts: 986
    Chunks:
     * Main Files: 11276
     * Scripts: 11031
    Strings: 0
    Routes: 705
    Experiments Count: 422

--- chunks/490029.js
+++ chunks/490029.js
@@ -1,44 +1,65 @@
 n.d(t, {
-    $j: () => c,
-    Ty: () => d,
+    $j: () => u,
+    Ty: () => f,
     lW: () => p,
-    sr: () => f,
-    zP: () => u
-});
+    sr: () => _,
+    zP: () => d
+}),
+    n(411104);
 var r = n(536285),
     i = n(996106),
     o = n(812967),
-    a = n(511405),
-    s = n(981631),
-    l = n(186901);
-function c() {
+    a = n(960048),
+    s = n(511405),
+    l = n(981631),
+    c = n(186901);
+function u() {
     r.default.connect();
 }
-function u() {
+function d() {
     r.default.disconnect();
 }
-function d(e, t) {
-    if (!__OVERLAY__) throw new i.Z({ errorCode: s.lTL.UNKNOWN_ERROR }, 'called from wrong app context');
-    r.default.subscribe(s.zMe.OVERLAY, { token: t }, (t) => e((0, a.H)(t)));
-}
 function f(e, t) {
-    if (__OVERLAY__) throw new i.Z({ errorCode: s.lTL.UNKNOWN_ERROR }, 'called from wrong app context');
-    o.Z.setCommandHandler(s.Etm.OVERLAY, {
-        scope: l.cE,
+    if (!__OVERLAY__) throw new i.Z({ errorCode: l.lTL.UNKNOWN_ERROR }, 'called from wrong app context');
+    r.default.subscribe(l.zMe.OVERLAY, { token: t }, (t) => e((0, s.H)(t)));
+}
+function _(e, t) {
+    if (__OVERLAY__) throw new i.Z({ errorCode: l.lTL.UNKNOWN_ERROR }, 'called from wrong app context');
+    o.Z.setCommandHandler(l.Etm.OVERLAY, {
+        scope: c.cE,
         handler(n) {
             let { args: r } = n;
-            if (!t(r.token)) throw new i.Z({ errorCode: s.lTL.INVALID_TOKEN }, 'Invalid RPC auth token provided');
-            e((0, a.H)(r));
+            if (!t(r.token)) throw new i.Z({ errorCode: l.lTL.INVALID_TOKEN }, 'Invalid RPC auth token provided');
+            e((0, s.H)(r));
         }
     }),
-        o.Z.setEventHandler(s.zMe.OVERLAY, {
-            scope: l.cE,
+        o.Z.setEventHandler(l.zMe.OVERLAY, {
+            scope: c.cE,
             handler(e) {
                 let { args: n } = e;
-                if (!t(n.token)) throw new i.Z({ errorCode: s.lTL.INVALID_TOKEN }, 'Invalid RPC auth token provided');
+                if (!t(n.token)) throw new i.Z({ errorCode: l.lTL.INVALID_TOKEN }, 'Invalid RPC auth token provided');
             }
         });
 }
 function p(e) {
-    __OVERLAY__ ? r.default.request(s.Etm.OVERLAY, (0, a.G)(e)) : o.Z.dispatchToSubscriptions(s.zMe.OVERLAY, {}, (0, a.G)(e));
+    if (__OVERLAY__) r.default.request(l.Etm.OVERLAY, (0, s.G)(e));
+    else
+        try {
+            o.Z.dispatchToSubscriptions(l.zMe.OVERLAY, {}, (0, s.G)(e));
+        } catch (i) {
+            let t = e.type,
+                n = [];
+            t === l.BmY.DISPATCH && (n = e.payloads.map((e) => e.type));
+            let r = Error('Error dispatching overlay RPC: '.concat(i.message));
+            throw (
+                (i.stack && (r.stack = i.stack),
+                a.Z.captureException(r, {
+                    extra: {
+                        eventType: t,
+                        payloadTypes: n
+                    }
+                }),
+                r)
+            );
+        }
 }
