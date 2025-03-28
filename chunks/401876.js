480f03844b1467ee7bb9e4c22c7399a6fd710c13
DiscrapperManager <admin@minerpl.xyz>
2025-03-28 01:51:16 +0000

    canaryRelease: 383409
    Main scripts: 3
    Lazy Loaded Scripts: 990
    Chunks:
     * Main Files: 11272
     * Scripts: 11069
    Strings: 0
    Routes: 705
    Experiments Count: 419

--- chunks/401876.js
+++ chunks/401876.js
@@ -1,35 +1,28 @@
-r.d(t, { Z: () => d });
+r.d(t, { Z: () => a });
 var n = r(200651),
     s = r(192379),
     l = r(481060),
-    i = r(377171),
-    c = r(854698),
-    o = r(388032),
-    a = r(9763);
-function d(e) {
-    let { startDate: t, recurrenceRule: r, onRecurrenceChange: d } = e,
-        u = s.useMemo(() => (0, c.zi)(t, r), [r, t]),
-        x = (0, c.P8)(t),
-        j = (e) => e.toString(),
-        m = (0, n.jsxs)('div', {
-            className: a.title,
-            children: [
-                o.NW.string(o.t['59TVxM']),
-                (0, n.jsx)(l.IGR, {
-                    text: o.NW.string(o.t.y2b7CA),
-                    color: i.Z.BG_BRAND
-                })
-            ]
+    i = r(854698),
+    c = r(388032),
+    o = r(580563);
+function a(e) {
+    let { startDate: t, recurrenceRule: r, onRecurrenceChange: a } = e,
+        d = s.useMemo(() => (0, i.zi)(t, r), [r, t]),
+        u = (0, i.P8)(t),
+        x = (e) => e.toString(),
+        j = (0, n.jsx)('div', {
+            className: o.title,
+            children: c.NW.string(c.t['59TVxM'])
         });
     return (0, n.jsx)(l.xJW, {
-        title: m,
+        title: j,
         required: !0,
         children: (0, n.jsx)(l.PhF, {
             placeholder: 'gaming',
-            options: x,
-            select: d,
-            serialize: j,
-            isSelected: (e) => null != u && j(e) === j(u)
+            options: u,
+            select: a,
+            serialize: x,
+            isSelected: (e) => null != d && x(e) === x(d)
         })
     });
 }
