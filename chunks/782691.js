490bccf5ddeceef22ffd8fd7f21204f635c0cff2
DiscrapperManager <admin@minerpl.xyz>
2025-04-01 20:05:53 +0000

    canaryRelease: 384791
    Main scripts: 3
    Lazy Loaded Scripts: 994
    Chunks:
     * Main Files: 11371
     * Scripts: 11300
    Strings: 0
    Routes: 706
    Experiments Count: 425

--- chunks/782691.js
+++ chunks/782691.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => C }), n(47120), n(26686), n(653041);
+n.d(t, { Z: () => C }), n(47120), n(653041), n(26686);
 var r = n(200651),
     i = n(192379),
     s = n(120356),
@@ -101,7 +101,28 @@ let j = {
                         W((r) => E(_({}, r), { [e]: (0, h.z)(t, n) }));
                     });
             },
-            U = (e) => {
+            U = (e, t) => {
+                let n = B(e);
+                null != n &&
+                    (0, h.i0)(n, (e) => {
+                        w((r) => {
+                            let i = [...r],
+                                s = r[t];
+                            if (null == s) return r;
+                            let a = _({}, s);
+                            return (
+                                null == a.randomizedSources && (a.randomizedSources = []),
+                                a.randomizedSources.push({
+                                    src: e,
+                                    filename: n.name
+                                }),
+                                (i[t] = a),
+                                i
+                            );
+                        });
+                    });
+            },
+            V = (e) => {
                 W((t) => E(_({}, t), { [e]: null }));
             };
         i.useEffect(() => {
@@ -116,16 +137,16 @@ let j = {
                         null != n && ((n.src = (0, h.$j)(n.base64)), W((e) => E(_({}, e), { [t]: n })));
                     });
             }, [t.config.stillFrames]);
-        let V = {
+        let G = {
                 effect: t,
                 upsertConfig: s
             },
-            G = i.useRef(V);
+            F = i.useRef(G);
         return (i.useEffect(() => {
-            G.current = V;
+            F.current = G;
         }),
         i.useEffect(() => {
-            let { effect: e, upsertConfig: t } = G.current;
+            let { effect: e, upsertConfig: t } = F.current;
             t({
                 id: e.id,
                 name: R,
@@ -363,7 +384,7 @@ let j = {
                                                                           size: c.PhG.TINY,
                                                                           color: c.Ttl.RED,
                                                                           look: c.iLD.OUTLINED,
-                                                                          onClick: () => U(t),
+                                                                          onClick: () => V(t),
                                                                           children: 'Clear'
                                                                       })
                                                               ]
@@ -445,20 +466,30 @@ let j = {
                                                                   (0, r.jsxs)(r.Fragment, {
                                                                       children: [
                                                                           (0, r.jsx)(c.X6q, {
-                                                                              variant: 'heading-sm/bold',
+                                                                              variant: 'heading-md/bold',
                                                                               children: 'Alternatives'
                                                                           }),
-                                                                          e.randomizedSources.map((e, t) =>
-                                                                              (0, r.jsx)(
-                                                                                  'img',
+                                                                          e.randomizedSources.map((e, t) => {
+                                                                              var n;
+                                                                              return (0, r.jsxs)(
+                                                                                  'div',
                                                                                   {
-                                                                                      src: e.src,
-                                                                                      className: b.layerPreview,
-                                                                                      alt: ''
+                                                                                      children: [
+                                                                                          null != e.filename &&
+                                                                                              (0, r.jsx)(c.X6q, {
+                                                                                                  variant: 'heading-sm/semibold',
+                                                                                                  children: e.filename
+                                                                                              }),
+                                                                                          (0, r.jsx)('img', {
+                                                                                              src: e.src,
+                                                                                              className: b.layerPreview,
+                                                                                              alt: ''
+                                                                                          })
+                                                                                      ]
                                                                                   },
-                                                                                  t
-                                                                              )
-                                                                          )
+                                                                                  null !== (n = e.filename) && void 0 !== n ? n : 'randomized-'.concat(t)
+                                                                              );
+                                                                          })
                                                                       ]
                                                                   })
                                                           ]
@@ -577,20 +608,7 @@ let j = {
                                                                       }),
                                                                       (0, r.jsx)(d.Z, {
                                                                           ref: T,
-                                                                          onChange: (e) => {
-                                                                              let n = e.currentTarget.files;
-                                                                              if (null == n) return;
-                                                                              let r = n[0],
-                                                                                  i = new FileReader();
-                                                                              (i.onload = (e) => {
-                                                                                  w((n) => {
-                                                                                      if (null == e.target || 'string' != typeof e.target.result) return n;
-                                                                                      let r = [...n];
-                                                                                      return null == n[t].randomizedSources && (n[t].randomizedSources = []), n[t].randomizedSources.push({ src: e.target.result }), r;
-                                                                                  });
-                                                                              }),
-                                                                                  i.readAsDataURL(r);
-                                                                          },
+                                                                          onChange: (e) => U(e, t),
                                                                           multiple: !1
                                                                       })
                                                                   ]
