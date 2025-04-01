2e700cd9b4a2166c47695f8cc68ea78404e5a8d3
DiscrapperManager <admin@minerpl.xyz>
2025-04-01 18:25:33 +0000

    canaryRelease: 384716
    Main scripts: 3
    Lazy Loaded Scripts: 994
    Chunks:
     * Main Files: 11372
     * Scripts: 11299
    Strings: 0
    Routes: 706
    Experiments Count: 424

--- chunks/34768.js
+++ chunks/34768.js
@@ -1,2 +1,2 @@
 n.d(t, { Z: () => r });
-let r = 'https://cdn.discordapp.com/assets/content/e7eb7a1f4bd448d45ea616d14cf6c4490a72556e7531b4b311da31df2c5bed4b.mp4';
+let r = 'https://cdn.discordapp.com/assets/content/9c0c235c790bbf305d22c6ba6ddb80e10252cb346bd53c940c945a343b55862b.mp4';
