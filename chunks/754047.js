b38f3ab90c58e3395dec288b96095b23d0349faf
DiscrapperManager <admin@minerpl.xyz>
2025-04-03 17:13:09 +0000

    canaryRelease: 385706
    Main scripts: 3
    Lazy Loaded Scripts: 996
    Chunks:
     * Main Files: 11374
     * Scripts: 11322
    Strings: 0
    Routes: 705
    Experiments Count: 426

--- chunks/754047.js
+++ chunks/754047.js
@@ -1,40 +1,41 @@
-n.d(t, { Z: () => a }), n(47120);
+n.d(t, { Z: () => s }), n(47120);
 var r = n(192379),
+    l = n(481060),
     o = n(493773),
     i = n(81897),
-    l = n(228168);
-function a(e) {
+    a = n(228168);
+function s(e) {
     let { shouldShowPopoutOnHover: t, handlePreload: n } = e,
-        [a, c] = r.useState(!1),
-        s = r.useRef(!1),
-        u = (0, i.Z)(),
+        [s, c] = r.useState(!1),
+        u = r.useRef(!1),
+        d = (0, i.Z)(),
         m = r.useRef(void 0),
         p = r.useRef(void 0),
-        d = r.useRef(void 0);
+        f = r.useRef(void 0);
     return ((0, o.zq)(() => {
-        clearTimeout(m.current), clearTimeout(p.current), clearTimeout(d.current);
+        clearTimeout(m.current), clearTimeout(p.current), clearTimeout(f.current);
     }),
     t)
         ? {
-              showPopoutFromHover: a,
+              showPopoutFromHover: s,
               onRequestClose: () => {
                   c(!1);
               },
               onMouseEnter: () => {
                   let e;
-                  (s.current = !0),
+                  (u.current = !0),
                       (m.current = setTimeout(() => {
-                          s.current && (e = n(u));
-                      }, l.a6)),
+                          u.current && (e = n(d));
+                      }, a.a6)),
                       (p.current = setTimeout(async () => {
-                          s.current && (null != e && (await e), c(!0));
-                      }, l.JX));
+                          !(0, l.$sL)() && u.current && (null != e && (await e), c(!0));
+                      }, a.JX));
               },
               onMouseLeave: () => {
-                  (s.current = !1),
-                      (d.current = setTimeout(() => {
-                          s.current || c(!1);
-                      }, l.Ig));
+                  (u.current = !1),
+                      (f.current = setTimeout(() => {
+                          u.current || c(!1);
+                      }, a.Ig));
               }
           }
         : {
