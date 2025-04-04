ad60ab82b59bc5f4a3410c8a1dead1be9b65a6d0
DiscrapperManager <admin@minerpl.xyz>
2025-04-04 22:46:03 +0000

    canaryRelease: 386569
    Main scripts: 3
    Lazy Loaded Scripts: 995
    Chunks:
     * Main Files: 11396
     * Scripts: 11350
    Strings: 0
    Routes: 707
    Experiments Count: 427

--- chunks/297651.js
+++ chunks/297651.js
@@ -1,4 +1,4 @@
-n.d(t, { E: () => p }), n(47120);
+n.d(t, { E: () => p });
 var r = n(192379),
     l = n(442837),
     a = n(626135),
@@ -16,46 +16,43 @@ function p(e, t) {
         g = (0, d.x)(),
         h = o.ZP.canUseCollectibles(g),
         m = r.useRef(null),
-        [_, v] = r.useState(!1),
-        [C, x] = r.useState(!1),
-        O = (0, u.B)('shop_product_card');
+        _ = (0, u.B)('shop_product_card'),
+        v = r.useCallback(() => {
+            if (!_) return;
+            let r = null != f ? (0, c.Vw)(f, h, !1) : null,
+                l = null != f ? (0, c.eu)(f, h, !1) : void 0;
+            a.default.track(b.rMx.COLLECTIBLES_TILE_IMPRESSION, {
+                collectibles_shop_session_id: null == p ? void 0 : p.sessionId,
+                sku_id: e,
+                display_price: null == r ? void 0 : r.amount,
+                display_price_currency: null == r ? void 0 : r.currency.toString(),
+                display_price_strikethrough: l,
+                position: null == p ? void 0 : p.tilePosition,
+                page_type: t,
+                page_category: null == p ? void 0 : p.pageCategory,
+                page_section: null == p ? void 0 : p.pageSection,
+                type: n,
+                category_position: null == p ? void 0 : p.categoryPosition
+            });
+        }, [null == p ? void 0 : p.sessionId, null == p ? void 0 : p.categoryPosition, null == p ? void 0 : p.pageCategory, null == p ? void 0 : p.pageSection, null == p ? void 0 : p.tilePosition, h, _, t, f, e, n]),
+        C = r.useCallback(
+            (e) => {
+                e
+                    ? null === m.current &&
+                      (m.current = setTimeout(() => {
+                          v(), (m.current = null);
+                      }, 1000))
+                    : null !== m.current && (clearTimeout(m.current), (m.current = null));
+            },
+            [v]
+        );
     return (
         r.useEffect(
-            () => (
-                _ && null === m.current
-                    ? (m.current = setTimeout(() => {
-                          x(!0);
-                      }, 1000))
-                    : _ || (null !== m.current && (clearTimeout(m.current), (m.current = null)), x(!1)),
-                () => {
-                    null !== m.current && (clearTimeout(m.current), (m.current = null));
-                }
-            ),
-            [_]
+            () => () => {
+                null !== m.current && (clearTimeout(m.current), (m.current = null));
+            },
+            []
         ),
-        r.useEffect(() => {
-            if (C && O) {
-                let r = null != f ? (0, c.Vw)(f, h, !1) : null,
-                    l = null != f ? (0, c.eu)(f, h, !1) : void 0;
-                a.default.track(b.rMx.COLLECTIBLES_TILE_IMPRESSION, {
-                    collectibles_shop_session_id: null == p ? void 0 : p.sessionId,
-                    sku_id: e,
-                    display_price: null == r ? void 0 : r.amount,
-                    display_price_currency: null == r ? void 0 : r.currency.toString(),
-                    display_price_strikethrough: l,
-                    position: null == p ? void 0 : p.tilePosition,
-                    page_type: t,
-                    page_category: null == p ? void 0 : p.pageCategory,
-                    page_section: null == p ? void 0 : p.pageSection,
-                    type: n,
-                    category_position: null == p ? void 0 : p.categoryPosition
-                });
-            }
-        }, [null == p ? void 0 : p.sessionId, null == p ? void 0 : p.categoryPosition, null == p ? void 0 : p.pageCategory, null == p ? void 0 : p.pageSection, null == p ? void 0 : p.tilePosition, C, h, O, t, f, e, n]),
-        {
-            handleCardVisibilityChange: (e) => {
-                v(e);
-            }
-        }
+        { handleCardVisibilityChange: C }
     );
 }
