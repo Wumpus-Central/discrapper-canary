98ac16fa2f6a5fc429bb1394bccc3f02858e70ad
DiscrapperManager <admin@minerpl.xyz>
2025-04-04 06:54:13 +0000

    canaryRelease: 386225
    Main scripts: 3
    Lazy Loaded Scripts: 995
    Chunks:
     * Main Files: 11383
     * Scripts: 11316
    Strings: 0
    Routes: 705
    Experiments Count: 425

--- chunks/532239.js
+++ chunks/532239.js
@@ -1,23 +1,24 @@
-n.d(t, { Z: () => u });
+n.d(t, { Z: () => d });
 var r = n(200651);
 n(192379);
 var i = n(481060),
     l = n(40851),
-    o = n(493754),
-    a = n(496675),
-    s = n(981631),
-    c = n(388032);
-function u(e, t, n, u) {
-    let d = {
+    o = n(340541),
+    a = n(493754),
+    s = n(496675),
+    c = n(981631),
+    u = n(388032);
+function d(e, t, n, d) {
+    let p = {
             streamUserId: n,
             channel: e,
-            applicationId: u,
+            applicationId: d,
             appContext: (0, l.bp)()
         },
-        p = () => {
+        h = () => {
             null != t &&
-                e.type === s.d4z.GUILD_VOICE &&
-                (0, o.W)(
+                e.type === c.d4z.GUILD_VOICE &&
+                (0, a.W)(
                     (function (e) {
                         for (var t = 1; t < arguments.length; t++) {
                             var n = null != arguments[t] ? arguments[t] : {},
@@ -42,22 +43,24 @@ function u(e, t, n, u) {
                                 });
                         }
                         return e;
-                    })({ guild: t }, d)
+                    })({ guild: t }, p)
                 );
-        };
-    return null != u
-        ? (0, r.jsx)(i.sNh, {
-              id: 'invite-to-stream',
-              label: c.NW.string(c.t['OzOM/v']),
-              action: p,
-              icon: i.ejJ
-          })
-        : null != t && null != e && a.Z.can(s.Plq.CREATE_INSTANT_INVITE, e)
-          ? (0, r.jsx)(i.sNh, {
-                id: 'invite-friends',
-                label: c.NW.string(c.t['6Qgren']),
-                action: p,
-                icon: i.ejJ
-            })
-          : null;
+        },
+        { enabled: f } = (0, o._k)({ location: 'more_settings_invite_item' }),
+        m = f ? i.oLu : i.ejJ;
+    if (null != d)
+        return (0, r.jsx)(i.sNh, {
+            id: 'invite-to-stream',
+            label: u.NW.string(u.t['OzOM/v']),
+            action: h,
+            icon: i.ejJ
+        });
+    if (null == t || null == e || !s.Z.can(c.Plq.CREATE_INSTANT_INVITE, e)) return null;
+    let g = f ? u.NW.string(u.t['EE+P0N']) : u.NW.string(u.t['6Qgren']);
+    return (0, r.jsx)(i.sNh, {
+        id: 'invite-friends',
+        label: g,
+        action: h,
+        icon: m
+    });
 }
