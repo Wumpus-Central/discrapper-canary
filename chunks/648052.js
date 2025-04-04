7def3677055568bb0d16ba1ed14261d98666c863
DiscrapperManager <admin@minerpl.xyz>
2025-04-04 20:53:23 +0000

    canaryRelease: 386504
    Main scripts: 3
    Lazy Loaded Scripts: 995
    Chunks:
     * Main Files: 11389
     * Scripts: 11320
    Strings: 0
    Routes: 706
    Experiments Count: 427

--- chunks/648052.js
+++ chunks/648052.js
@@ -1,34 +1,36 @@
-n.d(t, { Z: () => u }), n(47120);
+n.d(t, { Z: () => d }), n(47120);
 var r = n(200651),
     i = n(192379),
     o = n(392711),
     a = n(485341),
-    s = n(184325),
-    l = n(228168);
-let c = {
-    [l.y0.BITE_SIZE]: 12,
-    [l.y0.FULL_SIZE]: 26,
-    [l.y0.PANEL]: 13
+    s = n(819325),
+    l = n(184325),
+    c = n(228168);
+let u = {
+    [c.y0.BITE_SIZE]: 12,
+    [c.y0.FULL_SIZE]: 26,
+    [c.y0.PANEL]: 13
 };
-function u(e) {
-    let { displayProfile: t, profileType: n, onClose: l, pendingBadges: u, shouldOpenBadgeTooltip: d } = e,
-        f = (0, a.Z)(t),
-        _ = i.useMemo(() => {
-            if (null == u) return f;
-            let e = new Set(f.map((e) => e.id));
-            return [...f, ...u.filter((t) => !e.has(t.id))];
-        }, [f, u]);
-    return 0 === _.length
+function d(e) {
+    let { displayProfile: t, profileType: n, onClose: c, pendingBadges: d, shouldOpenBadgeTooltip: f } = e,
+        _ = (0, a.Z)(t),
+        p = i.useMemo(() => {
+            if (null == d) return (0, s.w)(_);
+            let e = new Set(_.map((e) => e.id)),
+                t = [..._, ...d.filter((t) => !e.has(t.id))];
+            return (0, s.w)(t);
+        }, [_, d]);
+    return 0 === p.length
         ? null
         : (0, r.jsx)(r.Fragment, {
-              children: (0, o.chunk)(_, c[n]).map((e) =>
+              children: (0, o.chunk)(p, u[n]).map((e) =>
                   (0, r.jsx)(
-                      s.Z,
+                      l.Z,
                       {
                           badges: e,
                           displayProfile: t,
-                          onClose: l,
-                          shouldOpenBadgeTooltip: d
+                          onClose: c,
+                          shouldOpenBadgeTooltip: f
                       },
                       e[0].id
                   )
