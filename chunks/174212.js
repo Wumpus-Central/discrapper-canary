80ebe8de0081642860cc41ad7a40e1e502cae844
DiscrapperManager <admin@minerpl.xyz>
2025-04-07 17:31:07 +0000

    canaryRelease: 386762
    Main scripts: 3
    Lazy Loaded Scripts: 995
    Chunks:
     * Main Files: 11397
     * Scripts: 11353
    Strings: 21140
    Routes: 707
    Experiments Count: 428

--- chunks/174212.js
+++ chunks/174212.js
@@ -1,11 +1,13 @@
-n.d(t, { Z: () => T }), n(47120);
+n.d(t, { Z: () => T }), n(388685);
 var r,
     i = n(442837),
     o = n(570140),
-    a = n(367907),
-    s = n(998698),
-    l = n(981631);
-function c(e, t, n) {
+    a = n(911969),
+    s = n(367907),
+    l = n(358085),
+    c = n(998698),
+    u = n(981631);
+function d(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -18,54 +20,15 @@ function c(e, t, n) {
         e
     );
 }
-function u(e) {
-    for (var t = 1; t < arguments.length; t++) {
-        var n = null != arguments[t] ? arguments[t] : {},
-            r = Object.keys(n);
-        'function' == typeof Object.getOwnPropertySymbols &&
-            (r = r.concat(
-                Object.getOwnPropertySymbols(n).filter(function (e) {
-                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
-                })
-            )),
-            r.forEach(function (t) {
-                c(e, t, n[t]);
-            });
-    }
-    return e;
-}
-function d(e, t) {
-    var n = Object.keys(e);
-    if (Object.getOwnPropertySymbols) {
-        var r = Object.getOwnPropertySymbols(e);
-        t &&
-            (r = r.filter(function (t) {
-                return Object.getOwnPropertyDescriptor(e, t).enumerable;
-            })),
-            n.push.apply(n, r);
-    }
-    return n;
-}
-function f(e, t) {
-    return (
-        (t = null != t ? t : {}),
-        Object.getOwnPropertyDescriptors
-            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : d(Object(t)).forEach(function (n) {
-                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
-              }),
-        e
-    );
-}
-let _ = new Map(),
-    p = new Map(),
-    h = new Map();
-function m(e) {
-    if (!p.has(e)) {
+let f = new Map(),
+    _ = new Map(),
+    p = new Map();
+function h(e) {
+    if (!_.has(e)) {
         var t;
-        p.set(e, {
-            commandId: null == (t = s.Z.getActiveCommand(e)) ? void 0 : t.id,
-            optionName: s.Z.getActiveOptionName(e),
+        _.set(e, {
+            commandId: null == (t = c.Z.getActiveCommand(e)) ? void 0 : t.id,
+            optionName: c.Z.getActiveOptionName(e),
             optionNameToAutocompleteQueries: new Map(),
             optionNameToLastResults: new Map(),
             optionNameToNonce: new Map(),
@@ -74,76 +37,88 @@ function m(e) {
             lastResponseNonce: void 0
         });
     }
-    return p.get(e);
+    return _.get(e);
 }
-function g() {
-    return _.clear(), p.clear(), !0;
+function m() {
+    return f.clear(), _.clear(), !0;
 }
-function E(e) {
+function g(e) {
     var t;
     let { nonce: n, channelId: r, query: i, name: o } = e,
-        a = m(r);
+        a = h(r);
     if (a.optionNameToLastQuery.get(o) === i) return !1;
     a.optionNameToLastQuery.set(o, i);
     let s = null == (t = a.optionNameToAutocompleteQueries.get(o)) ? void 0 : t.get(i);
     if (null != s) return (a.lastErrored = !1), a.optionNameToLastResults.set(o, s), !0;
     let l = a.optionNameToNonce.get(o);
     if (
-        (null != l && _.delete(l),
-        _.set(n, {
+        (null != l && f.delete(l),
+        f.set(n, {
             channelId: r,
             query: i,
             name: o
         }),
-        h.set(n, new Date()),
+        p.set(n, new Date()),
         a.optionNameToNonce.set(o, n),
         a.lastErrored)
     )
         return (a.lastErrored = !1), !0;
 }
+let E = (0, l.isDesktop)();
 function b(e) {
-    var t, n;
-    let { choices: r, nonce: i } = e,
-        o = _.get(i);
-    if (null == o) return !1;
-    _.delete(i);
-    let s =
+    var t, n, r;
+    let { choices: i, nonce: o } = e,
+        l = f.get(o);
+    if (null == l) return !1;
+    f.delete(o);
+    let d = h(l.channelId);
+    null == d.optionNameToAutocompleteQueries.get(l.name) && d.optionNameToAutocompleteQueries.set(l.name, new Map());
+    let _ = E && (null == (t = c.Z.getActiveOption(l.channelId)) ? void 0 : t.type) === a.jw.INTEGER,
+        m =
             null !=
-            (n =
-                null == r
+            (r =
+                null == i
                     ? void 0
-                    : r.map((e) => {
-                          var t;
-                          return f(u({}, e), { displayName: null != (t = e.name_localized) ? t : e.name });
+                    : i.map((e) => {
+                          let { value: t, name_localized: n, name: r } = e;
+                          return {
+                              displayName: null != n ? n : r,
+                              name: r,
+                              value: (t = _ ? Number(t) : t)
+                          };
                       }))
-                ? n
+                ? r
                 : [],
-        c = h.get(i),
-        d = null != c ? new Date().getTime() - c.getTime() : 0;
-    (0, a.yw)(l.rMx.APPLICATION_COMMAND_OPTION_STRING_AUTOCOMPLETE_PERFORMANCE, {
-        duration_ms: d,
-        error: !1,
-        num_options: s.length
-    }),
-        h.delete(i);
-    let p = m(o.channelId);
-    return null == p.optionNameToAutocompleteQueries.get(o.name) && p.optionNameToAutocompleteQueries.set(o.name, new Map()), null == (t = p.optionNameToAutocompleteQueries.get(o.name)) || t.set(o.query, s), p.optionNameToLastQuery.get(o.name) === o.query && ((p.lastErrored = !1), p.optionNameToLastResults.set(o.name, s)), (p.lastResponseNonce = i), !0;
+        g = p.get(o),
+        b = null != g ? new Date().getTime() - g.getTime() : 0;
+    return (
+        (0, s.yw)(u.rMx.APPLICATION_COMMAND_OPTION_STRING_AUTOCOMPLETE_PERFORMANCE, {
+            duration_ms: b,
+            error: !1,
+            num_options: m.length
+        }),
+        p.delete(o),
+        null == (n = d.optionNameToAutocompleteQueries.get(l.name)) || n.set(l.query, m),
+        d.optionNameToLastQuery.get(l.name) === l.query && ((d.lastErrored = !1), d.optionNameToLastResults.set(l.name, m)),
+        (d.lastResponseNonce = o),
+        !0
+    );
 }
 function y(e) {
     let { nonce: t } = e;
     if (null == t) return !1;
-    let n = _.get(t);
+    let n = f.get(t);
     if (null == n) return !1;
-    _.delete(t);
-    let r = h.get(t),
+    f.delete(t);
+    let r = p.get(t),
         i = null != r ? new Date().getTime() - r.getTime() : 0;
     return (
-        (0, a.yw)(l.rMx.APPLICATION_COMMAND_OPTION_STRING_AUTOCOMPLETE_PERFORMANCE, {
+        (0, s.yw)(u.rMx.APPLICATION_COMMAND_OPTION_STRING_AUTOCOMPLETE_PERFORMANCE, {
             duration_ms: i,
             error: !0
         }),
-        h.delete(t),
-        (m(n.channelId).lastErrored = !0),
+        p.delete(t),
+        (h(n.channelId).lastErrored = !0),
         !0
     );
 }
@@ -156,34 +131,34 @@ function O(e) {
     I(t, null == n ? void 0 : n.id);
 }
 function I(e, t) {
-    let n = s.Z.getActiveOptionName(e),
-        r = p.get(e);
+    let n = c.Z.getActiveOptionName(e),
+        r = _.get(e);
     return null != r && (t !== r.commandId || n !== r.optionName) && (null != t && t !== r.commandId && (r.optionNameToLastResults.clear(), r.optionNameToNonce.clear(), r.optionNameToLastQuery.clear(), r.optionNameToAutocompleteQueries.clear()), (r.lastErrored = !1), (r.commandId = t), (r.optionName = n), !0);
 }
 class S extends (r = i.ZP.Store) {
     initialize() {
-        this.waitFor(s.Z);
+        this.waitFor(c.Z);
     }
     getLastErrored(e) {
-        return m(e).lastErrored;
+        return h(e).lastErrored;
     }
     getAutocompleteChoices(e, t, n) {
         var r;
-        return null == (r = m(e).optionNameToAutocompleteQueries.get(t)) ? void 0 : r.get(n);
+        return null == (r = h(e).optionNameToAutocompleteQueries.get(t)) ? void 0 : r.get(n);
     }
     getAutocompleteLastChoices(e, t) {
-        return m(e).optionNameToLastResults.get(t);
+        return h(e).optionNameToLastResults.get(t);
     }
     getLastResponseNonce(e) {
-        return m(e).lastResponseNonce;
+        return h(e).lastResponseNonce;
     }
 }
-c(S, 'displayName', 'ApplicationCommandAutocompleteStore');
+d(S, 'displayName', 'ApplicationCommandAutocompleteStore');
 let T = new S(o.Z, {
-    CONNECTION_OPEN: g,
-    LOGOUT: g,
-    CHANNEL_SELECT: g,
-    APPLICATION_COMMAND_AUTOCOMPLETE_REQUEST: E,
+    CONNECTION_OPEN: m,
+    LOGOUT: m,
+    CHANNEL_SELECT: m,
+    APPLICATION_COMMAND_AUTOCOMPLETE_REQUEST: g,
     APPLICATION_COMMAND_AUTOCOMPLETE_RESPONSE: b,
     INTERACTION_FAILURE: y,
     APPLICATION_COMMAND_SET_ACTIVE_COMMAND: v,
