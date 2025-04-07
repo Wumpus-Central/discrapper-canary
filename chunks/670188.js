69e65616882cbf5ac5b789b7ae31fdc72aa32cf8
DiscrapperManager <admin@minerpl.xyz>
2025-04-07 22:27:51 +0000

    canaryRelease: 387024
    Main scripts: 3
    Lazy Loaded Scripts: 991
    Chunks:
     * Main Files: 11396
     * Scripts: 11343
    Strings: 21112
    Routes: 708
    Experiments Count: 425

--- chunks/670188.js
+++ chunks/670188.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => O }), n(388685);
+n.d(t, { Z: () => I }), n(388685);
 var r = n(200651),
     i = n(192379),
     o = n(442837),
@@ -8,8 +8,9 @@ var r = n(200651),
     c = n(184301),
     u = n(347475),
     d = n(228168),
-    f = n(231338);
-function _(e, t, n) {
+    f = n(231338),
+    _ = n(744790);
+function p(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -22,7 +23,7 @@ function _(e, t, n) {
         e
     );
 }
-function p(e) {
+function h(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -33,12 +34,12 @@ function p(e) {
                 })
             )),
             r.forEach(function (t) {
-                _(e, t, n[t]);
+                p(e, t, n[t]);
             });
     }
     return e;
 }
-function h(e, t) {
+function m(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -50,29 +51,29 @@ function h(e, t) {
     }
     return n;
 }
-function m(e, t) {
+function g(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : h(Object(t)).forEach(function (n) {
+            : m(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
     );
 }
-function g(e, t) {
+function E(e, t) {
     if (null == e) return {};
     var n,
         r,
-        i = E(e, t);
+        i = b(e, t);
     if (Object.getOwnPropertySymbols) {
         var o = Object.getOwnPropertySymbols(e);
         for (r = 0; r < o.length; r++) (n = o[r]), !(t.indexOf(n) >= 0) && Object.prototype.propertyIsEnumerable.call(e, n) && (i[n] = e[n]);
     }
     return i;
 }
-function E(e, t) {
+function b(e, t) {
     if (null == e) return {};
     var n,
         r,
@@ -81,96 +82,97 @@ function E(e, t) {
     for (r = 0; r < o.length; r++) (n = o[r]), t.indexOf(n) >= 0 || (i[n] = e[n]);
     return i;
 }
-let b = (0, a.pxk)(a.EFr.SIZE_80),
-    y = {
+let y = (0, a.pxk)(a.EFr.SIZE_80),
+    v = {
         onMouseDown: f.dG,
         onClick: f.dG,
         onKeyDown: f.dG,
         'aria-controls': void 0,
         'aria-expanded': !1
     },
-    v = {
+    O = {
         isShown: !1,
         position: void 0
     };
-function O(e) {
-    var { children: t, userId: n, user: f, guildId: _, channelId: h, avatarUrl: E, preload: O, renderPopout: I, onRequestClose: S, onClosePopout: T, shouldShow: N, shouldPreload: A = !0, shouldShowOnHover: C = !1 } = e,
-        R = g(e, ['children', 'userId', 'user', 'guildId', 'channelId', 'avatarUrl', 'preload', 'renderPopout', 'onRequestClose', 'onClosePopout', 'shouldShow', 'shouldPreload', 'shouldShowOnHover']);
-    let P = (0, o.e7)([l.default], () => l.default.getCurrentUser()),
-        w = (0, o.e7)([l.default], () => (null != f ? f : l.default.getUser(n))),
-        [D, L] = i.useState(!1),
-        [x, M] = i.useState(!1),
-        k = i.useRef(!1),
-        j = i.useRef(void 0),
+function I(e) {
+    var { children: t, userId: n, user: f, guildId: p, channelId: m, avatarUrl: b, preload: I, renderPopout: S, onRequestClose: T, onClosePopout: N, shouldShow: A, shouldPreload: C = !0, shouldShowOnHover: R = !1 } = e,
+        P = E(e, ['children', 'userId', 'user', 'guildId', 'channelId', 'avatarUrl', 'preload', 'renderPopout', 'onRequestClose', 'onClosePopout', 'shouldShow', 'shouldPreload', 'shouldShowOnHover']);
+    let w = (0, o.e7)([l.default], () => l.default.getCurrentUser()),
+        D = (0, o.e7)([l.default], () => (null != f ? f : l.default.getUser(n))),
+        [L, x] = i.useState(!1),
+        [M, k] = i.useState(!1),
+        j = i.useRef(!1),
         U = i.useRef(void 0),
-        G = i.useRef(void 0);
+        G = i.useRef(void 0),
+        B = i.useRef(void 0);
     if (
         ((0, s.zq)(() => {
-            clearTimeout(j.current), clearTimeout(U.current), clearTimeout(G.current);
+            clearTimeout(U.current), clearTimeout(G.current), clearTimeout(B.current);
         }),
-        null == w || null == P)
+        null == D || null == w)
     )
-        return t(y, v);
-    let B = () =>
-            null != O
-                ? O()
-                : (0, c.Z)(w.id, null != E ? E : w.getAvatarURL(_, b), {
-                      guildId: _,
-                      channelId: h
+        return t(v, O);
+    let V = () =>
+            null != I
+                ? I()
+                : (0, c.Z)(D.id, null != b ? b : D.getAvatarURL(p, y), {
+                      guildId: p,
+                      channelId: m
                   }),
-        V = (e) =>
-            x
+        F = (e) =>
+            M
                 ? (0, r.jsx)(a.xxz, {})
-                : null != I
-                  ? I(e)
+                : null != S
+                  ? S(e)
                   : (0, r.jsx)(
                         u.Z,
-                        m(p({}, R, e), {
-                            userId: w.id,
-                            user: w,
-                            guildId: _,
-                            channelId: h,
+                        g(h({}, P, e), {
+                            userId: D.id,
+                            user: D,
+                            guildId: p,
+                            channelId: m,
                             closePopout: () => {
-                                e.closePopout(), null == T || T();
+                                e.closePopout(), null == N || N();
                             }
                         })
                     );
-    return C
+    return R
         ? (0, r.jsx)('div', {
+              className: _.hoverable,
               onMouseEnter: () => {
-                  (k.current = !0),
-                      A &&
-                          (j.current = setTimeout(async () => {
-                              if (k.current)
+                  (j.current = !0),
+                      C &&
+                          (U.current = setTimeout(async () => {
+                              if (j.current)
                                   try {
-                                      M(!0), await B();
+                                      k(!0), await V();
                                   } finally {
-                                      M(!1);
+                                      k(!1);
                                   }
                           }, d.a6)),
-                      (U.current = setTimeout(() => {
-                          !(0, a.$sL)() && k.current && L(!0);
+                      (G.current = setTimeout(() => {
+                          !(0, a.$sL)() && j.current && x(!0);
                       }, d.JX));
               },
               onMouseLeave: () => {
-                  (k.current = !1),
-                      (G.current = setTimeout(() => {
-                          k.current || L(!1);
+                  (j.current = !1),
+                      (B.current = setTimeout(() => {
+                          j.current || x(!1);
                       }, d.Ig));
               },
               children: (0, r.jsx)(
                   a.yRy,
-                  m(
-                      p(
+                  g(
+                      h(
                           {
-                              shouldShow: !0 === N || D,
-                              preload: !A || x || D ? void 0 : B,
-                              renderPopout: V,
+                              shouldShow: !0 === A || L,
+                              preload: !C || M || L ? void 0 : V,
+                              renderPopout: F,
                               onRequestClose: () => {
-                                  L(!1), null == S || S();
+                                  x(!1), null == T || T();
                               }
                           },
-                          R
+                          P
                       ),
                       { children: t }
                   )
@@ -178,15 +180,15 @@ function O(e) {
           })
         : (0, r.jsx)(
               a.yRy,
-              m(
-                  p(
+              g(
+                  h(
                       {
-                          shouldShow: N,
-                          preload: A ? B : void 0,
-                          renderPopout: V,
-                          onRequestClose: S
+                          shouldShow: A,
+                          preload: C ? V : void 0,
+                          renderPopout: F,
+                          onRequestClose: T
                       },
-                      R
+                      P
                   ),
                   { children: t }
               )
---
+++ chunks/744790.js
@@ -0,0 +1,1 @@
+e.exports = { hoverable: 'hoverable__76c22' };
