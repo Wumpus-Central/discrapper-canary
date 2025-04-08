796f955c84028b50f0748c39a362f58a3925c791
DiscrapperManager <admin@minerpl.xyz>
2025-04-08 23:09:59 +0000

    canaryRelease: 387587
    Main scripts: 3
    Lazy Loaded Scripts: 992
    Chunks:
     * Main Files: 11409
     * Scripts: 11329
    Strings: 21130
    Routes: 708
    Experiments Count: 419

--- chunks/819366.js
+++ chunks/819366.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => A });
+n.d(t, { Z: () => C }), n(388685);
 var r = n(200651),
     i = n(192379),
     o = n(873546),
@@ -9,16 +9,17 @@ var r = n(200651),
     u = n(797610),
     d = n(927723),
     f = n(124072),
-    _ = n(670188),
-    p = n(592125),
-    h = n(271383),
-    m = n(430824),
-    g = n(699516),
-    E = n(594174),
-    b = n(5192),
-    y = n(51144),
-    v = n(377668);
-function O(e, t, n) {
+    _ = n(823415),
+    p = n(670188),
+    h = n(592125),
+    m = n(271383),
+    g = n(430824),
+    E = n(699516),
+    b = n(594174),
+    y = n(5192),
+    v = n(51144),
+    O = n(377668);
+function I(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -31,7 +32,7 @@ function O(e, t, n) {
         e
     );
 }
-function I(e) {
+function S(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -42,12 +43,12 @@ function I(e) {
                 })
             )),
             r.forEach(function (t) {
-                O(e, t, n[t]);
+                I(e, t, n[t]);
             });
     }
     return e;
 }
-function S(e, t) {
+function T(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -59,18 +60,18 @@ function S(e, t) {
     }
     return n;
 }
-function T(e, t) {
+function N(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : S(Object(t)).forEach(function (n) {
+            : T(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
     );
 }
-function N(e) {
+function A(e) {
     let { userId: t, className: o, children: a } = e,
         l = i.useCallback(() => {
             (0, d.k)({ kind: 'user' });
@@ -80,7 +81,7 @@ function N(e) {
                 null != t &&
                     (0, s.jW)(e, async () => {
                         let { default: e } = await n.e('25073').then(n.bind(n, 80742));
-                        return (n) => (0, r.jsx)(e, T(I({}, n), { userId: t }));
+                        return (n) => (0, r.jsx)(e, N(S({}, n), { userId: t }));
                     });
             },
             [t]
@@ -92,70 +93,80 @@ function N(e) {
         children: a
     });
 }
-function A(e) {
-    let { className: t, userId: i, channelId: d, parsedUserId: O, content: S, inlinePreview: A = !1, viewingChannelId: C } = e,
-        { analyticsLocations: R } = (0, c.ZP)(l.Z.USER_MENTION),
-        P = (0, a.e7)([E.default], () => E.default.getUser(i)),
-        w = (0, a.e7)([p.Z], () => p.Z.getChannel(d)),
-        D = null != w ? w.getGuildId() : null,
-        L =
-            A || null == P || null == D || null == d
+function C(e) {
+    let { className: t, userId: d, channelId: I, parsedUserId: T, content: C, inlinePreview: R = !1, viewingChannelId: P } = e,
+        { analyticsLocations: w } = (0, c.ZP)(l.Z.USER_MENTION),
+        D = (0, a.e7)([b.default], () => b.default.getUser(d)),
+        L = (0, a.e7)([h.Z], () => h.Z.getChannel(I)),
+        x = null != L ? L.getGuildId() : null,
+        M =
+            R || null == D || null == x || null == I
                 ? void 0
                 : (e) => {
-                      null != w &&
+                      null != L &&
                           (0, s.jW)(e, async () => {
                               let { default: e } = await Promise.all([n.e('94342'), n.e('79695'), n.e('13351'), n.e('98783'), n.e('97589'), n.e('7717'), n.e('20204')]).then(n.bind(n, 757387));
                               return (t) =>
                                   (0, r.jsx)(
                                       e,
-                                      T(I({}, t), {
-                                          viewingChannelId: C,
-                                          user: P,
-                                          channel: w,
-                                          guildId: D
+                                      N(S({}, t), {
+                                          viewingChannelId: P,
+                                          user: D,
+                                          channel: L,
+                                          guildId: x
                                       })
                                   );
                           });
                   },
-        x = y.ZP.useName(P),
-        M = (0, a.e7)([p.Z, h.ZP, g.Z], () => b.ZP.getNickname(D, d, P)),
-        k = m.Z.getGuild(D),
-        j = (0, u.Ib)(k, w) && i === v.fL ? v.jM : null;
-    if (null == P)
-        return (0, r.jsx)(N, {
-            userId: O,
+        k = v.ZP.useName(D),
+        j = (0, a.e7)([h.Z, m.ZP, E.Z], () => y.ZP.getNickname(x, I, D)),
+        U = g.Z.getGuild(x),
+        G = (0, u.Ib)(U, L),
+        [B, V] = i.useState(!1),
+        F = (0, _.ic)({ location: 'UserMention' }),
+        Z = G && d === O.fL ? O.jM : null;
+    if (null == D)
+        return (0, r.jsx)(A, {
+            userId: T,
             className: t,
-            children: S
+            children: C
         });
-    let U = (e) =>
+    let H = (e) =>
         (0, r.jsx)(
             f.Z,
-            T(
-                I(
+            N(
+                S(
                     {
                         className: t,
-                        onContextMenu: L,
-                        color: j
+                        onContextMenu: M,
+                        color: Z,
+                        onClick: () => V((e) => !e)
                     },
                     e
                 ),
-                { children: '@'.concat(null != M ? M : x) }
+                { children: '@'.concat(null != j ? j : k) }
             )
         );
-    return A
+    return R
         ? (0, r.jsx)(c.Gt, {
-              value: R,
-              children: U()
+              value: w,
+              children: H()
           })
         : (0, r.jsx)(c.Gt, {
-              value: R,
-              children: (0, r.jsx)(_.Z, {
-                  user: P,
-                  guildId: null != D ? D : void 0,
-                  channelId: d,
+              value: w,
+              children: (0, r.jsx)(p.Z, {
+                  user: D,
+                  guildId: null != x ? x : void 0,
+                  channelId: I,
                   position: o.tq ? 'top' : 'right',
-                  clickTrap: !0,
-                  children: (e) => U(e)
+                  clickTrap: B,
+                  shouldShow: B,
+                  shouldShowOnHover: F,
+                  onRequestClose: () => V(!1),
+                  children: function (e) {
+                      for (var t = arguments.length, n = Array(t > 1 ? t - 1 : 0), r = 1; r < t; r++) n[r - 1] = arguments[r];
+                      return H(n);
+                  }
               })
           });
 }
