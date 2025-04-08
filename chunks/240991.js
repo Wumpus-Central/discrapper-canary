824db4f0836df7c827a8112faa0bdd3252b99fbc
DiscrapperManager <admin@minerpl.xyz>
2025-04-08 00:35:44 +0000

    canaryRelease: 387125
    Main scripts: 3
    Lazy Loaded Scripts: 991
    Chunks:
     * Main Files: 11398
     * Scripts: 11344
    Strings: 21112
    Routes: 708
    Experiments Count: 425

--- chunks/240991.js
+++ chunks/240991.js
@@ -17,7 +17,13 @@ var r = n(392711),
     d = n(428595);
 n(454585);
 var f = n(364458);
-let _ = (0, f.Z)([d.Z.PROFILE_BIO_RULES, (0, u.Z)({ enableBuildOverrides: !1 })]),
+let _ = (0, f.Z)([
+        d.Z.PROFILE_BIO_RULES,
+        (0, u.Z)({
+            enableBuildOverrides: !1,
+            mustConfirmExternalLink: !0
+        })
+    ]),
     p = new (a())({ max: 2000 }),
     h = c.w4(_),
     m = void 0;
