1b9ffdbcdf28463ebfea2df0de153e7bd60a6f21
DiscrapperManager <admin@minerpl.xyz>
2025-04-09 19:35:55 +0000

    canaryRelease: 388003
    Main scripts: 3
    Lazy Loaded Scripts: 989
    Chunks:
     * Main Files: 11409
     * Scripts: 11316
    Strings: 21133
    Routes: 708
    Experiments Count: 421

--- chunks/931056.js
+++ chunks/931056.js
@@ -6,55 +6,75 @@ var r = n(192379),
     a = n(158776);
 let s = [],
     c = [],
-    u = [c, s];
+    u = [];
 function d(e, t) {
-    let [n, d] = (0, i.Wu)(
-        [a.Z],
-        () => {
-            if (!t.isPrivate()) return u;
-            let n = [],
-                r = [];
-            return (e.forEach((e) => {
-                var t, i;
-                let l = null == (t = e.application) ? void 0 : t.id,
-                    o = null == (i = e.activity) ? void 0 : i.party_id;
-                if (null != l && null != o) {
-                    let t = e.author.id;
-                    if (
-                        null !=
-                        a.Z.findActivity(
-                            t,
-                            (e) => {
-                                var t;
-                                return e.application_id === l && (null == (t = e.party) ? void 0 : t.id) === o;
-                            },
-                            null,
-                            !0
-                        )
-                    )
-                        return void n.push(e.id);
-                    let i = e.timestamp.getTime(),
-                        s = {
-                            userId: t,
-                            applicationId: l,
-                            partyId: o,
-                            messageId: e.id,
-                            channelId: e.channel_id,
-                            inviteTime: i
-                        };
-                    r.push(s);
-                }
-            }),
-            0 === n.length && 0 === r.length)
-                ? u
-                : 0 === n.length
-                  ? [c, r]
-                  : 0 === r.length
-                    ? [n, s]
-                    : [n, r];
-        },
-        [e, t]
-    );
+    let [n, d] = (function (e, t) {
+        let [n, l] = r.useMemo(
+                () =>
+                    (function (e, t) {
+                        if (!t.isPrivate()) return [u, c];
+                        let n = e.filter((e) => {
+                                var t, n;
+                                return (null == (t = e.application) ? void 0 : t.id) != null && (null == (n = e.activity) ? void 0 : n.party_id) != null;
+                            }),
+                            r = n.map((e) => e.id);
+                        return [n, r];
+                    })(e, t),
+                [e, t]
+            ),
+            o = (0, i.Wu)(
+                [a.Z],
+                () => {
+                    let e = [];
+                    return (
+                        n.forEach((t) => {
+                            null !=
+                                a.Z.findActivity(
+                                    t.author.id,
+                                    (e) => {
+                                        var n, r, i;
+                                        return e.application_id === (null == (n = t.application) ? void 0 : n.id) && (null == (r = e.party) ? void 0 : r.id) === (null == (i = t.activity) ? void 0 : i.party_id);
+                                    },
+                                    null,
+                                    !0
+                                ) && e.push(t.id);
+                        }),
+                        e
+                    );
+                },
+                [n]
+            );
+        return [
+            l,
+            r.useMemo(
+                () =>
+                    (function (e, t) {
+                        if (0 === e.length) return s;
+                        let n = [];
+                        return (
+                            e.forEach((e) => {
+                                var r, i;
+                                let l = null == (r = e.application) ? void 0 : r.id,
+                                    o = null == (i = e.activity) ? void 0 : i.party_id;
+                                if (e.id in t || null == l || null == o) return;
+                                let a = e.timestamp.getTime(),
+                                    s = {
+                                        userId: e.author.id,
+                                        applicationId: l,
+                                        partyId: o,
+                                        messageId: e.id,
+                                        channelId: e.channel_id,
+                                        inviteTime: a
+                                    };
+                                n.push(s);
+                            }),
+                            n
+                        );
+                    })(n, o),
+                [n, o]
+            )
+        ];
+    })(e, t);
     return (
         r.useEffect(() => {
             for (let e of d) o.Z.isSubscribed(e) || (0, l.L)(e);
