df992678cfe4668141c1f558787029a430906583
DiscrapperManager <admin@minerpl.xyz>
2025-04-09 20:29:40 +0000

    canaryRelease: 388044
    Main scripts: 3
    Lazy Loaded Scripts: 989
    Chunks:
     * Main Files: 11413
     * Scripts: 11308
    Strings: 21133
    Routes: 708
    Experiments Count: 420

--- chunks/658138.js
+++ chunks/658138.js
@@ -1,4 +1,4 @@
-n.d(t, { D: () => N });
+n.d(t, { D: () => h });
 var r = n(200651),
     i = n(192379),
     l = n(990547),
@@ -7,45 +7,44 @@ var r = n(200651),
     c = n(213609),
     s = n(377171),
     u = n(626135),
-    d = n(436952),
-    f = n(839606),
-    O = n(54480),
-    b = n(981631),
-    p = n(388032),
-    h = n(837229);
-function N(e) {
-    let { openPickerForChannel: t } = (0, f.B)(),
+    d = n(839606),
+    f = n(54480),
+    O = n(981631),
+    b = n(388032),
+    p = n(837229);
+function h(e) {
+    let { openPickerForChannel: t } = (0, d.B)(),
         n = (0, i.useCallback)(() => {
-            o.default.selectPrivateChannel(e.id), t(e.id), u.default.track(b.rMx.CHAT_WALLPAPER_DM_CONTEXT_MENU_ENTRY_POINT_CLICKED, { channel_id: e.id });
+            o.default.selectPrivateChannel(e.id), t(e.id), u.default.track(O.rMx.CHAT_WALLPAPER_DM_CONTEXT_MENU_ENTRY_POINT_CLICKED, { channel_id: e.id });
         }, [e.id, t]),
-        { canAccessPicker: N, entryPoint: y } = (0, O.m)({
+        { canAccessPicker: h } = (0, f.m)({
             location: 'useChannelWallpaperItem',
             selectedChannelId: e.id
         }),
-        _ = y === d.FN.CONTEXT_MENU && e.isPrivate() && N;
+        y = e.isPrivate() && h;
     return ((0, c.Z)(
         {
             type: l.ImpressionTypes.MENU,
             name: l.ImpressionNames.CHAT_WALLPAPER_DM_CONTEXT_MENU_ENTRY_POINT
         },
-        { disableTrack: !_ }
+        { disableTrack: !y }
     ),
-    _)
+    y)
         ? (0, r.jsx)(a.sNh, {
               id: 'set-wallpaper',
               label: (0, r.jsxs)('div', {
-                  className: h.label,
+                  className: p.label,
                   children: [
-                      p.NW.string(p.t.GixvUl),
+                      b.NW.string(b.t.GixvUl),
                       (0, r.jsx)(a.IGR, {
-                          text: p.NW.string(p.t.y2b7CA),
+                          text: b.NW.string(b.t.y2b7CA),
                           color: s.Z.BG_BRAND
                       })
                   ]
               }),
               icon: () =>
                   (0, r.jsx)('div', {
-                      className: h.iconWrapper,
+                      className: p.iconWrapper,
                       children: (0, r.jsx)(a.SrA, { size: 'xs' })
                   }),
               action: n
