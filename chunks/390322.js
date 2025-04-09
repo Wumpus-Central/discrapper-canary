27204ae5fb807d120f53beb58a79db33b486c759
DiscrapperManager <admin@minerpl.xyz>
2025-04-09 21:25:47 +0000

    canaryRelease: 388108
    Main scripts: 3
    Lazy Loaded Scripts: 989
    Chunks:
     * Main Files: 11412
     * Scripts: 11289
    Strings: 21134
    Routes: 708
    Experiments Count: 418

--- chunks/390322.js
+++
@@ -1,8 +0,0 @@
-n.d(t, { Z: () => o });
-var r = n(192379),
-    s = n(618158);
-function o(e) {
-    let { children: t } = e,
-        { preventIdle: n, allowIdle: o } = (0, s.Y)('popup');
-    return r.useEffect(() => (n(), () => o()), [o, n]), t;
-}
