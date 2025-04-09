4a28700747752cf4c86303bc4f8902d1b2a2c3f1
DiscrapperManager <admin@minerpl.xyz>
2025-04-09 22:26:31 +0000

    canaryRelease: 388165
    Main scripts: 3
    Lazy Loaded Scripts: 989
    Chunks:
     * Main Files: 11411
     * Scripts: 11309
    Strings: 21137
    Routes: 708
    Experiments Count: 420

--- chunks/131565.js
+++ chunks/131565.js
@@ -1,41 +1,50 @@
-n.d(t, { Z: () => f }), n(388685);
+n.d(t, { Z: () => g }), n(388685);
 var r = n(200651),
     i = n(192379),
     l = n(442837),
     o = n(357352),
     a = n(565942),
-    s = n(411149),
-    c = n(115215),
-    u = n(151480),
-    d = n(680783),
-    p = n(24848);
-function h(e) {
+    s = n(626135),
+    c = n(411149),
+    u = n(115215),
+    d = n(151480),
+    p = n(680783),
+    h = n(981631),
+    f = n(24848);
+function m(e) {
     let { chatWallpaperState: t } = e,
-        n = (0, l.e7)([s.Z], () => {
-            let e = s.Z.getWallpaperById(t.wallpaperId);
+        n = (0, l.e7)([c.Z], () => {
+            let e = c.Z.getWallpaperById(t.wallpaperId);
             return null == e ? null : (0, o.I)(e.default.asset);
         }),
-        [c, d] = i.useState(n);
+        [u, p] = i.useState(n);
     return (i.useEffect(() => {
-        null == n && t.isViewable && s.Z.shouldFetchWallpapers && (0, u.k9)();
+        null == n && t.isViewable && c.Z.shouldFetchWallpapers && (0, d.k9)();
     }, [n, t.isViewable]),
     i.useEffect(() => {
-        if (c !== n) {
-            if (null == n) return void d(null);
-            (0, a.p)(n, (e) => {
-                e === n && d(e);
-            });
-        }
-    }, [c, n]),
-    t.isViewable && null != c)
+        if (u === n) return;
+        if (null == n) return void p(null);
+        let e = performance.now();
+        (0, a.p)(n, (t) => {
+            if (t === n) {
+                p(t);
+                let n = performance.now();
+                s.default.track(h.rMx.CHAT_WALLPAPER_DOWNLOADED, {
+                    time_used_in_ms: n - e,
+                    asset_url: t
+                });
+            }
+        });
+    }, [u, n]),
+    t.isViewable && null != u)
         ? (0, r.jsx)('div', {
-              className: p.wallpaperContainer,
-              style: { backgroundImage: 'url('.concat(c, ')') }
+              className: f.wallpaperContainer,
+              style: { backgroundImage: 'url('.concat(u, ')') }
           })
         : null;
 }
-function f(e) {
+function g(e) {
     let { channel: t } = e,
-        n = (0, d.Z)(t.id);
-    return null == n || !n.isViewable || (!t.isDM() && !t.isMultiUserDM() && !t.isGroupDM()) || (c.t(t) && !n.isPreview) ? null : (0, r.jsx)(h, { chatWallpaperState: n });
+        n = (0, p.Z)(t.id);
+    return null == n || !n.isViewable || (!t.isDM() && !t.isMultiUserDM() && !t.isGroupDM()) || (u.t(t) && !n.isPreview) ? null : (0, r.jsx)(m, { chatWallpaperState: n });
 }
--- chunks/981631.js
+++ chunks/981631.js
@@ -2402,6 +2402,7 @@ var N =
     (r.CHAT_WALLPAPER_USER_SIDE_CLEAR_TOGGLED = 'chat_wallpaper_user_side_clear_toggled'),
     (r.CHAT_WALLPAPER_NITRO_LINK_CLICKED = 'chat_wallpaper_nitro_link_clicked'),
     (r.CHAT_WALLPAPER_WALLPAPER_OPTION_SELECTED = 'chat_wallpaper_wallpaper_option_selected'),
+    (r.CHAT_WALLPAPER_DOWNLOADED = 'chat_wallpaper_downloaded'),
     (r.RING_TO_GUILD_VC_BANNER_SHOWN = 'impression_ring_to_guild_vc_banner_shown'),
     (r.RING_TO_GUILD_VC_MENU_ITEM_SHOWN = 'impression_ring_to_guild_vc_menu_item_shown'),
     (r.RING_CALL_DECLINED = 'ring_call_declined'),
