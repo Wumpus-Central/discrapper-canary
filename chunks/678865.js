e9594a04998a1ae915b598a0a49a45af025582cd
DiscrapperManager <admin@minerpl.xyz>
2025-04-09 17:57:44 +0000

    canaryRelease: 387918
    Main scripts: 3
    Lazy Loaded Scripts: 988
    Chunks:
     * Main Files: 11409
     * Scripts: 11311
    Strings: 21133
    Routes: 708
    Experiments Count: 420

--- chunks/678865.js
+++ chunks/678865.js
@@ -1,15 +1,12 @@
-n.d(t, { Z: () => i });
-var r = n(481060),
-    a = n(695346);
-function i() {
-    switch (a.co.getSetting()) {
-        case r.Skl.IDLE:
-            return r.Skl.IDLE;
-        case r.Skl.DND:
-            return r.Skl.DND;
-        case r.Skl.INVISIBLE:
-            return r.Skl.INVISIBLE;
-        default:
-            return r.Skl.ONLINE;
-    }
+n.d(t, { Z: () => o }), n(314940);
+var r = n(278074),
+    a = n(481060),
+    i = n(695346);
+function o() {
+    let e = i.co.getSetting();
+    return (0, r.EQ)(e)
+        .with(a.Skl.IDLE, () => a.Skl.IDLE)
+        .with(a.Skl.DND, () => a.Skl.DND)
+        .with(a.Skl.INVISIBLE, () => a.Skl.INVISIBLE)
+        .otherwise(() => a.Skl.ONLINE);
 }
