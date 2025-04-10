2ce7adb9cf8ac39f7dda0de3a61fb59382f3b5d0
DiscrapperManager <admin@minerpl.xyz>
2025-04-10 19:14:27 +0000

    canaryRelease: 388484
    Main scripts: 3
    Lazy Loaded Scripts: 989
    Chunks:
     * Main Files: 11413
     * Scripts: 11307
    Strings: 21146
    Routes: 708
    Experiments Count: 420

--- chunks/431286.js
+++ chunks/431286.js
@@ -1,67 +1,71 @@
-n.d(t, { i: () => v }), n(388685);
+n.d(t, { i: () => O }), n(388685);
 var r = n(200651),
     i = n(192379),
-    l = n(442837),
-    o = n(704215),
-    a = n(952265),
-    s = n(481060),
-    c = n(570140),
-    u = n(605236),
-    d = n(243778),
-    p = n(341907),
-    h = n(819640),
-    f = n(822857),
-    g = n(479766),
-    m = n(331663),
-    b = n(921944),
-    _ = n(46140);
-let y = 'orb-announcement-modal-key';
-function v() {
-    let { hasLayers: e } = (0, l.cj)([h.Z], () => ({ hasLayers: h.Z.hasLayers() })),
-        { onboardingModalOpenedPrior: t } = (0, l.cj)([g.Z], () => ({ onboardingModalOpenedPrior: g.Z.onboardingModalOpenedPrior })),
-        { enabled: v } = (0, f.W)({ location: 'virtual_currency_announcement_modal' }),
-        [O, j] = (0, d.US)(v ? [o.z.VIRTUAL_CURRENCY_ONBOARDING_ANNOUNCEMENT_MODAL] : [], ...m.b.useSelectedDismissibleContent),
-        C = (0, s.s9z)(s.JQI);
+    l = n(636977),
+    o = n(442837),
+    a = n(704215),
+    s = n(952265),
+    c = n(481060),
+    u = n(570140),
+    d = n(605236),
+    p = n(243778),
+    h = n(341907),
+    f = n(819640),
+    g = n(822857),
+    m = n(479766),
+    b = n(331663),
+    _ = n(921944),
+    y = n(46140);
+let v = 'orb-announcement-modal-key';
+function O() {
+    let { hasLayers: e } = (0, o.cj)([f.Z], () => ({ hasLayers: f.Z.hasLayers() })),
+        { onboardingModalOpenedPrior: t } = (0, o.cj)([m.Z], () => ({ onboardingModalOpenedPrior: m.Z.onboardingModalOpenedPrior })),
+        { enabled: O } = (0, g.W)({ location: 'virtual_currency_announcement_modal' }),
+        [j, C] = (0, p.US)(O ? [a.z.VIRTUAL_CURRENCY_ONBOARDING_ANNOUNCEMENT_MODAL] : [], ...b.b.useSelectedDismissibleContent),
+        x = (0, c.s9z)(c.JQI);
     i.useEffect(() => {
-        v &&
+        O &&
             !t &&
-            O === o.z.VIRTUAL_CURRENCY_ONBOARDING_ANNOUNCEMENT_MODAL &&
+            j === a.z.VIRTUAL_CURRENCY_ONBOARDING_ANNOUNCEMENT_MODAL &&
             (e ||
-                C ||
-                (c.Z.dispatch({ type: 'VIRTUAL_CURRENCY_ONBOARDING_MODAL_OPEN' }),
-                (0, a.ZD)(
+                x ||
+                (u.Z.dispatch({ type: 'VIRTUAL_CURRENCY_ONBOARDING_MODAL_OPEN' }),
+                (0, s.ZD)(
                     async () => {
                         let { AnnouncementModal: e } = await n.e('86653').then(n.bind(n, 993318));
                         return function (t) {
                             let { onClose: n, transitionState: i } = t,
-                                l = async () => {
-                                    await n(), j(b.L.USER_DISMISS);
+                                o = async () => {
+                                    await n(), C(_.L.USER_DISMISS);
                                 };
                             return (0, r.jsx)(e, {
                                 transitionState: i,
-                                onClose: l,
+                                onClose: o,
                                 ctaOnClick: () => {
-                                    j(b.L.TAKE_ACTION),
-                                        (0, u.EW)(o.z.VIRTUAL_CURRENCY_DISCOVERY_ONBOARDING_COACHMARK, {
-                                            dismissAction: b.L.INDIRECT_ACTION,
-                                            groupName: b.R.VIRTUAL_CURRENCY_ONBOARDING
+                                    C(_.L.TAKE_ACTION),
+                                        (0, d.EW)(a.z.VIRTUAL_CURRENCY_DISCOVERY_ONBOARDING_COACHMARK, {
+                                            dismissAction: _.L.INDIRECT_ACTION,
+                                            groupName: _.R.VIRTUAL_CURRENCY_ONBOARDING
                                         }),
-                                        (0, u.EW)(o.z.VIRTUAL_CURRENCY_SHOP_ONBOARDING_COACHMARK, {
-                                            dismissAction: b.L.INDIRECT_ACTION,
-                                            groupName: b.R.VIRTUAL_CURRENCY_ONBOARDING
+                                        (0, d.EW)(a.z.VIRTUAL_CURRENCY_SHOP_ONBOARDING_COACHMARK, {
+                                            dismissAction: _.L.INDIRECT_ACTION,
+                                            groupName: _.R.VIRTUAL_CURRENCY_ONBOARDING
                                         }),
-                                        (0, p.navigateToQuestHome)({ questId: _.V6 }),
+                                        (0, h.navigateToQuestHome)({
+                                            questId: y.V6,
+                                            fromContent: l.j.ORBS_ANNOUNCEMENT_MODAL
+                                        }),
                                         n();
                                 }
                             });
                         };
                     },
                     {
-                        modalKey: y,
+                        modalKey: v,
                         onCloseRequest: () => {
-                            j(b.L.USER_DISMISS), (0, s.Mr3)(y);
+                            C(_.L.USER_DISMISS), (0, c.Mr3)(v);
                         }
                     }
                 )));
-    }, [O, v, t, j, e, C]);
+    }, [j, O, t, C, e, x]);
 }
--- chunks/963102.js
+++ chunks/963102.js
@@ -1,96 +1,97 @@
-n.d(t, { I: () => j }), n(539854);
+n.d(t, { I: () => y }), n(539854);
 var r = n(200651),
     l = n(192379),
     a = n(120356),
     o = n.n(a),
-    i = n(442837),
-    s = n(481060),
-    c = n(37234),
-    u = n(425493),
-    d = n(410030),
-    b = n(984370),
-    p = n(341907),
-    f = n(822857),
-    g = n(775451),
+    i = n(636977),
+    s = n(442837),
+    c = n(481060),
+    u = n(37234),
+    d = n(425493),
+    b = n(410030),
+    p = n(984370),
+    f = n(341907),
+    g = n(822857),
+    m = n(775451),
     h = n(594174),
-    m = n(471731),
-    _ = n(335131),
+    _ = n(471731),
+    C = n(335131),
     v = n(215023),
-    C = n(388032),
-    x = n(774457);
-function O(e) {
+    x = n(388032),
+    O = n(774457);
+function j(e) {
     let { tab: t, displayText: n, selected: l, handleTransition: a } = e;
-    return (0, r.jsx)(b.Z.Title, {
+    return (0, r.jsx)(p.Z.Title, {
         onClick: () => a(t),
-        wrapperClassName: x.tabWrapper,
-        className: o()(x.tab, { [x.selected]: l }),
+        wrapperClassName: O.tabWrapper,
+        className: o()(O.tab, { [O.selected]: l }),
         children: n
     });
 }
-function j(e) {
-    let { isFullScreen: t, isLayer: n, onClose: a, selectedTab: j, handleTransition: y } = e,
-        k = (0, d.ZP)(),
-        S = (0, i.e7)([h.default], () => h.default.getCurrentUser()),
-        P = (null == S ? void 0 : S.isStaff()) || (null == S ? void 0 : S.isStaffPersonal()) || !1,
-        { enabled: E } = (0, f.W)({ location: 'collectibles_shop_header_bar' }),
-        w = [
+function y(e) {
+    let { isFullScreen: t, isLayer: n, onClose: a, selectedTab: y, handleTransition: k } = e,
+        S = (0, b.ZP)(),
+        P = (0, s.e7)([h.default], () => h.default.getCurrentUser()),
+        E = (null == P ? void 0 : P.isStaff()) || (null == P ? void 0 : P.isStaffPersonal()) || !1,
+        { enabled: w } = (0, g.W)({ location: 'collectibles_shop_header_bar' }),
+        B = [
             {
                 tab: v.AW.HOME,
-                displayText: C.NW.string(C.t.ijDDw8)
+                displayText: x.NW.string(x.t.ijDDw8)
             },
             {
                 tab: v.AW.CATALOG,
-                displayText: C.NW.string(C.t.xFcotb)
+                displayText: x.NW.string(x.t.xFcotb)
             }
         ];
-    E &&
-        w.push({
+    w &&
+        B.push({
             tab: v.AW.ORBS,
-            displayText: C.NW.string(C.t['KUYR+P'])
+            displayText: x.NW.string(x.t['KUYR+P'])
         });
-    let I = n ? _.DR : a,
-        B = l.useCallback(() => {
-            t && (I(), (0, c.Ou)()), (0, p.navigateToQuestHome)();
+    let I = n ? C.DR : a,
+        N = l.useCallback(() => {
+            t && (I(), (0, u.Ou)()), (0, f.navigateToQuestHome)({ fromContent: i.j.ORBS_BALANCE_MENU });
         }, [I, t]);
-    return (0, r.jsx)(s.f6W, {
-        theme: k,
+    return (0, r.jsx)(c.f6W, {
+        theme: S,
         children: (e) =>
-            (0, r.jsxs)(b.Z, {
-                className: o()(e, x.headerBar, { [x.fullscreenHeaderBar]: t }),
-                innerClassname: E ? x.headerBarInner : void 0,
-                toolbar: t || !P ? null : (0, r.jsx)(l.Fragment, {}),
+            (0, r.jsxs)(p.Z, {
+                className: o()(e, O.headerBar, { [O.fullscreenHeaderBar]: t }),
+                innerClassname: w ? O.headerBarInner : void 0,
+                toolbar: t || !E ? null : (0, r.jsx)(l.Fragment, {}),
                 children: [
-                    (0, r.jsx)(m.Z, { className: x.discordLogo }),
-                    (0, r.jsx)(b.Z.Title, { children: C.NW.string(C.t.pWG4zc) }),
+                    (0, r.jsx)(_.Z, { className: O.discordLogo }),
+                    (0, r.jsx)(p.Z.Title, { children: x.NW.string(x.t.pWG4zc) }),
                     (0, r.jsx)('div', {
-                        className: x.tabs,
-                        children: w.map((e) => {
+                        className: O.tabs,
+                        children: B.map((e) => {
                             let { tab: t, displayText: n } = e;
                             return (0, r.jsx)(
-                                O,
+                                j,
                                 {
                                     tab: t,
                                     displayText: n,
-                                    selected: j === t,
-                                    handleTransition: y
+                                    selected: y === t,
+                                    handleTransition: k
                                 },
                                 t
                             );
                         })
                     }),
-                    (E || t) &&
+                    (w || t) &&
                         (0, r.jsxs)('div', {
-                            className: x.alignedRightContent,
+                            className: O.alignedRightContent,
                             children: [
-                                E &&
-                                    (0, r.jsx)(g.V9, {
-                                        cardAlignment: g.V9.CardAlignment.END,
-                                        ctaText: C.NW.string(C.t.VC4Mq6),
-                                        ctaOnClick: B,
-                                        className: x.balanceWidgetMenu
+                                w &&
+                                    (0, r.jsx)(m.V9, {
+                                        cardAlignment: m.V9.CardAlignment.END,
+                                        ctaText: x.NW.string(x.t.VC4Mq6),
+                                        ctaOnClick: N,
+                                        className: O.balanceWidgetMenu
                                     }),
                                 t &&
-                                    (0, r.jsx)(u.Z, {
+                                    (0, r.jsx)(d.Z, {
                                         closeAction: I,
                                         keybind: 'ESC'
                                     })
