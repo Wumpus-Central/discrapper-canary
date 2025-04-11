ecb13181f849c2f85e577bc88bef31c8b71646cd
DiscrapperManager <admin@minerpl.xyz>
2025-04-11 18:51:35 +0000

    canaryRelease: 388979
    Main scripts: 3
    Lazy Loaded Scripts: 983
    Chunks:
     * Main Files: 11869
     * Scripts: 10835
    Strings: 21151
    Routes: 708
    Experiments Count: 421

--- chunks/86203.js
+++ chunks/86203.js
@@ -1,31 +1,28 @@
-n.d(t, { Z: () => u });
+n.d(t, { Z: () => c });
 var r = n(200651);
 n(192379);
 var i = n(481060),
     l = n(493683),
     o = n(726521),
-    a = n(718538),
-    s = n(868807),
-    c = n(388032);
-function u(e) {
-    let { channel: t, buttonSize: n = i.PhG.TINY, color: u = i.zxk.Colors.RED, className: d, onReportClick: p, onReportSubmit: h, onMouseEnter: f, onMouseLeave: m } = e,
-        { error: g, loaded: b } = (0, s.a)(t),
-        _ = t.isDM() ? t.getRecipientId() : null,
-        y = (0, a.V)(t.id, _);
-    if (null == y && (b || g)) return null;
-    let C = () => {
-        null == h || h(), l.Z.closePrivateChannel(t.id);
+    a = n(868807),
+    s = n(388032);
+function c(e) {
+    let { channel: t, buttonSize: n = i.PhG.TINY, color: c = i.zxk.Colors.RED, className: u, onReportClick: d, onReportSubmit: p, onMouseEnter: h, onMouseLeave: f } = e,
+        { error: m, loaded: g, message: b } = (0, a.a)(t);
+    if (null == b && (g || m)) return null;
+    let _ = () => {
+        null == p || p(), l.Z.closePrivateChannel(t.id);
     };
     return (0, r.jsx)(i.zxk, {
-        className: d,
+        className: u,
         size: n,
-        color: u,
-        disabled: null == y,
+        color: c,
+        disabled: null == b,
         onClick: (e) => {
-            e.stopPropagation(), null == p || p(), null != y && (0, o.WL)(y, C);
+            e.stopPropagation(), null == d || d(), null != b && (0, o.WL)(b, _);
         },
-        onMouseEnter: f,
-        onMouseLeave: m,
-        children: c.NW.string(c.t['+78Pfn'])
+        onMouseEnter: h,
+        onMouseLeave: f,
+        children: s.NW.string(s.t['+78Pfn'])
     });
 }
