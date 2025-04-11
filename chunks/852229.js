b648eb48e2b5301270bafd61f05c44496cd2a4ce
DiscrapperManager <admin@minerpl.xyz>
2025-04-11 22:25:14 +0000

    canaryRelease: 389114
    Main scripts: 3
    Lazy Loaded Scripts: 983
    Chunks:
     * Main Files: 11890
     * Scripts: 10833
    Strings: 21153
    Routes: 708
    Experiments Count: 419

--- chunks/852229.js
+++ chunks/852229.js
@@ -1,11 +1,24 @@
-function r(e) {
-    let t = document.body;
-    if (null == t) throw Error('[Utils] ClipboardUtils.copy(): assert failed: document.body != null');
-    let n = document.createRange(),
-        r = window.getSelection(),
-        i = document.createElement('textarea');
-    (i.value = e), (i.contentEditable = 'true'), (i.style.visibility = 'none'), t.appendChild(i), n.selectNodeContents(i), null == r || r.removeAllRanges(), null == r || r.addRange(n), i.focus(), i.setSelectionRange(0, e.length);
-    let o = document.execCommand('copy');
-    return t.removeChild(i), o;
+n.d(t, {
+    J: () => o,
+    a: () => i
+}),
+    n(415506);
+var r = n(231338);
+function i() {
+    return 'object' == typeof window.navigator && 'clipboard' in window.navigator;
 }
-n.d(t, { J: () => r }), n(415506);
+function o(e) {
+    let t = !1;
+    if (i())
+        try {
+            let n = new ClipboardItem({ 'text/plain': e });
+            window.navigator.clipboard.write([n]).catch(r.dG), (t = !0);
+        } catch (e) {}
+    if (t) return t;
+    let n = document.body;
+    if (null == n) throw Error('[Utils] ClipboardUtils.copy(): assert failed: document.body != null');
+    let o = document.createRange(),
+        a = window.getSelection(),
+        s = document.createElement('textarea');
+    return (s.value = e), (s.contentEditable = 'true'), (s.style.visibility = 'none'), n.appendChild(s), o.selectNodeContents(s), null == a || a.removeAllRanges(), null == a || a.addRange(o), s.focus(), s.setSelectionRange(0, e.length), (t = document.execCommand('copy')), n.removeChild(s), t;
+}
