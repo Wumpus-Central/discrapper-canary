d4c547baa0ba9c934006467e900c581114b49825
DiscrapperManager <admin@minerpl.xyz>
2025-04-11 14:56:57 +0000

    canaryRelease: 388832
    Main scripts: 3
    Lazy Loaded Scripts: 981
    Chunks:
     * Main Files: 11871
     * Scripts: 10831
    Strings: 21146
    Routes: 708
    Experiments Count: 420

--- chunks/113434.js
+++ chunks/113434.js
@@ -501,9 +501,12 @@ function ey(e, t) {
                     .with({ taskType: s.X.PLAY_ON_DESKTOP_V2 }, () => M.cd.DESKTOP)
                     .with({ taskType: s.X.PLAY_ACTIVITY }, () => M.cd.DESKTOP)
                     .with({ taskType: s.X.WATCH_VIDEO }, () => M.cd.DESKTOP)
+                    .with({ taskType: s.X.WATCH_VIDEO_ON_MOBILE }, () => M.cd.DESKTOP)
                     .with({ taskType: s.X.STREAM_ON_DESKTOP }, () => M.cd.DESKTOP)
                     .with({ taskType: s.X.PLAY_ON_XBOX }, () => M.cd.CONSOLE)
                     .with({ taskType: s.X.PLAY_ON_PLAYSTATION }, () => M.cd.CONSOLE)
+                    .with({ taskType: s.X.ACHIEVEMENT_IN_GAME }, () => M.cd.DESKTOP)
+                    .with({ taskType: s.X.ACHIEVEMENT_IN_ACTIVITY }, () => M.cd.DESKTOP)
                     .exhaustive(),
             [t]
         ),
