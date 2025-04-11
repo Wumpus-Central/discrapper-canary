7f03edc672504e9ccb52c2bb48ea1b95e9c942bc
DiscrapperManager <admin@minerpl.xyz>
2025-04-11 18:30:58 +0000

    canaryRelease: 388945
    Main scripts: 3
    Lazy Loaded Scripts: 983
    Chunks:
     * Main Files: 11870
     * Scripts: 10834
    Strings: 21151
    Routes: 708
    Experiments Count: 421

--- chunks/720449.js
+++ chunks/720449.js
@@ -1,38 +1,41 @@
-n.d(t, { Z: () => c }), n(781311);
+n.d(t, { Z: () => d }), n(781311);
 var r = n(913527),
     a = n.n(r),
-    i = n(695346),
+    i = n(381499),
+    l = n(695346),
     o = n(626135),
-    l = n(337953),
-    s = n(875425),
-    u = n(981631);
-function c(e) {
-    let { text: t, emojiInfo: n, clearAfter: r, analyticsContext: c, createdAtMs: d, prompt: m } = e,
-        f = t.trim();
-    if (!(f.length > 0) && null == n) return i.Ok.updateSetting(void 0);
+    s = n(337953),
+    u = n(875425),
+    c = n(981631);
+function d(e) {
+    let { text: t, emojiInfo: n, clearAfter: r, analyticsContext: d, createdAtMs: m, prompt: f, customStatusLabel: p } = e,
+        h = t.trim();
+    if (!(h.length > 0) && null == n) return l.Ok.updateSetting(void 0);
     {
-        let e = i.Ok.updateSetting({
-            text: f.length > 0 ? f : '',
+        let e = l.Ok.updateSetting({
+            text: h.length > 0 ? h : '',
             expiresAtMs:
-                null != r && r !== s.FO.DONT_CLEAR
+                null != r && r !== u.FO.DONT_CLEAR
                     ? String(
                           a()()
-                              .add((0, l.Z)(r), 'ms')
+                              .add((0, s.Z)(r), 'ms')
                               .toDate()
                               .getTime()
                       )
                     : '0',
             emojiId: null != n && null != n.id ? n.id : '0',
             emojiName: null != n ? n.name : '',
-            createdAtMs: String(null != d ? d : a()().toDate().getTime())
+            createdAtMs: String(null != m ? m : a()().toDate().getTime()),
+            label: null != p ? i.Gm.create({ value: p }) : void 0
         });
         return (
-            o.default.track(u.rMx.CUSTOM_STATUS_UPDATED, {
-                location: null != c ? c.location : null,
+            o.default.track(c.rMx.CUSTOM_STATUS_UPDATED, {
+                location: null != d ? d.location : null,
                 emoji_type: null == n ? null : null != n.id ? 'custom' : 'unicode',
-                text_len: f.length,
+                text_len: h.length,
                 clear_after: null != r ? ''.concat(r) : null,
-                prompt_type: null == m ? void 0 : m.value
+                prompt_type: null == f ? void 0 : f.value,
+                label: null != p ? p : null
             }),
             e
         );
