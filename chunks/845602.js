bfe3fdce9d02bc2e5d193ee017aa8ea703822861
DiscrapperManager <admin@minerpl.xyz>
2025-04-11 19:48:28 +0000

    canaryRelease: 389016
    Main scripts: 3
    Lazy Loaded Scripts: 983
    Chunks:
     * Main Files: 11871
     * Scripts: 10833
    Strings: 21151
    Routes: 708
    Experiments Count: 420

--- chunks/845602.js
+++ chunks/845602.js
@@ -1,58 +1,128 @@
-n.d(t, {
+r.d(t, {
     E: () => x,
     Z: () => C
 }),
-    n(388685);
-var r = n(200651),
-    o = n(192379),
-    a = n(120356),
-    i = n.n(a),
-    s = n(866442),
-    l = n(481060),
-    c = n(957537),
-    u = n(839008),
-    d = n(755641),
-    p = n(742409),
-    f = n(369509),
-    _ = n(116175),
-    m = n(680278),
-    g = n(388032),
-    b = n(606217);
+    r(388685);
+var n = r(200651),
+    o = r(192379),
+    a = r(120356),
+    i = r.n(a),
+    l = r(866442),
+    s = r(481060),
+    c = r(957537),
+    u = r(839008),
+    d = r(797967),
+    p = r(755641),
+    f = r(742409),
+    g = r(369509),
+    b = r(141006),
+    m = r(981631),
+    _ = r(116175),
+    j = r(680278),
+    y = r(388032),
+    O = r(606217),
+    h = r(183375);
 function C() {
-    let e = (0, l.Icv)((0, s.Rf)(11156283), (0, s.Rf)(16730184), void 0, 'username');
-    return (0, r.jsx)('div', {
-        className: b.messageContainer,
-        children: (0, r.jsx)(p.l, {
-            avatar: c.Z,
-            username: 'Cherry',
-            usernameStyle: e.text.gradientStyle,
-            usernameClassName: i()(e.text.gradientClassName, e.gradient.gradientClassName),
-            message: g.NW.string(m.Z.eTHkQU)
-        })
+    let e = o.useMemo(() => Math.floor(Math.random() * b.tl.length), []),
+        [t, r] = o.useState({
+            primary_color: b.tl[e].start,
+            secondary_color: b.tl[e].end
+        }),
+        a = (0, s.Icv)((0, l.Rf)(t.primary_color), (0, l.Rf)(t.secondary_color), void 0, 'username');
+    return (0, n.jsxs)('div', {
+        className: O.container,
+        children: [
+            (0, n.jsx)('div', {
+                className: O.messageContainer,
+                children: (0, n.jsx)(f.l, {
+                    avatar: c.Z,
+                    username: 'Cherry',
+                    usernameStyle: a.text.gradientStyle,
+                    usernameClassName: i()(a.text.gradientClassName, a.gradient.gradientClassName, h.gradientAlwaysAnimate),
+                    message: y.NW.string(j.Z.eTHkQU)
+                })
+            }),
+            (0, n.jsx)(d.default, {
+                defaultColor: m.p6O,
+                colors: b.tl,
+                value: t.primary_color,
+                secondaryValue: t.secondary_color,
+                onChange: (e) => {
+                    var n, o;
+                    return r(
+                        ((n = (function (e) {
+                            for (var t = 1; t < arguments.length; t++) {
+                                var r = null != arguments[t] ? arguments[t] : {},
+                                    n = Object.keys(r);
+                                'function' == typeof Object.getOwnPropertySymbols &&
+                                    (n = n.concat(
+                                        Object.getOwnPropertySymbols(r).filter(function (e) {
+                                            return Object.getOwnPropertyDescriptor(r, e).enumerable;
+                                        })
+                                    )),
+                                    n.forEach(function (t) {
+                                        var n;
+                                        (n = r[t]),
+                                            t in e
+                                                ? Object.defineProperty(e, t, {
+                                                      value: n,
+                                                      enumerable: !0,
+                                                      configurable: !0,
+                                                      writable: !0
+                                                  })
+                                                : (e[t] = n);
+                                    });
+                            }
+                            return e;
+                        })({}, t)),
+                        (o = o = { primary_color: e }),
+                        Object.getOwnPropertyDescriptors
+                            ? Object.defineProperties(n, Object.getOwnPropertyDescriptors(o))
+                            : (function (e, t) {
+                                  var r = Object.keys(e);
+                                  if (Object.getOwnPropertySymbols) {
+                                      var n = Object.getOwnPropertySymbols(e);
+                                      r.push.apply(r, n);
+                                  }
+                                  return r;
+                              })(Object(o)).forEach(function (e) {
+                                  Object.defineProperty(n, e, Object.getOwnPropertyDescriptor(o, e));
+                              }),
+                        n)
+                    );
+                },
+                onChangeGradientColors: (e) =>
+                    r({
+                        primary_color: e[0],
+                        secondary_color: e[1],
+                        tertiary_color: void 0
+                    })
+            })
+        ]
     });
 }
 function x(e) {
     let { guildId: t } = e,
-        [n, a] = o.useState(_.ZD.HEART);
-    return (0, r.jsxs)('div', {
-        className: b.container,
+        [r, a] = o.useState(_.ZD.HEART);
+    return (0, n.jsxs)('div', {
+        className: O.container,
         children: [
-            (0, r.jsx)('div', {
-                className: b.messageContainer,
-                children: (0, r.jsx)(p.l, {
+            (0, n.jsx)('div', {
+                className: O.messageContainer,
+                children: (0, n.jsx)(f.l, {
                     avatar: u.Z,
                     username: 'Roka',
                     usernameStyle: { color: 'var(--brand-400)' },
-                    message: g.NW.string(m.Z.eTHkQU),
-                    decorations: (0, r.jsx)(f.S, {
+                    message: y.NW.string(j.Z.eTHkQU),
+                    decorations: (0, n.jsx)(g.S, {
                         guildId: t,
                         tag: 'GGEZ',
-                        badge: n
+                        badge: r
                     })
                 })
             }),
-            (0, r.jsx)(d.g, {
-                selectedBadge: n,
+            (0, n.jsx)(p.g, {
+                selectedBadge: r,
                 onBadgeClicked: a
             })
         ]
