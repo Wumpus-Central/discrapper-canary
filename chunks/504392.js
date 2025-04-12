31e3859086de62238aaf5e418a9cdad6a5805420
DiscrapperManager <admin@minerpl.xyz>
2025-04-12 07:00:16 +0000

    canaryRelease: 389263
    Main scripts: 3
    Lazy Loaded Scripts: 983
    Chunks:
     * Main Files: 11894
     * Scripts: 10828
    Strings: 21154
    Routes: 708
    Experiments Count: 421

--- chunks/504392.js
+++ chunks/504392.js
@@ -1,4 +1,4 @@
-t.d(n, { Z: () => E }), t(953529);
+t.d(n, { Z: () => w }), t(953529);
 var r = t(200651),
     o = t(192379),
     s = t(120356),
@@ -7,45 +7,51 @@ var r = t(200651),
     l = t(481060),
     c = t(377171),
     d = t(984370),
-    u = t(713081),
+    u = t(430824),
+    m = t(713081),
     p = t(905128),
-    m = t(27237),
-    x = t(51859),
-    _ = t(953478),
+    x = t(27237),
+    _ = t(51859),
+    f = t(953478),
     v = t(460608),
-    f = t(279604),
-    g = t(332538),
+    g = t(279604),
+    h = t(332538),
     j = t(535396),
-    b = t(921944),
-    h = t(680278),
-    N = t(388032),
-    Z = t(782285);
-let C = [j.Us.LEVEL, j.Us.PERK];
-function E(e) {
+    b = t(981631),
+    N = t(921944),
+    C = t(680278),
+    Z = t(388032),
+    E = t(782285);
+let I = [j.Us.LEVEL, j.Us.PERK];
+function w(e) {
     let { guildId: n, powerupSkuId: t } = e,
-        s = (0, g.ZP)(n);
+        s = (0, h.ZP)(n);
     o.useEffect(() => {
-        (0, u.Sn)(n), (0, u.Fm)(n);
+        (0, m.Sn)(n), (0, m.Fm)(n);
     }, [n]),
         o.useEffect(() => {
             var e;
-            (0, u.jd)(n), null == s || null == (e = s.popout) || e.markAsDismissed(b.L.AUTO_DISMISS);
+            (0, m.jd)(n), null == s || null == (e = s.popout) || e.markAsDismissed(N.L.AUTO_DISMISS);
         }, [n, s]);
-    let E = (0, a.e7)([p.Z], () => p.Z.getStateForGuild(n)),
-        I = o.useRef(!1);
+    let w = (0, a.e7)([p.Z], () => p.Z.getStateForGuild(n)),
+        T = (0, a.e7)([u.Z], () => {
+            var e;
+            return (null == (e = u.Z.getGuild(n)) ? void 0 : e.hasFeature(b.oNc.INTERNAL_EMPLOYEE_ONLY)) === !0;
+        }),
+        S = o.useRef(!1);
     return (o.useEffect(() => {
         var e, r;
         if (null == t) return;
-        let o = null == E || null == (r = E.powerups) || null == (e = r.get) ? void 0 : e.call(r, t);
-        null == o || I.current || ((0, f.KE)(n, o), (I.current = !0));
-    }, [n, t, null == E ? void 0 : E.powerups]),
-    (null == E ? void 0 : E.catalog) == null)
+        let o = null == w || null == (r = w.powerups) || null == (e = r.get) ? void 0 : e.call(r, t);
+        null == o || S.current || ((0, g.KE)(n, o), (S.current = !0));
+    }, [n, t, null == w ? void 0 : w.powerups]),
+    (null == w ? void 0 : w.catalog) == null)
         ? null
         : (0, r.jsxs)('div', {
-              className: Z.container,
+              className: E.container,
               children: [
                   (0, r.jsxs)(d.Z, {
-                      className: Z.toolbar,
+                      className: E.toolbar,
                       hideSearch: !0,
                       toolbar: (0, r.jsx)('div', {}),
                       children: [
@@ -54,99 +60,124 @@ function E(e) {
                               disabled: !0,
                               'aria-label': ''
                           }),
-                          (0, r.jsx)(d.Z.Title, { children: N.NW.string(h.Z.yv3DJC) })
+                          (0, r.jsx)(d.Z.Title, { children: Z.NW.string(C.Z.yv3DJC) })
                       ]
                   }),
                   (0, r.jsxs)('div', {
-                      className: Z.contentContainer,
+                      className: E.contentContainer,
                       children: [
-                          (0, r.jsx)('div', {
-                              className: Z.powerupsContainer,
-                              children: C.map((e) => {
-                                  let t = E.catalog.get(e);
-                                  if (null == t) return;
-                                  let { title: o, description: s } = (function (e) {
-                                          switch (e) {
-                                              case j.Us.LEVEL:
-                                                  return {
-                                                      title: N.NW.string(h.Z['TXY/b2']),
-                                                      description: N.NW.string(h.Z.aJv4PD)
-                                                  };
-                                              case j.Us.PERK:
-                                                  return {
-                                                      title: N.NW.string(h.Z.TV3Vm5),
-                                                      description: N.NW.string(h.Z.NHjM1t)
-                                                  };
-                                          }
-                                      })(e),
-                                      a = e === j.Us.LEVEL ? l.zJl : 'div';
-                                  return (0, r.jsxs)(
-                                      'div',
-                                      {
-                                          className: Z.powerupsSection,
+                          (0, r.jsxs)('div', {
+                              className: E.powerupsContainer,
+                              children: [
+                                  T &&
+                                      (0, r.jsxs)('div', {
+                                          className: E.staffContainer,
                                           children: [
                                               (0, r.jsxs)('div', {
+                                                  className: E.staffHeader,
                                                   children: [
-                                                      (0, r.jsxs)('div', {
-                                                          className: Z.headerContainer,
-                                                          children: [
-                                                              (0, r.jsx)(l.X6q, {
-                                                                  color: 'header-primary',
-                                                                  variant: 'heading-lg/semibold',
-                                                                  children: o
-                                                              }),
-                                                              e === j.Us.PERK &&
-                                                                  (0, r.jsx)(l.IGR, {
-                                                                      text: N.NW.string(N.t.oW0eUV),
-                                                                      color: c.Z.BG_BRAND
-                                                                  })
-                                                          ]
+                                                      (0, r.jsx)(l.Ymb, {
+                                                          color: l.TVs.colors.TEXT_BRAND,
+                                                          size: 'sm'
                                                       }),
-                                                      (0, r.jsx)(l.Text, {
-                                                          color: 'text-muted',
-                                                          variant: 'text-sm/medium',
-                                                          children: s
+                                                      (0, r.jsx)(l.X6q, {
+                                                          variant: 'heading-sm/semibold',
+                                                          children: 'Staff Server Notice'
                                                       })
                                                   ]
                                               }),
-                                              (0, r.jsx)(a, {
-                                                  orientation: 'horizontal',
-                                                  className: i()(Z.powerupContainer, { [Z.powerupsLevelContainer]: e === j.Us.LEVEL }),
-                                                  fade: !0,
-                                                  children: t.map((e, o) => {
-                                                      switch (e.type) {
-                                                          case j.Us.LEVEL:
-                                                              return (0, r.jsx)(
-                                                                  m.ZP,
-                                                                  {
-                                                                      guildId: n,
-                                                                      index: o,
-                                                                      powerup: e,
-                                                                      nextPowerup: t[o + 1]
-                                                                  },
-                                                                  'powerup-'.concat(e.skuId)
-                                                              );
-                                                          case j.Us.PERK:
-                                                              return (0, r.jsx)(
-                                                                  x.Z,
-                                                                  {
-                                                                      guildId: n,
-                                                                      powerup: e
-                                                                  },
-                                                                  'powerup-'.concat(e.skuId)
-                                                              );
-                                                      }
-                                                  })
+                                              (0, r.jsx)(l.Text, {
+                                                  variant: 'text-sm/medium',
+                                                  children: 'Staff servers have the perks of every level, even if they\u2019re not enabled. This may (read: will) break parts of the Boosting UI. Additional perks such as Enhanced Role Colors and Server Tags still need to be purchased with boosts.'
                                               })
                                           ]
-                                      },
-                                      'section-'.concat(e)
-                                  );
-                              })
+                                      }),
+                                  I.map((e) => {
+                                      let t = w.catalog.get(e);
+                                      if (null == t) return;
+                                      let { title: o, description: s } = (function (e) {
+                                              switch (e) {
+                                                  case j.Us.LEVEL:
+                                                      return {
+                                                          title: Z.NW.string(C.Z['TXY/b2']),
+                                                          description: Z.NW.string(C.Z.aJv4PD)
+                                                      };
+                                                  case j.Us.PERK:
+                                                      return {
+                                                          title: Z.NW.string(C.Z.TV3Vm5),
+                                                          description: Z.NW.string(C.Z.NHjM1t)
+                                                      };
+                                              }
+                                          })(e),
+                                          a = e === j.Us.LEVEL ? l.zJl : 'div';
+                                      return (0, r.jsxs)(
+                                          'div',
+                                          {
+                                              className: E.powerupsSection,
+                                              children: [
+                                                  (0, r.jsxs)('div', {
+                                                      children: [
+                                                          (0, r.jsxs)('div', {
+                                                              className: E.headerContainer,
+                                                              children: [
+                                                                  (0, r.jsx)(l.X6q, {
+                                                                      color: 'header-primary',
+                                                                      variant: 'heading-lg/semibold',
+                                                                      children: o
+                                                                  }),
+                                                                  e === j.Us.PERK &&
+                                                                      (0, r.jsx)(l.IGR, {
+                                                                          text: Z.NW.string(Z.t.oW0eUV),
+                                                                          color: c.Z.BG_BRAND
+                                                                      })
+                                                              ]
+                                                          }),
+                                                          (0, r.jsx)(l.Text, {
+                                                              color: 'text-muted',
+                                                              variant: 'text-sm/medium',
+                                                              children: s
+                                                          })
+                                                      ]
+                                                  }),
+                                                  (0, r.jsx)(a, {
+                                                      orientation: 'horizontal',
+                                                      className: i()(E.powerupContainer, { [E.powerupsLevelContainer]: e === j.Us.LEVEL }),
+                                                      fade: !0,
+                                                      children: t.map((e, o) => {
+                                                          switch (e.type) {
+                                                              case j.Us.LEVEL:
+                                                                  return (0, r.jsx)(
+                                                                      x.ZP,
+                                                                      {
+                                                                          guildId: n,
+                                                                          index: o,
+                                                                          powerup: e,
+                                                                          nextPowerup: t[o + 1]
+                                                                      },
+                                                                      'powerup-'.concat(e.skuId)
+                                                                  );
+                                                              case j.Us.PERK:
+                                                                  return (0, r.jsx)(
+                                                                      _.Z,
+                                                                      {
+                                                                          guildId: n,
+                                                                          powerup: e
+                                                                      },
+                                                                      'powerup-'.concat(e.skuId)
+                                                                  );
+                                                          }
+                                                      })
+                                                  })
+                                              ]
+                                          },
+                                          'section-'.concat(e)
+                                      );
+                                  })
+                              ]
                           }),
                           (0, r.jsxs)('div', {
-                              className: Z.sidebarContainer,
-                              children: [(0, r.jsx)(_.Z, { guildId: n }), (0, r.jsx)(v.Z, { guildId: n })]
+                              className: E.sidebarContainer,
+                              children: [(0, r.jsx)(f.Z, { guildId: n }), (0, r.jsx)(v.Z, { guildId: n })]
                           })
                       ]
                   })
--- chunks/782285.js
+++ chunks/782285.js
@@ -2,6 +2,8 @@ e.exports = {
     container: 'container__89463',
     toolbar: 'toolbar__89463',
     contentContainer: 'contentContainer__89463',
+    staffContainer: 'staffContainer__89463',
+    staffHeader: 'staffHeader__89463',
     powerupsContainer: 'powerupsContainer__89463',
     sidebarContainer: 'sidebarContainer__89463',
     powerupsSection: 'powerupsSection__89463',
