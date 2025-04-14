a7852501755e9e9407e0255b7cb1d018fcd5a5df
DiscrapperManager <admin@minerpl.xyz>
2025-04-14 19:15:26 +0000

    canaryRelease: 389550
    Main scripts: 3
    Lazy Loaded Scripts: 977
    Chunks:
     * Main Files: 11896
     * Scripts: 10746
    Strings: 21172
    Routes: 708
    Experiments Count: 421

--- chunks/501809.js
+++ chunks/501809.js
@@ -1,38 +1,67 @@
-var r = n(442837),
-    i = n(570140),
-    l = n(38618);
-let a = Object.freeze({
+n(388685);
+var r,
+    i = n(442837),
+    l = n(570140),
+    a = n(38618),
+    o = n(592125),
+    s = n(709054);
+function c(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
+let u = Object.freeze({
         online: null,
         total: null
     }),
-    o = {},
-    s = null;
-class c extends r.ZP.Store {
+    d = {},
+    p = {},
+    h = null;
+class f extends (r = i.ZP.PersistedStore) {
+    initialize(e) {
+        this.waitFor(a.Z, o.Z), (p = null != e ? e : d);
+    }
+    getState() {
+        return p;
+    }
     getMemberCount(e) {
         var t;
-        return null != (t = o[e]) ? t : a;
+        return null != (t = p[e]) ? t : u;
     }
     requestCount(e, t) {
-        (s = {
+        (h = {
             guildId: e,
             channelId: t
         }),
-            l.Z.getSocket().requestChannelMemberCount(e, t);
+            a.Z.getSocket().requestChannelMemberCount(e, t);
     }
 }
-new c(i.Z, {
-    CONNECTION_OPEN: function () {
-        (o = {}), null != s && l.Z.getSocket().requestChannelMemberCount(s.guildId, s.channelId);
-    },
-    CHANNEL_MEMBER_COUNT_UPDATE: function (e) {
-        let { channelId: t, online: n, total: r } = e;
-        return (
-            (null != n || null != r) &&
-                (o[t] = {
-                    online: n,
-                    total: r
-                }),
-            !0
-        );
-    }
-});
+c(f, 'displayName', 'ChannelMemberCountStore'),
+    c(f, 'persistKey', 'channelMemberCounts'),
+    new f(l.Z, {
+        CONNECTION_OPEN: function () {
+            null != h && a.Z.getSocket().requestChannelMemberCount(h.guildId, h.channelId),
+                s.default.keys(p).forEach((e) => {
+                    null == o.Z.getChannel(e) && delete p[e];
+                });
+        },
+        CHANNEL_MEMBER_COUNT_UPDATE: function (e) {
+            let { channelId: t, online: n, total: r } = e;
+            return (
+                (null != n || null != r) &&
+                    (p[t] = {
+                        online: n,
+                        total: r
+                    }),
+                !0
+            );
+        }
+    });
