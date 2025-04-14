be27a862278e1a96ef71a27417768861b6b5b76b
DiscrapperManager <admin@minerpl.xyz>
2025-04-14 17:09:47 +0000

    canaryRelease: 389427
    Main scripts: 3
    Lazy Loaded Scripts: 977
    Chunks:
     * Main Files: 11893
     * Scripts: 10747
    Strings: 21170
    Routes: 708
    Experiments Count: 421

--- chunks/346071.js
+++ chunks/346071.js
@@ -1,47 +1,52 @@
-n.d(t, { n: () => l });
+n.d(t, { n: () => d });
 var r = n(200651);
 n(192379);
-var i = n(481060),
-    o = n(886969),
-    a = n(388032),
-    s = n(429680);
-function l(e) {
+var i = n(442837),
+    o = n(481060),
+    a = n(886969),
+    s = n(580130),
+    l = n(981631),
+    c = n(388032),
+    u = n(429680);
+function d(e) {
     let { fractionalPremiumInfo: t } = e,
-        n = (0, o.Z)(t.endsAt),
-        l = '';
+        n = (0, a.Z)(t.endsAt),
+        d = (0, i.e7)([s.Z], () => s.Z.get(t.currentEntitlementId));
+    if ((null == d ? void 0 : d.sourceType) === l.kNB.REVERSE_TRIAL) return null;
+    let f = '';
     return (n.days > 0 && n.hours > 0
-        ? (l = a.NW.format(a.t['6r9LLi'], {
+        ? (f = c.NW.format(c.t['6r9LLi'], {
               days: n.days,
               hours: n.hours
           }))
         : n.days > 0
-          ? (l = a.NW.format(a.t.c2BbVF, { days: n.days }))
-          : n.hours > 0 && (l = a.NW.format(a.t['8Xm6uL'], { hours: n.hours })),
-    '' === l)
+          ? (f = c.NW.format(c.t.c2BbVF, { days: n.days }))
+          : n.hours > 0 && (f = c.NW.format(c.t['8Xm6uL'], { hours: n.hours })),
+    '' === f)
         ? null
         : (0, r.jsxs)('div', {
-              className: s.activeFractionalPremiumBannerContainer,
+              className: u.activeFractionalPremiumBannerContainer,
               children: [
                   (0, r.jsxs)('div', {
-                      className: s.nitroIconContainer,
+                      className: u.nitroIconContainer,
                       children: [
-                          (0, r.jsx)(i.SrA, {
+                          (0, r.jsx)(o.SrA, {
                               size: 'md',
                               color: 'white',
-                              className: s.iconFractional
+                              className: u.iconFractional
                           }),
-                          (0, r.jsx)(i.SrA, {
+                          (0, r.jsx)(o.SrA, {
                               size: 'md',
                               color: 'currentColor',
-                              className: s.iconFractionalDark
+                              className: u.iconFractionalDark
                           })
                       ]
                   }),
-                  (0, r.jsx)(i.Text, {
+                  (0, r.jsx)(o.Text, {
                       variant: 'text-sm/normal',
-                      children: l
+                      children: f
                   }),
-                  (0, r.jsx)('div', { className: s.activeFractionalPremiumBannerBackgroundImage })
+                  (0, r.jsx)('div', { className: u.activeFractionalPremiumBannerBackgroundImage })
               ]
           });
 }
