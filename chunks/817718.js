3ae07e4f811a3517a8200fe1d7e8a702e2bd8bdc
DiscrapperManager <admin@minerpl.xyz>
2025-04-15 19:38:42 +0000

    canaryRelease: 390107
    Main scripts: 3
    Lazy Loaded Scripts: 977
    Chunks:
     * Main Files: 11898
     * Scripts: 10751
    Strings: 21186
    Routes: 709
    Experiments Count: 423

--- chunks/817718.js
+++ chunks/817718.js
@@ -1,43 +1,49 @@
 n.d(t, {
     U3: () => p,
-    XM: () => d,
-    hd: () => c,
+    XM: () => u,
+    hd: () => l,
     lI: () => s,
-    py: () => u
+    py: () => d
 });
-var r = n(481060),
-    a = n(819640),
-    i = n(585483),
-    l = n(981631);
-let o = () => a.Z.hasLayers() || (0, r.$sL)(),
-    s = {
-        binds: ['right'],
-        comboKeysBindGlobal: !0,
-        action() {
-            if (!o()) return i.S.dispatch(l.CkL.CAROUSEL_NEXT), !1;
-        }
-    },
-    c = {
-        binds: ['left'],
-        comboKeysBindGlobal: !0,
-        action() {
-            if (!o()) return i.S.dispatch(l.CkL.CAROUSEL_PREV), !1;
-        }
-    },
-    d = {
+var a = n(481060),
+    r = n(819640),
+    o = n(585483),
+    i = n(981631);
+let c = () => r.Z.hasLayers() || (0, a.$sL)(),
+    s =
+        12633 == n.j
+            ? {
+                  binds: ['right'],
+                  comboKeysBindGlobal: !0,
+                  action() {
+                      if (!c()) return o.S.dispatch(i.CkL.CAROUSEL_NEXT), !1;
+                  }
+              }
+            : null,
+    l =
+        12633 == n.j
+            ? {
+                  binds: ['left'],
+                  comboKeysBindGlobal: !0,
+                  action() {
+                      if (!c()) return o.S.dispatch(i.CkL.CAROUSEL_PREV), !1;
+                  }
+              }
+            : null,
+    u = {
         binds: ['right', 'down', 'space'],
         comboKeysBindGlobal: !0,
-        action: () => (i.S.dispatch(l.CkL.MODAL_CAROUSEL_NEXT), !1)
+        action: () => (o.S.dispatch(i.CkL.MODAL_CAROUSEL_NEXT), !1)
     },
-    u = {
+    d = {
         binds: ['left', 'up'],
         comboKeysBindGlobal: !0,
-        action: () => (i.S.dispatch(l.CkL.MODAL_CAROUSEL_PREV), !1)
+        action: () => (o.S.dispatch(i.CkL.MODAL_CAROUSEL_PREV), !1)
     },
     p = {
         binds: ['esc'],
         comboKeysBindGlobal: !0,
         action() {
-            if (i.S.hasSubscribers(l.CkL.MODAL_CLOSE)) return i.S.dispatch(l.CkL.MODAL_CLOSE), !1;
+            if (o.S.hasSubscribers(i.CkL.MODAL_CLOSE)) return o.S.dispatch(i.CkL.MODAL_CLOSE), !1;
         }
     };
