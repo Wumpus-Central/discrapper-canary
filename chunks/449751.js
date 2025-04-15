bb2e1249b7fc0e35504cb5897b1d53126ae90f7a
DiscrapperManager <admin@minerpl.xyz>
2025-04-15 18:29:57 +0000

    canaryRelease: 390048
    Main scripts: 3
    Lazy Loaded Scripts: 977
    Chunks:
     * Main Files: 11898
     * Scripts: 10751
    Strings: 21186
    Routes: 709
    Experiments Count: 423

--- chunks/449751.js
+++ chunks/449751.js
@@ -1,26 +1,25 @@
-n.d(t, { Z: () => u });
+n.d(t, { Z: () => c });
 var r = n(200651);
 n(192379);
-var i = n(852229),
-    a = n(481060),
-    l = n(626135),
-    o = n(572004),
-    s = n(981631),
-    c = n(388032);
-function u(e, t) {
-    return o.wS && e.type !== s.uaV.GUILD_INVITE_REMINDER
-        ? (0, r.jsx)(a.sNh, {
+var i = n(481060),
+    a = n(626135),
+    l = n(572004),
+    o = n(981631),
+    s = n(388032);
+function c(e, t) {
+    return l.wS && e.type !== o.uaV.GUILD_INVITE_REMINDER
+        ? (0, r.jsx)(i.sNh, {
               id: 'copy-link',
-              label: c.NW.string(c.t.Xrt5Pj),
-              icon: a.xPt,
+              label: s.NW.string(s.t.Xrt5Pj),
+              icon: i.xPt,
               action: () => {
-                  (0, i.J)(
+                  (0, l.JG)(
                       ''
                           .concat(location.protocol, '//')
                           .concat(location.host)
-                          .concat(s.Z5c.CHANNEL(t.guild_id, t.id, e.id))
+                          .concat(o.Z5c.CHANNEL(t.guild_id, t.id, e.id))
                   ),
-                      l.default.track(s.rMx.MESSAGE_LINK_COPIED, {
+                      a.default.track(o.rMx.MESSAGE_LINK_COPIED, {
                           message_id: e.id,
                           channel: e.channel_id
                       });
