338924e780f0ddacd9930eb172f6d09fa335e4d9
DiscrapperManager <admin@minerpl.xyz>
2025-04-15 20:12:41 +0000

    canaryRelease: 390139
    Main scripts: 3
    Lazy Loaded Scripts: 970
    Chunks:
     * Main Files: 11898
     * Scripts: 10746
    Strings: 21186
    Routes: 709
    Experiments Count: 422

--- chunks/131565.js
+++ chunks/131565.js
@@ -24,44 +24,40 @@ function b(e) {
                       wallpaperThumbhashDataUrl: null != e.default.thumbhash ? (0, l.xS)(g.from(e.default.thumbhash, 'base64')) : void 0
                   };
         }),
-        [h, b] = i.useState(n),
-        [y, _] = i.useState(d);
+        [h, b] = i.useState(d);
     return (i.useEffect(() => {
         null == n && t.isViewable && u.Z.shouldFetchWallpapers && (0, p.k9)();
     }, [n, t.isViewable]),
     i.useEffect(() => {
-        if (h === n) return;
-        if (null == n) {
-            b(void 0), _(void 0);
-            return;
-        }
+        if (null == n) return void b(void 0);
         let e = performance.now();
-        (0, s.p)(n, (t) => {
-            if (t === n) {
-                b(t), _(d);
-                let n = performance.now();
-                c.default.track(f.rMx.CHAT_WALLPAPER_DOWNLOADED, {
-                    time_used_in_ms: n - e,
-                    asset_url: t
-                });
-            }
-        });
-    }, [h, n, d]),
+        b(d),
+            (0, s.p)(n, (t) => {
+                if (t === n) {
+                    b(void 0);
+                    let n = performance.now();
+                    c.default.track(f.rMx.CHAT_WALLPAPER_DOWNLOADED, {
+                        time_used_in_ms: n - e,
+                        asset_url: t
+                    });
+                }
+            });
+    }, [n, d]),
     t.isViewable)
         ? (0, r.jsxs)(r.Fragment, {
               children: [
-                  null != y &&
+                  null != h &&
                       (0, r.jsx)('div', {
                           className: m.thumbhashContainer,
                           children: (0, r.jsx)('img', {
-                              src: y,
+                              src: h,
                               alt: '',
                               'aria-hidden': !0
                           })
                       }),
                   (0, r.jsx)('div', {
                       className: m.wallpaperContainer,
-                      style: { backgroundImage: null != h ? 'url('.concat(h, ')') : void 0 }
+                      style: { backgroundImage: null != n ? 'url('.concat(n, ')') : void 0 }
                   })
               ]
           })
