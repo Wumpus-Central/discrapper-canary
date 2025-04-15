733664f14cb398c626bcdf36e29b3f1a5f1750e9
DiscrapperManager <admin@minerpl.xyz>
2025-04-15 16:47:07 +0000

    canaryRelease: 389950
    Main scripts: 3
    Lazy Loaded Scripts: 977
    Chunks:
     * Main Files: 11897
     * Scripts: 10752
    Strings: 21186
    Routes: 709
    Experiments Count: 422

--- chunks/745579.js
+++ chunks/745579.js
@@ -1,27 +1,36 @@
-n.d(t, { Z: () => s });
-var r = n(70956),
-    a = n(823379),
-    i = n(875425),
-    l = n(388032);
-let o = (e) => l.NW.data.formatTime(e, { format: 'short' });
-function s(e) {
+n.d(t, { Z: () => u });
+var r = n(55935),
+    a = n(70956),
+    i = n(823379),
+    l = n(875425),
+    o = n(388032);
+let s = (e) => {
+    let t = Date.now() + e;
+    return (0, r.KC)(new Date(), new Date(t))
+        ? o.NW.data.formatTime(t, { format: 'short' })
+        : o.NW.formatToPlainString(o.t['sdRx+P'], {
+              date: o.NW.data.formatRelativeTime(1, 'day', { numeric: 'auto' }),
+              time: o.NW.data.formatTime(t, { format: 'short' })
+          });
+};
+function u(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
     switch (e) {
-        case i.FO.TODAY:
-            if (t) return ''.concat(l.NW.formatToPlainString(l.t.Rea2gY, { hours: 24 }), ' (').concat(o(Date.now() + r.Z.Millis.DAY), ')');
-            return l.NW.string(l.t.GQmLra);
-        case i.FO.HOURS_4:
-            if (t) return ''.concat(l.NW.formatToPlainString(l.t.Rea2gY, { hours: 4 }), ' (').concat(o(Date.now() + 4 * r.Z.Millis.HOUR), ')');
-            return l.NW.formatToPlainString(l.t.Rea2gY, { hours: 4 });
-        case i.FO.HOURS_1:
-            if (t) return ''.concat(l.NW.formatToPlainString(l.t.Rea2gY, { hours: 1 }), ' (').concat(o(Date.now() + r.Z.Millis.HOUR), ')');
-            return l.NW.formatToPlainString(l.t.Rea2gY, { hours: 1 });
-        case i.FO.MINUTES_30:
-            if (t) return ''.concat(l.NW.formatToPlainString(l.t.TS3eJS, { minutes: 30 }), ' (').concat(o(Date.now() + 30 * r.Z.Millis.MINUTE), ')');
-            return l.NW.formatToPlainString(l.t.TS3eJS, { minutes: 30 });
-        case i.FO.DONT_CLEAR:
-            return l.NW.string(l.t.bRn8cn);
+        case l.FO.TODAY:
+            if (t) return ''.concat(o.NW.formatToPlainString(o.t.Rea2gY, { hours: 24 }), ' (').concat(s(a.Z.Millis.DAY), ')');
+            return o.NW.string(o.t.GQmLra);
+        case l.FO.HOURS_4:
+            if (t) return ''.concat(o.NW.formatToPlainString(o.t.Rea2gY, { hours: 4 }), ' (').concat(s(4 * a.Z.Millis.HOUR), ')');
+            return o.NW.formatToPlainString(o.t.Rea2gY, { hours: 4 });
+        case l.FO.HOURS_1:
+            if (t) return ''.concat(o.NW.formatToPlainString(o.t.Rea2gY, { hours: 1 }), ' (').concat(s(a.Z.Millis.HOUR), ')');
+            return o.NW.formatToPlainString(o.t.Rea2gY, { hours: 1 });
+        case l.FO.MINUTES_30:
+            if (t) return ''.concat(o.NW.formatToPlainString(o.t.TS3eJS, { minutes: 30 }), ' (').concat(s(30 * a.Z.Millis.MINUTE), ')');
+            return o.NW.formatToPlainString(o.t.TS3eJS, { minutes: 30 });
+        case l.FO.DONT_CLEAR:
+            return o.NW.string(o.t.bRn8cn);
         default:
-            (0, a.vE)(e);
+            (0, i.vE)(e);
     }
 }
