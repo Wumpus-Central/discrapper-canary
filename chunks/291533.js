2be68cbcc1db88210519cd13d1460d86de426d5c
DiscrapperManager <admin@minerpl.xyz>
2025-04-16 23:00:24 +0000

    canaryRelease: 390914
    Main scripts: 3
    Lazy Loaded Scripts: 971
    Chunks:
     * Main Files: 11898
     * Scripts: 10744
    Strings: 21205
    Routes: 709
    Experiments Count: 415

--- chunks/291533.js
+++ chunks/291533.js
@@ -1,11 +1,13 @@
-n.d(t, { G: () => a });
+n.d(t, { G: () => c });
 var r = n(442837),
     i = n(704215),
     l = n(243778),
     o = n(999382),
-    s = n(562511);
-function a(e) {
-    let t = (0, s.A2)(e),
-        n = (0, r.e7)([o.Z], () => o.Z.isOpen());
-    return (0, l.bf)(t && !n ? i.z.GUILD_TAG_AVAILABLE_COACHMARK : null, { cooldownDurationMs: 1800000 });
+    s = n(819640),
+    a = n(562511);
+function c(e) {
+    let t = (0, a.A2)(e),
+        n = (0, r.e7)([o.Z], () => o.Z.isOpen()),
+        c = (0, r.e7)([s.Z], () => s.Z.hasLayers());
+    return (0, l.bf)(!t || n || c ? null : i.z.GUILD_TAG_AVAILABLE_COACHMARK, { cooldownDurationMs: 1800000 });
 }
