c3dc33eed7436fa149ac0f18518ed0f1cd48f449
DiscrapperManager <admin@minerpl.xyz>
2025-04-16 07:41:38 +0000

    canaryRelease: 390508
    Main scripts: 3
    Lazy Loaded Scripts: 971
    Chunks:
     * Main Files: 11901
     * Scripts: 10746
    Strings: 21196
    Routes: 709
    Experiments Count: 421

--- chunks/486311.js
+++ chunks/486311.js
@@ -1,20 +1,22 @@
-n.d(t, { Z: () => v }), n(388685);
+n.d(t, { Z: () => C }), n(388685);
 var r = n(200651),
     i = n(192379),
     l = n(120356),
     o = n.n(l),
-    s = n(481060),
-    a = n(434479),
-    c = n(713081),
-    u = n(326660),
-    d = n(441536),
-    h = n(933104),
-    p = n(332538),
-    f = n(921944),
-    g = n(680278),
-    m = n(388032),
-    b = n(388298);
-function _(e) {
+    s = n(442837),
+    a = n(481060),
+    c = n(434479),
+    u = n(819640),
+    d = n(713081),
+    h = n(326660),
+    p = n(441536),
+    f = n(933104),
+    g = n(332538),
+    m = n(921944),
+    b = n(680278),
+    y = n(388032),
+    _ = n(388298);
+function v(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -39,7 +41,7 @@ function _(e) {
     }
     return e;
 }
-let y = {
+let O = {
         animation: {
             BEG: 0,
             END: 75
@@ -49,82 +51,81 @@ let y = {
             END: 376
         }
     },
-    v = i.memo(function (e) {
+    C = i.memo(function (e) {
         let { guildId: t, selected: l } = e,
-            v = (0, p.ZP)(t),
-            O = i.useCallback(() => {
+            C = (0, g.ZP)(t),
+            S = i.useCallback(() => {
                 var e;
-                (0, c.jd)(t), (0, d.Z)(t), null == v || null == (e = v.popout) || e.markAsDismissed(f.L.INDIRECT_ACTION);
-            }, [t, v]),
-            C = i.useCallback(() => {
+                (0, d.jd)(t), (0, p.Z)(t), null == C || null == (e = C.popout) || e.markAsDismissed(m.L.INDIRECT_ACTION);
+            }, [t, C]),
+            j = i.useCallback(() => {
                 var e;
-                switch (null == v || null == (e = v.popout) ? void 0 : e.type) {
-                    case u.J.LEVEL_REACHED:
-                        return (0, r.jsx)(h.jG, _({ guildId: t }, v.popout));
-                    case u.J.PERKS_AVAILABLE:
-                        return (0, r.jsx)(h.$h, _({ guildId: t }, v.popout));
-                    case u.J.PERKS_PURCHASABLE:
-                        return (0, r.jsx)(h.yI, _({ guildId: t }, v.popout));
+                switch (null == C || null == (e = C.popout) ? void 0 : e.type) {
+                    case h.J.LEVEL_REACHED:
+                        return (0, r.jsx)(f.jG, v({ guildId: t }, C.popout));
+                    case h.J.PERKS_AVAILABLE:
+                        return (0, r.jsx)(f.$h, v({ guildId: t }, C.popout));
+                    case h.J.PERKS_PURCHASABLE:
+                        return (0, r.jsx)(f.yI, v({ guildId: t }, C.popout));
                     default:
                         return (0, r.jsx)('div', {});
                 }
-            }, [t, null == v ? void 0 : v.popout]),
-            [S, j] = i.useState(!1),
-            [E, x] = i.useState(null),
-            N = null == E ? 'animation' : 'LOOP';
+            }, [t, null == C ? void 0 : C.popout]),
+            E = (0, a.s9z)(a.JQI),
+            x = (0, s.e7)([u.Z], () => u.Z.hasLayers()),
+            [N, I] = i.useState(null),
+            P = null == N ? 'animation' : 'LOOP';
         i.useEffect(() => {
-            (null == v ? void 0 : v.popout) == null && x(null);
-        }, [null == v ? void 0 : v.popout]);
-        let I = i.useCallback((e) => {
-            x(e);
+            (null == C ? void 0 : C.popout) == null && I(null);
+        }, [null == C ? void 0 : C.popout]);
+        let w = i.useCallback((e) => {
+            I(e);
         }, []);
-        return (0, r.jsx)(s.yRy, {
-            shouldShow: (null == v ? void 0 : v.popout) != null && !S,
+        return (0, r.jsx)(a.yRy, {
+            shouldShow: (null == C ? void 0 : C.popout) != null && !(E || x),
             nudgeAlignIntoViewport: !0,
             animationPosition: 'bottom',
             position: 'right',
-            renderPopout: C,
-            onRequestOpen: () => j(!1),
-            onRequestClose: () => j(!0),
+            renderPopout: j,
             children: (e) => {
-                var i, c;
+                var i, s;
                 return (0, r.jsx)(
-                    a.m,
-                    ((i = _({}, e)),
-                    (c = c =
+                    c.m,
+                    ((i = v({}, e)),
+                    (s = s =
                         {
-                            className: b.container,
+                            className: _.container,
                             id: 'skill-trees-'.concat(t),
                             renderIcon: (e) =>
-                                (0, r.jsx)(s.$Eu, {
+                                (0, r.jsx)(a.$Eu, {
                                     size: 'md',
                                     className: e,
                                     color: 'currentColor'
                                 }),
                             background:
-                                (null == v ? void 0 : v.popout) != null &&
+                                (null == C ? void 0 : C.popout) != null &&
                                 (0, r.jsx)('div', {
-                                    className: b.lottieContainer,
-                                    children: (0, r.jsx)(s.kci, {
-                                        nextScene: N,
-                                        className: b.lottie,
-                                        sceneSegments: y,
+                                    className: _.lottieContainer,
+                                    children: (0, r.jsx)(a.kci, {
+                                        nextScene: P,
+                                        className: _.lottie,
+                                        sceneSegments: O,
                                         importData: () => n.e('50821').then(n.t.bind(n, 548421, 19)),
-                                        onScenePlay: I,
+                                        onScenePlay: w,
                                         rendererSettings: { preserveAspectRatio: 'xMidYMid slice' }
                                     })
                                 }),
                             text: (0, r.jsx)('span', {
-                                className: o()({ [b.textImportant]: (null == v ? void 0 : v.showUnread) === !0 }),
-                                children: m.NW.string(g.Z.yv3DJC)
+                                className: o()({ [_.textImportant]: (null == C ? void 0 : C.showUnread) === !0 }),
+                                children: y.NW.string(b.Z.yv3DJC)
                             }),
                             selected: l,
-                            onClick: O,
-                            showUnread: (null == v ? void 0 : v.showUnread) === !0,
-                            trailing: null == v ? void 0 : v.trailing
+                            onClick: S,
+                            showUnread: (null == C ? void 0 : C.showUnread) === !0,
+                            trailing: null == C ? void 0 : C.trailing
                         }),
                     Object.getOwnPropertyDescriptors
-                        ? Object.defineProperties(i, Object.getOwnPropertyDescriptors(c))
+                        ? Object.defineProperties(i, Object.getOwnPropertyDescriptors(s))
                         : (function (e, t) {
                               var n = Object.keys(e);
                               if (Object.getOwnPropertySymbols) {
@@ -132,8 +133,8 @@ let y = {
                                   n.push.apply(n, r);
                               }
                               return n;
-                          })(Object(c)).forEach(function (e) {
-                              Object.defineProperty(i, e, Object.getOwnPropertyDescriptor(c, e));
+                          })(Object(s)).forEach(function (e) {
+                              Object.defineProperty(i, e, Object.getOwnPropertyDescriptor(s, e));
                           }),
                     i)
                 );
