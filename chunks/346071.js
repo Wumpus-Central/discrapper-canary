d093987fb391f27fb4c1efd29cb5157f4430a2c1
DiscrapperManager <admin@minerpl.xyz>
2025-04-16 19:00:53 +0000

    canaryRelease: 390730
    Main scripts: 3
    Lazy Loaded Scripts: 970
    Chunks:
     * Main Files: 11898
     * Scripts: 10747
    Strings: 21200
    Routes: 709
    Experiments Count: 416

--- chunks/346071.js
+++ chunks/346071.js
@@ -10,24 +10,37 @@ var i = n(442837),
     u = n(388032),
     d = n(429680);
 function f(e) {
-    let { fractionalPremiumInfo: t } = e,
-        n = t.fractionalState === l.a$.FP_SUB_PAUSED,
-        f = (0, a.Z)(t.endsAt, n),
-        _ = (0, i.e7)([s.Z], () => s.Z.get(t.currentEntitlementId));
-    if ((null == _ ? void 0 : _.sourceType) === c.kNB.REVERSE_TRIAL) return null;
-    let p = '';
-    return (f.days > 0 && f.hours > 0
-        ? (p = u.NW.format(u.t['6r9LLi'], {
-              days: f.days,
-              hours: f.hours
+    let { fractionalPremiumInfo: t, variant: n = 'default', style: f } = e,
+        _ = t.fractionalState === l.a$.FP_SUB_PAUSED,
+        p = (0, a.Z)(t.endsAt, _),
+        h = (0, i.e7)([s.Z], () => s.Z.get(t.currentEntitlementId));
+    if ((null == h ? void 0 : h.sourceType) === c.kNB.REVERSE_TRIAL) return null;
+    let m = '',
+        g = {
+            trial: {
+                days_and_hours: u.t.SQyVVV,
+                days: u.t['2ifAYG'],
+                hours: u.t.kZSTYm
+            },
+            default: {
+                days_and_hours: u.t['6r9LLi'],
+                days: u.t.c2BbVF,
+                hours: u.t['8Xm6uL']
+            }
+        };
+    return (p.days > 0 && p.hours > 0
+        ? (m = u.NW.format(g[n].days_and_hours, {
+              days: p.days,
+              hours: p.hours
           }))
-        : f.days > 0
-          ? (p = u.NW.format(u.t.c2BbVF, { days: f.days }))
-          : f.hours > 0 && (p = u.NW.format(u.t['8Xm6uL'], { hours: f.hours })),
-    '' === p)
+        : p.days > 0
+          ? (m = u.NW.format(g[n].days, { days: p.days }))
+          : p.hours > 0 && (m = u.NW.format(g[n].hours, { hours: p.hours })),
+    '' === m)
         ? null
         : (0, r.jsxs)('div', {
               className: d.activeFractionalPremiumBannerContainer,
+              style: f,
               children: [
                   (0, r.jsxs)('div', {
                       className: d.nitroIconContainer,
@@ -46,7 +59,7 @@ function f(e) {
                   }),
                   (0, r.jsx)(o.Text, {
                       variant: 'text-sm/normal',
-                      children: p
+                      children: m
                   }),
                   (0, r.jsx)('div', { className: d.activeFractionalPremiumBannerBackgroundImage })
               ]
