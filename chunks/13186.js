5bd34b31e0d7f3d925c30d9a5d5b751d98899391
DiscrapperManager <admin@minerpl.xyz>
2025-04-17 17:01:23 +0000

    canaryRelease: 391115
    Main scripts: 3
    Lazy Loaded Scripts: 971
    Chunks:
     * Main Files: 11898
     * Scripts: 10747
    Strings: 21205
    Routes: 709
    Experiments Count: 415

--- chunks/13186.js
+++ chunks/13186.js
@@ -1,60 +1,68 @@
 n.d(t, {
-    U: () => u,
-    Z: () => d
+    U: () => h,
+    Z: () => p
 });
 var r,
     i = n(200651),
     l = n(192379),
     o = n(120356),
     s = n.n(o),
-    a = n(481060),
-    c = n(341402),
-    u = (((r = {}).TOP = 'top'), (r.BOTTOM = 'bottom'), r);
-let d = l.forwardRef(function (e, t) {
-    let { body: n, header: r, artClassName: l, buttonText: o, onClose: u, art: d, align: h } = e;
-    return (0, i.jsx)('div', {
-        className: c.tooltip,
-        ref: t,
-        children: (0, i.jsxs)('div', {
-            className: c.content,
-            children: [
-                (0, i.jsx)('div', {
-                    className: l,
-                    children: d
-                }),
-                (0, i.jsxs)('div', {
-                    className: c.body,
-                    children: [
-                        (0, i.jsx)(a.X6q, {
-                            className: c.header,
-                            variant: 'heading-md/bold',
-                            color: 'always-white',
-                            children: r
-                        }),
-                        null == n
-                            ? null
-                            : 'string' == typeof n
-                              ? (0, i.jsx)(a.Text, {
-                                    variant: 'text-sm/normal',
-                                    color: 'always-white',
-                                    children: n
-                                })
-                              : n
-                    ]
-                }),
-                (0, i.jsx)('div', {
-                    className: c.buttonContainer,
-                    children: (0, i.jsx)(a.zxk, {
-                        className: c.button,
-                        onClick: (e) => {
-                            null == u || u(e);
-                        },
-                        color: a.zxk.Colors.BRAND,
-                        children: o
-                    })
-                }),
-                (0, i.jsx)('div', { className: s()(c.pointer, { [c.pointerBottom]: 'bottom' === h }) })
-            ]
+    a = n(990547),
+    c = n(481060),
+    u = n(213609),
+    d = n(341402),
+    h = (((r = {}).TOP = 'top'), (r.BOTTOM = 'bottom'), r);
+let p = l.forwardRef(function (e, t) {
+    let { body: n, header: r, artClassName: l, buttonText: o, onClose: h, art: p, align: f } = e;
+    return (
+        (0, u.Z)({
+            type: a.ImpressionTypes.POPOUT,
+            name: a.ImpressionNames.CHAT_WALLPAPER_DM_LIST_COACHMARK
+        }),
+        (0, i.jsx)('div', {
+            className: d.tooltip,
+            ref: t,
+            children: (0, i.jsxs)('div', {
+                className: d.content,
+                children: [
+                    (0, i.jsx)('div', {
+                        className: l,
+                        children: p
+                    }),
+                    (0, i.jsxs)('div', {
+                        className: d.body,
+                        children: [
+                            (0, i.jsx)(c.X6q, {
+                                className: d.header,
+                                variant: 'heading-md/bold',
+                                color: 'always-white',
+                                children: r
+                            }),
+                            null == n
+                                ? null
+                                : 'string' == typeof n
+                                  ? (0, i.jsx)(c.Text, {
+                                        variant: 'text-sm/normal',
+                                        color: 'always-white',
+                                        children: n
+                                    })
+                                  : n
+                        ]
+                    }),
+                    (0, i.jsx)('div', {
+                        className: d.buttonContainer,
+                        children: (0, i.jsx)(c.zxk, {
+                            className: d.button,
+                            onClick: (e) => {
+                                null == h || h(e);
+                            },
+                            color: c.zxk.Colors.BRAND,
+                            children: o
+                        })
+                    }),
+                    (0, i.jsx)('div', { className: s()(d.pointer, { [d.pointerBottom]: 'bottom' === f }) })
+                ]
+            })
         })
-    });
+    );
 });
