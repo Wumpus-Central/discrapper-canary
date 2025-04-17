2bdfe520d5a93ac63e38ae2e8aaea4049dad7005
DiscrapperManager <admin@minerpl.xyz>
2025-04-17 20:04:29 +0000

    canaryRelease: 391256
    Main scripts: 3
    Lazy Loaded Scripts: 971
    Chunks:
     * Main Files: 11898
     * Scripts: 10756
    Strings: 21206
    Routes: 709
    Experiments Count: 417

--- chunks/987209.js
+++ chunks/987209.js
@@ -1,6 +1,7 @@
 n.d(t, {
-    KB: () => O,
-    wD: () => y
+    KB: () => I,
+    b6: () => T,
+    wD: () => v
 }),
     n(388685);
 var r = n(200651),
@@ -15,13 +16,14 @@ var r = n(200651),
     f = n(975104),
     _ = n(669079),
     p = n(563132),
-    h = n(474936),
-    m = n(388032);
-let g = h.Cj.STANDARD_BOX,
-    E = void 0,
-    [b, y, v] = (0, f.Z)();
-function O(e) {
-    let { isGift: t = !1, giftRecipient: n, giftMessage: f, giftStyle: y, giftingOrigin: v, children: O } = e,
+    h = n(981631),
+    m = n(474936),
+    g = n(388032);
+let E = m.Cj.STANDARD_BOX,
+    b = void 0,
+    [y, v, O] = (0, f.Z)();
+function I(e) {
+    let { isGift: t = !1, giftRecipient: n, giftMessage: f, giftStyle: h, giftingOrigin: v, children: O } = e,
         { selectedSkuId: I } = (0, p.JL)(),
         [S, T] = i.useState(n),
         [N, A] = i.useState(),
@@ -29,12 +31,12 @@ function O(e) {
         P = (0, _.pO)(S),
         w = (0, l.TX)(),
         { enabled: D } = c.O.useExperiment({ location: 'gift card' }),
-        L = E;
-    P && ((L = w ? h.Cj.SEASONAL_STANDARD_BOX : null != y ? y : g), D && (L = h.Cj.NITROWEEN_STANDARD));
+        L = b;
+    P && ((L = w ? m.Cj.SEASONAL_STANDARD_BOX : null != h ? h : E), D && (L = m.Cj.NITROWEEN_STANDARD));
     let [x, M] = i.useState(L),
         k = (0, d.iE)(),
         [j, U] = i.useState(),
-        [G, B] = i.useState(t && (0, _.MY)(S) === _.xr.CUSTOM_MESSAGE_EMOJI_SOUNDBOARD && null == f ? m.NW.string(m.t.ZkOo1d) : f),
+        [G, B] = i.useState(t && (0, _.MY)(S) === _.xr.CUSTOM_MESSAGE_EMOJI_SOUNDBOARD && null == f ? g.NW.string(g.t.ZkOo1d) : f),
         [F, V] = i.useState(void 0),
         [Z, H] = i.useState(void 0),
         W = (0, _.E5)(I, t),
@@ -59,7 +61,7 @@ function O(e) {
         ),
         $ = (0, u.x8)(),
         ee = (0, o.Wu)([a.Z], () => a.Z.recommendedGiftSkuIds);
-    return (0, r.jsx)(b.Provider, {
+    return (0, r.jsx)(y.Provider, {
         value: {
             isGift: t,
             giftCode: W,
@@ -92,3 +94,27 @@ function O(e) {
         children: O
     });
 }
+let S = {
+        isGift: !1,
+        setGiftRecipient: h.dG4,
+        setGiftRecipientError: h.dG4,
+        setValidatingGiftRecipient: h.dG4,
+        selectedGiftStyle: void 0,
+        setSelectedGiftStyle: h.dG4,
+        giftCode: null,
+        sendGiftMessage: h.dG4,
+        hasSentMessage: !1,
+        isSendingMessage: !1,
+        giftMessageError: void 0,
+        alreadyHasHalloweenDeco: void 0,
+        recommendedGiftSkuIds: [],
+        claimableRewards: void 0,
+        setSelectedGiftingPromotionReward: h.dG4
+    },
+    T = (e) => {
+        let { children: t } = e;
+        return (0, r.jsx)(y.Provider, {
+            value: S,
+            children: t
+        });
+    };
