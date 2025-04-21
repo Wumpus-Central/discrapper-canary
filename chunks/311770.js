2d06d0180dede2301ac5e500be36994391ebbd78
DiscrapperManager <admin@minerpl.xyz>
2025-04-21 18:08:03 +0000

    canaryRelease: 392060
    Main scripts: 3
    Lazy Loaded Scripts: 1213
    Chunks:
     * Main Files: 11902
     * Scripts: 11001
    Strings: 21202
    Routes: 709
    Experiments Count: 416

--- chunks/311770.js
+++ chunks/311770.js
@@ -1,47 +1,69 @@
-s.d(e, { EmojiStudioModal: () => r });
-var o = s(200651);
-s(192379);
-var l = s(481060),
-    a = s(375727),
-    n = s(388032),
-    d = s(807179);
-let i = (t) => {
-        let { userImage: e } = t;
-        return (0, o.jsxs)('main', {
+l.d(t, { EmojiStudioModal: () => g }), l(388685);
+var n = l(200651),
+    a = l(192379),
+    s = l(442837),
+    o = l(481060),
+    d = l(999382),
+    i = l(430824),
+    r = l(496675),
+    c = l(375727),
+    u = l(981631),
+    h = l(388032),
+    m = l(807179);
+let b = (e) => {
+        let { userImage: t } = e,
+            { guilds: l, currentGuildId: s } = j(),
+            [d, i] = a.useState(s);
+        return (0, n.jsxs)('main', {
             children: [
-                (0, o.jsx)('div', {
-                    className: d.left,
-                    children: (0, o.jsx)('img', {
-                        src: e.data,
+                (0, n.jsx)('div', {
+                    className: m.left,
+                    children: (0, n.jsx)('img', {
+                        src: t.data,
                         alt: 'Uploaded content'
                     })
                 }),
-                (0, o.jsxs)('div', {
-                    className: d.right,
+                (0, n.jsxs)('div', {
+                    className: m.right,
                     children: [
-                        (0, o.jsx)(l.X6q, {
+                        (0, n.jsx)(o.X6q, {
                             variant: 'heading-lg/bold',
-                            children: n.intl.string(n.t.DU0dy8)
+                            children: h.intl.string(h.t.DU0dy8)
                         }),
-                        (0, o.jsx)(l.olH, {
-                            onClick: a.y,
-                            className: d.closeButton
+                        (0, n.jsx)(o.q4e, {
+                            options: l.map((e) => ({
+                                label: e.name,
+                                value: e.id
+                            })),
+                            value: d,
+                            onChange: (e) => i(e)
+                        }),
+                        (0, n.jsx)(o.olH, {
+                            onClick: c.y,
+                            className: m.closeButton
                         })
                     ]
                 })
             ]
         });
     },
-    r = (t) => {
-        let { transitionState: e, userImage: s } = t;
-        return (0, o.jsx)(l.Y0X, {
-            transitionState: e,
-            size: l.CgR.LARGE,
-            className: d.modalRoot,
-            children: (0, o.jsx)(l.hzk, {
+    g = (e) => {
+        let { transitionState: t, userImage: l } = e;
+        return (0, n.jsx)(o.Y0X, {
+            transitionState: t,
+            size: o.CgR.LARGE,
+            className: m.modalRoot,
+            children: (0, n.jsx)(o.hzk, {
                 scrollbarType: 'none',
-                className: d.modalContent,
-                children: (0, o.jsx)(i, { userImage: s })
+                className: m.modalContent,
+                children: (0, n.jsx)(b, { userImage: l })
             })
         });
-    };
+    },
+    j = () => ({
+        guilds: (0, s.e7)([i.Z, r.Z], () => Object.values(i.Z.getGuilds()).filter((e) => r.Z.can(u.Plq.CREATE_GUILD_EXPRESSIONS, e))),
+        currentGuildId: (0, s.e7)([d.Z, r.Z], () => {
+            let e = d.Z.getGuild();
+            return r.Z.can(u.Plq.CREATE_GUILD_EXPRESSIONS, e) && null != e ? e.id : null;
+        })
+    });
