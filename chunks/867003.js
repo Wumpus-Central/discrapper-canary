f2a5580f8590c57200ce317c0a5f67670ea6cdfe
DiscrapperManager <admin@minerpl.xyz>
2025-04-21 16:09:18 +0000

    canaryRelease: 391974
    Main scripts: 3
    Lazy Loaded Scripts: 1213
    Chunks:
     * Main Files: 11900
     * Scripts: 11000
    Strings: 21193
    Routes: 709
    Experiments Count: 416

--- chunks/867003.js
+++ chunks/867003.js
@@ -1,8 +1,8 @@
 n.d(t, {
-    L0: () => h,
-    Rr: () => _,
-    tp: () => g,
-    wT: () => b
+    L0: () => g,
+    Rr: () => b,
+    tp: () => _,
+    wT: () => x
 });
 var r = n(704215),
     i = n(605236),
@@ -14,17 +14,18 @@ var r = n(704215),
     u = n(982863),
     d = n(987562),
     p = n(981631);
-let m = o.Z.Millis.WEEK,
-    f = o.Z.Millis.DAYS_30,
-    h = () => {
+let m = 3 * o.Z.Millis.DAY,
+    f = o.Z.Millis.WEEK,
+    h = o.Z.Millis.DAYS_30,
+    g = () => {
         if (!(0, s.nZ)('block_user_feedback_utils')) return !1;
         let e = l.Z.getSinces();
         return Object.keys(e).some((t) => {
             let n = Date.now() - Date.parse(e[t]);
-            return l.Z.isBlocked(t) && n > m && n < f;
+            return l.Z.isBlocked(t) && n > f && n < h;
         });
     },
-    g = (e, t, n, r) => {
+    _ = (e, t, n, r) => {
         a.default.track(p.rMx.BLOCK_USER_FEEDBACK_SUBMITTED, {
             rating: e,
             feedback: t,
@@ -32,17 +33,19 @@ let m = o.Z.Millis.WEEK,
             skipped: r
         });
     },
-    _ = () => {
-        if (!(0, u.C$)('ignore_user_feedback_utils')) return !1;
-        let { isDismissed: e } = (0, i.Fo)(r.z.NAGBAR_NOTICE_IGNORE_USER_FEEDBACK, { cooldownDurationMs: d.pv });
-        if (e) return !1;
-        let t = c.Z.getIgnoreTimestamps();
-        return Object.keys(t).some((e) => {
-            let n = Date.now() - Number(t[e]);
-            return l.Z.isIgnored(e) && n > m && n < f;
+    b = () => {
+        let { enabled: e, shouldGetShorterIgnoreDuration: t } = (0, u.C$)('ignore_user_feedback_utils');
+        if (!e) return !1;
+        let { isDismissed: n } = (0, i.Fo)(r.z.NAGBAR_NOTICE_IGNORE_USER_FEEDBACK, { cooldownDurationMs: d.pv });
+        if (n) return !1;
+        let a = t ? m : f,
+            o = c.Z.getIgnoreTimestamps();
+        return Object.keys(o).some((e) => {
+            let t = Date.now() - Number(o[e]);
+            return l.Z.isIgnored(e) && t > a && t < h;
         });
     },
-    b = (e, t, n, r) => {
+    x = (e, t, n, r) => {
         a.default.track(p.rMx.IGNORE_USER_FEEDBACK_SUBMITTED, {
             rating: e,
             feedback: t,
--- chunks/982863.js
+++ chunks/982863.js
@@ -3,16 +3,29 @@ let r = (0, n(818083).B)({
     kind: 'user',
     id: '2025-04_ignore_user_feedback',
     label: 'Ignore User Feedback Experiment',
-    defaultConfig: { enabled: !1 },
+    defaultConfig: {
+        enabled: !1,
+        shouldGetShorterIgnoreDuration: !1
+    },
     treatments: [
         {
             id: 1,
             label: 'Enable Ignore User Feedback Survey',
-            config: { enabled: !0 }
+            config: {
+                enabled: !0,
+                shouldGetShorterIgnoreDuration: !1
+            }
+        },
+        {
+            id: 2,
+            label: 'Enable Ignore User Feedback Survey w/ shorter duration for testing',
+            config: {
+                enabled: !0,
+                shouldGetShorterIgnoreDuration: !0
+            }
         }
     ]
 });
 function i(e) {
-    let { enabled: t } = r.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
-    return t;
+    return r.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
 }
