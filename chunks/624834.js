c3e37daa3823bb4a3f3466ef65444bfc11a13b1b
DiscrapperManager <admin@minerpl.xyz>
2025-04-23 14:10:49 +0000

    canaryRelease: 392972
    Main scripts: 3
    Lazy Loaded Scripts: 1214
    Chunks:
     * Main Files: 11904
     * Scripts: 10981
    Strings: 21221
    Routes: 710
    Experiments Count: 416

--- chunks/624834.js
+++ chunks/624834.js
@@ -7,26 +7,26 @@ var r = n(200651),
     s = n(481060),
     u = n(2052),
     c = n(638880),
-    d = n(413458),
-    f = n(620662),
-    m = n(778569),
-    p = n(100527),
-    E = n(906732),
-    g = n(835473),
-    v = n(314897),
-    h = n(592125),
-    S = n(594174),
+    d = n(620662),
+    f = n(778569),
+    m = n(100527),
+    p = n(906732),
+    E = n(835473),
+    g = n(592125),
+    v = n(831506),
+    h = n(594174),
+    S = n(823379),
     b = n(849171),
     y = n(981631),
     O = n(388032),
     Z = n(164914);
 function j(e) {
     let { participant: t, width: n, channelId: i } = e,
-        { analyticsLocations: a } = (0, E.ZP)(p.Z.PRESENCE_ACTIVITY_TILE),
+        { analyticsLocations: a } = (0, p.ZP)(m.Z.PRESENCE_ACTIVITY_TILE),
         { activity: o } = t,
         s = o.application_id,
-        [u] = (0, g.Z)([s]),
-        { url: c } = (0, m.Z)({
+        [u] = (0, E.Z)([s]),
+        { url: c } = (0, f.Z)({
             applicationId: s,
             names: b.Ix,
             size: b.bn
@@ -38,7 +38,7 @@ function j(e) {
             }),
             [t]
         );
-    return (0, r.jsx)(E.Gt, {
+    return (0, r.jsx)(p.Gt, {
         value: a,
         children: (0, r.jsxs)('div', {
             className: Z.container,
@@ -64,42 +64,51 @@ function j(e) {
 }
 function I(e) {
     var t, n;
-    let { width: i, channelId: m, participant: p, applicationId: j } = e,
+    let { width: i, channelId: f, participant: m, applicationId: j } = e,
         I = (0, b.V_)(i),
         [_] = (0, b.Ym)(i),
-        P = (0, o.e7)([h.Z], () => h.Z.getChannel(m)),
-        x = (0, o.e7)([S.default, v.default], () => ((0, d.J)(p, v.default) ? null : S.default.getUser(p.userId))),
-        w = l.useMemo(() => (null != x ? [x] : []), [x]),
-        A = (0, g.q)(j),
-        { analyticsLocations: R } = (0, E.ZP)(),
-        N = (0, u.O)(),
-        C = p.activity.session_id,
-        T = (0, f.Z)(p.activity, y.xjy.EMBEDDED) && (0, f.Z)(p.activity, y.xjy.CONTEXTLESS),
-        [D, M] = l.useState(!1),
-        L = null == x ? void 0 : x.id,
-        k = l.useCallback(
+        P = (0, o.e7)([g.Z], () => g.Z.getChannel(f)),
+        w = null == (n = m.activity) || null == (t = n.party) ? void 0 : t.id,
+        x = (0, o.Wu)(
+            [h.default, v.Z],
+            () => {
+                var e;
+                return Array.from(null != (e = v.Z.getParty(w)) ? e : new Set())
+                    .map((e) => h.default.getUser(e))
+                    .filter(S.lm);
+            },
+            [w]
+        ),
+        A = (0, o.e7)([h.default], () => h.default.getUser(m.userId), [m]),
+        R = (0, E.q)(j),
+        { analyticsLocations: N } = (0, p.ZP)(),
+        C = (0, u.O)(),
+        T = m.activity.session_id,
+        D = (0, d.Z)(m.activity, y.xjy.EMBEDDED) && (0, d.Z)(m.activity, y.xjy.CONTEXTLESS),
+        [M, L] = l.useState(!1),
+        k = null == A ? void 0 : A.id,
+        U = l.useCallback(
             async (e) => {
-                var t, n;
                 e.stopPropagation(),
                     null != j &&
-                        null != L &&
-                        (M(!0),
+                        null != k &&
+                        (L(!0),
                         await (0, c.Z)({
                             applicationId: j,
-                            activityChannelId: m,
-                            locationObject: N.location,
-                            analyticsLocations: R,
-                            joinUserId: L,
-                            joinSessionId: C,
-                            partyId: null == (n = p.activity) || null == (t = n.party) ? void 0 : t.id,
+                            activityChannelId: f,
+                            locationObject: C.location,
+                            analyticsLocations: N,
+                            joinUserId: k,
+                            joinSessionId: T,
+                            partyId: w,
                             instanceId: void 0,
-                            isContextlessActivity: T
+                            isContextlessActivity: D
                         }),
-                        M(!1));
+                        L(!1));
             },
-            [j, L, N.location, R, C, m, T, null == (n = p.activity) || null == (t = n.party) ? void 0 : t.id]
+            [j, k, C.location, N, T, f, D, w]
         );
-    return null == P || null == x || null == A
+    return null == P || null == A || null == R
         ? null
         : (0, r.jsxs)('div', {
               className: Z.splash,
@@ -108,7 +117,7 @@ function I(e) {
                       avatarSize: _,
                       guildId: P.guild_id,
                       channelId: P.id,
-                      users: w
+                      users: x
                   }),
                   (0, r.jsx)(s.Text, {
                       className: a()(Z.subheader, {
@@ -116,7 +125,7 @@ function I(e) {
                           [Z.medium]: I === b.nR.MEDIUM
                       }),
                       variant: 'text-sm/normal',
-                      children: O.intl.formatToPlainString(O.t['7Uuia2'], { username: x.username })
+                      children: O.intl.formatToPlainString(O.t['7Uuia2'], { username: A.username })
                   }),
                   (0, r.jsx)(s.Text, {
                       className: a()(Z.header, {
@@ -124,13 +133,13 @@ function I(e) {
                           [Z.medium]: I === b.nR.MEDIUM
                       }),
                       variant: 'text-sm/normal',
-                      children: A.name
+                      children: R.name
                   }),
                   (0, r.jsx)('div', {
                       className: Z.buttons,
                       children: (0, r.jsx)(s.zxk, {
-                          onClick: k,
-                          submitting: D,
+                          onClick: U,
+                          submitting: M,
                           size: (0, b.ac)(I),
                           className: Z.button,
                           color: s.zxk.Colors.PRIMARY,
