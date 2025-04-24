52b62745228ea5763f74f53f4ed28ed85e30ac38
DiscrapperManager <admin@minerpl.xyz>
2025-04-24 19:15:40 +0000

    canaryRelease: 393537
    Main scripts: 3
    Lazy Loaded Scripts: 1217
    Chunks:
     * Main Files: 11905
     * Scripts: 10986
    Strings: 21234
    Routes: 711
    Experiments Count: 419

---
+++ chunks/52923.js
@@ -0,0 +1,14 @@
+n.d(t, { Z: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-04_keyframe_on_join',
+    label: 'Emit a keyframe when a new user joins the MLS group',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enable',
+            config: { enabled: !0 }
+        }
+    ]
+});
