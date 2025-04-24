b3582d625325f2cfde3878a6bfa5eba7960285a3
DiscrapperManager <admin@minerpl.xyz>
2025-04-24 17:51:20 +0000

    canaryRelease: 393476
    Main scripts: 3
    Lazy Loaded Scripts: 1217
    Chunks:
     * Main Files: 11904
     * Scripts: 10985
    Strings: 21232
    Routes: 711
    Experiments Count: 419

--- chunks/982204.js
+++ chunks/982204.js
@@ -1,7 +1,8 @@
 n.d(t, {
-    B: () => h,
+    B: () => m,
     E: () => p
-});
+}),
+    n(953529);
 var r = n(200651);
 n(192379);
 var i = n(512722),
@@ -20,31 +21,37 @@ function _(e) {
         ? (0, r.jsx)(l.C, {
               sku: t,
               value: n,
-              className: f.subscriptionCostRow
+              className: f.invoiceRow
           })
         : (0, r.jsx)(s.B1, {
               label: t.name,
               value: n,
-              className: f.subscriptionCostRow
+              className: f.invoiceRow
           });
 }
 let p = (e) => {
-    let { sku: t, value: n } = e;
-    return (0, r.jsx)(s.aO, {
-        className: f.invoice,
-        children: (0, r.jsx)(_, {
-            sku: t,
-            value: n
-        })
-    });
-};
-function h(e) {
+        let { sku: t, value: n } = e;
+        return (0, r.jsx)(s.aO, {
+            className: f.invoice,
+            children: (0, r.jsx)(_, {
+                sku: t,
+                value: n
+            })
+        });
+    },
+    h = (e) => {
+        if (null == e.discounts || 0 === e.discounts.length) return null;
+        let t = e.discounts[0];
+        return 0 === t.amount ? null : t;
+    };
+function m(e) {
     let { sku: t, skuPricePreview: n } = e;
     a()(null != n.amount, 'SKU must have a price set.'), a()(null != n.invoice_items && 1 === n.invoice_items.length, 'SKU preview must have single line item');
     let i = n.invoice_items[0],
         o = null != i.unit_price ? i.unit_price.amount : n.subtotal,
         l = !n.tax_inclusive && n.tax > 0,
-        u = o !== n.amount;
+        u = h(i),
+        p = o !== n.amount;
     return (0, r.jsxs)(s.aO, {
         className: f.invoice,
         children: [
@@ -52,20 +59,26 @@ function h(e) {
                 sku: t,
                 value: (0, c.T4)(o, n.currency)
             }),
+            null != u &&
+                (0, r.jsx)(s.B1, {
+                    label: u.description,
+                    value: (0, c.T4)(-1 * u.amount, n.currency),
+                    className: f.invoiceDiscountRow
+                }),
             l &&
                 (0, r.jsx)(s.B1, {
                     label: d.intl.string(d.t['/I8zmJ']),
                     value: (0, c.T4)(n.tax, n.currency),
-                    className: f.subscriptionCostRow
+                    className: f.invoiceRow
                 }),
-            u &&
+            p &&
                 (0, r.jsxs)(r.Fragment, {
                     children: [
                         (0, r.jsx)(s.UN, {}),
                         (0, r.jsx)(s.az, {
                             label: d.intl.format(d.t['+B5KfH'], {}),
                             value: (0, c.T4)(n.amount, n.currency),
-                            className: f.subscriptionCostRow
+                            className: f.invoiceRow
                         })
                     ]
                 })
