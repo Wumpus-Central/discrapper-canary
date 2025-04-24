db79ef7627bf21aec36228d3e3cd463adc8fe0f1
DiscrapperManager <admin@minerpl.xyz>
2025-04-24 18:00:00 +0000

    canaryRelease: 393487
    Main scripts: 3
    Lazy Loaded Scripts: 1217
    Chunks:
     * Main Files: 11904
     * Scripts: 10985
    Strings: 21232
    Routes: 711
    Experiments Count: 419

--- chunks/568248.js
+++ chunks/568248.js
@@ -1,24 +1,26 @@
-r.d(t, { Z: () => f }), r(953529), r(388685);
+r.d(t, { Z: () => O }), r(953529), r(388685);
 var n = r(200651),
     l = r(192379),
-    i = r(95398),
-    o = r(169525),
-    a = r(499376),
-    c = r(128854),
-    s = r(524444),
-    u = r(295435),
-    d = r(217702),
-    p = r(583760);
-function f(e) {
-    let { media: t, spoiler: r, description: f } = e,
-        b = (0, a.pU)(t),
-        { gifAutoPlay: O } = (0, c.c)(),
-        [m, y] = l.useState(r),
-        j = (e) => {
+    i = r(120356),
+    o = r.n(i),
+    a = r(95398),
+    c = r(169525),
+    s = r(499376),
+    u = r(128854),
+    d = r(524444),
+    p = r(295435),
+    f = r(217702),
+    b = r(583760);
+function O(e) {
+    let { media: t, spoiler: r, description: i } = e,
+        O = (0, s.pU)(t),
+        { gifAutoPlay: m } = (0, u.c)(),
+        [y, j] = l.useState(r),
+        g = (e) => {
             var r, l;
-            return 'IMAGE' !== b
-                ? (0, n.jsx)(u.S, {
-                      className: p.imgContainer,
+            return 'IMAGE' !== O
+                ? (0, n.jsx)(p.S, {
+                      className: b.imgContainer,
                       media: t,
                       placeholderWidth: 85,
                       placeholderHeight: 85,
@@ -26,11 +28,11 @@ function f(e) {
                       maxHeight: 85,
                       hiddenSpoilers: e
                   })
-                : (0, n.jsx)(s.Yi, {
-                      containerClassName: p.imgContainer,
-                      imageClassName: p.img,
+                : (0, n.jsx)(d.Yi, {
+                      containerClassName: o()(b.imgContainer, { [b.hiddenSpoiler]: e }),
+                      imageClassName: b.img,
                       src: t.proxyUrl,
-                      alt: f,
+                      alt: i,
                       original: t.url,
                       placeholder: t.placeholder,
                       placeholderVersion: t.placeholderVersion,
@@ -41,18 +43,18 @@ function f(e) {
                       maxHeight: 170,
                       minWidth: 85,
                       minHeight: 85,
-                      autoPlay: O && !e,
-                      mediaLayoutType: d.hV.MOSAIC,
+                      autoPlay: m && !e,
+                      mediaLayoutType: f.hV.MOSAIC,
                       reducedSizeAltTextButton: !0
                   });
         };
     return r
-        ? (0, n.jsx)(i.ZP, {
-              type: i.ZP.Types.ATTACHMENT,
-              reason: o.wk.SPOILER,
-              obscured: m,
-              onToggleObscurity: () => y((e) => !e),
-              children: (e) => j(e)
+        ? (0, n.jsx)(a.ZP, {
+              type: a.ZP.Types.ATTACHMENT,
+              reason: c.wk.SPOILER,
+              obscured: y,
+              onToggleObscurity: () => j((e) => !e),
+              children: (e) => g(e)
           })
-        : j(!1);
+        : g(!1);
 }
--- chunks/583760.js
+++ chunks/583760.js
@@ -1,4 +1,5 @@
 e.exports = {
     imgContainer: 'imgContainer__8f9ad',
-    img: 'img__8f9ad'
+    img: 'img__8f9ad',
+    hiddenSpoiler: 'hiddenSpoiler__8f9ad'
 };
