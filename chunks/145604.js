2ae24d3f4f6ddd0786117becf80e1666ac5c8f94
DiscrapperManager <admin@minerpl.xyz>
2025-04-25 20:20:52 +0000

    canaryRelease: 394027
    Main scripts: 3
    Lazy Loaded Scripts: 1218
    Chunks:
     * Main Files: 11906
     * Scripts: 11002
    Strings: 21208
    Routes: 712
    Experiments Count: 419

--- chunks/145604.js
+++ chunks/145604.js
@@ -180,7 +180,9 @@ class Q extends i.PureComponent {
                         voice_channel_id: E,
                         voice_channel_type: v,
                         voice_channel_bitrate: x,
-                        voice_channel_guild_id: O
+                        voice_channel_guild_id: O,
+                        distributor_game_id: V.sku,
+                        hidden_by_distributor: V.hidden
                     }),
                         null != a && _.ZP.addExecutableTrackedByAnalytics(a);
                 }, 10000),
