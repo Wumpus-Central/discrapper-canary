a1661432a59681618e84fcbce88fadce42ebd04d
DiscrapperManager <admin@minerpl.xyz>
2025-04-25 23:07:08 +0000

    canaryRelease: 394147
    Main scripts: 3
    Lazy Loaded Scripts: 1218
    Chunks:
     * Main Files: 11907
     * Scripts: 11009
    Strings: 21211
    Routes: 712
    Experiments Count: 418

--- chunks/162461.js
+++ chunks/162461.js
@@ -1,10 +1,11 @@
 n.d(t, {
     DG: () => o,
-    Mc: () => s,
-    iC: () => f,
-    nP: () => d,
-    sA: () => l,
-    wm: () => c
+    E2: () => l,
+    Mc: () => c,
+    iC: () => p,
+    nP: () => _,
+    sA: () => u,
+    wm: () => d
 });
 var r = n(818083),
     i = n(109911),
@@ -32,33 +33,54 @@ let o = (0, r.B)({
     }),
     s = (0, r.B)({
         kind: 'user',
-        id: '2025-04_hotwheels_v3_model',
-        label: 'Next iteration of the activity feed ranking model.',
-        commonTriggerPoint: a.$P.CONNECTION_OPEN,
+        id: '2025-04_hotwheels_holdout_the_sequel',
+        label: 'Tracking a holdout hashed to member list experiment for data readouts',
         defaultConfig: {},
         treatments: [
             {
-                id: 3,
-                label: 'Noteworthy',
+                id: 0,
+                label: 'Holdout',
                 config: {}
             },
             {
-                id: 15,
-                label: 'ML model V3',
+                id: 1,
+                label: 'Shadow Treatment',
                 config: {}
             }
         ]
     });
 function l(e) {
+    return s.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
+}
+let c = (0, r.B)({
+    kind: 'user',
+    id: '2025-04_hotwheels_v3_model',
+    label: 'Next iteration of the activity feed ranking model.',
+    commonTriggerPoint: a.$P.CONNECTION_OPEN,
+    defaultConfig: {},
+    treatments: [
+        {
+            id: 3,
+            label: 'Noteworthy',
+            config: {}
+        },
+        {
+            id: 15,
+            label: 'ML model V3',
+            config: {}
+        }
+    ]
+});
+function u(e) {
     let { enabled: t } = o.getCurrentConfig({ location: e }, { autoTrackExposure: !0 }),
         n = (0, i.Ji)(e, !1);
     return t || n;
 }
-function c(e) {
+function d(e) {
     let { enabled: t, impressionCappingEnabled: n } = o.getCurrentConfig({ location: e }, { autoTrackExposure: !1 });
     return t && !0 === n;
 }
-let u = (0, r.B)({
+let f = (0, r.B)({
     kind: 'user',
     id: '2024-08_content_inventory_analytics_sampling',
     label: 'Content Inventory Analytics Sampling',
@@ -71,10 +93,10 @@ let u = (0, r.B)({
         }
     ]
 });
-function d(e) {
-    return u.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
+function _(e) {
+    return f.getCurrentConfig({ location: e }, { autoTrackExposure: !0 });
 }
-let f = (0, r.B)({
+let p = (0, r.B)({
     kind: 'user',
     id: '2025-02_game_profile_reenable',
     label: 'Re-enable content inventory on game profile',
