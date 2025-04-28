40269311c51b1829c938f7eba96c1d87f25ed203
DiscrapperManager <admin@minerpl.xyz>
2025-04-28 15:38:33 +0000

    canaryRelease: 394271
    Main scripts: 3
    Lazy Loaded Scripts: 1219
    Chunks:
     * Main Files: 11909
     * Scripts: 11011
    Strings: 21214
    Routes: 712
    Experiments Count: 418

---
+++ chunks/281863.js
@@ -0,0 +1,45 @@
+n.d(t, { Z: () => c }), n(388685);
+var r = n(200651),
+    i = n(192379),
+    l = n(226690),
+    o = n(119315),
+    a = n(506262);
+let s = !1,
+    c = () => {
+        let [e, t] = i.useState(!1),
+            c = e ? a.activityIndicatorAnimation : s ? a.indicatorDotAfterAnimation : a.indicatorDotDuringAnimation;
+        return (
+            i.useEffect(() => {
+                let e = setTimeout(() => {
+                    s = !0;
+                }, 4000);
+                return () => {
+                    clearTimeout(e);
+                };
+            }, [e]),
+            (0, r.jsxs)('div', {
+                className: a.channelAppLauncherWordlePromo,
+                children: [
+                    (0, r.jsx)(l.F, {
+                        className: a.animationContainer,
+                        delay: s ? void 0 : 1000,
+                        onComplete: () => {
+                            (s = !0), t(!0);
+                        },
+                        importData: () => n.e('21797').then(n.t.bind(n, 307530, 19)),
+                        shouldAnimate: !s,
+                        autoplay: !s,
+                        loop: !1
+                    }),
+                    (0, r.jsx)(o.r, {
+                        additionalStyles: c,
+                        top: -1,
+                        right: -1,
+                        size: 7,
+                        showBlurpleTreatment: !0,
+                        maskColor: o.T.PRIMARY
+                    })
+                ]
+            })
+        );
+    };
