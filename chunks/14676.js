ba20b62e4697fa2626a9a5a736dd0ea0411f6530
DiscrapperManager <admin@minerpl.xyz>
2025-04-30 18:16:25 +0000

    canaryRelease: 395377
    Main scripts: 3
    Lazy Loaded Scripts: 1219
    Chunks:
     * Main Files: 11900
     * Scripts: 11014
    Strings: 21067
    Routes: 710
    Experiments Count: 418

---
+++ chunks/14676.js
@@ -0,0 +1,42 @@
+n.d(t, { Z: () => u });
+var i = n(200651);
+n(192379);
+var r = n(693789),
+    s = n(481060),
+    l = n(37234),
+    a = n(703656),
+    o = n(272008),
+    c = n(981631),
+    d = n(283294);
+let u = function () {
+    return (0, i.jsxs)(i.Fragment, {
+        children: [
+            (0, i.jsx)(s.X6q, {
+                variant: 'heading-xl/semibold',
+                children: 'Quest Minor Reward Capping Config'
+            }),
+            (0, i.jsxs)('div', {
+                className: d.container,
+                children: [
+                    (0, i.jsx)(s.Text, {
+                        variant: 'text-md/normal',
+                        color: 'info-help-text',
+                        style: { marginBottom: '8px' },
+                        children: 'Click the button below to clear your Minor Reward Capping History.'
+                    }),
+                    (0, i.jsx)(r.zx, {
+                        onClick: () => {
+                            (0, o.it)(), (0, l.xf)(), (0, a.uL)(c.Z5c.QUEST_HOME);
+                        },
+                        children: 'Clear History'
+                    }),
+                    (0, i.jsx)(s.Text, {
+                        variant: 'text-md/normal',
+                        color: 'info-help-text',
+                        children: 'Your may have to refresh your client after being redirected to the Quest Home in order to see these changes.'
+                    })
+                ]
+            })
+        ]
+    });
+};
