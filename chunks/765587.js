bd69294ee9fe82651428b52cd140f33ca23824ef
DiscrapperManager <admin@minerpl.xyz>
2025-05-01 17:18:23 +0000

    canaryRelease: 395790
    Main scripts: 3
    Lazy Loaded Scripts: 1213
    Chunks:
     * Main Files: 11891
     * Scripts: 10989
    Strings: 21059
    Routes: 710
    Experiments Count: 413

---
+++ chunks/765587.js
@@ -0,0 +1,17 @@
+n.d(t, { l: () => a });
+var r = n(818083),
+    l = n(987338);
+let a = (0, r.B)({
+    kind: 'user',
+    id: '2025-04_mobile_shop_pdp_design_refresh',
+    label: 'a scalable PDP for variants and new products in mobile shop',
+    commonTriggerPoint: l.$P.COLLECTIBLES_SHOP_OPEN,
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Turn on Mobile Shop PDP design refresh',
+            config: { enabled: !0 }
+        }
+    ]
+});
