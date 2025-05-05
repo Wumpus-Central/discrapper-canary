0ee389ec6a4773ec3d87f3ca2a388d129c8124c8
DiscrapperManager <admin@minerpl.xyz>
2025-05-05 22:32:57 +0000

    canaryRelease: 396897
    Main scripts: 3
    Lazy Loaded Scripts: 1215
    Chunks:
     * Main Files: 11897
     * Scripts: 11003
    Strings: 21091
    Routes: 709
    Experiments Count: 418

---
+++ chunks/118445.js
@@ -0,0 +1,14 @@
+n.d(t, { Z: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-04_network_aware_sockets',
+    label: 'Network aware sockets',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enable',
+            config: { enabled: !0 }
+        }
+    ]
+});
