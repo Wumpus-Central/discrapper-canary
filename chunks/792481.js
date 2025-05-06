d35cd3cb239fa0863697d97638cae53f8a4cf3b9
DiscrapperManager <admin@minerpl.xyz>
2025-05-06 21:29:23 +0000

    canaryRelease: 397295
    Main scripts: 3
    Lazy Loaded Scripts: 1214
    Chunks:
     * Main Files: 11903
     * Scripts: 11003
    Strings: 21096
    Routes: 709
    Experiments Count: 418

---
+++ chunks/792481.js
@@ -0,0 +1,17 @@
+n.d(t, { X: () => l });
+var r = n(818083),
+    i = n(987338);
+let l = (0, r.B)({
+    id: '2025-04_tenure_level_up_moment',
+    kind: 'user',
+    label: 'Tenure Level Up Moment',
+    defaultConfig: { enabled: !1 },
+    commonTriggerPoint: i.$P.CONNECTION_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: 'Enables new animation and UI for the moment Nitro tenure levels up.',
+            config: { enabled: !0 }
+        }
+    ]
+});
