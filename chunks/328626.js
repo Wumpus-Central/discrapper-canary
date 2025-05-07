77319ee15808155e450ef8098829a91d7b96b1ce
DiscrapperManager <admin@minerpl.xyz>
2025-05-07 20:20:35 +0000

    canaryRelease: 397608
    Main scripts: 3
    Lazy Loaded Scripts: 1214
    Chunks:
     * Main Files: 11907
     * Scripts: 11005
    Strings: 21120
    Routes: 709
    Experiments Count: 417

---
+++ chunks/328626.js
@@ -0,0 +1,31 @@
+n.d(t, {
+    g: () => i,
+    y: () => a
+}),
+    n(388685);
+var r,
+    l = n(73800),
+    i = (((r = {}).MOUNTED = 'mounted'), (r.SORT_OUT = 'sort-out'), (r.SORT_IN = 'sort-in'), (r.SHUFFLE_OUT = 'shuffle-out'), (r.SHUFFLE_IN = 'shuffle-in'), (r.FINISHED = 'finished'), r);
+let a = () => {
+    let [e, t] = l.useState('mounted');
+    return {
+        animationPhase: e,
+        startAnimation: l.useCallback((e) => {
+            let { isShuffling: n, onOutroComplete: r } = e;
+            t(n ? 'shuffle-out' : 'sort-out'),
+                setTimeout(
+                    () => {
+                        r(),
+                            t(n ? 'shuffle-in' : 'sort-in'),
+                            setTimeout(
+                                () => {
+                                    t('finished');
+                                },
+                                n ? 150 : 300
+                            );
+                    },
+                    n ? 350 : 300
+                );
+        }, [])
+    };
+};
