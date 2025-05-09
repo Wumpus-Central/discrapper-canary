f9c17ca3cdd23eeb230fc526fc6e6b398b91dfb2
DiscrapperManager <admin@minerpl.xyz>
2025-05-09 03:19:33 +0000

    canaryRelease: 398252
    Main scripts: 3
    Lazy Loaded Scripts: 1212
    Chunks:
     * Main Files: 11917
     * Scripts: 10999
    Strings: 21134
    Routes: 709
    Experiments Count: 415

---
+++ chunks/251360.js
@@ -0,0 +1,4 @@
+n.d(t, { k: () => s });
+var r = n(73800),
+    o = n(231338);
+let s = r.createContext({ onAssetLoadComplete: o.dG });
---
+++ chunks/879438.js
@@ -0,0 +1,20 @@
+n.d(t, { z: () => s });
+var r = n(381814),
+    i = n(731965),
+    l = n(5881),
+    a = n(46140),
+    o = n(231338);
+let s = (0, r.U)((e, t) => ({
+    startTime: performance.now(),
+    elapsed: 0,
+    lastOutputLogTime: -5000,
+    logger: null,
+    registerAssetLoad: () => {
+        var n;
+        let r = null != (n = t().logger) ? n : (0, l.T)({ location: a.dr.QUEST_HOME_DESKTOP });
+        if (r.info === o.dG || t().elapsed > 60000) return;
+        let s = performance.now() - t().startTime,
+            c = { elapsed: s };
+        s - t().lastOutputLogTime > 1000 && (r.info('QuestHome asset loading duration so far: '.concat(s, 'ms')), (c.lastOutputLogTime = s)), null == t().logger && (c.logger = r), (0, i.j)(() => e(c));
+    }
+}));
