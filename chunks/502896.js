50cc125bd3ef37a4bf890a3c042e506a67f04f34
DiscrapperManager <admin@minerpl.xyz>
2025-05-09 04:36:15 +0000

    canaryRelease: 398267
    Main scripts: 3
    Lazy Loaded Scripts: 1212
    Chunks:
     * Main Files: 11917
     * Scripts: 10999
    Strings: 21168
    Routes: 709
    Experiments Count: 415

---
+++ chunks/502896.js
@@ -0,0 +1,16 @@
+e.exports = {
+    rootContainer: 'rootContainer_dd04e8',
+    rootContainerLoading: 'rootContainerLoading_dd04e8',
+    claimedRootContainer: 'claimedRootContainer_dd04e8',
+    headerContainer: 'headerContainer_dd04e8',
+    headerBackground: 'headerBackground_dd04e8',
+    headerForeground: 'headerForeground_dd04e8',
+    previewContainer: 'previewContainer_dd04e8',
+    close: 'close_dd04e8',
+    footerContainer: 'footerContainer_dd04e8',
+    gradient: 'gradient_dd04e8',
+    heading: 'heading_dd04e8',
+    text: 'text_dd04e8',
+    confettiCanvas: 'confettiCanvas_dd04e8',
+    rewardTile: 'rewardTile_dd04e8'
+};
