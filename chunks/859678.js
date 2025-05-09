951f63354278f614abddb30a4577a9490596c665
DiscrapperManager <admin@minerpl.xyz>
2025-05-09 20:22:29 +0000

    canaryRelease: 398478
    Main scripts: 3
    Lazy Loaded Scripts: 1212
    Chunks:
     * Main Files: 11917
     * Scripts: 11007
    Strings: 21171
    Routes: 709
    Experiments Count: 415

---
+++ chunks/859678.js
@@ -0,0 +1,16 @@
+n.d(t, { G: () => a });
+var r = n(73800),
+    i = n(15624),
+    o = n(866483);
+function a(e) {
+    let t = i.Z.useCanUIRequestGatewaySocket();
+    r.useEffect(() => {
+        if (t)
+            return (
+                o.m$(e),
+                () => {
+                    o.HY(e);
+                }
+            );
+    }, [e, t]);
+}
