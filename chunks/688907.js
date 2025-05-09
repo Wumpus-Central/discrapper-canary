c505c4546056ddd6be718e3b94141f44be5da0b8
DiscrapperManager <admin@minerpl.xyz>
2025-05-09 16:54:25 +0000

    canaryRelease: 398334
    Main scripts: 3
    Lazy Loaded Scripts: 1212
    Chunks:
     * Main Files: 11914
     * Scripts: 11002
    Strings: 21170
    Routes: 709
    Experiments Count: 415

---
+++ chunks/688907.js
@@ -0,0 +1,7 @@
+n.d(t, { B: () => s });
+var i = n(524437),
+    r = n(695346);
+function s() {
+    let e = r.YT.useSetting();
+    return e === i.Tv.ACTIVITY_NOTIFICATIONS_UNSET ? i.Tv.ONLY_GAMES_PLAYED : e;
+}
