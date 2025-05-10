1fd315205e8df130df909008a78f1ea831c2d4bd
DiscrapperManager <admin@minerpl.xyz>
2025-05-10 00:25:13 +0000

    canaryRelease: 398605
    Main scripts: 3
    Lazy Loaded Scripts: 1213
    Chunks:
     * Main Files: 11917
     * Scripts: 11018
    Strings: 21172
    Routes: 709
    Experiments Count: 415

---
+++ chunks/803038.js
@@ -0,0 +1,17 @@
+n.d(t, { M: () => l });
+var r = n(818083),
+    i = n(987338);
+let l = (0, r.B)({
+    id: '2025-05_client_theme_color_picker',
+    kind: 'user',
+    label: 'Client Theme Color Picker',
+    defaultConfig: { enabled: !1 },
+    commonTriggerPoint: i.$P.CONNECTION_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: 'Enables setting client theme color picker',
+            config: { enabled: !0 }
+        }
+    ]
+});
