94275fd9a9ce02d50f91be046c486a82cf3611b5
DiscrapperManager <admin@minerpl.xyz>
2025-05-12 18:37:49 +0000

    canaryRelease: 398738
    Main scripts: 3
    Lazy Loaded Scripts: 1213
    Chunks:
     * Main Files: 11923
     * Scripts: 11011
    Strings: 21172
    Routes: 709
    Experiments Count: 413

---
+++ chunks/608530.js
@@ -0,0 +1,31 @@
+n.d(t, { L: () => i });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-05_upload_refactor',
+    label: 'Aligns attachment and non-attachment message sending',
+    defaultConfig: {
+        enabled: !1,
+        showSetting: !1
+    },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enabled',
+            config: {
+                enabled: !0,
+                showSetting: !1
+            }
+        },
+        {
+            id: 2,
+            label: 'Disabled',
+            config: {
+                enabled: !0,
+                showSetting: !0
+            }
+        }
+    ]
+});
+function i(e) {
+    return r.getCurrentConfig({ location: e }).enabled;
+}
