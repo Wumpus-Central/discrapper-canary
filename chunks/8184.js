259526755f9bc2810bc6b242ca40a70f868a327d
DiscrapperManager <admin@minerpl.xyz>
2025-05-15 18:45:36 +0000

    canaryRelease: 399775
    Main scripts: 4
    Lazy Loaded Scripts: 1212
    Chunks:
     * Main Files: 11935
     * Scripts: 11037
    Strings: 21189
    Routes: 708
    Experiments Count: 418

---
+++ chunks/8184.js
@@ -0,0 +1,14 @@
+n.d(t, { Z: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-05_dob_in_unclaimed_flow',
+    label: 'Age Gate in Unclaimed Account Flow',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Show birthday fields before registration',
+            config: { enabled: !0 }
+        }
+    ]
+});
