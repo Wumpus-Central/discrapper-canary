8f0fdaa24149bd59cdef253014592917004a2906
DiscrapperManager <admin@minerpl.xyz>
2025-05-15 16:16:40 +0000

    canaryRelease: 399699
    Main scripts: 4
    Lazy Loaded Scripts: 1244
    Chunks:
     * Main Files: 11935
     * Scripts: 11064
    Strings: 21159
    Routes: 708
    Experiments Count: 416

---
+++ chunks/407908.js
@@ -0,0 +1,41 @@
+n.d(t, { Z: () => b });
+var r = n(255367),
+    i = n(73800),
+    l = n(481060),
+    o = n(40851),
+    a = n(549879),
+    s = n(109590),
+    c = n(189432),
+    u = n(665149),
+    d = n(359110),
+    p = n(626135),
+    h = n(981631),
+    f = n(50493),
+    m = n(388032),
+    g = n(776571);
+function b(e) {
+    var t, n;
+    let { channel: b } = e,
+        _ = i.useContext(p.AnalyticsContext),
+        x = (0, o.Aq)(),
+        y = m.intl.string(g.default['Beo/7u']),
+        { firstMessage: C } = (0, s.cl)(b),
+        v = null == C || null == (t = C.messageSnapshots) ? void 0 : t[0],
+        j = null == v || null == (n = v.moderatorReport) ? void 0 : n.reported_user_id;
+    return b.isModeratorReportChannel() && null != j
+        ? (0, r.jsx)(u.ZP.Icon, {
+              onClick: () => {
+                  (0, d.Kh)(b.id),
+                      (0, a.i)(),
+                      (0, c.r)(b.guild_id, j, b.id, {
+                          modViewPanel: f.k.INFO,
+                          sourceLocation: null != location ? location : _.location
+                      }),
+                      x.dispatch(h.CkL.POPOUT_CLOSE);
+              },
+              tooltip: y,
+              icon: l.HI3,
+              'aria-label': y
+          })
+        : null;
+}
