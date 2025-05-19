e69e733a597b173dbb3741b9cc166b518c7c09a5
DiscrapperManager <admin@minerpl.xyz>
2025-05-19 21:23:09 +0000

    canaryRelease: 400693
    Main scripts: 4
    Lazy Loaded Scripts: 1212
    Chunks:
     * Main Files: 11947
     * Scripts: 11056
    Strings: 21202
    Routes: 708
    Experiments Count: 419

---
+++ chunks/268445.js
@@ -0,0 +1,14 @@
+n.d(t, { l: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-05_gift_code_lookup',
+    label: 'Gift Code Lookup',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Gift code lookup enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
