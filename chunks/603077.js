2cd77951f82e5986bef51b0c7b3eeb4471738b66
DiscrapperManager <admin@minerpl.xyz>
2025-05-20 19:11:34 +0000

    canaryRelease: 401059
    Main scripts: 4
    Lazy Loaded Scripts: 1219
    Chunks:
     * Main Files: 11920
     * Scripts: 11095
    Strings: 21234
    Routes: 709
    Experiments Count: 418

---
+++ chunks/603077.js
@@ -0,0 +1,14 @@
+r.d(t, { Z: () => n });
+let n = (0, r(818083).B)({
+    kind: 'user',
+    id: '2025-03_quests_premium_rewards_holdout',
+    label: 'Quest Premium Rewards Holdout',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enable Quest Premium Rewards',
+            config: { enabled: !0 }
+        }
+    ]
+});
