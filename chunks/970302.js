65986f19918ed9e6276c5dd0923dc5159874213e
DiscrapperManager <admin@minerpl.xyz>
2025-05-20 01:07:36 +0000

    canaryRelease: 400820
    Main scripts: 4
    Lazy Loaded Scripts: 1213
    Chunks:
     * Main Files: 11946
     * Scripts: 11056
    Strings: 21218
    Routes: 708
    Experiments Count: 416

---
+++ chunks/970302.js
@@ -0,0 +1,2 @@
+t.r(o), t.d(o, { default: () => s });
+let s = JSON.parse('{"xRdYAQ":"Custom themes [Beta] are not available on mobile.","2hAz8f":"Customize your theme","23QUzs":"New! Custom Themes","TRCE4u":"Match your mood with endless color combinations. Try it now, only with Nitro."}');
