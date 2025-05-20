a8d95d664b1d09ba2e44a7b4f0bca3c482a06134
DiscrapperManager <admin@minerpl.xyz>
2025-05-20 21:01:33 +0000

    canaryRelease: 401155
    Main scripts: 4
    Lazy Loaded Scripts: 1219
    Chunks:
     * Main Files: 11920
     * Scripts: 11097
    Strings: 21237
    Routes: 709
    Experiments Count: 418

---
+++ chunks/574887.js
@@ -0,0 +1,183 @@
+n.d(t, { Z: () => v });
+var o = n(255367);
+n(73800);
+var r = n(120356),
+    c = n.n(r),
+    i = n(442837),
+    a = n(481060),
+    l = n(410030),
+    s = n(726542),
+    d = n(367907),
+    u = n(122021),
+    f = n(275759),
+    m = n(888496),
+    p = n(768762),
+    b = n(706454),
+    _ = n(49012),
+    y = n(981631),
+    h = n(856651),
+    g = n(388032),
+    x = n(738873);
+function j(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            o = Object.keys(n);
+        'function' == typeof Object.getOwnPropertySymbols &&
+            (o = o.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                })
+            )),
+            o.forEach(function (t) {
+                var o;
+                (o = n[t]),
+                    t in e
+                        ? Object.defineProperty(e, t, {
+                              value: o,
+                              enumerable: !0,
+                              configurable: !0,
+                              writable: !0
+                          })
+                        : (e[t] = o);
+            });
+    }
+    return e;
+}
+function O(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : (function (e, t) {
+                  var n = Object.keys(e);
+                  if (Object.getOwnPropertySymbols) {
+                      var o = Object.getOwnPropertySymbols(e);
+                      n.push.apply(n, o);
+                  }
+                  return n;
+              })(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+function I(e) {
+    var t;
+    let { account: n, theme: r, locale: c, userId: i } = e,
+        l = null != (t = n.metadata) ? t : {},
+        b = (0, f.FI)(l[h.PC.CREATED_AT], c),
+        I = s.Z.get((0, u.rR)(n.type));
+    return (0, o.jsxs)('div', {
+        className: x.connection,
+        children: [
+            (0, o.jsx)(a.ua7, {
+                text: null == I ? void 0 : I.name,
+                children: (e) =>
+                    (0, o.jsx)(
+                        'img',
+                        O(j({}, e), {
+                            alt: g.intl.formatToPlainString(g.t.rtm15O, { name: null == I ? void 0 : I.name }),
+                            className: x.platformIcon,
+                            src: (0, a.wjy)(r) ? (null == I ? void 0 : I.icon.darkSVG) : null == I ? void 0 : I.icon.lightSVG
+                        })
+                    )
+            }),
+            (0, o.jsxs)('div', {
+                className: x.connectionInfo,
+                children: [
+                    (0, o.jsxs)('div', {
+                        children: [
+                            (() => {
+                                var e;
+                                let t = null == I || null == (e = I.getPlatformUserUrl) ? void 0 : e.call(I, n);
+                                return null == t
+                                    ? (0, o.jsx)(a.ua7, {
+                                          overflowOnly: !0,
+                                          text: n.name,
+                                          children: (e) =>
+                                              (0, o.jsx)(
+                                                  a.Text,
+                                                  O(j({}, e), {
+                                                      variant: 'text-sm/normal',
+                                                      children: n.name
+                                                  })
+                                              )
+                                      })
+                                    : (0, o.jsx)(a.eee, {
+                                          href: t,
+                                          className: x.platformUrlAnchor,
+                                          useDefaultUnderlineStyles: !1,
+                                          onClick: (e) => {
+                                              (0, d.yw)(y.rMx.CONNECTED_ACCOUNT_VIEWED, {
+                                                  platform_type: n.type,
+                                                  other_user_id: i
+                                              }),
+                                                  (0, _.q)(
+                                                      {
+                                                          href: t,
+                                                          trusted: (null == I ? void 0 : I.type) !== y.ABu.DOMAIN
+                                                      },
+                                                      e
+                                                  );
+                                          },
+                                          children: (0, o.jsxs)('div', {
+                                              className: x.accountName,
+                                              children: [
+                                                  (0, o.jsx)(a.ua7, {
+                                                      overflowOnly: !0,
+                                                      text: n.name,
+                                                      children: (e) =>
+                                                          (0, o.jsx)(
+                                                              a.Text,
+                                                              O(j({}, e), {
+                                                                  variant: 'text-sm/normal',
+                                                                  children: n.name
+                                                              })
+                                                          )
+                                                  }),
+                                                  (0, o.jsx)(p.Z, {
+                                                      className: x.platformUrlIcon,
+                                                      direction: p.Z.Directions.RIGHT
+                                                  })
+                                              ]
+                                          })
+                                      });
+                            })(),
+                            null != b &&
+                                (0, o.jsx)(a.Text, {
+                                    variant: 'text-xs/normal',
+                                    children: g.intl.format(g.t['9rfonp'], { date: b })
+                                })
+                        ]
+                    }),
+                    (0, o.jsx)('div', {
+                        className: x.metadata,
+                        children: n.type === y.ABu.REDDIT ? (0, m.oP)(l) : n.type === y.ABu.STEAM ? (0, m.Dq)(l) : n.type === y.ABu.BLUESKY || n.type === y.ABu.MASTODON || n.type === y.ABu.TWITTER ? (0, m.rJ)(l) : n.type === y.ABu.PAYPAL ? (0, m.li)(l) : n.type === y.ABu.EBAY ? (0, m.ul)(l) : n.type === y.ABu.TIKTOK ? (0, m.hf)(l) : null
+                    })
+                ]
+            })
+        ]
+    });
+}
+function v(e) {
+    let { connections: t, userId: n, className: r } = e,
+        a = (0, l.ZP)(),
+        s = (0, i.e7)([b.default], () => b.default.locale);
+    return 0 === t.length
+        ? null
+        : (0, o.jsx)('div', {
+              className: c()(x.container, r),
+              children: t.map((e) =>
+                  (0, o.jsx)(
+                      I,
+                      {
+                          account: e,
+                          userId: n,
+                          theme: a,
+                          locale: s
+                      },
+                      ''.concat(e.type, ':').concat(e.id)
+                  )
+              )
+          });
+}
---
+++ chunks/738873.js
@@ -0,0 +1,10 @@
+e.exports = {
+    container: 'container__9bfb9',
+    connection: 'connection__9bfb9',
+    connectionInfo: 'connectionInfo__9bfb9',
+    platformIcon: 'platformIcon__9bfb9',
+    accountName: 'accountName__9bfb9',
+    platformUrlAnchor: 'platformUrlAnchor__9bfb9',
+    platformUrlIcon: 'platformUrlIcon__9bfb9',
+    metadata: 'metadata__9bfb9'
+};
