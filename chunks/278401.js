44fcab45306bcf457f5c48819cec3fefdad89942
DiscrapperManager <admin@minerpl.xyz>
2025-05-20 15:57:51 +0000

    canaryRelease: 400924
    Main scripts: 4
    Lazy Loaded Scripts: 1216
    Chunks:
     * Main Files: 11931
     * Scripts: 11073
    Strings: 21220
    Routes: 708
    Experiments Count: 417

---
+++ chunks/278401.js
@@ -0,0 +1,15 @@
+n.d(t, { Z: () => r });
+let i = (0, n(818083).B)({
+        kind: 'user',
+        id: '2025-05_virtual_currency_onboarding_experience',
+        label: 'Virtual Currency Onboarding Experience',
+        defaultConfig: { enabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'Enable Virtual Currency onboarding experience',
+                config: { enabled: !0 }
+            }
+        ]
+    }),
+    r = 12633 == n.j ? i : null;
