ee06c3212c2f06032a1fc5808a6c703b87c8f3ac
DiscrapperManager <admin@minerpl.xyz>
2025-05-21 19:17:02 +0000

    canaryRelease: 401614
    Main scripts: 4
    Lazy Loaded Scripts: 1219
    Chunks:
     * Main Files: 11918
     * Scripts: 11112
    Strings: 21246
    Routes: 709
    Experiments Count: 421

---
+++ chunks/614716.js
@@ -0,0 +1,34 @@
+n.d(t, { Z: () => _ });
+var r = n(442837),
+    i = n(40851),
+    o = n(906732),
+    a = n(499254),
+    s = n(827498),
+    l = n(397698),
+    c = n(541716),
+    u = n(592125),
+    d = n(944486),
+    f = n(981631);
+function _(e) {
+    let { applicationId: t, onClose: n } = e,
+        { newestAnalyticsLocation: _ } = (0, o.ZP)(),
+        p = (0, r.e7)([u.Z, d.Z], () => u.Z.getChannel(d.Z.getChannelId())),
+        h = (0, i.bp)() === f.IlC.POPOUT;
+    return () => {
+        null == n || n(),
+            null == p || (null == p ? void 0 : p.isVocal())
+                ? (0, l.Z)({
+                      context:
+                          null != p
+                              ? {
+                                    type: 'channel',
+                                    channel: p
+                                }
+                              : { type: 'contextless' },
+                      analyticsLocation: _,
+                      openInPopout: h,
+                      initialState: { applicationId: t }
+                  })
+                : (0, a.__)(s._b.TEXT, c.Ie.NORMAL, { applicationId: t });
+    };
+}
