c02fb282db4b24f6e4078adeeb50e7bf221be997
DiscrapperManager <admin@minerpl.xyz>
2025-05-22 21:16:22 +0000

    canaryRelease: 402200
    Main scripts: 4
    Lazy Loaded Scripts: 1218
    Chunks:
     * Main Files: 11915
     * Scripts: 11105
    Strings: 21253
    Routes: 709
    Experiments Count: 424

---
+++ chunks/900916.js
@@ -0,0 +1,17 @@
+n.d(t, { F: () => l });
+var r = n(818083),
+    i = n(987338);
+let l = (0, r.B)({
+    kind: 'user',
+    id: '2025-05_mention_badge_on_dm',
+    label: 'Mention badge on DM',
+    commonTriggerPoint: i.$P.DM_GDM_LIST_RENDER,
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
