103c40de3b2dadbf453f918aea3e1ca364c36949
DiscrapperManager <admin@minerpl.xyz>
2025-05-22 01:40:02 +0000

    canaryRelease: 401836
    Main scripts: 4
    Lazy Loaded Scripts: 1219
    Chunks:
     * Main Files: 11919
     * Scripts: 11115
    Strings: 21252
    Routes: 709
    Experiments Count: 425

---
+++ chunks/383451.js
@@ -0,0 +1,41 @@
+n.d(t, {
+    H: () => a,
+    f: () => l
+});
+var i = n(818083),
+    r = n(984134);
+let s = (0, i.B)({
+    kind: 'user',
+    id: '2025-05_push_to_talk_latching',
+    label: 'Push to Talk Latching',
+    defaultConfig: { enableLatching: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enable latching feature',
+            config: { enableLatching: !0 }
+        }
+    ]
+});
+function l(e) {
+    let { location: t, autoTrackExposure: n = !0 } = e,
+        { isInHoldout: i } = r.h.getCurrentConfig({ location: t }, { autoTrackExposure: n });
+    return s.getCurrentConfig(
+        { location: t },
+        {
+            disable: i,
+            autoTrackExposure: n
+        }
+    );
+}
+function a(e) {
+    let { location: t, autoTrackExposure: n = !0 } = e,
+        { isInHoldout: i } = r.h.useExperiment({ location: t }, { autoTrackExposure: n });
+    return s.useExperiment(
+        { location: t },
+        {
+            disable: i,
+            autoTrackExposure: n
+        }
+    );
+}
