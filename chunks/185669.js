4e44cbb5b681dec3268c205d9b04bb962de8623d
DiscrapperManager <admin@minerpl.xyz>
2025-05-23 20:11:13 +0000

    canaryRelease: 402616
    Main scripts: 4
    Lazy Loaded Scripts: 1216
    Chunks:
     * Main Files: 11910
     * Scripts: 11117
    Strings: 21265
    Routes: 709
    Experiments Count: 425

---
+++ chunks/185669.js
@@ -0,0 +1,19 @@
+n.d(t, { g: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2024-11_connection_flow_refactor_v2',
+    label: 'Connection Flow Refactor',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 0,
+            label: 'Disabled',
+            config: { enabled: !1 }
+        },
+        {
+            id: 1,
+            label: 'Enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
