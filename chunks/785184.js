60479aee28cf658741819cf081f0c4b802999ab4
DiscrapperManager <admin@minerpl.xyz>
2025-05-23 05:07:59 +0000

    canaryRelease: 402396
    Main scripts: 4
    Lazy Loaded Scripts: 1216
    Chunks:
     * Main Files: 11909
     * Scripts: 11110
    Strings: 21258
    Routes: 709
    Experiments Count: 424

--- chunks/785184.js
+++ chunks/785184.js
@@ -3,16 +3,16 @@ var i = n(255367),
     r = n(73800),
     s = n(7093),
     l = n(101966),
-    a = n(804413);
+    a = n(40347);
 let o = r.lazy(() => n.e('22274').then(n.bind(n, 269792)));
 function c() {
-    let e = (0, a.Z)(),
-        t = (0, l.Z)({ location: 'TenureLevelUpModal' }),
-        n = (0, s.Z)((e) => e.shouldRenderTenureLevelUp);
-    return t && n && null != e
+    let e = (0, l.Z)({ location: 'TenureLevelUpModal' }),
+        t = (0, s.Z)((e) => e.shouldRenderTenureLevelUp),
+        n = (0, a.Z)();
+    return e && t && null != n
         ? (0, i.jsx)(r.Suspense, {
               fallback: null,
-              children: (0, i.jsx)(o, {})
+              children: (0, i.jsx)(o, { levelUpData: n })
           })
         : null;
 }
