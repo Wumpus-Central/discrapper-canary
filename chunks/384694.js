72cf868fb9d696b20cb6c60e6bb5481127d73796
DiscrapperManager <admin@minerpl.xyz>
2025-05-28 15:49:08 +0000

    canaryRelease: 403387
    Main scripts: 4
    Lazy Loaded Scripts: 1218
    Chunks:
     * Main Files: 11923
     * Scripts: 11136
    Strings: 21334
    Routes: 709
    Experiments Count: 425

---
+++ chunks/384694.js
@@ -0,0 +1,83 @@
+r.d(t, { q: () => h }), r(388685);
+var n = r(255367),
+    a = r(73800),
+    i = r(442837),
+    l = r(481060),
+    s = r(430824),
+    o = r(496675),
+    c = r(768581),
+    u = r(981631),
+    d = r(588687);
+let p = (e) => ({
+        label: e.name,
+        value: e.id
+    }),
+    g = (e) => o.Z.can(u.Plq.CREATE_GUILD_EXPRESSIONS, e),
+    h = (e) => {
+        let { onChange: t, selected: r } = e,
+            o = (0, i.e7)([s.Z], () => s.Z.getGuilds()),
+            u = a.useMemo(() => Object.values(o).filter(g).map(p), [o]),
+            h = a.useCallback(
+                (e) => {
+                    let [t] = e;
+                    if (null == t || null == t.value) return null;
+                    let r = o[t.value];
+                    if (null == r) return t.label;
+                    let a = c.ZP.getGuildIconURL({
+                        id: r.id,
+                        icon: r.icon,
+                        size: 24,
+                        canAnimate: !1
+                    });
+                    return a
+                        ? (0, n.jsx)('img', {
+                              src: a,
+                              alt: t.label,
+                              className: d.selectedIcon
+                          })
+                        : t.label;
+                },
+                [o]
+            ),
+            m = a.useCallback(
+                (e) => {
+                    if (null == e || null == e.value) return null;
+                    let t = o[e.value];
+                    if (null == t) return e.label;
+                    let r = c.ZP.getGuildIconURL({
+                        id: t.id,
+                        icon: t.icon,
+                        size: 32,
+                        canAnimate: !1
+                    });
+                    return r
+                        ? (0, n.jsxs)('div', {
+                              className: d.guildSelectOption,
+                              children: [
+                                  (0, n.jsx)('img', {
+                                      src: r,
+                                      alt: e.label
+                                  }),
+                                  (0, n.jsx)(l.Text, {
+                                      variant: 'text-sm/medium',
+                                      color: 'header-muted',
+                                      className: d.guildSelectOptionLabel,
+                                      children: e.label
+                                  })
+                              ]
+                          })
+                        : e.label;
+                },
+                [o]
+            );
+        return (0, n.jsx)(l.q4e, {
+            className: d.guildSelector,
+            onChange: t,
+            options: u,
+            popoutPosition: 'top',
+            popoutWidth: 240,
+            renderOptionLabel: m,
+            renderOptionValue: h,
+            value: r
+        });
+    };
---
+++ chunks/588687.js
@@ -0,0 +1,6 @@
+e.exports = {
+    guildSelector: 'guildSelector_f494f4',
+    selectedIcon: 'selectedIcon_f494f4',
+    guildSelectOption: 'guildSelectOption_f494f4',
+    guildSelectOptionLabel: 'guildSelectOptionLabel_f494f4'
+};
