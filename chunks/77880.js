0bb3efe528d0b7e1ce8f70eab025c76aa16830c6
DiscrapperManager <admin@minerpl.xyz>
2025-05-28 19:40:31 +0000

    canaryRelease: 403545
    Main scripts: 4
    Lazy Loaded Scripts: 1218
    Chunks:
     * Main Files: 11939
     * Scripts: 11133
    Strings: 21336
    Routes: 709
    Experiments Count: 426

---
+++ chunks/77880.js
@@ -0,0 +1,37 @@
+n.d(t, {
+    A: () => o,
+    u: () => a
+});
+var r = n(818083),
+    i = n(987338);
+let a = (0, r.B)({
+    kind: 'user',
+    id: '2025-05_change_disconnect_icon_experiment',
+    label: 'Change Disconnect Icon',
+    defaultConfig: {
+        changeLeaveCallAndActivityIcons: !1,
+        isRTCIconRed: !1
+    },
+    commonTriggerPoint: i.$P.VOICE_CALL,
+    treatments: [
+        {
+            id: 1,
+            label: 'Use door icon',
+            config: {
+                changeLeaveCallAndActivityIcons: !0,
+                isRTCIconRed: !1
+            }
+        },
+        {
+            id: 2,
+            label: 'Use door icon with red RTC icon',
+            config: {
+                changeLeaveCallAndActivityIcons: !0,
+                isRTCIconRed: !0
+            }
+        }
+    ]
+});
+function o(e) {
+    return a.useExperiment(e, { autoTrackExposure: !1 });
+}
