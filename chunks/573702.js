788fb5f8789c15cd71477c312782392df771ec4c
DiscrapperManager <admin@minerpl.xyz>
2025-05-30 20:39:48 +0000

    canaryRelease: 404499
    Main scripts: 4
    Lazy Loaded Scripts: 1281
    Chunks:
     * Main Files: 11938
     * Scripts: 11213
    Strings: 21358
    Routes: 709
    Experiments Count: 422

---
+++ chunks/573702.js
@@ -0,0 +1,27 @@
+n.d(t, { Z: () => d });
+var r = n(255367);
+n(73800);
+var i = n(120356),
+    a = n.n(i),
+    o = n(481060),
+    s = n(639119),
+    l = n(474936),
+    c = n(388032),
+    u = n(856045);
+let d = (e) => {
+    let { isTier2: t, discountAmount: n } = e,
+        i = (0, s.N)(),
+        d = null != i && i.trial_id === l.a7,
+        f = c.intl.string(c.t.IBYG5e);
+    return (
+        void 0 !== n ? (f = c.intl.formatToPlainString(c.t.iiLbvr, { percent: n })) : d && (f = c.intl.string(c.t.gtNqJS)),
+        (0, r.jsx)('div', {
+            className: a()(u.badgeContainer, t ? u.tier2 : u.tier0),
+            children: (0, r.jsx)(o.Text, {
+                variant: 'text-xxs/bold',
+                className: u.badgeText,
+                children: f
+            })
+        })
+    );
+};
---
+++ chunks/856045.js
@@ -0,0 +1,6 @@
+e.exports = {
+    badgeContainer: 'badgeContainer_fc0249',
+    tier2: 'tier2_fc0249',
+    tier0: 'tier0_fc0249',
+    badgeText: 'badgeText_fc0249'
+};
