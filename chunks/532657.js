589a1c2f56b10fe93930d1454e7a0e050dc6d440
DiscrapperManager <admin@minerpl.xyz>
2025-06-02 08:56:06 +0000

    canaryRelease: 404758
    Main scripts: 4
    Lazy Loaded Scripts: 1281
    Chunks:
     * Main Files: 11938
     * Scripts: 11222
    Strings: 21362
    Routes: 709
    Experiments Count: 422

---
+++ chunks/532657.js
@@ -0,0 +1,110 @@
+n.d(t, { Z: () => p });
+var r = n(255367),
+    i = n(73800),
+    a = n(120356),
+    o = n.n(a),
+    s = n(481060),
+    l = n(556638),
+    c = n(843280);
+function u(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
+function d(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        'function' == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                })
+            )),
+            r.forEach(function (t) {
+                u(e, t, n[t]);
+            });
+    }
+    return e;
+}
+function f(e, t) {
+    var n = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+        var r = Object.getOwnPropertySymbols(e);
+        t &&
+            (r = r.filter(function (t) {
+                return Object.getOwnPropertyDescriptor(e, t).enumerable;
+            })),
+            n.push.apply(n, r);
+    }
+    return n;
+}
+function _(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : f(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+function p(e) {
+    let { text: t, tooltipText: n, textVariant: a = 'text-xs/medium', textClassName: u, 'aria-label': f, icon: p, canTruncate: h = !0, hideTooltip: m = !1, hideText: g = !1 } = e,
+        E = i.useRef(null),
+        b = () => {
+            let e = null != n ? n : t;
+            return (0, r.jsxs)(r.Fragment, {
+                children: [
+                    p,
+                    (0, r.jsx)(s.Text, {
+                        variant: 'text-sm/medium',
+                        color: 'none',
+                        className: o()(c.tooltip, u),
+                        children: e
+                    })
+                ]
+            });
+        },
+        y = {
+            variant: a,
+            color: 'none',
+            className: o()(h && c.truncated, u)
+        },
+        O = null != t && null == n && h,
+        v = !m && (null != n || O || g);
+    return null == p && g
+        ? null
+        : v
+          ? (0, r.jsx)(s.ua7, {
+                text: b(),
+                'aria-label': f,
+                tooltipContentClassName: o()(c.container, c.activitiesTooltip),
+                delay: l.X,
+                children: (e) => {
+                    let { onMouseEnter: i, onMouseLeave: a } = e;
+                    return (0, r.jsxs)('div', {
+                        className: o()(c.container, c.textWithIconContainer),
+                        onMouseEnter: () => {
+                            let { current: e } = E,
+                                t = null != e && e.offsetWidth < e.scrollWidth;
+                            (null != n || t || g) && (null == i || i());
+                        },
+                        onMouseLeave: a,
+                        children: [p, !g && (0, r.jsx)(s.Text, _(d({ ref: E }, y), { children: t }))]
+                    });
+                }
+            })
+          : (0, r.jsxs)(r.Fragment, {
+                children: [p, !g && (0, r.jsx)(s.Text, _(d({}, y), { children: t }))]
+            });
+}
