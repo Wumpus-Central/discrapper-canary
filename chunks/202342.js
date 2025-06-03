0bc4c4ac4cc64bee63c4584e92b2dec9ce97d379
DiscrapperManager <admin@minerpl.xyz>
2025-06-03 19:40:02 +0000

    canaryRelease: 405401
    Main scripts: 4
    Lazy Loaded Scripts: 1281
    Chunks:
     * Main Files: 11938
     * Scripts: 11220
    Strings: 21384
    Routes: 710
    Experiments Count: 424

---
+++ chunks/202342.js
@@ -0,0 +1,17 @@
+n.d(t, { n: () => a });
+var r = n(818083),
+    l = n(987338);
+let a = (0, r.B)({
+    kind: 'user',
+    id: '2025-05_mobile_shop_gifting',
+    label: 'Mobile Shop Gifting',
+    commonTriggerPoint: l.$P.COLLECTIBLES_SHOP_OPEN,
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Turn on Mobile Shop Gifting',
+            config: { enabled: !0 }
+        }
+    ]
+});
