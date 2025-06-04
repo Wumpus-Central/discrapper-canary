9416f28cdbb05faab3e08bba16e773d9265e8087
DiscrapperManager <admin@minerpl.xyz>
2025-06-04 19:28:32 +0000

    canaryRelease: 405876
    Main scripts: 4
    Lazy Loaded Scripts: 1285
    Chunks:
     * Main Files: 11942
     * Scripts: 11211
    Strings: 21397
    Routes: 711
    Experiments Count: 423

--- chunks/249433.js
+++
@@ -1,6 +0,0 @@
-e.exports = {
-    cardContainer: 'cardContainer__8833c',
-    card: 'card__8833c',
-    cardContent: 'cardContent__8833c',
-    selectedCard: 'selectedCard__8833c'
-};
--- chunks/808110.js
+++
@@ -1,5 +0,0 @@
-e.exports = {
-    sortDropdown: 'sortDropdown_bb26d9',
-    sortDropdownInner: 'sortDropdownInner_bb26d9',
-    clearText: 'clearText_bb26d9'
-};
