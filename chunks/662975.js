abfdaa28094d03294f87c0f941df0c5ee99f4daf
DiscrapperManager <admin@minerpl.xyz>
2025-06-04 18:27:50 +0000

    canaryRelease: 405835
    Main scripts: 4
    Lazy Loaded Scripts: 1284
    Chunks:
     * Main Files: 11940
     * Scripts: 11223
    Strings: 21397
    Routes: 711
    Experiments Count: 423

---
+++ chunks/662975.js
@@ -0,0 +1,11 @@
+n.d(t, { g: () => l });
+var r = n(442837),
+    i = n(131704),
+    a = n(592125),
+    o = n(944486),
+    s = n(803038);
+function l() {
+    let e = s.M.useExperiment({ location: 'useCanShowCustomThemeCoachmark' }).enabled,
+        t = (0, r.e7)([a.Z, o.Z], () => a.Z.getChannel(o.Z.getChannelId()));
+    return !!e && null != t && (!!(0, i.zi)(t.type) || !!t.isPrivate());
+}
