bae552be4ab5d55cf63c059c876d87aa7fb11b34
DiscrapperManager <admin@minerpl.xyz>
2025-06-04 02:25:46 +0000

    canaryRelease: 405663
    Main scripts: 4
    Lazy Loaded Scripts: 1282
    Chunks:
     * Main Files: 11938
     * Scripts: 11216
    Strings: 21386
    Routes: 711
    Experiments Count: 424

---
+++ chunks/111677.js
@@ -0,0 +1,25 @@
+n.d(t, { t: () => i });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-06_flamingo_signed_catalog',
+    label: 'Flamingo Signed Catalog',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
+function i(e) {
+    let { location: t, autoTrackExposure: n, disable: i = !1 } = e,
+        { enabled: a } = r.getCurrentConfig(
+            { location: t },
+            {
+                autoTrackExposure: n,
+                disable: i
+            }
+        );
+    return a;
+}
