e98f80c4ca70932271b23670462cc09922e0caf7
DiscrapperManager <admin@minerpl.xyz>
2025-06-05 21:18:07 +0000

    canaryRelease: 406482
    Main scripts: 4
    Lazy Loaded Scripts: 1288
    Chunks:
     * Main Files: 11945
     * Scripts: 11235
    Strings: 21449
    Routes: 711
    Experiments Count: 422

---
+++ chunks/886412.js
@@ -0,0 +1,18 @@
+n.d(t, { VJ: () => i });
+let r = (0, n(818083).B)({
+    id: '2025-05_safety_flows',
+    label: 'Safety Flows',
+    kind: 'user',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
+function i(e) {
+    let { location: t } = e;
+    return r.getCurrentConfig({ location: t }, { autoTrackExposure: !1 }).enabled;
+}
