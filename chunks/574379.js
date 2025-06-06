bc86f496b9bb0e0a8efacfeec5b5606e28fad2c2
DiscrapperManager <admin@minerpl.xyz>
2025-06-06 18:26:58 +0000

    canaryRelease: 406822
    Main scripts: 4
    Lazy Loaded Scripts: 1288
    Chunks:
     * Main Files: 11949
     * Scripts: 11233
    Strings: 21469
    Routes: 711
    Experiments Count: 421

--- chunks/574379.js
+++ chunks/574379.js
@@ -1,10 +1,10 @@
 n.r(t),
     n.d(t, {
-        default: () => r,
-        messagesLoader: () => a
+        default: () => a,
+        messagesLoader: () => i
     });
 let { createLoader: l } = n(249288),
-    a = l(
+    i = l(
         {
             bg: () => n.e('32682').then(n.t.bind(n, 709981, 19)),
             cs: () => n.e('10874').then(n.t.bind(n, 427945, 19)),
@@ -40,5 +40,5 @@ let { createLoader: l } = n(249288),
         },
         'en-US'
     ),
-    { makeMessagesProxy: i } = n(249288),
-    r = i(a);
+    { makeMessagesProxy: r } = n(249288),
+    a = r(i);
