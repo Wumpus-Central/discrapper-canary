cdb7c864a6dc88a9795ae25b7c1c9f8cb195ce3d
DiscrapperManager <admin@minerpl.xyz>
2025-06-09 18:08:30 +0000

    canaryRelease: 407199
    Main scripts: 4
    Lazy Loaded Scripts: 1287
    Chunks:
     * Main Files: 11957
     * Scripts: 11240
    Strings: 21481
    Routes: 711
    Experiments Count: 422

---
+++ chunks/170293.js
@@ -0,0 +1,18 @@
+n.d(t, { $: () => a });
+var i = n(481060),
+    r = n(668781),
+    s = n(743498),
+    l = n(388032);
+function a(e) {
+    r.Z.show({
+        title: l.intl.string(l.t.RpjdLy),
+        body: l.intl.string(l.t.nOSopa),
+        confirmText: l.intl.string(l.t['sKJ7+f']),
+        confirmColor: i.Ttl.RED,
+        cancelText: l.intl.string(l.t['7wZ1GR']),
+        onConfirm: () => {
+            (0, s.v6)(null);
+        },
+        onCancel: e
+    });
+}
---
+++ chunks/420439.js
@@ -0,0 +1,44 @@
+n.d(t, { Z: () => d });
+var r,
+    i = n(442837),
+    a = n(570140),
+    o = n(750180);
+function s(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
+let l = { lastInitAttemptMayHaveCrashed: !1 };
+class c extends (r = i.ZP.PersistedStore) {
+    initialize(e) {
+        null != e && (l = e);
+    }
+    getState() {
+        return l;
+    }
+    getLastInitAttemptMayHaveCrashed() {
+        return l.lastInitAttemptMayHaveCrashed;
+    }
+}
+function u(e) {
+    let { state: t } = e;
+    switch (t) {
+        case o.O.LOADING:
+            l.lastInitAttemptMayHaveCrashed = !0;
+            break;
+        case o.O.LOADED:
+        case o.O.FAILED:
+        case o.O.UNSUPPORTED:
+            l.lastInitAttemptMayHaveCrashed = !1;
+    }
+}
+s(c, 'displayName', 'VoiceFilterPersistedStore'), s(c, 'persistKey', 'VoiceFilterPersistedStore');
+let d = new c(a.Z, { VOICE_FILTER_NATIVE_MODULE_STATE_CHANGE: u });
