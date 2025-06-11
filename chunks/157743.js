7234de0b49d126550c342b8850ee24dc16918bc1
DiscrapperManager <admin@minerpl.xyz>
2025-06-11 13:46:40 +0000

    canaryRelease: 408212
    Main scripts: 4
    Lazy Loaded Scripts: 1317
    Chunks:
     * Main Files: 11960
     * Scripts: 11279
    Strings: 21501
    Routes: 712
    Experiments Count: 423

---
+++ chunks/157743.js
@@ -0,0 +1,55 @@
+n.d(a, { default: () => d });
+var s = n(255367);
+n(73800);
+var r = n(481060),
+    o = n(918125),
+    t = n(488131),
+    c = n(124368),
+    i = n(388032),
+    l = n(576359);
+function d(e) {
+    let { guildId: a, onClose: n, transitionState: d } = e;
+    return (0, s.jsx)(r.Y0X, {
+        className: l.modal,
+        transitionState: d,
+        'aria-label': i.intl.string(i.t.B2panJ),
+        size: r.CgR.DYNAMIC,
+        parentComponent: 'GuildActiveThreadsModal',
+        children: (0, s.jsxs)('div', {
+            className: l.container,
+            children: [
+                (0, s.jsxs)('div', {
+                    className: l.header,
+                    children: [
+                        (0, s.jsx)(r.or_, {
+                            size: 'md',
+                            color: 'currentColor',
+                            className: l.threadIcon
+                        }),
+                        (0, s.jsx)(r.X6q, {
+                            variant: 'heading-md/semibold',
+                            className: l.__invalid_title,
+                            children: i.intl.string(i.t['dOWb+f'])
+                        }),
+                        (0, s.jsx)('div', { className: l.spacer }),
+                        (0, s.jsx)(r.P3F, {
+                            className: l.closeIcon,
+                            onClick: n,
+                            'aria-label': i.intl.string(i.t.cpT0Cg),
+                            children: (0, s.jsx)(r.Dio, {
+                                size: 'md',
+                                color: 'currentColor'
+                            })
+                        })
+                    ]
+                }),
+                (0, s.jsx)(o.Z, {
+                    guildId: a,
+                    goToThread: (e, a) => {
+                        n(), (0, t.ok)(e, !a, c.on.GUILD_ACTIVE_THREADS_MODAL);
+                    }
+                })
+            ]
+        })
+    });
+}
