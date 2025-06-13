0724a1c193877d3859d0d70afc70571169d504b7
DiscrapperManager <admin@minerpl.xyz>
2025-06-13 19:21:27 +0000

    canaryRelease: 409417
    Main scripts: 4
    Lazy Loaded Scripts: 1319
    Chunks:
     * Main Files: 11962
     * Scripts: 11309
    Strings: 21509
    Routes: 712
    Experiments Count: 428

---
+++ chunks/397047.js
@@ -0,0 +1,4 @@
+n.d(t, { $: () => r });
+var r = (function (e) {
+    return (e[(e.BOGO = 1)] = 'BOGO'), (e[(e.THIRD_PARTY_INBOUND = 3)] = 'THIRD_PARTY_INBOUND'), (e[(e.THIRD_PARTY_OUTBOUND = 4)] = 'THIRD_PARTY_OUTBOUND'), (e[(e.MARKETING_MOMENT = 5)] = 'MARKETING_MOMENT'), e;
+})({});
---
+++ chunks/605338.js
@@ -0,0 +1,71 @@
+n.d(t, { Z: () => l }), n(997841);
+var r = n(81825),
+    i = n(397047),
+    a = n(630388),
+    o = n(474936);
+function s(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
+class l extends r.Z {
+    static createFromServer(e) {
+        var t, n, r, i, a, o, s, c;
+        return new l({
+            id: e.id,
+            trialId: e.trial_id,
+            startDate: new Date(e.start_date),
+            endDate: new Date(e.end_date),
+            outboundRedemptionEndDate: null != e.outbound_redemption_end_date ? new Date(e.outbound_redemption_end_date) : null,
+            inboundHeaderText: null != (t = e.inbound_header_text) ? t : '',
+            inboundBodyText: null != (n = e.inbound_body_text) ? n : '',
+            inboundHelpCenterLink: null != (r = e.inbound_help_center_link) ? r : '',
+            outboundTitle: null != (i = e.outbound_title) ? i : '',
+            outboundRedemptionModalBody: null != (a = e.outbound_redemption_modal_body) ? a : '',
+            outboundTermsAndConditions: null != (o = e.outbound_terms_and_conditions) ? o : '',
+            outboundRedemptionPageLink: null != (s = e.outbound_redemption_page_link) ? s : '',
+            outboundRedemptionUrlFormat: null != (c = e.outbound_redemption_url_format) ? c : '',
+            flags: e.flags,
+            inboundRestrictedCountries: e.inbound_restricted_countries,
+            outboundRestrictedCountries: e.outbound_restricted_countries,
+            promotionType: e.promotion_type
+        });
+    }
+    get isThirdPartyOutbound() {
+        return this.promotionType === i.$.THIRD_PARTY_OUTBOUND;
+    }
+    get isBogo() {
+        return this.promotionType === i.$.BOGO;
+    }
+    get isMarketingMoment() {
+        return this.promotionType === i.$.MARKETING_MOMENT;
+    }
+    hasFlag(e) {
+        return (0, a.yE)(this.flags, e);
+    }
+    isCountryRestricted(e) {
+        switch (this.promotionType) {
+            case i.$.THIRD_PARTY_INBOUND:
+                return this.inboundRestrictedCountries.includes(e);
+            case i.$.THIRD_PARTY_OUTBOUND:
+                return this.outboundRestrictedCountries.includes(e);
+            default:
+                return !1;
+        }
+    }
+    isRedeemableByTrialUsers() {
+        return this.hasFlag(o.TD.IS_OUTBOUND_REDEEMABLE_BY_TRIAL_USERS);
+    }
+    constructor(e) {
+        var t, n, r;
+        super(), s(this, 'id', void 0), s(this, 'trialId', void 0), s(this, 'startDate', void 0), s(this, 'endDate', void 0), s(this, 'outboundRedemptionEndDate', void 0), s(this, 'inboundHeaderText', void 0), s(this, 'inboundBodyText', void 0), s(this, 'inboundHelpCenterLink', void 0), s(this, 'outboundTitle', void 0), s(this, 'outboundRedemptionModalBody', void 0), s(this, 'outboundTermsAndConditions', void 0), s(this, 'outboundRedemptionPageLink', void 0), s(this, 'outboundRedemptionUrlFormat', void 0), s(this, 'flags', void 0), s(this, 'inboundRestrictedCountries', void 0), s(this, 'outboundRestrictedCountries', void 0), s(this, 'promotionType', void 0), (this.id = e.id), (this.trialId = e.trialId), (this.startDate = e.startDate), (this.endDate = e.endDate), (this.outboundRedemptionEndDate = e.outboundRedemptionEndDate), (this.inboundHeaderText = e.inboundHeaderText), (this.inboundBodyText = e.inboundBodyText), (this.inboundHelpCenterLink = e.inboundHelpCenterLink), (this.outboundTitle = e.outboundTitle), (this.outboundRedemptionModalBody = e.outboundRedemptionModalBody), (this.outboundTermsAndConditions = e.outboundTermsAndConditions), (this.outboundRedemptionPageLink = e.outboundRedemptionPageLink), (this.outboundRedemptionUrlFormat = e.outboundRedemptionUrlFormat), (this.flags = null != (t = e.flags) ? t : 0), (this.inboundRestrictedCountries = null != (n = e.inboundRestrictedCountries) ? n : []), (this.outboundRestrictedCountries = null != (r = e.outboundRestrictedCountries) ? r : []), (this.promotionType = e.promotionType);
+    }
+}
