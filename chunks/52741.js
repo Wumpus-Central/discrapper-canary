d60b5f38818b46bdc806053186c689e5e6b9e773
DiscrapperManager <admin@minerpl.xyz>
2025-06-13 18:15:22 +0000

    canaryRelease: 409348
    Main scripts: 4
    Lazy Loaded Scripts: 1319
    Chunks:
     * Main Files: 11967
     * Scripts: 11301
    Strings: 21512
    Routes: 712
    Experiments Count: 428

---
+++ chunks/52741.js
@@ -0,0 +1,124 @@
+n.d(t, { Z: () => j });
+var i = n(255367);
+n(73800);
+var r = n(120356),
+    l = n.n(r),
+    a = n(913527),
+    s = n.n(a),
+    o = n(442837),
+    c = n(481060),
+    u = n(594174),
+    d = n(78839),
+    m = n(74538),
+    C = n(140465),
+    p = n(104494),
+    g = n(639119),
+    x = n(230916),
+    f = n(504865),
+    h = n(474936),
+    I = n(388032),
+    v = n(753023);
+function T(e) {
+    var t;
+    let { defaultPriceString: n, subscriptionPlan: r, discountOffer: l } = e,
+        a = (0, x.A)(r);
+    return (0, i.jsx)(i.Fragment, {
+        children: I.intl.format(I.t.sJTwHR, {
+            numMonths: null != (t = l.discount.user_usage_limit) ? t : h.rt,
+            discountedPrice: a,
+            regularPrice: n
+        })
+    });
+}
+function j(e) {
+    var t, n, r, a, x, j, P;
+    let { isGift: E, premiumTier: b, offerTierMatchesCard: _, offerType: S, showYearlyPrice: N, priceOptions: R, textVariant: Z, className: y, isApplicationHome: w, enablePremiumRebrandDesign: L, headerClassName: M, headingVariant: O = 'heading-md/normal', headingColor: H } = e,
+        A = (0, o.e7)([d.ZP], () => d.ZP.getPremiumTypeSubscription()),
+        U = (0, o.e7)([u.default], () => u.default.getCurrentUser()),
+        k = (0, C.t7)(),
+        D = (0, C.lr)(),
+        G = b === h.p9.TIER_0 ? h.Si.TIER_0 : h.Si.TIER_2,
+        V = (null == A ? void 0 : A.hasActiveTrial) ? (null == U ? void 0 : U.premiumType) : k ? h.p9.TIER_2 : null,
+        F = (0, g.N)(),
+        B = (0, p.Ng)(),
+        W = null == F ? void 0 : F.subscription_trial;
+    if (!w && !E && null != V && b === V && null != A && null != A.planIdFromItems) {
+        let e = null != A.trialEndsAt ? s()(null == A ? void 0 : A.trialEndsAt).diff(s()(), 'd') : 0,
+            a = h.GP[A.planIdFromItems],
+            o = m.ZP.formatPriceString(m.ZP.getDefaultPrice(a.id), a.interval);
+        return (0, i.jsx)(c.X6q, {
+            variant: O,
+            color: H,
+            className: l()(v.trialHeader, M),
+            children:
+                S === h.C.PREMIUM_TRIAL
+                    ? I.intl.format(I.t['2CGBrq'], {
+                          remainingTime: e,
+                          price: o
+                      })
+                    : A.planIdFromItems === h.Xh.PREMIUM_YEAR_TIER_2
+                      ? I.intl.format(I.t['+qqh6u'], {
+                            percent: null != (t = null == D ? void 0 : D.percentage) ? t : h.Bo,
+                            regularPrice: o
+                        })
+                      : I.intl.formatToPlainString(I.t['3Ziutb'], {
+                            percent: null != (n = null == D ? void 0 : D.percentage) ? n : h.M_,
+                            regularPrice: o,
+                            numMonths: null != (r = null == D ? void 0 : D.duration) ? r : h.rt
+                        })
+        });
+    }
+    if (!w && !E && _) {
+        let e = m.ZP.formatPriceString(m.ZP.getDefaultPrice(b === h.p9.TIER_0 ? h.Xh.PREMIUM_MONTH_TIER_0 : h.Xh.PREMIUM_MONTH_TIER_2), h.rV.MONTH);
+        if (S === h.C.PREMIUM_TRIAL)
+            return (0, i.jsx)(c.X6q, {
+                variant: O,
+                color: H,
+                className: l()(v.trialHeader, M),
+                children: I.intl.format(I.t['9vyovr'], {
+                    planName: (0, m.aq)(null != (x = h.IW[null != (a = null == W ? void 0 : W.sku_id) ? a : h.Si.NONE]) ? x : h.Xh.PREMIUM_MONTH_TIER_2),
+                    duration: (0, m.if)({
+                        intervalType: null != (j = null == W ? void 0 : W.interval) ? j : h.rV.DAY,
+                        intervalCount: null != (P = null == W ? void 0 : W.interval_count) ? P : 30,
+                        capitalize: !1
+                    }),
+                    price: e
+                })
+            });
+        if (null != B)
+            return (0, i.jsx)(c.X6q, {
+                variant: O,
+                color: H,
+                className: l()(v.trialHeader, M),
+                children: (0, i.jsx)(T, {
+                    defaultPriceString: e,
+                    subscriptionPlan: h.Xh.PREMIUM_MONTH_TIER_2,
+                    discountOffer: B
+                })
+            });
+    }
+    return (0, i.jsxs)(i.Fragment, {
+        children: [
+            (0, i.jsx)(f.Z, {
+                subscriptionTier: G,
+                isGift: E,
+                className: null != y ? y : v.price,
+                priceOptions: R,
+                variant: Z,
+                isApplicationHome: w,
+                enablePremiumRebrandDesign: L
+            }),
+            N &&
+                (0, i.jsx)(f.Z, {
+                    subscriptionTier: G,
+                    interval: h.rV.YEAR,
+                    className: null != y ? y : v.price,
+                    isGift: E,
+                    priceOptions: R,
+                    variant: Z,
+                    isApplicationHome: w,
+                    enablePremiumRebrandDesign: L
+                })
+        ]
+    });
+}
---
+++ chunks/753023.js
@@ -0,0 +1,4 @@
+e.exports = {
+    trialHeader: 'trialHeader__784e4',
+    price: 'price__784e4'
+};
