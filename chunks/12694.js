981357903c8e81429432207fb1e4633a7a553390
DiscrapperManager <admin@minerpl.xyz>
2025-06-13 21:38:38 +0000

    canaryRelease: 409563
    Main scripts: 4
    Lazy Loaded Scripts: 1319
    Chunks:
     * Main Files: 11960
     * Scripts: 11306
    Strings: 21509
    Routes: 712
    Experiments Count: 427

---
+++ chunks/12694.js
@@ -0,0 +1,21 @@
+n.d(t, { V: () => d });
+var r = n(73800),
+    i = n(442837),
+    l = n(481060),
+    o = n(695346),
+    s = n(885110),
+    a = n(626135),
+    c = n(316496),
+    u = n(981631);
+function d() {
+    let { variantName: e } = (0, c.D)('not sharing link'),
+        t = o.G6.useSetting(),
+        n = (0, i.e7)([s.Z], () => s.Z.getStatus());
+    (0, r.useEffect)(() => {
+        a.default.track(u.rMx.RUNNING_GAME_CARD_STATE_CHANGED, {
+            show_current_game: t,
+            status: n,
+            rtc_popout_available: 'treatment_2' === e || !t || n === l.Skl.INVISIBLE
+        });
+    }, [t, n, e]);
+}
