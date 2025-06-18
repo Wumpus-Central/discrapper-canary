761e3d019c199d1a98fd7b97d8602ea67cdac8cc
DiscrapperManager <admin@minerpl.xyz>
2025-06-18 23:27:08 +0000

    canaryRelease: 411076
    Main scripts: 4
    Lazy Loaded Scripts: 1317
    Chunks:
     * Main Files: 11974
     * Scripts: 11300
    Strings: 21517
    Routes: 712
    Experiments Count: 424

---
+++ chunks/122567.js
@@ -0,0 +1,35 @@
+n.d(t, { y: () => r }), n(73800);
+let r = (0, n(392711).throttle)(i, 300, {
+    leading: !0,
+    trailing: !0
+});
+function i(e) {
+    var t, n, r;
+    let { listRef: i, searchQuery: a, nitroLockedSectionStates: o, scrollTop: s } = e;
+    if ('' !== a)
+        return {
+            isNitroLockedSectionVisible: !1,
+            areOnlyNitroLockedSectionsVisible: !1
+        };
+    let l = null == i || null == (t = i.current) ? void 0 : t.getSectionDescriptors(),
+        c = null == (r = i.current) || null == (n = r.getListDimensions()) ? void 0 : n.height;
+    if (null == l || l.length !== o.length || null == c || c <= 0)
+        return {
+            isNitroLockedSectionVisible: !1,
+            areOnlyNitroLockedSectionsVisible: !1
+        };
+    let u = s,
+        d = s + c,
+        f = !1,
+        _ = !1;
+    return (
+        o.forEach((e, t) => {
+            let n = l[t];
+            n.offset.top < d && n.offset.bottom > u && (e.isNitroLocked ? (f = !0) : (_ = !0));
+        }),
+        {
+            isNitroLockedSectionVisible: f,
+            areOnlyNitroLockedSectionsVisible: f && !_
+        }
+    );
+}
