9300cd2408ddb247b40bdad4adc14859113bc459
DiscrapperManager <admin@minerpl.xyz>
2025-06-20 18:39:17 +0000

    canaryRelease: 411325
    Main scripts: 4
    Lazy Loaded Scripts: 1316
    Chunks:
     * Main Files: 11983
     * Scripts: 11294
    Strings: 21524
    Routes: 712
    Experiments Count: 425

---
+++ chunks/544627.js
@@ -0,0 +1,4 @@
+e.exports = {
+    linkIcon: 'linkIcon__2c32f',
+    rowContainer: 'rowContainer__2c32f'
+};
---
+++ chunks/60986.js
@@ -0,0 +1,37 @@
+n.d(t, { K: () => m });
+var r = n(255367),
+    i = n(73800),
+    l = n(481060),
+    s = n(37234),
+    a = n(749681),
+    o = n(258971),
+    c = n(49898),
+    d = n(388032),
+    u = n(544627);
+function m(e) {
+    let t = i.useCallback(() => {
+        (0, a.transitionToGlobalDiscovery)({
+            tab: c.GlobalDiscoveryTab.APPS,
+            newSessionState: {
+                guildId: e.guildId,
+                entrypoint: {
+                    name: o.xF.GUILD_SETTINGS,
+                    guildSettingsSection: e.section
+                }
+            }
+        }),
+            (0, s.xf)();
+    }, [e.guildId, e.section]);
+    return (0, r.jsxs)(l.P3F, {
+        onClick: t,
+        className: u.rowContainer,
+        children: [
+            (0, r.jsx)('div', { children: d.intl.string(d.t.AKcFUl) }),
+            (0, r.jsx)(l.rgF, {
+                size: 'xs',
+                color: 'currentColor',
+                className: u.linkIcon
+            })
+        ]
+    });
+}
