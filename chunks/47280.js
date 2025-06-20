a8329558eff6d8f10c4fe38e1826af4ce79986c9
DiscrapperManager <admin@minerpl.xyz>
2025-06-20 21:29:30 +0000

    canaryRelease: 411420
    Main scripts: 4
    Lazy Loaded Scripts: 1316
    Chunks:
     * Main Files: 11980
     * Scripts: 11287
    Strings: 21524
    Routes: 712
    Experiments Count: 423

--- chunks/47280.js
+++ chunks/47280.js
@@ -1,24 +1,24 @@
 n.d(t, {
     Z: () => o,
-    k: () => a
+    j: () => a
 });
 var r = n(818083),
     i = n(987338);
 let a = (0, r.B)({
     kind: 'user',
-    id: '2025-06_payment_modal_nitro_rebrand_experiment',
-    label: 'Payment Modal Nitro Rebrand Experiment',
+    id: '2025-09_premium_brand_refresh_ga',
+    label: 'Premium Brand Refresh GA',
     defaultConfig: { enabled: !1 },
     commonTriggerPoint: i.$P.CONNECTION_OPEN,
     treatments: [
         {
             id: 0,
-            label: 'User sees control payment modal',
+            label: 'User sees control',
             config: { enabled: !1 }
         },
         {
             id: 1,
-            label: 'User sees nitro rebrand payment modal',
+            label: 'User sees premium brand refresh',
             config: { enabled: !0 }
         }
     ]
