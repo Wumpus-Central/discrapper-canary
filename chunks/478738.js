11e8ba11d6f2668bdaa7f4dd9c95980b50c45c64
DiscrapperManager <admin@minerpl.xyz>
2025-06-24 21:45:41 +0000

    canaryRelease: 412374
    Main scripts: 4
    Lazy Loaded Scripts: 1314
    Chunks:
     * Main Files: 11965
     * Scripts: 11301
    Strings: 21560
    Routes: 712
    Experiments Count: 426

---
+++ chunks/478738.js
@@ -0,0 +1,42 @@
+n.d(t, { Z: () => u }), n(539854);
+var r,
+    i,
+    l,
+    o = n(442837),
+    s = n(570140);
+let a = [];
+class c extends (r = o.ZP.Store) {
+    getGeoRestrictedGuilds() {
+        return a;
+    }
+}
+(l = 'GeoRestrictedGuildStore'),
+    (i = 'displayName') in c
+        ? Object.defineProperty(c, i, {
+              value: l,
+              enumerable: !0,
+              configurable: !0,
+              writable: !0
+          })
+        : (c[i] = l);
+let u = new c(s.Z, {
+    CONNECTION_OPEN: function (e) {
+        a = e.geoRestrictedGuilds;
+    },
+    GUILD_DELETE: function (e) {
+        let { guild: t } = e;
+        if (-1 === a.findIndex((e) => e.id === t.id)) return !1;
+        a = a.filter((e) => e.id !== t.id);
+    },
+    GUILD_GEO_RESTRICTED: function (e) {
+        let t = a.filter((t) => t.id !== e.guildId);
+        t.push({
+            id: e.guildId,
+            name: e.name,
+            icon: e.icon,
+            unavailable: !0,
+            geo_restricted: !0
+        }),
+            (a = t);
+    }
+});
