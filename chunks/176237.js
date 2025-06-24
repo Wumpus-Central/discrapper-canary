160dbd96811e5edfdb8bb982137bef227a0dda40
DiscrapperManager <admin@minerpl.xyz>
2025-06-24 21:01:08 +0000

    canaryRelease: 412331
    Main scripts: 4
    Lazy Loaded Scripts: 1314
    Chunks:
     * Main Files: 11965
     * Scripts: 11300
    Strings: 21560
    Routes: 712
    Experiments Count: 426

---
+++ chunks/176237.js
@@ -0,0 +1,27 @@
+t.d(n, {
+    $: () => a,
+    D: () => r
+});
+var l = t(818083);
+let r = (0, t(427164).le)({
+        name: '2025-06-apex-aa-evolving-tenure-badge-web',
+        kind: 'user',
+        defaultConfig: { value: 'blue' },
+        variations: {
+            0: { value: 'blue' },
+            1: { value: 'green' }
+        }
+    }),
+    a = (0, l.B)({
+        id: '2025-06_apex_client_aa_rollout',
+        kind: 'user',
+        label: 'Apex Client SDK AA Test Rollout',
+        defaultConfig: { enabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'Enabled',
+                config: { enabled: !0 }
+            }
+        ]
+    });
