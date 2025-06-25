eea910beb123916f5594d9361ea37c9208817252
DiscrapperManager <admin@minerpl.xyz>
2025-06-25 00:14:32 +0000

    canaryRelease: 412482
    Main scripts: 4
    Lazy Loaded Scripts: 1313
    Chunks:
     * Main Files: 11967
     * Scripts: 11299
    Strings: 21560
    Routes: 712
    Experiments Count: 427

---
+++ chunks/822253.js
@@ -0,0 +1,14 @@
+n.d(t, { S: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-06_async_audio_device',
+    label: 'Asynchronous Audio Device Manager Controls',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Asynchronous Audio Device Manager Controls',
+            config: { enabled: !0 }
+        }
+    ]
+});
