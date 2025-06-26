76d810ee6659c4d3504556233fbdec69ea809865
DiscrapperManager <admin@minerpl.xyz>
2025-06-26 22:48:57 +0000

    canaryRelease: 413611
    Main scripts: 4
    Lazy Loaded Scripts: 1312
    Chunks:
     * Main Files: 11966
     * Scripts: 11329
    Strings: 21566
    Routes: 712
    Experiments Count: 421

---
+++ chunks/929255.js
@@ -0,0 +1,46 @@
+n.d(t, { Z: () => f });
+var r = n(255367),
+    l = n(120356),
+    a = n.n(l),
+    i = n(381585),
+    o = n(193227),
+    s = n(312030),
+    c = n(763891),
+    u = n(215023),
+    d = n(484920);
+let p = [u.AW.AVATAR_DECORATIONS, u.AW.PROFILE_EFFECTS, u.AW.NAMEPLATES, u.AW.BUNDLES];
+function f(e) {
+    let { isFullScreen: t, scrollerRef: n, tab: l, sortedCategories: u, setIsGiftEasterEggEnabled: f, isGiftEasterEggEnabled: b, setCategoryRef: g } = e,
+        { handlePageChange: h, currentPage: m } = (0, s.h)({
+            scrollerRef: n,
+            sortedCategories: u
+        });
+    return (0, r.jsx)('div', {
+        className: d.pageWrapper,
+        children: (0, r.jsx)('main', {
+            className: a()(d.page, { [d.pageFullscreen]: t }),
+            children: p.includes(l)
+                ? (0, r.jsx)(
+                      c.Z,
+                      {
+                          isFullScreen: t,
+                          scrollerRef: n,
+                          tab: l
+                      },
+                      l
+                  )
+                : (0, r.jsx)(i.k0, {
+                      newValue: { pageIndex: m },
+                      children: (0, r.jsx)(o.Z, {
+                          isFullScreen: t,
+                          sortedCategories: u,
+                          setCategoryRef: g,
+                          setIsGiftEasterEggEnabled: f,
+                          isGiftEasterEggEnabled: b,
+                          currentPage: m,
+                          handlePageChange: h
+                      })
+                  })
+        })
+    });
+}
