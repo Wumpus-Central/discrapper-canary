c8632ca0110740adc0b426e788aa9fc50804ccf2
DiscrapperManager <admin@minerpl.xyz>
2025-06-26 20:27:20 +0000

    canaryRelease: 413460
    Main scripts: 4
    Lazy Loaded Scripts: 1313
    Chunks:
     * Main Files: 11966
     * Scripts: 11336
    Strings: 21564
    Routes: 712
    Experiments Count: 426

--- chunks/857039.js
+++
@@ -1,9 +0,0 @@
-n.d(t, { Z: () => a });
-var r = n(987209),
-    i = n(599659);
-function a(e) {
-    let { location: t } = e,
-        { enabled: n } = i.O.useExperiment({ location: t }, { autoTrackExposure: !1 }),
-        { alreadyHasHalloweenDeco: a, isGift: o } = (0, r.wD)();
-    return n && o && !1 === a;
-}
