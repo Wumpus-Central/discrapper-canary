afaccc41409423a336f327cb162de628ca57dce3
DiscrapperManager <admin@minerpl.xyz>
2025-06-27 16:56:45 +0000

    canaryRelease: 413765
    Main scripts: 4
    Lazy Loaded Scripts: 1312
    Chunks:
     * Main Files: 11966
     * Scripts: 11321
    Strings: 21566
    Routes: 712
    Experiments Count: 421

--- chunks/192360.js
+++
@@ -1,8 +0,0 @@
-e.exports = {
-    nameInput: 'nameInput__38eb6',
-    wrapInput: 'wrapInput__38eb6',
-    blur: 'blur__38eb6',
-    ghost: 'ghost__38eb6',
-    bookend: 'bookend__38eb6',
-    spacer: 'spacer__38eb6'
-};
--- chunks/632261.js
+++
@@ -1,1 +0,0 @@
-e.exports = { errorBanner: 'errorBanner__3405b' };
