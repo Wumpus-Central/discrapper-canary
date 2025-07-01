69719bb48adfbcfd48ffdd088b9880898a38525f
DiscrapperManager <admin@minerpl.xyz>
2025-07-01 14:41:34 +0000

    canaryRelease: 414599
    Main scripts: 4
    Lazy Loaded Scripts: 1317
    Chunks:
     * Main Files: 11968
     * Scripts: 11327
    Strings: 21605
    Routes: 714
    Experiments Count: 422

---
+++ chunks/978056.js
@@ -0,0 +1,153 @@
+t.a(e, async function (e, r) {
+    try {
+        t.d(n, {
+            Kq: () => u,
+            ZP: () => s
+        });
+        var l = t(255367),
+            i = t(7301),
+            o = t(73800),
+            c = e([i]);
+        i = (c.then ? (await c)() : c)[0];
+        let f = (0, o.createContext)(null);
+        function u() {
+            return (0, o.useContext)(f);
+        }
+        let _ = new Set(['bold', 'italic', 'underline', 'strikethrough', 'spoiler', 'emoji', 'timestamp', 'mention', 'link', 'code', 'code_block', 'heading', 'list', 'quote', 'small']);
+        function a({ node: e, renderers: n, ...t }) {
+            let r = n[e.type];
+            if (
+                (!(function (e, n) {
+                    if (null == e) throw Error(`Attempted to render "${n}" but no renderer was provided`);
+                })(r, e.type),
+                Array.isArray(e.value))
+            )
+                return (0, l.jsx)(r, {
+                    node: e,
+                    ...t,
+                    children: (0, l.jsx)(d, {
+                        nodes: e.value,
+                        renderers: n
+                    })
+                });
+            switch (e.type) {
+                case 'mention':
+                case 'timestamp':
+                case 'emoji':
+                case 'code_block':
+                    return (0, l.jsx)(r, {
+                        node: e,
+                        ...e.value,
+                        ...t
+                    });
+                case 'link': {
+                    let i = e.value,
+                        o = null;
+                    if ('normal' === i.type) {
+                        let {
+                            value: { text: e, url: t }
+                        } = i;
+                        o = e
+                            ? (0, l.jsx)(d, {
+                                  nodes: e,
+                                  renderers: n
+                              })
+                            : t;
+                    }
+                    return (0, l.jsx)(r, {
+                        node: e,
+                        ...e.value,
+                        ...t,
+                        children: o
+                    });
+                }
+                case 'text':
+                case 'code':
+                    return (0, l.jsx)(r, {
+                        node: e,
+                        ...t,
+                        children: e.value
+                    });
+                case 'heading':
+                    return (0, l.jsx)(r, {
+                        node: e,
+                        level: e.value.level,
+                        ...t,
+                        children: (0, l.jsx)(d, {
+                            nodes: e.value.content,
+                            renderers: n
+                        })
+                    });
+                case 'list': {
+                    let i = n.listItem ?? 'li';
+                    return (0, l.jsx)(r, {
+                        node: e,
+                        ...e.value,
+                        ...t,
+                        children: e.value.items.map((t, r) =>
+                            (0, l.jsx)(
+                                i,
+                                {
+                                    node: t,
+                                    siblings: e.value.items,
+                                    index: r,
+                                    children: (0, l.jsx)(d, {
+                                        nodes: t.content,
+                                        renderers: n
+                                    })
+                                },
+                                r
+                            )
+                        )
+                    });
+                }
+                case 'empty':
+                    return (0, l.jsx)(r, { ...t });
+                case 'small':
+                    return (0, l.jsx)(r, {
+                        node: e,
+                        ...t,
+                        children: (0, l.jsx)(d, {
+                            nodes: e.value.content,
+                            renderers: n
+                        })
+                    });
+                default:
+                    throw TypeError(`Unknown node type "${e.type}"`);
+            }
+        }
+        function d({ nodes: e, renderers: n }) {
+            return (0, l.jsx)(l.Fragment, {
+                children: e.map((t, r) =>
+                    (0, l.jsx)(
+                        a,
+                        {
+                            node: t,
+                            renderers: n,
+                            siblings: e,
+                            index: r
+                        },
+                        r
+                    )
+                )
+            });
+        }
+        function s({ content: e, renderers: n }) {
+            let t = (0, o.useMemo)(() => {
+                    let e = Object.keys(n).filter((e) => _.has(e));
+                    return e.length > 0 ? e : null;
+                }, [n]),
+                r = (0, o.useMemo)(() => (0, i.Q)(e, t), [e, t]);
+            return (0, l.jsx)(f.Provider, {
+                value: r,
+                children: (0, l.jsx)(d, {
+                    nodes: r,
+                    renderers: n
+                })
+            });
+        }
+        r();
+    } catch (e) {
+        r(e);
+    }
+});
