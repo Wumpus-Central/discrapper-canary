d1ef17549bba992a8f832910c791f2d150a6aff7
DiscrapperManager <admin@minerpl.xyz>
2025-07-01 16:43:33 +0000

    canaryRelease: 414648
    Main scripts: 4
    Lazy Loaded Scripts: 1317
    Chunks:
     * Main Files: 11968
     * Scripts: 11329
    Strings: 21606
    Routes: 714
    Experiments Count: 422

---
+++ chunks/794231.js
@@ -0,0 +1,14 @@
+n.d(t, { Z: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-07_virtual_currency_beta_tag',
+    label: 'Virtual Currency Beta Tag',
+    defaultConfig: { showBetaTag: !0 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Disable Beta Tag on Virtual Currency surfaces',
+            config: { showBetaTag: !1 }
+        }
+    ]
+});
