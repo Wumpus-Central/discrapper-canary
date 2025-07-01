4d35a9dc9ed8263c3543c065b6c6b3d1dc8d1c12
DiscrapperManager <admin@minerpl.xyz>
2025-07-01 19:53:41 +0000

    canaryRelease: 414824
    Main scripts: 4
    Lazy Loaded Scripts: 1317
    Chunks:
     * Main Files: 11969
     * Scripts: 11333
    Strings: 21609
    Routes: 714
    Experiments Count: 423

---
+++ chunks/133340.js
@@ -0,0 +1,17 @@
+n.d(t, { t: () => l });
+var r = n(818083),
+    i = n(987338);
+let l = (0, r.B)({
+    kind: 'user',
+    id: '2025-07_cloud_play_cta',
+    commonTriggerPoint: i.$P.CONNECTION_OPEN,
+    label: 'Cloud Play CTA Experiment',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enable Cloud Play CTA',
+            config: { enabled: !0 }
+        }
+    ]
+});
