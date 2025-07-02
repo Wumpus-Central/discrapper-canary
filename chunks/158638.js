42be75c7247e870dc0e55732da20705c78d8744d
DiscrapperManager <admin@minerpl.xyz>
2025-07-02 00:47:31 +0000

    canaryRelease: 415040
    Main scripts: 4
    Lazy Loaded Scripts: 1317
    Chunks:
     * Main Files: 11970
     * Scripts: 11335
    Strings: 21636
    Routes: 714
    Experiments Count: 424

---
+++ chunks/158638.js
@@ -0,0 +1,16 @@
+n.d(t, { s: () => i });
+var r = n(818083);
+(n(981631), n(647086));
+let i = (0, r.B)({
+    kind: 'guild',
+    id: '2025-06_vanity_url_perk',
+    label: 'Vanity URL Standalone Perk',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enable ability to purchase Vanity URL Standalone Perk',
+            config: { enabled: !0 }
+        }
+    ]
+});
