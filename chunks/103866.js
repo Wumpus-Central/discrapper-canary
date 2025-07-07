cadc4accefbbf9702a1cf4bc0ba8771330b0138f
DiscrapperManager <admin@minerpl.xyz>
2025-07-07 22:42:08 +0000

    canaryRelease: 416273
    Main scripts: 4
    Lazy Loaded Scripts: 1319
    Chunks:
     * Main Files: 11981
     * Scripts: 11388
    Strings: 21689
    Routes: 715
    Experiments Count: 424

---
+++ chunks/103866.js
@@ -0,0 +1,55 @@
+(n.d(t, { f: () => d }), n(388685));
+var r = n(255367),
+    i = n(73800),
+    a = n(120356),
+    o = n.n(a),
+    s = n(392711),
+    l = n(793030),
+    c = n(147479),
+    u = n(946598);
+function d(e) {
+    let { controls: t, children: n } = e,
+        [a, d] = i.useState(!1),
+        [f, _] = i.useState(!1),
+        p = i.useRef(null),
+        h = i.useRef(null),
+        m = (0, s.throttle)(() => {
+            var e;
+            (null == p ? void 0 : p.current) != null && null != t && d((null == (e = p.current) ? void 0 : e.getDistanceFromTop()) > 12);
+        }, 200);
+    return (i.useLayoutEffect(() => {
+        var e, t;
+        let n = new ResizeObserver(() => {
+                var e, t;
+                let n = null != (t = null == p || null == (e = p.current) ? void 0 : e.getScrollerNode()) ? t : null;
+                if (null == n || (null == h ? void 0 : h.current) == null) return;
+                let r = n.getBoundingClientRect();
+                _(h.current.getBoundingClientRect().height > r.height);
+            }),
+            r = null != (t = null == p || null == (e = p.current) ? void 0 : e.getScrollerNode()) ? t : null;
+        return (null != r && n.observe(r), (null == h ? void 0 : h.current) != null && n.observe(h.current), () => n.disconnect());
+    }, []),
+    null == n && null == t)
+        ? null
+        : (0, r.jsxs)(l.y5, {
+              children: [
+                  (0, r.jsx)('div', { className: o()(u.bodySpacerTop, { [u.bodySpacerTopBorder]: f && null == t }) }),
+                  null != t &&
+                      (0, r.jsx)('header', {
+                          className: o()(u.bodyControls, { [u.bodyControlsWithFade]: a }),
+                          children: t
+                      }),
+                  (0, r.jsx)(c.yW, {
+                      ref: p,
+                      className: u.body,
+                      onScroll: m,
+                      children: (0, r.jsx)('main', {
+                          ref: h,
+                          className: o()(u.bodyInner, { [u.bodyInnerShouldScroll]: f }),
+                          children: n
+                      })
+                  }),
+                  (0, r.jsx)('div', { className: o()(u.bodySpacerBottom, { [u.bodySpacerBottomBorder]: f }) })
+              ]
+          });
+}
---
+++ chunks/213734.js
@@ -0,0 +1,15 @@
+n.d(t, { m: () => s });
+var r = n(255367);
+n(73800);
+var i = n(120356),
+    a = n.n(i),
+    o = n(946598);
+function s(e) {
+    let { children: t } = e;
+    return null == t
+        ? null
+        : (0, r.jsx)('footer', {
+              className: a()(o.footer, o.section),
+              children: t
+          });
+}
