aad3b32053937acd1f53cc417ea53ddd992e35c7
DiscrapperManager <admin@minerpl.xyz>
2025-07-08 17:52:46 +0000

    canaryRelease: 416509
    Main scripts: 4
    Lazy Loaded Scripts: 1319
    Chunks:
     * Main Files: 11981
     * Scripts: 11390
    Strings: 21691
    Routes: 715
    Experiments Count: 426

---
+++ chunks/906734.js
@@ -0,0 +1,17 @@
+n.d(t, { A: () => l });
+var r = n(818083),
+    i = n(987338);
+let l = (0, r.B)({
+    id: '2025-06_display_name_styles',
+    kind: 'user',
+    label: 'Display Name Styles',
+    defaultConfig: { enabled: !1 },
+    commonTriggerPoint: i.$P.CONNECTION_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: 'Enables the display name styles Nitro perk.',
+            config: { enabled: !0 }
+        }
+    ]
+});
