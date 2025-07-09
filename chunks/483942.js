00358cd938c152e9cffff482dfae4de2644676e9
DiscrapperManager <admin@minerpl.xyz>
2025-07-09 20:28:08 +0000

    canaryRelease: 417199
    Main scripts: 4
    Lazy Loaded Scripts: 1351
    Chunks:
     * Main Files: 11993
     * Scripts: 11420
    Strings: 21720
    Routes: 715
    Experiments Count: 450

---
+++ chunks/483942.js
@@ -0,0 +1,401 @@
+n.d(t, {
+    Aq: () => O,
+    De: () => d,
+    Eo: () => h,
+    Fe: () => I,
+    I1: () => m,
+    J9: () => c,
+    Jg: () => T,
+    L$: () => s,
+    OP: () => R,
+    Pv: () => f,
+    Qj: () => p,
+    d3: () => N,
+    f5: () => u,
+    f7: () => v,
+    kF: () => E,
+    kS: () => b,
+    l2: () => o,
+    lE: () => g,
+    mq: () => l,
+    nS: () => C,
+    ur: () => _,
+    xs: () => A,
+    yR: () => y,
+    zk: () => S
+});
+var r = n(722733),
+    i = n(987170),
+    a = n(987338);
+let o = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__connectionopen_simple',
+        kind: 'user',
+        label: 'Apex AA Validation Connection Open Simple',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.CONNECTION_OPEN
+    }),
+    s = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__connectionopen_hqdtesters',
+        kind: 'user',
+        label: 'Apex AA Validation Connection Open HQDTesters',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.CONNECTION_OPEN
+    }),
+    l = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__connectionopen_android',
+        kind: 'user',
+        label: 'Apex AA Validation Connection Open Android Only',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.CONNECTION_OPEN
+    }),
+    c = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__connectionopen_ios',
+        kind: 'user',
+        label: 'Apex AA Validation Connection Open iOS Only',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.CONNECTION_OPEN
+    }),
+    u = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__connectionopen_userlocale',
+        kind: 'user',
+        label: 'Apex AA Validation Connection Open User Locale',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.CONNECTION_OPEN
+    }),
+    d = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__connectionopen_usercountry',
+        kind: 'user',
+        label: 'Apex AA Validation Connection Open User Country',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.CONNECTION_OPEN
+    }),
+    f = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__voicecall_simple',
+        kind: 'user',
+        label: 'Apex AA Validation Voice Call Simple',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.VOICE_CALL
+    }),
+    _ = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__voicecall_hqdtesters',
+        kind: 'user',
+        label: 'Apex AA Validation Voice Call HQDTesters',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.VOICE_CALL
+    }),
+    p = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__voicecall_android',
+        kind: 'user',
+        label: 'Apex AA Validation Voice Call Android Only',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.VOICE_CALL
+    }),
+    h = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__voicecall_ios',
+        kind: 'user',
+        label: 'Apex AA Validation Voice Call iOS Only',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.VOICE_CALL
+    }),
+    m = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__voicecall_userlocale',
+        kind: 'user',
+        label: 'Apex AA Validation Voice Call User Locale',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.VOICE_CALL
+    }),
+    g = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__voicecall_usercountry',
+        kind: 'user',
+        label: 'Apex AA Validation Voice Call User Country',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.VOICE_CALL
+    }),
+    E = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__paymentflowstarted_simple',
+        kind: 'user',
+        label: 'Apex AA Validation Payment Flow Started Simple',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.PAYMENT_FLOW_STARTED
+    }),
+    b = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__paymentflowstarted_hqdtesters',
+        kind: 'user',
+        label: 'Apex AA Validation Payment Flow Started HQDTesters',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.PAYMENT_FLOW_STARTED
+    }),
+    y = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__paymentflowstarted_android',
+        kind: 'user',
+        label: 'Apex AA Validation Payment Flow Started Android Only',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.PAYMENT_FLOW_STARTED
+    }),
+    O = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__paymentflowstarted_ios',
+        kind: 'user',
+        label: 'Apex AA Validation Payment Flow Started iOS Only',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.PAYMENT_FLOW_STARTED
+    }),
+    v = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__paymentflowstarted_userlocale',
+        kind: 'user',
+        label: 'Apex AA Validation Payment Flow Started User Locale',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.PAYMENT_FLOW_STARTED
+    }),
+    I = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__paymentflowstarted_usercountry',
+        kind: 'user',
+        label: 'Apex AA Validation Payment Flow Started User Country',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.PAYMENT_FLOW_STARTED
+    }),
+    T = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__collectiblesshopopen_simple',
+        kind: 'user',
+        label: 'Apex AA Validation Collectibles Shop Open Simple',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.COLLECTIBLES_SHOP_OPEN
+    }),
+    S = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__collectiblesshopopen_hqdtesters',
+        kind: 'user',
+        label: 'Apex AA Validation Collectibles Shop Open HQDTesters',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.COLLECTIBLES_SHOP_OPEN
+    }),
+    A = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__collectiblesshopopen_android',
+        kind: 'user',
+        label: 'Apex AA Validation Collectibles Shop Open Android Only',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.COLLECTIBLES_SHOP_OPEN
+    }),
+    N = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__collectiblesshopopen_ios',
+        kind: 'user',
+        label: 'Apex AA Validation Collectibles Shop Open iOS Only',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.COLLECTIBLES_SHOP_OPEN
+    }),
+    C = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__collectiblesshopopen_userlocale',
+        kind: 'user',
+        label: 'Apex AA Validation Collectibles Shop Open User Locale',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.COLLECTIBLES_SHOP_OPEN
+    }),
+    R = (0, i.Z)({
+        id: '2025-07_apex_aa_validation__clientside__collectiblesshopopen_usercountry',
+        kind: 'user',
+        label: 'Apex AA Validation Collectibles Shop Open User Country',
+        defaultConfig: { isEnabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'AA test enabled. Noop on UI effect.',
+                config: { isEnabled: !0 }
+            }
+        ],
+        commonTriggerPoint: a.$P.COLLECTIBLES_SHOP_OPEN
+    });
+((0, r.Z)({
+    name: '2025-07-apex-aa-validation--clientside--simple',
+    kind: 'user',
+    defaultConfig: { isEnabled: !1 },
+    variations: { 0: { isEnabled: !0 } }
+}),
+    (0, r.Z)({
+        name: '2025-07-apex-aa-validation--clientside--hqdtesters',
+        kind: 'user',
+        defaultConfig: { isEnabled: !1 },
+        variations: { 0: { isEnabled: !0 } }
+    }),
+    (0, r.Z)({
+        name: '2025-07-apex-aa-validation--clientside--android',
+        kind: 'user',
+        defaultConfig: { isEnabled: !1 },
+        variations: { 0: { isEnabled: !0 } }
+    }),
+    (0, r.Z)({
+        name: '2025-07-apex-aa-validation--clientside--ios',
+        kind: 'user',
+        defaultConfig: { isEnabled: !1 },
+        variations: { 0: { isEnabled: !0 } }
+    }),
+    (0, r.Z)({
+        name: '2025-07-apex-aa-validation--clientside--userlocale',
+        kind: 'user',
+        defaultConfig: { isEnabled: !1 },
+        variations: { 0: { isEnabled: !0 } }
+    }),
+    (0, r.Z)({
+        name: '2025-07-apex-aa-validation--clientside--usercountry',
+        kind: 'user',
+        defaultConfig: { isEnabled: !1 },
+        variations: { 0: { isEnabled: !0 } }
+    }));
