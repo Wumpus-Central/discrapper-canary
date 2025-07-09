aa924a85ea42516a07807d306756e8975c27f889
DiscrapperManager <admin@minerpl.xyz>
2025-07-09 23:13:26 +0000

    canaryRelease: 417371
    Main scripts: 4
    Lazy Loaded Scripts: 1351
    Chunks:
     * Main Files: 11999
     * Scripts: 11430
    Strings: 21724
    Routes: 715
    Experiments Count: 452

---
+++ chunks/501638.js
@@ -0,0 +1,25 @@
+n.d(t, { Z: () => i });
+var r = n(255367),
+    l = n(481060),
+    o = n(388032),
+    a = n(961933);
+function i() {
+    return (0, r.jsxs)('div', {
+        className: a.container,
+        children: [
+            (0, r.jsx)('img', {
+                src: 'https://cdn.discordapp.com/assets/content/a72233587aaf964fc327663677974641a235719ad6445da58f931094cb799f66.png',
+                alt: o.intl.string(o.t.oezC39),
+                className: a.img
+            }),
+            (0, r.jsx)(l.X6q, {
+                variant: 'heading-xl/semibold',
+                children: o.intl.string(o.t.oezC39)
+            }),
+            (0, r.jsx)(l.Text, {
+                variant: 'text-md/medium',
+                children: o.intl.string(o.t['Tc/Ndn'])
+            })
+        ]
+    });
+}
---
+++ chunks/961933.js
@@ -0,0 +1,4 @@
+e.exports = {
+    container: 'container__8b924',
+    img: 'img__8b924'
+};
