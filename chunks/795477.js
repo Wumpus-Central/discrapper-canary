7b0310313f04af8b0e03babf89d1c60058c1a041
DiscrapperManager <admin@minerpl.xyz>
2025-07-09 22:11:26 +0000

    canaryRelease: 417313
    Main scripts: 4
    Lazy Loaded Scripts: 1351
    Chunks:
     * Main Files: 11998
     * Scripts: 11428
    Strings: 21722
    Routes: 715
    Experiments Count: 450

---
+++ chunks/795477.js
@@ -0,0 +1,24 @@
+n.d(t, { Z: () => i });
+var r = n(255367),
+    l = n(481060),
+    o = n(388032),
+    a = n(319471);
+let i = () =>
+    (0, r.jsxs)('div', {
+        className: a.error,
+        children: [
+            (0, r.jsx)('img', {
+                className: a.errorImage,
+                src: 'https://cdn.discordapp.com/assets/content/ca0857da281051f734229e1994112aaa95b21d6f7fce7a1e509357d94c58a949.png',
+                alt: o.intl.string(o.t['p8+qtb'])
+            }),
+            (0, r.jsx)(l.X6q, {
+                variant: 'heading-xl/semibold',
+                children: o.intl.string(o.t['p8+qtb'])
+            }),
+            (0, r.jsx)(l.Text, {
+                variant: 'text-md/medium',
+                children: o.intl.string(o.t.UEiyvr)
+            })
+        ]
+    });
