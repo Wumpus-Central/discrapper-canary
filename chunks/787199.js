7e79095ab92926e2bbbcff056289647134bba0c9
DiscrapperManager <admin@minerpl.xyz>
2025-07-11 23:06:16 +0000

    canaryRelease: 418474
    Main scripts: 4
    Lazy Loaded Scripts: 1352
    Chunks:
     * Main Files: 12036
     * Scripts: 11462
    Strings: 21736
    Routes: 718
    Experiments Count: 456
    Apex Experiments Count: 10

---
+++ chunks/787199.js
@@ -0,0 +1,14 @@
+n.d(t, { Z: () => r });
+let r = (0, n(818083).B)({
+    kind: 'guild',
+    id: '2025-07_redundant_audio_coding',
+    label: 'Redundant Audio Coding',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enable Redundant Audio Coding (RED)',
+            config: { enabled: !0 }
+        }
+    ]
+});
