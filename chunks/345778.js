1cb638f6a2620f6eeac040a40e10ff989e99de3f
DiscrapperManager <admin@minerpl.xyz>
2025-07-11 23:15:49 +0000

    canaryRelease: 418477
    Main scripts: 4
    Lazy Loaded Scripts: 1352
    Chunks:
     * Main Files: 12036
     * Scripts: 11463
    Strings: 21736
    Routes: 718
    Experiments Count: 457
    Apex Experiments Count: 10

---
+++ chunks/345778.js
@@ -0,0 +1,17 @@
+n.d(t, { R: () => o });
+var r = n(818083),
+    l = n(987338);
+let o = (0, r.B)({
+    kind: 'user',
+    id: '2025-07_mobile_shop_featured_page',
+    label: 'Mobile Shop Featured Page',
+    defaultConfig: { showMobileShopFeaturedPage: !1 },
+    commonTriggerPoint: l.$P.COLLECTIBLES_SHOP_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: 'Use mobile shop featured page',
+            config: { showMobileShopFeaturedPage: !0 }
+        }
+    ]
+});
