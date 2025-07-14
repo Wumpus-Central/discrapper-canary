f72672e16ccdbd0033c99b2a48ca9455bffa9939
DiscrapperManager <admin@minerpl.xyz>
2025-07-14 17:20:33 +0000

    canaryRelease: 418651
    Main scripts: 4
    Lazy Loaded Scripts: 1352
    Chunks:
     * Main Files: 12055
     * Scripts: 11476
    Strings: 21745
    Routes: 718
    Experiments Count: 458
    Apex Experiments Count: 10

---
+++ chunks/979283.js
@@ -0,0 +1,18 @@
+n.d(t, { O: () => i });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-07_camera_toggle_sound',
+    label: 'Camera Toggle Sound',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Sound plays when camera is toggled',
+            config: { enabled: !0 }
+        }
+    ]
+});
+function i() {
+    let { enabled: e } = r.getCurrentConfig({ location: 'soundpacks' }, { autoTrackExposure: !0 });
+    return e;
+}
