0e0ce1b3d8cbd5cee280f9037447bf74ad023f17
DiscrapperManager <admin@minerpl.xyz>
2025-07-14 20:30:24 +0000

    canaryRelease: 418742
    Main scripts: 4
    Lazy Loaded Scripts: 1352
    Chunks:
     * Main Files: 12056
     * Scripts: 11477
    Strings: 21749
    Routes: 718
    Experiments Count: 458
    Apex Experiments Count: 10

---
+++ chunks/725739.js
@@ -0,0 +1,9 @@
+n.d(t, { Z: () => l });
+var r = n(97519),
+    i = n(982183);
+let l = (0, r.U)((e) => ({
+    selectedFilter: i.V5.ALL,
+    setSelectedFilter: (t) => {
+        e({ selectedFilter: t });
+    }
+}));
