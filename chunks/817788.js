6f976fc76c3b9fc6b4d70fc7e357f201870f73eb
DiscrapperManager <admin@minerpl.xyz>
2025-07-15 19:36:56 +0000

    canaryRelease: 419266
    Main scripts: 4
    Lazy Loaded Scripts: 1352
    Chunks:
     * Main Files: 12070
     * Scripts: 11498
    Strings: 21774
    Routes: 722
    Experiments Count: 460
    Apex Experiments Count: 11

---
+++ chunks/817788.js
@@ -0,0 +1,11 @@
+n.d(t, {
+    SQ: () => i,
+    eB: () => r
+});
+let r = '363445589247131668',
+    i = {
+        UNIVERSE_ID: 'universeid',
+        PLACE_ID: 'placeid',
+        JOB_ID: 'sid',
+        USER_ID: 'userid'
+    };
