f902f22329d333cf8157e4ab1ebda8b58f66190c
DiscrapperManager <admin@minerpl.xyz>
2025-07-16 22:42:40 +0000

    canaryRelease: 419907
    Main scripts: 4
    Lazy Loaded Scripts: 1348
    Chunks:
     * Main Files: 12085
     * Scripts: 11486
    Strings: 21792
    Routes: 723
    Experiments Count: 464
    Apex Experiments Count: 15

---
+++ chunks/384278.js
@@ -0,0 +1,76 @@
+n.d(t, { M1: () => c });
+var r = n(70956),
+    i = n(818083),
+    s = n(427164),
+    l = n(987338);
+let o = (0, s.le)({
+        kind: 'user',
+        name: '2025-07-typing-indicator-delay',
+        defaultConfig: {
+            delayMs: 1.5 * r.Z.Millis.SECOND,
+            inExperiment: !1
+        },
+        variations: {
+            0: {
+                delayMs: 1.5 * r.Z.Millis.SECOND,
+                inExperiment: !0
+            },
+            1: {
+                delayMs: 2 * r.Z.Millis.SECOND,
+                inExperiment: !0
+            },
+            2: {
+                delayMs: 3 * r.Z.Millis.SECOND,
+                inExperiment: !0
+            }
+        }
+    }),
+    a = (0, i.B)({
+        kind: 'user',
+        id: '2025-07_typing_indicator_delay',
+        label: 'Typing Indicator Delay',
+        defaultConfig: {
+            delayMs: 1.5 * r.Z.Millis.SECOND,
+            inExperiment: !1
+        },
+        treatments: [
+            {
+                id: 0,
+                label: '1.5 second delay',
+                config: {
+                    delayMs: 1.5 * r.Z.Millis.SECOND,
+                    inExperiment: !0
+                }
+            },
+            {
+                id: 1,
+                label: '2 second delay',
+                config: {
+                    delayMs: 2 * r.Z.Millis.SECOND,
+                    inExperiment: !0
+                }
+            },
+            {
+                id: 2,
+                label: '3 second delay',
+                config: {
+                    delayMs: 3 * r.Z.Millis.SECOND,
+                    inExperiment: !0
+                }
+            }
+        ],
+        commonTriggerPoint: l.$P.CONNECTION_OPEN
+    }),
+    c = (e) => {
+        let { delayMs: t, inExperiment: n } = a.getCurrentConfig({ location: e });
+        if (n)
+            return {
+                delayMs: t,
+                inExperiment: !0
+            };
+        let { delayMs: r, inExperiment: i } = o.getConfig({ location: e });
+        return {
+            delayMs: r,
+            inExperiment: i
+        };
+    };
