383e46ae47516880ad0caa20146153ba08a7ef6e
DiscrapperManager <admin@minerpl.xyz>
2025-07-16 20:42:01 +0000

    canaryRelease: 419825
    Main scripts: 4
    Lazy Loaded Scripts: 1351
    Chunks:
     * Main Files: 12085
     * Scripts: 11497
    Strings: 21796
    Routes: 723
    Experiments Count: 463
    Apex Experiments Count: 14

---
+++ chunks/47840.js
@@ -0,0 +1,47 @@
+n.d(t, { Z: () => u });
+var i = n(255367);
+n(73800);
+var r = n(71347),
+    s = n(481060),
+    a = n(117791),
+    l = n(740594),
+    o = n(70466),
+    c = n(981631),
+    d = n(700109);
+let u = function (e) {
+    let { isVisible: t, subscriptionTier: n, isEligibleForBogoPromotion: u } = e,
+        m = (0, s.q_F)({
+            transform: t ? 'translateY(-100%)' : 'translateY(0%)',
+            opacity: +!!t,
+            config: {
+                tension: 120,
+                friction: 12
+            }
+        }),
+        p = { section: c.jXE.MARKETING_FLOATING_CTA },
+        { buttonText: g, marketingSubscriptionTierSkuId: h } = (0, o.G)({
+            subscriptionTier: n,
+            hasActivePromotion: !!u,
+            isPersistentCTA: !0
+        });
+    return (0, i.jsx)(r.animated.div, {
+        className: d.wrapper,
+        style: m,
+        children: (0, i.jsxs)('div', {
+            className: d.buttonContainer,
+            children: [
+                (0, i.jsx)(l.Z, {
+                    variant: 'expressive',
+                    size: 'md',
+                    buttonTextOverride: g,
+                    subscriptionTier: h,
+                    premiumModalAnalyticsLocation: p
+                }),
+                (0, i.jsx)(a.Z, {
+                    variant: 'secondary',
+                    size: 'md'
+                })
+            ]
+        })
+    });
+};
