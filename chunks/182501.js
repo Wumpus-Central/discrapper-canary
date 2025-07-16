6659d03daf841431ed3f11f80a1513196162c850
DiscrapperManager <admin@minerpl.xyz>
2025-07-16 18:20:46 +0000

    canaryRelease: 419717
    Main scripts: 4
    Lazy Loaded Scripts: 1351
    Chunks:
     * Main Files: 12086
     * Scripts: 11489
    Strings: 21789
    Routes: 723
    Experiments Count: 464
    Apex Experiments Count: 13

---
+++ chunks/182501.js
@@ -0,0 +1,83 @@
+n.d(t, { Kr: () => s });
+var r = n(987170),
+    i = n(722733);
+let a = (0, r.Z)({
+        id: '2025-07_gif_picker_delay',
+        kind: 'user',
+        label: 'GIF Picker Delay',
+        defaultConfig: {
+            enabled: !1,
+            delayMs: 0,
+            inExperiment: !1
+        },
+        treatments: [
+            {
+                id: 0,
+                label: 'Control',
+                config: {
+                    enabled: !1,
+                    delayMs: 0,
+                    inExperiment: !0
+                }
+            },
+            {
+                id: 1,
+                label: 'Delay 100 ms',
+                config: {
+                    enabled: !0,
+                    delayMs: 100,
+                    inExperiment: !0
+                }
+            },
+            {
+                id: 2,
+                label: 'Delay 200 ms',
+                config: {
+                    enabled: !0,
+                    delayMs: 200,
+                    inExperiment: !0
+                }
+            }
+        ]
+    }),
+    o = (0, i.Z)({
+        name: '2025-07-gif-picker-delay',
+        kind: 'user',
+        defaultConfig: {
+            enabled: !1,
+            delayMs: 0,
+            inExperiment: !1
+        },
+        variations: {
+            0: {
+                enabled: !1,
+                delayMs: 0,
+                inExperiment: !0
+            },
+            1: {
+                enabled: !0,
+                delayMs: 100,
+                inExperiment: !0
+            },
+            2: {
+                enabled: !0,
+                delayMs: 200,
+                inExperiment: !0
+            }
+        }
+    }),
+    s = (e) => {
+        let { enabled: t, delayMs: n, inExperiment: r } = a.getCurrentConfig({ location: e });
+        if (r)
+            return {
+                enabled: t,
+                delayMs: n,
+                inExperiment: !0
+            };
+        let { enabled: i, delayMs: s, inExperiment: l } = o.getConfig({ location: e });
+        return {
+            enabled: i,
+            delayMs: s,
+            inExperiment: l
+        };
+    };
