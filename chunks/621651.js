3c19c445295ca1e099a333cce509ae61dd2c3b84
DiscrapperManager <admin@minerpl.xyz>
2025-07-17 19:04:37 +0000

    canaryRelease: 420170
    Main scripts: 4
    Lazy Loaded Scripts: 1347
    Chunks:
     * Main Files: 12087
     * Scripts: 11484
    Strings: 21796
    Routes: 723
    Experiments Count: 466
    Apex Experiments Count: 15

---
+++ chunks/621651.js
@@ -0,0 +1,19 @@
+n.d(t, { Z: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-07_calculated_yearly_discount_percentage',
+    label: 'Calculated Yearly Discount Percentage Experiment',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 0,
+            label: 'Use existing yearly discount percentage',
+            config: { enabled: !1 }
+        },
+        {
+            id: 1,
+            label: 'Use calculated yearly discount percentage',
+            config: { enabled: !0 }
+        }
+    ]
+});
