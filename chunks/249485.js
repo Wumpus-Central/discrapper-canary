c4c9a18251ec07ea4e7bd73d1e504f26b461f68d
DiscrapperManager <admin@minerpl.xyz>
2025-07-18 00:50:37 +0000

    canaryRelease: 420386
    Main scripts: 4
    Lazy Loaded Scripts: 1347
    Chunks:
     * Main Files: 12085
     * Scripts: 11476
    Strings: 21796
    Routes: 722
    Experiments Count: 466
    Apex Experiments Count: 15

---
+++ chunks/249485.js
@@ -0,0 +1,23 @@
+n.d(t, { _: () => l });
+let r = (0, n(818083).B)({
+        id: '2025-07_video_quest_debug_gate',
+        kind: 'user',
+        label: 'Video Quest Debug Gate',
+        defaultConfig: { enabled: !1 },
+        treatments: [
+            {
+                id: 0,
+                label: 'Control',
+                config: { enabled: !1 }
+            },
+            {
+                id: 1,
+                label: 'Enabled',
+                config: { enabled: !0 }
+            }
+        ]
+    }),
+    l = (e) => {
+        let { location: t } = e;
+        return r.useExperiment({ location: t }, { autoTrackExposure: !1 }).enabled;
+    };
