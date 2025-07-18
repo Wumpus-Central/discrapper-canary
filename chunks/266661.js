b1d20b4951254599fd2964865c16854cfa3480ba
DiscrapperManager <admin@minerpl.xyz>
2025-07-18 23:59:44 +0000

    canaryRelease: 420870
    Main scripts: 4
    Lazy Loaded Scripts: 1351
    Chunks:
     * Main Files: 12095
     * Scripts: 11477
    Strings: 21803
    Routes: 722
    Experiments Count: 468
    Apex Experiments Count: 15

---
+++ chunks/266661.js
@@ -0,0 +1,70 @@
+n.d(t, { M: () => u });
+var r = n(818083),
+    i = n(987338);
+function a(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
+function o(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        ('function' == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                })
+            )),
+            r.forEach(function (t) {
+                a(e, t, n[t]);
+            }));
+    }
+    return e;
+}
+function s(e, t) {
+    var n = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+        var r = Object.getOwnPropertySymbols(e);
+        (t &&
+            (r = r.filter(function (t) {
+                return Object.getOwnPropertyDescriptor(e, t).enumerable;
+            })),
+            n.push.apply(n, r));
+    }
+    return n;
+}
+function l(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : s(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+let c = { disableKrispVAD: !1 },
+    u = (0, r.B)({
+        kind: 'user',
+        id: '2025-07_krisp_vad_negative_test',
+        label: 'Krisp VAD Negative Experiment',
+        commonTriggerPoint: i.$P.VOICE_CALL,
+        defaultConfig: c,
+        treatments: [
+            {
+                id: 1,
+                label: 'Disable Krisp VAD',
+                config: l(o({}, c), { disableKrispVAD: !0 })
+            }
+        ]
+    });
