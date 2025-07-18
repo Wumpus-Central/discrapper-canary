e5d9fc066c0f896a0b1b4c9c37d4bf2293d3e3ad
DiscrapperManager <admin@minerpl.xyz>
2025-07-18 18:38:03 +0000

    canaryRelease: 420602
    Main scripts: 4
    Lazy Loaded Scripts: 1349
    Chunks:
     * Main Files: 12094
     * Scripts: 11489
    Strings: 21800
    Routes: 722
    Experiments Count: 468
    Apex Experiments Count: 15

--- chunks/554549.js
+++
@@ -1,7 +0,0 @@
-n.d(t, { Z: () => l });
-var r = n(442837),
-    i = n(271383);
-function l(e, t) {
-    let n = (0, r.e7)([i.ZP], () => (null != e ? i.ZP.getMember(t, e) : void 0), [e, t]);
-    return (null == n ? void 0 : n.premiumSince) == null;
-}
---
+++ chunks/761700.js
@@ -0,0 +1,14 @@
+n.d(t, { Z: () => s });
+var r = n(442837),
+    i = n(271383),
+    l = n(819640),
+    o = n(232062);
+function s(e, t) {
+    let n = (function (e, t) {
+            let n = (0, r.e7)([i.ZP], () => (null != e ? i.ZP.getMember(t, e) : void 0), [e, t]);
+            return (null == n ? void 0 : n.premiumSince) == null;
+        })(e, t),
+        s = (0, o.W)(t, 'useShowErsForBoostersCoachmark'),
+        a = (0, r.e7)([l.Z], () => l.Z.hasLayers());
+    return n && s && !a;
+}
