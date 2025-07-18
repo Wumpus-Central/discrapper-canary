5513eef259783dfca642ee4af48f354969bd03b8
DiscrapperManager <admin@minerpl.xyz>
2025-07-18 19:25:18 +0000

    canaryRelease: 420647
    Main scripts: 4
    Lazy Loaded Scripts: 1349
    Chunks:
     * Main Files: 12093
     * Scripts: 11475
    Strings: 21787
    Routes: 722
    Experiments Count: 467
    Apex Experiments Count: 15

---
+++ chunks/984802.js
@@ -0,0 +1,26 @@
+n.d(t, {
+    A6: () => d,
+    Ll: () => f,
+    Zp: () => u
+});
+var r = n(442837),
+    i = n(780384),
+    a = n(514361),
+    o = n(210887),
+    s = n(740492),
+    l = n(981631);
+function c() {
+    let e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : o.Z,
+        { darkSidebar: t } = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : s.ZP,
+        { gradientPreset: n } = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : a.Z;
+    return (0, i.ap)(e.theme) && t && null == n;
+}
+function u() {
+    return c();
+}
+function d() {
+    return (0, r.e7)([o.Z, s.ZP, a.Z], () => c(o.Z, s.ZP, a.Z));
+}
+function f() {
+    return d() ? l.BRd.DARK : void 0;
+}
