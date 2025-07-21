68845cc1cc42cb77bab81f934377b3c03df2093d
DiscrapperManager <admin@minerpl.xyz>
2025-07-21 18:56:50 +0000

    canaryRelease: 421125
    Main scripts: 4
    Lazy Loaded Scripts: 1351
    Chunks:
     * Main Files: 12092
     * Scripts: 11477
    Strings: 21809
    Routes: 722
    Experiments Count: 467
    Apex Experiments Count: 15

--- chunks/1899.js
+++
@@ -1,1 +0,0 @@
-e.exports = { pictureInPictureVideo: 'pictureInPictureVideo__29048' };
---
+++ chunks/522501.js
@@ -0,0 +1,9 @@
+n.d(t, {
+    ZP: () => c,
+    _X: () => l,
+    mK: () => s
+});
+let { Layer: r, LayerContainer: i, LayerProvider: a, layerContext: o } = (0, n(302901).Xv)('PictureInPicture'),
+    s = i,
+    l = a,
+    c = r;
