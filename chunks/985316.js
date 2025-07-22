0dee264fd570ee7ae64f8ea811942d8972d8eb3b
DiscrapperManager <admin@minerpl.xyz>
2025-07-22 20:59:40 +0000

    canaryRelease: 421704
    Main scripts: 4
    Lazy Loaded Scripts: 1381
    Chunks:
     * Main Files: 12093
     * Scripts: 11508
    Strings: 21836
    Routes: 721
    Experiments Count: 464
    Apex Experiments Count: 15

---
+++ chunks/985316.js
@@ -0,0 +1,16 @@
+n.d(t, {
+    Px: () => a,
+    W$: () => i,
+    jF: () => r
+});
+let r = {
+        mass: 1,
+        tension: 1000,
+        friction: 48
+    },
+    i = {
+        mass: 1,
+        tension: 1200,
+        friction: 80
+    },
+    a = r;
