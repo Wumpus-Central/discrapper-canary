35894990bb826e9d161413fd6bb7c3e3c8f5d872
DiscrapperManager <admin@minerpl.xyz>
2025-07-24 23:29:38 +0000

    canaryRelease: 422950
    Main scripts: 4
    Lazy Loaded Scripts: 1383
    Chunks:
     * Main Files: 12107
     * Scripts: 11465
    Strings: 21872
    Routes: 721
    Experiments Count: 467
    Apex Experiments Count: 15

---
+++ chunks/238359.js
@@ -0,0 +1,16 @@
+(r.d(t, { D: () => d }), r(388685));
+var n = r(73800),
+    l = r(442837),
+    i = r(597688),
+    o = r(1870),
+    a = r(212161),
+    s = r(365943),
+    c = r(25251);
+let u = (e) => (0, a.H)(e) && e.items.length > 0 && null != e.items[0].id && null == c.Z.getProfileEffectById(e.items[0].id),
+    d = () => {
+        let e = (0, l.e7)([i.Z], () => i.Z.products),
+            t = (0, l.e7)([o.Z], () => o.Z.purchases);
+        n.useEffect(() => {
+            [...e.values(), ...t.values()].some(u) && (0, s.t)(!0);
+        }, [e, t]);
+    };
