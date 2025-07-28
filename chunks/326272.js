182476fd2ab45fb6399af8056c22833093cce758
DiscrapperManager <admin@minerpl.xyz>
2025-07-28 16:51:06 +0000

    canaryRelease: 423722
    Main scripts: 4
    Lazy Loaded Scripts: 1383
    Chunks:
     * Main Files: 12121
     * Scripts: 11414
    Strings: 21851
    Routes: 721
    Experiments Count: 471
    Apex Experiments Count: 15

---
+++ chunks/326272.js
@@ -0,0 +1,21 @@
+n.d(t, {
+    O: () => l,
+    Z: () => o
+});
+var r = n(818083),
+    i = n(987338);
+let l = (0, r.B)({
+        kind: 'user',
+        id: '2025-07_virtual_currency_reheat_experiment',
+        label: 'Virtual Currency Reheat Experiment',
+        commonTriggerPoint: i.$P.CONNECTION_OPEN,
+        defaultConfig: { enabled: !1 },
+        treatments: [
+            {
+                id: 1,
+                label: 'Enable Virtual Currency reheat experience',
+                config: { enabled: !0 }
+            }
+        ]
+    }),
+    o = l;
