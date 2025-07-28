80d9dc49be7e5941daeb651cc0d8ca5781bc97c7
DiscrapperManager <admin@minerpl.xyz>
2025-07-28 22:56:27 +0000

    canaryRelease: 424119
    Main scripts: 4
    Lazy Loaded Scripts: 1387
    Chunks:
     * Main Files: 12058
     * Scripts: 11488
    Strings: 21832
    Routes: 721
    Experiments Count: 469
    Apex Experiments Count: 15

---
+++ chunks/145843.js
@@ -0,0 +1,103 @@
+n.d(t, { Z: () => _ });
+var r = n(255367);
+n(73800);
+var i = n(206074),
+    a = n(915863),
+    o = n(388032);
+function s(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
+function l(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        ('function' == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                })
+            )),
+            r.forEach(function (t) {
+                s(e, t, n[t]);
+            }));
+    }
+    return e;
+}
+function c(e, t) {
+    var n = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+        var r = Object.getOwnPropertySymbols(e);
+        (t &&
+            (r = r.filter(function (t) {
+                return Object.getOwnPropertyDescriptor(e, t).enumerable;
+            })),
+            n.push.apply(n, r));
+    }
+    return n;
+}
+function u(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : c(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+function d(e, t) {
+    if (null == e) return {};
+    var n,
+        r,
+        i = f(e, t);
+    if (Object.getOwnPropertySymbols) {
+        var a = Object.getOwnPropertySymbols(e);
+        for (r = 0; r < a.length; r++) ((n = a[r]), !(t.indexOf(n) >= 0) && Object.prototype.propertyIsEnumerable.call(e, n) && (i[n] = e[n]));
+    }
+    return i;
+}
+function f(e, t) {
+    if (null == e) return {};
+    var n,
+        r,
+        i = {},
+        a = Object.keys(e);
+    for (r = 0; r < a.length; r++) ((n = a[r]), t.indexOf(n) >= 0 || (i[n] = e[n]));
+    return i;
+}
+function _(e) {
+    var { ButtonComponent: t = a.Z, application: n, location: s } = e,
+        c = d(e, ['ButtonComponent', 'application', 'location']);
+    let f = (0, i.Z)({
+            application: n,
+            location: s
+        }),
+        _ = () => {
+            null == f || f();
+        };
+    return (0, r.jsx)(
+        t,
+        u(
+            l(
+                {
+                    onClick: _,
+                    fullWidth: !0
+                },
+                c
+            ),
+            { children: o.intl.string(o.t['jaYS/v']) }
+        ),
+        'cloud-play'
+    );
+}
