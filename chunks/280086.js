9c69b67d76dc772397379f71d489e3b3ac0ff422
DiscrapperManager <admin@minerpl.xyz>
2025-07-28 16:59:34 +0000

    canaryRelease: 423729
    Main scripts: 4
    Lazy Loaded Scripts: 1384
    Chunks:
     * Main Files: 12121
     * Scripts: 11410
    Strings: 21851
    Routes: 721
    Experiments Count: 471
    Apex Experiments Count: 15

--- chunks/280086.js
+++
@@ -1,8 +0,0 @@
-e.exports = {
-    eligibilityModal: 'eligibilityModal__17e0d',
-    eligibilityHeader: 'eligibilityHeader__17e0d',
-    eligibilityHeaderText: 'eligibilityHeaderText__17e0d',
-    eligibilityHeaderImage: 'eligibilityHeaderImage__17e0d',
-    eligibilityHeaderCloseButton: 'eligibilityHeaderCloseButton__17e0d',
-    paddedContainer: 'paddedContainer__17e0d'
-};
