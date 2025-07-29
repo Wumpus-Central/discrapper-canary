985ec765b5f0815ad7ec1ebd23401d2f9ded6e6d
DiscrapperManager <admin@minerpl.xyz>
2025-07-29 22:47:57 +0000

    canaryRelease: 424795
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 12067
     * Scripts: 11500
    Strings: 21883
    Routes: 721
    Experiments Count: 466
    Apex Experiments Count: 15

---
+++ chunks/38273.js
@@ -0,0 +1,96 @@
+n.d(t, { G: () => d });
+var r = n(70466),
+    i = n(864218);
+function a(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
+function o(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        ('function' == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                })
+            )),
+            r.forEach(function (t) {
+                a(e, t, n[t]);
+            }));
+    }
+    return e;
+}
+function s(e, t) {
+    var n = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+        var r = Object.getOwnPropertySymbols(e);
+        (t &&
+            (r = r.filter(function (t) {
+                return Object.getOwnPropertyDescriptor(e, t).enumerable;
+            })),
+            n.push.apply(n, r));
+    }
+    return n;
+}
+function l(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : s(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+function c(e, t) {
+    if (null == e) return {};
+    var n,
+        r,
+        i = u(e, t);
+    if (Object.getOwnPropertySymbols) {
+        var a = Object.getOwnPropertySymbols(e);
+        for (r = 0; r < a.length; r++) ((n = a[r]), !(t.indexOf(n) >= 0) && Object.prototype.propertyIsEnumerable.call(e, n) && (i[n] = e[n]));
+    }
+    return i;
+}
+function u(e, t) {
+    if (null == e) return {};
+    var n,
+        r,
+        i = {},
+        a = Object.keys(e);
+    for (r = 0; r < a.length; r++) ((n = a[r]), t.indexOf(n) >= 0 || (i[n] = e[n]));
+    return i;
+}
+let d = (e) => {
+    var { subscriptionTier: t, hasActivePromotion: n = !1, useShorterCTA: a = !1, isPersistentCTA: s = !1 } = e,
+        u = c(e, ['subscriptionTier', 'hasActivePromotion', 'useShorterCTA', 'isPersistentCTA']);
+    let d = {
+            subscriptionTier: t,
+            hasActivePromotion: n,
+            useShorterCTA: a,
+            isPersistentCTA: s
+        },
+        { buttonText: _, marketingSubscriptionTierSkuId: f } = (0, r.G)(d),
+        { subscribeButtonProps: p } = (0, i.F)(
+            l(o({}, u), {
+                subscriptionTier: f,
+                buttonTextOverride: _
+            })
+        );
+    return {
+        subscribeButtonProps: p,
+        subscriptionTier: f
+    };
+};
---
+++ chunks/638212.js
@@ -0,0 +1,92 @@
+n.d(t, { Z: () => _ });
+var r = n(255367),
+    i = n(740594),
+    a = n(38273);
+function o(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
+function s(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        ('function' == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                })
+            )),
+            r.forEach(function (t) {
+                o(e, t, n[t]);
+            }));
+    }
+    return e;
+}
+function l(e, t) {
+    var n = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+        var r = Object.getOwnPropertySymbols(e);
+        (t &&
+            (r = r.filter(function (t) {
+                return Object.getOwnPropertyDescriptor(e, t).enumerable;
+            })),
+            n.push.apply(n, r));
+    }
+    return n;
+}
+function c(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : l(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+function u(e, t) {
+    if (null == e) return {};
+    var n,
+        r,
+        i = d(e, t);
+    if (Object.getOwnPropertySymbols) {
+        var a = Object.getOwnPropertySymbols(e);
+        for (r = 0; r < a.length; r++) ((n = a[r]), !(t.indexOf(n) >= 0) && Object.prototype.propertyIsEnumerable.call(e, n) && (i[n] = e[n]));
+    }
+    return i;
+}
+function d(e, t) {
+    if (null == e) return {};
+    var n,
+        r,
+        i = {},
+        a = Object.keys(e);
+    for (r = 0; r < a.length; r++) ((n = a[r]), t.indexOf(n) >= 0 || (i[n] = e[n]));
+    return i;
+}
+function _(e) {
+    var { subscriptionTier: t, hasActivePromotion: n, useShorterCTA: o, isPersistentCTA: l, buttonTextOverride: d, defaultTextOverride: _, iconOverride: f, variantOverride: p } = e,
+        h = u(e, ['subscriptionTier', 'hasActivePromotion', 'useShorterCTA', 'isPersistentCTA', 'buttonTextOverride', 'defaultTextOverride', 'iconOverride', 'variantOverride']);
+    let m = {
+            subscriptionTier: t,
+            hasActivePromotion: n,
+            useShorterCTA: o,
+            isPersistentCTA: l,
+            buttonTextOverride: d,
+            defaultTextOverride: _,
+            iconOverride: f,
+            variantOverride: p
+        },
+        { subscribeButtonProps: g, subscriptionTier: E } = (0, a.G)(m);
+    return (0, r.jsx)(i.Z, c(s({}, h, g), { subscriptionTier: E }));
+}
