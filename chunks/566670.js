0868c409ccf5557533cc006cb864473d76192611
DiscrapperManager <admin@minerpl.xyz>
2025-07-29 19:53:24 +0000

    canaryRelease: 424561
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 12058
     * Scripts: 11486
    Strings: 21870
    Routes: 721
    Experiments Count: 465
    Apex Experiments Count: 15

--- chunks/566670.js
+++
@@ -1,7 +0,0 @@
-e.exports = {
-    headerDescription: 'headerDescription_ababa2',
-    modalRoot: 'modalRoot_ababa2',
-    modalBody: 'modalBody_ababa2',
-    modalFooter: 'modalFooter_ababa2',
-    modalHeader: 'modalHeader_ababa2'
-};
