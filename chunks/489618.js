9b88a5839f2e1a44b3dd62ed4d57cb4351d0d3c2
DiscrapperManager <admin@minerpl.xyz>
2025-07-29 23:08:32 +0000

    canaryRelease: 424822
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 12067
     * Scripts: 11502
    Strings: 21883
    Routes: 721
    Experiments Count: 467
    Apex Experiments Count: 15

---
+++ chunks/489618.js
@@ -0,0 +1,20 @@
+n.d(t, { Z: () => i });
+let i = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-07_right_click_redemption',
+    label: 'Right Click Redemption',
+    defaultConfig: {
+        dotsInsteadOfCloseButton: !1,
+        rearrangeContextMenu: !1
+    },
+    treatments: [
+        {
+            id: 1,
+            label: 'Context Menu Instead of Close Button',
+            config: {
+                dotsInsteadOfCloseButton: !0,
+                rearrangeContextMenu: !1
+            }
+        }
+    ]
+});
