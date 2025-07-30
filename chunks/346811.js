6cced3a07b592d08ade877b5fbd771fc4a695e70
DiscrapperManager <admin@minerpl.xyz>
2025-07-30 00:59:28 +0000

    canaryRelease: 424919
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 12068
     * Scripts: 11511
    Strings: 21885
    Routes: 721
    Experiments Count: 468
    Apex Experiments Count: 16

---
+++ chunks/346811.js
@@ -0,0 +1,10 @@
+n.d(t, { Z: () => r });
+let r = (0, n(427164).le)({
+    name: '2025-07-nameplate-per-server',
+    kind: 'user',
+    defaultConfig: { enabled: !1 },
+    variations: {
+        0: { enabled: !1 },
+        1: { enabled: !0 }
+    }
+});
