0027b1dce4286ec468085e8724b52662a6eed0db
DiscrapperManager <admin@minerpl.xyz>
2025-07-30 20:25:22 +0000

    canaryRelease: 425236
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 12075
     * Scripts: 11508
    Strings: 21888
    Routes: 721
    Experiments Count: 472
    Apex Experiments Count: 16

---
+++ chunks/148807.js
@@ -0,0 +1,36 @@
+n.d(t, { Z: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-07_cuda_cache_management',
+    label: 'CUDA cache management',
+    defaultConfig: {
+        prewarm: !1,
+        clear: !1
+    },
+    treatments: [
+        {
+            id: 1,
+            label: 'Prewarm + Clear',
+            config: {
+                prewarm: !0,
+                clear: !0
+            }
+        },
+        {
+            id: 2,
+            label: 'Prewarm only',
+            config: {
+                prewarm: !0,
+                clear: !1
+            }
+        },
+        {
+            id: 3,
+            label: 'Clear only',
+            config: {
+                prewarm: !1,
+                clear: !0
+            }
+        }
+    ]
+});
