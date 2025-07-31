d25fde4d804f29f362b8eca818fd9847dca069bd
DiscrapperManager <admin@minerpl.xyz>
2025-07-31 21:56:11 +0000

    canaryRelease: 425831
    Main scripts: 4
    Lazy Loaded Scripts: 1399
    Chunks:
     * Main Files: 12052
     * Scripts: 11569
    Strings: 21947
    Routes: 722
    Experiments Count: 478
    Apex Experiments Count: 16

--- chunks/842054.js
+++
@@ -1,8 +0,0 @@
-t.exports = {
-    modal: 'modal__914fc',
-    closeBtn: 'closeBtn__914fc',
-    header: 'header__914fc',
-    title: 'title__914fc',
-    subtext: 'subtext__914fc',
-    ctaBtn: 'ctaBtn__914fc'
-};
