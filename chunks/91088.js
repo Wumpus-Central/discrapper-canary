894e1dd943edf8c6d6ccc192a3a7aa33ce87ab55
DiscrapperManager <admin@minerpl.xyz>
2025-07-31 21:12:24 +0000

    canaryRelease: 425782
    Main scripts: 4
    Lazy Loaded Scripts: 1399
    Chunks:
     * Main Files: 12048
     * Scripts: 11570
    Strings: 21947
    Routes: 722
    Experiments Count: 478
    Apex Experiments Count: 16

---
+++ chunks/91088.js
@@ -0,0 +1,14 @@
+n.d(t, { P: () => r });
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2025-07_remove_voice_user_overflow_menu',
+    label: 'Remove Voice User Overflow Menu',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Remove the Voice User Overflow Menu',
+            config: { enabled: !0 }
+        }
+    ]
+});
