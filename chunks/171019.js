32eb1927c72381416e56d2ea11826a0eef2220af
DiscrapperManager <admin@minerpl.xyz>
2025-07-31 21:47:21 +0000

    canaryRelease: 425818
    Main scripts: 4
    Lazy Loaded Scripts: 1399
    Chunks:
     * Main Files: 12052
     * Scripts: 11570
    Strings: 21947
    Routes: 722
    Experiments Count: 478
    Apex Experiments Count: 16

---
+++ chunks/171019.js
@@ -0,0 +1,50 @@
+n.d(t, { Z: () => d });
+var r = n(255367),
+    i = n(831209),
+    a = n(442837),
+    o = n(481060),
+    s = n(597688),
+    l = n(884697),
+    c = n(388032),
+    u = n(697490);
+let d = (e) => {
+    let { isPurchaseSection: t, isPremiumSection: n, canUsePremiumCollectibles: d, skuId: _ } = e,
+        f = (0, l.Yq)(_),
+        p = (0, a.e7)([s.Z], () => {
+            let e = s.Z.getProduct(_);
+            return (0, l.G1)(e);
+        });
+    return t || (n && d)
+        ? null
+        : f
+          ? (0, r.jsx)(o.lBU, {
+                className: u.newBadge,
+                text: (0, r.jsxs)('div', {
+                    className: u.newBadgeText,
+                    children: [
+                        (0, r.jsx)(o.mBM, {
+                            size: 'xxs',
+                            color: 'currentColor'
+                        }),
+                        c.intl.string(c.t.y2b7CA)
+                    ]
+                })
+            })
+          : (0, r.jsx)(o.G2e, {
+                icon: p
+                    ? () =>
+                          (0, r.jsx)(o.SrA, {
+                              size: 'custom',
+                              color: 'currentColor',
+                              width: 14,
+                              height: 14
+                          })
+                    : () =>
+                          (0, r.jsx)(o.mBM, {
+                              size: 'xxs',
+                              color: 'currentColor'
+                          }),
+                color: i.Z.BACKGROUND_ACCENT,
+                className: u.iconBadge
+            });
+};
