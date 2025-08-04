042367c372780620aa1d787c6f4bc4a3dd42ce79
DiscrapperManager <admin@minerpl.xyz>
2025-08-04 21:18:08 +0000

    canaryRelease: 426832
    Main scripts: 4
    Lazy Loaded Scripts: 1398
    Chunks:
     * Main Files: 12071
     * Scripts: 11567
    Strings: 21954
    Routes: 723
    Experiments Count: 480
    Apex Experiments Count: 16

---
+++ chunks/46062.js
@@ -0,0 +1,14 @@
+n.d(t, { T: () => i });
+let i = (0, n(818083).B)({
+    id: '2025-07_ostrich',
+    label: 'Ostrich',
+    kind: 'user',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enabled',
+            config: { enabled: !0 }
+        }
+    ]
+});
