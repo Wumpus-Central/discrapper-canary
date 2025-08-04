9b8dbc26c4b323637081f9738924b1277e41f31f
DiscrapperManager <admin@minerpl.xyz>
2025-08-04 22:05:30 +0000

    canaryRelease: 426884
    Main scripts: 4
    Lazy Loaded Scripts: 1398
    Chunks:
     * Main Files: 12070
     * Scripts: 11570
    Strings: 21955
    Routes: 723
    Experiments Count: 478
    Apex Experiments Count: 16

--- chunks/717215.js
+++
@@ -1,8 +0,0 @@
-e.exports = {
-    body: 'body__33efa',
-    headerIconContainer: 'headerIconContainer__33efa',
-    iconBackground: 'iconBackground__33efa',
-    headerIcon: 'headerIcon__33efa',
-    title: 'title__33efa',
-    subtitle: 'subtitle__33efa'
-};
