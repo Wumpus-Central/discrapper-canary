e17510cc0c346f0c2d0aba05cd6ec3c13efa9385
DiscrapperManager <admin@minerpl.xyz>
2025-08-04 15:00:14 +0000

    canaryRelease: 426568
    Main scripts: 4
    Lazy Loaded Scripts: 1398
    Chunks:
     * Main Files: 12064
     * Scripts: 11567
    Strings: 21947
    Routes: 722
    Experiments Count: 479
    Apex Experiments Count: 16

---
+++ chunks/265153.js
@@ -0,0 +1,14 @@
+n.d(t, { Z: () => r });
+let r = (0, n(818083).B)({
+    kind: 'guild',
+    id: '2025-07_mobile_member_applications',
+    label: 'Mobile Members Application Reviews',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'User review membership applications',
+            config: { enabled: !0 }
+        }
+    ]
+});
