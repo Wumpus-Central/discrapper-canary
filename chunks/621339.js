f27ec3c11a194d8d741f61baa6f3b08dd2dca335
DiscrapperManager <admin@minerpl.xyz>
2025-08-06 21:00:25 +0000

    canaryRelease: 428134
    Main scripts: 4
    Lazy Loaded Scripts: 1405
    Chunks:
     * Main Files: 14144
     * Scripts: 9607
    Strings: 22032
    Routes: 722
    Experiments Count: 482
    Apex Experiments Count: 17

---
+++ chunks/621339.js
@@ -0,0 +1,15 @@
+n.d(t, { Z: () => s });
+var i = n(657707),
+    o = n(509613),
+    l = n(393681),
+    r = n(388032);
+let a = (0, o.x1)("AccountPane", {
+        useTitle: () => r.intl.string(r.t["JAIM/v"]),
+        layout: [],
+        render: l.Z,
+    }),
+    s = (0, o.wf)("Account", {
+        useTitle: () => r.intl.string(r.t["JAIM/v"]),
+        icon: i.tBG,
+        layout: [a],
+    });
