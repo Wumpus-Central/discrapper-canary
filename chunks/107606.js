de312d125b4ac32849e90faed32165b60497a95e
DiscrapperManager <admin@minerpl.xyz>
2025-08-06 04:30:36 +0000

    canaryRelease: 427707
    Main scripts: 4
    Lazy Loaded Scripts: 1405
    Chunks:
     * Main Files: 14135
     * Scripts: 9605
    Strings: 22032
    Routes: 722
    Experiments Count: 481
    Apex Experiments Count: 16

---
+++ chunks/107606.js
@@ -0,0 +1,27 @@
+a.d(t, { Z: () => c });
+var n = a(255367);
+a(73800);
+var r = a(442837),
+    l = a(481060),
+    i = a(31336),
+    s = a(19759),
+    o = a(716561);
+function c() {
+    let e = (0, r.e7)([s.Z], () => s.Z.isRandomlyChangingPremiumOptions);
+    return (0, n.jsx)('div', {
+        className: o.container,
+        children: (0, n.jsx)('div', {
+            'data-button-hoisted-classname-wrapper': !0,
+            className: o.button,
+            children: (0, n.jsx)(l.xJW, {
+                children: (0, n.jsx)(l.j7V, {
+                    value: e,
+                    note: 'Randomizes theme, avatar decorations, and nameplates, every 5 seconds',
+                    onChange: (e) => (0, i.Qh)({ isRandomlyChangingPremiumOptions: e }),
+                    hideBorder: !0,
+                    children: 'Randomize Premium Features'
+                })
+            })
+        })
+    });
+}
---
+++ chunks/176038.js
@@ -0,0 +1,90 @@
+(a.d(t, { Z: () => _ }), a(388685), a(457542));
+var n = a(73800),
+    r = a(442837),
+    l = a(809206),
+    i = a(153867),
+    s = a(1870),
+    o = a(884697),
+    c = a(223143),
+    d = a(164946),
+    u = a(675478),
+    m = a(25990),
+    x = a(594174),
+    h = a(74538),
+    p = a(981631),
+    f = a(469115);
+let b = [p.BRd.DARK, p.BRd.LIGHT, p.BRd.DARKER, p.BRd.MIDNIGHT];
+function v(e) {
+    return e[Math.floor(Math.random() * e.length)];
+}
+async function j() {
+    let e = (function () {
+            let e = x.default.getCurrentUser();
+            return h.ZP.canUseClientThemes(e) ? Object.keys(f.qt).map((e) => Number(e)) : [];
+        })(),
+        t = e.length > 0 && Math.random() > 0.7;
+    try {
+        if (t) {
+            let t,
+                n = [...e, null],
+                r = v(n);
+            if (null != r) {
+                var a;
+                let e = f.qt[r];
+                t = null != (a = null == e ? void 0 : e.theme) ? a : p.BRd.DARK;
+            } else t = v([p.BRd.DARK, p.BRd.DARKER, p.BRd.MIDNIGHT]);
+            await i.ZI(
+                {
+                    theme: t,
+                    backgroundGradientPresetId: null != r ? r : void 0,
+                    customUserThemeSettings: void 0
+                },
+                u.fy.INFREQUENT_USER_ACTION
+            );
+        } else {
+            let e = v(b);
+            await i.ZI(
+                {
+                    theme: e,
+                    backgroundGradientPresetId: void 0,
+                    customUserThemeSettings: void 0
+                },
+                u.fy.INFREQUENT_USER_ACTION
+            );
+        }
+    } catch (e) {}
+}
+async function g(e, t) {
+    let a = e.length > 0,
+        n = t.length > 0;
+    if (a || n)
+        try {
+            if (a) {
+                let t = v([...e, null]);
+                (0, l.cV)(t);
+            }
+            if (n) {
+                let e = v([...t, null]);
+                (0, l.Rx)(e);
+            }
+            let r = m.Z.getAllPending(),
+                i = (0, d.ED)(r);
+            await (0, l.Mn)(i).finally(l.si);
+        } catch (e) {}
+}
+function _() {
+    (0, c.mw)();
+    let [e, t] = (0, r.Wu)([s.Z], () => {
+            let e = s.Z.purchases;
+            return [(0, o.Ad)(e), (0, o.jT)(e)];
+        }),
+        a = n.useCallback(() => {
+            (j(), g(e, t));
+        }, [e, t]);
+    n.useEffect(() => {
+        let e = setInterval(() => {
+            a();
+        }, 5000);
+        return () => clearInterval(e);
+    }, [a]);
+}
