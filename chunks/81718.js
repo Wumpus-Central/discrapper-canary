c8086f66003157c778672a4d3440e9fb9d3d62cc
DiscrapperManager <admin@minerpl.xyz>
2025-08-06 19:14:35 +0000

    canaryRelease: 427979
    Main scripts: 4
    Lazy Loaded Scripts: 1405
    Chunks:
     * Main Files: 14141
     * Scripts: 9601
    Strings: 22032
    Routes: 722
    Experiments Count: 480
    Apex Experiments Count: 17

--- chunks/81718.js
+++
@@ -1,7 +0,0 @@
-e.exports = {
-    row: 'row_fec266',
-    rowContent: 'rowContent_fec266',
-    iconContainer: 'iconContainer_fec266',
-    threadIcon: 'threadIcon_fec266',
-    personWavingIcon: 'personWavingIcon_fec266'
-};
