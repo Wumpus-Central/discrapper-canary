437d0f44baa1b26f91f8b4bf090f07a84b3e18f8
DiscrapperManager <admin@minerpl.xyz>
2025-08-07 20:28:57 +0000

    canaryRelease: 428785
    Main scripts: 4
    Lazy Loaded Scripts: 1408
    Chunks:
     * Main Files: 14162
     * Scripts: 9618
    Strings: 22069
    Routes: 722
    Experiments Count: 483
    Apex Experiments Count: 17

---
+++ chunks/881410.js
@@ -0,0 +1,120 @@
+n.d(t, { Z: () => u }), n(388685), n(781311);
+var r = n(255367),
+    l = n(73800),
+    o = n(423802),
+    i = n(481060),
+    a = n(510231),
+    c = n(388032),
+    s = n(591334);
+function d(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        "function" == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                }),
+            )),
+            r.forEach(function (t) {
+                var r;
+                (r = n[t]),
+                    t in e
+                        ? Object.defineProperty(e, t, {
+                              value: r,
+                              enumerable: !0,
+                              configurable: !0,
+                              writable: !0,
+                          })
+                        : (e[t] = r);
+            });
+    }
+    return e;
+}
+function u() {
+    let [e] = (0, i.ynZ)(),
+        t = l.useRef(null),
+        n = l.useCallback((e) => {}, []),
+        { options: u, matchSorterOptions: f } = (0, a.h)(),
+        p = l.useCallback(
+            (e) => {
+                var t, n;
+                return "" === e.trim()
+                    ? u
+                    : (0, o.Lu)(
+                          u,
+                          e,
+                          ((t = d({}, f)),
+                          (n = n = { threshold: o.Lu.rankings.CONTAINS }),
+                          Object.getOwnPropertyDescriptors
+                              ? Object.defineProperties(t, Object.getOwnPropertyDescriptors(n))
+                              : (function (e, t) {
+                                    var n = Object.keys(e);
+                                    if (Object.getOwnPropertySymbols) {
+                                        var r = Object.getOwnPropertySymbols(e);
+                                        n.push.apply(n, r);
+                                    }
+                                    return n;
+                                })(Object(n)).forEach(function (e) {
+                                    Object.defineProperty(t, e, Object.getOwnPropertyDescriptor(n, e));
+                                }),
+                          t),
+                      );
+            },
+            [u, f],
+        );
+    return (0, r.jsx)(i.yRy, {
+        targetElementRef: t,
+        position: "bottom",
+        align: "center",
+        renderPopout: (t) => {
+            let { closePopout: l } = t;
+            return (0, r.jsx)(i.DBG, {
+                className: s.gameSearchCombobox,
+                placeholder: c.intl.string(c.t["5h0QOD"]),
+                autoFocus: !0,
+                value: e,
+                onChange: (e) => {
+                    n(e), l();
+                },
+                onClose: l,
+                multiSelect: !1,
+                showScrollbar: !0,
+                maxVisibleItems: 7,
+                children: (e) =>
+                    p(e).map((e) =>
+                        (0, r.jsx)(
+                            i.lo1,
+                            {
+                                value: String(e.value),
+                                children: (0, r.jsx)(i.lo1.Label, {
+                                    children: (0, r.jsx)(i.Text, {
+                                        variant: "text-md/medium",
+                                        color: "header-secondary",
+                                        children: e.label,
+                                    }),
+                                }),
+                            },
+                            String(e.value),
+                        ),
+                    ),
+            });
+        },
+        children: (e) =>
+            (0, r.jsx)("div", {
+                ref: t,
+                children: (0, r.jsx)(
+                    i.zxk,
+                    d(
+                        {
+                            variant: "primary",
+                            size: "sm",
+                            text: c.intl.string(c.t.uqw8wM),
+                            "aria-haspopup": "listbox",
+                        },
+                        e,
+                    ),
+                ),
+            }),
+    });
+}
