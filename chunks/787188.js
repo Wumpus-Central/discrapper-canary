ae87b427234fef346725c5874b9dd976d85a336a
DiscrapperManager <admin@minerpl.xyz>
2025-08-07 16:40:48 +0000

    canaryRelease: 428578
    Main scripts: 4
    Lazy Loaded Scripts: 1406
    Chunks:
     * Main Files: 14158
     * Scripts: 9615
    Strings: 22055
    Routes: 722
    Experiments Count: 484
    Apex Experiments Count: 17

---
+++ chunks/787188.js
@@ -0,0 +1,33 @@
+n.d(t, { _: () => l });
+var r = n(535396),
+    o = n(93841),
+    i = n(388032);
+function l(e, t, n) {
+    var l, s, a;
+    return (null == (l = t.sourceEntitlement) ? void 0 : l.ends_at) != null
+        ? {
+              type: "expiring",
+              expiringAt: t.sourceEntitlement.ends_at,
+          }
+        : n && null != e.storeRemovalDate
+          ? {
+                type: "removing",
+                removingAt: e.storeRemovalDate,
+            }
+          : t.type === r.A3.LEVEL_ACTIVATED
+            ? {
+                  type: "active",
+                  statusText: i.intl.formatToPlainString(o.default.WRRYUV, {
+                      perkName:
+                          null != (a = null == (s = t.sourcePowerup) ? void 0 : s.title)
+                              ? a
+                              : i.intl.string(i.t.BfF6EB),
+                  }),
+              }
+            : t.type === r.A3.POWERUP_ACTIVATED
+              ? {
+                    type: "active",
+                    statusText: i.intl.string(o.default.FFLkm5),
+                }
+              : null;
+}
