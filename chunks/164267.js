163105732e7122bb031267f5e62aafef80c1d1b0
DiscrapperManager <admin@minerpl.xyz>
2025-08-08 16:27:43 +0000

    canaryRelease: 429185
    Main scripts: 4
    Lazy Loaded Scripts: 1413
    Chunks:
     * Main Files: 14170
     * Scripts: 9618
    Strings: 22077
    Routes: 722
    Experiments Count: 486
    Apex Experiments Count: 17

---
+++ chunks/164267.js
@@ -0,0 +1,79 @@
+r.r(t), r.d(t, { default: () => j });
+var n = r(255367),
+    o = r(73800),
+    i = r(120356),
+    c = r.n(i),
+    s = r(103269),
+    u = r(616259),
+    a = r(94662);
+function p(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var r = null != arguments[t] ? arguments[t] : {},
+            n = Object.keys(r);
+        "function" == typeof Object.getOwnPropertySymbols &&
+            (n = n.concat(
+                Object.getOwnPropertySymbols(r).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(r, e).enumerable;
+                }),
+            )),
+            n.forEach(function (t) {
+                var n;
+                (n = r[t]),
+                    t in e
+                        ? Object.defineProperty(e, t, {
+                              value: n,
+                              enumerable: !0,
+                              configurable: !0,
+                              writable: !0,
+                          })
+                        : (e[t] = n);
+            });
+    }
+    return e;
+}
+function f(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : (function (e, t) {
+                  var r = Object.keys(e);
+                  if (Object.getOwnPropertySymbols) {
+                      var n = Object.getOwnPropertySymbols(e);
+                      r.push.apply(r, n);
+                  }
+                  return r;
+              })(Object(t)).forEach(function (r) {
+                  Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r));
+              }),
+        e
+    );
+}
+function l() {
+    return (0, n.jsx)("div", { className: a.customPointer });
+}
+function b() {
+    return (0, n.jsx)("div", { className: c()(a.customPointer, a.huePointer) });
+}
+let O = (0, s.iS)((e) =>
+        (0, n.jsxs)("div", {
+            className: a.pickerWrapper,
+            children: [
+                (0, n.jsx)("div", {
+                    className: a.saturation,
+                    children: (0, n.jsx)(u.Saturation, f(p({}, e), { pointer: l })),
+                }),
+                (0, n.jsx)("div", {
+                    className: a.hue,
+                    children: (0, n.jsx)(
+                        u.Hue,
+                        f(p({}, e), {
+                            direction: "horizontal",
+                            pointer: b,
+                        }),
+                    ),
+                }),
+            ],
+        }),
+    ),
+    j = o.memo(O);
