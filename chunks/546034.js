2ef0a273c5fb6e7e65c9dba2abefa09065342b03
DiscrapperManager <admin@minerpl.xyz>
2025-08-08 16:00:15 +0000

    canaryRelease: 429172
    Main scripts: 4
    Lazy Loaded Scripts: 1412
    Chunks:
     * Main Files: 14170
     * Scripts: 9616
    Strings: 22079
    Routes: 722
    Experiments Count: 486
    Apex Experiments Count: 17

--- chunks/546034.js
+++
@@ -1,5 +0,0 @@
-e.exports = {
-    largeCountdownPill: "largeCountdownPill_e270ac",
-    largeCountdownPillText: "largeCountdownPillText_e270ac",
-    largePillIcon: "largePillIcon_e270ac",
-};
--- chunks/93015.js
+++
@@ -1,7 +0,0 @@
-e.exports = {
-    unavailableCounterContainer: "unavailableCounterContainer_a466a0",
-    unavailableCounterPill: "unavailableCounterPill_a466a0",
-    unavailableCounterPillText: "unavailableCounterPillText_a466a0",
-    countDownText: "countDownText_a466a0",
-    tooltip: "tooltip_a466a0",
-};
