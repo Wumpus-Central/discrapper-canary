bbffee79f36a94f6a18d2aebe1e160fefc5c95eb
DiscrapperManager <admin@minerpl.xyz>
2025-08-08 00:05:22 +0000

    canaryRelease: 429018
    Main scripts: 4
    Lazy Loaded Scripts: 1412
    Chunks:
     * Main Files: 14169
     * Scripts: 9624
    Strings: 22073
    Routes: 722
    Experiments Count: 489
    Apex Experiments Count: 17

---
+++ chunks/422202.js
@@ -0,0 +1,41 @@
+n.d(t, {
+    P: () => s,
+    _: () => o,
+});
+var r = n(818083),
+    i = n(751823);
+let l = (0, r.B)({
+    kind: "user",
+    id: "2025-08_device_changed_notification",
+    label: "Device-changed notification experiment",
+    defaultConfig: { shouldShowDeviceChangedNotifications: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: "Show device-changed notifications",
+            config: { shouldShowDeviceChangedNotifications: !0 },
+        },
+    ],
+});
+function o(e) {
+    let { location: t, autoTrackExposure: n = !0 } = e,
+        { isInHoldout: r } = i.L.getCurrentConfig({ location: t }, { autoTrackExposure: n });
+    return l.getCurrentConfig(
+        { location: t },
+        {
+            disable: r,
+            autoTrackExposure: n,
+        },
+    );
+}
+function s(e) {
+    let { location: t, autoTrackExposure: n = !0 } = e,
+        { isInHoldout: r } = i.L.useExperiment({ location: t }, { autoTrackExposure: n });
+    return l.useExperiment(
+        { location: t },
+        {
+            disable: r,
+            autoTrackExposure: n,
+        },
+    );
+}
