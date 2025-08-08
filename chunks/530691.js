e02da24491d9a201d79f4a37c0382962b60009bd
DiscrapperManager <admin@minerpl.xyz>
2025-08-08 21:31:09 +0000

    canaryRelease: 429430
    Main scripts: 4
    Lazy Loaded Scripts: 1415
    Chunks:
     * Main Files: 14172
     * Scripts: 9616
    Strings: 22078
    Routes: 722
    Experiments Count: 486
    Apex Experiments Count: 17

---
+++ chunks/530691.js
@@ -0,0 +1,39 @@
+n.d(t, {
+    S: () => a,
+    f: () => s,
+});
+var r = n(73800),
+    i = n(818083),
+    o = n(46140);
+let a = (0, i.B)({
+    id: "2025-08_mobile_video_quests_on_desktop",
+    kind: "user",
+    label: "Mobile Video Quests on Desktop",
+    defaultConfig: {
+        enabled: !1,
+        enrollOnDesktop: !1,
+    },
+    treatments: [
+        {
+            id: 1,
+            label: "Enabled, enrollment occurs on desktop",
+            config: {
+                enabled: !0,
+                enrollOnDesktop: !0,
+            },
+        },
+        {
+            id: 2,
+            label: "Enabled, enrollment occurs on mobile",
+            config: {
+                enabled: !0,
+                enrollOnDesktop: !1,
+            },
+        },
+    ],
+});
+function s(e) {
+    let { location: t, questFeatures: n } = e,
+        i = r.useMemo(() => n.includes(o.S7.MOBILE_ONLY_QUEST_PUSH_TO_MOBILE), [n]);
+    return a.useExperiment({ location: t }, { autoTrackExposure: i }).enabled && i;
+}
---
+++ chunks/570387.js
@@ -0,0 +1,31 @@
+t.d(r, { default: () => d }), t(190126), t(368063), t(65234), t(111804), t(490233), t(97749), t(388685);
+var n = t(255367),
+    o = t(73800),
+    s = t(82659),
+    i = t(198993),
+    l = t(388032),
+    a = t(815278);
+let c = [],
+    d = function (e) {
+        let { transitionState: r, onClose: t, questId: d, questName: f } = e,
+            u = o.useMemo(() => {
+                let e = new URL("".concat(window.location.origin, "/quests/").concat(d));
+                return e.searchParams.append("referrer_id", "quest_home_qr_code"), e.toString();
+            }, [d]);
+        return (0, n.jsx)(s.Modal, {
+            actions: c,
+            onClose: t,
+            size: "md",
+            subtitle: l.intl.format(l.t.y2Dgd3, { questName: f }),
+            title: l.intl.string(l.t.tQWM2N),
+            transitionState: r,
+            children: (0, n.jsx)("div", {
+                className: a.qrCodeWrapper,
+                children: (0, n.jsx)(i.c2, {
+                    size: 200,
+                    text: u,
+                    overlaySize: i.cK.SIZE_40,
+                }),
+            }),
+        });
+    };
