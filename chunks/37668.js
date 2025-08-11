95f12f8554c4c557b7f1d2d037af8ae0747c8d96
DiscrapperManager <admin@minerpl.xyz>
2025-08-11 20:47:38 +0000

    canaryRelease: 429951
    Main scripts: 4
    Lazy Loaded Scripts: 1416
    Chunks:
     * Main Files: 14189
     * Scripts: 9620
    Strings: 22123
    Routes: 723
    Experiments Count: 486
    Apex Experiments Count: 19

---
+++ chunks/37668.js
@@ -0,0 +1,66 @@
+i.d(e, { default: () => w });
+var n = i(255367),
+    r = i(73800),
+    s = i(442837),
+    a = i(82659),
+    l = i(481060),
+    c = i(594174),
+    d = i(63063),
+    o = i(86419),
+    u = i(778414),
+    x = i(388032),
+    h = i(39766);
+function p(t) {
+    let { widget: e, user: i } = t;
+    return (0, n.jsx)(u.Z, {
+        widget: e,
+        user: i,
+        isGameFetching: () => !1,
+        showHeaderActionButtons: !1,
+    });
+}
+function w(t) {
+    let { transitionState: e, widget: i, userId: u, onClose: w } = t,
+        g = r.useCallback(() => {
+            (0, o.y8)(i.type), w();
+        }, [i.type, w]),
+        v = (0, s.e7)([c.default], () => c.default.getUser(u));
+    return null == v
+        ? null
+        : (0, n.jsxs)(a.Modal, {
+              transitionState: e,
+              onClose: w,
+              title: x.intl.string(x.t.Mm07YW),
+              subtitle: x.intl.format(x.t["x+djIy"], { helpUrl: d.w }),
+              actionBarInput: (0, n.jsx)("div", {}),
+              actions: [
+                  {
+                      variant: "secondary",
+                      text: x.intl.string(x.t["ETE/oK"]),
+                      onClick: w,
+                  },
+                  {
+                      variant: "critical-primary",
+                      text: x.intl.string(x.t.Mm07YW),
+                      onClick: g,
+                  },
+              ],
+              children: [
+                  (0, n.jsx)("div", {
+                      className: h.widgetPreview,
+                      children: (0, n.jsx)(p, {
+                          widget: i,
+                          user: v,
+                      }),
+                  }),
+                  (0, n.jsx)("div", {
+                      className: h.hintText,
+                      children: (0, n.jsx)(l.Text, {
+                          color: "text-secondary",
+                          variant: "text-sm/normal",
+                          children: x.intl.string(x.t.bQNGeX),
+                      }),
+                  }),
+              ],
+          });
+}
