5e7066cc9ad941b17f4fb5e219ec41432ec4ca2b
DiscrapperManager <admin@minerpl.xyz>
2025-08-11 03:36:48 +0000

    canaryRelease: 429655
    Main scripts: 4
    Lazy Loaded Scripts: 1415
    Chunks:
     * Main Files: 14185
     * Scripts: 9614
    Strings: 22112
    Routes: 723
    Experiments Count: 486
    Apex Experiments Count: 19

---
+++ chunks/277941.js
@@ -0,0 +1,64 @@
+n.d(t, { Z: () => o });
+var r = n(255367);
+n(73800);
+var i = n(481060),
+    l = n(388032);
+let o = (e) => {
+    let { onGift: t, tooltipDelay: n } = e;
+    return (0, r.jsx)(i.ua7, {
+        text: l.intl.string(l.t["JCFN//"]),
+        delay: n,
+        children: (e) => {
+            var n, o;
+            return (0, r.jsx)(
+                i.hU,
+                ((n = (function (e) {
+                    for (var t = 1; t < arguments.length; t++) {
+                        var n = null != arguments[t] ? arguments[t] : {},
+                            r = Object.keys(n);
+                        "function" == typeof Object.getOwnPropertySymbols &&
+                            (r = r.concat(
+                                Object.getOwnPropertySymbols(n).filter(function (e) {
+                                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                                }),
+                            )),
+                            r.forEach(function (t) {
+                                var r;
+                                (r = n[t]),
+                                    t in e
+                                        ? Object.defineProperty(e, t, {
+                                              value: r,
+                                              enumerable: !0,
+                                              configurable: !0,
+                                              writable: !0,
+                                          })
+                                        : (e[t] = r);
+                            });
+                    }
+                    return e;
+                })({}, e)),
+                (o = o =
+                    {
+                        "aria-label": l.intl.string(l.t["JCFN//"]),
+                        variant: "primary",
+                        icon: i.OgN,
+                        size: "md",
+                        onClick: t,
+                    }),
+                Object.getOwnPropertyDescriptors
+                    ? Object.defineProperties(n, Object.getOwnPropertyDescriptors(o))
+                    : (function (e, t) {
+                          var n = Object.keys(e);
+                          if (Object.getOwnPropertySymbols) {
+                              var r = Object.getOwnPropertySymbols(e);
+                              n.push.apply(n, r);
+                          }
+                          return n;
+                      })(Object(o)).forEach(function (e) {
+                          Object.defineProperty(n, e, Object.getOwnPropertyDescriptor(o, e));
+                      }),
+                n),
+            );
+        },
+    });
+};
---
+++ chunks/382263.js
@@ -0,0 +1,141 @@
+n.d(t, { PT: () => x });
+var i = n(255367);
+n(73800);
+var a = n(442837),
+    l = n(481060),
+    s = n(225657),
+    r = n(600164),
+    o = n(927699),
+    c = n(987716),
+    u = n(311821),
+    d = n(594174),
+    f = n(55563),
+    p = n(987209),
+    m = n(563132),
+    b = n(409813),
+    g = n(27034),
+    h = n(474936),
+    y = n(388032),
+    j = n(565121);
+let x = {
+    key: b.h8.GIFT_CUSTOMIZATION,
+    renderStep: (e) =>
+        (0, i.jsx)(
+            O,
+            (function (e) {
+                for (var t = 1; t < arguments.length; t++) {
+                    var n = null != arguments[t] ? arguments[t] : {},
+                        i = Object.keys(n);
+                    "function" == typeof Object.getOwnPropertySymbols &&
+                        (i = i.concat(
+                            Object.getOwnPropertySymbols(n).filter(function (e) {
+                                return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                            }),
+                        )),
+                        i.forEach(function (t) {
+                            var i;
+                            (i = n[t]),
+                                t in e
+                                    ? Object.defineProperty(e, t, {
+                                          value: i,
+                                          enumerable: !0,
+                                          configurable: !0,
+                                          writable: !0,
+                                      })
+                                    : (e[t] = i);
+                        });
+                }
+                return e;
+            })({}, e),
+        ),
+    options: {
+        isLargeModal: !0,
+        useBreadcrumbLabel: () => y.intl.string(y.t["W685+f"]),
+    },
+};
+function v(e) {
+    let { onStepChange: t, onBackClick: n, showBackButton: a = !1, disabled: s = !1, loading: r = !1 } = e,
+        { hasPaymentSources: o } = (0, m.JL)(),
+        c = o ? b.h8.REVIEW : b.h8.ADD_PAYMENT_STEPS;
+    return (0, i.jsxs)(i.Fragment, {
+        children: [
+            (0, i.jsx)(l.zxk, {
+                onClick: () => t(c),
+                disabled: s,
+                loading: r,
+                text: y.intl.string(y.t.XiOHRU),
+            }),
+            a ? (0, i.jsx)(u.Z, { onClick: n }) : null,
+        ],
+    });
+}
+function O(e) {
+    var t;
+    let { handleStepChange: n, handleClose: u } = e,
+        { customGiftMessage: b = "", setCustomGiftMessage: x, giftRecipient: O } = (0, p.wD)(),
+        { selectedSkuId: C } = (0, m.JL)(),
+        T = (0, a.e7)([d.default], () => d.default.getCurrentUser()),
+        P = (0, a.e7)([f.Z], () => (null != C ? f.Z.get(C) : null), [C]),
+        k = async (e, t) => {};
+    return (0, i.jsxs)(i.Fragment, {
+        children: [
+            (0, i.jsx)(g.C3, {
+                children: (0, i.jsxs)("div", {
+                    className: j.stepBody,
+                    children: [
+                        (0, i.jsxs)("div", {
+                            className: j.bodyColumnLeft,
+                            children: [
+                                (0, i.jsx)(l.X6q, {
+                                    variant: "heading-lg/semibold",
+                                    color: "header-primary",
+                                    className: j.title,
+                                    children: y.intl.formatToPlainString(y.t.UIOtjo, {
+                                        skuName: null != (t = null == P ? void 0 : P.name) ? t : "this app",
+                                    }),
+                                }),
+                                (0, i.jsx)(l.Text, {
+                                    variant: "text-md/normal",
+                                    color: "header-secondary",
+                                    className: j.subtitle,
+                                    children: y.intl.string(y.t.GHZfIC),
+                                }),
+                                (0, i.jsx)(c.q, {}),
+                            ],
+                        }),
+                        (0, i.jsxs)("div", {
+                            className: j.bodyColumnRight,
+                            children: [
+                                (0, i.jsx)(s.Z, {
+                                    selectedSkuId: C,
+                                    validateSelectedGift: k,
+                                    className: j.sendTo,
+                                }),
+                                (0, i.jsx)(o.Z, {
+                                    sectionTitle: y.intl.string(y.t.B3miEx),
+                                    onTextChange: (e) => (null == x ? void 0 : x(e)),
+                                    pendingText: b,
+                                    currentText: b,
+                                    disableThemedBackground: !0,
+                                    className: j.customGiftMessageWrapper,
+                                    innerClassName: j.customGiftMessage,
+                                }),
+                            ],
+                        }),
+                    ],
+                }),
+            }),
+            (0, i.jsx)(g.O3, {
+                children: (0, i.jsx)(l.mzw, {
+                    justify: r.Z.Justify.BETWEEN,
+                    align: r.Z.Align.CENTER,
+                    children: (0, i.jsx)(v, {
+                        onStepChange: n,
+                        onBackClick: u,
+                        disabled: null == O || O.id === (null == T ? void 0 : T.id) || b.length > h.$n,
+                    }),
+                }),
+            }),
+        ],
+    });
+}
