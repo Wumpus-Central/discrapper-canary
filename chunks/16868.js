1e355c5a7fb8208a09f8144f7c8633f465dfcdce
DiscrapperManager <admin@minerpl.xyz>
2025-08-12 14:20:55 +0000

    canaryRelease: 430234
    Main scripts: 4
    Lazy Loaded Scripts: 1417
    Chunks:
     * Main Files: 14192
     * Scripts: 9639
    Strings: 22157
    Routes: 724
    Experiments Count: 487
    Apex Experiments Count: 20

---
+++ chunks/16868.js
@@ -0,0 +1,10 @@
+n.d(t, { H: () => r });
+let r = (0, n(427164).le)({
+    name: "2025-08-collapsed-completed-quest-bar",
+    kind: "user",
+    defaultConfig: { enabled: !1 },
+    variations: {
+        0: { enabled: !1 },
+        1: { enabled: !0 },
+    },
+});
---
+++ chunks/688927.js
@@ -0,0 +1,88 @@
+n.d(t, { Z: () => p });
+var r = n(255367),
+    s = n(120356),
+    o = n.n(s),
+    a = n(481060),
+    i = n(113434),
+    l = n(497505),
+    c = n(670638),
+    u = n(388032),
+    d = n(910);
+function p(e) {
+    let { quest: t, onCtxMenuOpen: n, onCtxMenuClose: s, onCtxMenuSelect: p } = e,
+        m = (0, i.B6)(t.config.expiresAt);
+    return (0, r.jsxs)("div", {
+        className: d.questAcceptedHeader,
+        children: [
+            (0, r.jsx)(a.Text, {
+                variant: "text-xxs/medium",
+                className: o()(d.flex, d.headerText),
+                children: u.intl.format(u.t["pX+fmp"], { expirationDate: m }),
+            }),
+            (0, r.jsx)(c.r, {
+                onOpen: n,
+                onClose: s,
+                onSelect: p,
+                questContent: l.jn.QUEST_BAR_V2,
+                quest: t,
+                shouldShowDisclosure: !1,
+                showShareLink: !0,
+                sourceQuestContent: l.jn.QUEST_BAR_V2,
+                children: (e) => {
+                    var t, n;
+                    return (0, r.jsx)(
+                        a.P3F,
+                        ((t = (function (e) {
+                            for (var t = 1; t < arguments.length; t++) {
+                                var n = null != arguments[t] ? arguments[t] : {},
+                                    r = Object.keys(n);
+                                "function" == typeof Object.getOwnPropertySymbols &&
+                                    (r = r.concat(
+                                        Object.getOwnPropertySymbols(n).filter(function (e) {
+                                            return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                                        }),
+                                    )),
+                                    r.forEach(function (t) {
+                                        var r;
+                                        (r = n[t]),
+                                            t in e
+                                                ? Object.defineProperty(e, t, {
+                                                      value: r,
+                                                      enumerable: !0,
+                                                      configurable: !0,
+                                                      writable: !0,
+                                                  })
+                                                : (e[t] = r);
+                                    });
+                            }
+                            return e;
+                        })({}, e)),
+                        (n = n =
+                            {
+                                className: d.submenuWrapper,
+                                "aria-label": u.intl.string(u.t.DEoVWV),
+                                children: (0, r.jsx)(a.xhG, {
+                                    size: "md",
+                                    color: "currentColor",
+                                    className: o()(d.submenuIcon, d.interactiveNormal),
+                                }),
+                            }),
+                        Object.getOwnPropertyDescriptors
+                            ? Object.defineProperties(t, Object.getOwnPropertyDescriptors(n))
+                            : (function (e, t) {
+                                  var n = Object.keys(e);
+                                  if (Object.getOwnPropertySymbols) {
+                                      var r = Object.getOwnPropertySymbols(e);
+                                      n.push.apply(n, r);
+                                  }
+                                  return n;
+                              })(Object(n)).forEach(function (e) {
+                                  Object.defineProperty(t, e, Object.getOwnPropertyDescriptor(n, e));
+                              }),
+                        t),
+                    );
+                },
+            }),
+        ],
+    });
+}
