0c8677580772cf7f577971f66fc5adf0621596f9
DiscrapperManager <admin@minerpl.xyz>
2025-08-12 16:59:57 +0000

    canaryRelease: 430298
    Main scripts: 4
    Lazy Loaded Scripts: 1420
    Chunks:
     * Main Files: 14194
     * Scripts: 9644
    Strings: 22159
    Routes: 724
    Experiments Count: 488
    Apex Experiments Count: 20

---
+++ chunks/296241.js
@@ -0,0 +1,15 @@
+n.d(t, { Z: () => o }), n(35282);
+var r = n(255367),
+    i = n(73800),
+    l = n(208450),
+    a = n(401623);
+let o = i.memo(function (e) {
+    let { searchContext: t } = e;
+    return (0, r.jsx)(l.o, {
+        searchContext: t,
+        className: a.search,
+        searchBarContainerClassName: a.searchBarContainer,
+        searchBarClassName: a.searchBar,
+        searchPopoutClassName: a.searchPopout,
+    });
+});
---
+++ chunks/819439.js
@@ -0,0 +1,35 @@
+n.d(t, { $: () => a });
+var r = n(818083);
+let i = {
+        enabled: !1,
+        inExperiment: !1,
+    },
+    o = (0, r.B)({
+        kind: "user",
+        id: "2025-08_desktop_top_level_search_v1",
+        label: "Desktop Top Level Search v1",
+        defaultConfig: i,
+        treatments: [
+            {
+                id: 0,
+                label: "Control group for desktop top level search v1",
+                config: {
+                    enabled: !1,
+                    inExperiment: !0,
+                },
+            },
+            {
+                id: 1,
+                label: "Enables desktop top level search v1",
+                config: {
+                    enabled: !0,
+                    inExperiment: !0,
+                },
+            },
+        ],
+    });
+function a(e) {
+    let { location: t, autoTrackExposure: n = !0 } = e,
+        { enabled: r } = o.useExperiment({ location: t }, { autoTrackExposure: n });
+    return r;
+}
