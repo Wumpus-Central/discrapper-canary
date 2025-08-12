f1e6244e971b6af492516e7cc593d8d253888caf
DiscrapperManager <admin@minerpl.xyz>
2025-08-12 16:36:32 +0000

    canaryRelease: 430277
    Main scripts: 4
    Lazy Loaded Scripts: 1420
    Chunks:
     * Main Files: 14193
     * Scripts: 9642
    Strings: 22159
    Routes: 724
    Experiments Count: 487
    Apex Experiments Count: 20

---
+++ chunks/424882.js
@@ -0,0 +1,8 @@
+a.r(t),
+    a.d(t, {
+        default: () => d,
+        metadata: () => s,
+    });
+let s = { fileBytes: 163629 },
+    d =
+        "https://cdn.discordapp.com/assets/content/c1363a4c21c7694c37c11fd80a6b2a78caa1632059dbbadb88c08e1fc5501fbc.lottiejson";
---
+++ chunks/543995.js
@@ -0,0 +1,8 @@
+n.d(t, { m: () => o });
+var r = n(47280),
+    i = n(474936);
+let o = () => {
+    let e = (0, r.ZP)({ location: "useGiftStyles" }),
+        t = i.QI;
+    return e && (t = [i.Cj.STANDARD_BOX]), t;
+};
