42bf451f115b1016126068bce96feaad68e4f101
DiscrapperManager <admin@minerpl.xyz>
2025-08-13 22:52:14 +0000

    canaryRelease: 431241
    Main scripts: 4
    Lazy Loaded Scripts: 1425
    Chunks:
     * Main Files: 14211
     * Scripts: 9658
    Strings: 22129
    Routes: 727
    Experiments Count: 483
    Apex Experiments Count: 19

---
+++ chunks/499513.js
@@ -0,0 +1,68 @@
+n.d(t, { Z: () => a });
+var r = n(823379),
+    i = n(131085),
+    l = n(388032);
+function a(e, t) {
+    return l.intl.formatToPlainString(l.t.g79C8f, {
+        primaryColor: s(e),
+        secondaryColor: s(t),
+    });
+}
+function s(e) {
+    switch (e) {
+        case i.KD.HOT_PINK:
+            return l.intl.string(l.t.DfOkjI);
+        case i.KD.LIGHT_PINK:
+            return l.intl.string(l.t["833OIS"]);
+        case i.KD.ORANGE:
+            return l.intl.string(l.t.i7TMkZ);
+        case i.KD.PEACH:
+            return l.intl.string(l.t.uHgff3);
+        case i.KD.GOLD:
+            return l.intl.string(l.t.EpIKg4);
+        case i.KD.LIGHT_YELLOW:
+            return l.intl.string(l.t["i9+g+v"]);
+        case i.KD.TEAL:
+            return l.intl.string(l.t["m82c+f"]);
+        case i.KD.MINT_GREEN:
+            return l.intl.string(l.t.xcoOBQ);
+        case i.KD.BLUE_TEAL:
+            return l.intl.string(l.t["5QmjOD"]);
+        case i.KD.LIGHT_BLUE:
+            return l.intl.string(l.t["/MyjZW"]);
+        case i.KD.PURPLE:
+            return l.intl.string(l.t["jp+PMj"]);
+        case i.KD.LAVENDER:
+            return l.intl.string(l.t.aQjtam);
+        case i.KD.VIOLET:
+            return l.intl.string(l.t.WSnCYG);
+        case i.KD.MAUVE:
+            return l.intl.string(l.t.wh9c5e);
+        case i.KD.DEEP_PURPLE:
+            return l.intl.string(l.t.TP5bJi);
+        case i.KD.ORCHID:
+            return l.intl.string(l.t.jFf82N);
+        case i.KD.RED:
+            return l.intl.string(l.t.yUcPHx);
+        case i.KD.SALMON:
+            return l.intl.string(l.t["+HA2GR"]);
+        case i.KD.BROWN:
+            return l.intl.string(l.t.PI13GB);
+        case i.KD.TAN:
+            return l.intl.string(l.t.hZU6aW);
+        case i.KD.OLIVE:
+            return l.intl.string(l.t["1lKfMT"]);
+        case i.KD.GRAY:
+            return l.intl.string(l.t["6Gh+v7"]);
+        case i.KD.BURGUNDY:
+            return l.intl.string(l.t.pvqjJi);
+        case i.KD.ROSE:
+            return l.intl.string(l.t.RMfRPz);
+        case i.KD.DARK_GRAY:
+            return l.intl.string(l.t.Ts4j0N);
+        case i.KD.LIGHT_GRAY:
+            return l.intl.string(l.t.ZBQ1JS);
+        default:
+            (0, r.vE)(e);
+    }
+}
