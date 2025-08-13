7ce739153a00b7024b2ca03a7f91e18e249ca88b
DiscrapperManager <admin@minerpl.xyz>
2025-08-13 22:24:32 +0000

    canaryRelease: 431214
    Main scripts: 4
    Lazy Loaded Scripts: 1424
    Chunks:
     * Main Files: 14209
     * Scripts: 9657
    Strings: 22129
    Routes: 727
    Experiments Count: 483
    Apex Experiments Count: 19

---
+++ chunks/271302.js
@@ -0,0 +1,14 @@
+n.d(t, { Z: () => r });
+let r = (0, n(987170).Z)({
+    kind: "user",
+    id: "2025-08_next_channel",
+    label: "Voice Channel Move Behavior Adjustment",
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: "Enabled.",
+            config: { enabled: !0 },
+        },
+    ],
+});
