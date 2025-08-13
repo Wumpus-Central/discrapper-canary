ba9b48fec0312fa597582f8dea3b2346963a9180
DiscrapperManager <admin@minerpl.xyz>
2025-08-13 21:43:40 +0000

    canaryRelease: 431176
    Main scripts: 4
    Lazy Loaded Scripts: 1424
    Chunks:
     * Main Files: 14207
     * Scripts: 9658
    Strings: 22128
    Routes: 727
    Experiments Count: 482
    Apex Experiments Count: 19

---
+++ chunks/277537.js
@@ -0,0 +1,17 @@
+n.d(t, { yI: () => l });
+var r = n(818083),
+    i = n(987338);
+let l = (0, r.B)({
+    kind: "user",
+    id: "2025-07_family_center_v2",
+    label: "Family Center V2",
+    defaultConfig: { enabled: !1 },
+    commonTriggerPoint: i.$P.CONNECTION_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: "Enabled",
+            config: { enabled: !0 },
+        },
+    ],
+});
--- chunks/916723.js
+++ chunks/916723.js
@@ -1,10 +1,10 @@
-n.r(e),
-    n.d(e, {
-        default: () => a,
-        messagesLoader: () => r,
+n.r(t),
+    n.d(t, {
+        default: () => b,
+        messagesLoader: () => i,
     });
-let { createLoader: i } = n(125697),
-    r = i(
+let { createLoader: h } = n(125697),
+    i = h(
         {
             bg: () => n.e("29162").then(n.t.bind(n, 340984, 19)),
             cs: () => n.e("11182").then(n.t.bind(n, 950246, 19)),
@@ -40,5 +40,5 @@ let { createLoader: i } = n(125697),
         },
         "en-US",
     ),
-    { makeMessagesProxy: l } = n(125697),
-    a = l(r);
+    { makeMessagesProxy: d } = n(125697),
+    b = d(i);
