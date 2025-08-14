045146451301310d91f1afbdecf16acca9a77db3
DiscrapperManager <admin@minerpl.xyz>
2025-08-14 22:42:19 +0000

    canaryRelease: 431840
    Main scripts: 4
    Lazy Loaded Scripts: 1422
    Chunks:
     * Main Files: 14070
     * Scripts: 9411
    Strings: 22129
    Routes: 728
    Experiments Count: 485
    Apex Experiments Count: 19

---
+++ chunks/992817.js
@@ -0,0 +1,49 @@
+n.d(t, {
+    $s: () => c,
+    Bx: () => l,
+});
+var r = n(818083),
+    i = n(592125),
+    o = n(944486),
+    a = n(594174);
+let s = (0, r.B)({
+    kind: "guild",
+    id: "2025-08_go_live_guild_notification",
+    label: "Go live notification guild experiment",
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: "Go live notification on by default",
+            config: { enabled: !0 },
+        },
+        {
+            id: 2,
+            label: "Go live notification includes game data if available",
+            config: { enabled: !0 },
+        },
+    ],
+});
+function l(e) {
+    let { guildId: t, location: n } = e;
+    return (
+        null != t &&
+        s.getCurrentConfig({
+            guildId: t,
+            location: n,
+        }).enabled
+    );
+}
+function c(e, t) {
+    let n = e;
+    if (null == n) {
+        if (null == a.default.getCurrentUser()) return !1;
+        n = o.Z.getVoiceChannelId();
+    }
+    if (null == n) return !1;
+    let r = i.Z.getChannel(n);
+    return l({
+        guildId: null == r ? void 0 : r.guild_id,
+        location: t,
+    });
+}
