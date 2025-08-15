74de2024aab10be279e0643eef37d348fa87a611
DiscrapperManager <admin@minerpl.xyz>
2025-08-15 23:22:02 +0000

    canaryRelease: 432434
    Main scripts: 4
    Lazy Loaded Scripts: 1482
    Chunks:
     * Main Files: 12093
     * Scripts: 11134
    Strings: 21902
    Routes: 729
    Experiments Count: 483
    Apex Experiments Count: 21

--- chunks/462997.js
+++ chunks/462997.js
@@ -1,33 +1,33 @@
 n.d(t, { q: () => a });
-var r = n(268685),
-    i = n(671955);
+var r = n(481060),
+    i = n(228168);
 let a = {
-    [i.l.POPOUT]: {
-        avatarSize: r.EF.SIZE_80,
+    [i.lY.POPOUT]: {
+        avatarSize: r.EFr.SIZE_80,
         avatarOffsetX: 10,
         avatarOffsetY: 4,
         bannerWidth: 300,
         bannerHeight: 105,
         themePadding: 4,
     },
-    [i.l.MODAL]: {
-        avatarSize: r.EF.SIZE_120,
+    [i.lY.MODAL]: {
+        avatarSize: r.EFr.SIZE_120,
         avatarOffsetX: 16,
         avatarOffsetY: 5,
         bannerWidth: 600,
         bannerHeight: 210,
         themePadding: 0,
     },
-    [i.l.MODAL_V2]: {
-        avatarSize: r.EF.SIZE_120,
+    [i.lY.MODAL_V2]: {
+        avatarSize: r.EFr.SIZE_120,
         avatarOffsetX: 24,
         avatarOffsetY: 0,
         bannerWidth: 400,
         bannerHeight: 140,
         themePadding: 0,
     },
-    [i.l.SIDEBAR]: {
-        avatarSize: r.EF.SIZE_80,
+    [i.lY.SIDEBAR]: {
+        avatarSize: r.EFr.SIZE_80,
         avatarOffsetX: 10,
         avatarOffsetY: 8,
         bannerWidth: 340,
--- chunks/671955.js
+++
@@ -1,4 +0,0 @@
-n.d(t, { l: () => r });
-var r = (function (e) {
-    return (e.POPOUT = "POPOUT"), (e.MODAL = "MODAL"), (e.MODAL_V2 = "MODAL_V2"), (e.SIDEBAR = "SIDEBAR"), e;
-})({});
