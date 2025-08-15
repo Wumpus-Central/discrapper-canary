9b15cbeb770819e27f8f4c8bea5f1a11d4766cec
DiscrapperManager <admin@minerpl.xyz>
2025-08-15 23:47:36 +0000

    canaryRelease: 432451
    Main scripts: 4
    Lazy Loaded Scripts: 1482
    Chunks:
     * Main Files: 12095
     * Scripts: 11134
    Strings: 21902
    Routes: 729
    Experiments Count: 483
    Apex Experiments Count: 22

---
+++ chunks/582751.js
@@ -0,0 +1,11 @@
+n.d(t, { Z: () => r });
+let r = (0, n(427164).le)({
+    name: "2025-08-call-bitrate-floor",
+    kind: "user",
+    defaultConfig: { minBitrate: 150000 },
+    variations: {
+        1: { minBitrate: 16000 },
+        2: { minBitrate: 32000 },
+        3: { minBitrate: 64000 },
+    },
+});
---
+++ chunks/671955.js
@@ -0,0 +1,4 @@
+n.d(t, { l: () => r });
+var r = (function (e) {
+    return (e.POPOUT = "POPOUT"), (e.MODAL = "MODAL"), (e.MODAL_V2 = "MODAL_V2"), (e.SIDEBAR = "SIDEBAR"), e;
+})({});
