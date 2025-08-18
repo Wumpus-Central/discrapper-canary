cb115f9d779895225e4a0aea991d96ca381cd4ff
DiscrapperManager <admin@minerpl.xyz>
2025-08-18 20:05:03 +0000

    canaryRelease: 432794
    Main scripts: 4
    Lazy Loaded Scripts: 1483
    Chunks:
     * Main Files: 12106
     * Scripts: 11128
    Strings: 21907
    Routes: 730
    Experiments Count: 483
    Apex Experiments Count: 24

---
+++ chunks/374291.js
@@ -0,0 +1,3 @@
+n.d(t, { Z: () => r });
+let r =
+    "https://cdn.discordapp.com/assets/content/88c4e6455eee306909f9e4cb2efae503fff53f7b80cd0dcfb0e354efdec25042.svg";
---
+++ chunks/708154.js
@@ -0,0 +1,52 @@
+n.d(t, { Z: () => d });
+var r = n(951288);
+n(647438);
+var i = n(680018),
+    a = n(481060),
+    o = n(550385),
+    s = n(119475),
+    l = n(388032),
+    c = n(574876),
+    u = n(374291);
+function d() {
+    let e = () => {
+        (0, o.XO)(o.wh.CUSTOM_THEME, { from: o.tE.CLIENT_THEMES_EDITOR });
+    };
+    return (0, r.jsxs)("div", {
+        className: c.container,
+        children: [
+            (0, r.jsxs)("div", {
+                className: c.topContent,
+                children: [
+                    (0, r.jsx)("img", {
+                        src: u.Z,
+                        className: c.bannerImage,
+                        alt: "",
+                    }),
+                    (0, r.jsxs)("div", {
+                        className: c.textContent,
+                        children: [
+                            (0, r.jsx)(a.Text, {
+                                variant: "text-md/semibold",
+                                color: "text-primary",
+                                children: l.intl.string(s.default.XP4jzM),
+                            }),
+                            (0, r.jsx)(a.Text, {
+                                variant: "text-sm/medium",
+                                color: "text-tertiary",
+                                children: l.intl.string(s.default.MJYvmJ),
+                            }),
+                        ],
+                    }),
+                ],
+            }),
+            (0, r.jsx)(i.z, {
+                variant: "secondary",
+                size: "sm",
+                text: l.intl.string(s.default.eGxkmp),
+                onClick: e,
+                fullWidth: !0,
+            }),
+        ],
+    });
+}
