385245702a20695f0c97c0b4e6320e71c1f8eb65
DiscrapperManager <admin@minerpl.xyz>
2025-08-18 17:51:19 +0000

    canaryRelease: 432690
    Main scripts: 4
    Lazy Loaded Scripts: 1480
    Chunks:
     * Main Files: 12100
     * Scripts: 11131
    Strings: 21904
    Routes: 730
    Experiments Count: 483
    Apex Experiments Count: 24

--- chunks/319719.js
+++
@@ -1,9 +0,0 @@
-e.exports = {
-    searchBox: "searchBox_d727b3",
-    searchBoxInput: "searchBoxInput_d727b3",
-    searchBoxInputWrapper: "searchBoxInputWrapper_d727b3",
-    cta: "cta_d727b3",
-    clear: "clear_d727b3",
-    clearIcon: "clearIcon_d727b3",
-    searchIcon: "searchIcon_d727b3",
-};
