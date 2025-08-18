8984bb59a1f24c4d3a64679079a3c43c1b0276a7
DiscrapperManager <admin@minerpl.xyz>
2025-08-18 16:38:55 +0000

    canaryRelease: 432626
    Main scripts: 4
    Lazy Loaded Scripts: 1482
    Chunks:
     * Main Files: 12099
     * Scripts: 11137
    Strings: 21904
    Routes: 730
    Experiments Count: 484
    Apex Experiments Count: 24

---
+++ chunks/220410.js
@@ -0,0 +1,10 @@
+n.d(t, { T: () => r });
+let r = (0, n(427164).le)({
+    name: "2025-08-game-detection-v2",
+    kind: "user",
+    defaultConfig: { useDetectionV2: !1 },
+    variations: {
+        0: { useDetectionV2: !1 },
+        1: { useDetectionV2: !0 },
+    },
+});
