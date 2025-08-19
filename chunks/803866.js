4708b2d110d30d31987472528e2ad178aed75019
DiscrapperManager <admin@minerpl.xyz>
2025-08-19 17:55:44 +0000

    canaryRelease: 433322
    Main scripts: 4
    Lazy Loaded Scripts: 1484
    Chunks:
     * Main Files: 12113
     * Scripts: 11133
    Strings: 21945
    Routes: 730
    Experiments Count: 485
    Apex Experiments Count: 26

---
+++ chunks/803866.js
@@ -0,0 +1,55 @@
+n.d(t, { N: () => d });
+var r = n(951288),
+    i = n(647438),
+    a = n(146187),
+    o = n(681803),
+    s = n(388032),
+    l = n(37751);
+function c(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0,
+              })
+            : (e[t] = n),
+        e
+    );
+}
+function u(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        "function" == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                }),
+            )),
+            r.forEach(function (t) {
+                c(e, t, n[t]);
+            });
+    }
+    return e;
+}
+function d(e) {
+    let { onClick: t, "aria-label": n = s.intl.string(s.t.cpT0Cg) } = e,
+        { firstFocusableItemProps: c } = i.useContext(o.M);
+    return (0, r.jsx)("div", {
+        className: l.closeButton,
+        children: (0, r.jsx)(
+            a.P,
+            u(
+                {
+                    size: "xs",
+                    "aria-label": n,
+                    onClick: t,
+                    variant: "overlay-secondary",
+                },
+                c,
+            ),
+        ),
+    });
+}
