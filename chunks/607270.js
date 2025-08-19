fc5c2c6e09d9df00262b62af30229ee780e7225b
DiscrapperManager <admin@minerpl.xyz>
2025-08-19 17:50:53 +0000

    canaryRelease: 433315
    Main scripts: 4
    Lazy Loaded Scripts: 1484
    Chunks:
     * Main Files: 12112
     * Scripts: 11133
    Strings: 21945
    Routes: 730
    Experiments Count: 485
    Apex Experiments Count: 26

---
+++ chunks/607270.js
@@ -0,0 +1,50 @@
+n.d(t, { Z: () => m });
+var i = n(951288);
+n(647438);
+var r = n(680018),
+    s = n(481060),
+    a = n(37234),
+    l = n(550385),
+    o = n(119475),
+    c = n(388032),
+    d = n(640744),
+    u = n(626869);
+function m() {
+    return (0, i.jsxs)("div", {
+        className: d.container,
+        children: [
+            (0, i.jsx)("div", {
+                className: d.iconContainer,
+                children: (0, i.jsx)("img", {
+                    src: u.Z,
+                    className: d.bannerImage,
+                    alt: "",
+                }),
+            }),
+            (0, i.jsxs)("div", {
+                className: d.textContent,
+                children: [
+                    (0, i.jsx)(s.X6q, {
+                        variant: "heading-lg/semibold",
+                        color: "text-primary",
+                        children: c.intl.string(o.default.CARxAA),
+                    }),
+                    (0, i.jsx)(s.Text, {
+                        variant: "text-sm/normal",
+                        children: c.intl.string(o.default.nla4RE),
+                    }),
+                ],
+            }),
+            (0, i.jsx)("div", {
+                className: d.buttonContainer,
+                children: (0, i.jsx)(r.z, {
+                    variant: "primary",
+                    text: c.intl.string(c.t.uw9zIy),
+                    onClick: () => {
+                        (0, l.XO)(l.wh.CUSTOM_THEME, { from: l.tE.SETTING }), (0, a.xf)();
+                    },
+                }),
+            }),
+        ],
+    });
+}
