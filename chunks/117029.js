1e5fc3e149e44b0ac9afaf0e5d220e5592f4f380
DiscrapperManager <admin@minerpl.xyz>
2025-08-19 16:10:16 +0000

    canaryRelease: 433185
    Main scripts: 4
    Lazy Loaded Scripts: 1484
    Chunks:
     * Main Files: 12112
     * Scripts: 11129
    Strings: 21946
    Routes: 730
    Experiments Count: 484
    Apex Experiments Count: 26

---
+++ chunks/117029.js
@@ -0,0 +1,82 @@
+r.d(t, {
+    J: () => d,
+    K: () => u,
+}),
+    r(388685),
+    r(472816),
+    r(794429);
+var n = r(647438),
+    l = r(442837),
+    i = r(669764),
+    a = r(592183),
+    o = r(224724),
+    c = r(938236),
+    s = r(747101);
+function u(e) {
+    var t, r;
+    let { bump: l, bumpMultiple: a, gameIds: o } = (0, c.b)(),
+        { remove: u, peekedGameIds: d } = (0, c.b)(),
+        { gameDataMap: f, isGameFetching: g } = (0, s.F)(null != (t = d[e]) ? t : []);
+    n.useEffect(() => {
+        var t;
+        for (let r of null != (t = d[e]) ? t : []) {
+            let t = f[r];
+            (null == t ? void 0 : t.coverImageUrl) != null || g(r) || u(r, e);
+        }
+    }, [d, f, g, u, e]);
+    let p = n.useMemo(() => {
+            var t;
+            return null != (t = o[e]) ? t : [];
+        }, [o, e]),
+        b = n.useCallback(
+            (t) => {
+                l(t, e);
+            },
+            [l, e],
+        ),
+        { gameDataMap: O, isGameFetching: m } = (0, s.F)(p),
+        [j, y] = n.useState([]),
+        v = n.useCallback((e) => i.Z.noDataAvailable(e), []),
+        x = ((r = p.map((e) => m(e))), n.useMemo(() => r.join("\x1F"), [r]));
+    return (
+        n.useEffect(() => {
+            let t = p.filter((e) => v(e));
+            t.length > 0 && a(t, e);
+        }, [O, x, p, e, a, v]),
+        n.useEffect(() => {
+            y(
+                p.map((e) => {
+                    let t = O[e];
+                    return {
+                        applicationId: e,
+                        gameName: null == t ? void 0 : t.name,
+                        imageSrc: null == t ? void 0 : t.coverImageUrl,
+                    };
+                }),
+            );
+        }, [p, O, e]),
+        {
+            games: j,
+            isGameFetching: m,
+            onAddGame: b,
+        }
+    );
+}
+function d(e, t) {
+    let [r, i, s, u] = (0, l.Wu)([o.Z], () => [
+            o.Z.suggestedFetchAttempted,
+            o.Z.suggestedFetchError,
+            o.Z.suggestedGameIds,
+            o.Z.suggestedFetchIsLoading,
+        ]),
+        { onLoad: d } = (0, c.b)();
+    n.useEffect(() => {
+        !r && e && a.Z.fetchSuggestedGames();
+    }, [r, e]);
+    let f = r && !u;
+    n.useEffect(() => {
+        if (!f) return;
+        let e = t.map((e) => e.games).flat();
+        d(i ? [] : s.suggestedGamesIds, i ? [] : s.suggestedWishlistGamesIds, e);
+    }, [f]);
+}
---
+++ chunks/938236.js
@@ -0,0 +1,108 @@
+r.d(t, { b: () => u }), r(388685);
+var n = r(392711),
+    l = r(879690),
+    i = r(296009),
+    a = r(804919);
+function o(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var r = null != arguments[t] ? arguments[t] : {},
+            n = Object.keys(r);
+        "function" == typeof Object.getOwnPropertySymbols &&
+            (n = n.concat(
+                Object.getOwnPropertySymbols(r).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(r, e).enumerable;
+                }),
+            )),
+            n.forEach(function (t) {
+                var n;
+                (n = r[t]),
+                    t in e
+                        ? Object.defineProperty(e, t, {
+                              value: n,
+                              enumerable: !0,
+                              configurable: !0,
+                              writable: !0,
+                          })
+                        : (e[t] = n);
+            });
+    }
+    return e;
+}
+function c(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : (function (e, t) {
+                  var r = Object.keys(e);
+                  if (Object.getOwnPropertySymbols) {
+                      var n = Object.getOwnPropertySymbols(e);
+                      r.push.apply(r, n);
+                  }
+                  return r;
+              })(Object(t)).forEach(function (r) {
+                  Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r));
+              }),
+        e
+    );
+}
+let s = Object.values((0, n.sampleSize)(a.qs, a.qs.length)),
+    u = (0, l.U)((e, t) => ({
+        stack: [],
+        wishlistStack: [],
+        gameIds: {},
+        peekedGameIds: {},
+        onLoad: (r, n, l) => {
+            let a = new Set(l.map((e) => e.applicationId));
+            for (let l of (e({
+                stack: [...r.filter((e) => !a.has(e)), ...s],
+                wishlistStack: [...n.filter((e) => !a.has(e)), ...s],
+            }),
+            Object.values(i.l)))
+                t().setNext(4, l);
+        },
+        setNext: (e, r) => {
+            let n = t().getNext(e, r);
+            t()._setGameIds(r, n);
+            let l = t().peekNext(5, r);
+            t()._setPeekedGameIds(r, l);
+        },
+        getNext: (e, r) => {
+            let n = r === i.l.WANT_TO_PLAY_GAMES ? t().wishlistStack : t().stack,
+                l = n.slice(0, e),
+                a = n.slice(e);
+            return t()._setStack(r, a), l;
+        },
+        peekNext: (e, r) => (r === i.l.WANT_TO_PLAY_GAMES ? t().wishlistStack : t().stack).slice(0, e),
+        bump: (e, r) => {
+            var n;
+            let l = null != (n = t().gameIds[r]) ? n : [],
+                i = l.indexOf(e);
+            if (-1 === i) return;
+            let a = [...l];
+            a.splice(i, 1);
+            let o = t().getNext(1, r),
+                c = t().peekNext(5, r);
+            t()._setGameIds(r, [...a, ...o]), t()._setPeekedGameIds(r, [...c, ...o]);
+        },
+        bumpMultiple: (e, r) => {
+            var n;
+            let l = (null != (n = t().gameIds[r]) ? n : []).filter((t) => !e.includes(t)),
+                i = t().getNext(4 - l.length, r),
+                a = t().peekNext(5, r);
+            t()._setGameIds(r, [...l, ...i]), t()._setPeekedGameIds(r, [...a, ...i]);
+        },
+        remove: (e, r) => {
+            let n = (r === i.l.WANT_TO_PLAY_GAMES ? t().wishlistStack : t().stack).filter((t) => t !== e);
+            t()._setStack(r, n), t()._setPeekedGameIds(r, t().peekNext(5, r));
+        },
+        _setGameIds: (t, r) => {
+            e((e) => ({ gameIds: c(o({}, e.gameIds), { [t]: r }) }));
+        },
+        _setStack: (t, r) => {
+            t === i.l.WANT_TO_PLAY_GAMES ? e({ wishlistStack: r }) : e({ stack: r });
+        },
+        _setPeekedGameIds: (t, r) => {
+            e((e) => ({ peekedGameIds: c(o({}, e.peekedGameIds), { [t]: r }) }));
+        },
+    }));
