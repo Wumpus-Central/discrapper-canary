ab4a46114e574b3aa352e3afc996ef924d0b3954
DiscrapperManager <admin@minerpl.xyz>
2025-08-20 18:50:38 +0000

    canaryRelease: 434335
    Main scripts: 4
    Lazy Loaded Scripts: 1388
    Chunks:
     * Main Files: 14825
     * Scripts: 8411
    Strings: 21975
    Routes: 730
    Experiments Count: 486
    Apex Experiments Count: 29

---
+++ chunks/535246.js
@@ -0,0 +1,10 @@
+n.d(t, { l: () => r });
+let r = (0, n(427164).le)({
+    name: "2025-08-remote-join-game-invite",
+    kind: "user",
+    defaultConfig: { enableRemoteJoin: !1 },
+    variations: {
+        0: { enableRemoteJoin: !1 },
+        1: { enableRemoteJoin: !0 },
+    },
+});
