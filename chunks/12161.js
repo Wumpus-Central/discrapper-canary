920af0b0f97884c0efcd6fb4720b7ddb1f3fc9c2
DiscrapperManager <admin@minerpl.xyz>
2025-08-21 15:55:45 +0000

    canaryRelease: 435236
    Main scripts: 4
    Lazy Loaded Scripts: 1358
    Chunks:
     * Main Files: 15017
     * Scripts: 8620
    Strings: 21983
    Routes: 731
    Experiments Count: 487
    Apex Experiments Count: 30

---
+++ chunks/12161.js
@@ -0,0 +1,17 @@
+n.d(t, { Z: () => l });
+var r = n(818083),
+    i = n(987338);
+let l = (0, r.B)({
+    kind: "user",
+    id: "2025-08_double_tap_to_react",
+    label: "Double Tap To React",
+    defaultConfig: { enabled: !1 },
+    commonTriggerPoint: i.$P.CONNECTION_OPEN,
+    treatments: [
+        {
+            id: 1,
+            label: "Enabled",
+            config: { enabled: !0 },
+        },
+    ],
+});
