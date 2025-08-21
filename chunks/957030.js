65be908a932132b17da0d97cb021a0a55912b3e9
DiscrapperManager <admin@minerpl.xyz>
2025-08-21 23:59:36 +0000

    canaryRelease: 435788
    Main scripts: 4
    Lazy Loaded Scripts: 1388
    Chunks:
     * Main Files: 15031
     * Scripts: 8651
    Strings: 22006
    Routes: 730
    Experiments Count: 486
    Apex Experiments Count: 30

---
+++ chunks/957030.js
@@ -0,0 +1,29 @@
+n.d(t, { Z: () => d });
+var r = n(951288),
+    i = n(442837),
+    a = n(481060),
+    o = n(256638),
+    s = n(594190),
+    l = n(865066),
+    c = n(981631),
+    u = n(388032);
+function d() {
+    let e = (0, i.e7)([s.ZP], () => s.ZP.getVisibleGame());
+    return null == e
+        ? null
+        : (0, r.jsxs)(a.qXd, {
+              color: a.DM8.DANGER,
+              children: [
+                  (0, r.jsx)(a.RyX, {
+                      noticeType: c.kVF.SYSTEM_SERVICE_WARNING,
+                      onClick: () => (0, o.c)(),
+                  }),
+                  u.intl.format(u.t.kxePRk, { gameName: e.name }),
+                  (0, r.jsx)(a.NoS, {
+                      onClick: l.sU,
+                      noticeType: c.kVF.SYSTEM_SERVICE_WARNING,
+                      children: u.intl.string(u.t["1iI46O"]),
+                  }),
+              ],
+          });
+}
