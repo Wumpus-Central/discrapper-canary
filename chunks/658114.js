a3a2318c511bcead490898cc73509c0afb84f39d
DiscrapperManager <admin@minerpl.xyz>
2025-08-22 00:20:23 +0000

    canaryRelease: 435804
    Main scripts: 4
    Lazy Loaded Scripts: 1388
    Chunks:
     * Main Files: 15034
     * Scripts: 8651
    Strings: 22007
    Routes: 730
    Experiments Count: 486
    Apex Experiments Count: 30

---
+++ chunks/658114.js
@@ -0,0 +1,20 @@
+n.d(t, { Z: () => l });
+var r = n(951288),
+    i = n(793030),
+    a = n(692547),
+    o = n(481060),
+    s = n(606610);
+function l(e) {
+    let { message: t } = e;
+    return (0, r.jsxs)("div", {
+        className: s.warningContainer,
+        children: [
+            (0, r.jsx)(o.aNP, { color: a.Z.colors.ICON_PRIMARY }),
+            (0, r.jsx)(i.xv, {
+                variant: "text-sm/medium",
+                color: "text-feedback-warning",
+                children: t,
+            }),
+        ],
+    });
+}
---
+++ chunks/709113.js
@@ -0,0 +1,11 @@
+n.d(t, { Z: () => l });
+var r = n(951288);
+n(647438);
+var i = n(445794),
+    a = n(563132),
+    o = n(658114),
+    s = n(388032);
+function l() {
+    let { selectedSkuId: e } = (0, a.JL)();
+    return (0, i.N3)({ skuId: e }) ? (0, r.jsx)(o.Z, { message: s.intl.format(s.t.fsOXXF, {}) }) : null;
+}
