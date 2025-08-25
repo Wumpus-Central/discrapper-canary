bd56a65e7a47f483739f2d138053ecd8aa9658cc
DiscrapperManager <admin@minerpl.xyz>
2025-08-25 16:03:49 +0000

    canaryRelease: 436554
    Main scripts: 4
    Lazy Loaded Scripts: 1383
    Chunks:
     * Main Files: 15066
     * Scripts: 8597
    Strings: 22041
    Routes: 730
    Experiments Count: 484
    Apex Experiments Count: 31

--- chunks/607270.js
+++ chunks/607270.js
@@ -1,49 +1,58 @@
-n.d(t, { Z: () => f });
+n.d(t, { Z: () => h });
 var r = n(951288);
 n(647438);
-var i = n(680018),
-    a = n(481060),
-    o = n(37234),
-    s = n(550385),
-    l = n(119475),
-    c = n(388032),
-    u = n(640744),
-    d = n(626869);
-function f() {
-    let e = () => {
-        (0, s.XO)(s.wh.CUSTOM_THEME, { from: s.tE.SETTING }), (0, o.xf)();
+var i = n(990547),
+    a = n(680018),
+    o = n(481060),
+    s = n(37234),
+    l = n(906732),
+    c = n(213609),
+    u = n(550385),
+    d = n(119475),
+    f = n(388032),
+    _ = n(640744),
+    p = n(758335);
+function h() {
+    let { analyticsLocations: e } = (0, l.ZP)();
+    (0, c.Z)({
+        name: i.ImpressionNames.CUSTOM_THEMES_APPEARANCE_SETTINGS_BANNER,
+        type: i.ImpressionTypes.VIEW,
+        properties: { location_stack: e },
+    });
+    let t = () => {
+        (0, u.XO)(u.wh.CUSTOM_THEME, { from: u.tE.SETTING }), (0, s.xf)();
     };
     return (0, r.jsxs)("div", {
-        className: u.container,
+        className: _.container,
         children: [
             (0, r.jsx)("div", {
-                className: u.iconContainer,
+                className: _.iconContainer,
                 children: (0, r.jsx)("img", {
-                    src: d.Z,
-                    className: u.bannerImage,
+                    src: p.Z,
+                    className: _.bannerImage,
                     alt: "",
                 }),
             }),
             (0, r.jsxs)("div", {
-                className: u.textContent,
+                className: _.textContent,
                 children: [
-                    (0, r.jsx)(a.X6q, {
+                    (0, r.jsx)(o.X6q, {
                         variant: "heading-lg/semibold",
                         color: "text-primary",
-                        children: c.intl.string(l.default.CARxAA),
+                        children: f.intl.string(d.default.CARxAA),
                     }),
-                    (0, r.jsx)(a.Text, {
+                    (0, r.jsx)(o.Text, {
                         variant: "text-sm/normal",
-                        children: c.intl.string(l.default.nla4RE),
+                        children: f.intl.string(d.default.nla4RE),
                     }),
                 ],
             }),
             (0, r.jsx)("div", {
-                className: u.buttonContainer,
-                children: (0, r.jsx)(i.z, {
+                className: _.buttonContainer,
+                children: (0, r.jsx)(a.z, {
                     variant: "primary",
-                    text: c.intl.string(c.t.uw9zIy),
-                    onClick: e,
+                    text: f.intl.string(f.t.uw9zIy),
+                    onClick: t,
                 }),
             }),
         ],
