6851406ebf5d969400b4a3ab6d4ae2edf99e6731
DiscrapperManager <admin@minerpl.xyz>
2025-08-25 19:21:41 +0000

    canaryRelease: 436733
    Main scripts: 4
    Lazy Loaded Scripts: 1382
    Chunks:
     * Main Files: 15069
     * Scripts: 8597
    Strings: 22042
    Routes: 730
    Experiments Count: 485
    Apex Experiments Count: 31

--- chunks/190014.js
+++ chunks/190014.js
@@ -2,51 +2,49 @@ n.d(t, { Z: () => b }), n(388685);
 var r = n(951288),
     i = n(647438),
     a = n(626135),
-    o = n(63063),
-    s = n(88658),
-    l = n(695346),
-    c = n(639814),
-    u = n(838436),
-    d = n(51331),
-    f = n(703115),
-    _ = n(526761),
-    p = n(726985),
-    h = n(736530),
-    m = n(981631),
-    g = n(388032);
+    o = n(88658),
+    s = n(695346),
+    l = n(639814),
+    c = n(838436),
+    u = n(51331),
+    d = n(703115),
+    f = n(526761),
+    _ = n(726985),
+    p = n(736530),
+    h = n(981631),
+    m = n(388032),
+    g = n(933554);
 function E(e) {
     let { ingress: t, guildId: n } = e,
-        c = l.iH.useSetting().includes(n),
-        _ = i.useCallback(
+        l = s.iH.useSetting().includes(n),
+        f = i.useCallback(
             (e) => {
-                let r = (0, s.rr)();
+                let r = (0, o.rr)();
                 e ? r.delete(n) : r.add(n),
-                    l.iH.updateSetting([...r]),
-                    a.default.track(m.rMx.USER_SERVER_PRIVACY_SETTINGS_ACTION, {
-                        action: f.Y.RESTRICT_GUILD_ACTIVITY_JOIN_TOGGLE,
+                    s.iH.updateSetting([...r]),
+                    a.default.track(h.rMx.USER_SERVER_PRIVACY_SETTINGS_ACTION, {
+                        action: d.Y.RESTRICT_GUILD_ACTIVITY_JOIN_TOGGLE,
                         ingress: t,
                         guild_id: n,
                     });
             },
             [t, n],
         );
-    return n === h.T
+    return n === p.T
         ? null
-        : (0, r.jsx)(u.U, {
-              setting: p.s6.PRIVACY_SERVER_SPECIFIC_PRIVACY_ACTIVITY_JOINING_V2,
-              children: (0, r.jsx)(d.Z, {
-                  title: g.intl.string(g.t["T+nevL"]),
-                  value: !c,
-                  onChange: _,
-                  note: g.intl.format(g.t.jXKQCg, {
-                      helpdeskArticle: o.Z.getArticleURL(m.BhN.ACTIVITY_STATUS_SETTINGS),
-                  }),
+        : (0, r.jsx)(c.U, {
+              setting: _.s6.PRIVACY_SERVER_SPECIFIC_PRIVACY_ACTIVITY_JOINING_V2,
+              children: (0, r.jsx)(u.Z, {
+                  title: m.intl.string(g.default.khuuzs),
+                  value: !l,
+                  onChange: f,
+                  note: m.intl.string(g.default["8EWsJy"]),
               }),
           });
 }
 function b(e) {
-    let { ingress: t = _.f4.USER_SETTINGS_PRIVACY_SAFETY, guildId: n } = e,
-        i = (0, c.is)();
+    let { ingress: t = f.f4.USER_SETTINGS_PRIVACY_SAFETY, guildId: n } = e,
+        i = (0, l.is)();
     return (0, r.jsx)(E, {
         ingress: t,
         guildId: null != n ? n : i,
