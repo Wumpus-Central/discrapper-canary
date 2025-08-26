e6773da9778d676983d32a3d429bc319728353b5
DiscrapperManager <admin@minerpl.xyz>
2025-08-26 19:56:14 +0000

    canaryRelease: 437368
    Main scripts: 4
    Lazy Loaded Scripts: 1381
    Chunks:
     * Main Files: 14710
     * Scripts: 8583
    Strings: 22055
    Routes: 727
    Experiments Count: 475
    Apex Experiments Count: 32

--- chunks/450279.js
+++ chunks/450279.js
@@ -13,7 +13,7 @@ function i(e, t, n) {
         e
     );
 }
-function a(e) {
+function o(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -29,7 +29,7 @@ function a(e) {
     }
     return e;
 }
-function o(e, t) {
+function a(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -46,7 +46,7 @@ function s(e, t) {
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : o(Object(t)).forEach(function (n) {
+            : a(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
@@ -61,31 +61,31 @@ function l(e) {
 function c(e) {
     return null === e || 0 === e;
 }
-function u(e, t, n, i, o) {
-    var u, d;
-    let f = {},
-        _ = {},
-        p = [],
-        h = [];
+function u(e, t, n, i, a) {
+    var u, d, f, _;
+    let p = {},
+        h = {},
+        m = [],
+        g = [];
     for (let t of e.values())
         switch (t.type) {
             case "candidate-pair":
-                f[t.id] = t;
+                p[t.id] = t;
                 break;
             case "codec":
-                _[t.id] = t;
+                h[t.id] = t;
                 break;
             case "inbound-rtp":
-                p.push(t);
+                m.push(t);
                 break;
             case "outbound-rtp":
-                h.push(t);
+                g.push(t);
         }
-    let m = Object.values(f).find((e) => "succeeded" === e.state);
-    if (void 0 === m) return null;
-    let g = [];
-    for (let e of h) {
-        let t = _[e.codecId];
+    let E = Object.values(p).find((e) => "succeeded" === e.state);
+    if (void 0 === E) return null;
+    let b = [];
+    for (let e of g) {
+        let t = h[e.codecId];
         if (null == t) continue;
         let i = {
             type: e.kind,
@@ -98,8 +98,8 @@ function u(e, t, n, i, o) {
             packetsSent: e.packetsSent,
             bitrateTarget: e.targetBitrate,
         };
-        if ("audio" === e.kind) g.push(s(a({}, i), { type: "audio" }));
-        else if ("video" === e.kind && o) {
+        if ("audio" === e.kind) b.push(s(o({}, i), { type: "audio" }));
+        else if ("video" === e.kind && a) {
             let t =
                 null !== e.frameWidth
                     ? {
@@ -107,8 +107,8 @@ function u(e, t, n, i, o) {
                           height: e.frameHeight,
                       }
                     : void 0;
-            g.push(
-                s(a({}, i), {
+            b.push(
+                s(o({}, i), {
                     framesEncoded: e.framesEncoded,
                     keyFramesEncoded: e.keyFramesEncoded,
                     firCount: e.firCount,
@@ -127,10 +127,10 @@ function u(e, t, n, i, o) {
             );
         }
     }
-    let E = {};
-    for (let e of p) {
-        let o = _[e.codecId];
-        if (null == o) continue;
+    let y = {};
+    for (let e of m) {
+        let a = h[e.codecId];
+        if (null == a) continue;
         let c = t(e.ssrc);
         if (null == c) continue;
         let u = {
@@ -141,7 +141,7 @@ function u(e, t, n, i, o) {
             sinkWantAsInt: (0, r.F)(n, e.ssrc),
             sinkWantLocal: (0, r.f)(i, e.ssrc, "video" === e.kind),
             sinkWantLocalAsInt: (0, r.F)(i, e.ssrc),
-            codec: l(o),
+            codec: l(a),
             bytesReceived: e.bytesReceived,
             packetsReceived: e.packetsReceived,
             packetsLost: e.packetsLost,
@@ -152,16 +152,16 @@ function u(e, t, n, i, o) {
                 void 0 !== e.jitterBufferDelay && void 0 !== e.jitterBufferEmittedCount
                     ? Math.round((1000 * e.jitterBufferDelay) / e.jitterBufferEmittedCount)
                     : 0;
-            null == E[c] && (E[c] = []),
-                E[c].push(
-                    s(a({}, u), {
+            null == y[c] && (y[c] = []),
+                y[c].push(
+                    s(o({}, u), {
                         audioLevel: e.audioLevel,
                         jitter: 1000 * e.jitter,
                         jitterBuffer: t,
                     }),
                 );
         } else if ("video" === e.kind) {
-            null == E[c] && (E[c] = []);
+            null == y[c] && (y[c] = []);
             let t =
                 null !== e.frameWidth
                     ? {
@@ -169,8 +169,8 @@ function u(e, t, n, i, o) {
                           height: e.frameHeight,
                       }
                     : void 0;
-            E[c].push(
-                s(a({}, u), {
+            y[c].push(
+                s(o({}, u), {
                     resolution: t,
                     framesDecoded: e.framesDecoded,
                     keyFramesDecoded: e.keyFramesDecoded,
@@ -193,17 +193,20 @@ function u(e, t, n, i, o) {
             );
         }
     }
-    let b = (null != (u = m.currentRoundTripTime) ? u : 0) * 1000;
+    let O =
+            "firefox" === (null != (u = platform.name) ? u : "unknown").toLowerCase() &&
+            142 === parseInt(null != (d = platform.version) ? d : "", 10),
+        v = (null != (f = E.currentRoundTripTime) ? f : 0) * (O ? 1 : 1000);
     return {
         transport: {
-            availableOutgoingBitrate: null != (d = m.availableOutgoingBitrate) ? d : 0,
-            bytesReceived: m.bytesReceived,
-            bytesSent: m.bytesSent,
-            ping: b,
+            availableOutgoingBitrate: null != (_ = E.availableOutgoingBitrate) ? _ : 0,
+            bytesReceived: E.bytesReceived,
+            bytesSent: E.bytesSent,
+            ping: v,
         },
         rtp: {
-            inbound: E,
-            outbound: g,
+            inbound: y,
+            outbound: b,
         },
     };
 }
