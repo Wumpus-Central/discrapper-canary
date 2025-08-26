7b08fdf7d278b694502eb143a5d800c11ef8bd29
DiscrapperManager <admin@minerpl.xyz>
2025-08-26 17:54:37 +0000

    canaryRelease: 437244
    Main scripts: 4
    Lazy Loaded Scripts: 1381
    Chunks:
     * Main Files: 14713
     * Scripts: 8580
    Strings: 22049
    Routes: 727
    Experiments Count: 482
    Apex Experiments Count: 31

--- chunks/393809.js
+++ chunks/393809.js
@@ -1,100 +1,57 @@
-n.d(r, { default: () => T }), n(388685);
-var e = n(951288),
-    i = n(647438),
-    a = n(755721),
-    o = n(481060),
-    c = n(53365),
-    s = n(377176),
-    l = n(388032),
-    E = n(690558);
-function T(t) {
-    let { guildId: r, transitionState: n, onClose: T } = t,
-        [_, A] = i.useState(!1),
-        [N, d] = i.useState(!1),
-        u = async () => {
-            A(!0);
+n.d(r, { default: () => l }), n(388685);
+var i = n(951288),
+    e = n(647438),
+    a = n(82659),
+    E = n(481060),
+    o = n(53365),
+    T = n(377176),
+    c = n(388032);
+function l(t) {
+    let { guildId: r, transitionState: n, onClose: l } = t,
+        [A, _] = e.useState(!1),
+        [u, N] = e.useState(!1),
+        s = async () => {
+            _(!0);
             try {
-                await (0, c.zo)(r), T();
+                await (0, o.zo)(r), l();
             } finally {
-                A(!1);
+                _(!1);
             }
         },
-        I = l.intl.string(l.t["22itmp"]);
-    return (0, e.jsxs)(o.Y0X, {
-        className: E.__invalid_container,
-        size: o.CgR.DYNAMIC,
+        O = c.intl.string(c.t["22itmp"]);
+    return (0, i.jsx)(a.Modal, {
+        size: "md",
         transitionState: n,
-        "aria-label": I,
-        parentComponent: "TermsNotAcceptedErrorModal",
-        children: [
-            (0, e.jsxs)(o.hzk, {
-                className: E.content,
-                children: [
-                    (0, e.jsxs)("div", {
-                        className: E.header,
-                        children: [
-                            (0, e.jsx)(o.Mgn, {
-                                size: "custom",
-                                color: "currentColor",
-                                width: 20,
-                                height: 20,
-                                className: E.warningIcon,
-                            }),
-                            (0, e.jsx)(o.LZC, {
-                                size: 8,
-                                horizontal: !0,
-                            }),
-                            (0, e.jsx)(o.X6q, {
-                                variant: "heading-xl/semibold",
-                                color: "header-primary",
-                                children: I,
-                            }),
-                        ],
-                    }),
-                    (0, e.jsx)(o.LZC, { size: 12 }),
-                    (0, e.jsx)(o.Text, {
-                        variant: "text-md/normal",
-                        color: "text-muted",
-                        className: E.__invalid_body,
-                        children: l.intl.string(l.t.kdbm9P),
-                    }),
-                    (0, e.jsx)(o.LZC, { size: 28 }),
-                    (0, e.jsx)(o.XZJ, {
-                        onChange: () => {
-                            d((t) => !t);
-                        },
-                        size: 20,
-                        type: o.XZJ.Types.INVERTED,
-                        value: N,
-                        children: (0, e.jsx)(o.Text, {
-                            variant: "text-md/normal",
-                            color: "header-secondary",
-                            children: (0, s.f)(),
-                        }),
-                    }),
-                ],
-            }),
-            (0, e.jsxs)(o.mzw, {
-                children: [
-                    (0, e.jsx)(o.zxk, {
-                        variant: "primary",
-                        text: l.intl.string(l.t["cY+Ooa"]),
-                        disabled: !N,
-                        onClick: u,
-                        loading: _,
-                    }),
-                    (0, e.jsx)(o.LZC, {
-                        size: 12,
-                        horizontal: !0,
-                    }),
-                    (0, e.jsx)(a.zx, {
-                        color: a.zx.Colors.PRIMARY,
-                        look: a.zx.Looks.OUTLINED,
-                        onClick: T,
-                        children: l.intl.string(l.t["ETE/oK"]),
-                    }),
-                ],
-            }),
+        onClose: l,
+        "aria-label": O,
+        title: O,
+        subtitle: c.intl.string(c.t.kdbm9P),
+        actions: [
+            {
+                variant: "secondary",
+                onClick: l,
+                text: c.intl.string(c.t["ETE/oK"]),
+            },
+            {
+                variant: "primary",
+                text: c.intl.string(c.t["cY+Ooa"]),
+                disabled: !u,
+                onClick: s,
+                loading: A,
+            },
         ],
+        children: (0, i.jsx)(E.XZJ, {
+            onChange: () => {
+                N((t) => !t);
+            },
+            size: 20,
+            type: E.XZJ.Types.INVERTED,
+            value: u,
+            children: (0, i.jsx)(E.Text, {
+                variant: "text-md/normal",
+                color: "header-secondary",
+                children: (0, T.f)(),
+            }),
+        }),
     });
 }
