f2bb276efd976122f0af50cc27aa2e3c59370ce7
DiscrapperManager <admin@minerpl.xyz>
2025-08-26 17:12:20 +0000

    canaryRelease: 437199
    Main scripts: 4
    Lazy Loaded Scripts: 1380
    Chunks:
     * Main Files: 14713
     * Scripts: 8582
    Strings: 22046
    Routes: 727
    Experiments Count: 482
    Apex Experiments Count: 31

--- chunks/225415.js
+++ chunks/225415.js
@@ -1,21 +1,22 @@
-n.d(t, { Z: () => I }), n(388685);
+n.d(t, { Z: () => A }), n(388685);
 var r = n(470167),
     i = n(904245),
-    a = n(911969),
-    o = n(147913),
+    o = n(911969),
+    a = n(147913),
     s = n(89892),
     l = n(710845),
-    c = n(681678),
-    u = n(128064),
-    d = n(592125),
-    f = n(375954),
-    _ = n(944486),
-    p = n(594174),
-    h = n(480916),
-    m = n(81643),
-    g = n(981631),
-    E = n(484710);
-function b(e, t, n) {
+    c = n(109590),
+    u = n(681678),
+    d = n(128064),
+    f = n(592125),
+    _ = n(375954),
+    p = n(944486),
+    h = n(594174),
+    m = n(480916),
+    g = n(81643),
+    E = n(981631),
+    b = n(484710);
+function y(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -28,70 +29,76 @@ function b(e, t, n) {
         e
     );
 }
-let y = new l.Z("AgeVerificationManager");
-function O(e) {
+let O = new l.Z("AgeVerificationManager");
+function v(e) {
     var t, n, i;
-    let { channelId: a, message: o } = e,
-        s = f.Z.getMessage(a, o.id);
+    let { channelId: o, message: a } = e,
+        s = _.Z.getMessage(o, a.id);
     if (
         (null == s || null == (n = s.embeds) || null == (t = n[0]) ? void 0 : t.type) ===
         r.h.AGE_VERIFICATION_SYSTEM_NOTIFICATION
     ) {
-        let e = null == (i = s.embeds[0].fields) ? void 0 : i.find((e) => e.rawName === m.g0.CONTENT_TYPE);
-        (null == e ? void 0 : e.rawValue) === m.L0.ERROR
-            ? (c.Z.showFailedToast(E.wQ.TIGGER_PAWTECT_ERROR), (0, h.zi)(h.o6.ERROR))
-            : (null == e ? void 0 : e.rawValue) === m.L0.VERIFIED_ADULT
-              ? (c.Z.showSuccessToast(E.wQ.TIGGER_PAWTECT_VERIFIED), (0, h.zi)(h.o6.VERIFIED_ADULT))
-              : (null == e ? void 0 : e.rawValue) === m.L0.VERIFIED_TEEN &&
-                (c.Z.showSuccessToast(E.wQ.TIGGER_PAWTECT_VERIFIED), (0, h.zi)(h.o6.VERIFIED_TEEN));
+        let e = null == (i = s.embeds[0].fields) ? void 0 : i.find((e) => e.rawName === g.g0.CONTENT_TYPE);
+        (null == e ? void 0 : e.rawValue) === g.L0.ERROR
+            ? (u.Z.showFailedToast(b.wQ.TIGGER_PAWTECT_ERROR), (0, m.zi)(m.o6.ERROR))
+            : (null == e ? void 0 : e.rawValue) === g.L0.VERIFIED_ADULT
+              ? (u.Z.showSuccessToast(b.wQ.TIGGER_PAWTECT_VERIFIED), (0, m.zi)(m.o6.VERIFIED_ADULT))
+              : (null == e ? void 0 : e.rawValue) === g.L0.VERIFIED_TEEN &&
+                (u.Z.showSuccessToast(b.wQ.TIGGER_PAWTECT_VERIFIED), (0, m.zi)(m.o6.VERIFIED_TEEN));
     }
 }
-class v extends o.Z {
+function I(e) {
+    i.Z.fetchMessages({
+        channelId: e,
+        limit: E.AQB,
+    });
+}
+function T(e) {
+    let t = f.Z.getChannel(e);
+    ((null == t ? void 0 : t.type) === E.d4z.GUILD_FORUM || (null == t ? void 0 : t.type) === E.d4z.GUILD_MEDIA) &&
+        (0, c.EB)(t);
+}
+class S extends a.Z {
     constructor(...e) {
         super(...e),
-            b(this, "_previousAgeVerificationStatus", null),
-            b(this, "handlePostConnectionOpen", () => {
+            y(this, "_previousAgeVerificationStatus", null),
+            y(this, "handlePostConnectionOpen", () => {
                 var e, t;
                 this._previousAgeVerificationStatus =
-                    null != (t = null == (e = p.default.getCurrentUser()) ? void 0 : e.ageVerificationStatus)
+                    null != (t = null == (e = h.default.getCurrentUser()) ? void 0 : e.ageVerificationStatus)
                         ? t
                         : null;
             }),
-            b(this, "handleCurrentUserUpdate", (e) => {
+            y(this, "handleCurrentUserUpdate", (e) => {
                 var t;
                 let { user: n } = e,
-                    r = null != (t = (0, p.transformUser)(n).ageVerificationStatus) ? t : null;
-                try {
-                    if (
+                    r = null != (t = (0, h.transformUser)(n).ageVerificationStatus) ? t : null,
+                    i =
                         this._previousAgeVerificationStatus !== r &&
-                        r === a.F$.VERIFIED_ADULT &&
-                        (0, u.c_)("age-verification-manager")
-                    ) {
-                        let e = _.Z.getChannelId(),
+                        r === o.F$.VERIFIED_ADULT &&
+                        (0, d.c_)("age-verification-manager");
+                try {
+                    if (i) {
+                        let e = p.Z.getChannelId(),
                             t = !1;
                         s.Z.forEach((n) => {
                             let { channelId: r } = n,
-                                i = d.Z.getChannel(r);
+                                i = f.Z.getChannel(r);
                             (null == i ? void 0 : i.nsfw) && (s.Z.clear(r), r === e && (t = !0));
                         }),
-                            t &&
-                                null != e &&
-                                i.Z.fetchMessages({
-                                    channelId: e,
-                                    limit: g.AQB,
-                                });
+                            t && null != e && (I(e), T(e));
                     }
                 } catch (e) {
-                    y.warn("Error clearing cache and refetching messages", e);
+                    O.warn("Error clearing cache and refetching messages", e);
                 } finally {
                     this._previousAgeVerificationStatus = r;
                 }
             }),
-            b(this, "actions", {
+            y(this, "actions", {
                 POST_CONNECTION_OPEN: this.handlePostConnectionOpen,
                 CURRENT_USER_UPDATE: this.handleCurrentUserUpdate,
-                MESSAGE_CREATE: O,
+                MESSAGE_CREATE: v,
             });
     }
 }
-let I = new v();
+let A = new S();
