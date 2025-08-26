9e0c18e1391e97a0e5e5ac48e577f2822323ffaf
DiscrapperManager <admin@minerpl.xyz>
2025-08-26 18:55:07 +0000

    canaryRelease: 437326
    Main scripts: 4
    Lazy Loaded Scripts: 1381
    Chunks:
     * Main Files: 14710
     * Scripts: 8583
    Strings: 22055
    Routes: 727
    Experiments Count: 475
    Apex Experiments Count: 32

--- chunks/920981.js
+++ chunks/920981.js
@@ -4,6 +4,5 @@ var a,
         (((a = {}).DEFAULT = "default"),
         (a.DEFAULT_WITH_ORB_SHELF = "default_with_orb_shelf"),
         (a.ORB_TAB = "orb_tab"),
-        (a.DEFAULT_WITH_ORB_FEATURED_BLOCK_AND_SHELF = "default_with_orb_featured_block_and_shelf"),
         (a.SUMMER_SALE_TAKEOVER = "summer_sale_takeover"),
         a);
