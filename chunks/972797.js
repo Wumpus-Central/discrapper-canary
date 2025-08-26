dd41c45ccf4e7088aad33cd04fa8cc6b34c013ff
DiscrapperManager <admin@minerpl.xyz>
2025-08-26 21:10:31 +0000

    canaryRelease: 437421
    Main scripts: 4
    Lazy Loaded Scripts: 1383
    Chunks:
     * Main Files: 14710
     * Scripts: 8580
    Strings: 22058
    Routes: 727
    Experiments Count: 475
    Apex Experiments Count: 32

--- chunks/972797.js
+++ chunks/972797.js
@@ -1,122 +1,68 @@
-A.d(t, { Z: () => u }), A(388685);
-var n = A(951288),
-    r = A(647438),
-    l = A(120356),
-    a = A.n(l),
-    o = A(755721),
-    i = A(481060),
-    s = A(313201),
-    d = A(388032),
-    c = A(750404);
+t.d(A, { Z: () => u }), t(388685);
+var n = t(951288),
+    r = t(647438),
+    a = t(120356),
+    l = t.n(a),
+    i = t(159691),
+    o = t(481060),
+    s = t(313201),
+    d = t(388032),
+    c = t(750404);
 function u(e) {
-    let { disabled: t = !1, disabledTooltip: A, published: l, submitting: u = !1, onClick: f } = e,
-        g = (0, s.Dt)(),
-        [p, m] = r.useState(l);
+    let { disabled: A = !1, disabledTooltip: t, published: a, submitting: u = !1, onClick: g } = e,
+        f = (0, s.Dt)(),
+        [p, m] = r.useState(a);
     return (
         r.useEffect(() => {
-            m((e) => e || l);
-        }, [l]),
+            m((e) => e || a);
+        }, [a]),
         (0, n.jsxs)("div", {
-            className: a()(
+            className: l()(
                 {
-                    [c.publishable]: !t && !l,
+                    [c.publishable]: !A && !a,
                     [c.wasEverPublished]: p,
                 },
-                c.container,
+                c.outerContainer,
             ),
             children: [
                 (0, n.jsxs)("div", {
-                    className: c.explanationColumn,
+                    className: c.container,
                     children: [
-                        (0, n.jsx)(i.X6q, {
-                            variant: "text-sm/semibold",
-                            color: "header-primary",
-                            children: l ? d.intl.string(d.t.VaaagY) : d.intl.string(d.t.oI8aQU),
+                        (0, n.jsxs)("div", {
+                            className: c.explanationColumn,
+                            children: [
+                                (0, n.jsx)(o.X6q, {
+                                    variant: "text-sm/semibold",
+                                    color: "header-primary",
+                                    children: a ? d.intl.string(d.t.VaaagY) : d.intl.string(d.t.oI8aQU),
+                                }),
+                                (0, n.jsx)(o.Text, {
+                                    id: f,
+                                    variant: "text-sm/normal",
+                                    color: "text-default",
+                                    children: a ? d.intl.string(d.t["0r6pbG"]) : d.intl.string(d.t["8MMHYG"]),
+                                }),
+                            ],
                         }),
-                        (0, n.jsx)(i.Text, {
-                            id: g,
-                            variant: "text-sm/normal",
-                            color: "text-default",
-                            children: l ? d.intl.string(d.t["0r6pbG"]) : d.intl.string(d.t["8MMHYG"]),
+                        (0, n.jsx)(i.zx, {
+                            text: a ? d.intl.string(d.t.DlJOvr) : d.intl.string(d.t.X7YGXF),
+                            icon: a ? o.kZF : o.T$Z,
+                            iconPosition: "end",
+                            variant: a ? "critical-primary" : "expressive",
+                            "aria-describedby": f,
+                            "aria-disabled": A,
+                            disabled: A,
+                            onClick: A ? void 0 : g,
+                            loading: u,
                         }),
                     ],
                 }),
-                (0, n.jsx)(i.ua7, {
-                    text: t ? A : void 0,
-                    children: (e) => {
-                        var A, r;
-                        return (0, n.jsxs)(
-                            o.zx,
-                            ((A = (function (e) {
-                                for (var t = 1; t < arguments.length; t++) {
-                                    var A = null != arguments[t] ? arguments[t] : {},
-                                        n = Object.keys(A);
-                                    "function" == typeof Object.getOwnPropertySymbols &&
-                                        (n = n.concat(
-                                            Object.getOwnPropertySymbols(A).filter(function (e) {
-                                                return Object.getOwnPropertyDescriptor(A, e).enumerable;
-                                            }),
-                                        )),
-                                        n.forEach(function (t) {
-                                            var n;
-                                            (n = A[t]),
-                                                t in e
-                                                    ? Object.defineProperty(e, t, {
-                                                          value: n,
-                                                          enumerable: !0,
-                                                          configurable: !0,
-                                                          writable: !0,
-                                                      })
-                                                    : (e[t] = n);
-                                        });
-                                }
-                                return e;
-                            })({}, e)),
-                            (r = r =
-                                {
-                                    className: a()(c.button, {
-                                        [c.wasEverPublished]: p,
-                                        [c.publishButton]: !l,
-                                        [c.disabledPublishButton]: t,
-                                    }),
-                                    innerClassName: c.buttonContents,
-                                    color: l ? o.zx.Colors.RED : o.zx.Colors.CUSTOM,
-                                    "aria-describedby": g,
-                                    "aria-disabled": t,
-                                    grow: !1,
-                                    onClick: t ? void 0 : f,
-                                    submitting: u,
-                                    children: [
-                                        l ? d.intl.string(d.t.DlJOvr) : d.intl.string(d.t.X7YGXF),
-                                        l
-                                            ? (0, n.jsx)(i.kZF, {
-                                                  size: "md",
-                                                  className: c.buttonIcon,
-                                                  color: "currentColor",
-                                              })
-                                            : (0, n.jsx)(i.T$Z, {
-                                                  size: "xs",
-                                                  color: "currentColor",
-                                                  className: c.buttonIcon,
-                                              }),
-                                    ],
-                                }),
-                            Object.getOwnPropertyDescriptors
-                                ? Object.defineProperties(A, Object.getOwnPropertyDescriptors(r))
-                                : (function (e, t) {
-                                      var A = Object.keys(e);
-                                      if (Object.getOwnPropertySymbols) {
-                                          var n = Object.getOwnPropertySymbols(e);
-                                          A.push.apply(A, n);
-                                      }
-                                      return A;
-                                  })(Object(r)).forEach(function (e) {
-                                      Object.defineProperty(A, e, Object.getOwnPropertyDescriptor(r, e));
-                                  }),
-                            A),
-                        );
-                    },
-                }),
+                A &&
+                    (0, n.jsx)(o.Text, {
+                        variant: "text-sm/normal",
+                        color: "text-muted",
+                        children: t,
+                    }),
             ],
         })
     );
