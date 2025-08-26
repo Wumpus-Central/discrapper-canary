c047b5688b5e586cb58580967defb2bda50678dc
DiscrapperManager <admin@minerpl.xyz>
2025-08-26 18:31:32 +0000

    canaryRelease: 437305
    Main scripts: 4
    Lazy Loaded Scripts: 1381
    Chunks:
     * Main Files: 14710
     * Scripts: 8584
    Strings: 22055
    Routes: 727
    Experiments Count: 477
    Apex Experiments Count: 32

--- chunks/12161.js
+++ chunks/12161.js
@@ -5,13 +5,27 @@ let l = (0, r.B)({
     kind: "user",
     id: "2025-08_double_tap_to_react",
     label: "Double Tap To React",
-    defaultConfig: { enabled: !1 },
+    defaultConfig: {
+        enabled: !1,
+        showToast: !1,
+    },
     commonTriggerPoint: i.$P.CONNECTION_OPEN,
     treatments: [
         {
             id: 1,
-            label: "Enabled",
-            config: { enabled: !0 },
+            label: "Enabled - Show Half Sheet Upsell",
+            config: {
+                enabled: !0,
+                showToast: !1,
+            },
+        },
+        {
+            id: 2,
+            label: "Enabled - Show Toast Upsell",
+            config: {
+                enabled: !0,
+                showToast: !0,
+            },
         },
     ],
 });
