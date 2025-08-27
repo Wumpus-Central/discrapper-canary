2adef4bf2077dc6b6853f816da59ad324a8b4b5e
DiscrapperManager <admin@minerpl.xyz>
2025-08-27 17:48:45 +0000

    canaryRelease: 437840
    Main scripts: 4
    Lazy Loaded Scripts: 1383
    Chunks:
     * Main Files: 14710
     * Scripts: 8588
    Strings: 22067
    Routes: 727
    Experiments Count: 474
    Apex Experiments Count: 33

--- chunks/262734.js
+++ chunks/262734.js
@@ -1,74 +1,65 @@
-n.d(t, { Z: () => P }), n(388685);
+n.d(t, { Z: () => I }), n(388685);
 var r = n(951288),
     i = n(647438),
     l = n(120356),
     a = n.n(l),
-    o = n(442837),
-    s = n(692547),
-    c = n(755721),
-    u = n(481060),
-    d = n(350810),
-    p = n(988980),
-    f = n(835473),
-    h = n(358221),
-    g = n(740492),
-    m = n(871499),
-    b = n(786915),
-    _ = n(944486),
-    O = n(510839),
-    E = n(214143),
-    y = n(610278),
-    v = n(408491),
-    I = n(981631),
-    C = n(918559),
-    S = n(388032),
-    N = n(909357);
-let T = {
-    [C.MI.NO_CHAT]: N.noChat,
-    [C.MI.RESIZABLE]: N.resizable,
+    o = n(755721),
+    s = n(481060),
+    c = n(350810),
+    u = n(988980),
+    d = n(835473),
+    p = n(871499),
+    f = n(786915),
+    h = n(510839),
+    g = n(214143),
+    m = n(610278),
+    b = n(408491),
+    _ = n(981631),
+    O = n(918559),
+    E = n(388032),
+    y = n(909357);
+let v = {
+    [O.MI.NO_CHAT]: y.noChat,
+    [O.MI.RESIZABLE]: y.resizable,
 };
-function P(e) {
+function I(e) {
     let { frame: t } = e,
-        n = (0, f.q)(t.applicationId),
-        l = (0, d.Z)(),
-        P = (0, o.e7)([_.Z], () => _.Z.getVoiceChannelId()),
-        j = (0, o.e7)([h.Z], () => null != P && h.Z.getChatOpen(P), [P]),
-        x = (0, o.e7)([g.ZP], () => g.ZP.callChatSidebarWidth),
-        A = (0, u.dQu)(s.Z.modules.chat.RESIZE_HANDLE_WIDTH),
-        Z = i.useRef(null),
-        w = i.useRef(null),
-        [L, R] = i.useState({
+        n = (0, d.q)(t.applicationId),
+        l = (0, c.Z)(),
+        I = i.useRef(null),
+        C = i.useRef(null),
+        [S, N] = i.useState({
             width: 0,
             height: 0,
         }),
-        D = i.useCallback(() => {
-            (0, O.eU)({
+        T = i.useCallback(() => {
+            (0, h.eU)({
                 applicationId: t.applicationId,
-                layoutMode: v.U.PIP,
+                layoutMode: b.U.PIP,
             });
         }, [t.applicationId]),
-        k = i.useCallback(() => {
-            (0, O.xT)({ applicationId: t.applicationId });
+        P = i.useCallback(() => {
+            (0, h.xT)({ applicationId: t.applicationId });
         }, [t.applicationId]);
     i.useLayoutEffect(() => {
-        if (null == w.current) return;
+        if (null == C.current) return;
         let e = new ResizeObserver(() => {
             var e, t, n, r;
-            R({
-                width: null != (n = null == (e = w.current) ? void 0 : e.clientWidth) ? n : 0,
-                height: null != (r = null == (t = w.current) ? void 0 : t.clientHeight) ? r : 0,
+            N({
+                width: null != (n = null == (e = C.current) ? void 0 : e.clientWidth) ? n : 0,
+                height: null != (r = null == (t = C.current) ? void 0 : t.clientHeight) ? r : 0,
             });
         });
-        return e.observe(w.current), () => e.disconnect();
+        return e.observe(C.current), () => e.disconnect();
     }, []),
         i.useEffect(() => {
             let e = (e) => {
-                null == Z.current ||
-                    Z.current.contains(e.target) ||
+                null == I.current ||
+                    I.current.contains(e.target) ||
                     l ||
-                    (0, O.eU)({
+                    (0, h.eU)({
                         applicationId: t.applicationId,
-                        layoutMode: v.U.PIP,
+                        layoutMode: b.U.PIP,
                     });
             };
             return (
@@ -78,76 +69,75 @@ function P(e) {
                 }
             );
         }, [t.applicationId, l]);
-    let M = L.width / Math.max(L.height, 1) < C.I0,
-        U = 0,
-        G = 0,
-        B = (0, p.Z)(null == n ? void 0 : n.id);
-    if (!B) {
-        let e = L.width,
-            t = L.height;
-        M
-            ? ((t = L.width / C.I0) > L.height && (e = (t = L.height) * C.I0), (G = (L.height - t) / 2))
-            : ((e = Math.min(L.height * C.I0)) > L.width && (t = (e = L.width) / C.I0), (U = (L.width - e) / 2));
+    let j = S.width / Math.max(S.height, 1) < O.I0,
+        x = 0,
+        A = 0,
+        Z = (0, u.Z)(null == n ? void 0 : n.id);
+    if (!Z) {
+        let e = S.width,
+            t = S.height;
+        j
+            ? ((t = S.width / O.I0) > S.height && (e = (t = S.height) * O.I0), (A = (S.height - t) / 2))
+            : ((e = Math.min(S.height * O.I0)) > S.width && (t = (e = S.width) / O.I0), (x = (S.width - e) / 2));
     }
-    let V = C.MI.NO_CHAT;
+    let w = O.MI.NO_CHAT;
     if (null == n) return null;
-    let H = (null != x ? x : I.$Y6) + A,
-        F = (0, y.ro)(t);
-    return (0, r.jsx)(u.f6W, {
-        theme: I.BRd.DARK,
+    let L = (0, m.ro)(t);
+    return (0, r.jsx)(s.f6W, {
+        theme: _.BRd.DARK,
         children: (e) =>
             (0, r.jsxs)("div", {
-                className: a()(N.wrapper, T[V], e),
-                ref: Z,
-                style: { right: j && !Number.isNaN(H) ? H : 0 },
+                className: a()(y.wrapper, v[w], e),
+                ref: I,
+                style: {},
                 children: [
                     (0, r.jsx)("div", {
-                        className: N.contextlessHeader,
+                        className: y.contextlessHeader,
                         children: (0, r.jsxs)("div", {
-                            className: N.buttonSection,
+                            className: y.buttonSection,
                             children: [
-                                (0, r.jsx)(m.d, {
+                                (0, r.jsx)(p.d, {
                                     isTrayButton: !0,
-                                    label: S.intl.string(S.t.brPQ5e),
-                                    onClick: D,
-                                    iconComponent: u.dOc,
+                                    label: E.intl.string(E.t.brPQ5e),
+                                    onClick: T,
+                                    iconComponent: s.dOc,
                                     themeable: !0,
                                 }),
-                                (0, r.jsx)(b.Z, {
-                                    appContext: I.IlC.APP,
+                                (0, r.jsx)(f.Z, {
+                                    appContext: _.IlC.APP,
                                     applicationId: n.id,
                                     shouldPrioritizeGroupPlusIcon: !0,
-                                    iconClassName: N.contextlessInviteButtonIcon,
+                                    iconClassName: y.contextlessInviteButtonIcon,
                                     isRichPresenceInvite: !0,
-                                    size: c.zx.Sizes.SMALL,
-                                    look: c.zx.Looks.FILLED,
-                                    buttonText: S.intl.string(S.t["6F9ivr"]),
-                                    color: c.zx.Colors.PRIMARY,
+                                    size: o.zx.Sizes.SMALL,
+                                    look: o.zx.Looks.FILLED,
+                                    buttonText: E.intl.string(E.t["6F9ivr"]),
+                                    color: o.zx.Colors.PRIMARY,
                                 }),
-                                (0, r.jsx)(m.d, {
+                                (0, r.jsx)(p.d, {
                                     isTrayButton: !0,
-                                    label: S.intl.string(S.t.cpT0Cg),
-                                    onClick: k,
-                                    iconComponent: u.Dio,
+                                    label: E.intl.string(E.t.cpT0Cg),
+                                    onClick: P,
+                                    iconComponent: s.Dio,
                                     themeable: !0,
                                 }),
                             ],
                         }),
                     }),
                     (0, r.jsx)("div", {
-                        className: N.activityPanelContainer,
+                        className: y.activityPanelContainer,
                         children: (0, r.jsx)("div", {
-                            className: a()(N.activityContainer, { [N.activityContainerNoMargin]: B }),
+                            className: a()(y.activityContainer, { [y.activityContainerNoMargin]: Z }),
                             style: {
-                                paddingLeft: U,
-                                paddingRight: U,
-                                paddingTop: G,
-                                paddingBottom: G,
+                                paddingLeft: x,
+                                paddingRight: x,
+                                paddingTop: A,
+                                paddingBottom: A,
                             },
-                            ref: w,
-                            children: (0, r.jsx)(E.Z, {
-                                className: N.iframe,
-                                embedId: F,
+                            ref: C,
+                            children: (0, r.jsx)(g.Z, {
+                                className: y.iframe,
+                                embedId: L,
                             }),
                         }),
                     }),
