76a2dd4bfebe6b92cd328d3d681fa51f305c466e
DiscrapperManager <admin@minerpl.xyz>
2025-08-27 18:20:31 +0000

    canaryRelease: 437884
    Main scripts: 4
    Lazy Loaded Scripts: 1385
    Chunks:
     * Main Files: 14714
     * Scripts: 8594
    Strings: 22067
    Routes: 730
    Experiments Count: 474
    Apex Experiments Count: 33

--- chunks/480884.js
+++ chunks/480884.js
@@ -1,106 +1,76 @@
-t.d(n, { default: () => h }), t(953529);
-var a = t(951288),
-    o = t(647438),
-    s = t(286379),
-    i = t(399606),
-    c = t(755721),
-    l = t(481060),
-    r = t(797614),
-    d = t(819557),
-    u = t(138201),
-    _ = t(359119),
-    m = t(473092),
-    x = t(636749),
-    b = t(981631),
-    v = t(388032),
-    g = t(549306);
-function h(e) {
+e.d(n, { default: () => x }), e(953529);
+var a = e(951288),
+    s = e(647438),
+    c = e(286379),
+    i = e(399606),
+    o = e(667202),
+    r = e(481060),
+    l = e(797614),
+    d = e(819557),
+    u = e(138201),
+    b = e(359119),
+    _ = e(473092),
+    g = e(636749),
+    f = e(981631),
+    p = e(388032),
+    v = e(308064);
+function x(t) {
     let {
             transitionState: n,
-            onClose: t,
-            channelId: h,
-            warningId: N,
+            onClose: e,
+            channelId: x,
+            warningId: h,
             senderId: k,
-            description: T,
-            safetyTipRows: f,
-            actionRows: j,
-            learnMore: p,
-        } = e,
-        B = (0, i.e7)([_.ZP], () => _.ZP.getChannelSafetyWarning(h, N));
+            description: m,
+            safetyTipRows: y,
+            actionRows: T,
+            learnMore: j,
+        } = t,
+        N = (0, i.e7)([b.ZP], () => b.ZP.getChannelSafetyWarning(x, h));
     return (
-        o.useEffect(() => {
-            (0, m.MC)(b.rMx.SAFETY_WARNING_MODAL_VIEWED, {
-                channelId: h,
-                warningId: N,
+        s.useEffect(() => {
+            (0, _.MC)(f.rMx.SAFETY_WARNING_MODAL_VIEWED, {
+                channelId: x,
+                warningId: h,
                 senderId: k,
-                warningType: null == B ? void 0 : B.type,
+                warningType: null == N ? void 0 : N.type,
             }),
-                r.Z.increment({ name: s.V.SAFETY_WARNING_MODAL_VIEW });
-        }, [h, N, k, B]),
-        (0, a.jsx)(l.Y0X, {
+                l.Z.increment({ name: c.V.SAFETY_WARNING_MODAL_VIEW });
+        }, [x, h, k, N]),
+        (0, a.jsx)(o.I, {
+            onClose: e,
             transitionState: n,
-            parentComponent: "MoreTipsModal",
-            children: (0, a.jsxs)(l.Ttm, {
-                style: { overflow: "hidden auto" },
+            graphic: {
+                type: "image",
+                src: v.Z,
+            },
+            gradientColor: "blue",
+            title: p.intl.string(p.t.lyt43N),
+            subtitle: m,
+            actions: [],
+            children: (0, a.jsxs)(r.Kqy, {
+                gap: 24,
                 children: [
-                    (0, a.jsxs)(l.xBx, {
-                        className: g.modalHeader,
+                    (0, a.jsxs)(r.Kqy, {
+                        gap: 8,
+                        children: [(0, a.jsx)(d.z, { children: y }), null != j ? j : null],
+                    }),
+                    (0, a.jsxs)(r.Kqy, {
+                        gap: 4,
                         children: [
-                            (0, a.jsxs)("div", {
-                                className: g.modalHeaderText,
-                                children: [
-                                    (0, a.jsx)(l.Text, {
-                                        variant: "eyebrow",
-                                        color: "status-positive-text",
-                                        children: v.intl.string(v.t.lyt43N),
-                                    }),
-                                    (0, a.jsx)(l.X6q, {
-                                        variant: "heading-xl/bold",
-                                        color: "status-positive-text",
-                                        children: T,
-                                    }),
-                                ],
+                            (0, a.jsx)(r.Text, {
+                                variant: "eyebrow",
+                                color: "header-secondary",
+                                children: p.intl.string(p.t.K5FKtb),
                             }),
-                            (0, a.jsx)(c.zx, {
-                                "aria-label": v.intl.string(v.t.cpT0Cg),
-                                look: c.zx.Looks.BLANK,
-                                size: c.zx.Sizes.NONE,
-                                onClick: t,
-                                innerClassName: g.closeButtonInner,
-                                className: g.closeButton,
-                                children: (0, a.jsx)(l.Dio, {
-                                    size: "sm",
-                                    color: "currentColor",
-                                    className: g.closeIcon,
-                                }),
-                            }),
+                            (0, a.jsx)(u.rT, { children: T }),
                         ],
                     }),
-                    (0, a.jsxs)(l.hzk, {
-                        className: g.modalContent,
-                        children: [
-                            (0, a.jsxs)("div", {
-                                className: g.mainSection,
-                                children: [(0, a.jsx)(d.z, { children: f }), null != p ? p : null],
-                            }),
-                            (0, a.jsxs)("div", {
-                                children: [
-                                    (0, a.jsx)(l.Text, {
-                                        className: g.moreHeading,
-                                        variant: "eyebrow",
-                                        color: "header-secondary",
-                                        children: v.intl.string(v.t.K5FKtb),
-                                    }),
-                                    (0, a.jsx)(u.rT, { children: j }),
-                                ],
-                            }),
-                            (0, a.jsx)(x.Z, {
-                                channelId: h,
-                                warningId: N,
-                                senderId: k,
-                                safetyWarning: B,
-                            }),
-                        ],
+                    (0, a.jsx)(g.Z, {
+                        channelId: x,
+                        warningId: h,
+                        senderId: k,
+                        safetyWarning: N,
                     }),
                 ],
             }),
