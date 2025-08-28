504611634070ec8150699ff1e53a45655007bb12
DiscrapperManager <admin@minerpl.xyz>
2025-08-28 18:47:57 +0000

    canaryRelease: 438535
    Main scripts: 4
    Lazy Loaded Scripts: 1386
    Chunks:
     * Main Files: 14721
     * Scripts: 8613
    Strings: 22090
    Routes: 730
    Experiments Count: 468
    Apex Experiments Count: 37

---
+++ chunks/753762.js
@@ -0,0 +1,31 @@
+n.d(t, { tm: () => l });
+var r = n(818083),
+    i = n(987338);
+let l = (0, r.B)({
+    id: "2025-09_you_bar",
+    label: "You Bar",
+    commonTriggerPoint: i.$P.CONNECTION_OPEN,
+    kind: "user",
+    defaultConfig: {
+        enabled: !1,
+        backButtonBadge: !1,
+    },
+    treatments: [
+        {
+            id: 1,
+            label: "You Bar",
+            config: {
+                enabled: !0,
+                backButtonBadge: !1,
+            },
+        },
+        {
+            id: 2,
+            label: "You Bar",
+            config: {
+                enabled: !0,
+                backButtonBadge: !0,
+            },
+        },
+    ],
+});
