23f35a21e2fe072cbd61417d820c00fa7f6ef4da
DiscrapperManager <admin@minerpl.xyz>
2025-08-30 01:04:12 +0000

    canaryRelease: 439158
    Main scripts: 4
    Lazy Loaded Scripts: 1356
    Chunks:
     * Main Files: 14742
     * Scripts: 8596
    Strings: 22108
    Routes: 730
    Experiments Count: 470
    Apex Experiments Count: 41

--- chunks/215739.js
+++ chunks/215739.js
@@ -1,12 +1,14 @@
-n.d(t, { Z: () => h }), n(388685);
-var r = n(846519),
-    i = n(147913),
-    a = n(246133),
-    o = n(734934),
-    s = n(695346),
-    l = n(885110),
-    c = n(981631);
-function u(e, t, n) {
+n.d(t, { Z: () => g }), n(388685);
+var r = n(381499),
+    i = n(846519),
+    a = n(147913),
+    o = n(246133),
+    s = n(734934),
+    l = n(695346),
+    c = n(675478),
+    u = n(885110),
+    d = n(981631);
+function f(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -19,86 +21,86 @@ function u(e, t, n) {
         e
     );
 }
-let d = new r.V7(),
-    f = new r.V7(),
-    _ = new r.V7();
-class p extends i.Z {
+let _ = new i.V7(),
+    p = new i.V7(),
+    h = new i.V7();
+class m extends a.Z {
     constructor(...e) {
         super(...e),
-            u(this, "actions", {
+            f(this, "actions", {
                 USER_SETTINGS_PROTO_UPDATE: () => this.handleUpdateProto(),
                 POST_CONNECTION_OPEN: () => this.handleUpdateProto(),
             }),
-            u(this, "handleUpdateProto", () => {
+            f(this, "handleUpdateProto", () => {
                 this.manageExpiringCustomStatus(),
                     this.manageExpiringStatus(),
                     this.lazilyMigrateStatusCreatedAt(),
                     this.manageExpiringFocusMode();
             }),
-            u(this, "manageExpiringCustomStatus", () => {
-                let e = s.Ok.getSetting();
-                if (null == e) _.stop();
+            f(this, "manageExpiringCustomStatus", () => {
+                let e = l.Ok.getSetting();
+                if (null == e) h.stop();
                 else if (null != e.expiresAtMs && "0" !== e.expiresAtMs) {
                     let t = new Date(Number(e.expiresAtMs)).getTime() - new Date().getTime();
+                    t > 0
+                        ? h.start(
+                              t,
+                              () => {
+                                  l.Ok.updateSetting(void 0);
+                              },
+                              !0,
+                          )
+                        : (l.Ok.updateSetting(void 0), h.stop());
+                } else null != h && h.stop();
+            }),
+            f(this, "manageExpiringStatus", () => {
+                let e = l.Cr.getSetting();
+                if (null != e && "0" !== e && u.Z.getStatus() !== d.Skl.ONLINE) {
+                    let t = new Date(Number(e)).getTime() - new Date().getTime();
                     t > 0
                         ? _.start(
                               t,
                               () => {
-                                  s.Ok.updateSetting(void 0);
-                              },
-                              !0,
-                          )
-                        : (s.Ok.updateSetting(void 0), _.stop());
-                } else null != _ && _.stop();
-            }),
-            u(this, "manageExpiringStatus", () => {
-                let e = s.Cr.getSetting();
-                if (null != e && "0" !== e && l.Z.getStatus() !== c.Skl.ONLINE) {
-                    let t = new Date(Number(e)).getTime() - new Date().getTime();
-                    t > 0
-                        ? d.start(
-                              t,
-                              () => {
-                                  (0, a.Z)({
-                                      nextStatus: c.Skl.ONLINE,
-                                      analyticsContext: { location: { object: c.qAy.CUSTOM_STATUS_MANAGER } },
+                                  (0, o.Z)({
+                                      nextStatus: d.Skl.ONLINE,
+                                      analyticsContext: { location: { object: d.qAy.CUSTOM_STATUS_MANAGER } },
                                   });
                               },
                               !0,
                           )
-                        : ((0, a.Z)({
-                              nextStatus: c.Skl.ONLINE,
-                              analyticsContext: { location: { object: c.qAy.CUSTOM_STATUS_MANAGER } },
+                        : ((0, o.Z)({
+                              nextStatus: d.Skl.ONLINE,
+                              analyticsContext: { location: { object: d.qAy.CUSTOM_STATUS_MANAGER } },
                           }),
-                          d.stop());
-                } else null != d && d.stop();
+                          _.stop());
+                } else null != _ && _.stop();
             }),
-            u(this, "lazilyMigrateStatusCreatedAt", () => {
-                if (l.Z.getStatus() !== c.Skl.ONLINE && null == s.P4.getSetting()) {
-                    let e = s.Cr.getSetting(),
-                        t = "0" !== e ? new Date(Number(e)).getTime() - new Date().getTime() : void 0;
-                    (0, a.Z)({
-                        nextStatus: l.Z.getStatus(),
-                        durationMillis: null != t && t > 0 ? t : void 0,
-                        disableTracking: !0,
-                    });
-                }
+            f(this, "lazilyMigrateStatusCreatedAt", () => {
+                u.Z.getStatus() !== d.Skl.ONLINE &&
+                    null == l.P4.getSetting() &&
+                    c.hW.updateAsync(
+                        "status",
+                        (e) => {
+                            e.statusCreatedAtMs = r.wA.create({ value: "".concat(Date.now()) });
+                        },
+                        c.fy.INFREQUENT_USER_ACTION,
+                    );
             }),
-            u(this, "manageExpiringFocusMode", () => {
-                let e = s.fv.getSetting();
+            f(this, "manageExpiringFocusMode", () => {
+                let e = l.fv.getSetting();
                 if (null != e && "0" !== e) {
                     let t = new Date(Number(e)).getTime() - new Date().getTime();
                     t > 0
-                        ? f.start(
+                        ? p.start(
                               t,
                               () => {
-                                  (0, o.oW)(!1);
+                                  (0, s.oW)(!1);
                               },
                               !0,
                           )
-                        : ((0, o.oW)(!1), f.stop());
-                } else null != f && f.stop();
+                        : ((0, s.oW)(!1), p.stop());
+                } else null != p && p.stop();
             });
     }
 }
-let h = new p();
+let g = new m();
