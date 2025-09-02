97a13d407c209bb966e541401dd74daf1a86685d
DiscrapperManager <admin@minerpl.xyz>
2025-09-02 20:31:34 +0000

    canaryRelease: 439445
    Main scripts: 4
    Lazy Loaded Scripts: 1355
    Chunks:
     * Main Files: 14778
     * Scripts: 8595
    Strings: 22153
    Routes: 730
    Experiments Count: 470
    Apex Experiments Count: 42

--- chunks/865066.js
+++ chunks/865066.js
@@ -1,32 +1,39 @@
 n.d(t, {
-    hi: () => d,
-    s2: () => f,
-    sU: () => _,
+    hi: () => f,
+    s2: () => _,
+    sU: () => p,
 });
 var r = n(442837),
     i = n(481060),
     a = n(594190),
-    o = n(998502),
-    s = n(765504),
-    l = n(388032);
-function c() {
-    return o.ZP.IsSystemServiceInstalled() || !1;
-}
+    o = n(358085),
+    s = n(998502),
+    l = n(765504),
+    c = n(388032);
 function u() {
-    return (o.ZP.CanSystemServiceBeInstalled() || !1) && s.Z.getConfig({ location: "testing" }).force;
+    return !!o.isPlatformEmbedded && (s.ZP.IsSystemServiceInstalled() || !1);
 }
 function d() {
-    return (0, r.e7)([a.ZP], () => a.ZP.canShowAdminWarning) && !c() && u();
+    return (
+        !!o.isPlatformEmbedded &&
+        (s.ZP.CanSystemServiceBeInstalled() || !1) &&
+        l.Z.getConfig({ location: "testing" }).force
+    );
 }
-function f(e) {
-    return e.canShowAdminWarning && !c() && u();
+function f() {
+    let e = (0, r.e7)([a.ZP], () => a.ZP.canShowAdminWarning);
+    return !!o.isPlatformEmbedded && e && !u() && d();
 }
-function _() {
-    o.ZP.InstallSystemService()
-        .then(() => {
-            (0, i.showToast)((0, i.createToast)(l.intl.string(l.t.kQnWb2), i.ToastType.SUCCESS));
-        })
-        .catch((e) => {
-            (0, i.showToast)((0, i.createToast)(l.intl.string(l.t.sdKYCA), i.ToastType.FAILURE)), console.error(e);
-        });
+function _(e) {
+    return !!o.isPlatformEmbedded && e.canShowAdminWarning && !u() && d();
+}
+function p() {
+    o.isPlatformEmbedded &&
+        s.ZP.InstallSystemService()
+            .then(() => {
+                (0, i.showToast)((0, i.createToast)(c.intl.string(c.t.kQnWb2), i.ToastType.SUCCESS));
+            })
+            .catch((e) => {
+                (0, i.showToast)((0, i.createToast)(c.intl.string(c.t.sdKYCA), i.ToastType.FAILURE)), console.error(e);
+            });
 }
