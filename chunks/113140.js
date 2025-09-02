893584a5504f97887fc58b3be1df292adf0a2717
DiscrapperManager <admin@minerpl.xyz>
2025-09-02 17:16:15 +0000

    canaryRelease: 439286
    Main scripts: 4
    Lazy Loaded Scripts: 1356
    Chunks:
     * Main Files: 14756
     * Scripts: 8596
    Strings: 22110
    Routes: 730
    Experiments Count: 470
    Apex Experiments Count: 42

--- chunks/113140.js
+++ chunks/113140.js
@@ -1,93 +1,51 @@
-t.d(e, { default: () => d }), t(953529);
-var c = t(951288);
-t(647438);
-var a = t(481060),
-    i = t(313201),
-    s = t(471253),
-    l = t(939863),
-    r = t(388032),
-    o = t(981038);
-function d(n) {
-    let { transitionState: e, onClose: t, channel: d } = n,
-        h = (0, i.Dt)(),
-        f = (n) => {
-            (0, s.RK)(d, n), t();
+e.d(n, { default: () => d });
+var c = e(951288);
+e(647438);
+var a = e(667202),
+    s = e(481060),
+    i = e(471253),
+    o = e(388032),
+    r = e(981038),
+    l = e(768982);
+function d(t) {
+    let { transitionState: n, onClose: e, channel: d } = t,
+        h = (t) => {
+            (0, i.RK)(d, t), e();
         };
-    return (0, c.jsx)(a.Y0X, {
-        size: a.CgR.SMALL,
-        transitionState: e,
-        "aria-labelledby": h,
-        className: o.container,
-        parentComponent: "InvitedToSpeakModal",
-        children: (0, c.jsxs)(a.hzk, {
-            className: o.content,
-            children: [
-                (0, c.jsx)(l.Z, {
-                    children: (0, c.jsx)("div", {
-                        className: o.iconBackground,
-                        children: (0, c.jsx)(a.S6n, {
+    return (0, c.jsx)(a.I, {
+        transitionState: n,
+        graphic: {
+            type: "image",
+            src: l.Z,
+        },
+        title: o.intl.string(o.t.Ul1RJS),
+        subtitle: o.intl.format(o.t["Z+3bW1"], {
+            stageChannelHook: () =>
+                (0, c.jsxs)(c.Fragment, {
+                    children: [
+                        (0, c.jsx)(s.ewx, {
                             size: "custom",
                             color: "currentColor",
-                            height: 40,
-                            width: 40,
-                            className: o.icon,
+                            height: 14,
+                            width: 14,
+                            className: r.channelIcon,
                         }),
-                    }),
-                }),
-                (0, c.jsx)(a.X6q, {
-                    className: o.header,
-                    variant: "heading-lg/semibold",
-                    children: r.intl.string(r.t.Ul1RJS),
-                }),
-                (0, c.jsx)(a.Text, {
-                    color: "header-secondary",
-                    className: o.description,
-                    variant: "text-sm/normal",
-                    children: r.intl.format(r.t["Z+3bW1"], {
-                        stageChannelHook: () =>
-                            (0, c.jsxs)("div", {
-                                className: o.channel,
-                                children: [
-                                    (0, c.jsx)(a.ewx, {
-                                        size: "custom",
-                                        color: "currentColor",
-                                        height: 14,
-                                        width: 14,
-                                        className: o.channelIcon,
-                                    }),
-                                    (0, c.jsx)(a.Text, {
-                                        className: o.channelName,
-                                        variant: "text-sm/normal",
-                                        children: d.name,
-                                    }),
-                                ],
-                            }),
-                    }),
-                }),
-                (0, c.jsxs)("div", {
-                    className: o.buttons,
-                    children: [
-                        (0, c.jsx)(a.zxk, {
-                            variant: "active",
-                            text: r.intl.string(r.t.MMlhsr),
-                            fullWidth: !0,
-                            onClick: () => f(!1),
-                        }),
-                        (0, c.jsx)(a.zxk, {
-                            variant: "critical-primary",
-                            text: r.intl.string(r.t.BVN4pK),
-                            fullWidth: !0,
-                            onClick: () => f(!0),
-                        }),
-                        (0, c.jsx)(a.zxk, {
-                            variant: "secondary",
-                            text: r.intl.string(r.t.L5eIZ2),
-                            fullWidth: !0,
-                            onClick: t,
-                        }),
+                        d.name,
                     ],
                 }),
-            ],
         }),
+        onClose: () => (h(!0), Promise.resolve()),
+        actions: [
+            {
+                text: o.intl.string(o.t.L5eIZ2),
+                onClick: () => e(),
+                variant: "secondary",
+            },
+            {
+                text: o.intl.string(o.t.MMlhsr),
+                onClick: () => h(!1),
+                variant: "active",
+            },
+        ],
     });
 }
