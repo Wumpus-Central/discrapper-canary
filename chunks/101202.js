9d7530804c7993e6b9747d777363e496287239bf
DiscrapperManager <admin@minerpl.xyz>
2025-09-04 21:16:16 +0200

    canaryRelease: 440417
    Main scripts: 4
    Lazy Loaded Scripts: 1357
    Chunks:
     * Main Files: 14774
     * Scripts: 8564
    Strings: 22138
    Routes: 732
    Experiments Count: 468
    Apex Experiments Count: 46

--- chunks/101202.js
+++ chunks/101202.js
@@ -1,12 +1,5 @@
-e.exports = {
-    modalRoot: "modalRoot__3297f",
-    modalHeader: "modalHeader__3297f",
-    modalTop: "modalTop__3297f",
-    iconFrame: "iconFrame__3297f",
-    closeBtn: "closeBtn__3297f",
-    modalContent: "modalContent__3297f",
-    upperBodyText: "upperBodyText__3297f",
+t.exports = {
     targetList: "targetList__3297f",
+    upperBodyText: "upperBodyText__3297f",
     targetItem: "targetItem__3297f",
-    footer: "footer__3297f",
 };
--- chunks/316210.js
+++ chunks/316210.js
@@ -1,174 +1,110 @@
-n.d(t, { default: () => f });
-var r = n(951288),
-    s = n(647438),
-    a = n(481060),
-    o = n(600164),
-    i = n(63063),
-    l = n(509212),
-    c = n(497505),
-    d = n(981631),
-    m = n(388032),
-    u = n(101202),
-    x = n(708901);
-function f(e) {
-    let { transitionState: t, onClose: n, questContent: f, quest: g } = e,
-        p = ((e) => {
-            switch (e) {
-                case c.jn.QUEST_BAR:
-                case c.jn.QUEST_BAR_V2:
+s.d(e, { default: () => g });
+var i = s(951288),
+    n = s(647438),
+    r = s(82659),
+    a = s(481060),
+    c = s(63063),
+    l = s(509212),
+    o = s(497505),
+    u = s(981631),
+    d = s(388032),
+    m = s(101202);
+function g(t) {
+    let { transitionState: e, onClose: s, questContent: g, quest: T } = t,
+        h = ((t) => {
+            switch (t) {
+                case o.jn.QUEST_BAR:
+                case o.jn.QUEST_BAR_V2:
                     return {
                         location: !0,
                         age: !0,
                         activity: !0,
                     };
-                case c.jn.ACTIVITY_PANEL:
-                case c.jn.QUESTS_EMBED:
-                case c.jn.QUEST_LIVE_STREAM:
+                case o.jn.ACTIVITY_PANEL:
+                case o.jn.QUESTS_EMBED:
+                case o.jn.QUEST_LIVE_STREAM:
                     return { activity: !0 };
                 default:
                     return;
             }
-        })(f),
-        _ = s.useMemo(() => {
-            var e;
+        })(g),
+        p = n.useMemo(() => {
+            var t;
             return (0, l.Fs)({
-                isTargetedDisclosure: null != p,
-                gamePublisher: g.config.messages.gamePublisher,
-                gameTitle: g.config.messages.gameTitle,
-                cosponsorName: null == (e = g.config.cosponsorMetadata) ? void 0 : e.name,
+                isTargetedDisclosure: null != h,
+                gamePublisher: T.config.messages.gamePublisher,
+                gameTitle: T.config.messages.gameTitle,
+                cosponsorName: null == (t = T.config.cosponsorMetadata) ? void 0 : t.name,
             });
-        }, [g, p]);
-    return (0, r.jsxs)(a.Y0X, {
-        transitionState: t,
-        size: a.CgR.DYNAMIC,
-        className: u.modalRoot,
-        parentComponent: "QuestsDisclosureModal",
+        }, [T, h]);
+    return (0, i.jsxs)(r.Modal, {
+        size: "sm",
+        transitionState: e,
+        onClose: s,
+        title: d.intl.string(d.t.GcsZKC),
+        actions: [
+            {
+                onClick: () => s(),
+                text: d.intl.string(d.t.cpT0Cg),
+            },
+        ],
         children: [
-            (0, r.jsxs)(a.xBx, {
-                direction: o.Z.Direction.VERTICAL,
-                separator: !1,
-                className: u.modalHeader,
-                children: [
-                    (0, r.jsxs)("div", {
-                        className: u.modalTop,
-                        children: [
-                            (0, r.jsx)("div", {
-                                className: u.iconFrame,
-                                children: (0, r.jsx)("img", {
-                                    alt: "",
-                                    src: x,
-                                }),
-                            }),
-                            (0, r.jsx)(a.olH, {
-                                className: u.closeBtn,
-                                onClick: n,
-                            }),
-                        ],
-                    }),
-                    (0, r.jsx)(a.X6q, {
-                        variant: "heading-xl/bold",
-                        children: m.intl.string(m.t.GcsZKC),
-                    }),
-                ],
+            (0, i.jsx)(a.Text, {
+                variant: "text-sm/medium",
+                color: "header-secondary",
+                className: m.upperBodyText,
+                children: p,
             }),
-            (0, r.jsxs)(a.hzk, {
-                className: u.modalContent,
-                children: [
-                    (0, r.jsx)(a.Text, {
-                        variant: "text-sm/medium",
-                        color: "header-secondary",
-                        className: u.upperBodyText,
-                        children: _,
-                    }),
-                    (0, r.jsx)(j, { targetedTraits: p }),
-                    (0, r.jsx)(a.Text, {
-                        variant: "text-xs/medium",
-                        color: "text-muted",
-                        children: m.intl.format(m.t.tzq9WV, {
-                            privacySettingsUrl: i.Z.getArticleURL(d.BhN.QUESTS_PRIVACY_CONTROLS),
-                        }),
-                    }),
-                ],
-            }),
-            (0, r.jsx)(a.mzw, {
-                className: u.footer,
-                children: (0, r.jsx)(a.zxk, {
-                    variant: "primary",
-                    text: m.intl.string(m.t.cpT0Cg),
-                    onClick: n,
+            (0, i.jsx)(x, { targetedTraits: h }),
+            (0, i.jsx)(a.Text, {
+                variant: "text-xs/medium",
+                color: "text-muted",
+                children: d.intl.format(d.t.tzq9WV, {
+                    privacySettingsUrl: c.Z.getArticleURL(u.BhN.QUESTS_PRIVACY_CONTROLS),
                 }),
             }),
         ],
     });
 }
-function g(e) {
-    let { location: t, age: n, activity: s } = e;
-    return (0, r.jsxs)("ul", {
-        className: u.targetList,
-        children: [
-            t &&
-                (0, r.jsx)(p, {
-                    icon: a.enf,
-                    children: m.intl.string(m.t.xQSdPj),
-                }),
-            n &&
-                (0, r.jsx)(p, {
-                    icon: a.tBG,
-                    children: m.intl.string(m.t.mYt7hY),
-                }),
-            s &&
-                (0, r.jsx)(p, {
-                    icon: a.iWm,
-                    children: m.intl.string(m.t.XAsWxc),
-                }),
-        ],
-    });
+function x(t) {
+    let { targetedTraits: e } = t;
+    if (null == e) return null;
+    {
+        let { location: t, age: s, activity: n } = e;
+        return (0, i.jsxs)("ul", {
+            className: m.targetList,
+            children: [
+                t &&
+                    (0, i.jsx)(T, {
+                        icon: a.enf,
+                        children: d.intl.string(d.t.xQSdPj),
+                    }),
+                s &&
+                    (0, i.jsx)(T, {
+                        icon: a.tBG,
+                        children: d.intl.string(d.t.mYt7hY),
+                    }),
+                n &&
+                    (0, i.jsx)(T, {
+                        icon: a.iWm,
+                        children: d.intl.string(d.t.XAsWxc),
+                    }),
+            ],
+        });
+    }
 }
-function j(e) {
-    let { targetedTraits: t } = e;
-    return null != t
-        ? (0, r.jsx)(
-              g,
-              (function (e) {
-                  for (var t = 1; t < arguments.length; t++) {
-                      var n = null != arguments[t] ? arguments[t] : {},
-                          r = Object.keys(n);
-                      "function" == typeof Object.getOwnPropertySymbols &&
-                          (r = r.concat(
-                              Object.getOwnPropertySymbols(n).filter(function (e) {
-                                  return Object.getOwnPropertyDescriptor(n, e).enumerable;
-                              }),
-                          )),
-                          r.forEach(function (t) {
-                              var r;
-                              (r = n[t]),
-                                  t in e
-                                      ? Object.defineProperty(e, t, {
-                                            value: r,
-                                            enumerable: !0,
-                                            configurable: !0,
-                                            writable: !0,
-                                        })
-                                      : (e[t] = r);
-                          });
-                  }
-                  return e;
-              })({}, t),
-          )
-        : null;
-}
-function p(e) {
-    let { icon: t, children: n } = e;
-    return (0, r.jsxs)(a.Text, {
+function T(t) {
+    let { icon: e, children: s } = t;
+    return (0, i.jsxs)(a.Text, {
         variant: "text-sm/medium",
         tag: "li",
-        className: u.targetItem,
+        className: m.targetItem,
         children: [
-            (0, r.jsx)(t, {
+            (0, i.jsx)(e, {
                 size: "xs",
                 color: "currentColor",
             }),
-            n,
+            s,
         ],
     });
 }
