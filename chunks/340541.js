ab6d35ddeb88360c534a5268bd7ed13f11cfd44a
DiscrapperManager <admin@minerpl.xyz>
2025-09-05 19:03:28 +0200

    canaryRelease: 440916
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 14811
     * Scripts: 8616
    Strings: 22198
    Routes: 731
    Experiments Count: 466
    Apex Experiments Count: 49

--- chunks/340541.js
+++ chunks/340541.js
@@ -49,7 +49,7 @@ let a = (0, r.B)({
     o = (0, r.B)({
         kind: "user",
         id: "2025-09_invite_refresh_entrypoints",
-        label: "Invites Refresh",
+        label: "Invites Refresh Entrypoints Only",
         defaultConfig: {
             isVoiceChannelEntrypointEnabled: !1,
             isInCallEntrypointEnabled: !1,
