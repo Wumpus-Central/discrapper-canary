cfb593fe5d17e462c6906026d68c2972bd79916c
DiscrapperManager <admin@minerpl.xyz>
2025-09-05 21:07:13 +0200

    canaryRelease: 441035
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 14818
     * Scripts: 8615
    Strings: 22201
    Routes: 731
    Experiments Count: 466
    Apex Experiments Count: 50

--- chunks/526167.js
+++ chunks/526167.js
@@ -1,45 +1,50 @@
 n.d(t, {
-    G6: () => y,
-    hY: () => m,
-    og: () => b,
-    rO: () => O,
-    vu: () => f,
+    G6: () => I,
+    hY: () => g,
+    og: () => v,
+    rO: () => T,
+    vu: () => _,
 });
 var r,
     i,
     a,
     o,
     s,
-    l = n(525654),
-    c = n.n(l);
-let u = (null != (r = c().name) ? r : "unknown").toLowerCase(),
-    d = "chrome" === u.toLowerCase() ? parseInt(null != (i = c().version) ? i : "", 10) : -1;
-function f() {
-    return d;
+    l,
+    c = n(525654),
+    u = n.n(c);
+let d = (null != (r = u().name) ? r : "unknown").toLowerCase(),
+    f = "chrome" === d.toLowerCase() ? parseInt(null != (i = u().version) ? i : "", 10) : -1;
+function _() {
+    return f;
 }
-let _ = "electron" === u.toLowerCase() ? parseInt(null != (a = c().version) ? a : "", 10) : -1;
-function p() {
-    return _;
+let p = "electron" === d.toLowerCase() ? parseInt(null != (a = u().version) ? a : "", 10) : -1;
+function h() {
+    return p;
 }
-let h = "firefox" === u.toLowerCase() ? parseInt(null != (o = c().version) ? o : "", 10) : -1;
-function m() {
-    return h;
+let m = "firefox" === d.toLowerCase() ? parseInt(null != (o = u().version) ? o : "", 10) : -1;
+function g() {
+    return m;
 }
-let g = "edge" === u.toLowerCase() ? parseInt(null != (s = c().version) ? s : "", 10) : -1;
-function E() {
-    return g;
-}
+let E = "edge" === d.toLowerCase() ? parseInt(null != (s = u().version) ? s : "", 10) : -1;
 function b() {
-    return -1 !== f() || -1 !== p() || -1 !== m() || -1 !== E();
+    return E;
 }
-function y() {
+let y = "safari" === d.toLowerCase() ? parseInt(null != (l = u().version) ? l : "", 10) : -1;
+function O() {
+    return y;
+}
+function v() {
+    return -1 !== _() || -1 !== h() || -1 !== g() || -1 !== b() || O() >= 14;
+}
+function I() {
     let e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : navigator.userAgent,
         t = e.toLowerCase();
     return -1 !== t.indexOf("safari") && -1 === t.indexOf("chrome") && -1 !== t.indexOf("version/");
 }
-function O() {
+function T() {
     var e;
     let t = window.navigator,
         n = (null == (e = t.mediaCapabilities) ? void 0 : e.decodingInfo) != null;
-    return y(t.userAgent) && n;
+    return I(t.userAgent) && n;
 }
