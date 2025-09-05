6e463f80d938b9c2aa75c8f979e836cce61a6edf
DiscrapperManager <admin@minerpl.xyz>
2025-09-05 21:51:56 +0200

    canaryRelease: 441090
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 14819
     * Scripts: 8614
    Strings: 22202
    Routes: 731
    Experiments Count: 466
    Apex Experiments Count: 50

--- chunks/187232.js
+++ chunks/187232.js
@@ -1,4 +1,4 @@
-n.d(t, { p: () => S });
+n.d(t, { p: () => A });
 var r = n(100527),
     i = n(717401),
     a = n(474936),
@@ -7,68 +7,69 @@ var r = n(100527),
     l = n(154658),
     c = n(362290),
     u = n(873048),
-    d = n(306225),
-    f = n(28986),
-    _ = n(194950),
-    p = n(103865),
-    h = n(29653);
-let m = {
+    d = n(905287),
+    f = n(306225),
+    _ = n(28986),
+    p = n(194950),
+    h = n(103865),
+    m = n(29653);
+let g = {
         skuId: "1287835633485877369",
         assetId: "a_33656b7ed12cde00c1826b654cf65590",
     },
-    g = {
+    E = {
         skuId: "1287835633515102228",
         assetId: "a_c4395b178205217ad09cae605ef11b1a",
     },
-    E = {
+    b = {
         skuId: "1287835633615765524",
         assetId: "a_41445f736db3525135b6b9e1122f2254",
     },
-    b = {
+    y = {
         skuId: "1287835633645125653",
         assetId: "a_a78819f4d41900aa7698a1bafaf6af41",
     },
-    y = {
+    O = {
         skuId: "1287835633674620949",
         assetId: "a_5e8abacc7a7454d6b08b5cc84cac1d80",
     },
-    O = {
+    v = {
         skuId: "1287835633703845888",
         assetId: "a_a8c5af1848e53cc221d149442c03828f",
     },
-    v = {
+    I = {
         skuId: "1287835633733341224",
         assetId: "a_4f2b75e5adff09709702613ea0e2cb70",
     },
-    I = {
+    T = {
         skuId: "1287835633762701382",
         assetId: "a_604d1f0f336d41089acc3f82f458a99e",
     },
-    T = {
+    S = {
         gradient: ["rgb(35, 86, 53)", "rgb(3, 15, 40)"],
     },
-    S = {
+    A = {
         variants: [
             {
                 listingId: "1303067552619958292",
                 name: "Cat Ears",
-                variants: [m, g],
+                variants: [g, E],
             },
             {
                 name: "Dark Hood",
-                variants: [E, b],
+                variants: [b, y],
             },
             {
                 name: "Witch Hat",
-                variants: [y, O],
+                variants: [O, v],
             },
             {
                 listingId: "1303072627061166090",
                 name: "Zombie Food",
-                variants: [v, I],
+                variants: [I, T],
             },
         ],
-        rewardAssetIdMap: (0, i.YV)([m, g, E, b, y, O, v, I]),
+        rewardAssetIdMap: (0, i.YV)([g, E, b, y, O, v, I, T]),
         chatGiftIcon: {
             trinketsAnimation: {
                 getAnimationData: () =>
@@ -113,7 +114,7 @@ let m = {
         },
         dismissibleContentVersion: 4,
         firstTimeNotice: {
-            getImageUrl: (e, t) => (t ? _.Z : f.Z),
+            getImageUrl: () => d.Z,
             title: () => s.intl.string(s.t.UPFiIi),
             description: () => s.intl.string(s.t["F/5kFB"]),
             cta: () => s.intl.string(s.t.RzWDqa),
@@ -125,23 +126,23 @@ let m = {
             heading: () => s.intl.string(s.t.ruVnPj),
             subheading: () => s.intl.string(s.t.q4BJSE),
             mobileSubheading: (e) => s.intl.formatToPlainString(s.t.eDBuFx, { rewardCount: e }),
-            gradientConfig: T,
-            getImageUrl: () => f.Z,
-            getBannerImageUrl: () => h.Z,
+            gradientConfig: S,
+            getImageUrl: () => _.Z,
+            getBannerImageUrl: () => m.Z,
             getBackgroundImageUrl: () => l.Z,
         },
         planSelectionBanner: {
             desktopBannerText: () => s.intl.string(s.t.mMgymZ),
             mobileBannerText: () => s.intl.string(s.t.dAqTcH),
-            getImageUrl: () => _.Z,
-            gradientConfig: () => T,
-            getBackgroundImageUrl: () => d.Z,
+            getImageUrl: () => p.Z,
+            gradientConfig: () => S,
+            getBackgroundImageUrl: () => f.Z,
         },
         billingSettingsMarketingBanner: {
             getAnimatedImageUrl: () => c.Z,
             getStaticImageUrl: () => u.Z,
             title: () => s.intl.string(s.t.CGgAyM),
             body: () => s.intl.string(s.t["9rInzc"]),
-            getBackgroundImageUrl: () => p.Z,
+            getBackgroundImageUrl: () => h.Z,
         },
     };
