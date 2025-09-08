aca722b74738b3d77c147766a2173a65166b2ef7
DiscrapperManager <admin@minerpl.xyz>
2025-09-08 23:52:41 +0200

    canaryRelease: 441773
    Main scripts: 4
    Lazy Loaded Scripts: 1388
    Chunks:
     * Main Files: 15085
     * Scripts: 8375
    Strings: 22234
    Routes: 731
    Experiments Count: 463
    Apex Experiments Count: 52

--- chunks/795990.js
+++ chunks/795990.js
@@ -1,40 +1,46 @@
-n.d(t, { Z: () => u });
-var r = n(951288);
-n(647438);
-var i = n(481060),
-    l = n(785717),
-    a = n(299560),
-    o = n(286957),
-    s = n(388032),
-    c = n(958461);
-function u() {
-    let { trackUserProfileEditAction: e } = (0, l.KZ)();
+n.d(t, { Z: () => p }), n(388685);
+var r = n(951288),
+    i = n(647438),
+    l = n(481060),
+    a = n(785717),
+    o = n(810473),
+    s = n(299560),
+    c = n(247397),
+    u = n(388032),
+    d = n(958461);
+function p() {
+    let { trackUserProfileEditAction: e } = (0, a.KZ)(),
+        t = (0, o.Z)(),
+        n = i.useMemo(() => {
+            let e = new Set(t);
+            return c.rR.filter((t) => e.has(t));
+        }, [t]);
     return (0, r.jsxs)("div", {
-        className: c.empty,
+        className: d.empty,
         children: [
             (0, r.jsxs)("div", {
-                className: c.textContainer,
+                className: d.textContainer,
                 children: [
-                    (0, r.jsx)(i.X6q, {
+                    (0, r.jsx)(l.X6q, {
                         variant: "heading-md/medium",
                         color: "header-primary",
-                        children: s.intl.string(s.t.oqalCw),
+                        children: u.intl.string(u.t.oqalCw),
                     }),
-                    (0, r.jsx)(i.Text, {
+                    (0, r.jsx)(l.Text, {
                         variant: "text-sm/normal",
                         color: "header-secondary",
-                        children: s.intl.string(s.t.O9SQ1d),
+                        children: u.intl.string(u.t.O9SQ1d),
                     }),
                 ],
             }),
             (0, r.jsx)("ul", {
-                className: c.widgetGrid,
-                "aria-label": s.intl.string(s.t["+EIBSE"]),
-                children: o.rR.map((t) =>
+                className: d.widgetGrid,
+                "aria-label": u.intl.string(u.t["+EIBSE"]),
+                children: n.map((t) =>
                     (0, r.jsx)(
                         "li",
                         {
-                            children: (0, r.jsx)(a.Z, {
+                            children: (0, r.jsx)(s.Z, {
                                 widgetType: t,
                                 size: "small",
                                 trackUserProfileEditAction: e,
