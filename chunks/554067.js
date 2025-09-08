a5180b758b6343c8105d4d583d7acf1787574de4
DiscrapperManager <admin@minerpl.xyz>
2025-09-08 20:43:32 +0200

    canaryRelease: 441568
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 14821
     * Scripts: 8618
    Strings: 22225
    Routes: 731
    Experiments Count: 463
    Apex Experiments Count: 52

--- chunks/554067.js
+++ chunks/554067.js
@@ -1,64 +1,67 @@
-n.d(t, { Z: () => p }), n(388685);
+n.d(t, { Z: () => m }), n(388685);
 var r = n(951288),
     l = n(647438),
-    a = n(481060),
-    s = n(626135),
-    i = n(381585),
-    o = n(501431),
-    c = n(215023),
-    u = n(981631),
-    d = n(388032),
-    g = n(535333);
-function p(e) {
+    a = n(442837),
+    s = n(481060),
+    o = n(706454),
+    i = n(626135),
+    c = n(381585),
+    u = n(501431),
+    d = n(215023),
+    g = n(981631),
+    f = n(388032),
+    p = n(535333);
+function m(e) {
     let { handleTransition: t, selectedTab: n } = e,
-        { searchQuery: p, onSetSearchQuery: f } = (0, o.S)(),
-        [m, _] = l.useState(""),
-        h = (0, i.sp)();
+        { searchQuery: m, onSetSearchQuery: h } = (0, u.S)(),
+        [_, C] = l.useState(""),
+        b = (0, c.sp)(),
+        E = (0, a.e7)([o.default], () => o.default.locale);
     return (
         l.useEffect(() => {
             let e = setTimeout(() => {
-                f(m);
+                h(_);
             }, 250);
             return () => clearTimeout(e);
-        }, [m, f]),
+        }, [_, h]),
         l.useEffect(() => {
-            _(p);
-        }, [p]),
-        (0, r.jsx)(a.P3F, {
+            C(m);
+        }, [m]),
+        (0, r.jsx)(s.P3F, {
             ignoreKeyPress: !0,
             onClick: () => {
-                n !== c.AW.CATALOG && t(c.AW.CATALOG),
-                    s.default.track(u.rMx.COLLECTIBLES_SHOP_ELEMENT_CLICKED, {
-                        collectibles_shop_session_id: null == h ? void 0 : h.sessionId,
-                        page_section: null == h ? void 0 : h.pageSection,
-                        page_category: null == h ? void 0 : h.pageCategory,
-                        page_index: null == h ? void 0 : h.pageIndex,
-                        page_size: null == h ? void 0 : h.pageSize,
+                n !== d.AW.CATALOG && t(d.AW.CATALOG),
+                    i.default.track(g.rMx.COLLECTIBLES_SHOP_ELEMENT_CLICKED, {
+                        collectibles_shop_session_id: null == b ? void 0 : b.sessionId,
+                        page_section: null == b ? void 0 : b.pageSection,
+                        page_category: null == b ? void 0 : b.pageCategory,
+                        page_index: null == b ? void 0 : b.pageIndex,
+                        page_size: null == b ? void 0 : b.pageSize,
                         cta_name: "search bar",
                         page_type: n,
                     });
             },
-            children: (0, r.jsx)(a.E1j, {
+            children: (0, r.jsx)(s.E1j, {
                 size: "sm",
-                className: g.searchBar,
+                className: p.searchBar,
                 onKeyDown: (e) => {
-                    "Enter" === e.key && f(m);
+                    "Enter" === e.key && h(_);
                 },
-                query: m,
-                onChange: _,
+                query: _,
+                onChange: C,
                 onClear: () => {
-                    _(""),
-                        s.default.track(u.rMx.COLLECTIBLES_SHOP_ELEMENT_CLICKED, {
-                            collectibles_shop_session_id: null == h ? void 0 : h.sessionId,
-                            page_section: null == h ? void 0 : h.pageSection,
-                            page_category: null == h ? void 0 : h.pageCategory,
-                            page_index: null == h ? void 0 : h.pageIndex,
-                            page_size: null == h ? void 0 : h.pageSize,
+                    C(""),
+                        i.default.track(g.rMx.COLLECTIBLES_SHOP_ELEMENT_CLICKED, {
+                            collectibles_shop_session_id: null == b ? void 0 : b.sessionId,
+                            page_section: null == b ? void 0 : b.pageSection,
+                            page_category: null == b ? void 0 : b.pageCategory,
+                            page_index: null == b ? void 0 : b.pageIndex,
+                            page_size: null == b ? void 0 : b.pageSize,
                             cta_name: "search bar clear",
                             page_type: n,
                         });
                 },
-                placeholder: d.intl.string(d.t["hIt/Nj"]),
+                placeholder: "en-US" === E ? f.intl.string(f.t.arz34O) : f.intl.string(f.t["hIt/Nj"]),
             }),
         })
     );
