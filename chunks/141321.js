926954329a7cdb5fa2ea70b9cd26a117bf5c00fc
DiscrapperManager <admin@minerpl.xyz>
2025-09-08 07:48:11 +0200

    canaryRelease: 441381
    Main scripts: 4
    Lazy Loaded Scripts: 1389
    Chunks:
     * Main Files: 14817
     * Scripts: 8618
    Strings: 22211
    Routes: 731
    Experiments Count: 466
    Apex Experiments Count: 51

--- chunks/141321.js
+++ chunks/141321.js
@@ -1,108 +1,122 @@
-n.d(t, { Z: () => _ });
+n.d(t, { Z: () => E });
 var r = n(951288),
     i = n(647438),
-    l = n(442837),
-    a = n(481060),
-    o = n(317632),
-    s = n(602478),
-    c = n(665149),
-    u = n(388035),
-    d = n(306680),
-    p = n(594174),
-    f = n(178088),
-    h = n(222940),
-    g = n(490897),
-    m = n(388032),
-    b = n(741047);
-function _(e) {
-    let { onOpen: t, onClose: n, className: _, "data-jump-section": O } = e,
-        E = i.useRef(null),
-        y = (0, l.e7)([p.default], () => p.default.getCurrentUser()),
-        v = i.useRef(null),
-        I = (0, l.e7)(
-            [d.ZP],
-            () => (null == y ? void 0 : y.id) != null && d.ZP.getMentionCount(y.id, g.W.NOTIFICATION_CENTER) > 0,
+    l = n(120356),
+    a = n.n(l),
+    o = n(442837),
+    s = n(212605),
+    c = n(681715),
+    u = n(481060),
+    d = n(686546),
+    p = n(719961),
+    f = n(388035),
+    h = n(306680),
+    g = n(594174),
+    m = n(178088),
+    b = n(490897),
+    _ = n(388032),
+    O = n(741047);
+function E(e) {
+    let { onOpen: t, onClose: n, "data-jump-section": l } = e,
+        E = (0, o.e7)([g.default], () => g.default.getCurrentUser()),
+        y = i.useRef(null),
+        v = (0, o.e7)(
+            [h.ZP],
+            () => (null == E ? void 0 : E.id) != null && h.ZP.getMentionCount(E.id, b.W.NOTIFICATION_CENTER) > 0,
         ),
-        { enabled: C } = s.Z.useExperiment({ location: "RecentsButton" }, { autoTrackExposure: !1 }),
-        S = (0, l.e7)([o.Z], () => C && o.Z.getUnseenInviteCount() > 0),
-        N = (0, a.dQu)(a.TVs.modules.chat.INPUT_ICON_SIZE);
-    return (0, r.jsx)(f.k, {
-        targetElementRef: v,
+        { titlebarIconSize: I, titlebarHoverHighlight: C } = (0, p.T)({ location: "RecentsButton" }),
+        S = "sm" === I ? "refresh_sm" : "md",
+        T = i.useMemo(
+            () =>
+                (0, r.jsx)(u.xx7, {
+                    size: S,
+                    color: "currentColor",
+                }),
+            [S],
+        );
+    return (0, r.jsx)(m.k, {
+        targetElementRef: y,
         onOpen: t,
         onClose: n,
-        badgeState: { badgeForYou: I },
+        badgeState: { badgeForYou: v },
         popoutPosition: "bottom",
         popoutAlign: "right",
         children: (e, t, n, i) => {
-            var l, o;
-            return (0, r.jsxs)("div", {
-                ref: v,
-                className: b.recentsIcon,
-                children: [
-                    C &&
-                        (0, r.jsx)(h.Z, {
-                            inboxIconRef: v,
-                            recentsPopoutShown: t,
-                        }),
-                    (0, r.jsx)(u.U, {
-                        location: "inbox-button",
-                        targetElementRef: E,
-                        children: (0, r.jsx)(
-                            c.JO,
-                            ((l = (function (e) {
-                                for (var t = 1; t < arguments.length; t++) {
-                                    var n = null != arguments[t] ? arguments[t] : {},
-                                        r = Object.keys(n);
-                                    "function" == typeof Object.getOwnPropertySymbols &&
-                                        (r = r.concat(
-                                            Object.getOwnPropertySymbols(n).filter(function (e) {
-                                                return Object.getOwnPropertyDescriptor(n, e).enumerable;
-                                            }),
-                                        )),
-                                        r.forEach(function (t) {
-                                            var r;
-                                            (r = n[t]),
-                                                t in e
-                                                    ? Object.defineProperty(e, t, {
-                                                          value: r,
-                                                          enumerable: !0,
-                                                          configurable: !0,
-                                                          writable: !0,
-                                                      })
-                                                    : (e[t] = r);
-                                        });
-                                }
-                                return e;
-                            })({}, n)),
-                            (o = o =
-                                {
-                                    ref: E,
-                                    className: _,
-                                    onClick: e,
-                                    icon: a.xx7,
-                                    iconSize: N,
-                                    "aria-label": m.intl.string(m.t.GSmTKC),
-                                    tooltip: t ? null : m.intl.string(m.t.GSmTKC),
-                                    selected: t,
-                                    showBadge: I || S || i,
-                                    "data-jump-section": O,
-                                }),
-                            Object.getOwnPropertyDescriptors
-                                ? Object.defineProperties(l, Object.getOwnPropertyDescriptors(o))
-                                : (function (e, t) {
-                                      var n = Object.keys(e);
-                                      if (Object.getOwnPropertySymbols) {
-                                          var r = Object.getOwnPropertySymbols(e);
-                                          n.push.apply(n, r);
-                                      }
-                                      return n;
-                                  })(Object(o)).forEach(function (e) {
-                                      Object.defineProperty(l, e, Object.getOwnPropertyDescriptor(o, e));
-                                  }),
-                            l),
-                        ),
-                    }),
-                ],
+            var o, p;
+            return (0, r.jsx)(f.U, {
+                location: "inbox-button",
+                targetElementRef: y,
+                children: (0, r.jsx)(c.u, {
+                    targetElementRef: y,
+                    shouldShow: !t,
+                    text: _.intl.string(_.t.GSmTKC),
+                    children: (0, r.jsx)(
+                        u.P3F,
+                        ((o = (function (e) {
+                            for (var t = 1; t < arguments.length; t++) {
+                                var n = null != arguments[t] ? arguments[t] : {},
+                                    r = Object.keys(n);
+                                "function" == typeof Object.getOwnPropertySymbols &&
+                                    (r = r.concat(
+                                        Object.getOwnPropertySymbols(n).filter(function (e) {
+                                            return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                                        }),
+                                    )),
+                                    r.forEach(function (t) {
+                                        var r;
+                                        (r = n[t]),
+                                            t in e
+                                                ? Object.defineProperty(e, t, {
+                                                      value: r,
+                                                      enumerable: !0,
+                                                      configurable: !0,
+                                                      writable: !0,
+                                                  })
+                                                : (e[t] = r);
+                                    });
+                            }
+                            return e;
+                        })({}, n)),
+                        (p = p =
+                            {
+                                tag: "div",
+                                innerRef: y,
+                                onClick: e,
+                                "data-jump-section": l,
+                                "aria-label": _.intl.string(_.t.GSmTKC),
+                                className: a()(O.clickable, { [O.withHighlight]: C }),
+                                children:
+                                    v || i
+                                        ? (0, r.jsxs)(r.Fragment, {
+                                              children: [
+                                                  (0, r.jsx)(d.ZP, {
+                                                      mask: d.ZP.Masks.HEADER_BAR_BADGE_BOTTOM,
+                                                      height: s.Z[S],
+                                                      width: s.Z[S],
+                                                      children: T,
+                                                  }),
+                                                  (0, r.jsx)("span", {
+                                                      className: a()(O.badge, { [O.smol]: "refresh_sm" === S }),
+                                                  }),
+                                              ],
+                                          })
+                                        : T,
+                            }),
+                        Object.getOwnPropertyDescriptors
+                            ? Object.defineProperties(o, Object.getOwnPropertyDescriptors(p))
+                            : (function (e, t) {
+                                  var n = Object.keys(e);
+                                  if (Object.getOwnPropertySymbols) {
+                                      var r = Object.getOwnPropertySymbols(e);
+                                      n.push.apply(n, r);
+                                  }
+                                  return n;
+                              })(Object(p)).forEach(function (e) {
+                                  Object.defineProperty(o, e, Object.getOwnPropertyDescriptor(p, e));
+                              }),
+                        o),
+                    ),
+                }),
             });
         },
     });
--- chunks/741047.js
+++ chunks/741047.js
@@ -1,1 +1,6 @@
-e.exports = { recentsIcon: "recentsIcon_c99c29" };
+e.exports = {
+    clickable: "clickable_c99c29",
+    withHighlight: "withHighlight_c99c29",
+    badge: "badge_c99c29",
+    smol: "smol_c99c29",
+};
