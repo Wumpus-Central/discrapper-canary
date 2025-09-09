d6f68ff68a1ffafa6da36145ec01f35c8eff6364
DiscrapperManager <admin@minerpl.xyz>
2025-09-09 05:13:06 +0200

    canaryRelease: 441981
    Main scripts: 4
    Lazy Loaded Scripts: 1388
    Chunks:
     * Main Files: 15099
     * Scripts: 8378
    Strings: 22229
    Routes: 731
    Experiments Count: 463
    Apex Experiments Count: 53

--- chunks/510231.js
+++ chunks/510231.js
@@ -1,41 +1,42 @@
 n.d(t, {
-    N: () => d,
-    h: () => f,
+    N: () => f,
+    h: () => _,
 }),
     n(583741);
 var r = n(647438),
     i = n(392711),
     a = n.n(i),
-    o = n(442837),
-    s = n(189451),
-    l = n(77498),
-    c = n(570911);
-function u(e) {
-    var t;
-    return null != (t = c.gQ.get(e)) ? t : 0;
-}
+    o = n(624238),
+    s = n(442837),
+    l = n(189451),
+    c = n(77498),
+    u = n(570911);
 function d(e) {
-    return (null == e ? void 0 : e.themes) != null && e.themes.includes(s.J8.EROTIC);
+    var t;
+    return null != (t = u.gQ.get(e)) ? t : 0;
 }
-function f() {
-    let e = (0, o.e7)([l.Z], () => l.Z.games),
+function f(e) {
+    return (null == e ? void 0 : e.themes) != null && e.themes.includes(l.J8.EROTIC);
+}
+function _() {
+    let e = (0, s.e7)([c.Z], () => c.Z.games),
         t = r.useMemo(
             () =>
                 a()(e)
-                    .filter((e) => !d(e))
+                    .filter((e) => !f(e) && !o.Fb.has(e.id))
                     .map((e) => ({
                         value: e.id,
                         label: e.name,
                     }))
                     .sortBy((e) => {
                         let { value: t } = e;
-                        return u(t);
+                        return d(t);
                     })
                     .reverse()
                     .value(),
             [e],
         ),
-        n = r.useCallback((e, t) => u(t.item.value) - u(e.item.value), []);
+        n = r.useCallback((e, t) => d(t.item.value) - d(e.item.value), []);
     return {
         options: t,
         matchSorterOptions: r.useMemo(
