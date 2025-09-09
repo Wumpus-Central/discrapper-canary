73252ae3415b19328d8852838e2a9a2ff3cd752b
DiscrapperManager <admin@minerpl.xyz>
2025-09-09 19:50:54 +0200

    canaryRelease: 442117
    Main scripts: 4
    Lazy Loaded Scripts: 1388
    Chunks:
     * Main Files: 15103
     * Scripts: 8378
    Strings: 22231
    Routes: 731
    Experiments Count: 463
    Apex Experiments Count: 54

--- chunks/126848.js
+++ chunks/126848.js
@@ -1,6 +1,6 @@
 n.d(t, {
-    Z: () => b,
-    x: () => E,
+    Z: () => v,
+    x: () => O,
 }),
     n(539854),
     n(388685);
@@ -9,59 +9,68 @@ var r = n(951288),
     a = n(120356),
     o = n.n(a),
     s = n(704215),
-    l = n(243778),
-    c = n(367074),
-    u = n(104494),
-    d = n(639119),
-    f = n(655525),
-    _ = n(924540),
-    p = n(170671),
-    h = n(921944),
-    m = n(474936),
-    g = n(563042);
-function E() {
+    l = n(481060),
+    c = n(44315),
+    u = n(436774),
+    d = n(243778),
+    f = n(367074),
+    _ = n(104494),
+    p = n(639119),
+    h = n(655525),
+    m = n(924540),
+    g = n(981631),
+    E = n(921944),
+    b = n(474936),
+    y = n(563042);
+function O() {
     let e,
         t = arguments.length > 0 && void 0 !== arguments[0] && arguments[0],
-        n = (0, d.N)(),
-        a = (0, u.Ng)(),
-        o = (0, c.Vi)(),
-        p = (null == n ? void 0 : n.trial_id) === m.a7,
-        g = [];
-    o && g.push(s.z.BOGO_2025_NITRO_TAB_BADGE);
-    let [E, b] = (0, l.US)(g, void 0, !0);
+        n = (0, p.N)(),
+        a = (0, _.Ng)(),
+        o = (0, f.Vi)(),
+        l = (null == n ? void 0 : n.trial_id) === b.a7,
+        c = [];
+    o && c.push(s.z.BOGO_2025_NITRO_TAB_BADGE);
+    let [u, g] = (0, d.US)(c, void 0, !0);
     return (
         i.useEffect(() => {
-            !0 === t && E === s.z.BOGO_2025_NITRO_TAB_BADGE && b(h.L.AUTO_DISMISS);
-        }, [b, t, E]),
+            !0 === t && u === s.z.BOGO_2025_NITRO_TAB_BADGE && g(E.L.AUTO_DISMISS);
+        }, [g, t, u]),
         o
-            ? (e = (0, r.jsx)(f.Z, {}))
+            ? (e = (0, r.jsx)(h.Z, {}))
             : null != a
-              ? (e = (0, r.jsx)(_.GN, {
+              ? (e = (0, r.jsx)(m.GN, {
                     isTabSelected: t,
                     userDiscount: a,
                     includesAmountOff: !1,
                 }))
               : null == n ||
-                p ||
-                (e = (0, r.jsx)(_.$H, {
+                l ||
+                (e = (0, r.jsx)(m.$H, {
                     isTabSelected: t,
                     trialOffer: n,
                 })),
         e
     );
 }
-function b(e) {
+function v(e) {
     var t, n;
     let { decoration: i, label: a, isSelected: s } = e,
-        l = (0, c.Vi)(),
-        u = null != (n = null != (t = E(s)) ? t : i) ? n : (0, r.jsx)(p.Z, { isSelected: s });
+        d = (0, f.Vi)(),
+        _ =
+            null != (n = null != (t = O(s)) ? t : i)
+                ? n
+                : (0, r.jsx)(l.SrA, {
+                      size: "sm",
+                      color: s ? (0, c.Lq)(g.Ilk.WHITE_500) : u.JX.PREMIUM_TIER_2,
+                  });
     return (0, r.jsxs)(r.Fragment, {
         children: [
             (0, r.jsxs)("div", {
-                className: o()(g.premiumLabel, { [g.selected]: s && !l }),
-                children: [a, u],
+                className: o()(y.premiumLabel, { [y.selected]: s && !d }),
+                children: [a, _],
             }),
-            (0, r.jsx)("div", { className: o()(g.background, { [g.selectedBackground]: !l && s }) }),
+            (0, r.jsx)("div", { className: o()(y.background, { [y.selectedBackground]: !d && s }) }),
         ],
     });
 }
