e87c27f451d941d4d0a5c43d7575bb8d479630aa
DiscrapperManager <admin@minerpl.xyz>
2025-09-09 03:04:09 +0200

    canaryRelease: 441961
    Main scripts: 4
    Lazy Loaded Scripts: 1388
    Chunks:
     * Main Files: 15099
     * Scripts: 8378
    Strings: 22234
    Routes: 731
    Experiments Count: 463
    Apex Experiments Count: 53

--- chunks/295536.js
+++ chunks/295536.js
@@ -22,7 +22,7 @@ function p(e) {
                   label: f.name,
                   icon: (0, r.jsx)(a.Z, {
                       game: f,
-                      size: a.A.XSMALL,
+                      className: d.icon,
                   }),
                   setStatus: () => (0, c.XE)(f.id, !0),
               }),
--- chunks/972526.js
+++ chunks/972526.js
@@ -1,1 +1,4 @@
-e.exports = { container: "container__912a0" };
+e.exports = {
+    container: "container__912a0",
+    icon: "icon__912a0",
+};
