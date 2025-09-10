0a333d313adcf5ba0665535cce6f8c1a526f313f
DiscrapperManager <admin@minerpl.xyz>
2025-09-10 21:32:04 +0200

    canaryRelease: 442886
    Main scripts: 4
    Lazy Loaded Scripts: 1398
    Chunks:
     * Main Files: 15099
     * Scripts: 8421
    Strings: 22270
    Routes: 732
    Experiments Count: 460
    Apex Experiments Count: 56

--- chunks/176757.js
+++ chunks/176757.js
@@ -1,29 +1,31 @@
-n.d(t, { Z: () => f });
+n.d(t, { Z: () => p });
 var r = n(442837),
     i = n(570140),
-    a = n(266198);
-let o = !1,
-    s = !1;
-function l() {
-    (o = !1), (s = !1);
+    a = n(266198),
+    o = n(219333),
+    s = n(304426);
+let l = !1,
+    c = !1;
+function u() {
+    (l = !1), (c = !1);
 }
-function c(e) {
-    e.entitlement.sku_id === a.Fw && (o = !0);
+function d(e) {
+    e.entitlement.sku_id === a.Fw && ((0, o.j)({ campaignID: s.C }), (l = !0));
 }
-function u(e) {
-    e.marketingCampaignId === a.Fw && (s = !0);
+function f(e) {
+    e.marketingCampaignId === a.Fw && (c = !0);
 }
-class d extends r.ZP.Store {
+class _ extends r.ZP.Store {
     get isSeptemberMarketingMomentEntitlementCreated() {
-        return o;
+        return l;
     }
     get isMarketingCampaignApplicationModalViewed() {
-        return s;
+        return c;
     }
 }
-let f = new d(i.Z, {
-    ENTITLEMENT_CREATE: c,
-    ENTITLEMENT_UPDATE: c,
-    MARKETING_CAMPAIGN_APPLICATION_MODAL_VIEWED: u,
-    LOGOUT: l,
+let p = new _(i.Z, {
+    ENTITLEMENT_CREATE: d,
+    ENTITLEMENT_UPDATE: d,
+    MARKETING_CAMPAIGN_APPLICATION_MODAL_VIEWED: f,
+    LOGOUT: u,
 });
