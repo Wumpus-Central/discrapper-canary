54750c736d9f810f0e55a499bb7605a6254ca565
DiscrapperManager <admin@minerpl.xyz>
2025-09-10 17:47:56 +0200

    canaryRelease: 442668
    Main scripts: 4
    Lazy Loaded Scripts: 1391
    Chunks:
     * Main Files: 15106
     * Scripts: 8394
    Strings: 22258
    Routes: 732
    Experiments Count: 460
    Apex Experiments Count: 54

--- chunks/176179.js
+++ chunks/176179.js
@@ -2,5 +2,4 @@ e.exports = {
     container: "container__879ae",
     image: "image__879ae",
     close: "close__879ae",
-    innerClose: "innerClose__879ae",
 };
--- chunks/800869.js
+++ chunks/800869.js
@@ -1,38 +1,44 @@
-n.d(t, { Z: () => d });
+n.d(t, { Z: () => p });
 var r = n(951288),
-    o = n(793030),
-    i = n(481060),
+    i = n(793030),
+    o = n(861066),
+    s = n(481060),
     l = n(921944),
-    s = n(989308),
-    a = n(388032),
-    c = n(176179),
-    u = n(719119);
-function d(e) {
+    a = n(989308),
+    c = n(388032),
+    u = n(176179),
+    d = n(719119);
+function p(e) {
     let { markAsDismissed: t } = e;
     return (0, r.jsxs)("div", {
-        className: c.container,
+        className: u.container,
         children: [
             (0, r.jsx)("img", {
                 alt: "",
-                className: c.image,
-                src: u,
+                className: u.image,
+                src: d,
             }),
-            (0, r.jsx)(o.xv, {
+            (0, r.jsx)(i.xv, {
                 color: "always-white",
                 variant: "text-md/semibold",
-                children: a.intl.string(s.default.diMhWV),
+                children: c.intl.string(a.default.diMhWV),
             }),
-            (0, r.jsx)(o.xv, {
+            (0, r.jsx)(i.xv, {
                 color: "always-white",
                 variant: "text-sm/medium",
-                children: a.intl.string(s.default.pycxTk),
+                children: c.intl.string(a.default.pycxTk),
             }),
-            (0, r.jsx)(i.olH, {
-                className: c.close,
-                innerClassName: c.innerClose,
-                onClick: () => {
-                    t(l.L.USER_DISMISS);
-                },
+            (0, r.jsx)("div", {
+                className: u.close,
+                children: (0, r.jsx)(o.h, {
+                    variant: "icon-only",
+                    size: "md",
+                    "aria-label": c.intl.string(c.t.cpT0Cg),
+                    onClick: () => {
+                        t(l.L.USER_DISMISS);
+                    },
+                    icon: s.Dio,
+                }),
             }),
         ],
     });
