175a47defdda8d77f83f0a31ef3187d91918ec19
DiscrapperManager <admin@minerpl.xyz>
2025-09-11 20:33:34 +0200

    canaryRelease: 443370
    Main scripts: 4
    Lazy Loaded Scripts: 1459
    Chunks:
     * Main Files: 15104
     * Scripts: 8485
    Strings: 22273
    Routes: 734
    Experiments Count: 459
    Apex Experiments Count: 58

--- chunks/918714.js
+++ chunks/918714.js
@@ -1,72 +1,68 @@
 n.d(t, { Z: () => x }), n(388685);
 var r = n(951288),
     i = n(647438),
-    a = n(481060),
-    l = n(480916),
-    o = n(17597),
-    c = n(624138),
-    s = n(845077),
-    u = n(132392),
-    d = n(118589),
-    f = n(535188),
-    p = n(856597),
+    l = n(481060),
+    a = n(893776),
+    o = n(480916),
+    c = n(17597),
+    s = n(624138),
+    u = n(845077),
+    d = n(132392),
+    f = n(118589),
+    p = n(535188),
+    h = n(856597),
     m = n(241945),
     y = n(388032),
     g = n(104940);
-let h = n(575703);
+let b = n(575703);
 function E(e) {
     let { task: t, handleSubmit: n, disabled: l } = e,
-        [o, c] = i.useState(t),
-        s = i.useMemo(
+        [a, o] = i.useState(t),
+        c = i.useMemo(
             () => ({
-                task: o,
-                setTask: c,
+                task: a,
+                setTask: o,
             }),
-            [o, c],
+            [a, o],
         );
     return (
         i.useEffect(() => {
-            c(t);
+            o(t);
         }, [t]),
-        (0, r.jsx)(u.y.Provider, {
-            value: s,
-            children: (0, r.jsxs)(a.Kqy, {
-                direction: "horizontal",
-                justify: "start",
-                className: g.body,
-                children: [
-                    (0, r.jsx)(p.Z, {}),
-                    (0, r.jsx)(f.Z, {
-                        onSubmit: n,
-                        disabled: l,
-                    }),
-                ],
-            }),
+        (0, r.jsxs)(d.y.Provider, {
+            value: c,
+            children: [
+                (0, r.jsx)(h.Z, {}),
+                (0, r.jsx)(p.Z, {
+                    onSubmit: n,
+                    disabled: l,
+                }),
+            ],
         })
     );
 }
 let x = function (e) {
     let { onClose: t } = e,
-        [n, u] = i.useState(null),
-        [f, p] = i.useState(!0),
+        [n, d] = i.useState(null),
+        [p, h] = i.useState(!0),
         [x, v] = i.useState(null),
-        [b, j] = i.useState(!1),
-        _ = i.useCallback(async () => {
-            p(!0), v(null);
+        [j, _] = i.useState(!1),
+        O = i.useCallback(async () => {
+            h(!0), v(null);
             try {
                 var e;
-                let n = await (0, s.PA)();
+                let n = await (0, u.PA)();
                 if (null == n) return void t();
-                !(0, c.Ew)(null == (e = n.ui_component) ? void 0 : e.component.type) &&
-                d.VZ.has(n.ui_component.component.type)
-                    ? u(n)
-                    : u({
+                !(0, s.Ew)(null == (e = n.ui_component) ? void 0 : e.component.type) &&
+                f.VZ.has(n.ui_component.component.type)
+                    ? d(n)
+                    : d({
                           task_id: n.task_id,
-                          task_type: d.UA.REFRESH_APP,
+                          task_type: f.UA.REFRESH_APP,
                           assignment_id: n.assignment_id,
                           ui_component: {
                               component: {
-                                  type: d.NS.REFRESH_APP,
+                                  type: f.NS.REFRESH_APP,
                                   data: {},
                               },
                           },
@@ -78,78 +74,118 @@ let x = function (e) {
             } catch (e) {
                 v(y.intl.string(m.default["/f++3t"]));
             } finally {
-                p(!1);
+                h(!1);
             }
         }, [t]),
-        O = i.useCallback(
+        A = i.useCallback(
             async (e) => {
                 if (null !== n) {
-                    j(!0);
+                    _(!0);
                     try {
                         let t = {
                             task_id: n.task_id,
                             flow_id: n.flow_context.flow_id,
                             data: e,
                         };
-                        await (0, s.Wl)(t), _();
+                        await (0, u.Wl)(t), O();
                     } catch (e) {
                         v(y.intl.string(m.default["+QRSxc"]));
                     } finally {
-                        j(!1);
+                        _(!1);
                     }
                 }
             },
-            [n, _],
+            [n, O],
         );
     i.useEffect(() => {
-        _();
-    }, [_]);
-    let A = i.useMemo(() => (null == n ? void 0 : n.task_type) === d.UA.AGE_VERIFICATION, [n]);
+        O();
+    }, [O]);
+    let S = i.useMemo(() => (null == n ? void 0 : n.task_type) === f.UA.AGE_VERIFICATION, [n]);
     return (0, r.jsxs)("div", {
         className: g.background,
         children: [
             (0, r.jsx)("img", {
                 className: g.artwork,
-                src: h,
+                src: b,
                 alt: "",
             }),
-            A
-                ? (0, r.jsx)(o.default, {
-                      transitionState: a.Dvm.ENTERED,
-                      entryPoint: l.cU.SAFETY_FLOWS,
+            S
+                ? (0, r.jsx)(c.default, {
+                      transitionState: l.Dvm.ENTERED,
+                      entryPoint: o.cU.SAFETY_FLOWS,
                       onClose: async () => {},
                       onComplete: async () => {
-                          await O({ type: d.rY.Empty });
+                          await A({ type: f.rY.Empty });
                       },
                   })
                 : (0, r.jsx)("div", {
                       className: g.safetyFlow,
-                      children: (0, r.jsxs)("div", {
-                          className: g.container,
-                          children: [
-                              f &&
-                                  (0, r.jsx)(a.Text, {
-                                      variant: "text-md/normal",
-                                      color: "text-muted",
-                                      children: y.intl.string(m.default.v0H5Tk),
-                                  }),
-                              null !== x &&
-                                  (0, r.jsx)(a.Text, {
-                                      variant: "text-md/normal",
-                                      color: "text-danger",
-                                      className: g.error,
-                                      children: x,
-                                  }),
-                              !f &&
-                                  null === x &&
-                                  null != n &&
-                                  (0, r.jsx)(E, {
-                                      task: n,
-                                      handleSubmit: O,
-                                      disabled: b,
-                                  }),
-                          ],
-                      }),
+                      children: p
+                          ? (0, r.jsx)(l.$jN, { type: l.$jN.Type.SPINNING_CIRCLE })
+                          : (0, r.jsx)("div", {
+                                className: g.container,
+                                children: (0, r.jsxs)(l.Kqy, {
+                                    direction: "horizontal",
+                                    justify: "start",
+                                    className: g.body,
+                                    children: [
+                                        null !== x &&
+                                            (0, r.jsxs)(l.Kqy, {
+                                                direction: "vertical",
+                                                justify: "space-between",
+                                                align: "center",
+                                                className: g.interimBody,
+                                                padding: 16,
+                                                children: [
+                                                    (0, r.jsxs)(l.Kqy, {
+                                                        direction: "vertical",
+                                                        gap: 4,
+                                                        children: [
+                                                            (0, r.jsx)(l.X6q, {
+                                                                variant: "heading-xl/semibold",
+                                                                children: y.intl.string(y.t.c6kn6O),
+                                                            }),
+                                                            (0, r.jsx)(l.Text, {
+                                                                variant: "text-md/normal",
+                                                                color: "text-muted",
+                                                                children: y.intl.string(y.t.ZUEGFh),
+                                                            }),
+                                                        ],
+                                                    }),
+                                                    (0, r.jsxs)(l.Kqy, {
+                                                        direction: "horizontal",
+                                                        justify: "center",
+                                                        align: "center",
+                                                        children: [
+                                                            (0, r.jsx)(l.zxk, {
+                                                                fullWidth: !0,
+                                                                variant: "secondary",
+                                                                text: y.intl.string(y.t["2jxGen"]),
+                                                                onClick: () => {
+                                                                    a.Z.logout("safety_flows_modal");
+                                                                },
+                                                            }),
+                                                            (0, r.jsx)(l.zxk, {
+                                                                fullWidth: !0,
+                                                                text: y.intl.string(y.t["7NqTJi"]),
+                                                                onClick: () => {
+                                                                    O();
+                                                                },
+                                                            }),
+                                                        ],
+                                                    }),
+                                                ],
+                                            }),
+                                        null === x &&
+                                            null != n &&
+                                            (0, r.jsx)(E, {
+                                                task: n,
+                                                handleSubmit: A,
+                                                disabled: j,
+                                            }),
+                                    ],
+                                }),
+                            }),
                   }),
         ],
     });
