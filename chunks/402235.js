783ae8f4496c1ab1a6828541287bd0e94a006ee6
DiscrapperManager <admin@minerpl.xyz>
2025-09-11 18:59:38 +0200

    canaryRelease: 443297
    Main scripts: 4
    Lazy Loaded Scripts: 1459
    Chunks:
     * Main Files: 15101
     * Scripts: 8485
    Strings: 22270
    Routes: 733
    Experiments Count: 459
    Apex Experiments Count: 58

--- chunks/402235.js
+++ chunks/402235.js
@@ -1,32 +1,24 @@
 n.d(t, {
-    ZP: () => l,
-    yH: () => c,
-    zI: () => u,
+    ZP: () => o,
+    yH: () => s,
+    zI: () => l,
 });
 var r = n(442837),
     i = n(430824),
-    a = n(232062),
-    o = n(944888),
-    s = n(981631);
+    a = n(981631);
+function o(e, t) {
+    return (0, r.e7)([i.Z], () => {
+        let t = i.Z.getGuild(e);
+        return null != t && t.features.has(a.oNc.ENHANCED_ROLE_COLORS);
+    });
+}
+function s(e, t) {
+    return (0, r.e7)([i.Z], () => {
+        let t = i.Z.getGuild(e);
+        return null != t && t.features.has(a.oNc.ENHANCED_ROLE_COLORS);
+    });
+}
 function l(e, t) {
-    let n = (0, r.e7)([i.Z], () => {
-            let t = i.Z.getGuild(e);
-            return null != t && t.features.has(s.oNc.ENHANCED_ROLE_COLORS);
-        }),
-        a = (0, o.ZP)(null != e ? e : void 0, null != t ? t : void 0);
-    return n || a;
-}
-function c(e, t) {
-    let n = (0, r.e7)([i.Z], () => {
-            let t = i.Z.getGuild(e);
-            return null != t && t.features.has(s.oNc.ENHANCED_ROLE_COLORS);
-        }),
-        l = (0, a.W)(e, "useHasEnhancedRoleColorsForRole") && null != t && (0, o.YB)(t);
-    return n || l;
-}
-function u(e, t) {
-    let n = i.Z.getGuild(e),
-        r = null != n && n.features.has(s.oNc.ENHANCED_ROLE_COLORS),
-        l = (0, a.Tq)(e, "getHasEnhancedRoleColorsForRole") && (0, o.YB)(t);
-    return r || l;
+    let n = i.Z.getGuild(e);
+    return null != n && n.features.has(a.oNc.ENHANCED_ROLE_COLORS);
 }
--- chunks/852898.js
+++ chunks/852898.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => h });
+n.d(t, { Z: () => p });
 var r = n(951288),
     i = n(647438),
     l = n(951284),
@@ -6,40 +6,39 @@ var r = n(951288),
     s = n(139387),
     o = n(726542),
     c = n(690221),
-    d = n(232062),
-    u = n(434404),
-    m = n(999382),
-    g = n(981631),
-    p = n(388032);
-function h(e, t) {
-    let n = (0, a.e7)([m.Z], () => m.Z.getProps().integrations);
+    d = n(434404),
+    u = n(999382),
+    m = n(981631),
+    g = n(388032);
+function p(e, t) {
+    let n = (0, a.e7)([u.Z], () => u.Z.getProps().integrations);
     return i.useMemo(
         () =>
             (function (e, t, n) {
-                var i, a, m, h;
+                var i, a, u, p;
                 let f;
                 if (!t.managed) return null;
-                let b = null == (i = t.tags) ? void 0 : i.bot_id,
-                    x = null == (a = t.tags) ? void 0 : a.integration_id,
-                    j = (null == (m = t.tags) ? void 0 : m.premium_subscriber) !== void 0,
-                    v = (null == (h = t.tags) ? void 0 : h.guild_connections) !== void 0;
+                let h = null == (i = t.tags) ? void 0 : i.bot_id,
+                    b = null == (a = t.tags) ? void 0 : a.integration_id,
+                    x = (null == (u = t.tags) ? void 0 : u.premium_subscriber) !== void 0,
+                    j = (null == (p = t.tags) ? void 0 : p.guild_connections) !== void 0;
                 if (
-                    (null != b
+                    (null != h
                         ? (f =
                               null == n
                                   ? void 0
                                   : n.find((e) => {
                                         var t;
                                         let { application: n } = e;
-                                        return (null == n || null == (t = n.bot) ? void 0 : t.id) === b;
+                                        return (null == n || null == (t = n.bot) ? void 0 : t.id) === h;
                                     }))
-                        : null != x &&
+                        : null != b &&
                           (f =
                               null == n
                                   ? void 0
                                   : n.find((e) => {
                                         let { id: t } = e;
-                                        return t === x;
+                                        return t === b;
                                     })),
                     null != f && null != f.application && null != f.name)
                 ) {
@@ -48,9 +47,9 @@ function h(e, t) {
                     [l.b.TWITCH, l.b.YOUTUBE].includes(f.type) &&
                         (t = "".concat(o.Z.get(f.type).name, " - ").concat(t));
                     let n = () => {
-                        u.Z.setSection(g.pNK.INTEGRATIONS), s.Z.setSection(g.b4C.APPLICATION, e);
+                        d.Z.setSection(m.pNK.INTEGRATIONS), s.Z.setSection(m.b4C.APPLICATION, e);
                     };
-                    return p.intl.format(p.t.FuXsWl, {
+                    return g.intl.format(g.t.FuXsWl, {
                         name: t,
                         integrationNameHook: (e, t) =>
                             (0, r.jsx)(
@@ -63,14 +62,8 @@ function h(e, t) {
                             ),
                     });
                 }
-                return j
-                    ? (0, d.Tq)(e, "useManagedRoleWarningText")
-                        ? p.intl.string(p.t.NqRten)
-                        : p.intl.string(p.t.oF6FYW)
-                    : v
-                      ? p.intl.string(p.t.ZQ37tL)
-                      : p.intl.string(p.t.k5d7DA);
-            })(e, t, n),
+                return x ? g.intl.string(g.t.oF6FYW) : j ? g.intl.string(g.t.ZQ37tL) : g.intl.string(g.t.k5d7DA);
+            })(0, t, n),
         [e, t, n],
     );
 }
