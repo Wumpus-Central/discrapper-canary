07421d056ae5770915dcc0d0fb7581c7d79a4246
DiscrapperManager <admin@minerpl.xyz>
2025-09-12 01:50:16 +0200

    canaryRelease: 443762
    Main scripts: 4
    Lazy Loaded Scripts: 1543
    Chunks:
     * Main Files: 15132
     * Scripts: 8585
    Strings: 22287
    Routes: 733
    Experiments Count: 458
    Apex Experiments Count: 60

--- chunks/905870.js
+++ chunks/905870.js
@@ -1,8 +1,8 @@
-n.d(t, { Z: () => m }), n(642613), n(388685), n(539854), n(361932), n(187205);
+n.d(t, { Z: () => g }), n(642613), n(388685), n(539854), n(361932), n(187205);
 var r = n(147913),
     i = n(579806),
-    o = n(626135),
-    a = n(70956),
+    a = n(626135),
+    o = n(70956),
     s = n(358085),
     l = n(848479),
     c = n(981631);
@@ -19,20 +19,21 @@ function u(e, t, n) {
         e
     );
 }
-let d = 15 * a.Z.Millis.MINUTE,
+let d = 15 * o.Z.Millis.MINUTE,
     f = 1835008,
-    _ = !0;
-function p() {
+    _ = !0,
+    p = 12;
+function h() {
     return s.isPlatformEmbedded && (0, s.isWindows)();
 }
-class h extends r.Z {
+class m extends r.Z {
     _initialize() {}
     _terminate() {
-        p() && (clearInterval(this._checkInterval), (this._checkInterval = null), l.Z.disablePerfMemoryHooks());
+        h() && (clearInterval(this._checkInterval), (this._checkInterval = null), l.Z.disablePerfMemoryHooks());
     }
     handlePostConnectionOpen() {
         var e, t;
-        if (!p()) return;
+        if (!h()) return;
         let n = null == (e = (t = i.Z.remoteApp).getReleaseChannel) ? void 0 : e.call(t);
         ("development" === n || "canary" === n) &&
             (this._checkInterval = setInterval(() => {
@@ -46,7 +47,7 @@ class h extends r.Z {
             if (null == n) return;
             (null != (t = null == (e = n.renderer) ? void 0 : e.wss_priv_kb) ? t : 0) > f &&
                 l.Z.enablePerfMemoryHooks({
-                    allocationThresholdKB: 128,
+                    allocationThresholdKB: p,
                     enableCallStackTracking: _,
                 }) &&
                 (this._heapHooksInstalled = !0);
@@ -55,35 +56,35 @@ class h extends r.Z {
             let e = l.Z.getPerfAttributedMemory();
             if (null == e) return;
             let t = [],
-                a = [],
+                o = [],
                 s = [],
                 u = [],
                 d = Object.entries(e);
-            for (let [e, o] of (d.sort((e, t) => {
+            for (let [e, a] of (d.sort((e, t) => {
                 var n, r;
                 let [, i] = e,
-                    [, o] = t;
+                    [, a] = t;
                 return (
-                    (null != (n = null == o ? void 0 : o.total_allocation_kb) ? n : 0) -
+                    (null != (n = null == a ? void 0 : a.total_allocation_kb) ? n : 0) -
                     (null != (r = null == i ? void 0 : i.total_allocation_kb) ? r : 0)
                 );
             }),
             d.slice(0, 10)))
-                null != o &&
+                null != a &&
                     (t.push(e),
-                    a.push(null != (n = o.total_allocation_kb) ? n : 0),
-                    s.push(null != (r = o.allocation_count) ? r : 0),
-                    u.push(null != (i = o.module_version) ? i : ""));
+                    o.push(null != (n = a.total_allocation_kb) ? n : 0),
+                    s.push(null != (r = a.allocation_count) ? r : 0),
+                    u.push(null != (i = a.module_version) ? i : ""));
             let f = l.Z.getPerfAttributedMemoryStats(),
                 p = null == f ? void 0 : f.events_dropped,
                 h = {
                     module_name: t,
-                    allocation_total_size_kb: a,
+                    allocation_total_size_kb: o,
                     allocation_count: s,
                     module_version: u,
                     events_dropped: p,
                 };
-            if ((o.default.track(c.rMx.DESKTOP_PERF_ATTRIBUTED_MODULE_MEMORY, h), _)) {
+            if ((a.default.track(c.rMx.DESKTOP_PERF_ATTRIBUTED_MODULE_MEMORY, h), _)) {
                 let e = d.slice(0, 3).map((e) => e[0]),
                     t = 3;
                 for (let n of e
@@ -103,7 +104,7 @@ class h extends r.Z {
                         callstack_frame_relative_offsets: n.frame_rel_offsets,
                         events_dropped: p,
                     };
-                    o.default.track(c.rMx.DESKTOP_PERF_ATTRIBUTED_MODULE_MEMORY_CALLSTACK, e);
+                    a.default.track(c.rMx.DESKTOP_PERF_ATTRIBUTED_MODULE_MEMORY_CALLSTACK, e);
                 }
             }
         }
@@ -115,4 +116,4 @@ class h extends r.Z {
             u(this, "actions", { POST_CONNECTION_OPEN: () => this.handlePostConnectionOpen() });
     }
 }
-let m = new h();
+let g = new m();
