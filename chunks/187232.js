9f775659748ccb74b73f1b59fa8db1a55f3d927f
DiscrapperManager <admin@minerpl.xyz>
2025-09-11 22:43:10 +0200

    canaryRelease: 443542
    Main scripts: 4
    Lazy Loaded Scripts: 1459
    Chunks:
     * Main Files: 15130
     * Scripts: 8497
    Strings: 22275
    Routes: 733
    Experiments Count: 458
    Apex Experiments Count: 60

--- chunks/187232.js
+++ chunks/187232.js
@@ -1,4 +1,4 @@
-n.d(t, { p: () => A });
+n.d(t, { p: () => P });
 var r = n(100527),
     i = n(717401),
     a = n(474936),
@@ -14,61 +14,90 @@ var r = n(100527),
     h = n(198026),
     m = n(543980);
 let g = {
-        skuId: "1287835633485877369",
-        assetId: "a_33656b7ed12cde00c1826b654cf65590",
+        skuId: "1415079278911033421",
+        assetId: "a_fe4a5080455bc20030589d3b930ebe7b",
+        a11yLabel: s.intl.formatToPlainString(s.t.pDrI0t, {
+            category: s.intl.string(s.t["6Zuiv7"]),
+            name: s.intl.string(s.t["9PzTtL"]),
+        }),
     },
     E = {
-        skuId: "1287835633515102228",
-        assetId: "a_c4395b178205217ad09cae605ef11b1a",
+        skuId: "1415079278911033417",
+        assetId: "a_ade36581b93e50a565541e1964d7eb6a",
+        a11yLabel: s.intl.formatToPlainString(s.t.pDrI0t, {
+            category: s.intl.string(s.t["2UeOlZ"]),
+            name: s.intl.string(s.t["9PzTtL"]),
+        }),
     },
     b = {
-        skuId: "1287835633615765524",
-        assetId: "a_41445f736db3525135b6b9e1122f2254",
+        skuId: "1415079278936064231",
+        assetId: "a_9fb28bf1ed0bed681898f57df3acec5b",
+        a11yLabel: s.intl.formatToPlainString(s.t.pDrI0t, {
+            category: s.intl.string(s.t["6Zuiv7"]),
+            name: s.intl.string(s.t.sBk8mZ),
+        }),
     },
     y = {
-        skuId: "1287835633645125653",
-        assetId: "a_a78819f4d41900aa7698a1bafaf6af41",
+        skuId: "1415079278936064234",
+        assetId: "a_f43fa3efdeda7f7d50fed5f3e5c5cef5",
+        a11yLabel: s.intl.formatToPlainString(s.t.pDrI0t, {
+            category: s.intl.string(s.t["2UeOlZ"]),
+            name: s.intl.string(s.t.sBk8mZ),
+        }),
     },
     O = {
-        skuId: "1287835633674620949",
-        assetId: "a_5e8abacc7a7454d6b08b5cc84cac1d80",
+        skuId: "1415079278923743237",
+        assetId: "a_0103146312bbffdb9a58ce4da3d901d1",
+        a11yLabel: s.intl.formatToPlainString(s.t.pDrI0t, {
+            category: s.intl.string(s.t["6Zuiv7"]),
+            name: s.intl.string(s.t.RbkXxc),
+        }),
     },
     v = {
-        skuId: "1287835633703845888",
-        assetId: "a_a8c5af1848e53cc221d149442c03828f",
+        skuId: "1415079278911033422",
+        assetId: "a_652117a3882f5ae4cc30e56d7ac013a7",
+        a11yLabel: s.intl.formatToPlainString(s.t.pDrI0t, {
+            category: s.intl.string(s.t["2UeOlZ"]),
+            name: s.intl.string(s.t.RbkXxc),
+        }),
     },
     I = {
-        skuId: "1287835633733341224",
-        assetId: "a_4f2b75e5adff09709702613ea0e2cb70",
+        skuId: "1415079278911033420",
+        assetId: "a_8655de5a2c086276231144432fefb8f0",
+        a11yLabel: s.intl.formatToPlainString(s.t.pDrI0t, {
+            category: s.intl.string(s.t["6Zuiv7"]),
+            name: s.intl.string(s.t.FW3Qp6),
+        }),
     },
     T = {
-        skuId: "1287835633762701382",
-        assetId: "a_604d1f0f336d41089acc3f82f458a99e",
+        skuId: "1415079278923743238",
+        assetId: "a_91bdfba540bf5f305f906e6588704ac6",
+        a11yLabel: s.intl.formatToPlainString(s.t.pDrI0t, {
+            category: s.intl.string(s.t["2UeOlZ"]),
+            name: s.intl.string(s.t.FW3Qp6),
+        }),
     },
     S = {
-        gradient: ["rgb(35, 86, 53)", "rgb(3, 15, 40)"],
+        name: s.intl.string(s.t["9PzTtL"]),
+        variants: [g, E],
     },
     A = {
-        variants: [
-            {
-                listingId: "1303067552619958292",
-                name: "Cat Ears",
-                variants: [g, E],
-            },
-            {
-                name: "Dark Hood",
-                variants: [b, y],
-            },
-            {
-                name: "Witch Hat",
-                variants: [O, v],
-            },
-            {
-                listingId: "1303072627061166090",
-                name: "Zombie Food",
-                variants: [I, T],
-            },
-        ],
+        name: s.intl.string(s.t.sBk8mZ),
+        variants: [b, y],
+    },
+    C = {
+        name: s.intl.string(s.t.RbkXxc),
+        variants: [O, v],
+    },
+    N = {
+        name: s.intl.string(s.t.FW3Qp6),
+        variants: [I, T],
+    },
+    R = {
+        gradient: ["rgb(35, 86, 53)", "rgb(3, 15, 40)"],
+    },
+    P = {
+        variants: [S, A, C, N],
         rewardAssetIdMap: (0, i.YV)([g, E, b, y, O, v, I, T]),
         chatGiftIcon: {
             trinketsAnimation: {
@@ -126,7 +155,7 @@ let g = {
             heading: () => s.intl.string(s.t.ruVnPj),
             subheading: () => s.intl.string(s.t.q4BJSE),
             mobileSubheading: (e) => s.intl.formatToPlainString(s.t.eDBuFx, { rewardCount: e }),
-            gradientConfig: S,
+            gradientConfig: R,
             getImageUrl: () => _.Z,
             getBannerImageUrl: () => m.Z,
             getBackgroundImageUrl: () => l.Z,
@@ -135,7 +164,7 @@ let g = {
             desktopBannerText: () => s.intl.string(s.t.mMgymZ),
             mobileBannerText: () => s.intl.string(s.t.dAqTcH),
             getImageUrl: () => p.Z,
-            gradientConfig: () => S,
+            gradientConfig: () => R,
             getBackgroundImageUrl: () => f.Z,
         },
         billingSettingsMarketingBanner: {
