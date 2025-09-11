dfd836d008869642cc30ae61e53890073ebfec50
DiscrapperManager <admin@minerpl.xyz>
2025-09-11 17:35:54 +0200

    canaryRelease: 443251
    Main scripts: 4
    Lazy Loaded Scripts: 1459
    Chunks:
     * Main Files: 15102
     * Scripts: 8485
    Strings: 22271
    Routes: 733
    Experiments Count: 460
    Apex Experiments Count: 57

--- chunks/802426.js
+++ chunks/802426.js
@@ -1,162 +1,130 @@
-n.d(t, { default: () => d }), n(953529);
-var r = n(951288),
-    o = n(647438),
-    a = n(481060),
-    s = n(795338),
-    i = n(921944),
-    c = n(388032),
-    l = n(333669);
-function d(e) {
+r.d(t, { default: () => p }), r(388685), r(953529);
+var n = r(951288),
+    o = r(647438),
+    i = r(82659),
+    a = r(481060),
+    c = r(921944),
+    s = r(388032),
+    l = r(333669);
+function p(e) {
     var t,
-        n,
-        { modalConfig: d, markAsDismissed: p } = e,
+        r,
+        { modalConfig: p, markAsDismissed: d } = e,
         u = (function (e, t) {
             if (null == e) return {};
-            var n,
-                r,
+            var r,
+                n,
                 o = (function (e, t) {
                     if (null == e) return {};
-                    var n,
-                        r,
+                    var r,
+                        n,
                         o = {},
-                        a = Object.keys(e);
-                    for (r = 0; r < a.length; r++) (n = a[r]), t.indexOf(n) >= 0 || (o[n] = e[n]);
+                        i = Object.keys(e);
+                    for (n = 0; n < i.length; n++) (r = i[n]), t.indexOf(r) >= 0 || (o[r] = e[r]);
                     return o;
                 })(e, t);
             if (Object.getOwnPropertySymbols) {
-                var a = Object.getOwnPropertySymbols(e);
-                for (r = 0; r < a.length; r++)
-                    (n = a[r]),
-                        !(t.indexOf(n) >= 0) && Object.prototype.propertyIsEnumerable.call(e, n) && (o[n] = e[n]);
+                var i = Object.getOwnPropertySymbols(e);
+                for (n = 0; n < i.length; n++)
+                    (r = i[n]),
+                        !(t.indexOf(r) >= 0) && Object.prototype.propertyIsEnumerable.call(e, r) && (o[r] = e[r]);
             }
             return o;
         })(e, ["modalConfig", "markAsDismissed"]);
-    let b = o.useCallback(
-        (e) => {
-            var t;
-            null == (t = d.onPrimaryClick) || t.call(d, e), p(i.L.TAKE_ACTION);
-        },
-        [d, p],
-    );
-    return (0, r.jsxs)(
-        a.Y0X,
+    let f = o.useCallback(
+            (e) => {
+                var t;
+                null == (t = p.onPrimaryClick) || t.call(p, e), d(c.L.TAKE_ACTION);
+            },
+            [p, d],
+        ),
+        y = [p.firstHeader, p.secondHeader].filter(Boolean).join(" "),
+        b = [
+            {
+                variant: "secondary",
+                text: s.intl.string(s.t["ETE/oK"]),
+                onClick: u.onClose,
+            },
+            ...(null != p.primaryButtonText
+                ? [
+                      {
+                          variant: "primary",
+                          text: p.primaryButtonText,
+                          onClick: f,
+                      },
+                  ]
+                : []),
+        ];
+    return (0, n.jsx)(
+        i.Modal,
         ((t = (function (e) {
             for (var t = 1; t < arguments.length; t++) {
-                var n = null != arguments[t] ? arguments[t] : {},
-                    r = Object.keys(n);
+                var r = null != arguments[t] ? arguments[t] : {},
+                    n = Object.keys(r);
                 "function" == typeof Object.getOwnPropertySymbols &&
-                    (r = r.concat(
-                        Object.getOwnPropertySymbols(n).filter(function (e) {
-                            return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                    (n = n.concat(
+                        Object.getOwnPropertySymbols(r).filter(function (e) {
+                            return Object.getOwnPropertyDescriptor(r, e).enumerable;
                         }),
                     )),
-                    r.forEach(function (t) {
-                        var r;
-                        (r = n[t]),
+                    n.forEach(function (t) {
+                        var n;
+                        (n = r[t]),
                             t in e
                                 ? Object.defineProperty(e, t, {
-                                      value: r,
+                                      value: n,
                                       enumerable: !0,
                                       configurable: !0,
                                       writable: !0,
                                   })
-                                : (e[t] = r);
+                                : (e[t] = n);
                     });
             }
             return e;
-        })(
+        })({}, u)),
+        (r = r =
             {
-                className: l.modal,
-                size: a.CgR.DYNAMIC,
-            },
-            u,
-        )),
-        (n = n =
-            {
-                parentComponent: "GuildPowerupRollbackModal",
-                children: [
-                    (0, r.jsx)(a.hzk, {
-                        className: l.modalContentContainer,
-                        scrollbarType: "none",
-                        children: (0, r.jsxs)("div", {
-                            className: l.container,
-                            children: [
-                                (0, r.jsx)(s.g, {}),
-                                (0, r.jsxs)("div", {
-                                    className: l.contentContainer,
-                                    children: [
-                                        (0, r.jsx)(a.X6q, {
-                                            variant: "heading-xl/extrabold",
-                                            children: d.firstHeader,
-                                        }),
-                                        (0, r.jsx)(a.X6q, {
-                                            variant: "heading-xl/extrabold",
-                                            children: d.secondHeader,
-                                        }),
-                                        (0, r.jsx)(a.Text, {
-                                            tag: "span",
-                                            className: l.description,
-                                            variant: "text-sm/normal",
-                                            children: d.firstBody,
-                                        }),
-                                        (0, r.jsx)(a.Text, {
-                                            tag: "span",
-                                            className: l.description,
-                                            variant: "text-sm/normal",
-                                            children: d.secondBody,
-                                        }),
-                                        (0, r.jsx)(a.Text, {
-                                            tag: "span",
-                                            className: l.description,
-                                            variant: "text-sm/normal",
-                                            children: d.thirdBody,
-                                        }),
-                                        (0, r.jsxs)("div", {
-                                            className: l.buttonContainer,
-                                            children: [
-                                                (0, r.jsx)("div", {
-                                                    "data-button-hoisted-classname-wrapper": !0,
-                                                    className: l.button,
-                                                    children: (0, r.jsx)(a.zxk, {
-                                                        variant: "secondary",
-                                                        text: c.intl.string(c.t["ETE/oK"]),
-                                                        onClick: u.onClose,
-                                                    }),
-                                                }),
-                                                null != d.primaryButtonText &&
-                                                    (0, r.jsx)("div", {
-                                                        "data-button-hoisted-classname-wrapper": !0,
-                                                        className: l.button,
-                                                        children: (0, r.jsx)(a.zxk, {
-                                                            variant: "primary",
-                                                            text: d.primaryButtonText,
-                                                            onClick: b,
-                                                        }),
-                                                    }),
-                                            ],
-                                        }),
-                                    ],
-                                }),
-                            ],
-                        }),
+                title: y,
+                actions: b,
+                size: "md",
+                children: (0, n.jsx)("div", {
+                    className: l.container,
+                    children: (0, n.jsxs)("div", {
+                        className: l.contentContainer,
+                        children: [
+                            (0, n.jsx)(a.Text, {
+                                tag: "span",
+                                className: l.description,
+                                variant: "text-sm/normal",
+                                children: p.firstBody,
+                            }),
+                            (0, n.jsx)(a.Text, {
+                                tag: "span",
+                                className: l.description,
+                                variant: "text-sm/normal",
+                                children: p.secondBody,
+                            }),
+                            (0, n.jsx)(a.Text, {
+                                tag: "span",
+                                className: l.description,
+                                variant: "text-sm/normal",
+                                children: p.thirdBody,
+                            }),
+                        ],
                     }),
-                    (0, r.jsx)(a.olH, {
-                        className: l.close,
-                        onClick: u.onClose,
-                    }),
-                ],
+                }),
             }),
         Object.getOwnPropertyDescriptors
-            ? Object.defineProperties(t, Object.getOwnPropertyDescriptors(n))
+            ? Object.defineProperties(t, Object.getOwnPropertyDescriptors(r))
             : (function (e, t) {
-                  var n = Object.keys(e);
+                  var r = Object.keys(e);
                   if (Object.getOwnPropertySymbols) {
-                      var r = Object.getOwnPropertySymbols(e);
-                      n.push.apply(n, r);
+                      var n = Object.getOwnPropertySymbols(e);
+                      r.push.apply(r, n);
                   }
-                  return n;
-              })(Object(n)).forEach(function (e) {
-                  Object.defineProperty(t, e, Object.getOwnPropertyDescriptor(n, e));
+                  return r;
+              })(Object(r)).forEach(function (e) {
+                  Object.defineProperty(t, e, Object.getOwnPropertyDescriptor(r, e));
               }),
         t),
     );
