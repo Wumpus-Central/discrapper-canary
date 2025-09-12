95e2c545985ebd8e89e871134e9345fe2dbeccd9
DiscrapperManager <admin@minerpl.xyz>
2025-09-12 22:13:49 +0200

    canaryRelease: 444082
    Main scripts: 4
    Lazy Loaded Scripts: 1545
    Chunks:
     * Main Files: 15138
     * Scripts: 8599
    Strings: 22307
    Routes: 734
    Experiments Count: 469
    Apex Experiments Count: 60

--- chunks/159240.js
+++ chunks/159240.js
@@ -3,5 +3,4 @@ e.exports = {
     infoIcon: "infoIcon_dbec16",
     text: "text_dbec16",
     cancelLink: "cancelLink_dbec16",
-    modalBody: "modalBody_dbec16",
 };
--- chunks/592889.js
+++ chunks/592889.js
@@ -1,149 +1,110 @@
-n.d(t, { Z: () => A }), n(388685);
-var r = n(951288),
-    i = n(647438),
-    a = n(120356),
-    o = n.n(a),
-    s = n(512722),
-    l = n.n(s),
-    c = n(442837),
-    u = n(780384),
-    d = n(481060),
-    f = n(355467),
-    _ = n(100527),
-    p = n(906732),
-    h = n(975298),
-    m = n(600164),
-    g = n(210887),
-    E = n(301766),
-    b = n(509545),
-    y = n(74538),
-    O = n(937615),
-    v = n(474936),
-    I = n(388032),
-    T = n(159240);
-function S(e) {
-    let { subscription: t, renewalMutations: n, transitionState: i, onClose: a, analyticsLocation: o } = e,
-        s = (0, c.e7)([g.Z], () => g.Z.theme),
-        { analyticsLocations: h } = (0, p.ZP)(_.Z.SUBSCRIPTION_CANCEL_DOWNGRADE_MODAL),
-        v = b.Z.get(t.planId);
-    l()(null != v, "Missing subscriptionPlan");
-    let S = (0, y.aS)(t.planId, !1, !1, {
-            paymentSourceId: t.paymentSourceId,
-            currency: t.currency,
-        }),
-        A = (0, O.og)((0, O.T4)(S.amount, S.currency), v.interval, v.intervalCount);
-    async function C() {
-        await (0, f.dP)(
-            t,
-            t.planId,
-            {
-                amount: 0,
-                currency: S.currency,
-            },
-            (0, y.UX)(t.items, S.currency, t.paymentSourceId),
-            h,
-            o,
-        ),
-            a();
-    }
-    return (0, r.jsxs)(d.Y0X, {
-        transitionState: i,
-        "aria-label": I.intl.string(I.t["E9kB4+"]),
-        parentComponent: "SubscriptionRenewalMutationNotice",
-        children: [
-            (0, r.jsxs)(d.xBx, {
-                justify: m.Z.Justify.BETWEEN,
-                separator: !1,
-                children: [
-                    (0, r.jsx)(d.vwX, {
-                        tag: d.RB0.H4,
-                        children: I.intl.string(I.t["E9kB4+"]),
-                    }),
-                    (0, r.jsx)(d.olH, { onClick: a }),
-                ],
-            }),
-            (0, r.jsx)(d.hzk, {
-                className: T.modalBody,
-                children: (0, E.Q0)(t.planId)
-                    ? I.intl.format(I.t.GMp54O, {
-                          downgradedPlan: y.ZP.getDisplayName(n.planId),
-                          existingRate: A,
-                      })
-                    : I.intl.format(I.t["vx/NZ2"], {
-                          existingPlan: y.ZP.getDisplayName(t.planId),
-                          downgradedPlan: y.ZP.getDisplayName(n.planId),
-                          existingRate: A,
-                      }),
-            }),
-            (0, r.jsx)(d.mzw, {
-                children: (0, r.jsxs)(d.hE2, {
-                    direction: "horizontal-reverse",
-                    align: "center",
-                    children: [
-                        (0, r.jsx)(d.zxk, {
-                            variant: "primary",
-                            text: I.intl.string(I.t.frE8KC),
-                            onClick: C,
-                        }),
-                        (0, r.jsx)(d.Avr, {
-                            onClick: a,
-                            variant: (0, u.ap)(s) ? "primary" : "always-white",
-                            text: I.intl.string(I.t.oEAioK),
-                        }),
-                    ],
-                }),
-            }),
-        ],
-    });
-}
-function A(e) {
-    let { subscription: t, renewalMutations: n, className: a, analyticsLocation: s } = e,
-        [l, c] = i.useState(!1),
-        u = (0, h.Z)(),
-        f = t.currentPeriodEnd;
+n.d(t, { Z: () => m });
+var r = n(951288);
+n(647438);
+var i = n(120356),
+    a = n.n(i),
+    o = n(481060),
+    s = n(975298),
+    l = n(74538),
+    c = n(474936),
+    u = n(388032),
+    d = n(159240);
+function f(e, t, n) {
     return (
-        u.fractionalState === v.a$.FP_SUB_PAUSED && (f = u.endsAt.toDate()),
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0,
+              })
+            : (e[t] = n),
+        e
+    );
+}
+function _(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        "function" == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                }),
+            )),
+            r.forEach(function (t) {
+                f(e, t, n[t]);
+            });
+    }
+    return e;
+}
+function p(e, t) {
+    var n = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+        var r = Object.getOwnPropertySymbols(e);
+        t &&
+            (r = r.filter(function (t) {
+                return Object.getOwnPropertyDescriptor(e, t).enumerable;
+            })),
+            n.push.apply(n, r);
+    }
+    return n;
+}
+function h(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : p(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+function m(e) {
+    let { subscription: t, renewalMutations: i, className: f, analyticsLocation: p } = e,
+        m = (0, s.Z)(),
+        g = t.currentPeriodEnd;
+    return (
+        m.fractionalState === c.a$.FP_SUB_PAUSED && (g = m.endsAt.toDate()),
         (0, r.jsxs)("div", {
-            className: o()(T.root, a),
+            className: a()(d.root, f),
             children: [
-                (0, r.jsx)(d.Mgn, {
+                (0, r.jsx)(o.Mgn, {
                     size: "custom",
                     width: 20,
                     height: 20,
                     color: "currentColor",
-                    className: T.infoIcon,
+                    className: d.infoIcon,
                 }),
                 (0, r.jsx)("div", {
-                    className: T.text,
-                    children: I.intl.format(I.t.ar1cPj, {
-                        planName: t.hasExternalPlanChange ? (0, y.zL)(n) : y.ZP.getDisplayName(n.planId),
-                        date: f,
+                    className: d.text,
+                    children: u.intl.format(u.t.ar1cPj, {
+                        planName: t.hasExternalPlanChange ? (0, l.zL)(i) : l.ZP.getDisplayName(i.planId),
+                        date: g,
                     }),
                 }),
                 t.isPurchasedExternally
                     ? null
-                    : (0, r.jsx)(d.eee, {
+                    : (0, r.jsx)(o.eee, {
                           onClick: (e) => {
-                              c(!0), e.preventDefault();
+                              e.preventDefault(),
+                                  (0, o.ZDy)(async () => {
+                                      let { default: e } = await n.e("19429").then(n.bind(n, 604468));
+                                      return (n) =>
+                                          (0, r.jsx)(
+                                              e,
+                                              h(_({}, n), {
+                                                  subscription: t,
+                                                  renewalMutations: i,
+                                                  analyticsLocation: p,
+                                              }),
+                                          );
+                                  });
                           },
-                          className: T.cancelLink,
-                          children: I.intl.string(I.t["ETE/oK"]),
+                          className: d.cancelLink,
+                          children: u.intl.string(u.t["ETE/oK"]),
                       }),
-                l
-                    ? (0, r.jsx)(d.u_l, {
-                          renderModal: (e) => {
-                              let { transitionState: i } = e;
-                              return (0, r.jsx)(S, {
-                                  subscription: t,
-                                  renewalMutations: n,
-                                  transitionState: i,
-                                  onClose: () => c(!1),
-                                  analyticsLocation: s,
-                              });
-                          },
-                          onCloseRequest: () => c(!1),
-                      })
-                    : null,
             ],
         })
     );
