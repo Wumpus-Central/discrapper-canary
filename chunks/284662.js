d078ab51f81a2b76fce465c434494c9d1bcb18b4
DiscrapperManager <admin@minerpl.xyz>
2025-09-15 23:05:06 +0200

    canaryRelease: 444625
    Main scripts: 4
    Lazy Loaded Scripts: 1545
    Chunks:
     * Main Files: 15128
     * Scripts: 8606
    Strings: 22322
    Routes: 735
    Experiments Count: 469
    Apex Experiments Count: 62

--- chunks/284662.js
+++ chunks/284662.js
@@ -1,16 +1,21 @@
-n.d(t, { d: () => l });
+n.d(t, { d: () => c });
 var r = n(647438),
     i = n(442837),
     a = n(538981),
     o = n(654588),
-    s = n(266198);
-let l = () => {
+    s = n(266198),
+    l = n(781110);
+let c = () => {
     var e;
-    let t = (0, i.e7)([o.Z], () => o.Z.state);
+    let t = (0, i.e7)([o.Z], () => o.Z.state),
+        { enabled: n } = l.X.useConfig({ location: "useIsMarketingCampaignEligible" });
     return (
         (0, r.useEffect)(() => {
-            t.shouldRefetchCampaignEligibility && !t.isFetchingCampaignEligibility && (0, a.f)({ campaignId: s.w_ });
-        }, [t.shouldRefetchCampaignEligibility, t.isFetchingCampaignEligibility]),
+            t.shouldRefetchCampaignEligibility &&
+                !t.isFetchingCampaignEligibility &&
+                n &&
+                (0, a.f)({ campaignId: s.w_ });
+        }, [t.shouldRefetchCampaignEligibility, t.isFetchingCampaignEligibility, n]),
         {
             isEligible: null != (e = t.isEligibleForCampaign) && e,
             isLoading: t.isFetchingCampaignEligibility,
