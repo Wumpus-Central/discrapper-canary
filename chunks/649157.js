3604b555fc3fa34e4040ae0b7a6cef3d57a58a89
DiscrapperManager <admin@minerpl.xyz>
2025-09-15 22:20:35 +0200

    canaryRelease: 444584
    Main scripts: 4
    Lazy Loaded Scripts: 1545
    Chunks:
     * Main Files: 15127
     * Scripts: 8606
    Strings: 22320
    Routes: 735
    Experiments Count: 469
    Apex Experiments Count: 61

--- chunks/649157.js
+++ chunks/649157.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => g }), n(388685);
+n.d(t, { Z: () => y }), n(388685);
 var r = n(951288),
     i = n(647438),
     a = n(442837),
@@ -6,10 +6,13 @@ var r = n(951288),
     s = n(481060),
     l = n(153867),
     c = n(706454),
-    u = n(388032),
-    d = n(558731),
-    f = n(444675);
-function _(e, t, n) {
+    u = n(313789),
+    d = n(518596),
+    f = n(981631),
+    _ = n(388032),
+    p = n(558731),
+    h = n(444675);
+function m(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -22,7 +25,7 @@ function _(e, t, n) {
         e
     );
 }
-function p(e) {
+function g(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -33,12 +36,12 @@ function p(e) {
                 }),
             )),
             r.forEach(function (t) {
-                _(e, t, n[t]);
+                m(e, t, n[t]);
             });
     }
     return e;
 }
-function h(e, t) {
+function E(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -50,47 +53,47 @@ function h(e, t) {
     }
     return n;
 }
-function m(e, t) {
+function b(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : h(Object(t)).forEach(function (n) {
+            : E(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
     );
 }
-function g() {
+function y() {
     let e = (0, a.e7)([c.default], () => c.default.locale),
-        [t, _] = i.useState(e),
-        [h] = i.useState(() =>
-            (0, u.getAvailableLocales)().map((e) => {
+        [t, m] = i.useState(e),
+        [E] = i.useState(() =>
+            (0, _.getAvailableLocales)().map((e) => {
                 let t;
                 try {
                     t = n(621287)("./".concat(e.value, ".png"));
                 } catch (e) {
                     t = n(1474);
                 }
-                return m(p({}, e), {
+                return b(g({}, e), {
                     name: (0, r.jsxs)("div", {
-                        className: d.option,
+                        className: p.option,
                         children: [
                             (0, r.jsx)("span", {
-                                className: d.localeName,
+                                className: p.localeName,
                                 children: e.name,
                             }),
                             (0, r.jsx)("span", {
-                                className: d.localizedName,
-                                children: u.intl.string(e.localizedName),
+                                className: p.localizedName,
+                                children: _.intl.string(e.localizedName),
                             }),
                             (0, r.jsx)("div", {
-                                className: d.flag,
+                                className: p.flag,
                                 "aria-hidden": !0,
                                 children: (0, r.jsx)("img", {
                                     alt: "",
                                     src: t,
-                                    className: d.flagImage,
+                                    className: p.flagImage,
                                 }),
                             }),
                         ],
@@ -98,18 +101,21 @@ function g() {
                 });
             }),
         ),
-        g = i.useCallback((e) => {
+        y = i.useCallback((e) => {
             let { value: t } = e;
-            _(t), f.nextTick(() => l.ZP.updateLocale(t));
+            m(t),
+                h.nextTick(() => {
+                    l.ZP.updateLocale(t), (0, s.Mr3)(d.I) && (0, d.Z)(u.n.LOCALE_PANEL, { section: f.oAB.LOCALE });
+                });
         }, []);
     return (0, r.jsx)(s.hjN, {
         tag: s.RB0.H1,
-        title: u.intl.string(u.t.IHMsPj),
+        title: _.intl.string(_.t.IHMsPj),
         children: (0, r.jsx)(s.hjN, {
             children: (0, r.jsx)(o.Gu, {
-                label: u.intl.string(u.t["mx+sp6"]),
-                onChange: g,
-                options: h,
+                label: _.intl.string(_.t["mx+sp6"]),
+                onChange: y,
+                options: E,
                 value: t,
             }),
         }),
