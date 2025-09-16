e21a706dd0fbdad9e481e6de7a659d6a22ac4b37
DiscrapperManager <admin@minerpl.xyz>
2025-09-16 19:49:32 +0200

    canaryRelease: 445096
    Main scripts: 4
    Lazy Loaded Scripts: 1544
    Chunks:
     * Main Files: 15142
     * Scripts: 8605
    Strings: 22346
    Routes: 735
    Experiments Count: 465
    Apex Experiments Count: 65

--- chunks/515108.js
+++ chunks/515108.js
@@ -1,32 +1,34 @@
-n.d(a, { Z: () => i });
+n.d(a, { Z: () => l });
 var t = n(951288);
 n(647438);
 var s = n(481060),
-    o = n(434717),
-    l = n(782034);
-function i(e) {
-    let { quest: a, transitionState: n, onClose: i, rewardContent: r } = e;
+    i = n(434717),
+    o = n(782034);
+function l(e) {
+    let { quest: a, transitionState: n, onClose: l, rewardContent: r } = e;
     return (0, t.jsxs)(s.Y0X, {
+        "data-migration-pending": !0,
         transitionState: n,
         size: s.CgR.DYNAMIC,
-        className: l.modal,
+        className: o.modal,
         parentComponent: "QuestRewardV2ModalBase",
         children: [
             (0, t.jsx)(s.olH, {
-                className: l.closeButton,
-                onClick: i,
+                "data-migration-pending": !0,
+                className: o.closeButton,
+                onClick: l,
                 withCircleBackground: !0,
             }),
             (0, t.jsxs)("div", {
-                className: l.modalContent,
+                className: o.modalContent,
                 children: [
                     (0, t.jsx)("div", {
-                        className: l.leftPanel,
+                        className: o.leftPanel,
                         children: r,
                     }),
                     (0, t.jsx)("div", {
-                        className: l.rightPanel,
-                        children: (0, t.jsx)(o.Z, { quest: a }),
+                        className: o.rightPanel,
+                        children: (0, t.jsx)(i.Z, { quest: a }),
                     }),
                 ],
             }),
