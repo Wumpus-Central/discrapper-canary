16c0863033374f8e5e316c7c6d422a6d3b6cbcb2
DiscrapperManager <admin@minerpl.xyz>
2025-09-16 05:07:55 +0200

    canaryRelease: 444879
    Main scripts: 4
    Lazy Loaded Scripts: 1545
    Chunks:
     * Main Files: 15137
     * Scripts: 8608
    Strings: 22342
    Routes: 735
    Experiments Count: 468
    Apex Experiments Count: 63

--- chunks/906605.js
+++ chunks/906605.js
@@ -1,9 +1,9 @@
 n.d(t, {
-    Sc: () => p,
-    UP: () => h,
-    XE: () => _,
-    Zx: () => d,
-    _s: () => f,
+    Sc: () => E,
+    UP: () => b,
+    XE: () => g,
+    Zx: () => h,
+    _s: () => m,
 }),
     n(388685);
 var r = n(570140),
@@ -11,71 +11,97 @@ var r = n(570140),
     a = n(592125),
     o = n(19780),
     s = n(944486),
-    l = n(626135),
-    c = n(106301),
-    u = n(981631);
-function d(e, t) {
+    l = n(938475),
+    c = n(626135),
+    u = n(106301),
+    d = n(981631);
+function f(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0,
+              })
+            : (e[t] = n),
+        e
+    );
+}
+function _(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        "function" == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                }),
+            )),
+            r.forEach(function (t) {
+                f(e, t, n[t]);
+            });
+    }
+    return e;
+}
+function p() {
+    var e;
+    let t = s.Z.getVoiceChannelId(),
+        n = o.Z.getMediaSessionId();
+    return null == t
+        ? {
+              channel_id: void 0,
+              guild_id: void 0,
+              media_session_id: n,
+              call_num_participants: 0,
+          }
+        : {
+              channel_id: t,
+              guild_id: null == (e = a.Z.getChannel(t)) ? void 0 : e.guild_id,
+              media_session_id: n,
+              call_num_participants: l.ZP.countVoiceStatesForChannel(t),
+          };
+}
+function h(e, t) {
     var n;
     let i = null == (n = a.Z.getChannel(s.Z.getVoiceChannelId())) ? void 0 : n.guild_id;
-    if (null == e || null == i) return void p(t);
+    if (null == e || null == i) return void E(t);
     r.Z.dispatch({
         type: "UPDATE_HANG_STATUS",
         status: e,
         guildId: i,
         saveAsDefault: t,
     }),
-        l.default.track(u.rMx.SET_HANG_STATUS, {
-            status_type: e,
-            channel_id: s.Z.getVoiceChannelId(),
-            guild_id: i,
-            media_session_id: o.Z.getMediaSessionId(),
-        });
+        c.default.track(d.rMx.SET_HANG_STATUS, _({ status_type: e }, p()));
 }
-function f(e, t, n) {
-    var i;
-    if ("" === e || null == t) return void p(n);
+function m(e, t, n) {
+    if ("" === e || null == t) return void E(n);
     r.Z.dispatch({
         type: "UPDATE_HANG_STATUS_CUSTOM",
         emoji: t,
         status: e,
         saveAsDefault: n,
     }),
-        l.default.track(u.rMx.SET_HANG_STATUS, {
-            status_type: e,
-            channel_id: s.Z.getVoiceChannelId(),
-            guild_id: null == (i = a.Z.getChannel(s.Z.getVoiceChannelId())) ? void 0 : i.guild_id,
-            media_session_id: o.Z.getMediaSessionId(),
-        });
+        c.default.track(d.rMx.SET_HANG_STATUS, _({ status_type: e }, p()));
 }
-function _(e, t) {
-    var n;
-    if (null == e) return void p(t);
+function g(e, t) {
+    if (null == e) return void E(t);
     r.Z.dispatch({
         type: "UPDATE_HANG_STATUS_GAME_ACTIVITY",
         applicationId: e,
         saveAsDefault: t,
     }),
-        l.default.track(u.rMx.SET_HANG_STATUS, {
-            status_type: "game_activity",
-            channel_id: s.Z.getVoiceChannelId(),
-            guild_id: null == (n = a.Z.getChannel(s.Z.getVoiceChannelId())) ? void 0 : n.guild_id,
-            media_session_id: o.Z.getMediaSessionId(),
-        });
+        c.default.track(d.rMx.SET_HANG_STATUS, _({ status_type: "game_activity" }, p()));
 }
-function p(e) {
-    var t;
+function E(e) {
     r.Z.dispatch({
         type: "CLEAR_HANG_STATUS",
         saveAsDefault: e,
     }),
-        l.default.track(u.rMx.CLEAR_HANG_STATUS, {
-            channel_id: s.Z.getVoiceChannelId(),
-            guild_id: null == (t = a.Z.getChannel(s.Z.getVoiceChannelId())) ? void 0 : t.guild_id,
-            media_session_id: o.Z.getMediaSessionId(),
-        });
+        c.default.track(d.rMx.CLEAR_HANG_STATUS, _({}, p()));
 }
-function h() {
-    let e = [c.Z.getCustomHangStatus(), ...c.Z.getRecentStatuses()].filter((e) => {
+function b() {
+    let e = [u.Z.getCustomHangStatus(), ...u.Z.getRecentStatuses()].filter((e) => {
         var t;
         return (
             null != e &&
