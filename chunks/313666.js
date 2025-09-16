d89d0f903f152985c1cdd30686594cfb06b189b1
DiscrapperManager <admin@minerpl.xyz>
2025-09-16 22:23:56 +0200

    canaryRelease: 445259
    Main scripts: 4
    Lazy Loaded Scripts: 1543
    Chunks:
     * Main Files: 15159
     * Scripts: 8607
    Strings: 22347
    Routes: 735
    Experiments Count: 465
    Apex Experiments Count: 65

---
+++ chunks/313666.js
@@ -0,0 +1,36 @@
+n.d(t, {
+    C: () => d,
+    R: () => u,
+});
+var r = n(951288),
+    i = n(120356),
+    a = n.n(i),
+    o = n(402453),
+    s = n(777207),
+    l = n(957618);
+let c = (e, t) => {
+    switch (e) {
+        case "new":
+            return t.NEW;
+        case "beta":
+            return t.BETA;
+        case "early_access":
+            return t.EARLY_ACCESS;
+        case "free_trial":
+            return t.BILLING_TRIAL_FREE_TRIAL_TEXT;
+        default:
+            return e.text;
+    }
+};
+function u(e) {
+    return "string" == typeof e || e.hasOwnProperty("text") ? { type: e } : e;
+}
+function d(e) {
+    let { type: t, variant: n = "default" } = e,
+        { i18n: i } = (0, o.ZF)();
+    return (0, r.jsx)(s.x, {
+        variant: "eyebrow",
+        className: a()(l.badge, l[n]),
+        children: c(t, i),
+    });
+}
--- chunks/580309.js
+++
@@ -1,6 +0,0 @@
-var r = {
-    badge: "badge_dca3ba",
-    default: "default_dca3ba",
-    expressive: "expressive_dca3ba",
-};
-n.r((e.exports = r));
