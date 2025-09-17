0d70ed9366120a366c3f85ce62f1e47afa27668e
DiscrapperManager <admin@minerpl.xyz>
2025-09-17 17:57:43 +0200

    canaryRelease: 445662
    Main scripts: 4
    Lazy Loaded Scripts: 1544
    Chunks:
     * Main Files: 15170
     * Scripts: 8607
    Strings: 22346
    Routes: 735
    Experiments Count: 463
    Apex Experiments Count: 64

--- chunks/385902.js
+++ chunks/385902.js
@@ -18,61 +18,72 @@ var r = n(647438),
     m = n(258640),
     b = n(388032);
 function O(e, t) {
-    let n = (0, l.e7)([s.Z], () => s.Z.getStateForGuild(e)),
-        r = null == n ? void 0 : n.allPowerups[i.A$],
-        o = (0, d.ZP)(e, r),
-        a = (0, c.q8)(e, t),
-        u = null == r ? void 0 : r.storeRemovalDate,
-        f = a && null != u && o.type !== h.A3.INACTIVE,
-        g = f
+    var n;
+    let r = (0, l.e7)([o.Z], () => {
+            var t;
+            return null == (t = o.Z.getGuild(e)) ? void 0 : t.features;
+        }),
+        a = (0, l.e7)([s.Z], () => s.Z.getStateForGuild(e)),
+        u = null == a ? void 0 : a.allPowerups[i.A$],
+        f = (0, d.ZP)(e, u),
+        O = (0, c.q8)(e, t),
+        y = null == u ? void 0 : u.storeRemovalDate,
+        _ = null != (n = null == r ? void 0 : r.has(g.oNc.PARTNERED)) && n,
+        v = O && null != y && !_ && f.type === h.A3.POWERUP_ACTIVATED,
+        j = v
             ? {
-                  title: b.intl.formatToPlainString(m.default.mgoPkZ, { perkName: null == r ? void 0 : r.title }),
-                  description: b.intl.formatToPlainString(m.default.UT9pkJ, { dateString: (0, p.Z)(u) }),
+                  title: b.intl.formatToPlainString(m.default.mgoPkZ, { perkName: null == u ? void 0 : u.title }),
+                  description: b.intl.formatToPlainString(m.default.UT9pkJ, { dateString: (0, p.Z)(y) }),
               }
             : null;
     return {
-        shouldShow: f,
-        notificationConfig: g,
+        shouldShow: v,
+        notificationConfig: j,
     };
 }
 function y(e, t) {
-    var n, d, h, O;
-    let y = (0, l.e7)([o.Z], () => o.Z.getGuild(e)),
-        _ = (0, l.e7)([a.Z], () => a.Z.hasLayers()),
-        v = null != (O = (0, u.Z)(e)) && O,
-        j = (0, c.q8)(e, t),
-        x = (0, l.e7)([s.Z], () => s.Z.getStateForGuild(e)),
-        C = (null == x || null == (n = x.unlockedPowerups) ? void 0 : n[i.A$]) != null,
-        E = null == x || null == (d = x.allPowerups) ? void 0 : d[i.A$],
-        S = null == E ? void 0 : E.storeRemovalDate,
-        P = null == x || null == (h = x.allPowerups) ? void 0 : h[i.XW],
-        { onActivate: I } = (0, f._C)(e, P),
-        N = !_ && v && j && C && null != E && null != S && null != P && null != y,
-        Z = r.useCallback(
+    var n, O, y, _;
+    let v = (0, l.e7)([o.Z], () => o.Z.getGuild(e)),
+        j = (0, l.e7)([a.Z], () => a.Z.hasLayers()),
+        x = null != (y = (0, u.Z)(e)) && y,
+        C = (0, c.q8)(e, t),
+        E = (0, l.e7)([s.Z], () => s.Z.getStateForGuild(e)),
+        S = null == E || null == (n = E.allPowerups) ? void 0 : n[i.A$],
+        P = (0, d.ZP)(e, S),
+        I = null == S ? void 0 : S.storeRemovalDate,
+        N = null == E || null == (O = E.allPowerups) ? void 0 : O[i.XW],
+        Z = (0, l.e7)([o.Z], () => {
+            var t;
+            return null == (t = o.Z.getGuild(e)) ? void 0 : t.features;
+        }),
+        w = null != (_ = null == Z ? void 0 : Z.has(g.oNc.PARTNERED)) && _,
+        { onActivate: T } = (0, f._C)(e, N),
+        A = !j && x && C && null != S && !w && P.type === h.A3.POWERUP_ACTIVATED && null != I && null != N && null != v,
+        R = r.useCallback(
             (e) => {
-                I(e);
+                T(e);
             },
-            [I],
+            [T],
         ),
-        w = r.useMemo(() => {
-            if (!N) return null;
-            let e = (0, p.Z)(S),
-                t = y.premiumTier === g.Eu4.TIER_2 ? b.intl.string(m.default["0uo/LC"]) : void 0;
+        D = r.useMemo(() => {
+            if (!A) return null;
+            let e = (0, p.Z)(I),
+                t = v.premiumTier === g.Eu4.TIER_2 ? b.intl.string(m.default["0uo/LC"]) : void 0;
             return {
-                firstHeader: E.title,
+                firstHeader: S.title,
                 secondHeader: b.intl.formatToPlainString(m.default["8imxAg"], { dateString: e }),
-                firstBody: b.intl.formatToPlainString(m.default["/bW9tb"], { serverName: y.name }),
+                firstBody: b.intl.formatToPlainString(m.default["/bW9tb"], { serverName: v.name }),
                 secondBody: b.intl.formatToPlainString(m.default.D09fdn, {
                     dateString: e,
-                    boostCount: E.cost,
+                    boostCount: S.cost,
                 }),
                 thirdBody: b.intl.string(m.default["+zvKPj"]),
                 primaryButtonText: t,
-                onPrimaryClick: null != t ? Z : void 0,
+                onPrimaryClick: null != t ? R : void 0,
             };
-        }, [N, E, S, y, Z]);
+        }, [A, S, I, v, R]);
     return {
-        shouldShow: N,
-        modalConfig: w,
+        shouldShow: A,
+        modalConfig: D,
     };
 }
