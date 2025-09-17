a688e72aaaf9059f97cf1944fb25cfda4cd460ad
DiscrapperManager <admin@minerpl.xyz>
2025-09-17 19:12:25 +0200

    canaryRelease: 445710
    Main scripts: 4
    Lazy Loaded Scripts: 1544
    Chunks:
     * Main Files: 15168
     * Scripts: 8607
    Strings: 22348
    Routes: 735
    Experiments Count: 463
    Apex Experiments Count: 64

--- chunks/143941.js
+++ chunks/143941.js
@@ -1,15 +1,18 @@
-n.d(t, { s: () => E });
+n.d(t, { s: () => O }), n(388685);
 var r = n(951288),
     i = n(647438),
     a = n(120356),
     o = n.n(a),
-    s = n(481060),
-    l = n(727637),
-    c = n(607550),
-    u = n(388032),
-    d = n(820657),
-    f = n(74866);
-function _(e, t, n) {
+    s = n(704215),
+    l = n(481060),
+    c = n(727637),
+    u = n(243778),
+    d = n(607550),
+    f = n(921944),
+    _ = n(388032),
+    p = n(820657),
+    h = n(74866);
+function m(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -22,7 +25,7 @@ function _(e, t, n) {
         e
     );
 }
-function p(e) {
+function g(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -33,12 +36,12 @@ function p(e) {
                 }),
             )),
             r.forEach(function (t) {
-                _(e, t, n[t]);
+                m(e, t, n[t]);
             });
     }
     return e;
 }
-function h(e, t) {
+function E(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -50,28 +53,28 @@ function h(e, t) {
     }
     return n;
 }
-function m(e, t) {
+function b(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : h(Object(t)).forEach(function (n) {
+            : E(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
     );
 }
-let g = (e) => {
+let y = (e) => {
         let { message: t } = e;
         return (0, r.jsxs)("div", {
-            className: f.toast,
+            className: h.toast,
             children: [
-                (0, r.jsx)(s.kSu, {
+                (0, r.jsx)(l.kSu, {
                     size: "refresh_sm",
-                    className: d.toastIcon,
-                    color: s.TVs.colors.STATUS_POSITIVE,
+                    className: p.toastIcon,
+                    color: l.TVs.colors.STATUS_POSITIVE,
                 }),
-                (0, r.jsx)(s.Text, {
+                (0, r.jsx)(l.Text, {
                     color: "text-default",
                     variant: "text-sm/semibold",
                     children: t,
@@ -79,45 +82,63 @@ let g = (e) => {
             ],
         });
     },
-    E = (e) => {
-        let { skuId: t, iconSize: n = 20, className: a, enableHoverEffect: f = !1 } = e,
-            _ = (0, c.ny)(t),
-            h = i.useRef(null),
-            E = _ ? u.intl.string(u.t.yr9TTU) : u.intl.string(u.t["8DkMER"]),
-            b = (0, l.Z)(h),
-            y = _ ? s.h_8 : s.Pzh,
-            O = _ || b ? d.wishlistedOrHoveredIconColor : d.normalIconColor,
-            v = i.useCallback(
+    O = (e) => {
+        let { skuId: t, iconSize: n = 20, className: a, enableHoverEffect: h = !1 } = e,
+            [m, E] = (0, u.US)([s.z.WISHLIST_NUX_TOOLTIP_AND_MODAL], void 0, !0),
+            O = m === s.z.WISHLIST_NUX_TOOLTIP_AND_MODAL,
+            v = (0, d.ny)(t),
+            I = i.useRef(null),
+            T = (0, c.Z)(I),
+            S = v ? l.h_8 : l.Pzh,
+            A = v || T ? p.wishlistedOrHoveredIconColor : p.normalIconColor,
+            C = i.useCallback(
                 (e) => {
                     e.stopPropagation(),
-                        _
-                            ? ((0, c.yj)(t),
-                              (0, s.showToast)(
-                                  (0, s.createToast)("", s.ToastType.CUSTOM, {
-                                      component: (0, r.jsx)(g, { message: u.intl.string(u.t.DSXOiI) }),
+                        v
+                            ? ((0, d.yj)(t),
+                              (0, l.showToast)(
+                                  (0, l.createToast)("", l.ToastType.CUSTOM, {
+                                      component: (0, r.jsx)(y, { message: _.intl.string(_.t.DSXOiI) }),
                                   }),
                               ))
-                            : ((0, c.iI)(t),
-                              (0, s.showToast)(
-                                  (0, s.createToast)("", s.ToastType.CUSTOM, {
-                                      component: (0, r.jsx)(g, { message: u.intl.string(u.t["3T2jbW"]) }),
+                            : ((0, d.iI)(t),
+                              (0, l.showToast)(
+                                  (0, l.createToast)("", l.ToastType.CUSTOM, {
+                                      component: (0, r.jsx)(y, { message: _.intl.string(_.t["3T2jbW"]) }),
                                   }),
-                              ));
+                              ),
+                              O && E(f.L.USER_DISMISS));
                 },
-                [_, t],
-            );
-        return (0, r.jsx)(s.ua7, {
-            text: E,
+                [v, t, O, E],
+            ),
+            N = v ? _.intl.string(_.t.yr9TTU) : _.intl.string(_.t["8DkMER"]),
+            R = O
+                ? (0, r.jsxs)(r.Fragment, {
+                      children: [
+                          (0, r.jsx)(l.Text, {
+                              variant: "text-sm/semibold",
+                              children: _.intl.string(_.t["47Rhc3"]),
+                          }),
+                          (0, r.jsx)(l.Text, {
+                              variant: "text-sm/medium",
+                              children: _.intl.string(_.t.PXjA0d),
+                          }),
+                      ],
+                  })
+                : N;
+        return (0, r.jsx)(l.ua7, {
+            text: R,
+            "aria-label": N,
             children: (e) =>
                 (0, r.jsx)(
-                    s.P3F,
-                    m(p({}, e), {
-                        className: o()(d.wishlistButton, f && d.withHover, a),
-                        innerRef: h,
-                        onClick: v,
-                        "aria-label": E,
-                        children: (0, r.jsx)(y, {
-                            colorClass: O,
+                    l.P3F,
+                    b(g({}, e), {
+                        className: o()(p.wishlistButton, h && p.withHover, a),
+                        innerRef: I,
+                        onClick: C,
+                        "aria-label": N,
+                        children: (0, r.jsx)(S, {
+                            colorClass: A,
                             size: "custom",
                             height: n,
                             width: n,
