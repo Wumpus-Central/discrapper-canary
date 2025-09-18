6a4e3b8076648b9606adb2bb3c2fb0fa2ca2aebd
DiscrapperManager <admin@minerpl.xyz>
2025-09-18 17:14:42 +0200

    canaryRelease: 446292
    Main scripts: 4
    Lazy Loaded Scripts: 1544
    Chunks:
     * Main Files: 15180
     * Scripts: 8602
    Strings: 22366
    Routes: 738
    Experiments Count: 461
    Apex Experiments Count: 67

--- chunks/454982.js
+++ chunks/454982.js
@@ -1,129 +1,128 @@
-n.d(t, { Z: () => _ }), n(388685);
+n.d(t, { Z: () => p }), n(388685);
 var r = n(951288),
     i = n(647438),
-    a = n(755721),
-    o = n(481060),
-    s = n(484614),
-    l = n(313201),
-    c = n(518638),
-    u = n(388032),
-    d = n(849879);
-function f(e) {
-    let { onClose: t } = e;
-    return (0, r.jsxs)(r.Fragment, {
-        children: [
-            (0, r.jsxs)(o.xBx, {
-                separator: !1,
-                className: d.errorHeader,
+    a = n(82659),
+    o = n(755721),
+    s = n(481060),
+    l = n(484614),
+    c = n(313201),
+    u = n(518638),
+    d = n(388032),
+    f = n(849879);
+function _(e) {
+    let { onClose: t, transitionState: n } = e,
+        i = () =>
+            (0, r.jsxs)("div", {
+                className: f.errorBody,
                 children: [
-                    (0, r.jsx)("div", { className: d.errorArt }),
-                    (0, r.jsx)(o.X6q, {
+                    (0, r.jsx)(s.X6q, {
                         variant: "heading-xl/semibold",
-                        children: u.intl.string(u.t.iufib2),
+                        children: d.intl.string(d.t.iufib2),
+                    }),
+                    (0, r.jsx)(s.Text, {
+                        variant: "text-md/normal",
+                        className: f.bodyText,
+                        children: d.intl.string(d.t.eAn6z8),
                     }),
                 ],
-            }),
-            (0, r.jsx)(o.hzk, {
-                className: d.errorBody,
-                children: (0, r.jsx)(o.Text, {
-                    variant: "text-md/normal",
-                    className: d.bodyText,
-                    children: u.intl.string(u.t.eAn6z8),
-                }),
-            }),
-            (0, r.jsx)(o.mzw, {
-                children: (0, r.jsx)(o.zxk, {
-                    variant: "primary",
-                    text: u.intl.string(u.t.cpT0Cg),
-                    onClick: t,
-                }),
-            }),
+            });
+    return (0, r.jsx)(a.Modal, {
+        title: "",
+        size: "md",
+        input: (0, r.jsx)("div", { className: f.errorArt }),
+        onClose: async () => await t(),
+        actions: [
+            {
+                text: d.intl.string(d.t.cpT0Cg),
+                variant: "primary",
+                onClick: t,
+            },
         ],
+        transitionState: n,
+        children: i(),
     });
 }
-let _ = function (e) {
-    let { onClose: t, onClaim: n, code: _, outboundPromotion: p, transitionState: h } = e,
-        [m, g] = i.useState(null),
-        E = (0, l.Dt)();
-    i.useEffect(() => {
-        null == _ &&
-            (0, c.A2)(p.id)
-                .then((e) => n(e))
-                .catch((e) => {
-                    var t;
-                    return g(null == e || null == (t = e.body) ? void 0 : t.code);
-                });
-    }, [_, p.id, n]);
-    let b = () =>
-        null != m
-            ? (0, r.jsx)(f, { onClose: t })
-            : null == _
-              ? (0, r.jsx)(o.hzk, { children: (0, r.jsx)(o.$jN, { className: d.loading }) })
-              : (0, r.jsxs)(r.Fragment, {
+let p = function (e) {
+    let { onClose: t, onClaim: n, code: p, outboundPromotion: h, transitionState: m } = e,
+        [g, E] = i.useState(null),
+        b = (0, c.Dt)();
+    if (
+        (i.useEffect(() => {
+            null == p &&
+                (0, u.A2)(h.id)
+                    .then((e) => n(e))
+                    .catch((e) => {
+                        var t;
+                        return E(null == e || null == (t = e.body) ? void 0 : t.code);
+                    });
+        }, [p, h.id, n]),
+        null != g)
+    )
+        return (0, r.jsx)(_, {
+            onClose: t,
+            transitionState: m,
+        });
+    if (null == p) return (0, r.jsx)(s.$jN, { className: f.loading });
+    let y = () =>
+        (0, r.jsxs)("div", {
+            className: f.headerContainer,
+            children: [
+                (0, r.jsx)("div", { className: f.art }),
+                (0, r.jsx)(s.X6q, {
+                    variant: "heading-xl/semibold",
+                    children: d.intl.string(d.t["23BfZm"]),
+                }),
+                (0, r.jsx)(s.Text, {
+                    variant: "text-md/normal",
+                    className: f.bodyText,
+                    children: h.outboundRedemptionModalBody,
+                }),
+            ],
+        });
+    return (0, r.jsx)(a.Modal, {
+        title: "",
+        size: "md",
+        onClose: async () => await t(),
+        input: y(),
+        actions: [
+            {
+                text: d.intl.string(d.t.TulDPj),
+                variant: "secondary",
+                onClick: async () => await t(),
+            },
+            {
+                text: d.intl.string(d.t["+zx47e"]),
+                variant: "primary",
+                onClick: () => {
+                    let e = (0, u.BU)(p, h);
+                    window.open(e, "_blank");
+                },
+            },
+        ],
+        transitionState: m,
+        "aria-label": b,
+        children: (0, r.jsxs)("div", {
+            children: [
+                (0, r.jsx)(s.$i$, { className: f.formDivider }),
+                (0, r.jsxs)(s.hjN, {
+                    title: d.intl.string(d.t.s9LFQk),
+                    className: f.formSection,
                     children: [
-                        (0, r.jsxs)(o.xBx, {
-                            separator: !1,
-                            className: d.headerContainer,
-                            children: [
-                                (0, r.jsx)("div", { className: d.art }),
-                                (0, r.jsx)(o.X6q, {
-                                    variant: "heading-xl/semibold",
-                                    children: u.intl.string(u.t["23BfZm"]),
-                                }),
-                            ],
+                        (0, r.jsx)(l.Z, {
+                            value: p,
+                            buttonColor: o.zx.Colors.BRAND,
+                            buttonLook: o.zx.Looks.FILLED,
+                            delay: 1000,
                         }),
-                        (0, r.jsxs)(o.hzk, {
-                            children: [
-                                (0, r.jsx)(o.Text, {
-                                    variant: "text-md/normal",
-                                    className: d.bodyText,
-                                    children: p.outboundRedemptionModalBody,
-                                }),
-                                (0, r.jsx)(o.$i$, { className: d.formDivider }),
-                                (0, r.jsxs)(o.hjN, {
-                                    title: u.intl.string(u.t.s9LFQk),
-                                    className: d.formSection,
-                                    children: [
-                                        (0, r.jsx)(s.Z, {
-                                            value: _,
-                                            buttonColor: a.zx.Colors.BRAND,
-                                            buttonLook: a.zx.Looks.FILLED,
-                                            delay: 1000,
-                                        }),
-                                        (0, r.jsx)(o.Text, {
-                                            variant: "text-xs/normal",
-                                            color: "header-secondary",
-                                            className: d.confirmationText,
-                                            children: u.intl.string(u.t["F+nFTU"]),
-                                        }),
-                                    ],
-                                }),
-                            ],
-                        }),
-                        (0, r.jsxs)(o.mzw, {
-                            children: [
-                                (0, r.jsx)(o.zxk, {
-                                    variant: "primary",
-                                    text: u.intl.string(u.t["+zx47e"]),
-                                    onClick: () => {
-                                        let e = (0, c.BU)(_, p);
-                                        window.open(e, "_blank");
-                                    },
-                                }),
-                                (0, r.jsx)(a.zx, {
-                                    onClick: t,
-                                    look: a.zx.Looks.LINK,
-                                    color: d.maybeLaterButton,
-                                    children: u.intl.string(u.t.TulDPj),
-                                }),
-                            ],
+                        (0, r.jsx)(s.Text, {
+                            variant: "text-xs/normal",
+                            color: "header-secondary",
+                            className: f.confirmationText,
+                            children: d.intl.string(d.t["F+nFTU"]),
                         }),
                     ],
-                });
-    return (0, r.jsx)(o.Y0X, {
-        transitionState: h,
-        "aria-labelledby": E,
-        parentComponent: "OutboundPromotionRedemptionModal",
-        children: b(),
+                }),
+            ],
+        }),
     });
 };
