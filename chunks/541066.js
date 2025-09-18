5355eb81fa805bc76e7be7a41914f79f47f59058
DiscrapperManager <admin@minerpl.xyz>
2025-09-18 22:04:12 +0200

    canaryRelease: 446566
    Main scripts: 4
    Lazy Loaded Scripts: 1544
    Chunks:
     * Main Files: 15185
     * Scripts: 8607
    Strings: 22368
    Routes: 738
    Experiments Count: 458
    Apex Experiments Count: 69

--- chunks/541066.js
+++ chunks/541066.js
@@ -1,122 +1,54 @@
-r.d(t, { default: () => a });
-var n = r(951288),
-    o = r(481060),
-    l = r(744993),
-    c = r(401561),
-    i = r(388032),
-    s = r(755986);
-function a(e) {
-    var t,
-        r,
-        { onAccept: a } = e,
-        u = (function (e, t) {
-            if (null == e) return {};
-            var r,
-                n,
-                o = (function (e, t) {
-                    if (null == e) return {};
-                    var r,
-                        n,
+n.d(e, { default: () => s });
+var r = n(951288),
+    o = n(82659),
+    i = n(744993),
+    l = n(401561),
+    a = n(388032);
+function s(t) {
+    var { onAccept: e } = t,
+        n = (function (t, e) {
+            if (null == t) return {};
+            var n,
+                r,
+                o = (function (t, e) {
+                    if (null == t) return {};
+                    var n,
+                        r,
                         o = {},
-                        l = Object.keys(e);
-                    for (n = 0; n < l.length; n++) (r = l[n]), t.indexOf(r) >= 0 || (o[r] = e[r]);
+                        i = Object.keys(t);
+                    for (r = 0; r < i.length; r++) (n = i[r]), e.indexOf(n) >= 0 || (o[n] = t[n]);
                     return o;
-                })(e, t);
+                })(t, e);
             if (Object.getOwnPropertySymbols) {
-                var l = Object.getOwnPropertySymbols(e);
-                for (n = 0; n < l.length; n++)
-                    (r = l[n]),
-                        !(t.indexOf(r) >= 0) && Object.prototype.propertyIsEnumerable.call(e, r) && (o[r] = e[r]);
+                var i = Object.getOwnPropertySymbols(t);
+                for (r = 0; r < i.length; r++)
+                    (n = i[r]),
+                        !(e.indexOf(n) >= 0) && Object.prototype.propertyIsEnumerable.call(t, n) && (o[n] = t[n]);
             }
             return o;
-        })(e, ["onAccept"]);
-    return (0, n.jsxs)(
-        o.Y0X,
-        ((t = (function (e) {
-            for (var t = 1; t < arguments.length; t++) {
-                var r = null != arguments[t] ? arguments[t] : {},
-                    n = Object.keys(r);
-                "function" == typeof Object.getOwnPropertySymbols &&
-                    (n = n.concat(
-                        Object.getOwnPropertySymbols(r).filter(function (e) {
-                            return Object.getOwnPropertyDescriptor(r, e).enumerable;
-                        }),
-                    )),
-                    n.forEach(function (t) {
-                        var n;
-                        (n = r[t]),
-                            t in e
-                                ? Object.defineProperty(e, t, {
-                                      value: n,
-                                      enumerable: !0,
-                                      configurable: !0,
-                                      writable: !0,
-                                  })
-                                : (e[t] = n);
-                    });
-            }
-            return e;
-        })({}, u)),
-        (r = r =
+        })(t, ["onAccept"]);
+    return (0, r.jsx)(o.Modal, {
+        title: a.intl.string(l.default.bnsSPj),
+        subtitle: a.intl.format(l.default.MaWfLi, {
+            provider: "Portkey",
+            providerUrl: "https://google.com/terms-of-service",
+        }),
+        size: "md",
+        actions: [
             {
-                className: s.modal,
-                size: o.CgR.DYNAMIC,
-                parentComponent: "PortkeyToSModal",
-                children: [
-                    (0, n.jsxs)(o.hzk, {
-                        className: s.modalContent,
-                        scrollbarType: "none",
-                        children: [
-                            (0, n.jsx)(o.X6q, {
-                                variant: "heading-lg/semibold",
-                                children: i.intl.string(c.default.bnsSPj),
-                            }),
-                            (0, n.jsx)(o.Text, {
-                                variant: "text-sm/medium",
-                                children: i.intl.format(c.default.MaWfLi, {
-                                    provider: "Portkey",
-                                    providerUrl: "https://google.com/terms-of-service",
-                                }),
-                            }),
-                        ],
-                    }),
-                    (0, n.jsxs)("div", {
-                        className: s.footer,
-                        children: [
-                            (0, n.jsx)(o.zxk, {
-                                fullWidth: !0,
-                                text: i.intl.string(i.t["/g10LC"]),
-                                variant: "secondary",
-                                onClick: u.onClose,
-                            }),
-                            (0, n.jsx)(o.zxk, {
-                                fullWidth: !0,
-                                text: i.intl.string(c.default.IfMFoK),
-                                variant: "primary",
-                                onClick: () => {
-                                    (0, l.g$)(!0, "Portkey"), null == a || a(), u.onClose();
-                                },
-                            }),
-                        ],
-                    }),
-                    (0, n.jsx)(o.olH, {
-                        onClick: u.onClose,
-                        className: s.closeButton,
-                    }),
-                ],
-            }),
-        Object.getOwnPropertyDescriptors
-            ? Object.defineProperties(t, Object.getOwnPropertyDescriptors(r))
-            : (function (e, t) {
-                  var r = Object.keys(e);
-                  if (Object.getOwnPropertySymbols) {
-                      var n = Object.getOwnPropertySymbols(e);
-                      r.push.apply(r, n);
-                  }
-                  return r;
-              })(Object(r)).forEach(function (e) {
-                  Object.defineProperty(t, e, Object.getOwnPropertyDescriptor(r, e));
-              }),
-        t),
-    );
+                text: a.intl.string(a.t["/g10LC"]),
+                onClick: n.onClose,
+                variant: "secondary",
+            },
+            {
+                text: a.intl.string(l.default.IfMFoK),
+                onClick: () => {
+                    (0, i.g$)(!0, "Portkey"), null == e || e(), n.onClose();
+                },
+                variant: "primary",
+            },
+        ],
+        transitionState: n.transitionState,
+        onClose: n.onClose,
+    });
 }
--- chunks/755986.js
+++
@@ -1,6 +0,0 @@
-e.exports = {
-    modal: "modal__0dc24",
-    modalContent: "modalContent__0dc24",
-    closeButton: "closeButton__0dc24",
-    footer: "footer__0dc24",
-};
