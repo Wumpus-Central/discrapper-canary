7533455be816c257545456bed7eb8fe9aa2944c0
DiscrapperManager <admin@minerpl.xyz>
2025-09-20 01:42:58 +0200

    canaryRelease: 447373
    Main scripts: 4
    Lazy Loaded Scripts: 1511
    Chunks:
     * Main Files: 15181
     * Scripts: 8583
    Strings: 22376
    Routes: 738
    Experiments Count: 458
    Apex Experiments Count: 70

--- chunks/188209.js
+++ chunks/188209.js
@@ -1,120 +1,279 @@
-n.d(t, { default: () => g });
-var a = n(951288),
-    r = n(647438),
+n.d(t, { default: () => A });
+var r = n(951288),
+    a = n(647438),
     i = n(442837),
-    s = n(481060),
-    o = n(110924),
-    l = n(448986),
-    c = n(475271),
-    d = n(569984),
-    u = n(497505),
-    f = n(804127),
-    h = n(566078),
-    C = n(602667),
-    m = n(652380),
-    p = n(644646),
-    v = n(515108),
-    _ = n(291967),
-    x = n(46140),
-    j = n(388032),
-    w = n(238905);
-function y(e) {
-    let { quest: t, questContent: n, sourceQuestContent: r, impressionRef: i } = e;
-    return (0, a.jsxs)("div", {
-        className: w.contentContainer,
+    o = n(693789),
+    s = n(82659),
+    l = n(481060),
+    c = n(484614),
+    u = n(110924),
+    d = n(448986),
+    f = n(475271),
+    m = n(246946),
+    p = n(572004),
+    C = n(617136),
+    h = n(509212),
+    v = n(569984),
+    j = n(497505),
+    y = n(804127),
+    x = n(566078),
+    g = n(602667),
+    b = n(652380),
+    w = n(644646),
+    _ = n(515108),
+    O = n(291967),
+    P = n(46140),
+    q = n(388032),
+    T = n(238905);
+function N(e) {
+    var t;
+    let {
+            quest: n,
+            questContent: a,
+            sourceQuestContent: u,
+            rewardCode: d,
+            rewardPlatform: f,
+            sharedQuestFields: v,
+            onClose: j,
+            transitionState: y,
+            impressionRef: x,
+        } = e,
+        g = (0, i.e7)([m.Z], () => m.Z.hidePersonalInformation),
+        b = (0, C.O5)();
+    if (null == d) return null;
+    let w =
+        null !=
+        (t = (0, h.C1)({
+            quest: n,
+            rewardCode: d,
+            selectedPlatformType: f,
+            sharedQuestFields: v,
+        }))
+            ? t
+            : "";
+    return (0, r.jsx)("div", {
+        ref: (e) => {
+            x.current = e;
+        },
+        children: (0, r.jsx)(s.Modal, {
+            size: "md",
+            title: q.intl.string(q.t.NkZ7OT),
+            actions: [
+                {
+                    variant: "primary",
+                    text: q.intl.string(q.t.i4jeWV),
+                    onClick: j,
+                },
+            ],
+            preview: (0, r.jsxs)(r.Fragment, {
+                children: [
+                    (0, r.jsx)(l.Text, {
+                        variant: "eyebrow",
+                        color: "interactive-normal",
+                        className: T.claimInstructionsCodeInputTitle,
+                        children: q.intl.string(q.t.srzsU1),
+                    }),
+                    (0, r.jsx)(c.Z, {
+                        value: g ? q.intl.string(q.t["0n2u0t"]) : d.code,
+                        delay: 1000,
+                        buttonColor: o.Tt.BRAND,
+                        onCopy: () => {
+                            g && (0, p.JG)(d.code),
+                                b({
+                                    questId: n.id,
+                                    questContent: a,
+                                    questContentCTA: C.jZ.COPY_REWARD_CODE,
+                                    sourceQuestContent: u,
+                                });
+                        },
+                    }),
+                ],
+            }),
+            onClose: j,
+            transitionState: y,
+            children: (0, r.jsx)(l.Text, {
+                variant: "text-sm/medium",
+                color: "text-default",
+                className: T.claimInstructionsText,
+                children: w,
+            }),
+        }),
+    });
+}
+function Z(e) {
+    let { quest: t, questContent: n, sourceQuestContent: a, impressionRef: i, onClaimInstructions: o } = e;
+    return (0, r.jsxs)("div", {
+        className: T.contentContainer,
         ref: (e) => {
             i.current = e;
         },
         children: [
-            (0, a.jsx)("div", {
-                className: w.rewardTileContainer,
-                children: (0, a.jsx)(p.Z, {
-                    className: w.rewardTileAsset,
+            (0, r.jsx)("div", {
+                className: T.rewardTileContainer,
+                children: (0, r.jsx)(w.Z, {
+                    className: T.rewardTileAsset,
                     quest: t,
                     questContent: n,
-                    location: x.dr.REWARD_CODE_MODAL,
-                    sourceQuestContent: r,
+                    location: P.dr.REWARD_CODE_MODAL,
+                    sourceQuestContent: a,
                 }),
             }),
-            (0, a.jsxs)("div", {
-                className: w.copyContainer,
+            (0, r.jsxs)("div", {
+                className: T.copyContainer,
                 children: [
-                    (0, a.jsx)(s.X6q, {
+                    (0, r.jsx)(l.X6q, {
                         variant: "heading-xl/bold",
                         color: "header-primary",
-                        className: w.heading,
-                        children: j.intl.string(j.t["5j/Zys"]),
+                        className: T.heading,
+                        children: q.intl.string(q.t["5j/Zys"]),
                     }),
-                    (0, a.jsx)(s.Text, {
+                    (0, r.jsx)(l.Text, {
                         variant: "text-sm/medium",
                         color: "text-secondary",
-                        className: w.text,
-                        children: j.intl.format(j.t.ESmp29, {
-                            onClaimInstructions: () => {},
-                        }),
+                        className: T.text,
+                        children: q.intl.format(q.t.ESmp29, { onClaimInstructions: o }),
                     }),
                 ],
             }),
-            (0, a.jsx)("div", { className: w.codeInputContainer }),
+            (0, r.jsx)("div", { className: T.codeInputContainer }),
         ],
     });
 }
-function g(e) {
-    let { quest: t, onClose: n, transitionState: s, preview: p, sourceQuestContent: x } = e,
-        j = h.r.build(t.config),
-        w = j.rewardPlatforms,
-        g = u.jn.REWARD_MODAL,
+function A(e) {
+    let { quest: t, onClose: n, transitionState: o, preview: s, sourceQuestContent: c } = e,
+        m = x.r.build(t.config),
+        p = m.rewardPlatforms[0],
+        C = j.jn.REWARD_MODAL,
         {
-            questStoreRewardCode: q,
-            isFetchingRewardCode: b,
-            isClaimingReward: N,
-        } = (0, i.cj)([d.Z], () => ({
-            questStoreRewardCode: d.Z.getRewardCode(t.id),
-            isFetchingRewardCode: d.Z.isFetchingRewardCode(t.id),
-            isClaimingReward: d.Z.isClaimingReward(t.id),
+            questStoreRewardCode: h,
+            isFetchingRewardCode: w,
+            isClaimingReward: P,
+        } = (0, i.cj)([v.Z], () => ({
+            questStoreRewardCode: v.Z.getRewardCode(t.id),
+            isFetchingRewardCode: v.Z.isFetchingRewardCode(t.id),
+            isClaimingReward: v.Z.isClaimingReward(t.id),
         })),
-        A = !0 === p ? (0, m.b)(t) : q,
-        { hasError: E, setHasError: Z } = (0, f.u)({
-            isClaimingReward: N,
-            isFetchingRewardCode: b,
+        q = !0 === s ? (0, b.b)(t) : h,
+        { hasError: T, setHasError: A } = (0, y.u)({
+            isClaimingReward: P,
+            isFetchingRewardCode: w,
             quest: t,
-            questContent: g,
+            questContent: C,
             requiresPlatformSelection: !1,
-            rewardCode: A,
-            selectedPlatformType: w[0],
-            preview: p,
+            rewardCode: q,
+            selectedPlatformType: p,
+            preview: s,
         });
     !(function (e) {
-        let t = (0, l.Z)(e),
-            n = c.default.useIsCaptchaModalOpen(),
-            a = (0, o.Z)(n),
-            i = !!(!n && a);
-        r.useEffect(() => {
+        let t = (0, d.Z)(e),
+            n = f.default.useIsCaptchaModalOpen(),
+            r = (0, u.Z)(n),
+            i = !!(!n && r);
+        a.useEffect(() => {
             i && t();
         }, [i, t]);
     })(() => {
-        Z(!0);
+        A(!0);
     });
-    let R = null == A && (b || N),
-        P = E && !N && !b;
-    return R || P
+    let E = null == q && (w || P),
+        I = T && !P && !w;
+    return E || I
         ? null
-        : (0, a.jsx)(C.A, {
+        : (0, r.jsx)(g.A, {
               questOrQuests: t,
-              questContent: g,
-              sourceQuestContent: x,
+              questContent: C,
+              sourceQuestContent: c,
               children: (e) =>
-                  (0, a.jsx)(v.Z, {
+                  (0, r.jsx)(_.Z, {
                       onClose: n,
-                      transitionState: s,
+                      transitionState: o,
                       quest: t,
-                      rewardContent: (0, a.jsx)(_.Z, {
-                          rewardName: j.defaultRewardName,
-                          children: (0, a.jsx)(y, {
+                      rewardContent: (0, r.jsx)(O.Z, {
+                          rewardName: m.defaultRewardName,
+                          children: (0, r.jsx)(Z, {
                               quest: t,
-                              questContent: g,
-                              sourceQuestContent: x,
+                              questContent: C,
+                              sourceQuestContent: c,
                               impressionRef: e,
+                              onClaimInstructions: () => {
+                                  (0, l.ZDy)(
+                                      () =>
+                                          Promise.resolve((e) =>
+                                              (0, r.jsx)(g.A, {
+                                                  questOrQuests: t,
+                                                  questContent: C,
+                                                  sourceQuestContent: c,
+                                                  children: (n) => {
+                                                      var a, i;
+                                                      return (0, r.jsx)(
+                                                          N,
+                                                          ((a = (function (e) {
+                                                              for (var t = 1; t < arguments.length; t++) {
+                                                                  var n = null != arguments[t] ? arguments[t] : {},
+                                                                      r = Object.keys(n);
+                                                                  "function" == typeof Object.getOwnPropertySymbols &&
+                                                                      (r = r.concat(
+                                                                          Object.getOwnPropertySymbols(n).filter(
+                                                                              function (e) {
+                                                                                  return Object.getOwnPropertyDescriptor(
+                                                                                      n,
+                                                                                      e,
+                                                                                  ).enumerable;
+                                                                              },
+                                                                          ),
+                                                                      )),
+                                                                      r.forEach(function (t) {
+                                                                          var r;
+                                                                          (r = n[t]),
+                                                                              t in e
+                                                                                  ? Object.defineProperty(e, t, {
+                                                                                        value: r,
+                                                                                        enumerable: !0,
+                                                                                        configurable: !0,
+                                                                                        writable: !0,
+                                                                                    })
+                                                                                  : (e[t] = r);
+                                                                      });
+                                                              }
+                                                              return e;
+                                                          })({}, e)),
+                                                          (i = i =
+                                                              {
+                                                                  quest: t,
+                                                                  questContent: C,
+                                                                  sourceQuestContent: c,
+                                                                  rewardCode: q,
+                                                                  rewardPlatform: p,
+                                                                  sharedQuestFields: m,
+                                                                  impressionRef: n,
+                                                              }),
+                                                          Object.getOwnPropertyDescriptors
+                                                              ? Object.defineProperties(
+                                                                    a,
+                                                                    Object.getOwnPropertyDescriptors(i),
+                                                                )
+                                                              : (function (e, t) {
+                                                                    var n = Object.keys(e);
+                                                                    if (Object.getOwnPropertySymbols) {
+                                                                        var r = Object.getOwnPropertySymbols(e);
+                                                                        n.push.apply(n, r);
+                                                                    }
+                                                                    return n;
+                                                                })(Object(i)).forEach(function (e) {
+                                                                    Object.defineProperty(
+                                                                        a,
+                                                                        e,
+                                                                        Object.getOwnPropertyDescriptor(i, e),
+                                                                    );
+                                                                }),
+                                                          a),
+                                                      );
+                                                  },
+                                              }),
+                                          ),
+                                      { stackingBehavior: "replace" },
+                                  );
+                              },
                           }),
                       }),
                   }),
--- chunks/238905.js
+++ chunks/238905.js
@@ -6,4 +6,6 @@ e.exports = {
     heading: "heading__42a46",
     text: "text__42a46",
     codeInputContainer: "codeInputContainer__42a46",
+    claimInstructionsCodeInputTitle: "claimInstructionsCodeInputTitle__42a46",
+    claimInstructionsText: "claimInstructionsText__42a46",
 };
