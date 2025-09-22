c5e5af8cd8a138a24e7c4fc9c5f0dd221d12fbe6
DiscrapperManager <admin@minerpl.xyz>
2025-09-22 20:34:31 +0200

    canaryRelease: 447654
    Main scripts: 4
    Lazy Loaded Scripts: 1511
    Chunks:
     * Main Files: 15178
     * Scripts: 8586
    Strings: 22387
    Routes: 738
    Experiments Count: 458
    Apex Experiments Count: 70

--- chunks/325100.js
+++ chunks/325100.js
@@ -1,11 +1,10 @@
-n.d(t, { uH: () => p }), n(35282);
+n.d(t, { uH: () => c }), n(35282);
 var r = n(593473),
-    i = n(843611),
-    a = n(873546);
+    i = n(843611);
 n(960048);
-var o = n(833549),
-    s = n(981631);
-function l(e, t, n) {
+var a = n(833549),
+    o = n(981631);
+function s(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -19,27 +18,11 @@ function l(e, t, n) {
     );
 }
 n(474936);
-let c = () => a.tq && a.bO,
-    u = (e) => {
+let l = (e, t, n) => !t.startsWith(o.Z5c.BILLING_MANAGE_SUBSCRIPTION) && n === o.MUe.APPLE_PAYMENT_LINK && e,
+    c = (e) => {
         let { location: t } = e,
-            { enabled: n } = MobileApplePaymentLinkExperiment.getConfig({ location: t });
-        return n;
-    },
-    d = (e) => {
-        let { location: t } = e,
-            n = f({ location: t });
-        return c() && n;
-    },
-    f = (e) => {
-        let { location: t } = e,
-            { enabled: n } = o.E.useConfig({ location: t });
-        return n;
-    },
-    _ = (e, t, n) => !t.startsWith(s.Z5c.BILLING_MANAGE_SUBSCRIPTION) && n === s.MUe.APPLE_PAYMENT_LINK && e,
-    p = (e) => {
-        let { location: t } = e,
-            { search: n, pathname: a } = (0, i.TH)(),
-            o = d({ location: t }),
-            { deep_link_type: s } = (0, r.parse)(n);
-        return _(o, a, s);
+            { search: n, pathname: o } = (0, i.TH)(),
+            { enabled: s } = a.E.useConfig({ location: t }),
+            { deep_link_type: c } = (0, r.parse)(n);
+        return l(s, o, c);
     };
