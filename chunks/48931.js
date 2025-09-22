7c1185792f4952b30e03ee898c46af1dd8b26cef
DiscrapperManager <admin@minerpl.xyz>
2025-09-22 21:42:21 +0200

    canaryRelease: 447707
    Main scripts: 4
    Lazy Loaded Scripts: 1511
    Chunks:
     * Main Files: 15208
     * Scripts: 8584
    Strings: 22388
    Routes: 738
    Experiments Count: 458
    Apex Experiments Count: 70

--- chunks/48931.js
+++ chunks/48931.js
@@ -1,26 +1,26 @@
-n.d(t, { Z: () => l });
+n.d(t, { Z: () => c });
 var r = n(951288);
 n(647438);
-var i = n(481060),
-    a = n(267097),
-    o = n(752053),
-    s = n(871017);
-function l(e) {
-    let { onClose: t } = e,
-        { refreshCategories: n } = (0, a.Z)();
-    return (0, r.jsxs)(r.Fragment, {
-        children: [
-            (0, r.jsx)(i.xBx, {
-                className: s.header,
-                children: (0, r.jsx)(i.olH, {
-                    className: s.closeButton,
-                    onClick: t,
-                }),
-            }),
-            (0, r.jsx)(o.Z, {
-                onRetry: n,
-                errorOrigin: o.i.GIFT_MODAL,
-            }),
-        ],
+var i = n(257465),
+    a = n(82659),
+    o = n(267097),
+    s = n(752053),
+    l = n(388032);
+function c(e) {
+    let { transitionState: t = i.D.ENTERED, onClose: n } = e,
+        { refreshCategories: c } = (0, o.Z)(),
+        u = async () => {
+            await n();
+        };
+    return (0, r.jsx)(a.Modal, {
+        transitionState: t,
+        onClose: u,
+        size: "sm",
+        title: l.intl.string(l.t["p8+qtb"]),
+        actions: [],
+        children: (0, r.jsx)(s.Z, {
+            onRetry: c,
+            errorOrigin: s.i.GIFT_MODAL,
+        }),
     });
 }
--- chunks/871017.js
+++
@@ -1,4 +0,0 @@
-e.exports = {
-    header: "header__693f1",
-    closeButton: "closeButton__693f1",
-};
