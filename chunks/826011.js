622c425cf808c613ba3ea8789039ec929e3f5472
DiscrapperManager <admin@minerpl.xyz>
2025-09-23 02:15:42 +0200

    canaryRelease: 447999
    Main scripts: 4
    Lazy Loaded Scripts: 1524
    Chunks:
     * Main Files: 15205
     * Scripts: 8609
    Strings: 22401
    Routes: 738
    Experiments Count: 456
    Apex Experiments Count: 70

--- chunks/826011.js
+++ chunks/826011.js
@@ -1,104 +1,79 @@
-s.d(t, { default: () => p });
-var a = s(951288),
-    r = s(647438),
-    n = s(304789),
+s.d(t, { default: () => m });
+var r = s(951288),
+    n = s(647438),
+    a = s(667202),
     i = s(481060),
     c = s(572004),
     o = s(998964),
     l = s(401561),
-    d = s(388032),
-    m = s(927607);
-function p(e) {
-    let { instance: t, onClose: s, transitionState: p } = e,
+    p = s(388032),
+    d = s(927607);
+function m(e) {
+    let { instance: t, onClose: s, transitionState: m } = e,
         x = (0, o.Z)(t),
-        h = r.useCallback(() => {
-            (0, c.JG)(x), (0, i.showToast)((0, i.createToast)(d.intl.string(d.t["+5kSoa"]), i.ToastType.SUCCESS));
+        h = n.useCallback(() => {
+            (0, c.JG)(x), (0, i.showToast)((0, i.createToast)(p.intl.string(p.t["+5kSoa"]), i.ToastType.SUCCESS));
         }, [x]);
-    return (0, a.jsxs)(i.Y0X, {
-        className: m.modal,
-        size: i.CgR.MEDIUM,
-        transitionState: p,
-        parentComponent: "PortkeyInstructionsModal",
-        children: [
-            (0, a.jsx)(i.olH, {
-                onClick: s,
-                className: m.closeButton,
-            }),
-            (0, a.jsx)(n.$, {
-                children: (0, a.jsxs)(i.hzk, {
-                    className: m.content,
-                    children: [
-                        (0, a.jsx)("img", {
-                            src: "",
-                            alt: "",
-                            className: m.gameImage,
-                        }),
-                        (0, a.jsxs)("div", {
-                            className: m.header,
-                            children: [
-                                (0, a.jsx)(i.X6q, {
-                                    variant: "heading-xl/semibold",
-                                    color: "header-primary",
-                                    children: t.name,
-                                }),
-                                (0, a.jsx)(i.Text, {
-                                    variant: "text-md/normal",
-                                    color: "text-muted",
-                                    children: t.planName,
-                                }),
-                            ],
-                        }),
-                        (0, a.jsx)("div", {
-                            className: m.stepsContainer,
-                            children: t.instructions.map((e, t) =>
-                                (0, a.jsxs)(
-                                    "div",
-                                    {
-                                        className: m.step,
-                                        children: [
-                                            (0, a.jsx)("div", {
-                                                className: m.stepNumber,
-                                                children: (0, a.jsx)(i.Text, {
-                                                    variant: "text-sm/semibold",
-                                                    color: "text-secondary",
-                                                    children: d.intl.formatToPlainString(l.default.e0RxEB, {
-                                                        step: t + 1,
-                                                    }),
-                                                }),
-                                            }),
-                                            (0, a.jsx)(i.Text, {
-                                                variant: "text-md/normal",
-                                                color: "header-primary",
-                                                children: e,
-                                            }),
-                                        ],
-                                    },
-                                    t,
-                                ),
-                            ),
-                        }),
-                        (0, a.jsxs)("div", {
-                            className: m.serverIpContainer,
-                            children: [
-                                (0, a.jsx)("div", {
-                                    className: m.serverIpInput,
-                                    children: (0, a.jsx)(i.Text, {
-                                        variant: "text-md/medium",
-                                        color: "header-primary",
-                                        children: x,
+    return (0, r.jsx)(a.I, {
+        graphic: {
+            src: "https://placehold.co/110x150",
+            type: "image",
+        },
+        title: t.name,
+        subtitle: t.planName,
+        size: "md",
+        transitionState: m,
+        onClose: s,
+        children: (0, r.jsxs)("div", {
+            className: d.content,
+            children: [
+                (0, r.jsx)("div", {
+                    className: d.stepsContainer,
+                    children: t.instructions.map((e, t) =>
+                        (0, r.jsxs)(
+                            "div",
+                            {
+                                className: d.step,
+                                children: [
+                                    (0, r.jsx)("div", {
+                                        className: d.stepNumber,
+                                        children: (0, r.jsx)(i.Text, {
+                                            variant: "text-sm/semibold",
+                                            color: "text-secondary",
+                                            children: p.intl.formatToPlainString(l.default.e0RxEB, { step: t + 1 }),
+                                        }),
                                     }),
-                                }),
-                                (0, a.jsx)(i.zxk, {
-                                    variant: "primary",
-                                    text: d.intl.string(d.t.OpuAlJ),
-                                    onClick: h,
-                                    icon: i.TIy,
-                                }),
-                            ],
+                                    (0, r.jsx)(i.Text, {
+                                        variant: "text-md/normal",
+                                        color: "header-primary",
+                                        children: e,
+                                    }),
+                                ],
+                            },
+                            t,
+                        ),
+                    ),
+                }),
+                (0, r.jsxs)("div", {
+                    className: d.serverIpContainer,
+                    children: [
+                        (0, r.jsx)("div", {
+                            className: d.serverIpInput,
+                            children: (0, r.jsx)(i.Text, {
+                                variant: "text-md/medium",
+                                color: "header-primary",
+                                children: x,
+                            }),
+                        }),
+                        (0, r.jsx)(i.zxk, {
+                            variant: "primary",
+                            text: p.intl.string(p.t.OpuAlJ),
+                            onClick: h,
+                            icon: i.TIy,
                         }),
                     ],
                 }),
-            }),
-        ],
+            ],
+        }),
     });
 }
--- chunks/927607.js
+++ chunks/927607.js
@@ -1,9 +1,5 @@
 e.exports = {
-    modal: "modal_f2e68c",
-    closeButton: "closeButton_f2e68c",
     content: "content_f2e68c",
-    gameImage: "gameImage_f2e68c",
-    header: "header_f2e68c",
     stepsContainer: "stepsContainer_f2e68c",
     step: "step_f2e68c",
     stepNumber: "stepNumber_f2e68c",
