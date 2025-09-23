51eb4735cde9e4a87d50d52dc2e38808879b9a75
DiscrapperManager <admin@minerpl.xyz>
2025-09-23 02:50:21 +0200

    canaryRelease: 448016
    Main scripts: 4
    Lazy Loaded Scripts: 1537
    Chunks:
     * Main Files: 15207
     * Scripts: 8609
    Strings: 22401
    Routes: 738
    Experiments Count: 456
    Apex Experiments Count: 71

---
+++ chunks/491966.js
@@ -0,0 +1,32 @@
+n.d(t, { Z: () => r });
+let r = (0, n(427164).le)({
+    name: "2025-09-idle-detection-adjustments",
+    kind: "user",
+    defaultConfig: {
+        settings_updates: !0,
+        power_events: !0,
+        generic_inputs: !1,
+    },
+    variations: {
+        1: {
+            settings_updates: !1,
+            power_events: !0,
+            generic_inputs: !1,
+        },
+        2: {
+            settings_updates: !0,
+            power_events: !1,
+            generic_inputs: !1,
+        },
+        3: {
+            settings_updates: !1,
+            power_events: !1,
+            generic_inputs: !1,
+        },
+        4: {
+            settings_updates: !1,
+            power_events: !1,
+            generic_inputs: !0,
+        },
+    },
+});
--- chunks/517100.js
+++ chunks/517100.js
@@ -1,16 +1,19 @@
-n.d(t, { Z: () => D });
+n.d(t, { Z: () => k });
 var r,
-    i = n(442837),
-    a = n(570140),
-    o = n(287734),
-    s = n(579806),
-    l = n(695346),
-    c = n(70956),
-    u = n(358085),
-    d = n(314897),
-    f = n(981631),
-    _ = n(65154);
-function p(e, t, n) {
+    i = n(954955),
+    a = n.n(i),
+    o = n(442837),
+    s = n(570140),
+    l = n(287734),
+    c = n(579806),
+    u = n(491966),
+    d = n(695346),
+    f = n(70956),
+    _ = n(358085),
+    p = n(314897),
+    h = n(981631),
+    m = n(65154);
+function g(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -23,132 +26,144 @@ function p(e, t, n) {
         e
     );
 }
-let h = Date.now(),
-    m = !1,
-    g = !1,
-    E = !1,
+let E = Date.now(),
     b = !1,
-    y = !1;
-function O() {
-    return E || b || ((0, u.isAndroid)() && y);
+    y = !1,
+    O = !1,
+    v = !1,
+    I = !1;
+function T() {
+    return O || v || ((0, _.isAndroid)() && I);
 }
-function v() {
-    let e = l.CM.getSetting();
-    0 === e || Date.now() - h > Math.min(e * c.Z.Millis.SECOND, f.OSm) || O()
-        ? g ||
-          a.Z.dispatch({
+function S() {
+    let e = d.CM.getSetting();
+    0 === e || Date.now() - E > Math.min(e * f.Z.Millis.SECOND, h.OSm) || T()
+        ? y ||
+          s.Z.dispatch({
               type: "AFK",
               afk: !0,
           })
-        : g &&
-          a.Z.dispatch({
+        : y &&
+          s.Z.dispatch({
               type: "AFK",
               afk: !1,
           });
 }
-function I() {
-    Date.now() - h > f.OSm || O()
-        ? m ||
-          a.Z.dispatch({
+function A() {
+    Date.now() - E > h.OSm || T()
+        ? b ||
+          s.Z.dispatch({
               type: "IDLE",
               idle: !0,
-              idleSince: h,
+              idleSince: E,
           })
-        : m &&
-          a.Z.dispatch({
+        : b &&
+          s.Z.dispatch({
               type: "IDLE",
               idle: !1,
           });
 }
-function T() {
-    I(), v();
+function C() {
+    A(), S();
 }
-function S() {
+function N() {
     var e;
     let t = (e) => {
-        (h = Math.max(Date.now() - e, h)), T(), setTimeout(S, 10 * c.Z.Millis.SECOND);
+        (E = Math.max(Date.now() - e, E)), C(), setTimeout(N, 10 * f.Z.Millis.SECOND);
     };
     if (
-        (null === s.Z || void 0 === s.Z || null == (e = s.Z.remotePowerMonitor) ? void 0 : e.getSystemIdleTimeMs) !=
+        (null === c.Z || void 0 === c.Z || null == (e = c.Z.remotePowerMonitor) ? void 0 : e.getSystemIdleTimeMs) !=
         null
     ) {
-        let e = s.Z.remotePowerMonitor.getSystemIdleTimeMs();
+        let e = c.Z.remotePowerMonitor.getSystemIdleTimeMs();
         e instanceof Promise ? e.then(t) : t(e);
     }
 }
-function A(e) {
-    m = e.idle;
-}
-function C(e) {
-    g = e.afk;
-}
-function N(e) {
-    let { userId: t, speakingFlags: n } = e;
-    return n !== _.Dg.NONE && t === d.default.getId() && P({}), !1;
-}
 function R(e) {
-    let { state: t } = e;
-    return (y = t === f.$7l.BACKGROUND), (h = Date.now()), T(), !1;
+    u.Z.getConfig({ location: "handlePowerEvent" }).power_events ? L({}) : (e && (E = -1 / 0), C());
+}
+if (!__OVERLAY__) {
+    _.isPlatformEmbedded && (null === c.Z || void 0 === c.Z ? void 0 : c.Z.remotePowerMonitor) != null
+        ? (N(),
+          c.Z.remotePowerMonitor.on("resume", () => {
+              (O = !1), R(!1);
+          }),
+          c.Z.remotePowerMonitor.on("suspend", () => {
+              (O = !0), R(!0), l.default.disconnect();
+          }),
+          c.Z.remotePowerMonitor.on("lock-screen", () => {
+              (v = !0), R(!0);
+          }),
+          c.Z.remotePowerMonitor.on("unlock-screen", () => {
+              (v = !1), R(!1);
+          }))
+        : setInterval(C, 30 * f.Z.Millis.SECOND);
+    let e = a()(() => {
+        u.Z.getConfig({ location: "handleGenericInput" }).generic_inputs && L({});
+    }, 500);
+    window.addEventListener("mouseup", e), window.addEventListener("wheel", e), window.addEventListener("keypress", e);
 }
 function P(e) {
+    b = e.idle;
+}
+function w(e) {
+    y = e.afk;
+}
+function D(e) {
+    let { userId: t, speakingFlags: n } = e;
+    return n !== m.Dg.NONE && t === p.default.getId() && L({}), !1;
+}
+function x(e) {
+    let { state: t } = e;
+    return (I = t === h.$7l.BACKGROUND), (E = Date.now()), C(), !1;
+}
+function L(e) {
     let { timestamp: t, type: n } = e,
         r = "OVERLAY_SET_NOT_IDLE" === n && null != t;
     return (
-        (!r || !(t <= h)) &&
-        ((h = r ? t : Date.now()),
+        (!r || !(t <= E)) &&
+        ((E = r ? t : Date.now()),
         __OVERLAY__
-            ? a.Z.dispatch({
+            ? s.Z.dispatch({
                   type: "OVERLAY_SET_NOT_IDLE",
-                  timestamp: h,
+                  timestamp: E,
               })
-            : T(),
+            : C(),
         !1)
     );
 }
-__OVERLAY__ ||
-    (u.isPlatformEmbedded && (null === s.Z || void 0 === s.Z ? void 0 : s.Z.remotePowerMonitor) != null
-        ? (S(),
-          s.Z.remotePowerMonitor.on("resume", () => {
-              (E = !1), P({});
-          }),
-          s.Z.remotePowerMonitor.on("suspend", () => {
-              (E = !0), P({}), o.default.disconnect();
-          }),
-          s.Z.remotePowerMonitor.on("lock-screen", () => {
-              (b = !0), P({});
-          }),
-          s.Z.remotePowerMonitor.on("unlock-screen", () => {
-              (b = !1), P({});
-          }))
-        : setInterval(T, 30 * c.Z.Millis.SECOND));
-class w extends (r = i.ZP.Store) {
+function j() {
+    if (!u.Z.getConfig({ location: "handleSettingsProtoUpdate" }).settings_updates) return !1;
+    L({});
+}
+class M extends (r = o.ZP.Store) {
     isIdle() {
-        return m;
-    }
-    isAFK() {
-        return g;
-    }
-    getIdleSince() {
-        return m ? h : null;
-    }
-    getSystemSuspended() {
-        return E;
-    }
-    getSystemLocked() {
         return b;
     }
+    isAFK() {
+        return y;
+    }
+    getIdleSince() {
+        return b ? E : null;
+    }
+    getSystemSuspended() {
+        return O;
+    }
+    getSystemLocked() {
+        return v;
+    }
 }
-p(w, "displayName", "IdleStore");
-let D = new w(a.Z, {
-    IDLE: A,
-    AFK: C,
-    SPEAKING: N,
-    APP_STATE_UPDATE: R,
-    OVERLAY_SET_NOT_IDLE: P,
-    CHANNEL_SELECT: P,
-    VOICE_CHANNEL_SELECT: P,
-    WINDOW_FOCUS: P,
-    OVERLAY_INITIALIZE: P,
-    OVERLAY_SET_INPUT_LOCKED: P,
-    USER_SETTINGS_PROTO_UPDATE: P,
+g(M, "displayName", "IdleStore");
+let k = new M(s.Z, {
+    IDLE: P,
+    AFK: w,
+    SPEAKING: D,
+    APP_STATE_UPDATE: x,
+    OVERLAY_SET_NOT_IDLE: L,
+    CHANNEL_SELECT: L,
+    VOICE_CHANNEL_SELECT: L,
+    WINDOW_FOCUS: L,
+    OVERLAY_INITIALIZE: L,
+    OVERLAY_SET_INPUT_LOCKED: L,
+    USER_SETTINGS_PROTO_UPDATE: j,
 });
