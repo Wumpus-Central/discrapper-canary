5b2b7099d0ca67b9b5f715183043826000f18fda
DiscrapperManager <admin@minerpl.xyz>
2025-09-24 20:37:41 +0200

    canaryRelease: 449007
    Main scripts: 4
    Lazy Loaded Scripts: 1557
    Chunks:
     * Main Files: 14895
     * Scripts: 8901
    Strings: 22433
    Routes: 740
    Experiments Count: 448
    Apex Experiments Count: 76

--- chunks/174609.js
+++ chunks/174609.js
@@ -1,6 +1,6 @@
 n.d(t, { Z: () => a });
 var r = n(358085),
     i = n(998502);
-function a(e) {
-    r.isPlatformEmbedded ? i.ZP.focus(e) : window.focus();
+function a(e, t) {
+    r.isPlatformEmbedded ? i.ZP.focus(e, t) : window.focus();
 }
--- chunks/618373.js
+++ chunks/618373.js
@@ -1,41 +1,53 @@
-n.d(t, { Q: () => d });
+n.d(t, { Q: () => h });
 var r = n(951288);
 n(647438);
 var i = n(442837),
     a = n(481060),
     o = n(594190),
-    s = n(865066),
-    l = n(809357),
-    c = n(44359),
-    u = n(388032);
-function d(e, t, n, i) {
-    return (0, r.jsx)(f, {
+    s = n(174609),
+    l = n(865066),
+    c = n(313789),
+    u = n(518596),
+    d = n(809357),
+    f = n(44359),
+    _ = n(981631),
+    p = n(388032);
+function h(e, t, n, i) {
+    return (0, r.jsx)(m, {
         keybind: e,
         message: t,
         messageWithoutKeybind: n,
         highlightAdminWarningIfElevated: i,
     });
 }
-function f(e) {
-    let { keybind: t, message: n, messageWithoutKeybind: d, highlightAdminWarningIfElevated: f = !1 } = e,
-        _ = (0, l.o)(),
-        p = (0, i.e7)([o.ZP], () => {
+function m(e) {
+    let { keybind: t, message: n, messageWithoutKeybind: h, highlightAdminWarningIfElevated: m = !1 } = e,
+        g = (0, d.o)(),
+        E = (0, i.e7)([o.ZP], () => {
             let e = o.ZP.getVisibleGame();
             return null != e && e.elevated && o.ZP.canShowAdminWarning;
         }),
-        h = (0, s.hi)();
-    return p
+        b = (0, l.hi)();
+    return E
         ? (0, r.jsx)(a.Text, {
               variant: "text-xxs/semibold",
-              color: f ? "text-feedback-warning" : "text-muted",
-              children: h ? u.intl.string(u.t.rqNDQk) : u.intl.string(u.t.PIeSHB),
+              color: m ? "text-feedback-warning" : "text-muted",
+              children: b
+                  ? p.intl.format(p.t.MVS9pa, {
+                        onSettingsClick: (e) => {
+                            e.stopPropagation(),
+                                (0, s.Z)(void 0, !0),
+                                (0, u.openUserSettings)(c.n.OVERLAY_PANEL, { section: _.oAB.OVERLAY });
+                        },
+                    })
+                  : p.intl.string(p.t.PIeSHB),
           })
-        : _
+        : g
           ? (0, r.jsx)(r.Fragment, {
-                children: u.intl.format(n, {
+                children: p.intl.format(n, {
                     keybind: t,
-                    keybindHook: (e) => (0, r.jsx)(c.Z, { keybind: t }, t.join("+")),
+                    keybindHook: (e) => (0, r.jsx)(f.Z, { keybind: t }, t.join("+")),
                 }),
             })
-          : d;
+          : h;
 }
