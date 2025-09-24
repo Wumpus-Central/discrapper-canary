67cbc2dd3d87d3283569728f58c5f7c75b01de1f
DiscrapperManager <admin@minerpl.xyz>
2025-09-24 20:51:42 +0200

    canaryRelease: 449026
    Main scripts: 4
    Lazy Loaded Scripts: 1557
    Chunks:
     * Main Files: 14895
     * Scripts: 8901
    Strings: 22433
    Routes: 740
    Experiments Count: 448
    Apex Experiments Count: 76

--- chunks/567400.js
+++ chunks/567400.js
@@ -1,6 +1,6 @@
-n.d(t, { O: () => i });
-let r = (0, n(427164).le)({
-    name: "2025-09-user-profile-wishlist",
+r.d(t, { Y: () => l });
+let n = (0, r(427164).le)({
+    name: "2025-09-user-profile-wishlist-editing",
     kind: "user",
     defaultConfig: { enabled: !1 },
     variations: {
@@ -8,7 +8,7 @@ let r = (0, n(427164).le)({
         1: { enabled: !0 },
     },
 });
-function i(e) {
+function l(e) {
     let { location: t } = e;
-    return r.useConfig({ location: t }).enabled;
+    return n.useConfig({ location: t }).enabled;
 }
