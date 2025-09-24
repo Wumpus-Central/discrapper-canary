9c90abc2f2c9a7097747f5b36d1fe8024b3523c5
DiscrapperManager <admin@minerpl.xyz>
2025-09-24 21:36:20 +0200

    canaryRelease: 449073
    Main scripts: 4
    Lazy Loaded Scripts: 1557
    Chunks:
     * Main Files: 14894
     * Scripts: 8900
    Strings: 22433
    Routes: 740
    Experiments Count: 447
    Apex Experiments Count: 76

--- chunks/822253.js
+++ chunks/822253.js
@@ -1,27 +1,7 @@
-n.d(t, { E: () => o });
-var r = n(818083),
-    i = n(751823);
-let a = (0, r.B)({
-    kind: "user",
-    id: "2025-06_async_audio_device",
-    label: "Desktop Asynchronous Audio Device Manager Controls",
-    defaultConfig: { enabled: !1 },
-    treatments: [
-        {
-            id: 1,
-            label: "Desktop Asynchronous Audio Device Manager Controls",
-            config: { enabled: !0 },
-        },
-    ],
-});
-function o(e) {
+n.d(t, { E: () => i });
+var r = n(751823);
+function i(e) {
     let { location: t, autoTrackExposure: n = !0 } = e,
-        { isInHoldout: r } = i.L.getCurrentConfig({ location: t }, { autoTrackExposure: n });
-    return a.getCurrentConfig(
-        { location: t },
-        {
-            disable: r,
-            autoTrackExposure: n,
-        },
-    );
+        { isInHoldout: i } = r.L.getCurrentConfig({ location: t }, { autoTrackExposure: n });
+    return { enabled: !i };
 }
