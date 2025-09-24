9ff56ef2334407edbea191ce69e74c01bf8f69e1
DiscrapperManager <admin@minerpl.xyz>
2025-09-24 22:01:01 +0200

    canaryRelease: 449091
    Main scripts: 4
    Lazy Loaded Scripts: 1557
    Chunks:
     * Main Files: 14894
     * Scripts: 8900
    Strings: 22430
    Routes: 740
    Experiments Count: 447
    Apex Experiments Count: 76

--- chunks/597679.js
+++ chunks/597679.js
@@ -1,18 +1,18 @@
 n.d(t, { Z: () => d });
-var i = n(951288),
-    l = n(657707),
-    r = n(509613),
+var r = n(951288),
+    i = n(657707),
+    l = n(509613),
+    o = n(313789),
     s = n(595242),
-    a = n(869235),
-    o = n(726985),
+    a = n(726985),
     u = n(388032);
-let c = (0, r.x1)(a.t.PREMIUM_GUILD_SUBSCRIPTIONS_PANE, {
+let c = (0, l.x1)(o.n.PREMIUM_GUILD_SUBSCRIPTIONS_PANE, {
         getLayout: () => [],
-        render: () => (0, i.jsx)(s.Z, {}),
+        render: () => (0, r.jsx)(s.Z, {}),
     }),
-    d = (0, r.wf)(a.t.PREMIUM_GUILD_SUBSCRIPTIONS_PANEL, {
-        useTitle: () => u.intl.string(u.t["/n7kz8"]),
-        icon: l.$Eu,
+    d = (0, l.wf)(o.n.PREMIUM_GUILD_SUBSCRIPTIONS_PANEL, {
+        useTitle: () => u.intl.string(u.t.Nn1lJy),
+        icon: i.$Eu,
         getLayout: () => [c],
-        legacySearchKey: o.s6.GUILD_BOOSTING,
+        legacySearchKey: a.s6.GUILD_BOOSTING,
     });
