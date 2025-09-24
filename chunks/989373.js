4bad298b70135c620783b8092e6e933603270854
DiscrapperManager <admin@minerpl.xyz>
2025-09-24 20:40:33 +0200

    canaryRelease: 449009
    Main scripts: 4
    Lazy Loaded Scripts: 1557
    Chunks:
     * Main Files: 14895
     * Scripts: 8901
    Strings: 22433
    Routes: 740
    Experiments Count: 448
    Apex Experiments Count: 76

--- chunks/989373.js
+++ chunks/989373.js
@@ -1,85 +1,85 @@
-n.r(t), n.d(t, { default: () => W }), n(415506), n(388685);
-var a = n(951288),
-    o = n(647438),
-    r = n(442837),
-    i = n(13245),
-    c = n(493773),
-    _ = n(686546),
-    l = n(710845),
-    d = n(522474),
-    u = n(238246),
-    s = n(355863),
-    f = n(944486),
-    p = n(808506),
-    h = n(358085);
-n(606206);
-var b = n(998502),
-    m = n(145597);
-n(371467);
-var v = n(41534),
-    g = n(837268),
-    I = n(554370),
-    C = n(371651),
-    y = n(610394),
-    w = n(757744),
-    x = n(981631);
+t.r(n), t.d(n, { default: () => S }), t(415506), t(388685);
+var a = t(951288),
+    o = t(647438),
+    r = t(442837),
+    i = t(13245),
+    c = t(493773),
+    _ = t(686546),
+    l = t(710845),
+    d = t(522474),
+    u = t(238246),
+    s = t(355863),
+    f = t(944486),
+    h = t(808506),
+    b = t(358085);
+t(606206);
+var p = t(998502),
+    g = t(145597);
+t(371467);
+var m = t(41534),
+    v = t(837268),
+    C = t(554370),
+    I = t(371651),
+    w = t(610394),
+    y = t(757744),
+    x = t(981631);
 let B = new l.Z("AppOverlay");
-async function T(e, t) {
-    let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : 1000,
+async function W(e, n) {
+    let t = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : 1000,
         a = arguments.length > 3 && void 0 !== arguments[3] ? arguments[3] : 3;
-    if (h.isPlatformEmbedded) {
+    if (b.isPlatformEmbedded) {
         try {
-            await b.ZP.isAlwaysOnTop(t);
+            await p.ZP.isAlwaysOnTop(n);
         } catch (e) {
-            B.error("Window does not exist while trying to show inactive", e), (0, v.D1)(e, g.gl.OutOfProcess);
+            B.error("Window does not exist while trying to show inactive", e), (0, m.D1)(e, v.gl.OutOfProcess);
         }
         for (let r = 0; r < a; r++)
             try {
-                if (!(await b.ZP.waitForIPCReady(n, e))) throw Error("IPC not ready");
-                b.ZP.showInactive(t);
+                if (!(await p.ZP.waitForIPCReady(t, e))) throw Error("IPC not ready");
+                p.ZP.showInactive(n);
                 return;
             } catch (e) {
                 var o;
                 if ((null == (o = e.message) ? void 0 : o.includes("IPC")) && r < a - 1) {
-                    let t = (n / 2) * Math.pow(2, r + 1);
-                    B.error("Failed to show inactive, retrying in ".concat(t, "ms"), e),
-                        await new Promise((e) => setTimeout(e, t));
-                } else throw ((0, v.D1)(e, g.gl.OutOfProcess), e);
+                    let n = (t / 2) * Math.pow(2, r + 1);
+                    B.error("Failed to show inactive, retrying in ".concat(n, "ms"), e),
+                        await new Promise((e) => setTimeout(e, n));
+                } else throw ((0, m.D1)(e, v.gl.OutOfProcess), e);
             }
     }
 }
-let W = o.memo(function (e) {
-    let { withTitleBar: t, windowKey: n } = e,
-        l = (function (e, t) {
-            let n = (0, r.e7)([y.ZP], () => !h.isPlatformEmbedded || y.ZP.isWindowHandleInitialized()),
+let S = o.memo(function (e) {
+    let { withTitleBar: n, windowKey: t } = e,
+        l = (function (e, n) {
+            let t = (0, r.e7)([w.ZP], () => !b.isPlatformEmbedded || w.ZP.isWindowHandleInitialized()),
                 a = (0, r.e7)([d.Z], () => d.Z.getWindow(e)),
                 _ = o.useRef(null),
                 l = o.useRef(null),
                 u = o.useRef(null),
-                b = (0, r.e7)([p.default], () => {
-                    let e = p.default.getFocusedPID();
-                    return !h.isPlatformEmbedded || (null != e && e !== m.UNSET_PID);
+                p = (0, r.e7)([h.default], () => {
+                    let e = h.default.getFocusedPID();
+                    return !b.isPlatformEmbedded || (null != e && e !== g.UNSET_PID);
                 }),
-                [v, I] = o.useState(!1),
-                w = o.useRef(!1),
+                [m, C] = o.useState(!1),
+                y = o.useRef(!1),
                 B = o.useCallback(() => {
-                    let e = (0, m.getPID)(),
-                        n = null != f.Z.getVoiceChannelId();
+                    let e = (0, g.getPID)(),
+                        t = null != f.Z.getVoiceChannelId();
                     i.Z.track(x.rMx.OVERLAY_INITIALIZED, {
-                        voice_widget_connected: n,
-                        text_widget_connected: y.ZP.isPinned(x.Odu.TEXT),
-                        overlay_render_method: g.gl[C.default.getOverlayMethod(e)],
-                        unpinned_widget_types: s.Z.getAllUnpinnedPinnedWidgets(t),
+                        voice_widget_connected: t,
+                        text_widget_connected: w.ZP.isPinned(x.Odu.TEXT),
+                        overlay_render_method: v.gl[I.default.getOverlayMethod(e)],
+                        unpinned_widget_types: s.Z.getAllUnpinnedPinnedWidgets(n),
                     }),
                         i.Z.oopUiInitialized();
-                }, [t]);
+                }, [n]);
             (0, c.ZP)(() => {
                 let e = (e) => {
-                    let t = null != a ? a : window;
-                    e.data === y.Il &&
-                        t.requestAnimationFrame(() => {
-                            t.requestAnimationFrame(() => {
-                                window.parent.postMessage(y.A8, "*");
+                    let n = null != a ? a : window;
+                    e.data === w.Il &&
+                        n.requestAnimationFrame(() => {
+                            n.requestAnimationFrame(() => {
+                                window.parent.postMessage(w.A8, "*");
                             });
                         });
                 };
@@ -90,8 +90,8 @@ let W = o.memo(function (e) {
                     }
                 );
             });
-            let W = o.useCallback(
-                    (e, t) => {
+            let S = o.useCallback(
+                    (e, n) => {
                         null == _.current &&
                             ((u.current = () => {
                                 null != _.current && e.cancelAnimationFrame(_.current),
@@ -99,47 +99,46 @@ let W = o.memo(function (e) {
                             }),
                             (_.current = e.requestAnimationFrame(async () => {
                                 try {
-                                    await T(e, t), i.Z.oopUiShowInactiveSuccess();
+                                    await W(e, n), i.Z.oopUiShowInactiveSuccess();
                                 } catch (e) {
-                                    i.Z.setOverlayCrashed((0, m.getPID)(), e);
+                                    i.Z.setOverlayCrashed((0, g.getPID)(), e);
                                     return;
                                 }
                                 l.current = e.setTimeout(() => {
-                                    I(!0), B(), (u.current = null);
+                                    C(!0), B(), (u.current = null);
                                 }, 100);
                             })));
                     },
                     [B],
                 ),
-                S = o.useRef(!1);
+                T = o.useRef(!1);
             return (
                 o.useEffect(() => {
-                    if (!w.current && null != a && n) {
-                        if (!b) {
-                            S.current ||
-                                (i.Z.updateOverlayState((0, m.getPID)(), g.mM.WAITING_FOR_PID_FOCUS), (S.current = !0));
+                    if (!y.current && null != a && t) {
+                        if (!p) {
+                            T.current ||
+                                (i.Z.updateOverlayState((0, g.getPID)(), v.mM.WAITING_FOR_PID_FOCUS), (T.current = !0));
                             return;
                         }
-                        (w.current = !0), W(a, e);
+                        (y.current = !0), S(a, e);
                     }
-                }, [W, b, e, a, n]),
+                }, [S, p, e, a, t]),
                 (0, c.zq)(() => {
                     var e;
                     null == (e = u.current) || e.call(u);
                 }),
-                v
+                m
             );
-        })(n, w.$S),
-        b = (0, m.getPID)(),
-        v = (0, r.e7)([p.default], () => p.default.isInputLocked(b), [b]);
+        })(t, y.$S),
+        p = (0, r.e7)([h.default], () => h.default.isFocusedPidInputLocked());
     return l
         ? (0, a.jsxs)(u.Z, {
               themeOverride: x.BRd.MIDNIGHT,
-              withTitleBar: t,
-              windowKey: n,
+              withTitleBar: n,
+              windowKey: t,
               title: "Discord Overlay",
-              hideModals: v,
-              children: [(0, a.jsx)(I.Z, {}), (0, a.jsx)(_.Co, {})],
+              hideModals: p,
+              children: [(0, a.jsx)(C.Z, {}), (0, a.jsx)(_.Co, {})],
           })
         : null;
 });
