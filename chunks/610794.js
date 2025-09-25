60f412fda32bb1abfc3801ae8b12879fcd3e7cdc
DiscrapperManager <admin@minerpl.xyz>
2025-09-25 23:17:03 +0200

    canaryRelease: 449846
    Main scripts: 4
    Lazy Loaded Scripts: 1557
    Chunks:
     * Main Files: 14883
     * Scripts: 8904
    Strings: 22441
    Routes: 740
    Experiments Count: 440
    Apex Experiments Count: 78

--- chunks/610794.js
+++ chunks/610794.js
@@ -1,178 +1,178 @@
-n.d(t, { Z: () => T }), n(388685), n(457542);
+n.d(t, { Z: () => I }), n(388685), n(457542);
 var r = n(951288),
     i = n(647438),
     a = n(442837),
     o = n(780384),
-    s = n(755721),
-    l = n(481060),
-    c = n(457330),
-    u = n(410030),
-    d = n(726542),
-    f = n(275759),
-    _ = n(231757),
-    p = n(888496),
-    h = n(706454),
-    m = n(553795),
-    g = n(63063),
-    E = n(981631),
-    b = n(856651),
-    y = n(388032),
-    O = n(673449);
-function v(e) {
+    s = n(481060),
+    l = n(457330),
+    c = n(410030),
+    u = n(726542),
+    d = n(275759),
+    f = n(231757),
+    _ = n(888496),
+    p = n(706454),
+    h = n(553795),
+    m = n(63063),
+    g = n(981631),
+    E = n(856651),
+    b = n(388032),
+    y = n(737823);
+function O(e) {
     var t;
-    let { account: n, refreshed: o, handleRefresh: c } = e,
-        [u, d] = i.useState(!1),
-        _ = null != (t = n.metadata) ? t : {},
-        m = (0, a.e7)([h.default], () => h.default.locale),
-        v = i.useCallback(async () => {
-            d(!0);
+    let { account: n, refreshed: o, handleRefresh: l } = e,
+        [c, u] = i.useState(!1),
+        f = null != (t = n.metadata) ? t : {},
+        h = (0, a.e7)([p.default], () => p.default.locale),
+        O = i.useCallback(async () => {
+            u(!0);
             try {
-                await c(n);
+                await l(n);
             } finally {
-                d(!1);
+                u(!1);
             }
-        }, [n, c]),
-        I = null;
+        }, [n, l]),
+        v = null;
     switch (n.type) {
-        case E.ABu.REDDIT:
-            I = (0, p.oP)(_, O.metadataItem);
+        case g.ABu.REDDIT:
+            v = (0, _.oP)(f, y.metadataItem);
             break;
-        case E.ABu.STEAM:
-            I = (0, p.Dq)(_, O.metadataItem);
+        case g.ABu.STEAM:
+            v = (0, _.Dq)(f, y.metadataItem);
             break;
-        case E.ABu.TWITTER:
-            I = (0, p.rJ)(_, O.metadataItem);
+        case g.ABu.TWITTER:
+            v = (0, _.rJ)(f, y.metadataItem);
             break;
-        case E.ABu.EBAY:
-            I = (0, p.ul)(_, O.metadataItem);
+        case g.ABu.EBAY:
+            v = (0, _.ul)(f, y.metadataItem);
             break;
-        case E.ABu.PAYPAL:
-            I = (0, p.li)(_, O.metadataItem);
+        case g.ABu.PAYPAL:
+            v = (0, _.li)(f, y.metadataItem);
             break;
-        case E.ABu.TIKTOK:
-            I = (0, p.hf)(_, O.metadataItem);
+        case g.ABu.TIKTOK:
+            v = (0, _.hf)(f, y.metadataItem);
     }
-    let T = (0, f.FI)(_[b.PC.CREATED_AT], m),
-        S = null,
-        A = y.intl.string(y.t.wzzjk5);
+    let I = (0, d.FI)(f[E.PC.CREATED_AT], h),
+        T = null,
+        S = b.intl.string(b.t.wzzjk5);
     return (
-        (null == I || 0 === I.length) &&
-            null == T &&
-            ((S = (0, r.jsx)(
-                l.Text,
+        (null == v || 0 === v.length) &&
+            null == I &&
+            ((T = (0, r.jsx)(
+                s.Text,
                 {
                     variant: "text-xs/normal",
                     color: "header-secondary",
-                    children: y.intl.format(y.t.Up2ni4, { helpdeskUrl: g.Z.getArticleURL(E.BhN.CONNECTION_DETAILS) }),
+                    children: b.intl.format(b.t.Up2ni4, { helpdeskUrl: m.Z.getArticleURL(g.BhN.CONNECTION_DETAILS) }),
                 },
                 "label",
             )),
-            (A = y.intl.string(y.t["LVh3//"]))),
-        o && (A = y.intl.string(y.t.i4jeWV)),
+            (S = b.intl.string(b.t["LVh3//"]))),
+        o && (S = b.intl.string(b.t.i4jeWV)),
         (0, r.jsxs)("div", {
-            className: O.metadataContainer,
+            className: y.metadataContainer,
             children: [
-                S,
-                null == I
+                T,
+                null == v
                     ? void 0
-                    : I.map((e, t) =>
+                    : v.map((e, t) =>
                           (0, r.jsxs)(r.Fragment, {
-                              children: [e, t < I.length - 1 ? (0, r.jsx)("span", { className: O.dot }) : null],
+                              children: [e, t < v.length - 1 ? (0, r.jsx)("span", { className: y.dot }) : null],
                           }),
                       ),
-                null != I && I.length > 0 && null != T ? (0, r.jsx)("div", { className: O.dot }) : null,
-                null != T
+                null != v && v.length > 0 && null != I ? (0, r.jsx)("div", { className: y.dot }) : null,
+                null != I
                     ? (0, r.jsx)(
-                          l.Text,
+                          s.Text,
                           {
                               variant: "text-xs/normal",
                               color: "header-secondary",
-                              children: y.intl.format(y.t["9rfonp"], { date: T }),
+                              children: b.intl.format(b.t["9rfonp"], { date: I }),
                           },
                           "member-since",
                       )
                     : null,
-                (0, r.jsx)(
-                    s.zx,
-                    {
-                        className: O.metadataRefreshButton,
-                        look: s.zx.Looks.OUTLINED,
-                        color: o ? s.zx.Colors.GREEN : s.zx.Colors.PRIMARY,
-                        size: s.zx.Sizes.MIN,
-                        submitting: u,
-                        disabled: o,
-                        "aria-label": y.intl.string(y.t.sCkLYG),
-                        onClick: o ? void 0 : v,
-                        children: A,
-                    },
-                    "refresh-button",
-                ),
+                (0, r.jsx)("div", {
+                    className: y.metadataRefreshButton,
+                    children: (0, r.jsx)(
+                        s.zxk,
+                        {
+                            size: "sm",
+                            variant: o ? "active" : "secondary",
+                            loading: c,
+                            disabled: o,
+                            "aria-label": b.intl.string(b.t.sCkLYG),
+                            onClick: O,
+                            text: S,
+                        },
+                        "refresh-button",
+                    ),
+                }),
             ],
         })
     );
 }
-function I(e) {
+function v(e) {
     let { account: t, handleRefresh: n, refreshedAccountIds: a } = e,
-        [s, f] = i.useState(t.visibility),
+        [d, _] = i.useState(t.visibility),
         [p, h] = i.useState(t.metadataVisibility),
-        m = (0, u.ZP)();
+        m = (0, c.ZP)();
     function g(e) {
         let { verified: n } = t,
             r = +!!e;
         if (e && !n) {
-            f(r),
-                (0, _.Z)({
+            _(r),
+                (0, f.Z)({
                     platformType: t.type,
                     location: "User Settings",
                 });
             return;
         }
-        f(r), c.Z.setVisibility(t.type, t.id, r);
+        _(r), l.Z.setVisibility(t.type, t.id, r);
     }
     function E(e) {
         let { verified: n } = t,
             r = +!!e;
         if (e && !n) {
             h(r),
-                (0, _.Z)({
+                (0, f.Z)({
                     platformType: t.type,
                     location: "User Settings",
                 });
             return;
         }
-        h(r), c.Z.setMetadataVisibility(t.type, t.id, r);
+        h(r), l.Z.setMetadataVisibility(t.type, t.id, r);
     }
     i.useEffect(() => {
-        f(t.visibility), h(t.metadataVisibility);
+        _(t.visibility), h(t.metadataVisibility);
     }, [t]);
-    let b = d.Z.get(t.type),
-        I = !0 === b.hasMetadata;
+    let v = u.Z.get(t.type),
+        I = !0 === v.hasMetadata;
     return (0, r.jsxs)(r.Fragment, {
         children: [
             (0, r.jsxs)("div", {
-                className: O.activityRow,
+                className: y.activityRow,
                 children: [
                     (0, r.jsx)("img", {
-                        alt: b.name,
-                        className: O.connectionIcon,
-                        src: (0, o.wj)(m) ? b.icon.darkSVG : b.icon.lightSVG,
+                        alt: v.name,
+                        className: y.connectionIcon,
+                        src: (0, o.wj)(m) ? v.icon.darkSVG : v.icon.lightSVG,
                     }),
                     (0, r.jsxs)("div", {
-                        className: O.activitySettings,
+                        className: y.activitySettings,
                         children: [
-                            (0, r.jsxs)(l.j7V, {
-                                className: O.visibilitySwitch,
+                            (0, r.jsxs)(s.j7V, {
+                                className: y.visibilitySwitch,
                                 hideBorder: !0,
-                                value: 1 === s,
+                                value: 1 === d,
                                 onChange: g,
                                 children: [
-                                    (0, r.jsx)(l.Text, {
+                                    (0, r.jsx)(s.Text, {
                                         variant: "text-md/semibold",
                                         color: "header-primary",
-                                        children: b.name,
+                                        children: v.name,
                                     }),
                                     I &&
-                                        (0, r.jsx)(v, {
+                                        (0, r.jsx)(O, {
                                             account: t,
                                             refreshed: a.includes(t.id),
                                             handleRefresh: n,
@@ -180,52 +180,52 @@ function I(e) {
                                 ],
                             }),
                             I &&
-                                (0, r.jsx)(l.j7V, {
-                                    className: O.additionalDetailsSwitch,
+                                (0, r.jsx)(s.j7V, {
+                                    className: y.additionalDetailsSwitch,
                                     hideBorder: !0,
-                                    disabled: 1 !== s || null == t.metadata,
+                                    disabled: 1 !== d || null == t.metadata,
                                     value: 1 === p,
                                     onChange: E,
-                                    children: (0, r.jsx)(l.Text, {
+                                    children: (0, r.jsx)(s.Text, {
                                         variant: "text-md/normal",
                                         color: "header-secondary",
-                                        children: y.intl.string(y.t["3l78ws"]),
+                                        children: b.intl.string(b.t["3l78ws"]),
                                     }),
                                 }),
                         ],
                     }),
                 ],
             }),
-            (0, r.jsx)("div", { className: O.divider }),
+            (0, r.jsx)("div", { className: y.divider }),
         ],
     });
 }
-function T() {
-    let e = (0, a.e7)([m.Z], () => m.Z.getAccounts()),
-        t = i.useMemo(() => e.filter((e) => d.Z.isSupported(e.type)), [e]),
+function I() {
+    let e = (0, a.e7)([h.Z], () => h.Z.getAccounts()),
+        t = i.useMemo(() => e.filter((e) => u.Z.isSupported(e.type)), [e]),
         [n, o] = i.useState([]),
-        s = i.useCallback(
+        c = i.useCallback(
             (e) =>
-                c.Z.refresh(e.type, e.id).finally(() => {
+                l.Z.refresh(e.type, e.id).finally(() => {
                     o((t) => [...t, e.id]);
                 }),
             [],
         );
     return 0 === t.length
         ? null
-        : (0, r.jsxs)(l.hjN, {
-              className: O.container,
+        : (0, r.jsxs)(s.hjN, {
+              className: y.container,
               children: [
-                  (0, r.jsx)(l.vwX, {
-                      className: O.title,
-                      children: y.intl.string(y.t.aw0GVV),
+                  (0, r.jsx)(s.vwX, {
+                      className: y.title,
+                      children: b.intl.string(b.t.aw0GVV),
                   }),
                   t.map((e) =>
                       (0, r.jsx)(
-                          I,
+                          v,
                           {
                               account: e,
-                              handleRefresh: s,
+                              handleRefresh: c,
                               refreshedAccountIds: n,
                           },
                           e.id,
