f2123d748490a0db1d8bbb968137594e60ffeb55
DiscrapperManager <admin@minerpl.xyz>
2025-09-25 22:35:26 +0200

    canaryRelease: 449780
    Main scripts: 4
    Lazy Loaded Scripts: 1557
    Chunks:
     * Main Files: 14884
     * Scripts: 8903
    Strings: 22433
    Routes: 740
    Experiments Count: 441
    Apex Experiments Count: 78

--- chunks/462923.js
+++ chunks/462923.js
@@ -1,175 +1,153 @@
-t.d(n, { Z: () => C }), t(388685);
+t.d(n, { Z: () => h }), t(388685);
 var a = t(951288),
     r = t(647438),
-    i = t(120356),
-    s = t.n(i),
-    o = t(660815),
-    c = t(442837),
-    l = t(704215),
-    d = t(481060),
-    u = t(243778),
-    b = t(690221),
-    m = t(77498),
-    g = t(572004),
-    f = t(639777),
-    p = t(444083),
-    x = t(998964),
-    j = t(340887),
-    v = t(408629),
-    _ = t(921944),
-    h = t(440589),
-    O = t(388032),
-    y = t(799427);
-function C(e) {
+    i = t(442837),
+    o = t(704215),
+    s = t(481060),
+    c = t(243778),
+    l = t(690221),
+    d = t(77498),
+    u = t(572004),
+    b = t(639777),
+    m = t(444083),
+    g = t(998964),
+    f = t(340887),
+    x = t(408629),
+    p = t(921944),
+    j = t(440589),
+    v = t(388032),
+    _ = t(799427);
+function h(e) {
     let { guildId: n, instance: t } = e,
-        i = (0, x.Z)(t),
-        C = "Portkey",
-        [N, I] = (0, u.US)([l.z.GAME_SERVER_HOSTING_PORTKEY_TOS]),
-        P = N !== l.z.GAME_SERVER_HOSTING_PORTKEY_TOS,
-        w = (0, c.e7)([m.Z], () => m.Z.getDetectableGame(t.gameId)),
-        S = r.useCallback(() => {
-            P
-                ? (0, j.Z)(t)
-                : (0, v.Z)({
-                      provider: C,
+        h = (0, g.Z)(t),
+        y = "Portkey",
+        [O, C] = (0, c.US)([o.z.GAME_SERVER_HOSTING_PORTKEY_TOS]),
+        N = O !== o.z.GAME_SERVER_HOSTING_PORTKEY_TOS,
+        P = (0, i.e7)([d.Z], () => d.Z.getDetectableGame(t.gameId)),
+        I = r.useCallback(() => {
+            N
+                ? (0, f.Z)(t)
+                : (0, x.Z)({
+                      provider: y,
                       onAccept: () => {
-                          I(_.L.TAKE_ACTION), (0, j.Z)(t);
+                          C(p.L.TAKE_ACTION), (0, f.Z)(t);
                       },
                   });
-        }, [t, P, I]),
-        T = r.useCallback(() => {
-            (0, g.JG)(i), (0, d.showToast)((0, d.createToast)(O.intl.string(O.t["+5kSoa"]), d.ToastType.SUCCESS));
-        }, [i]),
-        E = (0, f.Z)(n);
+        }, [t, N, C]),
+        w = r.useCallback(() => {
+            (0, u.JG)(h), (0, s.showToast)((0, s.createToast)(v.intl.string(v.t["+5kSoa"]), s.ToastType.SUCCESS));
+        }, [h]),
+        S = (0, b.Z)(n);
     return (0, a.jsxs)("div", {
-        className: y.card,
+        className: _.card,
         children: [
             (0, a.jsxs)("div", {
-                className: y.serverHeader,
+                className: _.serverHeader,
                 children: [
                     (0, a.jsx)("img", {
                         src: "https://via.placeholder.com/80x80/4CAF50/FFFFFF?text=MC",
                         alt: "",
-                        className: y.gameImage,
+                        className: _.gameImage,
                     }),
                     (0, a.jsxs)("div", {
-                        className: y.serverInfo,
+                        className: _.serverInfo,
                         children: [
-                            (0, a.jsx)(d.X6q, {
+                            (0, a.jsx)(s.X6q, {
                                 variant: "heading-sm/medium",
                                 children: t.name,
                             }),
-                            (0, a.jsxs)(d.Text, {
+                            (0, a.jsxs)(s.Text, {
                                 variant: "text-sm/medium",
                                 color: "text-tertiary",
-                                children: [null != w ? "".concat(w.name, " \u2022 ") : "", t.planName],
+                                children: [null != P ? "".concat(P.name, " \u2022 ") : "", t.planName],
                             }),
                         ],
                     }),
                 ],
             }),
             (0, a.jsx)("div", {
-                className: y.serverDetails,
+                className: _.serverDetails,
                 children: (0, a.jsxs)("div", {
-                    className: y.detailsGrid,
+                    className: _.detailsGrid,
                     children: [
                         (0, a.jsxs)("div", {
-                            className: y.detailItem,
+                            className: _.detailItem,
                             children: [
-                                (0, a.jsx)(d.Text, {
+                                (0, a.jsx)(s.Text, {
                                     variant: "text-xs/semibold",
-                                    children: O.intl.string(h.default.X0IaiI),
+                                    children: v.intl.string(j.default.X0IaiI),
                                 }),
-                                (0, a.jsx)(d.Text, {
+                                (0, a.jsx)(s.Text, {
                                     variant: "text-sm/medium",
                                     children: "".concat(t.onlineConnectionsCount, " / ").concat(t.maxConnectionsCount),
                                 }),
                             ],
                         }),
                         (0, a.jsxs)("div", {
-                            className: y.detailItem,
+                            className: _.detailItem,
                             children: [
-                                (0, a.jsx)(d.Text, {
+                                (0, a.jsx)(s.Text, {
                                     variant: "text-xs/semibold",
-                                    children: O.intl.string(h.default["9pw/yM"]),
+                                    children: v.intl.string(j.default["9pw/yM"]),
                                 }),
                                 (0, a.jsx)("div", {
-                                    className: y.serverIpContainer,
-                                    children: P
+                                    className: _.serverIpContainer,
+                                    children: N
                                         ? (0, a.jsxs)(a.Fragment, {
                                               children: [
-                                                  (0, a.jsx)(d.Text, {
+                                                  (0, a.jsx)(s.Text, {
                                                       variant: "text-sm/medium",
-                                                      children: i,
+                                                      children: h,
                                                   }),
-                                                  (0, a.jsx)(d.P3F, {
-                                                      className: y.copyButton,
-                                                      "aria-label": O.intl.string(O.t.OpuAlJ),
-                                                      onClick: T,
-                                                      children: (0, a.jsx)(d.TIy, { size: "sm" }),
+                                                  (0, a.jsx)(s.P3F, {
+                                                      className: _.copyButton,
+                                                      "aria-label": v.intl.string(v.t.OpuAlJ),
+                                                      onClick: w,
+                                                      children: (0, a.jsx)(s.TIy, { size: "sm" }),
                                                   }),
                                               ],
                                           })
-                                        : (0, a.jsx)(b.Z, {
+                                        : (0, a.jsx)(l.Z, {
                                               onClick: () => {
-                                                  (0, v.Z)({
-                                                      provider: C,
+                                                  (0, x.Z)({
+                                                      provider: y,
                                                       onAccept: () => {
-                                                          I(_.L.TAKE_ACTION), (0, j.Z)(t);
+                                                          C(p.L.TAKE_ACTION), (0, f.Z)(t);
                                                       },
                                                   });
                                               },
-                                              children: O.intl.string(h.default.UwXK29),
+                                              children: v.intl.string(j.default.UwXK29),
                                           }),
                                 }),
                             ],
                         }),
                         (0, a.jsxs)("div", {
-                            className: y.detailItem,
+                            className: _.detailItem,
                             children: [
-                                (0, a.jsx)(d.Text, {
+                                (0, a.jsx)(s.Text, {
                                     variant: "text-xs/semibold",
-                                    children: O.intl.string(h.default.MQQIu7),
+                                    children: v.intl.string(j.default.MQQIu7),
                                 }),
                                 (0, a.jsxs)("div", {
-                                    className: y.statusContainer,
+                                    className: _.statusContainer,
                                     children: [
-                                        (0, a.jsx)("span", { className: s()(y.statusIndicator, y[t.status]) }),
-                                        (0, a.jsx)(d.Text, {
+                                        (0, a.jsx)("span", { className: _.statusIndicator }),
+                                        (0, a.jsx)(s.Text, {
                                             variant: "text-sm/medium",
-                                            children: (function (e) {
-                                                switch (e) {
-                                                    case o.V.STARTING:
-                                                        return O.intl.string(h.default["7XF8pK"]);
-                                                    case o.V.STARTUP_FAILED:
-                                                        return O.intl.string(h.default["C/096+"]);
-                                                    case o.V.OFFLINE:
-                                                        return O.intl.string(h.default.Bz9gcH);
-                                                    case o.V.ONLINE:
-                                                        return O.intl.string(h.default["7q4iGR"]);
-                                                    case o.V.DELETED:
-                                                        return O.intl.string(h.default["UvP/1N"]);
-                                                    case o.V.MISSING_STOCK:
-                                                        return O.intl.string(h.default.ssC3cX);
-                                                    case o.V.SLEEPING:
-                                                        return O.intl.string(h.default.PS8AMT);
-                                                    default:
-                                                        return O.intl.string(h.default.Bz9gcH);
-                                                }
-                                            })(t.status),
+                                            children: t.status,
                                         }),
                                     ],
                                 }),
                             ],
                         }),
                         (0, a.jsxs)("div", {
-                            className: y.detailItem,
+                            className: _.detailItem,
                             children: [
-                                (0, a.jsx)(d.Text, {
+                                (0, a.jsx)(s.Text, {
                                     variant: "text-xs/semibold",
-                                    children: O.intl.string(h.default.fqrxlJ),
+                                    children: v.intl.string(j.default.fqrxlJ),
                                 }),
-                                (0, a.jsx)(d.Text, {
+                                (0, a.jsx)(s.Text, {
                                     variant: "text-sm/medium",
                                     children: t.regionName,
                                 }),
@@ -179,19 +157,19 @@ function C(e) {
                 }),
             }),
             (0, a.jsxs)("div", {
-                className: y.serverActions,
+                className: _.serverActions,
                 children: [
-                    (0, a.jsx)(d.zxk, {
+                    (0, a.jsx)(s.zxk, {
                         fullWidth: !0,
-                        text: O.intl.string(h.default["0TMXHh"]),
-                        onClick: S,
+                        text: v.intl.string(j.default["0TMXHh"]),
+                        onClick: I,
                         variant: "primary",
                     }),
-                    E &&
+                    S &&
                         null != t.gameServerPanelUrl &&
-                        (0, a.jsx)(d.zxk, {
+                        (0, a.jsx)(s.zxk, {
                             fullWidth: !0,
-                            text: O.intl.string(h.default["jO3u+/"]),
+                            text: v.intl.string(j.default["jO3u+/"]),
                             onClick: () => {
                                 window.open(t.gameServerPanelUrl, "_blank");
                             },
@@ -200,8 +178,8 @@ function C(e) {
                 ],
             }),
             (0, a.jsx)("div", {
-                className: y.overflowMenu,
-                children: (0, a.jsx)(p.Z, {
+                className: _.overflowMenu,
+                children: (0, a.jsx)(m.Z, {
                     guildId: n,
                     instance: t,
                 }),
