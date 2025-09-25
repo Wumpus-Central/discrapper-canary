ee77839eeb2f54b16543144febe1643584ab96db
DiscrapperManager <admin@minerpl.xyz>
2025-09-26 00:58:57 +0200

    canaryRelease: 449965
    Main scripts: 4
    Lazy Loaded Scripts: 1556
    Chunks:
     * Main Files: 14861
     * Scripts: 8894
    Strings: 22316
    Routes: 739
    Experiments Count: 438
    Apex Experiments Count: 78

--- chunks/749210.js
+++ chunks/749210.js
@@ -536,9 +536,18 @@ let F = {
     },
     nsfwReturnToSafety(e) {
         let t = (0, c._I)(e);
-        if (null == e || t) return void (0, h.uL)(N.Z5c.FRIENDS);
+        if (null == e || t)
+            return void (0, h.uL)(N.Z5c.FRIENDS, {
+                navigationReplace: !1,
+                openChannel: !0,
+            });
         let n = E.ZP.getDefaultChannel(e);
-        null == n || (0, c.Y3)(n) ? (0, h.uL)(N.Z5c.FRIENDS) : (0, h.uL)(N.Z5c.CHANNEL(e, n.id));
+        null == n || (0, c.Y3)(n)
+            ? (0, h.uL)(N.Z5c.FRIENDS, {
+                  navigationReplace: !1,
+                  openChannel: !0,
+              })
+            : (0, h.uL)(N.Z5c.CHANNEL(e, n.id));
     },
     escapeToDefaultChannel(e) {
         let t = E.ZP.getDefaultChannel(e);
