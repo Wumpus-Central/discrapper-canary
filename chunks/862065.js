1a1975fef2d637da91996a091f6cf69fe4f2c9f6
DiscrapperManager <admin@minerpl.xyz>
2025-09-26 00:10:26 +0200

    canaryRelease: 449910
    Main scripts: 4
    Lazy Loaded Scripts: 1557
    Chunks:
     * Main Files: 14882
     * Scripts: 8903
    Strings: 22437
    Routes: 740
    Experiments Count: 439
    Apex Experiments Count: 78

--- chunks/862065.js
+++ chunks/862065.js
@@ -1,13 +1,14 @@
-n.d(t, { default: () => l });
+n.d(t, { default: () => u }), n(388685), n(457542);
 var r = n(951288),
-    o = n(625651),
-    a = n(744993),
-    i = n(401561),
-    c = n(388032),
-    s = n(775488);
-function l(e) {
+    o = n(647438),
+    a = n(625651),
+    i = n(744993),
+    c = n(440589),
+    s = n(388032),
+    l = n(593997);
+function u(e) {
     var { guildId: t, gameInstance: n } = e,
-        l = (function (e, t) {
+        u = (function (e, t) {
             if (null == e) return {};
             var n,
                 r,
@@ -28,8 +29,10 @@ function l(e) {
             }
             return o;
         })(e, ["guildId", "gameInstance"]);
+    let [d, f] = o.useState(!1),
+        [m, b] = o.useState(void 0);
     return (0, r.jsx)(
-        o.f,
+        a.f,
         (function (e) {
             for (var t = 1; t < arguments.length; t++) {
                 var n = null != arguments[t] ? arguments[t] : {},
@@ -59,22 +62,33 @@ function l(e) {
                 size: "sm",
                 title: n.name,
                 body: (0, r.jsx)("div", {
-                    className: s.imageContainer,
+                    className: l.imageContainer,
                     children: (0, r.jsx)("img", {
                         alt: "",
                         src: "",
-                        className: s.image,
+                        className: l.image,
                     }),
                 }),
-                warningText: c.intl.string(i.default.oHNuWl),
-                isLoading: !1,
+                warningText: s.intl.string(c.default.oHNuWl),
+                isLoading: d,
+                error: m,
                 onDeactivate: () => {
-                    (0, a.s9)(t, n.planId, n.entitlementId).then(() => {
-                        l.onClose();
-                    });
+                    b(void 0),
+                        f(!0),
+                        (0, i.s9)(t, n.planId, n.entitlementId)
+                            .then(() => {
+                                u.onClose();
+                            })
+                            .catch((e) => {
+                                var t;
+                                b(null != (t = e.body.message) ? t : e.message);
+                            })
+                            .finally(() => {
+                                f(!1);
+                            });
                 },
             },
-            l,
+            u,
         ),
     );
 }
