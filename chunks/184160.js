319c2f2dafdb387e71d1f376e4c0b177aec5a1c0
DiscrapperManager <admin@minerpl.xyz>
2025-09-25 02:26:59 +0200

    canaryRelease: 449377
    Main scripts: 4
    Lazy Loaded Scripts: 1557
    Chunks:
     * Main Files: 14890
     * Scripts: 8900
    Strings: 22431
    Routes: 740
    Experiments Count: 443
    Apex Experiments Count: 76

--- chunks/184160.js
+++ chunks/184160.js
@@ -1,58 +1,60 @@
-n.d(t, { x: () => f });
+n.d(t, { x: () => m });
 var r = n(951288);
 n(647438);
 var i = n(512722),
-    l = n.n(i),
-    s = n(780384),
-    a = n(755721),
-    o = n(410030),
+    a = n.n(i),
+    o = n(780384),
+    l = n(755721),
+    s = n(410030),
     C = n(807163),
     d = n(74538),
     c = n(987209),
-    p = n(563132),
-    u = n(409813),
-    x = n(614223),
-    h = n(388032),
-    _ = n(172463);
-function f(e) {
+    u = n(563132),
+    p = n(409813),
+    _ = n(614223),
+    f = n(388032),
+    h = n(172463);
+function m(e) {
     let { handleStepChange: t, handleClose: n } = e,
-        i = (0, o.ZP)(),
+        i = (0, s.ZP)(),
         {
-            activeSubscription: f,
-            selectedSkuId: j,
-            setSelectedPlanId: m,
-            startedPaymentFlowWithPaymentSourcesRef: L,
-        } = (0, p.JL)(),
-        { isGift: g } = (0, c.wD)(),
-        b = null != f ? (0, d.Af)(f) : null,
-        y = null != b ? (0, d.gq)(b.planId) : null,
-        S = null != b ? (0, d.Rd)(b.planId) : null,
-        E = (0, x.Kp)({
+            activeSubscription: m,
+            selectedSkuId: x,
+            setSelectedPlanId: j,
+            startedPaymentFlowWithPaymentSourcesRef: g,
+        } = (0, u.JL)(),
+        { isGift: b } = (0, c.wD)(),
+        L = null != m ? (0, d.Af)(m) : null,
+        y = null != L ? (0, d.gq)(L.planId) : null,
+        v = null != L ? (0, d.Rd)(L.planId) : null,
+        S = (0, _.Kp)({
             isTrial: !1,
-            isGift: g,
-            selectedSkuId: j,
-            startedPaymentFlowWithPaymentSources: L.current,
+            isGift: b,
+            selectedSkuId: x,
+            startedPaymentFlowWithPaymentSources: g.current,
         });
     return (
-        l()(null != S, "Expected premium type"),
+        a()(null != v, "Expected premium type"),
         (0, r.jsx)(C.Z, {
-            premiumType: S,
-            titleText: h.intl.string(h.t["7VcWW1"]),
-            subtitleText: h.intl.format(h.t.Qk34Ii, { subscriptionName: y }),
+            premiumType: v,
+            titleText: f.intl.string(f.t["7VcWW1"]),
+            subtitleText: f.intl.format(f.t.Qk34Ii, { subscriptionName: y }),
             footer: (0, r.jsxs)("div", {
-                className: _.whatYouLoseButtonContainer,
+                className: h.whatYouLoseButtonContainer,
                 children: [
-                    (0, r.jsx)(a.zx, {
+                    (0, r.jsx)(l.zx, {
+                        "data-migration-pending": !0,
                         onClick: () => {
-                            E ? (m((0, x.nA)(j, f)), t(u.h8.REVIEW)) : t(u.h8.PLAN_SELECT);
+                            S ? (j((0, _.nA)(x, m)), t(p.h8.REVIEW)) : t(p.h8.PLAN_SELECT);
                         },
-                        children: h.intl.string(h.t["3PatS0"]),
+                        children: f.intl.string(f.t["3PatS0"]),
                     }),
-                    (0, r.jsx)(a.zx, {
-                        look: a.zx.Looks.LINK,
-                        color: (0, s.wj)(i) ? a.zx.Colors.WHITE : a.zx.Colors.PRIMARY,
+                    (0, r.jsx)(l.zx, {
+                        "data-migration-pending": !0,
+                        look: l.zx.Looks.LINK,
+                        color: (0, o.wj)(i) ? l.zx.Colors.WHITE : l.zx.Colors.PRIMARY,
                         onClick: n,
-                        children: h.intl.string(h.t.h9tkAA),
+                        children: f.intl.string(f.t.h9tkAA),
                     }),
                 ],
             }),
