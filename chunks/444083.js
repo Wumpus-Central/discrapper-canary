ff2e7cca1ae7ce8d259503d7a5a16900d350b2ed
DiscrapperManager <admin@minerpl.xyz>
2025-09-30 00:42:28 +0200

    canaryRelease: 451107
    Main scripts: 4
    Lazy Loaded Scripts: 1565
    Chunks:
     * Main Files: 14783
     * Scripts: 9000
    Strings: 22372
    Routes: 744
    Experiments Count: 436
    Apex Experiments Count: 82

--- chunks/444083.js
+++ chunks/444083.js
@@ -1,121 +1,124 @@
-n.d(t, { Z: () => b });
-var r = n(951288),
-    s = n(647438),
-    a = n(481060),
-    i = n(239091),
-    o = n(100527),
-    c = n(809741),
-    l = n(473682),
-    u = n(401561),
-    d = n(388032),
-    m = n(587983);
-let p = {
+t.d(n, { Z: () => f });
+var a = t(951288),
+    r = t(647438),
+    i = t(481060),
+    s = t(239091),
+    o = t(100527),
+    l = t(639777),
+    c = t(809741),
+    d = t(473682),
+    u = t(440589),
+    b = t(388032),
+    m = t(412247);
+let g = {
     steps: {
-        [l.Vb.SERVER_SETTINGS]: {
+        [d.Vb.SERVER_SETTINGS]: {
             onBack: { type: "close" },
             onNext: { type: "save" },
         },
     },
 };
-function b(e) {
-    let { guildId: t, instance: n } = e,
-        l = s.useRef(null);
-    return (0, r.jsx)(a.yRy, {
-        targetElementRef: l,
+function f(e) {
+    let { guildId: n, instance: t } = e,
+        d = r.useRef(null),
+        f = (0, l.Z)(n);
+    return (0, a.jsx)(i.yRy, {
+        targetElementRef: d,
         align: "top",
         position: "right",
         animationPosition: "bottom",
         disablePointerEvents: !1,
         renderPopout: (e) => {
-            let { closePopout: s } = e;
-            return (0, r.jsx)(a.v2r, {
+            let { closePopout: r } = e;
+            return (0, a.jsx)(i.v2r, {
                 navId: "portkey-popout-context-menu",
                 onClose: () => {
-                    (0, i.Zy)(), s();
+                    (0, s.Zy)(), r();
                 },
-                "aria-label": d.intl.string(u.default.xCq1DA),
+                "aria-label": b.intl.string(u.default.xCq1DA),
                 onSelect: void 0,
-                children: (0, r.jsxs)(a.kSQ, {
+                children: (0, a.jsxs)(i.kSQ, {
                     children: [
-                        (0, r.jsx)(a.sNh, {
+                        (0, a.jsx)(i.sNh, {
                             id: "get-support",
-                            icon: a.kBi,
-                            label: d.intl.string(u.default["TciV8/"]),
+                            icon: i.kBi,
+                            label: b.intl.string(u.default["TciV8/"]),
                             action: () => {},
                         }),
-                        (0, r.jsx)(a.sNh, {
-                            id: "settings",
-                            icon: a.ewm,
-                            label: d.intl.string(u.default.NUTl4u),
-                            action: () => {
-                                (0, c.Z)({
-                                    guildId: t,
-                                    stepConfig: p,
-                                    initialPortkeyInstance: n,
-                                    analyticsLocation: o.Z.PORTKEY_OVERVIEW,
-                                });
-                            },
-                        }),
+                        f &&
+                            (0, a.jsx)(i.sNh, {
+                                id: "settings",
+                                icon: i.ewm,
+                                label: b.intl.string(u.default.NUTl4u),
+                                action: () => {
+                                    (0, c.Z)({
+                                        guildId: n,
+                                        stepConfig: g,
+                                        initialPortkeyInstance: t,
+                                        analyticsLocation: o.Z.PORTKEY_OVERVIEW,
+                                    });
+                                },
+                            }),
                     ],
                 }),
             });
         },
         children: (e) => {
-            var t, n;
-            return (0, r.jsx)(
-                a.P3F,
-                ((t = (function (e) {
-                    for (var t = 1; t < arguments.length; t++) {
-                        var n = null != arguments[t] ? arguments[t] : {},
-                            r = Object.keys(n);
+            var n, t;
+            return (0, a.jsx)(
+                i.P3F,
+                ((n = (function (e) {
+                    for (var n = 1; n < arguments.length; n++) {
+                        var t = null != arguments[n] ? arguments[n] : {},
+                            a = Object.keys(t);
                         "function" == typeof Object.getOwnPropertySymbols &&
-                            (r = r.concat(
-                                Object.getOwnPropertySymbols(n).filter(function (e) {
-                                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                            (a = a.concat(
+                                Object.getOwnPropertySymbols(t).filter(function (e) {
+                                    return Object.getOwnPropertyDescriptor(t, e).enumerable;
                                 }),
                             )),
-                            r.forEach(function (t) {
-                                var r;
-                                (r = n[t]),
-                                    t in e
-                                        ? Object.defineProperty(e, t, {
-                                              value: r,
+                            a.forEach(function (n) {
+                                var a;
+                                (a = t[n]),
+                                    n in e
+                                        ? Object.defineProperty(e, n, {
+                                              value: a,
                                               enumerable: !0,
                                               configurable: !0,
                                               writable: !0,
                                           })
-                                        : (e[t] = r);
+                                        : (e[n] = a);
                             });
                     }
                     return e;
                 })(
                     {
-                        "aria-label": d.intl.string(d.t.UKOtz8),
+                        "aria-label": b.intl.string(b.t.UKOtz8),
                         className: m.clickable,
                     },
                     e,
                 )),
-                (n = n =
+                (t = t =
                     {
-                        innerRef: l,
-                        children: (0, r.jsx)(a.xhG, {
+                        innerRef: d,
+                        children: (0, a.jsx)(i.xhG, {
                             color: "currentColor",
                             size: "sm",
                         }),
                     }),
                 Object.getOwnPropertyDescriptors
-                    ? Object.defineProperties(t, Object.getOwnPropertyDescriptors(n))
-                    : (function (e, t) {
-                          var n = Object.keys(e);
+                    ? Object.defineProperties(n, Object.getOwnPropertyDescriptors(t))
+                    : (function (e, n) {
+                          var t = Object.keys(e);
                           if (Object.getOwnPropertySymbols) {
-                              var r = Object.getOwnPropertySymbols(e);
-                              n.push.apply(n, r);
+                              var a = Object.getOwnPropertySymbols(e);
+                              t.push.apply(t, a);
                           }
-                          return n;
-                      })(Object(n)).forEach(function (e) {
-                          Object.defineProperty(t, e, Object.getOwnPropertyDescriptor(n, e));
+                          return t;
+                      })(Object(t)).forEach(function (e) {
+                          Object.defineProperty(n, e, Object.getOwnPropertyDescriptor(t, e));
                       }),
-                t),
+                n),
             );
         },
     });
