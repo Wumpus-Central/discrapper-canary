3759cc41c9efb5ec875791ee76287403f251c0fe
DiscrapperManager <admin@minerpl.xyz>
2025-09-29 22:23:24 +0200

    canaryRelease: 450984
    Main scripts: 4
    Lazy Loaded Scripts: 1565
    Chunks:
     * Main Files: 14785
     * Scripts: 8994
    Strings: 22356
    Routes: 744
    Experiments Count: 436
    Apex Experiments Count: 82

--- chunks/462991.js
+++
@@ -1,1 +0,0 @@
-t.exports = "/assets/ce3bc1396de73afe.svg";
--- chunks/951269.js
+++ chunks/951269.js
@@ -1,56 +1,20 @@
-s.d(e, { default: () => d });
-var n = s(951288);
-s(647438);
-var a = s(755721),
-    i = s(481060),
-    l = s(313201),
-    o = s(388032),
-    c = s(727047),
-    r = s(462991);
-function d(t) {
-    let { onClose: e, transitionState: s, kind: d } = t,
-        u = (0, l.Dt)();
-    return (0, n.jsxs)(i.Y0X, {
-        transitionState: s,
-        "aria-labelledby": u,
-        size: i.CgR.SMALL,
-        parentComponent: "InaccessibleLinkModal",
-        children: [
-            (0, n.jsxs)(i.hzk, {
-                className: c.content,
-                children: [
-                    (0, n.jsx)(i.olH, {
-                        className: c.closeButton,
-                        onClick: e,
-                    }),
-                    (0, n.jsx)("img", {
-                        className: c.image,
-                        src: r,
-                        alt: "",
-                    }),
-                    (0, n.jsx)(i.X6q, {
-                        id: u,
-                        variant: "heading-xl/bold",
-                        color: "header-primary",
-                        children: o.intl.string(o.t.r0DLNj),
-                    }),
-                    (0, n.jsx)(i.Text, {
-                        className: c.subtitle,
-                        variant: "text-md/normal",
-                        color: "header-secondary",
-                        children: "user" === d ? o.intl.string(o.t.Fqqbho) : o.intl.string(o.t["6Y0JlJ"]),
-                    }),
-                ],
-            }),
-            (0, n.jsx)(i.mzw, {
-                children: (0, n.jsx)(a.zx, {
-                    className: c.button,
-                    color: a.zx.Colors.BRAND,
-                    onClick: e,
-                    size: a.zx.Sizes.LARGE,
-                    children: o.intl.string(o.t.BddRzc),
-                }),
-            }),
+n.d(i, { default: () => l });
+var s = n(951288);
+n(647438);
+var r = n(82659),
+    e = n(388032);
+function l(t) {
+    let { onClose: i, transitionState: n, kind: l } = t;
+    return (0, s.jsx)(r.Modal, {
+        title: e.intl.string(e.t.r0DLNj),
+        subtitle: "user" === l ? e.intl.string(e.t.Fqqbho) : e.intl.string(e.t["6Y0JlJ"]),
+        actions: [
+            {
+                text: e.intl.string(e.t.BddRzc),
+                onClick: i,
+            },
         ],
+        onClose: i,
+        transitionState: n,
     });
 }
