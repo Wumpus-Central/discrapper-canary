cd21bcd586d1b28f3e9c4cb28f8b6700afbff463
DiscrapperManager <admin@minerpl.xyz>
2025-09-30 23:15:26 +0200

    canaryRelease: 451642
    Main scripts: 4
    Lazy Loaded Scripts: 1566
    Chunks:
     * Main Files: 14793
     * Scripts: 9011
    Strings: 22390
    Routes: 745
    Experiments Count: 433
    Apex Experiments Count: 83

--- chunks/520977.js
+++ chunks/520977.js
@@ -1,135 +1,109 @@
-r.d(t, { default: () => O });
+r.d(e, { default: () => O });
 var n = r(951288),
-    i = r(647438),
-    o = r(442837),
-    c = r(481060),
-    l = r(313201),
-    s = r(237997),
-    a = r(451478),
+    o = r(647438),
+    i = r(442837),
+    c = r(82659),
+    l = r(237997),
+    s = r(451478),
     u = r(358085),
-    p = r(998502),
-    d = r(145597),
-    f = r(981631),
-    b = r(388032);
-function O(e) {
-    var t,
+    a = r(998502),
+    f = r(145597),
+    b = r(981631),
+    p = r(388032);
+function O(t) {
+    var e,
         r,
-        { transitionState: O, onClose: y, contextKey: j } = e,
-        h = (function (e, t) {
-            if (null == e) return {};
+        { transitionState: O, onClose: y, contextKey: d } = t,
+        g = (function (t, e) {
+            if (null == t) return {};
             var r,
                 n,
-                i = (function (e, t) {
-                    if (null == e) return {};
+                o = (function (t, e) {
+                    if (null == t) return {};
                     var r,
                         n,
-                        i = {},
-                        o = Object.keys(e);
-                    for (n = 0; n < o.length; n++) (r = o[n]), t.indexOf(r) >= 0 || (i[r] = e[r]);
-                    return i;
-                })(e, t);
+                        o = {},
+                        i = Object.keys(t);
+                    for (n = 0; n < i.length; n++) (r = i[n]), e.indexOf(r) >= 0 || (o[r] = t[r]);
+                    return o;
+                })(t, e);
             if (Object.getOwnPropertySymbols) {
-                var o = Object.getOwnPropertySymbols(e);
-                for (n = 0; n < o.length; n++)
-                    (r = o[n]),
-                        !(t.indexOf(r) >= 0) && Object.prototype.propertyIsEnumerable.call(e, r) && (i[r] = e[r]);
+                var i = Object.getOwnPropertySymbols(t);
+                for (n = 0; n < i.length; n++)
+                    (r = i[n]),
+                        !(e.indexOf(r) >= 0) && Object.prototype.propertyIsEnumerable.call(t, r) && (o[r] = t[r]);
             }
-            return i;
-        })(e, ["transitionState", "onClose", "contextKey"]);
-    let g = (0, l.Dt)(),
-        x = (0, o.e7)([a.Z], () => a.Z.isFocused()),
-        P = j === f.IlC.APP,
-        m = (0, d.getPID)(),
-        v = (0, o.e7)([s.default], () => s.default.isLocked(m));
-    return (i.useEffect(() => {
-        (x || P || v) && (null == y || y());
-    }, [x, y, P, v]),
-    x || v)
+            return o;
+        })(t, ["transitionState", "onClose", "contextKey"]);
+    let j = (0, i.e7)([s.Z], () => s.Z.isFocused()),
+        P = d === b.IlC.APP,
+        w = (0, f.getPID)(),
+        v = (0, i.e7)([l.default], () => l.default.isLocked(w));
+    return (o.useEffect(() => {
+        (j || P || v) && (null == y || y());
+    }, [j, y, P, v]),
+    j || v)
         ? null
-        : (0, n.jsxs)(
-              c.Y0X,
-              ((t = (function (e) {
-                  for (var t = 1; t < arguments.length; t++) {
-                      var r = null != arguments[t] ? arguments[t] : {},
+        : (0, n.jsx)(
+              c.Modal,
+              ((e = (function (t) {
+                  for (var e = 1; e < arguments.length; e++) {
+                      var r = null != arguments[e] ? arguments[e] : {},
                           n = Object.keys(r);
                       "function" == typeof Object.getOwnPropertySymbols &&
                           (n = n.concat(
-                              Object.getOwnPropertySymbols(r).filter(function (e) {
-                                  return Object.getOwnPropertyDescriptor(r, e).enumerable;
+                              Object.getOwnPropertySymbols(r).filter(function (t) {
+                                  return Object.getOwnPropertyDescriptor(r, t).enumerable;
                               }),
                           )),
-                          n.forEach(function (t) {
+                          n.forEach(function (e) {
                               var n;
-                              (n = r[t]),
-                                  t in e
-                                      ? Object.defineProperty(e, t, {
+                              (n = r[e]),
+                                  e in t
+                                      ? Object.defineProperty(t, e, {
                                             value: n,
                                             enumerable: !0,
                                             configurable: !0,
                                             writable: !0,
                                         })
-                                      : (e[t] = n);
+                                      : (t[e] = n);
                           });
                   }
-                  return e;
-              })(
-                  {
-                      transitionState: O,
-                      "aria-labelledby": g,
-                  },
-                  h,
-              )),
+                  return t;
+              })({}, g)),
               (r = r =
                   {
-                      size: c.CgR.SMALL,
-                      parentComponent: "FocusMainAppModal",
-                      children: [
-                          (0, n.jsx)(c.xBx, {
-                              children: (0, n.jsx)(c.X6q, {
-                                  variant: "heading-xl/semibold",
-                                  children: b.intl.string(b.t.DJ8ojI),
-                              }),
-                          }),
-                          (0, n.jsx)(c.hzk, {
-                              children: (0, n.jsx)(c.Text, {
-                                  color: "text-default",
-                                  variant: "text-md/medium",
-                                  children: b.intl.string(b.t["E+Ph7O"]),
-                              }),
-                          }),
-                          (0, n.jsx)(c.mzw, {
-                              children: (0, n.jsxs)(c.hE2, {
-                                  direction: "horizontal-reverse",
-                                  children: [
-                                      (0, n.jsx)(c.zxk, {
-                                          variant: "primary",
-                                          text: b.intl.string(b.t.ELRJQk),
-                                          onClick: () => {
-                                              u.isPlatformEmbedded ? p.ZP.focus() : window.focus(), y();
-                                          },
-                                      }),
-                                      (0, n.jsx)(c.zxk, {
-                                          variant: "secondary",
-                                          text: b.intl.string(b.t.FgK5QE),
-                                          onClick: () => (null == y ? void 0 : y()),
-                                      }),
-                                  ],
-                              }),
-                          }),
+                      title: p.intl.string(p.t.DJ8ojI),
+                      subtitle: p.intl.string(p.t["E+Ph7O"]),
+                      actions: [
+                          {
+                              variant: "secondary",
+                              text: p.intl.string(p.t.FgK5QE),
+                              onClick: () => (null == y ? void 0 : y()),
+                          },
+                          {
+                              variant: "primary",
+                              text: p.intl.string(p.t.ELRJQk),
+                              onClick: () => {
+                                  u.isPlatformEmbedded ? a.ZP.focus() : window.focus(), y();
+                              },
+                          },
                       ],
+                      onClose: y,
+                      transitionState: O,
                   }),
               Object.getOwnPropertyDescriptors
-                  ? Object.defineProperties(t, Object.getOwnPropertyDescriptors(r))
-                  : (function (e, t) {
-                        var r = Object.keys(e);
+                  ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(r))
+                  : (function (t, e) {
+                        var r = Object.keys(t);
                         if (Object.getOwnPropertySymbols) {
-                            var n = Object.getOwnPropertySymbols(e);
+                            var n = Object.getOwnPropertySymbols(t);
                             r.push.apply(r, n);
                         }
                         return r;
-                    })(Object(r)).forEach(function (e) {
-                        Object.defineProperty(t, e, Object.getOwnPropertyDescriptor(r, e));
+                    })(Object(r)).forEach(function (t) {
+                        Object.defineProperty(e, t, Object.getOwnPropertyDescriptor(r, t));
                     }),
-              t),
+              e),
           );
 }
