bfd7630f1e4df729230990c865fa9c4a6daa74f1
DiscrapperManager <admin@minerpl.xyz>
2025-10-02 01:48:47 +0200

    canaryRelease: 452442
    Main scripts: 4
    Lazy Loaded Scripts: 1611
    Chunks:
     * Main Files: 14798
     * Scripts: 9061
    Strings: 22401
    Routes: 745
    Experiments Count: 431
    Apex Experiments Count: 85

--- chunks/305945.js
+++ chunks/305945.js
@@ -1,4 +1,4 @@
-n.d(t, { T: () => A }), n(388685);
+n.d(t, { T: () => R }), n(388685);
 var r = n(951288),
     i = n(647438),
     a = n(120356),
@@ -9,8 +9,9 @@ var r = n(951288),
     u = n(673824),
     d = n(213305),
     f = n(155127),
-    _ = n(406751);
-function p(e, t, n) {
+    _ = n(753187),
+    p = n(659780);
+function h(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -23,7 +24,7 @@ function p(e, t, n) {
         e
     );
 }
-function h(e) {
+function m(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -34,12 +35,12 @@ function h(e) {
                 }),
             )),
             r.forEach(function (t) {
-                p(e, t, n[t]);
+                h(e, t, n[t]);
             });
     }
     return e;
 }
-function m(e, t) {
+function g(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -51,33 +52,54 @@ function m(e, t) {
     }
     return n;
 }
-function g(e, t) {
+function E(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : m(Object(t)).forEach(function (n) {
+            : g(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
     );
 }
-let E = {
+function b(e, t) {
+    if (null == e) return {};
+    var n,
+        r,
+        i = y(e, t);
+    if (Object.getOwnPropertySymbols) {
+        var a = Object.getOwnPropertySymbols(e);
+        for (r = 0; r < a.length; r++)
+            (n = a[r]), !(t.indexOf(n) >= 0) && Object.prototype.propertyIsEnumerable.call(e, n) && (i[n] = e[n]);
+    }
+    return i;
+}
+function y(e, t) {
+    if (null == e) return {};
+    var n,
+        r,
+        i = {},
+        a = Object.keys(e);
+    for (r = 0; r < a.length; r++) (n = a[r]), t.indexOf(n) >= 0 || (i[n] = e[n]);
+    return i;
+}
+let O = {
         mass: 1,
         tension: 250,
     },
-    b = "M5.13231 6.72963L6.7233 5.13864L14.855 13.2704L13.264 14.8614L5.13231 6.72963Z",
-    y = "M13.2704 5.13864L14.8614 6.72963L6.72963 14.8614L5.13864 13.2704L13.2704 5.13864Z",
-    O = "M6.56666 11.0013L6.56666 8.96683L13.5667 8.96683L13.5667 11.0013L6.56666 11.0013Z",
-    v = "M13.5582 8.96683L13.5582 11.0013L6.56192 11.0013L6.56192 8.96683L13.5582 8.96683Z",
-    I = "M7.89561 14.8538L6.30462 13.2629L14.3099 5.25755L15.9009 6.84854L7.89561 14.8538Z",
-    T = "M4.08643 11.0903L5.67742 9.49929L9.4485 13.2704L7.85751 14.8614L4.08643 11.0903Z";
-function S(e, t, n, i) {
+    v = "M5.13231 6.72963L6.7233 5.13864L14.855 13.2704L13.264 14.8614L5.13231 6.72963Z",
+    I = "M13.2704 5.13864L14.8614 6.72963L6.72963 14.8614L5.13864 13.2704L13.2704 5.13864Z",
+    T = "M6.56666 11.0013L6.56666 8.96683L13.5667 8.96683L13.5667 11.0013L6.56666 11.0013Z",
+    S = "M13.5582 8.96683L13.5582 11.0013L6.56192 11.0013L6.56192 8.96683L13.5582 8.96683Z",
+    A = "M7.89561 14.8538L6.30462 13.2629L14.3099 5.25755L15.9009 6.84854L7.89561 14.8538Z",
+    C = "M4.08643 11.0903L5.67742 9.49929L9.4485 13.2704L7.85751 14.8614L4.08643 11.0903Z";
+function N(e, t, n, i) {
     let a = e.to({
             output: [t, n],
         }),
-        o = i ? [b, b, I, I] : [b, O, O, I],
-        l = i ? [y, y, T, T] : [y, v, v, T];
+        o = i ? [v, v, A, A] : [v, T, T, A],
+        l = i ? [I, I, C, C] : [I, S, S, C];
     return (0, r.jsxs)("svg", {
         viewBox: "0 0 20 20",
         fill: "none",
@@ -99,103 +121,113 @@ function S(e, t, n, i) {
         ],
     });
 }
-function A(e) {
-    let { id: t, onChange: n, checked: a, disabled: p, focusProps: m, innerRef: b } = e,
-        { reducedMotion: y } = i.useContext(d.S),
-        O = i.useRef(null),
-        [v, I] = i.useState(!1),
-        T = (0, u.d)(l.Z.colors.INTERACTIVE_MUTED).spring(),
-        A = (0, u.d)(l.Z.colors.BG_BRAND).spring(),
-        { state: C, opacity: N } = (0, c.q)(
+function R(e) {
+    let { onChange: t, checked: n, disabled: a, focusProps: h, innerRef: g } = e,
+        y = b(e, ["onChange", "checked", "disabled", "focusProps", "innerRef"]),
+        { reducedMotion: v } = i.useContext(d.S),
+        I = i.useRef(null),
+        [T, S] = i.useState(!1),
+        A = (0, u.d)(l.Z.colors.INTERACTIVE_MUTED).spring(),
+        C = (0, u.d)(l.Z.colors.BG_BRAND).spring(),
+        { state: R } = (0, c.q)(
             {
-                config: E,
-                opacity: p ? 0.5 : 1,
-                state: v ? (a ? 0.7 : 0.3) : +!!a,
+                config: O,
+                state: T ? (n ? 0.7 : 0.3) : +!!n,
             },
             "animate-always",
         );
-    function R(e) {
-        I(!1), e.stopPropagation(), null == n || n(e.currentTarget.checked);
-    }
     function P(e) {
-        p || e.repeat || ((" " === e.key || "Enter" === e.key) && I(!0));
+        S(!1), e.stopPropagation(), null == t || t(e.currentTarget.checked);
     }
     function w(e) {
+        a || e.repeat || ((" " === e.key || "Enter" === e.key) && S(!0));
+    }
+    function D(e) {
         var t;
-        p || !v || e.repeat || (I(!1), "Enter" === e.key && (null == (t = O.current) || t.click()));
+        a || !T || e.repeat || (S(!1), "Enter" === e.key && (null == (t = I.current) || t.click()));
     }
     return (0, r.jsx)(
-        f.tE,
-        g(h({}, m), {
-            within: !0,
-            offset: -2,
-            children: (0, r.jsxs)(s.animated.div, {
-                "data-toggleable-component": "switch",
-                className: o()(_.container, {
-                    [_.checked]: a,
-                    [_.disabled]: p,
-                }),
-                onMouseDown: () => !p && I(!0),
-                onMouseUp: () => I(!1),
-                onMouseLeave: () => I(!1),
-                style: {
-                    opacity: N,
-                    backgroundColor: C.to({
-                        output: [T, A],
-                    }),
-                },
-                children: [
-                    (0, r.jsxs)(s.animated.svg, {
-                        className: _.slider,
-                        viewBox: "0 0 28 20",
-                        preserveAspectRatio: "xMinYMid meet",
-                        style: {
-                            left: C.to({
-                                range: [0, 0.3, 0.7, 1],
-                                output: [-4, 1, 8, 12],
+        _.N,
+        E(m({}, y), {
+            disabled: a,
+            layout: "horizontal",
+            children: (e) =>
+                (0, r.jsx)(
+                    f.tE,
+                    E(m({}, h), {
+                        within: !0,
+                        offset: -2,
+                        children: (0, r.jsxs)(s.animated.div, {
+                            "data-toggleable-component": "switch",
+                            className: o()(p.container, {
+                                [p.checked]: n,
+                                [p.disabled]: a,
                             }),
-                        },
-                        "aria-hidden": !0,
-                        children: [
-                            (0, r.jsx)(s.animated.rect, {
-                                fill: "white",
-                                x: C.to({
-                                    range: [0, 0.3, 0.7, 1],
-                                    output: [4, 0, 0, 4],
+                            onMouseDown: () => !a && S(!0),
+                            onMouseUp: () => S(!1),
+                            onMouseLeave: () => S(!1),
+                            style: {
+                                backgroundColor: R.to({
+                                    output: [A, C],
                                 }),
-                                y: C.to({
-                                    range: [0, 0.3, 0.7, 1],
-                                    output: [0, 1, 1, 0],
+                            },
+                            children: [
+                                (0, r.jsxs)(s.animated.svg, {
+                                    className: p.slider,
+                                    viewBox: "0 0 28 20",
+                                    preserveAspectRatio: "xMinYMid meet",
+                                    style: {
+                                        left: R.to({
+                                            range: [0, 0.3, 0.7, 1],
+                                            output: [-4, 1, 8, 12],
+                                        }),
+                                    },
+                                    "aria-hidden": !0,
+                                    children: [
+                                        (0, r.jsx)(s.animated.rect, {
+                                            fill: "white",
+                                            x: R.to({
+                                                range: [0, 0.3, 0.7, 1],
+                                                output: [4, 0, 0, 4],
+                                            }),
+                                            y: R.to({
+                                                range: [0, 0.3, 0.7, 1],
+                                                output: [0, 1, 1, 0],
+                                            }),
+                                            height: R.to({
+                                                range: [0, 0.3, 0.7, 1],
+                                                output: [20, 18, 18, 20],
+                                            }),
+                                            width: R.to({
+                                                range: [0, 0.3, 0.7, 1],
+                                                output: [20, 28, 28, 20],
+                                            }),
+                                            rx: "10",
+                                        }),
+                                        N(R, A, C, v.enabled),
+                                    ],
                                 }),
-                                height: C.to({
-                                    range: [0, 0.3, 0.7, 1],
-                                    output: [20, 18, 18, 20],
+                                (0, r.jsx)("input", {
+                                    id: e.controlId,
+                                    "aria-describedby": e.describedById,
+                                    "aria-errormessage": e.errorMessageId,
+                                    "aria-invalid": null != e.errorMessageId,
+                                    type: "checkbox",
+                                    ref: (e) => {
+                                        (I.current = e), null != g && (g.current = e);
+                                    },
+                                    className: p.input,
+                                    tabIndex: a ? -1 : 0,
+                                    onKeyDown: w,
+                                    onKeyUp: D,
+                                    onChange: P,
+                                    checked: n,
+                                    disabled: a,
                                 }),
-                                width: C.to({
-                                    range: [0, 0.3, 0.7, 1],
-                                    output: [20, 28, 28, 20],
-                                }),
-                                rx: "10",
-                            }),
-                            S(C, T, A, y.enabled),
-                        ],
+                            ],
+                        }),
                     }),
-                    (0, r.jsx)("input", {
-                        id: t,
-                        type: "checkbox",
-                        ref: (e) => {
-                            (O.current = e), null != b && (b.current = e);
-                        },
-                        className: _.input,
-                        tabIndex: p ? -1 : 0,
-                        onKeyDown: P,
-                        onKeyUp: w,
-                        onChange: R,
-                        checked: a,
-                        disabled: p,
-                    }),
-                ],
-            }),
+                ),
         }),
     );
 }
