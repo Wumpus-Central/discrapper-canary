a6df9ad1cef8ecd0573213ce19ee043ee3bf4f2b
DiscrapperManager <admin@minerpl.xyz>
2025-10-01 22:07:10 +0200

    canaryRelease: 452211
    Main scripts: 4
    Lazy Loaded Scripts: 1610
    Chunks:
     * Main Files: 14791
     * Scripts: 9056
    Strings: 22392
    Routes: 745
    Experiments Count: 433
    Apex Experiments Count: 82

--- chunks/826011.js
+++ chunks/826011.js
@@ -1,66 +1,83 @@
-s.d(t, { default: () => h });
-var r = s(951288),
-    n = s(647438),
-    a = s(442837),
-    i = s(667202),
-    l = s(481060),
-    c = s(572004),
-    o = s(744993),
-    d = s(693587),
-    p = s(943970),
-    u = s(998964),
-    m = s(401561),
-    x = s(388032),
-    v = s(927607);
-function h(e) {
-    let { guildId: t, instance: s, onClose: h, transitionState: f } = e,
-        I = (0, a.e7)([d.Z], () => {
+s.d(t, { default: () => I });
+var n = s(951288),
+    r = s(647438),
+    i = s(660815),
+    a = s(793030),
+    l = s(442837),
+    c = s(481060),
+    o = s(494620),
+    d = s(572004),
+    p = s(744993),
+    u = s(693587),
+    m = s(943970),
+    x = s(998964),
+    f = s(440589),
+    h = s(388032),
+    v = s(999852);
+function I(e) {
+    let { guildId: t, instance: s, onClose: I, transitionState: j } = e,
+        C = (0, l.e7)([u.Z], () => {
             var e;
-            return null == (e = d.Z.getStateForGuild(t)) ? void 0 : e.instructions[s.planId];
+            return null == (e = u.Z.getStateForGuild(t)) ? void 0 : e.instructions[s.planId];
         });
-    n.useEffect(() => {
-        null == I && (0, o.iX)(t, s.planId);
-    }, [t, s.planId, I]);
-    let j = (0, u.Z)(s),
-        C = n.useCallback(() => {
-            (0, c.JG)(j), (0, l.showToast)((0, l.createToast)(x.intl.string(x.t["+5kSoa"]), l.ToastType.SUCCESS));
-        }, [j]),
-        N = (0, p.Z)(s.gameId, "cover");
-    return (0, r.jsx)(i.ExpressiveModal, {
+    r.useEffect(() => {
+        null == C && (0, p.iX)(t, s.planId);
+    }, [t, s.planId, C]);
+    let N = (0, x.Z)(s),
+        g = r.useCallback(() => {
+            (0, d.JG)(N), (0, c.showToast)((0, c.createToast)(h.intl.string(h.t["+5kSoa"]), c.ToastType.SUCCESS));
+        }, [N]),
+        T = (0, m.Z)(s.gameId, "cover"),
+        _ = r.useMemo(() => {
+            switch (s.status) {
+                case i.V.STARTING:
+                    return h.intl.string(f.default["1XVBDQ"]);
+                case i.V.OFFLINE:
+                    return h.intl.string(f.default.m4inWl);
+                default:
+                    return;
+            }
+        }, [s.status]);
+    return (0, n.jsx)(a.ExpressiveModal, {
         graphic: {
-            src: N,
+            src: T,
             type: "image",
         },
         title: s.name,
-        subtitle: x.intl.string(m.default["8Fgoo6"]),
+        subtitle: h.intl.string(f.default["8Fgoo6"]),
         size: "md",
-        transitionState: f,
-        onClose: h,
+        transitionState: j,
+        onClose: I,
         children:
-            null == I
-                ? (0, r.jsx)(l.$jN, {})
-                : (0, r.jsxs)("div", {
+            null == C
+                ? (0, n.jsx)(c.$jN, {})
+                : (0, n.jsxs)("div", {
                       className: v.content,
                       children: [
-                          (0, r.jsx)("div", {
+                          null != _ &&
+                              (0, n.jsx)(o.Z, {
+                                  className: v.infoBox,
+                                  children: _,
+                              }),
+                          (0, n.jsx)("div", {
                               className: v.stepsContainer,
-                              children: I.map((e, t) =>
-                                  (0, r.jsxs)(
+                              children: C.map((e, t) =>
+                                  (0, n.jsxs)(
                                       "div",
                                       {
                                           className: v.step,
                                           children: [
-                                              (0, r.jsx)("div", {
+                                              (0, n.jsx)("div", {
                                                   className: v.stepNumber,
-                                                  children: (0, r.jsx)(l.Text, {
+                                                  children: (0, n.jsx)(c.Text, {
                                                       variant: "text-sm/semibold",
                                                       color: "text-secondary",
-                                                      children: x.intl.formatToPlainString(m.default.e0RxEB, {
+                                                      children: h.intl.formatToPlainString(f.default.e0RxEB, {
                                                           step: t + 1,
                                                       }),
                                                   }),
                                               }),
-                                              (0, r.jsx)(l.Text, {
+                                              (0, n.jsx)(c.Text, {
                                                   variant: "text-md/normal",
                                                   color: "header-primary",
                                                   children: e,
@@ -71,22 +88,22 @@ function h(e) {
                                   ),
                               ),
                           }),
-                          (0, r.jsxs)("div", {
+                          (0, n.jsxs)("div", {
                               className: v.serverIpContainer,
                               children: [
-                                  (0, r.jsx)("div", {
+                                  (0, n.jsx)("div", {
                                       className: v.serverIpInput,
-                                      children: (0, r.jsx)(l.Text, {
+                                      children: (0, n.jsx)(c.Text, {
                                           variant: "text-md/medium",
                                           color: "header-primary",
-                                          children: j,
+                                          children: N,
                                       }),
                                   }),
-                                  (0, r.jsx)(l.zxk, {
+                                  (0, n.jsx)(c.zxk, {
                                       variant: "primary",
-                                      text: x.intl.string(x.t.OpuAlJ),
-                                      onClick: C,
-                                      icon: l.TIy,
+                                      text: h.intl.string(h.t.OpuAlJ),
+                                      onClick: g,
+                                      icon: c.TIy,
                                   }),
                               ],
                           }),
--- chunks/999852.js
+++ chunks/999852.js
@@ -1,5 +1,6 @@
 e.exports = {
     content: "content_f2e68c",
+    infoBox: "infoBox_f2e68c",
     stepsContainer: "stepsContainer_f2e68c",
     step: "step_f2e68c",
     stepNumber: "stepNumber_f2e68c",
