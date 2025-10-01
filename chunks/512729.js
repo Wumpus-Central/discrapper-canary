098bf424874f5045c9f8721be26b7e51e94712e7
DiscrapperManager <admin@minerpl.xyz>
2025-10-02 00:34:19 +0200

    canaryRelease: 452366
    Main scripts: 4
    Lazy Loaded Scripts: 1611
    Chunks:
     * Main Files: 14796
     * Scripts: 9059
    Strings: 22398
    Routes: 745
    Experiments Count: 432
    Apex Experiments Count: 82

--- chunks/512729.js
+++ chunks/512729.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => p }), n(413496), n(433524), n(35282), n(421244), n(388685);
+n.d(t, { Z: () => p }), n(413496), n(433524), n(35282), n(421244), n(388685), n(539854);
 var r = n(392711),
     i = n(327432),
     a = n(657871),
@@ -7,21 +7,22 @@ var r = n(392711),
     l = n(887490);
 let c = "line",
     u = [],
-    d = RegExp("\\b(?<!@|/)(\\w{2,})\\b[".concat((0, r.escapeRegExp)(o.x), "]*"), "g"),
+    d = RegExp("(?<prefix>^|\\s+)(?<word>\\w{2,})[".concat((0, r.escapeRegExp)(o.x), "]*"), "g"),
     f = /\s/;
-function _(e, t, n) {
-    return e
-        .matchAll(d)
-        .map((e) => ({
-            start: e.index,
-            end: e.index + e[1].length,
-            trailingEnd: e.index + e[0].length,
-            results: (0, o.r)(t, e[1], n).results,
-        }))
-        .filter((e) => {
-            let { results: t } = e;
-            return t.suggestions.length > 0;
-        });
+function* _(e, t, n) {
+    for (let i of e.matchAll(d)) {
+        var r;
+        if (null == i.groups) continue;
+        let { prefix: e, word: a } = i.groups,
+            { results: s } = (0, o.r)(t, a, n);
+        if (s.suggestions.length < 1) continue;
+        let l = null != (r = null == e ? void 0 : e.length) ? r : 0;
+        yield {
+            start: i.index + l,
+            end: i.index + l + a.length,
+            results: s,
+        };
+    }
 }
 function p(e, t, n, r) {
     var o;
@@ -42,34 +43,22 @@ function p(e, t, n, r) {
     if (!l.aj.isType(E, c) || !i.e6.includes(l.bN.range(e, g), h.anchor)) return u;
     let b = h.anchor.offset;
     for (; b > 0 && !f.test(m.text[b - 1]); ) b--;
-    return [
-        ..._(m.text.substring(b), n, r)
-            .filter((e) =>
-                i.e6.includes(
-                    {
-                        anchor: {
-                            path: g,
-                            offset: b + e.start,
-                        },
-                        focus: {
-                            path: g,
-                            offset: b + e.trailingEnd,
-                        },
-                    },
-                    h.anchor,
-                ),
-            )
-            .map((e) => ({
-                anchor: {
-                    path: g,
-                    offset: b + e.start,
-                },
-                focus: {
-                    path: g,
-                    offset: b + e.end,
-                },
-                mentionSuggestion: !0,
-                mentionSuggestionSimpleColorVariant: "simple" === p,
-            })),
-    ];
+    let y = h.anchor.offset;
+    for (; y < m.text.length && !f.test(m.text[y]); ) y++;
+    let O = m.text.substring(b, y),
+        v = [];
+    for (let e of _(O, n, r))
+        v.push({
+            anchor: {
+                path: g,
+                offset: b + e.start,
+            },
+            focus: {
+                path: g,
+                offset: b + e.end,
+            },
+            mentionSuggestion: !0,
+            mentionSuggestionSimpleColorVariant: "simple" === p,
+        });
+    return v;
 }
