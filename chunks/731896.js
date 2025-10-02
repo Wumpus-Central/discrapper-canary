fa3aff303093110b419531841d9bd075477b8745
DiscrapperManager <admin@minerpl.xyz>
2025-10-02 23:51:05 +0200

    canaryRelease: 452935
    Main scripts: 4
    Lazy Loaded Scripts: 1613
    Chunks:
     * Main Files: 14801
     * Scripts: 9065
    Strings: 22429
    Routes: 745
    Experiments Count: 431
    Apex Experiments Count: 86

--- chunks/731896.js
+++ chunks/731896.js
@@ -9,35 +9,31 @@ var r = n(647438),
     u = n(365943),
     d = n(25251);
 let f = (e) => {
-    var t;
-    let n = (0, i.e7)([o.Z], () => {
+    let t = (0, i.e7)([d.Z], () => d.Z.getProfileEffect(e)),
+        n = (0, i.e7)([o.Z, s.Z], () => {
             if (null == e) return;
             let t = o.Z.getProduct(e);
-            if ((0, c.H)(null == t ? void 0 : t.items[0]))
-                return {
-                    id: t.items[0].id,
-                    skuId: t.items[0].skuId,
-                    config: t.items[0],
-                };
+            if ((0, c.H)(null == t ? void 0 : t.items[0])) return t.items[0];
+            let n = s.Z.getPurchase(e);
+            if ((0, c.H)(null == n ? void 0 : n.items[0])) return n.items[0];
         }),
-        f = (0, i.e7)([s.Z], () => {
-            if (null == e) return;
-            let t = s.Z.getPurchase(e);
-            if ((0, c.H)(null == t ? void 0 : t.items[0]))
-                return {
-                    id: t.items[0].id,
-                    skuId: t.items[0].skuId,
-                    config: t.items[0],
-                };
-        }),
-        _ = (0, i.e7)([d.Z], () => d.Z.getProfileEffect(e)),
-        p = null != (t = null != n ? n : f) ? t : _,
-        h = null != e && null == p,
-        m = (0, l.F)("useProfileEffectPreset");
+        f = r.useMemo(
+            () =>
+                null != n
+                    ? {
+                          id: n.id,
+                          skuId: n.skuId,
+                          config: n,
+                      }
+                    : t,
+            [n, t],
+        ),
+        _ = null != e && null == f,
+        p = (0, l.F)("useProfileEffectPreset");
     return (
         r.useEffect(() => {
-            h && (m ? (0, a.lW)(e) : (0, u.t)(!0));
-        }, [h, e, m]),
-        p
+            _ && (p ? (0, a.lW)(e) : (0, u.t)(!0));
+        }, [_, e, p]),
+        f
     );
 };
