04e2799607d01db574cf837b35e9277427c8e617
DiscrapperManager <admin@minerpl.xyz>
2025-10-02 19:40:38 +0200

    canaryRelease: 452673
    Main scripts: 4
    Lazy Loaded Scripts: 1612
    Chunks:
     * Main Files: 14796
     * Scripts: 9062
    Strings: 22409
    Routes: 745
    Experiments Count: 430
    Apex Experiments Count: 86

--- chunks/396445.js
+++ chunks/396445.js
@@ -1,14 +1,15 @@
 n.d(t, {
-    J7: () => y,
-    SS: () => L,
-    oc: () => T,
-    rC: () => j,
+    J7: () => L,
+    NT: () => I,
+    SS: () => j,
+    oc: () => y,
+    rC: () => k,
 }),
     n(388685);
 var r = n(951288),
     l = n(647438),
-    i = n(979554),
-    a = n(685816),
+    a = n(979554),
+    i = n(685816),
     s = n(399606),
     o = n(100527),
     c = n(906732),
@@ -17,8 +18,8 @@ var r = n(951288),
     g = n(597688),
     p = n(1870),
     f = n(501678),
-    _ = n(994587),
-    C = n(429368),
+    C = n(994587),
+    _ = n(429368),
     h = n(237031),
     m = n(453713),
     b = n(201964),
@@ -26,8 +27,9 @@ var r = n(951288),
     v = n(956472),
     S = n(642909),
     x = n(215023),
-    O = n(981631);
-let T = (e, t, n, r) => {
+    O = n(981631),
+    T = n(388032);
+let y = (e, t, n, r) => {
         u.default.track(O.rMx.COLLECTIBLES_SHOP_ELEMENT_CLICKED, {
             collectibles_shop_session_id: null == n ? void 0 : n.sessionId,
             sku_id: e.skuId,
@@ -36,68 +38,68 @@ let T = (e, t, n, r) => {
             page_category: t === x.AW.HOME || null == n ? void 0 : n.pageCategory,
             page_index: t === x.AW.CATALOG ? (null == n ? void 0 : n.pageIndex) : void 0,
             page_size: t === x.AW.CATALOG ? (null == n ? void 0 : n.pageSize) : void 0,
-            tile_type: i.Z[e.type],
+            tile_type: a.Z[e.type],
             tile_position: String(null == n ? void 0 : n.tilePosition),
             cta_name: r,
         });
     },
-    y = (e, t) => {
-        let n = (0, _.Nd)(),
+    L = (e, t) => {
+        let n = (0, C.Nd)(),
             r = null == n ? void 0 : n.tab,
-            i = (0, f.Vc)(),
-            u = null == i ? void 0 : i.blockType,
+            a = (0, f.Vc)(),
+            u = null == a ? void 0 : a.blockType,
             p = (0, d.sp)(),
-            { analyticsLocations: C } = (0, c.ZP)(o.Z.COLLECTIBLES_SHOP_CARD),
+            { analyticsLocations: _ } = (0, c.ZP)(o.Z.COLLECTIBLES_SHOP_CARD),
             m = (0, s.e7)([g.Z], () => g.Z.getCategoryForProduct(e.skuId)),
             b = l.useRef(null),
             E = (0, S.G)("useHandlePreviewClick"),
-            O = null != u && u === a.z.HERO && E ? x.Ch.THUMBNAIL : x.Ch.DEFAULT;
+            O = null != u && u === i.z.HERO && E ? x.Ch.THUMBNAIL : x.Ch.DEFAULT;
         return l.useCallback(
             (t) => (n) => {
                 if (null == m) return;
                 let l = (0, v.oQ)({ product: e });
                 (b.current = n.currentTarget),
-                    T(e, r, p, null),
+                    y(e, r, p, null),
                     (0, h.T)({
                         product: e,
                         category: m,
                         shouldCheckoutWithOrbs: l,
-                        analyticsLocations: C,
+                        analyticsLocations: _,
                         analyticsSource: t,
                         returnRef: b,
                         tab: r,
                         variantType: O,
                     });
             },
-            [e, r, m, C, p, O],
+            [e, r, m, _, p, O],
         )(t);
     },
-    L = (e) => {
-        let { previewingVariantIndex: t, handleEntering: n, handleLeaving: a } = (0, E.f)(e),
+    j = (e) => {
+        let { previewingVariantIndex: t, handleEntering: n, handleLeaving: i } = (0, E.f)(e),
             o = (0, s.e7)([p.Z], () => p.Z.purchases),
-            c = (0, C.o)(e, o),
+            c = (0, _.o)(e, o),
             u = (0, b.o)(e, o, t),
             [d, g] = l.useState(!1);
-        if (e.type === i.Z.VARIANTS_GROUP) {
+        if (e.type === a.Z.VARIANTS_GROUP) {
             let l = (0, r.jsx)(m.P, {
                 variantGroupProduct: e,
                 previewingVariantIndexProps: {
                     previewingVariantIndex: t,
                     handleEntering: n,
-                    handleLeaving: a,
+                    handleLeaving: i,
                 },
                 selectedVariantIndex: c,
                 setIsHoveringOnSwitch: g,
                 minimal: !0,
-                alternativeBackgroundColor: (null == u ? void 0 : u.type) === i.Z.PROFILE_EFFECT,
+                alternativeBackgroundColor: (null == u ? void 0 : u.type) === a.Z.PROFILE_EFFECT,
             });
             return [c, l];
         }
         return [c, null];
     },
-    j = (e) => {
+    k = (e) => {
         if (null == e) return null;
-        if (e.type === i.Z.VARIANTS_GROUP) {
+        if (e.type === a.Z.VARIANTS_GROUP) {
             if (null == e.variants || 0 === e.variants.length) return null;
             let t = e.variants[0];
             if (null == t) return null;
@@ -105,4 +107,16 @@ let T = (e, t, n, r) => {
             return n.type;
         }
         return e.type;
+    },
+    I = (e) => {
+        switch (k(e)) {
+            case a.Z.AVATAR_DECORATION:
+                return T.intl.formatToPlainString(T.t.lvBzLi, { product: e.name });
+            case a.Z.PROFILE_EFFECT:
+                return T.intl.formatToPlainString(T.t.eR7moK, { product: e.name });
+            case a.Z.NAMEPLATE:
+                return T.intl.formatToPlainString(T.t.YFOwHh, { product: e.name });
+            default:
+                return e.name;
+        }
     };
--- chunks/844035.js
+++ chunks/844035.js
@@ -1,8 +1,8 @@
 n.d(t, { Z: () => E });
 var r = n(951288),
     l = n(647438),
-    i = n(120356),
-    a = n.n(i),
+    a = n(120356),
+    i = n.n(a),
     s = n(622535),
     o = n(481060),
     c = n(100527),
@@ -11,36 +11,38 @@ var r = n(951288),
     g = n(993222),
     p = n(932083),
     f = n(719075),
-    h = n(558261),
-    C = n(819882),
-    _ = n(786040),
+    C = n(558261),
+    _ = n(311583),
+    h = n(396445),
     m = n(215023),
-    b = n(217726);
+    b = n(751677);
 let E = (e) => {
-    let { product: t, variantSwitcher: n, selectedVariantIndex: i } = e,
+    let { product: t, variantSwitcher: n, selectedVariantIndex: a } = e,
         E = (0, u.Nd)(),
-        S = null == E ? void 0 : E.tab,
-        v = l.useRef(null),
-        { handleCardVisibilityChange: x } = (0, d.E)(t.skuId, S === m.AW.CATALOG ? "full" : S),
-        O = (0, _.J7)(t, c.Z.COLLECTIBLES_SHOP_CARD);
+        v = null == E ? void 0 : E.tab,
+        S = l.useRef(null),
+        { handleCardVisibilityChange: x } = (0, d.E)(t.skuId, v === m.AW.CATALOG ? "full" : v),
+        O = (0, h.J7)(t, c.Z.COLLECTIBLES_SHOP_CARD);
     return (0, r.jsx)(s.$, {
-        innerRef: v,
+        innerRef: S,
         onChange: x,
         threshold: 0,
-        children: (0, r.jsx)(o.tEY, {
+        children: (0, r.jsx)("div", {
+            className: b.baseProductCardHoverContainer,
             children: (0, r.jsx)(o.kL8, {
-                ref: v,
+                ref: S,
                 onClick: O,
-                "aria-label": t.name,
+                "aria-label": (0, h.NT)(t),
+                className: b.baseProductCardContainer,
                 children: (0, r.jsxs)("div", {
-                    className: b.baseProductCardContainer,
+                    className: b.baseProductCardContentContainer,
                     children: [
                         (0, r.jsx)(g.Z, { product: t }),
                         (0, r.jsxs)("div", {
                             className: b.innerContainer,
                             children: [
                                 (0, r.jsxs)("div", {
-                                    className: a()(b.baseProductCardDetailsContainer, {
+                                    className: i()(b.baseProductCardDetailsContainer, {
                                         [b.hasVariantSwitcher]: null != n,
                                     }),
                                     children: [
@@ -64,15 +66,15 @@ let E = (e) => {
                                 wrap: !1,
                                 fullWidth: !0,
                                 children: [
-                                    (0, r.jsx)(h.Z, {
-                                        product: t,
-                                        cardRef: v,
-                                        selectedVariantIndex: i,
-                                    }),
                                     (0, r.jsx)(C.Z, {
                                         product: t,
-                                        cardRef: v,
-                                        selectedVariantIndex: i,
+                                        cardRef: S,
+                                        selectedVariantIndex: a,
+                                    }),
+                                    (0, r.jsx)(_.Z, {
+                                        product: t,
+                                        cardRef: S,
+                                        selectedVariantIndex: a,
                                     }),
                                 ],
                             }),
