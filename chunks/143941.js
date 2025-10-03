c7c11d661434984fab25a02ea4d1d6d664b93a6c
DiscrapperManager <admin@minerpl.xyz>
2025-10-03 20:08:40 +0200

    canaryRelease: 453340
    Main scripts: 4
    Lazy Loaded Scripts: 1611
    Chunks:
     * Main Files: 14831
     * Scripts: 9069
    Strings: 22445
    Routes: 747
    Experiments Count: 430
    Apex Experiments Count: 86

--- chunks/143941.js
+++ chunks/143941.js
@@ -1,4 +1,4 @@
-r.d(t, { a: () => E }), r(388685);
+r.d(t, { a: () => x }), r(388685);
 var n = r(951288),
     l = r(647438),
     i = r(120356),
@@ -13,11 +13,11 @@ var n = r(951288),
     b = r(277511),
     y = r(602733),
     O = r(594174),
-    v = r(884697),
-    g = r(724994),
-    m = r(921944),
-    j = r(388032),
-    h = r(820657);
+    m = r(884697),
+    j = r(724994),
+    g = r(921944),
+    v = r(388032),
+    h = r(890088);
 function P(e) {
     for (var t = 1; t < arguments.length; t++) {
         var r = null != arguments[t] ? arguments[t] : {},
@@ -43,7 +43,7 @@ function P(e) {
     }
     return e;
 }
-function x(e, t) {
+function E(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
@@ -61,88 +61,88 @@ function x(e, t) {
         e
     );
 }
-let E = (e) => {
+let x = (e) => {
     let {
             product: t,
             iconSize: i = 20,
-            className: E,
+            className: x,
             enableHoverEffect: _ = !1,
-            isCardHovered: w = !0,
-            selectedVariantIndex: C,
+            isCardHovered: C = !0,
+            selectedVariantIndex: w,
         } = e,
         [S, A] = (0, p.US)([s.z.WISHLIST_NUX_TOOLTIP_AND_MODAL], void 0, !0),
-        N = S === s.z.WISHLIST_NUX_TOOLTIP_AND_MODAL,
-        { analyticsLocations: I } = (0, d.ZP)(),
-        T = O.default.getCurrentUser(),
-        L = null != T ? f.Z.getFirstWishlistId(T.id) : null,
-        k = l.useMemo(() => {
+        T = S === s.z.WISHLIST_NUX_TOOLTIP_AND_MODAL,
+        { analyticsLocations: N } = (0, d.ZP)(),
+        I = O.default.getCurrentUser(),
+        D = null != I ? f.Z.getFirstWishlistId(I.id) : null,
+        L = l.useMemo(() => {
             var e;
-            return t.type === o.Z.VARIANTS_GROUP && null != C && (null == (e = t.variants) ? void 0 : e[C]) != null
-                ? t.variants[C]
+            return t.type === o.Z.VARIANTS_GROUP && null != w && (null == (e = t.variants) ? void 0 : e[w]) != null
+                ? t.variants[w]
                 : t;
-        }, [t, C]),
-        D = k.skuId,
-        R = (0, y.n)(L, D),
+        }, [t, w]),
+        k = L.skuId,
+        R = (0, y.n)(D, k),
         M = l.useRef(null),
         [F, U] = l.useState(null);
     l.useEffect(() => {
         U(null);
-    }, [D]);
+    }, [k]);
     let Z = null !== F ? F : R,
         B = (0, u.Z)(M),
-        V = Z ? c.h_8 : c.Pzh,
-        Y = Z || B ? h.wishlistedOrHoveredIconColor : h.normalIconColor,
-        { isPurchased: W } = (0, g.L)(k),
-        G = (0, v.fp)(k),
-        z = (0, v.x6)(k),
-        H = (0, v.G1)(k),
+        Y = Z ? c.h_8 : c.Pzh,
+        W = Z || B ? h.wishlistedOrHoveredIconColor : h.normalIconColor,
+        { isPurchased: V } = (0, j.L)(L),
+        z = (0, m.fp)(L),
+        G = (0, m.x6)(L),
+        H = (0, m.G1)(L),
         K = l.useCallback(
             async (e) => {
-                if ((e.stopPropagation(), e.currentTarget.blur(), Z && null != L)) {
+                if ((e.stopPropagation(), e.currentTarget.blur(), Z && null != D)) {
                     U(!1);
                     try {
-                        await b.Z.removeSkuFromWishlist(L, D, I), U(null);
+                        await b.Z.removeSkuFromWishlist(D, k, N), U(null);
                     } catch (e) {
                         U(null),
-                            (0, c.showToast)((0, c.createToast)(j.intl.string(j.t.F8FvU1), c.ToastType.FAILURE)),
-                            c.uvj.announce(j.intl.string(j.t.F8FvU1));
+                            (0, c.showToast)((0, c.createToast)(v.intl.string(v.t.F8FvU1), c.ToastType.FAILURE)),
+                            c.uvj.announce(v.intl.string(v.t.F8FvU1));
                     }
                 } else {
                     U(!0);
                     try {
-                        await b.Z.addSkuToWishlist(D, I),
+                        await b.Z.addSkuToWishlist(k, N),
                             U(null),
-                            N &&
+                            T &&
                                 ((0, c.ZDy)(async () => {
                                     let { default: e } = await r.e("36340").then(r.bind(r, 874533));
-                                    return (r) => (0, n.jsx)(e, x(P({}, r), { product: t }));
+                                    return (r) => (0, n.jsx)(e, E(P({}, r), { product: t }));
                                 }),
-                                A(m.L.USER_DISMISS));
+                                A(g.L.USER_DISMISS));
                     } catch (e) {
                         U(null),
-                            (0, c.showToast)((0, c.createToast)(j.intl.string(j.t.F8FvU1), c.ToastType.FAILURE)),
-                            c.uvj.announce(j.intl.string(j.t.F8FvU1));
+                            (0, c.showToast)((0, c.createToast)(v.intl.string(v.t.F8FvU1), c.ToastType.FAILURE)),
+                            c.uvj.announce(v.intl.string(v.t.F8FvU1));
                     }
                 }
             },
-            [I, Z, D, t, N, A, L, U],
+            [N, Z, k, t, T, A, D, U],
         );
-    if ((!w && !Z) || null == T) return null;
-    if (G || z || H || W) {
-        let e = j.intl.string(j.t["50TX9v"]);
+    if ((!C && !Z) || null == I || V) return null;
+    if (z || G || H) {
+        let e = v.intl.string(v.t["50TX9v"]);
         return (
-            z ? (e = j.intl.string(j.t.UfDp3N)) : G && (e = j.intl.string(j.t.KsFBMj)),
+            G ? (e = v.intl.string(v.t.UfDp3N)) : z && (e = v.intl.string(v.t.KsFBMj)),
             (0, n.jsx)(c.ua7, {
                 text: e,
                 children: (e) =>
                     (0, n.jsx)(
                         c.P3F,
-                        x(P({}, e), {
-                            className: a()(h.wishlistButton, h.disabledButton, E),
+                        E(P({}, e), {
+                            className: a()(h.wishlistButton, h.disabledButton, x),
                             innerRef: M,
                             "aria-disabled": !0,
                             onClick: (e) => e.stopPropagation(),
-                            children: (0, n.jsx)(V, {
+                            children: (0, n.jsx)(Y, {
                                 colorClass: h.disabledIconColor,
                                 size: "custom",
                                 height: i,
@@ -153,34 +153,34 @@ let E = (e) => {
             })
         );
     }
-    let X = Z ? j.intl.string(j.t.yr9TTU) : j.intl.string(j.t["8DkMER"]),
-        J = N
+    let X = Z ? v.intl.string(v.t.yr9TTU) : v.intl.string(v.t["8DkMER"]),
+        q = T
             ? (0, n.jsxs)(n.Fragment, {
                   children: [
                       (0, n.jsx)(c.Text, {
                           variant: "text-sm/semibold",
-                          children: j.intl.string(j.t["47Rhc3"]),
+                          children: v.intl.string(v.t["47Rhc3"]),
                       }),
                       (0, n.jsx)(c.Text, {
                           variant: "text-sm/medium",
-                          children: j.intl.string(j.t.PXjA0d),
+                          children: v.intl.string(v.t.PXjA0d),
                       }),
                   ],
               })
             : X;
     return (0, n.jsx)(c.ua7, {
-        text: J,
+        text: q,
         "aria-label": X,
         children: (e) =>
             (0, n.jsx)(
                 c.P3F,
-                x(P({}, e), {
-                    className: a()(h.wishlistButton, _ && h.withHover, E),
+                E(P({}, e), {
+                    className: a()(h.wishlistButton, _ && h.withHover, x),
                     innerRef: M,
                     onClick: K,
                     "aria-label": X,
-                    children: (0, n.jsx)(V, {
-                        colorClass: Y,
+                    children: (0, n.jsx)(Y, {
+                        colorClass: W,
                         size: "custom",
                         height: i,
                         width: i,
