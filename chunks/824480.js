7677b66e7f95b99450da185ca581e6008d373da5
DiscrapperManager <admin@minerpl.xyz>
2025-10-04 01:46:26 +0200

    canaryRelease: 453706
    Main scripts: 4
    Lazy Loaded Scripts: 1611
    Chunks:
     * Main Files: 14828
     * Scripts: 9072
    Strings: 22449
    Routes: 748
    Experiments Count: 432
    Apex Experiments Count: 84

--- chunks/824480.js
+++ chunks/824480.js
@@ -1,20 +1,22 @@
-t.d(e, { d: () => d });
-var n = t(647438),
-    r = t(442837),
-    s = t(232567),
-    l = t(515970),
-    a = t(5888);
-function d() {
-    let i = (0, r.e7)([a.Z], () => a.Z.getFetchState()),
-        { sidekicks: e } = (0, r.e7)([a.Z], () => a.Z.getCheckpointData());
-    (0, n.useEffect)(() => {
-        i !== a.p.FETCHING && i !== a.p.SUCCESS && (0, l.p)();
-    }, [i]),
-        (0, n.useEffect)(() => {
-            null != e &&
-                e.forEach((i) => {
-                    let { userId: e } = i;
-                    return (0, s.PR)(e);
+r.d(i, { d: () => c });
+var e = r(647438),
+    n = r(442837),
+    a = r(232567),
+    s = r(516373),
+    l = r(515970),
+    d = r(5888);
+function c() {
+    let t = (0, n.e7)([d.Z], () => d.Z.getFetchState()),
+        { sidekicks: i } = (0, n.e7)([d.Z], () => d.Z.getCheckpointData()),
+        r = (0, s.D)();
+    (0, e.useEffect)(() => {
+        t !== d.p.FETCHING && t !== d.p.SUCCESS && null != r && (0, l.p)();
+    }, [t, r]),
+        (0, e.useEffect)(() => {
+            null != i &&
+                i.forEach((t) => {
+                    let { userId: i } = t;
+                    return (0, a.PR)(i);
                 });
-        }, [e]);
+        }, [i]);
 }
