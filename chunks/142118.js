a0507f6c5925b9414fdc67cf0230b0610146437a
DiscrapperManager <admin@minerpl.xyz>
2025-10-06 23:42:34 +0200

    canaryRelease: 454154
    Main scripts: 4
    Lazy Loaded Scripts: 1612
    Chunks:
     * Main Files: 14831
     * Scripts: 9075
    Strings: 22468
    Routes: 748
    Experiments Count: 430
    Apex Experiments Count: 86

--- chunks/142118.js
+++ chunks/142118.js
@@ -1,23 +1,23 @@
-n.d(t, { Z: () => a });
-var i = n(951288),
+n.d(t, { Z: () => s });
+var r = n(951288),
     l = n(647438),
-    r = n(481060),
-    o = n(36361),
-    s = n(476804);
-let a = l.memo(function (e) {
+    o = n(481060),
+    i = n(36361),
+    a = n(476804);
+let s = l.memo(function (e) {
     let { node: t } = e,
-        { useTitle: n, layout: a, render: u, init: c } = t,
-        d = null == n ? void 0 : n();
-    return (l.useEffect(() => {
-        null == c || c();
-    }, [c]),
-    null != u)
-        ? (0, i.jsx)(u, {})
-        : (0, i.jsx)(r.hjN, {
-              tag: r.RB0.H1,
-              className: s.category,
-              title: d,
-              titleClassName: s.categoryTitle,
-              children: a.map((e) => (0, i.jsx)(o.Z, { node: e }, e.key)),
-          });
+        { useTitle: n, layout: s, init: c } = t,
+        u = null == n ? void 0 : n();
+    return (
+        l.useEffect(() => {
+            null == c || c();
+        }, [c]),
+        (0, r.jsx)(o.hjN, {
+            tag: o.RB0.H1,
+            className: a.category,
+            title: u,
+            titleClassName: a.categoryTitle,
+            children: s.map((e) => (0, r.jsx)(i.Z, { node: e }, e.key)),
+        })
+    );
 });
--- chunks/357660.js
+++ chunks/357660.js
@@ -1,19 +1,19 @@
 n.d(t, { T: () => f }), n(415506);
 var r = n(951288),
-    o = n(647438),
-    l = n(442837),
+    l = n(647438),
+    o = n(442837),
     i = n(215569),
     a = n(481060),
     s = n(924052),
     c = n(670304),
-    u = n(880745);
+    u = n(764516);
 function d(e) {
     let { notice: t, children: n } = e,
         { showNotice: c, handleStoreUpdate: d } = (0, s.Cu)(),
         f = null == t ? void 0 : t.stores;
-    o.useEffect(() => {
+    l.useEffect(() => {
         if (null != f) {
-            let e = new l.Fh(f, () => {
+            let e = new o.Fh(f, () => {
                 d(f);
             });
             return (
@@ -25,7 +25,7 @@ function d(e) {
             );
         }
     }, [f, d]);
-    let b = o.useMemo(() => {
+    let m = l.useMemo(() => {
         if (null == t || !c) return null;
         let { element: e } = t;
         return (0, r.jsx)(a.oXn, {
@@ -38,18 +38,15 @@ function d(e) {
             n,
             (0, r.jsx)(i.W, {
                 component: "div",
-                children: b,
+                children: m,
             }),
         ],
     });
 }
 function f(e) {
     let { node: t } = e,
-        n = t.render,
-        l = o.useRef(null);
-    if (null != n) return (0, r.jsx)(n, {});
-    if (1 !== t.layout.length)
-        throw Error("Panels with multiple panes must define a render prop to handle the multiple panes");
+        n = l.useRef(null);
+    if (1 !== t.layout.length) throw Error("Panels do not currently support multiple panes");
     return (0, r.jsx)(d, {
         notice: t.notice,
         children: (0, r.jsx)(
@@ -58,9 +55,9 @@ function f(e) {
                 className: u.scroller,
                 children: (0, r.jsx)("div", {
                     className: u.panel,
-                    ref: l,
+                    ref: n,
                     children: (0, r.jsx)(a.JcV, {
-                        containerRef: l,
+                        containerRef: n,
                         children: (0, r.jsx)(c.Z, { node: t.layout[0] }),
                     }),
                 }),
