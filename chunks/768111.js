9e49a434294a45e6ba982272b2f6dfbf6680f0e1
DiscrapperManager <admin@minerpl.xyz>
2025-10-08 06:42:31 +0200

    canaryRelease: 455063
    Main scripts: 4
    Lazy Loaded Scripts: 1612
    Chunks:
     * Main Files: 14830
     * Scripts: 9082
    Strings: 22528
    Routes: 748
    Experiments Count: 429
    Apex Experiments Count: 89

--- chunks/768111.js
+++ chunks/768111.js
@@ -1,70 +1,71 @@
-t.d(n, { Z: () => v }), t(539854);
+t.d(n, { Z: () => g }), t(539854);
 var l = t(370523),
     i = t(607550),
     r = t(699516),
     o = t(621853),
-    s = t(46841),
-    a = t(837411),
-    c = t(477448),
-    d = t(612600),
-    u = t(585305),
-    m = t(806729),
-    p = t(517157),
-    f = t(708108),
-    h = t(146078),
-    x = t(228168),
-    j = t(388032);
-function v(e) {
+    s = t(567400),
+    a = t(46841),
+    c = t(837411),
+    d = t(477448),
+    u = t(612600),
+    m = t(585305),
+    p = t(806729),
+    f = t(517157),
+    h = t(708108),
+    x = t(146078),
+    j = t(228168),
+    v = t(388032);
+function g(e) {
     let { user: n, currentUser: t } = e,
-        { mutualFriendsCount: v, mutualGuilds: g } = (0, m.Z)(n),
-        b = null == g ? void 0 : g.length,
-        y = (0, u.Z)(n),
-        I = (0, c.Z)({ location: "useUserProfileModalV2TabBarItems" }),
+        { mutualFriendsCount: g, mutualGuilds: b } = (0, p.Z)(n),
+        y = null == b ? void 0 : b.length,
+        I = (0, m.Z)(n),
         Z = (0, d.Z)({ location: "useUserProfileModalV2TabBarItems" }),
-        O = (0, p.Z)(n.id),
-        N = (0, s.S)({ location: "useUserProfileModalV2TabBarItems" }),
-        P = (0, a.Z)(n.id),
-        T = [],
-        A = n.id === (null == t ? void 0 : t.id),
-        E = o.Z.getFirstWishlistId(n.id),
-        S = null != E,
-        C = S ? o.Z.getWishlistSettings(n.id, E) : null,
-        _ = (S ? i.Z.getWishlistItems(E) : []).length > 0,
-        w = O.length > 0;
-    ((A && I) || (w && Z)) &&
-        T.push({
-            text: j.intl.string(j.t.laViw8),
-            section: x.oh.WIDGETS,
+        O = (0, u.Z)({ location: "useUserProfileModalV2TabBarItems" }),
+        N = (0, f.Z)(n.id),
+        P = (0, s.Y)({ location: "useUserProfileModalV2TabBarItems" }),
+        T = (0, a.S)({ location: "useUserProfileModalV2TabBarItems" }),
+        A = (0, c.Z)(n.id),
+        E = [],
+        S = n.id === (null == t ? void 0 : t.id),
+        C = o.Z.getFirstWishlistId(n.id),
+        _ = null != C,
+        w = _ ? o.Z.getWishlistSettings(n.id, C) : null,
+        D = (_ ? i.Z.getWishlistItems(C) : []).length > 0,
+        R = N.length > 0;
+    ((S && Z) || (R && O)) &&
+        E.push({
+            text: v.intl.string(v.t.laViw8),
+            section: j.oh.WIDGETS,
             subsection: void 0,
         }),
-        T.push({
-            text: j.intl.string(j.t.chq59f),
-            section: x.oh.ACTIVITY,
+        E.push({
+            text: v.intl.string(v.t.chq59f),
+            section: j.oh.ACTIVITY,
             subsection: void 0,
         });
-    let D = !1 === n.nsfwAllowed,
-        R = r.Z.isFriend(n.id),
-        L = (null == C ? void 0 : C.visibility) === l.f.PUBLIC;
+    let L = !1 === n.nsfwAllowed,
+        M = r.Z.isFriend(n.id),
+        U = (null == w ? void 0 : w.visibility) === l.f.PUBLIC;
     return (
-        N &&
-            (A || (_ && L && P && (!D || (D && R)))) &&
-            T.push({
-                text: j.intl.string(j.t["7lZ31N"]),
-                section: x.oh.WISHLIST,
+        ((S && P) || (!S && T && D && U && A && (!L || (L && M)))) &&
+            E.push({
+                text: v.intl.string(v.t["7lZ31N"]),
+                section: j.oh.WISHLIST,
                 subsection: void 0,
             }),
         n.id !== (null == t ? void 0 : t.id) &&
-            y &&
-            (T.push({
-                text: (0, f.Z)(v),
-                section: x.oh.MUTUAL_FRIENDS,
+            I &&
+            (E.push({
+                text: (0, h.Z)(g),
+                section: j.oh.MUTUAL_FRIENDS,
                 subsection: void 0,
             }),
-            T.push({
-                text: (0, h.Z)(b),
-                section: x.oh.MUTUAL_GUILDS,
+            E.push({
+                text: (0, x.Z)(y),
+                section: j.oh.MUTUAL_GUILDS,
                 subsection: void 0,
             })),
-        T
+        E
     );
 }
