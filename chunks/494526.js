976f43c58464397dd45686ce8cad146554f7da04
DiscrapperManager <admin@minerpl.xyz>
2025-10-08 20:50:07 +0200

    canaryRelease: 455356
    Main scripts: 4
    Lazy Loaded Scripts: 1619
    Chunks:
     * Main Files: 14832
     * Scripts: 9085
    Strings: 22553
    Routes: 748
    Experiments Count: 429
    Apex Experiments Count: 90

--- chunks/494526.js
+++ chunks/494526.js
@@ -191,8 +191,9 @@ function L(e) {
                 }, [a, d, r, l]),
                 i.useEffect(() => {
                     let t = Date.now(),
-                        n = "wss:".concat(window.GLOBAL_ENV.REMOTE_AUTH_ENDPOINT, "/?v=2"),
-                        r = new WebSocket(n);
+                        n = "".concat(window.GLOBAL_ENV.REMOTE_AUTH_ENDPOINT, "/?v=2");
+                    n.startsWith("//") && (n = "wss:".concat(n));
+                    let r = new WebSocket(n);
                     T.info("[0ms] connecting to ".concat(n));
                     let i = (e) => T.info("[".concat("".concat(Date.now() - t, "ms"), "] ").concat(e)),
                         l = null,
