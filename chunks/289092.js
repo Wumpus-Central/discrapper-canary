4fd7d9cbe5f1eb2331fbc33ccfe17cf316fcccde
DiscrapperManager <admin@minerpl.xyz>
2025-10-10 22:12:23 +0200

    canaryRelease: 455790
    Main scripts: 4
    Lazy Loaded Scripts: 1618
    Chunks:
     * Main Files: 14852
     * Scripts: 9075
    Strings: 22562
    Routes: 748
    Experiments Count: 428
    Apex Experiments Count: 91

--- chunks/289092.js
+++ chunks/289092.js
@@ -1,10 +1,4 @@
-n.d(t, { X: () => _ });
-var r = n(951288);
-n(647438);
-var i = n(692547),
-    a = n(331595),
-    o = n(267843);
-function s(e, t, n) {
+function r(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -17,23 +11,7 @@ function s(e, t, n) {
         e
     );
 }
-function l(e) {
-    for (var t = 1; t < arguments.length; t++) {
-        var n = null != arguments[t] ? arguments[t] : {},
-            r = Object.keys(n);
-        "function" == typeof Object.getOwnPropertySymbols &&
-            (r = r.concat(
-                Object.getOwnPropertySymbols(n).filter(function (e) {
-                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
-                }),
-            )),
-            r.forEach(function (t) {
-                s(e, t, n[t]);
-            });
-    }
-    return e;
-}
-function c(e, t) {
+function i(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -45,30 +23,7 @@ function c(e, t) {
     }
     return n;
 }
-function u(e, t) {
-    return (
-        (t = null != t ? t : {}),
-        Object.getOwnPropertyDescriptors
-            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : c(Object(t)).forEach(function (n) {
-                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
-              }),
-        e
-    );
-}
-function d(e, t) {
-    if (null == e) return {};
-    var n,
-        r,
-        i = f(e, t);
-    if (Object.getOwnPropertySymbols) {
-        var a = Object.getOwnPropertySymbols(e);
-        for (r = 0; r < a.length; r++)
-            (n = a[r]), !(t.indexOf(n) >= 0) && Object.prototype.propertyIsEnumerable.call(e, n) && (i[n] = e[n]);
-    }
-    return i;
-}
-function f(e, t) {
+function a(e, t) {
     if (null == e) return {};
     var n,
         r,
@@ -77,29 +32,4 @@ function f(e, t) {
     for (r = 0; r < a.length; r++) (n = a[r]), t.indexOf(n) >= 0 || (i[n] = e[n]);
     return i;
 }
-let _ = (e) => {
-    var t,
-        n,
-        { size: s = "md", width: c, height: f, color: _ = i.Z.colors.INTERACTIVE_NORMAL, colorClass: p = "" } = e,
-        h = d(e, ["size", "width", "height", "color", "colorClass"]);
-    let m = (0, o.m)(s),
-        g = null != (t = null == m ? void 0 : m.width) ? t : c,
-        E = null != (n = null == m ? void 0 : m.height) ? n : f;
-    return (0, r.jsx)(
-        "svg",
-        u(l({}, (0, a.Z)(h)), {
-            xmlns: "http://www.w3.org/2000/svg",
-            width: g,
-            height: E,
-            fill: "none",
-            viewBox: "0 0 24 24",
-            children: (0, r.jsx)("path", {
-                fill: "string" == typeof _ ? _ : _.css,
-                fillRule: "evenodd",
-                d: "M13.05 1.4a1.59 1.59 0 0 0-2.1 0l-4.9 4A3.37 3.37 0 0 0 5 8v8c0 1.07.4 2.06 1.05 2.6l4.9 4c.65.53 1.45.53 2.1 0l4.9-4A3.37 3.37 0 0 0 19 16V8c0-1.07-.4-2.06-1.05-2.6l-4.9-4ZM12 4.41 8.06 7.64c-.03.08-.06.2-.06.36v8c0 .16.03.28.06.36L12 19.59l3.94-3.23c.03-.08.06-.2.06-.36V8c0-.16-.03-.28-.06-.36L12 4.41Z",
-                clipRule: "evenodd",
-                className: p,
-            }),
-        }),
-    );
-};
+n(951288), n(647438), n(692547), n(331595), n(267843);
