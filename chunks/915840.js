5756e64a606fb9d52fffb6b806a88ec242135dd5
DiscrapperManager <admin@minerpl.xyz>
2025-10-11 02:54:04 +0200

    canaryRelease: 456479
    Main scripts: 4
    Lazy Loaded Scripts: 1602
    Chunks:
     * Main Files: 14895
     * Scripts: 9036
    Strings: 22597
    Routes: 748
    Experiments Count: 425
    Apex Experiments Count: 93

--- chunks/915840.js
+++ chunks/915840.js
@@ -704,6 +704,24 @@ let { Themes: r } = n(15202).V,
                     opacity: 1,
                 },
             },
+            CHECKBOX_BORDER_HOVER: {
+                [r.DARK]: {
+                    raw: "NEUTRAL_12",
+                    opacity: 1,
+                },
+                [r.LIGHT]: {
+                    raw: "NEUTRAL_49",
+                    opacity: 1,
+                },
+                [r.MIDNIGHT]: {
+                    raw: "NEUTRAL_23",
+                    opacity: 1,
+                },
+                [r.DARKER]: {
+                    raw: "NEUTRAL_19",
+                    opacity: 1,
+                },
+            },
             CHECKBOX_BORDER_HOVER_SELECTED: {
                 [r.DARK]: {
                     raw: "NEUTRAL_12",
@@ -2486,7 +2504,43 @@ let { Themes: r } = n(15202).V,
                     opacity: 1,
                 },
             },
+            RADIO_BORDER_DEFAULT: {
+                [r.DARK]: {
+                    raw: "NEUTRAL_12",
+                    opacity: 1,
+                },
+                [r.LIGHT]: {
+                    raw: "NEUTRAL_49",
+                    opacity: 1,
+                },
+                [r.MIDNIGHT]: {
+                    raw: "NEUTRAL_23",
+                    opacity: 1,
+                },
+                [r.DARKER]: {
+                    raw: "NEUTRAL_19",
+                    opacity: 1,
+                },
+            },
             RADIO_BORDER_DEFAULT_SELECTED: {
+                [r.DARK]: {
+                    raw: "NEUTRAL_12",
+                    opacity: 1,
+                },
+                [r.LIGHT]: {
+                    raw: "NEUTRAL_49",
+                    opacity: 1,
+                },
+                [r.MIDNIGHT]: {
+                    raw: "NEUTRAL_23",
+                    opacity: 1,
+                },
+                [r.DARKER]: {
+                    raw: "NEUTRAL_19",
+                    opacity: 1,
+                },
+            },
+            RADIO_BORDER_HOVER: {
                 [r.DARK]: {
                     raw: "NEUTRAL_12",
                     opacity: 1,
@@ -2666,7 +2720,43 @@ let { Themes: r } = n(15202).V,
                     opacity: 1,
                 },
             },
+            SWITCH_BORDER_DEFAULT: {
+                [r.DARK]: {
+                    raw: "NEUTRAL_12",
+                    opacity: 1,
+                },
+                [r.LIGHT]: {
+                    raw: "NEUTRAL_49",
+                    opacity: 1,
+                },
+                [r.MIDNIGHT]: {
+                    raw: "NEUTRAL_23",
+                    opacity: 1,
+                },
+                [r.DARKER]: {
+                    raw: "NEUTRAL_19",
+                    opacity: 1,
+                },
+            },
             SWITCH_BORDER_DEFAULT_SELECTED: {
+                [r.DARK]: {
+                    raw: "NEUTRAL_12",
+                    opacity: 1,
+                },
+                [r.LIGHT]: {
+                    raw: "NEUTRAL_49",
+                    opacity: 1,
+                },
+                [r.MIDNIGHT]: {
+                    raw: "NEUTRAL_23",
+                    opacity: 1,
+                },
+                [r.DARKER]: {
+                    raw: "NEUTRAL_19",
+                    opacity: 1,
+                },
+            },
+            SWITCH_BORDER_HOVER: {
                 [r.DARK]: {
                     raw: "NEUTRAL_12",
                     opacity: 1,
