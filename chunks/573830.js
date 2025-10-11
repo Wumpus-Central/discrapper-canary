a2b4a016d4cbfcc205a02c1eeb904d5203838c09
DiscrapperManager <admin@minerpl.xyz>
2025-10-11 04:02:34 +0200

    canaryRelease: 456642
    Main scripts: 4
    Lazy Loaded Scripts: 1632
    Chunks:
     * Main Files: 14900
     * Scripts: 9067
    Strings: 22607
    Routes: 748
    Experiments Count: 425
    Apex Experiments Count: 93

--- chunks/573830.js
+++ chunks/573830.js
@@ -1,15 +1,18 @@
-n.d(t, { J: () => N }), n(388685), n(583741);
+n.d(t, { J: () => w }), n(388685), n(583741);
 var r = n(951288),
     i = n(647438),
-    a = n(958610),
-    o = n(137317),
-    s = n(692547),
-    l = n(251052),
-    c = n(673824),
-    u = n(213305),
-    d = n(155127),
-    f = n(571755);
-function _(e, t, n) {
+    a = n(799189),
+    o = n(731602),
+    s = n(599273),
+    l = n(922462),
+    c = n(692547),
+    u = n(251052),
+    d = n(673824),
+    f = n(213305),
+    _ = n(155127),
+    p = n(635041),
+    h = n(571755);
+function m(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -22,7 +25,7 @@ function _(e, t, n) {
         e
     );
 }
-function p(e) {
+function g(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -33,12 +36,12 @@ function p(e) {
                 }),
             )),
             r.forEach(function (t) {
-                _(e, t, n[t]);
+                m(e, t, n[t]);
             });
     }
     return e;
 }
-function h(e, t) {
+function E(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -50,53 +53,53 @@ function h(e, t) {
     }
     return n;
 }
-function m(e, t) {
+function b(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : h(Object(t)).forEach(function (n) {
+            : E(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
     );
 }
-let g = "M4.10585 5.3837L5.37864 4.11091L11.884 10.61632L10.6112 11.88912L4.10585 5.3837Z",
-    E = "M10.61632 4.11091L11.88912 5.3837L5.3837 11.88912L4.11091 10.61632L10.61632 4.11091Z",
-    b = "M5.25333 8.80104L5.25333 7.17346L10.85336 7.17346L10.85336 8.80104L5.25333 8.80104Z",
-    y = "M10.84656 7.17346L10.84656 8.80104L5.24954 8.80104L5.24954 7.17346L10.84656 7.17346Z",
-    O = "M6.31649 11.88304L5.0437 10.61032L11.44792 4.20604L12.72072 5.47883L6.31649 11.88304Z",
-    v = "M3.26914 8.87224L4.54194 7.59943L7.5588 10.61632L6.28601 11.88912L3.26914 8.87224Z",
-    I = [0, 0.1, 0.4, 0.6, 0.9, 1],
-    T = [0, 0.1, 0.9, 1],
-    S = [0, 0.1, 0.2, 0.5, 0.7, 1],
-    A = [0, 0.3, 0.5, 0.8, 0.9, 1];
-function C(e, t, n, i) {
+let y = "M4.10585 5.3837L5.37864 4.11091L11.884 10.61632L10.6112 11.88912L4.10585 5.3837Z",
+    O = "M10.61632 4.11091L11.88912 5.3837L5.3837 11.88912L4.11091 10.61632L10.61632 4.11091Z",
+    v = "M5.25333 8.80104L5.25333 7.17346L10.85336 7.17346L10.85336 8.80104L5.25333 8.80104Z",
+    I = "M10.84656 7.17346L10.84656 8.80104L5.24954 8.80104L5.24954 7.17346L10.84656 7.17346Z",
+    T = "M6.31649 11.88304L5.0437 10.61032L11.44792 4.20604L12.72072 5.47883L6.31649 11.88304Z",
+    S = "M3.26914 8.87224L4.54194 7.59943L7.5588 10.61632L6.28601 11.88912L3.26914 8.87224Z",
+    A = [0, 0.1, 0.4, 0.6, 0.9, 1],
+    C = [0, 0.1, 0.9, 1],
+    N = [0, 0.1, 0.2, 0.5, 0.7, 1],
+    R = [0, 0.3, 0.5, 0.8, 0.9, 1];
+function P(e, t, n, i) {
     let a = e.to({
-            range: T,
+            range: C,
             output: [t, t, n, n],
             extrapolate: "clamp",
         }),
-        s = i ? [g, g, O, O] : [g, b, b, O],
-        l = i ? [E, E, v, v] : [E, y, y, v];
+        o = i ? [y, y, T, T] : [y, v, v, T],
+        l = i ? [O, O, S, S] : [O, I, I, S];
     return (0, r.jsx)("svg", {
         viewBox: "0 0 24 24",
         fill: "none",
         children: (0, r.jsxs)("g", {
             transform: "translate(4 4)",
             children: [
-                (0, r.jsx)(o.animated.path, {
+                (0, r.jsx)(s.animated.path, {
                     fill: a,
                     d: e.to({
-                        range: T,
-                        output: s,
+                        range: C,
+                        output: o,
                         extrapolate: "clamp",
                     }),
                 }),
-                (0, r.jsx)(o.animated.path, {
+                (0, r.jsx)(s.animated.path, {
                     fill: a,
                     d: e.to({
-                        range: T,
+                        range: C,
                         output: l,
                         extrapolate: "clamp",
                     }),
@@ -105,126 +108,139 @@ function C(e, t, n, i) {
         }),
     });
 }
-function N(e) {
+function w(e) {
     let {
             checked: t,
             disabled: n,
-            id: _,
-            onChange: h,
-            focusProps: g,
-            hasIcon: E = !1,
-            describedBy: b,
-            innerRef: y,
+            id: m,
+            onChange: E,
+            focusProps: y,
+            hasIcon: O = !1,
+            describedBy: v,
+            innerRef: I,
         } = e,
-        { reducedMotion: O } = i.useContext(u.S),
-        v = (0, c.d)(s.Z.colors.SWITCH_BACKGROUND_DEFAULT).spring(),
-        N = (0, c.d)(s.Z.colors.SWITCH_BACKGROUND_DEFAULT).spring(),
-        R = (0, c.d)(s.Z.colors.SWITCH_BACKGROUND_DEFAULT_SELECTED).spring(),
-        P = (0, c.d)(s.Z.colors.SWITCH_BACKGROUND_ACTIVE).spring(),
-        w = (0, c.d)(s.Z.colors.SWITCH_BORDER_DEFAULT).spring(),
-        D = (0, c.d)(s.Z.colors.SWITCH_BORDER_DEFAULT_SELECTED).spring(),
-        L = (0, c.d)(s.Z.colors.SWITCH_BACKGROUND_HOVER).spring(),
-        x = (0, c.d)(s.Z.colors.SWITCH_BACKGROUND_HOVER_SELECTED).spring(),
-        M = (0, c.d)(s.Z.colors.SWITCH_BORDER_HOVER).spring(),
-        j = (0, c.d)(s.Z.colors.SWITCH_BORDER_HOVER_SELECTED).spring(),
-        k = (0, c.d)(s.Z.colors.SWITCH_THUMB_FOREGROUND_DEFAULT).spring(),
-        U = (0, c.d)(s.Z.colors.SWITCH_THUMB_FOREGROUND_ACTIVE).spring(),
-        G = (0, c.d)(s.Z.colors.SWITCH_THUMB_BACKGROUND_DEFAULT).spring(),
-        B = (0, c.d)(s.Z.colors.SWITCH_THUMB_BACKGROUND_DEFAULT_SELECTED).spring(),
-        [Z, F] = i.useState(!1),
-        [V, H] = i.useState(!1),
-        Y = i.useRef(null),
-        W = i.useRef(null),
-        { state: K, opacity: z } = (0, l.q)(
+        { reducedMotion: T } = i.useContext(f.S),
+        S = i.useRef(null),
+        w = i.useRef(null),
+        D = null != I ? I : w,
+        L = (0, l.l)({
+            isSelected: t,
+            onChange: E,
+            isDisabled: n,
+        }),
+        {
+            inputProps: x,
+            labelProps: M,
+            isPressed: j,
+        } = (0, a.h)(
+            {
+                id: m,
+                isDisabled: n,
+                "aria-describedby": v,
+            },
+            L,
+            D,
+        ),
+        { hoverProps: k, isHovered: U } = (0, o.X)({ isDisabled: n }),
+        G = (0, d.d)(c.Z.colors.SWITCH_BACKGROUND_DEFAULT).spring(),
+        B = (0, d.d)(c.Z.colors.SWITCH_BACKGROUND_DEFAULT).spring(),
+        Z = (0, d.d)(c.Z.colors.SWITCH_BACKGROUND_DEFAULT_SELECTED).spring(),
+        F = (0, d.d)(c.Z.colors.SWITCH_BACKGROUND_ACTIVE).spring(),
+        V = (0, d.d)(c.Z.colors.SWITCH_BORDER_DEFAULT).spring(),
+        H = (0, d.d)(c.Z.colors.SWITCH_BORDER_DEFAULT_SELECTED).spring(),
+        Y = (0, d.d)(c.Z.colors.SWITCH_BACKGROUND_HOVER).spring(),
+        W = (0, d.d)(c.Z.colors.SWITCH_BACKGROUND_HOVER_SELECTED).spring(),
+        K = (0, d.d)(c.Z.colors.SWITCH_BORDER_HOVER).spring(),
+        z = (0, d.d)(c.Z.colors.SWITCH_BORDER_HOVER_SELECTED).spring(),
+        q = (0, d.d)(c.Z.colors.SWITCH_THUMB_FOREGROUND_DEFAULT).spring(),
+        X = (0, d.d)(c.Z.colors.SWITCH_THUMB_FOREGROUND_ACTIVE).spring(),
+        Q = (0, d.d)(c.Z.colors.SWITCH_THUMB_BACKGROUND_DEFAULT).spring(),
+        J = (0, d.d)(c.Z.colors.SWITCH_THUMB_BACKGROUND_DEFAULT_SELECTED).spring(),
+        { state: $, opacity: ee } = (0, u.q)(
             {
                 config: { duration: 300 },
                 opacity: n ? 0.5 : 1,
-                state: Z ? (t ? A[A.length - 2] : S[1]) : +!!t,
+                state: j ? (t ? R[R.length - 2] : N[1]) : +!!t,
             },
             "animate-always",
         ),
-        q = function (e) {
+        et = function (e) {
             let n = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : [...e].reverse();
             return t
-                ? K.to({
-                      range: A,
+                ? $.to({
+                      range: R,
                       output: n,
                   })
-                : K.to({
-                      range: S,
+                : $.to({
+                      range: N,
                       output: e,
                   });
         };
     return (0, r.jsx)(
-        d.tE,
-        m(p({}, g), {
-            ringTarget: Y,
-            focusTarget: W,
+        _.tE,
+        b(g({}, y), {
+            ringTarget: S,
+            focusTarget: D,
             within: !0,
             offset: -2,
-            children: (0, r.jsx)(a.r, {
-                id: _,
-                className: f.container,
-                onHoverChange: (e) => !n && H(e),
-                onMouseDown: () => !n && F(!0),
-                onMouseUp: () => F(!1),
-                onMouseLeave: () => {
-                    F(!1);
-                },
-                inputRef: null != y ? y : W,
-                isDisabled: n,
-                isSelected: t,
-                onChange: (e) => {
-                    F(!1), h(e);
-                },
-                "aria-describedby": b,
-                children: (0, r.jsx)(o.animated.div, {
-                    ref: Y,
-                    className: f.switchIndicator,
-                    "data-size": "md",
-                    "data-mana-component": "switch",
-                    style: {
-                        opacity: z,
-                        backgroundColor: K.to({
-                            range: T,
-                            output: [Z ? N : V ? L : v, Z ? N : V ? L : v, Z ? P : V ? x : R, Z ? P : V ? x : R],
-                            extrapolate: "clamp",
-                        }),
-                        border: "1px solid",
-                        borderColor: K.to({
-                            range: T,
-                            output: [Z || V ? M : w, Z || V ? M : w, Z || V ? j : D, Z || V ? j : D],
-                            extrapolate: "clamp",
-                        }),
-                    },
-                    children: (0, r.jsxs)(o.animated.svg, {
-                        className: f.thumb,
-                        viewBox: "0 0 24 24",
-                        preserveAspectRatio: "xMidYMin meet",
-                        style: {
-                            left: K.to({
-                                range: I,
-                                output: [1, 1, 1, 24, 24, 24],
-                            }),
-                        },
-                        "aria-hidden": !0,
-                        children: [
-                            (0, r.jsx)(o.animated.rect, {
-                                fill: K.to({
-                                    range: T,
-                                    output: [G, G, B, B],
+            children: (0, r.jsxs)(
+                "label",
+                b(g({}, M), {
+                    className: h.container,
+                    children: [
+                        (0, r.jsx)(
+                            s.animated.div,
+                            b(g({}, k), {
+                                ref: S,
+                                className: h.switchIndicator,
+                                "data-size": "md",
+                                "data-mana-component": "switch",
+                                style: {
+                                    opacity: ee,
+                                    backgroundColor: $.to({
+                                        range: C,
+                                        output: [U ? Y : G, B, F, U ? W : Z],
+                                        extrapolate: "clamp",
+                                    }),
+                                    border: "1px solid",
+                                    borderColor: $.to({
+                                        range: C,
+                                        output: [U ? K : V, U ? K : V, U ? z : H, U ? z : H],
+                                        extrapolate: "clamp",
+                                    }),
+                                },
+                                children: (0, r.jsxs)(s.animated.svg, {
+                                    className: h.thumb,
+                                    viewBox: "0 0 24 24",
+                                    preserveAspectRatio: "xMidYMin meet",
+                                    style: {
+                                        left: $.to({
+                                            range: A,
+                                            output: [1, 1, 1, 24, 24, 24],
+                                        }),
+                                    },
+                                    "aria-hidden": !0,
+                                    children: [
+                                        (0, r.jsx)(s.animated.rect, {
+                                            fill: $.to({
+                                                range: C,
+                                                output: [Q, Q, J, J],
+                                            }),
+                                            x: et([4, 4, 2.5, 1, 2.5, 4]),
+                                            y: et([4, 7, 2.5, 7, 2.5, 4]),
+                                            width: et([16, 16, 19, 22, 19, 16]),
+                                            height: et([16, 10, 19, 10, 19, 16]),
+                                            rx: et([8, 5, 9.5, 5, 9.5, 8]),
+                                        }),
+                                        O && P($, q, X, T.enabled),
+                                    ],
                                 }),
-                                x: q([4, 4, 2.5, 1, 2.5, 4]),
-                                y: q([4, 7, 2.5, 7, 2.5, 4]),
-                                width: q([16, 16, 19, 22, 19, 16]),
-                                height: q([16, 10, 19, 10, 19, 16]),
-                                rx: q([8, 5, 9.5, 5, 9.5, 8]),
                             }),
-                            E && C(K, k, U, O.enabled),
-                        ],
-                    }),
+                        ),
+                        (0, r.jsx)(p.n, { children: (0, r.jsx)("input", b(g({}, x), { ref: D })) }),
+                    ],
                 }),
-            }),
+            ),
         }),
     );
 }
---
+++ chunks/611333.js
@@ -0,0 +1,58 @@
+n.d(t, { O: () => l });
+var r = n(586235),
+    i = n(436773),
+    a = n(408558),
+    o = n(146810),
+    s = n(600711);
+function l(e, t, n) {
+    let {
+            isDisabled: l = !1,
+            isReadOnly: c = !1,
+            value: u,
+            name: d,
+            children: f,
+            "aria-label": _,
+            "aria-labelledby": p,
+            validationState: h = "valid",
+            isInvalid: m,
+        } = e,
+        g = (e) => {
+            e.stopPropagation(), t.setSelected(e.target.checked);
+        },
+        E = null != _ || null != p;
+    null != f || E || console.warn("If you do not provide children, you must specify an aria-label for accessibility");
+    let { pressProps: b, isPressed: y } = (0, o.r)({ isDisabled: l }),
+        { pressProps: O, isPressed: v } = (0, o.r)({
+            onPress() {
+                var e;
+                t.toggle(), null == (e = n.current) || e.focus();
+            },
+            isDisabled: l || c,
+        }),
+        { focusableProps: I } = (0, s.kc)(e, n),
+        T = (0, r.d)(b, I),
+        S = (0, i.z)(e, { labelable: !0 });
+    return (
+        (0, a.y)(n, t.isSelected, t.setSelected),
+        {
+            labelProps: (0, r.d)(O, { onClick: (e) => e.preventDefault() }),
+            inputProps: (0, r.d)(S, {
+                "aria-invalid": m || "invalid" === h || void 0,
+                "aria-errormessage": e["aria-errormessage"],
+                "aria-controls": e["aria-controls"],
+                "aria-readonly": c || void 0,
+                onChange: g,
+                disabled: l,
+                ...(null == u ? {} : { value: u }),
+                name: d,
+                type: "checkbox",
+                ...T,
+            }),
+            isSelected: t.isSelected,
+            isPressed: y || v,
+            isDisabled: l,
+            isReadOnly: c,
+            isInvalid: m || "invalid" === h,
+        }
+    );
+}
