460f1e657bf11acb3c0aa8a98ac1650c06fedadd
DiscrapperManager <admin@minerpl.xyz>
2025-10-11 04:35:31 +0200

    canaryRelease: 456732
    Main scripts: 4
    Lazy Loaded Scripts: 1633
    Chunks:
     * Main Files: 14901
     * Scripts: 9068
    Strings: 22607
    Routes: 748
    Experiments Count: 425
    Apex Experiments Count: 93

---
+++ chunks/181174.js
@@ -0,0 +1,11 @@
+e.exports = {
+    editProfileContainer: "editProfileContainer__7bf69",
+    opened: "opened__7bf69",
+    customizationSection: "customizationSection__7bf69",
+    condensed: "condensed__7bf69",
+    header: "header__7bf69",
+    headerIconWrapper: "headerIconWrapper__7bf69",
+    title: "title__7bf69",
+    caret: "caret__7bf69",
+    serverNickname: "serverNickname__7bf69",
+};
