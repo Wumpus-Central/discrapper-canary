6d8e6cdbd7f2707498eb66c39c3735821612ecfe
DiscrapperManager <admin@minerpl.xyz>
2025-10-11 11:09:56 +0200

    canaryRelease: 457178
    Main scripts: 4
    Lazy Loaded Scripts: 1634
    Chunks:
     * Main Files: 14906
     * Scripts: 9074
    Strings: 22614
    Routes: 750
    Experiments Count: 425
    Apex Experiments Count: 97

--- chunks/694338.js
+++ chunks/694338.js
@@ -1,144 +1,159 @@
-n.d(t, { e: () => c });
-var r = n(706454),
-    i = n(74538),
-    a = n(937615),
-    o = n(474936),
-    s = n(981631),
-    l = n(388032);
-let c = () => [
-    {
-        id: 17,
-        title: l.intl.string(l.t.Ij3Zmp),
-        rows: [
+n.d(t, { e: () => d }), n(539854);
+var r = n(594928),
+    i = n(706454),
+    a = n(74538),
+    o = n(937615),
+    s = n(474936),
+    l = n(981631),
+    c = n(388032),
+    u = n(693450);
+let d = () => {
+    let e = r.wL.useConfig({ location: "useGetPlanComparisonTableRowsApplicationHome" }).enabled,
+        t = [
             {
                 id: 10,
-                label: l.intl.string(l.t["svn/YW"]),
+                label: c.intl.string(c.t["svn/YW"]),
                 tier0ColumnData: { includes: !0 },
                 tier2ColumnData: { includes: !0 },
             },
             {
                 id: 7,
-                label: l.intl.string(l.t.ID5B6e),
+                label: c.intl.string(c.t.ID5B6e),
                 tier0ColumnData: { includes: !1 },
                 tier2ColumnData: { includes: !0 },
             },
             {
                 id: 13,
-                label: l.intl.string(l.t.PBUrx8),
+                label: c.intl.string(c.t.PBUrx8),
                 tier0ColumnData: { includes: !1 },
                 tier2ColumnData: { includes: !0 },
             },
-        ],
-    },
-    {
-        id: 18,
-        title: l.intl.string(l.t.Wme3nZ),
-        rows: [
-            {
-                id: 0,
-                label: l.intl.string(l.t.LrUABg),
-                tier0ColumnData: { includes: !0 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 1,
-                label: l.intl.string(l.t.RIRtn5),
-                tier0ColumnData: { includes: !0 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 2,
-                label: l.intl.string(l.t.Uukj4u),
-                tier0ColumnData: { includes: !0 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 12,
-                label: l.intl.string(l.t.NIKDqK),
+        ];
+    return (
+        e &&
+            t.push({
+                id: 22,
+                label: c.intl.string(u.default["86GtGB"]),
                 tier0ColumnData: { includes: !1 },
                 tier2ColumnData: { includes: !0 },
+            }),
+        [
+            {
+                id: 17,
+                title: c.intl.string(c.t.Ij3Zmp),
+                rows: t,
             },
             {
-                id: 9,
-                label: l.intl.string(l.t["5OAKh4"]),
-                tier0ColumnData: { includes: !1 },
-                tier2ColumnData: { includes: !0 },
-            },
-        ],
-    },
-    {
-        id: 19,
-        title: l.intl.string(l.t["6b3ydH"]),
-        rows: [
-            {
-                id: 4,
-                label: l.intl.string(l.t.ufhQCw),
-                tier0ColumnData: { text: (0, i.v9)(o.PremiumTypes.TIER_0) },
-                tier2ColumnData: { text: (0, i.v9)(o.PremiumTypes.TIER_2) },
-            },
-            {
-                id: 11,
-                label: l.intl.string(l.t.qQxxVV),
-                tier0ColumnData: { includes: !0 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 3,
-                label: l.intl.string(l.t["9kRJS0"]),
-                tier0ColumnData: { includes: !0 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 21,
-                label: l.intl.string(l.t["5BJqND"]),
-                tier0ColumnData: { includes: !1 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 5,
-                label: l.intl.string(l.t.VwxlMz),
-                tier0ColumnData: { includes: !1 },
-                tier2ColumnData: { includes: !0 },
+                id: 18,
+                title: c.intl.string(c.t.Wme3nZ),
+                rows: [
+                    {
+                        id: 0,
+                        label: c.intl.string(c.t.LrUABg),
+                        tier0ColumnData: { includes: !0 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 1,
+                        label: c.intl.string(c.t.RIRtn5),
+                        tier0ColumnData: { includes: !0 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 2,
+                        label: c.intl.string(c.t.Uukj4u),
+                        tier0ColumnData: { includes: !0 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 12,
+                        label: c.intl.string(c.t.NIKDqK),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 9,
+                        label: c.intl.string(c.t["5OAKh4"]),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                ],
             },
             {
-                id: 6,
-                label: l.intl.formatToPlainString(l.t.nyhDp6, {
-                    numBoosts: o.cb,
-                    percentageOff: (0, a.T3)(r.default.locale, o.Rr / 100),
-                }),
-                tier0ColumnData: { includes: !1 },
-                tier2ColumnData: { includes: !0 },
+                id: 19,
+                title: c.intl.string(c.t["6b3ydH"]),
+                rows: [
+                    {
+                        id: 4,
+                        label: c.intl.string(c.t.ufhQCw),
+                        tier0ColumnData: { text: (0, a.v9)(s.PremiumTypes.TIER_0) },
+                        tier2ColumnData: { text: (0, a.v9)(s.PremiumTypes.TIER_2) },
+                    },
+                    {
+                        id: 11,
+                        label: c.intl.string(c.t.qQxxVV),
+                        tier0ColumnData: { includes: !0 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 3,
+                        label: c.intl.string(c.t["9kRJS0"]),
+                        tier0ColumnData: { includes: !0 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 21,
+                        label: c.intl.string(c.t["5BJqND"]),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 5,
+                        label: c.intl.string(c.t.VwxlMz),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 6,
+                        label: c.intl.formatToPlainString(c.t.nyhDp6, {
+                            numBoosts: s.cb,
+                            percentageOff: (0, o.T3)(i.default.locale, s.Rr / 100),
+                        }),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 16,
+                        label: c.intl.string(c.t["93xPy8"]),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 8,
+                        label: c.intl.string(c.t.IzrZHx),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 20,
+                        label: c.intl.string(c.t.Rj1Qys),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 15,
+                        label: c.intl.formatToPlainString(c.t["8crdzM"], { maxChars: l.en1 }),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                    {
+                        id: 14,
+                        label: c.intl.string(c.t["il8+nJ"]),
+                        tier0ColumnData: { includes: !1 },
+                        tier2ColumnData: { includes: !0 },
+                    },
+                ],
             },
-            {
-                id: 16,
-                label: l.intl.string(l.t["93xPy8"]),
-                tier0ColumnData: { includes: !1 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 8,
-                label: l.intl.string(l.t.IzrZHx),
-                tier0ColumnData: { includes: !1 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 20,
-                label: l.intl.string(l.t.Rj1Qys),
-                tier0ColumnData: { includes: !1 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 15,
-                label: l.intl.formatToPlainString(l.t["8crdzM"], { maxChars: s.en1 }),
-                tier0ColumnData: { includes: !1 },
-                tier2ColumnData: { includes: !0 },
-            },
-            {
-                id: 14,
-                label: l.intl.string(l.t["il8+nJ"]),
-                tier0ColumnData: { includes: !1 },
-                tier2ColumnData: { includes: !0 },
-            },
-        ],
-    },
-];
+        ]
+    );
+};
