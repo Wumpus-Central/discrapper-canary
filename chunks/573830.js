b580c950d5149b3d6b40ba46426227fad8ea589a
DiscrapperManager <admin@minerpl.xyz>
2025-10-11 06:56:20 +0200

    canaryRelease: 456975
    Main scripts: 4
    Lazy Loaded Scripts: 1634
    Chunks:
     * Main Files: 14898
     * Scripts: 9074
    Strings: 22610
    Routes: 749
    Experiments Count: 425
    Apex Experiments Count: 95

--- chunks/573830.js
+++ chunks/573830.js
@@ -74,14 +74,12 @@ let y = "M4.10585 5.3837L5.37864 4.11091L11.884 10.61632L10.6112 11.88912L4.1058
     C = [0, 0.1, 0.9, 1],
     N = [0, 0.1, 0.2, 0.5, 0.7, 1],
     R = [0, 0.3, 0.5, 0.8, 0.9, 1];
-function P(e, t, n, i) {
-    let a = e.to({
-            range: C,
-            output: [t, t, n, n],
-            extrapolate: "clamp",
-        }),
-        o = i ? [y, y, T, T] : [y, v, v, T],
-        l = i ? [O, O, S, S] : [O, I, I, S];
+function P(e, t, n) {
+    let i = e.to({
+        range: C,
+        output: [t, t, n, n],
+        extrapolate: "clamp",
+    });
     return (0, r.jsx)("svg", {
         viewBox: "0 0 24 24",
         fill: "none",
@@ -89,18 +87,18 @@ function P(e, t, n, i) {
             transform: "translate(4 4)",
             children: [
                 (0, r.jsx)(s.animated.path, {
-                    fill: a,
+                    fill: i,
                     d: e.to({
                         range: C,
-                        output: o,
+                        output: [y, v, v, T],
                         extrapolate: "clamp",
                     }),
                 }),
                 (0, r.jsx)(s.animated.path, {
-                    fill: a,
+                    fill: i,
                     d: e.to({
                         range: C,
-                        output: l,
+                        output: [O, I, I, S],
                         extrapolate: "clamp",
                     }),
                 }),
@@ -158,7 +156,7 @@ function w(e) {
         J = (0, d.d)(c.Z.colors.SWITCH_THUMB_BACKGROUND_DEFAULT_SELECTED).spring(),
         { state: $, opacity: ee } = (0, u.q)(
             {
-                config: { duration: 300 },
+                config: { duration: T.enabled ? 200 : 300 },
                 opacity: n ? 0.5 : 1,
                 state: j ? (t ? R[R.length - 2] : N[1]) : +!!t,
             },
@@ -214,10 +212,17 @@ function w(e) {
                                     viewBox: "0 0 24 24",
                                     preserveAspectRatio: "xMidYMin meet",
                                     style: {
-                                        left: $.to({
-                                            range: A,
-                                            output: [1, 1, 1, 24, 24, 24],
-                                        }),
+                                        left: $.to(
+                                            T.enabled
+                                                ? {
+                                                      range: C,
+                                                      output: [1, 1, 24, 24],
+                                                  }
+                                                : {
+                                                      range: A,
+                                                      output: [1, 1, 1, 24, 24, 24],
+                                                  },
+                                        ),
                                     },
                                     "aria-hidden": !0,
                                     children: [
@@ -226,13 +231,13 @@ function w(e) {
                                                 range: C,
                                                 output: [Q, Q, J, J],
                                             }),
-                                            x: et([4, 4, 2.5, 1, 2.5, 4]),
-                                            y: et([4, 7, 2.5, 7, 2.5, 4]),
-                                            width: et([16, 16, 19, 22, 19, 16]),
-                                            height: et([16, 10, 19, 10, 19, 16]),
-                                            rx: et([8, 5, 9.5, 5, 9.5, 8]),
+                                            x: T.enabled ? 4 : et([4, 4, 2.5, 1, 2.5, 4]),
+                                            y: T.enabled ? 4 : et([4, 7, 2.5, 7, 2.5, 4]),
+                                            width: T.enabled ? 16 : et([16, 16, 19, 22, 19, 16]),
+                                            height: T.enabled ? 16 : et([16, 10, 19, 10, 19, 16]),
+                                            rx: T.enabled ? 8 : et([8, 5, 9.5, 5, 9.5, 8]),
                                         }),
-                                        O && P($, q, X, T.enabled),
+                                        O && P($, q, X),
                                     ],
                                 }),
                             }),
