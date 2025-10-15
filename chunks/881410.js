959ae2683dad4f8a3fd8bf06241029102e3c9d27
DiscrapperManager <admin@minerpl.xyz>
2025-10-15 23:10:17 +0200

    canaryRelease: 458214
    Main scripts: 4
    Lazy Loaded Scripts: 1635
    Chunks:
     * Main Files: 14919
     * Scripts: 9086
    Strings: 22628
    Routes: 750
    Experiments Count: 424
    Apex Experiments Count: 99

--- chunks/881410.js
+++ chunks/881410.js
@@ -1,174 +1,172 @@
-n.d(t, {
-    wl: () => O,
-    yX: () => m,
+r.d(t, {
+    wl: () => j,
+    yX: () => y,
 }),
-    n(388685),
-    n(781311);
-var r = n(951288),
-    i = n(647438),
-    a = n(423802),
-    o = n(481060),
-    l = n(510231),
-    c = n(785717),
-    s = n(86419),
-    u = n(388032),
-    d = n(302627);
-function f(e) {
+    r(388685),
+    r(781311);
+var n = r(951288),
+    i = r(647438),
+    a = r(423802),
+    o = r(442837),
+    l = r(481060),
+    c = r(510231),
+    s = r(706454),
+    u = r(785717),
+    d = r(86419),
+    g = r(388032),
+    f = r(302627);
+function p(e) {
     for (var t = 1; t < arguments.length; t++) {
-        var n = null != arguments[t] ? arguments[t] : {},
-            r = Object.keys(n);
+        var r = null != arguments[t] ? arguments[t] : {},
+            n = Object.keys(r);
         "function" == typeof Object.getOwnPropertySymbols &&
-            (r = r.concat(
-                Object.getOwnPropertySymbols(n).filter(function (e) {
-                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+            (n = n.concat(
+                Object.getOwnPropertySymbols(r).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(r, e).enumerable;
                 }),
             )),
-            r.forEach(function (t) {
-                var r;
-                (r = n[t]),
+            n.forEach(function (t) {
+                var n;
+                (n = r[t]),
                     t in e
                         ? Object.defineProperty(e, t, {
-                              value: r,
+                              value: n,
                               enumerable: !0,
                               configurable: !0,
                               writable: !0,
                           })
-                        : (e[t] = r);
+                        : (e[t] = n);
             });
     }
     return e;
 }
-function g(e, t) {
+function b(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
             : (function (e, t) {
-                  var n = Object.keys(e);
+                  var r = Object.keys(e);
                   if (Object.getOwnPropertySymbols) {
-                      var r = Object.getOwnPropertySymbols(e);
-                      n.push.apply(n, r);
+                      var n = Object.getOwnPropertySymbols(e);
+                      r.push.apply(r, n);
                   }
-                  return n;
-              })(Object(t)).forEach(function (n) {
-                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+                  return r;
+              })(Object(t)).forEach(function (r) {
+                  Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r));
               }),
         e
     );
 }
-function p(e, t) {
+function m(e, t) {
     if (null == e) return {};
-    var n,
-        r,
+    var r,
+        n,
         i = (function (e, t) {
             if (null == e) return {};
-            var n,
-                r,
+            var r,
+                n,
                 i = {},
                 a = Object.keys(e);
-            for (r = 0; r < a.length; r++) (n = a[r]), t.indexOf(n) >= 0 || (i[n] = e[n]);
+            for (n = 0; n < a.length; n++) (r = a[n]), t.indexOf(r) >= 0 || (i[r] = e[r]);
             return i;
         })(e, t);
     if (Object.getOwnPropertySymbols) {
         var a = Object.getOwnPropertySymbols(e);
-        for (r = 0; r < a.length; r++)
-            (n = a[r]), !(t.indexOf(n) >= 0) && Object.prototype.propertyIsEnumerable.call(e, n) && (i[n] = e[n]);
+        for (n = 0; n < a.length; n++)
+            (r = a[n]), !(t.indexOf(r) >= 0) && Object.prototype.propertyIsEnumerable.call(e, r) && (i[r] = e[r]);
     }
     return i;
 }
-function b(e) {
-    var { widgetType: t, widget: n, children: b } = e,
-        m = p(e, ["widgetType", "widget", "children"]);
-    let [O] = (0, o.ynZ)(),
-        [y, j] = i.useState(""),
-        x = i.useRef(""),
-        v = i.useMemo(() => new Set(n.games.map((e) => e.applicationId)), [n.games]),
-        { trackUserProfileEditAction: h } = (0, c.KZ)(),
-        _ = i.useCallback(
+function O(e) {
+    var { widgetType: t, widget: r, children: O } = e,
+        y = m(e, ["widgetType", "widget", "children"]);
+    let [j] = (0, l.ynZ)(),
+        [x, h] = i.useState(""),
+        v = i.useRef(""),
+        _ = i.useMemo(() => new Set(r.games.map((e) => e.applicationId)), [r.games]),
+        { trackUserProfileEditAction: w } = (0, u.KZ)(),
+        P = (0, o.e7)([s.default], () => ["en-US", "en-GB"].includes(s.default.locale)),
+        I = i.useCallback(
             (e) => {
-                (0, s.ES)(t, { applicationId: e }),
-                    o.uvj.announce(u.intl.string(u.t.q0U3DA)),
-                    h({
+                (0, d.ES)(t, { applicationId: e }),
+                    l.uvj.announce(g.intl.string(g.t.q0U3DA)),
+                    w({
                         action: "GAME_ADDED",
                         gameId: e,
                         widgetEdited: t,
                     });
             },
-            [t, h],
+            [t, w],
         ),
-        { options: w, matchSorterOptions: P } = (0, l.h)(),
-        I = i.useCallback(
-            (e) => ("" === e.trim() ? w : (0, a.Lu)(w, e, g(f({}, P), { threshold: a.Lu.rankings.CONTAINS }))),
-            [w, P],
+        { options: S, matchSorterOptions: E } = (0, c.h)(),
+        T = i.useCallback(
+            (e) => ("" === e.trim() ? S : (0, a.Lu)(S, e, b(p({}, E), { threshold: a.Lu.rankings.CONTAINS }))),
+            [S, E],
         ),
-        S = i.useCallback(
+        D = i.useCallback(
             (e) => {
-                "" === y.trim() &&
+                "" === x.trim() &&
                     "" !== e.trim() &&
-                    h({
+                    w({
                         action: "GAME_SEARCH_SESSION_STARTED",
                         widgetEdited: t,
                         numCharacters: e.trim().length,
-                        numResults: I(e).length,
+                        numResults: T(e).length,
                     }),
-                    j(e),
-                    (x.current = e);
+                    h(e),
+                    (v.current = e);
             },
-            [y, h, t, I],
+            [x, w, t, T],
         ),
-        E = i.useMemo(
+        k = i.useMemo(
             () =>
-                "" !== y.trim()
-                    ? u.intl.format(u.t.ZoearK, {
-                          searchTerm: y.trim(),
-                          surveyUrl: "https://discord.sjc1.qualtrics.com/jfe/form/SV_0TIqLrxbsfJYS4C",
-                      })
-                    : u.intl.string(u.t.QwSXv7),
-            [y],
+                "" !== x.trim() && P ? g.intl.format(g.t.jhiTsL, { searchTerm: x.trim() }) : g.intl.string(g.t.QwSXv7),
+            [x, P],
         );
-    return (0, r.jsx)(
-        o.yRy,
-        g(f({}, m), {
+    return (0, n.jsx)(
+        l.yRy,
+        b(p({}, y), {
             onRequestOpen: () => {
-                h({
+                w({
                     action: "PRESS_ADD_GAME",
                     widgetEdited: t,
                 }),
-                    j(""),
-                    (x.current = "");
+                    h(""),
+                    (v.current = "");
             },
             onRequestClose: () => {
-                h({
+                w({
                     action: "GAME_SEARCH_SESSION_ENDED",
                     widgetEdited: t,
-                    numCharacters: x.current.trim().length,
-                    numResults: I(x.current).length,
+                    numCharacters: v.current.trim().length,
+                    numResults: T(v.current).length,
                 });
             },
             renderPopout: (e) => {
                 let { closePopout: t } = e;
-                return (0, r.jsx)(o.DBG, {
-                    className: d.gameSearchCombobox,
-                    placeholder: u.intl.string(u.t["5h0QOD"]),
+                return (0, n.jsx)(l.DBG, {
+                    className: f.gameSearchCombobox,
+                    placeholder: g.intl.string(g.t["5h0QOD"]),
                     autoFocus: !0,
-                    value: O,
+                    value: j,
                     onChange: (e) => {
-                        _(e), t();
+                        I(e), t();
                     },
                     multiSelect: !1,
                     maxVisibleItems: 7,
-                    emptyStateText: E,
+                    emptyStateText: k,
                     emptyStateHeader: "",
-                    onQueryChange: S,
+                    onQueryChange: D,
                     children: (e) =>
-                        I(e).map((e) =>
-                            (0, r.jsx)(
-                                o.lo1,
+                        T(e).map((e) =>
+                            (0, n.jsx)(
+                                l.lo1,
                                 {
-                                    disabled: v.has(e.value),
+                                    disabled: _.has(e.value),
                                     value: String(e.value),
-                                    children: (0, r.jsx)(o.lo1.Label, {
-                                        children: (0, r.jsx)(o.Text, {
+                                    children: (0, n.jsx)(l.lo1.Label, {
+                                        children: (0, n.jsx)(l.Text, {
                                             variant: "text-md/medium",
                                             color: "header-secondary",
                                             children: e.label,
@@ -180,36 +178,36 @@ function b(e) {
                         ),
                 });
             },
-            children: (e) => b(e),
+            children: (e) => O(e),
         }),
     );
 }
-function m(e) {
+function y(e) {
     var { disabled: t } = e,
-        n = p(e, ["disabled"]);
+        r = m(e, ["disabled"]);
     let a = i.useRef(null);
-    return (0, r.jsx)(
-        b,
-        g(
-            f(
+    return (0, n.jsx)(
+        O,
+        b(
+            p(
                 {
                     targetElementRef: a,
                     position: "bottom",
                     align: "center",
                 },
-                n,
+                r,
             ),
             {
                 children: (e) =>
-                    (0, r.jsx)(
-                        o.Button,
-                        f(
+                    (0, n.jsx)(
+                        l.Button,
+                        p(
                             {
                                 buttonRef: a,
                                 variant: "secondary",
                                 size: "sm",
-                                icon: o.qJs,
-                                text: u.intl.string(u.t.SgTOtb),
+                                icon: l.qJs,
+                                text: g.intl.string(g.t.SgTOtb),
                                 disabled: t,
                             },
                             e,
@@ -219,12 +217,12 @@ function m(e) {
         ),
     );
 }
-function O(e) {
+function j(e) {
     let t = i.useRef(null);
-    return (0, r.jsx)(
-        b,
-        g(
-            f(
+    return (0, n.jsx)(
+        O,
+        b(
+            p(
                 {
                     targetElementRef: t,
                     position: "right",
@@ -234,18 +232,18 @@ function O(e) {
             ),
             {
                 children: (e) =>
-                    (0, r.jsx)(
-                        o.P3F,
-                        g(
-                            f(
+                    (0, n.jsx)(
+                        l.P3F,
+                        b(
+                            p(
                                 {
                                     innerRef: t,
-                                    className: d.coverButton,
-                                    "aria-label": u.intl.string(u.t.SgTOtb),
+                                    className: f.coverButton,
+                                    "aria-label": g.intl.string(g.t.SgTOtb),
                                 },
                                 e,
                             ),
-                            { children: (0, r.jsx)(o.svS, { color: "currentColor" }) },
+                            { children: (0, n.jsx)(l.svS, { color: "currentColor" }) },
                         ),
                     ),
             },
