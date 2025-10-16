e00e5723784d54659fc0a5fc3d4adc40171a0209
DiscrapperManager <admin@minerpl.xyz>
2025-10-16 21:20:12 +0200

    canaryRelease: 458691
    Main scripts: 4
    Lazy Loaded Scripts: 1629
    Chunks:
     * Main Files: 14922
     * Scripts: 9078
    Strings: 22651
    Routes: 751
    Experiments Count: 426
    Apex Experiments Count: 102

--- chunks/370774.js
+++ chunks/370774.js
@@ -1,11 +1,11 @@
 n.d(t, {
-    D: () => y,
-    O4: () => v,
-    Tj: () => T,
-    YH: () => E,
-    bl: () => b,
-    j4: () => O,
-    ux: () => I,
+    D: () => O,
+    O4: () => I,
+    Tj: () => S,
+    YH: () => b,
+    bl: () => y,
+    j4: () => v,
+    ux: () => T,
 });
 var r = n(995638),
     i = n(843611),
@@ -15,72 +15,73 @@ var r = n(995638),
     l = n(768943),
     c = n(175006),
     u = n(313789),
-    d = n(592125),
-    f = n(430824);
+    d = n(526665),
+    f = n(592125),
+    _ = n(430824);
 n(375954);
-var _ = n(306680);
+var p = n(306680);
 n(594174), n(70956);
-var p = n(709054),
-    h = n(787879),
-    m = n(982183),
-    g = n(981631);
-function E(e) {
-    let { messageId: t, channelId: n, guildId: r, ReadStateStore_: i = _.ZP, GuildStore_: a = f.Z } = e;
+var h = n(709054),
+    m = n(787879),
+    g = n(982183),
+    E = n(981631);
+function b(e) {
+    let { messageId: t, channelId: n, guildId: r, ReadStateStore_: i = p.ZP, GuildStore_: a = _.Z } = e;
     if (!i.hasUnread(n)) return !1;
     let o = a.getGuild(r);
     if (null == o || null == o.joinedAt) return !1;
     let s = i.ackMessageId(n);
     if (null == s) {
         let e = a.getGuild(r);
-        null != e && null != e.joinedAt && (s = p.default.fromTimestamp(e.joinedAt.getTime()));
+        null != e && null != e.joinedAt && (s = h.default.fromTimestamp(e.joinedAt.getTime()));
     }
-    return p.default.compare(t, s) > 0;
+    return h.default.compare(t, s) > 0;
 }
-function b(e) {
+function y(e) {
     let { id: t } = e,
-        n = (0, r.default)(new Date(), p.default.extractTimestamp(t));
-    return 0 === n ? m.KZ.TODAY : 1 === n ? m.KZ.YESTERDAY : m.KZ.OLDER;
+        n = (0, r.default)(new Date(), h.default.extractTimestamp(t));
+    return 0 === n ? g.KZ.TODAY : 1 === n ? g.KZ.YESTERDAY : g.KZ.OLDER;
 }
-function y() {
+function O() {
     let { pathname: e } = (0, i.TH)();
-    return e.startsWith(g.Z5c.CHANNEL(g.STv));
+    return e.startsWith(E.Z5c.CHANNEL(E.STv));
 }
-function O(e) {
+function v(e) {
     {
         let { openUserSettings: t } = n(518596);
-        t(u.n.NOTIFICATIONS_PANEL, {
-            section: g.oAB.NOTIFICATIONS,
+        t((0, d.KV)("openNotificationSettings") ? u.n.NOTIFICATIONS_PANEL : u.n.LEGACY_NOTIFICATIONS_SETTINGS_PANEL, {
+            section: E.oAB.NOTIFICATIONS,
             analyticsLocations: e,
         });
     }
 }
-function v() {
-    let e = (0, a.Wu)([h.Z], () => {
+function I() {
+    let e = (0, a.Wu)([m.Z], () => {
             var e;
-            return null != (e = h.Z.getNotifyingChannelIds()) ? e : [];
+            return null != (e = m.Z.getNotifyingChannelIds()) ? e : [];
         }),
-        t = (0, a.Wu)([_.ZP], () => e.filter((e) => _.ZP.hasUnread(e)), [e]),
+        t = (0, a.Wu)([p.ZP], () => e.filter((e) => p.ZP.hasUnread(e)), [e]),
         n = (0, a.Wu)([o.ZP], () => {
             var e;
             return null != (e = o.ZP.getSettingsFilteredMentions()) ? e : [];
         });
     return {
-        unreadRecentMentionsCount: (0, a.e7)([_.ZP, d.Z, f.Z], () => {
+        unreadRecentMentionsCount: (0, a.e7)([p.ZP, f.Z, _.Z], () => {
             var e, t;
             return null !=
                 (t =
                     null == n ||
                     null ==
                         (e = n.filter((e) => {
-                            let t = d.Z.getBasicChannel(e.channel_id);
+                            let t = f.Z.getBasicChannel(e.channel_id);
                             return (
                                 null != t &&
-                                E({
+                                b({
                                     messageId: e.id,
                                     channelId: e.channel_id,
                                     guildId: t.guild_id,
-                                    ReadStateStore_: _.ZP,
-                                    GuildStore_: f.Z,
+                                    ReadStateStore_: p.ZP,
+                                    GuildStore_: _.Z,
                                 })
                             );
                         }))
@@ -92,11 +93,11 @@ function v() {
         unreadChannelIds: t,
     };
 }
-function I() {
+function T() {
     let { enabled: e, inInbox: t } = s.Z.useExperiment({ location: "NotificationsInboxUtils" }),
         n = (0, a.e7)([l.Z], () => l.Z.getSavedMessageCount());
     return e && t && (n > 0 || (0, c.Z)());
 }
-function T(e, t) {
-    return null != t && e.channelId === t.channelId && p.default.compare(e.id, t.messageId) >= 0;
+function S(e, t) {
+    return null != t && e.channelId === t.channelId && h.default.compare(e.id, t.messageId) >= 0;
 }
