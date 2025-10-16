cc62e12e9bef74eaaea4d440948904e7d1044c7a
DiscrapperManager <admin@minerpl.xyz>
2025-10-16 02:01:09 +0200

    canaryRelease: 458371
    Main scripts: 4
    Lazy Loaded Scripts: 1631
    Chunks:
     * Main Files: 14926
     * Scripts: 9080
    Strings: 22646
    Routes: 750
    Experiments Count: 425
    Apex Experiments Count: 99

--- chunks/389458.js
+++ chunks/389458.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => P }), n(388685), n(704826), n(35282), n(642613), n(368063);
+n.d(t, { Z: () => w }), n(388685), n(704826), n(35282), n(642613), n(368063);
 var r,
     i = n(442837),
     a = n(570140),
@@ -18,118 +18,119 @@ function c(e, t, n) {
         e
     );
 }
-let u = 250,
-    d = 0.001,
-    f = 0.01,
-    _ = 10,
-    p = o.t.createEstimate(d, f),
-    h = new Map(),
-    m = 0;
-function g(e) {
+let u = 5,
+    d = 250,
+    f = 0.001,
+    _ = 0.01,
+    p = 10,
+    h = o.t.createEstimate(f, _),
+    m = new Map(),
+    g = 0;
+function E(e) {
     return e.replace(/[^\w\s]|\b\d+\b/g, "");
 }
-function E(e) {
-    return g(e)
+function b(e) {
+    return E(e)
         .replace(
             /(https?:\/\/[^\s]+|ftp:\/\/[^\s]+|www\.[^\s]+|[^\s]*\.[a-z]{2,}\/[^\s]*|[^\s]*\.(?:com|org|net)[^\s]*|<a?:(\w+):(\d+)>|<@(?:!|&)?\d+>)/g,
             "",
         )
         .split(/\s+/);
 }
-function b(e) {
+function y(e) {
     return Array.from(e.entries()).sort((e, t) => t[1] - e[1]);
 }
-function y(e, t) {
+function O(e, t) {
     if (e.size <= t) return;
-    let n = b(e);
+    let n = y(e);
     e.clear();
     for (let r = 0; r < t; r++) {
         let [t, i] = n[r];
         e.set(t, i);
     }
 }
-function O() {
+function v() {
     let e = 1000,
         t = 5000,
         n = 0.05;
-    return m < 1000 || Math.random() < (1 - n) * Math.max(0, 1 - (m - e) / (t - e)) + n;
+    return g < 1000 || Math.random() < (1 - n) * Math.max(0, 1 - (g - e) / (t - e)) + n;
 }
-function v(e) {
+function I(e) {
     let t = !1;
     for (let n of e)
-        if (O())
-            for (let e of E(n.content.toLowerCase())) {
-                if (0 === e.length || e.length > 5) continue;
-                p.update(e);
-                let n = p.query(e);
-                h.set(e, n), n > m && (m = n), (t = !0);
+        if (v())
+            for (let e of b(n.content.toLowerCase())) {
+                if (0 === e.length || e.length > u) continue;
+                h.update(e);
+                let n = h.query(e);
+                m.set(e, n), n > g && (g = n), (t = !0);
             }
-    y(h, u), t && R.emitChange();
+    O(m, d), t && P.emitChange();
 }
-function I() {
+function T() {
     let e = l.V.getCurrentConfig({ location: "WordFrequencyStore" }, { autoTrackExposure: !1 }).enabled,
         t = (0, s.kB)("WordFrequencyStore", { autoTrackExposure: !1 }).enabled;
     return e || t;
 }
-function T(e) {
+function S(e) {
     return (
-        !!I() &&
+        !!T() &&
         (requestIdleCallback(() => {
-            v(e);
+            I(e);
         }),
         !1)
     );
 }
-let S = (e) => {
+let A = (e) => {
         let { messages: t } = e;
-        return T(t);
-    },
-    A = (e) => {
-        let { message: t } = e;
-        return T([t]);
+        return S(t);
     },
     C = (e) => {
-        (p = o.t.createEstimate(d, f)), h.clear(), (m = 0);
+        let { message: t } = e;
+        return S([t]);
+    },
+    N = (e) => {
+        (h = o.t.createEstimate(f, _)), m.clear(), (g = 0);
     };
-class N extends (r = i.ZP.PersistedStore) {
+class R extends (r = i.ZP.PersistedStore) {
     initialize(e) {
         if (null != e) {
             var t;
             if (e.wordSketchData)
                 try {
-                    e.wordSketchData && (p = o.t.fromJSON(e.wordSketchData));
+                    e.wordSketchData && (h = o.t.fromJSON(e.wordSketchData));
                 } catch (e) {
-                    p = o.t.createEstimate(d, f);
+                    h = o.t.createEstimate(f, _);
                 }
-            h = new Map(null != (t = e.wordCounts) ? t : []);
+            m = new Map(null != (t = e.wordCounts) ? t : []);
         }
-        let n = b(h);
+        let n = y(m);
         if (n.length > 0) {
             let [, e] = n[0];
-            m = e;
+            g = e;
         }
     }
     getState() {
         return {
-            wordCounts: b(h),
-            wordSketchData: p.toJSON(),
+            wordCounts: y(m),
+            wordSketchData: h.toJSON(),
         };
     }
     getMaxWordCount() {
-        return m;
+        return g;
     }
     getAllWordsSorted() {
-        return b(h);
+        return y(m);
     }
     isFrequentlyUsedWord(e) {
-        let t = h.get(e.toLowerCase());
-        return null != t && t > _;
+        let t = m.get(e.toLowerCase());
+        return null != t && t > p;
     }
 }
-c(N, "displayName", "WordFrequencyStore"), c(N, "persistKey", "WordFrequencyStore");
-let R = new N(a.Z, {
-        LOAD_MESSAGES_SUCCESS: S,
-        MESSAGE_CREATE: A,
-        DEV_TOOLS_WORD_FREQUENCY_RESET: C,
+c(R, "displayName", "WordFrequencyStore"), c(R, "persistKey", "WordFrequencyStore");
+let P = new R(a.Z, {
+        LOAD_MESSAGES_SUCCESS: A,
+        MESSAGE_CREATE: C,
+        DEV_TOOLS_WORD_FREQUENCY_RESET: N,
     }),
-    P = R;
+    w = P;
