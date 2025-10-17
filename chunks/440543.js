31b47dd6518e6848e3aaffe78246229b5342cafc
DiscrapperManager <admin@minerpl.xyz>
2025-10-18 00:45:11 +0200

    canaryRelease: 459506
    Main scripts: 4
    Lazy Loaded Scripts: 1627
    Chunks:
     * Main Files: 14931
     * Scripts: 9072
    Strings: 22631
    Routes: 753
    Experiments Count: 423
    Apex Experiments Count: 105

--- chunks/440543.js
+++
@@ -1,9 +0,0 @@
-e.exports = {
-    wrapper: "wrapper__3820c",
-    content: "content__3820c",
-    nav: "nav__3820c",
-    banner: "banner__3820c",
-    pfxBanner: "pfxBanner__3820c",
-    nameplateBanner: "nameplateBanner__3820c",
-    bundlesBanner: "bundlesBanner__3820c",
-};
---
+++ chunks/511130.js
@@ -0,0 +1,9 @@
+e.exports = {
+    wrapper: "wrapper__7a068",
+    content: "content__7a068",
+    nav: "nav__7a068",
+    banner: "banner__7a068",
+    pfxBanner: "pfxBanner__7a068",
+    nameplateBanner: "nameplateBanner__7a068",
+    bundlesBanner: "bundlesBanner__7a068",
+};
