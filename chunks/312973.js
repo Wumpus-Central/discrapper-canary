1b365b1b7e11e3a7d9b4a671006978133e6e2227
DiscrapperManager <admin@minerpl.xyz>
2025-10-17 22:29:12 +0200

    canaryRelease: 459363
    Main scripts: 4
    Lazy Loaded Scripts: 1628
    Chunks:
     * Main Files: 14938
     * Scripts: 9083
    Strings: 22674
    Routes: 752
    Experiments Count: 426
    Apex Experiments Count: 105

---
+++ chunks/312973.js
@@ -0,0 +1,11 @@
+n.d(t, { $: () => i });
+let r = (0, n(722733).Z)({
+    kind: "user",
+    name: "2025-10-fetch-promotions-endpoint",
+    defaultConfig: { enabled: !1 },
+    variations: { 1: { enabled: !0 } },
+});
+function i(e) {
+    let { location: t } = e;
+    return r.getConfig({ location: t }).enabled;
+}
--- chunks/349540.js
+++ chunks/349540.js
@@ -1,12 +1,13 @@
-n.d(t, { Z: () => d }), n(388685);
+n.d(t, { Z: () => f }), n(388685);
 var r = n(147913),
     i = n(594174),
     a = n(74538);
 n(367074);
 var o = n(163684),
-    s = n(748770),
-    l = n(474936);
-function c(e, t, n) {
+    s = n(312973),
+    l = n(748770),
+    c = n(474936);
+function u(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -19,32 +20,33 @@ function c(e, t, n) {
         e
     );
 }
-class u extends r.Z {
+class d extends r.Z {
     async onPostConnectionOpen() {
         this.maybeFetchActiveOutboundPromotions(), await this.maybeFetchBogoPromotion();
     }
     maybeFetchActiveOutboundPromotions() {
         let e = i.default.getCurrentUser(),
-            t = a.ZP.isPremiumExactly(e, l.PremiumTypes.TIER_2),
-            n = o.g.getCurrentConfig(
+            t = a.ZP.isPremiumExactly(e, c.PremiumTypes.TIER_2),
+            n = (0, s.$)({ location: "maybeFetchActiveOutboundPromotions" }),
+            r = o.g.getCurrentConfig(
                 { location: "maybeFetchActiveOutboundPromotions" },
                 {
                     autoTrackExposure: !1,
                     disable: t,
                 },
             ).enabled;
-        (t || n) && (0, s.vM)();
+        (n || t || r) && (0, l.vM)();
     }
     constructor(...e) {
         super(...e),
-            c(this, "actions", {
+            u(this, "actions", {
                 POST_CONNECTION_OPEN: this.onPostConnectionOpen.bind(this),
                 EXPERIMENTS_FETCH_SUCCESS: this.onPostConnectionOpen.bind(this),
             }),
-            c(this, "maybeFetchBogoPromotion", async () => {
+            u(this, "maybeFetchBogoPromotion", async () => {
                 let e = i.default.getCurrentUser();
-                a.ZP.isPremiumExactly(e, l.PremiumTypes.TIER_2);
+                a.ZP.isPremiumExactly(e, c.PremiumTypes.TIER_2);
             });
     }
 }
-let d = new u();
+let f = new d();
