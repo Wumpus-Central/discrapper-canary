f7715ee54849993ac980fa1f101bb23c0f75676f
DiscrapperManager <admin@minerpl.xyz>
2025-10-18 01:57:33 +0200

    canaryRelease: 459564
    Main scripts: 4
    Lazy Loaded Scripts: 1627
    Chunks:
     * Main Files: 14933
     * Scripts: 9072
    Strings: 22633
    Routes: 757
    Experiments Count: 423
    Apex Experiments Count: 106

--- chunks/357.js
+++ chunks/357.js
@@ -1,8 +1,9 @@
-n.d(t, { Z: () => s });
+n.d(t, { Z: () => l });
 var r = n(581282),
-    i = n(8853),
-    a = n(81825);
-function o(e, t, n) {
+    i = n(647943),
+    a = n(8853),
+    o = n(81825);
+function s(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -15,19 +16,23 @@ function o(e, t, n) {
         e
     );
 }
-class s extends a.Z {
+class l extends o.Z {
     static createFromServer(e) {
-        return new s({
+        let t = new TextDecoder("utf-8", { ignoreBOM: !0 });
+        return new l({
             id: e.id,
             componentType: e.component_type,
-            properties: i.t.fromBinary((0, r.c)(e.properties)),
+            properties: a.t.fromBinary((0, r.c)(e.properties), {
+                readUnknownField: !0,
+                readerFactory: (e) => new i.o(e, t),
+            }),
         });
     }
     constructor(e) {
         super(),
-            o(this, "id", void 0),
-            o(this, "componentType", void 0),
-            o(this, "properties", void 0),
+            s(this, "id", void 0),
+            s(this, "componentType", void 0),
+            s(this, "properties", void 0),
             (this.id = e.id),
             (this.componentType = e.componentType),
             (this.properties = e.properties);
