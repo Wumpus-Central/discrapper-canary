507a0569b72fea9066e678b6ab9a975cd57dfb5c
DiscrapperManager <admin@minerpl.xyz>
2025-10-23 21:58:52 +0200

    canaryRelease: 461188
    Main scripts: 4
    Lazy Loaded Scripts: 1628
    Chunks:
     * Main Files: 14944
     * Scripts: 9079
    Strings: 22647
    Routes: 757
    Experiments Count: 422
    Apex Experiments Count: 108

--- chunks/205557.js
+++ chunks/205557.js
@@ -1,4 +1,4 @@
-n.r(t), n.d(t, { default: () => j });
+n.r(t), n.d(t, { default: () => T });
 var r = n(951288),
     i = n(647438),
     l = n(399606),
@@ -12,73 +12,75 @@ var r = n(951288),
     f = n(27457),
     p = n(937995),
     w = n(592125),
-    y = n(358221),
+    y = n(944486),
+    I = n(358221),
     m = n(598006),
-    I = n(768864),
-    C = n(354459),
-    O = n(981631),
-    b = n(388032),
-    g = n(542123);
-let P = [s._];
-function j(e) {
+    C = n(768864),
+    O = n(354459),
+    b = n(981631),
+    g = n(388032),
+    P = n(542123);
+let j = [s._];
+function T(e) {
     let { windowKey: t, channelId: n, participantId: s } = e,
-        j = (0, l.e7)([w.Z], () => w.Z.getChannel(n)),
-        T = (0, l.e7)([y.Z], () => y.Z.getParticipant(n, s), [n, s]),
-        E = null != j && null != T;
+        T = (0, l.e7)([w.Z], () => w.Z.getChannel(n)),
+        E = (0, l.e7)([I.Z], () => I.Z.getParticipant(n, s), [n, s]),
+        M = null != T && null != E,
+        v = (0, l.e7)([y.Z], () => y.Z.getVoiceChannelId() === n);
     i.useEffect(() => {
-        E || d.xv(t);
-    }, [E, t]);
-    let M = i.useCallback(
+        (M && v) || d.xv(t);
+    }, [M, v, t]);
+    let U = i.useCallback(
             (e) => {
                 let { unmountWindow: t } = e;
                 a.Z.returnParticipant(n, s), t();
             },
             [n, s],
         ),
-        v = (0, I.B)({
-            channel: j,
-            appContext: O.IlC.POPOUT,
+        x = (0, C.B)({
+            channel: T,
+            appContext: b.IlC.POPOUT,
         }),
-        U = i.useCallback(() => {
-            (0, C._5)(T) && (0, o.rn)(T.stream, { forceMultiple: !0 });
-        }, [T]),
-        x = i.useMemo(
+        D = i.useCallback(() => {
+            (0, O._5)(E) && (0, o.rn)(E.stream, { forceMultiple: !0 });
+        }, [E]),
+        S = i.useMemo(
             () =>
-                null == T
-                    ? b.intl.string(b.t.lfzt24)
-                    : T.type === C.fO.USER
-                      ? T.user.username
-                      : T.type === C.fO.STREAM
-                        ? b.intl.formatToPlainString(b.t["/DC1y9"], { username: T.user.username })
-                        : b.intl.string(b.t["8vlBo7"]),
-            [T],
+                null == E
+                    ? g.intl.string(g.t.lfzt24)
+                    : E.type === O.fO.USER
+                      ? E.user.username
+                      : E.type === O.fO.STREAM
+                        ? g.intl.formatToPlainString(g.t["/DC1y9"], { username: E.user.username })
+                        : g.intl.string(g.t["8vlBo7"]),
+            [E],
         ),
-        D = (0, h.Z)();
+        R = (0, h.Z)();
     return (0, r.jsx)(c.Z, {
         withTitleBar: !0,
-        keybinds: P,
+        keybinds: j,
         windowKey: t,
-        title: x,
+        title: S,
         channelId: n,
-        onBeforeUnload: M,
+        onBeforeUnload: U,
         hideModals: !0,
-        children: E
+        children: M
             ? (0, r.jsx)(u.Z.Provider, {
-                  value: j.guild_id,
+                  value: T.guild_id,
                   children: (0, r.jsx)(p.ZP, {
                       timeout: 2000,
                       children: () =>
                           (0, r.jsx)(f.ZP, {
-                              className: g.tile,
-                              participant: T,
-                              channel: j,
-                              width: D.width,
+                              className: P.tile,
+                              participant: E,
+                              channel: T,
+                              width: R.width,
                               inCall: !0,
                               popoutType: m.P.CALL_TILE,
                               focused: !0,
                               noBorder: !0,
-                              onContextMenu: v,
-                              onClick: U,
+                              onContextMenu: x,
+                              onClick: D,
                           }),
                   }),
               })
