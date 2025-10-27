3dc3d3199d0623f901a859d6bddddb92a7e4d70e
DiscrapperManager <admin@minerpl.xyz>
2025-10-27 18:04:29 +0100

    canaryRelease: 461885
    Main scripts: 4
    Lazy Loaded Scripts: 1630
    Chunks:
     * Main Files: 14947
     * Scripts: 9125
    Strings: 22647
    Routes: 757
    Experiments Count: 423
    Apex Experiments Count: 109

--- chunks/73249.js
+++ chunks/73249.js
@@ -52,9 +52,9 @@ function x(e) {
 function P() {
     (0, c.Mr3)(j.Q);
 }
-let R = r.forwardRef(function (e, t) {
-    var { tooltipText: n } = e,
-        r = (function (e, t) {
+function R(e) {
+    var { tooltipText: t } = e,
+        n = (function (e, t) {
             if (null == e) return {};
             var n,
                 i,
@@ -76,22 +76,22 @@ let R = r.forwardRef(function (e, t) {
             return r;
         })(e, ["tooltipText"]);
     return (0, i.jsx)(s.u, {
-        text: n,
+        text: t,
         position: "bottom",
+        asContainer: !0,
         children: (0, i.jsx)(
             c.hU,
             x(
                 {
-                    buttonRef: t,
                     variant: "icon-only",
-                    "aria-label": n,
+                    "aria-label": t,
                     size: "sm",
                 },
-                r,
+                n,
             ),
         ),
     });
-});
+}
 function L() {
     let { zoomed: e, setZoomed: t } = (0, v.Y)();
     return (0, i.jsx)(R, {
@@ -352,7 +352,7 @@ function Z(e) {
               children: (e) => {
                   let { onClick: t } = e;
                   return (0, i.jsx)(R, {
-                      ref: a,
+                      buttonRef: a,
                       tooltipText: C.intl.string(C.t["UKOtz+"]),
                       onClick: () => {
                           (0, S.yg)(S.uG.MORE_BUTTON_PRESSED), s(!o);
