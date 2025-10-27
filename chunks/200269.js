e130deddad4c7208b77ec77b8a005a5d159ca596
DiscrapperManager <admin@minerpl.xyz>
2025-10-27 22:16:21 +0100

    canaryRelease: 462057
    Main scripts: 4
    Lazy Loaded Scripts: 1629
    Chunks:
     * Main Files: 15150
     * Scripts: 9122
    Strings: 22656
    Routes: 757
    Experiments Count: 421
    Apex Experiments Count: 110

--- chunks/200269.js
+++ chunks/200269.js
@@ -1,9 +1,8 @@
 n.d(t, {
-    H: () => l,
-    x: () => u,
-    y: () => s,
-}),
-    n(388685);
+    H: () => u,
+    x: () => d,
+    y: () => c,
+});
 var r = n(951288),
     i = n(647438);
 function a(e, t, n) {
@@ -35,59 +34,46 @@ function o(e) {
     }
     return e;
 }
-function s(e) {
+function s(e, t) {
+    return {
+        getLevelAndMarkUsed: () => ((t.current = !0), e),
+        consumed: t,
+        level: e,
+    };
+}
+let l = i.createContext(s(2, { current: !1 }));
+function c(e) {
     let { component: t, children: n, forceLevel: a } = e,
-        o = i.useContext(c),
-        [s, l] = i.useState(null != t),
-        u = 1,
-        d = 1;
-    null != a
-        ? null != t
-            ? ((u = a), (d = a + 1))
-            : (d = a)
-        : null != t
-          ? ((u = o.level), (d = o.level + 1))
-          : (d = o.level + +!!s);
-    let f = i.useMemo(
-            () => ({
-                level: u,
-                getLevelAndMarkUsed: () => (l(!0), u),
-            }),
-            [u],
-        ),
-        _ = i.useMemo(
-            () => ({
-                level: d,
-                getLevelAndMarkUsed: () => (l(!0), d),
-            }),
-            [d],
-        );
+        o = i.useContext(l),
+        c = null != a ? a : o.level,
+        u = c;
+    (null != t || (null == a && o.consumed.current)) && u++;
+    let d = i.useRef(!1),
+        f = i.useRef(!1),
+        _ = i.useMemo(() => s(c, d), [c]),
+        p = i.useMemo(() => s(u, f), [u]);
     return (0, r.jsxs)(r.Fragment, {
         children: [
             null != t
-                ? (0, r.jsx)(c.Provider, {
-                      value: f,
+                ? (0, r.jsx)(l.Provider, {
+                      value: _,
                       children: t,
                   })
                 : null,
-            (0, r.jsx)(c.Provider, {
-                value: _,
+            (0, r.jsx)(l.Provider, {
+                value: p,
                 children: n,
             }),
         ],
     });
 }
-function l(e) {
-    let t = u(),
+function u(e) {
+    let t = d(),
         n = "h".concat(Math.min(t, 6));
     return (0, r.jsx)(n, o({}, t > 6 ? { "data-excessive-heading-level": t } : {}, e));
 }
-let c = i.createContext({
-    getLevelAndMarkUsed: () => 2,
-    level: 2,
-});
-function u() {
-    let { getLevelAndMarkUsed: e } = i.useContext(c);
+function d() {
+    let { getLevelAndMarkUsed: e } = i.useContext(l);
     return e();
 }
-c.displayName = "HeadingLevelContext";
+l.displayName = "HeadingLevelContext";
