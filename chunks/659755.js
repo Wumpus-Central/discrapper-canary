57b2fb32bb201e31be51c42b740260bad75fde34
DiscrapperManager <admin@minerpl.xyz>
2025-10-28 19:42:57 +0100

    canaryRelease: 462492
    Main scripts: 4
    Lazy Loaded Scripts: 1633
    Chunks:
     * Main Files: 14912
     * Scripts: 9388
    Strings: 22680
    Routes: 759
    Experiments Count: 421
    Apex Experiments Count: 111

--- chunks/659755.js
+++ chunks/659755.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => O }), n(388685);
+n.d(t, { Z: () => S }), n(388685);
 var r = n(951288),
     l = n(120356),
     i = n.n(l),
@@ -11,37 +11,38 @@ var r = n(951288),
     g = n(429368),
     p = n(724994),
     f = n(786040),
-    C = n(616066),
-    h = n(58201),
-    _ = n(201964),
-    m = n(1327);
-let b = new Set([o.Z.BUNDLE, o.Z.NAMEPLATE, o.Z.PROFILE_EFFECT]),
-    v = (e) => {
+    C = n(508925),
+    h = n(616066),
+    _ = n(58201),
+    m = n(201964),
+    b = n(1327);
+let v = new Set([o.Z.BUNDLE, o.Z.NAMEPLATE, o.Z.PROFILE_EFFECT]),
+    E = (e) => {
         let { product: t, isCardHovered: n } = e,
             l = (0, f.rC)(t),
             { isPurchased: o, isPartiallyOwnedBundle: s } = (0, p.L)(t),
-            a = null != l && b.has(l);
+            a = null != l && v.has(l);
         return (0, r.jsxs)(r.Fragment, {
             children: [
                 (0, r.jsx)("div", {
-                    className: i()(m.productPreview, {
-                        [m.faded]: (o || s) && !n,
-                        [m.fullPreview]: a,
+                    className: i()(b.productPreview, {
+                        [b.faded]: (o || s) && !n,
+                        [b.fullPreview]: a,
                     }),
-                    children: (0, r.jsx)(E, {
+                    children: (0, r.jsx)(x, {
                         product: t,
                         isCardHovered: n,
                     }),
                 }),
-                o && (0, r.jsx)(x, { hidden: n }),
+                o && (0, r.jsx)(O, { hidden: n }),
             ],
         });
     },
-    E = (e) => {
+    x = (e) => {
         let { product: t, isCardHovered: n } = e,
             l = (0, a.e7)([d.default], () => d.default.getCurrentUser()),
             i = (0, f.rC)(t),
-            s = (0, _.o)(t);
+            s = (0, m.o)(t);
         switch (i) {
             case o.Z.PROFILE_EFFECT:
                 return (0, r.jsx)(u.Z, {
@@ -51,7 +52,7 @@ let b = new Set([o.Z.BUNDLE, o.Z.NAMEPLATE, o.Z.PROFILE_EFFECT]),
                     removeSetHeight: !0,
                 });
             case o.Z.AVATAR_DECORATION:
-                return (0, r.jsx)(C.R, {
+                return (0, r.jsx)(h.R, {
                     item: s,
                     user: l,
                     isHighlighted: n,
@@ -60,28 +61,33 @@ let b = new Set([o.Z.BUNDLE, o.Z.NAMEPLATE, o.Z.PROFILE_EFFECT]),
                 });
             case o.Z.NAMEPLATE:
             case o.Z.BUNDLE:
+                return null;
             case o.Z.EXTERNAL_SKU:
+                return (0, r.jsx)(C.b, {
+                    product: t,
+                    animationState: n ? "on" : "off",
+                });
             default:
                 return null;
         }
     },
-    x = (e) => {
+    O = (e) => {
         let { hidden: t } = e;
         return (0, r.jsx)(s.sV5, {
             size: "custom",
             color: c.TVs.colors.INTERACTIVE_ACTIVE,
             width: 40,
             height: 40,
-            className: i()(m.productPreviewIconOverlay, { [m.hidden]: t }),
+            className: i()(b.productPreviewIconOverlay, { [b.hidden]: t }),
         });
     },
-    O = (e) => {
+    S = (e) => {
         let { skuId: t, isCardHovered: n } = e,
             l = (0, f.LJ)(t),
             i = (0, g.o)(l);
         if (null == l) return null;
-        let o = (0, h.W)(l, i);
-        return (0, r.jsx)(v, {
+        let o = (0, _.W)(l, i);
+        return (0, r.jsx)(E, {
             product: o,
             isCardHovered: n,
         });
