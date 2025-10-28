803826dba6919b5ee955087cb244b027bef23c47
DiscrapperManager <admin@minerpl.xyz>
2025-10-28 21:43:29 +0100

    canaryRelease: 462594
    Main scripts: 4
    Lazy Loaded Scripts: 1632
    Chunks:
     * Main Files: 14911
     * Scripts: 9392
    Strings: 22682
    Routes: 760
    Experiments Count: 421
    Apex Experiments Count: 111

--- chunks/479713.js
+++ chunks/479713.js
@@ -1,11 +1,18 @@
-n.d(t, { P: () => a });
+n.d(t, { P: () => s }), n(804061), n(704826), n(35282);
 var r = n(830121),
-    l = n(388032);
-function a(e, t, n) {
-    if (null != (0, r.el)(e)) return e;
-    let a = l.intl.formatToMarkdownString(l.t.dZJpdG, {
+    l = n(591759),
+    a = n(388032);
+let i = [r.el];
+function s(e, t, n) {
+    let r = !1,
+        s = e.replaceAll(l.Z.URL_REGEX, (e) => {
+            let t = i.some((t) => null != t(e));
+            return t && (r = !0), t ? e : "`".concat(e, "`");
+        });
+    if (r) return s;
+    let u = a.intl.formatToMarkdownString(a.t.dZJpdG, {
         applicationName: t.name,
         link: n,
     });
-    return "".concat(e, "\n\n").concat(a);
+    return "".concat(s, "\n\n").concat(u);
 }
