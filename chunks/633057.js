754dd1374d234affb4c92114fce145a31cf2c944
DiscrapperManager <admin@minerpl.xyz>
2025-10-29 20:58:07 +0100

    canaryRelease: 463050
    Main scripts: 4
    Lazy Loaded Scripts: 1625
    Chunks:
     * Main Files: 15169
     * Scripts: 9136
    Strings: 22687
    Routes: 761
    Experiments Count: 421
    Apex Experiments Count: 112

--- chunks/633057.js
+++ chunks/633057.js
@@ -1,60 +1,20 @@
-e.d(n, { default: () => l });
-var i = e(951288);
-e(647438);
-var s = e(481060),
-    a = e(388032),
-    o = e(881873),
-    r = e(134519);
-function l(t) {
-    let { transitionState: n, onClose: e } = t;
-    return (0, i.jsxs)(s.Y0X, {
-        transitionState: n,
-        className: o.root,
-        parentComponent: "InsufficientInvitePermission",
-        children: [
-            (0, i.jsxs)(s.xBx, {
-                separator: !1,
-                className: o.noScroll,
-                children: [
-                    (0, i.jsx)(s.olH, {
-                        className: o.closeButton,
-                        onClick: e,
-                    }),
-                    (0, i.jsx)("div", {
-                        className: o.imageContainer,
-                        children: (0, i.jsx)("img", {
-                            src: r,
-                            className: o.image,
-                            alt: "",
-                        }),
-                    }),
-                ],
-            }),
-            (0, i.jsx)(s.hzk, {
-                children: (0, i.jsxs)("div", {
-                    className: o.content,
-                    children: [
-                        (0, i.jsx)(s.Heading, {
-                            variant: "heading-xl/semibold",
-                            className: o.title,
-                            children: a.intl.string(a.t.xSV3nU),
-                        }),
-                        (0, i.jsx)(s.Text, {
-                            variant: "text-md/normal",
-                            className: o.text,
-                            children: a.intl.string(a.t.jPMGbB),
-                        }),
-                        (0, i.jsx)("div", {
-                            className: o.buttonContainer,
-                            children: (0, i.jsx)(s.Button, {
-                                variant: "primary",
-                                text: a.intl.string(a.t["NX+WJN"]),
-                                onClick: e,
-                            }),
-                        }),
-                    ],
-                }),
-            }),
+n.d(i, { default: () => a });
+var s = n(951288);
+n(647438);
+var e = n(159691),
+    r = n(388032);
+function a(t) {
+    let { transitionState: i, onClose: n } = t;
+    return (0, s.jsx)(e.u_l, {
+        title: r.intl.string(r.t.xSV3nU),
+        subtitle: r.intl.string(r.t.jPMGbB),
+        actions: [
+            {
+                text: r.intl.string(r.t["NX+WJN"]),
+                onClick: n,
+            },
         ],
+        transitionState: i,
+        onClose: n,
     });
 }
