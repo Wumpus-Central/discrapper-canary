8ff14e200ae76fa863d7504ddc3cb971aa5a41d3
DiscrapperManager <admin@minerpl.xyz>
2025-10-30 00:59:22 +0100

    canaryRelease: 463268
    Main scripts: 4
    Lazy Loaded Scripts: 1626
    Chunks:
     * Main Files: 15156
     * Scripts: 9145
    Strings: 22688
    Routes: 761
    Experiments Count: 416
    Apex Experiments Count: 115

--- chunks/874139.js
+++ chunks/874139.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => h }), n(539854);
+n.d(t, { Z: () => p }), n(539854);
 var r = n(951288),
     i = n(647438),
     l = n(481060),
@@ -8,9 +8,9 @@ var r = n(951288),
     c = n(5337),
     d = n(388032),
     u = n(815743);
-function h(e) {
-    let { wishlistItems: t = [], profileOwner: n, onClick: h } = e,
-        { analyticsLocations: p } = (0, a.ZP)(),
+function p(e) {
+    let { wishlistItems: t = [], profileOwner: n, onClick: p } = e,
+        { analyticsLocations: h } = (0, a.ZP)(),
         { cards: f, singleCollectibleItem: g } = (0, i.useMemo)(() => {
             let e = [],
                 r = null;
@@ -22,10 +22,10 @@ function h(e) {
                     (0 === e.length && (r = l),
                     e.push(
                         (0, c.c)(l, {
-                            moreCount: a ? t.length - 4 : void 0,
+                            moreCount: a ? t.length - 4 + 1 : void 0,
                             profileOwner: n,
-                            analyticsLocations: p,
-                            onViewWishlist: h,
+                            analyticsLocations: h,
+                            onViewWishlist: p,
                         }),
                     ));
             }
@@ -33,13 +33,13 @@ function h(e) {
                 cards: e,
                 singleCollectibleItem: 1 === e.length ? r : null,
             };
-        }, [t, n, p, h]);
+        }, [t, n, h, p]);
     if (0 === f.length) return null;
     let m =
         1 === f.length && null != g
             ? (0, c.g)(g, {
                   profileOwner: n,
-                  analyticsLocations: p,
+                  analyticsLocations: h,
               })
             : f;
     return (0, r.jsxs)(o.Z.Overlay, {
