ed73061a246022afce1872ffaf48491ebecdafbc
DiscrapperManager <admin@minerpl.xyz>
2025-10-30 18:36:51 +0100

    canaryRelease: 463417
    Main scripts: 4
    Lazy Loaded Scripts: 1642
    Chunks:
     * Main Files: 15181
     * Scripts: 9176
    Strings: 22753
    Routes: 761
    Experiments Count: 416
    Apex Experiments Count: 115

--- chunks/7284.js
+++ chunks/7284.js
@@ -1,59 +1,36 @@
 n.d(t, {
-    U: () => _,
-    j: () => p,
+    U: () => f,
+    j: () => _,
 });
 var r = n(120356),
     i = n.n(r),
     a = n(342905),
     o = n(526167),
     s = n(594928),
-    l = n(181430),
-    c = n(352609),
+    l = n(817330),
+    c = n(181430),
     u = n(806539);
 let d = {
-        name: c.default.ZEL6mz,
-        className: "",
-    },
-    f = {
-        [a.C.DEFAULT]: d,
-        [a.C.CHERRY_BOMB]: {
-            name: c.default.rN7cuX,
-            className: u.cherryBomb,
-        },
-        [a.C.CHICLE]: {
-            name: c.default.CbHHnL,
-            className: u.chicle,
-        },
-        [a.C.MUSEO_MODERNO]: {
-            name: c.default.iEcEKO,
-            className: u.museoModerno,
-        },
-        [a.C.NEO_CASTEL]: {
-            name: c.default.DL7jLZ,
-            className: u.neoCastel,
-        },
-        [a.C.PIXELIFY]: {
-            name: c.default.jq4aRp,
-            className: u.pixelify,
-        },
-        [a.C.SINISTRE]: {
-            name: c.default.jV9DN4,
-            className: u.sinistre,
-        },
-        [a.C.ZILLA_SLAB]: {
-            name: c.default.KMR8rT,
-            className: u.zillaSlab,
-        },
+    [a.C.DEFAULT]: "",
+    [a.C.CHERRY_BOMB]: u.cherryBomb,
+    [a.C.CHICLE]: u.chicle,
+    [a.C.MUSEO_MODERNO]: u.museoModerno,
+    [a.C.NEO_CASTEL]: u.neoCastel,
+    [a.C.PIXELIFY]: u.pixelify,
+    [a.C.SINISTRE]: u.sinistre,
+    [a.C.ZILLA_SLAB]: u.zillaSlab,
+};
+function f(e) {
+    var t;
+    return {
+        name: (0, l.Z)(e),
+        className: null != (t = d[e]) ? t : "",
     };
-function _(e) {
-    var t;
-    return null != (t = f[e]) ? t : d;
 }
-function p(e) {
-    var t;
-    let { displayNameStyles: n, inProfile: r } = e,
-        { includeNonProfile: a } = s.fN.useExperiment({ location: "useDisplayNameStylesFont" });
-    if (!(0, l.Y)({ location: "useDisplayNameStylesFont" }) || (!r && !a) || null == n) return "";
-    let c = null != (t = f[n.fontId]) ? t : d;
-    return "" === c.className ? "" : i()(u.dnsFont, c.className, { [u.safari]: (0, o.G6)() });
+function _(e) {
+    let { displayNameStyles: t, inProfile: n } = e,
+        { includeNonProfile: r } = s.fN.useExperiment({ location: "useDisplayNameStylesFont" });
+    if (!(0, c.Y)({ location: "useDisplayNameStylesFont" }) || (!n && !r) || null == t) return "";
+    let a = f(t.fontId);
+    return "" === a.className ? "" : i()(u.dnsFont, a.className, { [u.safari]: (0, o.G6)() });
 }
