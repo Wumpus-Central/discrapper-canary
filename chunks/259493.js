99ea022508ba678a219dba66106971e829c3e9be
DiscrapperManager <admin@minerpl.xyz>
2025-10-31 01:00:12 +0100

    canaryRelease: 463753
    Main scripts: 4
    Lazy Loaded Scripts: 1642
    Chunks:
     * Main Files: 15366
     * Scripts: 9174
    Strings: 22777
    Routes: 761
    Experiments Count: 412
    Apex Experiments Count: 118

---
+++ chunks/259493.js
@@ -0,0 +1,7 @@
+n.d(t, { Z: () => r });
+let r = (0, n(427164).le)({
+    name: "2025-10-web-view-webm-embeds",
+    kind: "user",
+    defaultConfig: { enabled: !1 },
+    variations: { 1: { enabled: !0 } },
+});
--- chunks/406432.js
+++ chunks/406432.js
@@ -1,36 +1,41 @@
 n.d(t, {
-    CO: () => s,
-    NU: () => b,
-    X2: () => y,
-    cb: () => E,
-    d$: () => u,
-    tw: () => l,
+    CO: () => c,
+    NU: () => T,
+    X2: () => S,
+    cb: () => v,
+    d$: () => f,
+    tw: () => u,
 }),
     n(388685),
     n(35282);
-var r = n(358085);
-let i = (e, t) => {
+var r = n(358085),
+    i = n(259493),
+    a = n(804059);
+let o = (e, t) => {
         if (null == e) return !1;
         let [n, r] = e.split(/\?/, 1);
         return t.test(n);
     },
-    a = (e, t) => {
+    s = (e, t) => {
         if (null == e) return !1;
         let [n, r] = e.split("/");
         return n === t;
     },
-    o = /\.(png|jpe?g|webp|gif|heic|heif|dng|avif)$/i,
-    s = (e) => null != e && o.test(e),
-    l = (e) => a(e, "image"),
-    c = /\.(webp|gif|avif)$/i,
-    u = (e) => i(e, c),
-    d = /\.(mp3|m4a|wav|aif|aiff|ogg|opus|flac)$/i,
-    f = /\.(mp3|m4a|wav|ogg|opus|flac)$/i,
+    l = /\.(png|jpe?g|webp|gif|heic|heif|dng|avif)$/i,
+    c = (e) => null != e && l.test(e),
+    u = (e) => s(e, "image"),
+    d = /\.(webp|gif|avif)$/i,
+    f = (e) => o(e, d),
     _ = /\.(mp3|m4a|wav|aif|aiff|ogg|opus|flac)$/i,
-    p = ((0, r.isIOS)() || (0, r.isAndroid)(), /\.(mp4|mov)$/i),
-    h = /\.(mp4|webm|mov)$/i,
-    m = /\.(mp4|webm|mov)$/i,
-    g = (0, r.isIOS)() ? p : (0, r.isAndroid)() ? h : m,
-    E = (e) => i(e, g),
-    b = (e) => null != e && g.test(e),
-    y = (e) => a(e, "video");
+    p = /\.(mp3|m4a|wav|ogg|opus|flac)$/i,
+    h = /\.(mp3|m4a|wav|aif|aiff|ogg|opus|flac)$/i,
+    m = ((0, r.isIOS)() || (0, r.isAndroid)(), /\.(webm)$/i),
+    g = /\.(mp4|mov)$/i,
+    E = /\.(mp4|webm|mov)$/i,
+    b = /\.(mp4|webm|mov)$/i,
+    y = (0, r.isIOS)() ? g : (0, r.isAndroid)() ? E : b,
+    O = (e) => (0, a.W)() && i.Z.getConfig({ location: "isWebPlayerVideoUrl" }).enabled && o(e, m),
+    v = (e) => o(e, y) || O(e),
+    I = (e) => null != e && (0, a.W)() && i.Z.getConfig({ location: "isWebPlayerVideoFile" }).enabled && m.test(e),
+    T = (e) => null != e && (y.test(e) || I(e)),
+    S = (e) => s(e, "video");
