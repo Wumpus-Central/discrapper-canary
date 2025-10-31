72dfe0407d0ccc384df5b9d829bc5613bf9bd2ae
DiscrapperManager <admin@minerpl.xyz>
2025-10-31 22:35:38 +0100

    canaryRelease: 464120
    Main scripts: 4
    Lazy Loaded Scripts: 1639
    Chunks:
     * Main Files: 15469
     * Scripts: 9129
    Strings: 22781
    Routes: 762
    Experiments Count: 410
    Apex Experiments Count: 117

--- chunks/824480.js
+++ chunks/824480.js
@@ -1,22 +1,11 @@
-r.d(t, { d: () => c });
-var e = r(647438),
-    n = r(442837),
-    a = r(232567),
-    l = r(516373),
-    s = r(515970),
-    d = r(5888);
-function c() {
-    let i = (0, n.e7)([d.Z], () => d.Z.getFetchState()),
-        { users: t } = (0, n.e7)([d.Z], () => d.Z.getCheckpointData()),
-        r = (0, l.D)();
-    (0, e.useEffect)(() => {
-        i !== d.p.FETCHING && i !== d.p.SUCCESS && null != r && (0, s.p)();
-    }, [i, r]),
-        (0, e.useEffect)(() => {
-            null != t &&
-                t.forEach((i) => {
-                    let { userId: t } = i;
-                    return (0, a.PR)(t);
-                });
-        }, [t]);
+r.d(t, { d: () => l });
+var n = r(647438),
+    e = r(442837),
+    a = r(515970),
+    s = r(5888);
+function l() {
+    let i = (0, e.e7)([s.Z], () => s.Z.getFetchState());
+    (0, n.useEffect)(() => {
+        i !== s.p.FETCHING && i !== s.p.SUCCESS && (0, a.p)();
+    }, [i]);
 }
--- chunks/860277.js
+++ chunks/860277.js
@@ -1,49 +1,37 @@
-r.d(t, { Z: () => v });
-var e = r(951288),
-    n = r(647438),
-    a = r(793030),
-    l = r(442837),
+r.d(t, { Z: () => p });
+var n = r(951288),
+    e = r(793030),
+    a = r(442837),
     s = r(99690),
-    d = r(594174),
-    c = r(5888),
-    o = r(198358),
-    x = r(612776),
-    u = r(268685),
-    p = r(506945),
-    m = r(388032);
-function v(i) {
+    l = r(5888),
+    d = r(198358),
+    c = r(612776),
+    o = r(268685),
+    x = r(506945),
+    u = r(388032);
+function p(i) {
     let { step: t } = i,
-        { users: r } = (0, l.e7)([c.Z], () => c.Z.getCheckpointData()),
-        v = (0, n.useMemo)(
-            () =>
-                r
-                    .map((i) => {
-                        let { userId: t } = i;
-                        return d.default.getUser(t);
-                    })
-                    .filter((i) => null != i),
-            [r],
-        );
-    return (0, e.jsxs)(o.Z, {
+        { users: r, sidekick: p } = (0, a.e7)([l.Z], () => l.Z.getCheckpointData());
+    return (0, n.jsxs)(d.Z, {
         children: [
-            t === x.ij.FRIENDS_INTRO &&
-                (0, e.jsxs)("div", {
+            t === c.ij.FRIENDS_INTRO &&
+                (0, n.jsxs)("div", {
                     children: [
-                        (0, e.jsx)(a.xvT, {
+                        (0, n.jsx)(e.xvT, {
                             variant: "display-md",
                             color: "text-primary",
-                            children: m.intl.string(p.default["4xZpq4"]),
+                            children: u.intl.string(x.default["4xZpq4"]),
                         }),
-                        v.slice(1).map((i) =>
-                            (0, e.jsxs)(
+                        r.map((i) =>
+                            (0, n.jsxs)(
                                 "div",
                                 {
                                     children: [
-                                        (0, e.jsx)(s.Z, {
+                                        (0, n.jsx)(s.Z, {
                                             user: i,
-                                            size: u.EF.SIZE_120,
+                                            size: o.EF.SIZE_120,
                                         }),
-                                        (0, e.jsx)(a.xvT, {
+                                        (0, n.jsx)(e.xvT, {
                                             variant: "display-lg",
                                             color: "text-primary",
                                             children: i.username,
@@ -55,26 +43,26 @@ function v(i) {
                         ),
                     ],
                 }),
-            t === x.ij.FRIENDS_BEST &&
-                (0, e.jsxs)("div", {
+            t === c.ij.FRIENDS_BEST &&
+                (0, n.jsxs)("div", {
                     children: [
-                        (0, e.jsx)(a.xvT, {
+                        (0, n.jsx)(e.xvT, {
                             variant: "display-md",
                             color: "text-primary",
-                            children: m.intl.string(p.default.d5HUsD),
+                            children: u.intl.string(x.default.d5HUsD),
                         }),
-                        (0, e.jsx)(s.Z, {
-                            user: v[0],
-                            size: u.EF.SIZE_120,
+                        (0, n.jsx)(s.Z, {
+                            user: p.user,
+                            size: o.EF.SIZE_120,
                         }),
-                        (0, e.jsx)(a.xvT, {
+                        (0, n.jsx)(e.xvT, {
                             variant: "display-md",
                             color: "text-primary",
-                            children: m.intl.format(p.default.wz2JuF, {
-                                user: v[0].username,
+                            children: u.intl.format(x.default.wz2JuF, {
+                                user: p.user.username,
                                 userHook: (i, t) =>
-                                    (0, e.jsx)(
-                                        a.xvT,
+                                    (0, n.jsx)(
+                                        e.xvT,
                                         {
                                             variant: "display-lg",
                                             color: "text-primary",
@@ -84,25 +72,25 @@ function v(i) {
                                     ),
                             }),
                         }),
-                        (0, e.jsx)(a.xvT, {
+                        (0, n.jsx)(e.xvT, {
                             variant: "display-sm",
                             color: "text-primary",
-                            children: m.intl.string(p.default.wWKySP),
+                            children: u.intl.string(x.default.wWKySP),
                         }),
-                        (0, e.jsx)(a.xvT, {
+                        (0, n.jsx)(e.xvT, {
                             variant: "display-sm",
                             color: "text-primary",
-                            children: r[0].numMessagesSent,
+                            children: p.numMessagesSent,
                         }),
-                        (0, e.jsx)(a.xvT, {
+                        (0, n.jsx)(e.xvT, {
                             variant: "display-sm",
                             color: "text-primary",
-                            children: m.intl.string(p.default.lXT6ns),
+                            children: u.intl.string(x.default.lXT6ns),
                         }),
-                        (0, e.jsx)(a.xvT, {
+                        (0, n.jsx)(e.xvT, {
                             variant: "display-sm",
                             color: "text-primary",
-                            children: r[0].numMinutesInVoice,
+                            children: p.numVoiceMinutes,
                         }),
                     ],
                 }),
