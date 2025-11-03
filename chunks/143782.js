e29a15f8211be16afed7a83df3d8802b81e5626b
DiscrapperManager <admin@minerpl.xyz>
2025-11-03 19:40:34 +0100

    canaryRelease: 464378
    Main scripts: 4
    Lazy Loaded Scripts: 1639
    Chunks:
     * Main Files: 15473
     * Scripts: 9136
    Strings: 22794
    Routes: 762
    Experiments Count: 410
    Apex Experiments Count: 116

--- chunks/143782.js
+++ chunks/143782.js
@@ -1,84 +1,49 @@
-t.d(n, { default: () => s });
-var r = t(951288);
-t(647438);
-var o = t(273352),
-    i = t(153867),
-    l = t(493773),
-    c = t(933557),
-    a = t(626135),
-    u = t(981631),
-    f = t(388032);
-let s = (e) => {
-    var { onConfirm: n, channel: t } = e,
-        s = (function (e, n) {
-            if (null == e) return {};
-            var t,
-                r,
-                o = (function (e, n) {
-                    if (null == e) return {};
-                    var t,
-                        r,
-                        o = {},
-                        i = Object.keys(e);
-                    for (r = 0; r < i.length; r++) (t = i[r]), n.indexOf(t) >= 0 || (o[t] = e[t]);
-                    return o;
-                })(e, n);
-            if (Object.getOwnPropertySymbols) {
-                var i = Object.getOwnPropertySymbols(e);
-                for (r = 0; r < i.length; r++)
-                    (t = i[r]),
-                        !(n.indexOf(t) >= 0) && Object.prototype.propertyIsEnumerable.call(e, t) && (o[t] = e[t]);
-            }
-            return o;
-        })(e, ["onConfirm", "channel"]);
-    let b = (0, c.ZP)(t);
-    return ((0, l.ZP)(() => {
-        a.default.track(u.rMx.OPEN_MODAL, {
+e.d(n, { default: () => h }), e(388685);
+var i = e(951288),
+    a = e(647438),
+    l = e(793030),
+    s = e(153867),
+    c = e(493773),
+    r = e(933557),
+    o = e(626135),
+    d = e(981631),
+    u = e(388032);
+let h = (t) => {
+    let { onConfirm: n, channel: e, onClose: h, transitionState: p } = t,
+        C = (0, r.ZP)(e),
+        [g, k] = a.useState(!1);
+    return ((0, c.ZP)(() => {
+        o.default.track(d.rMx.OPEN_MODAL, {
             type: "Voice channel change confirmation",
-            channel_id: t.id,
+            channel_id: e.id,
         });
     }),
-    null == b)
+    null == C)
         ? null
-        : (0, r.jsx)(
-              o.default,
-              (function (e) {
-                  for (var n = 1; n < arguments.length; n++) {
-                      var t = null != arguments[n] ? arguments[n] : {},
-                          r = Object.keys(t);
-                      "function" == typeof Object.getOwnPropertySymbols &&
-                          (r = r.concat(
-                              Object.getOwnPropertySymbols(t).filter(function (e) {
-                                  return Object.getOwnPropertyDescriptor(t, e).enumerable;
-                              }),
-                          )),
-                          r.forEach(function (n) {
-                              var r;
-                              (r = t[n]),
-                                  n in e
-                                      ? Object.defineProperty(e, n, {
-                                            value: r,
-                                            enumerable: !0,
-                                            configurable: !0,
-                                            writable: !0,
-                                        })
-                                      : (e[n] = r);
-                          });
-                  }
-                  return e;
-              })(
-                  {
-                      confirmText: f.intl.string(f.t["cY+Oob"]),
-                      secondaryConfirmText: f.intl.string(f.t["JdIQ/Y"]),
-                      title: f.intl.string(f.t["0LZN5F"]),
-                      cancelText: f.intl.string(f.t["ETE/oC"]),
-                      onConfirm: n,
-                      onConfirmSecondary: () => {
-                          i.ZP.updatedUnsyncedSettings({ disableVoiceChannelChangeAlert: !0 });
-                      },
-                      body: f.intl.format(f.t["vA+uEs"], { channel: b }),
+        : (0, i.jsx)(l.Modal, {
+              title: u.intl.string(u.t["0LZN5F"]),
+              subtitle: u.intl.format(u.t["vA+uEs"], { channel: C }),
+              transitionState: p,
+              size: "md",
+              onClose: h,
+              actionBarInput: (0, i.jsx)(l.XZJ, {
+                  checked: g,
+                  onChange: (t) => {
+                      s.ZP.updatedUnsyncedSettings({ disableVoiceChannelChangeAlert: t }), k(t);
                   },
-                  s,
-              ),
-          );
+                  label: u.intl.string(u.t["JdIQ/Y"]),
+              }),
+              actions: [
+                  {
+                      variant: "secondary",
+                      text: u.intl.string(u.t["ETE/oC"]),
+                      onClick: h,
+                  },
+                  {
+                      variant: "primary",
+                      text: u.intl.string(u.t["cY+Oob"]),
+                      onClick: n,
+                  },
+              ],
+          });
 };
