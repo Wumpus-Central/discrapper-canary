2140535f995b3e9c75e2c73eae7f5b242f0a52c6
DiscrapperManager <admin@minerpl.xyz>
2025-11-04 19:04:42 +0100

    canaryRelease: 464826
    Main scripts: 4
    Lazy Loaded Scripts: 1641
    Chunks:
     * Main Files: 15487
     * Scripts: 9184
    Strings: 22833
    Routes: 764
    Experiments Count: 411
    Apex Experiments Count: 115

--- chunks/590026.js
+++ chunks/590026.js
@@ -3,12 +3,11 @@ var r = n(647438),
     i = n(876215),
     l = n(442837),
     a = n(146282),
-    s = n(354655),
-    o = n(442853),
-    c = n(520951),
-    d = n(897674),
-    u = n(206583);
-let p = new Set([
+    s = n(442853),
+    o = n(520951),
+    c = n(897674),
+    d = n(206583);
+let u = new Set([
     i.s.PLAYED_GAME,
     i.s.WATCHED_MEDIA,
     i.s.TOP_GAME,
@@ -17,18 +16,17 @@ let p = new Set([
     i.s.LAUNCHED_ACTIVITY,
 ]);
 function h(e) {
-    let t = (0, d.Z)({ id: u.YN.GLOBAL_FEED });
-    (t = (0, o.Z)({
+    let t = (0, c.Z)({ id: d.YN.GLOBAL_FEED });
+    (t = (0, s.Z)({
         entries: t,
         channelId: e,
     })),
-        (t = (0, s.Z)({ entries: t })),
-        (t = r.useMemo(() => (null == t ? void 0 : t.filter((e) => p.has(e.content_type))), [t]));
-    let { entries: n, filteredIds: i } = (0, c.Z)(t);
+        (t = r.useMemo(() => (null == t ? void 0 : t.filter((e) => u.has(e.content_type))), [t]));
+    let { entries: n, filteredIds: i } = (0, o.Z)(t);
     return (
         (t = n),
         {
-            requestId: (0, l.e7)([a.Z], () => a.Z.getFeedRequestId(u.YN.GLOBAL_FEED)),
+            requestId: (0, l.e7)([a.Z], () => a.Z.getFeedRequestId(d.YN.GLOBAL_FEED)),
             entries: t,
             impressionCappedEntryIds: i,
         }
--- chunks/935302.js
+++
@@ -1,7 +0,0 @@
-n.d(t, { C: () => r });
-let r = (0, n(427164).le)({
-    name: "2025-09-fortnite-dedupe",
-    kind: "user",
-    defaultConfig: { enabled: !1 },
-    variations: { 1: { enabled: !0 } },
-});
