0dba17b4dc3cd7ded5641426df81ecfd4099ccf6
DiscrapperManager <admin@minerpl.xyz>
2025-11-04 19:57:19 +0100

    canaryRelease: 464870
    Main scripts: 4
    Lazy Loaded Scripts: 1641
    Chunks:
     * Main Files: 15488
     * Scripts: 9184
    Strings: 22833
    Routes: 764
    Experiments Count: 411
    Apex Experiments Count: 116

--- chunks/394606.js
+++ chunks/394606.js
@@ -1,17 +1,25 @@
-n.d(t, { Z: () => c });
+n.d(t, { Z: () => s });
 var r = n(399606),
     i = n(669764),
     a = n(210753),
     l = n(77498),
-    o = n(388032);
-function c(e) {
+    o = n(768581),
+    c = n(388032);
+function s(e) {
     var t;
-    let { coverImageUrl: n, localizedName: c, isFetching: s } = (0, a.$)(e),
-        u = (0, r.e7)([l.Z], () => l.Z.getDetectableGame(e)),
-        d = (0, r.e7)([i.Z], () => i.Z.canFetch(e));
+    let { coverImageUrl: n, localizedName: s, isFetching: u } = (0, a.$)(e),
+        d = (0, r.e7)([l.Z], () => l.Z.getDetectableGame(e)),
+        f = (0, r.e7)([i.Z], () => i.Z.canFetch(e));
     return {
-        coverImageUrl: n,
-        gameName: null != (t = null != c ? c : null == u ? void 0 : u.name) ? t : o.intl.string(o.t.GIWFlF),
-        isLoading: d || s,
+        coverImageUrl:
+            null != n
+                ? n
+                : o.ZP.getGameAssetURL({
+                      id: e,
+                      hash: null == d ? void 0 : d.cover_image_hash,
+                      keepAspectRatio: !0,
+                  }),
+        gameName: null != (t = null != s ? s : null == d ? void 0 : d.name) ? t : c.intl.string(c.t.GIWFlF),
+        isLoading: f || u,
     };
 }
