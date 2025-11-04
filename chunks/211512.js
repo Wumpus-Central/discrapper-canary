2c290020243e76d4987ea586cdf53ec291a158e9
DiscrapperManager <admin@minerpl.xyz>
2025-11-04 23:53:15 +0100

    canaryRelease: 465108
    Main scripts: 4
    Lazy Loaded Scripts: 1639
    Chunks:
     * Main Files: 15488
     * Scripts: 9188
    Strings: 22850
    Routes: 764
    Experiments Count: 411
    Apex Experiments Count: 117

--- chunks/211512.js
+++ chunks/211512.js
@@ -1,26 +1,33 @@
-n.d(t, { I: () => i });
+n.d(t, { I: () => a }), n(388685), n(457542);
 var r = n(951288),
-    l = n(481060),
-    o = n(550964);
-function i(e) {
+    l = n(647438),
+    o = n(481060),
+    i = n(550964);
+function a(e) {
     let { node: t } = e,
-        { useLabel: n, useTitle: i, useSubtitle: a, useVariant: c, useDisabled: s, onClick: u } = t,
-        d = i(),
-        f = null == a ? void 0 : a(),
-        b = n(),
-        p = null == c ? void 0 : c(),
-        y = null == s ? void 0 : s();
-    return (0, r.jsx)(o.X, {
-        children: (0, r.jsx)(l.gNt, {
-            label: d,
-            description: f,
-            disabled: y,
+        { useLabel: n, useTitle: a, useSubtitle: c, useVariant: s, useDisabled: u, onClick: d } = t,
+        [f, b] = l.useState(!1),
+        p = l.useCallback(() => {
+            let e = d();
+            e instanceof Promise && (b(!0), e.finally(() => b(!1)));
+        }, [d]),
+        y = a(),
+        j = null == c ? void 0 : c(),
+        m = n(),
+        v = null == s ? void 0 : s(),
+        g = null == u ? void 0 : u();
+    return (0, r.jsx)(i.X, {
+        children: (0, r.jsx)(o.gNt, {
+            label: y,
+            description: j,
+            disabled: g,
             layout: "horizontal",
-            children: (0, r.jsx)(l.Button, {
-                onClick: u,
-                text: b,
-                variant: p,
-                disabled: y,
+            children: (0, r.jsx)(o.Button, {
+                onClick: p,
+                text: m,
+                variant: v,
+                disabled: g,
+                loading: f,
             }),
         }),
     });
