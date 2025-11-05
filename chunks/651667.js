8a615622467715128dc1117fd1b529928c83a6dd
DiscrapperManager <admin@minerpl.xyz>
2025-11-05 20:49:56 +0100

    canaryRelease: 465368
    Main scripts: 4
    Lazy Loaded Scripts: 1640
    Chunks:
     * Main Files: 15486
     * Scripts: 9191
    Strings: 22873
    Routes: 764
    Experiments Count: 409
    Apex Experiments Count: 115

--- chunks/651667.js
+++ chunks/651667.js
@@ -1,6 +1,6 @@
 function a(e) {
-    var t, n, a, r, i, l, s;
-    let o = e.skus
+    var t, n, a, r, i, l, s, o, c, d;
+    let u = e.skus
             .map((e) => {
                 let t = e.tenant_metadata.plan_features.map((e) => ({
                     title: e.title,
@@ -14,25 +14,28 @@ function a(e) {
                 };
             })
             .sort((e, t) => t.cost - e.cost),
-        c = o.length > 0 ? Math.min(...o.map((e) => e.cost)) : 0;
+        m = u.length > 0 ? Math.min(...u.map((e) => e.cost)) : 0;
     return {
         id: e.id,
         name: e.name,
         gameId:
             null !=
-            (s =
+            (d =
                 null == (a = e.tenant_metadata) || null == (n = a.guild_monetization) || null == (t = n.game_server)
                     ? void 0
                     : t.game_application_id)
-                ? s
+                ? d
                 : "",
         provider:
             null == (l = e.tenant_metadata) || null == (i = l.guild_monetization) || null == (r = i.game_server)
                 ? void 0
                 : r.provider,
-        plans: o,
-        baseCost: c,
-        disabled: e.disabled,
+        plans: u,
+        baseCost: m,
+        disabled:
+            null == (c = e.tenant_metadata) || null == (o = c.guild_monetization) || null == (s = o.game_server)
+                ? void 0
+                : s.disabled,
     };
 }
 n.d(t, { m: () => a }), n(953529), n(642613), n(388685);
