ee5cadc01d9a1892c698cbc2df931096611e7736
DiscrapperManager <admin@minerpl.xyz>
2025-11-06 06:00:04 +0100

    canaryRelease: 465612
    Main scripts: 4
    Lazy Loaded Scripts: 1642
    Chunks:
     * Main Files: 15494
     * Scripts: 9192
    Strings: 22876
    Routes: 769
    Experiments Count: 409
    Apex Experiments Count: 116

--- chunks/69376.js
+++ chunks/69376.js
@@ -8,7 +8,6 @@ function o(e) {
     let { channelId: t, memberIds: n, guildId: o, currentMemberId: c, shuffling: u } = e;
     return (0, r.jsx)("ul", {
         className: l.memberList,
-        "data-member-count": n.length,
         "data-animated": "true",
         children: n.map((e) => {
             let n = u ? (0, a.iD)(t, e) : null;
--- chunks/980811.js
+++ chunks/980811.js
@@ -1,33 +1,34 @@
-n.r(t), n.d(t, { default: () => s }), n(388685), n(457542);
+n.r(t), n.d(t, { default: () => d }), n(388685), n(457542);
 var r = n(951288),
     a = n(647438),
     i = n(493773),
     l = n(989373),
     o = n(522474),
     c = n(238246),
-    u = n(922588);
-function s(e) {
-    let { windowKey: t, channelId: n, guildId: s } = e,
-        d = a.useRef(null),
-        [_, m] = a.useState(!1);
+    u = n(51144),
+    s = n(922588);
+function d(e) {
+    let { windowKey: t, channelId: n, guildId: d } = e,
+        _ = a.useRef(null),
+        [m, f] = a.useState(!1);
     return ((0, i.ZP)(() => {
         let e = o.Z.getWindow(t);
         null != e &&
-            (0, l.waitForCSSLoad)(e, d).finally(() => {
-                m(!0);
+            (0, l.waitForCSSLoad)(e, _).finally(() => {
+                f(!0);
             });
     }),
     (0, i.zq)(() => {
-        clearInterval(d.current);
+        clearInterval(_.current);
     }),
-    _)
+    m && (0, u.vP)())
         ? (0, r.jsx)(c.Z, {
               withTitleBar: !0,
               windowKey: t,
               title: "Shuffle Names",
-              children: (0, r.jsx)(u.Z, {
+              children: (0, r.jsx)(s.Z, {
                   channelId: n,
-                  guildId: s,
+                  guildId: d,
               }),
           })
         : null;
