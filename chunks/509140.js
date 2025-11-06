6d79e55f8aa445e63846cc48a09b890cb834e975
DiscrapperManager <admin@minerpl.xyz>
2025-11-06 18:25:11 +0100

    canaryRelease: 465684
    Main scripts: 4
    Lazy Loaded Scripts: 1641
    Chunks:
     * Main Files: 15495
     * Scripts: 9195
    Strings: 22880
    Routes: 769
    Experiments Count: 409
    Apex Experiments Count: 116

--- chunks/509140.js
+++ chunks/509140.js
@@ -90,7 +90,7 @@ let N = new C(
         __OVERLAY__ || !f.iP
             ? {}
             : {
-                  CONNECTION_OPEN: A,
+                  POST_CONNECTION_OPEN: A,
                   EXPERIMENT_OVERRIDE_BUCKET: v,
                   OVERLAY_SET_ENABLED: I,
                   OVERLAY_V3_LOAD_NATIVE_MODULE: T,
