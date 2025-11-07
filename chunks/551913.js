7f3b304ee4ae63687e76f71f55a4c1df914a2fbe
DiscrapperManager <admin@minerpl.xyz>
2025-11-07 20:29:59 +0100

    canaryRelease: 466112
    Main scripts: 4
    Lazy Loaded Scripts: 1638
    Chunks:
     * Main Files: 15582
     * Scripts: 9096
    Strings: 22886
    Routes: 770
    Experiments Count: 409
    Apex Experiments Count: 117

--- chunks/551913.js
+++ chunks/551913.js
@@ -1,41 +1,44 @@
-n.d(t, { Z: () => u });
+n.d(t, { Z: () => p });
 var r = n(951288),
     i = n(647438),
-    l = n(907862),
-    a = n(703656),
-    o = n(981631),
-    s = n(388032),
-    c = n(11269);
-function u(e) {
-    let { onNavigateToQuestHome: t, onRender: n, onRequestClose: u, targetElementRef: d } = e,
-        p = i.useCallback(() => {
-            t(), (0, a.uL)(o.Z5c.QUEST_HOME_V2);
+    l = n(442837),
+    a = n(907862),
+    s = n(703656),
+    o = n(706454),
+    c = n(981631),
+    u = n(388032),
+    d = n(11269);
+function p(e) {
+    let { onNavigateToQuestHome: t, onRender: n, onRequestClose: p, targetElementRef: f } = e,
+        h = (0, l.e7)([o.default], () => o.default.locale),
+        g = i.useCallback(() => {
+            t(), (0, s.uL)(c.Z5c.QUEST_HOME_V2);
         }, [t]);
     return (
         i.useEffect(() => {
             n();
         }, [n]),
-        (0, r.jsx)(l.J2, {
+        (0, r.jsx)(a.J2, {
             actions: [
                 {
-                    text: s.intl.string(s.t.facYnD),
+                    text: u.intl.string(u.t.facYnD),
                     variant: "primary",
-                    onClick: p,
+                    onClick: g,
                 },
             ],
             align: "top",
-            body: s.intl.string(s.t.o4NGXc),
+            body: u.intl.string(u.t.o4NGXc),
             caretConfig: { align: "start" },
             gradientColor: "purple",
             graphic: {
                 type: "image",
-                src: c.Z,
+                src: d.Z,
             },
-            onRequestClose: u,
+            onRequestClose: p,
             position: "right",
             size: "lg",
-            targetElementRef: d,
-            title: s.intl.string(s.t["2/2YdK"]),
+            targetElementRef: f,
+            title: "en-US" === h || "en-GB" === h ? u.intl.string(u.t.EDDchq) : u.intl.string(u.t["2/2YdK"]),
         })
     );
 }
