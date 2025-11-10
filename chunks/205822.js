1924ff63ecc49824e38f651d85d0ea8277b05a71
DiscrapperManager <admin@minerpl.xyz>
2025-11-10 19:52:15 +0100

    canaryRelease: 466478
    Main scripts: 4
    Lazy Loaded Scripts: 1637
    Chunks:
     * Main Files: 15585
     * Scripts: 9115
    Strings: 22924
    Routes: 771
    Experiments Count: 407
    Apex Experiments Count: 120

--- chunks/205822.js
+++ chunks/205822.js
@@ -8,8 +8,8 @@ var n = A(951288),
     s = A(310752),
     d = A(531643),
     u = A(388032),
-    c = A(374450),
-    g = A(162130);
+    c = A(854835),
+    g = A(287540);
 function f(e, t, A) {
     return (
         t in e
@@ -129,7 +129,6 @@ class h extends r.Component {
                         (0, d.openUploadError)({
                             title: u.intl.string(u.t.azO1Pe),
                             help: u.intl.string(u.t["Koklr/"]),
-                            icons: t.props.icons,
                         })),
                     !1)
                 );
--- chunks/531643.js
+++ chunks/531643.js
@@ -1,25 +1,20 @@
 n.d(t, {
-    A: () => v,
-    openUploadError: () => I,
+    A: () => g,
+    openUploadError: () => E,
 });
 var r = n(951288);
 n(647438);
-var i = n(120356),
-    a = n.n(i),
-    o = n(952265),
-    s = n(466377),
-    l = n(313201),
-    c = n(174609),
-    u = n(283066),
-    d = n(355314),
-    f = n(911311),
-    _ = n(703656),
-    p = n(626135),
-    h = n(310752),
-    m = n(731994),
-    g = n(981631),
-    E = n(374450);
-function b(e, t, n) {
+var i = n(952265),
+    a = n(159691),
+    o = n(174609),
+    s = n(283066),
+    l = n(355314),
+    c = n(911311),
+    u = n(703656),
+    d = n(626135),
+    f = n(981631),
+    _ = n(388032);
+function p(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -32,7 +27,7 @@ function b(e, t, n) {
         e
     );
 }
-function y(e) {
+function h(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -43,64 +38,51 @@ function y(e) {
                 }),
             )),
             r.forEach(function (t) {
-                b(e, t, n[t]);
+                p(e, t, n[t]);
             });
     }
     return e;
 }
-function O(e) {
-    let { title: t, help: n, showPremiumUpsell: i, transitionState: o, icons: b, fileSize: y, onClose: O } = e,
-        v = (0, l.Dt)(),
-        I = null != b ? b : m.J6,
-        T = (0, u.Z)({ location: "UploadError" }),
-        S = () => {
-            (0, c.Z)(),
-                O(),
-                p.default.track(g.rMx.PREMIUM_PROMOTION_OPENED, {
-                    location_section: g.jXE.FILE_UPLOAD_UPSELL_MODAL,
-                    location_object: g.qAy.NAVIGATION_LINK,
+function m(e) {
+    let { title: t, help: n, showPremiumUpsell: i, transitionState: p, fileSize: h, onClose: m } = e,
+        g = (0, s.Z)({ location: "UploadError" }),
+        E = () => {
+            (0, o.Z)(),
+                m(),
+                d.default.track(f.rMx.PREMIUM_PROMOTION_OPENED, {
+                    location_section: f.jXE.FILE_UPLOAD_UPSELL_MODAL,
+                    location_object: f.qAy.NAVIGATION_LINK,
                 }),
-                (0, _.uL)(g.Z5c.APPLICATION_STORE);
+                (0, u.uL)(f.Z5c.APPLICATION_STORE);
         };
     return i
-        ? T
-            ? (0, r.jsx)(f.Z, {
-                  transitionState: o,
-                  onClose: O,
-                  handleLearnMore: S,
+        ? g
+            ? (0, r.jsx)(c.Z, {
+                  transitionState: p,
+                  onClose: m,
+                  handleLearnMore: E,
               })
-            : (0, r.jsx)(d.Z, {
-                  transitionState: o,
-                  onClose: O,
-                  handleLearnMore: S,
-                  fileSize: y,
+            : (0, r.jsx)(l.Z, {
+                  transitionState: p,
+                  onClose: m,
+                  handleLearnMore: E,
+                  fileSize: h,
               })
-        : (0, r.jsx)(s.Y0, {
-              size: s.Cg.DYNAMIC,
-              "aria-labelledby": v,
-              transitionState: o,
-              parentComponent: "UploadError",
-              children: (0, r.jsx)("div", {
-                  className: a()(E.uploadDropModal, E.error),
-                  children: (0, r.jsxs)("div", {
-                      className: E.inner,
-                      children: [
-                          (0, r.jsx)(h.Z, { icons: I }),
-                          (0, r.jsx)("div", {
-                              id: v,
-                              className: E.title,
-                              children: t,
-                          }),
-                          (0, r.jsx)("div", {
-                              className: E.instructions,
-                              children: n,
-                          }),
-                      ],
-                  }),
-              }),
+        : (0, r.jsx)(a.u_l, {
+              title: t,
+              subtitle: n,
+              transitionState: p,
+              actions: [
+                  {
+                      text: _.intl.string(_.t["NX+WJN"]),
+                      onClick: m,
+                      variant: "primary",
+                  },
+              ],
+              onClose: m,
           });
 }
-let v = "UPLOAD_ERROR_MODAL_KEY";
-function I(e) {
-    (0, o.h7)((t) => (0, r.jsx)(O, y({}, t, e)), { modalKey: v });
+let g = "UPLOAD_ERROR_MODAL_KEY";
+function E(e) {
+    (0, i.h7)((t) => (0, r.jsx)(m, h({}, t, e)), { modalKey: g });
 }
