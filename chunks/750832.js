16d1b3bb5a2b8b3d94ef68ae79dd2b33e2dfa47c
DiscrapperManager <admin@minerpl.xyz>
2025-11-10 21:25:51 +0100

    canaryRelease: 466570
    Main scripts: 4
    Lazy Loaded Scripts: 1636
    Chunks:
     * Main Files: 15588
     * Scripts: 9120
    Strings: 22931
    Routes: 771
    Experiments Count: 383
    Apex Experiments Count: 115

--- chunks/750832.js
+++ chunks/750832.js
@@ -1,4 +1,10 @@
-n.d(t, { S: () => a });
+n.d(t, { S: () => o });
 var r = n(81643),
-    i = n(128064);
-let a = () => (0, r.sf)() && (0, i.c_)("shouldAgeVerifyForDMDefaultOff");
+    i = n(128064),
+    a = n(312870);
+let o = () => {
+    let e = (0, a.U)("shouldAgeVerifyForDMDefaultOff"),
+        t = (0, i.c_)("shouldAgeVerifyForDMDefaultOff"),
+        n = e || t;
+    return (0, r.sf)() && n;
+};
