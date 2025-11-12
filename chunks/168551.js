9bb8313f9e16e1052bc6a2a2a21e8727abc15747
DiscrapperManager <admin@minerpl.xyz>
2025-11-13 00:20:23 +0100

    canaryRelease: 467399
    Main scripts: 4
    Lazy Loaded Scripts: 1698
    Chunks:
     * Main Files: 15611
     * Scripts: 9186
    Strings: 23028
    Routes: 771
    Experiments Count: 380
    Apex Experiments Count: 114

--- chunks/168551.js
+++ chunks/168551.js
@@ -1,59 +1,61 @@
 n.d(t, {
-    PQ: () => f,
-    ZP: () => h,
-    e3: () => l.e3,
+    PQ: () => p,
+    ZP: () => g,
+    e3: () => u.e3,
 });
 var r = n(647438),
-    i = n(392711),
-    a = n(442837),
-    o = n(210887),
-    s = n(581883),
-    l = n(575196),
-    c = n(233398),
-    u = n(514361),
-    d = n(803038);
-let f = "data-client-themes",
-    _ = () => {
-        let e = (0, a.e7)([u.Z], () => u.Z.gradientPreset);
+    i = n(120356),
+    a = n.n(i),
+    o = n(392711),
+    s = n(442837),
+    l = n(210887),
+    c = n(581883),
+    u = n(575196),
+    d = n(233398),
+    f = n(514361),
+    _ = n(803038);
+let p = "data-client-themes",
+    h = () => {
+        let e = (0, s.e7)([f.Z], () => f.Z.gradientPreset);
         return r.useMemo(() => {
             if (null == e) return null;
-            let t = u.Z.getLinearGradient();
-            return null == t ? null : (0, l.yf)(e, t);
+            let t = f.Z.getLinearGradient();
+            return null == t ? null : (0, u.yf)(e, t);
         }, [e]);
     };
-function p() {
-    let e = d.Mc.useExperiment({ location: "RootThemeContextProvider" }).enabled,
-        t = (0, a.e7)([o.Z], () => o.Z.theme),
-        { colors: n, chassisMixAmount: u, gradientAngle: f, setAll: _ } = (0, c.Ig)(),
-        p = (0, a.e7)([s.Z], () => {
+function m() {
+    let e = _.Mc.useExperiment({ location: "RootThemeContextProvider" }).enabled,
+        t = (0, s.e7)([l.Z], () => l.Z.theme),
+        { colors: n, chassisMixAmount: i, gradientAngle: a, setAll: f } = (0, d.Ig)(),
+        p = (0, s.e7)([c.Z], () => {
             var e, t;
-            return null == (t = s.Z.settings.appearance) || null == (e = t.clientThemeSettings)
+            return null == (t = c.Z.settings.appearance) || null == (e = t.clientThemeSettings)
                 ? void 0
                 : e.customUserThemeSettings;
         }),
         h = r.useRef(void 0);
     return (
         r.useEffect(() => {
-            if (!(0, i.isEqual)(p, h.current)) {
+            if (!(0, o.isEqual)(p, h.current)) {
                 var e, t, n;
                 (h.current = p),
-                    _({
+                    f({
                         colors: null != (e = null == p ? void 0 : p.colors) ? e : [],
                         gradientAngle: null != (t = null == p ? void 0 : p.gradientAngle) ? t : 0,
-                        chassisMixAmount: null != (n = null == p ? void 0 : p.baseMix) ? n : c.BH,
+                        chassisMixAmount: null != (n = null == p ? void 0 : p.baseMix) ? n : d.BH,
                     });
             }
-        }, [p, _]),
-        r.useMemo(() => (e && 0 !== n.length ? (0, l.gj)(n, f, u, t) : null), [n, u, f, e, t])
+        }, [p, f]),
+        r.useMemo(() => (e && 0 !== n.length ? (0, u.gj)(n, a, i, t) : null), [n, i, a, e, t])
     );
 }
-function h() {
-    let e = _(),
-        t = p();
+function g() {
+    let e = h(),
+        t = m();
     return null != t
         ? {
               clientThemesCSS: t,
-              clientThemesClassName: l.e3,
+              clientThemesClassName: a()(u.e3, u.pX),
           }
         : null === e
           ? {
@@ -62,6 +64,6 @@ function h() {
             }
           : {
                 clientThemesCSS: e,
-                clientThemesClassName: l.e3,
+                clientThemesClassName: u.e3,
             };
 }
--- chunks/575196.js
+++ chunks/575196.js
@@ -1,8 +1,9 @@
 n.d(t, {
     e3: () => l,
-    gj: () => O,
-    yf: () => v,
-    yz: () => I,
+    gj: () => v,
+    pX: () => c,
+    yf: () => I,
+    yz: () => T,
 }),
     n(539854),
     n(388685);
@@ -12,29 +13,30 @@ var r = n(688619),
     o = n(780384),
     s = n(629935);
 let l = "custom-theme-background",
-    c = 0.05,
-    u = 0.9,
-    d = 30,
-    f = 0.7,
-    _ = 60,
-    p = 0.4,
-    h = 30,
-    m = -0.2,
-    g = 30,
-    E = 40,
-    b = 0.1,
-    y = 0.05;
-function O(e, t, n, r) {
+    c = "custom-client-theme",
+    u = 0.05,
+    d = 0.9,
+    f = 30,
+    _ = 0.7,
+    p = 60,
+    h = 0.4,
+    m = 30,
+    g = -0.2,
+    E = 30,
+    b = 40,
+    y = 0.1,
+    O = 0.05;
+function v(e, t, n, r) {
     let a = e.slice();
     1 === e.length && a.push(e[0]);
-    let O = e.map((e) => i()(e).luminance()),
-        v = i()(e[O.indexOf(Math.min(...O))]),
-        I = i()(e[O.indexOf(Math.max(...O))]),
+    let c = e.map((e) => i()(e).luminance()),
+        v = i()(e[c.indexOf(Math.min(...c))]),
+        I = i()(e[c.indexOf(Math.max(...c))]),
         T = "linear-gradient(".concat(t, "deg, ").concat(a.join(", "), ")"),
         S = (0, o.wj)(r),
         A = {
-            light: i()(I).set("hsl.s", 1).set("hsl.l", u),
-            dark: i()(v).set("hsl.s", 1).set("hsl.l", c),
+            light: i()(I).set("hsl.s", 1).set("hsl.l", d),
+            dark: i()(v).set("hsl.s", 1).set("hsl.l", u),
         },
         C = 100 - n,
         N = "\n      --custom-theme-background: "
@@ -48,18 +50,18 @@ function O(e, t, n, r) {
                 "--custom-background-gradient-opacity-mix-amount",
                 ": ",
             )
-            .concat(((d + f * C) / 100).toFixed(2), ";\n      ", "--custom-theme-border-color-amount", ": 5%;\n      "),
+            .concat(((f + _ * C) / 100).toFixed(2), ";\n      ", "--custom-theme-border-color-amount", ": 5%;\n      "),
         R = S
             ? "\n      "
                   .concat(s.Yk.THEME_BASE_MIX_AMOUNT, ": ")
-                  .concat(_ + p * C, "%;\n      ")
+                  .concat(p + h * C, "%;\n      ")
                   .concat(s.Yk.THEME_TEXT_MIX_AMOUNT, ": ")
-                  .concat(g, "%;\n    ")
+                  .concat(E, "%;\n    ")
             : "\n      "
                   .concat(s.Yk.THEME_BASE_MIX_AMOUNT, ": ")
-                  .concat(h + m * n, "%;\n      ")
+                  .concat(m + g * n, "%;\n      ")
                   .concat(s.Yk.THEME_TEXT_MIX_AMOUNT, ": ")
-                  .concat(E, "%;\n    "),
+                  .concat(b, "%;\n    "),
         P = "\n      "
             .concat(s.Yk.DARK_THEME_BASE_COLOR, ": ")
             .concat(A.dark.css(), ";\n      ")
@@ -68,11 +70,11 @@ function O(e, t, n, r) {
             .concat(s.Yk.DARK_THEME_TEXT_COLOR, ": ")
             .concat(
                 i()(v)
-                    .set("hsl.l", ((100 - b * C) / 100).toFixed(2))
+                    .set("hsl.l", ((100 - y * C) / 100).toFixed(2))
                     .css(),
                 ";\n    ",
             ),
-        w = "\n      "
+        D = "\n      "
             .concat(s.Yk.LIGHT_THEME_BASE_COLOR, ": ")
             .concat(A.light.css(), ";\n      ")
             .concat(s.Yk.LIGHT_THEME_BASE_COLOR_HSL, ": ")
@@ -80,7 +82,7 @@ function O(e, t, n, r) {
             .concat(s.Yk.LIGHT_THEME_TEXT_COLOR, ": ")
             .concat(
                 i()(I)
-                    .set("hsl.l", ((y * C) / 100).toFixed(2))
+                    .set("hsl.l", ((O * C) / 100).toFixed(2))
                     .css(),
                 ";\n    ",
             );
@@ -89,9 +91,9 @@ function O(e, t, n, r) {
         .concat(N, "\n      ")
         .concat(R, "\n      ")
         .concat(P, "\n      ")
-        .concat(w, "\n      }");
+        .concat(D, "\n      }");
 }
-function v(e, t) {
+function I(e, t) {
     let n = a.b[e.colors[0].token].hex,
         r = a.b[e.colors[e.colors.length - 1].token].hex,
         i = (0, s.W4)({
@@ -116,7 +118,7 @@ function v(e, t) {
         .concat(r, ";\n      ")
         .concat(c, "\n    }");
 }
-let I = function (e) {
+let T = function (e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : 0,
         n = e.slice();
     return 1 === e.length && n.push(e[0]), "linear-gradient(".concat(t, "deg, ").concat(n.join(", "), ")");
