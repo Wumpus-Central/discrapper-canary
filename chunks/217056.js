f8aa113ac0f16ec272052e40f3ef9e808bd7369d
DiscrapperManager <admin@minerpl.xyz>
2025-11-14 17:33:39 +0100

    canaryRelease: 468278
    Main scripts: 4
    Lazy Loaded Scripts: 1698
    Chunks:
     * Main Files: 15642
     * Scripts: 9209
    Strings: 23066
    Routes: 771
    Experiments Count: 380
    Apex Experiments Count: 113

--- chunks/217056.js
+++ chunks/217056.js
@@ -1,4 +1,4 @@
-a.r(p), a.d(p, { default: () => d });
-let d = JSON.parse(
-    '{"aFBQ3d":["aFBQ3d"],"3QwQKb":["3QwQKb"],"fWbI1+":["fWbI1+"],"oqw/KW":["oqw/KW"],"U/DzKb":["U/DzKb"],"1RH5t9":["1RH5t9"],"XF1dDX":["XF1dDX"],"tjpGQG":["tjpGQG"],"mnt50A":["mnt50A"],"LANYBF":["LANYBF"],"vc2xys":["vc2xys"],"/a/UoP":["/a/UoP"],"O15JWp":["O15JWp"],"/x7DVG":["/x7DVG"]}',
+d.r(a), d.d(a, { default: () => p });
+let p = JSON.parse(
+    '{"aFBQ3d":["aFBQ3d"],"3QwQKb":["3QwQKb"],"fWbI1+":["fWbI1+"],"oqw/KW":["oqw/KW"],"U/DzKb":["U/DzKb"],"G6K/+s":["G6K/+s"],"ahfbl7":["ahfbl7"],"+XfJ5v":["+XfJ5v"],"1RH5t9":["1RH5t9"],"XF1dDX":["XF1dDX"],"tjpGQG":["tjpGQG"],"Nu9LNm":["Nu9LNm"],"mnt50A":["mnt50A"],"LANYBF":["LANYBF"],"vc2xys":["vc2xys"],"/a/UoP":["/a/UoP"],"O15JWp":["O15JWp"],"/x7DVG":["/x7DVG"],"dkyqkH":["dkyqkH"]}',
 );
