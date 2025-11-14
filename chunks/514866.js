9adc3af5c54265c54acf4cf1ecc65dba805313da
DiscrapperManager <admin@minerpl.xyz>
2025-11-14 01:50:21 +0100

    canaryRelease: 468122
    Main scripts: 4
    Lazy Loaded Scripts: 1698
    Chunks:
     * Main Files: 15632
     * Scripts: 9198
    Strings: 23055
    Routes: 771
    Experiments Count: 380
    Apex Experiments Count: 112

--- chunks/514866.js
+++ chunks/514866.js
@@ -1,8 +1,8 @@
-n.d(t, { Z: () => _ });
+n.d(t, { Z: () => S });
 var a = n(951288);
 n(647438);
-var i = n(120356),
-    r = n.n(i),
+var r = n(120356),
+    i = n.n(r),
     l = n(668757),
     s = n(481060),
     o = n(168107),
@@ -11,13 +11,16 @@ var i = n(120356),
     u = n(110478),
     m = n(677106),
     p = n(906605),
-    h = n(423932),
-    x = n(695346),
-    g = n(306052),
-    f = n(594174),
-    b = n(517269),
-    v = n(197571);
-let j = () => {
+    h = n(359119),
+    x = n(423932),
+    g = n(695346),
+    f = n(306052),
+    b = n(944486),
+    v = n(594174),
+    j = n(134612),
+    _ = n(517269),
+    y = n(197571);
+let C = () => {
     Promise.all([n.e("52030"), n.e("1286")])
         .then(n.bind(n, 24031))
         .then((e) => {
@@ -40,39 +43,99 @@ let j = () => {
             );
         });
 };
-function _() {
-    let e = f.default.getCurrentUser(),
+function S() {
+    let e = v.default.getCurrentUser(),
         t = null == e ? void 0 : e.isStaff(),
-        n = x.Rt.useSetting();
+        r = g.Rt.useSetting();
     return (0, a.jsxs)(s.Ttm, {
         children: [
             (0, a.jsx)("div", {
-                className: r()(b.buttonsContainer, v.marginBottom20),
-                children: (0, a.jsx)(g.g, { isDevTools: !0 }),
+                className: i()(_.buttonsContainer, y.marginBottom20),
+                children: (0, a.jsx)(f.g, { isDevTools: !0 }),
             }),
             (0, a.jsx)("div", {
-                className: r()(b.buttonsContainer, v.marginBottom8),
+                className: i()(_.buttonsContainer, y.marginBottom8),
                 children: (0, a.jsx)(s.Button, {
                     variant: "primary",
                     size: "sm",
                     text: "Launch MFA",
-                    onClick: j,
+                    onClick: C,
                 }),
             }),
             (0, a.jsx)("div", {
-                className: r()(b.buttonsContainer, v.marginBottom8),
+                className: i()(_.buttonsContainer, y.marginBottom8),
                 children: (0, a.jsx)(s.Button, {
                     variant: "primary",
                     size: "sm",
                     text: "Launch Vibing Wumpus",
-                    onClick: () => (0, h.Z)(),
+                    onClick: () => (0, x.Z)(),
+                }),
+            }),
+            (0, a.jsx)("div", {
+                className: i()(_.buttonsContainer, y.marginBottom8),
+                children: (0, a.jsx)(s.Button, {
+                    variant: "primary",
+                    size: "sm",
+                    text: "Launch Safety Tools Modal",
+                    onClick: () =>
+                        (() => {
+                            let e = b.Z.getChannelId(),
+                                t = null != e ? e : "21154681615024128";
+                            (0, s.ZDy)(
+                                async () => {
+                                    let { default: e } = await Promise.all([
+                                        n.e("37031"),
+                                        n.e("89650"),
+                                        n.e("76144"),
+                                    ]).then(n.bind(n, 611446));
+                                    return (n) =>
+                                        (0, a.jsx)(
+                                            e,
+                                            (function (e) {
+                                                for (var t = 1; t < arguments.length; t++) {
+                                                    var n = null != arguments[t] ? arguments[t] : {},
+                                                        a = Object.keys(n);
+                                                    "function" == typeof Object.getOwnPropertySymbols &&
+                                                        (a = a.concat(
+                                                            Object.getOwnPropertySymbols(n).filter(function (e) {
+                                                                return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                                                            }),
+                                                        )),
+                                                        a.forEach(function (t) {
+                                                            var a;
+                                                            (a = n[t]),
+                                                                t in e
+                                                                    ? Object.defineProperty(e, t, {
+                                                                          value: a,
+                                                                          enumerable: !0,
+                                                                          configurable: !0,
+                                                                          writable: !0,
+                                                                      })
+                                                                    : (e[t] = a);
+                                                        });
+                                                }
+                                                return e;
+                                            })(
+                                                {
+                                                    otherUserId: "9876543210",
+                                                    channelId: t,
+                                                    warningId: "test-warning-id",
+                                                    warningType: h.pj.INAPPROPRIATE_CONVERSATION_TIER_2,
+                                                },
+                                                n,
+                                            ),
+                                        );
+                                },
+                                { modalKey: j.X_ },
+                            );
+                        })(),
                 }),
             }),
             t &&
                 (0, a.jsxs)(a.Fragment, {
                     children: [
                         (0, a.jsx)("div", {
-                            className: r()(b.buttonsContainer, v.marginBottom8),
+                            className: i()(_.buttonsContainer, y.marginBottom8),
                             children: (0, a.jsx)(s.Button, {
                                 variant: "primary",
                                 size: "sm",
@@ -81,7 +144,7 @@ function _() {
                             }),
                         }),
                         (0, a.jsx)("div", {
-                            className: r()(b.buttonsContainer, v.marginBottom8),
+                            className: i()(_.buttonsContainer, y.marginBottom8),
                             children: (0, a.jsx)(s.Button, {
                                 variant: "primary",
                                 size: "sm",
@@ -90,7 +153,7 @@ function _() {
                             }),
                         }),
                         (0, a.jsx)("div", {
-                            className: r()(b.buttonsContainer, v.marginBottom8),
+                            className: i()(_.buttonsContainer, y.marginBottom8),
                             children: (0, a.jsx)(s.Button, {
                                 variant: "primary",
                                 size: "sm",
@@ -99,7 +162,7 @@ function _() {
                             }),
                         }),
                         (0, a.jsx)("div", {
-                            className: r()(b.buttonsContainer, v.marginBottom8),
+                            className: i()(_.buttonsContainer, y.marginBottom8),
                             children: (0, a.jsx)(s.Button, {
                                 variant: "primary",
                                 size: "sm",
@@ -111,7 +174,7 @@ function _() {
                             }),
                         }),
                         (0, a.jsx)("div", {
-                            className: r()(b.buttonsContainer, v.marginBottom8),
+                            className: i()(_.buttonsContainer, y.marginBottom8),
                             children: (0, a.jsx)(s.Button, {
                                 variant: "primary",
                                 size: "sm",
@@ -120,7 +183,7 @@ function _() {
                             }),
                         }),
                         (0, a.jsx)("div", {
-                            className: r()(b.buttonsContainer, v.marginBottom8),
+                            className: i()(_.buttonsContainer, y.marginBottom8),
                             children: (0, a.jsx)(s.Button, {
                                 variant: "primary",
                                 size: "sm",
@@ -129,11 +192,11 @@ function _() {
                             }),
                         }),
                         (0, a.jsx)("div", {
-                            className: r()(b.buttonsContainer, v.marginBottom20),
+                            className: i()(_.buttonsContainer, y.marginBottom20),
                             children: (0, a.jsx)(s.rsf, {
                                 label: "Ignore profile speedbump disabled",
-                                checked: n,
-                                onChange: (e) => x.Rt.updateSetting(e),
+                                checked: r,
+                                onChange: (e) => g.Rt.updateSetting(e),
                             }),
                         }),
                     ],
