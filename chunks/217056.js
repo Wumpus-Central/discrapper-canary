996cd066fd676e18faae343c2ec326fdec20f7f8
DiscrapperManager <admin@minerpl.xyz>
2025-11-17 21:47:03 +0100

    canaryRelease: 469043
    Main scripts: 4
    Lazy Loaded Scripts: 1703
    Chunks:
     * Main Files: 15644
     * Scripts: 9223
    Strings: 23087
    Routes: 772
    Experiments Count: 377
    Apex Experiments Count: 115

--- chunks/217056.js
+++ chunks/217056.js
@@ -1,4 +1,4 @@
-d.r(a), d.d(a, { default: () => p });
+d.r(t), d.d(t, { default: () => p });
 let p = JSON.parse(
-    '{"aFBQ3d":["aFBQ3d"],"3QwQKb":["3QwQKb"],"fWbI1+":["fWbI1+"],"oqw/KW":["oqw/KW"],"U/DzKb":["U/DzKb"],"G6K/+s":["G6K/+s"],"ahfbl7":["ahfbl7"],"+XfJ5v":["+XfJ5v"],"1RH5t9":["1RH5t9"],"XF1dDX":["XF1dDX"],"tjpGQG":["tjpGQG"],"Nu9LNm":["Nu9LNm"],"mnt50A":["mnt50A"],"LANYBF":["LANYBF"],"vc2xys":["vc2xys"],"/a/UoP":["/a/UoP"],"O15JWp":["O15JWp"],"/x7DVG":["/x7DVG"],"dkyqkH":["dkyqkH"]}',
+    '{"aFBQ3d":["aFBQ3d"],"5xN/C1":["5xN/C1"],"ah1Ecm":["ah1Ecm"],"3QwQKb":["3QwQKb"],"fWbI1+":["fWbI1+"],"oqw/KW":["oqw/KW"],"U/DzKb":["U/DzKb"],"G6K/+s":["G6K/+s"],"ahfbl7":["ahfbl7"],"+XfJ5v":["+XfJ5v"],"1RH5t9":["1RH5t9"],"XF1dDX":["XF1dDX"],"tjpGQG":["tjpGQG"],"Nu9LNm":["Nu9LNm"],"mnt50A":["mnt50A"],"LANYBF":["LANYBF"],"vc2xys":["vc2xys"],"/a/UoP":["/a/UoP"],"O15JWp":["O15JWp"],"/x7DVG":["/x7DVG"],"dkyqkH":["dkyqkH"]}',
 );
