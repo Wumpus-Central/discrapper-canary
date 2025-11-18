a94b511fe6e015443236b4f83c3ce7977049c6eb
DiscrapperManager <admin@minerpl.xyz>
2025-11-18 08:00:35 +0100

    canaryRelease: 469410
    Main scripts: 4
    Lazy Loaded Scripts: 1704
    Chunks:
     * Main Files: 15655
     * Scripts: 9222
    Strings: 23141
    Routes: 771
    Experiments Count: 376
    Apex Experiments Count: 114

--- chunks/415397.js
+++ chunks/415397.js
@@ -1,4 +1,4 @@
-n.d(t, { B: () => x }), n(388685);
+n.d(t, { B: () => j }), n(388685);
 var i = n(54381),
     r = n(473749),
     l = n(120356),
@@ -6,18 +6,19 @@ var i = n(54381),
     o = n(512722),
     s = n.n(o),
     c = n(399606),
-    u = n(481060),
-    d = n(630810),
-    p = n(518950),
-    h = n(271383),
-    f = n(430824),
-    m = n(51144),
-    g = n(771027),
-    b = n(981631),
-    y = n(388032),
-    C = n(764194),
-    v = n(254477);
-function _(e) {
+    u = n(681715),
+    d = n(481060),
+    p = n(630810),
+    h = n(518950),
+    f = n(271383),
+    m = n(430824),
+    g = n(51144),
+    b = n(771027),
+    y = n(981631),
+    C = n(388032),
+    v = n(764194),
+    _ = n(254477);
+function x(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             i = Object.keys(n);
@@ -42,21 +43,21 @@ function _(e) {
     }
     return e;
 }
-function x(e) {
-    let { channel: t, inviteKey: l, onHoverOrFocus: o, setPopoutRef: c, closePopout: d } = e,
+function j(e) {
+    let { channel: t, inviteKey: l, onHoverOrFocus: o, setPopoutRef: c, closePopout: u } = e,
         p = r.useRef(null),
-        h = (0, g.Tu)(p),
-        m = (0, g.j1)({ channel: t });
+        h = (0, b.Tu)(p),
+        f = (0, b.j1)({ channel: t });
     r.useEffect(() => {
         null == c || c(p.current);
     }, [c]),
         r.useEffect(() => {
             null == o || o(h.isHoveringOrFocusing);
         }, [o, h]);
-    let x = r.useCallback(() => {
-        let e = f.Z.getGuild(t.guild_id);
+    let g = r.useCallback(() => {
+        let e = m.Z.getGuild(t.guild_id);
         s()(null != e, "VoiceInviteSuggestionsPopover must be used in context of a guild"),
-            (0, u.ZDy)(async () => {
+            (0, d.ZDy)(async () => {
                 let { default: r } = await Promise.all([n.e("49049"), n.e("7654"), n.e("68971")]).then(
                     n.bind(n, 560114),
                 );
@@ -64,12 +65,12 @@ function x(e) {
                     var l, a;
                     return (0, i.jsx)(
                         r,
-                        ((l = _({}, n)),
+                        ((l = x({}, n)),
                         (a = a =
                             {
                                 guild: e,
                                 channel: t,
-                                source: b.t4x.VOICE_INVITE_SUGGESTIONS,
+                                source: y.t4x.VOICE_INVITE_SUGGESTIONS,
                             }),
                         Object.getOwnPropertyDescriptors
                             ? Object.defineProperties(l, Object.getOwnPropertyDescriptors(a))
@@ -87,20 +88,20 @@ function x(e) {
                     );
                 };
             }),
-            d();
-    }, [t, d]);
+            u();
+    }, [t, u]);
     return (0, i.jsx)("div", {
         ref: p,
-        "aria-label": y.intl.string(y.t.o53CL2),
-        className: a()(v.popover, C.popover),
+        "aria-label": C.intl.string(C.t.o53CL2),
+        className: a()(_.popover, v.popover),
         children: (0, i.jsx)("div", {
-            className: C.inner,
+            className: v.inner,
             children: (0, i.jsxs)("ul", {
-                className: C.list,
+                className: v.list,
                 children: [
-                    m.map((e) =>
+                    f.map((e) =>
                         (0, i.jsx)(
-                            j,
+                            O,
                             {
                                 channel: t,
                                 user: e,
@@ -109,23 +110,23 @@ function x(e) {
                             e.id,
                         ),
                     ),
-                    (0, i.jsxs)(u.P3F, {
+                    (0, i.jsxs)(d.P3F, {
                         tag: "li",
-                        onClick: x,
-                        className: a()(C.row, C.clickable),
+                        onClick: g,
+                        className: a()(v.row, v.clickable),
                         children: [
                             (0, i.jsx)("div", {
-                                className: C.leading,
-                                children: (0, i.jsx)(u.oLu, {
+                                className: v.leading,
+                                children: (0, i.jsx)(d.oLu, {
                                     size: "refresh_sm",
-                                    className: C.icon,
+                                    className: v.icon,
                                 }),
                             }),
-                            (0, i.jsx)(u.Text, {
+                            (0, i.jsx)(d.Text, {
                                 variant: "text-md/medium",
-                                className: C.name,
+                                className: v.name,
                                 lineClamp: 1,
-                                children: y.intl.string(y.t.NOP3Ry),
+                                children: C.intl.string(C.t.NOP3Ry),
                             }),
                         ],
                     }),
@@ -134,84 +135,93 @@ function x(e) {
         }),
     });
 }
-function j(e) {
+function O(e) {
     let { channel: t, user: n, inviteKey: l } = e,
-        o = r.useRef(null),
-        s = (0, c.e7)([h.ZP], () => h.ZP.getMember(t.guild_id, n.id), [t.guild_id, n.id]),
-        { isHoveringOrFocusing: f } = (0, g.Tu)(o),
-        [b, v] = r.useState(null),
-        x = "success" === b || "sending" === b || null == l,
+        a = r.useRef(null),
+        o = (0, c.e7)([f.ZP], () => f.ZP.getMember(t.guild_id, n.id), [t.guild_id, n.id]),
+        { isHoveringOrFocusing: s } = (0, b.Tu)(a),
+        [m, y] = r.useState(null),
+        _ = "success" === m || "sending" === m || null == l,
         j = r.useCallback(() => {
-            x ||
+            _ ||
                 (null != l &&
-                    (v("sending"),
-                    d.Z.enqueue(
+                    (y("sending"),
+                    p.Z.enqueue(
                         {
-                            type: d.m.USER,
+                            type: p.m.USER,
                             user: n,
                             inviteKey: l,
                             location: "VoiceInviteSuggestionsPopover",
                             inviteAnalyticsMetadata: {},
                         },
                         (e) => {
-                            v(e ? "success" : null);
+                            y(e ? "success" : null);
                         },
                     )));
-        }, [x, l, n]),
+        }, [_, l, n]),
         {
             avatarDecorationSrc: O,
             avatarSrc: E,
             eventHandlers: S,
-        } = (0, p.Z)({
+        } = (0, h.Z)({
             userId: n.id,
-            size: u.EFr.SIZE_24,
-            animateOnHover: !f,
+            size: d.EFr.SIZE_24,
+            animateOnHover: !s,
             guildId: t.guild_id,
         });
-    if (null == s) return null;
-    let P = m.ZP.getName(n);
-    return (0, i.jsxs)(u.P3F, {
-        innerRef: o,
-        tag: "li",
-        "aria-label": y.intl.string(y.t.jYnGPG),
-        className: a()(C.row, { [C.clickable]: !x }),
-        onClick: j,
-        "aria-disabled": x,
+    if (null == o) return null;
+    let P = g.ZP.getName(n),
+        I = C.intl.string(C.t.jYnGPG);
+    return (0, i.jsxs)("li", {
+        ref: a,
+        className: v.row,
         children: [
             (0, i.jsx)(
-                u.qEK,
-                _(
+                d.qEK,
+                x(
                     {
-                        className: C.avatar,
+                        className: v.avatar,
                         "aria-label": P,
-                        size: u.EFr.SIZE_24,
+                        size: d.EFr.SIZE_24,
                         src: E,
                         avatarDecoration: O,
                     },
                     S,
                 ),
             ),
-            (0, i.jsx)(u.Text, {
+            (0, i.jsx)(d.Text, {
                 variant: "text-md/medium",
-                className: C.name,
+                className: v.name,
                 lineClamp: 1,
                 children: P,
             }),
-            "sending" === b &&
+            null == m &&
+                (0, i.jsx)(u.u, {
+                    text: C.intl.string(C.t["EE+P0H"]),
+                    children: (0, i.jsx)(d.hU, {
+                        icon: d.oLu,
+                        disabled: _,
+                        onClick: j,
+                        "aria-label": I,
+                        size: "sm",
+                        variant: "secondary",
+                    }),
+                }),
+            "sending" === m &&
                 (0, i.jsx)("div", {
-                    className: C.dots,
-                    children: (0, i.jsx)(u.bbz, {
+                    className: v.dots,
+                    children: (0, i.jsx)(d.bbz, {
                         dotRadius: 2,
                         themed: !0,
                     }),
                 }),
-            "success" === b &&
-                (0, i.jsx)(u.Text, {
+            "success" === m &&
+                (0, i.jsx)(d.Text, {
                     variant: "text-sm/normal",
                     color: "text-muted",
-                    className: C.status,
+                    className: v.status,
                     lineClamp: 1,
-                    children: y.intl.string(y.t["8BEiNn"]),
+                    children: C.intl.string(C.t["8BEiNn"]),
                 }),
         ],
     });
--- chunks/764194.js
+++ chunks/764194.js
@@ -1,8 +1,8 @@
 e.exports = {
     popover: "popover__1bad5",
     inner: "inner__1bad5",
-    row: "row__1bad5",
     list: "list__1bad5",
+    row: "row__1bad5",
     clickable: "clickable__1bad5",
     avatar: "avatar__1bad5",
     icon: "icon__1bad5",
