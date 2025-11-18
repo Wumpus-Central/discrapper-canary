a2e5b13bc5bd630d940623d47e6c6da848d18cf9
DiscrapperManager <admin@minerpl.xyz>
2025-11-18 01:56:04 +0100

    canaryRelease: 469312
    Main scripts: 4
    Lazy Loaded Scripts: 1703
    Chunks:
     * Main Files: 15650
     * Scripts: 9221
    Strings: 23103
    Routes: 771
    Experiments Count: 376
    Apex Experiments Count: 114

--- chunks/535618.js
+++ chunks/535618.js
@@ -11,19 +11,19 @@ var a = i(54381),
     _ = i(775644),
     x = i(493773),
     f = i(410030),
-    C = i(100527),
-    m = i(906732),
-    g = i(535139),
+    m = i(100527),
+    g = i(906732),
+    C = i(535139),
     p = i(812206),
     h = i(594174),
-    j = i(626135),
-    S = i(877481),
-    T = i(669079),
+    S = i(626135),
+    T = i(877481),
+    j = i(669079),
     k = i(823379),
     I = i(51144),
-    L = i(579201),
-    N = i(683668),
-    E = i(55121),
+    E = i(579201),
+    L = i(683668),
+    N = i(55121),
     v = i(438926),
     y = i(582113),
     R = i(981631),
@@ -41,61 +41,75 @@ function D(t) {
             customGiftMessage: d,
             analyticsLocations: u,
         } = t,
-        [f, g] = e.useState({ state: "LINK" }),
-        [h, S] = e.useState(!1),
-        T = (0, l.e7)([p.Z], () => p.Z.getApplication(i.applicationId)),
-        { analyticsLocations: I } = (0, m.ZP)(null != u ? u : [], C.Z.SOCIAL_LAYER_GIFT_CODE_CLAIM_MODAL);
+        [f, C] = e.useState({ state: "LINK" }),
+        [h, T] = e.useState(!1),
+        j = (0, l.e7)([p.Z], () => p.Z.getApplication(i.applicationId)),
+        { analyticsLocations: I } = (0, g.ZP)(null != u ? u : [], m.Z.SOCIAL_LAYER_GIFT_CODE_CLAIM_MODAL),
+        E = null != d && "" !== d;
     (0, x.ZP)(() => {
-        j.default.track(R.rMx.OPEN_MODAL, {
+        S.default.track(R.rMx.OPEN_MODAL, {
             location_stack: I,
             type: y.tb,
             sku_id: i.id,
-            application_id: null == T ? void 0 : T.id,
+            application_id: null == j ? void 0 : j.id,
         });
     }),
         e.useEffect(() => {
+            S.default.track(R.rMx.GIFT_ACCEPT_STEP, {
+                to_step: f.state,
+                location_stack: I,
+                has_custom_message: E,
+                is_custom_message_edited: E && d !== A.intl.string(A.t.ZkOo1U),
+                gift_style: o.giftStyle,
+                gift_code: o.code,
+                emoji_name: null,
+                sound_id: null,
+                product_line: i.productLine,
+            });
+        }, [f.state, o, I, E, d, i.productLine]),
+        e.useEffect(() => {
             o.redeemed &&
                 "LINK" === f.state &&
-                g({
+                C({
                     state: "ERROR",
                     error: new r.yZ(A.intl.string(A.t.Iw2TUW), R.evJ.INVALID_GIFT_REDEMPTION_EXHAUSTED),
                 });
         }, [o, f.state]);
     let L = e.useCallback(async () => {
         var t, n;
-        j.default.track(R.rMx.SLAYER_STOREFRONT_GIFT_CODE_CLAIM_CLICKED, {
+        S.default.track(R.rMx.SLAYER_STOREFRONT_GIFT_CODE_CLAIM_CLICKED, {
             location_stack: I,
             sku_id: i.id,
-            application_id: null == T ? void 0 : T.id,
+            application_id: null == j ? void 0 : j.id,
             gift_code: o.code,
             gift_style: null != (n = null == (t = o.giftStyle) ? void 0 : t.toString()) ? n : null,
-            has_custom_message: null != d && "" !== d,
-            is_custom_message_edited: null != d && d !== A.intl.string(A.t.ZkOo1U),
+            has_custom_message: E,
+            is_custom_message_edited: E && d !== A.intl.string(A.t.ZkOo1U),
             emoji_name: null,
             sound_id: null,
         });
         try {
-            S(!0),
+            T(!0),
                 await _.Z.redeemGiftCode({
                     code: o.code,
                     options: { channelId: c },
                 }),
-                g({ state: "SUCCESS" });
+                C({ state: "SUCCESS" });
         } catch (t) {
-            g({
+            C({
                 state: "ERROR",
                 error: t,
             });
         } finally {
-            S(!1);
+            T(!1);
         }
-    }, [o, g, c, I, i.id, null == T ? void 0 : T.id, d]);
-    if (null == T) return null;
+    }, [o, C, c, I, i.id, null == j ? void 0 : j.id, E, d]);
+    if (null == j) return null;
     switch (f.state) {
         case "LINK":
             return (0, a.jsx)(G, {
                 sku: i,
-                application: T,
+                application: j,
                 transitionState: n,
                 onClose: s,
                 successStateButtonText: A.intl.string(A.t.go1e4x),
@@ -106,7 +120,7 @@ function D(t) {
         case "SUCCESS":
             return (0, a.jsx)(Z, {
                 transitionState: n,
-                application: T,
+                application: j,
                 sku: i,
                 giftCode: o,
                 customGiftMessage: d,
@@ -126,7 +140,7 @@ function D(t) {
 let P = (t) => {
     let { transitionState: n, onClose: i, error: e } = t,
         s = h.default.getCurrentUser(),
-        o = (0, T.QW)(e, s),
+        o = (0, j.QW)(e, s),
         l = (0, f.ZP)(),
         r = (0, d.wj)(l);
     return (0, a.jsxs)(c.IX, {
@@ -154,11 +168,11 @@ function G(t) {
             isSuccessButtonLoading: d,
             analyticsLocations: _,
         } = t,
-        { hasAlreadyLinked: x, canStartAuthorization: f, startAuthorization: C, fetched: m } = (0, g.F)(i);
+        { hasAlreadyLinked: x, canStartAuthorization: f, startAuthorization: m, fetched: g } = (0, C.F)(i);
     if (
         (e.useEffect(() => {
-            m &&
-                j.default.track(R.rMx.SLAYER_STOREFRONT_LINK_ACCOUNT_STEP_VIEWED, {
+            g &&
+                S.default.track(R.rMx.SLAYER_STOREFRONT_LINK_ACCOUNT_STEP_VIEWED, {
                     location_stack: _,
                     sku_id: n.id,
                     application_id: i.id,
@@ -166,8 +180,8 @@ function G(t) {
                     is_account_linked: x,
                     can_start_authorization: f,
                 });
-        }, [m, n.id, i.id, _, x, f]),
-        !m)
+        }, [g, n.id, i.id, _, x, f]),
+        !g)
     )
         return (0, a.jsx)(c.IX, {
             transitionState: s,
@@ -190,18 +204,18 @@ function G(t) {
               variant: "primary",
               text: A.intl.string(A.t["VDAhr+"]),
               onClick: () => {
-                  j.default.track(R.rMx.SLAYER_STOREFRONT_ACCOUNT_LINK_CLICKED, {
+                  S.default.track(R.rMx.SLAYER_STOREFRONT_ACCOUNT_LINK_CLICKED, {
                       location_stack: _,
                       sku_id: n.id,
                       application_id: i.id,
                       is_gift: !0,
                   }),
-                      C();
+                      m();
               },
               icon: c.uIJ,
               disabled: !f,
           };
-    return (0, a.jsx)(E.M, {
+    return (0, a.jsx)(N.M, {
         sku: n,
         application: i,
         transitionState: s,
@@ -223,29 +237,29 @@ function Z(t) {
             customGiftMessage: d,
             analyticsLocations: _,
         } = t,
-        x = (0, N.a)(s),
-        f = (0, L.I)(s),
-        C = (0, l.e7)([h.default], () => h.default.getUser(r.userId)),
-        m = I.ZP.getName(C);
+        x = (0, L.a)(s),
+        f = (0, E.I)(s),
+        m = (0, l.e7)([h.default], () => h.default.getUser(r.userId)),
+        g = I.ZP.getName(m);
     if (null == x)
         return (0, a.jsx)(c.IX, {
             transitionState: n,
             onClose: i,
             children: (0, a.jsx)(c.fef, { children: (0, a.jsx)(u.$jN, {}) }),
         });
-    let g =
+    let C =
             x && !f
                 ? {
                       variant: "primary",
                       text: A.intl.string(A.t["s+J8Dl"]),
                       onClick: () => {
-                          j.default.track(R.rMx.SLAYER_STOREFRONT_GAME_LAUNCH_CLICKED, {
+                          S.default.track(R.rMx.SLAYER_STOREFRONT_GAME_LAUNCH_CLICKED, {
                               location_stack: _,
                               sku_id: e.id,
                               application_id: s.id,
                               is_gift: !0,
                           }),
-                              S.Z.launchGame(s.id);
+                              T.Z.launchGame(s.id);
                       },
                       icon: c.rgF,
                   }
@@ -253,7 +267,7 @@ function Z(t) {
                       variant: "primary",
                       text: A.intl.string(A.t.cpT0Cq),
                       onClick: () => {
-                          j.default.track(R.rMx.SLAYER_STOREFRONT_CLAIM_MODAL_CLOSE_BUTTON_CLICKED, {
+                          S.default.track(R.rMx.SLAYER_STOREFRONT_CLAIM_MODAL_CLOSE_BUTTON_CLICKED, {
                               location_stack: _,
                               sku_id: e.id,
                               application_id: s.id,
@@ -271,7 +285,7 @@ function Z(t) {
         children: [
             (0, a.jsx)(v.y, {
                 sku: e,
-                title: A.intl.formatToPlainString(A.t["6yrIzU"], { sender: m }),
+                title: A.intl.formatToPlainString(A.t["6yrIzU"], { sender: g }),
             }),
             (0, a.jsxs)(c.fef, {
                 children: [
@@ -295,7 +309,7 @@ function Z(t) {
                 ],
             }),
             (0, a.jsx)(c.Go$, {
-                actions: [g],
+                actions: [C],
                 actionsFullWidth: !0,
             }),
         ],
