201140410c1485ca6e8022acf3b37e97b9c709a5
DiscrapperManager <admin@minerpl.xyz>
2025-11-19 23:28:41 +0100

    canaryRelease: 470176
    Main scripts: 4
    Lazy Loaded Scripts: 1704
    Chunks:
     * Main Files: 15677
     * Scripts: 9230
    Strings: 23189
    Routes: 771
    Experiments Count: 376
    Apex Experiments Count: 117

--- chunks/225657.js
+++ chunks/225657.js
@@ -1,28 +1,45 @@
-n.d(t, { Z: () => f });
+n.d(t, { Z: () => p }), n(388685);
 var r = n(54381),
     i = n(442837),
     a = n(481060),
     o = n(700582),
     s = n(987209),
-    l = n(699516),
-    c = n(594174),
-    u = n(51144),
-    d = n(388032);
-let f = (e) => {
-    let { selectedSkuId: t, validateSelectedGift: n, className: f, searchableSelectWrapperClassName: _ } = e,
-        p = (0, i.Wu)([l.Z], () => l.Z.getFriendIDs()),
-        h = (0, i.Wu)([c.default], () => c.default.filter((e) => p.includes(e.id) && !e.bot), [p]),
-        { giftRecipient: m, setGiftRecipient: g } = (0, s.wD)();
+    l = n(752048),
+    c = n(699516),
+    u = n(594174),
+    d = n(51144),
+    f = n(69143),
+    _ = n(388032);
+let p = (e) => {
+    let { selectedSkuId: t, validateSelectedGift: n, className: p, searchableSelectWrapperClassName: h } = e,
+        { affineGiftingEnabled: m, affinityThreshold: g } = f.Z.useConfig({
+            location: "Collectibles Gift Recipient Picker",
+        }),
+        E = (0, i.Wu)([c.Z], () => c.Z.getFriendIDs()),
+        b = (0, i.Wu)([l.Z], () =>
+            l.Z.getUserAffinities()
+                .filter((e) => {
+                    let { isFriend: t, communicationProbability: n, vcProbability: r } = e;
+                    return !t && (n >= g || r > g);
+                })
+                .map((e) => {
+                    let { otherUserId: t } = e;
+                    return t;
+                }),
+        ),
+        y = [...E, ...(m ? b : [])],
+        O = (0, i.Wu)([u.default], () => u.default.filter((e) => y.includes(e.id) && !e.bot), [y]),
+        { giftRecipient: v, setGiftRecipient: I } = (0, s.wD)();
     if (null == t) return null;
-    let E = (e) => {
-        n(e, t), g(e);
+    let T = (e) => {
+        n(e, t), I(e);
     };
     return (0, r.jsx)("div", {
-        className: f,
+        className: p,
         children: (0, r.jsx)(a.VcW, {
-            wrapperClassName: _,
-            label: d.intl.string(d.t.xFn72s),
-            placeholder: d.intl.string(d.t.R0vK0N),
+            wrapperClassName: h,
+            label: _.intl.string(_.t.xFn72s),
+            placeholder: _.intl.string(_.t.R0vK0N),
             renderOptionPrefix: (e) =>
                 (null == e ? void 0 : e.value) == null
                     ? null
@@ -30,11 +47,11 @@ let f = (e) => {
                           user: e.value,
                           size: a.EFr.SIZE_20,
                       }),
-            value: m,
-            onChange: E,
-            options: h.map((e) => ({
+            value: v,
+            onChange: T,
+            options: O.map((e) => ({
                 value: e,
-                label: "".concat(u.ZP.getUserTag(e)),
+                label: "".concat(d.ZP.getUserTag(e)),
             })),
         }),
     });
---
+++ chunks/69143.js
@@ -0,0 +1,23 @@
+n.d(t, { Z: () => r });
+let r = (0, n(427164).le)({
+    name: "2025-11-affine-collectible-gifting",
+    kind: "user",
+    defaultConfig: {
+        affineGiftingEnabled: !1,
+        affinityThreshold: 1,
+    },
+    variations: {
+        0: {
+            affineGiftingEnabled: !1,
+            affinityThreshold: 1,
+        },
+        1: {
+            affineGiftingEnabled: !0,
+            affinityThreshold: 0.3,
+        },
+        2: {
+            affineGiftingEnabled: !0,
+            affinityThreshold: 0.5,
+        },
+    },
+});
