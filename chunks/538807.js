6764a7b896fac18bbacd8579647f32fdfe7086e4
DiscrapperManager <admin@minerpl.xyz>
2025-11-19 01:37:03 +0100

    canaryRelease: 469876
    Main scripts: 4
    Lazy Loaded Scripts: 1705
    Chunks:
     * Main Files: 15655
     * Scripts: 9224
    Strings: 23183
    Routes: 771
    Experiments Count: 377
    Apex Experiments Count: 115

--- chunks/538807.js
+++ chunks/538807.js
@@ -2,5 +2,4 @@ r.exports = {
     badgeImageContainer: "badgeImageContainer__9953c",
     badgeImage: "badgeImage__9953c",
     progressCircleContainer: "progressCircleContainer__9953c",
-    progressCircleText: "progressCircleText__9953c",
 };
--- chunks/558592.js
+++ chunks/558592.js
@@ -1,44 +1,46 @@
-a.r(e),
-    a.d(e, {
-        BadgeImageWithProgressCircle: () => i,
-        default: () => l,
+e.r(a),
+    e.d(a, {
+        BadgeImageWithProgressCircle: () => l,
+        default: () => d,
     });
-var c = a(54381);
-a(473749);
-var n = a(925477),
-    o = a(410030),
-    s = a(457082),
-    t = a(768917);
-let i = (r) => {
-        let { src: e, alt: a, progressCircleText: i, progressCirclePercent: l, progressCircleUrgency: d } = r,
-            g = (0, o.ZP)();
+var c = e(54381);
+e(473749);
+var n = e(925477),
+    o = e(793030),
+    t = e(410030),
+    s = e(457082),
+    i = e(538807);
+let l = (r) => {
+        let { src: a, alt: e, progressCircleText: l, progressCirclePercent: d, progressCircleUrgency: g } = r,
+            u = (0, t.ZP)();
         return (0, c.jsxs)("div", {
-            className: t.badgeImageContainer,
+            className: i.badgeImageContainer,
             children: [
                 (0, c.jsx)("img", {
-                    alt: a,
-                    src: e,
-                    className: t.badgeImage,
+                    alt: e,
+                    src: a,
+                    className: i.badgeImage,
                 }),
-                null != i &&
-                    null != l &&
+                null != l &&
                     null != d &&
+                    null != g &&
                     (0, c.jsx)("div", {
-                        className: t.progressCircleContainer,
+                        className: i.progressCircleContainer,
                         children: (0, c.jsx)(s.Z, {
-                            innerRingPercent: l,
-                            outerRingColor: "critical" === d ? "var(--red-new-74)" : "var(--opacity-orange-36)",
-                            innerRingColor: "critical" === d ? "var(--red-new-43)" : "var(--opacity-orange-96)",
+                            innerRingPercent: d,
+                            outerRingColor: "critical" === g ? "var(--red-new-74)" : "var(--opacity-orange-36)",
+                            innerRingColor: "critical" === g ? "var(--red-new-43)" : "var(--opacity-orange-96)",
                             strokeWidth: 7,
-                            backgroundCircleColor: (0, n.wj)(g) ? "var(--opacity-black-72)" : "var(--opacity-white-72)",
+                            backgroundCircleColor: (0, n.wj)(u) ? "var(--opacity-black-72)" : "var(--opacity-white-72)",
                             backgroundCircleBlurAmount: 2,
-                            children: (0, c.jsx)("span", {
-                                className: t.progressCircleText,
-                                children: i,
+                            children: (0, c.jsx)(o.xvT, {
+                                variant: "text-sm/semibold",
+                                color: "text-secondary",
+                                children: l,
                             }),
                         }),
                     }),
             ],
         });
     },
-    l = i;
+    d = l;
