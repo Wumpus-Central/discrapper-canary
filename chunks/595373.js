505cb616e2577bfa21e44a4f43aec348a6d9d04c
DiscrapperManager <admin@minerpl.xyz>
2025-11-20 20:05:45 +0100

    canaryRelease: 470868
    Main scripts: 4
    Lazy Loaded Scripts: 1705
    Chunks:
     * Main Files: 15695
     * Scripts: 9236
    Strings: 23210
    Routes: 772
    Experiments Count: 373
    Apex Experiments Count: 120

--- chunks/595373.js
+++ chunks/595373.js
@@ -1,6 +1,6 @@
 n.d(e, {
+    J: () => N,
     Z: () => f,
-    p: () => N,
 });
 var i = n(54381),
     l = n(473749),
@@ -11,20 +11,20 @@ var i = n(54381),
     o = n(509613),
     c = n(131951),
     E = n(313789),
-    S = n(325808),
-    T = n(962100),
+    T = n(325808),
+    S = n(962100),
     d = n(518596),
     I = n(345655),
     O = n(981631),
     g = n(65154),
     _ = n(388032);
 function N() {
-    return (0, s.e7)([c.Z], () => c.Z.getActiveInputProfile() !== I._.STUDIO);
+    return (0, s.e7)([c.Z], () => c.Z.getMode() === g.pM.PUSH_TO_TALK);
 }
 function A() {
     let t = (0, s.e7)([c.Z], () => c.Z.getMode() === g.pM.PUSH_TO_TALK),
         e = l.useCallback((t) => {
-            (0, S.XH)(t ? g.pM.PUSH_TO_TALK : g.pM.VOICE_ACTIVITY, [a.Z.USER_SETTINGS_VOICE_AND_VIDEO]);
+            (0, T.XH)(t ? g.pM.PUSH_TO_TALK : g.pM.VOICE_ACTIVITY, [a.Z.USER_SETTINGS_VOICE_AND_VIDEO]);
         }, []),
         n = u.FB ? _.intl.string(_.t.tG4Np5) : _.intl.string(_.t.JMyQin),
         o = (0, s.e7)([c.Z], () => c.Z.getMode()),
@@ -34,7 +34,7 @@ function A() {
                     ? _.intl.format(_.t.HVvn5T, {
                           onClick: () => (0, d.openUserSettings)(E.n.KEYBINDS_PANEL, { section: O.oAB.KEYBINDS }),
                       })
-                    : _.intl.format(_.t.zvMPOc, { onDownloadClick: () => (0, T.y)("Help Text PTT") }),
+                    : _.intl.format(_.t.zvMPOc, { onDownloadClick: () => (0, S.y)("Help Text PTT") }),
             [o],
         );
     return (0, i.jsxs)(r.Kqy, {
@@ -54,7 +54,9 @@ function A() {
     });
 }
 let f = (0, o.ON)(E.n.VOICE_PUSH_TO_TALK_SETTING, {
-    usePredicate: N,
+    usePredicate: function () {
+        return (0, s.e7)([c.Z], () => c.Z.getActiveInputProfile() !== I._.STUDIO);
+    },
     render: () => (0, i.jsx)(A, {}),
     useSearchTerms: () => [
         u.FB ? _.intl.string(_.t.tG4Np5) : _.intl.string(_.t.JMyQin),
