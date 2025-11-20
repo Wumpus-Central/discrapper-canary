7f75040da506b05fb74f9a0ae6fa974c2c9fd77e
DiscrapperManager <admin@minerpl.xyz>
2025-11-20 20:24:02 +0100

    canaryRelease: 470885
    Main scripts: 4
    Lazy Loaded Scripts: 1705
    Chunks:
     * Main Files: 15695
     * Scripts: 9236
    Strings: 23211
    Routes: 772
    Experiments Count: 373
    Apex Experiments Count: 120

--- chunks/841081.js
+++ chunks/841081.js
@@ -1,17 +1,17 @@
-i.d(t, { default: () => p });
-var r = i(54381),
-    n = i(473749),
-    a = i(481060),
-    l = i(987209),
-    s = i(563132),
-    o = i(963249),
-    c = i(701464),
-    d = i(474936),
-    u = i(981631),
-    m = i(388032),
-    f = i(926845);
+r.d(t, { default: () => p });
+var i = r(54381),
+    n = r(473749),
+    a = r(481060),
+    l = r(987209),
+    s = r(563132),
+    o = r(963249),
+    c = r(701464),
+    d = r(474936),
+    u = r(981631),
+    m = r(388032),
+    f = r(926845);
 function h(e) {
-    let { giftRecipient: t, onClose: i, transitionState: l, analyticsLocations: h, analyticsObject: p } = e,
+    let { giftRecipient: t, onClose: r, transitionState: l, analyticsLocations: h, analyticsObject: p } = e,
         { priceOptions: g } = (0, s.JL)(),
         b = n.useCallback(
             (e) => {
@@ -30,55 +30,57 @@ function h(e) {
                                   object: u.qAy.BUTTON_ICON,
                                   objectType: u.AnalyticsObjectTypes.GIFT,
                               },
-                });
+                }),
+                    r();
             },
-            [t, h, p],
+            [t, h, p, r],
         );
-    return (0, r.jsxs)(a.Y0X, {
+    return (0, i.jsxs)(a.Y0X, {
         transitionState: l,
         size: a.CgR.DYNAMIC,
         className: f.modalContainer,
         parentComponent: "GiftOptionSelectModal",
         children: [
-            (0, r.jsx)(a.xBx, {
+            (0, i.jsx)(a.xBx, {
                 className: f.modalHeader,
-                children: (0, r.jsxs)(a.Kqy, {
+                children: (0, i.jsxs)(a.Kqy, {
                     direction: "horizontal",
                     justify: "space-between",
                     children: [
-                        (0, r.jsx)(a.Heading, {
+                        (0, i.jsx)(a.Heading, {
                             variant: "heading-md/semibold",
                             children: m.intl.string(m.t["wg/30i"]),
                         }),
-                        (0, r.jsx)(a.olH, { onClick: i }),
+                        (0, i.jsx)(a.olH, { onClick: r }),
                     ],
                 }),
             }),
-            (0, r.jsx)(a.hzk, {
+            (0, i.jsx)(a.hzk, {
                 className: f.modalBody,
-                children: (0, r.jsx)(c.Z, {
+                children: (0, i.jsx)(c.Z, {
                     onSelectSku: b,
                     giftRecipient: t,
                     priceOptions: g,
+                    onWishlistItemClick: r,
                 }),
             }),
         ],
     });
 }
 function p(e) {
-    let { giftRecipient: t, onClose: i, transitionState: n, analyticsLocations: a, analyticsObject: o } = e;
-    return (0, r.jsx)(s.PaymentContextProvider, {
+    let { giftRecipient: t, onClose: r, transitionState: n, analyticsLocations: a, analyticsObject: o } = e;
+    return (0, i.jsx)(s.PaymentContextProvider, {
         activeSubscription: null,
         stepConfigs: [],
         skuIDs: d.nS,
         isGift: !0,
-        children: (0, r.jsx)(l.KB, {
+        children: (0, i.jsx)(l.KB, {
             isGift: !0,
             giftRecipient: t,
             giftingOrigin: d.Wt.DM_CHANNEL,
-            children: (0, r.jsx)(h, {
+            children: (0, i.jsx)(h, {
                 giftRecipient: t,
-                onClose: i,
+                onClose: r,
                 transitionState: n,
                 analyticsLocations: a,
                 analyticsObject: o,
