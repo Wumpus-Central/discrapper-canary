c3831f3f542fdbac602e4a10b66d49e56dd9ff0b
DiscrapperManager <admin@minerpl.xyz>
2025-11-21 06:39:59 +0100

    canaryRelease: 471368
    Main scripts: 4
    Lazy Loaded Scripts: 1767
    Chunks:
     * Main Files: 15691
     * Scripts: 9312
    Strings: 23184
    Routes: 775
    Experiments Count: 373
    Apex Experiments Count: 119

--- chunks/720983.js
+++ chunks/720983.js
@@ -1,103 +1,105 @@
-l.d(n, { Z: () => _ });
-var t = l(54381),
-    i = l(473749),
-    a = l(120356),
-    r = l.n(a),
-    o = l(793030),
-    d = l(442837),
-    s = l(410030),
-    c = l(984370),
-    u = l(567400),
-    p = l(892001),
-    g = l(594174),
-    I = l(73346),
-    x = l(705338),
-    m = l(228168),
-    h = l(388032),
-    f = l(260999);
+t.d(n, { Z: () => C });
+var l = t(54381),
+    i = t(473749),
+    a = t(120356),
+    o = t.n(a),
+    s = t(793030),
+    r = t(442837),
+    d = t(410030),
+    c = t(100527),
+    u = t(984370),
+    p = t(567400),
+    g = t(892001),
+    I = t(594174),
+    x = t(73346),
+    _ = t(705338),
+    h = t(228168),
+    m = t(388032),
+    f = t(260999);
 function v(e) {
-    let { icon: n, onClick: l, ariaLabel: i, className: a } = e;
-    return (0, t.jsx)(o.P3F, {
-        className: r()(f.iconButton, a),
-        onClick: l,
+    let { icon: n, onClick: t, ariaLabel: i, className: a } = e;
+    return (0, l.jsx)(s.P3F, {
+        className: o()(f.iconButton, a),
+        onClick: t,
         "aria-label": i,
         children: n,
     });
 }
-function _(e) {
-    let { guildId: n, storefront: l, selectedPageIndex: a } = e,
-        _ = (0, o.wjy)((0, s.ZP)()),
-        C = (0, u.Y)({ location: "SocialLayerStorefrontHeader" }),
-        j = (0, d.e7)([g.default], () => g.default.getCurrentUser()),
-        k = i.useCallback(() => {
-            (null == j ? void 0 : j.id) != null &&
-                (0, p.openUserProfileModal)({
-                    userId: j.id,
-                    section: m.oh.WISHLIST,
+function C(e) {
+    let { guildId: n, storefront: t, selectedPageIndex: a } = e,
+        C = (0, s.wjy)((0, d.ZP)()),
+        j = (0, p.Y)({ location: "SocialLayerStorefrontHeader" }),
+        k = (0, r.e7)([I.default], () => I.default.getCurrentUser()),
+        b = i.useCallback(() => {
+            (null == k ? void 0 : k.id) != null &&
+                (0, g.openUserProfileModal)({
+                    userId: k.id,
+                    section: h.oh.WISHLIST,
                     showGuildProfile: !1,
+                    sourceAnalyticsLocations: [c.Z.SOCIAL_LAYER_STOREFRONT],
                 });
-        }, [j]);
-    if (null == l) return null;
-    let b = null != l.logoAssetId ? (0, I._W)(l.applicationId, l.logoAssetId, 75) : null,
-        Z = null != l.lightThemeLogoAssetId ? (0, I._W)(l.applicationId, l.lightThemeLogoAssetId, 75) : null,
-        N = null;
+        }, [k]);
+    if (null == t) return null;
+    let Z = null != t.logoAssetId ? (0, x._W)(t.applicationId, t.logoAssetId, 75) : null,
+        N = null != t.lightThemeLogoAssetId ? (0, x._W)(t.applicationId, t.lightThemeLogoAssetId, 75) : null,
+        S = null;
     return (
-        (N = _ ? (null != b ? b : Z) : null != Z ? Z : b),
-        (0, t.jsxs)(c.Z, {
+        (S = C ? (null != Z ? Z : N) : null != N ? N : Z),
+        (0, l.jsxs)(u.Z, {
             disableDoubleClick: !0,
             className: f.headerContainer,
             children: [
-                (0, t.jsxs)(o.P3F, {
+                (0, l.jsxs)(s.P3F, {
                     onClick: () => {
-                        (0, x.Z)({
+                        (0, _.Z)({
                             guildId: n,
                             pageIndex: 0,
                         });
                     },
                     className: f.titleContainer,
                     children: [
-                        null != N &&
-                            (0, t.jsx)("img", {
+                        null != S &&
+                            (0, l.jsx)("img", {
                                 className: f.logo,
-                                src: N,
-                                alt: l.title,
+                                src: S,
+                                alt: t.title,
                             }),
-                        (0, t.jsx)(c.Z.Title, { children: l.title }),
+                        (0, l.jsx)(u.Z.Title, { children: t.title }),
                     ],
                 }),
-                (0, t.jsx)("div", {
+                (0, l.jsx)("div", {
                     className: f.titles,
-                    children: l.pages.map((e, l) =>
-                        (0, t.jsx)(
-                            c.Z.Title,
+                    children: t.pages.map((e, t) =>
+                        (0, l.jsx)(
+                            u.Z.Title,
                             {
                                 onClick: () => {
-                                    (0, x.Z)({
+                                    (0, _.Z)({
                                         guildId: n,
-                                        pageIndex: l,
+                                        pageIndex: t,
                                     });
                                 },
                                 wrapperClassName: f.titleWrapper,
-                                className: r()(f.pageTitleContainer, { [f.selectedPageIndicator]: a === l }),
-                                children: (0, t.jsx)(o.xvT, {
+                                className: o()(f.pageTitleContainer, { [f.selectedPageIndicator]: a === t }),
+                                children: (0, l.jsx)(s.xvT, {
                                     variant: "text-sm/medium",
                                     children: e.title,
                                 }),
                             },
-                            "".concat(e.title, "-").concat(l),
+                            "".concat(e.title, "-").concat(t),
                         ),
                     ),
                 }),
-                C &&
-                    (0, t.jsx)("div", {
+                j &&
+                    (0, l.jsx)("div", {
                         className: f.alignedRightContent,
-                        children: (0, t.jsx)(v, {
-                            icon: (0, t.jsx)(o.h_8, {
+                        children: (0, l.jsx)(v, {
+                            icon: (0, l.jsx)(s.h_8, {
                                 size: "xs",
                                 color: "currentColor",
                             }),
-                            onClick: k,
-                            ariaLabel: h.intl.string(h.t["7lZ31J"]),
+                            onClick: b,
+                            ariaLabel: m.intl.string(m.t["7lZ31J"]),
                             className: f.wishlistButton,
                         }),
                     }),
