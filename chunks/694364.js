99089b2eb2c7364c9c8dc8c1bf15c7ca74272744
DiscrapperManager <admin@minerpl.xyz>
2025-11-21 01:12:12 +0100

    canaryRelease: 471119
    Main scripts: 4
    Lazy Loaded Scripts: 1707
    Chunks:
     * Main Files: 15685
     * Scripts: 9251
    Strings: 23219
    Routes: 774
    Experiments Count: 373
    Apex Experiments Count: 119

--- chunks/694364.js
+++ chunks/694364.js
@@ -1,14 +1,11 @@
-t.d(n, { J: () => s });
+t.d(n, { J: () => o });
 var r = t(873546),
     l = t(884697),
-    i = t(152563),
-    a = t(27123),
-    o = t(786040);
-let s = (e, n) => {
-    let t = (0, i.t)("collectibles-shop-gift-button"),
-        s = (0, l.G1)(e),
-        c = (0, l.rN)(e),
-        u = (0, a.r1)(e),
-        d = (0, l.x6)(e) && !t;
-    return !s && !c && !u && n !== o.tA.ORBS && !d && !r.tq;
+    i = t(27123),
+    a = t(786040);
+let o = (e, n) => {
+    let t = (0, l.G1)(e),
+        o = (0, l.rN)(e),
+        s = (0, i.r1)(e);
+    return !t && !o && !s && n !== a.tA.ORBS && !r.tq;
 };
