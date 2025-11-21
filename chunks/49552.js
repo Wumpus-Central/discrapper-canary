54bdb0cfc5d3fafb7fdf4b1917ad4809440542d9
DiscrapperManager <admin@minerpl.xyz>
2025-11-21 17:06:33 +0100

    canaryRelease: 471420
    Main scripts: 4
    Lazy Loaded Scripts: 1767
    Chunks:
     * Main Files: 15691
     * Scripts: 9314
    Strings: 23184
    Routes: 775
    Experiments Count: 373
    Apex Experiments Count: 119

--- chunks/49552.js
+++ chunks/49552.js
@@ -2,8 +2,6 @@ var r = {
     progressContainer: "progressContainer__9258b",
     blue: "blue__9258b",
     orange: "orange__9258b",
-    unset: "unset__9258b",
-    fill: "fill__9258b",
     progress: "progress__9258b",
     glow: "glow__9258b",
     empty: "empty__9258b",
--- chunks/527685.js
+++ chunks/527685.js
@@ -19,14 +19,20 @@ let u = (e) => {
         p = (0, s.ZP)(),
         h = null != (t = null == d ? void 0 : d[p]) ? t : null == d ? void 0 : d.default,
         m = i.useMemo(() => ((n - a) / (c - a)) * 100, [n, a, c]),
-        g = 24 + (m / 100) * 30,
-        E = (g - 4) / 2,
-        b = n === a;
+        g = n === a,
+        E = _
+            ? {
+                  "--custom-background": null == h ? void 0 : h.background,
+                  "--custom-gradient-start": null == h ? void 0 : h.gradientStart,
+                  "--custom-gradient-end": null == h ? void 0 : h.gradientEnd,
+                  "--custom-gradient-glow": null == h ? void 0 : h.gradientEnd,
+              }
+            : {};
     return (0, r.jsx)("div", {
         className: o()(l.progressContainer, l[u]),
-        style: { background: _ ? (null == h ? void 0 : h.background) : void 0 },
+        style: E,
         children: (0, r.jsxs)("div", {
-            className: o()(l.progress, { [l.empty]: b }),
+            className: o()(l.progress, { [l.empty]: g }),
             style: { width: "".concat(m, "%") },
             role: "meter",
             "aria-valuenow": n,
@@ -34,67 +40,8 @@ let u = (e) => {
             "aria-valuemax": c,
             "aria-labelledby": f,
             children: [
-                (0, r.jsxs)("svg", {
-                    width: "100%",
-                    height: g,
-                    style: { transform: "translate(0, -".concat(E, "px)") },
-                    className: o()(l.glow, { [l.empty]: b }),
-                    viewBox: "0 0 220 24",
-                    preserveAspectRatio: "none",
-                    fill: "none",
-                    xmlns: "http://www.w3.org/2000/svg",
-                    children: [
-                        (0, r.jsx)("g", {
-                            filter: "url(#filter0_f_650_66545)",
-                            children: (0, r.jsx)("path", {
-                                className: o()(l.fill, l[u]),
-                                d: "M220 6.70215L220 16.7021L115.211 12.3494L6.70215 11.7022L115.211 11.0549L220 6.70215Z",
-                                fill: _ ? (null == h ? void 0 : h.gradientEnd) : void 0,
-                            }),
-                        }),
-                        (0, r.jsx)("defs", {
-                            children: (0, r.jsxs)("filter", {
-                                id: "filter0_f_650_66545",
-                                x: "-10",
-                                y: "-15",
-                                width: "240",
-                                height: "54",
-                                filterUnits: "userSpaceOnUse",
-                                colorInterpolationFilters: "sRGB",
-                                children: [
-                                    (0, r.jsx)("feFlood", {
-                                        floodOpacity: "0",
-                                        result: "BackgroundImageFix",
-                                    }),
-                                    (0, r.jsx)("feBlend", {
-                                        mode: "normal",
-                                        in: "SourceGraphic",
-                                        in2: "BackgroundImageFix",
-                                        result: "shape",
-                                    }),
-                                    (0, r.jsx)("feGaussianBlur", {
-                                        stdDeviation: "3.35106",
-                                        result: "effect1_foregroundBlur_650_66545",
-                                    }),
-                                ],
-                            }),
-                        }),
-                    ],
-                }),
-                (0, r.jsx)("div", {
-                    className: o()(l.bar, l[u], { [l.empty]: b }),
-                    style: {
-                        background: _
-                            ? b
-                                ? null == h
-                                    ? void 0
-                                    : h.gradientEnd
-                                : "linear-gradient(90deg, "
-                                      .concat(null == h ? void 0 : h.gradientStart, " 0%, ")
-                                      .concat(null == h ? void 0 : h.gradientEnd, " 100%)")
-                            : void 0,
-                    },
-                }),
+                (0, r.jsx)("div", { className: o()(l.glow, { [l.empty]: g }) }),
+                (0, r.jsx)("div", { className: o()(l.bar, l[u], { [l.empty]: g }) }),
             ],
         }),
     });
