5e4e9d9ee5380c504be2caa57a60fd2900a3ee61
DiscrapperManager <admin@minerpl.xyz>
2025-11-21 19:40:04 +0100

    canaryRelease: 471538
    Main scripts: 4
    Lazy Loaded Scripts: 1766
    Chunks:
     * Main Files: 15697
     * Scripts: 9316
    Strings: 23193
    Routes: 775
    Experiments Count: 373
    Apex Experiments Count: 120

--- chunks/193183.js
+++ chunks/193183.js
@@ -2,4 +2,5 @@ e.exports = {
     neverShow: "neverShow__6480d",
     cancelButton: "cancelButton__6480d",
     footerButtons: "footerButtons__6480d",
+    radioMessageContainer: "radioMessageContainer__6480d",
 };
--- chunks/30575.js
+++ chunks/30575.js
@@ -4,44 +4,44 @@ var a = n(54381),
     l = n(755721),
     r = n(481060),
     o = n(493773),
-    c = n(626135),
+    s = n(626135),
     d = n(326279),
-    s = n(533583),
+    c = n(533583),
     u = n(981631),
     m = n(388032),
     N = n(193183);
 function T(e) {
     switch (e) {
-        case s.Q.INPUT:
-            return s.a.INPUT;
-        case s.Q.OUTPUT:
-            return s.a.OUTPUT;
-        case s.Q.INPUT_AND_OUTPUT:
-            return s.a.INPUT_AND_OUTPUT;
+        case c.Q.INPUT:
+            return c.a.INPUT;
+        case c.Q.OUTPUT:
+            return c.a.OUTPUT;
+        case c.Q.INPUT_AND_OUTPUT:
+            return c.a.INPUT_AND_OUTPUT;
         default:
             return null;
     }
 }
 function p(e) {
     let t,
-        { device: n, certifiedDeviceMetadata: p, onClose: f, transitionState: v } = e,
+        { device: n, certifiedDeviceMetadata: p, onClose: v, transitionState: f } = e,
         [U, h] = (0, i.useState)(null != n ? T(n.type) : null),
         _ = (0, i.useRef)(!1);
-    function D() {
+    function x() {
         (_.current = !0), d.Wz(n.displayName);
     }
     (0, o.zq)(() => {
-        _.current || D();
+        _.current || x();
     }),
         (0, i.useEffect)(() => {
-            c.default.track(u.rMx.OPEN_MODAL, {
+            s.default.track(u.rMx.OPEN_MODAL, {
                 type: null != p ? "New Audio Device Detected - Certified" : "New Audio Device Detected - Not Certified",
                 device_name: null != p ? "".concat(p.vendor.name, " ").concat(p.model.name) : n.displayName,
             });
         }, [n.displayName, p]);
-    let x = null != p ? m.intl.string(m.t.kiqvHd) : m.intl.string(m.t.AbnGI5);
+    let D = null != p ? m.intl.string(m.t.kiqvHd) : m.intl.string(m.t.AbnGI5);
     switch (n.type) {
-        case s.Q.INPUT:
+        case c.Q.INPUT:
             t =
                 null != p
                     ? m.intl.format(m.t.NqNqy2, {
@@ -50,7 +50,7 @@ function p(e) {
                       })
                     : m.intl.format(m.t.stSaRi, { name: n.displayName });
             break;
-        case s.Q.OUTPUT:
+        case c.Q.OUTPUT:
             t =
                 null != p
                     ? m.intl.format(m.t.uPgqIA, {
@@ -60,24 +60,28 @@ function p(e) {
                     : m.intl.format(m.t.jyhYp1, { name: n.displayName });
             break;
         default:
-            t = (0, a.jsxs)(i.Fragment, {
+            t = (0, a.jsxs)("div", {
+                className: N.radioMessageContainer,
                 children: [
-                    null != p
-                        ? m.intl.format(m.t["4Xyzxy"], {
-                              vendorName: p.vendor.name,
-                              modelName: p.model.name,
-                          })
-                        : m.intl.format(m.t.Z7XQcr, { name: n.displayName }),
+                    (0, a.jsx)("div", {
+                        children:
+                            null != p
+                                ? m.intl.format(m.t["4Xyzxy"], {
+                                      vendorName: p.vendor.name,
+                                      modelName: p.model.name,
+                                  })
+                                : m.intl.format(m.t.Z7XQcr, { name: n.displayName }),
+                    }),
                     (0, a.jsx)(r.FXm, {
                         value: (function (e) {
                             if (null == e) return null;
                             switch (e) {
-                                case s.a.INPUT:
-                                    return s.Q.INPUT;
-                                case s.a.OUTPUT:
-                                    return s.Q.OUTPUT;
-                                case s.a.INPUT_AND_OUTPUT:
-                                    return s.Q.INPUT_AND_OUTPUT;
+                                case c.a.INPUT:
+                                    return c.Q.INPUT;
+                                case c.a.OUTPUT:
+                                    return c.Q.OUTPUT;
+                                case c.a.INPUT_AND_OUTPUT:
+                                    return c.Q.INPUT_AND_OUTPUT;
                                 default:
                                     return null;
                             }
@@ -86,15 +90,15 @@ function p(e) {
                         options: [
                             {
                                 name: m.intl.string(m.t.vhxqwT),
-                                value: s.Q.INPUT_AND_OUTPUT,
+                                value: c.Q.INPUT_AND_OUTPUT,
                             },
                             {
                                 name: m.intl.string(m.t["Kqs9+J"]),
-                                value: s.Q.INPUT,
+                                value: c.Q.INPUT,
                             },
                             {
                                 name: m.intl.string(m.t.GGlM3e),
-                                value: s.Q.OUTPUT,
+                                value: c.Q.OUTPUT,
                             },
                         ],
                     }),
@@ -102,15 +106,15 @@ function p(e) {
             });
     }
     return (0, a.jsxs)(r.Y0X, {
-        transitionState: v,
-        "aria-label": x,
+        transitionState: f,
+        "aria-label": D,
         parentComponent: "ConnectedDeviceDetectionModal",
         children: [
             (0, a.jsx)(r.xBx, {
                 separator: !1,
                 children: (0, a.jsx)(r.Heading, {
                     variant: "heading-lg/semibold",
-                    children: x,
+                    children: D,
                 }),
             }),
             (0, a.jsx)(r.hzk, {
@@ -128,7 +132,7 @@ function p(e) {
                                     onClick: () => {
                                         (_.current = !0),
                                             d.Gl(n.displayName),
-                                            c.default.track(u.rMx.MODAL_DISMISSED, {
+                                            s.default.track(u.rMx.MODAL_DISMISSED, {
                                                 type:
                                                     null != p
                                                         ? "New Audio Device Detected - Certified"
@@ -136,7 +140,7 @@ function p(e) {
                                                 location_object: n.displayName,
                                                 dismiss_type: "permanent",
                                             }),
-                                            f();
+                                            v();
                                     },
                                     children: m.intl.formatToPlainString(m.t.cwS3E6, { device: n.displayName }),
                                 }),
@@ -157,7 +161,7 @@ function p(e) {
                                 variant: "secondary",
                                 text: m.intl.string(m.t.vPBgq7),
                                 onClick: () => {
-                                    D(), f();
+                                    x(), v();
                                 },
                             }),
                         }),
@@ -167,7 +171,7 @@ function p(e) {
                             onClick: () => {
                                 (_.current = !0),
                                     null != U && d.mo(n.displayName, U, "New Audio Device Detected Modal"),
-                                    f();
+                                    v();
                             },
                         }),
                     ],
