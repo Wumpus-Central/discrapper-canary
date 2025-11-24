368796d68945f80dee3e29f68184d335e21eceb8
DiscrapperManager <admin@minerpl.xyz>
2025-11-24 18:29:33 +0100

    canaryRelease: 472040
    Main scripts: 4
    Lazy Loaded Scripts: 1767
    Chunks:
     * Main Files: 15690
     * Scripts: 9342
    Strings: 23214
    Routes: 776
    Experiments Count: 373
    Apex Experiments Count: 119

--- chunks/687131.js
+++ chunks/687131.js
@@ -1,38 +1,37 @@
-n.d(t, { z: () => d }), n(997841);
+n.d(t, { z: () => u }), n(997841);
 var r = n(95015),
     i = n(757266),
     a = n(797258),
     o = n(358085),
-    s = n(535246),
-    l = n(212517),
-    c = n(981631);
-function u(e) {
+    s = n(212517),
+    l = n(981631);
+function c(e) {
     let { platform: t } = e;
     return (
-        !!(0, l.D)(e) ||
+        !!(0, s.D)(e) ||
         ((0, o.isAndroid)()
-            ? t === c.M7m.ANDROID
+            ? t === l.M7m.ANDROID
             : (0, o.isIOS)()
-              ? t === c.M7m.IOS
+              ? t === l.M7m.IOS
               : (0, o.isWeb)()
-                ? t === c.M7m.DESKTOP
+                ? t === l.M7m.DESKTOP
                 : !!(0, o.isDesktop)() &&
                   null != e.application_id &&
                   i.Z.isConnected(e.application_id) &&
-                  t === c.M7m.DESKTOP)
+                  t === l.M7m.DESKTOP)
     );
 }
-function d(e) {
+function u(e) {
     var t, n, i;
-    if (!(0, s.v)() || null == e) return null;
+    if (null == e) return null;
     let o = e.application_id;
-    if (null == o || !(0, r.yE)(null != (t = e.flags) ? t : 0, c.xjy.SUPPORTS_GATEWAY_ACTIVITY_ACTION_JOIN))
+    if (null == o || !(0, r.yE)(null != (t = e.flags) ? t : 0, l.xjy.SUPPORTS_GATEWAY_ACTIVITY_ACTION_JOIN))
         return null;
-    let l = a.Z.getRemoteApplicationActivity(o);
-    return null == l || u(l)
+    let s = a.Z.getRemoteApplicationActivity(o);
+    return null == s || c(s)
         ? null
-        : (0, r.yE)(null != (n = l.flags) ? n : 0, c.xjy.SUPPORTS_GATEWAY_ACTIVITY_ACTION_JOIN) &&
-            null != (i = l.platform)
+        : (0, r.yE)(null != (n = s.flags) ? n : 0, l.xjy.SUPPORTS_GATEWAY_ACTIVITY_ACTION_JOIN) &&
+            null != (i = s.platform)
           ? i
           : null;
 }
