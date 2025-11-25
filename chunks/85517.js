c167fb5b513f066e04695e7025a3101fc0733f19
DiscrapperManager <admin@minerpl.xyz>
2025-11-25 22:56:53 +0100

    canaryRelease: 472839
    Main scripts: 4
    Lazy Loaded Scripts: 1801
    Chunks:
     * Main Files: 15692
     * Scripts: 9390
    Strings: 23239
    Routes: 776
    Experiments Count: 371
    Apex Experiments Count: 123

--- chunks/85517.js
+++ chunks/85517.js
@@ -151,8 +151,8 @@ function j(e) {
                                         className: C.logo,
                                     }),
                                     (0, r.jsx)(u.Text, {
-                                        color: "text-secondary",
-                                        variant: "text-sm/semibold",
+                                        color: "text-primary",
+                                        variant: "text-sm/normal",
                                         className: C.sponsoredTag,
                                         children: I.intl.string(I.t.OJjFi5),
                                     }),
