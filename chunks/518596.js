8cd25ef60f18266d70aefe27930c59e6bdd8aced
DiscrapperManager <admin@minerpl.xyz>
2025-11-25 01:16:07 +0100

    canaryRelease: 472428
    Main scripts: 4
    Lazy Loaded Scripts: 1798
    Chunks:
     * Main Files: 15692
     * Scripts: 9376
    Strings: 23226
    Routes: 776
    Experiments Count: 370
    Apex Experiments Count: 120

--- chunks/518596.js
+++ chunks/518596.js
@@ -1,10 +1,10 @@
 n.r(t),
     n.d(t, {
-        USER_SETTINGS_MODAL_KEY: () => b,
-        getUserSettingsSectionsByWebUserSettings: () => T,
-        getWebUserSettingsByUserSettingsSections: () => I,
-        openUserSettings: () => y,
-        openUserSettingsFromParsedUrl: () => S,
+        USER_SETTINGS_MODAL_KEY: () => y,
+        getUserSettingsSectionsByWebUserSettings: () => S,
+        getWebUserSettingsByUserSettingsSections: () => T,
+        openUserSettings: () => O,
+        openUserSettingsFromParsedUrl: () => A,
     }),
     n(388685),
     n(35282);
@@ -13,12 +13,13 @@ var r = n(54381),
     a = n(570140),
     o = n(37234),
     s = n(230711),
-    l = n(996435),
-    c = n(626135),
-    u = n(313789),
-    d = n(526665),
-    f = n(981631);
-function _(e, t, n) {
+    l = n(924557),
+    c = n(996435),
+    u = n(626135),
+    d = n(313789),
+    f = n(526665),
+    _ = n(981631);
+function p(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -31,7 +32,7 @@ function _(e, t, n) {
         e
     );
 }
-function p(e) {
+function h(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -42,12 +43,12 @@ function p(e) {
                 }),
             )),
             r.forEach(function (t) {
-                _(e, t, n[t]);
+                p(e, t, n[t]);
             });
     }
     return e;
 }
-function h(e, t) {
+function m(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -59,22 +60,22 @@ function h(e, t) {
     }
     return n;
 }
-function m(e, t) {
+function g(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : h(Object(t)).forEach(function (n) {
+            : m(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
     );
 }
-function g(e, t) {
+function E(e, t) {
     if (null == e) return {};
     var n,
         r,
-        i = E(e, t);
+        i = b(e, t);
     if (Object.getOwnPropertySymbols) {
         var a = Object.getOwnPropertySymbols(e);
         for (r = 0; r < a.length; r++)
@@ -82,7 +83,7 @@ function g(e, t) {
     }
     return i;
 }
-function E(e, t) {
+function b(e, t) {
     if (null == e) return {};
     var n,
         r,
@@ -91,100 +92,101 @@ function E(e, t) {
     for (r = 0; r < a.length; r++) (n = a[r]), t.indexOf(n) >= 0 || (i[n] = e[n]);
     return i;
 }
-let b = "USER_SETTINGS_MODAL_MODAL_KEY";
-async function y(e) {
-    let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : { section: f.oAB.ACCOUNT },
+let y = "USER_SETTINGS_MODAL_MODAL_KEY";
+async function O(e) {
+    let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : { section: _.oAB.ACCOUNT },
         s = arguments.length > 2 ? arguments[2] : void 0;
-    var { subsection: c, stackingBehavior: u = "replaceAll" } = t,
-        _ = g(t, ["subsection", "stackingBehavior"]);
+    var { subsection: l, stackingBehavior: u = "replaceAll" } = t,
+        d = E(t, ["subsection", "stackingBehavior"]);
     a.Z.dispatch(
-        p(
+        h(
             {
                 type: "USER_SETTINGS_MODAL_OPEN",
-                subsection: null != c ? c : null,
+                subsection: null != l ? l : null,
             },
-            _,
+            d,
         ),
     ),
-        (0, d.yP)("openUserSettings")
-            ? (0, i.nfh)(b)
-                ? l.Z.setState({ targetKey: e })
+        (0, f.yP)("openUserSettings")
+            ? (0, i.nfh)(y)
+                ? c.Z.setState({ targetKey: e })
                 : await (0, i.ZDy)(
                       async () => {
-                          let { default: t } = await Promise.all([n.e("9452"), n.e("62191")]).then(n.bind(n, 245286));
-                          return (n) => (0, r.jsx)(t, m(p({}, n), { target: e }));
+                          let { default: t } = await Promise.all([n.e("9452"), n.e("46984")]).then(n.bind(n, 245286));
+                          return (n) => (0, r.jsx)(t, g(h({}, n), { target: e }));
                       },
                       {
-                          modalKey: b,
+                          modalKey: y,
                           stackingBehavior: u,
                           stackNextByDefault: !0,
                       },
                   )
-            : (0, o.jN)(f.S9g.USER_SETTINGS),
+            : (0, o.jN)(_.S9g.USER_SETTINGS),
         null == s || s();
-}
-function O(e) {
-    let { section: t, subsection: n, urlOrigin: r } = e;
-    c.default.track(f.rMx.USER_SETTINGS_URL_PARSED, {
-        user_settings_section: t,
-        user_settings_subsection: n,
-        user_settings_url_origin: r,
-    });
 }
 function v(e) {
     let { section: t, subsection: n, urlOrigin: r } = e;
-    c.default.track(f.rMx.USER_SETTINGS_URL_PARSING_FAILED, {
+    u.default.track(_.rMx.USER_SETTINGS_URL_PARSED, {
         user_settings_section: t,
         user_settings_subsection: n,
         user_settings_url_origin: r,
     });
 }
-function I() {
-    let e = (0, d.KV)("getWebUserSettingFromSection");
-    return new Map([
-        [f.oAB.ACCOUNT, u.n.ACCOUNT_PANEL],
-        [f.oAB.PROFILE_CUSTOMIZATION, u.n.PROFILE_PANEL],
-        [f.oAB.CONTENT_AND_SOCIAL, u.n.CONTENT_AND_SOCIAL_PANEL],
-        [f.oAB.DATA_AND_PRIVACY, u.n.DATA_AND_PRIVACY_PANEL],
-        [f.oAB.FAMILY_CENTER, u.n.FAMILY_CENTER_PANEL],
-        [f.oAB.SESSIONS, u.n.SESSIONS_PANEL],
-        [f.oAB.AUTHORIZED_APPS, u.n.AUTHORIZED_APPS_PANEL],
-        [f.oAB.CONNECTIONS, u.n.CONNECTIONS_PANEL],
-        [f.oAB.CLIPS, u.n.CLIPS_PANEL],
-        [f.oAB.CF_WARP, u.n.PRIVATE_BROWSING_PANEL],
-        [f.oAB.PREMIUM, u.n.NITRO_PANEL],
-        [f.oAB.GUILD_BOOSTING, u.n.PREMIUM_GUILD_SUBSCRIPTIONS_PANEL],
-        [f.oAB.SUBSCRIPTIONS, u.n.SUBSCRIPTIONS_PANEL],
-        [f.oAB.INVENTORY, u.n.GIFT_PANEL],
-        [f.oAB.BILLING, u.n.BILLING_PANEL],
-        [f.oAB.APPEARANCE, u.n.APPEARANCE_PANEL],
-        [f.oAB.ACCESSIBILITY, u.n.ACCESSIBILITY_PANEL],
-        [f.oAB.VOICE, u.n.VOICE_AND_VIDEO_PANEL],
-        [f.oAB.POGGERMODE, u.n.POGGERMODE_PANEL],
-        [f.oAB.TEXT, u.n.CHAT_PANEL],
-        [f.oAB.NOTIFICATIONS, e ? u.n.NOTIFICATIONS_PANEL : u.n.LEGACY_NOTIFICATIONS_SETTINGS_PANEL],
-        [f.oAB.KEYBINDS, u.n.KEYBINDS_PANEL],
-        [f.oAB.LOCALE, u.n.LOCALE_PANEL],
-        [f.oAB.WINDOWS, u.n.WINDOWS_PANEL],
-        [f.oAB.LINUX, u.n.LINUX_PANEL],
-        [f.oAB.STREAMER_MODE, u.n.STREAMER_MODE_PANEL],
-        [f.oAB.ADVANCED, u.n.ADVANCED_PANEL],
-        [f.oAB.ACTIVITY_PRIVACY, u.n.ACTIVITY_PRIVACY_PANEL],
-        [f.oAB.REGISTERED_GAMES, u.n.REGISTERED_GAMES_PANEL],
-        [f.oAB.OVERLAY, u.n.OVERLAY_PANEL],
-        [f.oAB.EXPERIMENTS, u.n.EXPERIMENTS_PANEL],
-        [f.oAB.DEVELOPER_OPTIONS, u.n.DEVELOPER_OPTIONS_PANEL],
-    ]);
+function I(e) {
+    let { section: t, subsection: n, urlOrigin: r } = e;
+    u.default.track(_.rMx.USER_SETTINGS_URL_PARSING_FAILED, {
+        user_settings_section: t,
+        user_settings_subsection: n,
+        user_settings_url_origin: r,
+    });
 }
 function T() {
-    let e = I(),
+    let e = (0, f.KV)("getWebUserSettingFromSection"),
+        t = new Map([
+            [_.oAB.ACCOUNT, d.n.ACCOUNT_PANEL],
+            [_.oAB.PROFILE_CUSTOMIZATION, d.n.PROFILE_PANEL],
+            [_.oAB.CONTENT_AND_SOCIAL, d.n.CONTENT_AND_SOCIAL_PANEL],
+            [_.oAB.DATA_AND_PRIVACY, d.n.DATA_AND_PRIVACY_PANEL],
+            [_.oAB.FAMILY_CENTER, d.n.FAMILY_CENTER_PANEL],
+            [_.oAB.SESSIONS, d.n.SESSIONS_PANEL],
+            [_.oAB.AUTHORIZED_APPS, d.n.AUTHORIZED_APPS_PANEL],
+            [_.oAB.CONNECTIONS, d.n.CONNECTIONS_PANEL],
+            [_.oAB.CLIPS, d.n.CLIPS_PANEL],
+            [_.oAB.CF_WARP, d.n.PRIVATE_BROWSING_PANEL],
+            [_.oAB.PREMIUM, d.n.NITRO_PANEL],
+            [_.oAB.GUILD_BOOSTING, d.n.PREMIUM_GUILD_SUBSCRIPTIONS_PANEL],
+            [_.oAB.SUBSCRIPTIONS, d.n.SUBSCRIPTIONS_PANEL],
+            [_.oAB.INVENTORY, d.n.GIFT_PANEL],
+            [_.oAB.BILLING, d.n.BILLING_PANEL],
+            [_.oAB.APPEARANCE, d.n.APPEARANCE_PANEL],
+            [_.oAB.ACCESSIBILITY, d.n.ACCESSIBILITY_PANEL],
+            [_.oAB.VOICE, d.n.VOICE_AND_VIDEO_PANEL],
+            [_.oAB.POGGERMODE, d.n.POGGERMODE_PANEL],
+            [_.oAB.TEXT, d.n.CHAT_PANEL],
+            [_.oAB.NOTIFICATIONS, e ? d.n.NOTIFICATIONS_PANEL : d.n.LEGACY_NOTIFICATIONS_SETTINGS_PANEL],
+            [_.oAB.KEYBINDS, d.n.KEYBINDS_PANEL],
+            [_.oAB.LOCALE, d.n.LOCALE_PANEL],
+            [_.oAB.WINDOWS, d.n.WINDOWS_PANEL],
+            [_.oAB.LINUX, d.n.LINUX_PANEL],
+            [_.oAB.STREAMER_MODE, d.n.STREAMER_MODE_PANEL],
+            [_.oAB.ADVANCED, d.n.ADVANCED_PANEL],
+            [_.oAB.ACTIVITY_PRIVACY, d.n.ACTIVITY_PRIVACY_PANEL],
+            [_.oAB.REGISTERED_GAMES, d.n.REGISTERED_GAMES_PANEL],
+            [_.oAB.OVERLAY, d.n.OVERLAY_PANEL],
+            [_.oAB.EXPERIMENTS, d.n.EXPERIMENTS_PANEL],
+            [_.oAB.DEVELOPER_OPTIONS, d.n.DEVELOPER_OPTIONS_PANEL],
+        ]);
+    return e && !(0, l.w2)() && t.delete(_.oAB.CLIPS), t;
+}
+function S() {
+    let e = T(),
         t = new Map();
     for (let [n, r] of e.entries()) t.set(r, n);
     return t;
 }
-function S(e) {
+function A(e) {
     let { match: t, urlOrigin: n, analyticsLocations: r } = e;
-    O({
+    v({
         section: t.section,
         subsection: t.subsection,
         urlOrigin: n,
@@ -196,19 +198,19 @@ function S(e) {
             analyticsLocations: r,
         });
     };
-    if ((0, d.yP)("renderUserSettingsRedirector")) {
-        let e = I(),
+    if ((0, f.yP)("renderUserSettingsRedirector")) {
+        let e = T(),
             r = e.get(t.section);
         if (null == r)
-            v({
+            I({
                 section: t.section,
                 subsection: t.subsection,
                 urlOrigin: n,
             }),
                 i();
         else {
-            let n = null != t.subsection && "" !== t.subsection ? e.get(f.oAB[t.subsection]) : null;
-            y(null != n ? n : r, {
+            let n = null != t.subsection && "" !== t.subsection ? e.get(_.oAB[t.subsection]) : null;
+            O(null != n ? n : r, {
                 section: t.section,
                 subsection: t.subsection,
             });
