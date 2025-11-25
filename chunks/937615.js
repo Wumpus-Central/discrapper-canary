22732c054bb1c51a14cc582bbed70b9de2ba05c2
DiscrapperManager <admin@minerpl.xyz>
2025-11-25 16:36:35 +0100

    canaryRelease: 472583
    Main scripts: 4
    Lazy Loaded Scripts: 1801
    Chunks:
     * Main Files: 15687
     * Scripts: 9390
    Strings: 23235
    Routes: 776
    Experiments Count: 371
    Apex Experiments Count: 120

--- chunks/937615.js
+++ chunks/937615.js
@@ -1,51 +1,53 @@
 n.d(t, {
-    T3: () => d,
-    T4: () => c,
-    og: () => u,
-    qr: () => p,
-    xg: () => f,
+    T3: () => f,
+    T4: () => u,
+    og: () => d,
+    qr: () => h,
+    xg: () => _,
 }),
     n(415506),
     n(704826),
     n(35282);
 var r = n(221513),
     i = n(706454),
-    a = n(74538),
-    o = n(474936),
-    s = n(388032);
-let l = Object.freeze(["en-CA", "en-AU", "en-NZ"]);
-function c(e, t, n) {
-    var a;
-    let o = null != n ? n : {},
-        s = null != (a = null == n ? void 0 : n.localeOverride) ? a : i.default.locale;
-    "en-US" === s && l.includes(i.default.systemLocale) && (o.currencyDisplay = "code");
-    let { maximumFractionDigits: c, minimumFractionDigits: u } = o;
-    return 0 === c && null == u && (o.minimumFractionDigits = 0), (0, r.T4)(e, t, s, o);
-}
+    a = n(358085),
+    o = n(74538),
+    s = n(474936),
+    l = n(388032);
+let c = Object.freeze(["en-CA", "en-AU", "en-NZ"]);
 function u(e, t, n) {
-    if (t === o.rV.YEAR) return s.intl.formatToPlainString(s.t["rS8FA+"], { price: e });
-    if (t === o.rV.MONTH && 1 === n) return s.intl.formatToPlainString(s.t.AbOLNu, { price: e });
-    if (t === o.rV.MONTH && n > 1)
-        return s.intl.formatToPlainString(s.t["Qc+9ww"], {
+    var o;
+    let s = null != n ? n : {},
+        l = null != (o = null == n ? void 0 : n.localeOverride) ? o : i.default.locale;
+    "en-US" === l && c.includes(i.default.systemLocale) && (s.currencyDisplay = "code"),
+        "en-US" === l && (0, a.isWindows)() && "en-GB" === i.default.systemLocale && (s.currencyDisplay = "code");
+    let { maximumFractionDigits: u, minimumFractionDigits: d } = s;
+    return 0 === u && null == d && (s.minimumFractionDigits = 0), (0, r.T4)(e, t, l, s);
+}
+function d(e, t, n) {
+    if (t === s.rV.YEAR) return l.intl.formatToPlainString(l.t["rS8FA+"], { price: e });
+    if (t === s.rV.MONTH && 1 === n) return l.intl.formatToPlainString(l.t.AbOLNu, { price: e });
+    if (t === s.rV.MONTH && n > 1)
+        return l.intl.formatToPlainString(l.t["Qc+9ww"], {
             price: e,
             intervalCount: n,
         });
     throw Error("Unsupported interval type: ".concat(t, ", and interval count: ").concat(n));
 }
-function d(e, t) {
+function f(e, t) {
     return Intl.NumberFormat(e, {
         style: "percent",
         minimumFractionDigits: 0,
     }).format(t);
 }
-function f(e) {
-    let t = "interval_count" in e ? e.interval_count : e.intervalCount,
-        n = (0, a.aS)(e.id);
-    return u(c(n.amount, n.currency), e.interval, t);
-}
 function _(e) {
+    let t = "interval_count" in e ? e.interval_count : e.intervalCount,
+        n = (0, o.aS)(e.id);
+    return d(u(n.amount, n.currency), e.interval, t);
+}
+function p(e) {
     return e.length > 5 ? e.replace(/\.00$/, "") : e;
 }
-function p(e, t, n) {
-    return _(c(e, t, n));
+function h(e, t, n) {
+    return p(u(e, t, n));
 }
