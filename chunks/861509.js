2ea7081ba96bd58697a137b262b90c1d90af83ca
DiscrapperManager <admin@minerpl.xyz>
2025-11-26 17:06:23 +0100

    canaryRelease: 473099
    Main scripts: 4
    Lazy Loaded Scripts: 1801
    Chunks:
     * Main Files: 15692
     * Scripts: 9391
    Strings: 23248
    Routes: 776
    Experiments Count: 370
    Apex Experiments Count: 124

--- chunks/861509.js
+++ chunks/861509.js
@@ -1,13 +1,14 @@
 n.d(e, { S: () => E });
 var i = n(509613),
-    l = n(313789),
+    r = n(313789),
+    l = n(910092),
     u = n(832518),
-    r = n(643400),
-    s = n(317379),
-    a = n(253109),
-    o = n(189015),
-    c = n(388032);
-let E = (0, i.Uc)(l.n.NOTIFICATION_SELECTION_FIELD_SET, {
-    useTitle: () => c.intl.string(c.t.FEVRDV),
-    buildLayout: () => [r.I, o.Z, u.M, a.F, s.f],
+    s = n(643400),
+    a = n(317379),
+    o = n(253109),
+    c = n(189015),
+    d = n(388032);
+let E = (0, i.Uc)(r.n.NOTIFICATION_SELECTION_FIELD_SET, {
+    useTitle: () => d.intl.string(d.t.FEVRDV),
+    buildLayout: () => [s.I, c.Z, l.r, u.M, o.F, a.f],
 });
---
+++ chunks/910092.js
@@ -0,0 +1,14 @@
+n.d(e, { r: () => a });
+var i = n(522558),
+    r = n(509613),
+    l = n(695346),
+    u = n(313789),
+    s = n(388032);
+let a = (0, r.qs)(u.n.FRIEND_ANNIVERSARY_NOTIFICATIONS, {
+    useTitle: () => s.intl.string(s.t.NjOMvh),
+    useBadge: () => "beta",
+    useValue: l.vc.useSetting,
+    setValue: l.vc.updateSetting,
+    useSearchTerms: () => [s.intl.string(s.t.hi4dSk)],
+    usePredicate: () => (0, i.i)("FriendAnniversaryNotifications"),
+});
