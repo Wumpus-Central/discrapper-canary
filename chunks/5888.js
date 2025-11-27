c5629f96d0e1cf2f2c521cf9353d4da65ce90aa6
DiscrapperManager <admin@minerpl.xyz>
2025-11-27 02:09:59 +0100

    canaryRelease: 473465
    Main scripts: 4
    Lazy Loaded Scripts: 1798
    Chunks:
     * Main Files: 15692
     * Scripts: 9375
    Strings: 23342
    Routes: 776
    Experiments Count: 364
    Apex Experiments Count: 123

--- chunks/5888.js
+++ chunks/5888.js
@@ -1,26 +1,45 @@
 n.d(t, {
-    Z: () => f,
+    Z: () => h,
     p: () => o,
 });
 var r,
     i,
-    l,
-    a = n(442837),
-    s = n(570140),
-    o =
-        (((r = {})[(r.INIT = 0)] = "INIT"),
-        (r[(r.FETCHING = 1)] = "FETCHING"),
-        (r[(r.SUCCESS = 2)] = "SUCCESS"),
-        (r[(r.ERROR = 3)] = "ERROR"),
-        r);
+    l = n(442837),
+    a = n(570140);
+function s(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0,
+              })
+            : (e[t] = n),
+        e
+    );
+}
+var o =
+    (((r = {})[(r.INIT = 0)] = "INIT"),
+    (r[(r.FETCHING = 1)] = "FETCHING"),
+    (r[(r.SUCCESS = 2)] = "SUCCESS"),
+    (r[(r.ERROR = 3)] = "ERROR"),
+    r);
 let c = {},
-    u = 0;
-function d() {
+    u = 0,
+    d = {
+        volume: 1,
+        isMuted: !1,
+    };
+function p() {
     (c = {}), (u = 0);
 }
-class p extends (l = a.ZP.Store) {
-    initialize() {
-        d();
+class f extends (i = l.ZP.PersistedStore) {
+    getState() {
+        return d;
+    }
+    initialize(e) {
+        p(), null != e && (d = e);
     }
     getCheckpointData() {
         return c;
@@ -28,17 +47,16 @@ class p extends (l = a.ZP.Store) {
     getFetchState() {
         return u;
     }
+    get volume() {
+        return d.volume;
+    }
+    get isMuted() {
+        return d.isMuted;
+    }
 }
-(i = "displayName") in p
-    ? Object.defineProperty(p, i, {
-          value: "CheckpointStore",
-          enumerable: !0,
-          configurable: !0,
-          writable: !0,
-      })
-    : (p[i] = "CheckpointStore");
-let f = new p(s.Z, {
-    CONNECTION_OPEN: d,
+s(f, "displayName", "CheckpointStore"), s(f, "persistKey", "CheckpointStore");
+let h = new f(a.Z, {
+    CONNECTION_OPEN: p,
     CHECKPOINT_FETCH_START: function () {
         u = 1;
     },
@@ -47,5 +65,11 @@ let f = new p(s.Z, {
     },
     CHECKPOINT_FETCH_FAILED: function () {
         u = 3;
+    },
+    CHECKPOINT_SET_VOLUME: function (e) {
+        d.volume = e.volume;
+    },
+    CHECKPOINT_TOGGLE_MUTE: function () {
+        d.isMuted = !d.isMuted;
     },
 });
