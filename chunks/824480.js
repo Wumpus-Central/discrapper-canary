87d3bb449067da3b5ea3059d0741047823999d56
DiscrapperManager <admin@minerpl.xyz>
2025-11-27 01:07:01 +0100

    canaryRelease: 473433
    Main scripts: 4
    Lazy Loaded Scripts: 1798
    Chunks:
     * Main Files: 15692
     * Scripts: 9375
    Strings: 23341
    Routes: 776
    Experiments Count: 364
    Apex Experiments Count: 123

--- chunks/824480.js
+++ chunks/824480.js
@@ -1,20 +1,22 @@
-i.d(e, { d: () => d });
-var r = i(473749),
-    n = i(442837),
-    l = i(224706),
-    a = i(515970),
-    s = i(5888);
-function d() {
-    let t = (0, n.e7)([s.Z], () => s.Z.getFetchState()),
-        e = (0, n.e7)([s.Z], () => s.Z.getCheckpointData()),
-        i = (0, r.useMemo)(
+i.d(e, { d: () => o });
+var n = i(473749),
+    r = i(442837),
+    l = i(496929),
+    a = i(224706),
+    s = i(515970),
+    d = i(5888),
+    c = i(981631);
+function o() {
+    let t = (0, r.e7)([d.Z], () => d.Z.getFetchState()),
+        e = (0, r.e7)([d.Z], () => d.Z.getCheckpointData()),
+        i = (0, n.useMemo)(
             () => (null != e.applications ? e.applications.applications.map((t) => t.game.id) : []),
             [e.applications],
         );
-    (0, r.useEffect)(() => {
-        t === s.p.INIT && (0, a.pg)();
+    (0, n.useEffect)(() => {
+        t === d.p.INIT && ((0, s.pg)(), (0, l.p0)({ entitlementType: c.qc2.FREE_PURCHASE }));
     }, [t]),
-        (0, r.useEffect)(() => {
-            l.Z.getDetectableGamesSupplemental(i);
+        (0, n.useEffect)(() => {
+            a.Z.getDetectableGamesSupplemental(i);
         }, [i]);
 }
