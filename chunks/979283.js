3207ca39251925ff97f22b4ae3479efde434e4de
DiscrapperManager <admin@minerpl.xyz>
2025-12-01 20:11:57 +0100

    canaryRelease: 473725
    Main scripts: 4
    Lazy Loaded Scripts: 1798
    Chunks:
     * Main Files: 15691
     * Scripts: 9374
    Strings: 23341
    Routes: 776
    Experiments Count: 364
    Apex Experiments Count: 121

--- chunks/979283.js
+++ chunks/979283.js
@@ -1,54 +1,23 @@
-n.d(t, { O: () => s });
-var r = n(818083),
-    i = n(427164);
-let l = (0, r.B)({
-        kind: "user",
-        id: "2025-07_camera_toggle_sound",
-        label: "Camera Toggle Sound",
-        defaultConfig: {
-            enabled: !1,
-            inExperiment: !1,
+n.d(t, { O: () => i });
+let r = (0, n(818083).B)({
+    kind: "user",
+    id: "2025-07_camera_toggle_sound",
+    label: "Camera Toggle Sound",
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 0,
+            label: "Sound does not play when camera is toggled",
+            config: { enabled: !1 },
         },
-        treatments: [
-            {
-                id: 0,
-                label: "Sound does not play when camera is toggled",
-                config: {
-                    enabled: !1,
-                    inExperiment: !0,
-                },
-            },
-            {
-                id: 1,
-                label: "Sound plays when camera is toggled",
-                config: {
-                    enabled: !0,
-                    inExperiment: !0,
-                },
-            },
-        ],
-    }),
-    a = (0, i.le)({
-        name: "2025-07-camera-toggle-sound",
-        kind: "user",
-        defaultConfig: {
-            enabled: !1,
-            inExperiment: !1,
+        {
+            id: 1,
+            label: "Sound plays when camera is toggled",
+            config: { enabled: !0 },
         },
-        variations: {
-            0: {
-                enabled: !1,
-                inExperiment: !0,
-            },
-            1: {
-                enabled: !0,
-                inExperiment: !0,
-            },
-        },
-    });
-function s() {
-    let { enabled: e, inExperiment: t } = l.getCurrentConfig({ location: "soundpacks" }, { autoTrackExposure: !0 });
-    if (t) return e;
-    let { enabled: n } = a.getConfig({ location: "soundpacks" });
-    return n;
+    ],
+});
+function i() {
+    let { enabled: e } = r.getCurrentConfig({ location: "soundpacks" }, { autoTrackExposure: !0 });
+    return e;
 }
