8c8c5f8311ee9b5f25bbd2c2b61eb9d3d5c1cdf9
DiscrapperManager <admin@minerpl.xyz>
2025-12-02 21:34:40 +0100

    canaryRelease: 474402
    Main scripts: 4
    Lazy Loaded Scripts: 1797
    Chunks:
     * Main Files: 15746
     * Scripts: 9331
    Strings: 23347
    Routes: 776
    Experiments Count: 366
    Apex Experiments Count: 125

--- chunks/102793.js
+++ chunks/102793.js
@@ -1,20 +1,37 @@
-n.d(t, { H: () => c });
+n.d(t, { H: () => u });
 var r = n(442837),
     i = n(722733),
     l = n(633289),
     a = n(751823);
 let o = (0, i.Z)({
-    kind: "user",
-    name: "2025-10-go-live-zoom-pan",
-    defaultConfig: { allowGoLiveZoomPan: !1 },
-    variations: { 1: { allowGoLiveZoomPan: !0 } },
-});
-function c(e) {
+        kind: "user",
+        name: "2025-10-go-live-zoom-pan",
+        defaultConfig: { allowGoLiveZoomPan: !1 },
+        variations: { 1: { allowGoLiveZoomPan: !0 } },
+    }),
+    c = (0, i.Z)({
+        kind: "user",
+        name: "2025-12-go-live-zoom-pan--mirror",
+        defaultConfig: { allowGoLiveZoomPan: !1 },
+        variations: { 1: { allowGoLiveZoomPan: !0 } },
+    }),
+    s = (0, i.Z)({
+        kind: "user",
+        name: "2025-12-go-live-zoom-pan--shadow",
+        defaultConfig: { allowGoLiveZoomPan: !1 },
+        variations: { 1: { allowGoLiveZoomPan: !0 } },
+    });
+function u(e) {
     let { location: t } = e;
     return (0, r.e7)([l.Z], () =>
         (function (e) {
-            let { location: t } = e,
-                { isInHoldout: n } = a.L.getCurrentConfig({ location: t }, { autoTrackExposure: !0 });
+            let { location: t } = e;
+            !(function (e) {
+                let { location: t } = e,
+                    { isInHoldout: n } = a.C.getCurrentConfig({ location: t }, { autoTrackExposure: !0 });
+                s.getConfig({ location: t }), n || c.getConfig({ location: t });
+            })({ location: t });
+            let { isInHoldout: n } = a.L.getCurrentConfig({ location: t }, { autoTrackExposure: !0 });
             return n ? o.definition.defaultConfig : o.getConfig({ location: t });
         })({ location: t }),
     );
--- chunks/883794.js
+++ chunks/883794.js
@@ -1,4 +1,4 @@
-n.d(t, { U: () => s });
+n.d(t, { U: () => c });
 var r = n(427164),
     i = n(751823);
 let a = {
@@ -50,15 +50,108 @@ let a = {
                 vadKrispActivationThreshold: 0.95,
             },
         },
+    }),
+    s = (0, r.le)({
+        kind: "user",
+        name: "2025-12-agc2-v2--mirror",
+        defaultConfig: {
+            noiseCancellationDuringProcessing: !1,
+            noiseCancellationConfig: { useAGC2: !1 },
+        },
+        variations: {
+            1: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: { useAGC2: !1 },
+            },
+            2: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.5,
+            },
+            3: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.7,
+            },
+            4: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.8,
+            },
+            5: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.9,
+            },
+            6: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.95,
+            },
+        },
+    }),
+    l = (0, r.le)({
+        kind: "user",
+        name: "2025-12-agc2-v2--shadow",
+        defaultConfig: {
+            noiseCancellationDuringProcessing: !1,
+            noiseCancellationConfig: { useAGC2: !1 },
+        },
+        variations: {
+            1: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: { useAGC2: !1 },
+            },
+            2: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.5,
+            },
+            3: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.7,
+            },
+            4: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.8,
+            },
+            5: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.9,
+            },
+            6: {
+                noiseCancellationDuringProcessing: !0,
+                noiseCancellationConfig: a,
+                vadKrispActivationThreshold: 0.95,
+            },
+        },
     });
-function s(e) {
+function c(e) {
+    let { location: t, disable: n = !1 } = e;
+    u({
+        location: t,
+        disable: n,
+    });
+    let { isInHoldout: r } = i.L.getCurrentConfig(
+        { location: t },
+        {
+            disable: n,
+            autoTrackExposure: !0,
+        },
+    );
+    return r || n ? o.definition.defaultConfig : o.getConfig({ location: t });
+}
+function u(e) {
     let { location: t, disable: n = !1 } = e,
-        { isInHoldout: r } = i.L.getCurrentConfig(
+        { isInHoldout: r } = i.C.getCurrentConfig(
             { location: t },
             {
                 disable: n,
                 autoTrackExposure: !0,
             },
         );
-    return r || n ? o.definition.defaultConfig : o.getConfig({ location: t });
+    l.getConfig({ location: t }), r || n || s.getConfig({ location: t });
 }
