35a565a0dcd4091856c1c6b393382d1ebcd74cba
DiscrapperManager <admin@minerpl.xyz>
2025-12-02 20:27:43 +0100

    canaryRelease: 474328
    Main scripts: 4
    Lazy Loaded Scripts: 1797
    Chunks:
     * Main Files: 15746
     * Scripts: 9331
    Strings: 23347
    Routes: 776
    Experiments Count: 365
    Apex Experiments Count: 121

--- chunks/812766.js
+++ chunks/812766.js
@@ -1,18 +1,19 @@
 n.d(e, { Z: () => o });
 var i = n(442837),
-    r = n(846027),
-    l = n(509613),
-    s = n(131951),
+    l = n(846027),
+    s = n(509613),
+    r = n(131951),
     u = n(313789),
     a = n(388032);
-let o = (0, l.qs)(u.n.STREAMING_ADVANCED_SCREENSHARE, {
+let o = (0, s.qs)(u.n.STREAMING_ADVANCED_SCREENSHARE, {
     useTitle: () => a.intl.string(a.t.GmWk2E),
+    useSearchTerms: () => [a.intl.string(a.t["Fj/xn1"])],
     useSubtitle: () => a.intl.string(a.t["Fj/xn1"]),
     usePredicate: function () {
-        return (0, i.e7)([s.Z], () => s.Z.supportsVideoHook());
+        return (0, i.e7)([r.Z], () => r.Z.supportsVideoHook());
     },
     useValue: function () {
-        return (0, i.e7)([s.Z], () => s.Z.getVideoHook());
+        return (0, i.e7)([r.Z], () => r.Z.getVideoHook());
     },
-    setValue: r.Z.setVideoHook,
+    setValue: l.Z.setVideoHook,
 });
