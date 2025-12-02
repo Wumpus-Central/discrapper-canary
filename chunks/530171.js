0fd3dcaa37abd7035df605652c07f6fefca89a0c
DiscrapperManager <admin@minerpl.xyz>
2025-12-02 22:49:26 +0100

    canaryRelease: 474503
    Main scripts: 4
    Lazy Loaded Scripts: 1796
    Chunks:
     * Main Files: 15765
     * Scripts: 9318
    Strings: 23349
    Routes: 776
    Experiments Count: 365
    Apex Experiments Count: 126

--- chunks/530171.js
+++ chunks/530171.js
@@ -1,75 +1,76 @@
-n.d(t, { I: () => v }), n(388685);
+n.d(t, { I: () => h }), n(388685);
 var r = n(54381),
-    l = n(473749),
-    i = n(120356),
-    o = n.n(i),
-    a = n(392711),
-    s = n(913793),
+    i = n(473749),
+    a = n(120356),
+    o = n.n(a),
+    s = n(392711),
+    l = n(913793),
     c = n(23893),
     u = n(657707),
     d = n(692547),
     f = n(481060),
     p = n(393903),
-    b = n(206403),
-    y = n(231338),
-    g = n(481446);
-function v(e) {
+    _ = n(206403),
+    m = n(481446);
+function h(e) {
     let {
             title: t,
             collapsedSubtitle: n,
-            isExpanded: i,
-            onExpandedChange: v,
-            onExpandedChangeComplete: m,
-            children: h,
-            animate: x = !0,
+            isExpanded: a,
+            onExpandedChange: h,
+            onExpandedChangeComplete: g,
+            children: E,
+            animate: b = !0,
         } = e,
-        j = l.useRef(null),
-        [O, C] = l.useState(null),
-        P = l.useRef(null),
-        S = l.useMemo(
-            () =>
-                null == m
-                    ? y.dG
-                    : (0, a.debounce)((e) => {
-                          null != e.target && m(i);
-                      }),
-            [i, m],
-        );
-    (0, p.s)(P, S, [], { enabled: null != m });
-    let _ = i && null != O;
+        y = i.useRef(null),
+        [O, v] = i.useState(null),
+        S = i.useRef(a),
+        I = i.useRef(!1);
+    i.useEffect(() => {
+        S.current !== a && ((I.current = !0), (S.current = a));
+    }, [a]);
+    let T = i.useCallback(
+            (e) => {
+                null != e.target && I.current && ((I.current = !1), null == g || g(a));
+            },
+            [a, g],
+        ),
+        A = i.useMemo(() => (0, s.debounce)(T, 50), [T]),
+        C = (0, p.y)(A, [], { enabled: null != g }),
+        N = a && null != O;
     return (
-        l.useLayoutEffect(() => {
-            null != j.current && null == O && C(j.current.clientHeight);
+        i.useLayoutEffect(() => {
+            null != y.current && null == O && v(y.current.clientHeight);
         }, [O]),
-        (0, r.jsxs)(s.pJ, {
-            ref: P,
-            isExpanded: i,
-            onExpandedChange: v,
+        (0, r.jsxs)(l.pJ, {
+            ref: C,
+            isExpanded: a,
+            onExpandedChange: h,
             children: [
-                (0, r.jsx)(b.x, {
-                    ref: j,
+                (0, r.jsx)(_.x, {
+                    ref: y,
                     style: null != O ? { minHeight: O } : void 0,
                     title: t,
-                    description: _ ? void 0 : n,
+                    description: N ? void 0 : n,
                     secondaryTrailingElement: (0, r.jsx)(f.tEY, {
                         children: (0, r.jsx)(c.z, {
                             slot: "trigger",
-                            className: g.triggerButton,
+                            className: m.triggerButton,
                             children: (0, r.jsx)(u.sXD, {
-                                className: o()(g.icon, { [g.iconClosed]: !i }),
+                                className: o()(m.icon, { [m.iconClosed]: !a }),
                                 color: d.Z.colors.ICON_LINK,
                             }),
                         }),
                     }),
-                    onClick: () => v(!i),
+                    onClick: () => h(!a),
                     headingColor: "text-link",
                 }),
-                (0, r.jsx)(s.V2, {
-                    className: o()(g.panel, { [g.disablePanelAnimation]: !x }),
+                (0, r.jsx)(l.V2, {
+                    className: o()(m.panel, { [m.disablePanelAnimation]: !b }),
                     children: (0, r.jsx)(f.Kqy, {
                         gap: 16,
                         padding: { top: 16 },
-                        children: h,
+                        children: E,
                     }),
                 }),
             ],
