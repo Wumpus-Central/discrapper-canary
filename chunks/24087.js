d1ccd6b346a4de9dc9b389229eefbc208e6054a8
DiscrapperManager <admin@minerpl.xyz>
2025-12-03 00:52:16 +0100

    canaryRelease: 474655
    Main scripts: 4
    Lazy Loaded Scripts: 1797
    Chunks:
     * Main Files: 15766
     * Scripts: 9318
    Strings: 23351
    Routes: 776
    Experiments Count: 364
    Apex Experiments Count: 126

--- chunks/24087.js
+++ chunks/24087.js
@@ -1,6 +1,6 @@
 n.d(t, {
-    l: () => c,
-    t: () => f,
+    l: () => u,
+    t: () => p,
 }),
     n(388685),
     n(642613),
@@ -12,36 +12,43 @@ var r = n(473749),
     s = n(763296),
     l = n(771784);
 function c(e) {
-    return [...e].sort((e, t) => a.default.compare(e.soundId, t.soundId));
+    return [...e].sort((e, t) => (e.available && !t.available ? -1 : !e.available && t.available ? 1 : 0));
 }
 function u(e) {
-    return [...e].sort((e, t) => a.default.compare(t.soundId, e.soundId));
+    let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
+        n = [...e].sort((e, t) => a.default.compare(e.soundId, t.soundId));
+    return t ? c(n) : n;
 }
-function d(e, t) {
-    let n = new Map(e.map((e) => [e.soundId, e])),
-        r = [];
-    return (
-        t.forEach((e) => {
-            let t = n.get(e);
-            null != t && (r.push(t), n.delete(e));
-        }),
-        [...r, ...c(Array.from(n.values()))]
-    );
+function d(e) {
+    let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
+        n = [...e].sort((e, t) => a.default.compare(t.soundId, e.soundId));
+    return t ? c(n) : n;
 }
-function f() {
+function f(e, t) {
+    let n = !(arguments.length > 2) || void 0 === arguments[2] || arguments[2],
+        r = new Map(e.map((e) => [e.soundId, e])),
+        i = [];
+    t.forEach((e) => {
+        let t = r.get(e);
+        null != t && (i.push(t), r.delete(e));
+    });
+    let a = [...i, ...u(Array.from(r.values()), n)];
+    return n ? c(a) : a;
+}
+function p() {
     let { sortingStrategy: e } = (0, l.q3)("useSoundOrganizer"),
         t = (0, i.e7)([s.Z], () => s.Z.getAllTopSoundsForGuilds()),
         n = (0, i.e7)([o.Z], () => o.Z.frecentlyPlayedSounds),
         a = r.useRef(n).current,
-        f = r.useCallback(
+        c = r.useCallback(
             (e) => {
                 if (0 === e.length) return e;
                 let n = e[0].guildId;
                 if (!e.every((e) => e.guildId === n)) return e;
                 let r = t.get(n);
                 return null == r
-                    ? c(e)
-                    : d(
+                    ? u(e)
+                    : f(
                           e,
                           r.map((e) => e.soundId),
                       );
@@ -50,21 +57,21 @@ function f() {
         ),
         p = r.useCallback(
             (e) =>
-                d(
+                f(
                     e,
                     a.map((e) => e.soundId),
                 ),
             [a],
         );
-    if (__OVERLAY__) return c;
+    if (__OVERLAY__) return u;
     switch (e) {
         case l.du.TOP_SOUNDS:
-            return f;
+            return c;
         case l.du.RECENTLY_USED_BY_USER:
             return p;
         case l.du.NEWEST_CREATION_DATE:
-            return u;
+            return d;
         default:
-            return c;
+            return u;
     }
 }
