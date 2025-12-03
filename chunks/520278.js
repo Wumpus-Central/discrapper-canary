51d13fa8cf2edb0fd42a9aa4db48ed43b7317609
DiscrapperManager <admin@minerpl.xyz>
2025-12-03 19:13:42 +0100

    canaryRelease: 474944
    Main scripts: 4
    Lazy Loaded Scripts: 1796
    Chunks:
     * Main Files: 15766
     * Scripts: 9310
    Strings: 23355
    Routes: 776
    Experiments Count: 363
    Apex Experiments Count: 127

--- chunks/520278.js
+++
@@ -1,5 +0,0 @@
-n.d(t, { J: () => r });
-function r(e, t, n, r) {
-    let { enabled: o } = e.useExperiment({ location: r }, { autoTrackExposure: !1 });
-    return !!n.config.features.includes(t) && (e.trackExposure({ location: r }), o);
-}
--- chunks/70417.js
+++ chunks/70417.js
@@ -1,65 +1,62 @@
-n.d(t, { Z: () => m }), n(388685);
+n.d(t, { Z: () => u }), n(388685);
 var r = n(473749),
     o = n(713447),
     l = n(754700),
-    i = n(937058),
-    a = n(475595),
-    s = n(215113),
-    c = n(324805),
-    u = n(981631);
-let d = new Set([u.IyS.FIVE_G, u.IyS.FOUR_G, u.IyS.UNKNOWN]);
-function m(e, t, n) {
-    var u;
-    let { quest: m } = r.useContext(s.VideoQuestModalContext),
-        { questConfig: f } = r.useContext(s.VideoQuestConfigContext),
-        p = r.useRef(!1),
-        v = r.useRef(null),
-        E = (0, i.z)(m, c.dr.VIDEO_MODAL),
-        g = null == (u = f.taskConfigV2.tasks[l.X.WATCH_VIDEO]) ? void 0 : u.assets,
-        O = r.useMemo(
+    i = n(475595),
+    a = n(215113),
+    s = n(981631);
+let c = new Set([s.IyS.FIVE_G, s.IyS.FOUR_G, s.IyS.UNKNOWN]);
+function u(e, t, n) {
+    var s;
+    let { quest: u } = r.useContext(a.VideoQuestModalContext),
+        { questConfig: d } = r.useContext(a.VideoQuestConfigContext),
+        m = r.useRef(!1),
+        f = r.useRef(null),
+        p = null == (s = d.taskConfigV2.tasks[l.X.WATCH_VIDEO]) ? void 0 : s.assets,
+        v = r.useMemo(
             () =>
-                null != g
-                    ? E && o.ZP.isSupported() && null != g.videoHls
-                        ? a.eC.VIDEO_PLAYER_VIDEO_HLS
-                        : d.has(t) || null == g.videoLowRes
-                          ? a.eC.VIDEO_PLAYER_VIDEO
-                          : a.eC.VIDEO_PLAYER_VIDEO_LOW_RES
+                null != p
+                    ? o.ZP.isSupported() && null != p.videoHls
+                        ? i.eC.VIDEO_PLAYER_VIDEO_HLS
+                        : c.has(t) || null == p.videoLowRes
+                          ? i.eC.VIDEO_PLAYER_VIDEO
+                          : i.eC.VIDEO_PLAYER_VIDEO_LOW_RES
                     : null,
-            [g, t, E],
+            [p, t],
         ),
-        h = r.useMemo(() => (null != O ? (0, a.fh)(m, O, void 0, !1) : null), [m, O]),
-        b = () => {
-            null != v.current &&
-                800000 !== v.current.config.minAutoBitrate &&
-                (v.current.config.minAutoBitrate = 800000);
+        E = r.useMemo(() => (null != v ? (0, i.fh)(u, v, void 0, !1) : null), [u, v]),
+        g = () => {
+            null != f.current &&
+                800000 !== f.current.config.minAutoBitrate &&
+                (f.current.config.minAutoBitrate = 800000);
         },
-        C = r.useCallback(() => {
-            null != v.current && (v.current.config.minAutoBitrate = 800000);
+        O = r.useCallback(() => {
+            null != f.current && (f.current.config.minAutoBitrate = 800000);
         }, []);
     return (
         r.useEffect(() => {
-            if (O !== a.eC.VIDEO_PLAYER_VIDEO_HLS || null == h || null == e.current || p.current) return;
-            (v.current = new o.ZP({
+            if (v !== i.eC.VIDEO_PLAYER_VIDEO_HLS || null == E || null == e.current || m.current) return;
+            (f.current = new o.ZP({
                 backBufferLength: 20,
                 maxBufferLength: 30,
                 startPosition: n,
                 startFragPrefetch: !0,
                 startLevel: -1,
             })),
-                v.current.on(o.ZP.Events.FRAG_LOADING, b),
-                v.current.loadSource(h.url),
-                v.current.attachMedia(e.current),
-                (p.current = !0);
-            let t = v.current;
+                f.current.on(o.ZP.Events.FRAG_LOADING, g),
+                f.current.loadSource(E.url),
+                f.current.attachMedia(e.current),
+                (m.current = !0);
+            let t = f.current;
             return () => {
-                null != t && t.off(o.ZP.Events.FRAG_LOADING, b);
+                null != t && t.off(o.ZP.Events.FRAG_LOADING, g);
             };
-        }, [h, O, e, n]),
+        }, [E, v, e, n]),
         {
-            videoAssetType: O,
-            videoAsset: h,
-            hlsRef: v,
-            onFirstChunkLoaded: C,
+            videoAssetType: v,
+            videoAsset: E,
+            hlsRef: f,
+            onFirstChunkLoaded: O,
         }
     );
 }
