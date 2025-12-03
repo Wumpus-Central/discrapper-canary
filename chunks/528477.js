c98613abe8c95eaffa10c1e394ad716d54c0919b
DiscrapperManager <admin@minerpl.xyz>
2025-12-03 16:43:08 +0100

    canaryRelease: 474845
    Main scripts: 4
    Lazy Loaded Scripts: 1796
    Chunks:
     * Main Files: 15766
     * Scripts: 9316
    Strings: 23355
    Routes: 776
    Experiments Count: 364
    Apex Experiments Count: 127

--- chunks/528477.js
+++ chunks/528477.js
@@ -1,4 +1,4 @@
-n.d(t, { n: () => E });
+n.d(t, { n: () => T });
 var a = n(54381),
     l = n(473749),
     i = n(159691),
@@ -6,134 +6,148 @@ var a = n(54381),
     s = n(100527),
     o = n(906732),
     c = n(336197),
-    d = n(612659),
-    u = n(313789),
-    m = n(518596),
-    p = n(626135),
-    h = n(323183),
-    x = n(5900),
-    f = n(299886),
-    g = n(37279),
-    b = n(773275),
-    v = n(981631),
-    j = n(474936),
-    _ = n(163646),
-    y = n(388032),
-    C = n(742098),
-    S = n(932889);
-let E = (e) => {
+    d = n(740594),
+    u = n(612659),
+    m = n(313789),
+    p = n(518596),
+    h = n(626135),
+    x = n(323183),
+    f = n(5900),
+    g = n(299886),
+    b = n(37279),
+    v = n(773275),
+    j = n(981631),
+    _ = n(474936),
+    y = n(163646),
+    C = n(388032),
+    S = n(742098),
+    E = n(932889);
+let T = (e) => {
     let { onClose: t } = e,
-        n = (0, d.m)(),
+        n = (0, u.m)(),
         {
-            installationStatus: E,
+            installationStatus: T,
             perkAvailableToUser: N,
-            isSupportedPrivateBrowsingPerkPlatform: T,
-            isWeb: O,
-            connectionStatus: P,
-        } = (0, b.xf)(),
-        { enabled: w } = f.H.useConfig({ location: "private_browsing_perk_settings_page" }),
-        I = (0, l.useCallback)(() => {
-            (0, c.Z)(v.Z5c.NITRO_HOME), t();
-        }, [t]),
+            isSupportedPrivateBrowsingPerkPlatform: O,
+            isWeb: P,
+            connectionStatus: w,
+        } = (0, v.xf)(),
+        { enabled: I } = g.H.useConfig({ location: "private_browsing_perk_settings_page" }),
         k = (0, l.useCallback)(() => {
-            (0, m.openUserSettings)(u.n.PRIVATE_BROWSING_PANEL, { section: v.oAB.CF_WARP });
+            (0, c.Z)(j.Z5c.NITRO_HOME), t();
+        }, [t]),
+        R = (0, l.useCallback)(() => {
+            (0, p.openUserSettings)(m.n.PRIVATE_BROWSING_PANEL, { section: j.oAB.CF_WARP });
         }, []),
-        R = [x.Ij.CONNECTED].includes(P),
-        A = T && w && ![x._n.ZERO_TRUST, x._n.ERROR].includes(E) && !O && (!n || N),
-        Z = (0, l.useMemo)(() => {
-            switch (P) {
-                case x.Ij.CONNECTED:
-                    return y.intl.string(_.default.FBu3XJ);
-                case x.Ij.INITIALIZING:
-                    return y.intl.string(_.default["00vkCU"]);
-                default:
-                    return y.intl.string(_.default["A70/wR"]);
-            }
-        }, [P]),
+        A = [f.Ij.CONNECTED].includes(w),
+        Z = O && I && ![f._n.ZERO_TRUST, f._n.ERROR].includes(T) && !P && (!n || N),
         D = (0, l.useMemo)(() => {
-            if (!N) return y.intl.string(_.default.KN6HOb);
-            switch (E) {
-                case x._n.INSTALLED:
-                case x._n.EXISTING_INSTALLATION:
-                    return Z;
+            switch (w) {
+                case f.Ij.CONNECTED:
+                    return C.intl.string(y.default.FBu3XJ);
+                case f.Ij.INITIALIZING:
+                    return C.intl.string(y.default["00vkCU"]);
                 default:
-                    return y.intl.string(_.default.KN6HOb);
+                    return C.intl.string(y.default["A70/wR"]);
             }
-        }, [E, N, Z]),
+        }, [w]),
         L = (0, l.useMemo)(() => {
+            if (!N) return C.intl.string(y.default.KN6HOb);
+            switch (T) {
+                case f._n.INSTALLED:
+                case f._n.EXISTING_INSTALLATION:
+                    return D;
+                default:
+                    return C.intl.string(y.default.KN6HOb);
+            }
+        }, [T, N, D]),
+        M = (0, l.useMemo)(() => {
             if (!N)
                 return (0, a.jsx)(i.xvT, {
-                    variant: "text-xs/medium",
+                    variant: "text-sm/normal",
                     color: "text-secondary",
-                    children: y.intl.format(_.default["0UfKTY"], { helpdeskArticle: h.w }),
+                    children: C.intl.format(y.default["0UfKTY"], { helpdeskArticle: x.w }),
                 });
-            switch (E) {
-                case x._n.INSTALLED:
-                case x._n.EXISTING_INSTALLATION:
+            switch (T) {
+                case f._n.INSTALLED:
+                case f._n.EXISTING_INSTALLATION:
                     return (0, a.jsx)(i.xvT, {
-                        variant: "text-sm/medium",
+                        variant: "text-sm/normal",
                         color: "text-secondary",
-                        children: R
-                            ? y.intl.format(_.default.zwW10o, {
+                        children: A
+                            ? C.intl.format(y.default.zwW10o, {
                                   onClick: () => {
-                                      (0, r.j)(v.rMx.NITRO_PRIVACY_UPSELL_PERKS_CLICKED, {}), I();
+                                      (0, r.j)(j.rMx.NITRO_PRIVACY_UPSELL_PERKS_CLICKED, {}), k();
                                   },
                               })
-                            : y.intl.format(_.default.D4hu0v, {
+                            : C.intl.format(y.default.D4hu0v, {
                                   onClick: () => {
-                                      (0, r.j)(v.rMx.NITRO_PRIVACY_UPSELL_SETTINGS_CLICKED, {}), k();
+                                      (0, r.j)(j.rMx.NITRO_PRIVACY_UPSELL_SETTINGS_CLICKED, {}), R();
                                   },
                               }),
                     });
                 default:
                     return (0, a.jsx)(i.xvT, {
-                        variant: "text-sm/medium",
+                        variant: "text-sm/normal",
                         color: "text-secondary",
-                        children: y.intl.format(_.default.D4hu0v, {
+                        children: C.intl.format(y.default.D4hu0v, {
                             onClick: () => {
-                                (0, r.j)(v.rMx.NITRO_PRIVACY_UPSELL_SETTINGS_CLICKED, {}), k();
+                                (0, r.j)(j.rMx.NITRO_PRIVACY_UPSELL_SETTINGS_CLICKED, {}), R();
                             },
                         }),
                     });
             }
-        }, [E, N, R, k, I]),
-        { analyticsLocations: M } = (0, o.ZP)(s.Z.MASKED_LINK);
+        }, [T, N, A, R, k]),
+        { analyticsLocations: U } = (0, o.ZP)(s.Z.MASKED_LINK);
     return ((0, l.useEffect)(() => {
-        A &&
-            p.default.track(v.rMx.PREMIUM_UPSELL_VIEWED, {
-                type: j.cd.PRIVACY_PERK_EXIT_MODAL,
-                location_stack: M,
+        Z &&
+            h.default.track(j.rMx.PREMIUM_UPSELL_VIEWED, {
+                type: _.cd.PRIVACY_PERK_EXIT_MODAL,
+                location_stack: U,
             });
-    }, [M, A]),
-    A)
+    }, [U, Z]),
+    Z)
         ? (0, a.jsxs)("div", {
-              className: C.container,
+              className: S.container,
               children: [
-                  (0, a.jsx)(g.v, { className: C.betaBadge }),
-                  (0, a.jsx)(i.$1m, {
-                      color: "nitro-pink",
-                      offsetBottom: -0.5,
-                      className: C.gradientBackground,
-                      children: (0, a.jsxs)("div", {
-                          className: C.contentContainer,
-                          children: [
-                              (0, a.jsx)("img", {
-                                  src: S,
-                                  className: C.graphicContainer,
-                                  alt: "Private Browsing Perk",
-                              }),
-                              (0, a.jsxs)("div", {
-                                  className: C.details,
-                                  children: [
-                                      (0, a.jsx)(i.X6q, {
-                                          variant: "heading-md/semibold",
-                                          children: D,
+                  (0, a.jsx)(b.v, { className: S.betaBadge }),
+                  (0, a.jsxs)("div", {
+                      className: S.contentContainer,
+                      children: [
+                          N &&
+                              (0, a.jsx)(i.$1m, {
+                                  className: S.imageWrapper,
+                                  offsetBottom: -5,
+                                  color: "nitro-pink",
+                                  children: (0, a.jsx)("div", {
+                                      className: S.imageContainer,
+                                      children: (0, a.jsx)("img", {
+                                          src: E,
+                                          className: S.graphicContainer,
+                                          alt: "Private Browsing Perk",
                                       }),
-                                      L,
-                                  ],
+                                  }),
                               }),
-                          ],
-                      }),
+                          (0, a.jsxs)("div", {
+                              className: S.details,
+                              children: [
+                                  (0, a.jsx)(i.X6q, {
+                                      variant: "heading-md/semibold",
+                                      children: L,
+                                  }),
+                                  M,
+                              ],
+                          }),
+                          !N &&
+                              (0, a.jsx)("div", {
+                                  className: S.ctaContainer,
+                                  children: (0, a.jsx)(d.Z, {
+                                      buttonTextOverride: C.intl.string(C.t["8x0jKT"]),
+                                      subscriptionTier: _.Si.TIER_2,
+                                      size: "sm",
+                                  }),
+                              }),
+                      ],
                   }),
               ],
           })
