8a95311a8ef352fbb942c0cbeadc3a788de672a5
DiscrapperManager <admin@minerpl.xyz>
2025-12-04 20:34:21 +0100

    canaryRelease: 475688
    Main scripts: 4
    Lazy Loaded Scripts: 1792
    Chunks:
     * Main Files: 15886
     * Scripts: 9287
    Strings: 23363
    Routes: 779
    Experiments Count: 360
    Apex Experiments Count: 127

--- chunks/304696.js
+++ chunks/304696.js
@@ -1,102 +1,92 @@
 n.d(t, {
-    AV: () => c,
-    Fs: () => u,
-    M1: () => E,
-    NL: () => f,
-    Nt: () => p,
-    T2: () => _,
-    V_: () => d,
-    f2: () => g,
-    zo: () => h,
+    AV: () => s,
+    Fs: () => l,
+    M1: () => h,
+    NL: () => u,
+    Nt: () => d,
+    T2: () => f,
+    V_: () => c,
+    f2: () => m,
+    zo: () => _,
 }),
     n(415506);
 var r = n(572004),
     i = n(617136),
-    a = n(254579),
-    o = n(283689),
-    s = n(324805),
-    l = n(388032);
-function c(e) {
+    a = n(324805),
+    o = n(388032);
+function s(e) {
     var t, n;
     let { quest: r, taskDetails: i, thirdPartyTaskDetails: a } = e;
-    if ((null == (t = r.userStatus) ? void 0 : t.completedAt) != null) return l.intl.string(l.t.BzFeTF);
+    if ((null == (t = r.userStatus) ? void 0 : t.completedAt) != null) return o.intl.string(o.t.BzFeTF);
     if ((null == (n = r.userStatus) ? void 0 : n.enrolledAt) != null) {
         let { percentComplete: e } = i,
             t = null != a ? a.percentComplete : e;
         return t >= 0.75
-            ? l.intl.string(l.t.gvCR4H)
+            ? o.intl.string(o.t.gvCR4H)
             : t >= 0.45 && t <= 0.55
-              ? l.intl.string(l.t.JNx8sG)
+              ? o.intl.string(o.t.JNx8sG)
               : t > 0
-                ? l.intl.string(l.t.JMbfnc)
-                : l.intl.string(l.t["7e5k7L"]);
+                ? o.intl.string(o.t.JMbfnc)
+                : o.intl.string(o.t["7e5k7L"]);
     }
-    return l.intl.formatToPlainString(l.t.EQa7os, { questName: r.config.messages.questName });
+    return o.intl.formatToPlainString(o.t.EQa7os, { questName: r.config.messages.questName });
 }
-function u(e) {
+function l(e) {
     let { isTargetedDisclosure: t, gamePublisher: n, gameTitle: r, cosponsorName: i } = e;
     return t
         ? null == i
-            ? l.intl.formatToPlainString(l.t.Piihy1, { gamePublisher: n })
-            : l.intl.formatToPlainString(l.t.DV47Gy, {
+            ? o.intl.formatToPlainString(o.t.Piihy1, { gamePublisher: n })
+            : o.intl.formatToPlainString(o.t.DV47Gy, {
                   gamePublisher: n,
                   cosponsorName: i,
               })
-        : l.intl.formatToPlainString(l.t.euizJY, {
+        : o.intl.formatToPlainString(o.t.euizJY, {
               gamePublisher: n,
               gameTitle: r,
           });
 }
+function c(e) {
+    return e.config.ctaConfig.buttonLabel;
+}
+function u(e) {
+    switch (e) {
+        case a.yq.SUGGESTED:
+            return o.intl.string(o.t.gBfXPZ);
+        case a.yq.MOST_RECENT:
+            return o.intl.string(o.t.K6oEu2);
+        case a.yq.EXPIRING_SOON:
+            return o.intl.string(o.t.IB22n3);
+        case a.yq.RECENTLY_ENROLLED:
+            return o.intl.string(o.t["BB+2tX"]);
+    }
+}
 function d(e) {
-    return null != e.config.ctaConfig.buttonLabel
-        ? e.config.ctaConfig.buttonLabel
-        : (0, a.q8)(e) ||
-            (0, o.Pb)(e) ||
-            (0, a.pO)(e) ||
-            (0, a.Vl)(e) ||
-            e.config.features.includes(s.S7.NON_GAMING_PLAY_QUEST)
-          ? l.intl.string(l.t.hvVgAZ)
-          : l.intl.string(l.t.lwQdjB);
+    switch (e) {
+        case a.UP.VIRTUAL_CURRENCY:
+            return o.intl.string(o.t.ElYQFS);
+        case a.UP.COLLECTIBLE:
+            return o.intl.string(o.t.Jg17Ut);
+        case a.UP.IN_GAME:
+            return o.intl.string(o.t["O/J2kr"]);
+        case a.oH.VIDEO:
+            return o.intl.string(o.t.e0iISA);
+        case a.oH.PLAY:
+            return o.intl.string(o.t["1nJR4p"]);
+    }
 }
 function f(e) {
     switch (e) {
-        case s.yq.SUGGESTED:
-            return l.intl.string(l.t.gBfXPZ);
-        case s.yq.MOST_RECENT:
-            return l.intl.string(l.t.K6oEu2);
-        case s.yq.EXPIRING_SOON:
-            return l.intl.string(l.t.IB22n3);
-        case s.yq.RECENTLY_ENROLLED:
-            return l.intl.string(l.t["BB+2tX"]);
-    }
-}
-function p(e) {
-    switch (e) {
-        case s.UP.VIRTUAL_CURRENCY:
-            return l.intl.string(l.t.ElYQFS);
-        case s.UP.COLLECTIBLE:
-            return l.intl.string(l.t.Jg17Ut);
-        case s.UP.IN_GAME:
-            return l.intl.string(l.t["O/J2kr"]);
-        case s.oH.VIDEO:
-            return l.intl.string(l.t.e0iISA);
-        case s.oH.PLAY:
-            return l.intl.string(l.t["1nJR4p"]);
-    }
-}
-function _(e) {
-    switch (e) {
         case "reward":
-            return l.intl.string(l.t.vjLqAU);
+            return o.intl.string(o.t.vjLqAU);
         case "task":
-            return l.intl.string(l.t.Hufmss);
+            return o.intl.string(o.t.Hufmss);
     }
 }
-let m = (e) => "".concat(location.protocol, "//").concat(location.host, "/quests/").concat(e);
-function h(e) {
+let p = (e) => "".concat(location.protocol, "//").concat(location.host, "/quests/").concat(e);
+function _(e) {
     return e.ctaConfig.link;
 }
-let g = (e, t) => {
+let m = (e, t) => {
     (0, i._3)({
         questId: e,
         questContent: t.content,
@@ -105,9 +95,9 @@ let g = (e, t) => {
         impressionId: t.impressionId,
         sourceQuestContent: t.sourceQuestContent,
     }),
-        (0, r.JG)(m(e));
+        (0, r.JG)(p(e));
 };
-function E(e) {
+function h(e) {
     if (0 === e.rewardsConfig.rewards.length) throw Error("Quest ".concat(e.id, " has no rewards configured"));
     return e.rewardsConfig.rewards[0];
 }
