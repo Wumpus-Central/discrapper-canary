1d1516900d6fb7401a39264580ca3dffd3e17614
DiscrapperManager <admin@minerpl.xyz>
2025-12-04 21:24:11 +0100

    canaryRelease: 475739
    Main scripts: 4
    Lazy Loaded Scripts: 1792
    Chunks:
     * Main Files: 15887
     * Scripts: 9287
    Strings: 23363
    Routes: 779
    Experiments Count: 360
    Apex Experiments Count: 128

---
+++ chunks/318370.js
@@ -0,0 +1,14 @@
+n.d(t, {
+    n: () => i,
+    y: () => a,
+});
+let r = (0, n(427164).le)({
+        name: "2025-12-stage-pawtect",
+        kind: "user",
+        defaultConfig: { enabled: !1 },
+        variations: { 1: { enabled: !0 } },
+    }),
+    i = (e) => r.useConfig({ location: e }).enabled;
+function a(e) {
+    return r.getConfig({ location: e }).enabled;
+}
