2bc6f6fa14244ee3683a7af71b50e3ee9b40b320
DiscrapperManager <admin@minerpl.xyz>
2025-12-08 18:39:41 +0100

    canaryRelease: 476876
    Main scripts: 4
    Lazy Loaded Scripts: 1797
    Chunks:
     * Main Files: 15920
     * Scripts: 9312
    Strings: 23388
    Routes: 783
    Experiments Count: 363
    Apex Experiments Count: 130

--- chunks/696287.js
+++ chunks/696287.js
@@ -1,15 +1,16 @@
-n.d(t, { Z: () => b }), n(388685);
-var r = n(846519),
-    i = n(147913),
-    a = n(509003),
-    o = n(77498),
-    s = n(19780),
-    l = n(626135),
-    c = n(70956),
-    u = n(581567),
-    d = n(594190),
-    f = n(981631);
-function p(e, t, n) {
+n.d(t, { Z: () => y }), n(388685);
+var r = n(772848),
+    i = n(846519),
+    a = n(147913),
+    o = n(509003),
+    s = n(77498),
+    l = n(19780),
+    c = n(626135),
+    u = n(70956),
+    d = n(581567),
+    f = n(594190),
+    p = n(981631);
+function _(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -22,25 +23,9 @@ function p(e, t, n) {
         e
     );
 }
-function _(e) {
-    for (var t = 1; t < arguments.length; t++) {
-        var n = null != arguments[t] ? arguments[t] : {},
-            r = Object.keys(n);
-        "function" == typeof Object.getOwnPropertySymbols &&
-            (r = r.concat(
-                Object.getOwnPropertySymbols(n).filter(function (e) {
-                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
-                }),
-            )),
-            r.forEach(function (t) {
-                p(e, t, n[t]);
-            });
-    }
-    return e;
-}
-let m = 5 * c.Z.Millis.MINUTE;
+let m = 5 * u.Z.Millis.MINUTE;
 function h(e) {
-    return e.distributor === f.GQo.ROBLOX ? (0, a.x3)(e) : null;
+    return e.distributor === p.GQo.ROBLOX ? (0, o.x3)(e) : null;
 }
 function g(e) {
     let t = null != e.name ? e.name : "",
@@ -48,67 +33,80 @@ function g(e) {
         r = h(e);
     return null != r && (n += ":".concat(r)), n;
 }
-class E extends i.Z {
+function E(e) {
+    return "".concat(g(e), ":").concat(e.pid);
+}
+class b extends a.Z {
     _terminate() {
-        this.stopHeartbeat();
+        this.heartbeatInterval.stop();
     }
-    maybeStartHeartbeat() {
-        this.heartbeatInterval.isStarted() ||
-            (this.logRunningGameHeartbeats(), this.heartbeatInterval.start(m, this.logRunningGameHeartbeats));
+    handleLogout() {
+        this.gameSessions.clear(), this.heartbeatInterval.stop();
     }
-    stopHeartbeat() {
-        this.heartbeatInterval.stop(), this.runningGameKeys.clear();
+    scheduleHeartbeatTracking() {
+        if ((this.processSessionChanges(), 0 === f.ZP.getRunningGames().length))
+            return void this.heartbeatInterval.stop();
+        this.heartbeatInterval.isStarted() || this.heartbeatInterval.start(m, this.logRunningGameHeartbeats);
     }
-    handlePostConnectionOpen() {
-        d.ZP.getRunningGames().length > 0 && this.maybeStartHeartbeat();
+    logHeartbeat(e, t, n, r) {
+        var i, a;
+        let u = performance.now(),
+            _ = n ? 0 : Math.round(u - t.lastHeartbeatTime),
+            m = null != (a = e.id) ? a : null == (i = s.Z.getGameByName(e.name)) ? void 0 : i.id;
+        c.default.track(p.rMx.RUNNING_GAME_HEARTBEAT, {
+            game_id: m,
+            game_name: e.name,
+            game_distributor: e.distributor,
+            game_distributor_game_id: e.sku,
+            game_metadata: (0, o.sD)(e),
+            game_executable: (0, d.N6)(e.exePath),
+            game_detection_enabled: (0, f.ik)(e),
+            initial_heartbeat: n,
+            final_heartbeat: r,
+            game_session_id: t.sessionId,
+            duration_tracked_ms: _,
+            rtc_connection_id: l.Z.getRTCConnectionId(),
+            media_session_id: l.Z.getMediaSessionId(),
+        }),
+            (t.lastHeartbeatTime = u);
+    }
+    processSessionChanges() {
+        let e = f.ZP.getRunningGames(),
+            t = performance.now(),
+            n = new Set();
+        for (let i of e) {
+            let e = E(i);
+            if ((n.add(e), !this.gameSessions.has(e))) {
+                let n = {
+                    sessionId: (0, r.Z)(),
+                    lastHeartbeatTime: t,
+                    runningGame: i,
+                };
+                this.gameSessions.set(e, n), this.logHeartbeat(i, n, !0, !1);
+            }
+        }
+        for (let [e, t] of this.gameSessions)
+            n.has(e) || (this.logHeartbeat(t.runningGame, t, !1, !0), this.gameSessions.delete(e));
     }
     constructor(...e) {
         super(...e),
-            p(this, "heartbeatInterval", new r.Xp()),
-            p(this, "runningGameKeys", new Set()),
-            p(this, "actions", {
+            _(this, "heartbeatInterval", new i.Xp()),
+            _(this, "gameSessions", new Map()),
+            _(this, "actions", {
                 RUNNING_GAMES_CHANGE: (e) => this.handleRunningGamesChanged(e),
-                LOGOUT: () => this.stopHeartbeat(),
-                CONNECTION_CLOSED: () => this.stopHeartbeat(),
-                POST_CONNECTION_OPEN: () => this.handlePostConnectionOpen(),
+                POST_CONNECTION_OPEN: () => this.scheduleHeartbeatTracking(),
+                LOGOUT: () => this.handleLogout(),
             }),
-            p(this, "handleRunningGamesChanged", (e) => {
-                let { games: t } = e;
-                if (0 === t.length) return void this.stopHeartbeat();
-                this.maybeStartHeartbeat();
+            _(this, "handleRunningGamesChanged", (e) => {
+                this.scheduleHeartbeatTracking();
             }),
-            p(this, "logRunningGameHeartbeats", () => {
-                let e = d.ZP.getRunningGames(),
-                    t = {
-                        rtc_connection_id: s.Z.getRTCConnectionId(),
-                        media_session_id: s.Z.getMediaSessionId(),
-                    },
-                    n = new Set();
-                e.forEach((e) => {
-                    var r, i;
-                    let s = g(e),
-                        c = !this.runningGameKeys.has(s),
-                        p = null != (i = e.id) ? i : null == (r = o.Z.getGameByName(e.name)) ? void 0 : r.id;
-                    l.default.track(
-                        f.rMx.RUNNING_GAME_HEARTBEAT,
-                        _(
-                            {
-                                game_id: p,
-                                game_name: e.name,
-                                game_distributor: e.distributor,
-                                game_distributor_game_id: e.sku,
-                                game_metadata: (0, a.sD)(e),
-                                game_executable: (0, u.N6)(e.exePath),
-                                game_detection_enabled: (0, d.ik)(e),
-                                initial_heartbeat: c,
-                            },
-                            t,
-                        ),
-                    ),
-                        n.add(g(e));
-                }),
-                    (this.runningGameKeys = n);
+            _(this, "logRunningGameHeartbeats", () => {
+                for (let e of f.ZP.getRunningGames()) {
+                    let t = E(e),
+                        n = this.gameSessions.get(t);
+                    null != n && this.logHeartbeat(e, n, !1, !1);
+                }
             });
     }
 }
-let b = new E();
+let y = new b();
