a8d6e3319804a3f29fa4d4ca743461694d5789cb
DiscrapperManager <admin@minerpl.xyz>
2025-12-09 22:33:01 +0100

    canaryRelease: 477460
    Main scripts: 4
    Lazy Loaded Scripts: 1797
    Chunks:
     * Main Files: 15933
     * Scripts: 9308
    Strings: 23396
    Routes: 783
    Experiments Count: 361
    Apex Experiments Count: 133

--- chunks/401460.js
+++ chunks/401460.js
@@ -1,11 +1,11 @@
-n.d(t, { Z: () => T }), n(388685);
+n.d(t, { Z: () => O }), n(388685);
 var r = n(54381),
     i = n(473749),
     a = n(120356),
     o = n.n(a),
     s = n(392711),
     l = n.n(s),
-    c = n(755721),
+    c = n(28664),
     u = n(481060),
     d = n(583901),
     f = n(997945),
@@ -14,109 +14,52 @@ var r = n(54381),
     m = n(981631),
     h = n(388032),
     g = n(63224);
-function E(e, t, n) {
-    return (
-        t in e
-            ? Object.defineProperty(e, t, {
-                  value: n,
-                  enumerable: !0,
-                  configurable: !0,
-                  writable: !0,
-              })
-            : (e[t] = n),
-        e
-    );
-}
-function b(e) {
-    for (var t = 1; t < arguments.length; t++) {
-        var n = null != arguments[t] ? arguments[t] : {},
-            r = Object.keys(n);
-        "function" == typeof Object.getOwnPropertySymbols &&
-            (r = r.concat(
-                Object.getOwnPropertySymbols(n).filter(function (e) {
-                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
-                }),
-            )),
-            r.forEach(function (t) {
-                E(e, t, n[t]);
-            });
-    }
-    return e;
-}
-function y(e, t) {
-    var n = Object.keys(e);
-    if (Object.getOwnPropertySymbols) {
-        var r = Object.getOwnPropertySymbols(e);
-        t &&
-            (r = r.filter(function (t) {
-                return Object.getOwnPropertyDescriptor(e, t).enumerable;
-            })),
-            n.push.apply(n, r);
-    }
-    return n;
-}
-function O(e, t) {
-    return (
-        (t = null != t ? t : {}),
-        Object.getOwnPropertyDescriptors
-            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : y(Object(t)).forEach(function (n) {
-                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
-              }),
-        e
-    );
-}
-let v = () => [
+let E = () => [
         h.intl.string(h.t["EgWTY+"]),
         h.intl.string(h.t.umBn5f),
         h.intl.string(h.t.dG1wD1),
         h.intl.string(h.t.SesI4S),
         h.intl.string(h.t.RnMLvl),
     ],
-    S = () => {
+    b = () => {
         var e;
-        return null != (e = l().sample(v())) ? e : h.intl.string(h.t["EgWTY+"]);
+        return null != (e = l().sample(E())) ? e : h.intl.string(h.t["EgWTY+"]);
     };
-function I(e) {
+function y(e) {
     let { id: t, className: n, onSelect: a, isSelected: s = !1, tabIndex: l, children: p, locked: h = !1 } = e,
         [E, y] = i.useState((0, _.aW)(t).name),
-        v = t === f.Ru.PIRATE,
-        I = (0, c.jm)({
-            label: E,
-            isSelected: s,
-        });
-    return (0, r.jsx)(u.aML, {
+        O = t === f.Ru.PIRATE;
+    return (0, r.jsx)(c.u, {
         text: E,
-        onTooltipShow: () => v && y(S()),
-        children: (e) =>
-            (0, r.jsxs)("div", {
-                className: g.appIconSelectionContainer,
-                children: [
-                    (0, r.jsx)(
-                        u.P3F,
-                        O(b({}, e, I), {
-                            tabIndex: null != l ? l : I.tabIndex,
-                            className: o()(g.appIconSelection, { [g.selected]: s }, n),
-                            onClick: s ? m.dG4 : () => (null == a ? void 0 : a(t)),
-                            children: p,
+        onTooltipShow: () => O && y(b()),
+        children: (0, r.jsxs)("div", {
+            className: g.appIconSelectionContainer,
+            children: [
+                (0, r.jsx)(u.P3F, {
+                    role: "radio",
+                    "aria-label": E,
+                    "aria-checked": s,
+                    tabIndex: null != l ? l : s ? 0 : -1,
+                    className: o()(g.appIconSelection, { [g.selected]: s }, n),
+                    onClick: s ? m.dG4 : () => (null == a ? void 0 : a(t)),
+                    children: p,
+                }),
+                s && (0, r.jsx)(d.oK, {}),
+                h &&
+                    (0, r.jsx)("div", {
+                        className: g.lockedBadgeContainer,
+                        children: (0, r.jsx)(u.mBM, {
+                            className: g.lockedBadge,
+                            color: "currentColor",
                         }),
-                    ),
-                    s && (0, r.jsx)(d.oK, {}),
-                    h &&
-                        (0, r.jsx)("div", {
-                            className: g.lockedBadgeContainer,
-                            children: (0, r.jsx)(u.mBM, {
-                                className: g.lockedBadge,
-                                color: "currentColor",
-                            }),
-                        }),
-                ],
-            }),
+                    }),
+            ],
+        }),
     });
 }
-function T(e) {
+function O(e) {
     let { icon: t, isSelected: n, disabled: i, tabIndex: a, onSelect: o, locked: s } = e;
-    return (0, r.jsx)(I, {
+    return (0, r.jsx)(y, {
         onSelect: i ? void 0 : o,
         isSelected: n,
         id: t.id,
