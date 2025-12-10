665b956b511f2605bdfc51bed0d8c56edb975f37
DiscrapperManager <admin@minerpl.xyz>
2025-12-10 23:31:48 +0100

    canaryRelease: 478482
    Main scripts: 4
    Lazy Loaded Scripts: 1803
    Chunks:
     * Main Files: 15927
     * Scripts: 9302
    Strings: 23410
    Routes: 784
    Experiments Count: 356
    Apex Experiments Count: 139

--- chunks/623624.js
+++ chunks/623624.js
@@ -1,4 +1,4 @@
-n.d(t, { f: () => m });
+n.d(t, { f: () => g });
 var r = n(54381);
 n(473749);
 var i = n(512722),
@@ -7,38 +7,41 @@ var i = n(512722),
     s = n(100527),
     l = n(906732),
     c = n(84615),
-    u = n(430824),
-    d = n(594174),
-    f = n(443002);
-let p = "BoostedGuildPerksModalConnected",
-    _ = (e) => {
+    u = n(70394),
+    d = n(430824),
+    f = n(594174),
+    p = n(443002),
+    _ = n(564394);
+let m = "BoostedGuildPerksModalConnected",
+    h = (e) => {
         let { guildId: t, close: n, location: i } = e,
-            c = (0, o.e7)([d.default], () => d.default.getCurrentUser());
+            c = (0, o.e7)([f.default], () => f.default.getCurrentUser());
         a()(null != c, "PremiumGuildPerksModalConnected: currentUser cannot be undefined");
-        let p = (0, o.e7)([u.Z], () => u.Z.getGuild(t), [t]),
-            { analyticsLocations: _ } = (0, l.ZP)(s.Z.BOOSTED_GUILD_PERKS_MODAL);
-        return null == p
-            ? null
-            : (0, r.jsx)(l.Gt, {
-                  value: _,
-                  children: (0, r.jsx)(f.Z, {
-                      analyticsLocation: i,
-                      onClose: n,
-                      guild: p,
-                  }),
-              });
+        let m = (0, o.e7)([d.Z], () => d.Z.getGuild(t), [t]),
+            { analyticsLocations: h } = (0, l.ZP)(s.Z.BOOSTED_GUILD_PERKS_MODAL),
+            g = (0, u.$)("BoostedGuildPerksModal");
+        if (null == m) return null;
+        let E = g ? _.Z : p.Z;
+        return (0, r.jsx)(l.Gt, {
+            value: h,
+            children: (0, r.jsx)(E, {
+                analyticsLocation: i,
+                onClose: n,
+                guild: m,
+            }),
+        });
     };
-function m(e) {
+function g(e) {
     let { guildId: t, location: n } = e;
     (0, c.q4)(
         (e) => {
             let { closeLayer: i } = e;
-            return (0, r.jsx)(_, {
+            return (0, r.jsx)(h, {
                 close: i,
                 guildId: t,
                 location: n,
             });
         },
-        { layerKey: p },
+        { layerKey: m },
     );
 }
---
+++ chunks/70394.js
@@ -0,0 +1,13 @@
+n.d(t, { $: () => i });
+let r = (0, n(722733).Z)({
+    name: "2025-12-boosting-marketing-page-refresh",
+    kind: "user",
+    defaultConfig: { enabled: !1 },
+    variations: {
+        0: { enabled: !1 },
+        1: { enabled: !0 },
+    },
+});
+function i(e) {
+    return r.useConfig({ location: e }).enabled;
+}
