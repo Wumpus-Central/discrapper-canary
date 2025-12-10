b294ee98c2a5a215bae99a0e2f56a1ad499863dc
DiscrapperManager <admin@minerpl.xyz>
2025-12-10 01:52:17 +0100

    canaryRelease: 477891
    Main scripts: 4
    Lazy Loaded Scripts: 1803
    Chunks:
     * Main Files: 15932
     * Scripts: 9303
    Strings: 23408
    Routes: 783
    Experiments Count: 362
    Apex Experiments Count: 137

--- chunks/111386.js
+++ chunks/111386.js
@@ -1,55 +1,46 @@
-n.d(t, { Z: () => p }), n(388685);
+n.d(t, { Z: () => d }), n(388685);
 var r = n(54381),
     i = n(473749),
     a = n(704215),
-    o = n(28664),
-    s = n(481060),
-    l = n(243778),
+    o = n(264418),
+    s = n(243778),
+    l = n(921944),
     c = n(388032),
-    u = n(170337),
-    d = n(442937);
-let f = () =>
-        (0, r.jsxs)("div", {
-            className: u.coachtipInner,
-            children: [
-                (0, r.jsx)("img", {
-                    src: d,
-                    alt: c.intl.string(c.t["9wkT13"]),
-                    className: u.coachtipAsset,
-                }),
-                (0, r.jsxs)("div", {
-                    className: u.coachtipTextContainer,
-                    children: [
-                        (0, r.jsx)(s.Heading, {
-                            variant: "heading-lg/semibold",
-                            children: c.intl.string(c.t.V5y3qZ),
-                        }),
-                        (0, r.jsx)(s.Text, {
-                            variant: "text-md/normal",
-                            color: "text-subtle",
-                            children: c.intl.string(c.t.eSDHDk),
-                        }),
-                    ],
-                }),
-            ],
-        }),
-    p = (e) => {
-        let { children: t } = e,
-            [n] = (0, l.US)([a.z.CONTENT_INVENTORY_ONE_CLICK_REPLY_COACHTIP]),
-            [s, c] = i.useState(!1);
-        return (i.useEffect(() => {
-            setTimeout(() => {
-                c(!0);
-            }, 300);
-        }),
-        n !== a.z.CONTENT_INVENTORY_ONE_CLICK_REPLY_COACHTIP)
-            ? t
-            : (0, r.jsx)(o.u, {
-                  __unsupportedReactNodeAsText: (0, r.jsx)(f, {}),
-                  position: "left",
-                  forceOpen: s,
-                  shouldShow: s,
-                  "aria-label": "test",
-                  children: (0, r.jsx)("div", { children: t }),
-              });
-    };
+    u = n(798690);
+let d = (e) => {
+    let { children: t } = e,
+        [n, d] = (0, s.US)([a.z.CONTENT_INVENTORY_ONE_CLICK_REPLY_COACHTIP]),
+        [f, p] = i.useState(!1),
+        _ = i.useRef(null);
+    i.useEffect(() => {
+        let e = setTimeout(() => {
+            p(!0);
+        }, 300);
+        return () => clearTimeout(e);
+    }, []);
+    let m = i.useCallback(() => {
+        d(l.L.USER_DISMISS);
+    }, [d]);
+    return n !== a.z.CONTENT_INVENTORY_ONE_CLICK_REPLY_COACHTIP
+        ? t
+        : (0, r.jsxs)(r.Fragment, {
+              children: [
+                  (0, r.jsx)("div", {
+                      ref: _,
+                      children: t,
+                  }),
+                  (0, r.jsx)(o.J, {
+                      targetElementRef: _,
+                      shouldShow: f,
+                      onRequestClose: m,
+                      position: "left",
+                      title: c.intl.string(c.t.V5y3qZ),
+                      body: c.intl.string(c.t.eSDHDk),
+                      graphic: {
+                          type: "image",
+                          src: u.Z,
+                      },
+                  }),
+              ],
+          });
+};
