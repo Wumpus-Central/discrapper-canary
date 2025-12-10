aa7fc97fcfe34686ebbb45e53987f55c73772af3
DiscrapperManager <admin@minerpl.xyz>
2025-12-10 10:58:56 +0100

    canaryRelease: 477901
    Main scripts: 4
    Lazy Loaded Scripts: 1803
    Chunks:
     * Main Files: 15932
     * Scripts: 9303
    Strings: 23408
    Routes: 783
    Experiments Count: 362
    Apex Experiments Count: 137

--- chunks/80753.js
+++ chunks/80753.js
@@ -1,70 +1,62 @@
-i.r(t), i.d(t, { default: () => p }), i(388685);
-var l = i(54381),
-    s = i(473749),
-    n = i(692547),
-    o = i(755721),
-    r = i(481060),
-    c = i(31336),
-    a = i(481637),
-    d = i(532810),
-    u = i(915312);
-function p() {
-    let [e, t] = s.useState(!1),
-        i = (0, u.LL)(),
-        { reporting: p } = a.Z.useExperiment({ location: "be7a1c_1" }, { autoTrackExposure: !1 });
-    return (s.useEffect(() => {
-        if (p && e && null != i)
+i.r(t), i.d(t, { default: () => u }), i(388685);
+var n = i(54381),
+    r = i(473749),
+    s = i(692547),
+    c = i(481060),
+    l = i(31336),
+    o = i(481637),
+    a = i(532810),
+    d = i(915312);
+function u() {
+    let [e, t] = r.useState(!1),
+        i = (0, d.LL)(),
+        { reporting: u } = o.Z.useExperiment({ location: "be7a1c_1" }, { autoTrackExposure: !1 });
+    return (r.useEffect(() => {
+        if (u && e && null != i)
             return (
                 i.forEach((e) => {
                     e.elements.forEach((e) => {
-                        e.setAttribute(d.OF, "");
+                        e.setAttribute(a.OF, "");
                     });
                 }),
                 () => {
                     i.forEach((e) => {
                         e.elements.forEach((e) => {
-                            e.removeAttribute(d.OF);
+                            e.removeAttribute(a.OF);
                         });
                     });
                 }
             );
-    }, [i, p, e]),
-    p && null != i && 0 !== i.length)
-        ? (0, l.jsxs)("div", {
+    }, [i, u, e]),
+    u && null != i && 0 !== i.length)
+        ? (0, n.jsxs)("div", {
               style: {
-                  backgroundColor: n.Z.colors.BACKGROUND_FEEDBACK_CRITICAL.css,
-                  color: n.Z.colors.TEXT_FEEDBACK_CRITICAL.css,
+                  backgroundColor: s.Z.colors.BACKGROUND_FEEDBACK_CRITICAL.css,
+                  color: s.Z.colors.TEXT_FEEDBACK_CRITICAL.css,
                   display: "flex",
                   alignItems: "center",
+                  justifyContent: "space-between",
                   padding: "8px 12px",
               },
               children: [
-                  (0, l.jsx)(r.Text, {
+                  (0, n.jsx)(c.Text, {
                       variant: "text-sm/semibold",
                       color: "text-feedback-critical",
                       children: "Accessibility compliance issue(s) detected, check in-app DevTools",
                   }),
-                  (0, l.jsxs)("div", {
-                      style: {
-                          display: "grid",
-                          gridAutoFlow: "column",
-                          gap: 4,
-                          marginLeft: "auto",
-                      },
+                  (0, n.jsxs)(c.ButtonGroup, {
+                      direction: "horizontal",
+                      size: "sm",
                       children: [
-                          (0, l.jsx)(o.zx, {
-                              size: o.zx.Sizes.TINY,
-                              color: o.zx.Colors.RED,
-                              style: { marginLeft: "auto" },
-                              onClick: c.SO,
-                              children: "Open DevTools",
+                          (0, n.jsx)(c.Button, {
+                              text: "Open DevTools",
+                              onClick: l.SO,
+                              variant: "critical-secondary",
                           }),
-                          (0, l.jsxs)(o.zx, {
-                              size: o.zx.Sizes.TINY,
-                              color: o.zx.Colors.RED,
-                              style: { marginLeft: "auto" },
+                          (0, n.jsx)(c.Button, {
                               onClick: () => t(!e),
-                              children: [e ? "Hide" : "Show", " Overlays"],
+                              text: "".concat(e ? "Hide" : "Show", " Overlays"),
+                              variant: "critical-primary",
                           }),
                       ],
                   }),
