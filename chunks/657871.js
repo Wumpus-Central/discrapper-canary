065f4b25ef9c6e36625419a2b0764fde2c42fdfb
DiscrapperManager <admin@minerpl.xyz>
2025-12-10 22:03:23 +0100

    canaryRelease: 478364
    Main scripts: 4
    Lazy Loaded Scripts: 1804
    Chunks:
     * Main Files: 15930
     * Scripts: 9303
    Strings: 23410
    Routes: 784
    Experiments Count: 357
    Apex Experiments Count: 138

--- chunks/657871.js
+++ chunks/657871.js
@@ -1,61 +1,8 @@
 n.d(t, {
-    kB: () => c,
-    zM: () => u,
+    kB: () => i,
+    zM: () => a,
 });
-var r = n(43169);
-function i(e, t, n) {
-    return (
-        t in e
-            ? Object.defineProperty(e, t, {
-                  value: n,
-                  enumerable: !0,
-                  configurable: !0,
-                  writable: !0,
-              })
-            : (e[t] = n),
-        e
-    );
-}
-function a(e) {
-    for (var t = 1; t < arguments.length; t++) {
-        var n = null != arguments[t] ? arguments[t] : {},
-            r = Object.keys(n);
-        "function" == typeof Object.getOwnPropertySymbols &&
-            (r = r.concat(
-                Object.getOwnPropertySymbols(n).filter(function (e) {
-                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
-                }),
-            )),
-            r.forEach(function (t) {
-                i(e, t, n[t]);
-            });
-    }
-    return e;
-}
-function o(e, t) {
-    var n = Object.keys(e);
-    if (Object.getOwnPropertySymbols) {
-        var r = Object.getOwnPropertySymbols(e);
-        t &&
-            (r = r.filter(function (t) {
-                return Object.getOwnPropertyDescriptor(e, t).enumerable;
-            })),
-            n.push.apply(n, r);
-    }
-    return n;
-}
-function s(e, t) {
-    return (
-        (t = null != t ? t : {}),
-        Object.getOwnPropertyDescriptors
-            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : o(Object(t)).forEach(function (n) {
-                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
-              }),
-        e
-    );
-}
-let l = (0, n(818083).B)({
+let r = (0, n(818083).B)({
     kind: "user",
     id: "2025-09_mention_suggestion",
     label: "Mention Suggestions",
@@ -142,13 +89,9 @@ let l = (0, n(818083).B)({
         },
     ],
 });
-function c(e, t) {
-    var n;
-    let i = (0, r.F4)(e);
-    return l.getCurrentConfig({ location: e }, s(a({}, t), { disable: i || (null != (n = t.disable) && n) }));
+function i(e, t) {
+    return r.getCurrentConfig({ location: e }, t);
 }
-function u(e, t) {
-    var n;
-    let i = (0, r.jX)(e);
-    return l.useExperiment({ location: e }, s(a({}, t), { disable: i || (null != (n = t.disable) && n) }));
+function a(e, t) {
+    return r.useExperiment({ location: e }, t);
 }
