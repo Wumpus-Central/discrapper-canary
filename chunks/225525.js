7d1f444c74cfa59651ee16cf7451366bebe8d6e2
DiscrapperManager <admin@minerpl.xyz>
2025-12-11 01:29:03 +0100

    canaryRelease: 478564
    Main scripts: 4
    Lazy Loaded Scripts: 1802
    Chunks:
     * Main Files: 15949
     * Scripts: 9284
    Strings: 23410
    Routes: 784
    Experiments Count: 356
    Apex Experiments Count: 140

--- chunks/225525.js
+++ chunks/225525.js
@@ -1,45 +1,55 @@
-r.d(e, { default: () => f }), r(388685);
+r.d(e, { default: () => u }), r(388685);
 var n = r(54381),
-    o = r(473749),
-    a = r(755721),
+    a = r(473749),
+    o = r(159691),
     i = r(481060),
     c = r(841183),
-    l = r(388032),
-    s = r(323964);
-function f(t) {
+    s = r(388032),
+    l = r(323964);
+function u(t) {
     var e,
         r,
-        { avatarId: f, storageHash: u, avatarDescription: b, onConfirmDelete: p, onClose: y } = t,
+        { avatarId: u, storageHash: f, avatarDescription: b, onConfirmDelete: p, onClose: y } = t,
         d = (function (t, e) {
             if (null == t) return {};
             var r,
                 n,
-                o = (function (t, e) {
+                a = (function (t, e) {
                     if (null == t) return {};
                     var r,
                         n,
-                        o = {},
-                        a = Object.keys(t);
-                    for (n = 0; n < a.length; n++) (r = a[n]), e.indexOf(r) >= 0 || (o[r] = t[r]);
-                    return o;
+                        a = {},
+                        o = Object.keys(t);
+                    for (n = 0; n < o.length; n++) (r = o[n]), e.indexOf(r) >= 0 || (a[r] = t[r]);
+                    return a;
                 })(t, e);
             if (Object.getOwnPropertySymbols) {
-                var a = Object.getOwnPropertySymbols(t);
-                for (n = 0; n < a.length; n++)
-                    (r = a[n]),
-                        !(e.indexOf(r) >= 0) && Object.prototype.propertyIsEnumerable.call(t, r) && (o[r] = t[r]);
+                var o = Object.getOwnPropertySymbols(t);
+                for (n = 0; n < o.length; n++)
+                    (r = o[n]),
+                        !(e.indexOf(r) >= 0) && Object.prototype.propertyIsEnumerable.call(t, r) && (a[r] = t[r]);
             }
-            return o;
+            return a;
         })(t, ["avatarId", "storageHash", "avatarDescription", "onConfirmDelete", "onClose"]);
-    let [O, m] = o.useState(!1),
-        { avatarSrc: v, eventHandlers: j } = (0, c.Z)({
-            avatarId: f,
-            storageHash: u,
+    let [O, v] = a.useState(!1),
+        { avatarSrc: j, eventHandlers: g } = (0, c.Z)({
+            avatarId: u,
+            storageHash: f,
             size: i.EFr.SIZE_120,
         }),
-        { onMouseEnter: g, onMouseLeave: x } = j;
-    return (0, n.jsxs)(
-        i.VoidConfirmModal,
+        { onMouseEnter: m, onMouseLeave: h } = g,
+        x = a.useCallback(async () => {
+            v(!0);
+            try {
+                await p();
+            } catch (t) {
+            } finally {
+                v(!1);
+            }
+            y();
+        }, [p, y]);
+    return (0, n.jsx)(
+        o.u_l,
         ((e = (function (t) {
             for (var e = 1; e < arguments.length; e++) {
                 var r = null != arguments[e] ? arguments[e] : {},
@@ -66,55 +76,54 @@ function f(t) {
             return t;
         })(
             {
-                bodyClassName: s.modalContent,
-                header: l.intl.string(l.t.rfaLTH),
-                confirmText: l.intl.string(l.t.Lh0uyy),
-                cancelText: l.intl.string(l.t["ETE/oC"]),
-                confirmButtonColor: a.zx.Colors.RED,
-                loading: O,
-                onConfirm: async () => {
-                    m(!0);
-                    try {
-                        await p();
-                    } catch (t) {
-                    } finally {
-                        m(!1);
-                    }
-                    y();
-                },
+                size: "sm",
+                title: s.intl.string(s.t.rfaLTH),
+                subtitle: s.intl.string(s.t.EKmxKc),
+                actions: [
+                    {
+                        text: s.intl.string(s.t["ETE/oC"]),
+                        variant: "secondary",
+                        onClick: y,
+                        disabled: O,
+                    },
+                    {
+                        text: s.intl.string(s.t.Lh0uyy),
+                        variant: "critical-primary",
+                        onClick: x,
+                        loading: O,
+                    },
+                ],
                 onClose: y,
             },
             d,
         )),
         (r = r =
             {
-                children: [
-                    (0, n.jsx)(i.Text, {
-                        variant: "text-sm/normal",
-                        color: "text-subtle",
-                        children: l.intl.string(l.t.EKmxKc),
-                    }),
-                    (0, n.jsx)("div", {
-                        onMouseEnter: g,
-                        onMouseLeave: x,
-                        className: s.avatarContainer,
-                        children: (0, n.jsx)("img", {
-                            src: v,
-                            alt: b,
-                            className: s.avatar,
+                children: (0, n.jsxs)("div", {
+                    className: l.modalContent,
+                    children: [
+                        (0, n.jsx)("div", {
+                            onMouseEnter: m,
+                            onMouseLeave: h,
+                            className: l.avatarContainer,
+                            children: (0, n.jsx)("img", {
+                                src: j,
+                                alt: b,
+                                className: l.avatar,
+                            }),
                         }),
-                    }),
-                    (0, n.jsx)(i.Text, {
-                        variant: "eyebrow",
-                        color: "text-feedback-positive",
-                        children: l.intl.string(l.t["2Ld8DM"]),
-                    }),
-                    (0, n.jsx)(i.Text, {
-                        variant: "text-xs/normal",
-                        color: "text-subtle",
-                        children: l.intl.string(l.t.Oe0xqt),
-                    }),
-                ],
+                        (0, n.jsx)(i.Text, {
+                            variant: "eyebrow",
+                            color: "text-feedback-positive",
+                            children: s.intl.string(s.t["2Ld8DM"]),
+                        }),
+                        (0, n.jsx)(i.Text, {
+                            variant: "text-xs/normal",
+                            color: "text-subtle",
+                            children: s.intl.string(s.t.Oe0xqt),
+                        }),
+                    ],
+                }),
             }),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(r))
