7bc96a193747e2181555bb0feba37f866a012baf
DiscrapperManager <admin@minerpl.xyz>
2025-12-16 00:20:49 +0100

    canaryRelease: 480294
    Main scripts: 3
    Lazy Loaded Scripts: 1803
    Chunks:
     * Main Files: 15807
     * Scripts: 9369
    Strings: 23456
    Routes: 783
    Experiments Count: 348
    Apex Experiments Count: 143

--- chunks/963278.js
+++ chunks/963278.js
@@ -1,27 +1,27 @@
-n.d(t, { Z: () => _ });
+n.d(t, { Z: () => h });
 var r = n(54381),
     l = n(473749),
-    i = n(120356),
-    o = n.n(i),
-    s = n(442837),
-    a = n(481060),
+    a = n(120356),
+    o = n.n(a),
+    i = n(442837),
+    s = n(481060),
     c = n(594174),
     u = n(381585),
     d = n(597688),
-    g = n(937510),
-    f = n(653126),
+    f = n(937510),
+    g = n(653126),
     p = n(786040),
-    m = n(215023),
-    h = n(388032),
-    C = n(310582);
-let _ = (e) => {
-    let { shelf: t, handleTransition: n, tab: i } = e,
-        _ = (0, s.e7)([c.default], () => c.default.getCurrentUser()),
-        b = (0, s.e7)([d.Z], () => d.Z.getCategory(t.categorySkuId)),
-        v = (0, u.sp)(),
-        E = l.useMemo(() => t.rankedSkuIds.map((e) => d.Z.getProduct(e)).filter((e) => null != e), [t.rankedSkuIds]),
-        S = (0, g.l)(E).slice(0, 4);
-    return null == _
+    b = n(215023),
+    m = n(388032),
+    C = n(222194);
+let h = (e) => {
+    let { shelf: t, handleTransition: n, tab: a } = e,
+        h = (0, i.e7)([c.default], () => c.default.getCurrentUser()),
+        v = (0, i.e7)([d.Z], () => d.Z.getCategory(t.categorySkuId)),
+        E = (0, u.sp)(),
+        S = l.useMemo(() => t.rankedSkuIds.map((e) => d.Z.getProduct(e)).filter((e) => null != e), [t.rankedSkuIds]),
+        _ = (0, f.l)(S).slice(0, 4);
+    return null == h
         ? null
         : (0, r.jsxs)("div", {
               className: o()(C.shelfBlockContainer, C.centeredSection),
@@ -29,19 +29,19 @@ let _ = (e) => {
                   (0, r.jsxs)("div", {
                       className: C.blockHeader,
                       children: [
-                          (0, r.jsx)(a.Heading, {
+                          (0, r.jsx)(s.Heading, {
                               variant: "heading-lg/semibold",
                               children: t.name,
                           }),
-                          (0, r.jsx)(a.Button, {
+                          (0, r.jsx)(s.Button, {
                               variant: "secondary",
-                              text: h.intl.formatToPlainString(h.t.bc9RBE, { category_name: t.name }),
+                              text: m.intl.formatToPlainString(m.t.bc9RBE, { category_name: t.name }),
                               onClick: () => {
                                   n({
                                       sourceButton: "shelf block see all",
                                       categorySkuId: t.categorySkuId,
                                       isInternalShopDeeplink: !0,
-                                      isOrbsExclusive: null == b ? void 0 : b.isOrbsExclusive,
+                                      isOrbsExclusive: (null == v ? void 0 : v.isOrbsExclusive) && a !== b.AW.ORBS,
                                   });
                               },
                           }),
@@ -50,7 +50,7 @@ let _ = (e) => {
                   (0, r.jsx)("div", {
                       className: C.feed,
                       children: (0, r.jsx)(r.Fragment, {
-                          children: S.map((e, n) => {
+                          children: _.map((e, n) => {
                               if (null == d.Z.getCategoryForProduct(e.skuId) || null == e) return null;
                               let l = e.skuId;
                               return (0, r.jsx)(
@@ -62,11 +62,11 @@ let _ = (e) => {
                                           categoryPosition: 2,
                                       },
                                       children: (0, r.jsx)(
-                                          f.Z,
+                                          g.Z,
                                           {
                                               skuId: l,
-                                              prioritizedCurrency: i === m.AW.ORBS ? p.tA.ORBS : void 0,
-                                              onClickAnalytics: (0, p.wO)(e, i, v),
+                                              prioritizedCurrency: a === b.AW.ORBS ? p.tA.ORBS : void 0,
+                                              onClickAnalytics: (0, p.wO)(e, a, E),
                                           },
                                           l,
                                       ),
