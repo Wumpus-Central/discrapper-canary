dff0f31fa21563dd24e24dbb78359967604f70e3
DiscrapperManager <admin@minerpl.xyz>
2025-12-15 23:16:29 +0100

    canaryRelease: 480230
    Main scripts: 3
    Lazy Loaded Scripts: 1803
    Chunks:
     * Main Files: 15809
     * Scripts: 9369
    Strings: 23452
    Routes: 783
    Experiments Count: 350
    Apex Experiments Count: 144

--- chunks/287880.js
+++ chunks/287880.js
@@ -1,9 +1,10 @@
 n.d(t, {
-    Ae: () => u,
-    Qe: () => m,
-    U0: () => h,
-    bV: () => _,
-    uZ: () => c,
+    Ae: () => m,
+    Qe: () => y,
+    SN: () => v,
+    U0: () => O,
+    bV: () => b,
+    uZ: () => _,
 }),
     n(410992),
     n(227481),
@@ -23,16 +24,69 @@ n.d(t, {
 var r,
     i,
     a,
-    o = n(943418);
-let s = null != (a = null == (r = window) ? void 0 : r.crypto) ? a : null == (i = window) ? void 0 : i.msCrypto,
-    l = "Uint8Array" in window,
-    c = null != s && "getRandomValues" in s && l,
-    u = "PublicKeyCredential" in window && l,
-    d = 20;
-function f(e) {
-    return s.getRandomValues(new Uint8Array(e));
+    o = n(943418),
+    s = n(960048);
+function l(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0,
+              })
+            : (e[t] = n),
+        e
+    );
 }
-function p(e) {
+function c(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        "function" == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                }),
+            )),
+            r.forEach(function (t) {
+                l(e, t, n[t]);
+            });
+    }
+    return e;
+}
+function u(e, t) {
+    var n = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+        var r = Object.getOwnPropertySymbols(e);
+        t &&
+            (r = r.filter(function (t) {
+                return Object.getOwnPropertyDescriptor(e, t).enumerable;
+            })),
+            n.push.apply(n, r);
+    }
+    return n;
+}
+function d(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : u(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+let f = null != (a = null == (r = window) ? void 0 : r.crypto) ? a : null == (i = window) ? void 0 : i.msCrypto,
+    p = "Uint8Array" in window,
+    _ = null != f && "getRandomValues" in f && p,
+    m = "PublicKeyCredential" in window && p,
+    h = 20;
+function g(e) {
+    return f.getRandomValues(new Uint8Array(e));
+}
+function E(e) {
     return o
         .encode(e)
         .toString("utf8")
@@ -41,17 +95,20 @@ function p(e) {
         .replace(/(\w{4})/g, "$1 ")
         .trim();
 }
-function _() {
-    return p(f(d));
+function b() {
+    return E(g(h));
 }
-function m(e) {
+function y(e) {
     return e.replace(/[\s._-]+/g, "").toUpperCase();
 }
-function h(e, t) {
+function O(e, t) {
     let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : "Discord";
     return "otpauth://totp/"
         .concat(encodeURI(n), ":")
         .concat(encodeURI(e), "?secret=")
-        .concat(m(t), "&issuer=")
+        .concat(y(t), "&issuer=")
         .concat(encodeURIComponent(n));
+}
+function v(e, t) {
+    s.Z.captureException(e, d(c({}, t), { tags: d(c({}, null == t ? void 0 : t.tags), { app_context: "webauthn" }) }));
 }
--- chunks/973810.js
+++ chunks/973810.js
@@ -4,8 +4,8 @@ var r = n(54381),
     i = n(849055),
     s = n(544891),
     a = n(481060),
-    o = n(358085),
-    c = n(960048),
+    o = n(287880),
+    c = n(358085),
     u = n(998502),
     d = n(507453),
     f = n(981631),
@@ -13,12 +13,12 @@ var r = n(54381),
 function g(e) {
     let { mfaChallenge: t, finish: n, setSlide: g, onClose: m, headerAlignStart: b } = e,
         [p, j] = l.useState(!1),
-        [y, x] = l.useState(null),
+        [x, y] = l.useState(null),
         { challenge: S } = t.methods.find((e) => "webauthn" === e.type),
         v = async () => {
-            j(!0), x(null);
+            j(!0), y(null);
             let e =
-                    o.isPlatformEmbedded && u.ZP.supportsFeature(f.eRX.WEBAUTHN)
+                    c.isPlatformEmbedded && u.ZP.supportsFeature(f.eRX.WEBAUTHN)
                         ? u.ZP.webAuthnAuthenticate(S)
                         : i.U2(JSON.parse(S)).then((e) => JSON.stringify(e)),
                 t = async (e) => {
@@ -28,14 +28,14 @@ function g(e) {
                             data: e,
                         });
                     } catch (e) {
-                        e instanceof s.Pd ? x(h.intl.string(h.t.xSCvBf)) : x(e.message);
+                        e instanceof s.Pd ? y(h.intl.string(h.t.xSCvBf)) : ((0, o.SN)(e, {}), y(e.message));
                     }
                 };
             try {
                 let n = await e;
                 await t(n);
             } catch (e) {
-                c.Z.captureException(e), x(h.intl.string(h.t.xSCvBf));
+                (0, o.SN)(e, { extra: { error_type: "webauthn_api_error" } }), y(h.intl.string(h.t.xSCvBf));
             } finally {
                 j(!1);
             }
@@ -46,7 +46,7 @@ function g(e) {
                 onClose: m,
                 headerAlignStart: b,
             }),
-            null != y && (0, r.jsx)(d.Z.SlideContent, { children: (0, r.jsx)(d.Z.SlideError, { error: y }) }),
+            null != x && (0, r.jsx)(d.Z.SlideContent, { children: (0, r.jsx)(d.Z.SlideError, { error: x }) }),
             (0, r.jsx)(d.Z.SlideFooter, {
                 mfaChallenge: t,
                 setSlide: g,
