429a4dc8a9509b447b56e942f5355b93c5ae5a60
DiscrapperManager <admin@minerpl.xyz>
2025-12-16 01:01:25 +0100

    canaryRelease: 480316
    Main scripts: 3
    Lazy Loaded Scripts: 1805
    Chunks:
     * Main Files: 15807
     * Scripts: 9373
    Strings: 23461
    Routes: 783
    Experiments Count: 348
    Apex Experiments Count: 143

--- chunks/367889.js
+++ chunks/367889.js
@@ -1,33 +1,39 @@
-n.d(e, { F: () => E });
+n.d(e, { F: () => N });
 var i = n(54381),
     l = n(657707),
     s = n(793030),
-    r = n(692547),
-    u = n(160913),
+    u = n(692547),
+    r = n(160913),
     a = n(813732),
     o = n(509613),
     c = n(970013),
-    d = n(313789),
-    g = n(726985),
-    S = n(388032);
-let T = (0, o.wf)(d.n.SUBSCRIPTIONS_PANEL, {
-        useTitle: () => S.intl.string(S.t.trSpHX),
+    T = n(313789),
+    S = n(526665),
+    d = n(684610),
+    E = n(726985),
+    g = n(388032);
+let I = (0, o.wf)(T.n.SUBSCRIPTIONS_PANEL, {
+        useTitle: () => g.intl.string(g.t.trSpHX),
         StronglyDiscouragedCustomComponent: a.Z,
         buildLayout: () => [],
     }),
-    E = (0, o.m7)(d.n.SUBSCRIPTIONS_SIDEBAR_ITEM, {
-        useTitle: () => S.intl.string(S.t.trSpHX),
-        getLegacySearchKey: () => g.s6.SUBSCRIPTIONS,
+    _ = (0, o.wf)(T.n.SUBSCRIPTIONS_PANEL, {
+        useTitle: () => g.intl.string(g.t.trSpHX),
+        buildLayout: () => [d.E],
+    }),
+    N = (0, o.m7)(T.n.SUBSCRIPTIONS_SIDEBAR_ITEM, {
+        useTitle: () => g.intl.string(g.t.trSpHX),
+        getLegacySearchKey: () => ((0, S.Gl)("SubscriptionsPanel") ? void 0 : E.s6.SUBSCRIPTIONS),
         icon: l.XOf,
         trailing: {
             type: c.W.STRONGLY_DISCOURAGED_CUSTOM,
             useCustomDecoration: () =>
-                (0, u.U)()
+                (0, r.U)()
                     ? (0, i.jsx)(s.Mgn, {
                           size: "xs",
-                          color: r.Z.unsafe_rawColors.YELLOW_300,
+                          color: u.Z.unsafe_rawColors.YELLOW_300,
                       })
                     : null,
         },
-        buildLayout: () => [T],
+        buildLayout: () => ((0, S.Gl)("SubscriptionsPanel") ? [_] : [I]),
     });
---
+++ chunks/684610.js
@@ -0,0 +1,9 @@
+n.d(e, { E: () => r });
+var i = n(509613),
+    l = n(313789),
+    s = n(702529),
+    u = n(388032);
+let r = (0, i.k4)(l.n.SUBSCRIPTIONS_CATEGORY, {
+    useSearchTerms: () => [u.intl.string(u.t.trSpHX), u.intl.string(u.t["2GKrvn"])],
+    buildLayout: () => [s.t],
+});
