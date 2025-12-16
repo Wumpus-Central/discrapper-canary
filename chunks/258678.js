418df3f5722130f20ea98ea9819dff3866ba85af
DiscrapperManager <admin@minerpl.xyz>
2025-12-16 20:31:51 +0100

    canaryRelease: 480570
    Main scripts: 3
    Lazy Loaded Scripts: 1807
    Chunks:
     * Main Files: 15825
     * Scripts: 9391
    Strings: 23467
    Routes: 783
    Experiments Count: 346
    Apex Experiments Count: 143

---
+++ chunks/258678.js
@@ -0,0 +1,4 @@
+e.exports = {
+    bannerButton: "fb7f9476ada049c8-bannerButton",
+    disabled: "fb7f9476ada049c8-disabled",
+};
--- chunks/321301.js
+++
@@ -1,4 +0,0 @@
-e.exports = {
-    bannerButton: "de800837163648e8-bannerButton",
-    disabled: "de800837163648e8-disabled",
-};
