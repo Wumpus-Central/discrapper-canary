801df90baa8f4e3e5e997971938c596c071603be
DiscrapperManager <admin@minerpl.xyz>
2025-12-16 17:59:56 +0100

    canaryRelease: 480464
    Main scripts: 3
    Lazy Loaded Scripts: 1805
    Chunks:
     * Main Files: 15824
     * Scripts: 9379
    Strings: 23464
    Routes: 783
    Experiments Count: 347
    Apex Experiments Count: 143

--- chunks/183620.js
+++ chunks/183620.js
@@ -1,13 +1,12 @@
-n.d(t, { Z: () => f }), n(388685);
+n.d(t, { Z: () => d }), n(388685);
 var r = n(54381),
     i = n(473749),
-    a = n(120356),
-    o = n.n(a),
-    s = n(481060),
-    l = n(816814),
-    c = n(388032),
-    u = n(44463);
-function d(e, t, n) {
+    a = n(775086),
+    o = n(481060),
+    s = n(816814),
+    l = n(388032),
+    c = n(44463);
+function u(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -20,95 +19,64 @@ function d(e, t, n) {
         e
     );
 }
-class f extends i.PureComponent {
+class d extends i.PureComponent {
     render() {
         let { transitionState: e } = this.props,
             { code: t, errorMessage: n, retrySuccess: i } = this.state,
-            a = i
-                ? (0, r.jsx)(s.Zbd, {
-                      type: s.Zbd.Types.SUCCESS,
-                      className: u.card,
-                      children: (0, r.jsx)(s.Text, {
+            s = i
+                ? (0, r.jsx)(o.Zbd, {
+                      type: o.Zbd.Types.SUCCESS,
+                      className: c.card,
+                      children: (0, r.jsx)(o.Text, {
                           variant: "text-md/normal",
-                          children: c.intl.string(c.t.j4qu8n),
+                          children: l.intl.string(l.t.j4qu8n),
                       }),
                   })
                 : null;
-        return (0, r.jsx)(s.Y0X, {
+        return (0, r.jsx)(a.Modal, {
+            title: l.intl.string(l.t["mGppp/"]),
+            actions: [
+                {
+                    text: l.intl.string(l.t["ETE/oC"]),
+                    onClick: this.props.onClose,
+                    variant: "secondary",
+                    disabled: this.state.isLoading,
+                },
+                {
+                    text: l.intl.string(l.t.piW6YS),
+                    type: "submit",
+                    disabled: this.state.isLoading || 0 === t.length,
+                    onClick: this.handleSubmit,
+                },
+            ],
+            onClose: this.props.onClose,
             transitionState: e,
-            parentComponent: "MFAConfirm",
-            children: (0, r.jsxs)("form", {
-                onSubmit: this.handleSubmit,
+            children: (0, r.jsxs)(o.Kqy, {
+                direction: "vertical",
+                gap: 8,
                 children: [
-                    (0, r.jsx)(s.xBx, {
-                        separator: !1,
-                        children: (0, r.jsx)(s.Heading, {
-                            variant: "heading-lg/semibold",
-                            children: c.intl.string(c.t["mGppp/"]),
-                        }),
+                    (0, r.jsx)(o.Text, {
+                        color: "text-default",
+                        variant: "text-md/normal",
+                        className: c.spacing,
+                        children: l.intl.string(l.t["37S9yU"]),
                     }),
-                    (0, r.jsxs)(s.hzk, {
-                        children: [
-                            (0, r.jsx)(s.Text, {
-                                color: "text-default",
-                                variant: "text-md/normal",
-                                className: u.spacing,
-                                children: c.intl.string(c.t["37S9yU"]),
-                            }),
-                            a,
-                            (0, r.jsxs)("div", {
-                                className: u.spacing,
-                                children: [
-                                    (0, r.jsx)(s.oil, {
-                                        inputRef: this.setRef,
-                                        label: c.intl.string(c.t.TjGb4Q),
-                                        onChange: this.handleCodeChange,
-                                        maxLength: 8,
-                                        value: t,
-                                        autoComplete: "one-time-code",
-                                        autoFocus: !0,
-                                    }),
-                                    null != this.state.errorMessage && "" !== this.state.errorMessage
-                                        ? (0, r.jsx)(s.Text, {
-                                              color: "text-feedback-critical",
-                                              variant: "text-xs/normal",
-                                              className: u.error,
-                                              children: n,
-                                          })
-                                        : null,
-                                    (0, r.jsxs)(s.Text, {
-                                        className: o()(u.__invalid_submitText, u.spacing),
-                                        variant: "text-sm/normal",
-                                        children: [
-                                            (0, r.jsx)("br", {}),
-                                            (0, r.jsx)(s.P3F, {
-                                                className: o()(u.spacing, u.__invalid_link),
-                                                onClick: this.handleRetry,
-                                                children: (0, r.jsx)(s.Anchor, { children: c.intl.string(c.t.PZgmxv) }),
-                                            }),
-                                        ],
-                                    }),
-                                ],
-                            }),
-                        ],
+                    s,
+                    (0, r.jsx)(o.oil, {
+                        inputRef: this.setRef,
+                        label: l.intl.string(l.t.TjGb4Q),
+                        onChange: this.handleCodeChange,
+                        maxLength: 8,
+                        value: t,
+                        autoComplete: "one-time-code",
+                        autoFocus: !0,
+                        error: n,
                     }),
-                    (0, r.jsx)(s.mzw, {
-                        children: (0, r.jsxs)(s.ButtonGroup, {
-                            direction: "horizontal-reverse",
-                            children: [
-                                (0, r.jsx)(s.Button, {
-                                    variant: "primary",
-                                    text: c.intl.string(c.t.piW6YS),
-                                    type: "submit",
-                                    disabled: this.state.isLoading || 0 === t.length,
-                                }),
-                                (0, r.jsx)(s.Button, {
-                                    variant: "secondary",
-                                    text: c.intl.string(c.t["ETE/oC"]),
-                                    onClick: this.props.onClose,
-                                    disabled: this.state.isLoading,
-                                }),
-                            ],
+                    (0, r.jsx)(o.Text, {
+                        variant: "text-sm/normal",
+                        children: (0, r.jsx)(o.P3F, {
+                            onClick: this.handleRetry,
+                            children: (0, r.jsx)(o.Anchor, { children: l.intl.string(l.t.PZgmxv) }),
                         }),
                     }),
                 ],
@@ -117,24 +85,24 @@ class f extends i.PureComponent {
     }
     constructor(...e) {
         super(...e),
-            d(this, "_input", void 0),
-            d(this, "state", {
+            u(this, "_input", void 0),
+            u(this, "state", {
                 code: "",
                 errorMessage: "",
                 retrySuccess: !1,
                 isLoading: !1,
             }),
-            d(this, "setRef", (e) => {
+            u(this, "setRef", (e) => {
                 this._input = e;
             }),
-            d(this, "handleRetry", async () => {
-                await l.Z.sendMFABackupCodesVerificationKeyEmail(this.props.password),
+            u(this, "handleRetry", async () => {
+                await s.Z.sendMFABackupCodesVerificationKeyEmail(this.props.password),
                     this.setState({ retrySuccess: !0 });
             }),
-            d(this, "handleSubmit", async (e) => {
+            u(this, "handleSubmit", async (e) => {
                 e.preventDefault(), this.setState({ isLoading: !0 });
                 try {
-                    await l.Z.confirmViewBackupCodes(this.state.code, !1), this.props.onClose();
+                    await s.Z.confirmViewBackupCodes(this.state.code, !1), this.props.onClose();
                 } catch (e) {
                     if (null == e.body) return;
                     e.body.message && this.setState({ errorMessage: e.body.message });
@@ -142,7 +110,7 @@ class f extends i.PureComponent {
                     this.setState({ isLoading: !1 });
                 }
             }),
-            d(this, "handleCodeChange", (e) => {
+            u(this, "handleCodeChange", (e) => {
                 this.setState({ code: e });
             });
     }
--- chunks/44463.js
+++ chunks/44463.js
@@ -1,5 +1,4 @@
 e.exports = {
     spacing: "a959b15304ce5719-spacing",
-    error: "a959b15304ce5719-error",
     card: "a959b15304ce5719-card a959b15304ce5719-spacing",
 };
