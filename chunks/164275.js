3c03d087bb4369335d31b3b75b2dfeeaea9dc684
DiscrapperManager <admin@minerpl.xyz>
2025-12-16 19:05:04 +0100

    canaryRelease: 480498
    Main scripts: 3
    Lazy Loaded Scripts: 1807
    Chunks:
     * Main Files: 15828
     * Scripts: 9387
    Strings: 23467
    Routes: 783
    Experiments Count: 347
    Apex Experiments Count: 143

--- chunks/164275.js
+++ chunks/164275.js
@@ -1,21 +1,23 @@
 n.d(t, {
-    M: () => u,
-    P: () => d,
+    M: () => l,
+    P: () => c,
 });
 var r = n(873546),
     i = n(325100),
     a = n(283066),
-    o = n(17100),
-    s = n(474936);
+    o = n(474936);
+function s(e, t, n) {
+    return !e && n === o.Si.TIER_2 && !t;
+}
 function l(e, t, n) {
-    return !e && n === s.Si.TIER_2 && !t;
+    return s(e, t, n) && !(0, a.Y)({ location: "wowMomentEligibilityUtils" });
 }
-function c() {
-    return (0, a.Y)({ location: "wowMomentEligibilityUtils" }) && (0, o.u)({ location: "wowMomentEligibilityUtils" });
-}
-function u(e, t, n) {
-    return l(e, t, n) && !c();
-}
-function d(e, t, n) {
-    return l(e, t, n) && c() && !r.eL && !r.bO && !(0, i.Ak)({ location: "wowMomentEligibilityUtils" });
+function c(e, t, n) {
+    return (
+        s(e, t, n) &&
+        (0, a.Y)({ location: "wowMomentEligibilityUtils" }) &&
+        !r.eL &&
+        !r.bO &&
+        !(0, i.Ak)({ location: "wowMomentEligibilityUtils" })
+    );
 }
