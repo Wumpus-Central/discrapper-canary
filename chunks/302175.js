cf9f3b807d6c51a0681c713c1f8b6b8aa79b63eb
DiscrapperManager <admin@minerpl.xyz>
2025-12-17 22:17:04 +0100

    canaryRelease: 481169
    Main scripts: 3
    Lazy Loaded Scripts: 1810
    Chunks:
     * Main Files: 15861
     * Scripts: 9433
    Strings: 23487
    Routes: 785
    Experiments Count: 346
    Apex Experiments Count: 147

--- chunks/302175.js
+++ chunks/302175.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => V }), n(953529), n(388685);
+n.d(t, { Z: () => W }), n(953529), n(388685);
 var r = n(54381),
     i = n(473749),
     a = n(120356),
@@ -7,29 +7,32 @@ var r = n(54381),
     l = n(907331),
     c = n(793030),
     u = n(442837),
-    d = n(481060),
-    f = n(100527),
-    p = n(906732),
-    _ = n(879892),
-    m = n(245004),
-    h = n(430824),
-    g = n(594174),
-    E = n(63063),
-    b = n(713081),
-    y = n(905128),
-    O = n(156732),
-    v = n(639777),
-    S = n(70300),
-    I = n(973772),
-    T = n(441536),
-    C = n(619733),
-    A = n(535396),
-    N = n(981631),
-    P = n(556970),
-    R = n(388032),
-    w = n(887143),
-    D = n(918820);
-function x(e, t, n) {
+    d = n(28664),
+    f = n(481060),
+    p = n(100527),
+    _ = n(906732),
+    m = n(879892),
+    h = n(245004),
+    g = n(430824),
+    E = n(594174),
+    b = n(63063),
+    y = n(713081),
+    O = n(905128),
+    v = n(156732),
+    S = n(639777),
+    I = n(70300),
+    T = n(973772),
+    C = n(441536),
+    A = n(619733),
+    N = n(282793),
+    P = n(535396),
+    R = n(981631),
+    w = n(353149),
+    D = n(556970),
+    x = n(388032),
+    L = n(887143),
+    j = n(918820);
+function M(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -42,7 +45,7 @@ function x(e, t, n) {
         e
     );
 }
-function L(e) {
+function k(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -53,12 +56,12 @@ function L(e) {
                 }),
             )),
             r.forEach(function (t) {
-                x(e, t, n[t]);
+                M(e, t, n[t]);
             });
     }
     return e;
 }
-function j(e, t) {
+function U(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -70,135 +73,141 @@ function j(e, t) {
     }
     return n;
 }
-function M(e, t) {
+function G(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : j(Object(t)).forEach(function (n) {
+            : U(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
     );
 }
-function k(e) {
+function Z(e) {
     let {
             guildId: t,
             activeStatus: n,
             title: a,
             description: c,
-            cost: E,
-            costDecorator: b,
-            imageUrl: y,
-            skuId: O,
-            isNew: S,
-            onClose: I,
-            onHover: C,
+            cost: b,
+            costDecorator: y,
+            imageUrl: O,
+            skuId: v,
+            isNew: I,
+            onClose: T,
+            onHover: A,
         } = e,
-        { analyticsLocations: x } = (0, p.ZP)(),
-        j = n !== A.A3.INACTIVE,
-        k = (0, v.Z)(t),
-        U = (0, u.e7)([g.default], () => g.default.getCurrentUser()),
-        [G, Z] = i.useState(!1),
-        [F, B] = i.useState(!1),
-        V = F,
-        H = i.useCallback(() => {
-            let e = h.Z.getGuild(t);
+        { analyticsLocations: M } = (0, _.ZP)(),
+        U = n !== P.A3.INACTIVE,
+        Z = (0, S.Z)(t),
+        F = (0, u.e7)([E.default], () => E.default.getCurrentUser()),
+        [B, V] = i.useState(!1),
+        [H, Y] = i.useState(!1),
+        W = H,
+        K = i.useCallback(() => {
+            let e = g.Z.getGuild(t);
             null != e &&
-                (0, _.u)({
+                (0, m.u)({
                     analyticsLocation: {
-                        page: N.ZY5.GUILD_POWERUPS_MARKETING,
-                        section: N.jXE.GUILD_POWERUPS_MARKETING_PERKS_SECTION,
+                        page: R.ZY5.GUILD_POWERUPS_MARKETING,
+                        section: R.jXE.GUILD_POWERUPS_MARKETING_PERKS_SECTION,
                     },
                     numberOfBoostsToAdd: 1,
-                    analyticsLocations: x,
+                    analyticsLocations: M,
                     guild: e,
                 });
-        }, [t, x]),
-        Y = i.useCallback(() => {
-            I(), (0, T.Z)(t, f.Z.GUILD_POWERUPS_MARKETING, O);
-        }, [t, O, I]),
-        W = {
+        }, [t, M]),
+        z = i.useCallback(() => {
+            T(), (0, C.Z)(t, p.Z.GUILD_POWERUPS_MARKETING, v);
+        }, [t, v, T]),
+        q = {
             tension: 400,
             friction: 30,
         },
-        K = (0, d.q_F)({
-            transform: V
+        Q = (0, f.q_F)({
+            transform: W
                 ? "translateX(-50%) translateY(16px) scale(1)"
                 : "translateX(-50%) translateY(24px) scale(1.40)",
             borderRadius: "0px",
             transformOrigin: "center 0%",
-            config: W,
+            config: q,
         }),
-        z = (0, d.q_F)({
-            y: V ? -25 : 0,
-            config: W,
+        X = (0, f.q_F)({
+            y: W ? -25 : 0,
+            config: q,
         }),
-        q = (0, d.q_F)({
-            opacity: +!!V,
-            transform: V ? "translateY(0)" : "translateY(20px)",
-            config: W,
+        J = (0, f.q_F)({
+            opacity: +!!W,
+            transform: W ? "translateY(0)" : "translateY(20px)",
+            config: q,
         }),
-        Q = i.useCallback((e) => {
-            e && Z(!0);
+        $ = i.useCallback((e) => {
+            e && V(!0);
         }, []),
-        X = (0, l.O)(Q);
+        ee = (0, l.O)($);
     return (0, r.jsxs)("div", {
-        className: o()(D.topPerksCard, D.animatedTopPerksCard, w.powerupCard, { [D.animate]: G }),
+        className: o()(j.topPerksCard, j.animatedTopPerksCard, L.powerupCard, { [j.animate]: B }),
         onMouseEnter: () => {
-            B(!0), null == C || C(!0);
+            Y(!0), null == A || A(!0);
         },
         onMouseLeave: () => {
-            B(!1), null == C || C(!1);
+            Y(!1), null == A || A(!1);
         },
         children: [
             (0, r.jsx)("div", {
-                className: D.intObserver,
-                ref: X,
+                className: j.intObserver,
+                ref: ee,
             }),
             (0, r.jsx)("div", {
-                className: D.topPerksCardImageWrapper,
+                className: j.topPerksCardImageWrapper,
                 children: (0, r.jsx)(s.animated.img, {
-                    className: o()(D.topPerksCardImage, w.image),
-                    src: y,
+                    className: o()(j.topPerksCardImage, L.image),
+                    src: O,
                     alt: "",
-                    style: K,
+                    style: Q,
                 }),
             }),
             (0, r.jsxs)(s.animated.div, {
-                style: M(L({}, z), { transform: z.y.to((e) => "translateY(".concat(e, "px)")) }),
-                className: o()(D.contentContainer, w.contentContainer),
+                style: G(k({}, X), { transform: X.y.to((e) => "translateY(".concat(e, "px)")) }),
+                className: o()(j.contentContainer, L.contentContainer),
                 children: [
-                    (0, r.jsx)(m.xm, {
+                    (0, r.jsx)(h.xm, {
                         heading: a,
                         description: c,
-                        label: R.intl.formatToPlainString(P.default.QOacIS, {
-                            quantity: E,
-                            decorator: null != b ? b : "",
+                        label: x.intl.formatToPlainString(D.default.QOacIS, {
+                            quantity: b,
+                            decorator: null != y ? y : "",
                         }),
-                        isEnabled: j,
+                        isEnabled: U,
                     }),
                     (0, r.jsxs)(s.animated.div, {
-                        style: q,
-                        className: D.buttonsContainer,
+                        style: J,
+                        className: j.buttonsContainer,
                         children: [
-                            (0, r.jsx)("div", {
-                                className: D.button,
-                                children: (0, r.jsx)(d.Button, {
-                                    variant: "primary",
-                                    text: R.intl.string(R.t.oPAx73),
-                                    onClick: H,
-                                    disabled: null == U ? void 0 : U.isPremiumGroupMember(),
-                                    fullWidth: !0,
+                            (0, r.jsx)(d.u, {
+                                text: x.intl.formatToPlainString(w.default["5xN/C1"], {
+                                    premiumGroupProductName: (0, N.sO)(),
+                                }),
+                                shouldShow: (null == F ? void 0 : F.isPremiumGroupMember()) === !0,
+                                children: (0, r.jsx)("div", {
+                                    className: j.button,
+                                    children: (0, r.jsx)(f.Button, {
+                                        variant: "primary",
+                                        text: x.intl.string(x.t.oPAx73),
+                                        onClick: K,
+                                        disabled: null == F ? void 0 : F.isPremiumGroupMember(),
+                                        fullWidth: !0,
+                                    }),
                                 }),
                             }),
-                            k &&
+                            Z &&
                                 (0, r.jsx)("div", {
-                                    className: D.button,
-                                    children: (0, r.jsx)(d.Button, {
+                                    className: j.button,
+                                    children: (0, r.jsx)(f.Button, {
                                         variant: "secondary",
-                                        text: R.intl.string(R.t.GoCQxU),
-                                        onClick: Y,
+                                        text: x.intl.string(x.t.GoCQxU),
+                                        onClick: z,
                                         fullWidth: !0,
                                     }),
                                 }),
@@ -206,21 +215,21 @@ function k(e) {
                     }),
                 ],
             }),
-            S &&
-                (0, r.jsx)(d.IGR, {
-                    className: D.topPerksCardNew,
-                    text: R.intl.string(R.t.y2b7CA),
+            I &&
+                (0, r.jsx)(f.IGR, {
+                    className: j.topPerksCardNew,
+                    text: x.intl.string(x.t.y2b7CA),
                 }),
-            G && (0, r.jsx)("div", { className: D.shineLine }),
+            B && (0, r.jsx)("div", { className: j.shineLine }),
         ],
     });
 }
-function U(e) {
+function F(e) {
     let { guildId: t, powerup: n, costDecorator: a, isNew: o, onClose: s } = e,
         [l, c] = i.useState(!1),
-        u = (0, I.ZP)(t, n).type,
-        d = (0, O.Z)(n, l);
-    return (0, r.jsx)(k, {
+        u = (0, T.ZP)(t, n).type,
+        d = (0, v.Z)(n, l);
+    return (0, r.jsx)(Z, {
         guildId: t,
         activeStatus: u,
         title: n.title,
@@ -234,43 +243,43 @@ function U(e) {
         onHover: (e) => c(e),
     });
 }
-let G = 3,
-    Z = new Set([C.fj]),
-    F = new Map([[C.fj, "+"]]),
-    B = i.forwardRef((e, t) => {
+let B = 3,
+    V = new Set([A.fj]),
+    H = new Map([[A.fj, "+"]]),
+    Y = i.forwardRef((e, t) => {
         let { guild: n, onClose: a } = e;
         i.useEffect(() => {
-            y.Z.shouldFetchCatalogForGuild(n.id) && (0, b.Sn)(n.id),
-                y.Z.shouldFetchPowerupsForGuild(n.id) && (0, b.BN)(n.id);
+            O.Z.shouldFetchCatalogForGuild(n.id) && (0, y.Sn)(n.id),
+                O.Z.shouldFetchPowerupsForGuild(n.id) && (0, y.BN)(n.id);
         }, [n.id]);
-        let o = (0, S.Z)(n.id),
-            s = (null != o ? o : []).slice(0, G);
+        let o = (0, I.Z)(n.id),
+            s = (null != o ? o : []).slice(0, B);
         return 0 === s.length
             ? null
             : (0, r.jsxs)("div", {
                   ref: t,
-                  className: w.container,
+                  className: L.container,
                   children: [
                       (0, r.jsx)(c.X6q, {
                           variant: "heading-xxl/extrabold",
-                          children: R.intl.string(P.default.wjI18Q),
+                          children: x.intl.string(D.default.wjI18Q),
                       }),
                       (0, r.jsx)(c.xvT, {
                           variant: "text-lg/medium",
-                          children: R.intl.format(P.default.S562fn, {
-                              helpDeskArticle: E.Z.getArticleURL(N.BhN.GUILD_BOOSTING_FAQ),
+                          children: x.intl.format(D.default.S562fn, {
+                              helpDeskArticle: b.Z.getArticleURL(R.BhN.GUILD_BOOSTING_FAQ),
                           }),
                       }),
                       (0, r.jsx)("div", {
-                          className: w.powerupsContainer,
+                          className: L.powerupsContainer,
                           children: s.map((e) =>
                               (0, r.jsx)(
-                                  U,
+                                  F,
                                   {
                                       guildId: n.id,
                                       powerup: e,
-                                      costDecorator: F.get(e.skuId),
-                                      isNew: Z.has(e.skuId),
+                                      costDecorator: H.get(e.skuId),
+                                      isNew: V.has(e.skuId),
                                       onClose: a,
                                   },
                                   "guild-powerup-marketing-".concat(e.skuId),
@@ -280,5 +289,5 @@ let G = 3,
                   ],
               });
     });
-B.displayName = "GuildPowerupsMarketingPowerupCards";
-let V = B;
+Y.displayName = "GuildPowerupsMarketingPowerupCards";
+let W = Y;
