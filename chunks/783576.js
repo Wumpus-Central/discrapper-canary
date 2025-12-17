e3e71e4de6ad2e42ff5ba861426c10901e4c658d
DiscrapperManager <admin@minerpl.xyz>
2025-12-17 18:18:24 +0100

    canaryRelease: 480995
    Main scripts: 3
    Lazy Loaded Scripts: 1808
    Chunks:
     * Main Files: 15858
     * Scripts: 9406
    Strings: 23478
    Routes: 784
    Experiments Count: 346
    Apex Experiments Count: 146

--- chunks/783576.js
+++ chunks/783576.js
@@ -1,35 +1,36 @@
-i.d(n, { default: () => c });
-var a = i(54381);
-i(473749);
-var e = i(481060),
-    s = i(782568),
-    r = i(313201),
-    o = i(388032),
-    l = i(471018);
-function c(t) {
-    let { href: n, onClose: c, transitionState: d } = t,
+n.d(a, { default: () => d });
+var e = n(54381);
+n(473749);
+var i = n(481060),
+    s = n(782568),
+    r = n(313201),
+    o = n(388032),
+    l = n(419543);
+function d(t) {
+    let { href: a, onClose: d, transitionState: c } = t,
         b = (0, r.Dt)();
-    return (0, a.jsxs)(e.Y0X, {
-        transitionState: d,
+    return (0, e.jsxs)(i.Y0X, {
+        "data-migration-pending": !0,
+        transitionState: c,
         "aria-labelledby": b,
         parentComponent: "SuspiciousDownloadModal",
         children: [
-            (0, a.jsx)("img", {
+            (0, e.jsx)("img", {
                 className: l.art,
-                src: i(902623),
+                src: n(902623),
                 alt: "",
             }),
-            (0, a.jsx)(e.hzk, {
-                children: (0, a.jsxs)("div", {
+            (0, e.jsx)(i.hzk, {
+                children: (0, e.jsxs)("div", {
                     className: l.modalContent,
                     children: [
-                        (0, a.jsx)(e.Heading, {
+                        (0, e.jsx)(i.Heading, {
                             id: b,
                             variant: "heading-lg/semibold",
                             className: l.title,
                             children: o.intl.string(o.t.XtDo9Z),
                         }),
-                        (0, a.jsx)(e.Text, {
+                        (0, e.jsx)(i.Text, {
                             variant: "text-md/normal",
                             className: l.body,
                             children: o.intl.string(o.t.L9yFko),
@@ -37,19 +38,19 @@ function c(t) {
                     ],
                 }),
             }),
-            (0, a.jsx)(e.mzw, {
-                children: (0, a.jsxs)(e.ButtonGroup, {
+            (0, e.jsx)(i.mzw, {
+                children: (0, e.jsxs)(i.ButtonGroup, {
                     children: [
-                        (0, a.jsx)(e.Button, {
+                        (0, e.jsx)(i.Button, {
                             variant: "primary",
                             text: o.intl.string(o.t.j7Vi2i),
-                            onClick: c,
+                            onClick: d,
                         }),
-                        (0, a.jsx)(e.Button, {
+                        (0, e.jsx)(i.Button, {
                             text: o.intl.string(o.t["/bHu89"]),
                             variant: "secondary",
                             onClick: () => {
-                                c(), (0, s.Z)(n, !0);
+                                d(), (0, s.Z)(a, !0);
                             },
                         }),
                     ],
