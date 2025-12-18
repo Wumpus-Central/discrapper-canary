23e981a6c524722be51fd67e4252907d84bc28c4
DiscrapperManager <admin@minerpl.xyz>
2025-12-18 01:27:50 +0100

    canaryRelease: 481320
    Main scripts: 3
    Lazy Loaded Scripts: 1810
    Chunks:
     * Main Files: 15863
     * Scripts: 9431
    Strings: 23488
    Routes: 785
    Experiments Count: 346
    Apex Experiments Count: 145

--- chunks/489574.js
+++ chunks/489574.js
@@ -1,30 +1,65 @@
-n.d(t, { k: () => _ });
+n.d(t, { k: () => g });
 var r = n(54381),
-    i = n(782568),
-    a = n(690221),
-    o = n(63063),
-    s = n(22189),
-    l = n(282793),
-    c = n(981631),
-    u = n(353149),
-    d = n(388032),
-    f = n(261778);
-let p = () => {
-        (0, i.Z)(o.Z.getArticleURL(c.BhN.PREMIUM_GROUP_ABOUT));
+    i = n(481060),
+    a = n(782568),
+    o = n(690221),
+    s = n(63063),
+    l = n(22189),
+    c = n(282793),
+    u = n(981631),
+    d = n(353149),
+    f = n(388032),
+    p = n(261778);
+function _(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0,
+              })
+            : (e[t] = n),
+        e
+    );
+}
+function m(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = null != arguments[t] ? arguments[t] : {},
+            r = Object.keys(n);
+        "function" == typeof Object.getOwnPropertySymbols &&
+            (r = r.concat(
+                Object.getOwnPropertySymbols(n).filter(function (e) {
+                    return Object.getOwnPropertyDescriptor(n, e).enumerable;
+                }),
+            )),
+            r.forEach(function (t) {
+                _(e, t, n[t]);
+            });
+    }
+    return e;
+}
+let h = () => {
+        (0, a.Z)(s.Z.getArticleURL(u.BhN.PREMIUM_GROUP_ABOUT));
     },
-    _ = () => ({
-        name: s.I.PREMIUM_GROUP,
-        title: d.intl.string(u.default.YkvksF),
-        subtitle: d.intl.formatToPlainString(u.default.JlyGQj, {
-            totalSeats: l.Q5,
-            premiumGroupProductName: (0, l.sO)(),
+    g = () => ({
+        name: l.I.PREMIUM_GROUP,
+        title: f.intl.string(d.default.YkvksF),
+        subtitle: f.intl.formatToPlainString(d.default.JlyGQj, {
+            totalSeats: c.Q5,
+            premiumGroupProductName: (0, c.sO)(),
         }),
-        description: (0, r.jsx)(a.Z, {
-            onClick: p,
-            children: d.intl.string(u.default.yYyGJH),
+        description: (0, r.jsx)(o.Z, {
+            onClick: h,
+            children: f.intl.string(d.default.yYyGJH),
         }),
-        descriptionCta: d.intl.string(d.t.jVcuVY),
-        pillText: d.intl.string(d.t.oW0eUd).toLocaleUpperCase(),
-        perkImage: f,
-        onCtaClick: () => {},
+        descriptionCta: f.intl.string(f.t.jVcuVY),
+        pillText: f.intl.string(f.t.oW0eUd).toLocaleUpperCase(),
+        perkImage: p,
+        onCtaClick: () => {
+            (0, i.ZDy)(async () => {
+                let { default: e } = await n.e("33638").then(n.bind(n, 837244));
+                return (t) => (0, r.jsx)(e, m({}, t));
+            });
+        },
     });
