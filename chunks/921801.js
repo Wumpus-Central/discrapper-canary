2dfd8d174be8a4c8322870cd59b25ae953fed1c8
DiscrapperManager <admin@minerpl.xyz>
2025-12-18 21:09:25 +0100

    canaryRelease: 481597
    Main scripts: 3
    Lazy Loaded Scripts: 1811
    Chunks:
     * Main Files: 15875
     * Scripts: 9485
    Strings: 23507
    Routes: 789
    Experiments Count: 346
    Apex Experiments Count: 145

--- chunks/921801.js
+++ chunks/921801.js
@@ -1,12 +1,24 @@
-n.d(t, { F: () => i }), n(473749);
-var r = n(991346);
-let i = (e) => {
+n.d(t, { F: () => s });
+var r = n(54381);
+n(473749);
+var i = n(526665),
+    a = n(991346);
+function o(e) {
     var t;
-    let { setting: n, children: i } = e,
-        a = (0, r.Pt)(),
-        o = (0, r.Gj)(n),
-        s = a[n];
+    let { setting: n, children: r } = e,
+        i = (0, a.Pt)(),
+        o = (0, a.Gj)(n),
+        s = i[n];
     return (null != s.predicate && (null == (t = s.predicate) ? void 0 : t.call(s)) !== !0) || (o.size > 0 && !o.has(n))
         ? null
-        : i;
-};
+        : r;
+}
+function s(e) {
+    let { setting: t, children: n } = e;
+    return (0, i.gN)("AccountProfileCardSetting")
+        ? n
+        : (0, r.jsx)(o, {
+              setting: t,
+              children: n,
+          });
+}
