f1597505f6dca3def48abff61f31959929f244b9
DiscrapperManager <admin@minerpl.xyz>
2025-12-19 10:05:05 +0100

    canaryRelease: 481879
    Main scripts: 3
    Lazy Loaded Scripts: 1811
    Chunks:
     * Main Files: 15877
     * Scripts: 9490
    Strings: 23515
    Routes: 788
    Experiments Count: 346
    Apex Experiments Count: 147

--- chunks/851519.js
+++ chunks/851519.js
@@ -6,8 +6,8 @@ var i = n(54381),
     r = n(627845),
     a = n(509613),
     o = n(313789),
-    S = n(501348),
-    T = n(329013),
+    T = n(501348),
+    S = n(329013),
     c = n(388032);
 let E = (0, a.ON)(o.n.APPEARANCE_CLIENT_THEMES, {
     useSearchTerms: () => [c.intl.string(c.t.Ksh3ik), c.intl.string(c.t["0EzVst"]), c.intl.string(c.t.OCOOiI)],
@@ -16,13 +16,12 @@ let E = (0, a.ON)(o.n.APPEARANCE_CLIENT_THEMES, {
         return (0, i.jsx)(s.C3N, {
             label: c.intl.string(c.t.Ksh3ik),
             children: t
-                ? (0, i.jsx)(s.Wn, {
-                      messageType: s.QYI.INFO,
-                      children: (0, r.b)() ? (0, i.jsx)(S.hU, {}) : c.intl.string(c.t.AUMSZP),
+                ? (0, i.jsx)(s.M14, {
+                      type: "info",
+                      children: (0, r.b)() ? (0, i.jsx)(T.hU, {}) : c.intl.string(c.t.AUMSZP),
                   })
-                : (0, i.jsx)(s.xJW, {
-                      title: (0, i.jsx)(S.n6, {}),
-                      children: (0, i.jsx)(T.i, {}),
+                : (0, i.jsxs)("div", {
+                      children: [(0, i.jsx)(T.n6, {}), (0, i.jsx)(S.i, {})],
                   }),
         });
     },
