370bbaf66c83804af37f5f678792393ecb850aad
DiscrapperManager <admin@minerpl.xyz>
2025-12-22 18:47:42 +0100

    canaryRelease: 482431
    Main scripts: 3
    Lazy Loaded Scripts: 1812
    Chunks:
     * Main Files: 15871
     * Scripts: 9419
    Strings: 23509
    Routes: 788
    Experiments Count: 345
    Apex Experiments Count: 146

--- chunks/407699.js
+++ chunks/407699.js
@@ -1,37 +1,26 @@
-n.d(t, { Z: () => p });
+n.d(t, { Z: () => c });
 var r = n(54381);
 n(473749);
-var i = n(120356),
-    a = n.n(i),
-    o = n(442837),
-    s = n(692547),
-    l = n(481060),
-    c = n(320582),
-    u = n(621853),
-    d = n(388032),
-    f = n(853761);
-function p(e) {
-    let { userId: t, size: n = "refresh_sm", className: i } = e,
-        p = (0, c.ML)(t),
-        _ = (0, c.vh)(t),
-        m = (0, o.e7)([u.Z], () => {
+var i = n(442837),
+    a = n(481060),
+    o = n(320582),
+    s = n(621853),
+    l = n(388032);
+function c(e) {
+    let { userId: t, className: n } = e,
+        c = (0, o.ML)(t),
+        u = (0, o.vh)(t),
+        d = (0, i.e7)([s.Z], () => {
             var e;
-            return null == (e = u.Z.getUserProfile(t)) ? void 0 : e.fetchError;
+            return null == (e = s.Z.getUserProfile(t)) ? void 0 : e.fetchError;
         });
-    return p.length > 0 || _.length > 0 || null == m
+    return c.length > 0 || u.length > 0 || null == d
         ? null
-        : (0, r.jsxs)("div", {
-              className: a()(f.container, i),
-              children: [
-                  (0, r.jsx)(l.Mgn, {
-                      size: n,
-                      color: s.Z.colors.STATUS_WARNING.css,
-                  }),
-                  (0, r.jsx)(l.Text, {
-                      variant: "text-sm/normal",
-                      color: "text-default",
-                      children: d.intl.string(d.t.p3Q9Y2),
-                  }),
-              ],
+        : (0, r.jsx)("div", {
+              className: n,
+              children: (0, r.jsx)(a.M14, {
+                  type: "warning",
+                  children: l.intl.string(l.t.p3Q9Y2),
+              }),
           });
 }
--- chunks/853761.js
+++
@@ -1,1 +0,0 @@
-e.exports = { container: "_079adf2c6804b7f3-container" };
