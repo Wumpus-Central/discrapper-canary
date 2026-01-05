14eaf6a7959c805b808556b50f747c43bf7582cc
DiscrapperManager <admin@minerpl.xyz>
2026-01-05 22:00:47 +0100

    canaryRelease: 482934
    Main scripts: 3
    Lazy Loaded Scripts: 1812
    Chunks:
     * Main Files: 15887
     * Scripts: 9422
    Strings: 23511
    Routes: 788
    Experiments Count: 343
    Apex Experiments Count: 143

--- chunks/785925.js
+++ chunks/785925.js
@@ -1,61 +1,71 @@
-n.d(t, { ZP: () => s });
-var i = n(544891),
-    r = n(570140),
-    l = n(960048),
-    a = n(535834),
-    o = n(981631);
-let s = {
+n.d(t, { ZP: () => c });
+var r = n(544891),
+    i = n(570140),
+    a = n(594174),
+    o = n(960048),
+    s = n(535834),
+    l = n(981631);
+let c = {
     connect: async (e) => {
         try {
-            let t = await i.tn.post({
-                url: o.ANM.HAVEN_CONNECT(e),
+            let t = await r.tn.post({
+                url: l.ANM.HAVEN_CONNECT(e),
                 rejectWithError: !0,
             });
             return (
-                r.Z.dispatch({
+                i.Z.dispatch({
                     type: "HAVEN_CONNECT",
-                    room: (0, a.CK)(t.body),
+                    room: (0, s.CK)(t.body),
                 }),
                 t
             );
         } catch (e) {
-            l.Z.captureException(e);
+            o.Z.captureException(e);
         }
     },
     disconnect: async (e) => {
         try {
-            await i.tn.post({
-                url: o.ANM.HAVEN_DISCONNECT(e),
+            await r.tn.post({
+                url: l.ANM.HAVEN_DISCONNECT(e),
                 rejectWithError: !0,
             });
-        } catch (e) {
-            l.Z.captureException(e);
+        } catch (t) {
+            if ((null == t ? void 0 : t.status) === 404) {
+                let t = a.default.getCurrentUser();
+                null != t &&
+                    i.Z.dispatch({
+                        type: "HAVEN_DISCONNECT",
+                        roomId: e,
+                        userId: t.id,
+                    });
+            }
+            o.Z.captureException(t);
         }
     },
     update: async (e, t) => {
-        let { position: n, avatar: r, seat: a } = t;
+        let { position: n, avatar: i, seat: a } = t;
         try {
-            await i.tn.post({
-                url: o.ANM.HAVEN_UPDATE(e),
+            await r.tn.post({
+                url: l.ANM.HAVEN_UPDATE(e),
                 body: {
                     position: n,
-                    avatar: r,
+                    avatar: i,
                     seat: a,
                 },
                 rejectWithError: !0,
             });
         } catch (e) {
-            l.Z.captureException(e);
+            o.Z.captureException(e);
         }
     },
     getAssets: async () => {
         try {
-            return await i.tn.get({
-                url: o.ANM.HAVEN_GET_ASSETS,
+            return await r.tn.get({
+                url: l.ANM.HAVEN_GET_ASSETS,
                 rejectWithError: !0,
             });
         } catch (e) {
-            return l.Z.captureException(e), null;
+            return o.Z.captureException(e), null;
         }
     },
 };
