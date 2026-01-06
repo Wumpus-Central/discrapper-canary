3659e297f6dd477628aec2eb7560357b3be3bd1f
DiscrapperManager <admin@minerpl.xyz>
2026-01-06 01:30:14 +0100

    canaryRelease: 483056
    Main scripts: 3
    Lazy Loaded Scripts: 1809
    Chunks:
     * Main Files: 15897
     * Scripts: 9385
    Strings: 23522
    Routes: 788
    Experiments Count: 342
    Apex Experiments Count: 144

--- chunks/250005.js
+++
@@ -1,1 +0,0 @@
-e.exports = { root: "_65143c54371ea030-root" };
--- chunks/60594.js
+++ chunks/60594.js
@@ -1,146 +1,145 @@
-n.d(t, { default: () => X }), n(388685);
-var r = n(54381),
-    i = n(473749),
-    s = n(120356),
-    l = n.n(s),
-    o = n(209739),
-    c = n.n(o),
-    a = n(990547),
-    d = n(268146),
-    u = n(442837),
-    f = n(159691),
-    h = n(481060),
-    p = n(668781),
-    x = n(211266),
-    m = n(579806),
-    g = n(89425),
-    v = n(100527),
-    j = n(906732),
-    _ = n(594190),
-    b = n(695346),
-    y = n(131951),
-    w = n(19780),
-    O = n(594174),
-    C = n(449224),
-    S = n(358085),
-    I = n(74538),
-    Z = n(299570),
-    E = n(960861),
-    P = n(989941),
-    N = n(562224),
-    T = n(21072),
-    R = n(183326),
-    M = n(914241),
-    A = n(443762),
-    L = n(741127),
-    U = n(530626),
-    D = n(492627),
-    G = n(741731),
-    H = n(562518),
-    B = n(208552),
-    k = n(544753),
-    F = n(195436),
-    W = n(70722),
-    V = n(715689),
-    z = n(388032),
-    Y = n(123795);
-function X(e) {
+r.d(t, { default: () => Y }), r(388685);
+var n = r(54381),
+    i = r(473749),
+    s = r(120356),
+    l = r.n(s),
+    o = r(209739),
+    c = r.n(o),
+    a = r(990547),
+    d = r(268146),
+    u = r(442837),
+    f = r(159691),
+    h = r(481060),
+    p = r(668781),
+    x = r(211266),
+    m = r(579806),
+    g = r(89425),
+    v = r(100527),
+    j = r(906732),
+    _ = r(594190),
+    b = r(695346),
+    y = r(131951),
+    w = r(19780),
+    O = r(594174),
+    C = r(449224),
+    S = r(358085),
+    I = r(74538),
+    Z = r(299570),
+    E = r(960861),
+    P = r(989941),
+    N = r(562224),
+    T = r(183326),
+    R = r(914241),
+    M = r(443762),
+    A = r(741127),
+    L = r(530626),
+    U = r(492627),
+    G = r(741731),
+    D = r(562518),
+    H = r(208552),
+    B = r(544753),
+    k = r(195436),
+    F = r(70722),
+    W = r(715689),
+    V = r(388032),
+    z = r(123795);
+function Y(e) {
     var t;
-    let { onClose: n, transitionState: s, sourcePID: o, analyticsLocations: X = [], selectSource: q = !0 } = e,
-        J = (0, u.e7)([_.ZP], () => {
+    let { onClose: r, transitionState: s, sourcePID: o, analyticsLocations: Y = [], selectSource: X = !0 } = e,
+        q = (0, u.e7)([_.ZP], () => {
             var e;
             return null != o && null != (e = _.ZP.getGameForPID(o)) ? e : void 0;
         }),
-        Q = i.useRef(performance.now()),
-        K = (0, u.e7)(
+        J = i.useRef(performance.now()),
+        Q = (0, u.e7)(
             [y.Z],
             () =>
                 (y.Z.getUseSystemScreensharePicker() || y.Z.getUseGamescopeCapture()) &&
                 ((0, S.isLinux)() ||
-                    ((0, S.isMac)() && c().satisfies(null === m.Z || void 0 === m.Z ? void 0 : m.Z.os.release, W.jR))),
+                    ((0, S.isMac)() && c().satisfies(null === m.Z || void 0 === m.Z ? void 0 : m.Z.os.release, F.jR))),
         ),
-        { analyticsLocations: $ } = (0, j.ZP)(X, v.Z.GO_LIVE_MODAL_V2),
-        ee = (0, u.e7)([O.default], () => O.default.getCurrentUser()),
-        et = (0, k.Z)(),
-        { state: en, dispatch: er } = (0, A.Ti)(J, ee, et),
-        ei = (0, x.Z)((0, S.isWindows)() && null != J && q ? "confirm" : "source_select");
-    (0, F.Z)(K, er, "confirm" === ei);
-    let es = !I.ZP.canStreamQuality(I.ZP.StreamQuality.HIGH, ee),
-        el = !(0, S.isLinux)(),
-        eo = "confirm" === ei,
-        ec = !q && null != J && !eo,
-        ea = eo || en.sourceType === d.vA.CAMERA,
-        ed = K && en.sourceType !== d.vA.CAMERA,
-        eu = i.useMemo(() => {
+        { analyticsLocations: K } = (0, j.ZP)(Y, v.Z.GO_LIVE_MODAL_V2),
+        $ = (0, u.e7)([O.default], () => O.default.getCurrentUser()),
+        ee = (0, B.Z)(),
+        { state: et, dispatch: er } = (0, M.Ti)(q, $, ee),
+        en = (0, x.Z)((0, S.isWindows)() && null != q && X ? "confirm" : "source_select");
+    (0, k.Z)(Q, er, "confirm" === en);
+    let ei = !I.ZP.canStreamQuality(I.ZP.StreamQuality.HIGH, $),
+        es = !(0, S.isLinux)(),
+        el = "confirm" === en,
+        eo = !X && null != q && !el,
+        ec = el || et.sourceType === d.vA.CAMERA,
+        ea = Q && et.sourceType !== d.vA.CAMERA,
+        ed = i.useMemo(() => {
             let e = [
                 {
-                    name: z.intl.string(z.t.tHoi7u),
+                    name: V.intl.string(V.t.tHoi7u),
                     value: d.vA.WINDOW,
                     icon: h.GON,
                 },
                 {
-                    name: z.intl.string(z.t.MhJ43A),
+                    name: V.intl.string(V.t.MhJ43A),
                     value: d.vA.CAMERA,
                     icon: h.Odl,
                 },
             ];
             return (
-                K ||
+                Q ||
                     e.splice(1, 0, {
-                        name: z.intl.string(z.t.slM8rG),
+                        name: V.intl.string(V.t.slM8rG),
                         value: d.vA.SCREEN,
                         icon: h.pzj,
                     }),
                 e
             );
-        }, [K]),
-        { sourceType: ef } = en,
-        eh = i.useCallback(
+        }, [Q]),
+        { sourceType: eu } = et,
+        ef = i.useCallback(
             async (e) => {
-                b.eo.updateSetting(en.notifyFriends), b.I0.updateSetting(en.hidePreview);
-                let [t, r] = await (0, N.Z)(e.hasOwnProperty("pid") ? e.pid : e, {
-                    preset: en.preset,
-                    fps: en.fps,
-                    resolution: en.resolution,
-                    soundshareEnabled: !en.muteStreamAudio,
-                    previewDisabled: en.hidePreview,
-                    goLiveModalDurationMs: performance.now() - Q.current,
-                    audioSourceId: en.audioSourceId,
-                    analyticsLocations: $,
+                b.eo.updateSetting(et.notifyFriends), b.I0.updateSetting(et.hidePreview);
+                let [t, n] = await (0, N.Z)(e.hasOwnProperty("pid") ? e.pid : e, {
+                    preset: et.preset,
+                    fps: et.fps,
+                    resolution: et.resolution,
+                    soundshareEnabled: !et.muteStreamAudio,
+                    previewDisabled: et.hidePreview,
+                    goLiveModalDurationMs: performance.now() - J.current,
+                    audioSourceId: et.audioSourceId,
+                    analyticsLocations: K,
                 });
-                if (t) return n();
-                "no permission" === r &&
+                if (t) return r();
+                "no permission" === n &&
                     p.Z.show({
-                        title: z.intl.string(z.t["X+mXeq"]),
-                        body: z.intl.string(z.t.MIJCzr),
+                        title: V.intl.string(V.t["X+mXeq"]),
+                        body: V.intl.string(V.t.MIJCzr),
                     }),
-                    n();
+                    r();
             },
-            [n, en, $],
+            [r, et, K],
         );
-    async function ep(e) {
-        (await (0, g.Z)({ channelId: e })) && null != J && eh(J);
+    async function eh(e) {
+        (await (0, g.Z)({ channelId: e })) && null != q && ef(q);
     }
-    let ex = i.useCallback(() => {
+    let ep = i.useCallback(() => {
         var e;
-        eh({
-            id: ("gamescope" === en.nativeSourceType ? "screen:" : "prepicked:") + en.nativeSourceType,
-            name: null != (e = E.ZP.getLastPickedContentTitle()) ? e : z.intl.string(z.t.KKcy95),
+        ef({
+            id: ("gamescope" === et.nativeSourceType ? "screen:" : "prepicked:") + et.nativeSourceType,
+            name: null != (e = E.ZP.getLastPickedContentTitle()) ? e : V.intl.string(V.t.KKcy95),
             url: "",
         });
-    }, [eh, en.nativeSourceType]);
-    return (0, r.jsx)(A.Yw, {
-        state: en,
+    }, [ef, et.nativeSourceType]);
+    return (0, n.jsx)(M.Yw, {
+        state: et,
         dispatch: er,
-        children: (0, r.jsx)(h.IX, {
+        children: (0, n.jsx)(h.IX, {
             transitionState: s,
-            onClose: n,
+            onClose: r,
             size: "xl",
             trackingProps: {
                 impression: {
                     impressionName: a.ImpressionNames.GO_LIVE_MODAL,
                     impressionProperties: {
-                        location_stack: $,
+                        location_stack: K,
                         application_id: (0, S.isWindows)()
                             ? null == (t = (0, P.Z)(_.ZP, C.Z))
                                 ? void 0
@@ -150,29 +149,29 @@ function X(e) {
                     },
                 },
             },
-            children: (0, r.jsxs)("div", {
-                className: l()(Y.root, {
-                    [Y.nativePicker]: K && null == J,
-                    [Y.channelSelector]: ec,
-                    [Y.confirmStep]: eo,
+            children: (0, n.jsxs)("div", {
+                className: l()(z.root, {
+                    [z.nativePicker]: Q && null == q,
+                    [z.channelSelector]: eo,
+                    [z.confirmStep]: el,
                 }),
                 children: [
-                    ec
-                        ? (0, r.jsx)(R.Z, {
-                              className: l()(Y.channelSelectorComponent, Y.withFooter),
-                              onSelectChannel: ep,
+                    eo
+                        ? (0, n.jsx)(T.Z, {
+                              className: l()(z.channelSelectorComponent, z.withFooter),
+                              onSelectChannel: eh,
                           })
-                        : eo
-                          ? (0, r.jsx)(M.Z, {})
-                          : (0, r.jsxs)(r.Fragment, {
+                        : el
+                          ? (0, n.jsx)(R.Z, {})
+                          : (0, n.jsxs)(n.Fragment, {
                                 children: [
-                                    (0, r.jsx)("div", {
-                                        className: Y.header,
-                                        children: (0, r.jsx)(h.sY7, {
-                                            className: Y.segmentedControl,
-                                            value: ef,
+                                    (0, n.jsx)("div", {
+                                        className: z.header,
+                                        children: (0, n.jsx)(h.sY7, {
+                                            className: z.segmentedControl,
+                                            value: eu,
                                             look: "pill",
-                                            optionClassName: Y.segmentedControlOption,
+                                            optionClassName: z.segmentedControlOption,
                                             onChange: (e) => {
                                                 let { value: t } = e;
                                                 return er({
@@ -180,15 +179,15 @@ function X(e) {
                                                     sourceType: t,
                                                 });
                                             },
-                                            options: eu,
+                                            options: ed,
                                         }),
                                     }),
-                                    (0, r.jsx)(f.w0Z, {
-                                        className: Y.content,
+                                    (0, n.jsx)(f.w0Z, {
+                                        className: z.content,
                                         children:
-                                            K && ef !== d.vA.CAMERA
-                                                ? (0, r.jsx)(L.Z, { onSourceSelect: ex })
-                                                : (0, r.jsx)(U.Z, {
+                                            Q && eu !== d.vA.CAMERA
+                                                ? (0, n.jsx)(A.Z, { onSourceSelect: ep })
+                                                : (0, n.jsx)(L.Z, {
                                                       onClick: function (e) {
                                                           var t;
                                                           (null == (t = e.id) ? void 0 : t.startsWith(d.vA.CAMERA))
@@ -196,62 +195,54 @@ function X(e) {
                                                                     type: "set_selected_source",
                                                                     source: e,
                                                                 })
-                                                              : eh(e);
+                                                              : ef(e);
                                                       },
                                                   }),
                                     }),
                                 ],
                             }),
-                    (0, r.jsxs)("div", {
-                        className: Y.footer,
+                    (0, n.jsxs)("div", {
+                        className: z.footer,
                         children: [
-                            (0, r.jsxs)("div", {
-                                className: Y.footerContent,
+                            (0, n.jsxs)("div", {
+                                className: z.footerContent,
                                 children: [
-                                    (0, r.jsx)(B.Z, {}),
-                                    (0, r.jsxs)("div", {
-                                        className: Y.rightButtonGroup,
+                                    (0, n.jsx)(H.Z, {}),
+                                    (0, n.jsxs)("div", {
+                                        className: z.rightButtonGroup,
                                         children: [
-                                            es && (0, r.jsx)(G.Z, { onClose: n }),
-                                            ed &&
-                                                !ea &&
-                                                (0, r.jsx)(T.Z, {
-                                                    mainCTADisabled: !el && "" === en.nativeSourceType,
-                                                    mainCTAOnClick: () => {
-                                                        "gamescope" === en.nativeSourceType
-                                                            ? ex()
-                                                            : ((0, Z.t)(), (0, Z.T)(en.nativeSourceType));
-                                                    },
-                                                    align: "right",
-                                                    ctaText: z.intl.string(
-                                                        "gamescope" === en.nativeSourceType
-                                                            ? V.default["5AyH/p"]
-                                                            : z.t.FiBjwU,
+                                            ei && (0, n.jsx)(G.Z, { onClose: r }),
+                                            (ea || ec) &&
+                                                (0, n.jsx)(h.Button, {
+                                                    variant: "primary",
+                                                    size: "md",
+                                                    text: V.intl.string(
+                                                        ea && "gamescope" !== et.nativeSourceType
+                                                            ? V.t.FiBjwU
+                                                            : W.default["5AyH/p"],
                                                     ),
-                                                    hideOptionsButton: !es,
-                                                }),
-                                            ea &&
-                                                !ed &&
-                                                (0, r.jsx)(T.Z, {
-                                                    mainCTADisabled: null == en.selectedSource,
-                                                    mainCTAOnClick: () => {
-                                                        null != en.selectedChannel
-                                                            ? ep(en.selectedChannel)
-                                                            : null != en.selectedSource && eh(en.selectedSource);
+                                                    onClick: () => {
+                                                        ea
+                                                            ? "gamescope" === et.nativeSourceType
+                                                                ? ep()
+                                                                : ((0, Z.t)(), (0, Z.T)(et.nativeSourceType))
+                                                            : null != et.selectedChannel
+                                                              ? eh(et.selectedChannel)
+                                                              : null != et.selectedSource && ef(et.selectedSource);
                                                     },
-                                                    align: "right",
-                                                    hideOptionsButton: !es,
-                                                    ctaText: z.intl.string(V.default["5AyH/p"]),
+                                                    disabled:
+                                                        (ea && !es && "" === et.nativeSourceType) ||
+                                                        (ec && null == et.selectedSource),
                                                 }),
-                                            (!es || !(ea || ed)) && (0, r.jsx)(D.Z, { align: "right" }),
+                                            (0, n.jsx)(U.Z, { align: "right" }),
                                         ],
                                     }),
                                 ],
                             }),
-                            es &&
-                                (0, r.jsx)("div", {
-                                    className: Y.upsellRibbonContainer,
-                                    children: (0, r.jsx)(H.Z, { onClose: n }),
+                            ei &&
+                                (0, n.jsx)("div", {
+                                    className: z.upsellRibbonContainer,
+                                    children: (0, n.jsx)(D.Z, { onClose: r }),
                                 }),
                         ],
                     }),
