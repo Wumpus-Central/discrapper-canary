eed34b2e3d0fd618dfae822411a35cb79e7f8f6d
DiscrapperManager <admin@minerpl.xyz>
2026-01-06 20:11:13 +0100

    canaryRelease: 483198
    Main scripts: 3
    Lazy Loaded Scripts: 1811
    Chunks:
     * Main Files: 15900
     * Scripts: 9392
    Strings: 23523
    Routes: 788
    Experiments Count: 342
    Apex Experiments Count: 144

--- chunks/623624.js
+++ chunks/623624.js
@@ -1,47 +1,44 @@
-n.d(t, { f: () => g });
+n.d(t, { f: () => m });
 var r = n(54381);
 n(473749);
-var i = n(512722),
-    a = n.n(i),
-    o = n(442837),
-    s = n(100527),
-    l = n(906732),
-    c = n(84615),
-    u = n(70394),
-    d = n(430824),
-    f = n(594174),
-    p = n(443002),
-    _ = n(564394);
-let m = "BoostedGuildPerksModalConnected",
-    h = (e) => {
-        let { guildId: t, close: n, location: i } = e,
-            c = (0, o.e7)([f.default], () => f.default.getCurrentUser());
-        a()(null != c, "PremiumGuildPerksModalConnected: currentUser cannot be undefined");
-        let m = (0, o.e7)([d.Z], () => d.Z.getGuild(t), [t]),
-            { analyticsLocations: h } = (0, l.ZP)(s.Z.BOOSTED_GUILD_PERKS_MODAL),
-            g = (0, u.$)("BoostedGuildPerksModal");
-        if (null == m) return null;
-        let E = g ? _.Z : p.Z;
-        return (0, r.jsx)(l.Gt, {
-            value: h,
-            children: (0, r.jsx)(E, {
-                analyticsLocation: i,
+var i = n(442837),
+    a = n(100527),
+    o = n(906732),
+    s = n(84615),
+    l = n(70394),
+    c = n(430824),
+    u = n(594174),
+    d = n(443002),
+    f = n(218319);
+let p = "BoostedGuildPerksModalConnected",
+    _ = (e) => {
+        let { guildId: t, close: n, location: s } = e,
+            p = (0, i.e7)([u.default], () => u.default.getCurrentUser()),
+            _ = (0, i.e7)([c.Z], () => c.Z.getGuild(t), [t]),
+            { analyticsLocations: m } = (0, o.ZP)(a.Z.BOOSTED_GUILD_PERKS_MODAL),
+            h = (0, l.$)("BoostedGuildPerksModal");
+        if (null == p || null == _) return null;
+        let g = h ? f.Z : d.Z;
+        return (0, r.jsx)(o.Gt, {
+            value: m,
+            children: (0, r.jsx)(g, {
+                analyticsLocation: s,
                 onClose: n,
-                guild: m,
+                guild: _,
             }),
         });
     };
-function g(e) {
+function m(e) {
     let { guildId: t, location: n } = e;
-    (0, c.q4)(
+    (0, s.q4)(
         (e) => {
             let { closeLayer: i } = e;
-            return (0, r.jsx)(h, {
+            return (0, r.jsx)(_, {
                 close: i,
                 guildId: t,
                 location: n,
             });
         },
-        { layerKey: m },
+        { layerKey: p },
     );
 }
