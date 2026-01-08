b9f808119481778bbed45f4d775ed2dc0ea5c7e1
DiscrapperManager <admin@minerpl.xyz>
2026-01-08 03:19:49 +0100

    canaryRelease: 483833
    Main scripts: 3
    Lazy Loaded Scripts: 1800
    Chunks:
     * Main Files: 15918
     * Scripts: 9350
    Strings: 23519
    Routes: 789
    Experiments Count: 337
    Apex Experiments Count: 143

--- chunks/953655.js
+++ chunks/953655.js
@@ -1,4 +1,4 @@
-n.d(t, { Z: () => m }), n(388685);
+n.d(t, { Z: () => b }), n(388685);
 var r = n(54381);
 n(473749);
 var l = n(120356),
@@ -6,136 +6,98 @@ var l = n(120356),
     s = n(87290),
     o = n(381585),
     i = n(884697),
-    c = n(953528),
-    u = n(788822),
-    d = n(388032),
-    f = n(222194);
-let g = (e, t, n) =>
-        (0, i.L9)(e) ? d.intl.string(d.t["h/uBCR"]) : n() && null != t && "" !== t ? d.intl.string(d.t.soka7y) : void 0,
-    b = (e) => {
-        let { handleTransition: t, featuredBlockRecord: n } = e,
-            l = (0, c.QH)("collectibles_featured_block");
+    c = n(788822),
+    u = n(388032),
+    d = n(222194);
+let f = (e) => {
+        let { handleTransition: t, featuredBlockRecord: n } = e;
         return (0, r.jsx)("div", {
-            className: a()(f.col2, f.centeredSection),
+            className: a()(d.col2, d.centeredSection),
             children:
                 null == n
                     ? void 0
-                    : n.subblocks.map((e, n) => {
-                          if (e.type === s.O.CATEGORY) {
-                              let a = g(e.unpublishedAt, e.categoryStoreListingId, () =>
-                                  ((e) => {
-                                      switch (l) {
-                                          case c.pj.NO_BADGE:
-                                              return !1;
-                                          case c.pj.FIRST_BLOCK_ONLY:
-                                              return 0 === e;
-                                          case c.pj.RIGHT_BLOCK_ONLY:
-                                              return 1 === e;
-                                          case c.pj.BOTH_BLOCKS:
-                                              return !0;
-                                          default:
-                                              return !1;
-                                      }
-                                  })(n),
-                              );
-                              return (0, r.jsx)(
-                                  o.k0,
-                                  {
-                                      newValue: {
-                                          categoryPosition: 1,
-                                          pageCategory: e.name,
-                                          pageSection: "featured_block",
-                                          tilePosition: n,
-                                      },
-                                      children: (0, r.jsx)(
-                                          u.q,
-                                          {
-                                              subblock: e,
-                                              badgeText: a,
-                                              handleTransition: t,
-                                          },
-                                          e.categoryStoreListingId,
-                                      ),
-                                  },
-                                  e.categoryStoreListingId,
-                              );
-                          }
-                          return null;
-                      }),
+                    : n.subblocks.map((e, n) =>
+                          e.type === s.O.CATEGORY
+                              ? (0, r.jsx)(
+                                    o.k0,
+                                    {
+                                        newValue: {
+                                            categoryPosition: 1,
+                                            pageCategory: e.name,
+                                            pageSection: "featured_block",
+                                            tilePosition: n,
+                                        },
+                                        children: (0, r.jsx)(
+                                            c.q,
+                                            {
+                                                subblock: e,
+                                                badgeText: (0, i.L9)(e.unpublishedAt)
+                                                    ? u.intl.string(u.t["h/uBCR"])
+                                                    : void 0,
+                                                handleTransition: t,
+                                            },
+                                            e.categoryStoreListingId,
+                                        ),
+                                    },
+                                    e.categoryStoreListingId,
+                                )
+                              : null,
+                      ),
         });
     },
-    p = (e) => {
-        let { handleTransition: t, categories: n } = e,
-            l = (0, c.QH)("collectibles_featured_block");
+    g = (e) => {
+        let { handleTransition: t, categories: n } = e;
         if (null == n || n.length < 2) return null;
-        let [s, i] = n,
-            d = g(null == s ? void 0 : s.unpublishedAt, null == s ? void 0 : s.storeListingId, () => {
-                switch (l) {
-                    case c.pj.FIRST_BLOCK_ONLY:
-                    case c.pj.BOTH_BLOCKS:
-                        return !0;
-                    default:
-                        return !1;
-                }
-            }),
-            b = g(null == i ? void 0 : i.unpublishedAt, null == i ? void 0 : i.storeListingId, () => {
-                switch (l) {
-                    case c.pj.RIGHT_BLOCK_ONLY:
-                    case c.pj.BOTH_BLOCKS:
-                        return !0;
-                    default:
-                        return !1;
-                }
-            });
+        let [l, s] = n;
         return (0, r.jsx)("div", {
-            className: a()(f.col2, f.centeredSection),
+            className: a()(d.col2, d.centeredSection),
             children: (0, r.jsxs)(o.k0, {
                 newValue: {
                     categoryPosition: 1,
-                    pageCategory: null != s ? s.name : null == i ? void 0 : i.name,
+                    pageCategory: null != l ? l.name : null == s ? void 0 : s.name,
                     pageSection: "featured_block",
-                    tilePosition: +(null == s),
+                    tilePosition: +(null == l),
                 },
                 children: [
-                    null != s &&
-                        (0, r.jsx)(u.q, {
-                            category: s,
-                            badgeText: d,
+                    null != l &&
+                        (0, r.jsx)(c.q, {
+                            category: l,
+                            badgeText: (0, i.L9)(l.unpublishedAt) ? u.intl.string(u.t["h/uBCR"]) : void 0,
                             handleTransition: t,
                         }),
-                    null != i &&
-                        (0, r.jsx)(u.q, {
-                            category: i,
-                            badgeText: b,
+                    null != s &&
+                        (0, r.jsx)(c.q, {
+                            category: s,
+                            badgeText: (0, i.L9)(s.unpublishedAt) ? u.intl.string(u.t["h/uBCR"]) : void 0,
                             handleTransition: t,
                         }),
                 ],
             }),
         });
     },
-    m = (e) => {
+    b = (e) => {
         let { isLoading: t, handleTransition: n, categories: l, featuredBlockRecord: s } = e;
         return t
             ? (0, r.jsxs)("div", {
-                  className: a()(f.col2, f.centeredSection),
+                  className: a()(d.col2, d.centeredSection),
                   children: [
                       (0, r.jsx)("div", {
-                          className: a()(f.featuredBlockSkeleton, f.featuredBlock),
-                          children: (0, r.jsx)("div", { className: f.featuredBlockSkeletonLogo }),
+                          className: a()(d.featuredBlockSkeleton, d.featuredBlock),
+                          children: (0, r.jsx)("div", { className: d.featuredBlockSkeletonLogo }),
                       }),
                       (0, r.jsx)("div", {
-                          className: a()(f.featuredBlockSkeleton, f.featuredBlock),
-                          children: (0, r.jsx)("div", { className: f.featuredBlockSkeletonLogo }),
+                          className: a()(d.featuredBlockSkeleton, d.featuredBlock),
+                          children: (0, r.jsx)("div", { className: d.featuredBlockSkeletonLogo }),
                       }),
                   ],
               })
             : null != s
-              ? (0, r.jsx)(b, {
+              ? (0, r.jsx)(f, {
                     featuredBlockRecord: s,
                     handleTransition: n,
                     isLoading: !1,
                 })
-              : (0, r.jsx)(p, {
+              : (0, r.jsx)(g, {
                     categories: l,
                     handleTransition: n,
                     isLoading: !1,
