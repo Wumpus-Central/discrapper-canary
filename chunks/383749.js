847954f802cb4c5a7bc4c89368c65ea3d746b786
DiscrapperManager <admin@minerpl.xyz>
2026-01-08 19:13:22 +0100

    canaryRelease: 483933
    Main scripts: 3
    Lazy Loaded Scripts: 1801
    Chunks:
     * Main Files: 15913
     * Scripts: 9357
    Strings: 23521
    Routes: 789
    Experiments Count: 336
    Apex Experiments Count: 144

--- chunks/383749.js
+++ chunks/383749.js
@@ -1,8 +1,10 @@
 e.exports = {
-    modal: "be5f0438771ae94c-modal",
     content: "be5f0438771ae94c-content",
     closeButton: "be5f0438771ae94c-closeButton",
     artwork: "be5f0438771ae94c-artwork",
+    titleContainer: "be5f0438771ae94c-titleContainer",
+    title: "be5f0438771ae94c-title",
+    subtitle: "be5f0438771ae94c-subtitle",
     features: "be5f0438771ae94c-features",
     feature: "be5f0438771ae94c-feature",
     featureCopy: "be5f0438771ae94c-featureCopy",
