1145127e68f70f3600fdef69dcc79369b7593d37
DiscrapperManager <admin@minerpl.xyz>
2026-01-09 13:58:06 +0100

    canaryRelease: 484227
    Main scripts: 3
    Lazy Loaded Scripts: 1799
    Chunks:
     * Main Files: 15922
     * Scripts: 9351
    Strings: 23512
    Routes: 790
    Experiments Count: 280
    Apex Experiments Count: 144

--- chunks/13439.js
+++ chunks/13439.js
@@ -1,41 +1,22 @@
-n.d(t, { eg: () => a });
-var r = n(818083),
-    i = n(987338);
-let l = (0, r.B)({
+n.d(t, { eg: () => i });
+let r = (0, n(427164).le)({
+    name: "2025-11-guild-powerups-mobile",
     kind: "user",
-    id: "2025-11-guild-powerups-mobile",
-    commonTriggerPoint: i.$P.CONNECTION_OPEN,
-    label: "Guild Powerups Mobile",
     defaultConfig: {
         enabled: !1,
         notificationsEnabled: !1,
     },
-    treatments: [
-        {
-            id: 1,
-            label: "Enable Guild Powerups Mobile",
-            config: {
-                enabled: !0,
-                notificationsEnabled: !1,
-            },
+    variations: {
+        1: {
+            enabled: !0,
+            notificationsEnabled: !1,
         },
-        {
-            id: 2,
-            label: "Enable Guild Powerups Mobile with Notifications",
-            config: {
-                enabled: !0,
-                notificationsEnabled: !0,
-            },
+        2: {
+            enabled: !0,
+            notificationsEnabled: !0,
         },
-    ],
+    },
 });
-function a(e) {
-    let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
-    return l.useExperiment(
-        { location: e },
-        {
-            autoTrackExposure: !1,
-            disable: t,
-        },
-    ).enabled;
+function i(e) {
+    return r.useConfig({ location: e }).enabled;
 }
