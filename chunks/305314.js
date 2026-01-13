d7f2bfbf50f58709b412d9a988327dd4e71d7cbd
DiscrapperManager <admin@minerpl.xyz>
2026-01-13 17:08:34 +0100

    canaryRelease: 485135
    Main scripts: 3
    Lazy Loaded Scripts: 1799
    Chunks:
     * Main Files: 15938
     * Scripts: 9342
    Strings: 23533
    Routes: 792
    Experiments Count: 275
    Apex Experiments Count: 141

--- chunks/305314.js
+++ chunks/305314.js
@@ -1,35 +1,23 @@
-n.d(e, { L: () => _ });
-var i = n(54381),
-    l = n(481060),
-    s = n(714338),
-    u = n(857595),
-    r = n(607070),
-    a = n(509613),
-    o = n(358085),
-    T = n(313789),
-    S = n(981631),
-    c = n(388032);
-let E = (0, a.UO)(T.n.APPEARANCE_ZOOM, {
-        useTitle: () => c.intl.string(c.t.i19n5L),
-        useSubtitle: () => c.intl.string(c.t.ip0uSf),
-        markers: S.yqN.ZOOM_SCALES,
-        stickToMarkers: !0,
-        minValue: S.yqN.ZOOM_SCALES["0"],
-        maxValue: S.yqN.ZOOM_SCALES[S.yqN.ZOOM_SCALES.length - 1],
-        getInitialValue: () => r.Z.zoom,
-        onValueRender: (t) => "".concat(t.toFixed(0), "%"),
-        asValueChanges: (t) => (0, u.cq)(t),
-    }),
-    d = (0, a.ON)(T.n.APPEARANCE_ZOOM_HELPER_TEXT, {
-        useSearchTerms: () => [c.intl.string(c.t.i19n5L), c.intl.string(c.t.ip0uSf)],
-        Component: () =>
-            (0, i.jsx)(l.Text, {
-                variant: "text-xs/normal",
-                color: "text-muted",
-                children: c.intl.format(c.t.OMkSUo, { modKey: s.Z.modKey }),
-            }),
-    }),
-    _ = (0, a.$l)(T.n.APPEARANCE_ZOOM_LIST, {
-        usePredicate: () => o.isPlatformEmbedded,
-        buildLayout: () => [E, d],
-    });
+n.d(e, { L: () => S });
+var i = n(714338),
+    l = n(857595),
+    s = n(607070),
+    u = n(509613),
+    r = n(358085),
+    a = n(313789),
+    o = n(981631),
+    T = n(388032);
+let S = (0, u.UO)(a.n.APPEARANCE_ZOOM, {
+    usePredicate: () => r.isPlatformEmbedded,
+    useTitle: () => T.intl.string(T.t.i19n5L),
+    useSubtitle: () => T.intl.string(T.t.ip0uSf),
+    markers: o.yqN.ZOOM_SCALES,
+    stickToMarkers: !0,
+    minValue: o.yqN.ZOOM_SCALES["0"],
+    maxValue: o.yqN.ZOOM_SCALES[o.yqN.ZOOM_SCALES.length - 1],
+    getInitialValue: () => s.Z.zoom,
+    onValueRender: (t) => "".concat(t.toFixed(0), "%"),
+    asValueChanges: (t) => (0, l.cq)(t),
+    useSearchTerms: () => [T.intl.string(T.t.ip0uSf)],
+    useHintText: () => T.intl.format(T.t.OMkSUo, { modKey: i.Z.modKey }),
+});
