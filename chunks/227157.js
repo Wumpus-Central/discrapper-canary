164702ec5aef31caab204dfcfd82579c20afe81b
DiscrapperManager <admin@minerpl.xyz>
2026-01-13 20:56:53 +0100

    canaryRelease: 485292
    Main scripts: 3
    Lazy Loaded Scripts: 1800
    Chunks:
     * Main Files: 15964
     * Scripts: 9344
    Strings: 23539
    Routes: 792
    Experiments Count: 274
    Apex Experiments Count: 140

--- chunks/227157.js
+++ chunks/227157.js
@@ -9,7 +9,10 @@ function c(e) {
     let { searchQuery: t = "" } = e,
         n = (0, l.ZP)((e) => e.recentlyShown),
         [c, d] = (0, s.Z)(),
-        u = c.filter((e) => e.toLowerCase().includes(t.toLowerCase())).filter((e) => !n.includes(r.z[e]));
+        u = c
+            .filter((e) => e.toLowerCase().includes(t.toLowerCase()))
+            .filter((e) => !n.includes(r.z[e]))
+            .slice(0, 3);
     return 0 === u.length
         ? null
         : (0, a.jsx)(i.C3N, {
