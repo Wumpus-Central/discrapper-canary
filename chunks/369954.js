d74a69f283c55454182791ce31aefc02bbe436b3
DiscrapperManager <admin@minerpl.xyz>
2026-01-14 20:03:01 +0100

    canaryRelease: 485727
    Main scripts: 3
    Lazy Loaded Scripts: 1798
    Chunks:
     * Main Files: 15948
     * Scripts: 9332
    Strings: 23483
    Routes: 787
    Experiments Count: 272
    Apex Experiments Count: 140

--- chunks/369954.js
+++ chunks/369954.js
@@ -1,23 +1,24 @@
-n.d(t, { J: () => I });
+n.d(t, { J: () => T });
 var r = n(54381),
     i = n(473749),
     a = n(793030),
     o = n(481060),
     s = n(493683),
-    l = n(230711),
-    c = n(511010),
-    u = n(981312),
+    l = n(511010),
+    c = n(981312),
+    u = n(313789),
     d = n(342386),
-    f = n(260722),
-    p = n(841409),
-    _ = n(780985),
-    m = n(631885),
-    h = n(292352),
-    g = n(981631),
-    E = n(517319),
-    b = n(388032),
-    y = n(837842);
-let O = (e) => {
+    f = n(518596),
+    p = n(260722),
+    _ = n(841409),
+    m = n(780985),
+    h = n(631885),
+    g = n(292352),
+    E = n(981631),
+    b = n(517319),
+    y = n(388032),
+    O = n(837842);
+let v = (e) => {
         let { title: t, buttonText: n, onButtonPress: i } = e;
         return (0, r.jsxs)(a.Kqy, {
             padding: {
@@ -42,13 +43,13 @@ let O = (e) => {
             ],
         });
     },
-    v = () => {
-        let e = (0, m.ZM)(),
+    S = () => {
+        let e = (0, h.ZM)(),
             t = () => {
                 (0, d.default)(), s.Z.openPrivateChannel({ recipientIds: e });
             },
             n = () => {
-                l.Z.setSection(g.oAB.CONTENT_AND_SOCIAL);
+                (0, f.openUserSettings)(u.n.CONTENT_AND_SOCIAL_PANEL, { section: E.oAB.CONTENT_AND_SOCIAL });
             };
         return (0, r.jsxs)(a.Kqy, {
             justify: "space-between",
@@ -62,12 +63,12 @@ let O = (e) => {
                     children: [
                         (0, r.jsx)(a.xvT, {
                             variant: "text-sm/semibold",
-                            children: b.intl.string(E.default.ahKIJO),
+                            children: y.intl.string(b.default.ahKIJO),
                         }),
                         (0, r.jsx)(a.xvT, {
                             variant: "text-md/medium",
                             color: "text-muted",
-                            children: b.intl.format(E.default.X9rW0j, {
+                            children: y.intl.format(b.default.X9rW0j, {
                                 openSettingsHook: (e, t) =>
                                     (0, r.jsx)(
                                         o.eee,
@@ -82,7 +83,7 @@ let O = (e) => {
                     ],
                 }),
                 (0, r.jsx)(o.Button, {
-                    text: b.intl.formatToPlainString(E.default.ggcjPR, { count: e.length }),
+                    text: y.intl.formatToPlainString(b.default.ggcjPR, { count: e.length }),
                     onClick: t,
                     variant: "secondary",
                     size: "sm",
@@ -90,12 +91,12 @@ let O = (e) => {
             ],
         });
     },
-    S = () => {
-        let e = (0, _.cz)(),
-            t = (0, _.$r)(),
-            { handleTabChange: n } = (0, p.Z)();
+    I = () => {
+        let e = (0, m.cz)(),
+            t = (0, m.$r)(),
+            { handleTabChange: n } = (0, _.Z)();
         i.useEffect(() => {
-            (null == e ? void 0 : e.id) != null && t && f.ZP.fetchTeenSettingsAndConsents(null == e ? void 0 : e.id);
+            (null == e ? void 0 : e.id) != null && t && p.ZP.fetchTeenSettingsAndConsents(null == e ? void 0 : e.id);
         }, [null == e ? void 0 : e.id, t]);
         let o = (e) => {
             n(e);
@@ -108,35 +109,35 @@ let O = (e) => {
                     children: [
                         (0, r.jsx)(a.xvT, {
                             variant: "text-sm/semibold",
-                            children: b.intl.string(E.default.ahKIJO),
+                            children: y.intl.string(b.default.ahKIJO),
                         }),
                         (0, r.jsx)(a.xvT, {
                             variant: "text-md/medium",
                             color: "text-muted",
-                            children: b.intl.string(E.default.Sv236e),
+                            children: y.intl.string(b.default.Sv236e),
                         }),
                     ],
                 }),
-                (0, r.jsx)(O, {
-                    title: b.intl.string(b.t["+o1pDZ"]),
-                    buttonText: b.intl.string(b.t.bt75uw),
-                    onButtonPress: () => o(h.dG.CONTENT_AND_SOCIAL),
+                (0, r.jsx)(v, {
+                    title: y.intl.string(y.t["+o1pDZ"]),
+                    buttonText: y.intl.string(y.t.bt75uw),
+                    onButtonPress: () => o(g.dG.CONTENT_AND_SOCIAL),
                 }),
-                (0, r.jsx)(c.Z, { className: y.divider }),
-                (0, r.jsx)(O, {
-                    title: b.intl.string(b.t.OAuOHD),
-                    buttonText: b.intl.string(b.t.bt75uw),
-                    onButtonPress: () => o(h.dG.DATA_AND_PRIVACY),
+                (0, r.jsx)(l.Z, { className: O.divider }),
+                (0, r.jsx)(v, {
+                    title: y.intl.string(y.t.OAuOHD),
+                    buttonText: y.intl.string(y.t.bt75uw),
+                    onButtonPress: () => o(g.dG.DATA_AND_PRIVACY),
                 }),
             ],
         });
     },
-    I = () => {
-        let e = (0, u.U)();
-        return 0 === (0, m.ZM)().length
+    T = () => {
+        let e = (0, c.U)();
+        return 0 === (0, h.ZM)().length
             ? null
             : (0, r.jsx)("div", {
-                  className: y.container,
-                  children: e ? (0, r.jsx)(v, {}) : (0, r.jsx)(S, {}),
+                  className: O.container,
+                  children: e ? (0, r.jsx)(S, {}) : (0, r.jsx)(I, {}),
               });
     };
