6c874fe1099b3f8f361933c55b94b24ffc40ee2a
DiscrapperManager <admin@minerpl.xyz>
2026-01-15 00:33:47 +0100

    canaryRelease: 485926
    Main scripts: 3
    Lazy Loaded Scripts: 1799
    Chunks:
     * Main Files: 15949
     * Scripts: 9339
    Strings: 23487
    Routes: 787
    Experiments Count: 272
    Apex Experiments Count: 139

--- chunks/579832.js
+++ chunks/579832.js
@@ -1,24 +1,28 @@
-n.d(t, { Z: () => P });
+n.d(t, { Z: () => x });
 var r = n(54381),
-    i = n(793030),
-    a = n(442837),
-    o = n(481060),
-    s = n(568836),
-    l = n(834129),
-    c = n(594174),
-    u = n(78839),
-    d = n(626135),
-    f = n(960048),
-    p = n(51144),
-    _ = n(703995),
-    m = n(260720),
-    h = n(282793),
-    g = n(981631),
-    E = n(353149),
-    b = n(388032),
-    y = n(887547),
-    O = n(617842);
-function v(e, t, n) {
+    i = n(392711),
+    a = n(793030),
+    o = n(442837),
+    s = n(481060),
+    l = n(568836),
+    c = n(834129),
+    u = n(518727),
+    d = n(594174),
+    f = n(78839),
+    p = n(626135),
+    _ = n(585483),
+    m = n(960048),
+    h = n(51144),
+    g = n(453227),
+    E = n(703995),
+    b = n(260720),
+    y = n(282793),
+    O = n(981631),
+    v = n(353149),
+    S = n(388032),
+    I = n(887547),
+    T = n(617842);
+function C(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -31,7 +35,7 @@ function v(e, t, n) {
         e
     );
 }
-function S(e) {
+function A(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -42,12 +46,12 @@ function S(e) {
                 }),
             )),
             r.forEach(function (t) {
-                v(e, t, n[t]);
+                C(e, t, n[t]);
             });
     }
     return e;
 }
-function I(e, t) {
+function N(e, t) {
     var n = Object.keys(e);
     if (Object.getOwnPropertySymbols) {
         var r = Object.getOwnPropertySymbols(e);
@@ -59,58 +63,85 @@ function I(e, t) {
     }
     return n;
 }
-function T(e, t) {
+function P(e, t) {
     return (
         (t = null != t ? t : {}),
         Object.getOwnPropertyDescriptors
             ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
-            : I(Object(t)).forEach(function (n) {
+            : N(Object(t)).forEach(function (n) {
                   Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
               }),
         e
     );
 }
-let C = (e) => {
-        let { disabled: t, invite: a, isExistingSub: s } = e,
-            l = () => {
+let R = (e) => {
+        let { disabled: t, invite: o, isExistingSub: l } = e,
+            c = () => {
+                if (null == o) return;
+                let e = o.subscription,
+                    t = o.id,
+                    a = d.default.getUser(o.primary_user);
                 if (null == a) return;
-                let e = a.subscription,
-                    t = a.id,
-                    i = c.default.getUser(a.primary_user);
-                if (null == i) return;
-                let l = (0, p.XM)(i);
-                d.default.track(g.rMx.PREMIUM_GROUP_INVITE_EMBED_ACCEPT_CLICKED, {
+                let c = (0, h.XM)(a);
+                p.default.track(O.rMx.PREMIUM_GROUP_INVITE_EMBED_ACCEPT_CLICKED, {
                     invite_id: t,
                     subscription_id: e,
-                }),
-                    (0, o.ZDy)(async () => {
+                });
+                let f = (0, i.uniqueId)("premium-group-accept-invite-modal"),
+                    m = !1;
+                (0, s.ZDy)(
+                    async () => {
                         let { default: i } = await n.e("26450").then(n.bind(n, 382927));
                         return (n) =>
                             (0, r.jsx)(
                                 i,
-                                T(S({}, n), {
+                                P(A({}, n), {
                                     premiumGroupSubscriptionId: e,
                                     premiumGroupInviteId: t,
-                                    premiumGroupPrimaryName: l,
-                                    isExistingSub: s,
+                                    premiumGroupPrimaryName: c,
+                                    isExistingSub: l,
+                                    onClose: async () => {
+                                        m ||
+                                            ((m = !0),
+                                            _.S.dispatch(O.CkL.WOW_MOMENT_CONFIRMATION_MODAL_CLOSED),
+                                            g.Z.isDisplayingWowMomentConfirmation && g.Z.isAnimated
+                                                ? setTimeout(() => {
+                                                      n.onClose();
+                                                  }, u.P)
+                                                : await n.onClose());
+                                    },
                                 }),
                             );
-                    });
+                    },
+                    {
+                        onCloseRequest: () => {
+                            m ||
+                                ((m = !0),
+                                _.S.dispatch(O.CkL.WOW_MOMENT_CONFIRMATION_MODAL_CLOSED),
+                                g.Z.isDisplayingWowMomentConfirmation && g.Z.isAnimated
+                                    ? setTimeout(() => {
+                                          (0, s.Mr3)(f);
+                                      }, u.P)
+                                    : (0, s.Mr3)(f));
+                        },
+                        modalKey: f,
+                    },
+                );
             };
-        return (0, r.jsx)(o.Button, {
+        return (0, r.jsx)(s.Button, {
             variant: "expressive",
             size: "md",
-            text: b.intl.string(E.default.rjuKse),
-            icon: i.SrA,
-            onClick: l,
+            text: S.intl.string(v.default.rjuKse),
+            icon: a.SrA,
+            onClick: c,
             disabled: t,
         });
     },
-    A = (e) => {
+    w = (e) => {
         let { disabled: t, invite: i, channel: a } = e,
-            s = () => {
+            o = () => {
                 if (null == i || !a.isDM())
-                    return void f.Z.captureMessage("CancelInviteButton onClick: unexpected state", {
+                    return void m.Z.captureMessage("CancelInviteButton onClick: unexpected state", {
                         extra: {
                             inviteIsNull: null == i,
                             channelIsDM: a.isDM(),
@@ -121,19 +152,19 @@ let C = (e) => {
                         },
                     });
                 let e = a.getRecipientId(),
-                    t = c.default.getUser(e);
+                    t = d.default.getUser(e);
                 null != t &&
-                    (d.default.track(g.rMx.PREMIUM_GROUP_INVITE_EMBED_CANCEL_CLICKED, {
+                    (p.default.track(O.rMx.PREMIUM_GROUP_INVITE_EMBED_CANCEL_CLICKED, {
                         invite_id: i.id,
                         subscription_id: i.subscription,
                         invited_user_id: e,
                     }),
-                    (0, o.ZDy)(async () => {
+                    (0, s.ZDy)(async () => {
                         let { default: e } = await n.e("72114").then(n.bind(n, 157312));
                         return (n) =>
                             (0, r.jsx)(
                                 e,
-                                T(S({}, n), {
+                                P(A({}, n), {
                                     subscriptionId: i.subscription,
                                     invitedUser: t,
                                     subscriptionGroupMemberId: i.id,
@@ -141,57 +172,57 @@ let C = (e) => {
                             );
                     }));
             };
-        return (0, r.jsx)(o.Button, {
+        return (0, r.jsx)(s.Button, {
             variant: "secondary",
             size: "md",
-            text: b.intl.string(E.default["eYHh+z"]),
-            onClick: s,
+            text: S.intl.string(v.default["eYHh+z"]),
+            onClick: o,
             disabled: t,
         });
     },
-    N = (e) => {
-        let { message: t, header: n, body: a, compact: s, actionButton: c } = e;
-        return (0, r.jsxs)(l.Z, {
-            className: y.systemMessageContainer,
-            compact: s,
-            iconNode: (0, r.jsx)(i.SrA, {
+    D = (e) => {
+        let { message: t, header: n, body: i, compact: o, actionButton: l } = e;
+        return (0, r.jsxs)(c.Z, {
+            className: I.systemMessageContainer,
+            compact: o,
+            iconNode: (0, r.jsx)(a.SrA, {
                 size: "md",
                 color: "currentColor",
             }),
             children: [
-                (0, r.jsx)(o.Text, {
+                (0, r.jsx)(s.Text, {
                     variant: "text-md/medium",
                     color: "text-strong",
                     children: t,
                 }),
-                (0, r.jsxs)(o.Kqy, {
+                (0, r.jsxs)(s.Kqy, {
                     direction: "horizontal",
-                    className: y.embedContainer,
+                    className: I.embedContainer,
                     children: [
                         (0, r.jsx)("img", {
-                            src: O,
+                            src: T,
                             alt: "",
-                            className: y.image,
+                            className: I.image,
                         }),
                         (0, r.jsxs)("div", {
-                            className: y.content,
+                            className: I.content,
                             children: [
-                                (0, r.jsx)(o.Cts, {
+                                (0, r.jsx)(s.Cts, {
                                     type: "beta",
                                     variant: "expressive",
                                 }),
                                 (0, r.jsx)("h2", {
-                                    className: y.title,
+                                    className: I.title,
                                     children: n,
                                 }),
-                                (0, r.jsx)(o.Text, {
+                                (0, r.jsx)(s.Text, {
                                     variant: "text-md/medium",
                                     color: "text-subtle",
-                                    children: a,
+                                    children: i,
                                 }),
                                 (0, r.jsx)("div", {
-                                    className: y.buttonContainer,
-                                    children: c,
+                                    className: I.buttonContainer,
+                                    children: l,
                                 }),
                             ],
                         }),
@@ -200,52 +231,52 @@ let C = (e) => {
             ],
         });
     },
-    P = (e) => {
+    x = (e) => {
         let { message: t, channel: n, compact: i } = e,
-            o = t.premiumGroupInviteId,
+            a = t.premiumGroupInviteId,
             {
-                inviteState: l,
-                isFetching: d,
-                invite: f,
+                inviteState: s,
+                isFetching: c,
+                invite: u,
                 currentUser: p,
-                premiumSubscription: g,
-            } = (0, a.cj)([_.Z, c.default, u.Z], () => {
+                premiumSubscription: _,
+            } = (0, o.cj)([E.Z, d.default, f.Z], () => {
                 var e;
                 return {
-                    inviteState: null != o ? _.Z.getInviteState(o) : h.bZ.NOT_FOUND,
-                    isFetching: null != o && _.Z.isFetching(o),
-                    invite: null != o ? (null == (e = _.Z.getInvite(o)) ? void 0 : e.invite) : null,
-                    currentUser: c.default.getCurrentUser(),
-                    premiumSubscription: u.Z.getPremiumSubscription(),
+                    inviteState: null != a ? E.Z.getInviteState(a) : y.bZ.NOT_FOUND,
+                    isFetching: null != a && E.Z.isFetching(a),
+                    invite: null != a ? (null == (e = E.Z.getInvite(a)) ? void 0 : e.invite) : null,
+                    currentUser: d.default.getCurrentUser(),
+                    premiumSubscription: f.Z.getPremiumSubscription(),
                 };
-            }, [o]);
+            }, [a]);
         if (null == p) return null;
-        let E = t.author,
-            b = p.id === E.id;
-        if (d || l === h.bZ.FETCHING || l === h.bZ.UNKNOWN) return (0, r.jsx)(s.OR, { isHorizontal: !0 });
-        let y = (0, m.xf)({
-            sender: E,
+        let m = t.author,
+            h = p.id === m.id;
+        if (c || s === y.bZ.FETCHING || s === y.bZ.UNKNOWN) return (0, r.jsx)(l.OR, { isHorizontal: !0 });
+        let g = (0, b.xf)({
+            sender: m,
             channel: n,
-            isSender: b,
-            inviteState: l,
+            isSender: h,
+            inviteState: s,
         });
-        if (null == y) return null;
-        let O = l === h.bZ.PENDING,
-            v = b
-                ? (0, r.jsx)(A, {
+        if (null == g) return null;
+        let O = s === y.bZ.PENDING,
+            v = h
+                ? (0, r.jsx)(w, {
                       disabled: !O,
-                      invite: f,
+                      invite: u,
                       channel: n,
                   })
-                : (0, r.jsx)(C, {
+                : (0, r.jsx)(R, {
                       disabled: !O,
-                      invite: f,
-                      isExistingSub: null != g,
+                      invite: u,
+                      isExistingSub: null != _,
                   });
-        return (0, r.jsx)(N, {
-            message: y.message,
-            header: y.header,
-            body: y.body,
+        return (0, r.jsx)(D, {
+            message: g.message,
+            header: g.header,
+            body: g.body,
             compact: i,
             actionButton: v,
         });
