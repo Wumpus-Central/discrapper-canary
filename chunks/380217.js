48f1ea9b3b09821fde656f2eeb846e71dbbe4f38
DiscrapperManager <admin@minerpl.xyz>
2026-01-15 16:46:32 +0100

    canaryRelease: 486081
    Main scripts: 3
    Lazy Loaded Scripts: 1799
    Chunks:
     * Main Files: 15946
     * Scripts: 9337
    Strings: 23490
    Routes: 787
    Experiments Count: 272
    Apex Experiments Count: 139

--- chunks/380217.js
+++ chunks/380217.js
@@ -1,29 +1,28 @@
-n.d(e, { o: () => _ });
+n.d(e, { o: () => d });
 var i = n(657707),
     l = n(509613),
     s = n(313789),
     u = n(526665),
     r = n(443702),
     a = n(87065),
-    o = n(743587),
-    T = n(43384),
-    S = n(726985),
-    c = n(388032);
-let E = (0, l.wf)(s.n.BILLING_PANEL, {
+    o = n(43384),
+    T = n(726985),
+    S = n(388032);
+let c = (0, l.wf)(s.n.BILLING_PANEL, {
         usePredicate: () => (0, u.gN)("BillingPanel"),
-        useTitle: () => c.intl.string(c.t.oeUm2s),
-        buildLayout: () => [a.$, T.L, o.B],
+        useTitle: () => S.intl.string(S.t.oeUm2s),
+        buildLayout: () => [a.$, o.L],
         hideInStreamerMode: !0,
     }),
-    d = (0, l.wf)(s.n.BILLING_PANEL, {
+    E = (0, l.wf)(s.n.BILLING_PANEL, {
         usePredicate: () => !(0, u.gN)("LegacyBillingPanel"),
-        useTitle: () => c.intl.string(c.t.oeUm2s),
+        useTitle: () => S.intl.string(S.t.oeUm2s),
         StronglyDiscouragedCustomComponent: r.ZP,
         buildLayout: () => [],
     }),
-    _ = (0, l.m7)(s.n.BILLING_SIDEBAR_ITEM, {
-        useTitle: () => c.intl.string(c.t.oeUm2s),
+    d = (0, l.m7)(s.n.BILLING_SIDEBAR_ITEM, {
+        useTitle: () => S.intl.string(S.t.oeUm2s),
         icon: i.ciH,
-        getLegacySearchKey: () => ((0, u.Gl)("BillingPanel") ? void 0 : S.s6.BILLING),
-        buildLayout: () => ((0, u.Gl)("BillingPanel") ? [E] : [d]),
+        getLegacySearchKey: () => ((0, u.Gl)("BillingPanel") ? void 0 : T.s6.BILLING),
+        buildLayout: () => ((0, u.Gl)("BillingPanel") ? [c] : [E]),
     });
--- chunks/43384.js
+++ chunks/43384.js
@@ -1,4 +1,4 @@
-n.d(e, { L: () => a });
+n.d(e, { L: () => o });
 var i = n(509613),
     l = n(313789),
     s = n(443702),
@@ -7,7 +7,11 @@ let r = (0, i.ON)(l.n.BILLING_TRANSACTION_HISTORY, {
         Component: s.v0,
         useSearchTerms: () => [u.intl.string(u.t.obLrcK)],
     }),
-    a = (0, i.k4)(l.n.BILLING_TRANSACTION_HISTORY_CATEGORY, {
+    a = (0, i.ON)(l.n.BILLING_REDEMPTION_REDIRECT, {
+        Component: s.B7,
+        useSearchTerms: () => [u.intl.string(u.t.obLrcK)],
+    }),
+    o = (0, i.k4)(l.n.BILLING_TRANSACTION_HISTORY_CATEGORY, {
         useTitle: () => u.intl.string(u.t.obLrcK),
-        buildLayout: () => [r],
+        buildLayout: () => [r, a],
     });
