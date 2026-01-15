28cb1cf8f3d58b14551c348dc57d79b6dfb34bdb
DiscrapperManager <admin@minerpl.xyz>
2026-01-15 22:04:17 +0100

    canaryRelease: 486305
    Main scripts: 3
    Lazy Loaded Scripts: 1802
    Chunks:
     * Main Files: 15955
     * Scripts: 9341
    Strings: 23487
    Routes: 787
    Experiments Count: 271
    Apex Experiments Count: 138

--- chunks/564428.js
+++ chunks/564428.js
@@ -1,9 +1,12 @@
-n.d(e, { a: () => r });
-var i = n(509613),
-    l = n(313789),
-    s = n(273927),
-    u = n(388032);
-let r = (0, i.ON)(l.n.CONTENT_AND_SOCIAL_BLOCKED_USERS, {
-    useSearchTerms: () => [u.intl.string(u.t.PFOUKW)],
-    Component: s.GF,
+n.d(e, { a: () => o });
+var i = n(442837),
+    l = n(509613),
+    s = n(699516),
+    u = n(313789),
+    r = n(273927),
+    a = n(388032);
+let o = (0, l.ON)(u.n.CONTENT_AND_SOCIAL_BLOCKED_USERS, {
+    useSearchTerms: () => [a.intl.string(a.t.PFOUKW)],
+    usePredicate: () => (0, i.e7)([s.Z], () => s.Z.getBlockedIDs().length > 0),
+    Component: r.GF,
 });
--- chunks/738070.js
+++ chunks/738070.js
@@ -1,9 +1,12 @@
-n.d(e, { s: () => r });
-var i = n(509613),
-    l = n(313789),
-    s = n(273927),
-    u = n(388032);
-let r = (0, i.ON)(l.n.CONTENT_AND_SOCIAL_IGNORED_USERS, {
-    useSearchTerms: () => [u.intl.string(u.t["93ZDWE"])],
-    Component: s.yo,
+n.d(e, { s: () => o });
+var i = n(442837),
+    l = n(509613),
+    s = n(699516),
+    u = n(313789),
+    r = n(273927),
+    a = n(388032);
+let o = (0, l.ON)(u.n.CONTENT_AND_SOCIAL_IGNORED_USERS, {
+    useSearchTerms: () => [a.intl.string(a.t["93ZDWE"])],
+    usePredicate: () => (0, i.e7)([s.Z], () => s.Z.getIgnoredIDs().length > 0),
+    Component: r.yo,
 });
