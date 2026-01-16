4bd815bfa0c87a35a3460a6eb73a700ca16404b4
DiscrapperManager <admin@minerpl.xyz>
2026-01-16 19:42:02 +0100

    canaryRelease: 486701
    Main scripts: 3
    Lazy Loaded Scripts: 1802
    Chunks:
     * Main Files: 15966
     * Scripts: 9344
    Strings: 23487
    Routes: 787
    Experiments Count: 271
    Apex Experiments Count: 137

--- chunks/83479.js
+++ chunks/83479.js
@@ -1,53 +1,61 @@
-n.d(t, { G: () => s }), n(539854), n(704826), n(35282);
+n.d(t, { G: () => o }), n(539854), n(388685), n(704826), n(35282);
 var r = n(473749),
     i = n(979554),
-    a = n(215023),
-    l = n(388032);
-let o = function (e) {
-        var t, n, r, a;
-        let o = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
-        if (o)
-            return l.intl.formatToPlainString(l.t["/0Yndu"], {
+    a = n(388032);
+let l = function (e) {
+        var t, n;
+        let r = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
+        if (r)
+            return a.intl.formatToPlainString(a.t["/0Yndu"], {
                 num: null == (t = e.bundledProducts) ? void 0 : t.length,
             });
-        let s = null != (n = e.bundledProducts) ? n : [],
-            c = s.find((e) => e.type === i.Z.AVATAR_DECORATION),
-            u = s.find((e) => e.type === i.Z.PROFILE_EFFECT),
-            d = s.find((e) => e.type === i.Z.NAMEPLATE);
-        if (null != d) {
-            let e = [];
-            null != c && e.push(l.intl.formatToPlainString(l.t.Ntv9Jt, { itemName: c.name })),
-                null != u && e.push(l.intl.formatToPlainString(l.t["3Y8q7a"], { itemName: u.name })),
-                null != d && e.push(l.intl.formatToPlainString(l.t["2keXky"], { itemName: d.name }));
-            let t = e.join(", ").replace(/, ([^,]*)$/, " & $1");
-            return l.intl.formatToPlainString(l.t.Ofrqj6, { joinedItems: t });
+        let l = null != (n = e.bundledProducts) ? n : [],
+            o = [],
+            c = !1;
+        for (let e of l)
+            switch (e.type) {
+                case i.Z.AVATAR_DECORATION:
+                    o.push(a.intl.formatToPlainString(a.t.Ntv9Jt, { itemName: e.name }));
+                    break;
+                case i.Z.PROFILE_EFFECT:
+                    o.push(a.intl.formatToPlainString(a.t["3Y8q7a"], { itemName: e.name }));
+                    break;
+                case i.Z.NAMEPLATE:
+                    o.push(a.intl.formatToPlainString(a.t["2keXky"], { itemName: e.name })), (c = !0);
+            }
+        if (c) {
+            let e = o.join(", ").replace(/, ([^,]*)$/, " & $1");
+            return a.intl.formatToPlainString(a.t.Ofrqj6, { joinedItems: e });
         }
-        return l.intl.formatToPlainString(l.t.BS1Cuy, {
-            decoName: null != (r = null == c ? void 0 : c.name) ? r : "",
-            pfxName: null != (a = null == u ? void 0 : u.name) ? a : "",
-        });
+        let s = o.join(" & ");
+        return a.intl.formatToPlainString(a.t.Ofrqj6, { joinedItems: s });
     },
-    s = function (e) {
+    o = function (e) {
         let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1],
             n = arguments.length > 2 ? arguments[2] : void 0;
         return (0, r.useMemo)(() => {
             if (null != n)
                 return 3 === n
-                    ? l.intl.string(l.t.QUjmjp)
+                    ? a.intl.string(a.t.QUjmjp)
                     : 7 === n
-                      ? l.intl.string(l.t.yPxJA2)
-                      : l.intl.string(l.t["o+VpXZ"]);
-            if (null != e && (0, a.o0)(e.skuId) && e.type !== i.Z.BUNDLE && null != e.summary && "" !== e.summary)
+                      ? a.intl.string(a.t.yPxJA2)
+                      : a.intl.string(a.t["o+VpXZ"]);
+            if (null != e && null != e.summary && "" !== e.summary) {
+                if (e.type === i.Z.BUNDLE && e.summary.includes("{joinedItems}")) {
+                    let n = l(e, t);
+                    return e.summary.replace("{joinedItems}", n);
+                }
                 return e.summary;
+            }
             switch (null == e ? void 0 : e.type) {
                 case i.Z.AVATAR_DECORATION:
-                    return l.intl.string(l.t["3lv7q2"]);
+                    return a.intl.string(a.t["3lv7q2"]);
                 case i.Z.PROFILE_EFFECT:
-                    return l.intl.string(l.t.VhJL72);
+                    return a.intl.string(a.t.VhJL72);
                 case i.Z.NAMEPLATE:
-                    return l.intl.string(l.t.ik37EZ);
+                    return a.intl.string(a.t.ik37EZ);
                 case i.Z.BUNDLE:
-                    return o(e, t);
+                    return l(e, t);
                 default:
                     return "";
             }
