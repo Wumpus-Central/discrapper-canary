2144925c5b95d8c135731c6c90ac21490037fde7
DiscrapperManager <admin@minerpl.xyz>
2026-01-20 19:59:28 +0100

    canaryRelease: 487238
    Main scripts: 3
    Lazy Loaded Scripts: 1804
    Chunks:
     * Main Files: 15967
     * Scripts: 9314
    Strings: 23494
    Routes: 788
    Experiments Count: 248
    Apex Experiments Count: 128

--- chunks/412412.js
+++ chunks/412412.js
@@ -1,14 +1,7 @@
-n.d(t, { t: () => o });
-var r = n(427164),
-    i = n(751823);
-let a = (0, r.le)({
-    name: "2025-11-capture-device-for-encode",
-    kind: "user",
-    defaultConfig: { enabled: !1 },
-    variations: { 1: { enabled: !0 } },
-});
-function o(e) {
+n.d(t, { t: () => i });
+var r = n(751823);
+function i(e) {
     let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
-        { isInHoldout: n } = i.L.getCurrentConfig({ location: e }, { autoTrackExposure: t });
-    return n ? { enabled: !1 } : a.getConfig({ location: e });
+        { isInHoldout: n } = r.L.getCurrentConfig({ location: e }, { autoTrackExposure: t });
+    return { enabled: !n };
 }
