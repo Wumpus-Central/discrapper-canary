14083bea9a6ffc9c900b0af66586cb6e21497f88
DiscrapperManager <admin@minerpl.xyz>
2026-01-21 17:16:58 +0100

    canaryRelease: 487591
    Main scripts: 3
    Lazy Loaded Scripts: 1863
    Chunks:
     * Main Files: 15980
     * Scripts: 9377
    Strings: 23503
    Routes: 788
    Experiments Count: 246
    Apex Experiments Count: 133

--- chunks/168020.js
+++ chunks/168020.js
@@ -1,29 +1,31 @@
-n.d(t, { R: () => i }), n(35282);
+n.d(t, { R: () => u }), n(35282);
 var r = n(54381);
 n(473749);
-var l = n(481060),
-    a = n(774078),
-    s = n(388032),
-    o = n(594054);
-let i = (e) => {
-    let { endDate: t, size: n = "md" } = e,
-        { days: i, hours: c, minutes: u } = (0, a.Z)(t),
-        d = (function (e, t, n) {
+var l = n(120356),
+    a = n.n(l),
+    s = n(481060),
+    i = n(774078),
+    o = n(388032),
+    c = n(594054);
+let u = (e) => {
+    let { endDate: t, size: n = "md", className: l } = e,
+        { days: u, hours: d, minutes: f } = (0, i.Z)(t),
+        g = (function (e, t, n) {
             let r = (e) => "".concat(e.toString().padStart(2, "0"));
             return [r(e), r(t), r(n)].join(":");
-        })(i, c, u);
+        })(u, d, f);
     return (0, r.jsxs)("div", {
-        className: o.container,
+        className: a()(c.container, l),
         role: "timer",
         children: [
-            d.split("").map((e, t) =>
+            g.split("").map((e, t) =>
                 ":" === e
                     ? (0, r.jsx)(
-                          l.Text,
+                          s.Text,
                           {
                               color: "none",
                               variant: "md" === n ? "heading-lg/extrabold" : "heading-xxl/extrabold",
-                              className: o.delineator,
+                              className: c.delineator,
                               "aria-hidden": !0,
                               tag: "div",
                               children: e,
@@ -31,11 +33,11 @@ let i = (e) => {
                           t,
                       )
                     : (0, r.jsx)(
-                          l.Text,
+                          s.Text,
                           {
                               color: "always-white",
                               variant: "md" === n ? "heading-md/bold" : "heading-xl/bold",
-                              className: o.number,
+                              className: c.number,
                               "aria-hidden": !0,
                               tag: "div",
                               children: e,
@@ -43,11 +45,11 @@ let i = (e) => {
                           t,
                       ),
             ),
-            (0, r.jsx)(l.nn4, {
-                children: s.intl.format(s.t.j6IyVe, {
-                    days: i,
-                    hours: c,
-                    minutes: u,
+            (0, r.jsx)(s.nn4, {
+                children: o.intl.format(o.t.j6IyVe, {
+                    days: u,
+                    hours: d,
+                    minutes: f,
                 }),
             }),
         ],
