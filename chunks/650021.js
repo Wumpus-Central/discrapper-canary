a39d2c1477fcc16a8078a2b16edce64f8614eae9
DiscrapperManager <admin@minerpl.xyz>
2026-01-22 18:39:33 +0100

    canaryRelease: 487970
    Main scripts: 3
    Lazy Loaded Scripts: 1863
    Chunks:
     * Main Files: 15992
     * Scripts: 9379
    Strings: 23511
    Routes: 788
    Experiments Count: 247
    Apex Experiments Count: 134

--- chunks/650021.js
+++ chunks/650021.js
@@ -1,4 +1,18 @@
-t.exports = {
-    image: "_97ca1bddcbdd3192-image",
-    text: "_97ca1bddcbdd3192-text",
+n.d(t, { Z: () => o });
+var r = n(59049),
+    a = n(14160),
+    l = n(247123),
+    i = n(207662);
+let o = {
+    id: "input-label",
+    selector:
+        'input, textarea, select, [role="checkbox"], [role="radio"], [role="radiogroup"], [role="searchbox"], [role="slider"], [role="textbox"], [role="combobox"]',
+    tags: ["wcag2a", "wcag131", "wcag412", "section508", "section508.22.a", "ACT"],
+    metadata: {
+        description: "Form inputs require a label",
+        help: "Please provide a name for this input with a HTML label, aria-label, or aria-labelledby.",
+    },
+    check: function (e) {
+        return "" === (0, r.AB)(e) ? ((0, i.Uu)(e) || !(0, a.p)(e) ? l.w : "Form input has no label") : l.w;
+    },
 };
--- chunks/841178.js
+++ chunks/841178.js
@@ -1,61 +1,30 @@
-n.d(i, { default: () => l });
-var e = n(54381);
+n.d(i, { default: () => e });
+var r = n(54381);
 n(473749);
-var r = n(481060),
-    a = n(600164),
-    s = n(313201),
-    d = n(952164),
-    o = n(388032),
-    c = n(650021);
-function l(t) {
-    let { transitionState: i, onClose: n } = t,
-        l = (0, s.Dt)();
-    return (0, e.jsxs)(r.Y0X, {
-        "aria-labelledby": l,
+var s = n(159691),
+    a = n(952164),
+    o = n(388032);
+function e(t) {
+    let { transitionState: i, onClose: n } = t;
+    return (0, r.jsx)(s.u_l, {
         transitionState: i,
-        size: r.CgR.SMALL,
-        parentComponent: "SpotifyPremiumUpgradeModal",
-        children: [
-            (0, e.jsx)(r.xBx, {
-                separator: !1,
-                children: (0, e.jsx)(r.Heading, {
-                    id: l,
-                    variant: "heading-lg/semibold",
-                    children: o.intl.formatToMarkdownString(o.t.DQsSGr, {}),
-                }),
-            }),
-            (0, e.jsxs)(r.hzk, {
-                children: [
-                    (0, e.jsx)(a.Z, {
-                        justify: a.Z.Justify.CENTER,
-                        children: (0, e.jsx)("div", { className: c.image }),
-                    }),
-                    (0, e.jsx)(r.Text, {
-                        className: c.text,
-                        variant: "text-sm/normal",
-                        children: o.intl.string(o.t.xGMwi9),
-                    }),
-                ],
-            }),
-            (0, e.jsx)(r.mzw, {
-                children: (0, e.jsxs)(r.ButtonGroup, {
-                    direction: "horizontal-reverse",
-                    children: [
-                        (0, e.jsx)(r.Button, {
-                            variant: "primary",
-                            text: o.intl.string(o.t.n8U5jd),
-                            onClick: function () {
-                                (0, d.KK)(), n();
-                            },
-                        }),
-                        (0, e.jsx)(r.Button, {
-                            variant: "secondary",
-                            text: o.intl.string(o.t["ETE/oC"]),
-                            onClick: n,
-                        }),
-                    ],
-                }),
-            }),
+        onClose: n,
+        size: "sm",
+        title: o.intl.formatToMarkdownString(o.t.DQsSGr, {}),
+        subtitle: o.intl.string(o.t.xGMwi9),
+        actions: [
+            {
+                variant: "secondary",
+                text: o.intl.string(o.t["ETE/oC"]),
+                onClick: n,
+            },
+            {
+                variant: "primary",
+                text: o.intl.string(o.t.n8U5jd),
+                onClick: function () {
+                    (0, a.KK)(), n();
+                },
+            },
         ],
     });
 }
