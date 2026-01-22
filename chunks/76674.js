2367464aac6f34d98bef14a8d7557b23bc42df99
DiscrapperManager <admin@minerpl.xyz>
2026-01-22 17:55:01 +0100

    canaryRelease: 487818
    Main scripts: 3
    Lazy Loaded Scripts: 1863
    Chunks:
     * Main Files: 15988
     * Scripts: 9377
    Strings: 23506
    Routes: 788
    Experiments Count: 247
    Apex Experiments Count: 133

--- chunks/76674.js
+++ chunks/76674.js
@@ -1,13 +1,4 @@
-r.d(t, { Q: () => o });
-async function o({
-    gif: e,
-    x: t,
-    y: o,
-    width: n,
-    height: a,
-    imageRotation: i = 0,
-    resizeWidth: c = null,
-    resizeHeight: l = null,
-}) {
-    return (await r.e("47674").then(r.bind(r, 305522))).crop_and_rotate_gif(e, t, o, n, a, i, c, l);
+r.d(t, { Z: () => a });
+function a() {
+    return r.e("47674").then(r.bind(r, 305522));
 }
