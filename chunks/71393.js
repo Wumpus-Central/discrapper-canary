2edd6dc84c62876a697f88dec2a14c951b433a4c
DiscrapperManager <admin@minerpl.xyz>
2026-01-28 00:34:11 +0100

    canaryRelease: 490197
    Main scripts: 3
    Lazy Loaded Scripts: 1950
    Chunks:
     * Main Files: 13755
     * Scripts: 11778
    Strings: 23590
    Routes: 787
    Experiments Count: 243
    Apex Experiments Count: 135

--- chunks/71393.js
+++ chunks/71393.js
@@ -1,16 +1,18 @@
 n.d(t, {
-    A: () => p,
+    A: () => E,
 }),
     n(896048);
-var r = n(23974),
-    i = n(260509),
-    a = n(860689),
-    s = n(661191),
-    o = n(961350),
-    l = n(652215),
-    c = n(349828);
+var r = n(357758),
+    i = n(867051),
+    a = n(942269),
+    o = n(260509),
+    s = n(860689),
+    l = n(661191),
+    c = n(961350),
+    u = n(652215),
+    d = n(349828);
 
-function u(e, t, n) {
+function f(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -24,7 +26,7 @@ function u(e, t, n) {
     );
 }
 
-function d(e) {
+function p(e) {
     for (var t = 1; t < arguments.length; t++) {
         var n = null != arguments[t] ? arguments[t] : {},
             r = Object.keys(n);
@@ -35,94 +37,135 @@ function d(e) {
                 }),
             )),
             r.forEach(function (t) {
-                u(e, t, n[t]);
+                f(e, t, n[t]);
             });
     }
     return e;
 }
-class f extends r.f {
+
+function _(e, t) {
+    var n = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+        var r = Object.getOwnPropertySymbols(e);
+        t &&
+            (r = r.filter(function (t) {
+                return Object.getOwnPropertyDescriptor(e, t).enumerable;
+            })),
+            n.push.apply(n, r);
+    }
+    return n;
+}
+
+function h(e, t) {
+    return (
+        (t = null != t ? t : {}),
+        Object.getOwnPropertyDescriptors
+            ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+            : _(Object(t)).forEach(function (n) {
+                  Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+              }),
+        e
+    );
+}
+
+function m(e) {
+    let t = e;
+    return (0, i.yE)(
+        o.vI,
+        h(p({}, t), {
+            features: (0, r.y)(t.features),
+            joinedAt: null != t.joinedAt ? new Date(t.joinedAt) : null,
+        }),
+    );
+}
+class g extends a.yW {
+    stateWrapper() {
+        return this.database;
+    }
     getGuildCount() {
-        return this.length();
+        return this.database.length();
     }
     constructor(...e) {
         super(...e),
-            u(this, "getGuild", (e) => {
-                if (null != e) return e === l.YYv ? c._ : this.get(e);
+            f(this, "database", this.addKVDatabase("guilds", m)),
+            f(this, "getGuild", (e) => {
+                if (null != e) return e === u.YYv ? d._ : this.database.get(e);
             }),
-            u(
+            f(
                 this,
                 "getGuilds",
-                this.memoized((e) => d({}, e)),
+                this.database.memoized((e) => p({}, e)),
             ),
-            u(
+            f(
                 this,
                 "getGuildsArray",
-                this.memoized((e) => Object.values(e)),
+                this.database.memoized((e) => Object.values(e)),
             ),
-            u(
+            f(
                 this,
                 "getGuildIds",
-                this.memoized((e) => s.default.keys(e)),
+                this.database.memoized((e) => l.default.keys(e)),
             );
     }
 }
-u(f, "displayName", "GuildStore");
-let p = new f({
+f(g, "displayName", "GuildStore");
+let E = new g({
     BACKGROUND_SYNC: (e, t) => {
         let { guilds: n } = e;
         for (let e of n) {
             let n = t.get(e.id);
-            null != n && "unavailable" !== e.data_mode && t.set(e.id, a.kI(e, n));
+            null != n && "unavailable" !== e.data_mode && t.set(e.id, s.kI(e, n));
         }
     },
     CONNECTION_OPEN: (e, t) => {
-        let { guilds: n } = e;
-        t.reset((e, t) => {
-            for (let r of n) e[r.id] = a.Wj(r, t[r.id]);
-        });
+        let { guilds: n } = e,
+            r = {};
+        for (let e of n) {
+            let n = t.get(e.id);
+            null != n && (r[e.id] = n);
+        }
+        for (let e of (t.clear(), n)) t.set(e.id, s.Wj(e, r[e.id]));
     },
     OVERLAY_INITIALIZE: (e, t) => {
         let { guilds: n } = e;
-        t.reset((e) => {
-            if (null != n)
-                for (let { properties: t, additionalFields: r } of n)
-                    e[t.id] = a.zT(t, {
+        if ((t.clear(), null != n))
+            for (let { properties: e, additionalFields: r } of n)
+                t.set(
+                    e.id,
+                    s.zT(e, {
                         joinedAt: null != r.joinedAt ? new Date(r.joinedAt) : null,
                         premiumSubscriberCount: r.premiumSubscriberCount,
-                    });
-        });
+                    }),
+                );
     },
     CACHE_LOADED: (e, t) => {
         let { guilds: n } = e;
-        t.reset((e) => {
-            for (let t of n) e[t.id] = a.$O(t);
-        });
+        for (let e of (t.clear(), n)) t.set(e.id, s.$O(e));
     },
     CACHE_LOADED_LAZY: (e, t) => {
         let { guilds: n } = e;
-        0 !== n.length &&
-            t.reset((e) => {
-                for (let t of n) e[t.id] = a.$O(t);
-            });
+        if (0 !== n.length) for (let e of (t.clear(), n)) t.set(e.id, s.$O(e));
     },
     GUILD_CREATE: (e, t) => {
-        let { guild: n } = e;
-        t.set(n.id, (e) => a.Wj(n, e));
+        let { guild: n } = e,
+            r = t.get(n.id);
+        t.set(n.id, s.Wj(n, r));
     },
     GUILD_UPDATE: (e, t) => {
-        let { guild: n } = e;
-        t.set(n.id, (e) => a.Y1(n, e));
+        let { guild: n } = e,
+            r = t.get(n.id);
+        t.set(n.id, s.Y1(n, r));
     },
     GUILD_DELETE: (e, t) => {
         let { guild: n } = e;
         n.unavailable || t.remove(n.id);
     },
     GUILD_MEMBER_ADD: (e, t) => {
-        let { guildId: n, joinedAt: r, user: a } = e,
-            s = o.default.getId(),
-            l = t.get(n);
-        if (s !== a.id || null == l) return;
-        let c = "string" == typeof r ? new Date(r) : r;
-        c !== l.joinedAt && null != c && t.set(n, (0, i.kn)(l, c));
+        let { guildId: n, joinedAt: r, user: i } = e,
+            a = c.default.getId(),
+            s = t.get(n);
+        if (a !== i.id || null == s) return;
+        let l = "string" == typeof r ? new Date(r) : r;
+        l !== s.joinedAt && null != l && t.set(n, (0, o.kn)(s, l));
     },
 });
