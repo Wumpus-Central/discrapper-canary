53563ce1aaea78339ab74b9c74d6ea61a2396270
DiscrapperManager <admin@minerpl.xyz>
2026-01-28 17:41:24 +0100

    canaryRelease: 490425
    Main scripts: 3
    Lazy Loaded Scripts: 1950
    Chunks:
     * Main Files: 13757
     * Scripts: 11778
    Strings: 23598
    Routes: 787
    Experiments Count: 243
    Apex Experiments Count: 136

--- chunks/598733.js
+++ chunks/598733.js
@@ -13,67 +13,64 @@ var r = n(627968),
     E = n(976860),
     d = n(71393),
     _ = n(967198),
-    T = n(449054),
-    A = n(652215),
+    A = n(449054),
+    T = n(652215),
     I = n(985018),
     O = n(163677),
     N = n(1624);
-let p =
-    21552 == n.j
-        ? () => {
-              let e = (0, a.bG)([_.A], () => _.A.getGuildId(), []),
-                  t = (0, a.bG)([d.A], () => d.A.getGuild(e), [e]),
-                  [n, l] = i.useState(!1);
-              if (null == t) return null;
-              let p = async () => {
-                  l(!0);
-                  try {
-                      T.cf(t.id),
-                          await u.A.joinGuild(t.id, {
-                              source: A.Q4z.NOTICE_BAR,
-                          });
-                  } catch (e) {
-                      l(!1);
-                  }
-              };
-              return (0, r.jsxs)("div", {
-                  className: o()(O.lm, N.lm),
-                  children: [
-                      (0, r.jsxs)(s.$n, {
-                          look: s.$n.Looks.OUTLINED,
-                          color: s.$n.Colors.WHITE,
-                          size: s.$n.Sizes.NONE,
-                          className: o()(O.x6, O.aX),
-                          innerClassName: O.gb,
-                          onClick: () => {
-                              (0, E.JK)().goBack();
-                          },
-                          children: [
-                              (0, r.jsx)(c.rJJ, {
-                                  size: "xs",
-                                  color: "currentColor",
-                                  className: O.UE,
-                              }),
-                              I.intl.string(I.t["13/7kX"]),
-                          ],
-                      }),
-                      (0, r.jsx)(c.Text, {
-                          className: O.wx,
-                          variant: "text-sm/normal",
-                          children: I.intl.string(I.t["N/y2WE"]),
-                      }),
-                      (0, r.jsx)(s.$n, {
-                          className: O.x6,
-                          look: s.$n.Looks.OUTLINED,
-                          color: s.$n.Colors.WHITE,
-                          size: s.$n.Sizes.NONE,
-                          submitting: n,
-                          onClick: p,
-                          children: I.intl.format(I.t.uHN7ny, {
-                              guild: t.name,
-                          }),
-                      }),
-                  ],
-              });
-          }
-        : null;
+let p = () => {
+    let e = (0, a.bG)([_.A], () => _.A.getGuildId(), []),
+        t = (0, a.bG)([d.A], () => d.A.getGuild(e), [e]),
+        [n, l] = i.useState(!1);
+    if (null == t) return null;
+    let p = async () => {
+        l(!0);
+        try {
+            A.cf(t.id),
+                await u.A.joinGuild(t.id, {
+                    source: T.Q4z.NOTICE_BAR,
+                });
+        } catch (e) {
+            l(!1);
+        }
+    };
+    return (0, r.jsxs)("div", {
+        className: o()(O.lm, N.lm),
+        children: [
+            (0, r.jsxs)(s.$n, {
+                look: s.$n.Looks.OUTLINED,
+                color: s.$n.Colors.WHITE,
+                size: s.$n.Sizes.NONE,
+                className: o()(O.x6, O.aX),
+                innerClassName: O.gb,
+                onClick: () => {
+                    (0, E.JK)().goBack();
+                },
+                children: [
+                    (0, r.jsx)(c.rJJ, {
+                        size: "xs",
+                        color: "currentColor",
+                        className: O.UE,
+                    }),
+                    I.intl.string(I.t["13/7kX"]),
+                ],
+            }),
+            (0, r.jsx)(c.Text, {
+                className: O.wx,
+                variant: "text-sm/normal",
+                children: I.intl.string(I.t["N/y2WE"]),
+            }),
+            (0, r.jsx)(s.$n, {
+                className: O.x6,
+                look: s.$n.Looks.OUTLINED,
+                color: s.$n.Colors.WHITE,
+                size: s.$n.Sizes.NONE,
+                submitting: n,
+                onClick: p,
+                children: I.intl.format(I.t.uHN7ny, {
+                    guild: t.name,
+                }),
+            }),
+        ],
+    });
+};
