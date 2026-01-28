93efb9c4efdaab25e4c3b2eeaeece5ed9e973461
DiscrapperManager <admin@minerpl.xyz>
2026-01-29 00:56:46 +0100

    canaryRelease: 490794
    Main scripts: 3
    Lazy Loaded Scripts: 1951
    Chunks:
     * Main Files: 13822
     * Scripts: 11805
    Strings: 23605
    Routes: 787
    Experiments Count: 240
    Apex Experiments Count: 135

--- chunks/528237.js
+++ chunks/528237.js
@@ -1,21 +1,24 @@
 i.d(e, {
-    m: () => o,
+    m: () => T,
 });
 var n = i(419954),
     l = i(253932),
     s = i(780964),
-    r = i(840065),
-    u = i(652215),
-    a = i(985018);
-let o = (0, n.zD)(s.X.TEXT_TO_SPEECH_COMMAND, {
-    useTitle: () => a.intl.string(a.t["btbS+Z"]),
-    useSubtitle: () =>
-        a.intl.format(a.t.Q5crhR, {
+    r = i(358776),
+    u = i(840065),
+    a = i(652215),
+    o = i(985018);
+let T = (0, n.zD)(s.X.TEXT_TO_SPEECH_COMMAND, {
+    useTitle: () => o.intl.string(o.t["btbS+Z"]),
+    useSubtitle: () => {
+        let t = (0, r.dk)("TextToSpeechCommandSetting");
+        return o.intl.format(o.t.Q5crhR, {
             onClick: () =>
-                (0, r.openUserSettings)(s.X.ACCESSIBILITY_PANEL, {
-                    section: u.nc_.ACCESSIBILITY,
+                (0, u.openUserSettings)(t ? s.X.TTS_PLAYBACK_RATE : s.X.ACCESSIBILITY_PANEL, {
+                    section: a.nc_.ACCESSIBILITY,
                 }),
-        }),
+        });
+    },
     useValue: l.on.useSetting,
     setValue: l.on.updateSetting,
 });
