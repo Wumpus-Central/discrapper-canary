2d7186f8aadf613cf4065be4d949eb87c9695c6b
DiscrapperManager <admin@minerpl.xyz>
2026-01-30 00:49:46 +0100

    canaryRelease: 491420
    Main scripts: 3
    Lazy Loaded Scripts: 1947
    Chunks:
     * Main Files: 13826
     * Scripts: 11772
    Strings: 23633
    Routes: 790
    Experiments Count: 240
    Apex Experiments Count: 135

--- chunks/493507.js
+++ chunks/493507.js
@@ -1,5 +1,5 @@
 n.d(t, {
-    A: () => m,
+    A: () => I,
 }),
     n(896048);
 var r,
@@ -9,10 +9,11 @@ var r,
     o = n(253932),
     s = n(617617),
     c = n(961350),
-    u = n(927813),
-    d = n(469679);
+    u = n(543465),
+    d = n(927813),
+    _ = n(469679);
 
-function _(e, t, n) {
+function f(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -25,69 +26,76 @@ function _(e, t, n) {
         e
     );
 }
-let f = 3 * u.A.Millis.DAY,
-    A = !1,
-    p = {};
+let A = 3 * d.A.Millis.DAY,
+    p = !1,
+    b = {};
 
-function b() {
+function h() {
     let e =
         !1 !== o.LJ.getSetting() &&
-        d.T.getConfig({
+        _.T.getConfig({
             location: "ReplyNudgeStore",
         }).enabled;
-    if (A === e) return !1;
-    A = e;
+    if (p === e) return !1;
+    p = e;
 }
-class h extends (r = l.Ay.PersistedStore) {
+
+function m() {
+    let e = !1;
+    for (let t of Object.keys(b)) u.Ay.isChannelMuted(null, t) && (delete b[t], (e = !0));
+    return e;
+}
+class g extends (r = l.Ay.PersistedStore) {
     initialize(e) {
         var t;
-        (p = (function (e) {
+        (b = (function (e) {
             let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : Date.now(),
                 n = {};
-            for (let [r, l] of Object.entries(e)) null != l && t - l < f && (n[r] = l);
+            for (let [r, l] of Object.entries(e)) null != l && t - l < A && (n[r] = l);
             return n;
         })(null != (t = null == e ? void 0 : e.nudgedChannels) ? t : {})),
-            this.waitFor(c.default, s.A, i.A),
-            this.syncWith([s.A, i.A], b);
+            this.waitFor(c.default, s.A, i.A, u.Ay),
+            this.syncWith([s.A, i.A], h),
+            this.syncWith([u.Ay], m);
     }
     getState() {
         return {
-            nudgedChannels: p,
+            nudgedChannels: b,
         };
     }
     getNudgeTimestamp(e) {
         var t;
-        return A && null != (t = p[e]) ? t : null;
+        return p && null != (t = b[e]) ? t : null;
     }
     isChannelNudged(e) {
         return null != this.getNudgeTimestamp(e);
     }
 }
-_(h, "displayName", "ReplyNudgeStore"), _(h, "persistKey", "ReplyNudgeStore");
-let m = new h(a.h, {
+f(g, "displayName", "ReplyNudgeStore"), f(g, "persistKey", "ReplyNudgeStore");
+let I = new g(a.h, {
     REPLY_NUDGE_SET: function (e) {
         let { channelId: t, timestamp: n } = e;
-        if (t in p) return !1;
-        p[t] = n;
+        if (u.Ay.isChannelMuted(null, t) || t in b) return !1;
+        b[t] = n;
     },
     MESSAGE_CREATE: function (e) {
         let { message: t } = e;
-        if (!(t.channel_id in p)) return !1;
-        delete p[t.channel_id];
+        if (!(t.channel_id in b)) return !1;
+        delete b[t.channel_id];
     },
     MESSAGE_REACTION_ADD: function (e) {
         let { channelId: t, userId: n } = e;
-        if (n !== c.default.getId() || !(t in p)) return !1;
-        delete p[t];
+        if (n !== c.default.getId() || !(t in b)) return !1;
+        delete b[t];
     },
     CHANNEL_DELETE: function (e) {
         let {
             channel: { id: t },
         } = e;
-        if (!(t in p)) return !1;
-        delete p[t];
+        if (!(t in b)) return !1;
+        delete b[t];
     },
     LOGOUT: function () {
-        (p = {}), (A = !1);
+        (b = {}), (p = !1);
     },
 });
