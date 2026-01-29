865de46c877665bdcbf96111340edf25fe4acfdd
DiscrapperManager <admin@minerpl.xyz>
2026-01-29 03:19:13 +0100

    canaryRelease: 490908
    Main scripts: 3
    Lazy Loaded Scripts: 1951
    Chunks:
     * Main Files: 13823
     * Scripts: 11806
    Strings: 23606
    Routes: 787
    Experiments Count: 240
    Apex Experiments Count: 137

--- chunks/500380.js
+++ chunks/500380.js
@@ -1,16 +1,26 @@
 n.d(t, {
-    t: () => a,
+    t: () => u,
 }),
+    n(896048),
     n(747238);
-let r = ["AN", "MI", "TP"],
-    i = "1f30e",
-    a = (e) => {
+var r = n(96337),
+    i = n(997101),
+    a = n(626584);
+let o = new Set(r.A.map((e) => e.alpha2)),
+    s = ["".concat(i.d.AN), "".concat(i.d.MI), "".concat(i.d.TP)],
+    l = "1f30e",
+    c = new a.A("GetFlagEmoji"),
+    u = (e) => {
         if (null == e) return "";
-        if (r.includes(e)) return n(256628)("./".concat(i, ".svg"));
-        let t = e
-            .toUpperCase()
-            .split("")
-            .map((e) => (127397 + e.charCodeAt(0)).toString(16))
-            .join("-");
-        return n(256628)("./".concat(t, ".svg"));
+        try {
+            if (s.includes(e) || !o.has(e)) return n(256628)("./".concat(l, ".svg"));
+            let t = e
+                .toUpperCase()
+                .split("")
+                .map((e) => (127397 + e.charCodeAt(0)).toString(16))
+                .join("-");
+            return n(256628)("./".concat(t, ".svg"));
+        } catch (e) {
+            return c.error("Error getting flag emoji: ", e), "";
+        }
     };
