539566aef566ff0f7e43c55b3db304959f491784
DiscrapperManager <admin@minerpl.xyz>
2026-01-29 19:17:53 +0100

    canaryRelease: 491120
    Main scripts: 3
    Lazy Loaded Scripts: 1951
    Chunks:
     * Main Files: 13825
     * Scripts: 11810
    Strings: 23619
    Routes: 787
    Experiments Count: 240
    Apex Experiments Count: 137

--- chunks/292103.js
+++ chunks/292103.js
@@ -1,8 +1,9 @@
 n.d(t, {
-    Ay: () => h,
-    S_: () => E,
-    zo: () => j,
-});
+    Ay: () => v,
+    S_: () => j,
+    zo: () => h,
+}),
+    n(896048);
 var r = n(627968);
 n(64700);
 var i = n(311907),
@@ -10,27 +11,26 @@ var i = n(311907),
     a = n(397927),
     o = n(964486),
     c = n(865116),
-    s = n(826673),
-    d = n(379848),
-    u = n(928658),
-    g = n(400528),
-    p = n(376708),
-    f = n(715757),
-    y = n(967198),
-    b = n(287809),
-    A = n(628691),
-    m = n(49999),
-    O = n(985018),
-    v = n(641131);
+    s = n(379848),
+    d = n(928658),
+    u = n(400528),
+    g = n(376708),
+    p = n(715757),
+    f = n(967198),
+    y = n(287809),
+    b = n(628691),
+    A = n(49999),
+    m = n(985018),
+    O = n(641131);
 
-function h(e) {
-    let t = (0, i.bG)([y.A], () => y.A.getGuildId()),
-        n = (0, f.Qo)(t);
-    return (0, A.ul)(e)
+function v(e) {
+    let t = (0, i.bG)([f.A], () => f.A.getGuildId()),
+        n = (0, p.Qo)(t);
+    return (0, b.ul)(e)
         ? (0, r.jsx)(a.Drp, {
               id: "report",
-              label: n ? O.intl.string(O.t.n5EBAJ) : O.intl.string(O.t.GwbdGe),
-              action: () => (0, u.V2)(e, "web_message_context_menu"),
+              label: n ? m.intl.string(m.t.n5EBAJ) : m.intl.string(m.t.GwbdGe),
+              action: () => (0, d.V2)(e, "web_message_context_menu"),
               icon: n ? a.pVd : a.iFK,
               leadingAccessory: {
                   type: "icon",
@@ -41,50 +41,41 @@ function h(e) {
         : null;
 }
 
-function j(e) {
-    let t = (0, i.bG)([g.A], () => g.A.hasReportedMessage(e.channel_id, e.id)),
-        n = (0, p.KB)(e);
+function h(e) {
+    let t = (0, i.bG)([u.A], () => u.A.hasReportedMessage(e.channel_id, e.id)),
+        n = (0, g.KB)(e),
+        c = n ? [l.M.REPORT_TO_MOD_NEW_TAG] : [],
+        [p, f] = (0, s.kn)(c);
     return ((0, o.l0)(() => {
-        n &&
-            (0, s.Dr)(l.M.REPORT_TO_MOD_NEW_TAG, {
-                dismissAction: m.i.AUTO_DISMISS,
-            });
+        n && f(A.i.AUTO_DISMISS);
     }),
     n)
-        ? (0, r.jsx)(d.Ay, {
-              contentTypes: [l.M.REPORT_TO_MOD_NEW_TAG],
-              children: (n) => {
-                  let { visibleContent: i } = n;
-                  return (0, r.jsx)(a.Drp, {
-                      id: "report-to-mod",
-                      label: t ? O.intl.string(v.default["8wsdng"]) : O.intl.string(v.default["1D+vqy"]),
-                      action: () => {
-                          (0, s.Dr)(l.M.REPORT_TO_MOD_NEW_TAG, {
-                              dismissAction: m.i.USER_DISMISS,
-                          }),
-                              (0, u.dy)(e);
-                      },
-                      icon: a.iFK,
-                      disabled: t,
-                      leadingAccessory: {
-                          type: "icon",
-                          icon: a.iFK,
-                      },
-                      badge: i === l.M.REPORT_TO_MOD_NEW_TAG ? "new" : void 0,
-                  });
+        ? (0, r.jsx)(a.Drp, {
+              id: "report-to-mod",
+              label: t ? m.intl.string(O.default["8wsdng"]) : m.intl.string(O.default["1D+vqy"]),
+              action: () => {
+                  f(A.i.USER_DISMISS), (0, d.dy)(e);
               },
+              icon: a.iFK,
+              disabled: t,
+              leadingAccessory: {
+                  type: "icon",
+                  icon: a.iFK,
+              },
+              badge: p === l.M.REPORT_TO_MOD_NEW_TAG ? "new" : void 0,
+              color: "danger",
           })
         : null;
 }
 
-function E(e) {
+function j(e) {
     let t = (0, i.bG)([c.Ay], () => c.Ay.get("iar_testing")),
-        n = (0, i.bG)([b.default], () => b.default.getCurrentUser());
-    return (0, A.ul)(e) && null != n && n.isStaff() && t
+        n = (0, i.bG)([y.default], () => y.default.getCurrentUser());
+    return (0, b.ul)(e) && null != n && n.isStaff() && t
         ? (0, r.jsx)(a.Drp, {
               id: "staff-test-message-report",
               label: "[STAFF] Test Message Report",
-              action: () => (0, u.Rj)(e, "web_message_context_menu"),
+              action: () => (0, d.Rj)(e, "web_message_context_menu"),
               icon: a.iFK,
               leadingAccessory: {
                   type: "icon",
