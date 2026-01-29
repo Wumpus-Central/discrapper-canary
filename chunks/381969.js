18911f8dfb60b60fb27485a60064b88d3495d7f0
DiscrapperManager <admin@minerpl.xyz>
2026-01-29 23:20:56 +0100

    canaryRelease: 491348
    Main scripts: 3
    Lazy Loaded Scripts: 1952
    Chunks:
     * Main Files: 13826
     * Scripts: 11802
    Strings: 23632
    Routes: 790
    Experiments Count: 240
    Apex Experiments Count: 135

--- chunks/381969.js
+++ chunks/381969.js
@@ -1,5 +1,5 @@
 n.d(t, {
-    A: () => m,
+    A: () => D,
     k: () => C,
 }),
     n(321073),
@@ -19,45 +19,45 @@ var r = n(627968),
     T = n(808728),
     I = n(71393),
     O = n(954571),
-    N = n(491026),
-    p = n(486418),
+    p = n(491026),
+    N = n(486418),
     R = n(920814),
     S = n(284683),
     P = n(634654),
     y = n(652215);
-let f = i.createContext(void 0);
+let f = 21552 == n.j ? i.createContext(void 0) : null;
 
 function C() {
     let e = i.useContext(f);
     return o()(null != e, "must use useGuildShopDisplayContext within a GuildShopDisplayContextProvider"), e;
 }
 
-function m(e) {
+function D(e) {
     var t;
     let { children: n, initialTab: l, guildId: o } = e,
         C = (0, a.bG)([I.A], () => I.A.getGuild(o)),
-        m = (0, a.bG)([_.A], () => _.A.isViewingServerShop(o)),
-        D = (0, p.P)(C, "guild_shop_page"),
-        g = (0, N.YZ)(o, "guild_shop_page"),
+        D = (0, a.bG)([_.A], () => _.A.isViewingServerShop(o)),
+        m = (0, N.P)(C, "guild_shop_page"),
+        g = (0, p.YZ)(o, "guild_shop_page"),
         h = (null == C ? void 0 : C.features.has(y.GuildFeatures.PRODUCTS_AVAILABLE_FOR_PURCHASE)) === !0,
-        b = (0, d.Oh)(null == C ? void 0 : C.id),
-        U = null == (t = (0, E.Tq)(o)) ? void 0 : t.server_shop_tab_order,
+        U = (0, d.Oh)(null == C ? void 0 : C.id),
+        b = null == (t = (0, E.Tq)(o)) ? void 0 : t.server_shop_tab_order,
         M = [],
         j = R.B.GUILD_SHOP_FULL_PREVIEW;
-    m || (h && b)
-        ? (M.push(U === S.HA.PRODUCTS_FIRST ? R.B.GUILD_PRODUCTS : R.B.GUILD_ROLE_SUBSCRIPTIONS),
-          M.push(U === S.HA.PRODUCTS_FIRST ? R.B.GUILD_ROLE_SUBSCRIPTIONS : R.B.GUILD_PRODUCTS),
+    D || (h && U)
+        ? (M.push(b === S.HA.PRODUCTS_FIRST ? R.B.GUILD_PRODUCTS : R.B.GUILD_ROLE_SUBSCRIPTIONS),
+          M.push(b === S.HA.PRODUCTS_FIRST ? R.B.GUILD_ROLE_SUBSCRIPTIONS : R.B.GUILD_PRODUCTS),
           (j = M[0]))
-        : h && !b
+        : h && !U
           ? (M.push(R.B.GUILD_PRODUCTS), (j = R.B.GUILD_PRODUCTS))
           : !h &&
-            b &&
+            U &&
             (M.push(R.B.GUILD_ROLE_SUBSCRIPTIONS),
             (j = R.B.GUILD_ROLE_SUBSCRIPTIONS),
-            D && M.push(R.B.GUILD_PRODUCTS_PREVIEW)),
+            m && M.push(R.B.GUILD_PRODUCTS_PREVIEW)),
         (l = null != l ? l : j);
     let [k, v] = i.useState(l),
-        L = (D && !(b && g)) || k === R.B.GUILD_PRODUCTS_PREVIEW;
+        L = (m && !(U && g)) || k === R.B.GUILD_PRODUCTS_PREVIEW;
     return (
         i.useEffect(() => {
             v(l);
