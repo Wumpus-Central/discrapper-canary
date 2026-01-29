611afba35b1ac5273cdceef1d97bee50f0096bf5
DiscrapperManager <admin@minerpl.xyz>
2026-01-30 00:03:02 +0100

    canaryRelease: 491383
    Main scripts: 3
    Lazy Loaded Scripts: 1947
    Chunks:
     * Main Files: 13826
     * Scripts: 11772
    Strings: 23633
    Routes: 790
    Experiments Count: 240
    Apex Experiments Count: 135

--- chunks/338769.js
+++ chunks/338769.js
@@ -1,24 +1,36 @@
-l.d(t, {
+n.d(t, {
     V: () => f,
-    Z: () => d,
+    Z: () => g,
 });
-var n,
-    r = l(64700),
-    s = l(311907),
-    a = l(248352),
-    i = l(797490),
-    o = l(758836),
-    c = l(788868),
-    u = l(985018),
-    d = (((n = {})[(n.COUNTDOWN = 0)] = "COUNTDOWN"), n);
+var r,
+    l = n(64700),
+    s = n(311907),
+    a = n(773669),
+    i = n(248352),
+    o = n(797490),
+    c = n(758836),
+    u = n(788868),
+    d = n(985018),
+    g = (((r = {})[(r.COUNTDOWN = 0)] = "COUNTDOWN"), r);
 
 function f(e, t) {
-    let l = (0, s.bG)([a.A], () => a.A.getUserDiscount(c.tU)),
-        n = (0, i.$)(t);
-    if (r.useMemo(() => null != l && n && e !== o.G2.ORBS, [l, n, e]))
+    let n = (0, s.bG)([i.A], () => i.A.getUserDiscount(u.tU)),
+        r = (0, s.bG)([a.default], () => a.default.locale),
+        g = (0, o.$)(t);
+    if (l.useMemo(() => null != n && g && e !== c.G2.ORBS, [n, g, e])) {
+        let e =
+            null != n && null != n.expiresAt
+                ? new Date(n.expiresAt).toLocaleDateString(r, {
+                      day: "numeric",
+                      month: "numeric",
+                  })
+                : void 0;
         return {
             type: 0,
-            countdownEndDate: null == l ? void 0 : l.expiresAt,
-            message: u.intl.format(u.t.WfqvDS, {}),
+            countdownEndDate: null == n ? void 0 : n.expiresAt,
+            message: d.intl.format(d.t.RCo9MF, {
+                date: e,
+            }),
         };
+    }
 }
