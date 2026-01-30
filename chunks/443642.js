38d8bcddc0b50f73eb711083dbff52fe42cad882
DiscrapperManager <admin@minerpl.xyz>
2026-01-30 18:37:45 +0100

    canaryRelease: 491636
    Main scripts: 3
    Lazy Loaded Scripts: 1947
    Chunks:
     * Main Files: 13829
     * Scripts: 11766
    Strings: 23637
    Routes: 790
    Experiments Count: 240
    Apex Experiments Count: 138

--- chunks/443642.js
+++ chunks/443642.js
@@ -85,58 +85,61 @@ let x = (0, p.xI)(a.A.STICKERS_CONSTANTS_STICKER_DIMENSION),
             });
         return (0, r.jsx)(u.f5, {
             value: S,
-            children: (0, r.jsx)(s.YNO, {
-                align: "center",
-                animation: s.YNO.Animation.TRANSLATE,
-                positionKey: j,
-                onRequestClose: () => {
-                    E(!0);
-                },
-                renderPopout: (e) =>
-                    (0, r.jsx)(
-                        h.A,
-                        v(y({}, e), {
-                            renderableSticker: t,
-                            channel: n,
-                            refreshPositionKey: () => C(String(Date.now())),
-                        }),
-                    ),
-                targetElementRef: p,
-                children: (e) => {
-                    let { onClick: n } = e;
-                    return (0, r.jsx)(
-                        l.m,
-                        v(y({}, d.Uk), {
-                            shouldShow: O,
-                            __unsupportedReactNodeAsText: (0, d.yR)(T),
-                            "aria-label": !1,
-                            onTooltipShow: () => {
-                                var e;
-                                null != (e = I) &&
-                                    c.Ay.trackWithMetadata(_.HAw.EXPRESSION_TOOLTIP_VIEWED, {
-                                        type: b.e.STICKER_IN_MESSAGE_HOVER,
-                                        expression_id: e.id,
-                                        expression_name: e.name,
-                                        is_animated: (0, f.Tw)(e.format_type),
-                                        is_custom: (0, f.zN)(e.type),
-                                    });
-                            },
-                            children: (0, r.jsx)(s.DUT, {
-                                innerRef: p,
-                                className: A.q7,
-                                onClick: (e) => {
-                                    E(!O), n(e);
+            children: (0, r.jsx)("div", {
+                className: A.be,
+                children: (0, r.jsx)(s.YNO, {
+                    align: "center",
+                    animation: s.YNO.Animation.TRANSLATE,
+                    positionKey: j,
+                    onRequestClose: () => {
+                        E(!0);
+                    },
+                    renderPopout: (e) =>
+                        (0, r.jsx)(
+                            h.A,
+                            v(y({}, e), {
+                                renderableSticker: t,
+                                channel: n,
+                                refreshPositionKey: () => C(String(Date.now())),
+                            }),
+                        ),
+                    targetElementRef: p,
+                    children: (e) => {
+                        let { onClick: n } = e;
+                        return (0, r.jsx)(
+                            l.m,
+                            v(y({}, d.Uk), {
+                                shouldShow: O,
+                                __unsupportedReactNodeAsText: (0, d.yR)(T),
+                                "aria-label": !1,
+                                onTooltipShow: () => {
+                                    var e;
+                                    null != (e = I) &&
+                                        c.Ay.trackWithMetadata(_.HAw.EXPRESSION_TOOLTIP_VIEWED, {
+                                            type: b.e.STICKER_IN_MESSAGE_HOVER,
+                                            expression_id: e.id,
+                                            expression_name: e.name,
+                                            is_animated: (0, f.Tw)(e.format_type),
+                                            is_custom: (0, f.zN)(e.type),
+                                        });
                                 },
-                                tag: "span",
-                                children: (0, r.jsx)(g.A, {
-                                    isInteracting: a,
-                                    size: x,
-                                    sticker: null != I ? I : t,
+                                children: (0, r.jsx)(s.DUT, {
+                                    innerRef: p,
+                                    className: A.q7,
+                                    onClick: (e) => {
+                                        E(!O), n(e);
+                                    },
+                                    tag: "span",
+                                    children: (0, r.jsx)(g.A, {
+                                        isInteracting: a,
+                                        size: x,
+                                        sticker: null != I ? I : t,
+                                    }),
                                 }),
                             }),
-                        }),
-                    );
-                },
+                        );
+                    },
+                }),
             }),
         });
     };
