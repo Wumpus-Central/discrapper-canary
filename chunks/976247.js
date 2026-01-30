63c58916782a361bcdc0d7773295ecca6783b14f
DiscrapperManager <admin@minerpl.xyz>
2026-01-30 21:13:22 +0100

    canaryRelease: 491759
    Main scripts: 3
    Lazy Loaded Scripts: 1945
    Chunks:
     * Main Files: 13831
     * Scripts: 11681
    Strings: 23638
    Routes: 790
    Experiments Count: 240
    Apex Experiments Count: 138

--- chunks/976247.js
+++ chunks/976247.js
@@ -1,13 +1,13 @@
 n.d(t, {
-    A: () => b,
+    A: () => y,
     z: () => g,
 }),
     n(896048);
 var r = n(627968),
     i = n(64700),
     a = n(503698),
-    s = n.n(a),
-    o = n(452282),
+    o = n.n(a),
+    s = n(452282),
     l = n(515718),
     c = n(369254),
     u = n(358731),
@@ -27,8 +27,8 @@ function g(e, t) {
             height: r,
         };
     let { width: i, height: a } = t,
-        s = window.innerWidth - 2 * m,
-        o = window.innerHeight - (e ? h : p) * 2,
+        o = window.innerWidth - 2 * m,
+        s = window.innerHeight - (e ? h : p) * 2,
         c = (0, l.Uj)({
             width: i,
             height: a,
@@ -38,8 +38,8 @@ function g(e, t) {
         u = (0, l.Uj)({
             width: i,
             height: a,
-            maxWidth: s,
-            maxHeight: o,
+            maxWidth: o,
+            maxHeight: s,
         });
     return c.width >= u.width ? c : u;
 }
@@ -59,23 +59,25 @@ function E(e) {
         }),
         i.useEffect(() => {
             if (t.length > 1) {
-                let e = t[(0, o.U3)(n - 1, t.length)],
-                    r = t[(0, o.U3)(n + 1, t.length)];
+                let e = t[(0, s.U3)(n - 1, t.length)],
+                    r = t[(0, s.U3)(n + 1, t.length)];
                 (0, c.Z5)(e, !0), t.length > 2 && (0, c.Z5)(r, !0);
             }
         }, [n, t]),
         (0, r.jsx)(u.Ay, {
             mode: u.nY.PINNED,
             children: (e) =>
-                (0, r.jsx)(
-                    "div",
-                    {
-                        className: s()(d.k4, e),
-                        children: f.width > 0 && f.height > 0 && a(f.width, f.height),
-                    },
-                    l.url,
-                ),
+                (0, r.jsx)("div", {
+                    className: o()(d.k4, e),
+                    children: (0, r.jsx)(
+                        "div",
+                        {
+                            children: f.width > 0 && f.height > 0 && a(f.width, f.height),
+                        },
+                        l.url,
+                    ),
+                }),
         })
     );
 }
-let b = i.memo(E);
+let y = i.memo(E);
