d243fcb3a29fdba1cb5c4f35cf07adc55cb35e00
DiscrapperManager <admin@minerpl.xyz>
2026-02-02 20:17:57 +0100

    canaryRelease: 492147
    Main scripts: 3
    Lazy Loaded Scripts: 1948
    Chunks:
     * Main Files: 13847
     * Scripts: 11718
    Strings: 23656
    Routes: 792
    Experiments Count: 238
    Apex Experiments Count: 140

--- chunks/904854.js
+++ chunks/904854.js
@@ -1,5 +1,5 @@
 n.d(t, {
-    A: () => _,
+    A: () => h,
 });
 var r = n(627968),
     i = n(64700),
@@ -14,93 +14,76 @@ var r = n(627968),
     m = n(888675),
     f = n(502197),
     g = n(985018),
-    h = n(443300);
+    _ = n(443300);
 
-function _(e) {
-    let { message: t, channel: n, compact: _ } = e,
-        b = (0, f.K)(t),
-        A = (0, a.yK)([u.default], () => {
+function h(e) {
+    var t;
+    let n,
+        h,
+        b,
+        y,
+        A,
+        v,
+        { message: O, channel: x, compact: E } = e,
+        j = (0, f.K)(O),
+        C = (0, a.yK)([u.default], () => {
             var e;
-            return (null == (e = t.call) ? void 0 : e.participants) != null
-                ? t.call.participants
+            return (null == (e = O.call) ? void 0 : e.participants) != null
+                ? O.call.participants
                       .map((e) => u.default.getUser(e))
                       .filter((e) => null != e)
-                      .filter((e) => e.id !== t.author.id)
+                      .filter((e) => e.id !== O.author.id)
                 : [];
-        }, [t.author.id, t.call]),
-        y = (0, a.bG)([o.A], () => o.A.getUserAffinitiesMap(), []),
-        v = i.useMemo(() => (0, c.L)(A, y, "VoiceSession - participants"), [A, y]),
-        x = (function (e, t, n, r, i) {
-            let l = (0, p.P)({
-                    user: n,
-                    channelId: t.id,
-                    guildId: t.guild_id,
-                    messageId: e.id,
-                }),
-                a = (0, p.P)({
-                    user: r[0],
-                    channelId: t.id,
-                    guildId: t.guild_id,
-                    messageId: e.id,
-                }),
-                s = (0, p.P)({
-                    user: r[1],
-                    channelId: t.id,
-                    guildId: t.guild_id,
-                    messageId: e.id,
-                }),
-                o = (0, d.Ay)(e),
-                c = (0, d.d8)(r[0], t),
-                u = (0, d.d8)(r[1], t);
-            if (null == i)
-                return g.intl.format(g.t["eX6e/3"], {
-                    username: o.nick,
-                    usernameHook: l(o),
-                });
-            switch (r.length) {
-                case 0:
-                    return g.intl.format(g.t.r618nP, {
-                        username: o.nick,
-                        usernameHook: l(o),
-                        duration: i,
-                    });
-                case 1:
-                    return g.intl.format(g.t["bqK+jY"], {
-                        username: o.nick,
-                        usernameHook: l(o),
-                        username2: null == c ? void 0 : c.nick,
-                        username2Hook: a(c),
-                        duration: i,
-                    });
-                case 2:
-                    return g.intl.format(g.t.kzbH4d, {
-                        username: o.nick,
-                        usernameHook: l(o),
-                        username2: null == c ? void 0 : c.nick,
-                        username2Hook: a(c),
-                        username3: null == u ? void 0 : u.nick,
-                        username3Hook: s(u),
-                        duration: i,
-                    });
-                default:
-                    return g.intl.format(g.t.HZzzH1, {
-                        username: o.nick,
-                        usernameHook: l(o),
-                        username2: null == c ? void 0 : c.nick,
-                        username2Hook: a(c),
-                        userCount: r.length - 1,
-                        duration: i,
-                    });
-            }
-        })(t, n, t.author, v, b);
+        }, [O.author.id, O.call]),
+        I = (0, a.bG)([o.A], () => o.A.getUserAffinitiesMap(), []),
+        S = i.useMemo(() => (0, c.L)(C, I, "VoiceSession - participants"), [C, I]),
+        T =
+            ((t = O.author),
+            (n = (0, p.P)({
+                user: t,
+                channelId: x.id,
+                guildId: x.guild_id,
+                messageId: O.id,
+            })),
+            (h = (0, p.P)({
+                user: S[0],
+                channelId: x.id,
+                guildId: x.guild_id,
+                messageId: O.id,
+            })),
+            (b = (0, p.P)({
+                user: S[1],
+                channelId: x.id,
+                guildId: x.guild_id,
+                messageId: O.id,
+            })),
+            (y = (0, d.Ay)(O)),
+            (A = (0, d.d8)(S[0], x)),
+            (v = (0, d.d8)(S[1], x)),
+            null == j
+                ? g.intl.format(g.t["eX6e/3"], {
+                      username: y.nick,
+                      usernameHook: n(y),
+                  })
+                : g.intl.format(g.t.YUbgR8, {
+                      userCount: S.length + 1,
+                      username: y.nick,
+                      usernameHook: n(y),
+                      username2: A.nick,
+                      username2Hook: h(A),
+                      username3: v.nick,
+                      username3Hook: b(v),
+                      otherCount: S.length - 1,
+                      duration: j,
+                  }));
     return (0, r.jsx)(m.A, {
         iconNode: (0, r.jsx)(l.HKD, {
             size: "md",
             color: s.LU0.colors.STATUS_POSITIVE,
         }),
-        iconContainerClassName: h.z,
-        timestamp: t.timestamp,
-        compact: _,
-        children: x,
+        iconContainerClassName: _.z,
+        timestamp: O.timestamp,
+        compact: E,
+        children: T,
     });
 }
