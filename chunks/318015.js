5c4ef6ef96cbf137405b9e75409e32c46da480c3
DiscrapperManager <admin@minerpl.xyz>
2026-02-02 23:45:19 +0100

    canaryRelease: 492364
    Main scripts: 3
    Lazy Loaded Scripts: 1978
    Chunks:
     * Main Files: 13853
     * Scripts: 11757
    Strings: 23666
    Routes: 792
    Experiments Count: 237
    Apex Experiments Count: 143

--- chunks/318015.js
+++ chunks/318015.js
@@ -1,46 +1,37 @@
 n.d(t, {
     AL: () => i.A,
-    RK: () => s,
-    sK: () => o,
+    RK: () => o,
+    sK: () => s,
 });
-var r = n(600975),
+var r = n(945810),
     i = n(766005);
-let a = (0, r.C)({
+let a = (0, r.mj)({
+    name: "2026-02-private-channel-hiding",
     kind: "user",
-    id: "2024-08_private_channel_hiding",
-    label: "Private Channel Hiding",
     defaultConfig: {
         enableObfuscation: !1,
         enableIntegrityCheck: !1,
     },
-    treatments: [
-        {
-            id: 1,
-            label: "Obfuscation Enabled",
-            config: {
-                enableObfuscation: !0,
-                enableIntegrityCheck: !1,
-            },
+    variations: {
+        1: {
+            enableObfuscation: !0,
+            enableIntegrityCheck: !1,
         },
-        {
-            id: 2,
-            label: "Obfuscation + Integrity Check Enabled",
-            config: {
-                enableObfuscation: !0,
-                enableIntegrityCheck: !0,
-            },
+        2: {
+            enableObfuscation: !0,
+            enableIntegrityCheck: !0,
         },
-    ],
+    },
 });
 
-function s(e) {
-    return a.getCurrentConfig({
+function o(e) {
+    return a.getConfig({
         location: e,
     }).enableObfuscation;
 }
 
-function o(e) {
-    return a.useExperiment({
+function s(e) {
+    return a.useConfig({
         location: e,
     }).enableObfuscation;
 }
