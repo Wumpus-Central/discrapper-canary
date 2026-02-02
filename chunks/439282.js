321cf7e5a4f0c233748d588a22d481451b6d3570
DiscrapperManager <admin@minerpl.xyz>
2026-02-02 23:35:32 +0100

    canaryRelease: 492348
    Main scripts: 3
    Lazy Loaded Scripts: 1978
    Chunks:
     * Main Files: 13853
     * Scripts: 11757
    Strings: 23666
    Routes: 792
    Experiments Count: 238
    Apex Experiments Count: 142

--- chunks/439282.js
+++ chunks/439282.js
@@ -6,26 +6,26 @@ n.d(t, {
 var r = n(627968),
     l = n(64700),
     i = n(503698),
-    a = n.n(i),
-    s = n(311907),
+    s = n.n(i),
+    a = n(311907),
     o = n(397927),
     c = n(80682),
     u = n(685399),
     d = n(833349),
-    f = n(765379),
-    p = n(713654),
-    h = n(565688),
-    b = n(93879),
-    g = n(961350),
-    m = n(71393),
+    p = n(765379),
+    h = n(713654),
+    g = n(565688),
+    f = n(93879),
+    m = n(961350),
+    b = n(71393),
     A = n(576705),
     y = n(290863),
     O = n(461213),
     j = n(977997),
-    v = n(403362),
-    x = n(246084),
-    E = n(667936),
-    _ = n(879349),
+    x = n(403362),
+    _ = n(246084),
+    v = n(667936),
+    E = n(879349),
     C = n(884415),
     S = n(611330),
     I = n(652215),
@@ -36,14 +36,14 @@ var r = n(627968),
 
 function R(e) {
     var t, i;
-    let a,
-        { channel: p, guild: m, onAction: A, voiceStates: j, isChannelSelected: x, shouldShowSettingNudge: _ } = e;
-    (a = l.useMemo(() => {
+    let s,
+        { channel: h, guild: b, onAction: A, voiceStates: j, isChannelSelected: _, shouldShowSettingNudge: E } = e;
+    (s = l.useMemo(() => {
         var e;
         return null == j || j.length > 50
             ? {}
             : {
-                  [p.getGuildId()]:
+                  [h.getGuildId()]:
                       null !=
                       (e = j.map((e) => {
                           let { user: t } = e;
@@ -52,57 +52,62 @@ function R(e) {
                           ? e
                           : [],
               };
-    }, [p, j])),
-        (0, c.E)(a, "VoiceChannelActivities");
-    let C = (0, u.Ay)(p),
+    }, [h, j])),
+        (0, c.E)(s, "VoiceChannelActivities");
+    let C = (0, u.Ay)(h),
         S = Array.from((0, u.Rz)(C).values()),
-        w = null != (t = null == j ? void 0 : j.filter(v.Vq)) ? t : [],
+        w = null != (t = null == j ? void 0 : j.filter(x.Vq)) ? t : [],
         R = () => (e) =>
             ([I.$pd.PLAYING, I.$pd.WATCHING].includes(e.type) &&
-                (null != e.assets || null != e.state || null != e.details || null != e.party)) ||
+                (null != e.name ||
+                    null != e.application_id ||
+                    null != e.assets ||
+                    null != e.state ||
+                    null != e.details ||
+                    null != e.party)) ||
             e.type === I.$pd.LISTENING,
-        M = (0, s.bG)(
-            [O.A, y.A, g.default],
+        L = (0, a.bG)(
+            [O.A, y.A, m.default],
             () => {
                 let e = {};
                 return (
                     w.forEach((t) => {
                         let n =
-                            t.user.id === g.default.getId()
+                            t.user.id === m.default.getId()
                                 ? O.A.findActivity(R(), !0)
                                 : y.A.findActivity(t.user.id, R());
-                        if (null != n && !(0, f.A)(n)) {
-                            var r, l, i, a;
-                            let s = ""
+                        if (null != n && !(0, p.A)(n)) {
+                            var r, l, i, s;
+                            let a = ""
                                     .concat(null != (r = n.application_id) ? r : "", ":")
-                                    .concat(null != (l = null == (a = n.party) ? void 0 : a.id) ? l : t.user.id),
+                                    .concat(null != (l = null == (s = n.party) ? void 0 : s.id) ? l : t.user.id),
                                 o =
-                                    null != (i = e[s])
+                                    null != (i = e[a])
                                         ? i
                                         : {
                                               members: [],
                                               activity: n,
                                           };
-                            o.members.push(t), (0, d.A)(o.activity, I.jUm.JOIN) || (o.activity = n), (e[s] = o);
+                            o.members.push(t), (0, d.A)(o.activity, I.jUm.JOIN) || (o.activity = n), (e[a] = o);
                         }
                     }),
                     Object.values(e)
                 );
             },
             [w],
-            s.My,
+            a.My,
         ),
-        L = (0, h.m)({
+        M = (0, g.m)({
             location: "voice_channel_activities",
         });
-    return M.length + S.length === 0
+    return L.length + S.length === 0
         ? null
         : (0, r.jsxs)(o.HOs, {
               className: P.kL,
               children: [
                   (0, r.jsx)(D, {
-                      channel: p,
-                      isChannelSelected: x,
+                      channel: h,
+                      isChannelSelected: _,
                       voiceStatesCount: null != (i = null == j ? void 0 : j.length) ? i : 0,
                   }),
                   (0, r.jsx)("div", {
@@ -111,53 +116,53 @@ function R(e) {
                   S.map((e, t) => {
                       var n;
                       return (0, r.jsx)(
-                          E.A,
+                          v.A,
                           {
                               embeddedApp: e,
                               presenceActivity: null != (n = e.presenceActivity) ? n : void 0,
-                              channel: p,
+                              channel: h,
                               onAction: A,
                           },
                           t,
                       );
                   }),
-                  M.map((e, t) => {
+                  L.map((e, t) => {
                       let { members: n, activity: i } = e;
-                      return L && null != i.application_id && N.sQ.has(i.application_id)
+                      return M && null != i.application_id && N.sQ.has(i.application_id)
                           ? (0, r.jsxs)(
                                 l.Fragment,
                                 {
                                     children: [
-                                        (0, r.jsx)(E.A, {
+                                        (0, r.jsx)(v.A, {
                                             presenceActivity: i,
-                                            channel: p,
+                                            channel: h,
                                             members: n,
                                             onAction: A,
                                         }),
-                                        (0, r.jsx)(b.A, {
+                                        (0, r.jsx)(f.A, {
                                             className: P.L,
                                             applicationId: i.application_id,
                                             userIds: n.map((e) => e.user.id),
                                             location: "voice_channel",
-                                            guildId: p.guild_id,
-                                            channelId: p.id,
+                                            guildId: h.guild_id,
+                                            channelId: h.id,
                                         }),
                                     ],
                                 },
                                 "".concat(t, "-with-gifting-breadcrumb"),
                             )
                           : (0, r.jsx)(
-                                E.A,
+                                v.A,
                                 {
                                     presenceActivity: i,
-                                    channel: p,
+                                    channel: h,
                                     members: n,
                                     onAction: A,
                                 },
                                 t,
                             );
                   }),
-                  _ &&
+                  E &&
                       (0, r.jsxs)(r.Fragment, {
                           children: [
                               (0, r.jsx)("div", {
@@ -208,7 +213,7 @@ function R(e) {
                                                           })({}, t)),
                                                           (l = l =
                                                               {
-                                                                  guild: m,
+                                                                  guild: b,
                                                               }),
                                                           Object.getOwnPropertyDescriptors
                                                               ? Object.defineProperties(
@@ -245,21 +250,21 @@ function R(e) {
 
 function D(e) {
     let { channel: t, isChannelSelected: n, voiceStatesCount: l } = e,
-        i = (0, s.bG)([A.A], () => !A.A.can(I.xBc.CONNECT, t)),
-        o = (0, s.bG)([j.A], () => j.A.hasVideo(t.id)),
-        c = (0, x.Ay)({
+        i = (0, a.bG)([A.A], () => !A.A.can(I.xBc.CONNECT, t)),
+        o = (0, a.bG)([j.A], () => j.A.hasVideo(t.id)),
+        c = (0, _.Ay)({
             channel: t,
             locked: i,
             video: o,
             selected: n,
         }),
-        u = (0, s.bG)([m.A], () => m.A.getGuild(t.guild_id));
-    return null == (0, p.gU)(t, u)
+        u = (0, a.bG)([b.A], () => b.A.getGuild(t.guild_id));
+    return null == (0, h.gU)(t, u)
         ? null
         : (0, r.jsxs)("div", {
-              className: a()(P.oT, w.oT),
+              className: s()(P.oT, w.oT),
               children: [
-                  (0, r.jsx)(_.A, {
+                  (0, r.jsx)(E.A, {
                       channel: t,
                   }),
                   c
