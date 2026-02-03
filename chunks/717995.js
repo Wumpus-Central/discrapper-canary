1544fca2bd63cf80e33f6c7d35c73eeced526331
DiscrapperManager <admin@minerpl.xyz>
2026-02-03 02:02:45 +0100

    canaryRelease: 492473
    Main scripts: 3
    Lazy Loaded Scripts: 1977
    Chunks:
     * Main Files: 13856
     * Scripts: 11754
    Strings: 23653
    Routes: 792
    Experiments Count: 235
    Apex Experiments Count: 144

--- chunks/717995.js
+++ chunks/717995.js
@@ -1,11 +1,12 @@
 n.d(t, {
     R: () => b,
-});
+}),
+    n(896048);
 var r = n(627968),
     i = n(64700),
     a = n(503698),
-    s = n.n(a),
-    o = n(108531),
+    o = n.n(a),
+    s = n(353709),
     l = n(508382),
     c = n(158954),
     u = n(502939),
@@ -14,9 +15,18 @@ var r = n(627968),
     p = n(112317),
     _ = n(348275),
     h = n(894524);
-let m = 11,
-    g = ["left", "center", "right"],
-    E = ["top", "center", "bottom"];
+
+function m(e, t, n) {
+    var r, i;
+    return null != (r = null != (i = null == e ? void 0 : e.ownerDocument) ? i : null == t ? void 0 : t.ownerDocument)
+        ? r
+        : null == n
+          ? void 0
+          : n.ownerDocument;
+}
+let g = 11,
+    E = ["left", "center", "right"],
+    y = ["top", "center", "bottom"];
 
 function b(e) {
     let {
@@ -24,36 +34,40 @@ function b(e) {
             isRendered: n = t,
             targetElementRef: a,
             targetElement: b,
-            anchorRef: y,
-            id: O,
+            anchorRef: O,
+            id: v,
             content: A,
-            position: v = "top",
+            position: I = "top",
             align: S = "center",
-            spacing: I = m,
-            caretConfig: T,
-            layerContext: C,
-            animationStyle: N,
+            spacing: T = g,
+            caretConfig: C,
+            layerContext: N,
+            animationStyle: w,
             positionKey: R,
         } = e,
-        { isRichTooltip: w } = (0, p.w6)(),
-        P = (0, u.D)("Tooltip"),
-        D = i.useMemo(() => (("left" === v || "right" === v ? E : g).includes(S) ? S : "center"), [v, S]);
+        { isRichTooltip: P } = (0, p.w6)(),
+        D = (0, u.D)("Tooltip"),
+        [L, x] = i.useState(null == b ? void 0 : b.ownerDocument);
+    i.useLayoutEffect(() => {
+        x(m(null == O ? void 0 : O.current, b, a.current));
+    }, [O, b, a]);
+    let M = i.useMemo(() => (("left" === I || "right" === I ? y : E).includes(S) ? S : "center"), [I, S]);
     if (!n) return null;
-    let x = (e) => {
+    let j = (e) => {
         var t, n, i, a;
         let { position: l, nudge: c } = e,
-            u = null != l ? l : v,
-            p = null != (t = null == T ? void 0 : T.align) ? t : null != c && 0 !== c ? "custom" : "center",
-            m = null != (n = null != (i = null == T ? void 0 : T.customOffset) ? i : c) ? n : 0,
+            u = null != l ? l : I,
+            p = null != (t = null == C ? void 0 : C.align) ? t : null != c && 0 !== c ? "custom" : "center",
+            m = null != (n = null != (i = null == C ? void 0 : C.customOffset) ? i : c) ? n : 0,
             g = {
-                position: null != (a = null == T ? void 0 : T.position) ? a : (0, _.l8)(u),
+                position: null != (a = null == C ? void 0 : C.position) ? a : (0, _.l8)(u),
                 align: p,
                 customOffset: "custom" === p ? m : void 0,
             },
             E = (0, r.jsxs)("div", {
-                id: O,
-                className: s()(h.YL, {
-                    [h.mj]: w,
+                id: v,
+                className: o()(h.YL, {
+                    [h.mj]: P,
                 }),
                 role: "tooltip",
                 "data-position": u,
@@ -74,27 +88,28 @@ function b(e) {
                     }),
                 ],
             });
-        return N
-            ? (0, r.jsx)(o.animated.div, {
-                  style: N,
+        return w
+            ? (0, r.jsx)(s.animated.div, {
+                  style: w,
                   children: E,
               })
             : E;
     };
-    if (P) {
-        let e = (0, l.Pv)(v, S),
+    if (D) {
+        let e = (0, l.Pv)(I, S),
             t = null != b ? b : a.current;
         return (0, r.jsx)(l.Ow, {
             className: h.BM,
             open: n,
-            spacing: I,
+            spacing: T,
             placement: e,
             reference: t,
+            ownerDocument: L,
             renderLayer: (e) => {
                 var t;
                 let { placement: n, shift: r } = e,
                     i = -(null != (t = null == r ? void 0 : r.x) ? t : 0);
-                return x({
+                return j({
                     position: (0, l.$Y)(n),
                     nudge: i,
                 });
@@ -102,20 +117,20 @@ function b(e) {
             children: () => null,
         });
     }
-    let L = (0, r.jsx)(d.QCO, {
-        targetRef: null != y ? y : a,
-        position: v,
-        align: D,
-        spacing: I,
+    let k = (0, r.jsx)(d.QCO, {
+        targetRef: null != O ? O : a,
+        position: I,
+        align: M,
+        spacing: T,
         positionKey: R,
         autoInvert: !0,
         nudgeAlignIntoViewport: !0,
-        children: x,
+        children: j,
     });
-    return null != C
+    return null != N
         ? (0, r.jsx)(c.Wdr, {
-              layerContext: C,
-              children: L,
+              layerContext: N,
+              children: k,
           })
-        : L;
+        : k;
 }
