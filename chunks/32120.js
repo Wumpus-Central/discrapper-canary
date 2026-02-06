efe42f1522d3a90620c87d7531cee6330bbd17dc
DiscrapperManager <admin@minerpl.xyz>
2026-02-07 00:15:45 +0100

    canaryRelease: 494638
    Main scripts: 3
    Lazy Loaded Scripts: 1977
    Chunks:
     * Main Files: 13809
     * Scripts: 11739
    Strings: 23837
    Routes: 793
    Experiments Count: 223
    Apex Experiments Count: 150

--- chunks/32120.js
+++ chunks/32120.js
@@ -1,23 +1,22 @@
 "use strict";
-n.d(t, { A: () => h });
+n.d(t, { A: () => u });
 var i = n(110259),
     s = n(562465),
     r = n(73153),
     l = n(198982),
     a = n(954571),
     o = n(499785),
-    c = n(916882),
-    d = n(652215);
+    c = n(652215);
 n(574454);
-var u = n(985018);
-let h = {
+var d = n(985018);
+let u = {
     resetSuggestions: () => r.h.dispatch({ type: "POMELO_SUGGESTIONS_RESET" }),
     async fetchSuggestionsRegistration(e) {
         let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : 1500;
         r.h.dispatch({ type: "POMELO_SUGGESTIONS_RESET" });
         try {
             let n = await s.Bo.get({
-                url: d.Rsh.POMELO_SUGGESTIONS_UNAUTHED,
+                url: c.Rsh.POMELO_SUGGESTIONS_UNAUTHED,
                 query: null == e ? void 0 : { global_name: e },
                 timeout: t,
                 rejectWithError: !0,
@@ -30,36 +29,35 @@ let h = {
         }
     },
     async fetchSuggestions(e) {
-        if ((0, c.E)())
-            try {
-                r.h.dispatch({ type: "POMELO_SUGGESTIONS_FETCH", usernameSuggestionLoading: !0 });
-                let t = await s.Bo.get({ url: d.Rsh.POMELO_SUGGESTIONS, timeout: e, rejectWithError: !0 });
-                if (t.ok && t.body?.username != null)
-                    return r.h.dispatch({ type: "POMELO_SUGGESTIONS_SUCCESS", suggestion: t.body });
-            } catch (e) {
-                return;
-            }
+        try {
+            r.h.dispatch({ type: "POMELO_SUGGESTIONS_FETCH", usernameSuggestionLoading: !0 });
+            let t = await s.Bo.get({ url: c.Rsh.POMELO_SUGGESTIONS, timeout: e, rejectWithError: !0 });
+            if (t.ok && t.body?.username != null)
+                return r.h.dispatch({ type: "POMELO_SUGGESTIONS_SUCCESS", suggestion: t.body });
+        } catch (e) {
+            return;
+        }
     },
     async attemptPomelo(e) {
         let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : "modal",
             n = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
             s = arguments.length > 3 && void 0 !== arguments[3] && arguments[3],
-            c =
+            u =
                 !1 === /^[A-Za-z0-9_.]*$/.test(e)
-                    ? u.intl.string(u.t.z7c4bP)
+                    ? d.intl.string(d.t.z7c4bP)
                     : e.includes("..")
-                      ? u.intl.string(u.t["C7G+gr"])
+                      ? d.intl.string(d.t["C7G+gr"])
                       : e.length < 2 || e.length > 32
-                        ? u.intl.formatToPlainString(u.t.IpijXA, { maxNum: 32, minNum: 2 })
+                        ? d.intl.formatToPlainString(d.t.IpijXA, { maxNum: 32, minNum: 2 })
                         : void 0;
-        if (null != c)
+        if (null != u)
             return (
-                a.default.track(d.HAw.POMELO_ERRORS, { reason: c, username_error: !0, location: t, one_click_flow: s }),
-                r.h.dispatch({ type: "POMELO_ATTEMPT_FAILURE", username: e, error: c })
+                a.default.track(c.HAw.POMELO_ERRORS, { reason: u, username_error: !0, location: t, one_click_flow: s }),
+                r.h.dispatch({ type: "POMELO_ATTEMPT_FAILURE", username: e, error: u })
             );
         try {
             let l = await o.A.post({
-                url: n ? d.Rsh.POMELO_ATTEMPT_UNAUTHED : d.Rsh.POMELO_ATTEMPT,
+                url: n ? c.Rsh.POMELO_ATTEMPT_UNAUTHED : c.Rsh.POMELO_ATTEMPT,
                 body: { username: e },
                 trackedActionData: {
                     event: i.NetworkActionNames.POMELO_ATTEMPT,
@@ -68,7 +66,7 @@ let h = {
                 rejectWithError: !1,
             });
             l.body.taken &&
-                a.default.track(d.HAw.POMELO_ERRORS, {
+                a.default.track(c.HAw.POMELO_ERRORS, {
                     reason: "already_taken",
                     username_error: !0,
                     location: t,
@@ -78,7 +76,7 @@ let h = {
         } catch (o) {
             let n = new l.LG(o),
                 i = n.getAnyErrorMessage() ?? void 0;
-            a.default.track(d.HAw.POMELO_ERRORS, { reason: i, username_error: !0, location: t, one_click_flow: s }),
+            a.default.track(c.HAw.POMELO_ERRORS, { reason: i, username_error: !0, location: t, one_click_flow: s }),
                 r.h.dispatch({
                     username: e,
                     type: "POMELO_ATTEMPT_FAILURE",
@@ -92,7 +90,7 @@ let h = {
         let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1],
             n = await o.A.post({
                 body: e,
-                url: d.Rsh.POMELO_CREATE,
+                url: c.Rsh.POMELO_CREATE,
                 trackedActionData: { event: i.NetworkActionNames.POMELO_CREATE, properties: { one_click_flow: t } },
                 rejectWithError: !1,
             });
--- chunks/571139.js
+++ chunks/571139.js
@@ -2,13 +2,13 @@ n.d(t, { A: () => P }), n(321073);
 var i = n(627968);
 n(64700);
 var l = n(503698),
-    r = n.n(l);
-if (21552 == n.j) var s = n(960488);
-var a = n(311907),
+    r = n.n(l),
+    s = n(960488),
+    a = n(311907),
     o = n(314116),
     E = n(397927),
-    _ = n(71393),
-    c = n(967198),
+    c = n(71393),
+    _ = n(967198),
     d = n(624458),
     u = n(202384),
     A = n(513461),
@@ -19,58 +19,55 @@ var a = n(311907),
     S = n(985018),
     O = n(121615),
     C = n(1624);
-let P =
-    21552 == n.j
-        ? () => {
-              let e = (0, a.bG)([c.A], () => c.A.getGuildId(), []),
-                  t = (0, a.bG)([_.A], () => _.A.getGuild(e), [e]),
-                  l = (0, a.bG)([I.A], () => (null != e ? I.A.getRequest(e) : null), [e]),
-                  P = (0, s.zy)(),
-                  D = (0, s.B6)(P.pathname, N.BVt.CHANNEL(t?.id, R.VV.GUILD_ONBOARDING))?.isExact === !0;
-              if (null == t || !(0, T.Qd)(t) || D) return null;
-              let p = l?.applicationStatus ?? A.B5.STARTED,
-                  U = null,
-                  m = null,
-                  M = null,
-                  h = [O.lm, C.lm];
-              switch (p) {
-                  case A.B5.SUBMITTED:
-                      (U = S.intl.string(S.t["5iLvSx"])),
-                          (m = S.intl.string(S.t.mqtdmQ)),
-                          (M = () => {
-                              (0, o.A)({
-                                  title: S.intl.string(S.t.aIz1oV),
-                                  subtitle: S.intl.string(S.t["13tjTU"]),
-                                  variant: "primary",
-                                  confirmText: S.intl.string(S.t["cY+Oob"]),
-                                  onConfirm: () => d.A.removeGuildJoinRequest(t.id),
-                              });
-                          });
-                      break;
-                  case A.B5.REJECTED:
-                      (U = S.intl.string(S.t.lk30cY)),
-                          (m = S.intl.string(S.t["8RrsHr"])),
-                          (M = () => {
-                              (0, E.mMO)(async () => {
-                                  let { default: e } = await n.e("37548").then(n.bind(n, 856103));
-                                  return (n) => (0, i.jsx)(e, { guildId: t.id, ...n });
-                              });
-                          }),
-                          h.push(O.z3);
-                      break;
-                  default:
-                      (U = S.intl.string(S.t.G5YKXP)),
-                          (m = S.intl.string(S.t["r8/DT+"])),
-                          (M = () => {
-                              (0, u.Ze)(t.id);
-                          });
-              }
-              return (0, i.jsxs)("div", {
-                  className: r()(...h),
-                  children: [
-                      (0, i.jsx)(E.Text, { className: O.wx, variant: "text-sm/normal", children: U }),
-                      (0, i.jsx)(E.Button, { variant: "overlay-primary", size: "sm", onClick: M, text: m }),
-                  ],
-              });
-          }
-        : null;
+let P = () => {
+    let e = (0, a.bG)([_.A], () => _.A.getGuildId(), []),
+        t = (0, a.bG)([c.A], () => c.A.getGuild(e), [e]),
+        l = (0, a.bG)([I.A], () => (null != e ? I.A.getRequest(e) : null), [e]),
+        P = (0, s.zy)(),
+        D = (0, s.B6)(P.pathname, N.BVt.CHANNEL(t?.id, R.VV.GUILD_ONBOARDING))?.isExact === !0;
+    if (null == t || !(0, T.Qd)(t) || D) return null;
+    let p = l?.applicationStatus ?? A.B5.STARTED,
+        m = null,
+        U = null,
+        h = null,
+        M = [O.lm, C.lm];
+    switch (p) {
+        case A.B5.SUBMITTED:
+            (m = S.intl.string(S.t["5iLvSx"])),
+                (U = S.intl.string(S.t.mqtdmQ)),
+                (h = () => {
+                    (0, o.A)({
+                        title: S.intl.string(S.t.aIz1oV),
+                        subtitle: S.intl.string(S.t["13tjTU"]),
+                        variant: "primary",
+                        confirmText: S.intl.string(S.t["cY+Oob"]),
+                        onConfirm: () => d.A.removeGuildJoinRequest(t.id),
+                    });
+                });
+            break;
+        case A.B5.REJECTED:
+            (m = S.intl.string(S.t.lk30cY)),
+                (U = S.intl.string(S.t["8RrsHr"])),
+                (h = () => {
+                    (0, E.mMO)(async () => {
+                        let { default: e } = await n.e("37548").then(n.bind(n, 856103));
+                        return (n) => (0, i.jsx)(e, { guildId: t.id, ...n });
+                    });
+                }),
+                M.push(O.z3);
+            break;
+        default:
+            (m = S.intl.string(S.t.G5YKXP)),
+                (U = S.intl.string(S.t["r8/DT+"])),
+                (h = () => {
+                    (0, u.Ze)(t.id);
+                });
+    }
+    return (0, i.jsxs)("div", {
+        className: r()(...M),
+        children: [
+            (0, i.jsx)(E.Text, { className: O.wx, variant: "text-sm/normal", children: m }),
+            (0, i.jsx)(E.Button, { variant: "overlay-primary", size: "sm", onClick: h, text: U }),
+        ],
+    });
+};
