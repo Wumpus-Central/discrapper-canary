ab06c35d687519899379b6bbd3e0b579d2d353bd
DiscrapperManager <admin@minerpl.xyz>
2026-02-06 20:16:44 +0100

    canaryRelease: 494431
    Main scripts: 3
    Lazy Loaded Scripts: 1977
    Chunks:
     * Main Files: 13794
     * Scripts: 11745
    Strings: 23828
    Routes: 793
    Experiments Count: 228
    Apex Experiments Count: 149

--- chunks/522958.js
+++ chunks/522958.js
@@ -1,62 +1,34 @@
-n.d(t, { n: () => c });
+n.d(t, { n: () => o });
 var a = n(627968),
     s = n(64700),
     i = n(397927),
     l = n(147925),
     r = n(159452);
 function o(e) {
-    let { text: t } = e;
-    return (0, a.jsx)("span", {
-        className: r.IR,
-        children: t
-            .split("")
-            .map((e, t) =>
-                (0, a.jsx)("span", { className: r.ZF, style: { "--custom-letter-index": t }, children: e }, t),
-            ),
-    });
-}
-function d(e) {
-    let { text: t } = e,
-        n = `⭐ ${t}`;
-    return (0, a.jsx)("span", {
-        className: r.IR,
-        children: n
-            .split("")
-            .map((e, t) =>
-                (0, a.jsx)("span", { className: r.EC, style: { "--custom-letter-index": t }, children: e }, t),
-            ),
-    });
-}
-function c(e) {
-    let { title: t, initExpanded: n, highlightMode: c = "none", children: u } = e,
-        [m, h] = s.useState(n),
-        x = s.useCallback(() => {
-            h(!m);
-        }, [m]),
-        p = "root" === c ? "status-warning" : "status-positive",
-        g = (e) =>
-            "migrated" === c
-                ? (0, a.jsx)(i.Text, { variant: e, children: (0, a.jsx)(o, { text: t }) })
-                : "migrated-root" === c
-                  ? (0, a.jsx)(i.Text, { variant: e, children: (0, a.jsx)(d, { text: t }) })
-                  : (0, a.jsx)(i.Text, { variant: e, color: p, children: t });
-    return null == u
-        ? (0, a.jsx)("div", { className: r.NF, children: g("text-md/normal") })
+    let { title: t, initExpanded: n, highlightMode: o = "none", children: d } = e,
+        [c, u] = s.useState(n),
+        m = s.useCallback(() => {
+            u(!c);
+        }, [c]),
+        h = "migrated" === o || "migrated-root" === o ? void 0 : "status-warning",
+        x = (e) => (0, a.jsx)(i.Text, { variant: e, color: h, children: t });
+    return null == d
+        ? (0, a.jsx)("div", { className: r.NF, children: x("text-md/normal") })
         : (0, a.jsxs)("div", {
               className: r.NF,
               children: [
                   (0, a.jsxs)(i.DUT, {
                       className: r.jr,
-                      onClick: x,
+                      onClick: m,
                       children: [
-                          g("text-md/bold"),
+                          x("text-md/bold"),
                           (0, a.jsx)(l.A, {
-                              direction: m ? l.A.Directions.DOWN : l.A.Directions.RIGHT,
+                              direction: c ? l.A.Directions.DOWN : l.A.Directions.RIGHT,
                               className: r.D,
                           }),
                       ],
                   }),
-                  m && (0, a.jsx)("div", { className: r.m4, children: u }),
+                  c && (0, a.jsx)("div", { className: r.m4, children: d }),
               ],
           });
 }
