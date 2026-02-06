fa55396ca526e3c96f058fabde2a5128e2a4e5dd
DiscrapperManager <admin@minerpl.xyz>
2026-02-06 18:01:56 +0100

    canaryRelease: 494310
    Main scripts: 3
    Lazy Loaded Scripts: 1973
    Chunks:
     * Main Files: 13790
     * Scripts: 11726
    Strings: 23829
    Routes: 793
    Experiments Count: 228
    Apex Experiments Count: 148

--- chunks/617108.js
+++ chunks/617108.js
@@ -7,47 +7,41 @@ var l = n(496431),
     a = n(532794),
     o = n(954571),
     E = n(422936),
-    _ = n(635995),
-    c = n(915516),
+    c = n(635995),
+    _ = n(915516),
     d = n(788868),
     u = n(652215),
     A = n(985018);
-let T =
-    21552 == n.j
-        ? function (e) {
-              let { dismissCurrentNotice: t, subscriptionTier: n } = e,
-                  { analyticsLocations: T } = (0, s.Ay)(r.A.PREMIUM_TIER_2_DISCOUNT_ENDING_NOTICE),
-                  I = (0, E.O)(),
-                  N = (0, l.A)(null != I && null != I.expires_at ? Date.parse(I.expires_at) : 0);
-              return null == I ||
-                  I.discount?.plan_ids.some((e) => d.hd[e].skuId !== n) ||
-                  null == I.expires_at ||
-                  Object.values(N).every((e) => 0 === e)
-                  ? null
-                  : (0, i.jsxs)(_.T0, {
-                        onClick: () => {
-                            t(),
-                                o.default.track(u.HAw.APP_NOTICE_CLOSED, {
-                                    notice_type: u.kqX.PREMIUM_TIER_2_DISCOUNT_ENDING,
-                                });
-                        },
-                        children: [
-                            (0, i.jsx)(_.In, { children: (0, c.rn)(N, Number(I.discount.amount)) }),
-                            (0, i.jsx)(_.fY, {
-                                onClick: () => {
-                                    (0, a.A)({
-                                        subscriptionTier: n,
-                                        analyticsLocations: T,
-                                        analyticsObject: {
-                                            page: u.liQ.IN_APP,
-                                            section: u.JJy.NOTIFICATION_BAR,
-                                            object: u.ZSU.BUTTON_CTA,
-                                        },
-                                    });
-                                },
-                                text: A.intl.string(A.t.zLXssK),
-                            }),
-                        ],
-                    });
-          }
-        : null;
+let T = function (e) {
+    let { dismissCurrentNotice: t, subscriptionTier: n } = e,
+        { analyticsLocations: T } = (0, s.Ay)(r.A.PREMIUM_TIER_2_DISCOUNT_ENDING_NOTICE),
+        I = (0, E.O)(),
+        N = (0, l.A)(null != I && null != I.expires_at ? Date.parse(I.expires_at) : 0);
+    return null == I ||
+        I.discount?.plan_ids.some((e) => d.hd[e].skuId !== n) ||
+        null == I.expires_at ||
+        Object.values(N).every((e) => 0 === e)
+        ? null
+        : (0, i.jsxs)(c.T0, {
+              onClick: () => {
+                  t(), o.default.track(u.HAw.APP_NOTICE_CLOSED, { notice_type: u.kqX.PREMIUM_TIER_2_DISCOUNT_ENDING });
+              },
+              children: [
+                  (0, i.jsx)(c.In, { children: (0, _.rn)(N, Number(I.discount.amount)) }),
+                  (0, i.jsx)(c.fY, {
+                      onClick: () => {
+                          (0, a.A)({
+                              subscriptionTier: n,
+                              analyticsLocations: T,
+                              analyticsObject: {
+                                  page: u.liQ.IN_APP,
+                                  section: u.JJy.NOTIFICATION_BAR,
+                                  object: u.ZSU.BUTTON_CTA,
+                              },
+                          });
+                      },
+                      text: A.intl.string(A.t.zLXssK),
+                  }),
+              ],
+          });
+};
