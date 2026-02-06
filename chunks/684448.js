78c23dc853b9a7163465a1faa5a24ff17c79fef7
DiscrapperManager <admin@minerpl.xyz>
2026-02-06 22:11:53 +0100

    canaryRelease: 494523
    Main scripts: 3
    Lazy Loaded Scripts: 1977
    Chunks:
     * Main Files: 13800
     * Scripts: 11743
    Strings: 23834
    Routes: 793
    Experiments Count: 226
    Apex Experiments Count: 150

--- chunks/684448.js
+++ chunks/684448.js
@@ -26,26 +26,26 @@ function h(e) {
             textSize: A = "xs",
             animateEmoji: I = !0,
             hideEmoji: T = !1,
-            hideTooltip: y = !1,
+            hideTooltip: S = !1,
         } = e,
-        S = (0, d.v)("ActivityStatus", m),
+        y = (0, d.v)("ActivityStatus", m),
         { defaultStatusVariant: v } = (0, l.$j)({ guildId: m?.guild_id, location: "CustomStatusActivityStatus" });
     if (null == t && null == g) return null;
-    let C = null != g && S,
-        b = C && null != E ? (0, u.Au)(E, g, v) : null,
-        N = t?.emoji,
-        R = b ?? t?.state,
+    let C = null != g && y,
+        N = C && null != E ? (0, u.Au)(E, g, v) : null,
+        b = t?.emoji,
+        R = N ?? t?.state,
         O = null != R && "" !== R,
         D = null;
     C && null != E
         ? (D = (0, r.jsx)(c.A, { userId: E, size: p, className: a()(i, f.W9), hangStatusActivity: g }))
-        : null == N || T || (D = (0, r.jsx)(_.A, { emoji: N, animate: I, className: i, hideTooltip: y || O }));
+        : null == b || T || (D = (0, r.jsx)(_.A, { emoji: b, animate: I, className: i, hideTooltip: S || O }));
     let L = O && (null != D ? ` ${R}` : R),
         w = () => {
-            let e = null != N && !T && !O;
-            return y || e
+            let e = null != b && !T && !O;
+            return S || e
                 ? (0, r.jsxs)(r.Fragment, { children: [D, L] })
-                : (0, r.jsxs)(s.A, { delay: 150, className: h, children: [D, L] });
+                : (0, r.jsxs)(s.A, { delay: 150, tooltipClassName: h, className: h, children: [D, L] });
         };
     return (0, r.jsx)(o.Text, { variant: `text-${A}/medium`, color: "none", className: a()(f.ps, n), children: w() });
 }
