bec6d9bf65bcba61076dc3913da7f4c9883fe7da
DiscrapperManager <admin@minerpl.xyz>
2026-02-07 01:24:45 +0100

    canaryRelease: 494698
    Main scripts: 3
    Lazy Loaded Scripts: 1972
    Chunks:
     * Main Files: 13798
     * Scripts: 11731
    Strings: 23698
    Routes: 794
    Experiments Count: 222
    Apex Experiments Count: 150

--- chunks/565478.js
+++ chunks/565478.js
@@ -1,27 +1,25 @@
 "use strict";
-n.d(t, { W: () => d });
+n.d(t, { W: () => c });
 var i = n(64700),
     s = n(735438),
     r = n(417597),
     l = n(32120),
-    a = n(686683),
-    o = n(574454),
-    c = n(871210);
-let d = function (e) {
+    a = n(574454),
+    o = n(871210);
+let c = function (e) {
     let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
         n = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
-        d = arguments.length > 3 && void 0 !== arguments[3] && arguments[3],
-        u = (0, r.bG)([o.A], () => o.A.validate(e), [e]),
-        h = (0, r.bG)([o.A], () => o.A.isRateLimited()),
-        _ = (0, a.G)(),
-        p = i.useMemo(
-            () => (0, s.debounce)((e) => l.A.attemptPomelo(e, n ? "registration" : "modal", n, d), _),
-            [_, n, d],
+        c = arguments.length > 3 && void 0 !== arguments[3] && arguments[3],
+        d = (0, r.bG)([a.A], () => a.A.validate(e), [e]),
+        u = (0, r.bG)([a.A], () => a.A.isRateLimited()),
+        h = i.useMemo(
+            () => (0, s.debounce)((e) => l.A.attemptPomelo(e, n ? "registration" : "modal", n, c), 800),
+            [n, c],
         );
     return (
         i.useEffect(() => {
-            t && !h && null == u && "" !== e && p(e);
-        }, [t, h, u, e, p]),
-        i.useMemo(() => (null != u ? (0, c.z2)(u) : void 0), [u])
+            t && !u && null == d && "" !== e && h(e);
+        }, [t, u, d, e, h]),
+        i.useMemo(() => (null != d ? (0, o.z2)(d) : void 0), [d])
     );
 };
