2f5bd94caa9a7fd09b65ac57be357220728941d2
DiscrapperManager <admin@minerpl.xyz>
2026-02-09 20:16:03 +0100

    canaryRelease: 494983
    Main scripts: 3
    Lazy Loaded Scripts: 1972
    Chunks:
     * Main Files: 13802
     * Scripts: 11727
    Strings: 23701
    Routes: 794
    Experiments Count: 215
    Apex Experiments Count: 149

--- chunks/33324.js
+++ chunks/33324.js
@@ -1,4 +1,4 @@
-n.d(t, { A: () => S }), n(321073);
+n.d(t, { A: () => E }), n(321073);
 var i = n(417597),
     l = n(41200),
     s = n(903223),
@@ -10,52 +10,49 @@ var i = n(417597),
     u = n(978165),
     h = n(960253),
     A = n(18905),
-    g = n(395504),
     m = n(488803),
     p = n(385160),
-    _ = n(363487),
-    x = n(871123),
+    g = n(363487),
+    _ = n(871123),
     f = n(916023),
-    E = n(590877),
-    C = n(281405),
-    I = n(652215);
-function S(e) {
+    x = n(281405),
+    C = n(652215);
+function E(e) {
     let t = (0, A.A)(e.id),
         n = (0, a.W)(e.id),
-        S = (0, h.vz)(e.id),
-        b = (0, l.r)(e),
-        N = (0, x.jz)(e),
-        T = (0, f.kt)({ location: "guild-action-rows" }),
-        j = (0, c.d)(e.id),
-        v = (0, i.bG)([o.h], () => o.h.getNewMemberActions(e.id), [e.id]),
-        y = (0, u.A)(e.id),
-        R = (0, d.jY)(e.id),
-        O = (0, r.fw)(e.id),
-        L = [],
-        D = e.features.has(I.GuildFeatures.HUB),
-        M = e.features.has(I.GuildFeatures.COMMUNITY),
-        G = (0, E.w)(!(0, g.WW)(e.id)),
-        U = e.features.has(I.GuildFeatures.ENABLED_MODERATION_EXPERIENCE_FOR_NON_COMMUNITY),
-        P = (0, _.A)(e.id);
+        E = (0, h.vz)(e.id),
+        I = (0, l.r)(e),
+        N = (0, _.jz)(e),
+        b = (0, f.kt)({ location: "guild-action-rows" }),
+        S = (0, c.d)(e.id),
+        T = (0, i.bG)([o.h], () => o.h.getNewMemberActions(e.id), [e.id]),
+        v = (0, u.A)(e.id),
+        y = (0, d.jY)(e.id),
+        j = (0, r.fw)(e.id),
+        R = [],
+        O = e.features.has(C.GuildFeatures.HUB),
+        L = e.features.has(C.GuildFeatures.COMMUNITY),
+        M = e.features.has(C.GuildFeatures.ENABLED_MODERATION_EXPERIENCE_FOR_NON_COMMUNITY),
+        D = (0, g.A)(e.id);
     (0, p.Ao)("useGuildActionRows");
-    let k = (0, m.C$)(e.id, "useGuildActionRows"),
-        w = e.features.has(I.GuildFeatures.GAME_SERVERS),
-        V = (0, s.ws)(e, { location: "guild-action-rows" });
+    let G = (0, m.C$)(e.id, "useGuildActionRows"),
+        U = e.features.has(C.GuildFeatures.GAME_SERVERS),
+        P = (0, s.ws)(e, { location: "guild-action-rows" });
     return (
-        D && L.push(C.n.GUILD_HUB_HEADER_OPTIONS),
-        !R && j && y && null != v && v.length > 0
-            ? L.push(C.n.GUILD_NEW_MEMBER_ACTIONS_PROGRESS_BAR)
-            : V && L.push(C.n.GUILD_PREMIUM_PROGRESS_BAR),
-        !D && j && L.push(C.n.GUILD_HOME),
-        t && L.push(C.n.GUILD_SCHEDULED_EVENTS),
-        !D && M && G && L.push(C.n.CHANNELS_AND_ROLES),
-        S && L.push(C.n.GUILD_ROLE_SUBSCRIPTIONS),
-        b && L.push(C.n.GUILD_SHOP),
-        N && T && L.push(C.n.GUILD_GAME_SHOP),
-        ((O && (M || U)) || (n && e.features.has(I.GuildFeatures.MEMBER_VERIFICATION_MANUAL_APPROVAL))) &&
-            L.push(C.n.GUILD_MOD_DASH_MEMBER_SAFETY),
-        P && L.push(C.n.GUILD_BOOSTS),
-        k && w && L.push(C.n.GAME_SERVERS),
-        L
+        O && R.push(x.n.GUILD_HUB_HEADER_OPTIONS),
+        !y && S && v && null != T && T.length > 0
+            ? R.push(x.n.GUILD_NEW_MEMBER_ACTIONS_PROGRESS_BAR)
+            : P && R.push(x.n.GUILD_PREMIUM_PROGRESS_BAR),
+        !O && S && R.push(x.n.GUILD_HOME),
+        t && R.push(x.n.GUILD_SCHEDULED_EVENTS),
+        !O && L && R.push(x.n.CHANNELS_AND_ROLES),
+        E && R.push(x.n.GUILD_ROLE_SUBSCRIPTIONS),
+        I && R.push(x.n.GUILD_SHOP),
+        N && b && R.push(x.n.GUILD_GAME_SHOP),
+        ((j && (L || M)) || (n && e.features.has(C.GuildFeatures.MEMBER_VERIFICATION_MANUAL_APPROVAL))) &&
+            R.push(x.n.GUILD_MOD_DASH_MEMBER_SAFETY),
+        D && R.push(x.n.GUILD_BOOSTS),
+        G && U && R.push(x.n.GAME_SERVERS),
+        R
     );
 }
