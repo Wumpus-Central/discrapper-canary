26827e1459da07339bfa3798a1e3464fde931359
DiscrapperManager <admin@minerpl.xyz>
2026-02-11 21:56:19 +0100

    canaryRelease: 495693
    Main scripts: 3
    Lazy Loaded Scripts: 1969
    Chunks:
     * Main Files: 13843
     * Scripts: 11706
    Strings: 23733
    Routes: 794
    Experiments Count: 205
    Apex Experiments Count: 157

--- chunks/549996.js
+++ chunks/549996.js
@@ -1,17 +1,17 @@
 "use strict";
-n.d(t, { c: () => u });
+n.d(t, { c: () => c });
 var r = n(64700),
     i = n(311907),
     a = n(816733),
     s = n(597758),
     o = n(412260),
     l = n(852218);
-function u(e) {
-    return (
-        r.useEffect(() => {
-            (0, s.BE)();
-        }, []),
-        (0, i.bG)([o.A, a.A], () => {
+let u = 864e5;
+function c(e) {
+    r.useEffect(() => {
+        (0, s.BE)();
+    }, []);
+    let t = (0, i.bG)([o.A, a.A], () => {
             let t = o.A.getMarketingComponentByType(e);
             if (null == t) return null;
             let n = t.promotionId,
@@ -21,6 +21,30 @@ function u(e) {
                 if (null == e || (null != e.expires_at && Date.parse(e.expires_at) < Date.now())) return null;
             }
             return t;
-        })
+        }),
+        n = (0, i.bG)([o.A], () => o.A.getPromotionByTypeAndId(l.pt.MARKETING_MOMENT, t?.promotionId ?? "")),
+        c = n?.endDate,
+        [d, _] = r.useState(!1),
+        f = r.useRef(null);
+    return (
+        r.useEffect(() => {
+            if (null != c) {
+                let e = c.getTime() - Date.now();
+                return (
+                    e > 0 && e < u
+                        ? (_(!1),
+                          clearTimeout(f.current),
+                          (f.current = setTimeout(() => {
+                              _(!0);
+                          }, e)))
+                        : e <= 0 && _(!0),
+                    () => {
+                        clearTimeout(f.current);
+                    }
+                );
+            }
+            _(!1), clearTimeout(f.current);
+        }, [c]),
+        d ? null : t
     );
 }
--- chunks/585265.js
+++ chunks/585265.js
@@ -1,42 +1,43 @@
-n.d(_, { default: () => u });
-var a = n(627968);
+"use strict";
+n.d(t, { default: () => b });
+var o = n(627968);
 n(64700);
 var r = n(158954),
-    o = n(397927),
-    t = n(744479),
+    i = n(397927),
+    a = n(744479),
     c = n(772538),
-    i = n(61610),
-    s = n(751461),
-    l = n(638324),
-    d = n(355097),
-    b = n(985018);
-function p(e) {
-    let { guildId: _ } = e;
-    return (0, a.jsxs)(t.A, {
+    s = n(61610),
+    l = n(751461),
+    d = n(638324),
+    u = n(355097),
+    _ = n(985018);
+function f(e) {
+    let { guildId: t } = e;
+    return (0, o.jsxs)(a.A, {
         children: [
-            (0, a.jsx)(s.A, { ingress: d.bf.SERVER_PRIVACY_MODAL, guildId: _ }),
-            (0, a.jsx)(l.A, { ingress: d.bf.SERVER_PRIVACY_MODAL, guildId: _ }),
-            (0, a.jsx)(i.A, { ingress: d.bf.SERVER_PRIVACY_MODAL, guildId: _ }),
-            (0, a.jsx)(c.A, { ingress: d.bf.SERVER_PRIVACY_MODAL, guildId: _ }),
-            (0, a.jsx)(o.Text, {
+            (0, o.jsx)(l.A, { ingress: u.bf.SERVER_PRIVACY_MODAL, guildId: t }),
+            (0, o.jsx)(d.A, { ingress: u.bf.SERVER_PRIVACY_MODAL, guildId: t }),
+            (0, o.jsx)(s.A, { ingress: u.bf.SERVER_PRIVACY_MODAL, guildId: t }),
+            (0, o.jsx)(c.A, { ingress: u.bf.SERVER_PRIVACY_MODAL, guildId: t }),
+            (0, o.jsx)(i.Text, {
                 color: "text-default",
                 variant: "text-sm/normal",
-                children: b.intl.format(b.t.LNsV04, {
+                children: _.intl.format(_.t.LNsV04, {
                     policyLink: "https://discord.com/safety/360043709612-our-policies",
                 }),
             }),
         ],
     });
 }
-function u(e) {
-    let { guild: _, transitionState: n, onClose: o } = e,
-        t = `${b.intl.string(b.t.BayiAo)}—${null != _ ? _.name : "??"}`;
-    return (0, a.jsx)(r.Modal, {
+function b(e) {
+    let { guild: t, transitionState: n, onClose: i } = e,
+        a = `${_.intl.string(_.t.BayiAo)}—${null != t ? t.name : "??"}`;
+    return (0, o.jsx)(r.Modal, {
         transitionState: n,
         size: "sm",
-        title: t,
-        actions: [{ onClick: () => o(), variant: "primary", text: b.intl.string(b.t.i4jeWR) }],
-        onClose: () => Promise.resolve(o()),
-        children: (0, a.jsx)(p, { guildId: _.id }),
+        title: a,
+        actions: [{ onClick: () => i(), variant: "primary", text: _.intl.string(_.t.i4jeWR) }],
+        onClose: () => Promise.resolve(i()),
+        children: (0, o.jsx)(f, { guildId: t.id }),
     });
 }
