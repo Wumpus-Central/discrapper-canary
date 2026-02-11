41e0c2aa6b53d04e4b836e2d8d02aac74ff56de0
DiscrapperManager <admin@minerpl.xyz>
2026-02-11 21:12:08 +0100

    canaryRelease: 495528
    Main scripts: 3
    Lazy Loaded Scripts: 1969
    Chunks:
     * Main Files: 13854
     * Scripts: 11688
    Strings: 23727
    Routes: 794
    Experiments Count: 205
    Apex Experiments Count: 154

--- chunks/620216.js
+++ chunks/620216.js
@@ -1,11 +1,10 @@
-"use strict";
-n.d(t, { t: () => r });
-var i,
-    r =
-        (((i = {}).ACCENT = "accent"),
-        (i.GREEN = "statusGreen"),
-        (i.YELLOW = "statusYellow"),
-        (i.ORANGE = "statusOrange"),
-        (i.RED = "statusRed"),
-        (i.NONE = "transparent"),
-        i);
+n.d(t, { t: () => a });
+var r,
+    a =
+        (((r = {}).ACCENT = "accent"),
+        (r.GREEN = "statusGreen"),
+        (r.YELLOW = "statusYellow"),
+        (r.ORANGE = "statusOrange"),
+        (r.RED = "statusRed"),
+        (r.NONE = "transparent"),
+        r);
