adaf7f5de4c5aeeebdf18f96aa0925f50c33a26b
DiscrapperManager <admin@minerpl.xyz>
2026-02-12 02:06:36 +0100

    canaryRelease: 496518
    Main scripts: 3
    Lazy Loaded Scripts: 1969
    Chunks:
     * Main Files: 13865
     * Scripts: 11691
    Strings: 23742
    Routes: 791
    Experiments Count: 203
    Apex Experiments Count: 157

--- chunks/857179.js
+++ chunks/857179.js
@@ -1,29 +1,31 @@
-n.d(t, { G: () => _, o: () => h });
+n.d(t, { G: () => p, o: () => m });
 var i = n(284009),
     r = n.n(i),
-    a = n(205693),
-    l = n(602674),
+    l = n(205693),
+    a = n(602674),
     s = n(76788),
-    o = n(536184),
-    d = n(430452),
-    c = n(904054);
-let u = new Map();
-async function A(e) {
-    let t = u.get(e);
+    o = n(738011),
+    d = n(536184),
+    c = n(430452),
+    u = n(728458),
+    A = n(904054);
+let h = new Map();
+async function _(e) {
+    let t = h.get(e);
     if (null != t) return t;
     let n = await (await fetch(e)).arrayBuffer(),
-        i = (0, l.v)(),
+        i = (0, a.v)(),
         r = await i?.decodeAudioData(n);
-    return null != r && u.set(e, r), r;
+    return null != r && h.set(e, r), r;
 }
-function h(e) {
-    let { soundKey: t, soundURL: n, soundVolume: i, reportSoundStartedPlaying: l } = e;
+function m(e) {
+    let { soundKey: t, soundURL: n, soundVolume: i, reportSoundStartedPlaying: a } = e;
     return new Promise(async (e, s) => {
-        let o = await A(n);
+        let o = await _(n);
         null == o && e(),
-            d.Ay.getMediaEngine().eachConnection((n) => {
-                n.context === a.x.DEFAULT &&
-                    (l(),
+            c.Ay.getMediaEngine().eachConnection((n) => {
+                n.context === l.x.DEFAULT &&
+                    (a(),
                     r()(null != o, "audioBuffer cannot be null here"),
                     n.startSamplesLocalPlayback(t, o, i, (t, n) => {
                         0 !== t ? s(Error(`${n}`)) : e();
@@ -31,22 +33,33 @@ function h(e) {
             });
     });
 }
-function _(e, t) {
-    let { soundKey: n, soundURL: i, soundVolume: r, reportSoundStartedPlaying: a } = e,
-        l = t.get(n);
-    if (null != l) {
-        l.currentTime = 0;
+function p(e, t) {
+    let { soundKey: n, soundURL: i, soundVolume: r, reportSoundStartedPlaying: l } = e,
+        a = t.get(n);
+    if (null != a) {
+        a.currentTime = 0;
         return;
     }
     return new Promise(async (e) => {
-        let l = new (await (0, o.A)(i))();
-        (l.src = i),
-            (l.volume = (0, c.A)(r)),
-            l.addEventListener(l instanceof s.A.OGVPlayer ? "loadedmetadata" : "canplaythrough", () => {
-                a(), t.set(n, l), l.play();
+        let a = new (await (0, d.A)(i))();
+        (a.src = i),
+            (a.volume = (0, A.A)(r)),
+            a.addEventListener(a instanceof s.A.OGVPlayer ? "loadedmetadata" : "canplaythrough", () => {
+                l(),
+                    t.set(n, a),
+                    a.play()?.catch((i) => {
+                        if ((0, o.u)(i)) {
+                            u.A.captureException(i, { tags: { errorType: "autoplay_policy", expected: "true" } }),
+                                t.delete(n),
+                                (a.src = ""),
+                                e();
+                            return;
+                        }
+                        throw i;
+                    });
             }),
-            l.addEventListener("ended", () => {
-                t.delete(n), (l.src = ""), e();
+            a.addEventListener("ended", () => {
+                t.delete(n), (a.src = ""), e();
             });
     });
 }
