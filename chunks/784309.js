e278be5c0b37b645a22a2fbe2aa9e94b90b6e2e6
DiscrapperManager <admin@minerpl.xyz>
2026-02-12 22:47:51 +0100

    canaryRelease: 496933
    Main scripts: 3
    Lazy Loaded Scripts: 1966
    Chunks:
     * Main Files: 13877
     * Scripts: 11690
    Strings: 23654
    Routes: 795
    Experiments Count: 201
    Apex Experiments Count: 159

--- chunks/784309.js
+++ chunks/784309.js
@@ -20,11 +20,11 @@ var i = n(627968),
     N = n(661191),
     j = n(427262),
     T = n(447066),
-    C = n(652215),
-    v = n(985018),
-    E = n(416337);
-function I(e, t) {
-    return null != t && /^data:/.test(t) ? t : (0, _.ku)({ id: e.id, avatar: t, discriminator: C.h3J });
+    v = n(652215),
+    C = n(985018),
+    I = n(416337);
+function E(e, t) {
+    return null != t && /^data:/.test(t) ? t : (0, _.ku)({ id: e.id, avatar: t, discriminator: v.h3J });
 }
 function S(e) {
     let {
@@ -40,23 +40,23 @@ function S(e) {
         [R, k] = l.useState(!1),
         [L] = l.useState(new o.Ep());
     l.useEffect(() => () => L.stop(), [L]);
-    let M = l.useMemo(() => I(n, n.avatar), [n]),
+    let M = l.useMemo(() => E(n, n.avatar), [n]),
         G = l.useCallback(() => {
-            let e = `${(0, r.TP)(!1)}${C.Rsh.WEBHOOK_INTEGRATION(n.id, n.token)}`;
+            let e = `${(0, r.TP)(!1)}${v.Rsh.WEBHOOK_INTEGRATION(n.id, n.token)}`;
             (0, f.C)(e);
         }, [n]),
         U = l.useCallback(() => {
             (0, d.A)({
-                title: v.intl.formatToPlainString(v.t.QVFjHh, { name: n.name }),
-                subtitle: v.intl.format(v.t["rIWe+5"], { name: n.name }),
-                confirmText: v.intl.string(v.t["W+K1Fs"]),
-                cancelText: v.intl.string(v.t.xNhj0O),
+                title: C.intl.formatToPlainString(C.t.QVFjHh, { name: n.name }),
+                subtitle: C.intl.format(C.t["rIWe+5"], { name: n.name }),
+                confirmText: C.intl.string(C.t["W+K1Fs"]),
+                cancelText: C.intl.string(C.t.xNhj0O),
                 onConfirm: () => {
                     x.A.delete(n.guild_id, n.id).catch((e) => {
                         let { status: t } = e;
                         429 === t
-                            ? m.A.show({ title: v.intl.string(v.t.N5riYn), body: v.intl.string(v.t.eAxcCc) })
-                            : m.A.show({ title: v.intl.string(v.t.N5riYn), body: v.intl.string(v.t["/4TwKf"]) });
+                            ? m.A.show({ title: C.intl.string(C.t.N5riYn), body: C.intl.string(C.t.eAxcCc) })
+                            : m.A.show({ title: C.intl.string(C.t.N5riYn), body: C.intl.string(C.t["/4TwKf"]) });
                     });
                 },
             });
@@ -65,7 +65,7 @@ function S(e) {
     null != n.user
         ? D.push({
               icon: u.O4,
-              text: v.intl.formatToPlainString(v.t["7EcUbr"], {
+              text: C.intl.formatToPlainString(C.t["7EcUbr"], {
                   user: ((e) => {
                       if (null == e) return null;
                       let t = new A.A(e);
@@ -76,23 +76,23 @@ function S(e) {
           })
         : D.push({
               icon: u.O4,
-              text: v.intl.formatToPlainString(v.t["7mv59O"], { timestamp: N.default.extractTimestamp(n.id) }),
+              text: C.intl.formatToPlainString(C.t["7mv59O"], { timestamp: N.default.extractTimestamp(n.id) }),
           });
     let P = null;
     return (
         S &&
             null != s &&
             (P = (0, i.jsxs)("div", {
-                className: E.rf,
+                className: I.rf,
                 children: [
-                    (0, i.jsx)(u.cGx, { className: E.fV }),
+                    (0, i.jsx)(u.cGx, { className: I.fV }),
                     (0, i.jsxs)(h.A, {
                         children: [
                             (0, i.jsx)(h.A.Child, {
                                 shrink: 1,
                                 grow: 0,
                                 children: (0, i.jsxs)(h.A, {
-                                    className: E.R3,
+                                    className: I.R3,
                                     direction: h.A.Direction.VERTICAL,
                                     children: [
                                         (0, i.jsx)(p.A, {
@@ -100,8 +100,8 @@ function S(e) {
                                             onChange: (e) => {
                                                 g.A.updateWebhook({ avatar: e });
                                             },
-                                            makeURL: (e) => I(n, e),
-                                            imageClassName: E.mr,
+                                            makeURL: (e) => E(n, e),
+                                            imageClassName: I.mr,
                                             showIcon: !0,
                                         }),
                                         null != O.avatar && "" !== O.avatar
@@ -123,7 +123,7 @@ function S(e) {
                                                 wrap: !0,
                                                 basis: "50%",
                                                 children: (0, i.jsx)(u.ksK, {
-                                                    label: v.intl.string(v.t.ukdxuo),
+                                                    label: C.intl.string(C.t.ukdxuo),
                                                     value: s.name,
                                                     onChange: (e) => {
                                                         g.A.updateWebhook({ name: e });
@@ -136,7 +136,7 @@ function S(e) {
                                                 basis: "50%",
                                                 children: (0, i.jsx)("div", {
                                                     children: (0, i.jsx)(u.ZiE, {
-                                                        label: v.intl.string(v.t.GK18KJ),
+                                                        label: C.intl.string(C.t.GK18KJ),
                                                         value: s.channel_id,
                                                         options: _,
                                                         formatOption: (e) => {
@@ -146,30 +146,32 @@ function S(e) {
                                                         onSelectionChange: (e) => {
                                                             g.A.updateWebhook({ channelId: e });
                                                         },
-                                                        placeholder: v.intl.string(v.t.r2ptsz),
+                                                        placeholder: C.intl.string(C.t.r2ptsz),
                                                         selectionMode: "single",
                                                     }),
                                                 }),
                                             }),
                                         ],
                                     }),
-                                    (0, i.jsx)(u.cGx, { className: E.Bd }),
+                                    (0, i.jsx)(u.cGx, { className: I.Bd }),
                                     (0, i.jsxs)(h.A, {
                                         children: [
                                             (0, i.jsx)(c.m_, {
-                                                text: v.intl.string(v.t.wwdb3g),
+                                                text: C.intl.string(C.t.wwdb3g),
                                                 shouldShow: R,
+                                                hideOnClick: !1,
+                                                forceOpen: R,
                                                 children: (0, i.jsx)("div", {
                                                     "data-button-hoisted-classname-wrapper": !0,
-                                                    className: E.cL,
+                                                    className: I.cL,
                                                     children: (0, i.jsx)(u.Button, {
                                                         variant: "secondary",
                                                         size: "sm",
-                                                        text: v.intl.string(v.t.Ae9rUW),
+                                                        text: C.intl.string(C.t.Ae9rUW),
                                                         "aria-label": "",
                                                         onClick: () => {
                                                             k(!0),
-                                                                u.ORC.announce(v.intl.string(v.t.wwdb3g)),
+                                                                u.ORC.announce(C.intl.string(C.t.wwdb3g)),
                                                                 L.start(1e3, () => k(!1)),
                                                                 G();
                                                         },
@@ -180,7 +182,7 @@ function S(e) {
                                             (0, i.jsx)(u.Button, {
                                                 variant: "critical-secondary",
                                                 size: "sm",
-                                                text: v.intl.string(v.t.jVrUnC),
+                                                text: C.intl.string(C.t.jVrUnC),
                                                 onClick: U,
                                             }),
                                         ],
@@ -194,19 +196,19 @@ function S(e) {
         (0, i.jsx)(u.ZpM, {
             editable: !0,
             id: t,
-            className: a()(E.Nr, y ? E.U6 : null),
+            className: a()(I.Nr, y ? I.U6 : null),
             children: (0, i.jsxs)(h.A, {
                 direction: h.A.Direction.VERTICAL,
                 children: [
                     (0, i.jsx)(u.DUT, {
-                        className: E.wx,
+                        className: I.wx,
                         "aria-expanded": S,
                         onClick: w,
                         children: (0, i.jsxs)(h.A, {
                             align: h.A.Align.CENTER,
                             children: [
                                 (0, i.jsx)(T.A, { name: n.name, imageSrc: M, details: D }),
-                                (0, i.jsx)(b.A, { className: E.eO, expanded: S, "aria-hidden": !0 }),
+                                (0, i.jsx)(b.A, { className: I.eO, expanded: S, "aria-hidden": !0 }),
                             ],
                         }),
                     }),
