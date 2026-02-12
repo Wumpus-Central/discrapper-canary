7d5f149d3c80e0eec1ced199b72ca85cb5f59524
DiscrapperManager <admin@minerpl.xyz>
2026-02-12 20:12:56 +0100

    canaryRelease: 496752
    Main scripts: 3
    Lazy Loaded Scripts: 1965
    Chunks:
     * Main Files: 13874
     * Scripts: 11686
    Strings: 23638
    Routes: 791
    Experiments Count: 201
    Apex Experiments Count: 158

--- chunks/646986.js
+++ chunks/646986.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { A: () => I }), n(321073);
+n.d(t, { A: () => y }), n(321073);
 var r = n(627968),
     i = n(64700),
     a = n(311907),
@@ -8,50 +8,52 @@ var r = n(627968),
     l = n(290863),
     u = n(461213),
     c = n(183555),
-    d = n(146655),
-    _ = n(489379),
-    f = n(80330),
+    d = n(622543),
+    _ = n(146655),
+    f = n(489379),
+    h = n(80330),
     p = n(837924),
-    h = n(842241),
-    m = n(282797),
-    g = n(518477),
-    E = n(818348),
-    A = n(985018);
-function I(e) {
-    let { user: t, currentUser: n, guildId: I, onOpenUserProfileModal: T, onClose: y } = e,
-        { analyticsLocations: S } = (0, o.Ay)(),
-        { trackUserProfileAction: v } = (0, c.NJ)(),
-        { live: C, stream: b } = (0, d.A)(t.id),
-        { voiceChannel: N, voiceActivity: R } = (0, _.A)({ userId: t.id, guildId: I }),
-        O = t.id === n.id,
-        D = (0, a.bG)([u.A, l.A], () => {
-            let e = O ? u.A.getStatus() : l.A.getStatus(t.id, I);
-            return e === E.cl.OFFLINE || e === E.cl.INVISIBLE;
+    g = n(842241),
+    E = n(282797),
+    A = n(518477),
+    I = n(818348),
+    T = n(985018);
+function y(e) {
+    let { user: t, currentUser: n, guildId: y, onOpenUserProfileModal: S, onClose: v } = e,
+        { analyticsLocations: C } = (0, o.Ay)(),
+        { trackUserProfileAction: b } = (0, c.NJ)(),
+        { live: N, stream: R } = (0, _.A)(t.id),
+        { voiceChannel: O, voiceActivity: D } = (0, f.A)({ userId: t.id, guildId: y }),
+        L = t.id === n.id,
+        w = (0, a.bG)([u.A, l.A], () => {
+            let e = L ? u.A.getStatus() : l.A.getStatus(t.id, y);
+            return e === I.cl.OFFLINE || e === I.cl.INVISIBLE;
         }),
-        { voiceActivityStatusEnabled: L } = (0, s.G)({ location: "UserProfileStackedActivity" }),
-        w = L && null == b && null == R && null != N,
-        x = i.useCallback(
+        x = (0, a.bG)([d.A], () => d.A.getUserProfile(t.id)?.private === !0),
+        { voiceActivityStatusEnabled: P } = (0, s.G)({ location: "UserProfileStackedActivity" }),
+        M = P && null == R && null == D && null != O,
+        k = i.useCallback(
             (e) => {
                 let i = [],
-                    a = { ...e, user: t, currentUser: n, onClose: y };
+                    a = { ...e, user: t, currentUser: n, onClose: v };
                 return (
-                    null != b && i.push((0, r.jsx)(h.A, { stream: b, ...a }, "stream")),
-                    C.forEach((e, t) => {
+                    null != R && i.push((0, r.jsx)(g.A, { stream: R, ...a }, "stream")),
+                    N.forEach((e, t) => {
                         i.push((0, r.jsx)(p.A, { activity: e, ...a }, `live-${t}`));
                     }),
-                    w && i.push((0, r.jsx)(m.A, { voiceChannel: N, ...a }, "voice")),
+                    M && i.push((0, r.jsx)(E.A, { voiceChannel: O, ...a }, "voice")),
                     i
                 );
             },
-            [n, w, C, y, b, t, N],
+            [n, M, N, v, R, t, O],
         );
-    return D
+    return w || x
         ? null
-        : (0, r.jsx)(f.A, {
-              renderCards: x,
-              heading: A.intl.string(A.t.J6STd9),
+        : (0, r.jsx)(h.A, {
+              renderCards: k,
+              heading: T.intl.string(T.t.J6STd9),
               onExpand: () => {
-                  v({ action: "PRESS_SHOW_MORE_ACTIVITY", analyticsLocations: S }), T?.({ tabSection: g.RP.ACTIVITY });
+                  b({ action: "PRESS_SHOW_MORE_ACTIVITY", analyticsLocations: C }), S?.({ tabSection: A.RP.ACTIVITY });
               },
           });
 }
