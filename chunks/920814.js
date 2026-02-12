b9e20c79718aa28277208e6ee6f13f0dc740359d
DiscrapperManager <admin@minerpl.xyz>
2026-02-12 23:31:18 +0100

    noisesSuppression 496983

--- chunks/920814.js
+++ chunks/920814.js
@@ -1,10 +1,8 @@
 n.d(t, { B: () => l });
 var i,
     l =
-        21552 == n.j
-            ? (((i = {}).GUILD_PRODUCTS = "guild_products"),
-              (i.GUILD_ROLE_SUBSCRIPTIONS = "guild_role_subscriptions"),
-              (i.GUILD_PRODUCTS_PREVIEW = "guild_products_preview"),
-              (i.GUILD_SHOP_FULL_PREVIEW = "guild_shop_full_preview"),
-              i)
-            : null;
+        (((i = {}).GUILD_PRODUCTS = "guild_products"),
+        (i.GUILD_ROLE_SUBSCRIPTIONS = "guild_role_subscriptions"),
+        (i.GUILD_PRODUCTS_PREVIEW = "guild_products_preview"),
+        (i.GUILD_SHOP_FULL_PREVIEW = "guild_shop_full_preview"),
+        i);
