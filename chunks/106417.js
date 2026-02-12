48c0a96cde41d8118f7354f807c97ed0e3007b75
DiscrapperManager <admin@minerpl.xyz>
2026-02-12 22:22:05 +0100

    canaryRelease: 496909
    Main scripts: 3
    Lazy Loaded Scripts: 1965
    Chunks:
     * Main Files: 13877
     * Scripts: 11686
    Strings: 23645
    Routes: 791
    Experiments Count: 201
    Apex Experiments Count: 158

--- chunks/106417.js
+++ chunks/106417.js
@@ -3,9 +3,9 @@ e.exports = {
     VU: "withGradient__553bf",
     pC: "withTakeoverContent__553bf",
     jR: "withOnboardingContent__553bf",
+    VG: "takeoverBackgroundImage__553bf",
     qS: "badge__553bf",
     q2: "shine__553bf",
     ys: "glow__553bf",
-    VG: "takeoverBackgroundImage__553bf",
     cl: "takeoverTooltipImage__553bf",
 };
