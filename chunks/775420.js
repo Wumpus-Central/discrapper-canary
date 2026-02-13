f1c0338fbbef03f55ca7a615a62db960c93d6b92
DiscrapperManager <admin@minerpl.xyz>
2026-02-13 20:20:29 +0100

    canaryRelease: 497418
    Main scripts: 3
    Lazy Loaded Scripts: 1964
    Chunks:
     * Main Files: 13877
     * Scripts: 11680
    Strings: 23661
    Routes: 796
    Experiments Count: 200
    Apex Experiments Count: 160

--- chunks/775420.js
+++ chunks/775420.js
@@ -1,41 +1,43 @@
 "use strict";
-n.d(t, { e6: () => d }), n(627968);
+n.d(t, { e6: () => _ }), n(627968);
 var r = n(64700),
     i = n(835245),
     a = n(311907),
     s = n(674658),
     o = n(623373),
     l = n(287809),
-    u = n(927578),
-    c = n(715054);
+    u = n(739508),
+    c = n(927578),
+    d = n(715054);
 (0, i.A)();
-let d = (e) => {
-    let { skuId: t, loadId: n, analyticsLocations: i, onCheckoutSuccess: d } = e,
-        _ = (0, a.bG)([l.default], () => u.Ay.canUseCollectibles(l.default.getCurrentUser())),
-        { product: f } = (0, s.q)(t),
-        h = (0, r.useMemo)(() => {
-            if (null == f) return null;
-            let e = (0, o.CW)({ product: f, isPremiumUser: _ }),
+let _ = (e) => {
+    let { skuId: t, loadId: n, analyticsLocations: i, onCheckoutSuccess: _ } = e,
+        f = (0, a.bG)([l.default], () => c.Ay.canUseCollectibles(l.default.getCurrentUser())),
+        { product: h } = (0, s.q)(t),
+        p = (0, r.useMemo)(() => {
+            if (null == h) return null;
+            let e = (0, o.CW)({ product: h, isPremiumUser: f }),
                 t = null !== e ? e.amount : null;
-            return { orbPrice: e, orbPriceAmount: t, product: f };
-        }, [f, _]),
-        { redeemVirtualCurrency: p, isSubmitting: g, error: E } = (0, c.Q)({ skuId: t, loadId: n }),
-        A = (0, r.useCallback)(
+            return { orbPrice: e, orbPriceAmount: t, product: h };
+        }, [h, f]);
+    p?.orbPriceAmount == null && (0, u.hD)("Orb price not found for product", { tags: { sku_id: t } });
+    let { redeemVirtualCurrency: g, isSubmitting: E, error: A } = (0, d.Q)({ skuId: t, loadId: n }),
+        I = (0, r.useCallback)(
             (e) => {
-                p(t, n, (n) => {
-                    d({ entitlements: n, skuId: t }), e();
+                g(t, n, (n) => {
+                    _({ entitlements: n, skuId: t }), e();
                 });
             },
-            [t, n, p, d],
+            [t, n, g, _],
         );
     return {
         skuId: t,
         loadId: n,
         analyticsLocations: i ?? [],
-        product: f,
-        orbProductContext: h,
-        onRedeemVirtualCurrency: A,
-        isRedeeming: g,
-        orbRedemptionError: E,
+        product: h,
+        orbProductContext: p,
+        onRedeemVirtualCurrency: I,
+        isRedeeming: E,
+        orbRedemptionError: A,
     };
 };
