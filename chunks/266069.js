331751c5fa90da96e71820c2808eaeda1abfb45f
DiscrapperManager <admin@minerpl.xyz>
2026-02-14 02:08:56 +0100

    canaryRelease: 497690
    Main scripts: 3
    Lazy Loaded Scripts: 1967
    Chunks:
     * Main Files: 13876
     * Scripts: 11688
    Strings: 23669
    Routes: 796
    Experiments Count: 199
    Apex Experiments Count: 160

--- chunks/266069.js
+++ chunks/266069.js
@@ -10,38 +10,38 @@ var i = n(627968),
     u = n(242919),
     h = n(985018),
     A = n(519501),
-    g = n(658122);
+    p = n(658122);
 function m(e) {
-    let { label: t, icon: n, setStatus: s, isFavorited: m, onFavoriteClick: p } = e,
-        [_, x] = l.useState(!1),
-        f = (0, r.bG)([u.A], () => u.A.getFavoritedStatuses().length >= u.x),
-        E = null != p,
-        C = l.useCallback(
+    let { label: t, icon: n, setStatus: s, isFavorited: m, onFavoriteClick: g } = e,
+        [_, f] = l.useState(!1),
+        x = (0, r.bG)([u.A], () => u.A.getFavoritedStatuses().length >= u.x),
+        C = null != g,
+        E = l.useCallback(
             (e) => {
-                e.stopPropagation(), p?.();
+                e.stopPropagation(), g?.();
             },
-            [p],
+            [g],
         ),
         I = m ? c.Gg5 : c.yA2;
     return (0, i.jsxs)(c.DUT, {
         onClick: s,
         onMouseEnter: () => {
-            x(!0);
+            f(!0);
         },
         onMouseLeave: () => {
-            x(!1);
+            f(!1);
         },
-        className: a()(g.item, g.labelContainer, A.kL),
+        className: a()(p.item, p.labelContainer, A.kL),
         children: [
-            (0, i.jsx)("div", { children: n }),
+            (0, i.jsx)("div", { className: A.P0, children: n }),
             (0, i.jsx)(c.Text, { variant: "text-md/normal", className: A.rv, children: t }),
             _ &&
-                E &&
+                C &&
                 (0, i.jsx)(d.m_, {
-                    text: f && !m ? h.intl.string(h.t.YSDH9n) : void 0,
+                    text: x && !m ? h.intl.string(h.t.YSDH9n) : void 0,
                     children: (0, i.jsx)(c.DUT, {
                         className: a()(A.ff, A.wB),
-                        onClick: C,
+                        onClick: E,
                         children: (0, i.jsx)(I, {
                             className: A.ut,
                             color: m ? o.A.colors.ICON_FEEDBACK_WARNING : "currentColor",
--- chunks/519501.js
+++ chunks/519501.js
@@ -1,5 +1,6 @@
 e.exports = {
     kL: "container__8c6be",
+    P0: "iconWrapper__8c6be",
     rv: "itemText__8c6be",
     ut: "closeIcon__8c6be",
     wB: "favoriteIcon__8c6be",
