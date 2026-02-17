6f4ff266b5b7c5cbad51b6b2363d71237238ecf8
DiscrapperManager <admin@minerpl.xyz>
2026-02-18 00:09:41 +0100

    canaryRelease: 498190
    Main scripts: 3
    Lazy Loaded Scripts: 1967
    Chunks:
     * Main Files: 13893
     * Scripts: 11699
    Strings: 23712
    Routes: 798
    Experiments Count: 197
    Apex Experiments Count: 166

--- chunks/533923.js
+++ chunks/533923.js
@@ -1,61 +1,62 @@
-n.d(t, { A: () => p });
+n.d(t, { A: () => f });
 var i = n(684013),
     l = n(334738),
     a = n(258585),
     s = n(72432),
     r = n(929921),
-    o = n(592598),
-    d = n(395011),
-    u = n(581730),
-    c = n(672396),
-    h = n(652215),
-    A = n(753070),
-    m = n(985018),
+    o = n(616356),
+    d = n(592598),
+    u = n(395011),
+    c = n(581730),
+    h = n(672396),
+    A = n(652215),
+    m = n(753070),
+    p = n(985018),
     g = n(426127);
-function p(e, t, n, p) {
-    if (o.A.isNotificationDisabled(c.KS.RequestToStream)) return null;
-    let f = t.username,
-        _ = m.intl.format(g.default.jTbTAF, { username: "", game: n.name }),
-        E = t.getAvatarURL(e.guild_id, 80),
-        { trackView: x, trackClick: S } = (0, u.Y9)(c.KS.RequestToStream, {
-            notif_type: c.KS.RequestToStream,
+function f(e, t, n, f) {
+    if (d.A.isNotificationDisabled(h.KS.RequestToStream) || null != o.A.getCurrentUserActiveStream()) return null;
+    let _ = t.username,
+        E = p.intl.format(g.default.jTbTAF, { username: "", game: n.name }),
+        x = t.getAvatarURL(e.guild_id, 80),
+        { trackView: S, trackClick: I } = (0, c.Y9)(h.KS.RequestToStream, {
+            notif_type: h.KS.RequestToStream,
             notif_user_id: t.id,
-            activity_type: h.xL.STREAM_REQUEST,
-            activity_name: p.name,
+            activity_type: A.xL.STREAM_REQUEST,
+            activity_name: f.name,
         });
     return {
-        icon: E,
-        title: f,
-        body: _,
-        confirmText: m.intl.string(g.default.UGbmBp),
-        cancelText: m.intl.string(m.t["tpXzJ+"]),
+        icon: x,
+        title: _,
+        body: E,
+        confirmText: p.intl.string(g.default.UGbmBp),
+        cancelText: p.intl.string(p.t["tpXzJ+"]),
         onNotificationShow: () => {
-            x();
+            S();
         },
         onConfirmClick: (e, t) => {
             let n = r.A.getState().preset;
-            if (n === A.jQ.PRESET_DOCUMENTS) {
+            if (n === m.jQ.PRESET_DOCUMENTS) {
                 let { allowAutoQuality: e } = (0, a.eO)({ location: "requestToStreamNotification" });
-                n = e ? A.jQ.PRESET_AUTO : A.jQ.PRESET_VIDEO;
+                n = e ? m.jQ.PRESET_AUTO : m.jQ.PRESET_VIDEO;
             }
-            (0, s.A)(d.A.getTargetPID(), { preset: n }), S("request-to-stream"), i.A.updateNotificationStatus(t);
+            (0, s.A)(u.A.getTargetPID(), { preset: n }), I("request-to-stream"), i.A.updateNotificationStatus(t);
         },
         onCancelClick: (t, n) => {
             (0, l.ack)(
                 e.id,
                 {
-                    section: h.JJy.OVERLAY,
-                    object: h.ZSU.ACK_DECLINE_REQUEST_TO_STREAM,
-                    objectType: h.AnalyticsObjectTypes.ACK_SEMI_AUTOMATIC,
+                    section: A.JJy.OVERLAY,
+                    object: A.ZSU.ACK_DECLINE_REQUEST_TO_STREAM,
+                    objectType: A.AnalyticsObjectTypes.ACK_SEMI_AUTOMATIC,
                 },
                 !0,
                 !0,
             ),
                 i.A.updateNotificationStatus(n),
-                S("decline");
+                I("decline");
         },
         onDismissClick: () => {
-            S("dismiss");
+            I("dismiss");
         },
     };
 }
