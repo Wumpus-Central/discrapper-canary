08d63d90c2cbd9032f74f0fb215155d727b270bd
DiscrapperManager <admin@minerpl.xyz>
2026-02-17 21:42:39 +0100

    canaryRelease: 498048
    Main scripts: 3
    Lazy Loaded Scripts: 1967
    Chunks:
     * Main Files: 13891
     * Scripts: 11696
    Strings: 23679
    Routes: 797
    Experiments Count: 198
    Apex Experiments Count: 164

--- chunks/169604.js
+++ chunks/169604.js
@@ -1,13 +1,10 @@
 n.d(t, { A: () => l });
 var i = n(73153);
-let l =
-    21552 == n.j
-        ? {
-              clearRemoteDisconnectVoiceChannelId() {
-                  i.h.dispatch({ type: "CLEAR_REMOTE_DISCONNECT_VOICE_CHANNEL_ID" });
-              },
-              clearLastSessionVoiceChannelId() {
-                  i.h.dispatch({ type: "CLEAR_LAST_SESSION_VOICE_CHANNEL_ID" });
-              },
-          }
-        : null;
+let l = {
+    clearRemoteDisconnectVoiceChannelId() {
+        i.h.dispatch({ type: "CLEAR_REMOTE_DISCONNECT_VOICE_CHANNEL_ID" });
+    },
+    clearLastSessionVoiceChannelId() {
+        i.h.dispatch({ type: "CLEAR_LAST_SESSION_VOICE_CHANNEL_ID" });
+    },
+};
--- chunks/832213.js
+++ chunks/832213.js
@@ -2,6 +2,8 @@
 n.d(t, { D: () => r });
 class r {
     cpuCoreCount;
+    setShouldCollectHermesInstrumentedStats(e) {}
+    getCurrentHermesInstrumentedStatsSummary() {}
     getCPUCoreCount() {
         return this.cpuCoreCount;
     }
