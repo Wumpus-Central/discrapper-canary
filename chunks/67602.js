4b15f23be1c448f09ddc807fede3aba2856b4a80
DiscrapperManager <admin@minerpl.xyz>
2026-02-17 20:29:31 +0100

    canaryRelease: 497990
    Main scripts: 3
    Lazy Loaded Scripts: 1967
    Chunks:
     * Main Files: 13890
     * Scripts: 11693
    Strings: 23679
    Routes: 797
    Experiments Count: 199
    Apex Experiments Count: 163

--- chunks/67602.js
+++ chunks/67602.js
@@ -1,24 +1,34 @@
-n.d(t, { A: () => d });
-var i = n(627968);
-n(64700);
-var r = n(975807),
-    l = n(64983),
-    a = n(652215),
-    s = n(49999),
-    o = n(985018);
-function d(e) {
+n.d(t, { A: () => h });
+var i = n(627968),
+    r = n(311907),
+    l = n(975807),
+    a = n(429913),
+    s = n(64983),
+    o = n(486020),
+    d = n(125022),
+    c = n(652215),
+    u = n(49999),
+    A = n(985018);
+function h(e) {
     let { guild: t, markAsDismissed: n } = e,
-        d = t.features.has(a.GuildFeatures.VERIFIED) ? o.intl.string(o.t.uUARXe) : o.intl.string(o.t["0Dx29f"]);
-    return (0, i.jsx)(l.A, {
+        h = (0, r.bG)([d.A], () => d.A.getUnclaimedGameIdsForGuild(t.id), [t.id]) ?? [],
+        _ = h[0],
+        m = (0, a.h)(_);
+    if (0 === h.length) return null;
+    let p = m?.name ?? A.intl.string(A.t.VQq92a),
+        g = o.Ay.getGameAssetURL({ id: _, hash: m?.coverImage, keepAspectRatio: !0 }),
+        E = t.features.has(c.GuildFeatures.VERIFIED) ? A.intl.string(A.t.uUARXe) : A.intl.string(A.t["0Dx29f"]);
+    return (0, i.jsx)(s.A, {
         guild: t,
-        onDismissed: () => n(s.i.USER_DISMISS),
-        title: o.intl.format(o.t.Q11WTQ, { gameName: "your game" }),
-        message: d,
-        cta: o.intl.string(o.t["2u6ZlY"]),
-        type: a.n5X.GAME_CLAIM,
-        image: "",
+        onDismissed: () => n(u.i.USER_DISMISS),
+        title: A.intl.format(A.t.Q11WTQ, { gameName: p }),
+        message: E,
+        cta: A.intl.string(A.t["2u6ZlY"]),
+        type: c.n5X.GAME_CLAIM,
+        image: g ?? "",
+        imageMarginX: 100,
         onClick: () => {
-            (0, r.A)(a.X7G.DEVELOPER_PORTAL_APPLICATIONS_GAME_IDENTITY), n(s.i.TAKE_ACTION);
+            (0, l.A)(c.X7G.DEVELOPER_PORTAL_APPLICATIONS_GAME_IDENTITY), n(u.i.TAKE_ACTION);
         },
     });
 }
--- chunks/878227.js
+++ chunks/878227.js
@@ -1,5 +1,14 @@
-n.d(t, { I: () => r });
-var i = n(558593);
-function r(e) {
-    return (0, i.q)(e ?? void 0, "useCanShowGameClaimCoachmark");
+n.d(t, { I: () => o });
+var i = n(311907),
+    r = n(576705),
+    l = n(125022),
+    a = n(558593),
+    s = n(652215);
+function o(e) {
+    let t = (0, a.q)(e, "useCanShowGameClaimCoachmark");
+    return (0, i.bG)(
+        [r.A, l.A],
+        () => !!t && !!r.A.canWithPartialContext(s.xBc.ADMINISTRATOR, { guildId: e }) && l.A.hasUnclaimedGames(e),
+        [e, t],
+    );
 }
