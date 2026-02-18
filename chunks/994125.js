038fc19d0b058ce465469986d13816c9cbfb5d5e
DiscrapperManager <admin@minerpl.xyz>
2026-02-18 04:50:51 +0100

    canaryRelease: 498370
    Main scripts: 3
    Lazy Loaded Scripts: 1997
    Chunks:
     * Main Files: 13900
     * Scripts: 11733
    Strings: 23723
    Routes: 798
    Experiments Count: 197
    Apex Experiments Count: 167

--- chunks/994125.js
+++ chunks/994125.js
@@ -1,15 +1,39 @@
-n.d(t, { K: () => o });
+n.d(t, { K: () => d });
 var a = n(64700),
-    s = n(311907),
-    i = n(73153),
-    l = n(429707),
-    r = n(274303);
-function o() {
-    let e = (0, s.cf)([r.A], () => ({ isLoading: r.A.getIsValidatingUsers(), multiAccountUsers: r.A.getUsers() }));
+    i = n(311907),
+    s = n(73153),
+    l = n(287809),
+    r = n(429707),
+    o = n(274303);
+function d() {
+    let e = (0, i.cf)([o.A, l.default], () => {
+        let e = o.A.getUsers(),
+            t = l.default.getCurrentUser();
+        return null == t ||
+            e.some((e) => {
+                let { id: n } = e;
+                return n === t.id;
+            })
+            ? { isLoading: o.A.getIsValidatingUsers(), multiAccountUsers: e }
+            : {
+                  isLoading: o.A.getIsValidatingUsers(),
+                  multiAccountUsers: [
+                      {
+                          id: t.id,
+                          avatar: t.avatar,
+                          username: t.username,
+                          discriminator: t.discriminator,
+                          tokenStatus: o.U.VALID,
+                          pushSyncToken: null,
+                      },
+                      ...e,
+                  ],
+              };
+    });
     return (
         a.useEffect(() => {
-            i.h.wait(() => {
-                l.F6();
+            s.h.wait(() => {
+                r.F6();
             });
         }, []),
         e
