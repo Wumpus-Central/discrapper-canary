fa8462b75311f91d75c7f691c3aca1ea10f6a66e
DiscrapperManager <admin@minerpl.xyz>
2026-02-18 20:07:41 +0100

    canaryRelease: 498622
    Main scripts: 3
    Lazy Loaded Scripts: 2031
    Chunks:
     * Main Files: 13252
     * Scripts: 12399
    Strings: 23745
    Routes: 798
    Experiments Count: 196
    Apex Experiments Count: 170

--- chunks/532197.js
+++ chunks/532197.js
@@ -1,17 +1,18 @@
-t.d(n, { A: () => A });
-var s = t(627968),
-    i = t(64700),
-    l = t(503698),
-    r = t.n(l),
-    a = t(397927),
-    d = t(262633);
-let o = { UP: "up", RIGHT: "right", DOWN: "down", LEFT: "left" },
-    c = { [o.UP]: "", [o.RIGHT]: d.pG, [o.DOWN]: d.TR, [o.LEFT]: d.kb };
-class u extends i.PureComponent {
-    static Directions = o;
+"use strict";
+n.d(t, { A: () => _ });
+var a = n(627968),
+    l = n(64700),
+    r = n(503698),
+    s = n.n(r),
+    i = n(397927),
+    o = n(262633);
+let d = { UP: "up", RIGHT: "right", DOWN: "down", LEFT: "left" },
+    c = { [d.UP]: "", [d.RIGHT]: o.pG, [d.DOWN]: o.TR, [d.LEFT]: o.kb };
+class u extends l.PureComponent {
+    static Directions = d;
     render() {
-        let { direction: e, className: n } = this.props;
-        return (0, s.jsx)(a.z$m, { size: "md", color: "currentColor", className: r()(n, { [c[e]]: !0 }) });
+        let { direction: e, className: t } = this.props;
+        return (0, a.jsx)(i.z$m, { size: "md", color: "currentColor", className: s()(t, { [c[e]]: !0 }) });
     }
 }
-let A = u;
+let _ = u;
