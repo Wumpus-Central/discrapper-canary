397df3fb6443c6ef9ab2983001eea70097444ec2
DiscrapperManager <admin@minerpl.xyz>
2026-02-19 21:28:51 +0100

    canaryRelease: 499402
    Main scripts: 3
    Lazy Loaded Scripts: 2025
    Chunks:
     * Main Files: 13325
     * Scripts: 12354
    Strings: 23754
    Routes: 798
    Experiments Count: 192
    Apex Experiments Count: 173

--- chunks/439547.js
+++ chunks/439547.js
@@ -1,5 +1,4 @@
-"use strict";
-n.d(t, { g: () => s });
+n.d(t, { g: () => l });
 let i = (0, n(600975).C)({
         kind: "user",
         id: "2025-02_voice_activity_status_mobile",
@@ -7,7 +6,7 @@ let i = (0, n(600975).C)({
         defaultConfig: { voiceActivityStatusEnabled: !1 },
         treatments: [{ id: 1, label: "Voice Activity Status Enabled", config: { voiceActivityStatusEnabled: !0 } }],
     }),
-    s = (e) => {
+    l = (e) => {
         let { location: t, autoTrackExposure: n = !0 } = e;
         return i.useExperiment({ location: t }, { autoTrackExposure: n });
     };
