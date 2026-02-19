8fa3b4faa064764f5ee8f7a60299157e7c464d3c
DiscrapperManager <admin@minerpl.xyz>
2026-02-19 07:33:35 +0100

    noisesSuppression 499107

--- chunks/449859.js
+++ chunks/449859.js
@@ -1,74 +1,75 @@
-n.d(t, { A: () => h });
-var a = n(627968),
-    i = n(64700),
-    l = n(503698),
-    o = n.n(l),
-    r = n(990078),
-    s = n(253932),
-    c = n(405269),
-    d = n(355938),
-    u = n(943815),
+"use strict";
+n.d(t, { A: () => A });
+var l = n(627968),
+    s = n(64700),
+    i = n(503698),
+    r = n.n(i),
+    a = n(990078),
+    o = n(253932),
+    u = n(405269),
+    c = n(355938),
+    d = n(943815),
     m = n(985018),
-    _ = n(679740);
-let p = i.memo(function (e) {
+    h = n(679740);
+let g = s.memo(function (e) {
         let {
                 children: t,
                 className: n,
-                compact: l = !1,
-                timestamp: p,
-                timestampFormat: h,
-                isVisibleOnlyOnHover: b = !1,
-                cozyAlt: g = !1,
-                isInline: A = !0,
-                id: I,
-                isEdited: y = !1,
-                application: T,
-                tooltipPosition: x,
+                compact: i = !1,
+                timestamp: g,
+                timestampFormat: A,
+                isVisibleOnlyOnHover: f = !1,
+                cozyAlt: x = !1,
+                isInline: v = !0,
+                id: N,
+                isEdited: b = !1,
+                application: j,
+                tooltipPosition: C,
             } = e,
-            C = i.useMemo(() => new Date(p), [p]),
-            E = i.useMemo(() => (0, c.K7)(C), [C]),
-            S = s.PZ.useSetting(),
-            v = i.useMemo(
-                () => (null != h ? (0, c.i$)(C, h, S) : l ? (0, c.i$)(C, "LT", S) : (0, c.mk)(C, !0)),
-                [C, h, l, S],
+            E = s.useMemo(() => new Date(g), [g]),
+            R = s.useMemo(() => (0, u.K7)(E), [E]),
+            I = o.PZ.useSetting(),
+            T = s.useMemo(
+                () => (null != A ? (0, u.i$)(E, A, I) : i ? (0, u.i$)(E, "LT", I) : (0, u.mk)(E, !0)),
+                [E, A, i, I],
             ),
-            j = i.useMemo(() => (l ? (0, u.A)(v) : null), [l, v]),
-            w = i.useMemo(() => (y ? m.intl.formatToPlainString(m.t.CDzOFd, { timeFormatted: E }) : E), [y, E]);
-        return (0, a.jsx)("span", {
-            className: o()(n, j, { [_.vE]: !0, [_.ET]: b, [_.YQ]: A, [_.CC]: g }),
+            y = s.useMemo(() => (i ? (0, d.A)(T) : null), [i, T]),
+            _ = s.useMemo(() => (b ? m.intl.formatToPlainString(m.t.CDzOFd, { timeFormatted: R }) : R), [b, R]);
+        return (0, l.jsx)("span", {
+            className: r()(n, y, { [h.vE]: !0, [h.ET]: f, [h.YQ]: v, [h.CC]: x }),
             children:
-                null == T
-                    ? (0, a.jsx)(r.m, {
+                null == j
+                    ? (0, l.jsx)(a.m, {
                           asContainer: !0,
-                          __unsupportedReactNodeAsText: (0, c.i$)(C, "LLLL"),
-                          "aria-label": w,
-                          position: x,
+                          __unsupportedReactNodeAsText: (0, u.i$)(E, "LLLL"),
+                          "aria-label": _,
+                          position: C,
                           delay: 750,
-                          children: (0, a.jsx)(f, { timeFormatted: v, timestamp: C, id: I, compact: l, children: t }),
+                          children: (0, l.jsx)(p, { timeFormatted: T, timestamp: E, id: N, compact: i, children: t }),
                       })
-                    : (0, a.jsx)(d.A, {
-                          application: T,
-                          compact: l,
-                          children: (0, a.jsx)(f, { timeFormatted: v, timestamp: C, id: I, compact: l, children: t }),
+                    : (0, l.jsx)(c.A, {
+                          application: j,
+                          compact: i,
+                          children: (0, l.jsx)(p, { timeFormatted: T, timestamp: E, id: N, compact: i, children: t }),
                       }),
         });
     }),
-    f = i.memo(function (e) {
-        let { tooltipProps: t, timeFormatted: n, children: i, compact: l, timestamp: o, id: r } = e;
-        return (0, a.jsx)("time", {
+    p = s.memo(function (e) {
+        let { tooltipProps: t, timeFormatted: n, children: s, compact: i, timestamp: r, id: a } = e;
+        return (0, l.jsx)("time", {
             ...t,
-            id: r,
-            dateTime: o.toISOString(),
+            id: a,
+            dateTime: r.toISOString(),
             children:
-                null != i
-                    ? i
-                    : (0, a.jsxs)(a.Fragment, {
+                null != s
+                    ? s
+                    : (0, l.jsxs)(l.Fragment, {
                           children: [
-                              (0, a.jsx)("i", { className: _.me, "aria-hidden": !0, children: l ? "[" : " — " }),
+                              (0, l.jsx)("i", { className: h.me, "aria-hidden": !0, children: i ? "[" : " — " }),
                               n,
-                              l && (0, a.jsxs)("i", { className: _.me, "aria-hidden": !0, children: ["]", " "] }),
+                              i && (0, l.jsxs)("i", { className: h.me, "aria-hidden": !0, children: ["]", " "] }),
                           ],
                       }),
         });
     }),
-    h = p;
+    A = g;
