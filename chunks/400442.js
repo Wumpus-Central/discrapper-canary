7a670bd0a926d5e39aa2a4654cf21f185b06dd76
DiscrapperManager <admin@minerpl.xyz>
2026-02-19 23:58:53 +0100

    canaryRelease: 499560
    Main scripts: 3
    Lazy Loaded Scripts: 2024
    Chunks:
     * Main Files: 13327
     * Scripts: 12353
    Strings: 23754
    Routes: 798
    Experiments Count: 191
    Apex Experiments Count: 174

--- chunks/400442.js
+++ chunks/400442.js
@@ -1,12 +1,18 @@
-i.d(e, { j: () => u });
-var n = i(46054),
-    l = i(419954),
-    s = i(253932),
-    r = i(780964),
-    a = i(985018);
-let u = (0, l.zD)(r.X.CHAT_EMOJI_CONVERT_EMOTICONS, {
-    useTitle: () => a.intl.string(a.t["79qal8"]),
-    useSubtitle: () => n.A.parse(a.intl.string(a.t["20dvuQ"])),
-    useValue: s.j7.useSetting,
-    setValue: s.j7.updateSetting,
+i.d(e, { j: () => A });
+var n = i(627968),
+    l = i(46054),
+    s = i(930101),
+    r = i(419954),
+    a = i(253932),
+    u = i(780964),
+    o = i(358776),
+    d = i(985018);
+let A = (0, r.zD)(u.X.CHAT_EMOJI_CONVERT_EMOTICONS, {
+    useTitle: () => d.intl.string(d.t["79qal8"]),
+    useSubtitle: () =>
+        (0, o.bp)("ConvertEmoticons")
+            ? d.intl.format(d.t.GejoQK, { emojiHook: (t, e) => (0, n.jsx)(s.H, { node: { name: "\uD83D\uDE42" } }, e) })
+            : l.A.parse(d.intl.string(d.t["20dvuQ"])),
+    useValue: a.j7.useSetting,
+    setValue: a.j7.updateSetting,
 });
