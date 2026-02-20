1c67805dd84aabcec7e8a9bc2765d3b65330d23e
DiscrapperManager <admin@minerpl.xyz>
2026-02-20 22:42:59 +0100

    canaryRelease: 500136
    Main scripts: 3
    Lazy Loaded Scripts: 2026
    Chunks:
     * Main Files: 13326
     * Scripts: 12362
    Strings: 23791
    Routes: 799
    Experiments Count: 186
    Apex Experiments Count: 178

--- chunks/326911.js
+++ chunks/326911.js
@@ -1,22 +1,24 @@
-n.d(t, { A: () => g });
+n.d(t, { A: () => x });
 var i = n(627968),
     s = n(64700),
-    r = n(311907),
-    a = n(397927),
-    l = n(869038),
-    o = n(499454),
-    c = n(351906),
-    d = n(954571),
-    u = n(45938),
-    _ = n(652215),
-    m = n(985018);
-class A extends s.Component {
+    a = n(311907),
+    l = n(397927),
+    r = n(869038),
+    o = n(459357),
+    c = n(99696),
+    d = n(499454),
+    u = n(351906),
+    _ = n(954571),
+    m = n(45938),
+    A = n(652215),
+    g = n(985018);
+class h extends s.Component {
     state = { codeInput: "", submitting: !1, hasError: !1, isPromoCode: !1 };
     get analyticsLocation() {
         let {
             analyticsContext: { location: e },
         } = this.props;
-        return { ...e, object: _.ZSU.BUTTON_CTA };
+        return { ...e, object: A.ZSU.BUTTON_CTA };
     }
     handleChange = (e) => {
         this.setState({ codeInput: e, hasError: !1 });
@@ -25,18 +27,24 @@ class A extends s.Component {
         e.preventDefault();
         let { codeInput: t } = this.state;
         if ("" === t) return;
-        let n = (0, u.Vd)(t);
-        if (null == n) return void this.setState({ hasError: !0 });
+        let n = t.trim();
         this.setState({ submitting: !0 });
         try {
-            let e = await l.A.resolveGiftCode(n);
-            if (null != e && null != e.giftCode.promotion)
+            if (this.props.acceptGiftCardRedemption)
+                try {
+                    await (0, c.Qp)(n), (0, c.HF)({ initialCode: n }), this.setState({ codeInput: "" });
+                    return;
+                } catch {}
+            let e = (0, m.Vd)(t);
+            if (null == e) return void this.setState({ hasError: !0 });
+            let i = await r.A.resolveGiftCode(e);
+            if (null != i && null != i.giftCode.promotion)
                 throw (this.setState({ isPromoCode: !0 }), Error("Cannnot redeem promotion code as gift"));
-            d.default.track(_.HAw.OPEN_MODAL, {
+            _.default.track(A.HAw.OPEN_MODAL, {
                 type: "gift_accept",
-                location: { ...this.analyticsLocation, object: _.ZSU.BUTTON_CTA },
+                location: { ...this.analyticsLocation, object: A.ZSU.BUTTON_CTA },
             }),
-                (0, o.h)({ processedCode: n }),
+                (0, d.h)({ processedCode: e }),
                 this.setState({ codeInput: "" });
         } catch (e) {
             this.setState({ hasError: !0 });
@@ -46,22 +54,22 @@ class A extends s.Component {
     };
     render() {
         let { obscureInput: e } = this.props,
-            { codeInput: t, submitting: n, hasError: s, isPromoCode: r } = this.state;
-        return (0, i.jsx)(a.nVY, {
-            label: m.intl.string(m.t["il+VCo"]),
+            { codeInput: t, submitting: n, hasError: s, isPromoCode: a } = this.state;
+        return (0, i.jsx)(l.nVY, {
+            label: g.intl.string(g.t["il+VCo"]),
             children: (0, i.jsx)("form", {
                 onSubmit: this.handleSubmit,
-                children: (0, i.jsxs)(a.M_l, {
+                children: (0, i.jsxs)(l.M_l, {
                     children: [
-                        (0, i.jsx)(a.ksK, {
-                            label: m.intl.string(m.t.SeKIoS),
+                        (0, i.jsx)(l.ksK, {
+                            label: g.intl.string(g.t.SeKIoS),
                             type: e ? "password" : "text",
                             value: t,
                             onChange: this.handleChange,
                             placeholder: "WUMP-AAAAA-BBBBB-CCCCC",
-                            error: !r && s ? m.intl.string(m.t.Y11a2u) : null,
-                            helperText: r
-                                ? m.intl.format(m.t.gPt3PE, {
+                            error: !a && s ? g.intl.string(g.t.Y11a2u) : null,
+                            helperText: a
+                                ? g.intl.format(g.t.gPt3PE, {
                                       promoLink: () => {
                                           window.open(`https://discord.com/billing/promotions/${t}`);
                                       },
@@ -69,9 +77,9 @@ class A extends s.Component {
                                 : null,
                             fullWidth: !0,
                         }),
-                        (0, i.jsx)(a.Button, {
+                        (0, i.jsx)(l.Button, {
                             variant: "primary",
-                            text: m.intl.string(m.t.KIpp7M),
+                            text: g.intl.string(g.t.KIpp7M),
                             type: "submit",
                             loading: n,
                         }),
@@ -81,8 +89,9 @@ class A extends s.Component {
         });
     }
 }
-function g() {
-    let e = s.useContext(d.AnalyticsContext),
-        t = (0, r.bG)([c.A], () => c.A.enabled);
-    return (0, i.jsx)(A, { analyticsContext: e, obscureInput: t });
+function x() {
+    let { enabled: e } = (0, o.c)({ location: "UserSettingsBilling" }),
+        t = s.useContext(_.AnalyticsContext),
+        n = (0, a.bG)([u.A], () => u.A.enabled);
+    return (0, i.jsx)(h, { analyticsContext: t, obscureInput: n, acceptGiftCardRedemption: e });
 }
