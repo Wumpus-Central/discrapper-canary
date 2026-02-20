e0cad22bb4672ce6ff7e6bab4eb6399fca62db5a
DiscrapperManager <admin@minerpl.xyz>
2026-02-20 02:16:49 +0100

    canaryRelease: 499705
    Main scripts: 3
    Lazy Loaded Scripts: 2024
    Chunks:
     * Main Files: 13327
     * Scripts: 12355
    Strings: 23757
    Routes: 798
    Experiments Count: 191
    Apex Experiments Count: 174

--- chunks/793574.js
+++ chunks/793574.js
@@ -481,6 +481,7 @@ var r = (function (e) {
         (e.USER_PROFILE_ACTIVITY_BUTTONS = "user profile activity buttons"),
         (e.USER_PROFILE_ACTIVITY_CLOUD_PLAY_SECTION = "user profile cloud play section"),
         (e.USER_PROFILE_WISHLIST = "user profile wishlist"),
+        (e.USER_PROFILE_EDIT_WISHLIST_ACTION_SHEET = "user profile edit wishlist action sheet"),
         (e.WISHLIST_BANNER = "wishlist banner"),
         (e.BLOCKED_PROFILE_MODAL = "blocked profile modal"),
         (e.IGNORED_PROFILE_MODAL = "ignored profile modal"),
