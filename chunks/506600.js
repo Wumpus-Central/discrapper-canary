208ddc5e8372e28874a4255c57c32f9825dc7274
DiscrapperManager <admin@minerpl.xyz>
2026-02-20 19:58:43 +0100

    canaryRelease: 499954
    Main scripts: 3
    Lazy Loaded Scripts: 2025
    Chunks:
     * Main Files: 13326
     * Scripts: 12361
    Strings: 23775
    Routes: 799
    Experiments Count: 188
    Apex Experiments Count: 176

--- chunks/506600.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-n.d(t, { P: () => r });
-let r = (0, n(945810).mj)({
-    name: "2026-02-capture-target-window",
-    kind: "user",
-    defaultConfig: { enabled: !1 },
-    variations: { 1: { enabled: !0 } },
-});
