ba057d228792364c497d0bde6bdcd73e0c6d4077
DiscrapperManager <admin@minerpl.xyz>
2026-02-20 17:57:17 +0100

    canaryRelease: 499851
    Main scripts: 3
    Lazy Loaded Scripts: 2025
    Chunks:
     * Main Files: 13328
     * Scripts: 12364
    Strings: 23775
    Routes: 798
    Experiments Count: 190
    Apex Experiments Count: 176

--- chunks/868018.js
+++ chunks/868018.js
@@ -1,26 +1,58 @@
-i.d(e, { O: () => A });
-var n = i(311907),
-    l = i(419954),
-    s = i(71393),
-    r = i(711014),
-    a = i(253932),
-    u = i(780964),
-    o = i(985018);
-let d = (0, l.AK)(u.X.PROFILE_PRIVACY_TO_ACTIVITY_PRIVACY_NAVIGATOR, {
-        useSubtitle: function () {
-            let t = a.JG.useSetting(),
-                e = (0, n.bG)([r.Ay, s.A], () => {
-                    let e = new Set(t);
-                    return r.Ay.getFlattenedGuildIds().filter((t) => null != s.A.getGuild(t) && !e.has(t));
-                }, [t]);
-            if (0 === e.length) return o.intl.format(o.t.QJIJ5p, {});
-            let i = s.A.getGuild(e[0]),
-                l = i?.name ?? "",
-                u = e.length - 1;
-            return 0 === u
-                ? o.intl.format(o.t["T+8J4A"], { guildName: l })
-                : o.intl.format(o.t["3JyODQ"], { guildName: l, count: u });
+i.d(e, { O: () => _ });
+var n = i(627968),
+    l = i(311907),
+    s = i(743790),
+    r = i(419954),
+    a = i(933297),
+    u = i(71393),
+    o = i(711014),
+    d = i(253932),
+    A = i(780964),
+    T = i(985018);
+function S() {
+    let t = d.JG.useSetting();
+    return (0, l.bG)([o.Ay, u.A], () => {
+        let e = new Set(t);
+        return o.Ay.getFlattenedGuildIds().filter((t) => null != u.A.getGuild(t) && !e.has(t));
+    }, [t]);
+}
+let E = {
+        type: a.wF.STACKED_ICONS,
+        useIcons: function () {
+            let t = S(),
+                e = (0, l.bG)(
+                    [u.A],
+                    () =>
+                        t
+                            .slice(0, 2)
+                            .map((t) => u.A.getGuild(t))
+                            .filter((t) => null != t),
+                    [t],
+                );
+            return 0 === e.length
+                ? null
+                : e.length >= 2
+                  ? {
+                        frontIcon: { icon: (0, n.jsx)(s.$, { guild: e[0], size: 48 }), shape: a.NF.SQUIRCLE },
+                        backIcon: { icon: (0, n.jsx)(s.$, { guild: e[1], size: 48 }), shape: a.NF.SQUIRCLE },
+                    }
+                  : { frontIcon: { icon: (0, n.jsx)(s.$, { guild: e[0], size: 48 }), shape: a.NF.SQUIRCLE } };
         },
-        destinationKey: u.X.ACTIVITY_PRIVACY_PANEL,
+    },
+    g = (0, r.AK)(A.X.PROFILE_PRIVACY_TO_ACTIVITY_PRIVACY_NAVIGATOR, {
+        useSubtitle: function () {
+            let t = S();
+            if (0 === t.length) return T.intl.format(T.t.QJIJ5p, {});
+            let e = u.A.getGuild(t[0]),
+                i = e?.name ?? "",
+                n = t.length - 1;
+            return 0 === n
+                ? T.intl.format(T.t["T+8J4A"], { guildName: i })
+                : T.intl.format(T.t["3JyODQ"], { guildName: i, count: n });
+        },
+        useTrailingDecoration: function () {
+            return E;
+        },
+        destinationKey: A.X.ACTIVITY_PRIVACY_PANEL,
     }),
-    A = (0, l.gN)(u.X.PROFILE_PRIVACY_RELATED_SETTINGS, { buildLayout: () => [d] });
+    _ = (0, r.gN)(A.X.PROFILE_PRIVACY_RELATED_SETTINGS, { buildLayout: () => [g] });
