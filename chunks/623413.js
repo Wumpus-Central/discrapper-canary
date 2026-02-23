95cc3e440c3cf6b4a7a013b762b7c1606fc685de
DiscrapperManager <admin@minerpl.xyz>
2026-02-23 22:01:12 +0100

    canaryRelease: 500791
    Main scripts: 3
    Lazy Loaded Scripts: 2030
    Chunks:
     * Main Files: 13336
     * Scripts: 12362
    Strings: 23801
    Routes: 799
    Experiments Count: 183
    Apex Experiments Count: 179

--- chunks/623413.js
+++ chunks/623413.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { A: () => C });
+n.d(t, { A: () => E });
 var i = n(627968),
     s = n(64700),
     l = n(311907),
@@ -7,71 +7,75 @@ var i = n(627968),
     a = n(956793),
     o = n(313961),
     c = n(164617),
-    d = n(202384),
-    u = n(51758),
-    h = n(175203),
-    A = n(426660),
-    p = n(403362),
-    g = n(69407),
-    m = n(110618),
-    _ = n(566388),
-    f = n(985018),
-    x = n(852887);
-let C = function (e) {
-    let { participants: t, channel: n, hasConnectPermission: C } = e,
-        E = (0, u.H)(n.guild_id),
-        I = s.useCallback(() => {
-            E ? (0, d.Ze)(n.guild_id, () => a.default.selectVoiceChannel(n.id)) : a.default.selectVoiceChannel(n.id);
-        }, [n.id, n.guild_id, E]),
-        b = t.filter((e) => e.type === g.wY.VOICE),
-        N = 4 === b.length ? 2 : 3,
-        S = (0, l.yK)([o.A], () => b.map((e) => o.A.getParticipant(n.id, e.id)).filter(p.Vq), [n.id, b]);
+    d = n(47167),
+    u = n(202384),
+    h = n(51758),
+    A = n(175203),
+    p = n(426660),
+    g = n(403362),
+    m = n(69407),
+    _ = n(110618),
+    f = n(566388),
+    x = n(985018),
+    C = n(852887);
+let E = function (e) {
+    let { participants: t, channel: n, hasConnectPermission: E } = e,
+        I = (0, h.H)(n.guild_id),
+        b = s.useCallback(() => {
+            I ? (0, u.Ze)(n.guild_id, () => a.default.selectVoiceChannel(n.id)) : a.default.selectVoiceChannel(n.id);
+        }, [n.id, n.guild_id, I]),
+        N = t.filter((e) => e.type === m.wY.VOICE),
+        S = (0, d.Ay)(n),
+        T = 4 === N.length ? 2 : 3,
+        y = (0, l.yK)([o.A], () => N.map((e) => o.A.getParticipant(n.id, e.id)).filter(g.Vq), [n.id, N]);
     return (0, i.jsxs)("div", {
-        className: x.kL,
+        className: C.kL,
         children: [
-            (0, i.jsx)(A.A, {}),
+            (0, i.jsx)(p.A, {}),
             (0, i.jsx)("div", {
-                className: x.os,
-                style: { maxWidth: 168 * N },
-                children: S.slice(0, 5).map((e) =>
-                    (0, i.jsx)(
-                        h.Ay,
-                        {
-                            participant: e,
-                            channel: n,
-                            className: x.Vs,
-                            inCall: !0,
-                            noVideoRender: !0,
-                            popoutType: c.N.NO_POPOUT,
-                            width: 48,
-                        },
-                        e.id,
+                className: C.os,
+                style: { maxWidth: 168 * T },
+                children: y
+                    .slice(0, 5)
+                    .map((e) =>
+                        (0, i.jsx)(
+                            A.Ay,
+                            {
+                                participant: e,
+                                channel: n,
+                                className: C.Vs,
+                                inCall: !0,
+                                noVideoRender: !0,
+                                popoutType: c.N.NO_POPOUT,
+                                width: 48,
+                            },
+                            e.id,
+                        ),
                     ),
-                ),
             }),
-            (0, i.jsx)(r.Heading, { className: x.HA, variant: "heading-xxl/normal", children: n.name }),
+            (0, i.jsx)(r.Heading, { className: C.HA, variant: "heading-xxl/normal", children: S }),
             (0, i.jsx)("div", {
-                className: x.Nu,
+                className: C.Nu,
                 children: (0, i.jsx)(r.Text, {
                     tag: "div",
                     color: "text-default",
                     variant: "heading-lg/normal",
-                    children: (0, m.DO)(n, b),
+                    children: (0, _.DO)(n, N),
                 }),
             }),
             (0, i.jsxs)("div", {
-                className: x.UD,
+                className: C.UD,
                 children: [
                     (0, i.jsx)("div", {
-                        className: x.PD,
+                        className: C.PD,
                         children: (0, i.jsx)(r.Button, {
                             variant: "overlay-primary",
-                            text: C ? f.intl.string(f.t["7vb2cc"]) : f.intl.string(f.t.TVBCKZ),
-                            onClick: I,
-                            disabled: !C,
+                            text: E ? x.intl.string(x.t["7vb2cc"]) : x.intl.string(x.t.TVBCKZ),
+                            onClick: b,
+                            disabled: !E,
                         }),
                     }),
-                    (0, i.jsx)(_.l, { channel: n }),
+                    (0, i.jsx)(f.l, { channel: n }),
                 ],
             }),
         ],
