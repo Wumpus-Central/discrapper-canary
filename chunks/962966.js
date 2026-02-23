fc17152c6e1bb0270e7b75e7e30e6449cf7ca29d
DiscrapperManager <admin@minerpl.xyz>
2026-02-23 20:24:12 +0100

    canaryRelease: 500665
    Main scripts: 3
    Lazy Loaded Scripts: 2030
    Chunks:
     * Main Files: 13335
     * Scripts: 12357
    Strings: 23800
    Routes: 799
    Experiments Count: 183
    Apex Experiments Count: 179

--- chunks/962966.js
+++ chunks/962966.js
@@ -3,7 +3,6 @@ e.exports = {
     kL: "container__218f7",
     XK: "isReducedWidth__218f7",
     iA: "isStandardGap__218f7",
-    L: "voiceChannelGiftingBreadcrumb__218f7",
     AF: "hangStatusContent__218f7",
     W9: "hangStatusIcon__218f7",
     YJ: "hangStatusText__218f7",
