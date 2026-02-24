0673e72924fb2bb1b6dde41b95b14caa53602a92
DiscrapperManager <admin@minerpl.xyz>
2026-02-24 17:51:18 +0100

    canaryRelease: 501226
    Main scripts: 3
    Lazy Loaded Scripts: 2031
    Chunks:
     * Main Files: 13348
     * Scripts: 12371
    Strings: 23826
    Routes: 801
    Experiments Count: 183
    Apex Experiments Count: 184

--- chunks/777135.js
+++ chunks/777135.js
@@ -1,13 +1,14 @@
-i.d(t, { V: () => _ });
+i.d(e, { V: () => T });
 var n = i(419954),
     l = i(780964),
     s = i(805060),
     r = i(962180),
     a = i(120051),
-    u = i(827315),
-    o = i(898560),
-    d = i(985018);
-let _ = (0, n.zZ)(l.X.DISPLAY_CHAT_CATEGORY, {
-    useTitle: () => d.intl.string(d.t["/VQax8"]),
-    buildLayout: () => [u.g, s.r, r.j, o.z, a.z],
+    u = i(100549),
+    o = i(827315),
+    d = i(898560),
+    A = i(985018);
+let T = (0, n.zZ)(l.X.DISPLAY_CHAT_CATEGORY, {
+    useTitle: () => A.intl.string(A.t["/VQax8"]),
+    buildLayout: () => [o.g, s.r, r.j, d.z, a.z, u.$],
 });
--- chunks/866352.js
+++ chunks/866352.js
@@ -3,8 +3,8 @@ var n = i(419954),
     l = i(780964),
     s = i(985018);
 let r = (0, n.AK)(l.X.DISPLAY_THEME_ACCESSIBLITY_NAVIGATOR, {
-        useSubtitle: () => s.intl.string(s.t["0SrV4T"]),
-        useSearchTerms: () => [s.intl.string(s.t["0SrV4T"])],
+        useSubtitle: () => s.intl.string(s.t.nhVQDJ),
+        useSearchTerms: () => [s.intl.string(s.t.nhVQDJ)],
         destinationKey: l.X.ACCESSIBILITY_PANEL,
     }),
     a = (0, n.gN)(l.X.DISPLAY_THEME_RELATED_SETTINGS, { buildLayout: () => [r] });
