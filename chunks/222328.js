11632845067dfb4e45cbdd51136b34d7e728f55d
DiscrapperManager <admin@minerpl.xyz>
2026-02-24 17:42:26 +0100

    canaryRelease: 501221
    Main scripts: 3
    Lazy Loaded Scripts: 2031
    Chunks:
     * Main Files: 13348
     * Scripts: 12370
    Strings: 23823
    Routes: 801
    Experiments Count: 183
    Apex Experiments Count: 184

--- chunks/222328.js
+++ chunks/222328.js
@@ -1,15 +1,6 @@
-i.d(e, { I: () => o });
-var n = i(554146),
-    l = i(379848),
-    s = i(419954),
-    r = i(780964),
-    a = i(790076);
-let u = (0, s.E2)(r.X.OVERLAY_NUX, {
-        Component: a.F,
-        useSearchTerms: () => [],
-        usePredicate: () => {
-            let [t] = (0, l.kn)([n.M.OVERLAY_OOP_SETTINGS_NUX], void 0, !0);
-            return t === n.M.OVERLAY_OOP_SETTINGS_NUX;
-        },
-    }),
-    o = (0, s.zZ)(r.X.OVERLAY_NUX_CATEGORY, { buildLayout: () => [u] });
+i.d(e, { I: () => a });
+var n = i(419954),
+    l = i(780964),
+    s = i(790076);
+let r = (0, n.E2)(l.X.OVERLAY_NUX, { Component: s.F, useSearchTerms: () => [] }),
+    a = (0, n.zZ)(l.X.OVERLAY_NUX_CATEGORY, { buildLayout: () => [r] });
