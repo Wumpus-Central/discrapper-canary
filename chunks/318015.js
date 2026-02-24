14fc78940b539d10039bfc9c50f5213427a3521f
DiscrapperManager <admin@minerpl.xyz>
2026-02-25 00:48:03 +0100

    canaryRelease: 501684
    Main scripts: 3
    Lazy Loaded Scripts: 2031
    Chunks:
     * Main Files: 13359
     * Scripts: 12376
    Strings: 23827
    Routes: 801
    Experiments Count: 182
    Apex Experiments Count: 189

--- chunks/318015.js
+++ chunks/318015.js
@@ -1,19 +1,20 @@
 "use strict";
-n.d(t, { AL: () => i.A, RK: () => s, sK: () => o });
+n.d(t, { AL: () => i.A, RK: () => a, sK: () => o });
 var r = n(945810),
     i = n(766005);
-let a = (0, r.mj)({
+let s = (0, r.mj)({
     name: "2026-02-private-channel-hiding",
     kind: "user",
     defaultConfig: { enableObfuscation: !1, enableIntegrityCheck: !1 },
     variations: {
         1: { enableObfuscation: !0, enableIntegrityCheck: !1 },
         2: { enableObfuscation: !0, enableIntegrityCheck: !0 },
+        3: { enableObfuscation: !1, enableIntegrityCheck: !1 },
     },
 });
-function s(e) {
-    return a.getConfig({ location: e }).enableObfuscation;
+function a(e) {
+    return s.getConfig({ location: e }).enableObfuscation;
 }
 function o(e) {
-    return a.useConfig({ location: e }).enableObfuscation;
+    return s.useConfig({ location: e }).enableObfuscation;
 }
