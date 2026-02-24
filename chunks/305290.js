04d2adb2d7048bd8aace6e83965ee597f7dfbdf8
DiscrapperManager <admin@minerpl.xyz>
2026-02-24 18:52:07 +0100

    canaryRelease: 501264
    Main scripts: 3
    Lazy Loaded Scripts: 2027
    Chunks:
     * Main Files: 13375
     * Scripts: 12366
    Strings: 23826
    Routes: 801
    Experiments Count: 183
    Apex Experiments Count: 185

--- chunks/305290.js
+++ chunks/305290.js
@@ -1,12 +1,11 @@
-i.d(e, { U: () => d });
+i.d(e, { U: () => o });
 var n = i(419954),
     l = i(780964),
     s = i(811131),
     r = i(193660),
     a = i(31118),
-    u = i(91743),
-    o = i(985018);
-let d = (0, n.zZ)(l.X.TEXT_READABILITY_CATEGORY, {
-    useTitle: () => o.intl.string(o.t["bxh/R7"]),
-    buildLayout: () => [s.B, a.k, r.w, u.Y],
+    u = i(985018);
+let o = (0, n.zZ)(l.X.TEXT_READABILITY_CATEGORY, {
+    useTitle: () => u.intl.string(u.t["bxh/R7"]),
+    buildLayout: () => [s.B, a.k, r.w],
 });
--- chunks/653022.js
+++ chunks/653022.js
@@ -1,4 +1,4 @@
-i.d(e, { Y: () => a });
+i.d(e, { V: () => a });
 var n = i(419954),
     l = i(780964),
     s = i(985018);
@@ -6,4 +6,4 @@ let r = (0, n.AK)(l.X.ACCESSIBILITY_TO_DISPLAY_NAVIGATOR, {
         useSubtitle: () => s.intl.string(s.t.aTfeGK),
         destinationKey: l.X.DISPLAY_PANEL,
     }),
-    a = (0, n.gN)(l.X.TEXT_READABILITY_RELATED_SETTINGS, { buildLayout: () => [r] });
+    a = (0, n.gN)(l.X.COLOR_AND_CONTRAST_RELATED_SETTINGS, { buildLayout: () => [r] });
