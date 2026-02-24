2227702c1fb8af7ddcac3d82b22a623ed390db62
DiscrapperManager <admin@minerpl.xyz>
2026-02-24 19:11:39 +0100

    canaryRelease: 501284
    Main scripts: 3
    Lazy Loaded Scripts: 2027
    Chunks:
     * Main Files: 13376
     * Scripts: 12366
    Strings: 23826
    Routes: 801
    Experiments Count: 183
    Apex Experiments Count: 185

--- chunks/802385.js
+++ chunks/802385.js
@@ -1,13 +1,19 @@
-i.d(e, { Q: () => o });
+i.d(e, { Q: () => A });
 var n = i(311907),
-    l = i(955572),
-    s = i(775602),
-    r = i(419954),
-    a = i(780964),
-    u = i(985018);
-let o = (0, r.zD)(a.X.HIGH_CONTRAST_MODE, {
-    useTitle: () => u.intl.string(u.t.aZlePv),
-    useSubtitle: () => u.intl.string(u.t["v2qF8+"]),
-    useValue: () => (0, n.bG)([s.A], () => s.A.isHighContrastModeEnabled),
-    setValue: (t) => (0, l.uh)(t ? s._.HIGH : s._.DEFAULT),
+    l = i(554146),
+    s = i(955572),
+    r = i(775602),
+    a = i(379848),
+    u = i(419954),
+    o = i(780964),
+    d = i(985018);
+let A = (0, u.zD)(o.X.HIGH_CONTRAST_MODE, {
+    useTitle: () => d.intl.string(d.t.aZlePv),
+    useSubtitle: () => d.intl.string(d.t["v2qF8+"]),
+    useValue: () => (0, n.bG)([r.A], () => r.A.isHighContrastModeEnabled),
+    setValue: (t) => (0, s.uh)(t ? r._.HIGH : r._.DEFAULT),
+    useBadge: () => {
+        let [t] = (0, a.kn)([l.M.ACCESSIBILITY_HIGH_CONTRAST_MODE_NEW_BADGE]);
+        return t === l.M.ACCESSIBILITY_HIGH_CONTRAST_MODE_NEW_BADGE ? "new" : void 0;
+    },
 });
