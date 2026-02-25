a7da19e63d730c6da41c7a825979366550916cba
DiscrapperManager <admin@minerpl.xyz>
2026-02-25 23:57:23 +0100

    canaryRelease: 502246
    Main scripts: 3
    Lazy Loaded Scripts: 2033
    Chunks:
     * Main Files: 13366
     * Scripts: 12389
    Strings: 23832
    Routes: 801
    Experiments Count: 181
    Apex Experiments Count: 193

--- chunks/430086.js
+++ chunks/430086.js
@@ -1,34 +1,34 @@
-n.d(t, { Fv: () => m, GY: () => A, M$: () => g, Oc: () => _, kF: () => u });
+n.d(t, { Fv: () => A, GY: () => g, M$: () => I, Oc: () => u, kF: () => c });
 var i = n(949645),
-    s = n(265422),
-    r = n(963169),
-    a = n(345942),
+    r = n(265422),
+    a = n(963169),
+    s = n(345942),
     l = n(309010),
     o = n(967198),
-    c = n(723702),
-    d = n(652215);
-let u = { binds: ["alt+down"], comboKeysBindGlobal: !0, action: () => ((0, i.A)(), !1) },
-    _ = { binds: ["alt+up"], comboKeysBindGlobal: !0, action: () => ((0, i.A)(-1), !1) },
-    m = {
+    d = n(723702),
+    _ = n(652215);
+let c = { binds: ["alt+down"], comboKeysBindGlobal: !0, action: () => ((0, i.A)(), !1) },
+    u = { binds: ["alt+up"], comboKeysBindGlobal: !0, action: () => ((0, i.A)(-1), !1) },
+    A = {
         binds: ["mod+alt+right"],
         comboKeysBindGlobal: !0,
         action() {
             let e;
-            if (null != o.A.getGuildId()) e = d.ME;
+            if (null != o.A.getGuildId()) e = _.ME;
             else {
                 let t = o.A.getLastSelectedGuildId();
-                null != t && ((e = t), (0, s.i)(t, l.A.getChannelId(t)));
+                null != t && ((e = t), (0, r.i)(t, l.A.getChannelId(t)));
             }
-            return null != e && (0, a.u)(e), !1;
+            return null != e && (0, s.u)(e), !1;
         },
     },
-    A = {
-        binds: (0, c.isMac)() ? ["mod+["] : ["alt+left"],
-        comboKeysBindGlobal: !0,
-        action: () => ((0, r.OE)("keybind"), !1),
-    },
     g = {
-        binds: (0, c.isMac)() ? ["mod+]"] : ["alt+right"],
+        binds: (0, d.isMac)() || (0, d.isMacWeb)() ? ["mod+["] : ["alt+left"],
         comboKeysBindGlobal: !0,
-        action: () => ((0, r.Qb)("keybind"), !1),
+        action: () => ((0, a.OE)("keybind"), !1),
+    },
+    I = {
+        binds: (0, d.isMac)() || (0, d.isMacWeb)() ? ["mod+]"] : ["alt+right"],
+        comboKeysBindGlobal: !0,
+        action: () => ((0, a.Qb)("keybind"), !1),
     };
