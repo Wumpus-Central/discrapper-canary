16fb8b08d7707a09763c29cf79394a9c8b5b312e
DiscrapperManager <admin@minerpl.xyz>
2026-02-25 22:33:27 +0100

    canaryRelease: 502127
    Main scripts: 3
    Lazy Loaded Scripts: 2033
    Chunks:
     * Main Files: 13366
     * Scripts: 12387
    Strings: 23831
    Routes: 801
    Experiments Count: 181
    Apex Experiments Count: 192

--- chunks/716970.js
+++ chunks/716970.js
@@ -1,3 +1,3 @@
 n.d(t, { C: () => i });
 var a,
-    i = (((a = {}).DEFAULT = "default"), (a.ORB_TAB = "orb_tab"), a);
+    i = (((a = {}).DEFAULT = "default"), (a.DEFAULT_ALT = "default_alt"), (a.ORB_TAB = "orb_tab"), a);
