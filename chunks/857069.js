c8dedde4cceea2ab405fe2139801625b1c2dbcab
DiscrapperManager <admin@minerpl.xyz>
2026-02-25 15:47:39 +0100

    canaryRelease: 501809
    Main scripts: 3
    Lazy Loaded Scripts: 2033
    Chunks:
     * Main Files: 13362
     * Scripts: 12378
    Strings: 23829
    Routes: 801
    Experiments Count: 181
    Apex Experiments Count: 189

--- chunks/857069.js
+++ chunks/857069.js
@@ -1,8 +1,8 @@
 n.d(t, { A: () => d, l: () => c });
 var i = n(405269),
     l = n(927813),
-    a = n(661191),
-    s = n(143413),
+    s = n(661191),
+    a = n(143413),
     r = n(652215);
 let o = 7 * l.A.Millis.MINUTE;
 function c(e, t, n) {
@@ -16,14 +16,16 @@ function c(e, t, n) {
 function d(e, t, n) {
     return (
         !!(n.hasFlag(r.pr7.HAS_THREAD) || n.isCommandType()) ||
+        t.blocked !== n.blocked ||
+        t.ignored !== n.ignored ||
         (n.type > r.lAJ.DEFAULT
-            ? !(0, s.A)(t) || n.type === r.lAJ.REPLY
+            ? !(0, a.A)(t) || n.type === r.lAJ.REPLY
             : !!(
-                  (0, s.A)(t) ||
+                  (0, a.A)(t) ||
                   t.author.id !== n.author.id ||
                   t.hasFlag(r.pr7.EPHEMERAL) !== n.hasFlag(r.pr7.EPHEMERAL) ||
                   (null != n.webhookId && t.author.username !== n.author.username) ||
-                  (e?.isForumPost() && t.id === a.default.castChannelIdAsMessageId(e.id)) ||
+                  (e?.isForumPost() && t.id === s.default.castChannelIdAsMessageId(e.id)) ||
                   !(0, i.ro)(t.timestamp, n.timestamp) ||
                   !(0, i.v0)(t.timestamp, n.timestamp, o) ||
                   (n.hasFlag(r.pr7.SUPPRESS_NOTIFICATIONS) && !t.hasFlag(r.pr7.SUPPRESS_NOTIFICATIONS)) ||
