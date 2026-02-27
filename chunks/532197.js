52fd5818aa0d9150825b63fc365d78a52b8d277d
DiscrapperManager <admin@minerpl.xyz>
2026-02-27 16:38:18 +0100

    noisesSuppression 503266

--- chunks/532197.js
+++ chunks/532197.js
@@ -1,18 +1,17 @@
-"use strict";
-n.d(t, { A: () => _ });
-var a = n(627968),
-    l = n(64700),
-    r = n(503698),
-    s = n.n(r),
-    i = n(397927),
-    o = n(262633);
-let d = { UP: "up", RIGHT: "right", DOWN: "down", LEFT: "left" },
-    c = { [d.UP]: "", [d.RIGHT]: o.pG, [d.DOWN]: o.TR, [d.LEFT]: o.kb };
-class u extends l.PureComponent {
-    static Directions = d;
+n.d(t, { A: () => A });
+var i = n(627968),
+    s = n(64700),
+    l = n(503698),
+    r = n.n(l),
+    a = n(397927),
+    d = n(262633);
+let o = { UP: "up", RIGHT: "right", DOWN: "down", LEFT: "left" },
+    c = { [o.UP]: "", [o.RIGHT]: d.pG, [o.DOWN]: d.TR, [o.LEFT]: d.kb };
+class u extends s.PureComponent {
+    static Directions = o;
     render() {
         let { direction: e, className: t } = this.props;
-        return (0, a.jsx)(i.z$m, { size: "md", color: "currentColor", className: s()(t, { [c[e]]: !0 }) });
+        return (0, i.jsx)(a.z$m, { size: "md", color: "currentColor", className: r()(t, { [c[e]]: !0 }) });
     }
 }
-let _ = u;
+let A = u;
