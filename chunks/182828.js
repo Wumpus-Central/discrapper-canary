2ecb9e9457c9a29d5249a513ebccccfc170f841c
DiscrapperManager <admin@minerpl.xyz>
2026-02-27 18:04:56 +0100

    canaryRelease: 503315
    Main scripts: 3
    Lazy Loaded Scripts: 2034
    Chunks:
     * Main Files: 13381
     * Scripts: 12413
    Strings: 23838
    Routes: 803
    Experiments Count: 180
    Apex Experiments Count: 195

--- chunks/182828.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-n.d(t, { o: () => r });
-let r = (0, n(143770).A)({
-    kind: "user",
-    id: "2025-01_avholdout",
-    label: "AV Holdout - H1 2025",
-    defaultConfig: { isInHoldout: !1 },
-    treatments: [{ id: 1, label: "Enables the holdout experiment", config: { isInHoldout: !0 } }],
-});
--- chunks/461285.js
+++ chunks/461285.js
@@ -1,16 +1,13 @@
 "use strict";
-n.d(t, { I: () => s });
-var r = n(600975),
-    i = n(182828);
-let a = (0, r.C)({
+n.d(t, { I: () => i });
+let r = (0, n(600975).C)({
     kind: "user",
     id: "2025-06_audio_bitrate_adaptation",
     label: "Audio Bitrate Adaptation",
     defaultConfig: { enabled: !1 },
     treatments: [{ id: 1, label: "Enable audio bitrate adaptation", config: { enabled: !0 } }],
 });
-function s(e) {
-    let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
-        { isInHoldout: n } = i.o.getCurrentConfig({ location: e }, { autoTrackExposure: t });
-    return a.getCurrentConfig({ location: e }, { disable: n, autoTrackExposure: t });
+function i(e) {
+    let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1];
+    return r.getCurrentConfig({ location: e }, { autoTrackExposure: t });
 }
