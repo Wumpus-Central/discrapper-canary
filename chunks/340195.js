722b2b0f8fa38fdfe1a068e9dc6d206ee1084b40
DiscrapperManager <admin@minerpl.xyz>
2026-02-27 18:59:37 +0100

    canaryRelease: 503355
    Main scripts: 3
    Lazy Loaded Scripts: 2034
    Chunks:
     * Main Files: 13383
     * Scripts: 12417
    Strings: 23838
    Routes: 803
    Experiments Count: 180
    Apex Experiments Count: 196

--- chunks/340195.js
+++ chunks/340195.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { A: () => h });
+n.d(t, { A: () => E });
 var r = n(627968);
 n(64700);
 var i = n(311907),
@@ -8,33 +8,38 @@ var i = n(311907),
     o = n(775602),
     l = n(607470),
     u = n(676279),
-    c = n(750572),
+    c = n(200430),
     d = n(154057),
-    _ = n(236806),
-    f = n(988297),
-    p = n(613702);
-function h(e) {
-    let { className: t } = e,
-        n = (0, i.bG)([o.A], () => o.A.useReducedMotion),
-        h = (0, a.Ay)(),
-        m = (0, s.qB1)(h);
-    if (n || (0, u.nr)()) {
-        let e = m ? f.A : d.A;
+    _ = n(609636),
+    f = n(236806),
+    p = n(988297),
+    h = n(307236),
+    m = n(613702);
+function E(e) {
+    let t,
+        { className: n } = e,
+        E = (0, i.bG)([o.A], () => o.A.useReducedMotion),
+        g = (0, a.Ay)(),
+        A = (0, s.qB1)(g);
+    if (E) {
+        let e = A ? p.A : d.A;
         return (0, r.jsxs)("div", {
-            className: t,
+            className: n,
             children: [(0, r.jsx)("div", { className: c.YL }), (0, r.jsx)("img", { src: e, alt: "" })],
         });
     }
-    let E = m ? p.A : _.A;
-    return (0, r.jsxs)("div", {
-        className: t,
-        children: [
-            (0, r.jsx)("div", { className: c.YL }),
-            (0, r.jsx)(
-                l.A,
-                { muted: !0, autoPlay: !0, playsInline: !0, loop: !0, children: (0, r.jsx)("source", { src: E }) },
-                E,
-            ),
-        ],
-    });
+    return (
+        (t = (0, u.TM)() ? (A ? h.A : _.A) : A ? m.A : f.A),
+        (0, r.jsxs)("div", {
+            className: n,
+            children: [
+                (0, r.jsx)("div", { className: c.YL }),
+                (0, r.jsx)(
+                    l.A,
+                    { muted: !0, autoPlay: !0, playsInline: !0, loop: !0, children: (0, r.jsx)("source", { src: t }) },
+                    t,
+                ),
+            ],
+        })
+    );
 }
