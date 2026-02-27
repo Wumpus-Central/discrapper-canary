5a839d1e0ca7d38c8890ae3c4ebb0f0f11cac92f
DiscrapperManager <admin@minerpl.xyz>
2026-02-27 20:22:44 +0100

    canaryRelease: 503453
    Main scripts: 3
    Lazy Loaded Scripts: 2034
    Chunks:
     * Main Files: 13388
     * Scripts: 12420
    Strings: 23839
    Routes: 803
    Experiments Count: 180
    Apex Experiments Count: 198

--- chunks/270537.js
+++ chunks/270537.js
@@ -1,75 +1,84 @@
 "use strict";
-n.d(t, { Qf: () => h, Vm: () => f });
+n.d(t, { Qf: () => T, Vm: () => h });
 var r = n(627968),
     i = n(64700),
     s = n(503698),
     a = n.n(s),
-    o = n(397927),
-    l = n(580630);
+    o = n(450873),
+    l = n.n(o),
+    u = n(397927),
+    c = n(580630);
 n(717201);
-var u = n(381247),
-    c = n(756366),
-    d = n(985018),
-    _ = n(794873);
-function f(e) {
-    let {
-            label: t,
-            totalDueLabel: n,
-            lineItems: s,
-            currency: a,
-            collapsedContentLabelOverride: c,
-            defaultExpanded: d = !1,
-        } = e,
-        f = s.length > 0,
-        m = i.useMemo(() => s.reduce((e, t) => e + t.amount, 0), [s]),
-        E = i.useMemo(() => s.reduce((e, t) => (t.amount < 0 ? e + Math.abs(t.amount) : e), 0), [s]);
-    return (0, r.jsxs)(r.Fragment, {
-        children: [
-            (0, r.jsx)("div", { className: _.y }),
-            (0, r.jsx)(u.h, {
-                label: t,
-                defaultExpanded: d,
-                isDisabled: !f,
-                collapsedContent:
-                    null != c
-                        ? (0, r.jsx)(o.Text, {
-                              variant: "text-md/medium",
-                              color: "text-feedback-positive",
-                              children: c,
-                          })
-                        : (0, r.jsx)(p, { amount: E, currency: a }),
-                children: s.map((e) => {
-                    let { id: t, label: n, amount: i, ...s } = e,
-                        o = (0, l.$g)(i, a);
-                    return (0, r.jsx)(
-                        u.i,
-                        { label: n, value: o, valueColor: i < 0 ? "text-feedback-positive" : "text-muted", ...s },
-                        t,
-                    );
-                }),
-            }),
-            null != n && (0, r.jsx)(h, { label: n, value: (0, l.$g)(m, a) }),
-        ],
+var d = n(381247),
+    _ = n(756366),
+    f = n(985018),
+    p = n(794873);
+function h(e) {
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)("div", { className: p.y }), (0, r.jsx)(A, { ...e })] });
+}
+function m(e) {
+    return "tax" === e.id;
+}
+function E(e) {
+    return 0 === e.amount;
+}
+function g(e) {
+    let t = e.filter((e) => !(m(e) && E(e))),
+        { tax: n = 0, other: r = 0 } = l()(t, (e) => (m(e) ? "tax" : "other"));
+    if (1 === r) {
+        if (0 === n) return [];
+        if (1 === n) return [t.find(m)];
+    }
+    return t;
+}
+function A(e) {
+    let { label: t, lineItems: n, currency: s, collapsedContentLabelOverride: a, defaultExpanded: o = !1 } = e,
+        l = n.length > 0,
+        _ = i.useMemo(() => n.reduce((e, t) => (t.amount < 0 ? e + Math.abs(t.amount) : e), 0), [n]),
+        f = g(n);
+    if (0 === f.length) return null;
+    if (1 === f.length) {
+        let { amount: e, ...t } = f[0],
+            n = (0, c.$g)(e, s);
+        return (0, r.jsx)(d.i, { value: n, valueColor: e < 0 ? "text-feedback-positive" : "text-muted", ...t });
+    }
+    return (0, r.jsx)(d.h, {
+        label: t,
+        defaultExpanded: o,
+        isDisabled: !l,
+        collapsedContent:
+            null != a
+                ? (0, r.jsx)(u.Text, { variant: "text-md/medium", color: "text-feedback-positive", children: a })
+                : (0, r.jsx)(I, { amount: _, currency: s }),
+        children: f.map((e) => {
+            let { id: t, label: n, amount: i, ...a } = e,
+                o = (0, c.$g)(i, s);
+            return (0, r.jsx)(
+                d.i,
+                { label: n, value: o, valueColor: i < 0 ? "text-feedback-positive" : "text-muted", ...a },
+                t,
+            );
+        }),
     });
 }
-function p(e) {
+function I(e) {
     let { amount: t, currency: n } = e;
     if (0 === t) return null;
-    let i = (0, l.$g)(t, n);
-    return (0, r.jsx)(o.Text, {
+    let i = (0, c.$g)(t, n);
+    return (0, r.jsx)(u.Text, {
         variant: "text-md/medium",
         color: "text-feedback-positive",
-        children: d.intl.format(c.default.pDVleg, { amount: i }),
+        children: f.intl.format(_.default.pDVleg, { amount: i }),
     });
 }
-function h(e) {
+function T(e) {
     let { label: t, value: n, className: i } = e;
-    return (0, r.jsxs)(o.Text, {
+    return (0, r.jsxs)(u.Text, {
         variant: "text-lg/semibold",
         color: "text-strong",
-        className: a()(_.p, i),
+        className: a()(p.p, i),
         children: [
-            (0, r.jsx)("span", { children: t ?? d.intl.string(c.default.Zxav97) }),
+            (0, r.jsx)("span", { children: t ?? f.intl.string(_.default.Zxav97) }),
             (0, r.jsx)("span", { children: n }),
         ],
     });
---
+++ chunks/450873.js
@@ -0,0 +1,6 @@
+var r = n(59221),
+    i = n(400095),
+    s = Object.prototype.hasOwnProperty;
+e.exports = i(function (e, t, n) {
+    s.call(e, n) ? ++e[n] : r(e, n, 1);
+});
