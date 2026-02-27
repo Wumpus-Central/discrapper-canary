5bf284858411d5e840cd1c818b2818c718c675b5
DiscrapperManager <admin@minerpl.xyz>
2026-02-27 02:11:07 +0100

    canaryRelease: 503108
    Main scripts: 3
    Lazy Loaded Scripts: 2035
    Chunks:
     * Main Files: 13381
     * Scripts: 12411
    Strings: 23836
    Routes: 803
    Experiments Count: 181
    Apex Experiments Count: 195

--- chunks/340195.js
+++ chunks/340195.js
@@ -1,38 +1,39 @@
 "use strict";
-n.d(t, { A: () => p });
+n.d(t, { A: () => h });
 var r = n(627968);
 n(64700);
 var i = n(311907),
-    a = n(397927),
-    s = n(736653),
+    s = n(397927),
+    a = n(736653),
     o = n(775602),
     l = n(607470),
-    u = n(200430),
-    c = n(154057),
-    d = n(236806),
-    _ = n(988297),
-    f = n(613702);
-function p(e) {
+    u = n(676279),
+    c = n(750572),
+    d = n(154057),
+    _ = n(236806),
+    f = n(988297),
+    p = n(613702);
+function h(e) {
     let { className: t } = e,
         n = (0, i.bG)([o.A], () => o.A.useReducedMotion),
-        p = (0, s.Ay)(),
-        h = (0, a.qB1)(p);
-    if (n) {
-        let e = h ? _.A : c.A;
+        h = (0, a.Ay)(),
+        m = (0, s.qB1)(h);
+    if (n || (0, u.nr)()) {
+        let e = m ? f.A : d.A;
         return (0, r.jsxs)("div", {
             className: t,
-            children: [(0, r.jsx)("div", { className: u.YL }), (0, r.jsx)("img", { src: e, alt: "" })],
+            children: [(0, r.jsx)("div", { className: c.YL }), (0, r.jsx)("img", { src: e, alt: "" })],
         });
     }
-    let m = h ? f.A : d.A;
+    let E = m ? p.A : _.A;
     return (0, r.jsxs)("div", {
         className: t,
         children: [
-            (0, r.jsx)("div", { className: u.YL }),
+            (0, r.jsx)("div", { className: c.YL }),
             (0, r.jsx)(
                 l.A,
-                { muted: !0, autoPlay: !0, playsInline: !0, loop: !0, children: (0, r.jsx)("source", { src: m }) },
-                m,
+                { muted: !0, autoPlay: !0, playsInline: !0, loop: !0, children: (0, r.jsx)("source", { src: E }) },
+                E,
             ),
         ],
     });
