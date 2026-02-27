2cd6f5d61bf3f79b9ce8f9386b61e15b54edd28f
DiscrapperManager <admin@minerpl.xyz>
2026-02-28 00:46:03 +0100

    canaryRelease: 503745
    Main scripts: 3
    Lazy Loaded Scripts: 2034
    Chunks:
     * Main Files: 13387
     * Scripts: 12417
    Strings: 23854
    Routes: 803
    Experiments Count: 177
    Apex Experiments Count: 198

--- chunks/473553.js
+++ chunks/473553.js
@@ -1,43 +1,44 @@
-n.d(t, { A: () => E });
+n.d(t, { A: () => I });
 var i = n(627968),
     r = n(64700),
     l = n(158954),
     a = n(311907),
-    s = n(859703),
-    o = n(341915),
-    d = n(714510),
-    c = n(890687),
-    u = n(639214),
-    A = n(73473),
-    h = n(646764),
-    _ = n(654487),
-    m = n(985018),
-    p = n(453561);
-function g(e) {
+    s = n(544028),
+    o = n(859703),
+    d = n(341915),
+    c = n(714510),
+    u = n(890687),
+    A = n(639214),
+    h = n(73473),
+    _ = n(646764),
+    m = n(654487),
+    p = n(985018),
+    g = n(453561);
+function E(e) {
     let { quest: t } = e,
-        n = (0, c.fc)(t),
-        a = (0, d.mU)({
+        n = (0, u.fc)(t),
+        a = (0, c.mU)({
             quest: t,
             taskDetails: n,
-            location: _.rE.QUEST_ACTIVITY_HEADER,
-            questContent: o.uF.RUNNING_ACTIVITY,
-            sourceQuestContent: o.uF.RUNNING_ACTIVITY,
+            location: m.rE.QUEST_ACTIVITY_HEADER,
+            questContent: d.uF.RUNNING_ACTIVITY,
+            sourceQuestContent: d.uF.RUNNING_ACTIVITY,
         }),
-        [s, u] = r.useState(!1);
+        [s, o] = r.useState(!1);
     return (0, i.jsxs)("div", {
-        className: p.w,
-        onMouseEnter: () => u(!0),
-        onMouseLeave: () => u(!1),
+        className: g.w,
+        onMouseEnter: () => o(!0),
+        onMouseLeave: () => o(!1),
         children: [
-            (0, i.jsx)(h.A, {
+            (0, i.jsx)(_.A, {
                 quest: t,
-                questContent: o.uF.RUNNING_ACTIVITY,
-                sourceQuestContent: o.uF.RUNNING_ACTIVITY,
-                className: p.Q,
+                questContent: d.uF.RUNNING_ACTIVITY,
+                sourceQuestContent: d.uF.RUNNING_ACTIVITY,
+                className: g.Q,
                 autoplay: s,
             }),
             (0, i.jsx)(l.$nd, {
-                text: m.intl.formatToPlainString(m.t.EAYZAr, { questName: t.config.messages.questName }),
+                text: p.intl.formatToPlainString(p.t.EAYZAr, { questName: t.config.messages.questName }),
                 variant: "secondary",
                 size: "sm",
             }),
@@ -45,20 +46,28 @@ function g(e) {
         ],
     });
 }
-function E(e) {
+function I(e) {
     let { applicationId: t } = e,
-        n = (0, a.bG)([s.A], () => s.A.quests),
-        l = r.useMemo(
-            () => (0, u.jm)(n, t, !0).find((e) => !e.config.features.includes(_.Li.ACTIVITY_QUEST_AUTO_ENROLLMENT)),
+        n = (0, a.bG)([o.A], () => o.A.quests),
+        c = (0, a.bG)([s.A], () => s.A.theme),
+        u = r.useMemo(
+            () => (0, A.jm)(n, t, !0).find((e) => !e.config.features.includes(m.Li.ACTIVITY_QUEST_AUTO_ENROLLMENT)),
             [n, t],
         );
-    return null == l
+    return null == u
         ? null
-        : (0, i.jsx)(A.R, {
-              questOrQuests: l,
-              questContent: o.uF.RUNNING_ACTIVITY,
-              sourceQuestContent: o.uF.RUNNING_ACTIVITY,
-              overrideVisibility: !0,
-              children: () => (0, i.jsx)(g, { quest: l }),
+        : (0, i.jsx)(l.NPJ, {
+              theme: c,
+              children: (e) =>
+                  (0, i.jsx)("div", {
+                      className: e,
+                      children: (0, i.jsx)(h.R, {
+                          questOrQuests: u,
+                          questContent: d.uF.RUNNING_ACTIVITY,
+                          sourceQuestContent: d.uF.RUNNING_ACTIVITY,
+                          overrideVisibility: !0,
+                          children: () => (0, i.jsx)(E, { quest: u }),
+                      }),
+                  }),
           });
 }
