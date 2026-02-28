7649696e1bd77689fa55dee4fe6e7f48d3e12ac8
DiscrapperManager <admin@minerpl.xyz>
2026-02-28 02:17:29 +0100

    canaryRelease: 503835
    Main scripts: 3
    Lazy Loaded Scripts: 2035
    Chunks:
     * Main Files: 13389
     * Scripts: 12423
    Strings: 23854
    Routes: 803
    Experiments Count: 177
    Apex Experiments Count: 199

--- chunks/384726.js
+++ chunks/384726.js
@@ -1,21 +1,20 @@
 "use strict";
-n.d(t, { m: () => i });
+n.d(t, { A: () => i });
 var r = n(832946);
 function i(e) {
     return null == e
         ? {}
-        : Object.keys(e).reduce((t, n) => {
-              if (null == e) return t;
-              let i = e[n];
+        : Object.entries(e).reduce((e, t) => {
+              let [n, i] = t;
               return (
-                  (t[n] = {
+                  (e[n] = {
                       countryPrices: {
                           countryCode: i.country_prices.country_code,
                           prices: i.country_prices.prices.map((e) => (0, r.n_)(e, !0)),
                       },
                       paymentSourcePrices: {},
                   }),
-                  t
+                  e
               );
           }, {});
 }
