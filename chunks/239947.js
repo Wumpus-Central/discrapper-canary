f65a336781eef82c73abb703e1cc300467cdd5e8
DiscrapperManager <admin@minerpl.xyz>
2026-03-02 22:20:49 +0100

    canaryRelease: 504266
    Main scripts: 3
    Lazy Loaded Scripts: 2035
    Chunks:
     * Main Files: 13384
     * Scripts: 12431
    Strings: 23869
    Routes: 803
    Experiments Count: 172
    Apex Experiments Count: 201

--- chunks/239947.js
+++ chunks/239947.js
@@ -82,6 +82,7 @@ var r = (function (e) {
             (e.GAME_COMMUNITY_MULTI_GUILD_UPSELL_CARD = "impression_game_community_multi_guild_upsell_card"),
             (e.GAME_CONSOLE_DEVICE_LIST = "impression_game_console_device_list"),
             (e.GAME_DETECTION_REPORT_MODAL = "impression_game_detection_report_modal"),
+            (e.GAME_SERVER_ACTIVITY_BUTTON = "impression_game_server_activity_button"),
             (e.GDM_SETTINGS_INVITES = "impression_gdm_settings_invites"),
             (e.GIFT_INTENT_ACTION_BUTTON = "impression_gift_intent_action_button"),
             (e.GIFT_INTENT_BADGE = "impression_gift_intent_badge"),
