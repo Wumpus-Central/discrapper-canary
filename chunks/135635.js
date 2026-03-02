3c3983189ce5105f1381492fab677d12bb647652
DiscrapperManager <admin@minerpl.xyz>
2026-03-02 19:38:56 +0100

    canaryRelease: 504071
    Main scripts: 3
    Lazy Loaded Scripts: 2035
    Chunks:
     * Main Files: 13384
     * Scripts: 12424
    Strings: 23856
    Routes: 803
    Experiments Count: 175
    Apex Experiments Count: 198

--- chunks/135635.js
+++ chunks/135635.js
@@ -1,4 +1,4 @@
-n.d(t, { A: () => d });
+n.d(t, { A: () => g });
 var l = n(64700),
     r = n(158954),
     a = n(311907),
@@ -6,37 +6,42 @@ var l = n(64700),
     s = n(387755),
     o = n(313961),
     E = n(325909),
-    u = n(994500),
-    c = n(806931),
-    _ = n(985018);
-function d(e) {
-    let { user: t, channel: n, location: d } = e,
-        A = (0, E.oP)(t, d, n.id),
-        g = (0, E.lQ)(t, d, n),
-        T = t.bot || t.system || t.isProvisional,
-        I = (0, a.bG)([o.A], () => o.A.getParticipant(n.id, t.id)),
-        N = null == I,
-        h = null != I && I.type === c.lp.USER && I.ringing,
-        m = (0, a.bG)([u.A], () => u.A.isFriend(t.id)),
-        S = !A && (!g || T),
-        G = (0, l.useCallback)(() => {
-            if (A) {
-                if (N) return void s.A.ring(n.id, [t.id], d);
-                h && s.A.stopRinging(n.id, [t.id]);
+    u = n(290863),
+    c = n(994500),
+    _ = n(806931),
+    d = n(818348),
+    A = n(985018);
+function g(e) {
+    let { user: t, channel: n, location: g } = e,
+        T = (0, E.oP)(t, g, n.id),
+        I = (0, E.lQ)(t, g, n),
+        N = t.bot || t.system || t.isProvisional,
+        h = (0, a.bG)([o.A], () => o.A.getParticipant(n.id, t.id)),
+        m = null == h,
+        S = null != h && h.type === _.lp.USER && h.ringing,
+        G = (0, a.bG)([c.A], () => c.A.isFriend(t.id)),
+        O = (0, a.bG)([u.A], () => u.A.getStatus(t.id) === d.cl.DND && n?.guild_id != null),
+        R = !T && (!I || N),
+        C = (0, l.useCallback)(() => {
+            if (T) {
+                if (m) return void s.A.ring(n.id, [t.id], g);
+                S && s.A.stopRinging(n.id, [t.id]);
             }
-        }, [N, h, A, d, n.id, t.id]);
+        }, [m, S, T, g, n.id, t.id]);
     return {
-        iconColor: h ? i.A.colors.ICON_FEEDBACK_CRITICAL : void 0,
-        icon: h ? r.zWQ : r._jp,
-        tooltipText: m
-            ? N
-                ? _.intl.string(_.t["3Hv9qQ"])
-                : h
-                  ? _.intl.string(_.t.ygslb0)
-                  : null
-            : _.intl.string(_.t.TGqF9g),
-        disabled: !A,
-        shouldHideButton: S,
-        onClick: G,
+        iconColor: S ? i.A.colors.ICON_FEEDBACK_CRITICAL : void 0,
+        icon: S ? r.zWQ : r._jp,
+        tooltipText: G
+            ? O
+                ? A.intl.string(A.t.jaNpQH)
+                : m
+                  ? A.intl.string(A.t["3Hv9qQ"])
+                  : S
+                    ? A.intl.string(A.t.ygslb0)
+                    : null
+            : A.intl.string(A.t.TGqF9g),
+        disabled: !T,
+        shouldHideButton: R,
+        onClick: C,
     };
 }
--- chunks/325909.js
+++ chunks/325909.js
@@ -1,44 +1,46 @@
 "use strict";
-n.d(t, { lQ: () => E, oP: () => m, pW: () => g });
+n.d(t, { lQ: () => A, oP: () => E, pW: () => g });
 var r = n(311907),
     i = n(890615),
-    a = n(721592),
-    s = n(164891),
+    s = n(721592),
+    a = n(164891),
     o = n(961350),
     l = n(470710),
     u = n(734057),
     c = n(696451),
     d = n(834942),
     _ = n(576705),
-    f = n(994500),
-    p = n(977997),
-    h = n(652215);
-function m(e) {
+    f = n(290863),
+    p = n(994500),
+    h = n(977997),
+    m = n(652215);
+function E(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : "useCanRing",
         n = arguments.length > 2 ? arguments[2] : void 0,
         i = (0, r.bG)([u.A], () => u.A.getChannel(n)),
-        a = (0, r.bG)([o.default], () => o.default.getId() === e.id),
-        s = (0, r.bG)([f.A], () => f.A.isFriend(e.id)),
-        l = i?.type,
-        c = null != l && h.kvI.CALLABLE.has(l),
-        d = E(e, t, i) || c;
-    return s && !a && !e.bot && !e.system && !e.isProvisional && d;
+        s = (0, r.bG)([o.default], () => o.default.getId() === e.id),
+        a = (0, r.bG)([p.A], () => p.A.isFriend(e.id)),
+        l = (0, r.bG)([f.A], () => f.A.getStatus(e.id) === m.clD.DND && i?.guild_id != null),
+        c = i?.type,
+        d = null != c && m.kvI.CALLABLE.has(c),
+        _ = A(e, t, i) || d;
+    return a && !l && !s && !e.bot && !e.system && !e.isProvisional && _;
 }
 function g(e) {
-    let t = h.kvI.CALLABLE.has(e.type),
-        n = e.type === h.rbe.GUILD_VOICE;
+    let t = m.kvI.CALLABLE.has(e.type),
+        n = e.type === m.rbe.GUILD_VOICE;
     if (t) {
         let t = l.A.getCall(e.id);
         return null != t && null != t.messageId && !l.A.isCallUnavailable(e.id);
     }
     if (n) {
-        let { enabled: t } = s.A.getCurrentConfig({ guildId: e.guild_id, location: "ring" }),
-            n = p.A.getVoiceState(e.guild_id, o.default.getId());
+        let { enabled: t } = a.A.getCurrentConfig({ guildId: e.guild_id, location: "ring" }),
+            n = h.A.getVoiceState(e.guild_id, o.default.getId());
         return t && null != n && n.channelId === e.id;
     }
     return !1;
 }
-function E(e) {
+function A(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : "useCanRingToGuildVoiceChannel",
         n = arguments.length > 2 ? arguments[2] : void 0,
         o = (0, r.bG)([_.A], () => null != n && (0, i.A)(n, _.A)),
@@ -49,8 +51,8 @@ function E(e) {
                 i = d.A.getCheck(t).canChat;
             return r && i;
         }),
-        { needSubscriptionToAccess: u } = (0, a.A)(n?.id),
-        { enabled: f } = s.A.useExperiment({ guildId: n?.guild_id, location: t }),
-        p = n?.type === h.rbe.GUILD_VOICE;
+        { needSubscriptionToAccess: u } = (0, s.A)(n?.id),
+        { enabled: f } = a.A.useExperiment({ guildId: n?.guild_id, location: t }),
+        p = n?.type === m.rbe.GUILD_VOICE;
     return f && p && l && o && !u;
 }
