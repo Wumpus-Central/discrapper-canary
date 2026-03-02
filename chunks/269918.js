e455494344609564fe5451e0ca9c8b2cdc35e1b7
DiscrapperManager <admin@minerpl.xyz>
2026-03-02 20:32:58 +0100

    canaryRelease: 504113
    Main scripts: 3
    Lazy Loaded Scripts: 2035
    Chunks:
     * Main Files: 13386
     * Scripts: 12424
    Strings: 23856
    Routes: 803
    Experiments Count: 174
    Apex Experiments Count: 198

--- chunks/269918.js
+++ chunks/269918.js
@@ -1,7 +1,7 @@
 "use strict";
 n.d(t, { A: () => i });
 let i = (0, n(945810).mj)({
-    name: "2026-2-game-account-linking-entry-point-recurring",
+    name: "2026-02-game-account-linking-entry-point-recurring",
     kind: "user",
     defaultConfig: { enabled: !1 },
     variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
