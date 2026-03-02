54ab5aa5f4ab4526420e79812b6f5243b5be5fa4
DiscrapperManager <admin@minerpl.xyz>
2026-03-02 03:03:55 +0100

    canaryRelease: 503924
    Main scripts: 3
    Lazy Loaded Scripts: 2035
    Chunks:
     * Main Files: 13385
     * Scripts: 12423
    Strings: 23854
    Routes: 803
    Experiments Count: 176
    Apex Experiments Count: 199

--- chunks/871751.js
+++ chunks/871751.js
@@ -868,7 +868,7 @@ class B extends s.PureComponent {
                 embed: { image: l, images: t, video: r, type: s, rawDescription: n },
             } = this.props,
             { maxMediaWidth: i, maxMediaHeight: a } = this.state;
-        if (!this.usesJustifiedAutoStyle()) return;
+        if (!this.usesJustifiedAutoStyle()) return { justifySelf: "start", alignSelf: "start" };
         let d = e ? void 0 : this.getMaxWidth(!1);
         if (void 0 === d && void 0 === t && s !== k.Auw.RICH) {
             let e = l ?? r;
