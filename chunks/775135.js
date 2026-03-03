329e24ce712a0d144ad4d1c432c3bc3df40eb833
DiscrapperManager <admin@minerpl.xyz>
2026-03-03 19:22:28 +0100

    canaryRelease: 504844
    Main scripts: 3
    Lazy Loaded Scripts: 2037
    Chunks:
     * Main Files: 13378
     * Scripts: 12445
    Strings: 23872
    Routes: 805
    Experiments Count: 169
    Apex Experiments Count: 195

--- chunks/775135.js
+++ chunks/775135.js
@@ -5,7 +5,7 @@ var i = n(627968),
     a = n(554146),
     s = n(506774),
     o = n(108722),
-    d = n(379848),
+    d = n(932001),
     c = n(975076),
     u = n(67602),
     A = n(571616),
@@ -34,53 +34,56 @@ function R(e) {
     let n,
         { guild: _ } = e,
         [p, g] = r.useState(s.w.get(v.hV) ?? 0),
-        E = (0, l.yK)(
-            v.cJ,
-            () =>
-                v.cA
-                    .filter((e) => {
-                        let { dismissibleContentType: t, store: n } = e;
-                        return !0 === n?.channelNoticePredicate(_, p) && !b.has(t);
-                    })
-                    .map((e) => e.dismissibleContentType),
-            [_, p],
-        ),
-        [I, f] = (0, d.kn)(E, y.m.CHANNEL_NOTICES),
-        C =
+        E =
             ((t = _.id),
             (n = (0, c.I)(t)),
             r.useMemo(() => {
                 let e = [];
                 return n && e.push(a.M.GAME_CLAIM_COACHMARK), e.length > 0 ? e : L;
             }, [n])),
-        [N, x] = (0, d.ww)(C, _.id, y.m.CHANNEL_NOTICES, !0),
-        R = I ?? N,
-        P = null != I ? f : x,
-        j = r.useCallback(() => {
+        [I, f] = (0, d.ww)(E, _.id, y.m.CHANNEL_NOTICES, !0),
+        C = null != I,
+        N = (0, l.yK)(
+            v.cJ,
+            () =>
+                C
+                    ? []
+                    : v.cA
+                          .filter((e) => {
+                              let { dismissibleContentType: t, store: n } = e;
+                              return !0 === n?.channelNoticePredicate(_, p) && !b.has(t);
+                          })
+                          .map((e) => e.dismissibleContentType),
+            [_, p, C],
+        ),
+        [x, R] = (0, d.kn)(N, y.m.CHANNEL_NOTICES),
+        P = I ?? x,
+        j = null != I ? f : R,
+        D = r.useCallback(() => {
             var e;
-            (e = Date.now()), s.w.set(v.hV, e), g(e), P(y.i.UNKNOWN);
-        }, [P]),
-        D = (() => {
-            switch (R) {
+            (e = Date.now()), s.w.set(v.hV, e), g(e), j(y.i.UNKNOWN);
+        }, [j]),
+        M = (() => {
+            switch (P) {
                 case a.M.CHANNEL_NOTICE_HUBLINK:
-                    return (0, i.jsx)(m.A, { guild: _, markAsDismissed: j });
+                    return (0, i.jsx)(m.A, { guild: _, markAsDismissed: D });
                 case a.M.CHANNEL_NOTICE_INVITE:
-                    return (0, i.jsx)(T.A, { guild: _, markAsDismissed: j });
+                    return (0, i.jsx)(T.A, { guild: _, markAsDismissed: D });
                 case a.M.CHANNEL_NOTICE_PREMIUM_GUILD_SUBSCRIPTION:
-                    return (0, i.jsx)(h.A, { guild: _, markAsDismissed: j });
+                    return (0, i.jsx)(h.A, { guild: _, markAsDismissed: D });
                 case a.M.CHANNEL_NOTICE_QUICKSWITCHER:
-                    return (0, i.jsx)(S.A, { guild: _, markAsDismissed: j });
+                    return (0, i.jsx)(S.A, { guild: _, markAsDismissed: D });
                 case a.M.CHANNEL_NOTICE_GUILD_BANNER:
-                    return (0, i.jsx)(A.A, { guild: _, markAsDismissed: j });
+                    return (0, i.jsx)(A.A, { guild: _, markAsDismissed: D });
                 case a.M.LINKED_ROLE_ADMIN_GUILD:
-                    return (0, i.jsx)(o.A, { guild: _, markAsDismissed: () => P(y.i.UNKNOWN) });
+                    return (0, i.jsx)(o.A, { guild: _, markAsDismissed: () => j(y.i.UNKNOWN) });
                 case a.M.GAME_CLAIM_COACHMARK:
-                    return (0, i.jsx)(u.A, { guild: _, markAsDismissed: P });
+                    return (0, i.jsx)(u.A, { guild: _, markAsDismissed: j });
                 default:
                     return null;
             }
         })();
-    return null == D ? null : (0, i.jsx)("div", { onContextMenu: O, children: D });
+    return null == M ? null : (0, i.jsx)("div", { onContextMenu: O, children: M });
 }
 let P = (e) => {
     let { guild: t } = e;
