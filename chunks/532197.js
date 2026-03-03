3521de29a20169d65843d234eea6103f3fcf2156
DiscrapperManager <admin@minerpl.xyz>
2026-03-03 01:51:24 +0100

    canaryRelease: 504514
    Main scripts: 3
    Lazy Loaded Scripts: 2036
    Chunks:
     * Main Files: 13381
     * Scripts: 12436
    Strings: 23870
    Routes: 805
    Experiments Count: 168
    Apex Experiments Count: 198

--- chunks/532197.js
+++ chunks/532197.js
@@ -1,18 +1,17 @@
-"use strict";
-n.d(t, { A: () => _ });
-var l = n(627968),
-    a = n(64700),
-    r = n(503698),
-    s = n.n(r),
-    i = n(397927),
-    o = n(262633);
-let c = { UP: "up", RIGHT: "right", DOWN: "down", LEFT: "left" },
-    d = { [c.UP]: "", [c.RIGHT]: o.pG, [c.DOWN]: o.TR, [c.LEFT]: o.kb };
-class u extends a.PureComponent {
-    static Directions = c;
+n.d(t, { A: () => A });
+var i = n(627968),
+    s = n(64700),
+    l = n(503698),
+    r = n.n(l),
+    a = n(397927),
+    d = n(262633);
+let o = { UP: "up", RIGHT: "right", DOWN: "down", LEFT: "left" },
+    c = { [o.UP]: "", [o.RIGHT]: d.pG, [o.DOWN]: d.TR, [o.LEFT]: d.kb };
+class u extends s.PureComponent {
+    static Directions = o;
     render() {
         let { direction: e, className: t } = this.props;
-        return (0, l.jsx)(i.z$m, { size: "md", color: "currentColor", className: s()(t, { [d[e]]: !0 }) });
+        return (0, i.jsx)(a.z$m, { size: "md", color: "currentColor", className: r()(t, { [c[e]]: !0 }) });
     }
 }
-let _ = u;
+let A = u;
--- chunks/734066.js
+++ chunks/734066.js
@@ -1,105 +1,70 @@
 "use strict";
-n.d(t, { BW: () => _, J: () => p, L_: () => o, Pm: () => u, Vr: () => f, cx: () => d, sw: () => c });
-var r = n(600975),
+n.d(t, { BW: () => h, J: () => E, L_: () => c, Pm: () => d, Vr: () => m, cx: () => p, sw: () => _ });
+var r = n(311907),
     i = n(945810),
-    s = n(430452);
-n(927578);
-var a = n(915618);
-n(788868);
-let o = (0, i.mj)({
-        kind: "user",
-        name: "2026-03-clips-experiment",
-        defaultConfig: {
-            enableClips: !1,
+    s = n(430452),
+    a = n(287809),
+    o = n(927578),
+    l = n(915618),
+    u = n(788868);
+let c = (0, i.mj)({
+    kind: "user",
+    name: "2026-03-clips-experiment",
+    defaultConfig: {
+        enableClips: !1,
+        ignorePlatformRestriction: !1,
+        showClipsHeaderEntrypoint: !1,
+        enableScreenshotKeybind: !1,
+        enableVoiceOnlyClips: !1,
+        enableAdvancedSignals: !1,
+    },
+    variations: {
+        1: {
+            enableClips: !0,
             ignorePlatformRestriction: !1,
-            showClipsHeaderEntrypoint: !1,
+            showClipsHeaderEntrypoint: !0,
             enableScreenshotKeybind: !1,
             enableVoiceOnlyClips: !1,
             enableAdvancedSignals: !1,
         },
-        variations: {
-            1: {
-                enableClips: !0,
-                ignorePlatformRestriction: !1,
-                showClipsHeaderEntrypoint: !0,
-                enableScreenshotKeybind: !1,
-                enableVoiceOnlyClips: !1,
-                enableAdvancedSignals: !1,
-            },
-            99: {
-                enableClips: !0,
-                ignorePlatformRestriction: !0,
-                showClipsHeaderEntrypoint: !0,
-                enableScreenshotKeybind: !0,
-                enableVoiceOnlyClips: !0,
-                enableAdvancedSignals: !0,
-            },
+        99: {
+            enableClips: !0,
+            ignorePlatformRestriction: !0,
+            showClipsHeaderEntrypoint: !0,
+            enableScreenshotKeybind: !0,
+            enableVoiceOnlyClips: !0,
+            enableAdvancedSignals: !0,
         },
-    }),
-    l = (0, r.C)({
-        kind: "user",
-        id: "2023-09_clips_nitro_early_access",
-        label: "Clips (Nitro EA)",
-        defaultConfig: {
-            enableClips: !1,
-            enablePremiumEarlyAccessAnnouncementCoachmark: !1,
-            enablePremiumEarlyAccessGoLiveRoadblock: !1,
-            enableScreenshotKeybind: !1,
-            enableVoiceOnlyClips: !1,
-            enableAdvancedSignals: !1,
-        },
-        treatments: [
-            {
-                id: 1,
-                label: "Clips Nitro EA Upsells Visible",
-                config: {
-                    enableClips: !1,
-                    enablePremiumEarlyAccessAnnouncementCoachmark: !0,
-                    enablePremiumEarlyAccessGoLiveRoadblock: !0,
-                    enableScreenshotKeybind: !1,
-                    enableVoiceOnlyClips: !1,
-                    enableAdvancedSignals: !1,
-                },
-            },
-            {
-                id: 2,
-                label: "Clips Enabled (Nitro)",
-                config: {
-                    enableClips: !0,
-                    enablePremiumEarlyAccessAnnouncementCoachmark: !0,
-                    enablePremiumEarlyAccessGoLiveRoadblock: !1,
-                    enableScreenshotKeybind: !1,
-                    enableVoiceOnlyClips: !1,
-                    enableAdvancedSignals: !1,
-                },
-            },
-        ],
-    });
-function u() {
-    if (!(0, a.A)(s.Ay)) return !1;
-    let { enableClips: e } = l.getCurrentConfig({ location: "areClipsEnabled" }, { autoTrackExposure: !1 }),
-        { enableClips: t } = o.getConfig({ location: "areClipsEnabled" });
+    },
+});
+function d() {
+    if (!(0, l.A)(s.Ay)) return !1;
+    let e = f(a.default.getCurrentUser()),
+        { enableClips: t } = c.getConfig({ location: "areClipsEnabled" });
     return e || t;
 }
-function c() {
-    let e = (0, a.A)(s.Ay),
-        { enableClips: t } = l.useExperiment({ location: "useEnableClips" }, { autoTrackExposure: !1 }),
-        { enableClips: n } = o.getConfig({ location: "useEnableClips" });
+function _() {
+    let e = (0, l.A)(s.Ay),
+        t = (0, r.bG)([a.default], () => f(a.default.getCurrentUser())),
+        { enableClips: n } = c.getConfig({ location: "useEnableClips" });
     return (n || t) && e;
 }
-function d() {
-    let { enableScreenshotKeybind: e } = o.getConfig({ location: "isScreenshotKeybindEnabled" });
-    return e;
-}
-function _() {
-    let { enableScreenshotKeybind: e } = o.useConfig({ location: "useScreenshotKeybindEnabled" });
-    return e;
-}
-function f() {
-    let { enableVoiceOnlyClips: e } = o.getConfig({ location: "areVoiceOnlyClipsEnabled" });
-    return e;
+function f(e) {
+    return o.Ay.isPremiumAtLeast(e?.premiumType, u.PremiumTypes.TIER_2);
 }
 function p() {
-    let { enableAdvancedSignals: e } = o.getConfig({ location: "areAdvancedSignalsEnabled" });
+    let { enableScreenshotKeybind: e } = c.getConfig({ location: "isScreenshotKeybindEnabled" });
+    return e;
+}
+function h() {
+    let { enableScreenshotKeybind: e } = c.useConfig({ location: "useScreenshotKeybindEnabled" });
+    return e;
+}
+function m() {
+    let { enableVoiceOnlyClips: e } = c.getConfig({ location: "areVoiceOnlyClipsEnabled" });
+    return e;
+}
+function E() {
+    let { enableAdvancedSignals: e } = c.getConfig({ location: "areAdvancedSignalsEnabled" });
     return e;
 }
