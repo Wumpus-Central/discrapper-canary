48489f0dabff2a2a8aeb34fee45c4bee4a2caef7
DiscrapperManager <admin@minerpl.xyz>
2026-03-03 14:16:31 +0100

    canaryRelease: 504677
    Main scripts: 3
    Lazy Loaded Scripts: 2036
    Chunks:
     * Main Files: 13380
     * Scripts: 12444
    Strings: 23872
    Routes: 805
    Experiments Count: 169
    Apex Experiments Count: 197

--- chunks/561923.js
+++ chunks/561923.js
@@ -1,64 +1,52 @@
 "use strict";
-n.d(t, { A: () => A });
-var i = n(627968),
-    s = n(64700),
-    l = n(503698),
-    r = n.n(l),
-    a = n(280513),
-    o = n(311907),
-    d = n(397927),
-    c = n(837011),
-    u = n(348943),
-    m = n(576055),
-    g = n(997509),
-    x = n(665346),
-    h = n(652215),
-    _ = n(985018),
-    p = n(7791);
-function A(e) {
-    let { profile: t, canManageGuild: n } = e,
-        l = t.id,
-        A = a.i.VISIBLE.has(t.visibility),
-        f = (0, o.bG)([c.A], () => c.A.getProfile(l)?.visibility === u.n.PUBLIC_WITH_RECRUITMENT, [l]),
-        j = s.useRef(null);
-    (0, x.A)(j, h.nd0.PROFILE_VISIBILITY);
-    let N = s.useCallback(() => {
-            A
-                ? g.A.updateGuildProfile(l, { visibility: u.n.RESTRICTED })
-                : g.A.updateGuildProfile(l, { visibility: f ? u.n.PUBLIC_WITH_RECRUITMENT : u.n.PUBLIC });
-        }, [l, f, A]),
-        E = s.useCallback(() => {
-            g.A.setSection(h.BEX.ACCESS, h.nd0.ACCESS_DISCOVERABLE);
-        }, []),
-        b = t.features?.includes(h.GuildFeatures.DISCOVERABLE);
-    return (0, i.jsxs)("div", {
-        className: p.Im,
-        ref: j,
-        children: [
-            (0, i.jsxs)("div", {
-                className: p.fi,
-                children: [
-                    (0, i.jsx)(d.dOG, {
-                        label: _.intl.string(_.t.fjHWen),
-                        description: _.intl.string(_.t.J1YOV6),
-                        checked: !A,
-                        onChange: N,
-                        disabled: b || !n,
-                    }),
-                    b &&
-                        (0, i.jsx)(d.Text, {
-                            variant: "text-xs/normal",
-                            color: "text-muted",
-                            children: _.intl.format(_.t.R8jY9g, {
-                                accessLink: (e, t) => (0, i.jsx)(d.MzZ, { onClick: E, children: e }, t),
-                            }),
-                        }),
-                ],
-            }),
-            (0, i.jsx)("div", {
-                className: r()(p.fi, { [p.Rl]: A }),
-                children: (0, i.jsx)(m.A, { guildId: l, name: null }),
-            }),
-        ],
-    });
+Object.defineProperty(e, "__esModule", { value: !0 });
+var o =
+        Object.assign ||
+        function (t) {
+            for (var e = 1; e < arguments.length; e++) {
+                var r = arguments[e];
+                for (var o in r) Object.prototype.hasOwnProperty.call(r, o) && (t[o] = r[o]);
+            }
+            return t;
+        },
+    n = u(r(223108)),
+    i = r(64700),
+    a = u(i);
+function u(t) {
+    return t && t.__esModule ? t : { default: t };
 }
+var s = {
+        bgColor: n.default.oneOfType([n.default.object, n.default.string]).isRequired,
+        bgD: n.default.string.isRequired,
+        fgColor: n.default.oneOfType([n.default.object, n.default.string]).isRequired,
+        fgD: n.default.string.isRequired,
+        size: n.default.number.isRequired,
+        title: n.default.string,
+        viewBoxSize: n.default.number.isRequired,
+        xmlns: n.default.string,
+    },
+    l = (0, i.forwardRef)(function (t, e) {
+        var r = t.bgColor,
+            n = t.bgD,
+            i = t.fgD,
+            u = t.fgColor,
+            s = t.size,
+            l = t.title,
+            f = t.viewBoxSize,
+            h = (function (t, e) {
+                var r = {};
+                for (var o in t) !(e.indexOf(o) >= 0) && Object.prototype.hasOwnProperty.call(t, o) && (r[o] = t[o]);
+                return r;
+            })(t, ["bgColor", "bgD", "fgD", "fgColor", "size", "title", "viewBoxSize"]);
+        return a.default.createElement(
+            "svg",
+            o({}, h, { height: s, ref: e, viewBox: "0 0 " + f + " " + f, width: s }),
+            l ? a.default.createElement("title", null, l) : null,
+            a.default.createElement("path", { d: n, fill: r }),
+            a.default.createElement("path", { d: i, fill: u }),
+        );
+    });
+(l.displayName = "QRCodeSvg"),
+    (l.propTypes = s),
+    (l.defaultProps = { title: void 0, xmlns: "http://www.w3.org/2000/svg" }),
+    (e.default = l);
