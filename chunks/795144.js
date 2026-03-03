b6b3cf4857a2790da5739c30699acc60d777702d
DiscrapperManager <admin@minerpl.xyz>
2026-03-03 22:21:39 +0100

    canaryRelease: 505076
    Main scripts: 3
    Lazy Loaded Scripts: 2038
    Chunks:
     * Main Files: 13377
     * Scripts: 12454
    Strings: 23907
    Routes: 805
    Experiments Count: 169
    Apex Experiments Count: 194

--- chunks/795144.js
+++ chunks/795144.js
@@ -1,34 +1,31 @@
-n.d(t, { A: () => o });
-var i = n(627968);
-n(64700);
-var a = n(311907),
-    r = n(397927),
-    l = n(967198),
-    d = n(652215),
-    s = n(985018);
-function o(e) {
-    let t = (0, a.bG)([l.A], () => l.A.getGuildId());
-    return (
-        e.type,
-        d.rbe.GUILD_CATEGORY,
-        __OVERLAY__ || t !== d.YYv
-            ? null
-            : (0, i.jsx)(r.Drp, {
-                  id: "set-channel-nickname",
-                  label: s.intl.string(s.t.dilOF6),
-                  action: function () {
-                      (0, r.mMO)(async () => {
-                          let { default: t } = await n.e("83504").then(n.bind(n, 872243));
-                          return (n) =>
-                              (0, i.jsx)(t, {
-                                  ...n,
-                                  channelId: e.id,
-                                  heading: s.intl.string(s.t.dilOF6),
-                                  formTitle: s.intl.string(s.t["621LJD"]),
-                                  allowReset: !0,
-                              });
-                      });
-                  },
-              })
-    );
+e.d(n, { A: () => s });
+var i = e(627968);
+e(64700);
+var l = e(311907),
+    a = e(397927),
+    r = e(967198),
+    d = e(652215),
+    o = e(985018);
+function s(t) {
+    let n = (0, l.bG)([r.A], () => r.A.getGuildId()),
+        s = t.type === d.rbe.GUILD_CATEGORY;
+    return __OVERLAY__ || n !== d.YYv
+        ? null
+        : (0, i.jsx)(a.Drp, {
+              id: "set-channel-nickname",
+              label: s ? o.intl.string(o.t.xXYKiP) : o.intl.string(o.t.dilOF6),
+              action: function () {
+                  (0, a.mMO)(async () => {
+                      let { default: n } = await e.e("83504").then(e.bind(e, 872243));
+                      return (e) =>
+                          (0, i.jsx)(n, {
+                              ...e,
+                              channelId: t.id,
+                              heading: s ? o.intl.string(o.t.xXYKiP) : o.intl.string(o.t.dilOF6),
+                              formTitle: s ? o.intl.string(o.t.OCAkGP) : o.intl.string(o.t["621LJD"]),
+                              allowReset: !1,
+                          });
+                  });
+              },
+          });
 }
