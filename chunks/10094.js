2c01ca5e2a1f614c24e2b6aeaec75a91b090ebe2
DiscrapperManager <admin@minerpl.xyz>
2026-03-04 01:38:40 +0100

    canaryRelease: 505358
    Main scripts: 3
    Lazy Loaded Scripts: 2038
    Chunks:
     * Main Files: 13374
     * Scripts: 12461
    Strings: 23918
    Routes: 805
    Experiments Count: 169
    Apex Experiments Count: 194

--- chunks/10094.js
+++ chunks/10094.js
@@ -1,11 +1,14 @@
-n.d(t, { O: () => r, Y: () => a });
+n.d(t, { O: () => l, Y: () => r });
 var i = n(73153),
-    s = n(287809);
-let r = (e, t) => {
+    s = n(963852),
+    a = n(287809);
+let l = (e, t) => {
         i.h.dispatch({ type: "SET_PREMIUM_TYPE_OVERRIDE", premiumType: e });
-        let n = t ?? s.default.getCurrentUser();
-        null != n && i.h.dispatch({ type: "UPDATE_CLIENT_PREMIUM_TYPE", user: n });
+        let n = t ?? a.default.getCurrentUser();
+        null != n &&
+            (i.h.dispatch({ type: "UPDATE_CLIENT_PREMIUM_TYPE", user: n }),
+            i.h.dispatch({ type: "CURRENT_USER_UPDATE", user: (0, s.Vi)(n) }));
     },
-    a = (e) => {
+    r = (e) => {
         i.h.dispatch({ type: "SET_CREATED_AT_OVERRIDE", createdAt: e });
     };
