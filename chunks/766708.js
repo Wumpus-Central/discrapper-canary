ddac4ee5a03846fc287d950ada9311df330119dc
DiscrapperManager <admin@minerpl.xyz>
2026-03-04 03:14:35 +0100

    canaryRelease: 505448
    Main scripts: 3
    Lazy Loaded Scripts: 2038
    Chunks:
     * Main Files: 13379
     * Scripts: 12463
    Strings: 23918
    Routes: 805
    Experiments Count: 169
    Apex Experiments Count: 194

--- chunks/766708.js
+++ chunks/766708.js
@@ -3,7 +3,7 @@ n.d(t, { rn: () => s, vz: () => i });
 var r = n(945810),
     i = (function (e) {
         return (
-            (e.FLOATING = "FLOATING"), (e.FLOATING_REDESIGNED = "FLOATING_REDESIGNED"), (e.BOTTOM_BAR = "BOTTOM_BAR"), e
+            (e.FLOATING = "FLOATING"), (e.FLOATING_EXPRESSIVE = "FLOATING_EXPRESSIVE"), (e.BOTTOM_BAR = "BOTTOM_BAR"), e
         );
     })({});
 let s = (0, r.mj)({
@@ -20,21 +20,84 @@ let s = (0, r.mj)({
     },
     variations: {
         1: {
-            upsellDisplayType: "FLOATING_REDESIGNED",
+            upsellDisplayType: "FLOATING_EXPRESSIVE",
+            floatingUpsellAlwaysVisible: !1,
+            subscribeButtonVariant: "expressive",
+            reorderDefaultSounds: !1,
+            enablePinnedDefaults: !1,
+            enableSoundPreview: !1,
+            showUpsellWithoutLockedSounds: !1,
+        },
+        2: {
+            upsellDisplayType: "FLOATING_EXPRESSIVE",
+            floatingUpsellAlwaysVisible: !0,
+            subscribeButtonVariant: "primary",
+            reorderDefaultSounds: !1,
+            enablePinnedDefaults: !1,
+            enableSoundPreview: !1,
+            showUpsellWithoutLockedSounds: !1,
+        },
+        3: {
+            upsellDisplayType: "BOTTOM_BAR",
+            floatingUpsellAlwaysVisible: !0,
+            subscribeButtonVariant: "expressive",
+            reorderDefaultSounds: !1,
+            enablePinnedDefaults: !1,
+            enableSoundPreview: !1,
+            showUpsellWithoutLockedSounds: !1,
+        },
+        4: {
+            upsellDisplayType: "BOTTOM_BAR",
+            floatingUpsellAlwaysVisible: !0,
+            subscribeButtonVariant: "primary",
+            reorderDefaultSounds: !1,
+            enablePinnedDefaults: !1,
+            enableSoundPreview: !1,
+            showUpsellWithoutLockedSounds: !1,
+        },
+        5: {
+            upsellDisplayType: "FLOATING_EXPRESSIVE",
+            floatingUpsellAlwaysVisible: !1,
+            subscribeButtonVariant: "expressive",
+            reorderDefaultSounds: !0,
+            enablePinnedDefaults: !0,
+            enableSoundPreview: !1,
+            showUpsellWithoutLockedSounds: !1,
+        },
+        6: {
+            upsellDisplayType: "FLOATING_EXPRESSIVE",
+            floatingUpsellAlwaysVisible: !0,
+            subscribeButtonVariant: "overlay-primary",
+            reorderDefaultSounds: !0,
+            enablePinnedDefaults: !0,
+            enableSoundPreview: !1,
+            showUpsellWithoutLockedSounds: !1,
+        },
+        7: {
+            upsellDisplayType: "FLOATING_EXPRESSIVE",
             floatingUpsellAlwaysVisible: !0,
             subscribeButtonVariant: "primary",
             reorderDefaultSounds: !0,
             enablePinnedDefaults: !0,
-            enableSoundPreview: !0,
+            enableSoundPreview: !1,
             showUpsellWithoutLockedSounds: !1,
         },
-        2: {
+        8: {
             upsellDisplayType: "BOTTOM_BAR",
             floatingUpsellAlwaysVisible: !0,
-            subscribeButtonVariant: "expressive",
+            subscribeButtonVariant: "overlay-primary",
             reorderDefaultSounds: !0,
             enablePinnedDefaults: !0,
-            enableSoundPreview: !0,
+            enableSoundPreview: !1,
+            showUpsellWithoutLockedSounds: !1,
+        },
+        9: {
+            upsellDisplayType: "BOTTOM_BAR",
+            floatingUpsellAlwaysVisible: !0,
+            subscribeButtonVariant: "primary",
+            reorderDefaultSounds: !0,
+            enablePinnedDefaults: !0,
+            enableSoundPreview: !1,
             showUpsellWithoutLockedSounds: !1,
         },
     },
