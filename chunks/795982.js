92643133f72f86a59db17a4a461086fb3028cb98
DiscrapperManager <admin@minerpl.xyz>
2026-03-04 02:51:10 +0100

    canaryRelease: 505436
    Main scripts: 3
    Lazy Loaded Scripts: 2038
    Chunks:
     * Main Files: 13379
     * Scripts: 12463
    Strings: 23918
    Routes: 805
    Experiments Count: 169
    Apex Experiments Count: 194

--- chunks/795982.js
+++ chunks/795982.js
@@ -1,4 +1,4 @@
-n.d(t, { A: () => k });
+n.d(t, { A: () => w });
 var i = n(627968),
     l = n(64700),
     r = n(158954),
@@ -10,65 +10,62 @@ var i = n(627968),
     u = n(958340),
     _ = n(384231),
     m = n(291812),
-    h = n(112758),
-    p = n(754459),
-    g = n(538355),
-    A = n(699352),
-    x = n(8455),
-    f = n(253932),
-    C = n(734057),
-    E = n(540999),
-    I = n(71393),
-    b = n(576705),
-    T = n(994500),
-    v = n(287809),
-    S = n(862804),
-    y = n(627950),
-    N = n(387408),
-    j = n(427209),
-    L = n(381941),
-    R = n(985018),
-    P = n(772004);
-function M() {
+    h = n(538355),
+    p = n(699352),
+    g = n(8455),
+    A = n(253932),
+    x = n(734057),
+    f = n(540999),
+    C = n(71393),
+    E = n(576705),
+    I = n(994500),
+    b = n(287809),
+    T = n(862804),
+    v = n(627950),
+    S = n(387408),
+    y = n(427209),
+    N = n(985018),
+    j = n(772004);
+function L() {
     return (0, i.jsxs)("div", {
-        className: P.N1,
+        className: j.N1,
         children: [
-            (0, i.jsx)(j.A, { size: "xs", className: P.nr, color: o.A.colors.TEXT_MUTED }),
+            (0, i.jsx)(y.A, { size: "xs", className: j.nr, color: o.A.colors.TEXT_MUTED }),
             (0, i.jsx)(r.EYj, {
-                className: P.TK,
+                className: j.TK,
                 variant: "text-sm/semibold",
                 color: "text-muted",
-                children: R.intl.string(R.t.ToyvLk),
+                children: N.intl.string(N.t.ToyvLk),
             }),
         ],
     });
 }
-function w(e) {
+function R(e) {
     let { message: t, snapshot: n, index: c } = e,
-        _ = l.useMemo(() => new S.j(t, n, c), [t, n, c]),
+        _ = l.useMemo(() => new T.j(t, n, c), [t, n, c]),
         m = (0, a.bG)(
-            [C.A, v.default, T.A, b.A, I.A, u.A],
-            () => _.getForwardInfo(C.A, v.default, T.A, b.A, I.A, u.A).footerInfo,
+            [x.A, b.default, I.A, E.A, C.A, u.A],
+            () => _.getForwardInfo(x.A, b.default, I.A, E.A, C.A, u.A).footerInfo,
             [_],
             s.A,
         ),
-        h = (0, a.bG)([C.A], () => C.A.getChannel(t.messageReference?.channel_id)),
+        h = (0, a.bG)([x.A], () => x.A.getChannel(t.messageReference?.channel_id)),
         p = l.useCallback(() => {
-            (0, y.A)(t);
+            (0, v.A)(t);
         }, [t]);
     return null == m
         ? null
         : (0, i.jsxs)(d.DUT, {
-              className: P.xQ,
+              className: j.xQ,
               onClick: p,
               "aria-label": m.accessibilityLabel,
               children: [
                   null != m.originIconUrl
-                      ? (0, i.jsx)("img", { className: P.yl, src: m.originIconUrl, alt: "" })
+                      ? (0, i.jsx)("img", { className: j.yl, src: m.originIconUrl, alt: "" })
                       : null,
-                  null == m.originIconUrl && h?.isThread() ? (0, i.jsx)(D, { channel: h }) : null,
+                  null == m.originIconUrl && h?.isThread() ? (0, i.jsx)(P, { channel: h }) : null,
                   (0, i.jsx)(r.EYj, {
-                      className: P.PJ,
+                      className: j.PJ,
                       variant: "text-sm/medium",
                       color: "none",
                       children: `${m.originLabel}  •  ${m.timestampLabel}`,
@@ -77,24 +74,24 @@ function w(e) {
               ],
           });
 }
-function D(e) {
+function P(e) {
     let { channel: t } = e,
-        n = (0, x.Vp)({ location: "ForwardFooter" }),
+        n = (0, g.Vp)({ location: "ForwardFooter" }),
         r = l.useMemo(() => (0, c.gU)(t), [t]);
-    return n.enabled && null != r ? (0, i.jsx)(r, { className: P.yl, size: "xs", color: "currentColor" }) : null;
+    return n.enabled && null != r ? (0, i.jsx)(r, { className: j.yl, size: "xs", color: "currentColor" }) : null;
 }
-function O(e) {
+function M(e) {
     let { message: t, snapshot: n, index: r } = e,
-        s = l.useMemo(() => (0, N.A)(t, n), [t, n]),
-        o = f.hD.useSetting(),
-        d = f.rs.useSetting(),
-        c = (0, a.bG)([E.A], () => E.A.isDeveloper),
+        s = l.useMemo(() => (0, S.A)(t, n), [t, n]),
+        o = A.hD.useSetting(),
+        d = A.rs.useSetting(),
+        c = (0, a.bG)([f.A], () => f.A.isDeveloper),
         u = (0, _.S)((s.editedTimestamp ?? s.timestamp).valueOf()),
         {
-            content: x,
-            hasSpoilerEmbeds: I,
-            hasBailedAst: b,
-        } = (0, g.A)(s, {
+            content: g,
+            hasSpoilerEmbeds: C,
+            hasBailedAst: E,
+        } = (0, h.A)(s, {
             hideSimpleEmbedContent: o && d,
             isInteracting: !1,
             formatInline: !1,
@@ -104,33 +101,30 @@ function O(e) {
             allowDevLinks: c,
             previewLinkTarget: !0,
         }),
-        T = (0, a.bG)([C.A], () => C.A.getChannel(t.channel_id)),
-        v = f.hH.useSetting(),
-        { setPopout: S } = (0, p.A)(s.id, L.Fd),
-        y = (0, h.VL)(s, T ?? { id: t.channel_id }, S);
-    return null == T
+        I = (0, a.bG)([x.A], () => x.A.getChannel(t.channel_id)),
+        b = A.hH.useSetting();
+    return null == I
         ? null
         : (0, i.jsxs)(
               "div",
               {
-                  className: P.kL,
+                  className: j.kL,
                   children: [
-                      (0, i.jsx)("div", { className: P.hD }),
+                      (0, i.jsx)("div", { className: j.hD }),
                       (0, i.jsxs)("div", {
-                          className: P.Qs,
+                          className: j.Qs,
                           children: [
-                              (0, i.jsx)(M, {}),
-                              (0, i.jsx)(m.Ay, { message: s, content: x, compact: v }),
-                              (0, A.A)({
-                                  channelMessageProps: { message: s, channel: T, compact: v },
-                                  hasSpoilerEmbeds: I,
-                                  hasBailedAst: b,
-                                  handleContextMenu: y,
+                              (0, i.jsx)(L, {}),
+                              (0, i.jsx)(m.Ay, { message: s, content: g, compact: b }),
+                              (0, p.A)({
+                                  channelMessageProps: { message: s, channel: I, compact: b },
+                                  hasSpoilerEmbeds: C,
+                                  hasBailedAst: E,
                                   isInteracting: !1,
                                   isMessageSnapshot: !0,
                                   renderThreadAccessory: !1,
                               }),
-                              (0, i.jsx)(w, { message: t, snapshot: n, index: r }),
+                              (0, i.jsx)(R, { message: t, snapshot: n, index: r }),
                           ],
                       }),
                   ],
@@ -138,9 +132,9 @@ function O(e) {
               r,
           );
 }
-function k(e) {
+function w(e) {
     let { message: t } = e;
     return (0, i.jsx)(i.Fragment, {
-        children: t.messageSnapshots.map((e, n) => (0, i.jsx)(O, { message: t, snapshot: e, index: n }, n)),
+        children: t.messageSnapshots.map((e, n) => (0, i.jsx)(M, { message: t, snapshot: e, index: n }, n)),
     });
 }
