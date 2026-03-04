82004b23fbb2d49e43f5fca8fc9dee837d5aef52
DiscrapperManager <admin@minerpl.xyz>
2026-03-04 18:50:32 +0100

    canaryRelease: 505655
    Main scripts: 3
    Lazy Loaded Scripts: 2038
    Chunks:
     * Main Files: 13378
     * Scripts: 12466
    Strings: 23922
    Routes: 805
    Experiments Count: 169
    Apex Experiments Count: 193

--- chunks/400442.js
+++ chunks/400442.js
@@ -1,7 +1,7 @@
 i.d(e, { j: () => A });
 var n = i(627968),
-    l = i(46054),
-    s = i(930101),
+    l = i(565645),
+    s = i(46054),
     r = i(419954),
     a = i(253932),
     u = i(780964),
@@ -11,8 +11,8 @@ let A = (0, r.zD)(u.X.CHAT_EMOJI_CONVERT_EMOTICONS, {
     useTitle: () => d.intl.string(d.t["79qal8"]),
     useSubtitle: () =>
         (0, o.bp)("ConvertEmoticons")
-            ? d.intl.format(d.t.GejoQK, { emojiHook: (t, e) => (0, n.jsx)(s.H, { node: { name: "\uD83D\uDE42" } }, e) })
-            : l.A.parse(d.intl.string(d.t["20dvuQ"])),
+            ? d.intl.format(d.t.GejoQK, { emojiHook: (t, e) => (0, n.jsx)(l.A, { emojiName: "\uD83D\uDE42" }, e) })
+            : s.A.parse(d.intl.string(d.t["20dvuQ"])),
     useValue: a.j7.useSetting,
     setValue: a.j7.updateSetting,
 });
