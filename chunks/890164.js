8f93e6d339f0cac3bb83a5dd1b88e064dd3ee6ee
DiscrapperManager <admin@minerpl.xyz>
2026-03-05 23:04:00 +0100

    canaryRelease: 506750
    Main scripts: 3
    Lazy Loaded Scripts: 2007
    Chunks:
     * Main Files: 13369
     * Scripts: 12458
    Strings: 23893
    Routes: 805
    Experiments Count: 165
    Apex Experiments Count: 190

--- chunks/890164.js
+++ chunks/890164.js
@@ -1,9 +1,15 @@
-n.d(t, { W1: () => a, lX: () => s });
-let i = (0, n(945810).mj)({
+n.d(t, { W1: () => r, lX: () => a });
+var i = n(945810),
+    s = n(945939);
+let l = (0, i.mj)({
         name: "2026-02-private-profiles",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
     }),
-    s = (e) => i.useConfig({ location: e }).enabled,
-    a = (e) => i.getConfig({ location: e }).enabled;
+    a = (e) => {
+        let t = l.useConfig({ location: e }).enabled,
+            n = (0, s.Q6)(e);
+        return t || n;
+    },
+    r = (e) => l.getConfig({ location: e }).enabled || (0, s.De)(e);
---
+++ chunks/945939.js
@@ -0,0 +1,9 @@
+n.d(t, { De: () => l, Q6: () => s });
+let i = (0, n(945810).mj)({
+        name: "2026-03-private-profiles-strict",
+        kind: "user",
+        defaultConfig: { enabled: !1 },
+        variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
+    }),
+    s = (e) => i.useConfig({ location: e }).enabled,
+    l = (e) => i.getConfig({ location: e }).enabled;
