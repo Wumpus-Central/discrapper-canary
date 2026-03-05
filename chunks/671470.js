c6495f474edf296f07412e80aafbeddc4c61b38d
DiscrapperManager <admin@minerpl.xyz>
2026-03-05 17:08:36 +0100

    canaryRelease: 506371
    Main scripts: 3
    Lazy Loaded Scripts: 2004
    Chunks:
     * Main Files: 13370
     * Scripts: 12435
    Strings: 23892
    Routes: 804
    Experiments Count: 165
    Apex Experiments Count: 193

--- chunks/671470.js
+++ chunks/671470.js
@@ -1,6 +1,42 @@
-t.d(e, { A: () => r }), t(321073), t(627968), t(64700), t(397927), t(422258);
-var i = t(93055);
-function r(n) {
-    return (0, i.Fe)(), (0, i.Af)(n.id), (0, i.Rm)(), __OVERLAY__, null;
+e.d(t, { A: () => s }), e(321073);
+var i = e(627968);
+e(64700);
+var r = e(397927),
+    l = e(422258),
+    a = e(93055),
+    d = e(985018);
+function s(n) {
+    let t = (0, a.Fe)(),
+        e = (0, a.Af)(n.id),
+        s = (0, a.Rm)();
+    if (__OVERLAY__ || !t || null == e) return null;
+    let [u, o] = (function (n) {
+        let t = [],
+            e = null;
+        for (let i of n) null == i.id ? (e = i) : t.push(i);
+        return [e, t];
+    })(s.filter((n) => n.id !== e?.parentId));
+    function c(n) {
+        null != e && (0, l.JD)(e.id, n);
+    }
+    if (null == u && 0 === o.length) return null;
+    let A = u?.id ?? null,
+        f = u?.name ?? d.intl.string(d.t.GSfOoo);
+    return (0, i.jsxs)(r.Drp, {
+        id: "move-to-category",
+        label: d.intl.string(d.t.FAplms),
+        children: [
+            null != u &&
+                (0, i.jsx)(r.rXV, {
+                    children: (0, i.jsx)(r.Drp, { id: "favorite-uncategorized", label: f, action: () => c(A) }),
+                }),
+            o.length > 0 &&
+                (0, i.jsx)(r.rXV, {
+                    children: o.map((n) => {
+                        let { id: t, name: e } = n;
+                        return (0, i.jsx)(r.Drp, { id: `favorite-${t}`, label: e, action: () => c(t) }, t);
+                    }),
+                }),
+        ],
+    });
 }
-t(985018);
