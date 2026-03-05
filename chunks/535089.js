990d4e206de3aa7859aef9a51436e134ad8acc97
DiscrapperManager <admin@minerpl.xyz>
2026-03-05 17:10:23 +0100

    noisesSuppression 506371

--- chunks/535089.js
+++ chunks/535089.js
@@ -1,15 +1,14 @@
-"use strict";
-n.d(t, { A: () => l });
+n.d(t, { A: () => a });
 var i = n(64700),
-    s = n(172218);
-function l(e) {
-    let { wishlistId: t, onAction: n, productLines: l } = e,
-        [a, r] = (0, i.useState)(!1),
+    l = n(172218);
+function a(e) {
+    let { wishlistId: t, onAction: n, productLines: a } = e,
+        [s, r] = (0, i.useState)(!1),
         o = (0, i.useCallback)(
             (e) => {
-                e && null != t && (n({ action: "VIEW_WISHLIST", wishlistId: t, productLines: l ?? void 0 }), r(!0));
+                e && null != t && (n({ action: "VIEW_WISHLIST", wishlistId: t, productLines: a ?? void 0 }), r(!0));
             },
-            [t, n, l],
+            [t, n, a],
         );
-    return (0, s.K)(o, void 0, null != t && !a);
+    return (0, l.K)(o, void 0, null != t && !s);
 }
