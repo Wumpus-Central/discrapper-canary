97bff15a82f209f2a3ca5e2a476b0b3a27a81f38
DiscrapperManager <admin@minerpl.xyz>
2026-03-05 21:04:49 +0100

    canaryRelease: 506602
    Main scripts: 3
    Lazy Loaded Scripts: 2008
    Chunks:
     * Main Files: 13369
     * Scripts: 12449
    Strings: 23892
    Routes: 805
    Experiments Count: 165
    Apex Experiments Count: 191

--- chunks/509870.js
+++ chunks/509870.js
@@ -1,21 +1,25 @@
-s.d(t, { A: () => C });
+s.d(t, { A: () => d });
 var i = s(284009),
     a = s.n(i),
     r = s(311907),
     l = s(851746),
-    n = s(985018);
-let C = (e) => {
+    n = s(492195),
+    C = s(985018);
+let d = (e) => {
     let { numNewSelected: t, anySelected: s } = e,
         i = (0, r.bG)([l.A], () => l.A.getReferralsRemaining());
     a()(null != i, "Referrals remaining should not be null");
-    let C = i - t;
+    let d = i - t,
+        o = (0, n.dF)({ location: "useReferralProgramSubheaderText" });
     return 0 === i
-        ? n.intl.string(n.t.mPmPri)
+        ? C.intl.string(C.t.mPmPri)
         : 0 !== t || s
-          ? 1 === C
-              ? n.intl.string(n.t.UfPk1t)
-              : C > 0
-                ? n.intl.formatToPlainString(n.t.hZnIz4, { numFriends: C })
-                : n.intl.formatToPlainString(n.t["3PJw6c"], { numReferrals: i })
-          : n.intl.formatToPlainString(n.t.GcKM5Z, { numReferrals: i });
+          ? 1 === d
+              ? C.intl.string(C.t.UfPk1t)
+              : d > 0
+                ? C.intl.formatToPlainString(C.t.hZnIz4, { numFriends: d })
+                : C.intl.formatToPlainString(C.t["3PJw6c"], { numReferrals: i })
+          : o
+            ? C.intl.string(C.t.DXgoi2)
+            : C.intl.formatToPlainString(C.t.GcKM5Z, { numReferrals: i });
 };
