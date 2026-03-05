d56a079712a4b445c64c98b61ed5eede61e91cf4
DiscrapperManager <admin@minerpl.xyz>
2026-03-05 21:09:22 +0100

    canaryRelease: 506605
    Main scripts: 3
    Lazy Loaded Scripts: 2007
    Chunks:
     * Main Files: 13369
     * Scripts: 12455
    Strings: 23894
    Routes: 805
    Experiments Count: 165
    Apex Experiments Count: 191

--- chunks/554703.js
+++ chunks/554703.js
@@ -1,8 +1,2 @@
 "use strict";
-e.exports = {
-    kL: "container__2994f",
-    wx: "header__2994f",
-    b: "closeButton__2994f",
-    rf: "body__2994f",
-    Qq: "rewardTile__2994f",
-};
+e.exports = { kL: "container__2994f", wx: "header__2994f", b: "closeButton__2994f", rf: "body__2994f" };
--- chunks/898375.js
+++ chunks/898375.js
@@ -1,63 +1,64 @@
 "use strict";
-n.d(t, { A: () => m });
+n.d(t, { A: () => p });
 var i = n(627968),
     s = n(64700),
     l = n(158954),
-    a = n(397927),
-    r = n(341915),
+    r = n(397927),
+    a = n(341915),
     o = n(18437),
     c = n(590202),
     d = n(453384),
     u = n(646764),
     h = n(545986),
     A = n(985018),
-    p = n(554703);
-function m(e) {
-    let { quest: t, questDescription: n, completedRatio: m, minimizeFrame: g, onClose: _ } = e,
+    m = n(554703);
+function p(e) {
+    let { quest: t, questDescription: n, completedRatio: p, minimizeFrame: g, onClose: _ } = e,
         f = (0, o.Ut)(),
         x = s.useCallback(() => {
             f({
                 questId: t.id,
-                questContent: r.uF.RUNNING_ACTIVITY,
+                questContent: a.uF.RUNNING_ACTIVITY,
                 questContentCTA: c.Cy.LEARN_MORE,
-                sourceQuestContent: r.uF.RUNNING_ACTIVITY,
+                sourceQuestContent: a.uF.RUNNING_ACTIVITY,
             }),
-                (0, h.navigateToQuestHome)({ fromContent: r.uF.RUNNING_ACTIVITY, questId: t.id }),
+                (0, h.navigateToQuestHome)({ fromContent: a.uF.RUNNING_ACTIVITY, questId: t.id }),
                 g(),
                 _();
         }, [t.id, f, g, _]);
     return (0, i.jsxs)("div", {
-        className: p.kL,
+        className: m.kL,
         children: [
             (0, i.jsxs)("div", {
-                className: p.wx,
+                className: m.wx,
                 children: [
                     (0, i.jsx)(l.EYj, {
                         variant: "text-md/semibold",
                         color: "text-strong",
                         children: A.intl.formatToPlainString(A.t.EAYZAr, { questName: t.config.messages.questName }),
                     }),
-                    (0, i.jsx)(a.DUT, {
-                        className: p.b,
+                    (0, i.jsx)(r.DUT, {
+                        className: m.b,
                         "aria-label": A.intl.string(A.t.cpT0Cq),
                         onClick: _,
-                        children: (0, i.jsx)(a.PGe, { size: "sm", color: "currentColor" }),
+                        children: (0, i.jsx)(r.PGe, { size: "sm", color: "currentColor" }),
                     }),
                 ],
             }),
             (0, i.jsxs)("div", {
-                className: p.rf,
+                className: m.rf,
                 children: [
-                    (0, i.jsx)(d.A, {
-                        size: 48,
-                        style: "default",
-                        percentComplete: m,
-                        children: (0, i.jsx)(u.A, {
-                            quest: t,
-                            questContent: r.uF.RUNNING_ACTIVITY,
-                            sourceQuestContent: r.uF.RUNNING_ACTIVITY,
-                            autoplay: !1,
-                            className: p.Qq,
+                    (0, i.jsx)("div", {
+                        children: (0, i.jsx)(d.A, {
+                            size: 48,
+                            percentComplete: p,
+                            children: (0, i.jsx)(u.A, {
+                                fullWidth: !0,
+                                quest: t,
+                                questContent: a.uF.RUNNING_ACTIVITY,
+                                sourceQuestContent: a.uF.RUNNING_ACTIVITY,
+                                autoplay: !1,
+                            }),
                         }),
                     }),
                     (0, i.jsx)(l.EYj, { variant: "text-sm/normal", color: "text-muted", children: n }),
