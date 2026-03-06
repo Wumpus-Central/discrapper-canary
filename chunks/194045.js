2e681581fed7955609fa4bc1bc182842ab8fbb8c
DiscrapperManager <admin@minerpl.xyz>
2026-03-06 20:00:39 +0100

    canaryRelease: 507291
    Main scripts: 3
    Lazy Loaded Scripts: 2005
    Chunks:
     * Main Files: 13357
     * Scripts: 12458
    Strings: 23918
    Routes: 805
    Experiments Count: 157
    Apex Experiments Count: 191

--- chunks/194045.js
+++ chunks/194045.js
@@ -1,4 +1,4 @@
-n.d(t, { A: () => x });
+n.d(t, { A: () => v });
 var i = n(627968);
 n(64700);
 var r = n(397927),
@@ -6,22 +6,23 @@ var r = n(397927),
     a = n(626584),
     s = n(450510),
     o = n(891540),
-    d = n(734057),
-    c = n(430452),
-    u = n(309010),
-    A = n(532624),
-    h = n(518647),
-    _ = n(796774),
-    m = n(209932),
+    d = n(594061),
+    c = n(734057),
+    u = n(430452),
+    A = n(309010),
+    h = n(532624),
+    _ = n(518647),
+    m = n(796774),
+    g = n(209932),
     p = n(123973),
-    g = n(646202),
-    E = n(102597),
-    I = n(904054),
-    f = n(857179),
-    C = n(652215),
-    T = n(731854);
-let N = new a.A("SoundboardManager");
-class S extends h.A {
+    E = n(646202),
+    I = n(102597),
+    f = n(904054),
+    C = n(857179),
+    T = n(652215),
+    N = n(731854);
+let S = new a.A("SoundboardManager");
+class x extends _.A {
     playingSoundsWeb = new Map();
     _initialize() {
         super._initialize(),
@@ -42,8 +43,8 @@ class S extends h.A {
                 l.h.unsubscribe("RTC_CONNECTION_STATE", this._handleRTCConnectionState));
     }
     _stopAndClearSounds = () => {
-        c.Ay.supports(T.O5.SAMPLE_PLAYBACK) &&
-            c.Ay.getMediaEngine().eachConnection((e) => {
+        u.Ay.supports(N.O5.SAMPLE_PLAYBACK) &&
+            u.Ay.getMediaEngine().eachConnection((e) => {
                 e.stopAllSamplesLocalPlayback();
             }),
             this.playingSoundsWeb.forEach((e) => {
@@ -57,34 +58,36 @@ class S extends h.A {
             let n = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : 1,
                 i = arguments.length > 2 ? arguments[2] : void 0,
                 r = arguments.length > 3 ? arguments[3] : void 0,
-                l = null != r && u.A.getVoiceChannelId() === r;
-            if ((null == r || l) && !c.Ay.isDeaf() && !m.A.isLocalSoundboardMuted(i))
+                l = null != r && A.A.getVoiceChannelId() === r;
+            if ((null == r || l) && !u.Ay.isDeaf() && !g.A.isLocalSoundboardMuted(i))
                 try {
                     let r = {
                         soundKey: `${i}-${t}`,
-                        soundURL: (0, E.A)(t),
-                        soundVolume: (0, I.A)(n),
-                        reportSoundStartedPlaying: () => (0, _.dZ)(t, i),
+                        soundURL: (0, I.A)(t),
+                        soundVolume: (0, f.A)(n),
+                        reportSoundStartedPlaying: () => (0, m.dZ)(t, i),
                     };
-                    c.Ay.supports(T.O5.SAMPLE_PLAYBACK) ? await (0, f.o)(r) : await (0, f.G)(r, e.playingSoundsWeb);
+                    u.Ay.supports(N.O5.SAMPLE_PLAYBACK) ? await (0, C.o)(r) : await (0, C.G)(r, e.playingSoundsWeb);
                 } catch (e) {
-                    N.warn(`Error playing soundboard sound: ${e.message}`);
+                    S.warn(`Error playing soundboard sound: ${e.message}`);
                 } finally {
-                    (0, _.g0)(t, i);
+                    (0, m.g0)(t, i);
                 }
         };
     })();
     _handleRTCConnectionState = (e) => {
         let { state: t } = e;
-        t === C.S7L.RTC_CONNECTED && (0, g.fN)("SoundboardManager") && (0, _.E7)({ disableAnalytics: !0 });
+        t === T.S7L.RTC_CONNECTED &&
+            (0, E.fN)("SoundboardManager") &&
+            (d.bW.loadIfNecessary(), (0, m.E7)({ disableAnalytics: !0 }));
     };
     _handleOverlaySoundboardSoundsFetchRequest = () => {
-        (0, _.E7)();
+        (0, m.E7)();
     };
     _handleOpenEducationModal = (e, t) => {
         if (null == e) return;
-        let l = d.A.getChannel(t),
-            a = A.Ay.getKeybindForAction(C.hCu.SOUNDBOARD_HOLD);
+        let l = c.A.getChannel(t),
+            a = h.Ay.getKeybindForAction(T.hCu.SOUNDBOARD_HOLD);
         null != l &&
             (0, p.Ay)(l) &&
             null != a &&
@@ -95,4 +98,4 @@ class S extends h.A {
             });
     };
 }
-let x = new S();
+let v = new x();
