43a04ea0b9c8658aed948c7d4da4f059612d8cd1
DiscrapperManager <admin@minerpl.xyz>
2026-03-06 02:29:51 +0100

    canaryRelease: 507025
    Main scripts: 3
    Lazy Loaded Scripts: 2005
    Chunks:
     * Main Files: 13352
     * Scripts: 12453
    Strings: 23894
    Routes: 805
    Experiments Count: 159
    Apex Experiments Count: 190

--- chunks/662367.js
+++ chunks/662367.js
@@ -1,30 +1,12 @@
-n.d(t, { i: () => r });
-var i = n(600975),
-    l = n(250105);
-let s = (0, i.C)({
-        kind: "user",
-        id: "2021-04_longer_group_dm_invites",
-        label: "Longer Group DM Invites",
-        defaultConfig: { isEnabled: !1, inviteMaxAgeSeconds: 86400 },
-        treatments: [
-            {
-                id: 1,
-                label: "Makes group DM invites expire after 1 week.",
-                config: { isEnabled: !0, inviteMaxAgeSeconds: 604800 },
-            },
-        ],
-    }),
-    a = (0, l.Ay)({
-        kind: "user",
-        name: "2026-02-longer-group-dm-invites",
-        defaultConfig: { isEnabled: !1, inviteMaxAgeSeconds: 86400 },
-        variations: {
-            0: { isEnabled: !1, inviteMaxAgeSeconds: 86400 },
-            1: { isEnabled: !0, inviteMaxAgeSeconds: 604800 },
-        },
-    });
-function r(e) {
+n.d(t, { i: () => l });
+let i = (0, n(250105).Ay)({
+    kind: "user",
+    name: "2026-02-longer-group-dm-invites",
+    defaultConfig: { inviteMaxAgeSeconds: 86400 },
+    variations: { 0: { inviteMaxAgeSeconds: 86400 }, 1: { inviteMaxAgeSeconds: 604800 } },
+});
+function l(e) {
     let { location: t } = e,
-        { isEnabled: n, inviteMaxAgeSeconds: i } = a.getConfig({ location: t });
-    return n ? i : s.getCurrentConfig({ location: t }).inviteMaxAgeSeconds;
+        { inviteMaxAgeSeconds: n } = i.getConfig({ location: t });
+    return n;
 }
