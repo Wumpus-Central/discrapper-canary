665ffaa7e15d61feeac95815637e6fc71f0ea961
DiscrapperManager <admin@minerpl.xyz>
2026-03-06 18:39:10 +0100

    canaryRelease: 507206
    Main scripts: 3
    Lazy Loaded Scripts: 2005
    Chunks:
     * Main Files: 13357
     * Scripts: 12452
    Strings: 23894
    Routes: 805
    Experiments Count: 157
    Apex Experiments Count: 191

--- chunks/947461.js
+++ chunks/947461.js
@@ -1,73 +1,71 @@
 "use strict";
-n.d(t, { A: () => E });
+n.d(t, { A: () => x });
 var i = n(627968),
     s = n(64700),
-    l = n(503698),
-    r = n.n(l),
-    a = n(554146),
-    o = n(192308),
-    c = n(990078),
-    d = n(397927),
-    u = n(932001),
-    h = n(652793),
-    A = n(976860),
-    m = n(652215),
-    p = n(746080),
-    g = n(49999),
-    _ = n(738072),
-    f = n(985018),
-    x = n(9339),
-    C = n(719550);
-let E = s.memo(function (e) {
-    let { guildId: t, selected: l } = e,
-        [E, I] = (0, u.ww)([a.M.GAME_SERVER_HOSTING_NEW_BADGE], t),
-        N = E === a.M.GAME_SERVER_HOSTING_NEW_BADGE,
-        b = s.useCallback(() => {
-            I(g.i.USER_DISMISS), (0, A.pX)(m.BVt.CHANNEL(t, p.VV.GAME_SERVERS));
-        }, [t, I]),
-        S = s.useCallback(
+    l = n(554146),
+    r = n(192308),
+    a = n(990078),
+    o = n(397927),
+    c = n(932001),
+    d = n(652793),
+    u = n(976860),
+    h = n(652215),
+    A = n(746080),
+    m = n(49999),
+    p = n(738072),
+    g = n(985018),
+    _ = n(9339),
+    f = n(719550);
+let x = s.memo(function (e) {
+    let { guildId: t, selected: x } = e,
+        [C, E] = (0, c.ww)([l.M.GAME_SERVER_HOSTING_NEW_BADGE], t),
+        I = C === l.M.GAME_SERVER_HOSTING_NEW_BADGE,
+        N = s.useCallback(() => {
+            E(m.i.USER_DISMISS), (0, u.pX)(h.BVt.CHANNEL(t, A.VV.GAME_SERVERS));
+        }, [t, E]),
+        b = s.useCallback(
             (e) => {
                 e.stopPropagation(),
-                    (0, o.openModalLazy)(async () => {
+                    (0, r.openModalLazy)(async () => {
                         let { default: e } = await n.e("2872").then(n.bind(n, 128459));
                         return (n) => (0, i.jsx)(e, { ...n, guildId: t });
                     });
             },
             [t],
         ),
-        T = (0, i.jsxs)("div", {
-            className: x.c,
+        S = (0, i.jsxs)("div", {
+            className: _.c,
             children: [
                 (0, i.jsx)("div", {
-                    className: r()(C.Xs, { [C.Tf]: !N }),
-                    children: (0, i.jsx)(c.m, {
-                        text: f.intl.string(f.t.fgq1gs),
+                    className: f.Xs,
+                    children: (0, i.jsx)(a.m, {
+                        text: g.intl.string(g.t.fgq1gs),
                         position: "top",
-                        children: (0, i.jsx)(d.DUT, {
-                            onClick: S,
-                            "aria-label": f.intl.string(f.t.fgq1gs),
-                            children: (0, i.jsx)(d.PGe, { size: "xs", color: "currentColor", className: C.gE }),
+                        children: (0, i.jsx)(o.DUT, {
+                            onClick: b,
+                            "aria-label": g.intl.string(g.t.fgq1gs),
+                            children: (0, i.jsx)(o.PGe, { size: "xs", color: "currentColor", className: f.gE }),
                         }),
                     }),
                 }),
-                N &&
+                I &&
                     (0, i.jsx)("div", {
-                        className: C.yW,
-                        children: (0, i.jsx)(d.LpS, {
+                        className: f.yW,
+                        children: (0, i.jsx)(o.LpS, {
                             disableColor: !0,
-                            text: f.intl.string(f.t.y2b7CA),
-                            className: x.q,
+                            text: g.intl.string(g.t.y2b7CA),
+                            className: _.q,
                         }),
                     }),
             ],
         });
-    return (0, i.jsx)(h.G, {
-        className: C.Ki,
+    return (0, i.jsx)(d.G, {
+        className: f.Ki,
         id: `game-server-empty-${t}`,
-        renderIcon: (e) => (0, i.jsx)(d._xR, { size: "md", className: e, color: "currentColor" }),
-        text: f.intl.string(_.default.vCzwM7),
-        selected: l,
-        onClick: b,
-        trailing: T,
+        renderIcon: (e) => (0, i.jsx)(o._xR, { size: "md", className: e, color: "currentColor" }),
+        text: g.intl.string(p.default.vCzwM7),
+        selected: x,
+        onClick: N,
+        trailing: S,
     });
 });
