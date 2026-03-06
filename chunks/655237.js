f8127d2a31c8b32e4a78776950ecfffb577d0c91
DiscrapperManager <admin@minerpl.xyz>
2026-03-06 17:21:48 +0100

    canaryRelease: 507158
    Main scripts: 3
    Lazy Loaded Scripts: 2005
    Chunks:
     * Main Files: 13353
     * Scripts: 12453
    Strings: 23894
    Routes: 805
    Experiments Count: 157
    Apex Experiments Count: 191

--- chunks/655237.js
+++ chunks/655237.js
@@ -1,32 +1,23 @@
 "use strict";
-n.d(t, { A: () => _ });
+n.d(t, { A: () => c });
 var r = n(311907),
     i = n(73153),
-    a = n(600975),
     s = n(961350);
-let o = {},
-    l = (0, a.C)({
-        kind: "user",
-        id: "2025-03_slayer_notif_supression_killswitch",
-        label: "Disable suppressing notifications with slayer game active",
-        defaultConfig: { enabled: !1 },
-        treatments: [{ id: 1, label: "Disable suppressing", config: { enabled: !0 } }],
-    });
-function u(e) {
-    delete o[e.socketId];
+let a = {};
+function o(e) {
+    delete a[e.socketId];
 }
-function c(e) {
-    delete o[e.socketId], e.suppressNotifications && (o[e.socketId] = e.targetUserId);
+function l(e) {
+    delete a[e.socketId], e.suppressNotifications && (a[e.socketId] = e.targetUserId);
 }
-class d extends r.Ay.Store {
+class u extends r.Ay.Store {
     initialize() {
         this.waitFor(s.default);
     }
     static displayName = "RpcNotificationSettingsStore";
     areSlayerNotificationsSuppressed() {
-        if (l.getCurrentConfig({ location: "Store" }, { autoTrackExposure: !1 }).enabled) return !1;
-        for (let e in o) if (o[e] === s.default.getId()) return !0;
+        for (let e in a) if (a[e] === s.default.getId()) return !0;
         return !1;
     }
 }
-let _ = new d(i.h, { RPC_APP_DISCONNECTED: u, SET_RPC_NOTIFICATION_SETTINGS: c });
+let c = new u(i.h, { RPC_APP_DISCONNECTED: o, SET_RPC_NOTIFICATION_SETTINGS: l });
