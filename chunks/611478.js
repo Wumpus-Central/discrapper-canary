43fe5476920d5d5d7ac38107a66df9e2beac8ad7
DiscrapperManager <admin@minerpl.xyz>
2026-03-06 22:55:07 +0100

    canaryRelease: 507484
    Main scripts: 3
    Lazy Loaded Scripts: 2005
    Chunks:
     * Main Files: 13360
     * Scripts: 12460
    Strings: 23918
    Routes: 805
    Experiments Count: 157
    Apex Experiments Count: 193

--- chunks/611478.js
+++ chunks/611478.js
@@ -1,48 +1,50 @@
-i.d(e, { P: () => g }), i(321073);
-var n = i(627968),
-    l = i(311907),
-    s = i(36149),
-    r = i(207560),
-    a = i(419954),
-    u = i(287809),
-    o = i(780964),
-    d = i(998252),
-    A = i(980172),
-    T = i(985018),
-    S = i(536242);
-let E = (0, a.E2)(o.X.ACCOUNT_PROFILE_SETTING, {
+n.d(t, { P: () => h }), n(321073);
+var i = n(627968),
+    s = n(311907),
+    l = n(397927),
+    a = n(36149),
+    r = n(207560),
+    o = n(419954),
+    d = n(287809),
+    c = n(780964),
+    u = n(998252),
+    _ = n(980172),
+    g = n(985018),
+    m = n(949268);
+let A = (0, o.E2)(c.X.ACCOUNT_PROFILE_SETTING, {
         useSearchTerms: function () {
-            let t = [
-                    T.intl.string(T.t.LYju5J),
-                    T.intl.string(T.t["9AjdkD"]),
-                    T.intl.string(T.t["+JkHPw"]),
-                    T.intl.string(T.t.oP5zGA),
-                    T.intl.string(T.t.Ulqq6K),
+            let e = [
+                    g.intl.string(g.t.LYju5J),
+                    g.intl.string(g.t["9AjdkD"]),
+                    g.intl.string(g.t["+JkHPw"]),
+                    g.intl.string(g.t.oP5zGA),
+                    g.intl.string(g.t.Ulqq6K),
                 ],
-                e = (0, r.fk)(),
-                i = (0, s.Y2)();
+                t = (0, r.fk)(),
+                n = (0, a.Y2)();
             return (
-                e &&
-                    (t.push(T.intl.string(T.t["/52UYy"])),
-                    t.push(T.intl.string(T.t.sK0dmH)),
-                    t.push(T.intl.string(T.t.XxRj7f)),
-                    t.push(T.intl.string(T.t.DVywUB)),
-                    t.push(T.intl.string(T.t["9KiIz6"])),
-                    i &&
-                        (t.push(T.intl.string(S.default.FTawSP)),
-                        t.push(T.intl.string(S.default["bD//cU"])),
-                        t.push(T.intl.string(S.default.SH6Tcv)))),
-                t
+                t &&
+                    (e.push(g.intl.string(g.t["/52UYy"])),
+                    e.push(g.intl.string(g.t.sK0dmH)),
+                    e.push(g.intl.string(g.t.XxRj7f)),
+                    e.push(g.intl.string(g.t.DVywUB)),
+                    e.push(g.intl.string(g.t["9KiIz6"])),
+                    n &&
+                        (e.push(g.intl.string(m.default.FTawSP)),
+                        e.push(g.intl.string(m.default["bD//cU"])),
+                        e.push(g.intl.string(m.default.SH6Tcv)))),
+                e
             );
         },
         Component: function () {
-            let t = (0, l.bG)([u.default], () => u.default.getCurrentUser());
-            return (0, n.jsxs)(n.Fragment, {
+            let e = (0, s.bG)([d.default], () => d.default.getCurrentUser());
+            return (0, i.jsxs)(l.BJc, {
+                gap: "md",
                 children: [
-                    null != t && (0, n.jsx)(d.A, { currentUser: t }),
-                    null != t && (0, n.jsx)(A.A, { currentUser: t }),
+                    null != e && (0, i.jsx)(u.A, { currentUser: e }),
+                    null != e && (0, i.jsx)(_.A, { currentUser: e }),
                 ],
             });
         },
     }),
-    g = (0, a.zZ)(o.X.ACCOUNT_PROFILE_CATEGORY, { buildLayout: () => [E] });
+    h = (0, o.zZ)(c.X.ACCOUNT_PROFILE_CATEGORY, { buildLayout: () => [A] });
