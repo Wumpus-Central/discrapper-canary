da14f6e7982e4db1792c3a222a107e6590dd7eb0
DiscrapperManager <admin@minerpl.xyz>
2026-03-06 02:59:59 +0100

    canaryRelease: 507040
    Main scripts: 3
    Lazy Loaded Scripts: 2005
    Chunks:
     * Main Files: 13352
     * Scripts: 12453
    Strings: 23894
    Routes: 805
    Experiments Count: 159
    Apex Experiments Count: 190

--- chunks/665171.js
+++ chunks/665171.js
@@ -1,71 +1,77 @@
 "use strict";
 n.d(t, {
-    Jr: () => v,
-    K: () => h,
-    KH: () => A,
-    QK: () => I,
-    Wp: () => g,
-    as: () => N,
-    cq: () => m,
-    e_: () => y,
-    jL: () => T,
-    pj: () => S,
-    tT: () => E,
-    z9: () => p,
+    Jr: () => N,
+    K: () => m,
+    KH: () => I,
+    QK: () => T,
+    Wp: () => A,
+    as: () => C,
+    cq: () => E,
+    e_: () => v,
+    jL: () => S,
+    pj: () => y,
+    tT: () => g,
+    z9: () => h,
 });
 var r = n(562465),
     i = n(73153),
-    s = n(287809),
-    a = n(954571),
-    o = n(371794),
-    l = n(79133),
-    u = n(532555),
-    c = n(627072),
-    d = n(760321),
-    _ = n(800007),
-    f = n(652215);
-function p(e) {
+    s = n(773669),
+    a = n(287809),
+    o = n(954571),
+    l = n(371794),
+    u = n(79133),
+    c = n(532555),
+    d = n(627072),
+    _ = n(760321),
+    f = n(800007),
+    p = n(652215);
+function h(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
     if (t)
         return void setTimeout(() => {
             i.h.dispatch({
                 type: "GAME_SERVER_FETCH_CATALOG_SUCCESS",
                 guildId: e,
-                catalog: l.q.reduce((e, t) => ((e[t.id] = t), e), {}),
+                catalog: u.q.reduce((e, t) => ((e[t.id] = t), e), {}),
             });
         }, 5e3);
-    let n = s.default.getCurrentUser()?.isStaff() ?? !1;
-    return (0, o.aP)({
-        url: f.Rsh.STOREFRONT_COLLECTION_WITH_PRODUCTS(_.q4),
-        query: { guild_id: e, include_unpublished_products: n, include_unpublished_collection: n },
+    let n = a.default.getCurrentUser()?.isStaff() ?? !1;
+    return (0, l.aP)({
+        url: p.Rsh.STOREFRONT_COLLECTION_WITH_PRODUCTS(f.q4),
+        query: {
+            locale: s.default.locale,
+            guild_id: e,
+            include_unpublished_products: n,
+            include_unpublished_collection: n,
+        },
         oldFormErrors: !0,
         rejectWithError: !1,
         retries: 2,
     }).then((t) => {
         let n = t.body.products.reduce((e, t) => {
-            let n = (0, c.o)(t);
+            let n = (0, d.o)(t);
             return (e[n.id] = n), e;
         }, {});
         i.h.dispatch({ type: "GAME_SERVER_FETCH_CATALOG_SUCCESS", guildId: e, catalog: n });
     });
 }
-function h() {
-    let e = s.default.getCurrentUser()?.isStaff() ?? !1;
-    return (0, o.aP)({
-        url: f.Rsh.STOREFRONT_COLLECTION_WITH_PRODUCTS(_.q4),
-        query: { include_unpublished_products: e, include_unpublished_collection: e },
+function m() {
+    let e = a.default.getCurrentUser()?.isStaff() ?? !1;
+    return (0, l.aP)({
+        url: p.Rsh.STOREFRONT_COLLECTION_WITH_PRODUCTS(f.q4),
+        query: { locale: s.default.locale, include_unpublished_products: e, include_unpublished_collection: e },
         oldFormErrors: !0,
         rejectWithError: !1,
         retries: 2,
     }).then((e) => {
         let t = e.body.products.reduce((e, t) => {
-            let n = (0, c.o)(t);
+            let n = (0, d.o)(t);
             return (e[n.id] = n), e;
         }, {});
         i.h.dispatch({ type: "GAME_SERVER_FETCH_GLOBAL_CATALOG_SUCCESS", catalog: t });
     });
 }
-function m(e) {
+function E(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1],
         n = arguments.length > 2 ? arguments[2] : void 0;
     return t
@@ -73,22 +79,22 @@ function m(e) {
               i.h.dispatch({
                   type: "GAME_SERVER_FETCH_INSTANCES_SUCCESS",
                   guildId: e,
-                  instances: l.L.reduce((e, t) => ((e[t.id] = t), e), {}),
+                  instances: u.L.reduce((e, t) => ((e[t.id] = t), e), {}),
               });
           }, 5e3),
           Promise.resolve())
-        : r.Bo.get({ url: f.Rsh.GAME_SERVERS(e), rejectWithError: !0, retries: 2, signal: n }).then((t) => {
+        : r.Bo.get({ url: p.Rsh.GAME_SERVERS(e), rejectWithError: !0, retries: 2, signal: n }).then((t) => {
               if (null != t.body) {
-                  let n = t.body.reduce((e, t) => ((e[t.id] = (0, u.A)(t)), e), {});
+                  let n = t.body.reduce((e, t) => ((e[t.id] = (0, c.A)(t)), e), {});
                   i.h.dispatch({ type: "GAME_SERVER_FETCH_INSTANCES_SUCCESS", guildId: e, instances: n });
               }
           });
 }
-function E(e, t) {
-    let n = s.default.getCurrentUser()?.isStaff() ?? !1;
-    return (0, o.aP)({
-        url: f.Rsh.STOREFRONT_PRODUCT_BY_SKU_ID(t),
-        query: { include_unpublished: n },
+function g(e, t) {
+    let n = a.default.getCurrentUser()?.isStaff() ?? !1;
+    return (0, l.aP)({
+        url: p.Rsh.STOREFRONT_PRODUCT_BY_SKU_ID(t),
+        query: { locale: s.default.locale, include_unpublished: n },
         rejectWithError: !0,
         retries: 3,
     }).then((n) => {
@@ -103,55 +109,55 @@ function E(e, t) {
         }
     });
 }
-function g(e, t) {
+function A(e, t) {
     e &&
-        a.default.track(f.HAw.GAME_SERVER_HOSTING_THIRD_PARTY_CONSENT_ACCEPTED, {
-            user_id: s.default.getCurrentUser()?.id,
+        o.default.track(p.HAw.GAME_SERVER_HOSTING_THIRD_PARTY_CONSENT_ACCEPTED, {
+            user_id: a.default.getCurrentUser()?.id,
             provider: t,
         });
 }
-function A() {
+function I() {
     i.h.dispatch({ type: "GAME_SERVER_REGION_PING_STATE_RESET" });
 }
-function I(e, t) {
+function T(e, t) {
     i.h.dispatch({ type: "GAME_SERVER_REGION_PING_STATE_UPDATE", pingUrl: e, state: t });
 }
-function T(e, t, n, i) {
+function S(e, t, n, i) {
     return r.Bo.post({
-        url: f.Rsh.GUILD_POWERUP_TOGGLE(e, t),
+        url: p.Rsh.GUILD_POWERUP_TOGGLE(e, t),
         body: { game_server_name: n, game_server_region: i },
         rejectWithError: !0,
         oldFormErrors: !0,
     });
 }
-function S(e, t, n, i) {
+function y(e, t, n, i) {
     return r.Bo.patch({
-        url: f.Rsh.GUILD_POWERUP_UPDATE(e, t),
+        url: p.Rsh.GUILD_POWERUP_UPDATE(e, t),
         body: { game_server_name: i, sku_id: n },
         rejectWithError: !0,
         oldFormErrors: !0,
     });
 }
-function y(e, t, n) {
+function v(e, t, n) {
     return r.Bo.del({
-        url: f.Rsh.GUILD_POWERUP_TOGGLE(e, t),
+        url: p.Rsh.GUILD_POWERUP_TOGGLE(e, t),
         query: { entitlement_id: n },
         rejectWithError: !0,
         oldFormErrors: !0,
     });
 }
-function v(e) {
-    return r.Bo.get({ url: f.Rsh.GAME_SERVER_REGIONS(e), rejectWithError: !0, oldFormErrors: !0, retries: 3 }).then(
+function N(e) {
+    return r.Bo.get({ url: p.Rsh.GAME_SERVER_REGIONS(e), rejectWithError: !0, oldFormErrors: !0, retries: 3 }).then(
         (e) => {
             i.h.dispatch({
                 type: "GAME_SERVER_FETCH_REGIONS_SUCCESS",
-                regions: e.body.map(d.A).sort((e, t) => e.name.localeCompare(t.name)),
+                regions: e.body.map(_.A).sort((e, t) => e.name.localeCompare(t.name)),
             });
         },
     );
 }
-function N(e, t) {
-    return r.Bo.post({ url: f.Rsh.GAME_SERVER_WAKE(e, t), rejectWithError: !0 }).then((t) => {
-        i.h.dispatch({ type: "GAME_SERVER_UPDATE_INSTANCE_SUCCESS", guildId: e, instance: (0, u.A)(t.body) });
+function C(e, t) {
+    return r.Bo.post({ url: p.Rsh.GAME_SERVER_WAKE(e, t), rejectWithError: !0 }).then((t) => {
+        i.h.dispatch({ type: "GAME_SERVER_UPDATE_INSTANCE_SUCCESS", guildId: e, instance: (0, c.A)(t.body) });
     });
 }
--- chunks/828596.js
+++ chunks/828596.js
@@ -1,41 +1,43 @@
-t.d(l, { M0: () => h, vZ: () => u });
+t.d(l, { M0: () => p, vZ: () => h });
 var r = t(574381),
     o = t(73153),
     n = t(198982);
 t(384726);
-var a = t(371794),
-    i = t(124338),
-    d = t(631467),
-    s = t(507832),
-    c = t(652215);
-async function u(e) {
+var a = t(773669),
+    i = t(371794),
+    d = t(124338),
+    s = t(631467),
+    c = t(507832),
+    u = t(652215);
+async function h(e) {
     let {
         collectionId: l,
         includeUnpublishedProducts: t = !1,
-        includeUnpublishedCollection: s = !1,
-        includePricing: u = !1,
-        ignoreCache: h = !1,
+        includeUnpublishedCollection: c = !1,
+        includePricing: h = !1,
+        ignoreCache: p = !1,
     } = e;
-    if (!l || i.A.isFetching(l)) return;
-    let p = i.A.getApiError(l);
-    if (p?.status !== 404 && p?.status !== 429)
+    if (!l || d.A.isFetching(l)) return;
+    let m = d.A.getApiError(l);
+    if (m?.status !== 404 && m?.status !== 429)
         try {
             o.h.dispatch({ type: "STOREFRONT_COLLECTION_WITH_PRODUCTS_FETCH", collectionId: l });
-            let e = await (0, a.aP)({
-                url: c.Rsh.STOREFRONT_COLLECTION_WITH_PRODUCTS(l),
+            let e = await (0, i.aP)({
+                url: u.Rsh.STOREFRONT_COLLECTION_WITH_PRODUCTS(l),
                 query: {
+                    locale: a.default.locale,
                     collection_id: l,
                     include_unpublished_products: t,
-                    include_unpublished_collection: s,
-                    include_pricing: u,
-                    ignore_cache: h,
+                    include_unpublished_collection: c,
+                    include_pricing: h,
+                    ignore_cache: p,
                     include_google_sku_ids: (0, r.m0)(),
                 },
                 rejectWithError: !0,
             });
             o.h.dispatch({
                 type: "STOREFRONT_COLLECTION_WITH_PRODUCTS_FETCH_SUCCESS",
-                collection: d.A.fromServer(e.body),
+                collection: s.A.fromServer(e.body),
             });
         } catch (t) {
             let e = new n.LG(t);
@@ -49,24 +51,25 @@ async function u(e) {
             );
         }
 }
-async function h(e) {
-    let { productId: l, includePricing: t = !1, includeUnpublished: d = !1, ignoreCache: u = !1 } = e;
-    if (!l || i.A.isFetching(l)) return;
-    let h = i.A.getApiError(l);
-    if (h?.status !== 404 && h?.status !== 429)
+async function p(e) {
+    let { productId: l, includePricing: t = !1, includeUnpublished: s = !1, ignoreCache: h = !1 } = e;
+    if (!l || d.A.isFetching(l)) return;
+    let p = d.A.getApiError(l);
+    if (p?.status !== 404 && p?.status !== 429)
         try {
             o.h.dispatch({ type: "STOREFRONT_PRODUCT_WITH_SKUS_FETCH", productId: l });
-            let e = await (0, a.aP)({
-                url: c.Rsh.STOREFRONT_PRODUCT_WITH_SKUS(l),
+            let e = await (0, i.aP)({
+                url: u.Rsh.STOREFRONT_PRODUCT_WITH_SKUS(l),
                 query: {
+                    locale: a.default.locale,
                     include_pricing: t,
-                    include_unpublished: d,
-                    ignore_cache: u,
+                    include_unpublished: s,
+                    ignore_cache: h,
                     include_google_sku_ids: (0, r.m0)(),
                 },
                 rejectWithError: !0,
             });
-            o.h.dispatch({ type: "STOREFRONT_PRODUCT_WITH_SKUS_FETCH_SUCCESS", product: s.A.fromServer(e.body) });
+            o.h.dispatch({ type: "STOREFRONT_PRODUCT_WITH_SKUS_FETCH_SUCCESS", product: c.A.fromServer(e.body) });
         } catch (t) {
             let e = new n.LG(t);
             throw (o.h.dispatch({ type: "STOREFRONT_PRODUCT_WITH_SKUS_FETCH_FAILURE", productId: l, apiError: e }), e);
