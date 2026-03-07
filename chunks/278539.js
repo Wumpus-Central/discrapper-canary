cd9c68f3b7305dbe495a40601537b2f15c7a2508
DiscrapperManager <admin@minerpl.xyz>
2026-03-07 02:08:59 +0100

    canaryRelease: 507704
    Main scripts: 3
    Lazy Loaded Scripts: 2005
    Chunks:
     * Main Files: 13363
     * Scripts: 12454
    Strings: 23927
    Routes: 805
    Experiments Count: 153
    Apex Experiments Count: 194

--- chunks/278539.js
+++ chunks/278539.js
@@ -1,27 +1,32 @@
 "use strict";
-n.d(t, { A: () => d });
+n.d(t, { A: () => _ });
 var r = n(64700),
     i = n(311907),
-    a = n(338932),
-    s = n(919395),
-    o = n(696451),
-    l = n(486020),
-    u = n(325445),
-    c = n(476324);
-let d = (e) => {
-    let { user: t, guildId: n, size: d, avatarDecorationOverride: _, onlyAnimateOnHoverOrFocus: f = !1 } = e,
-        [p, h] = r.useState(!1),
-        { canAnimate: m } = (0, a.T)(p, f),
-        g = (0, i.bG)([o.Ay], () => (null != n && null != t ? o.Ay.getMember(n, t.id) : null)),
-        E = (0, u.A)((0, s.lw)({ userValue: t?.avatarDecoration, guildValue: g?.avatarDecoration, guildId: n })),
-        A = r.useMemo(
-            () => (0, l.F_)({ avatarDecoration: void 0 !== _ ? _ : E, canAnimate: m, size: d }),
-            [_, E, m, d],
+    s = n(338932),
+    a = n(940622),
+    o = n(919395),
+    l = n(696451),
+    u = n(486020),
+    c = n(325445),
+    d = n(476324);
+let _ = (e) => {
+    let { user: t, guildId: n, size: _, avatarDecorationOverride: f, onlyAnimateOnHoverOrFocus: p = !1 } = e,
+        [h, m] = r.useState(!1),
+        { canAnimate: E } = (0, s.T)(h, p),
+        g = (0, i.bG)([l.Ay], () => (null != n && null != t ? l.Ay.getMember(n, t.id) : null)),
+        A = (0, c.A)((0, o.lw)({ userValue: t?.avatarDecoration, guildValue: g?.avatarDecoration, guildId: n })),
+        I = (0, a.VU)(),
+        T = r.useMemo(
+            () =>
+                null != I && "" !== I
+                    ? I
+                    : (0, u.F_)({ avatarDecoration: void 0 !== f ? f : A, canAnimate: E, size: _ }),
+            [I, f, A, E, _],
         );
     return {
-        avatarPlaceholderSrc: c,
-        avatarDecorationSrc: A,
-        isAvatarDecorationAnimating: m,
-        eventHandlers: { onMouseEnter: r.useCallback(() => h(!0), []), onMouseLeave: r.useCallback(() => h(!1), []) },
+        avatarPlaceholderSrc: d,
+        avatarDecorationSrc: T,
+        isAvatarDecorationAnimating: E,
+        eventHandlers: { onMouseEnter: r.useCallback(() => m(!0), []), onMouseLeave: r.useCallback(() => m(!1), []) },
     };
 };
--- chunks/929283.js
+++ chunks/929283.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { i: () => h });
+n.d(t, { i: () => p });
 var r = n(627968);
 n(64700);
 var i = n(503698),
@@ -9,42 +9,39 @@ var i = n(503698),
     l = n(954921),
     u = n(278539),
     c = n(837921),
-    d = n(940622),
-    _ = n(985018),
-    f = n(776205);
-let p = c.Ay.getEnableHardwareAcceleration() ? o.JsQ : o.euF,
-    h = (e) => {
+    d = n(985018),
+    _ = n(776205);
+let f = c.Ay.getEnableHardwareAcceleration() ? o.JsQ : o.euF,
+    p = (e) => {
         let {
                 item: t,
                 user: n,
                 isHighlighted: i = !1,
                 avatarSize: c = o._3J.SIZE_152,
-                avatarPlaceholderSrc: h,
-                className: m,
-                showDefaultAvatar: E = !1,
-                disableAnimation: g = !1,
+                avatarPlaceholderSrc: p,
+                className: h,
+                showDefaultAvatar: m = !1,
+                disableAnimation: E = !1,
             } = e,
-            A = (0, l.Te)(c),
-            I = (0, d.VU)(),
+            g = (0, l.Te)(c),
             {
-                avatarDecorationSrc: T,
-                eventHandlers: S,
-                avatarPlaceholderSrc: y,
+                avatarDecorationSrc: A,
+                eventHandlers: I,
+                avatarPlaceholderSrc: T,
             } = (0, u.A)({
                 user: n,
                 avatarDecorationOverride: t?.type === a.R.AVATAR_DECORATION ? t : void 0,
-                size: A,
+                size: g,
                 onlyAnimateOnHoverOrFocus: !i,
             }),
-            v = null != I && "" !== I ? I : T,
-            N = _.intl.formatToPlainString(_.t.Do2lxE, { a11y_text: t.label });
-        return (0, r.jsx)(p, {
-            ...(g ? {} : S),
-            avatarDecoration: v,
-            src: i && !E ? n?.getAvatarURL(void 0, 152, !0) : (h ?? y),
-            imageClassName: i && !E ? f.WG : void 0,
-            className: s()(m, f.my),
+            S = d.intl.formatToPlainString(d.t.Do2lxE, { a11y_text: t.label });
+        return (0, r.jsx)(f, {
+            ...(E ? {} : I),
+            avatarDecoration: A,
+            src: i && !m ? n?.getAvatarURL(void 0, 152, !0) : (p ?? T),
+            imageClassName: i && !m ? _.WG : void 0,
+            className: s()(h, _.my),
             size: c,
-            "aria-label": N,
+            "aria-label": S,
         });
     };
