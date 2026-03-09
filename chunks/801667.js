402d796bea2eb57d44e3f26dc42cc64aa07854aa
DiscrapperManager <admin@minerpl.xyz>
2026-03-09 16:38:12 +0100

    canaryRelease: 507880
    Main scripts: 3
    Lazy Loaded Scripts: 2005
    Chunks:
     * Main Files: 13364
     * Scripts: 12454
    Strings: 23929
    Routes: 805
    Experiments Count: 153
    Apex Experiments Count: 195

--- chunks/801667.js
+++ chunks/801667.js
@@ -1,43 +1,50 @@
-n.d(t, { A: () => _ });
+n.d(t, { A: () => x });
 var i = n(64700),
     l = n(311907),
     s = n(554146),
     a = n(932001),
-    r = n(498642),
-    o = n(71393),
-    c = n(320501),
-    d = n(576705),
-    u = n(927813),
-    h = n(661191),
-    m = n(302909),
-    A = n(342220),
-    g = n(652215);
-let p = 90 * u.A.Millis.DAY,
-    f = 14 * u.A.Millis.DAY;
-function _(e) {
-    let t = (0, l.bG)([o.A], () => o.A.getGuild(e.guild_id)),
-        n = (0, l.bG)([r.A], () => r.A.getMemberCount(t?.id) ?? 0),
-        u = (0, l.bG)([c.A], () => c.A.getLastMessage(e.id)),
-        _ = (0, l.bG)([d.A], () => null != t && d.A.can(g.xBc.ADMINISTRATOR, t)),
-        E = (0, A.A)(),
-        { enabled: x } = m.A.useConfig({ location: "useIsEligibleForFirstBoosterUpsell" }),
-        [C] = (0, a.Wl)(s.M.FIRST_BOOSTER_UPSELL_OVERSEER, { cooldownDurationMs: 0, numTimesToRecur: 3 }),
-        S = C === s.M.FIRST_BOOSTER_UPSELL_OVERSEER;
+    r = n(153488),
+    o = n(197305),
+    c = n(498642),
+    d = n(71393),
+    u = n(320501),
+    h = n(576705),
+    m = n(927813),
+    A = n(661191),
+    g = n(302909),
+    p = n(342220),
+    f = n(652215);
+let _ = 90 * m.A.Millis.DAY,
+    E = 14 * m.A.Millis.DAY;
+function x(e) {
+    let t = (0, l.bG)([d.A], () => d.A.getGuild(e.guild_id)),
+        n = (0, l.bG)([c.A], () => c.A.getMemberCount(t?.id) ?? 0),
+        m = (0, l.bG)([u.A], () => u.A.getLastMessage(e.id)),
+        x = (0, l.bG)([h.A], () => null != t && h.A.can(f.xBc.ADMINISTRATOR, t)),
+        C = (0, l.bG)([o.A], () => o.A.getGuildAffinity(e.guild_id)?.score),
+        S = (0, l.bG)([r.A], () => r.A.hasConsented(f.YAq.PERSONALIZATION)),
+        I = (0, p.A)(),
+        { enabled: T } = g.A.useConfig({ location: "useIsEligibleForFirstBoosterUpsell" }),
+        [N] = (0, a.Wl)(s.M.FIRST_BOOSTER_UPSELL_OVERSEER, { cooldownDurationMs: 0, numTimesToRecur: 3 }),
+        y = N === s.M.FIRST_BOOSTER_UPSELL_OVERSEER;
     return i.useMemo(
         () =>
             !(
-                !x ||
+                !T ||
+                !y ||
                 !S ||
-                e.type !== g.rbe.GUILD_TEXT ||
+                null == C ||
+                C <= 17.06 ||
+                e.type !== f.rbe.GUILD_TEXT ||
                 null == t ||
-                h.default.age(t.id) < p ||
+                A.default.age(t.id) < _ ||
                 null == t.premiumSubscriberCount ||
                 0 !== t.premiumSubscriberCount ||
                 n < 10 ||
-                u?.id == null ||
-                h.default.age(u.id) > f
+                m?.id == null ||
+                A.default.age(m.id) > E
             ) &&
-            (_ || E),
-        [x, S, e.type, t, n, u?.id, _, E],
+            (x || I),
+        [T, y, S, e.type, t, n, C, m?.id, x, I],
     );
 }
