0500846774a8937598f4012f36c7d85b9f00318b
DiscrapperManager <admin@minerpl.xyz>
2026-03-09 22:56:54 +0100

    canaryRelease: 508197
    Main scripts: 3
    Lazy Loaded Scripts: 2007
    Chunks:
     * Main Files: 13361
     * Scripts: 12464
    Strings: 23966
    Routes: 802
    Experiments Count: 152
    Apex Experiments Count: 192

--- chunks/354062.js
+++ chunks/354062.js
@@ -1,4 +1,4 @@
-s.d(t, { A: () => U });
+s.d(t, { A: () => M });
 var r = s(627968),
     i = s(64700),
     n = s(503698),
@@ -17,59 +17,58 @@ var r = s(627968),
     g = s(975571),
     j = s(723702),
     v = s(290805),
-    A = s(429432),
-    S = s(830012),
+    A = s(830012),
     y = s(905552),
-    C = s(688465),
-    b = s(207975),
-    w = s(502075),
-    I = s(765682),
-    E = s(332800),
-    N = s(985018),
-    O = s(243486);
-function T(e, t, s, r) {
+    S = s(688465),
+    C = s(207975),
+    b = s(502075),
+    w = s(765682),
+    I = s(332800),
+    E = s(985018),
+    N = s(243486);
+function O(e, t, s, r) {
     let i = (0, j.isWindows)() ? (0, y.A)(h.Ay, _.A) : null,
         n = h.Ay.getRunningGames(),
         l = s?.split(":")[1],
         a = e.id.split(":")[1];
     if (null != l && l === a) return 0;
     if (null != t && t === e.id) return 5;
-    if (null != i && (0, S.A)(e.id, i.windowHandle)) return 4;
-    if (null != n.find((t) => (0, S.A)(e.id, t.windowHandle))) return 3;
+    if (null != i && (0, A.A)(e.id, i.windowHandle)) return 4;
+    if (null != n.find((t) => (0, A.A)(e.id, t.windowHandle))) return 3;
     let o = r?.[a];
-    return null != o && w.F2.has(o) ? 2 : 1;
+    return null != o && b.F2.has(o) ? 2 : 1;
 }
-function R(e) {
+function T(e) {
     let { source: t } = e;
     return t.id.startsWith(c.fS.SCREEN)
         ? (0, r.jsx)(u.kN9, { size: "xs" })
         : t.id.startsWith(c.fS.WINDOW)
           ? null != t.icon && "" !== t.icon
-              ? (0, r.jsx)("img", { src: t.icon, className: O.pI, alt: "" })
+              ? (0, r.jsx)("img", { src: t.icon, className: N.pI, alt: "" })
               : (0, r.jsx)(u.WxK, { size: "xs" })
           : (0, r.jsx)(u.npA, { size: "xs" });
 }
-function M(e) {
+function R(e) {
     let { source: t, selected: s, onClick: i } = e,
         { url: n, name: a } = t;
     return (0, r.jsxs)(u.DUT, {
         onClick: () => i(t),
-        className: l()(O.sP, { [O.Lv]: s }),
+        className: l()(N.sP, { [N.Lv]: s }),
         children: [
             (0, r.jsx)("div", {
-                className: O.ji,
+                className: N.ji,
                 children: (0, r.jsxs)("div", {
-                    className: O.KG,
+                    className: N.KG,
                     children: [
-                        (0, r.jsx)("img", { src: n, className: O.f5, alt: "" }),
+                        (0, r.jsx)("img", { src: n, className: N.f5, alt: "" }),
                         (0, r.jsx)("div", {
-                            className: O.qe,
+                            className: N.qe,
                             children: (0, r.jsx)("div", {
-                                className: O.K3,
+                                className: N.K3,
                                 children: (0, r.jsx)(u.Text, {
                                     variant: "text-sm/medium",
                                     color: "currentColor",
-                                    children: N.intl.string(N.t.z7WGhv),
+                                    children: E.intl.string(E.t.z7WGhv),
                                 }),
                             }),
                         }),
@@ -77,16 +76,16 @@ function M(e) {
                 }),
             }),
             (0, r.jsxs)("div", {
-                className: O.Uw,
+                className: N.Uw,
                 children: [
-                    (0, r.jsx)(R, { source: t }),
-                    (0, r.jsx)(u.Text, { variant: "text-sm/medium", className: O.VZ, children: a }),
+                    (0, r.jsx)(T, { source: t }),
+                    (0, r.jsx)(u.Text, { variant: "text-sm/medium", className: N.VZ, children: a }),
                 ],
             }),
         ],
     });
 }
-function U(e) {
+function M(e) {
     let { onClick: t } = e,
         [
             {
@@ -97,90 +96,86 @@ function U(e) {
                 fetchingSources: _,
                 selectedSource: g,
                 discordSourceId: y,
-                hasPermission: w,
+                hasPermission: b,
             },
-        ] = (0, C.tS)(),
-        { smarterSourceOrdering: E } = (0, A.s)({ location: "GoLiveSourceGrid" }),
-        R = (0, d.bG)([h.Ay], () => h.Ay.getRunningGames()),
-        U = (0, d.cf)([h.Ay], () => {
+        ] = (0, S.tS)(),
+        I = (0, d.bG)([h.Ay], () => h.Ay.getRunningGames()),
+        T = (0, d.cf)([h.Ay], () => {
             let e = h.Ay.getCandidateGames(),
                 t = {};
             for (let s of e) null != s.windowHandle && (t[s.windowHandle] = s.exeName);
             return t;
         }),
-        L = (0, d.bG)([m.A], () => m.A.quests),
-        D = (0, b.A)(L, R, s);
+        M = (0, d.bG)([m.A], () => m.A.quests),
+        G = (0, C.A)(M, I, s);
     i.useEffect(() => ((0, x.a2)(), x.e0), []);
-    let P = i.useMemo(
-        () => (E ? [...s].sort((e, t) => T(t, D?.source.id, y, U) - T(e, D?.source.id, y, U)) : s),
-        [D, E, s, y, U],
-    );
+    let D = i.useMemo(() => [...s].sort((e, t) => O(t, G?.source.id, y, T) - O(e, G?.source.id, y, T)), [G, s, y, T]);
     if (_) {
-        if (!1 === w) {
+        if (!1 === b) {
             let e = !1;
             if ((0, j.isMac)()) {
                 let t = f.A?.app.getVersion();
                 e = !v.M || "0.0.0" === t || o().satisfies(t, "0.0.363");
             }
             return (0, r.jsxs)("div", {
-                className: O.Ye,
+                className: N.Ye,
                 children: [
                     (0, r.jsxs)(u.Text, {
-                        className: O.kc,
+                        className: N.kc,
                         variant: "text-md/normal",
                         color: "text-feedback-critical",
-                        children: [N.intl.string(N.t["kW5h/W"]), (0, r.jsx)("br", {}), N.intl.string(N.t["5Jvu1R"])],
+                        children: [E.intl.string(E.t["kW5h/W"]), (0, r.jsx)("br", {}), E.intl.string(E.t["5Jvu1R"])],
                     }),
                     e &&
                         (0, r.jsx)(u.Button, {
-                            text: N.intl.string(N.t["XgZk+u"]),
+                            text: E.intl.string(E.t["XgZk+u"]),
                             onClick: () => {
-                                p.A.openSettings(I.iL.SCREEN_RECORDING);
+                                p.A.openSettings(w.iL.SCREEN_RECORDING);
                             },
                         }),
                 ],
             });
         }
-        return (0, r.jsx)("div", { className: O.Lq, children: (0, r.jsx)(u.y$y, {}) });
+        return (0, r.jsx)("div", { className: N.Lq, children: (0, r.jsx)(u.y$y, {}) });
     }
-    let F = [];
-    return ((F = a === c.fS.WINDOW ? P : a === c.fS.SCREEN ? l : n), a === c.fS.CAMERA && 0 === F.length)
-        ? (0, r.jsx)(G, {})
+    let L = [];
+    return ((L = a === c.fS.WINDOW ? D : a === c.fS.SCREEN ? l : n), a === c.fS.CAMERA && 0 === L.length)
+        ? (0, r.jsx)(U, {})
         : (0, r.jsx)("div", {
-              className: O.zr,
-              children: F.map((e) =>
+              className: N.zr,
+              children: L.map((e) =>
                   (0, r.jsx)(
-                      M,
+                      R,
                       {
                           onClick: t,
                           source: e,
-                          selected: null != g && "windowHandle" in g ? (0, S.A)(e.id, g.windowHandle) : g?.id === e.id,
+                          selected: null != g && "windowHandle" in g ? (0, A.A)(e.id, g.windowHandle) : g?.id === e.id,
                       },
                       e.id,
                   ),
               ),
           });
 }
-function G() {
+function U() {
     return (0, r.jsxs)("div", {
-        className: O.p$,
+        className: N.p$,
         children: [
-            (0, r.jsx)(u.npA, { size: "md", color: u.LU0.colors.ICON_MUTED, className: O.Dw }),
+            (0, r.jsx)(u.npA, { size: "md", color: u.LU0.colors.ICON_MUTED, className: N.Dw }),
             (0, r.jsx)(u.Text, {
                 variant: "text-lg/medium",
                 color: "text-strong",
-                className: O.jU,
-                children: N.intl.string(E.default["/z3YaZ"]),
+                className: N.jU,
+                children: E.intl.string(I.default["/z3YaZ"]),
             }),
             (0, r.jsx)(u.Text, {
                 variant: "text-sm/medium",
                 color: "text-subtle",
-                className: O.C3,
-                children: N.intl.string(E.default.agwSGA),
+                className: N.C3,
+                children: E.intl.string(I.default.agwSGA),
             }),
             (0, r.jsx)(u.Text, {
                 variant: "text-sm/semibold",
-                children: N.intl.format(E.default.xpVzr3, { helpdeskURL: g.A.getArticleURL(0x53d41ade17) }),
+                children: E.intl.format(I.default.xpVzr3, { helpdeskURL: g.A.getArticleURL(0x53d41ade17) }),
             }),
         ],
     });
