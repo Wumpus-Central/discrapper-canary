745f47c2c1e47100dc67f6703a3b1894f560a357
DiscrapperManager <admin@minerpl.xyz>
2026-03-09 16:09:11 +0100

    canaryRelease: 507873
    Main scripts: 3
    Lazy Loaded Scripts: 2005
    Chunks:
     * Main Files: 13364
     * Scripts: 12454
    Strings: 23929
    Routes: 805
    Experiments Count: 153
    Apex Experiments Count: 195

--- chunks/195548.js
+++ chunks/195548.js
@@ -1,78 +1,76 @@
 n.d(t, { A: () => R });
 var i = n(627968),
     s = n(397927),
-    a = n(290595),
-    l = n(964486),
+    l = n(290595),
+    a = n(964486),
     r = n(332921),
     o = n(814278),
-    c = n(890164),
-    d = n(104954),
+    d = n(890164),
+    c = n(104954),
     u = n(740625),
     _ = n(657364),
-    m = n(986666),
-    A = n(713031),
-    g = n(8271),
+    g = n(986666),
+    m = n(713031),
+    A = n(8271),
     h = n(309272),
-    x = n(589984),
-    p = n(549200),
+    p = n(589984),
+    x = n(549200),
     E = n(871930),
-    C = n(320134),
-    T = n(433809),
-    S = n(428854),
-    I = n(259274),
-    f = n(531525),
-    N = n(985018);
-function b() {
-    return (0, c.lX)("ProfilePrivacyCategory")
-        ? (0, i.jsx)(C.A, {
-              title: N.intl.string(N.t.ul884f),
-              description: N.intl.format(N.t.N4jSgR, {
-                  learnMoreUrl: "https://support.discord.com/hc/articles/private-profiles",
-              }),
-              webSetting: f.H.PRIVACY_PROFILE_VISIBILITY,
-              children: (0, i.jsx)(T.A, {}),
+    T = n(320134),
+    S = n(433809),
+    C = n(428854),
+    f = n(259274),
+    I = n(531525),
+    b = n(985018);
+function N() {
+    return (0, d.lX)("ProfilePrivacyCategory")
+        ? (0, i.jsx)(T.A, {
+              title: b.intl.string(b.t.ul884f),
+              description: b.intl.string(b.t.J0SFL2),
+              webSetting: I.H.PRIVACY_PROFILE_VISIBILITY,
+              children: (0, i.jsx)(S.A, {}),
           })
         : null;
 }
-function j() {
-    return (0, i.jsxs)(C.A, {
-        title: N.intl.string(N.t.WWaFn5),
-        description: N.intl.format(N.t["/6sFWa"], { helpArticle: (0, o.aW)() }),
-        webSetting: f.H.PRIVACY_AND_SAFETY_VOICE_SECURITY,
-        children: [(0, i.jsx)(p.A, {}), (0, i.jsx)(I.A, {})],
+function v() {
+    return (0, i.jsxs)(T.A, {
+        title: b.intl.string(b.t.WWaFn5),
+        description: b.intl.format(b.t["/6sFWa"], { helpArticle: (0, o.aW)() }),
+        webSetting: I.H.PRIVACY_AND_SAFETY_VOICE_SECURITY,
+        children: [(0, i.jsx)(x.A, {}), (0, i.jsx)(f.A, {})],
     });
 }
-function v() {
-    return (0, i.jsx)(C.A, {
-        title: N.intl.string(N.t.BG7QsQ),
-        webSetting: f.H.PRIVACY_AND_SAFETY_RECEIVE_DISCORD_DATA_CATEGORY,
+function j() {
+    return (0, i.jsx)(T.A, {
+        title: b.intl.string(b.t.BG7QsQ),
+        webSetting: I.H.PRIVACY_AND_SAFETY_RECEIVE_DISCORD_DATA_CATEGORY,
         children: (0, i.jsx)(_.A, {}),
     });
 }
 function O() {
-    return (0, i.jsxs)(C.A, {
-        title: N.intl.string(N.t.SRZyHg),
-        webSetting: f.H.PRIVACY_AND_SAFETY_DISCORD_DATA_USAGE_CATEGORY,
+    return (0, i.jsxs)(T.A, {
+        title: b.intl.string(b.t.SRZyHg),
+        webSetting: I.H.PRIVACY_AND_SAFETY_DISCORD_DATA_USAGE_CATEGORY,
         children: [
-            (0, i.jsx)(x.A, {}),
+            (0, i.jsx)(p.A, {}),
             (0, i.jsx)(h.A, {}),
-            (0, i.jsx)(g.A, {}),
             (0, i.jsx)(A.A, {}),
-            (0, i.jsx)(E.h, { setting: f.H.PRIVACY_DATA_CLIPS, children: (0, i.jsx)(r.A, {}) }),
-            (0, i.jsx)(m.z, {}),
+            (0, i.jsx)(m.A, {}),
+            (0, i.jsx)(E.h, { setting: I.H.PRIVACY_DATA_CLIPS, children: (0, i.jsx)(r.A, {}) }),
+            (0, i.jsx)(g.z, {}),
         ],
     });
 }
 function R() {
     return (
-        (0, l.Ay)(() => {
-            (0, a.Q)(), (0, d.s)();
+        (0, a.Ay)(() => {
+            (0, l.Q)(), (0, c.s)();
         }),
         (0, i.jsx)(u.R, {
-            header: N.intl.string(N.t.OAuOHD),
-            notice: (0, i.jsx)(S.A, { page: S.R.DATA_AND_PRIVACY }),
+            header: b.intl.string(b.t.OAuOHD),
+            notice: (0, i.jsx)(C.A, { page: C.R.DATA_AND_PRIVACY }),
             children: (0, i.jsxs)(s.lVW, {
-                children: [(0, i.jsx)(O, {}), (0, i.jsx)(b, {}), (0, i.jsx)(v, {}), (0, i.jsx)(j, {})],
+                children: [(0, i.jsx)(O, {}), (0, i.jsx)(N, {}), (0, i.jsx)(j, {}), (0, i.jsx)(v, {})],
             }),
         })
     );
--- chunks/251139.js
+++ chunks/251139.js
@@ -1,14 +1,13 @@
-i.d(e, { w: () => o });
-var n = i(419954),
-    l = i(890164),
-    s = i(780964),
-    r = i(868018),
-    a = i(692964),
-    u = i(985018);
-let o = (0, n.zZ)(s.X.PROFILE_PRIVACY_CATEGORY, {
-    usePredicate: () => (0, l.lX)("ProfilePrivacyCategory"),
-    useTitle: () => u.intl.string(u.t.ul884f),
-    useSubtitle: () =>
-        u.intl.format(u.t.N4jSgR, { learnMoreUrl: "https://support.discord.com/hc/articles/private-profiles" }),
-    buildLayout: () => [a.I, r.O],
+n.d(t, { w: () => d });
+var i = n(419954),
+    s = n(890164),
+    l = n(780964),
+    a = n(868018),
+    r = n(692964),
+    o = n(985018);
+let d = (0, i.zZ)(l.X.PROFILE_PRIVACY_CATEGORY, {
+    usePredicate: () => (0, s.lX)("ProfilePrivacyCategory"),
+    useTitle: () => o.intl.string(o.t.ul884f),
+    useSubtitle: () => o.intl.string(o.t.J0SFL2),
+    buildLayout: () => [r.I, a.O],
 });
