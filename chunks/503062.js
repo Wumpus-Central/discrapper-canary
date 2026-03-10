03fd42a0854781ca2c4210ad57d13e3d540db2fe
DiscrapperManager <admin@minerpl.xyz>
2026-03-10 20:14:45 +0100

    canaryRelease: 508700
    Main scripts: 3
    Lazy Loaded Scripts: 2009
    Chunks:
     * Main Files: 13358
     * Scripts: 12481
    Strings: 23964
    Routes: 802
    Experiments Count: 150
    Apex Experiments Count: 191

--- chunks/503062.js
+++ chunks/503062.js
@@ -1,37 +1,35 @@
-n.d(t, { A: () => j });
+n.d(t, { A: () => I });
 var i = n(627968),
     l = n(64700),
     a = n(311907),
     s = n(397927),
     r = n(442433),
     o = n(297413),
-    c = n(439547),
     d = n(714114),
-    u = n(729551),
-    g = n(178418),
+    c = n(729551),
+    u = n(178418),
     m = n(87664),
-    x = n(290863),
-    f = n(939496),
-    p = n(652215),
-    h = n(996988),
-    _ = n(794904);
-let A = s._3J.SIZE_40,
-    I = (0, s.FT9)(A);
-function j(e) {
-    let { user: t, status: j, guildId: v, channelId: E, onSelect: T } = e,
-        { theme: b, themeType: N } = (0, f.E)(),
-        S = l.useMemo(() => t.isNonUserBot() || (0, g.c)(t, E), [t, E]),
-        { activities: y, isMobileOnline: C } = (0, a.cf)([x.A], () => ({
-            activities: x.A.getActivities(t.id),
-            isMobileOnline: x.A.isMobileOnline(t.id),
+    g = n(290863),
+    x = n(939496),
+    f = n(652215),
+    p = n(996988),
+    h = n(794904);
+let _ = s._3J.SIZE_40,
+    A = (0, s.FT9)(_);
+function I(e) {
+    let { user: t, status: I, guildId: j, channelId: v, onSelect: E } = e,
+        { theme: T, themeType: b } = (0, x.E)(),
+        N = l.useMemo(() => t.isNonUserBot() || (0, u.c)(t, v), [t, v]),
+        { activities: S, isMobileOnline: y } = (0, a.cf)([g.A], () => ({
+            activities: g.A.getActivities(t.id),
+            isMobileOnline: g.A.isMobileOnline(t.id),
         })),
-        k = (0, m.A)(t.id),
-        { voiceActivityStatusEnabled: R } = (0, c.g)({ location: "UserProfileMutualFriendRow" }),
-        { voiceChannel: w } = (0, d.A)({ userId: R ? t.id : void 0, guildId: v });
+        C = (0, m.A)(t.id),
+        { voiceChannel: R } = (0, d.A)({ userId: t.id, guildId: j });
     return (0, i.jsxs)(s.DUT, {
-        focusProps: N === h.d.MODAL_V2 ? { offset: { top: 4, right: 4, left: 4 } } : { offset: { right: 8 } },
-        className: _.nM,
-        onClick: T,
+        focusProps: b === p.d.MODAL_V2 ? { offset: { top: 4, right: 4, left: 4 } } : { offset: { right: 8 } },
+        className: h.nM,
+        onClick: E,
         onContextMenu: (e) => {
             (0, r.L3)(e, async () => {
                 let { default: e } = await Promise.all([n.e("97262"), n.e("3795"), n.e("32418"), n.e("57600")]).then(
@@ -42,19 +40,19 @@ function j(e) {
         },
         children: [
             (0, i.jsx)(s.euF, {
-                src: t.getAvatarURL(v, I),
+                src: t.getAvatarURL(j, A),
                 "aria-label": t.username,
-                size: A,
-                status: S ? p.clD.UNKNOWN : j,
-                statusBackdropColor: S ? void 0 : (0, s.C$5)(b),
-                isMobile: C,
-                className: _.my,
+                size: _,
+                status: N ? f.clD.UNKNOWN : I,
+                statusBackdropColor: N ? void 0 : (0, s.C$5)(T),
+                isMobile: y,
+                className: h.my,
             }),
             (0, i.jsxs)("div", {
-                className: _.zH,
+                className: h.zH,
                 children: [
-                    (0, i.jsx)(o.A, { user: t, className: _.Tc, discriminatorClass: _.D2 }),
-                    (0, i.jsx)(u.A, { user: t, activities: y, applicationStream: k, voiceChannel: w }),
+                    (0, i.jsx)(o.A, { user: t, className: h.Tc, discriminatorClass: h.D2 }),
+                    (0, i.jsx)(c.A, { user: t, activities: S, applicationStream: C, voiceChannel: R }),
                 ],
             }),
         ],
