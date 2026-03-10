c606bc582b1d5c428185f8b96c5f3084cb496323
DiscrapperManager <admin@minerpl.xyz>
2026-03-10 23:43:18 +0100

    canaryRelease: 508934
    Main scripts: 3
    Lazy Loaded Scripts: 2009
    Chunks:
     * Main Files: 13357
     * Scripts: 12482
    Strings: 23966
    Routes: 802
    Experiments Count: 150
    Apex Experiments Count: 192

--- chunks/614203.js
+++ chunks/614203.js
@@ -1,99 +1,98 @@
-"use strict";
-s.d(e, { A: () => b });
-var a = s(627968),
-    n = s(64700),
-    r = s(621466),
-    i = s(311907),
-    c = s(397927),
-    l = s(711581),
-    o = s(638128),
-    p = s(975571),
-    u = s(454235),
-    d = s(652215),
-    g = s(985018);
-function b(t) {
-    let { text: e = "", target: s } = t,
-        b = (0, i.bG)([o.A], () => o.A.isEnabled()),
-        f = (0, i.bG)([o.A], () => o.A.hasLearnedWord(e), [e]),
-        x = n.useRef({ ...t, spellcheckEnabled: b });
-    x.current = { ...t, spellcheckEnabled: b };
-    let h = n.useRef(!1),
-        [m, T] = n.useState(!1),
-        [y, S] = n.useState([]),
-        C = n.useCallback(() => {
-            let { spellcheckEnabled: t, text: e, onHeightUpdate: s } = x.current;
+l.d(e, { A: () => E });
+var n = l(627968),
+    i = l(64700),
+    r = l(621466),
+    s = l(311907),
+    a = l(397927),
+    c = l(711581),
+    u = l(638128),
+    o = l(975571),
+    d = l(454235),
+    p = l(652215),
+    h = l(985018);
+function E(t) {
+    let { text: e = "", target: l } = t,
+        E = (0, s.bG)([u.A], () => u.A.isEnabled()),
+        f = (0, s.bG)([u.A], () => u.A.hasLearnedWord(e), [e]),
+        L = i.useRef({ ...t, spellcheckEnabled: E });
+    L.current = { ...t, spellcheckEnabled: E };
+    let m = i.useRef(!1),
+        [x, C] = i.useState(!1),
+        [b, A] = i.useState([]),
+        T = i.useCallback(() => {
+            let { spellcheckEnabled: t, text: e, onHeightUpdate: l } = L.current;
             t &&
-                (0, u.TT)() &&
-                Promise.all([(0, u.PS)(e, !0), (0, u.Os)(e, !0)]).then((t) => {
-                    let [e, a] = t;
-                    h.current && (T(e), S(a), s());
+                (0, d.TT)() &&
+                Promise.all([(0, d.PS)(e, !0), (0, d.Os)(e, !0)]).then((t) => {
+                    let [e, n] = t;
+                    m.current && (C(e), A(n), l());
                 });
         }, []),
-        E = n.useCallback(() => {
-            if ((0, r.vq)(s, HTMLInputElement) || (0, r.vq)(s, HTMLTextAreaElement)) {
-                let { selectionStart: t, selectionEnd: e, value: a } = s;
-                (s.value = ""), (s.value = a), "email" !== s.type && ((s.selectionStart = t), (s.selectionEnd = e));
+        g = i.useCallback(() => {
+            if ((0, r.vq)(l, HTMLInputElement) || (0, r.vq)(l, HTMLTextAreaElement)) {
+                let { selectionStart: t, selectionEnd: e, value: n } = l;
+                (l.value = ""), (l.value = n), "email" !== l.type && ((l.selectionStart = t), (l.selectionEnd = e));
             }
-        }, [s]);
-    return (n.useEffect(() => {
-        let t = (0, u.nL)(C);
+        }, [l]);
+    return (i.useEffect(() => {
+        let t = (0, d.nL)(T);
         return (
-            (h.current = !0),
+            (m.current = !0),
             () => {
-                (h.current = !1), t();
+                (m.current = !1), t();
             }
         );
-    }, [C]),
-    n.useEffect(() => {
-        C();
-    }, [e, b, C]),
-    (0, u.TT)())
+    }, [T]),
+    i.useEffect(() => {
+        T();
+    }, [e, E, T]),
+    (0, d.TT)())
         ? [
-              y.map((t, e) =>
-                  (0, a.jsx)(
-                      c.Drp,
+              b.map((t, e) =>
+                  (0, n.jsx)(
+                      a.Drp,
                       {
                           id: `correction-${e}`,
                           label: t,
                           action: () => {
-                              (0, u.rK)(t), s.focus();
+                              (0, d.rK)(t), l.focus();
                           },
                       },
                       `correction-${e}`,
                   ),
               ),
-              (0, a.jsxs)(a.Fragment, {
+              (0, n.jsxs)(n.Fragment, {
                   children: [
-                      m
-                          ? (0, a.jsx)(c.Drp, {
+                      x
+                          ? (0, n.jsx)(a.Drp, {
                                 id: "add-to-dictionary",
-                                label: g.intl.string(g.t.HJmG1G),
+                                label: h.intl.string(h.t.HJmG1G),
                                 action: () => {
-                                    (0, l.NG)(e), E();
+                                    (0, c.NG)(e), g();
                                 },
                             })
                           : null,
                       f &&
-                          (0, a.jsx)(c.Drp, {
+                          (0, n.jsx)(a.Drp, {
                               id: "remove-from-dictionary",
-                              label: g.intl.string(g.t.xXqIX0),
+                              label: h.intl.string(h.t.xXqIX0),
                               action: () => {
-                                  (0, l.QZ)(e), E();
+                                  (0, c.QZ)(e), g();
                               },
                           }),
-                      (0, a.jsx)(c.sLh, {
+                      (0, n.jsx)(a.sLh, {
                           id: "spellcheck",
-                          label: g.intl.string(g.t.TKkotf),
-                          checked: b,
+                          label: h.intl.string(h.t.TKkotf),
+                          checked: E,
                           action: () => {
-                              (0, l.Np)(), E();
+                              (0, c.Np)(), g();
                           },
                       }),
-                      b
-                          ? (0, a.jsx)(c.Drp, {
+                      E
+                          ? (0, n.jsx)(a.Drp, {
                                 id: "languages",
-                                label: g.intl.string(g.t.OlOHDH),
-                                action: () => window.open(p.A.getArticleURL(d.MVz.SPELLCHECK)),
+                                label: h.intl.string(h.t.OlOHDH),
+                                action: () => window.open(o.A.getArticleURL(p.MVz.SPELLCHECK)),
                             })
                           : null,
                   ],
