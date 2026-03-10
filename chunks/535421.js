7a39cbca354a892ec987a3cd1998a4f2fc7f2dd5
DiscrapperManager <admin@minerpl.xyz>
2026-03-10 21:51:52 +0100

    canaryRelease: 508796
    Main scripts: 3
    Lazy Loaded Scripts: 2009
    Chunks:
     * Main Files: 13357
     * Scripts: 12482
    Strings: 23965
    Routes: 802
    Experiments Count: 150
    Apex Experiments Count: 192

--- chunks/535421.js
+++ chunks/535421.js
@@ -1,35 +1,35 @@
-n.d(t, { A: () => _ });
+n.d(t, { A: () => m });
 var i = n(627968);
 n(64700);
-var l = n(284009),
-    a = n.n(l),
-    r = n(283823),
+var r = n(284009),
+    l = n.n(r),
+    a = n(283823),
     s = n(455207),
     o = n(542287),
     d = n(589022),
     c = n(734057),
     u = n(287809),
-    m = n(943667);
-function _(e, t) {
+    _ = n(943667);
+function m(e, t) {
     if ((0, s.MZ)(t)) return (0, i.jsx)(o.A, {});
-    if ((0, m.A)(t)) {
+    if ((0, _.A)(t)) {
         let n = (t?.author?.username ?? "").split(" ").slice(0, -1).join(" "),
-            { guild_id: l } = t.messageReference;
-        if (null != l) return (0, i.jsx)(r.A, { setPopoutRef: e.setPopoutRef, guildId: l, name: n });
+            { guild_id: r } = t.messageReference;
+        if (null != r) return (0, i.jsx)(a.A, { setPopoutRef: e.setPopoutRef, guildId: r, name: n });
     }
     if (null != t.interaction && "SENDING" === t.state) return (0, i.jsx)(i.Fragment, {});
-    let n = null != t.webhookId ? t.author : u.default.getUser(t.author.id);
-    a()(null != n, "renderUserGuildPopout: user should never be null");
-    let l = u.default.getCurrentUser();
-    a()(null != l, "renderUserGuildPopout: currentUser should never be null");
-    let _ = c.A.getChannel(t.channel_id);
+    let n = null != t.webhookId ? t.author : (u.default.getUser(t.author.id) ?? t.author);
+    l()(null != n, "renderUserGuildPopout: user should never be null");
+    let r = u.default.getCurrentUser();
+    l()(null != r, "renderUserGuildPopout: currentUser should never be null");
+    let m = c.A.getChannel(t.channel_id);
     return (
-        a()(null != _, "renderUserGuildPopout: channel should never be null"),
+        l()(null != m, "renderUserGuildPopout: channel should never be null"),
         (0, i.jsx)(d.A, {
             ...e,
             user: n,
-            currentUser: l,
-            guildId: _.guild_id,
+            currentUser: r,
+            guildId: m.guild_id,
             channelId: t.channel_id,
             messageId: t.id,
         })
