36c0ecdc4db23b6001d5055f429560e9741146cd
DiscrapperManager <admin@minerpl.xyz>
2026-03-11 22:22:04 +0100

    canaryRelease: 509632
    Main scripts: 3
    Lazy Loaded Scripts: 2041
    Chunks:
     * Main Files: 13328
     * Scripts: 12508
    Strings: 23975
    Routes: 803
    Experiments Count: 145
    Apex Experiments Count: 191

--- chunks/307006.js
+++ chunks/307006.js
@@ -1,48 +1,50 @@
 "use strict";
-n.d(t, { A: () => h });
-var r = n(311907),
-    i = n(73153),
-    a = n(160745);
-let s = null,
-    o = !1,
-    l = null;
-function u(e) {
+n.d(t, { A: () => E });
+var r = n(735438),
+    i = n.n(r),
+    s = n(311907),
+    a = n(73153),
+    o = n(160745);
+let l = null,
+    u = !1,
+    c = null;
+function d(e) {
     let { connected: t } = e;
-    o = t;
+    u = t;
 }
-function c() {
-    o = !1;
+function _() {
+    u = !1;
 }
-function d() {
-    o = !1;
+function f() {
+    u = !1;
 }
-function _(e) {
+function p(e) {
     let { products: t, storeFront: n } = e;
-    (s = t), (l = n);
+    (l = t), (c = n);
 }
-function f(e) {
+function h(e) {
     let { storeFront: t } = e;
-    l = t;
+    c = t;
 }
-class p extends r.Ay.Store {
+class m extends s.Ay.Store {
     static displayName = "GenericIAPStore";
     isGenericIapConnected() {
-        return o;
+        return u;
     }
-    hasGenericSubscription() {
-        return null != s && s.some((e) => e.identifier === a.Yq.GENERIC_SUBSCRIPTION);
+    genericProductsLoaded() {
+        return null != l && i().filter(l, (e) => o.lI.includes(e.identifier)).length === o.lI.length;
     }
     getProducts() {
-        return s;
-    }
-    getStoreFront() {
         return l;
     }
+    getStoreFront() {
+        return c;
+    }
 }
-let h = new p(i.h, {
-    IAP_LOAD_GENERIC_PRODUCTS: _,
-    GENERIC_IAP_INIT_CONNECTION: u,
-    GENERIC_IAP_INIT_CONNECTION_FAILED: c,
-    GENERIC_IAP_END_CONNECTION: d,
-    GENERIC_IAP_SET_STORE_FRONT: f,
+let E = new m(a.h, {
+    IAP_LOAD_GENERIC_PRODUCTS: p,
+    GENERIC_IAP_INIT_CONNECTION: d,
+    GENERIC_IAP_INIT_CONNECTION_FAILED: _,
+    GENERIC_IAP_END_CONNECTION: f,
+    GENERIC_IAP_SET_STORE_FRONT: h,
 });
