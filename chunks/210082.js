d1393970c74814529eb30324cac1e98fb55150b9
DiscrapperManager <admin@minerpl.xyz>
2026-03-11 19:10:29 +0100

    canaryRelease: 509368
    Main scripts: 3
    Lazy Loaded Scripts: 2010
    Chunks:
     * Main Files: 13332
     * Scripts: 12480
    Strings: 23975
    Routes: 803
    Experiments Count: 147
    Apex Experiments Count: 192

--- chunks/210082.js
+++ chunks/210082.js
@@ -1,28 +1,33 @@
 "use strict";
-n.d(t, { A: () => d });
+n.d(t, { A: () => u });
 var i = n(311907),
-    s = n(734057),
-    l = n(576705),
+    s = n(863005),
+    l = n(734057),
+    a = n(576705),
     r = n(222823),
-    a = n(543465),
-    o = n(661191),
-    c = n(818348);
-function d(e) {
-    return (0, i.cf)([s.A, r.Ay, a.Ay, l.A], () =>
-        o.default.keys(e).reduce(
+    o = n(543465),
+    c = n(661191),
+    d = n(818348);
+function u(e) {
+    return (0, i.cf)([s.A, l.A, r.Ay, o.Ay, a.A], () =>
+        c.default.keys(e).reduce(
             (e, t) => {
-                let n = s.A.getChannel(t),
-                    i = n?.isGuildVocal();
-                return (
-                    (e.badge = e.badge + r.Ay.getMentionCount(t)),
+                let n = l.A.getChannel(t),
+                    i = n?.getGuildId(),
+                    u = n?.isGuildVocal();
+                if (
+                    ((e.badge = e.badge + r.Ay.getMentionCount(t)),
                     (e.unread =
                         e.unread ||
-                        (!i &&
-                            l.A.can(c.xB.VIEW_CHANNEL, n) &&
-                            r.Ay.hasUnread(t) &&
-                            !a.Ay.isChannelMuted(n?.getGuildId(), t))),
-                    e
-                );
+                        (!u && a.A.can(d.xB.VIEW_CHANNEL, n) && r.Ay.hasUnread(t) && !o.Ay.isChannelMuted(i, t))),
+                    null != i)
+                ) {
+                    let n = s.A.getActiveJoinedRelevantThreadsForParent(i, t),
+                        l = s.A.getActiveJoinedUnreadThreadsForParent(i, t);
+                    for (let t in ((e.unread = e.unread || c.default.keys(l).some((e) => e in n)), n))
+                        e.badge += r.Ay.getMentionCount(t);
+                }
+                return e;
             },
             { badge: 0, unread: !1 },
         ),
