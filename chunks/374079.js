faca8b940efb476167e779446308a01ce48986a9
DiscrapperManager <admin@minerpl.xyz>
2026-03-11 23:50:29 +0100

    canaryRelease: 509753
    Main scripts: 3
    Lazy Loaded Scripts: 2041
    Chunks:
     * Main Files: 13328
     * Scripts: 12511
    Strings: 23981
    Routes: 803
    Experiments Count: 144
    Apex Experiments Count: 191

--- chunks/374079.js
+++ chunks/374079.js
@@ -12,8 +12,8 @@ var i = n(627968),
     h = n(532794),
     _ = n(295140),
     m = n(788868),
-    g = n(652215),
-    p = n(49999),
+    p = n(652215),
+    g = n(49999),
     E = n(49370),
     I = n(985018),
     f = n(32743);
@@ -21,11 +21,11 @@ function C(e) {
     let { targetElementRef: t, dismissPopover: n, children: C, numOrbs: T } = e,
         { analyticsLocations: N } = (0, A.Ay)(u.A.PREMIUM_MARKETING_NITRO_HOME_ORBS_SECTION),
         S = r.useCallback(() => {
-            n(p.i.TAKE_ACTION),
+            n(g.i.TAKE_ACTION),
                 (0, h.A)({
                     subscriptionTier: m.pe.TIER_2,
                     analyticsLocations: N,
-                    analyticsObject: { object: g.ZSU.BUTTON_CTA, objectType: g.AnalyticsObjectTypes.TIER_2 },
+                    analyticsObject: { object: p.ZSU.BUTTON_CTA, objectType: p.AnalyticsObjectTypes.TIER_2 },
                 });
         }, [n, N]);
     return (0, i.jsxs)(i.Fragment, {
@@ -33,7 +33,7 @@ function C(e) {
             (0, i.jsx)("div", { children: C }),
             (0, i.jsx)(l.x, {
                 targetElementRef: t,
-                onRequestClose: () => n(p.i.USER_DISMISS),
+                onRequestClose: () => n(g.i.USER_DISMISS),
                 alignmentStrategy: "edge",
                 align: "top",
                 position: "right",
@@ -42,18 +42,16 @@ function C(e) {
                 children: (0, i.jsxs)("div", {
                     "data-mana-component": "popover",
                     children: [
-                        (0, i.jsx)(o.q, { onClick: () => n(p.i.USER_DISMISS), variant: "color-mix" }),
+                        (0, i.jsx)(o.q, { onClick: () => n(g.i.USER_DISMISS), variant: "color-mix" }),
                         (0, i.jsx)("div", { className: f.O, children: (0, i.jsx)(_.A, { orbsCount: T }) }),
                         (0, i.jsx)(d.D, {
                             title: I.intl.string(E.default.hx5AFp),
-                            body: (0, i.jsx)("span", { children: I.intl.format(E.default.otQEii, { orbsCount: T }) }),
+                            body: I.intl.formatToPlainString(E.default.otQEii, { orbsCount: T }),
                             badge: { type: "beta", variant: "expressive" },
                             className: f.Q,
                         }),
                         (0, i.jsx)(a.Z, {
-                            actions: [
-                                { text: I.intl.string(I.t.pj0XBN), variant: "expressive", icon: c.tvc, onClick: S },
-                            ],
+                            actions: [{ text: I.intl.string(I.t.pj0XBN), variant: "primary", icon: c.tvc, onClick: S }],
                         }),
                         (0, i.jsx)(s.F, {}),
                     ],
