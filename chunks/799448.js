d74d36e488637371e7937c705835782d902c31bf
DiscrapperManager <admin@minerpl.xyz>
2026-03-11 22:01:49 +0100

    canaryRelease: 509595
    Main scripts: 3
    Lazy Loaded Scripts: 2041
    Chunks:
     * Main Files: 13328
     * Scripts: 12508
    Strings: 23975
    Routes: 803
    Experiments Count: 145
    Apex Experiments Count: 191

--- chunks/799448.js
+++ chunks/799448.js
@@ -1,17 +1,18 @@
 "use strict";
-n.d(t, { E: () => f });
+n.d(t, { E: () => p });
 var r = n(735438),
     i = n.n(r),
     s = n(562465),
     a = n(451988),
     o = n(73153),
-    l = n(652215);
-let u = 20;
-async function c(e) {
+    l = n(136086),
+    u = n(652215);
+let c = 20;
+async function d(e) {
     try {
         let t = (
             await s.Bo.get({
-                url: l.Rsh.GAMES,
+                url: u.Rsh.GAMES,
                 query: { game_ids: e, with_supplemental_data: !0 },
                 rejectWithError: !0,
             })
@@ -21,17 +22,15 @@ async function c(e) {
         o.h.dispatch({ type: "GAME_FETCH_FAILURE", gameIds: e });
     }
 }
-class d {
+class _ {
     _promises = new Set();
     _pending = new Set();
     _flushHandler = new a.J_(32, () => this._flush());
     request(e) {
-        return (
-            e.forEach((e) => this._pending.add(e)),
-            new Promise((e) => {
-                this._promises.add({ resolve: e }), this._flushHandler.delay(!1);
-            })
-        );
+        for (let t of e) l.A.hasNoData(t) || this._pending.add(t);
+        return new Promise((e) => {
+            this._promises.add({ resolve: e }), this._flushHandler.delay(!1);
+        });
     }
     async _flush() {
         let e = [...this._pending];
@@ -39,14 +38,14 @@ class d {
         let t = [...this._promises];
         this._promises.clear(),
             0 === e.length ||
-                (o.h.dispatch({ type: "GAME_FETCH", gameIds: e }), await Promise.all(i().chunk(e, u).map(c))),
+                (o.h.dispatch({ type: "GAME_FETCH", gameIds: e }), await Promise.all(i().chunk(e, c).map(d))),
             this.resolvePromises(t);
     }
     resolvePromises(e) {
         e.forEach((e) => e.resolve());
     }
 }
-let _ = new d();
-async function f(e) {
-    0 !== e.length && (await _.request(e));
+let f = new _();
+async function p(e) {
+    0 !== e.length && (await f.request(e));
 }
