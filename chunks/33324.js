f48ebc7166fa0a9dba076d87f3c1c228906898c0
DiscrapperManager <admin@minerpl.xyz>
2026-03-11 19:12:34 +0100

    canaryRelease: 509371
    Main scripts: 3
    Lazy Loaded Scripts: 2010
    Chunks:
     * Main Files: 13332
     * Scripts: 12479
    Strings: 23975
    Routes: 803
    Experiments Count: 147
    Apex Experiments Count: 191

--- chunks/33324.js
+++ chunks/33324.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { A: () => N }), n(321073);
+n.d(t, { A: () => I }), n(321073);
 var i = n(417597),
     s = n(554146),
     l = n(41200),
@@ -15,48 +15,46 @@ var i = n(417597),
     p = n(18905),
     g = n(488803),
     _ = n(969117),
-    f = n(385160),
-    x = n(363487),
-    C = n(871123),
-    E = n(281405),
-    I = n(652215);
-function N(e) {
+    f = n(363487),
+    x = n(871123),
+    C = n(281405),
+    E = n(652215);
+function I(e) {
     let t = (0, p.A)(e.id),
         n = (0, o.W)(e.id),
-        N = (0, m.vz)(e.id),
-        b = (0, l.r)(e),
-        S = (0, C.jz)(e),
-        T = (0, h.d)(e.id),
-        v = (0, i.bG)([d.h], () => d.h.getNewMemberActions(e.id), [e.id]),
-        y = (0, A.A)(e.id),
-        j = (0, u.jY)(e.id),
-        R = (0, c.fw)(e.id),
-        O = [],
-        L = e.features.has(I.GuildFeatures.HUB),
-        M = e.features.has(I.GuildFeatures.COMMUNITY),
-        D = e.features.has(I.GuildFeatures.ENABLED_MODERATION_EXPERIENCE_FOR_NON_COMMUNITY),
-        G = (0, x.A)(e.id);
-    (0, f.Ao)("useGuildActionRows");
-    let U = (0, g.C$)(e.id, "useGuildActionRows"),
-        P = e.features.has(I.GuildFeatures.GAME_SERVERS),
-        w = (0, _.N)("useGuildActionRows"),
-        [k] = (0, r.kn)(U && w && !P ? [s.M.EMPTY_GAME_SERVER_TAB] : [], void 0, !0),
-        V = (0, a.ws)(e, { location: "guild-action-rows" });
+        I = (0, m.vz)(e.id),
+        N = (0, l.r)(e),
+        b = (0, x.jz)(e),
+        S = (0, h.d)(e.id),
+        T = (0, i.bG)([d.h], () => d.h.getNewMemberActions(e.id), [e.id]),
+        v = (0, A.A)(e.id),
+        y = (0, u.jY)(e.id),
+        j = (0, c.fw)(e.id),
+        R = [],
+        O = e.features.has(E.GuildFeatures.HUB),
+        L = e.features.has(E.GuildFeatures.COMMUNITY),
+        M = e.features.has(E.GuildFeatures.ENABLED_MODERATION_EXPERIENCE_FOR_NON_COMMUNITY),
+        D = (0, f.A)(e.id),
+        G = (0, g.C$)(e.id, "useGuildActionRows"),
+        U = e.features.has(E.GuildFeatures.GAME_SERVERS),
+        P = (0, _.N)("useGuildActionRows"),
+        [w] = (0, r.kn)(G && P && !U ? [s.M.EMPTY_GAME_SERVER_TAB] : [], void 0, !0),
+        k = (0, a.ws)(e, { location: "guild-action-rows" });
     return (
-        L && O.push(E.n.GUILD_HUB_HEADER_OPTIONS),
-        !j && T && y && null != v && v.length > 0
-            ? O.push(E.n.GUILD_NEW_MEMBER_ACTIONS_PROGRESS_BAR)
-            : V && O.push(E.n.GUILD_PREMIUM_PROGRESS_BAR),
-        !L && T && O.push(E.n.GUILD_HOME),
-        t && O.push(E.n.GUILD_SCHEDULED_EVENTS),
-        !L && M && O.push(E.n.CHANNELS_AND_ROLES),
-        N && O.push(E.n.GUILD_ROLE_SUBSCRIPTIONS),
-        b && O.push(E.n.GUILD_SHOP),
-        S && O.push(E.n.GUILD_GAME_SHOP),
-        ((R && (M || D)) || (n && e.features.has(I.GuildFeatures.MEMBER_VERIFICATION_MANUAL_APPROVAL))) &&
-            O.push(E.n.GUILD_MOD_DASH_MEMBER_SAFETY),
-        G && O.push(E.n.GUILD_BOOSTS),
-        U && (P ? O.push(E.n.GAME_SERVERS) : null != k && O.push(E.n.GAME_SERVERS_EMPTY)),
-        O
+        O && R.push(C.n.GUILD_HUB_HEADER_OPTIONS),
+        !y && S && v && null != T && T.length > 0
+            ? R.push(C.n.GUILD_NEW_MEMBER_ACTIONS_PROGRESS_BAR)
+            : k && R.push(C.n.GUILD_PREMIUM_PROGRESS_BAR),
+        !O && S && R.push(C.n.GUILD_HOME),
+        t && R.push(C.n.GUILD_SCHEDULED_EVENTS),
+        !O && L && R.push(C.n.CHANNELS_AND_ROLES),
+        I && R.push(C.n.GUILD_ROLE_SUBSCRIPTIONS),
+        N && R.push(C.n.GUILD_SHOP),
+        b && R.push(C.n.GUILD_GAME_SHOP),
+        ((j && (L || M)) || (n && e.features.has(E.GuildFeatures.MEMBER_VERIFICATION_MANUAL_APPROVAL))) &&
+            R.push(C.n.GUILD_MOD_DASH_MEMBER_SAFETY),
+        D && R.push(C.n.GUILD_BOOSTS),
+        G && (U ? R.push(C.n.GAME_SERVERS) : null != w && R.push(C.n.GAME_SERVERS_EMPTY)),
+        R
     );
 }
