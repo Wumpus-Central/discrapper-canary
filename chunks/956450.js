f91b22140f382d6755ff22d6b15ed495ee451e86
DiscrapperManager <admin@minerpl.xyz>
2026-03-12 22:30:57 +0100

    canaryRelease: 510453
    Main scripts: 3
    Lazy Loaded Scripts: 2039
    Chunks:
     * Main Files: 13326
     * Scripts: 12528
    Strings: 23993
    Routes: 803
    Experiments Count: 139
    Apex Experiments Count: 196

--- chunks/956450.js
+++ chunks/956450.js
@@ -1,40 +1,43 @@
-I.d(_, { default: () => t });
-var A = I(627968);
-I(64700);
-var S = I(397927),
-    T = I(780964),
-    C = I(840065),
-    O = I(257110),
-    N = I(706069),
-    R = I(652215),
-    e = I(531525),
-    P = I(524988);
-let t = (E) => {
-    let { transitionState: _, onClose: I, classificationId: t, source: L } = E,
-        D = (0, O.A)(),
-        i = () => {
-            I(),
-                (0, C.openUserSettings)(T.X.ACCOUNT_PANEL, {
-                    section: R.nc_.ACCOUNT,
-                    subsection: e.H.PRIVACY_AND_SAFETY_STANDING,
-                });
+E.d(A, { default: () => a });
+var e = E(627968);
+E(64700);
+var I = E(397927),
+    T = E(780964),
+    C = E(840065),
+    t = E(393033),
+    S = E(257110),
+    i = E(706069),
+    n = E(652215),
+    N = E(531525),
+    R = E(265626);
+let a = (_) => {
+    let { transitionState: A, onClose: E, classificationId: a, source: l } = _,
+        O = (0, S.A)(),
+        s = (0, t.W$)(),
+        o = () => {
+            E(),
+                s ||
+                    (0, C.openUserSettings)(T.X.ACCOUNT_PANEL, {
+                        section: n.nc_.ACCOUNT,
+                        subsection: N.H.PRIVACY_AND_SAFETY_STANDING,
+                    });
         };
-    return (0, A.jsxs)(S.EOs, {
-        className: P.CR,
-        transitionState: _,
+    return (0, e.jsxs)(I.EOs, {
+        className: R.CR,
+        transitionState: A,
         parentComponent: "ClassificationDetailModal",
         "data-migration-pending": !0,
         children: [
-            (0, A.jsx)("div", {
-                className: P.Hc,
-                children: (0, A.jsx)(S.s_y, { "data-migration-pending": !0, className: P.g8, onClick: I }),
+            (0, e.jsx)("div", {
+                className: R.Hc,
+                children: (0, e.jsx)(I.s_y, { "data-migration-pending": !0, className: R.g8, onClick: E }),
             }),
-            (0, A.jsx)(S.$mQ, {
+            (0, e.jsx)(I.$mQ, {
                 "data-migration-pending": !0,
-                className: P.jE,
-                children: D
-                    ? (0, A.jsx)(S.y$y, {})
-                    : (0, A.jsx)(N.A, { classificationId: t, source: L, onError: i, onClose: i }),
+                className: R.jE,
+                children: O
+                    ? (0, e.jsx)(I.y$y, {})
+                    : (0, e.jsx)(i.A, { classificationId: a, source: l, onError: o, onClose: o }),
             }),
         ],
     });
