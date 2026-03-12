8c602b090be9b498172e31d6800befda4c614170
DiscrapperManager <admin@minerpl.xyz>
2026-03-12 16:51:40 +0100

    canaryRelease: 510036
    Main scripts: 3
    Lazy Loaded Scripts: 2039
    Chunks:
     * Main Files: 13328
     * Scripts: 12511
    Strings: 23985
    Routes: 803
    Experiments Count: 142
    Apex Experiments Count: 194

--- chunks/673277.js
+++ chunks/673277.js
@@ -1,7 +1,10 @@
-t.d(r, { A: () => l });
-var d = t(627968);
+t.d(l, { A: () => n });
+var r = t(627968);
 t(64700);
-let l = (e) => {
-    let { verticalMargin: r = 16, padding: t = 0, children: l } = e;
-    return (0, d.jsx)("section", { style: { marginTop: r, marginBottom: r, padding: t }, children: l });
+let n = (e) => {
+    let { verticalMargin: l = 16, padding: t = 0, children: n } = e;
+    return (0, r.jsx)("section", {
+        style: { alignSelf: "start", marginTop: l, marginBottom: l, padding: t },
+        children: n,
+    });
 };
