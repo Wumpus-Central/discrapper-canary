466ec6611928607e44ebc1304dd79ba3206f7ca5
DiscrapperManager <admin@minerpl.xyz>
2026-03-12 02:17:06 +0100

    canaryRelease: 509901
    Main scripts: 3
    Lazy Loaded Scripts: 2039
    Chunks:
     * Main Files: 13327
     * Scripts: 12510
    Strings: 23984
    Routes: 803
    Experiments Count: 142
    Apex Experiments Count: 193

--- chunks/306751.js
+++
@@ -1,4 +0,0 @@
-t.r(s), t.d(s, { default: () => i });
-let i = JSON.parse(
-    '{"yn/63q":["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod. ",[8,"$link",["Terms apply."],[[1,"termsLink"]]]],"1tMD15":["Lorem ipsum dolor sit amet, consectetur"]}',
-);
