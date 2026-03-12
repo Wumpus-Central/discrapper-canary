391f752954c6686f3762188056d65034ac54e170
DiscrapperManager <admin@minerpl.xyz>
2026-03-12 17:13:15 +0100

    canaryRelease: 510048
    Main scripts: 3
    Lazy Loaded Scripts: 2039
    Chunks:
     * Main Files: 13327
     * Scripts: 12511
    Strings: 23985
    Routes: 803
    Experiments Count: 141
    Apex Experiments Count: 194

--- chunks/719129.js
+++ chunks/719129.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { GH: () => I, Zi: () => h, c1: () => T, r6: () => S });
+n.d(t, { GH: () => A, Zi: () => p, c1: () => I, r6: () => T });
 var r = n(73153),
     i = n(386406),
     s = n(56562),
@@ -9,54 +9,50 @@ var r = n(73153),
     u = n(954571),
     c = n(723702),
     d = n(837921),
-    _ = n(241696),
-    f = n(652215);
-let p = { development: [0, 0, 0, 0], canary: [1, 0, 30, 10], ptb: [1, 0, 1005, 2], stable: [1, 0, 9001, 2] };
-function h() {
-    return !d.Ay?.isModuleVersionAtLeast?.("discord_hook", p);
+    _ = n(652215);
+let f = { development: [0, 0, 0, 0], canary: [1, 0, 30, 10], ptb: [1, 0, 1005, 2], stable: [1, 0, 9001, 2] };
+function p() {
+    return !d.Ay?.isModuleVersionAtLeast?.("discord_hook", f);
 }
-let m = null;
-async function E() {
+let h = null;
+async function m() {
     if (!(0, c.isWindows)()) return Promise.reject(Error("Hook is only available on Windows"));
-    if (h()) return Promise.reject(Error("Hook module is too old"));
+    if (p()) return Promise.reject(Error("Hook module is too old"));
     await d.Ay.ensureModule("discord_hook");
     let e = await d.Ay.requireModule("discord_hook");
-    return A(e), e;
+    return g(e), e;
 }
-async function g() {
-    return null != m ? m : (m = await E());
+async function E() {
+    return null != h ? h : (h = await m());
 }
-function A(e) {
+function g(e) {
     if (null == e.setFlags) return;
     let t = 0,
-        n = _.A.getCurrentConfig({ location: "edd7d3_1" }, { autoTrackExposure: !1 });
-    n.enableCrashReporting && (console.log("Hook: Enabling crash reporting."), (t |= 1));
-    let r = l.default.getCurrentUser();
-    null != r && (r.isStaff() || n.enableCrashTrigger) && (console.log("Hook: Enabling crash trigger."), (t |= 2)),
-        e.setFlags(t);
+        n = l.default.getCurrentUser();
+    null != n && n.isStaff() && (console.log("Hook: Enabling crash trigger."), (t |= 2)), e.setFlags(t);
 }
-function I(e, t) {
-    return E().then((n) => {
+function A(e, t) {
+    return m().then((n) => {
         let l = a.Ay.getGameForPID(e),
             c = l?.name,
             d = null != l ? o.A.findGame(l) : null,
-            _ = null;
+            f = null;
         return new Promise((o) => {
             let l = (e, n) => {
-                    u.default.track(f.HAw.HOOK_RESULT, {
+                    u.default.track(_.HAw.HOOK_RESULT, {
                         game_name: c,
                         game_id: null == d ? null : d.id,
                         success: n,
                         error: e,
                         ...t,
                     }),
-                        null != _ && (clearTimeout(_), (_ = null)),
+                        null != f && (clearTimeout(f), (f = null)),
                         n ? o() : o((e = e ?? "Unknown hook error"));
                 },
                 p = a.Ay.getOverlayOptionsForPID(e),
                 h = { ...s.gH, ...p, elevate: a.Ay.shouldElevateProcessForPID(e) };
             null == h.allowHook || h.allowHook
-                ? ((_ = setTimeout(() => {
+                ? ((f = setTimeout(() => {
                       n.cancelAttachToProcess(e), l("Timed out waiting for hook response", !1);
                   }, 12e4)),
                   n.attachToProcess(e, h, l),
@@ -65,13 +61,13 @@ function I(e, t) {
         });
     });
 }
-function T(e) {
-    return E().then((t) => {
+function I(e) {
+    return m().then((t) => {
         t.cancelAttachToProcess(e);
     });
 }
-function S() {
-    return g()
+function T() {
+    return E()
         .then((e) => (null != e.findSteamProcess ? e.findSteamProcess() : null))
         .catch(() => null);
 }
