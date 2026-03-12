3a913f61ad3c214c83763d9b89ce68a00924cdca
DiscrapperManager <admin@minerpl.xyz>
2026-03-13 00:19:02 +0100

    canaryRelease: 510591
    Main scripts: 3
    Lazy Loaded Scripts: 2039
    Chunks:
     * Main Files: 13325
     * Scripts: 12529
    Strings: 23994
    Routes: 803
    Experiments Count: 137
    Apex Experiments Count: 197

--- chunks/835072.js
+++ chunks/835072.js
@@ -1,56 +1,54 @@
 "use strict";
-n.d(t, { A: () => f, f: () => _ });
+n.d(t, { A: () => _, f: () => d });
 var r = n(627968);
 n(64700);
 var i = n(934551),
-    a = n(765379),
-    s = n(82149),
-    o = n(412242),
-    l = n(566903),
-    u = n(864436),
-    c = n(200041),
-    d = n(652215);
-let _ = function (e) {
+    s = n(765379),
+    a = n(82149),
+    o = n(566903),
+    l = n(864436),
+    u = n(200041),
+    c = n(652215);
+let d = function (e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
-    return (0, s.Cy)(e)
+    return (0, a.Cy)(e)
         ? i.StageIcon
-        : (0, a.A)(e)
+        : (0, s.A)(e)
           ? t
               ? i.GameControllerIcon
               : i.AppsIcon
-          : e.type === d.$pd.PLAYING
+          : e.type === c.$pd.PLAYING
             ? i.GameControllerIcon
-            : e.type === d.$pd.LISTENING
+            : e.type === c.$pd.LISTENING
               ? i.MusicIcon
-              : e.type === d.$pd.WATCHING || e.type === d.$pd.STREAMING
+              : e.type === c.$pd.WATCHING || e.type === c.$pd.STREAMING
                 ? i.TvIcon
-                : e.type === d.$pd.COMPETING
+                : e.type === c.$pd.COMPETING
                   ? i.GameControllerIcon
                   : null;
 };
-function f(e) {
+function _(e) {
     let {
             activity: t,
             textVariant: n,
             textClassName: i,
-            iconClassName: a,
-            hideIcon: s = !1,
-            hideText: d = !1,
-            hideTooltip: f = !1,
-            canTruncate: p = !0,
+            iconClassName: s,
+            hideIcon: a = !1,
+            hideText: c = !1,
+            hideTooltip: _ = !1,
+            canTruncate: f = !0,
         } = e,
-        { descriptiveTextEnabled: h } = (0, o.n)({ location: "PresenceActivityStatusWithCombinedTooltip" }),
-        { text: m, tooltip: g } = (0, l.A)(t, h),
-        E = _(t),
-        A = null != E && !s;
-    return (0, r.jsx)(c.A, {
-        icon: A ? (0, r.jsx)(u.A, { icon: E, className: a }) : void 0,
-        text: m ?? "",
+        { text: p, tooltip: h } = (0, o.A)(t, !0),
+        m = d(t),
+        E = null != m && !a;
+    return (0, r.jsx)(u.A, {
+        icon: E ? (0, r.jsx)(l.A, { icon: m, className: s }) : void 0,
+        text: p ?? "",
         textVariant: n,
         textClassName: i,
-        hideTooltip: f,
-        canTruncate: p,
-        "aria-label": g ?? "",
-        hideText: d,
+        hideTooltip: _,
+        canTruncate: f,
+        "aria-label": h ?? "",
+        hideText: c,
     });
 }
