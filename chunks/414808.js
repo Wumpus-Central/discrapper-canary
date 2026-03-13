56ecf8a3d5a10dc4b4dce64fe9bf3ec360ce5cad
DiscrapperManager <admin@minerpl.xyz>
2026-03-13 15:37:58 +0100

    canaryRelease: 510769
    Main scripts: 3
    Lazy Loaded Scripts: 2072
    Chunks:
     * Main Files: 13326
     * Scripts: 12575
    Strings: 24008
    Routes: 803
    Experiments Count: 138
    Apex Experiments Count: 198

--- chunks/414808.js
+++ chunks/414808.js
@@ -1,4 +1,4 @@
-n.d(t, { A: () => x });
+n.d(t, { A: () => E });
 var l = n(627968),
     s = n(64700),
     a = n(311907),
@@ -11,23 +11,23 @@ var l = n(627968),
     m = n(93055),
     A = n(49999),
     p = n(788868),
-    f = n(665606),
+    f = n(525736),
     h = n(985018),
-    E = n(988572);
-function x(e) {
+    x = n(988572);
+function E(e) {
     let { targetElementRef: t, markMenuItemPopoverAsDismissed: n } = e,
-        [x, N] = s.useState("top"),
+        [E, N] = s.useState("top"),
         g = s.useRef(null),
-        T = s.useRef(null),
-        { hasAccess: v } = (0, m.TW)("FavoritesOnboardingMenuItemPopover"),
-        { analyticsLocations: S } = (0, u.Ay)(o.A.FAVORITES_GUILD_MENU_ITEM),
+        v = s.useRef(null),
+        { hasAccess: S } = (0, m.TW)("FavoritesOnboardingMenuItemPopover"),
+        { analyticsLocations: T } = (0, u.Ay)(o.A.FAVORITES_GUILD_MENU_ITEM),
         C = (0, a.bG)([d.A], () => d.A.hasStoredFavorites());
     s.useLayoutEffect(() => {
         if (null == t.current) return;
         let e = () => {
-            null == T.current &&
-                (T.current = window.requestAnimationFrame(() => {
-                    (T.current = null),
+            null == v.current &&
+                (v.current = window.requestAnimationFrame(() => {
+                    (v.current = null),
                         (() => {
                             let e = t.current?.getBoundingClientRect();
                             if (null == e) return;
@@ -41,35 +41,35 @@ function x(e) {
             window.addEventListener("resize", e),
             window.addEventListener("scroll", e, !0),
             () => {
-                null != T.current && (window.cancelAnimationFrame(T.current), (T.current = null)),
+                null != v.current && (window.cancelAnimationFrame(v.current), (v.current = null)),
                     window.removeEventListener("resize", e),
                     window.removeEventListener("scroll", e, !0);
             }
         );
     }, [t]);
     let _ = s.useCallback(() => {
-            n?.(A.i.TAKE_ACTION), v || (0, c.A)({ subscriptionTier: p.pe.TIER_2, analyticsLocations: S });
-        }, [n, S, v]),
+            n?.(A.i.TAKE_ACTION), S || (0, c.A)({ subscriptionTier: p.pe.TIER_2, analyticsLocations: T });
+        }, [n, T, S]),
         j = C ? f.default.TWuDTt : f.default["25YCHl"];
     return (0, l.jsx)(i.AM, {
         targetElementRef: t,
-        gradientColor: "purple",
+        gradientColor: "nitro-pink",
         position: "right",
         alignmentStrategy: "edge",
-        align: x,
-        caretConfig: { align: "top" === x ? "start" : "end" },
+        align: E,
+        caretConfig: { align: "top" === E ? "start" : "end" },
         popoverRef: g,
         badge: { type: "beta", variant: "expressive" },
-        graphic: { src: E.A, type: "image", aspectRatio: "16/9" },
-        title: h.intl.string(v ? j : f.default.A3ZjbM),
-        body: h.intl.string(v ? f.default.DFjrWw : f.default.qJUI2v),
+        graphic: { src: x.A, type: "image", aspectRatio: "16/9" },
+        title: h.intl.string(S ? j : f.default.A3ZjbM),
+        body: h.intl.string(S ? f.default.DFjrWw : f.default.qJUI2v),
         onRequestClose: () => n?.(A.i.USER_DISMISS),
         actions: [
             {
-                icon: v ? void 0 : r.tvc,
-                text: v ? h.intl.string(f.default["+h9aza"]) : h.intl.string(f.default["avps/2"]),
+                icon: S ? void 0 : r.tvc,
+                text: S ? h.intl.string(f.default["+h9aza"]) : h.intl.string(f.default["avps/2"]),
                 onClick: () => _(),
-                variant: v ? "secondary" : "expressive",
+                variant: S ? "secondary" : "expressive",
             },
         ],
     });
--- chunks/730253.js
+++ chunks/730253.js
@@ -3,8 +3,8 @@ n.d(t, { A: () => B }), n(321073);
 var i = n(627968),
     s = n(64700),
     l = n(837381),
-    r = n(311907),
-    a = n(342494),
+    a = n(311907),
+    r = n(342494),
     o = n(397927),
     c = n(308528),
     d = n(933958),
@@ -56,34 +56,34 @@ function B(e) {
         { favoriteAdded: et, clearFavoriteAdded: en } = (0, y.CJ)(),
         ei = s.useRef(null),
         { analyticsLocations: es } = (0, A.Ay)(h.A.FAVORITES_GUILD_BUTTON),
-        [el, er] = s.useState(!1),
-        ea = (0, R.$)("favorite-server-context"),
+        [el, ea] = s.useState(!1),
+        er = (0, R.$)("favorite-server-context"),
         {
             favoriteGuildEnabled: eo,
             favoriteGuildMuted: ec,
             favoriteChannels: ed,
-        } = (0, r.cf)([T.A], () => ({
+        } = (0, a.cf)([T.A], () => ({
             favoriteGuildEnabled: T.A.favoriteGuildEnabled,
             favoriteChannels: T.A.getFavoriteChannels(),
             favoriteGuildMuted: T.A.favoriteGuildMuted,
         })),
-        eu = (0, r.bG)([N.A], () => N.A.getChannelId(L.YYv)),
-        eh = (0, r.bG)([I.A], () => I.A.getChannel(eu)),
+        eu = (0, a.bG)([N.A], () => N.A.getChannelId(L.YYv)),
+        eh = (0, a.bG)([I.A], () => I.A.getChannel(eu)),
         eA = (0, x.A)((e) => e.guildId) === L.YYv,
         { badge: em, unread: ep } = (0, j.A)(ed),
         eg =
-            ((B = null != (t = (0, r.bG)([N.A], () => N.A.getVoiceChannelId())) && null != ed[t]),
-            (H = (0, r.bG)([E.A], () => {
+            ((B = null != (t = (0, a.bG)([N.A], () => N.A.getVoiceChannelId())) && null != ed[t]),
+            (H = (0, a.bG)([E.A], () => {
                 if (!B) return !1;
                 let e = E.A.getCurrentUserActiveStream();
                 return null != e && null != ed[e.channelId];
             })),
-            (F = (0, r.bG)([E.A], () => E.A.getAllApplicationStreams().some((e) => null != ed[e.channelId]))),
-            (K = (0, r.bG)([b.A], () => B && null != t && b.A.hasVideo(t), [B, t])),
-            (W = (0, r.yK)([d.Ay], () =>
+            (F = (0, a.bG)([E.A], () => E.A.getAllApplicationStreams().some((e) => null != ed[e.channelId]))),
+            (K = (0, a.bG)([b.A], () => B && null != t && b.A.hasVideo(t), [B, t])),
+            (W = (0, a.yK)([d.Ay], () =>
                 S.default.keys(ed).reduce((e, t) => (e.push(...d.Ay.getEmbeddedActivitiesForChannel(t)), e), []),
             )),
-            (Y = (0, r.bG)([d.Ay], () =>
+            (Y = (0, a.bG)([d.Ay], () =>
                 Array.from(d.Ay.getSelfEmbeddedActivities().values()).some((e) => {
                     let { location: t } = e,
                         n = (0, u.H)(t);
@@ -165,12 +165,12 @@ function B(e) {
                                 onClick: () => {
                                     $ && eE(!1);
                                 },
-                                onMouseEnter: () => er(!0),
-                                onMouseLeave: () => er(!1),
+                                onMouseEnter: () => ea(!0),
+                                onMouseLeave: () => ea(!1),
                                 onMouseDown: function () {
                                     null != eh && c.A.preload(eh.guild_id, eh.id);
                                 },
-                                onContextMenu: ea,
+                                onContextMenu: er,
                                 children: (0, i.jsx)(o.Gg5, {
                                     size: "custom",
                                     color: "currentColor",
@@ -183,9 +183,9 @@ function B(e) {
                     }),
                 }),
                 $ &&
-                    (0, i.jsx)(a.AM, {
+                    (0, i.jsx)(r.AM, {
                         targetElementRef: ei,
-                        gradientColor: "purple",
+                        gradientColor: "nitro-pink",
                         position: "right",
                         alignmentStrategy: "edge",
                         align: "top",
