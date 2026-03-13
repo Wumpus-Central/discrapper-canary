5963db8010dc54689ad2da990ce75b7739a0fe63
DiscrapperManager <admin@minerpl.xyz>
2026-03-13 22:25:41 +0100

    canaryRelease: 511166
    Main scripts: 3
    Lazy Loaded Scripts: 2072
    Chunks:
     * Main Files: 13333
     * Scripts: 12577
    Strings: 24031
    Routes: 803
    Experiments Count: 134
    Apex Experiments Count: 200

--- chunks/534409.js
+++ chunks/534409.js
@@ -1,44 +1,23 @@
 "use strict";
-n.d(t, { k5: () => a, lV: () => s, qK: () => i });
-let r = (0, n(600975).C)({
+n.d(t, { k5: () => s, lV: () => a, qK: () => i });
+let r = (0, n(945810).mj)({
     kind: "user",
-    id: "2025-04_desktop_refresh_fast_follows",
-    label: "Desktop Refresh Fast Follows",
+    name: "2025-04-desktop-refresh-fast-follows",
     defaultConfig: { largerGuildIcons: !1, darkerGuildListBackground: !1, distinctBorders: !1 },
-    treatments: [
-        {
-            id: 1,
-            label: "Larger Guild Icons",
-            config: { largerGuildIcons: !0, darkerGuildListBackground: !1, distinctBorders: !1 },
-        },
-        {
-            id: 2,
-            label: "Darker Guild List Background",
-            config: { largerGuildIcons: !1, darkerGuildListBackground: !0, distinctBorders: !1 },
-        },
-        {
-            id: 3,
-            label: "Larger Guild Icons and Darker Guild List Background",
-            config: { largerGuildIcons: !0, darkerGuildListBackground: !0, distinctBorders: !1 },
-        },
-        {
-            id: 4,
-            label: "More Distinct Chassis Borders",
-            config: { largerGuildIcons: !1, darkerGuildListBackground: !1, distinctBorders: !0 },
-        },
-        {
-            id: 5,
-            label: "Larger Guild Icons and More Distinct Chassis Borders",
-            config: { largerGuildIcons: !0, darkerGuildListBackground: !1, distinctBorders: !0 },
-        },
-    ],
+    variations: {
+        1: { largerGuildIcons: !0, darkerGuildListBackground: !1, distinctBorders: !1 },
+        2: { largerGuildIcons: !1, darkerGuildListBackground: !0, distinctBorders: !1 },
+        3: { largerGuildIcons: !0, darkerGuildListBackground: !0, distinctBorders: !1 },
+        4: { largerGuildIcons: !1, darkerGuildListBackground: !1, distinctBorders: !0 },
+        5: { largerGuildIcons: !0, darkerGuildListBackground: !1, distinctBorders: !0 },
+    },
 });
 function i(e) {
     return !1;
 }
-function a(e) {
-    return r.useExperiment({ location: e }).darkerGuildListBackground;
-}
 function s(e) {
-    return r.useExperiment({ location: e }).distinctBorders;
+    return r.useConfig({ location: e }).darkerGuildListBackground;
+}
+function a(e) {
+    return r.useConfig({ location: e }).distinctBorders;
 }
