3227d039a8be8165ec9d32f5d49a2c3f9a2b04fd
DiscrapperManager <admin@minerpl.xyz>
2026-03-17 00:52:02 +0100

    canaryRelease: 511958
    Main scripts: 3
    Lazy Loaded Scripts: 2072
    Chunks:
     * Main Files: 13360
     * Scripts: 12560
    Strings: 24056
    Routes: 803
    Experiments Count: 126
    Apex Experiments Count: 201

--- chunks/41197.js
+++ chunks/41197.js
@@ -8,9 +8,10 @@ var i = n(419954),
     d = n(100549),
     c = n(827315),
     u = n(898560),
-    _ = n(985018);
+    _ = n(461616),
+    m = n(985018);
 let g = (0, i.zZ)(s.X.DISPLAY_MESSAGES_CATEGORY, {
-    useTitle: () => _.intl.string(_.t.OIgYlQ),
-    useSearchTerms: () => [_.intl.string(_.t["/VQax8"])],
-    buildLayout: () => [c.g, l.r, a.j, u.z, o.z, r.t, d.$],
+    useTitle: () => m.intl.string(m.t.OIgYlQ),
+    useSearchTerms: () => [m.intl.string(m.t["/VQax8"])],
+    buildLayout: () => [c.g, l.r, a.j, u.z, o.z, _.F, r.t, d.$],
 });
