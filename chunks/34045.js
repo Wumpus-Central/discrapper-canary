a85369a3dbe227b712505b162167f45a35ab57fa
DiscrapperManager <admin@minerpl.xyz>
2026-03-16 22:59:32 +0100

    canaryRelease: 511823
    Main scripts: 3
    Lazy Loaded Scripts: 2073
    Chunks:
     * Main Files: 13358
     * Scripts: 12563
    Strings: 24045
    Routes: 803
    Experiments Count: 129
    Apex Experiments Count: 199

--- chunks/34045.js
+++ chunks/34045.js
@@ -4,10 +4,6 @@ let r = {
     nitro_unsubbed: 100,
     checkpoint_completed: 10,
     quest_completed: 25,
-    warp_vpn_coachmark_dismissed_nitro: 10,
-    warp_vpn_coachmark_dismissed_non_nitro: 10,
-    warp_vpn_settings_bounce_nitro: 50,
-    warp_vpn_settings_bounce_non_nitro: 50,
     ad_impression_quest_bar_opt_in: 10,
     ad_impression_quest_bar_opt_out: 100,
 };
