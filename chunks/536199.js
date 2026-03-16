e1355060a10c8e726b332671719848efdc4ffa33
DiscrapperManager <admin@minerpl.xyz>
2026-03-16 17:32:37 +0100

    canaryRelease: 511457
    Main scripts: 3
    Lazy Loaded Scripts: 2072
    Chunks:
     * Main Files: 13341
     * Scripts: 12576
    Strings: 24038
    Routes: 803
    Experiments Count: 133
    Apex Experiments Count: 199

--- chunks/536199.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-n.d(t, { z: () => r });
-let r = (0, n(600975).C)({
-    kind: "user",
-    id: "2025-07_desktop_high_contrast_mode",
-    label: "Desktop High Contrast Mode",
-    defaultConfig: { enabled: !1 },
-    treatments: [{ id: 1, label: "Enabled", config: { enabled: !0 } }],
-});
