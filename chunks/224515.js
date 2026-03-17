bafcaddf68ce2ef4819120f0c685b54eb30cc776
DiscrapperManager <admin@minerpl.xyz>
2026-03-17 16:50:13 +0100

    canaryRelease: 512129
    Main scripts: 3
    Lazy Loaded Scripts: 2071
    Chunks:
     * Main Files: 13375
     * Scripts: 12560
    Strings: 24057
    Routes: 803
    Experiments Count: 126
    Apex Experiments Count: 203

--- chunks/224515.js
+++ chunks/224515.js
@@ -1,41 +1,49 @@
-n.d(t, { A: () => E });
+n.d(t, { A: () => C });
 var i = n(627968),
-    l = n(64700),
-    s = n(503698),
-    a = n.n(s),
+    s = n(64700),
+    l = n(503698),
+    a = n.n(l),
     r = n(311907),
     o = n(397927),
     d = n(73153),
     c = n(384904),
     u = n(923408),
-    h = n(73825),
-    A = n(160946),
-    g = n(859241),
-    m = n(295405),
-    p = n(166403),
-    _ = n(653183),
-    x = n(652215),
-    f = n(417711);
-function E() {
-    l.useEffect(() => {
+    _ = n(73825),
+    m = n(160946),
+    g = n(178929),
+    A = n(859241),
+    h = n(295405),
+    x = n(166403),
+    p = n(653183),
+    T = n(843838),
+    E = n(652215),
+    S = n(417711);
+function C() {
+    let e = (0, g.j)("UserSettingsPremiumGuildSubscriptions");
+    s.useEffect(() => {
         d.h.wait(() => {
-            c.hP(), c.$o(), (0, u.CD)(), (0, h.zS)(null, null, x.tF5.DISCOVERY), (0, u.tO)(!0);
+            c.hP(), c.$o(), (0, u.CD)(), (0, _.zS)(null, null, E.tF5.DISCOVERY), (0, u.tO)(!0);
         });
     }, []);
-    let { hasFetchedSubscriptions: e, premiumSubscription: t } = (0, r.cf)([p.A], () => ({
-            hasFetchedSubscriptions: p.A.hasFetchedSubscriptions(),
-            premiumSubscription: p.A.getPremiumTypeSubscription(),
+    let { hasFetchedSubscriptions: t, premiumSubscription: n } = (0, r.cf)([x.A], () => ({
+            hasFetchedSubscriptions: x.A.hasFetchedSubscriptions(),
+            premiumSubscription: x.A.getPremiumTypeSubscription(),
         })),
-        n = (0, A.Y)(),
-        s = (0, r.bG)([m.A], () => m.A.hasFetchedPaymentSources),
-        E = (0, r.bG)([g.A], () => g.A.isFetchingCurrentUserAppliedBoosts);
-    return e && n && s && !E
+        l = (0, m.Y)(),
+        C = (0, r.bG)([h.A], () => h.A.hasFetchedPaymentSources),
+        f = (0, r.bG)([A.A], () => A.A.isFetchingCurrentUserAppliedBoosts);
+    return t && l && C && !f
         ? (0, i.jsxs)("div", {
-              className: f.kL,
+              className: S.kL,
               children: [
-                  (0, i.jsx)("div", { className: f.Tp }),
-                  (0, i.jsx)("div", { className: f.Qs, children: (0, i.jsx)(_.A, { premiumSubscription: t }) }),
+                  (0, i.jsx)("div", { className: S.Tp }),
+                  (0, i.jsx)("div", {
+                      className: S.Qs,
+                      children: e
+                          ? (0, i.jsx)(T.A, { premiumSubscription: n })
+                          : (0, i.jsx)(p.A, { premiumSubscription: n }),
+                  }),
               ],
           })
-        : (0, i.jsx)("div", { className: a()(f.kL, f.Lq), children: (0, i.jsx)(o.y$y, {}) });
+        : (0, i.jsx)("div", { className: a()(S.kL, S.Lq), children: (0, i.jsx)(o.y$y, {}) });
 }
