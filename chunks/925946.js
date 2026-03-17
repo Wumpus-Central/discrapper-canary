44169ad5cdd1c5831076fe162d38c60dbf591044
DiscrapperManager <admin@minerpl.xyz>
2026-03-17 16:09:52 +0100

    canaryRelease: 512115
    Main scripts: 3
    Lazy Loaded Scripts: 2071
    Chunks:
     * Main Files: 13375
     * Scripts: 12558
    Strings: 24057
    Routes: 803
    Experiments Count: 126
    Apex Experiments Count: 202

--- chunks/925946.js
+++ chunks/925946.js
@@ -1,33 +1,40 @@
 "use strict";
-n.d(t, { A: () => h });
+n.d(t, { A: () => E });
 var r = n(64700),
     i = n(397927),
     s = n(308528),
     a = n(843472),
-    o = n(432222),
-    l = n(976860),
-    u = n(734057),
-    c = n(954571),
-    d = n(652215),
-    _ = n(381941),
-    f = n(901123),
-    p = n(985018);
-function h(e, t) {
-    let [n, h] = r.useState(!1);
+    o = n(56562),
+    l = n(432222),
+    u = n(976860),
+    c = n(734057),
+    d = n(320501),
+    _ = n(954571),
+    f = n(652215),
+    p = n(381941),
+    h = n(901123),
+    m = n(985018);
+function E(e, t) {
+    let [n, E] = r.useState(!1);
     return r.useCallback(async () => {
         let r = await s.A.openPrivateChannel({ recipientIds: e, location: t }),
-            m = u.A.getChannel(r);
-        if (null == m) throw Error("Failed to open private channel");
+            g = c.A.getChannel(r);
+        if (null == g) throw Error("Failed to open private channel");
         if (!n) {
-            h(!0), (0, l.pX)(d.BVt.CHANNEL(f.ME, m.id));
+            E(!0), (0, u.pX)(f.BVt.CHANNEL(h.ME, g.id));
             try {
-                await a.A.sendStickers(m.id, [o.w], "", { location: _.Hx.SEND_WAVE });
+                await a.A.sendStickers(g.id, [l.w], "", { location: p.Hx.SEND_WAVE }),
+                    a.A.jumpToMessage({
+                        channelId: g.id,
+                        messageId: d.A.getLastMessage(g.id)?.id ?? "",
+                        jumpType: o.US.INSTANT,
+                    });
             } catch (e) {
                 e.ok ||
                     429 !== e.status ||
-                    (0, i.showToast)((0, i.createToast)(p.intl.string(p.t.Whhv4w), i.ToastType.FAILURE));
+                    (0, i.showToast)((0, i.createToast)(m.intl.string(m.t.Whhv4w), i.ToastType.FAILURE));
             }
-            c.default.track(d.HAw.WAVE_CTA_CLICKED, { source: t }), h(!1);
+            _.default.track(f.HAw.WAVE_CTA_CLICKED, { source: t }), E(!1);
         }
     }, [t, n, e]);
 }
