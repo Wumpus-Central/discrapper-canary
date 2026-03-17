23a315e2b2642d49fecbcc145b5c319492b98ddb
DiscrapperManager <admin@minerpl.xyz>
2026-03-17 20:10:57 +0100

    canaryRelease: 512296
    Main scripts: 3
    Lazy Loaded Scripts: 2069
    Chunks:
     * Main Files: 13377
     * Scripts: 12511
    Strings: 24058
    Routes: 803
    Experiments Count: 124
    Apex Experiments Count: 203

--- chunks/477394.js
+++ chunks/477394.js
@@ -1,7 +1,6 @@
 "use strict";
-n.d(t, { a: () => i });
-var r = n(600975);
-let i = (0, n(945810).mj)({
+n.d(t, { a: () => r });
+let r = (0, n(945810).mj)({
     name: "2025-08-view-roblox-subgames-in-rich-presence",
     kind: "user",
     defaultConfig: { enabled: !1, showPlayButton: !1, alternativeTitle: !1 },
@@ -12,11 +11,4 @@ let i = (0, n(945810).mj)({
         3: { enabled: !0, showPlayButton: !1, alternativeTitle: !0 },
         4: { enabled: !0, showPlayButton: !0, alternativeTitle: !0 },
     },
-});
-(0, r.C)({
-    kind: "user",
-    id: "2025-08_roblox_subgames_rollout",
-    label: "Roblox subgames fetch rollout",
-    defaultConfig: { enabled: !1 },
-    treatments: [{ id: 1, label: "Enables fetching roblox subgame application info", config: { enabled: !0 } }],
 });
