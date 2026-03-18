5d33ed3c91b0820dcf77e557d6b6a8331b0368ab
DiscrapperManager <admin@minerpl.xyz>
2026-03-18 01:05:24 +0100

    canaryRelease: 512622
    Main scripts: 3
    Lazy Loaded Scripts: 2069
    Chunks:
     * Main Files: 13376
     * Scripts: 12521
    Strings: 24060
    Routes: 803
    Experiments Count: 120
    Apex Experiments Count: 209

--- chunks/84483.js
+++ chunks/84483.js
@@ -1,38 +1,44 @@
 "use strict";
-n.d(t, { uX: () => a, v9: () => o });
+n.d(t, { uX: () => o, v9: () => l });
 var r = n(250105);
 let i = { control: 0, treatment_a: 250, treatment_b: 500, treatment_c: 250, treatment_d: 500 },
     s = (0, r.Ay)({
         name: "2025-12-nitro-s-rewards",
         kind: "user",
-        defaultConfig: { treatment: "control", receivesUpdatedUpsellPopover: !1 },
+        defaultConfig: { treatment: "control" },
         variations: {
-            0: { treatment: "control", receivesUpdatedUpsellPopover: !1 },
-            1: { treatment: "treatment_a", receivesUpdatedUpsellPopover: !1 },
-            2: { treatment: "treatment_b", receivesUpdatedUpsellPopover: !1 },
-            3: { treatment: "treatment_c", receivesUpdatedUpsellPopover: !0 },
-            4: { treatment: "treatment_d", receivesUpdatedUpsellPopover: !0 },
+            0: { treatment: "control" },
+            1: { treatment: "treatment_a" },
+            2: { treatment: "treatment_b" },
+            3: { treatment: "treatment_c" },
+            4: { treatment: "treatment_d" },
         },
+    }),
+    a = (0, r.Ay)({
+        name: "2026-03-nitro-s-rewards-v2-test",
+        kind: "user",
+        defaultConfig: { treatment: "control" },
+        variations: { 0: { treatment: "control" }, 1: { treatment: "treatment_c" }, 2: { treatment: "treatment_d" } },
     });
-function a(e) {
-    let t = s.useConfig({ location: e }),
-        n = t.treatment ?? "control",
-        r = "control" !== n;
-    return {
-        treatment: n,
-        isInTreatment: r,
-        receivesUpdatedUpsellPopover: t.receivesUpdatedUpsellPopover ?? !1,
-        orbsRewardAmount: i[n],
-    };
-}
 function o(e) {
+    let t = s.useConfig({ location: e }),
+        n = a.useConfig({ location: e }),
+        r = t.treatment ?? "control";
+    if ("control" !== r)
+        return { treatment: r, isInTreatment: !0, receivesUpdatedUpsellPopover: !1, orbsRewardAmount: i[r] };
+    let o = n.treatment ?? "control";
+    return "control" !== o
+        ? { treatment: o, isInTreatment: !0, receivesUpdatedUpsellPopover: !0, orbsRewardAmount: i[o] }
+        : { treatment: "control", isInTreatment: !1, receivesUpdatedUpsellPopover: !1, orbsRewardAmount: 0 };
+}
+function l(e) {
     let t = s.getConfig({ location: e }),
-        n = t.treatment ?? "control",
-        r = "control" !== n;
-    return {
-        treatment: n,
-        isInTreatment: r,
-        receivesUpdatedUpsellPopover: t.receivesUpdatedUpsellPopover ?? !1,
-        orbsRewardAmount: i[n],
-    };
+        n = a.getConfig({ location: e }),
+        r = t.treatment ?? "control";
+    if ("control" !== r)
+        return { treatment: r, isInTreatment: !0, receivesUpdatedUpsellPopover: !1, orbsRewardAmount: i[r] };
+    let o = n.treatment ?? "control";
+    return "control" !== o
+        ? { treatment: o, isInTreatment: !0, receivesUpdatedUpsellPopover: !0, orbsRewardAmount: i[o] }
+        : { treatment: "control", isInTreatment: !1, receivesUpdatedUpsellPopover: !1, orbsRewardAmount: 0 };
 }
