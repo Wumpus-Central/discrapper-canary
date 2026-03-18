b5a9527a1f8ca0192c9a0c4590bce6978029006b
DiscrapperManager <admin@minerpl.xyz>
2026-03-18 22:49:28 +0100

    canaryRelease: 513124
    Main scripts: 3
    Lazy Loaded Scripts: 2071
    Chunks:
     * Main Files: 13378
     * Scripts: 12521
    Strings: 24074
    Routes: 802
    Experiments Count: 115
    Apex Experiments Count: 213

--- chunks/945939.js
+++ chunks/945939.js
@@ -3,7 +3,7 @@ let i = (0, n(945810).mj)({
         name: "2026-03-private-profiles-strict",
         kind: "user",
         defaultConfig: { enabled: !1 },
-        variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
+        variations: { 0: { enabled: !1 }, 1: { enabled: !0 }, 2: { enabled: !0 }, 3: { enabled: !0 } },
     }),
     s = (e) => i.useConfig({ location: e }).enabled,
     l = (e) => i.getConfig({ location: e }).enabled;
