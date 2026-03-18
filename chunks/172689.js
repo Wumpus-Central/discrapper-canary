5f38c1135109c7373e5892a5612e425f86b09fc5
DiscrapperManager <admin@minerpl.xyz>
2026-03-18 20:50:24 +0100

    canaryRelease: 512976
    Main scripts: 3
    Lazy Loaded Scripts: 2070
    Chunks:
     * Main Files: 13379
     * Scripts: 12519
    Strings: 24064
    Routes: 802
    Experiments Count: 116
    Apex Experiments Count: 213

--- chunks/172689.js
+++ chunks/172689.js
@@ -2,13 +2,13 @@ n.d(t, { M: () => c });
 var i = n(378729),
     s = n(474774),
     l = n(419954),
-    r = n(253932),
-    a = n(780964),
+    a = n(253932),
+    r = n(780964),
     o = n(985018),
     d = n(79220);
-let c = (0, l.zD)(a.X.GAME_UPDATE_NOTIFICATIONS, {
+let c = (0, l.zD)(r.X.GAME_UPDATE_NOTIFICATIONS, {
     useTitle: () => o.intl.string(d.default["3TO4/r"]),
-    useValue: r.JV.useSetting,
+    useValue: a.JV.useSetting,
     setValue: s.l,
-    usePredicate: () => (0, i.e8)({ location: "SettingsRendererConfig" }).showToggle,
+    usePredicate: () => (0, i.e)("SettingsRendererConfig").showToggle,
 });
--- chunks/378729.js
+++ chunks/378729.js
@@ -1,17 +1,8 @@
-i.d(e, { e8: () => r });
-var n = i(600975),
-    l = i(428835);
-let s = (0, n.C)({
-        id: "2025-11_game_update_notification",
+n.d(t, { e: () => s });
+let i = (0, n(945810).mj)({
+        name: "2026-03-game-update-notification",
         kind: "user",
-        label: "Game Update Notification",
         defaultConfig: { showToggle: !1 },
-        treatments: [
-            {
-                id: 1,
-                label: "User can receive game update notifications. Show settings toggle.",
-                config: { showToggle: !0 },
-            },
-        ],
+        variations: { 0: { showToggle: !1 }, 1: { showToggle: !0 } },
     }),
-    r = (t) => (0, l.u5)(s, { ...t, autoTrackExposure: !1 });
+    s = (e) => i.useConfig({ location: e });
