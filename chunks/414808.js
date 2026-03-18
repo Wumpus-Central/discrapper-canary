53b6cf273eb8bd85e30dd33288f44e7b5e29ccff
DiscrapperManager <admin@minerpl.xyz>
2026-03-18 23:12:39 +0100

    canaryRelease: 513160
    Main scripts: 3
    Lazy Loaded Scripts: 2071
    Chunks:
     * Main Files: 13377
     * Scripts: 12521
    Strings: 24074
    Routes: 802
    Experiments Count: 115
    Apex Experiments Count: 212

--- chunks/414808.js
+++ chunks/414808.js
@@ -1,38 +1,38 @@
-n.d(t, { A: () => E });
+n.d(t, { A: () => h });
 var l = n(627968),
     s = n(64700),
-    a = n(311907),
-    i = n(342494),
+    i = n(311907),
+    a = n(342494),
     r = n(397927),
-    o = n(793574),
-    u = n(688810),
+    u = n(793574),
+    o = n(688810),
     c = n(532794),
     d = n(181079),
     m = n(93055),
     A = n(49999),
-    p = n(788868),
-    f = n(525736),
-    h = n(985018),
-    x = n(988572);
-function E(e) {
+    E = n(788868),
+    p = n(665606),
+    f = n(985018),
+    S = n(988572);
+function h(e) {
     let { targetElementRef: t, markMenuItemPopoverAsDismissed: n } = e,
-        [E, N] = s.useState("top"),
-        g = s.useRef(null),
-        v = s.useRef(null),
-        { hasAccess: S } = (0, m.TW)("FavoritesOnboardingMenuItemPopover"),
-        { analyticsLocations: T } = (0, u.Ay)(o.A.FAVORITES_GUILD_MENU_ITEM),
-        C = (0, a.bG)([d.A], () => d.A.hasStoredFavorites());
+        [h, T] = s.useState("top"),
+        x = s.useRef(null),
+        _ = s.useRef(null),
+        { hasAccess: N } = (0, m.TW)("FavoritesOnboardingMenuItemPopover"),
+        { analyticsLocations: g } = (0, o.Ay)(u.A.FAVORITES_GUILD_MENU_ITEM),
+        C = (0, i.bG)([d.A], () => d.A.hasStoredFavorites());
     s.useLayoutEffect(() => {
         if (null == t.current) return;
         let e = () => {
-            null == v.current &&
-                (v.current = window.requestAnimationFrame(() => {
-                    (v.current = null),
+            null == _.current &&
+                (_.current = window.requestAnimationFrame(() => {
+                    (_.current = null),
                         (() => {
                             let e = t.current?.getBoundingClientRect();
                             if (null == e) return;
-                            let n = g.current?.getBoundingClientRect().height ?? 0;
-                            N(window.innerHeight - e.top < n ? "bottom" : "top");
+                            let n = x.current?.getBoundingClientRect().height ?? 0;
+                            T(window.innerHeight - e.top < n ? "bottom" : "top");
                         })();
                 }));
         };
@@ -41,36 +41,31 @@ function E(e) {
             window.addEventListener("resize", e),
             window.addEventListener("scroll", e, !0),
             () => {
-                null != v.current && (window.cancelAnimationFrame(v.current), (v.current = null)),
+                null != _.current && (window.cancelAnimationFrame(_.current), (_.current = null)),
                     window.removeEventListener("resize", e),
                     window.removeEventListener("scroll", e, !0);
             }
         );
     }, [t]);
-    let _ = s.useCallback(() => {
-            n?.(A.i.TAKE_ACTION), S || (0, c.A)({ subscriptionTier: p.pe.TIER_2, analyticsLocations: T });
-        }, [n, T, S]),
-        j = C ? f.default.TWuDTt : f.default["25YCHl"];
-    return (0, l.jsx)(i.AM, {
+    let v = s.useCallback(() => {
+            n?.(A.i.TAKE_ACTION), N || (0, c.A)({ subscriptionTier: E.pe.TIER_2, analyticsLocations: g });
+        }, [n, g, N]),
+        I = C ? p.default.TWuDTt : p.default["25YCHl"];
+    return (0, l.jsx)(a.AM, {
         targetElementRef: t,
         gradientColor: "nitro-pink",
         position: "right",
         alignmentStrategy: "edge",
-        align: E,
-        caretConfig: { align: "top" === E ? "start" : "end" },
-        popoverRef: g,
+        align: h,
+        caretConfig: { align: "top" === h ? "start" : "end" },
+        popoverRef: x,
         badge: { type: "beta", variant: "expressive" },
-        graphic: { src: x.A, type: "image", aspectRatio: "16/9" },
-        title: h.intl.string(S ? j : f.default.A3ZjbM),
-        body: h.intl.string(S ? f.default.DFjrWw : f.default.qJUI2v),
+        graphic: { src: S.A, type: "image", aspectRatio: "16/9" },
+        title: f.intl.string(N ? I : p.default.A3ZjbM),
+        body: f.intl.string(N ? p.default.DFjrWw : p.default.qJUI2v),
         onRequestClose: () => n?.(A.i.USER_DISMISS),
-        actions: [
-            {
-                icon: S ? void 0 : r.tvc,
-                text: S ? h.intl.string(f.default["+h9aza"]) : h.intl.string(f.default["avps/2"]),
-                onClick: () => _(),
-                variant: S ? "secondary" : "expressive",
-            },
-        ],
+        actions: N
+            ? void 0
+            : [{ icon: r.tvc, text: f.intl.string(p.default["avps/2"]), onClick: () => v(), variant: "expressive" }],
     });
 }
