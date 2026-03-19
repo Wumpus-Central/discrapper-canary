99cad8d3cc759c1bb9b8a8e7053034dd9faa7615
DiscrapperManager <admin@minerpl.xyz>
2026-03-19 22:44:57 +0100

    canaryRelease: 513924
    Main scripts: 3
    Lazy Loaded Scripts: 2072
    Chunks:
     * Main Files: 13378
     * Scripts: 12518
    Strings: 24085
    Routes: 802
    Experiments Count: 113
    Apex Experiments Count: 217

--- chunks/999450.js
+++ chunks/999450.js
@@ -3,19 +3,22 @@ var a = n(627968),
     l = n(64700),
     i = n(793574),
     r = n(688810),
-    s = n(317560),
-    c = n(563988),
-    o = n(305080),
-    d = n(954506),
-    u = n(818348),
+    s = n(44724),
+    c = n(317560),
+    o = n(563988),
+    d = n(305080),
+    u = n(954506),
     m = n(985018);
 function x() {
-    let { socialLayerStorefrontRecommendationsData: e } = (0, o.c)(),
+    let { socialLayerStorefrontRecommendationsData: e } = (0, d.c)(),
         { analyticsLocations: t } = (0, r.Ay)([i.A.GAME_PROFILE]),
-        n = l.useCallback(
+        n = l.useCallback(() => {
+            e?.guildId != null && (0, s.default)({ guildId: e.guildId });
+        }, [e]),
+        x = l.useCallback(
             (n) => {
                 e?.guildId != null &&
-                    (0, s.R)({
+                    (0, c.R)({
                         skuId: n,
                         applicationId: e.application.id,
                         guildId: e.guildId,
@@ -26,10 +29,10 @@ function x() {
             [e, t],
         );
     if (null == e) return null;
-    let { skuIds: x } = e;
-    return (0, a.jsx)(d.A, {
+    let { skuIds: f } = e;
+    return (0, a.jsx)(u.A, {
         title: m.intl.string(m.t.WDdlUb),
-        onClickViewAll: u.tE,
-        children: (0, a.jsx)(c.R, { skuIds: x, analyticsLocations: t, onCardClick: n }),
+        onClickViewAll: n,
+        children: (0, a.jsx)(o.R, { skuIds: f, analyticsLocations: t, onCardClick: x }),
     });
 }
