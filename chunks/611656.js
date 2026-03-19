74594feb9a5c0389eaf87bd84f40976c0dea08cb
DiscrapperManager <admin@minerpl.xyz>
2026-03-19 21:46:41 +0100

    canaryRelease: 513844
    Main scripts: 3
    Lazy Loaded Scripts: 2072
    Chunks:
     * Main Files: 13378
     * Scripts: 12522
    Strings: 24085
    Routes: 802
    Experiments Count: 113
    Apex Experiments Count: 218

--- chunks/611656.js
+++ chunks/611656.js
@@ -1,61 +1,67 @@
 "use strict";
-n.d(t, { Ay: () => A, oS: () => g }), n(321073);
+n.d(t, { Ay: () => T, oS: () => I }), n(321073);
 var r = n(64700),
     i = n(284009),
     s = n.n(i),
     a = n(763956),
-    o = n(311907),
-    l = n(587895),
-    u = n(429913),
-    c = n(20015),
-    d = n(136086),
-    _ = n(885151),
-    f = n(954571),
-    p = n(652215);
-let h = function (e, t) {
+    o = n(574381),
+    l = n(311907),
+    u = n(587895),
+    c = n(429913),
+    d = n(20015),
+    _ = n(136086),
+    f = n(885151),
+    p = n(954571),
+    h = n(829806),
+    m = n(652215);
+let E = function (e, t) {
         let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : [],
             r = arguments.length > 3 ? arguments[3] : void 0;
-        f.default.track(p.HAw.GAME_PROFILE_ENTRY_POINT_AVAILABLE, {
+        p.default.track(m.HAw.GAME_PROFILE_ENTRY_POINT_AVAILABLE, {
             game_profile_available: e,
             application_id: t,
             rejection_reason: n,
             source: r,
         });
     },
-    m = (e, t) => {
+    g = (e, t) => {
         let n = [];
         return null == e || null == t
             ? ["no match"]
-            : ((0, c.n)(t, p.gfo.GAME_PROFILE_DISABLED) && n.push("profile disabled"),
+            : ((0, d.n)(t, m.gfo.GAME_PROFILE_DISABLED) && n.push("profile disabled"),
               e.themes.includes(a.W.EROTIC) && n.push("nsfw"),
               (null == e.summary || 0 === e.summary.length) && n.push("no summary"),
               (null == e.summary || 0 === e.summary.length) && n.push("no summary"),
               n);
     },
-    E = (e, t) => 0 === m(e, t).length,
-    g = (e) => E(d.A.getGame(e)?.supplementalData, l.A.getApplication(e)),
-    A = (e) => {
+    A = (e, t) => 0 === g(e, t).length,
+    I = (e) => A(_.A.getGame(e)?.supplementalData, u.A.getApplication(e)),
+    T = (e) => {
         let { applicationId: t = "", source: n, trackEntryPointImpression: i = !0 } = e,
             a = r.useRef(!1),
-            c = (0, o.bG)([l.A], () => l.A.getApplication(t)),
-            f = r.useMemo(
-                () => [t, ...(c?.linkedGames?.map((e) => e.id) ?? [])].filter((e) => null != e && "" !== e),
-                [t, c],
+            d =
+                ((0, h.kW)("useShouldOpenGameProfileModal") && ((0, o.m0)() || (0, o.un)())) ||
+                (0, o.xl)() ||
+                (0, o.HZ)(),
+            p = (0, l.bG)([u.A], () => u.A.getApplication(t)),
+            m = r.useMemo(
+                () => [t, ...(p?.linkedGames?.map((e) => e.id) ?? [])].filter((e) => null != e && "" !== e),
+                [t, p],
             );
-        (0, u.A)(f), (0, _.W)(f);
-        let p = (0, o.bG)([d.A, l.A], () => f.find((e) => E(d.A.getGame(e)?.supplementalData, l.A.getApplication(e)))),
-            g = (0, o.bG)([d.A, l.A], () => f.every((e) => !d.A.isFetching(e) && !l.A.isFetchingApplication(e)));
+        (0, c.A)(d ? m : []), (0, f.W)(d ? m : []);
+        let I = (0, l.bG)([_.A, u.A], () => m.find((e) => A(_.A.getGame(e)?.supplementalData, u.A.getApplication(e)))),
+            T = (0, l.bG)([_.A, u.A], () => m.every((e) => !_.A.isFetching(e) && !u.A.isFetchingApplication(e)));
         return (
             r.useEffect(() => {
-                if (!a.current && i && null != c && (g || null != p)) {
-                    let e = null != p ? d.A.getGame(p) : d.A.getGame(c.id);
+                if (!a.current && d && i && null != p && (T || null != I)) {
+                    let e = null != I ? _.A.getGame(I) : _.A.getGame(p.id);
                     if (null != e) {
                         s()(null != n, "Cannot track a Game Profile Entry Point Impressions without a source.");
-                        let t = m(e.supplementalData, c);
-                        h(null != p, e.applicationId, t, n), (a.current = !0);
+                        let t = g(e.supplementalData, p);
+                        E(null != I, e.applicationId, t, n), (a.current = !0);
                     }
                 }
-            }, [p, c, g, n, i]),
-            { shouldOpenGameProfile: null != p, applicationId: p }
+            }, [I, p, d, T, n, i]),
+            { shouldOpenGameProfile: d && null != I, applicationId: I }
         );
     };
