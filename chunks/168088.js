3f08f76732f42fa70e3e2bd960a5882b970bf126
DiscrapperManager <admin@minerpl.xyz>
2026-03-19 16:40:14 +0100

    canaryRelease: 513482
    Main scripts: 3
    Lazy Loaded Scripts: 2071
    Chunks:
     * Main Files: 13378
     * Scripts: 12512
    Strings: 24078
    Routes: 802
    Experiments Count: 114
    Apex Experiments Count: 215

--- chunks/168088.js
+++ chunks/168088.js
@@ -1,27 +1,28 @@
-i.d(e, { default: () => _ });
-var a = i(627968),
-    c = i(64700),
-    n = i(397927),
+i.d(e, { default: () => g });
+var n = i(627968),
+    a = i(64700),
+    c = i(397927),
     s = i(793574),
     d = i(688810),
-    p = i(532794),
-    r = i(976860),
+    r = i(532794),
+    p = i(976860),
     l = i(668267),
     o = i(652215),
     u = i(788868),
     f = i(665606),
     h = i(985018),
     v = i(988572);
-function _(t) {
-    let { onClose: e, source: i = "channel_context_menu", ..._ } = t,
-        { analyticsLocations: b } = (0, d.Ay)(s.A.FAVORITES_GUILD_UPSELL_MODAL);
+function g(t) {
+    let { onClose: e, source: i = "channel_context_menu", ...g } = t,
+        { analyticsLocations: _ } = (0, d.Ay)(s.A.FAVORITES_GUILD_UPSELL_MODAL);
     return (
-        c.useEffect(() => {
+        a.useEffect(() => {
             (0, l.XC)(i);
         }, [i]),
-        (0, a.jsx)(n.kpP, {
-            ..._,
+        (0, n.jsx)(c.kpP, {
+            ...g,
             onClose: e,
+            gradientColor: "nitro-pink",
             badge: { type: "beta", variant: "expressive" },
             title: h.intl.string(f.default.aA0vO8),
             subtitle: h.intl.string(f.default["WaP/lz"]),
@@ -31,15 +32,15 @@ function _(t) {
                     text: h.intl.string(f.default["+dSwhE"]),
                     variant: "secondary",
                     onClick: () => {
-                        e(), (0, l.mv)("upsell_modal"), (0, r.uh)(o.YYv);
+                        e(), (0, l.mv)("upsell_modal"), (0, p.uh)(o.YYv);
                     },
                 },
                 {
                     text: h.intl.string(h.t.pj0XBN),
                     variant: "expressive",
-                    icon: n.tvc,
+                    icon: c.tvc,
                     onClick: () => {
-                        e(), (0, p.A)({ subscriptionTier: u.pe.TIER_2, analyticsLocations: b });
+                        e(), (0, r.A)({ subscriptionTier: u.pe.TIER_2, analyticsLocations: _ });
                     },
                 },
             ],
