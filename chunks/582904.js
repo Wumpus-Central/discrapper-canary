c844f6b1fef5265e1cfa7bdcc3c95cab89deeaec
DiscrapperManager <admin@minerpl.xyz>
2026-03-23 20:43:27 +0100

    canaryRelease: 514954
    Main scripts: 3
    Lazy Loaded Scripts: 2072
    Chunks:
     * Main Files: 13383
     * Scripts: 12507
    Strings: 24105
    Routes: 800
    Experiments Count: 105
    Apex Experiments Count: 223

--- chunks/582904.js
+++ chunks/582904.js
@@ -1,64 +1,58 @@
 "use strict";
-n.d(t, { Ln: () => _, Z0: () => g, kt: () => p }), n(839272);
+n.d(t, { Ln: () => f, Z0: () => g, kt: () => p }), n(321073);
 var i = n(64700),
     s = n(311907),
     l = n(394577),
-    r = n(21119),
-    a = n(696451),
-    o = n(576705),
-    c = n(287809),
-    d = n(607567),
-    u = n(403362),
-    h = n(605431),
-    A = n(366251),
-    m = n(652215);
+    a = n(21119),
+    r = n(95701),
+    o = n(696451),
+    c = n(576705),
+    d = n(287809),
+    u = n(607567),
+    h = n(488926),
+    A = n(605431),
+    m = n(366251),
+    _ = n(652215);
 function p(e) {
     let { channel: t } = e,
         n = t?.guild_id,
-        { totalSuggestions: o } = l.A.useExperiment(
+        { totalSuggestions: i } = l.A.useExperiment(
             { guildId: n, location: "useVoiceInviteSuggestions" },
             { autoTrackExposure: !1 },
-        ),
-        h = (0, s.bG)([r.A], () => r.A.getUserAffinitiesMap(), []),
-        A = new Set(
-            (0, s.bG)([d.Ay], () => (null == t ? [] : d.Ay.getVoiceStatesForChannel(t).map((e) => e.user.id)), [t]),
-        ),
-        m = (0, s.yK)(
-            [a.Ay, c.default],
-            () =>
-                a.Ay.getMembers(n)
-                    .map((e) => c.default.getUser(e.userId))
-                    .filter(u.Vq)
-                    .filter((e) => !A.has(e.id)),
-            [n, A],
         );
-    return i
-        .useMemo(
-            () =>
-                m.toSorted((e, t) => {
-                    let { id: n } = e,
-                        { id: i } = t;
-                    return (h.get(i)?.vcProbability ?? 0) - (h.get(n)?.vcProbability ?? 0);
-                }),
-            [m, h],
-        )
-        .slice(0, o);
+    return (0, s.yK)([a.A, u.Ay, o.Ay, d.default], () => {
+        if (null == t) return [];
+        let e = a.A.getUserAffinitiesMap(),
+            s = null != t ? u.Ay.getVoiceStatesForChannel(t) : [],
+            l = new Set();
+        s.forEach((e) => l.add(e.user.id));
+        let c = [];
+        for (let e of o.Ay.getMembers(n)) {
+            if (l.has(e.userId)) continue;
+            let n = d.default.getUser(e.userId);
+            if (null == n) continue;
+            let i = (0, r.TA)(t.type);
+            (0, h.$3)({ permission: i, user: n, context: t }) && c.push(n);
+        }
+        let A = (t) => e.get(t)?.vcProbability ?? 0;
+        return c.sort((e, t) => A(t.id) - A(e.id)).slice(0, i);
+    }, [t, n, i]);
 }
 function g(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {},
         { collapsed: n = !1 } = t,
-        l = (0, s.bG)([A.A], () => A.A.getShouldShowPopover(e.id), [e.id]);
+        l = (0, s.bG)([m.A], () => m.A.getShouldShowPopover(e.id), [e.id]);
     return {
         shouldShow: l && !n,
         dismiss: i.useCallback(() => {
-            (0, h.w)(e.id);
+            (0, A.w)(e.id);
         }, [e]),
     };
 }
-function _(e) {
+function f(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {},
         { collapsed: n = !1 } = t,
-        i = A.A.getShouldShowPopover(e.id),
-        s = o.A.can(m.xBc.CREATE_INSTANT_INVITE, e);
+        i = m.A.getShouldShowPopover(e.id),
+        s = c.A.can(_.xBc.CREATE_INSTANT_INVITE, e);
     return i && s && !n;
 }
