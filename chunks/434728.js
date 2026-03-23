c6224f6b3dfb2c6025f160a707bbc205ee17ac87
DiscrapperManager <admin@minerpl.xyz>
2026-03-23 22:36:21 +0100

    canaryRelease: 515097
    Main scripts: 3
    Lazy Loaded Scripts: 2072
    Chunks:
     * Main Files: 13387
     * Scripts: 12510
    Strings: 24105
    Routes: 800
    Experiments Count: 105
    Apex Experiments Count: 223

--- chunks/434728.js
+++ chunks/434728.js
@@ -1,13 +1,14 @@
-n.d(t, { O: () => s });
+n.d(t, { O: () => l });
 var i = n(397927),
     r = n(186111),
     a = n(203982),
-    l = n(652215);
-let s = {
-    binds: ["mod+f"],
+    s = n(652215);
+let l = {
+    binds: ["mod+f", "mod+shift+f"],
     comboKeysBindGlobal: !0,
-    action() {
-        if (!(r.A.hasLayers() || (0, i.ueM)()))
-            return a._.dispatch(l.jej.FOCUS_SEARCH, { prefillCurrentChannel: !0 }), !1;
+    action(e, t) {
+        if (r.A.hasLayers() || (0, i.ueM)()) return;
+        let n = !t.includes("shift");
+        return a._.dispatch(s.jej.FOCUS_SEARCH, { prefillCurrentChannel: n }), !1;
     },
 };
