8bf17696117cd768a83eaa3eaf0b54757369706a
DiscrapperManager <admin@minerpl.xyz>
2026-03-24 00:22:58 +0100

    canaryRelease: 515243
    Main scripts: 3
    Lazy Loaded Scripts: 2076
    Chunks:
     * Main Files: 13378
     * Scripts: 12521
    Strings: 24176
    Routes: 800
    Experiments Count: 105
    Apex Experiments Count: 222

---
+++ chunks/142839.js
@@ -0,0 +1,9 @@
+"use strict";
+n.d(t, { D: () => r });
+var r = (function (e) {
+    return (
+        (e[(e.INCREASED_FILE_UPLOAD_SIZE = 0)] = "INCREASED_FILE_UPLOAD_SIZE"),
+        (e[(e.INCREASED_GUILD_LIMIT = 1)] = "INCREASED_GUILD_LIMIT"),
+        e
+    );
+})({});
--- chunks/817857.js
+++ chunks/817857.js
@@ -1,24 +1,29 @@
 "use strict";
-n.d(t, { Nh: () => s, c$: () => i });
+n.d(t, { Nh: () => o, c$: () => a });
 var r = n(136722);
-function i(e) {
+n(988506);
+var i = n(142839);
+function s(e) {
+    if (null != e.kind)
+        switch (e.kind.type) {
+            case i.D.INCREASED_FILE_UPLOAD_SIZE:
+                return { type: i.D.INCREASED_FILE_UPLOAD_SIZE, maxSize: e.kind.max_size };
+            case i.D.INCREASED_GUILD_LIMIT:
+                return { type: i.D.INCREASED_GUILD_LIMIT, maxGuilds: e.kind.max_guilds };
+            default:
+                return;
+        }
+}
+function a(e) {
     if (null == e) return null;
     let t = {};
-    for (let [n, r] of Object.entries(e.config_by_perk))
-        t[n] = {
-            source: r.source,
-            increasedFileUploadSize:
-                null != r.increased_file_upload_size ? { maxSize: r.increased_file_upload_size.max_size } : void 0,
-            increasedGuildLimit:
-                null != r.increased_guild_limit ? { maxGuilds: r.increased_guild_limit.max_guilds } : void 0,
-        };
+    for (let [n, r] of Object.entries(e.config_by_perk)) t[n] = { source: r.source, kind: s(r) };
     return { activePerksBitmask: e.active_perks_bitmask, configByPerk: t, rulesVersion: e.rules_version };
 }
-function s(e, t) {
+function o(e, t) {
     if (null == e) return !1;
     let n = e.activePerksBitmask,
         i = Math.floor(t / 64),
         s = t % 64;
     return !(i >= n.length) && r.zy(r.iu(n[i]), r.jB(s));
 }
-n(988506);
