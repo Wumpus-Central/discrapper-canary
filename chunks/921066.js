eebe69a6b1e83dfb041b68fafa64f509863e8023
DiscrapperManager <admin@minerpl.xyz>
2026-03-23 21:42:08 +0100

    canaryRelease: 515015
    Main scripts: 3
    Lazy Loaded Scripts: 2072
    Chunks:
     * Main Files: 13387
     * Scripts: 12507
    Strings: 24105
    Routes: 800
    Experiments Count: 105
    Apex Experiments Count: 223

--- chunks/921066.js
+++ chunks/921066.js
@@ -1,144 +1,127 @@
-n.d(t, { A: () => S });
+n.d(t, { A: () => U });
 var i,
     a = n(627968),
     l = n(64700),
-    r = n(311907),
-    s = n(473193),
-    o = n(397927),
-    c = n(775602),
-    d = n(497566),
-    u = n(881635),
-    _ = n(567771),
-    m = n(168350),
-    A = n(649963),
-    E = n(763754),
-    I = n(652215),
-    T = n(985018),
-    f = n(942066),
-    N = n(810309),
-    C = n(274993),
-    g = n(218924);
-function h() {
-    let e = (0, r.bG)([c.A], () => c.A.useReducedMotion),
-        t = (i || (i = n.t(l, 2))).useContext(s.C)?.animate ?? !1;
+    r = n(503698),
+    s = n.n(r),
+    o = n(311907),
+    c = n(473193),
+    d = n(397927),
+    u = n(775602),
+    _ = n(497566),
+    m = n(881635),
+    A = n(567771),
+    E = n(168350),
+    I = n(649963),
+    T = n(763754),
+    f = n(652215),
+    N = n(985018),
+    g = n(625780),
+    C = n(810309),
+    h = n(274993),
+    p = n(218924);
+let R = { id: null, name: "❤️", animated: !1 };
+function x() {
+    let e = (0, o.bG)([u.A], () => u.A.useReducedMotion),
+        t = (i || (i = n.t(l, 2))).useContext(c.C)?.animate ?? !1;
     return !e && t;
 }
-function p(e) {
-    let { message: t, guild: n, usernameHook: i } = e,
-        l = h(),
-        r = (0, E.Ay)(t),
-        s = i(r),
-        c = (0, _.A)(t);
+function S(e) {
+    let { className: t, showProgressBar: n } = e,
+        i = x();
     return (0, a.jsxs)("div", {
-        className: f.kL,
+        className: t,
         children: [
-            (0, a.jsx)(m.A, { className: f.Tp, forceReducedMotion: !l }),
-            (0, a.jsxs)("div", {
-                className: f.rc,
-                children: [
-                    (0, a.jsx)("img", { className: f.iQ, src: N.A, alt: "", "aria-hidden": !0 }),
-                    (0, a.jsx)("img", { className: f.CC, src: l ? C.A : g.A, alt: "", "aria-hidden": !0 }),
-                ],
+            n && (0, a.jsx)("img", { className: g.iQ, src: C.A, alt: "", "aria-hidden": !0 }),
+            (0, a.jsx)("img", { className: n ? g.yt : g.V_, src: i ? h.A : p.A, alt: "", "aria-hidden": !0 }),
+        ],
+    });
+}
+function O(e) {
+    let { className: t, title: n, body: i } = e;
+    return (0, a.jsxs)("div", {
+        className: s()(g.Qs, t),
+        children: [
+            (0, a.jsx)(d.Text, { variant: "text-md/semibold", className: g.DD, children: n }),
+            (0, a.jsx)(d.Text, { variant: "text-md/normal", className: g.rf, children: i }),
+        ],
+    });
+}
+function M(e) {
+    let { guild: t, message: n, canReact: i = !1 } = e,
+        r = l.useCallback(() => {
+            (0, I.BB)(n.channel_id, n.id, R, I.qN.MESSAGE);
+        }, [n.channel_id, n.id]);
+    return (0, a.jsxs)("div", {
+        className: g.qr,
+        children: [
+            (0, a.jsx)(m.A, {
+                guild: t,
+                analyticsLocation: {
+                    page: f.liQ.GUILD_CHANNEL,
+                    section: f.JJy.CHANNEL_TEXT_AREA,
+                    object: f.ZSU.BOOST_ANNOUNCEMENT_UPSELL,
+                },
             }),
-            (0, a.jsxs)("div", {
-                className: f.Qs,
-                children: [
-                    (0, a.jsx)(o.Text, {
-                        variant: "text-md/semibold",
-                        className: f.DD,
-                        children: T.intl.format(T.t["7t1Ecm"], {
-                            username: r.nick,
-                            usernameHook: s,
-                            numSubscriptions: c,
-                        }),
+            i &&
+                (0, a.jsx)(d.vN3, {
+                    children: (0, a.jsx)("button", {
+                        className: g.Zf,
+                        onClick: r,
+                        "aria-label": N.intl.string(N.t.eaRpLR),
+                        children: (0, a.jsx)("span", { role: "img", "aria-hidden": "true", children: "❤️" }),
                     }),
-                    (0, a.jsx)(o.Text, {
-                        variant: "text-md/normal",
-                        className: f.rf,
-                        children: T.intl.string(T.t.nybLqy),
-                    }),
-                ],
+                }),
+        ],
+    });
+}
+function D(e) {
+    let { message: t, guild: n, usernameHook: i } = e,
+        l = x(),
+        r = (0, T.Ay)(t),
+        s = i(r),
+        o = (0, A.A)(t);
+    return (0, a.jsxs)("div", {
+        className: g.kL,
+        children: [
+            (0, a.jsx)(E.A, { className: g.Tp, forceReducedMotion: !l }),
+            (0, a.jsx)(S, { className: g.rc, showProgressBar: !0 }),
+            (0, a.jsx)(O, {
+                title: N.intl.format(N.t["7t1Ecm"], { username: r.nick, usernameHook: s, numSubscriptions: o }),
+                body: N.intl.string(N.t.nybLqy),
             }),
             null != n &&
                 (0, a.jsxs)(a.Fragment, {
                     children: [
-                        (0, a.jsx)(d.A, { className: f.WM, guildId: n.id }),
-                        (0, a.jsx)("div", {
-                            className: f.qr,
-                            children: (0, a.jsx)(u.A, {
-                                guild: n,
-                                analyticsLocation: {
-                                    page: I.liQ.GUILD_CHANNEL,
-                                    section: I.JJy.CHANNEL_TEXT_AREA,
-                                    object: I.ZSU.BOOST_ANNOUNCEMENT_UPSELL,
-                                },
-                            }),
-                        }),
+                        (0, a.jsx)(_.A, { className: g.WM, guildId: n.id }),
+                        (0, a.jsx)(M, { guild: n, message: t }),
                     ],
                 }),
         ],
     });
 }
-let R = { id: null, name: "❤️", animated: !1 };
-function x(e) {
+function P(e) {
     let { message: t, guild: n, usernameHook: i } = e,
-        r = h(),
-        s = (0, E.Ay)(t),
-        c = i(s),
-        d = l.useCallback(() => {
-            (0, A.BB)(t.channel_id, t.id, R, A.qN.MESSAGE);
-        }, [t.channel_id, t.id]);
+        l = x(),
+        r = (0, T.Ay)(t),
+        s = i(r);
     return (0, a.jsxs)("div", {
-        className: f.kL,
+        className: g.kL,
         children: [
-            (0, a.jsx)(m.A, { className: f.Tp, forceReducedMotion: !r }),
-            (0, a.jsx)("div", {
-                className: f.VA,
-                children: (0, a.jsx)("img", { className: f.V_, src: r ? C.A : g.A, alt: "", "aria-hidden": !0 }),
+            (0, a.jsx)(E.A, { className: g.Tp, forceReducedMotion: !l }),
+            (0, a.jsx)(S, { className: g.VA }),
+            (0, a.jsx)(O, {
+                className: g.sf,
+                title: N.intl.format(N.t["/1IQGD"], { username: r.nick, usernameHook: s }),
+                body: N.intl.string(N.t.nIEjuk),
             }),
-            (0, a.jsxs)("div", {
-                className: f.sf,
-                children: [
-                    (0, a.jsx)(o.Text, {
-                        variant: "text-md/semibold",
-                        className: f.DD,
-                        children: T.intl.format(T.t["/1IQGD"], { username: s.nick, usernameHook: c }),
-                    }),
-                    (0, a.jsx)(o.Text, {
-                        variant: "text-md/normal",
-                        className: f.rf,
-                        children: T.intl.string(T.t.nIEjuk),
-                    }),
-                ],
-            }),
-            null != n &&
-                (0, a.jsxs)("div", {
-                    className: f.qr,
-                    children: [
-                        (0, a.jsx)(u.A, {
-                            guild: n,
-                            analyticsLocation: {
-                                page: I.liQ.GUILD_CHANNEL,
-                                section: I.JJy.CHANNEL_TEXT_AREA,
-                                object: I.ZSU.BOOST_ANNOUNCEMENT_UPSELL,
-                            },
-                        }),
-                        (0, a.jsx)(o.vN3, {
-                            children: (0, a.jsx)("button", {
-                                className: f.Zf,
-                                onClick: d,
-                                "aria-label": T.intl.string(T.t.eaRpLR),
-                                children: (0, a.jsx)("span", { role: "img", "aria-hidden": "true", children: "❤️" }),
-                            }),
-                        }),
-                    ],
-                }),
+            null != n && (0, a.jsx)(M, { guild: n, message: t, canReact: !0 }),
         ],
     });
 }
-function S(e) {
+function U(e) {
     let { message: t, guild: n, usernameHook: i } = e;
-    return t.hasFlag(I.pr7.IS_FIRST_BOOSTER)
-        ? (0, a.jsx)(x, { message: t, guild: n, usernameHook: i })
-        : (0, a.jsx)(p, { message: t, guild: n, usernameHook: i });
+    return t.hasFlag(f.pr7.IS_FIRST_BOOSTER)
+        ? (0, a.jsx)(P, { message: t, guild: n, usernameHook: i })
+        : (0, a.jsx)(D, { message: t, guild: n, usernameHook: i });
 }
