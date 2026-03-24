9bebebc5aa1216e7b4a84ed517f27d8b7cef7baa
DiscrapperManager <admin@minerpl.xyz>
2026-03-24 19:14:44 +0100

    canaryRelease: 515689
    Main scripts: 3
    Lazy Loaded Scripts: 2076
    Chunks:
     * Main Files: 13377
     * Scripts: 12526
    Strings: 24195
    Routes: 800
    Experiments Count: 100
    Apex Experiments Count: 224

--- chunks/419954.js
+++ chunks/419954.js
@@ -1,25 +1,25 @@
 n.d(t, {
     AK: () => N,
-    D1: () => A,
-    E2: () => b,
-    FW: () => h,
-    Hn: () => S,
+    D1: () => g,
+    E2: () => v,
+    FW: () => p,
+    Hn: () => b,
     Hr: () => a,
-    Qx: () => f,
-    Tf: () => x,
-    WI: () => l,
-    bd: () => m,
-    dT: () => d,
-    eA: () => g,
-    gN: () => E,
+    Qx: () => C,
+    Tf: () => T,
+    WI: () => r,
+    bd: () => _,
+    dT: () => c,
+    eA: () => A,
+    gN: () => x,
     hl: () => s,
     i4: () => o,
-    sN: () => I,
-    t_: () => c,
-    v_: () => C,
+    sN: () => S,
+    t_: () => d,
+    v_: () => f,
     zC: () => u,
-    zD: () => T,
-    zZ: () => _,
+    zD: () => E,
+    zZ: () => m,
 });
 var i = n(963935);
 function s(e) {
@@ -27,75 +27,76 @@ function s(e) {
         if ("buildLayout" in t && "function" == typeof t.buildLayout) {
             let n = t.buildLayout().map(e),
                 { buildLayout: i, ...s } = t,
-                r = { ...s, layout: n };
-            return n.forEach((e) => (e.parent = r)), r;
+                l = { ...s, layout: n };
+            return n.forEach((e) => (e.parent = l)), l;
         }
         return t;
     })(e);
 }
-function r(e, t, n) {
+function l(e, t, n) {
     return { ...n, key: e, type: t };
 }
 function a(e) {
-    return r("$Root", i.Z6.ROOT, e);
+    return l("$Root", i.Z6.ROOT, e);
 }
-function l(e, t) {
-    return r(e, i.Z6.SECTION, t);
+function r(e, t) {
+    return l(e, i.Z6.SECTION, t);
 }
 function o(e, t) {
-    return r(e, i.Z6.SIDEBAR_ITEM, t);
-}
-function c(e, t) {
-    return r(e, i.Z6.PANEL, t);
+    return l(e, i.Z6.SIDEBAR_ITEM, t);
 }
 function d(e, t) {
-    return r(e, i.Z6.TAB_ITEM, t);
+    return l(e, i.Z6.PANEL, t);
+}
+function c(e, t) {
+    return l(e, i.Z6.TAB_ITEM, t);
 }
 function u(e, t) {
-    return r(e, i.Z6.SPLIT, t);
-}
-function _(e, t) {
-    return r(e, i.Z6.CATEGORY, t);
+    return l(e, i.Z6.SPLIT, t);
 }
 function m(e, t) {
-    return r(e, i.Z6.ACCORDION, t);
+    return l(e, i.Z6.CATEGORY, t);
 }
-function A(e, t) {
-    return r(e, i.Z6.LIST, t);
+function _(e, t) {
+    return l(e, i.Z6.ACCORDION, t);
 }
 function g(e, t) {
-    return r(e, i.Z6.NESTED_PANEL, t);
+    return l(e, i.Z6.LIST, t);
 }
-function E(e, t) {
-    return r(e, i.Z6.RELATED, t);
-}
-function h(e, t) {
-    return r(e, i.Z6.FIELD_SET, t);
-}
-function p(e, t, n) {
-    return { ...n, key: e, type: t };
-}
-function C(e, t) {
-    return p(e, i.Z6.STATIC, t);
+function A(e, t) {
+    return l(e, i.Z6.NESTED_PANEL, t);
 }
 function x(e, t) {
-    return p(e, i.Z6.BUTTON, t);
+    return l(e, i.Z6.RELATED, t);
 }
-function T(e, t) {
-    return p(e, i.Z6.TOGGLE, t);
+function p(e, t) {
+    return l(e, i.Z6.FIELD_SET, t);
 }
-function I(e, t) {
-    return p(e, i.Z6.SLIDER, t);
-}
-function S(e, t) {
-    return p(e, i.Z6.SELECT, t);
+function h(e, t, n) {
+    return { ...n, key: e, type: t };
 }
 function f(e, t) {
-    return p(e, i.Z6.RADIO, t);
+    return h(e, i.Z6.STATIC, t);
 }
-function N(e, t) {
-    return p(e, i.Z6.NAVIGATOR, t);
+function T(e, t) {
+    return h(e, i.Z6.BUTTON, t);
+}
+function E(e, t) {
+    return h(e, i.Z6.TOGGLE, t);
+}
+function S(e, t) {
+    return h(e, i.Z6.SLIDER, t);
 }
 function b(e, t) {
-    return p(e, i.Z6.CUSTOM, t);
+    let n = t.selectionMode ?? "single";
+    return { ...t, selectionMode: n, key: e, type: i.Z6.SELECT };
+}
+function C(e, t) {
+    return h(e, i.Z6.RADIO, t);
+}
+function N(e, t) {
+    return h(e, i.Z6.NAVIGATOR, t);
+}
+function v(e, t) {
+    return h(e, i.Z6.CUSTOM, t);
 }
--- chunks/98254.js
+++ chunks/98254.js
@@ -1,43 +1,51 @@
-n.d(t, { A: () => o });
+n.d(t, { A: () => c });
 var i = n(627968),
-    s = n(397927),
-    l = n(600812),
-    a = n(791498),
-    r = n(244242);
-function o(e) {
+    s = n(64700),
+    l = n(397927),
+    a = n(600812),
+    r = n(791498),
+    o = n(244242);
+function d(e, t) {
+    return (n) => {
+        t(), e(n);
+    };
+}
+function c(e) {
     let { node: t } = e,
         {
-            useValue: n,
-            useTitle: o,
-            useSubtitle: d,
-            useOptions: c,
-            clearable: u,
-            usePersistentBadge: _,
-            getDismissibleBadges: g,
+            useTitle: n,
+            useSubtitle: c,
+            useValue: u,
+            setValue: m,
+            useOptions: _,
+            clearable: g,
+            closeOnSelect: A,
+            wrapTags: x,
+            selectionMode: p,
+            usePersistentBadge: h,
+            getDismissibleBadges: f,
         } = t,
-        m = t.setValue,
-        A = o(),
-        h = d?.(),
-        p = n(),
-        x = c(),
-        E = _?.(),
-        T = g?.(),
-        S = (0, l.A)({ persistentBadge: E, dismissibleBadges: T }),
-        C = (0, a.q)(t);
-    return (0, i.jsx)(r.L, {
-        children: (0, i.jsx)(s.l6P, {
-            selectionMode: "single",
-            label: A,
-            description: h,
-            options: x,
-            value: p,
-            clearable: u,
-            onSelectionChange: (e) => {
-                C(), m(e);
-            },
+        T = n(),
+        E = c?.(),
+        S = u(),
+        b = _(),
+        C = h?.(),
+        N = f?.(),
+        v = (0, a.A)({ persistentBadge: C, dismissibleBadges: N }),
+        I = (0, r.q)(t),
+        j = s.useMemo(() => ({ selectionMode: p, value: S, onSelectionChange: d(m, I) }), [p, S, m, I]);
+    return (0, i.jsx)(o.L, {
+        children: (0, i.jsx)(l.l6P, {
+            ...j,
+            label: T,
+            description: E,
+            options: b,
+            wrapTags: x,
+            closeOnSelect: A,
+            clearable: g,
             layout: "horizontal-responsive",
             fullWidth: !0,
-            badge: S,
+            badge: v,
         }),
     });
 }
