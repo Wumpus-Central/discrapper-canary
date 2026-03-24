287c82418765263c3bb992b369c39f6df875b7cf
DiscrapperManager <admin@minerpl.xyz>
2026-03-24 01:13:14 +0100

    canaryRelease: 515297
    Main scripts: 3
    Lazy Loaded Scripts: 2076
    Chunks:
     * Main Files: 13378
     * Scripts: 12521
    Strings: 24179
    Routes: 800
    Experiments Count: 105
    Apex Experiments Count: 222

--- chunks/251441.js
+++ chunks/251441.js
@@ -1,4 +1,4 @@
-d.r(e), d.d(e, { default: () => i });
+d.r(a), d.d(a, { default: () => i });
 let i = JSON.parse(
-    '{"OacAil":["Available"],"/MxjmI":["More ",[1,"gameName"]," game updates?"],"3TO4/r":["Game Updates"],"diGDe0":["Receive notifications when a game you have played is updated."],"/iXnwU":["Page failed to load"],"4Wc+CC":[[2,"count"],"d ago"],"a0fXhG":["In ",[2,"count"],"d"]}',
+    '{"OacAil":["Available"],"/MxjmI":["More ",[1,"gameName"]," game updates?"],"3TO4/r":["Game Updates"],"diGDe0":["Receive a notification when a game you have played is updated."],"ZZIP+o":["A Game I\'ve played in the past receives an update"],"/iXnwU":["Page failed to load"],"4Wc+CC":[[2,"count"],"d ago"],"a0fXhG":["In ",[2,"count"],"d"]}',
 );
