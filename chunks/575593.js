214e0b4b181e46a9b39975694441dbfde263392d
DiscrapperManager <admin@minerpl.xyz>
2026-03-24 02:36:35 +0100

    canaryRelease: 515357
    Main scripts: 3
    Lazy Loaded Scripts: 2078
    Chunks:
     * Main Files: 13378
     * Scripts: 12531
    Strings: 24200
    Routes: 800
    Experiments Count: 105
    Apex Experiments Count: 223

--- chunks/575593.js
+++ chunks/575593.js
@@ -6,6 +6,7 @@ var r = (function (e) {
         (e[(e.AVATAR_DECORATION = 0)] = "AVATAR_DECORATION"),
         (e[(e.PROFILE_EFFECT = 1)] = "PROFILE_EFFECT"),
         (e[(e.NAMEPLATE = 2)] = "NAMEPLATE"),
+        (e[(e.PROFILE_FRAME = 3)] = "PROFILE_FRAME"),
         (e[(e.BUNDLE = 1e3)] = "BUNDLE"),
         (e[(e.VARIANTS_GROUP = 2e3)] = "VARIANTS_GROUP"),
         (e[(e.EXTERNAL_SKU = 3e3)] = "EXTERNAL_SKU"),
--- chunks/727170.js
+++ chunks/727170.js
@@ -2,8 +2,8 @@
 n.d(t, { A: () => _ });
 var r = n(64700),
     i = n(575593),
-    a = n(417597),
-    s = n(954571),
+    s = n(417597),
+    a = n(954571),
     o = n(590180),
     l = n(993408),
     u = n(652215),
@@ -11,6 +11,7 @@ var r = n(64700),
 let d = {
         [i.R.AVATAR_DECORATION]: c.Ae.AVATAR_DECORATION,
         [i.R.PROFILE_EFFECT]: c.Ae.PROFILE_EFFECT,
+        [i.R.PROFILE_FRAME]: void 0,
         [i.R.NAMEPLATE]: void 0,
         [i.R.NONE]: void 0,
         [i.R.BUNDLE]: void 0,
@@ -18,15 +19,15 @@ let d = {
         [i.R.EXTERNAL_SKU]: void 0,
     },
     _ = (e) => {
-        let t = (0, a.bG)([o.A], () => o.A.products);
+        let t = (0, s.bG)([o.A], () => o.A.products);
         return (0, r.useCallback)(
             (n) => {
                 let { type: r, skuId: i } = n,
-                    a = t.get(i);
-                s.default.track(u.HAw.PREMIUM_FEATURE_TRY_OUT, {
+                    s = t.get(i);
+                a.default.track(u.HAw.PREMIUM_FEATURE_TRY_OUT, {
                     feature_name: d[r],
-                    feature_tier: (0, l.G0)(a) ? c.tz.FREE : c.tz.PREMIUM_STANDARD,
-                    feature_selection: a?.name,
+                    feature_tier: (0, l.G0)(s) ? c.tz.FREE : c.tz.PREMIUM_STANDARD,
+                    feature_selection: s?.name,
                     location_stack: e,
                 });
             },
