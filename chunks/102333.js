475731b145be15d3e91dc0a7fd38def173540366
DiscrapperManager <admin@minerpl.xyz>
2026-03-26 00:44:10 +0100

    canaryRelease: 516927
    Main scripts: 3
    Lazy Loaded Scripts: 2214
    Chunks:
     * Main Files: 13384
     * Scripts: 12750
    Strings: 24249
    Routes: 799
    Experiments Count: 89
    Apex Experiments Count: 227

--- chunks/102333.js
+++ chunks/102333.js
@@ -1,10 +1,9 @@
-i.d(e, { h: () => u });
-var n = i(419954),
-    l = i(780964),
-    s = i(959770),
-    r = i(887870),
-    a = i(985018);
-let u = (0, n.zZ)(l.X.NOTIFICATIONS_BADGES_CATEGORY, {
+n.d(t, { h: () => r });
+var i = n(419954),
+    s = n(780964),
+    l = n(959770),
+    a = n(985018);
+let r = (0, i.zZ)(s.X.NOTIFICATIONS_BADGES_CATEGORY, {
     useTitle: () => a.intl.string(a.t.l6w3Vj),
-    buildLayout: () => [s.f, r.N],
+    buildLayout: () => [l.f],
 });
--- chunks/177164.js
+++ chunks/177164.js
@@ -1,11 +1,12 @@
-i.d(e, { w: () => o });
-var n = i(419954),
-    l = i(780964),
-    s = i(385465),
-    r = i(949059),
-    a = i(897027),
-    u = i(985018);
-let o = (0, n.zZ)(l.X.NOTIFICATIONS_OVERVIEW_CATEGORY, {
-    useTitle: () => u.intl.string(u.t["/dp6yY"]),
-    buildLayout: () => [s.d, a.H, r.p],
+n.d(t, { w: () => c });
+var i = n(419954),
+    s = n(780964),
+    l = n(385465),
+    a = n(949059),
+    r = n(897027),
+    o = n(288905),
+    d = n(985018);
+let c = (0, i.zZ)(s.X.NOTIFICATIONS_OVERVIEW_CATEGORY, {
+    useTitle: () => d.intl.string(d.t["/dp6yY"]),
+    buildLayout: () => [l.d, o.N, r.H, a.p],
 });
