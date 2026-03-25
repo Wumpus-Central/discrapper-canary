f8f10b1be833bbbe956ebc0c87c6ee9dba69aed1
DiscrapperManager <admin@minerpl.xyz>
2026-03-26 00:02:11 +0100

    canaryRelease: 516874
    Main scripts: 3
    Lazy Loaded Scripts: 2213
    Chunks:
     * Main Files: 13381
     * Scripts: 12744
    Strings: 24248
    Routes: 799
    Experiments Count: 89
    Apex Experiments Count: 226

--- chunks/93055.js
+++ chunks/93055.js
@@ -1,5 +1,5 @@
 "use strict";
-a.d(t, { Af: () => p, CJ: () => b, DZ: () => g, Rm: () => A, TW: () => f, e4: () => h, m_: () => m }), a(321073);
+a.d(t, { Af: () => m, CJ: () => A, DZ: () => g, Rm: () => b, TW: () => f, e4: () => h, m_: () => p }), a(321073);
 var n = a(311907),
     i = a(873298);
 a(734057);
@@ -8,20 +8,20 @@ var r = a(967198),
     o = a(474090);
 a(661191);
 var l = a(181079),
-    _ = a(379587),
-    c = a(11531),
+    c = a(379587),
+    _ = a(11531),
     d = a(5180),
     u = a(788868);
 function f(e) {
-    let { enabled: t, hasHigherPrivileges: a } = (0, _.m)({ location: e }),
+    let { enabled: t, hasHigherPrivileges: a } = (0, c.m)({ location: e }),
         i = (0, n.bG)([s.default], () => s.default.getCurrentUser()),
         r = o.Ay.isPremiumExactly(i, u.PremiumTypes.TIER_2);
     return { hasAccess: t && r, isExperimentEnabled: t, hasHigherPrivileges: a };
 }
-function p(e) {
+function m(e) {
     return (0, n.bG)([l.A], () => l.A.getFavorite(e));
 }
-function m() {
+function p() {
     let e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : l.A.getFavoriteChannels(),
         t = [{ id: null, name: "" }];
     for (let a in e) {
@@ -30,8 +30,8 @@ function m() {
     }
     return t.sort((t, a) => (e[t.id]?.order ?? 0) - (e[a.id]?.order ?? 0)), t;
 }
-function A() {
-    return (0, n.bG)([l.A], () => m(l.A.getFavoriteChannels()));
+function b() {
+    return (0, n.bG)([l.A], () => p(l.A.getFavoriteChannels()));
 }
 function g() {
     let e = (0, n.bG)([r.A], () => r.A.getGuildId());
@@ -45,13 +45,13 @@ function h(e, t) {
             () => null != e && (l.A.isFavorite(e.id) || (e.isThread() && l.A.isFavorite(e.parent_id))),
             [e],
         );
-    return !a || (i && r) ? (e ?? null) : null;
+    return a && (!i || !r || e?.isCategory()) ? null : (e ?? null);
 }
-function b() {
-    let e = (0, c.J)((e) => e.favoriteAdded);
+function A() {
+    let e = (0, _.J)((e) => e.favoriteAdded);
     return {
         favoriteAdded: e,
-        notifyFavoriteAdded: (0, c.J)((e) => e.notifyFavoriteAdded),
-        clearFavoriteAdded: (0, c.J)((e) => e.clearFavoriteAdded),
+        notifyFavoriteAdded: (0, _.J)((e) => e.notifyFavoriteAdded),
+        clearFavoriteAdded: (0, _.J)((e) => e.clearFavoriteAdded),
     };
 }
