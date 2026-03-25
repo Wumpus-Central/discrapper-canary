bc8ef810040123952d1a1a015e580e00be183e7e
DiscrapperManager <admin@minerpl.xyz>
2026-03-25 19:49:18 +0100

    canaryRelease: 516512
    Main scripts: 3
    Lazy Loaded Scripts: 2211
    Chunks:
     * Main Files: 13377
     * Scripts: 12742
    Strings: 24205
    Routes: 799
    Experiments Count: 92
    Apex Experiments Count: 224

--- chunks/469993.js
+++ chunks/469993.js
@@ -1,43 +1,42 @@
 "use strict";
-n.d(t, { KD: () => f, ME: () => d, Sm: () => _, Z1: () => a, oS: () => s });
-var r = n(967245),
-    i = n(652215);
-function s() {
-    let { isUserEligibleForCreatorMonetization: e } = r.i.useExperiment(
-        { location: "e3bb71_1" },
-        { autoTrackExposure: !1 },
-    );
-    return e;
+n.d(t, { KD: () => m, ME: () => p, Sm: () => h, Z1: () => u, oS: () => l });
+var r = n(311907),
+    i = n(287809),
+    s = n(967245),
+    a = n(652215);
+let o = new Set(["US"]);
+function l() {
+    return (0, r.bG)([i.default], () => {
+        let e = i.default.getCurrentUser()?.storeCountry?.country;
+        return null != e && o.has(e);
+    });
 }
-function a() {
-    let { isUserEligibleForCreatorMonetization: e } = r.i.getCurrentConfig(
-        { location: "e3bb71_2" },
-        { autoTrackExposure: !1 },
-    );
-    return e;
-}
-function o(e) {
-    return null != e;
-}
-function l(e) {
-    return null != e;
-}
-function u(e) {
-    return null != e && e.features.has(i.GuildFeatures.CREATOR_MONETIZABLE_WHITEGLOVE);
+function u() {
+    let e = i.default.getCurrentUser()?.storeCountry?.country;
+    return null != e && o.has(e);
 }
 function c(e) {
-    return null != e && e.features.has(i.GuildFeatures.CREATOR_MONETIZABLE_WHITEGLOVE);
+    return null != e;
 }
 function d(e) {
-    let t = o(e?.id),
-        n = u(e);
-    return t || n;
+    return null != e;
 }
 function _(e) {
-    return l(e?.id) || c(e);
+    return null != e && e.features.has(a.GuildFeatures.CREATOR_MONETIZABLE_WHITEGLOVE);
 }
-function f() {
-    let { enableCreatorMonetizationNagActivateForUser: e } = r.w.getCurrentConfig(
+function f(e) {
+    return null != e && e.features.has(a.GuildFeatures.CREATOR_MONETIZABLE_WHITEGLOVE);
+}
+function p(e) {
+    let t = c(e?.id),
+        n = _(e);
+    return t || n;
+}
+function h(e) {
+    return d(e?.id) || f(e);
+}
+function m() {
+    let { enableCreatorMonetizationNagActivateForUser: e } = s.w.getCurrentConfig(
         { location: "e3bb71_11" },
         { autoTrackExposure: !0 },
     );
--- chunks/967245.js
+++ chunks/967245.js
@@ -1,29 +1,15 @@
 "use strict";
-n.d(t, { i: () => i, w: () => s });
-var r = n(600975);
-let i = (0, r.C)({
-        kind: "user",
-        id: "2022-04_creator_monetization_country_allowlist",
-        label: "Creator Monetization Available in Country",
-        defaultConfig: { isUserEligibleForCreatorMonetization: !1 },
-        treatments: [
-            {
-                id: 1,
-                label: "Enables users to view Creator Monetization entrypoints",
-                config: { isUserEligibleForCreatorMonetization: !0 },
-            },
-        ],
-    }),
-    s = (0, r.C)({
-        kind: "user",
-        id: "2023-01_creator_monetization_nag_activate_users",
-        label: "Creator Monetization Nag Activate Users",
-        defaultConfig: { enableCreatorMonetizationNagActivateForUser: !1 },
-        treatments: [
-            {
-                id: 1,
-                label: "Enables users to see nag bar to activate server subscriptions",
-                config: { enableCreatorMonetizationNagActivateForUser: !0 },
-            },
-        ],
-    });
+n.d(t, { w: () => r });
+let r = (0, n(600975).C)({
+    kind: "user",
+    id: "2023-01_creator_monetization_nag_activate_users",
+    label: "Creator Monetization Nag Activate Users",
+    defaultConfig: { enableCreatorMonetizationNagActivateForUser: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: "Enables users to see nag bar to activate server subscriptions",
+            config: { enableCreatorMonetizationNagActivateForUser: !0 },
+        },
+    ],
+});
