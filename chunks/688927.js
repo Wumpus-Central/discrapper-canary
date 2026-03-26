a2f22fa8eb67d077707f1a44b780af5b23bc040b
DiscrapperManager <admin@minerpl.xyz>
2026-03-26 18:47:56 +0100

    canaryRelease: 517334
    Main scripts: 3
    Lazy Loaded Scripts: 2216
    Chunks:
     * Main Files: 13385
     * Scripts: 12763
    Strings: 24306
    Routes: 800
    Experiments Count: 88
    Apex Experiments Count: 228

--- chunks/688927.js
+++ chunks/688927.js
@@ -1,11 +1,11 @@
 n.d(t, { A: () => c });
 var l = n(627968),
     a = n(64700),
-    i = n(397927),
-    r = n(46054),
+    r = n(397927),
+    i = n(46054),
     s = n(861662),
     d = n(985018),
-    o = n(888791);
+    o = n(991764);
 let c = (e) => {
     let {
             node: { header: t, subheader: n, button: c },
@@ -14,17 +14,22 @@ let c = (e) => {
             hideTitle: m = !1,
         } = e,
         x = c?.type === "submit",
-        p = a.useMemo(() => r.A.reactParserFor({ ...r.A.defaultRules, link: s.B }), []);
+        p = a.useMemo(() => i.A.reactParserFor({ ...i.A.defaultRules, link: s.B }), []);
     return (0, l.jsxs)("div", {
         className: o.N,
         children: [
             m || null == t || "" === t
                 ? null
-                : (0, l.jsx)(i.Heading, { variant: "heading-lg/semibold", color: "text-strong", children: t }),
+                : (0, l.jsx)(r.Heading, { variant: "heading-lg/semibold", color: "text-strong", children: t }),
             null != n && "" !== t
-                ? (0, l.jsx)(i.Text, { variant: "text-md/normal", color: "text-subtle", children: p(n) })
+                ? (0, l.jsx)(r.Text, {
+                      variant: "text-md/normal",
+                      color: "text-subtle",
+                      className: o.m,
+                      children: p(n),
+                  })
                 : null,
-            x && !u && !_ && (0, l.jsx)(i.Text, { variant: "text-sm/normal", children: d.intl.format(d.t.Q0tSKT, {}) }),
+            x && !u && !_ && (0, l.jsx)(r.Text, { variant: "text-sm/normal", children: d.intl.format(d.t.Q0tSKT, {}) }),
         ],
     });
 };
--- chunks/991764.js
+++ chunks/991764.js
@@ -1,1 +1,1 @@
-e.exports = { N: "headerContainer_bdedd9" };
+e.exports = { N: "headerContainer_bdedd9", m: "subheader_bdedd9" };
