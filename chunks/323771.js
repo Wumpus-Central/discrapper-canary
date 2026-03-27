4c3235ac8e7bfe1d3da63cdf4b962bcd72c1aa1e
DiscrapperManager <admin@minerpl.xyz>
2026-03-27 20:28:14 +0100

    canaryRelease: 518408
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13391
     * Scripts: 12777
    Strings: 24352
    Routes: 802
    Experiments Count: 81
    Apex Experiments Count: 229

--- chunks/323771.js
+++ chunks/323771.js
@@ -1,1 +1,3 @@
-p.exports = JSON.parse('{"M6cPwB":["Snekugle"]}');
+e.exports = JSON.parse(
+    '{"+utqaz":["Baggrundsmulighed for gave: ",[1,"giftStyle"]],"B9XqQk":["Kasse"],"AJ4iir":["Kage"],"P5keo3":["Kiste"],"w84vET":["Kaffe"],"6dCq/u":["Kop"],"+HMF8k":["Nitro-ween"],"vd1fu/":["Julekasse"],"aubYGR":["Julekage"],"vjxYqU":["Julekiste"],"bHuJLa":["Julekaffe"],"M6cPwB":["Snekugle"],"GzPel1":["Kasse"]}',
+);
--- chunks/751196.js
+++ chunks/751196.js
@@ -1,3 +1,3 @@
 a.exports = JSON.parse(
-    '{"xE3lEA":["xE3lEA"],"fNMuJd":["fNMuJd"],"OAahQa":["OAahQa"],"i6Y0Ax":["i6Y0Ax"],"jkCzLT":["jkCzLT"]}',
+    '{"xE3lEA":["xE3lEA"],"9lfe0y":["9lfe0y"],"fNMuJd":["fNMuJd"],"OAahQa":["OAahQa"],"lCPu7w":["lCPu7w"],"i6Y0Ax":["i6Y0Ax"],"jkCzLT":["jkCzLT"]}',
 );
