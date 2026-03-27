4537f23a5bbbd0bab66d4ede6bafc5de95de151b
DiscrapperManager <admin@minerpl.xyz>
2026-03-27 17:27:47 +0100

    canaryRelease: 518122
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13391
     * Scripts: 12773
    Strings: 24346
    Routes: 801
    Experiments Count: 83
    Apex Experiments Count: 227

--- chunks/384826.js
+++ chunks/384826.js
@@ -1,14 +1,14 @@
-n.d(t, { A: () => o });
+n.d(t, { A: () => d });
 var l = n(627968),
     a = n(64700),
-    i = n(397927),
-    r = n(518977),
-    s = n(473169);
-let o = (e) => {
-    let { element: t, onChange: n, state: o } = e,
-        d = t.name,
+    r = n(397927),
+    i = n(518977),
+    s = n(885106);
+let d = (e) => {
+    let { element: t, onChange: n, state: d } = e,
+        o = t.name,
         { title: c, options: u } = t.data,
-        _ = o?.[d]?.value ?? void 0,
+        _ = d?.[o]?.value ?? void 0,
         [m, x] = a.useState(_);
     a.useEffect(() => {
         x(_);
@@ -19,28 +19,29 @@ let o = (e) => {
                     .map((e) => {
                         let t = e.label;
                         try {
-                            t = (0, r.Gw)(e.value);
+                            t = (0, i.Gw)(e.value);
                         } catch {}
                         return { id: e.id ?? e.value, value: e.value, label: t };
                     })
                     .sort((e, t) => e.label.localeCompare(t.label)),
             [u],
         ),
-        g = a.useCallback(
+        h = a.useCallback(
             (e) => {
-                null != e && (x(e), n(d, e));
+                null != e && (x(e), n(o, e));
             },
-            [n, d],
+            [n, o],
         );
     return (0, l.jsx)("div", {
         className: s.QB,
-        children: (0, l.jsx)(i.ZiE, {
+        children: (0, l.jsx)(r.ZiE, {
             label: c,
             value: m,
             required: t.should_submit_data,
-            onSelectionChange: g,
+            onSelectionChange: h,
             options: p,
             selectionMode: "single",
+            maxOptionsVisible: 4,
             fullWidth: !0,
         }),
     });
