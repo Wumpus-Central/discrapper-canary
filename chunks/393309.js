7aa68b5a0ac0a1a58bd2e15fffa84864e8c85166
DiscrapperManager <admin@minerpl.xyz>
2026-03-27 21:51:18 +0100

    canaryRelease: 518527
    Main scripts: 3
    Lazy Loaded Scripts: 2218
    Chunks:
     * Main Files: 13390
     * Scripts: 12771
    Strings: 24363
    Routes: 802
    Experiments Count: 80
    Apex Experiments Count: 228

--- chunks/393309.js
+++ chunks/393309.js
@@ -1,35 +1,36 @@
 "use strict";
-n.d(t, { EN: () => M, Iy: () => x, Nw: () => G, jk: () => w, l1: () => k, r$: () => U, w0: () => F }), n(321073);
+n.d(t, { EN: () => P, Iy: () => x, Nw: () => F, jk: () => M, l1: () => U, r$: () => G, w0: () => V }), n(321073);
 var r = n(64700),
     i = n(665260),
     s = n(562465),
-    a = n(73153),
-    o = n(157559),
-    l = n(465532),
-    u = n(843472),
-    c = n(608299),
-    d = n(207777),
-    _ = n(496040),
-    f = n(853742),
-    p = n(301169),
-    h = n(451909),
-    m = n(195880),
-    E = n(677413),
-    g = n(734057),
-    A = n(31717),
-    I = n(320501),
-    T = n(101392),
-    S = n(927813),
-    y = n(661191),
-    v = n(292348),
-    N = n(268761),
-    C = n(406704),
-    R = n(474078),
-    O = n(37411),
-    b = n(652215),
-    D = n(381941),
-    L = n(985018),
-    w = (function (e) {
+    a = n(582754),
+    o = n(73153),
+    l = n(157559),
+    u = n(465532),
+    c = n(843472),
+    d = n(608299),
+    _ = n(207777),
+    f = n(496040),
+    p = n(853742),
+    h = n(301169),
+    m = n(451909),
+    E = n(195880),
+    g = n(677413),
+    A = n(734057),
+    I = n(31717),
+    T = n(320501),
+    S = n(101392),
+    y = n(927813),
+    v = n(661191),
+    N = n(292348),
+    C = n(268761),
+    R = n(406704),
+    O = n(474078),
+    b = n(37411),
+    D = n(652215),
+    L = n(381941),
+    w = n(985018),
+    M = (function (e) {
         return (
             (e[(e.Disabled = 1)] = "Disabled"),
             (e[(e.Enabled = 2)] = "Enabled"),
@@ -37,25 +38,25 @@ var r = n(64700),
             e
         );
     })({});
-function M(e) {
-    let t = (0, C.n)(e);
-    return (0, C.Tb)(e) ? (t ? 2 : 3) : 1;
+function P(e) {
+    let t = (0, R.n)(e);
+    return (0, R.Tb)(e) ? (t ? 2 : 3) : 1;
 }
 function x(e, t) {
     return 3 === t || (e.isPrivate ?? !1);
 }
-function P(e, t) {
+function k(e, t) {
     return e.length > t ? e.substring(0, t) + "..." : e;
 }
-function k(e, t) {
-    let n = null == t ? null : I.A.getMessage(e.id, t),
+function U(e, t) {
+    let n = null == t ? null : T.A.getMessage(e.id, t),
         r = n?.embeds?.[0]?.rawTitle ?? "",
         i = n?.poll?.question?.text ?? "";
-    if ("" !== r) return P(r, 40);
+    if ("" !== r) return k(r, 40);
     {
-        if ("" !== i) return P(i, 80);
-        let t = h.Ay.unparse(n?.content ?? "", e.id, !0),
-            r = (0, R.A)(t.split("\n")[0], !0);
+        if ("" !== i) return k(i, 80);
+        let t = m.Ay.unparse(n?.content ?? "", e.id, !0),
+            r = (0, O.A)(t.split("\n")[0], !0);
         r = r.replace(/^[ #-]+/, "");
         let s = [];
         for (;;) {
@@ -72,18 +73,18 @@ function k(e, t) {
             if (t.length > 40) break;
             a = t;
         }
-        return P(a, 40);
+        return k(a, 40);
     }
 }
-function U(e) {
+function G(e) {
     let {
         parentChannel: t,
         parentMessageId: n,
         threadSettings: i,
         privateThreadMode: a,
         location: o,
-        onThreadCreated: u,
-        useDefaultThreadName: d,
+        onThreadCreated: l,
+        useDefaultThreadName: c,
         uploadHandler: _,
     } = e;
     return r.useCallback(
@@ -91,58 +92,58 @@ function U(e) {
             let p = null == n,
                 h = x(i, a),
                 m = i.name ?? "";
-            if ("" === m && d) {
-                let e = k(t, n);
-                m = "" !== e ? e : L.intl.string(L.t["7Xm5QI"]);
+            if ("" === m && c) {
+                let e = U(t, n);
+                m = "" !== e ? e : w.intl.string(w.t["7Xm5QI"]);
             }
-            let E = (0, N.Gl)(t),
-                I = g.A.getChannel(y.default.castMessageIdAsChannelId(n)),
-                T = await B(t, [], void 0, () => {
-                    let e = null != n ? b.Rsh.CHANNEL_MESSAGE_THREADS(t.id, n) : b.Rsh.CHANNEL_THREADS(t.id);
+            let E = (0, C.Gl)(t),
+                g = A.A.getChannel(v.default.castMessageIdAsChannelId(n)),
+                T = await H(t, [], void 0, () => {
+                    let e = null != n ? D.Rsh.CHANNEL_MESSAGE_THREADS(t.id, n) : D.Rsh.CHANNEL_THREADS(t.id);
                     return s.Bo.post({
                         url: e,
                         body: {
                             name: m,
                             type: h
-                                ? b.rbe.PRIVATE_THREAD
-                                : t.type === b.rbe.GUILD_ANNOUNCEMENT
-                                  ? b.rbe.ANNOUNCEMENT_THREAD
-                                  : b.rbe.PUBLIC_THREAD,
+                                ? D.rbe.PRIVATE_THREAD
+                                : t.type === D.rbe.GUILD_ANNOUNCEMENT
+                                  ? D.rbe.ANNOUNCEMENT_THREAD
+                                  : D.rbe.PUBLIC_THREAD,
                             auto_archive_duration: E,
                             location: o,
                         },
                         rejectWithError: !1,
                     });
                 });
-            T !== I &&
-                (l.A.clearDraft(t.id, A.C.ThreadSettings),
-                l.A.clearDraft(t.id, A.C.FirstThreadMessage),
-                u?.(T),
-                (p || e.length > 0 || (null != r && r.length > 0) || (null != f && f.length > 0)) && V(T, e, r, f, _)),
-                c.A.clearAll(t.id, A.C.FirstThreadMessage);
+            T !== g &&
+                (u.A.clearDraft(t.id, I.C.ThreadSettings),
+                u.A.clearDraft(t.id, I.C.FirstThreadMessage),
+                l?.(T),
+                (p || e.length > 0 || (null != r && r.length > 0) || (null != f && f.length > 0)) && B(T, e, r, f, _)),
+                d.A.clearAll(t.id, I.C.FirstThreadMessage);
         },
-        [t, n, i, u, a, o, d, _],
+        [t, n, i, l, a, o, c, _],
     );
 }
-function G(e, t, n, r, i) {
-    return B(e, [], void 0, () =>
+function F(e, t, n, r, i) {
+    return H(e, [], void 0, () =>
         s.Bo.post({
-            url: b.Rsh.CHANNEL_THREADS(e.id),
+            url: D.Rsh.CHANNEL_THREADS(e.id),
             body: { name: t, type: n, auto_archive_duration: r, location: i },
             rejectWithError: !1,
         }),
     );
 }
-function F(e) {
-    let { parentChannel: t, name: n, appliedTags: a, analyticsLocations: o, onThreadCreated: u, upload: d } = e;
+function V(e) {
+    let { parentChannel: t, name: n, appliedTags: a, analyticsLocations: o, onThreadCreated: l, upload: c } = e;
     return r.useCallback(
         async (e, r, _) => {
-            let h,
+            let f,
                 m = 0,
-                [g, I] = (0, E.A)(e);
-            g && ((e = I), (m = (0, i.UI)(m, b.pr7.SUPPRESS_NOTIFICATIONS)));
-            let T = (0, N.Gl)(t, null),
-                S = b.Rsh.CHANNEL_THREADS(t.id) + "?use_nested_fields=true",
+                [E, A] = (0, g.A)(e);
+            E && ((e = A), (m = (0, i.UI)(m, D.pr7.SUPPRESS_NOTIFICATIONS)));
+            let T = (0, C.Gl)(t, null),
+                S = D.Rsh.CHANNEL_THREADS(t.id) + "?use_nested_fields=true",
                 y = {
                     name: n,
                     auto_archive_duration: T,
@@ -151,12 +152,12 @@ function F(e) {
                 };
             if (null != _ && _.length > 0)
                 try {
-                    let e = await d(_);
-                    (h = e.uploaderFile), (y.message.attachments = e.files.map((e, t) => (0, v.OW)(e, t)));
+                    let e = await c(_);
+                    (f = e.uploaderFile), (y.message.attachments = e.files.map((e, t) => (0, N.OW)(e, t)));
                 } catch (i) {
                     let { file: e, code: n, reason: r } = i;
                     throw (
-                        ((0, p.k)({
+                        ((0, h.k)({
                             file: e,
                             guildId: t.getGuildId(),
                             analyticsLocations: o ?? [],
@@ -166,72 +167,73 @@ function F(e) {
                         i)
                     );
                 }
-            let C = await B(t, o, h, () => s.Bo.post({ url: S, body: y, rejectWithError: !1 }));
+            let v = await H(t, o, f, () => s.Bo.post({ url: S, body: y, rejectWithError: !1 }));
             return (
-                l.A.clearDraft(t.id, A.C.ThreadSettings),
-                l.A.clearDraft(t.id, A.C.FirstThreadMessage),
-                c.A.clearAll(t.id, A.C.FirstThreadMessage),
-                (0, f.Lj)({ guildId: t.guild_id, channelId: t.id, postId: C.id }),
-                u?.(C),
-                C
+                u.A.clearDraft(t.id, I.C.ThreadSettings),
+                u.A.clearDraft(t.id, I.C.FirstThreadMessage),
+                d.A.clearAll(t.id, I.C.FirstThreadMessage),
+                (0, p.Lj)({ guildId: t.guild_id, channelId: t.id, postId: v.id }),
+                l?.(v),
+                v
             );
         },
-        [t, n, a, u, o, d],
+        [t, n, a, l, o, c],
     );
 }
-function V(e, t, n, r, i) {
+function B(e, t, n, r, i) {
     if (null != i && null != r && r.length > 0) i(e, r, t, n);
-    else if (null != n && n.length > 0) return u.A.sendStickers(e.id, n, t, { location: D.Hx.THREAD_CREATION });
-    else return u.A.sendMessage(e.id, h.Ay.parse(e, t), void 0, { location: D.Hx.THREAD_CREATION });
+    else if (null != n && n.length > 0) return c.A.sendStickers(e.id, n, t, { location: L.Hx.THREAD_CREATION });
+    else return c.A.sendMessage(e.id, m.Ay.parse(e, t), void 0, { location: L.Hx.THREAD_CREATION });
 }
-async function B(e, t, n, r) {
+async function H(e, t, n, r) {
     let i,
         s = e.isForumLikeChannel();
     try {
         (i = await r()),
             null == i.body
-                ? o.A.show({ title: L.intl.string(L.t.j2d6Km), body: L.intl.string(L.t.fEptJP) })
-                : (a.h.dispatch({ type: "SLOWMODE_RESET_COOLDOWN", slowmodeType: T.R.CreateThread, channelId: e.id }),
-                  a.h.dispatch({ type: "THREAD_CREATE_LOCAL", channelId: i.body.id }));
+                ? l.A.show({ title: w.intl.string(w.t.j2d6Km), body: w.intl.string(w.t.fEptJP) })
+                : (o.h.dispatch({ type: "SLOWMODE_RESET_COOLDOWN", slowmodeType: S.R.CreateThread, channelId: e.id }),
+                  o.h.dispatch({ type: "THREAD_CREATE_LOCAL", channelId: i.body.id }),
+                  a.OR.announce(w.intl.string(s ? w.t.zDAG2N : w.t.XkUoBb)));
     } catch (r) {
-        if (r.body?.code === b.t02.TOO_MANY_THREADS)
-            o.A.show({
-                title: s ? L.intl.string(L.t.vWNFkx) : L.intl.string(L.t["1KEdvB"]),
-                body: s ? L.intl.string(L.t.KGaiEK) : L.intl.string(L.t.P0wT5S),
+        if (r.body?.code === D.t02.TOO_MANY_THREADS)
+            l.A.show({
+                title: s ? w.intl.string(w.t.vWNFkx) : w.intl.string(w.t["1KEdvB"]),
+                body: s ? w.intl.string(w.t.KGaiEK) : w.intl.string(w.t.P0wT5S),
             });
-        else if (r.body?.code === b.t02.TOO_MANY_ANNOUNCEMENT_THREADS)
-            o.A.show({ title: L.intl.string(L.t["1KEdvB"]), body: L.intl.string(L.t.jDMxz2) });
-        else if (r.body?.code === b.t02.SLOWMODE_RATE_LIMITED) {
+        else if (r.body?.code === D.t02.TOO_MANY_ANNOUNCEMENT_THREADS)
+            l.A.show({ title: w.intl.string(w.t["1KEdvB"]), body: w.intl.string(w.t.jDMxz2) });
+        else if (r.body?.code === D.t02.SLOWMODE_RATE_LIMITED) {
             let t = r.body.retry_after ?? 0;
             t > 0 &&
-                a.h.dispatch({
+                o.h.dispatch({
                     type: "SLOWMODE_SET_COOLDOWN",
                     channelId: e.id,
-                    slowmodeType: T.R.CreateThread,
-                    cooldownMs: t * S.A.Millis.SECOND,
+                    slowmodeType: S.R.CreateThread,
+                    cooldownMs: t * y.A.Millis.SECOND,
                 });
         } else if (429 === r.status)
-            o.A.show({
-                title: s ? L.intl.string(L.t.vWNFkx) : L.intl.string(L.t["1KEdvB"]),
-                body: L.intl.string(L.t.Whhv4w),
+            l.A.show({
+                title: s ? w.intl.string(w.t.vWNFkx) : w.intl.string(w.t["1KEdvB"]),
+                body: w.intl.string(w.t.Whhv4w),
             });
-        else if (O.$j.has(r.body?.code)) throw r;
-        else if (r.body?.code === b.t02.INVALID_FORM_BODY && r.body?.errors?.name != null) throw r;
-        else if (O.F4.has(r.body?.code)) {
+        else if (b.$j.has(r.body?.code)) throw r;
+        else if (r.body?.code === D.t02.INVALID_FORM_BODY && r.body?.errors?.name != null) throw r;
+        else if (b.F4.has(r.body?.code)) {
             if (null != n)
-                if (r.body?.code === b.t02.EXPLICIT_CONTENT) {
-                    let t = (0, m.m)();
+                if (r.body?.code === D.t02.EXPLICIT_CONTENT) {
+                    let t = (0, E.m)();
                     null != r.body.attachments &&
                         r.body.attachments.length > 0 &&
-                        (a.h.dispatch({
+                        (o.h.dispatch({
                             type: "MESSAGE_EXPLICIT_CONTENT_FP_CREATE",
                             messageId: t,
                             channelId: e.id,
                             attachments: r.body.attachments,
                         }),
-                        (0, _.A)(e.id, t));
+                        (0, f.A)(e.id, t));
                 } else
-                    (0, p.k)({
+                    (0, h.k)({
                         file: n,
                         guildId: e.getGuildId(),
                         analyticsLocations: t ?? [],
@@ -240,12 +242,12 @@ async function B(e, t, n, r) {
                     });
             return new Promise((e, t) => {
                 null == r.body && t(),
-                    d.A.addConditionalChangeListener(() => {
-                        let n = d.A.getAndDeleteMostRecentUserCreatedThreadId();
+                    _.A.addConditionalChangeListener(() => {
+                        let n = _.A.getAndDeleteMostRecentUserCreatedThreadId();
                         if (null != n) {
-                            let r = g.A.getChannel(n);
+                            let r = A.A.getChannel(n);
                             return (
-                                a.h.wait(() => {
+                                o.h.wait(() => {
                                     null == r ? t() : e(r);
                                 }),
                                 !1
@@ -253,15 +255,15 @@ async function B(e, t, n, r) {
                         }
                     });
             });
-        } else o.A.show({ title: L.intl.string(L.t.j2d6Km), body: L.intl.string(L.t.fEptJP) });
+        } else l.A.show({ title: w.intl.string(w.t.j2d6Km), body: w.intl.string(w.t.fEptJP) });
     }
-    let l = await new Promise((e, t) => {
+    let u = await new Promise((e, t) => {
         null == i.body && t(),
-            g.A.addConditionalChangeListener(() => {
-                let t = g.A.getChannel(i.body.id);
+            A.A.addConditionalChangeListener(() => {
+                let t = A.A.getChannel(i.body.id);
                 if (null != t)
                     return (
-                        a.h.wait(() => {
+                        o.h.wait(() => {
                             e(t);
                         }),
                         !1
@@ -269,7 +271,7 @@ async function B(e, t, n, r) {
             });
     });
     try {
-        await u.A.fetchMessages({ channelId: l.id, limit: b.EMb });
+        await c.A.fetchMessages({ channelId: u.id, limit: D.EMb });
     } catch (e) {}
-    return l;
+    return u;
 }
