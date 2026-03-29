3ee4c1fc24332546bb4892f03adcc2575d14a7ed
DiscrapperManager <admin@minerpl.xyz>
2026-03-29 12:47:45 +0200

    canaryRelease: 518933
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13388
     * Scripts: 12776
    Strings: 24373
    Routes: 802
    Experiments Count: 77
    Apex Experiments Count: 228

--- chunks/589051.js
+++ chunks/589051.js
@@ -1,39 +1,20 @@
 "use strict";
 n.d(t, {
-    E: () => S,
-    H: () => _,
-    M8: () => T,
-    NI: () => I,
-    X4: () => g,
-    aS: () => f,
-    lR: () => u,
-    m5: () => l,
-    o0: () => h,
-    sq: () => c,
+    E: () => I,
+    H: () => c,
+    M8: () => A,
+    NI: () => g,
+    X4: () => m,
+    aS: () => d,
+    lR: () => o,
+    o0: () => f,
+    sq: () => l,
 });
 var r = n(812729),
     i = n.n(r),
     s = n(600975),
     a = n(945810);
 let o = (0, s.C)({
-    kind: "user",
-    id: "2025-10_overlay_v3_one_click_go_live",
-    label: "Overlay V3 One Click Go Live",
-    defaultConfig: { oneClickGoLiveEnabled: !1, useStreamCtaCopy: !1 },
-    treatments: [
-        { id: 1, label: "Use Stream CTA Copy", config: { oneClickGoLiveEnabled: !1, useStreamCtaCopy: !0 } },
-        {
-            id: 2,
-            label: "Enable One Click Go Live with Stream CTA Copy",
-            config: { oneClickGoLiveEnabled: !0, useStreamCtaCopy: !0 },
-        },
-    ],
-});
-function l(e) {
-    let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1];
-    return o.getCurrentConfig({ location: e }, { autoTrackExposure: t });
-}
-let u = (0, s.C)({
     id: "2025-10_overlay_default_keybind",
     label: "Overlay Default Keybind",
     kind: "user",
@@ -44,11 +25,11 @@ let u = (0, s.C)({
         { id: 3, label: "Ctrl + L Keybind", config: { keybindOverride: "ctrl+l" } },
     ],
 });
-function c(e) {
+function l(e) {
     let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1];
-    return u.getCurrentConfig({ location: e }, { autoTrackExposure: t });
+    return o.getCurrentConfig({ location: e }, { autoTrackExposure: t });
 }
-let d = (0, s.C)({
+let u = (0, s.C)({
     kind: "user",
     id: "2025-10_overlay_negative_widget_testing",
     label: "Overlay Negative Widget Testing",
@@ -117,42 +98,42 @@ let d = (0, s.C)({
         },
     ],
 });
-function _(e) {
-    let t = f(e, !1);
-    if (i()(d.definition.defaultConfig, t)) return "control";
-    let n = d.definition.treatments.find((e) => i()(e.config, t));
+function c(e) {
+    let t = d(e, !1);
+    if (i()(u.definition.defaultConfig, t)) return "control";
+    let n = u.definition.treatments.find((e) => i()(e.config, t));
     return null == n ? "control" : `treatment-${n.id}`;
 }
-function f(e) {
+function d(e) {
     let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1];
-    return d.getCurrentConfig({ location: e }, { autoTrackExposure: t });
+    return u.getCurrentConfig({ location: e }, { autoTrackExposure: t });
 }
-let p = (0, s.C)({
+let _ = (0, s.C)({
     id: "2025-11_overlay_welcome_notification",
     label: "Overlay Welcome Notification",
     kind: "user",
     defaultConfig: { enabled: !1 },
     treatments: [{ id: 1, label: "Modular Experience", config: { enabled: !0 } }],
 });
-function h(e) {
+function f(e) {
     let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1];
-    return p.getCurrentConfig({ location: e }, { autoTrackExposure: t });
+    return _.getCurrentConfig({ location: e }, { autoTrackExposure: t });
 }
-let m = (0, s.C)({
+let p = (0, s.C)({
     id: "2025-11_overlay_bug_reporter",
     label: "Overlay Bug Reporter",
     kind: "user",
     defaultConfig: { enabled: !1 },
     treatments: [{ id: 1, label: "Enable Bug Reporter", config: { enabled: !0 } }],
 });
-function E(e) {
+function h(e) {
     let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1];
-    return m.getCurrentConfig({ location: e }, { autoTrackExposure: t });
+    return p.getCurrentConfig({ location: e }, { autoTrackExposure: t });
 }
-function g(e) {
-    return m.useExperiment({ location: e }).enabled;
+function m(e) {
+    return p.useExperiment({ location: e }).enabled;
 }
-let A = (0, a.mj)({
+let E = (0, a.mj)({
     name: "2025-11-overlay-chat",
     kind: "user",
     defaultConfig: { hasChat: !1, hasFriendList: !1, showNowPlayingForDifferentGames: !1 },
@@ -162,16 +143,12 @@ let A = (0, a.mj)({
         3: { hasChat: !0, hasFriendList: !0, showNowPlayingForDifferentGames: !0 },
     },
 });
-function I(e) {
-    return A.getConfig({ location: e });
+function g(e) {
+    return E.getConfig({ location: e });
 }
-function T(e) {
-    return A.useConfig({ location: e });
+function A(e) {
+    return E.useConfig({ location: e });
 }
-function S() {
-    l("OVERLAY_INITIALIZED"),
-        f("OVERLAY_INITIALIZED"),
-        h("OVERLAY_INITIALIZED"),
-        E("OVERLAY_INITIALIZED"),
-        I("OVERLAY_INITIALIZED");
+function I() {
+    d("OVERLAY_INITIALIZED"), f("OVERLAY_INITIALIZED"), h("OVERLAY_INITIALIZED"), g("OVERLAY_INITIALIZED");
 }
--- chunks/749471.js
+++ chunks/749471.js
@@ -1,167 +1,142 @@
 "use strict";
-n.d(t, { A: () => w }), n(321073), n(667532);
+n.d(t, { A: () => b }), n(321073), n(667532);
 var r = n(627968);
 n(64700);
 var i = n(503698),
     s = n.n(i),
     a = n(158954),
     o = n(554146),
-    l = n(192308),
-    u = n(298990),
-    c = n(684013),
-    d = n(793574),
-    _ = n(429913),
-    f = n(263577),
-    p = n(826673),
-    h = n(589051),
-    m = n(547463),
-    E = n(823901),
-    g = n(592598),
-    A = n(395011),
-    I = n(243612),
-    T = n(119191),
-    S = n(581730),
-    y = n(672396),
-    v = n(652215),
-    N = n(231723),
-    C = n(360469),
-    R = n(985018),
-    O = n(654249),
-    b = n(284839);
-let D = !0;
-function L(e) {
+    l = n(298990),
+    u = n(684013),
+    c = n(429913),
+    d = n(263577),
+    _ = n(826673),
+    f = n(589051),
+    p = n(547463),
+    h = n(823901),
+    m = n(592598),
+    E = n(395011),
+    g = n(243612),
+    A = n(119191),
+    I = n(581730),
+    T = n(672396),
+    S = n(652215),
+    y = n(360469),
+    v = n(985018),
+    N = n(654249),
+    C = n(284839);
+let R = !0;
+function O(e) {
     let { game: t } = e,
-        n = (0, _.h)(t.id);
-    return null == n ? null : (0, r.jsx)(f.V, { src: n.getIconURL(C.iu.LARGE), size: 40 });
+        n = (0, c.h)(t.id);
+    return null == n ? null : (0, r.jsx)(d.V, { src: n.getIconURL(y.iu.LARGE), size: 40 });
 }
-function w(e, t) {
-    if (g.A.isNotificationDisabled(y.KS.WelcomeNudge)) return null;
-    let { enabled: i } = (0, h.o0)("welcomeNotification"),
-        { trackView: _, trackClick: f } = (0, S.Y9)(y.KS.WelcomeNudge, {
-            notif_type: y.KS.WelcomeNudge,
-            secondary_notif_types: t.map((e) => y.Jr[e.type]),
+function b(e, t) {
+    if (m.A.isNotificationDisabled(T.KS.WelcomeNudge)) return null;
+    let { enabled: i } = (0, f.o0)("welcomeNotification"),
+        { trackView: c, trackClick: d } = (0, I.Y9)(T.KS.WelcomeNudge, {
+            notif_type: T.KS.WelcomeNudge,
+            secondary_notif_types: t.map((e) => T.Jr[e.type]),
         }),
-        C = {},
-        w = [],
-        M = [];
-    for (let o of t)
-        switch (o.type) {
-            case y.Jr.WELCOME: {
-                let t = (0, I.tg)(e?.altId ?? e?.id);
+        y = {},
+        b = [],
+        D = [];
+    for (let n of t)
+        switch (n.type) {
+            case T.Jr.WELCOME: {
+                let t = (0, g.tg)(e?.altId ?? e?.id);
                 null != t &&
-                    ((C.cancelText = R.intl.string(R.t["6F9ivu"])),
-                    (C.onCancelClick = (e, n) => {
-                        f("unlock"),
-                            c.A.updateNotificationStatus(n),
-                            c.A.setInputLocked(!1, A.A.getTargetPID()),
-                            (0, u.qf)(t, !1, v.BRT.POPOUT);
+                    ((y.cancelText = v.intl.string(v.t["6F9ivu"])),
+                    (y.onCancelClick = (e, n) => {
+                        d("unlock"),
+                            u.A.updateNotificationStatus(n),
+                            u.A.setInputLocked(!1, E.A.getTargetPID()),
+                            (0, l.qf)(t, !1, S.BRT.POPOUT);
                     })),
-                    i && M.push((0, r.jsx)("div", { className: b.N7, children: (0, T.bi)((0, S.hM)(), R.t.mRbUco) }));
+                    i && D.push((0, r.jsx)("div", { className: C.N7, children: (0, A.bi)((0, I.hM)(), v.t.mRbUco) }));
                 break;
             }
-            case y.Jr.GO_LIVE_VOICE:
-            case y.Jr.GO_LIVE_NON_VOICE: {
-                let { oneClickGoLiveEnabled: e, useStreamCtaCopy: t } = (0, h.m5)("welcomeNotification"),
-                    s = R.intl.string(t ? R.t.pEuzii : R.t.U76Ft2),
-                    o = e
-                        ? (e) => {
-                              let t = () => {
-                                      f("go-live-modal"), c.A.setInputLocked(!1, A.A.getTargetPID());
-                                  },
-                                  n = () => {
-                                      f("one-click-go-live");
-                                  };
-                              (0, E.H)({
-                                  pid: A.A.getTargetPID(),
-                                  analyticsLocation: v.ThZ.OVERLAY_NUDGE,
-                                  allowOneClickGoLive: !0,
-                                  onBeforeShowModal: t,
-                                  onOneClickGoLive: n,
-                                  appContext: v.BRT.POPOUT,
-                              });
-                          }
-                        : (e) => {
-                              f("go-live-modal");
-                              let t = A.A.getTargetPID();
-                              c.A.setInputLocked(!1, t),
-                                  (0, l.openModalLazy)(
-                                      async () => {
-                                          let { default: e } = await Promise.all([n.e("96811"), n.e("48697")]).then(
-                                              n.bind(n, 648230),
-                                          );
-                                          return (n) =>
-                                              (0, r.jsx)(e, {
-                                                  ...n,
-                                                  selectSource: !1,
-                                                  sourcePID: t,
-                                                  analyticsLocations: [d.A.OVERLAY_NUDGE],
-                                              });
-                                      },
-                                      { contextKey: N.KX },
-                                  );
-                          };
+            case T.Jr.GO_LIVE_VOICE:
+            case T.Jr.GO_LIVE_NON_VOICE: {
+                let e = v.intl.string(v.t.U76Ft2),
+                    t = (e) => {
+                        let t = () => {
+                                d("go-live-modal"), u.A.setInputLocked(!1, E.A.getTargetPID());
+                            },
+                            n = () => {
+                                d("one-click-go-live");
+                            };
+                        (0, h.H)({
+                            pid: E.A.getTargetPID(),
+                            analyticsLocation: S.ThZ.OVERLAY_NUDGE,
+                            allowOneClickGoLive: !0,
+                            onBeforeShowModal: t,
+                            onOneClickGoLive: n,
+                            appContext: S.BRT.POPOUT,
+                        });
+                    };
                 i
-                    ? M.push((0, r.jsx)(a.$nd, { size: "sm", variant: "active", onClick: (e) => o(e), text: s }))
-                    : ((C.confirmText = s),
-                      (C.onConfirmClick = (e, t) => {
-                          c.A.updateNotificationStatus(t), o(e);
+                    ? D.push((0, r.jsx)(a.$nd, { size: "sm", variant: "active", onClick: (e) => t(e), text: e }))
+                    : ((y.confirmText = e),
+                      (y.onConfirmClick = (e, n) => {
+                          u.A.updateNotificationStatus(n), t(e);
                       }));
                 break;
             }
-            case y.Jr.CONTENT_INVENTORY: {
-                C.onNotificationShow = () => {
-                    c.A.track(v.HAw.OVERLAY_GAME_INVITE_NOTIFICATION_SHOWN, {
-                        user_ids: o.entries.map((e) => e.author_id),
-                        entry_ids: o.entries.map((e) => e.id),
+            case T.Jr.CONTENT_INVENTORY: {
+                y.onNotificationShow = () => {
+                    u.A.track(S.HAw.OVERLAY_GAME_INVITE_NOTIFICATION_SHOWN, {
+                        user_ids: n.entries.map((e) => e.author_id),
+                        entry_ids: n.entries.map((e) => e.id),
                     });
                 };
                 let t = () =>
-                    (0, r.jsx)(m.ru, {
+                    (0, r.jsx)(p.ru, {
                         gamingId: e?.altId ?? e?.id,
                         maxUserShowCount: 5,
                         variant: i ? "subtle" : "default",
-                        className: s()({ [b.kL]: !0, [b.Wz]: i }),
+                        className: s()({ [C.kL]: !0, [C.Wz]: i }),
                     });
-                i ? w.push(t) : (C.renderFooter = t);
+                i ? b.push(t) : (y.renderFooter = t);
             }
         }
-    M.length > 0 && w.unshift(() => (0, r.jsx)("div", { className: s()(b.kL, b.Wz, b.SH), children: M })),
-        null == C.renderFooter &&
-            w.length > 0 &&
-            (C.renderFooter = (e, t, n) =>
-                (0, r.jsx)("div", { className: b.NG, children: w.filter((e) => null != e).map((r) => r(e, t, n)) }));
-    let x = (0, p.k8)(o.M.OVERLAY_OOP_WELCOME_NUX),
-        P = i
+    D.length > 0 && b.unshift(() => (0, r.jsx)("div", { className: s()(C.kL, C.Wz, C.SH), children: D })),
+        null == y.renderFooter &&
+            b.length > 0 &&
+            (y.renderFooter = (e, t, n) =>
+                (0, r.jsx)("div", { className: C.NG, children: b.filter((e) => null != e).map((r) => r(e, t, n)) }));
+    let L = (0, _.k8)(o.M.OVERLAY_OOP_WELCOME_NUX),
+        w = i
             ? void 0
             : () =>
-                  (0, T.sI)((0, S.Jn)(), R.t["z8/sgJ"], R.intl.string(R.t.jZkzVJ), {
-                      highlightAdminWarningIfElevated: D,
+                  (0, A.sI)((0, I.Jn)(), v.t["z8/sgJ"], v.intl.string(v.t.jZkzVJ), {
+                      highlightAdminWarningIfElevated: R,
                   }),
-        k = i
+        M = i
             ? (0, r.jsx)(a.DZT, {
                   variant: "heading-md/semibold",
                   color: "text-strong",
-                  children: R.intl.string(R.t.KWDIrh),
+                  children: v.intl.string(v.t.KWDIrh),
               })
-            : R.intl.string(R.t.KWDIrh),
-        U = null != e ? (0, r.jsx)(L, { game: e }) : (0, r.jsx)("img", { src: n(513653), className: O.Kk, alt: "" });
+            : v.intl.string(v.t.KWDIrh),
+        x = null != e ? (0, r.jsx)(O, { game: e }) : (0, r.jsx)("img", { src: n(513653), className: N.Kk, alt: "" });
     return {
-        className: i ? b.fV : void 0,
-        icon: U,
-        title: k,
-        hint: P,
-        ...C,
+        className: i ? C.fV : void 0,
+        icon: x,
+        title: M,
+        hint: w,
+        ...y,
         onNotificationShow: (e) => {
-            _(), x || (0, p.Dr)(o.M.OVERLAY_OOP_WELCOME_NUX), C.onNotificationShow?.(e);
+            c(), L || (0, _.Dr)(o.M.OVERLAY_OOP_WELCOME_NUX), y.onNotificationShow?.(e);
         },
         onNotificationClick: (e, t) => {
-            f("unlock"),
-                c.A.setInputLocked(!1, A.A.getTargetPID()),
-                x || (0, p.Dr)(o.M.OVERLAY_OOP_WELCOME_NUX),
-                C.onNotificationClick?.(e, t);
+            d("unlock"),
+                u.A.setInputLocked(!1, E.A.getTargetPID()),
+                L || (0, _.Dr)(o.M.OVERLAY_OOP_WELCOME_NUX),
+                y.onNotificationClick?.(e, t);
         },
         onDismissClick: (e, t) => {
-            f("dismiss"), x || (0, p.Dr)(o.M.OVERLAY_OOP_WELCOME_NUX), C.onDismissClick?.(e, t);
+            d("dismiss"), L || (0, _.Dr)(o.M.OVERLAY_OOP_WELCOME_NUX), y.onDismissClick?.(e, t);
         },
     };
 }
