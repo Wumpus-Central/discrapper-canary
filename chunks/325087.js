b72b6056f9b0568d58dd3e614c9787b1cba91167
DiscrapperManager <admin@minerpl.xyz>
2026-03-30 19:53:54 +0200

    canaryRelease: 519244
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13322
     * Scripts: 12821
    Strings: 24392
    Routes: 802
    Experiments Count: 76
    Apex Experiments Count: 227

--- chunks/325087.js
+++ chunks/325087.js
@@ -1,19 +1,23 @@
 n.d(t, { A: () => c });
 var i = n(627968),
-    r = n(64700),
+    a = n(64700),
     l = n(342494),
-    a = n(976860),
+    r = n(976860),
     s = n(652215),
     o = n(985018),
     d = n(78701);
 function c(e) {
     let { onNavigateToQuestHome: t, onRender: n, onRequestClose: c, targetElementRef: u } = e,
-        A = r.useCallback(() => {
-            t(), (0, a.pX)(s.BVt.QUEST_HOME);
-        }, [t]);
+        A = a.useCallback(() => {
+            t(), (0, r.pX)(s.BVt.QUEST_HOME);
+        }, [t]),
+        [h, _] = a.useState(!1);
     return (
-        r.useEffect(() => {
-            n();
+        a.useEffect(() => {
+            let e = setTimeout(() => {
+                _(!0), n();
+            }, 1e3);
+            return () => clearTimeout(e);
         }, [n]),
         (0, i.jsx)(l.AM, {
             actions: [{ text: o.intl.string(o.t.facYnD), variant: "primary", onClick: A }],
@@ -23,6 +27,7 @@ function c(e) {
             gradientColor: "purple",
             graphic: { type: "image", src: d.A },
             onRequestClose: c,
+            shouldShow: h,
             position: "right",
             size: "lg",
             targetElementRef: u,
