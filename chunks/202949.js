f2ed178713108d245b50f659a969659d246ab891
DiscrapperManager <admin@minerpl.xyz>
2026-03-31 00:28:42 +0200

    canaryRelease: 519715
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13329
     * Scripts: 12828
    Strings: 24398
    Routes: 802
    Experiments Count: 70
    Apex Experiments Count: 230

--- chunks/202949.js
+++ chunks/202949.js
@@ -1,89 +1,35 @@
-n.d(t, { Q: () => f, z: () => S });
+n.d(t, { Q: () => g });
 var i = n(735438),
     s = n.n(i),
-    l = n(311907),
-    a = n(93465),
-    r = n(589051),
-    o = n(592598),
-    d = n(532624),
-    c = n(242286),
-    u = n(256415),
-    m = n(954571),
-    _ = n(350535),
-    g = n(780964),
-    x = n(652215),
-    A = n(672396),
-    h = n(985018);
-let p = T(null);
-function f() {
+    l = n(592598),
+    a = n(532624),
+    r = n(242286),
+    o = n(256415),
+    d = n(954571),
+    c = n(350535),
+    u = n(652215),
+    m = n(672396);
+let _ = x(null);
+function g() {
     var e;
-    let t = T(p);
-    (e = p),
+    let t = x(_);
+    (e = _),
         s().isEqual(s().omit(t, "old_enabled"), s().omit(e, "old_enabled")) ||
-            (m.default.track(x.HAw.OVERLAY_SETTINGS_UPDATED, { ...t }), (p = t));
+            (d.default.track(u.HAw.OVERLAY_SETTINGS_UPDATED, { ...t }), (_ = t));
 }
-function T(e) {
-    let t = u.default.getNotificationPositionMode(),
-        n = t !== x.G6Q.DISABLED,
-        i = d.Ay.getOverlayKeybind(),
-        s = d.Ay.getOverlayChatKeybind();
+function x(e) {
+    let t = o.default.getNotificationPositionMode(),
+        n = t !== u.G6Q.DISABLED,
+        i = a.Ay.getOverlayKeybind(),
+        s = a.Ay.getOverlayChatKeybind();
     return {
-        enabled: c.default.enabled,
+        enabled: r.default.enabled,
         notifications_enabled: n,
         notifications_position: n ? t : null,
-        text_notifications_mode: o.A.isNotificationDisabled(A.KS.TextChat) ? "DISABLED" : "ENABLED",
-        hotkey: null != i ? (0, _.dI)(i.shortcut) : null,
-        text_activation_hotkey: null != s ? (0, _.dI)(s.shortcut) : null,
-        text_opacity_slider: u.default.getTextWidgetOpacity(),
-        old_enabled: e?.enabled ?? c.default.enabled,
+        text_notifications_mode: l.A.isNotificationDisabled(m.KS.TextChat) ? "DISABLED" : "ENABLED",
+        hotkey: null != i ? (0, c.dI)(i.shortcut) : null,
+        text_activation_hotkey: null != s ? (0, c.dI)(s.shortcut) : null,
+        text_opacity_slider: o.default.getTextWidgetOpacity(),
+        old_enabled: e?.enabled ?? r.default.enabled,
     };
-}
-function S(e) {
-    return [
-        {
-            title: h.t.eVE4LX,
-            description: h.t["72WNqk"],
-            disabledSetting: a.M.TEXT_CHAT,
-            key: g.X.OVERLAY_NOTIFICATIONS_TEXT_CHAT,
-        },
-        {
-            title: h.t.oifnSh,
-            description: h.t.bgU5r0,
-            disabledSetting: a.M.WELCOME_GENERAL,
-            key: g.X.OVERLAY_NOTIFICATIONS_WELCOME,
-        },
-        {
-            title: h.t.hqsZJW,
-            description: h.t.kHjdqc,
-            disabledSetting: a.M.GO_LIVE_NUDGE,
-            key: g.X.OVERLAY_NOTIFICATIONS_GO_LIVE,
-        },
-        {
-            title: h.t.sop3rn,
-            description: h.t.pjgffc,
-            disabledSetting: a.M.GAME_ACTIVITY,
-            key: g.X.OVERLAY_NOTIFICATIONS_GAME_ACTIVITY,
-        },
-        {
-            title: h.t["2QVhbb"],
-            description: h.t.wQ4ilB,
-            disabledSetting: a.M.NOW_PLAYING,
-            key: g.X.OVERLAY_NOTIFICATIONS_NOW_PLAYING,
-        },
-        e && {
-            title: h.t.giM9fA,
-            description: h.t.EhAfWj,
-            disabledSetting: a.M.NOW_PLAYING_DIFFERENT_GAMES,
-            key: g.X.OVERLAY_NOTIFICATIONS_NOW_PLAYING_DIFFERENT_GAMES,
-            usePredicate: () => {
-                let { showNowPlayingForDifferentGames: e } = (0, r.M8)(
-                        "OverlayV3NowPlayingDifferentGamesNotificationSetting",
-                    ),
-                    t = (0, l.bG)([o.A], () => o.A.isNotificationDisabledBySetting(a.M.NOW_PLAYING));
-                return !!e && !t;
-            },
-        },
-    ]
-        .filter((e) => !1 !== e)
-        .filter(Boolean);
 }
--- chunks/775121.js
+++ chunks/775121.js
@@ -1,9 +1,9 @@
 "use strict";
-n.d(t, { A: () => C }), n(321073);
+n.d(t, { A: () => N }), n(321073);
 var r = n(507392),
     i = n.n(r),
-    a = n(64460),
-    s = n(626584),
+    s = n(64460),
+    a = n(626584),
     o = n(549205),
     l = n(954571),
     u = n(723702),
@@ -14,9 +14,9 @@ let d = {},
     p = (0, o.I)(window),
     h = (0, u.isMac)() || (0, u.isMacWeb)() ? "cmd" : "ctrl",
     m = (0, u.isMac)() || (0, u.isMacWeb)() ? "opt" : "alt",
-    g = (0, u.isMac)() || (0, u.isMacWeb)() ? "return" : "enter",
-    E = [
-        ...a.J.binds,
+    E = (0, u.isMac)() || (0, u.isMacWeb)() ? "return" : "enter",
+    g = [
+        ...s.J.binds,
         "mod+shift+[",
         "mod+shift+]",
         "mod+[",
@@ -31,6 +31,7 @@ let d = {},
         "mod+plus",
         "mod+minus",
         "mod+0",
+        "cmd+shift+/",
     ].map((e) => e.replace("mod", h)),
     A = () => [],
     I = [];
@@ -38,48 +39,48 @@ function T(e, t) {
     let n = (e) => t(e, e.key);
     document.addEventListener(e, n), I.push(() => document.removeEventListener(e, n));
 }
-function y(e) {
+function S(e) {
     let t = [];
     for (let n of Object.values(e)) null != n && t.push(...n.binds);
     return t.map((e) => e.replace("mod", h));
 }
-function S(e, t) {
+function y(e, t) {
     return (n, r) => (l.default.track(c.HAw.KEYBOARD_SHORTCUT_USED, { shortcut_name: e, shortcut_combo: r }), t(n, r));
 }
 function v(e) {
     for (let [t, n] of Object.entries(e)) {
         if (null == n) continue;
         let e = A();
-        u.isPlatformEmbedded || (e = e.concat(E));
+        u.isPlatformEmbedded || (e = e.concat(g));
         let r = n.binds.filter((t) => ((t = t.replace("mod", h)), 0 > e.indexOf(t)));
         if (0 === r.length) continue;
         let i = n.comboKeysBindGlobal ? p.bindGlobal : p.bind;
         if (
-            (null != n.action && i.call(p, r, S(t, n.action)),
-            null != n.keyup && i.call(p, r, S(t, n.keyup), "keyup"),
+            (null != n.action && i.call(p, r, y(t, n.action)),
+            null != n.keyup && i.call(p, r, y(t, n.keyup), "keyup"),
             null != n.keydown)
         ) {
             let e = r.indexOf("any-character");
             -1 !== e && (T("keydown", n.keydown), r.splice(e, 1)),
-                r.length > 0 && i.call(p, r, S(t, n.keydown), "keydown");
+                r.length > 0 && i.call(p, r, y(t, n.keydown), "keydown");
         }
-        null != n.keypress && i.call(p, r, S(t, n.keypress), "keypress");
+        null != n.keypress && i.call(p, r, y(t, n.keypress), "keypress");
     }
 }
 (0, u.isDesktop)() && new (i())(document.documentElement).bind("backspace", (e) => e.preventDefault());
-let C = {
+let N = {
     combokeys: p,
     modKey: h,
     altKey: m,
-    returnKey: g,
+    returnKey: E,
     setGetKeybindList(e) {
         A = e;
     },
     checkDupes(e) {
         let t = new Set(),
             n = [];
-        for (let r of y(e)) t.has(r) && n.push(r), t.add(r);
-        n.length > 0 && new s.A("Keybinds").warn("Duplicate keyboard shortcuts defined:", n);
+        for (let r of S(e)) t.has(r) && n.push(r), t.add(r);
+        n.length > 0 && new a.A("Keybinds").warn("Duplicate keyboard shortcuts defined:", n);
     },
     setLayout(e) {
         d = e;
@@ -101,7 +102,7 @@ let C = {
         f && this.hasBind(e) && p.unbind(e);
     },
     hasBind(e) {
-        let t = y(d);
+        let t = S(d);
         return (e = (e = e.replace("meta", "cmd")).replace(/right |left /i, "").trim()), t.includes(e);
     },
 };
