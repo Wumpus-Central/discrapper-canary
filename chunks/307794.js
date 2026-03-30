8047921a3dc7b4fa24683ebfd6b28d24d813b32a
DiscrapperManager <admin@minerpl.xyz>
2026-03-31 00:36:35 +0200

    canaryRelease: 519721
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13329
     * Scripts: 12828
    Strings: 24398
    Routes: 802
    Experiments Count: 70
    Apex Experiments Count: 230

--- chunks/307794.js
+++ chunks/307794.js
@@ -1,25 +1,25 @@
-a.d(n, { default: () => k });
+a.d(n, { default: () => m });
 var o = a(627968);
 a(64700);
 var e = a(827734),
-    r = a(397927),
-    s = a(688810),
+    s = a(397927),
+    r = a(688810),
     d = a(87719),
     i = a(244975),
-    c = a(788868),
-    l = a(652215),
+    l = a(788868),
+    c = a(652215),
     p = a(985018),
     f = a(167520),
     u = a(790344),
     A = a(929103);
 function C(t, n) {
-    return (0, o.jsx)(r.LpS, { text: t, className: u.Ad, color: e.A.unsafe_rawColors.BRAND_500.css }, n);
+    return (0, o.jsx)(s.LpS, { text: t, className: u.Ad, color: e.A.unsafe_rawColors.BRAND_500.css }, n);
 }
-function k(t) {
+function m(t) {
     let { guildCount: n, onClose: a, analyticsLocations: e, ...u } = t,
-        k = function (t, n) {
+        m = function (t, n) {
             return (0, o.jsx)(
-                r.DUT,
+                s.DUT,
                 {
                     className: f.C,
                     tag: "span",
@@ -31,23 +31,21 @@ function k(t) {
                 n,
             );
         },
-        { analyticsLocations: m } = (0, s.Ay)(e);
-    return (0, o.jsx)(s.f5, {
-        value: m,
+        { analyticsLocations: L } = (0, r.Ay)(e);
+    return (0, o.jsx)(r.f5, {
+        value: L,
         children: (0, o.jsx)(i.A, {
             artURL: A.A,
             onClose: a,
-            type: c.e.GUILD_CAP_MODAL_UPSELL,
+            type: l.e.GUILD_CAP_MODAL_UPSELL,
             title: p.intl.string(p.t["CoNXB+"]),
-            secondaryCTA: p.intl.string(p.t["13/7kX"]),
-            onSecondaryClick: a,
             body:
-                n < l.qlD
-                    ? p.intl.format(p.t["5qLH7B"], { guildCount: n, onAndMore: k, newBadgeHook: C })
-                    : p.intl.format(p.t.mk9CS6, { onAndMore: k, newBadgeHook: C }),
+                n < c.qlD
+                    ? p.intl.format(p.t["5qLH7B"], { guildCount: n, onAndMore: m, newBadgeHook: C })
+                    : p.intl.format(p.t.mk9CS6, { onAndMore: m, newBadgeHook: C }),
             context:
-                n < l.qlD ? p.intl.formatToPlainString(p.t["C+Hqzs"], { guildCount: n }) : p.intl.string(p.t.m0xavd),
-            glowUp: p.intl.format(p.t["6Dl5X1"], { onAndMore: k }),
+                n < c.qlD ? p.intl.formatToPlainString(p.t["C+Hqzs"], { guildCount: n }) : p.intl.string(p.t.m0xavd),
+            glowUp: p.intl.format(p.t["6Dl5X1"], { onAndMore: m }),
             ...u,
         }),
     });
