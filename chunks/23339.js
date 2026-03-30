966d5732f65d44552abed95a00fca6b1db5d0cbc
DiscrapperManager <admin@minerpl.xyz>
2026-03-30 16:18:34 +0200

    canaryRelease: 519050
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13387
     * Scripts: 12782
    Strings: 24373
    Routes: 802
    Experiments Count: 76
    Apex Experiments Count: 228

--- chunks/23339.js
+++ chunks/23339.js
@@ -1,17 +1,15 @@
-"use strict";
-r.d(t, { S8: () => o, sS: () => i }), r(621466);
-let n = /[\u0300-\u036f]/g,
-    i = null == String.prototype.normalize ? (e) => e : (e) => e.normalize("NFD").replace(n, "").normalize("NFC"),
-    o =
-        null == String.prototype.normalize
-            ? (e) => e
-            : function (e) {
-                  let t = r(209034),
-                      n = "";
-                  return (
-                      [...e.normalize("NFD")].forEach((e) => {
-                          n += t[e] ?? e;
-                      }),
-                      n.normalize("NFD").toLocaleLowerCase()
-                  );
-              };
+t.d(r, { S8: () => n }), t(621466);
+String.prototype.normalize;
+let n =
+    null == String.prototype.normalize
+        ? (e) => e
+        : function (e) {
+              let r = t(209034),
+                  n = "";
+              return (
+                  [...e.normalize("NFD")].forEach((e) => {
+                      n += r[e] ?? e;
+                  }),
+                  n.normalize("NFD").toLocaleLowerCase()
+              );
+          };
