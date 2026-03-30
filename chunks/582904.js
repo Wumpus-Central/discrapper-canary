bf77b3b2a47cd94798419133cea8116683cf3aca
DiscrapperManager <admin@minerpl.xyz>
2026-03-30 02:23:24 +0200

    canaryRelease: 518967
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13387
     * Scripts: 12776
    Strings: 24373
    Routes: 802
    Experiments Count: 76
    Apex Experiments Count: 228

--- chunks/582904.js
+++ chunks/582904.js
@@ -1,58 +1,53 @@
 "use strict";
-n.d(t, { Ln: () => f, Z0: () => g, kt: () => p }), n(321073);
+n.d(t, { Ln: () => p, Z0: () => g, kt: () => _ }), n(321073);
 var i = n(64700),
     s = n(311907),
-    l = n(394577),
-    a = n(21119),
-    r = n(95701),
-    o = n(696451),
-    c = n(576705),
-    d = n(287809),
-    u = n(607567),
-    h = n(488926),
-    A = n(605431),
-    m = n(366251),
-    _ = n(652215);
-function p(e) {
+    l = n(21119),
+    a = n(95701),
+    r = n(696451),
+    o = n(576705),
+    c = n(287809),
+    d = n(607567),
+    u = n(488926),
+    h = n(605431),
+    A = n(366251),
+    m = n(652215);
+function _(e) {
     let { channel: t } = e,
-        n = t?.guild_id,
-        { totalSuggestions: i } = l.A.useExperiment(
-            { guildId: n, location: "useVoiceInviteSuggestions" },
-            { autoTrackExposure: !1 },
-        );
-    return (0, s.yK)([a.A, u.Ay, o.Ay, d.default], () => {
+        n = t?.guild_id;
+    return (0, s.yK)([l.A, d.Ay, r.Ay, c.default], () => {
         if (null == t) return [];
-        let e = a.A.getUserAffinitiesMap(),
-            s = null != t ? u.Ay.getVoiceStatesForChannel(t) : [],
-            l = new Set();
-        s.forEach((e) => l.add(e.user.id));
-        let c = [];
-        for (let e of o.Ay.getMembers(n)) {
-            if (l.has(e.userId)) continue;
-            let n = d.default.getUser(e.userId);
+        let e = l.A.getUserAffinitiesMap(),
+            i = null != t ? d.Ay.getVoiceStatesForChannel(t) : [],
+            s = new Set();
+        i.forEach((e) => s.add(e.user.id));
+        let o = [];
+        for (let e of r.Ay.getMembers(n)) {
+            if (s.has(e.userId)) continue;
+            let n = c.default.getUser(e.userId);
             if (null == n) continue;
-            let i = (0, r.TA)(t.type);
-            (0, h.$3)({ permission: i, user: n, context: t }) && c.push(n);
+            let i = (0, a.TA)(t.type);
+            (0, u.$3)({ permission: i, user: n, context: t }) && o.push(n);
         }
-        let A = (t) => e.get(t)?.vcProbability ?? 0;
-        return c.sort((e, t) => A(t.id) - A(e.id)).slice(0, i);
-    }, [t, n, i]);
+        let h = (t) => e.get(t)?.vcProbability ?? 0;
+        return o.sort((e, t) => h(t.id) - h(e.id)).slice(0, 5);
+    }, [t, n]);
 }
 function g(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {},
         { collapsed: n = !1 } = t,
-        l = (0, s.bG)([m.A], () => m.A.getShouldShowPopover(e.id), [e.id]);
+        l = (0, s.bG)([A.A], () => A.A.getShouldShowPopover(e.id), [e.id]);
     return {
         shouldShow: l && !n,
         dismiss: i.useCallback(() => {
-            (0, A.w)(e.id);
+            (0, h.w)(e.id);
         }, [e]),
     };
 }
-function f(e) {
+function p(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {},
         { collapsed: n = !1 } = t,
-        i = m.A.getShouldShowPopover(e.id),
-        s = c.A.can(_.xBc.CREATE_INSTANT_INVITE, e);
+        i = A.A.getShouldShowPopover(e.id),
+        s = o.A.can(m.xBc.CREATE_INSTANT_INVITE, e);
     return i && s && !n;
 }
--- chunks/587952.js
+++ chunks/587952.js
@@ -1,11 +1,10 @@
 "use strict";
-n.d(t, { $: () => d });
-var r = n(394577),
-    i = n(186369),
-    s = n(140547),
-    a = n(581007),
-    o = n(164891),
-    l = n(164696),
-    u = n(840251),
-    c = n(688151);
-let d = new u.E([r.A, o.A, s.Eh, i.S, a.aC, l.Ay], c.$G.VOICE_CALL, { location: "voice call initiated" });
+n.d(t, { $: () => c });
+var r = n(186369),
+    i = n(140547),
+    s = n(581007),
+    a = n(164891),
+    o = n(164696),
+    l = n(840251),
+    u = n(688151);
+let c = new l.E([a.A, i.Eh, r.S, s.aC, o.Ay], u.$G.VOICE_CALL, { location: "voice call initiated" });
