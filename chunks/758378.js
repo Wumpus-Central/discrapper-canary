ac29bb564d714e2c71321e0e90d4ac1a50514dd7
DiscrapperManager <admin@minerpl.xyz>
2026-03-31 00:34:30 +0200

    canaryRelease: 519719
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13329
     * Scripts: 12828
    Strings: 24398
    Routes: 802
    Experiments Count: 70
    Apex Experiments Count: 230

--- chunks/758378.js
+++ chunks/758378.js
@@ -11,15 +11,13 @@ var i = n(627968),
     m = n(985018),
     _ = n(595241);
 function g() {
-    let e = (0, o.XQ)(),
-        [t, n] = s.useState(!1),
-        [g, x] = s.useState(() => (0, o.TC)()),
-        A = (0, l.bG)([r.Ay], () => r.Ay.getSystemServiceStatus("input-service")),
-        h = s.useCallback(async () => {
-            n(!0), g ? await (0, o.z8)("windows-settings") : await (0, o.sL)("windows-settings"), n(!1), x((0, o.TC)());
-        }, [g]);
-    if (!e) return null;
-    let p = "running" === A.state;
+    let [e, t] = s.useState(!1),
+        [n, g] = s.useState(() => (0, o.TC)()),
+        x = (0, l.bG)([r.Ay], () => r.Ay.getSystemServiceStatus("input-service")),
+        A = s.useCallback(async () => {
+            t(!0), n ? await (0, o.z8)("windows-settings") : await (0, o.sL)("windows-settings"), t(!1), g((0, o.TC)());
+        }, [n]),
+        h = "running" === x.state;
     return (0, i.jsxs)("div", {
         className: _.q,
         children: [
@@ -38,11 +36,11 @@ function g() {
                             helpCenterLink: c.A.getArticleURL(u.MVz.SYSTEM_SERVICE),
                         }),
                     }),
-                    g
+                    n
                         ? (0, i.jsx)(a.Text, {
                               variant: "text-sm/normal",
-                              color: p ? "text-feedback-positive" : "text-feedback-warning",
-                              children: p
+                              color: h ? "text-feedback-positive" : "text-feedback-warning",
+                              children: h
                                   ? m.intl.string(m.t.KHVMkW)
                                   : m.intl.format(m.t["5Rlr0b"], {
                                         status: (function (e) {
@@ -68,17 +66,17 @@ function g() {
                                                 default:
                                                     (0, d.xb)(e.state);
                                             }
-                                        })(A),
+                                        })(x),
                                     }),
                           })
                         : null,
                 ],
             }),
             (0, i.jsx)(a.Button, {
-                variant: g ? "critical-secondary" : "primary",
-                loading: t,
-                onClick: h,
-                text: g ? m.intl.string(m.t.pAwbdL) : m.intl.string(m.t["1iI46O"]),
+                variant: n ? "critical-secondary" : "primary",
+                loading: e,
+                onClick: A,
+                text: n ? m.intl.string(m.t.pAwbdL) : m.intl.string(m.t["1iI46O"]),
             }),
         ],
     });
