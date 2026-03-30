10a8b7d0580da47807b631fc7f99e2cae0ae2c50
DiscrapperManager <admin@minerpl.xyz>
2026-03-30 19:58:27 +0200

    canaryRelease: 519251
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13321
     * Scripts: 12821
    Strings: 24392
    Routes: 802
    Experiments Count: 76
    Apex Experiments Count: 227

--- chunks/667532.js
+++ chunks/667532.js
@@ -1,38 +1,34 @@
-var n = t(557939),
-    o = t(250594),
-    a = t(503199),
-    i = t(496538),
-    s = t(762503),
-    u = t(732376);
-n(
+"use strict";
+var r = n(557939),
+    i = n(250594),
+    s = n(503199),
+    a = n(496538),
+    o = n(762503),
+    l = n(732376),
+    u = 1 !== [].unshift(0),
+    c = function () {
+        try {
+            Object.defineProperty([], "length", { writable: !1 }).unshift();
+        } catch (e) {
+            return e instanceof TypeError;
+        }
+    };
+r(
+    { target: "Array", proto: !0, arity: 1, forced: u || !c() },
     {
-        target: "Array",
-        proto: !0,
-        arity: 1,
-        forced:
-            1 !== [].unshift(0) ||
-            !(function () {
-                try {
-                    Object.defineProperty([], "length", { writable: !1 }).unshift();
-                } catch (r) {
-                    return r instanceof TypeError;
+        unshift: function (e) {
+            var t = i(this),
+                n = s(t),
+                r = arguments.length;
+            if (r) {
+                l(n + r);
+                for (var u = n; u--; ) {
+                    var c = u + r;
+                    u in t ? (t[c] = t[u]) : o(t, c);
                 }
-            })(),
-    },
-    {
-        unshift: function (r) {
-            var e = o(this),
-                t = a(e),
-                n = arguments.length;
-            if (n) {
-                u(t + n);
-                for (var l = t; l--; ) {
-                    var d = l + n;
-                    l in e ? (e[d] = e[l]) : s(e, d);
-                }
-                for (var c = 0; c < n; c++) e[c] = arguments[c];
+                for (var d = 0; d < r; d++) t[d] = arguments[d];
             }
-            return i(e, t + n);
+            return a(t, n + r);
         },
     },
 );
--- chunks/963169.js
+++ chunks/963169.js
@@ -1,16 +1,17 @@
-n.d(e, { MS: () => a, OE: () => o, Qb: () => c });
-var i = n(73153),
-    r = n(976860),
-    l = n(954571),
-    s = n(652215);
-function a() {
-    i.h.dispatch({ type: "ROUTE_CHANGED" });
+"use strict";
+n.d(t, { MS: () => o, OE: () => l, Qb: () => u });
+var r = n(73153),
+    i = n(976860),
+    s = n(954571),
+    a = n(652215);
+function o() {
+    r.h.dispatch({ type: "ROUTE_CHANGED" });
 }
-function o(t) {
-    (0, r.TX)() &&
-        (l.default.track(s.HAw.APP_BACK_FORWARD_NAVIGATED, { nav_direction: -1, nav_trigger: t }), (0, r.aX)());
+function l(e) {
+    (0, i.TX)() &&
+        (s.default.track(a.HAw.APP_BACK_FORWARD_NAVIGATED, { nav_direction: -1, nav_trigger: e }), (0, i.aX)());
 }
-function c(t) {
-    (0, r.TX)() &&
-        (l.default.track(s.HAw.APP_BACK_FORWARD_NAVIGATED, { nav_direction: 1, nav_trigger: t }), (0, r.sY)());
+function u(e) {
+    (0, i.TX)() &&
+        (s.default.track(a.HAw.APP_BACK_FORWARD_NAVIGATED, { nav_direction: 1, nav_trigger: e }), (0, i.sY)());
 }
