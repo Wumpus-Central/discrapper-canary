55df163df17119363718181603d66d8a4c448bf8
DiscrapperManager <admin@minerpl.xyz>
2026-03-31 23:54:23 +0200

    canaryRelease: 520641
    Main scripts: 3
    Lazy Loaded Scripts: 2218
    Chunks:
     * Main Files: 13321
     * Scripts: 12844
    Strings: 24374
    Routes: 804
    Experiments Count: 64
    Apex Experiments Count: 231

--- chunks/723764.js
+++ chunks/723764.js
@@ -1,4 +1,4 @@
-p.r(e), p.d(e, { default: () => s });
-let s = JSON.parse(
-    '{"YnnXb6":["YnnXb6"],"vycLU2":["vycLU2"],"ZeOhh9":["ZeOhh9"],"/Xhh40":["/Xhh40"],"MVB/Ab":["MVB/Ab"],"1S6oAo":["1S6oAo"]}',
+p.r(h), p.d(h, { default: () => b });
+let b = JSON.parse(
+    '{"YnnXb6":["YnnXb6"],"vycLU2":["vycLU2"],"ZeOhh9":["ZeOhh9"],"/Xhh40":["/Xhh40"],"spsZYI":["spsZYI"],"MVB/Ab":["MVB/Ab"],"sbMMtk":["sbMMtk"],"1S6oAo":["1S6oAo"]}',
 );
