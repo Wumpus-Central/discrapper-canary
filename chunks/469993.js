0d7d051461232ed4e669c5fca8d00fff5b293a49
DiscrapperManager <admin@minerpl.xyz>
2026-03-31 19:02:25 +0200

    canaryRelease: 520218
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13325
     * Scripts: 12835
    Strings: 24372
    Routes: 802
    Experiments Count: 70
    Apex Experiments Count: 229

--- chunks/469993.js
+++ chunks/469993.js
@@ -1,33 +1,37 @@
 "use strict";
-n.d(t, { KD: () => f, ME: () => _, Z1: () => u, oS: () => l });
+n.d(t, { KD: () => h, ME: () => p, Z1: () => d, oS: () => c });
 var r = n(311907),
     i = n(287809),
-    s = n(967245),
-    a = n(652215);
-let o = new Set(["US"]);
-function l() {
-    return (0, r.bG)([i.default], () => {
-        let e = i.default.getCurrentUser()?.storeCountry?.country;
-        return null != e && o.has(e);
+    s = n(615405),
+    a = n(967245),
+    o = n(652215);
+let l = new Set(["US"]);
+function u() {
+    return i.default.getCurrentUser()?.storeCountry?.country ?? s.A.ipCountryCode;
+}
+function c() {
+    return (0, r.bG)([i.default, s.A], () => {
+        let e = u();
+        return null != e && l.has(e);
     });
 }
-function u() {
-    let e = i.default.getCurrentUser()?.storeCountry?.country;
-    return null != e && o.has(e);
-}
-function c(e) {
-    return null != e;
-}
-function d(e) {
-    return null != e && e.features.has(a.GuildFeatures.CREATOR_MONETIZABLE_WHITEGLOVE);
+function d() {
+    let e = u();
+    return null != e && l.has(e);
 }
 function _(e) {
-    let t = c(e?.id),
-        n = d(e);
+    return null != e;
+}
+function f(e) {
+    return null != e && e.features.has(o.GuildFeatures.CREATOR_MONETIZABLE_WHITEGLOVE);
+}
+function p(e) {
+    let t = _(e?.id),
+        n = f(e);
     return t || n;
 }
-function f() {
-    let { enableCreatorMonetizationNagActivateForUser: e } = s.w.getCurrentConfig(
+function h() {
+    let { enableCreatorMonetizationNagActivateForUser: e } = a.w.getCurrentConfig(
         { location: "e3bb71_11" },
         { autoTrackExposure: !0 },
     );
