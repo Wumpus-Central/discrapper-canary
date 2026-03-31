160e0f80137c019544d4355a2b6d2ac08dbe1bb5
DiscrapperManager <admin@minerpl.xyz>
2026-04-01 00:44:23 +0200

    canaryRelease: 520720
    Main scripts: 3
    Lazy Loaded Scripts: 2218
    Chunks:
     * Main Files: 13319
     * Scripts: 12844
    Strings: 24402
    Routes: 804
    Experiments Count: 62
    Apex Experiments Count: 231

--- chunks/854514.js
+++ chunks/854514.js
@@ -1,8 +1,8 @@
-n.d(t, { Ae: () => v, Ay: () => b, Hk: () => O });
+n.d(t, { Ae: () => v, Ay: () => y, Hk: () => O });
 var i = n(627968),
-    a = n(64700),
-    l = n(503698),
-    r = n.n(l),
+    l = n(64700),
+    a = n(503698),
+    r = n.n(a),
     s = n(323889),
     o = n(311907),
     d = n(397927),
@@ -12,24 +12,24 @@ var i = n(627968),
     h = n(579473),
     _ = n(561844),
     m = n(590202),
-    g = n(415441),
-    p = n(73473),
+    p = n(415441),
+    g = n(73473),
     E = n(139384),
     I = n(729168),
     f = n(7832),
     C = n(654487),
     T = n(49999),
     N = n(985018),
-    S = n(684027),
-    x = n(311401);
+    S = n(225130),
+    x = n(192164);
 function v() {
     return (0, i.jsx)("div", { className: S.Np });
 }
-function y(e) {
+function b(e) {
     let { assetImage: t, assetVideo: n } = e,
-        l = a.useRef(null);
-    (0, E.A)(l, C.rE.QUEST_HOME_DESKTOP);
-    let s = a.useMemo(() => (null != n ? (0, h.WV)(n.url) : null) ?? t.url, [n, t]);
+        a = l.useRef(null);
+    (0, E.A)(a, C.rE.QUEST_HOME_DESKTOP);
+    let s = l.useMemo(() => (null != n ? (0, h.WV)(n.url) : null) ?? t.url, [n, t]);
     return null == n
         ? (0, i.jsxs)("div", {
               className: S.Tv,
@@ -55,10 +55,10 @@ function y(e) {
                   (0, i.jsx)(v, {}),
                   (0, i.jsx)("div", {
                       className: S.LO,
-                      children: (0, i.jsx)(g.N, {
+                      children: (0, i.jsx)(p.N, {
                           showImage: !1,
                           showVideo: !0,
-                          assetRef: l,
+                          assetRef: a,
                           imageAsset: {
                               alt: n.altText,
                               className: S.LY,
@@ -76,88 +76,80 @@ function y(e) {
               ],
           });
 }
-function b(e) {
+function y(e) {
     let { config: t, onQuestCtaClick: n } = e,
-        l = (0, o.bG)([u.A], () => u.A.quests),
+        a = (0, o.bG)([u.A], () => u.A.quests),
         { dismissQuestHomeHeroContent: r } = (0, f.I)();
-    return (
-        a.useEffect(() => {
-            r(T.i.INDIRECT_ACTION);
-        }, [r]),
-        (0, i.jsx)(p.R, {
-            adContentId: t.campaignId,
-            adCreativeType: s.p.QUEST_HOME_HERO,
-            questContent: A.uF.QUEST_HOME_HERO,
-            sourceQuestContent: A.uF.QUEST_HOME_HERO,
-            children: (e) =>
-                (0, i.jsx)(I.A, {
-                    ref: e,
-                    topContent: (0, i.jsxs)(d.BJc, {
-                        direction: "horizontal",
-                        justify: "space-between",
-                        align: "start",
-                        children: [
-                            (0, i.jsx)("img", {
-                                src: t.assetSponsorImage.url,
-                                alt: t.assetSponsorImage.altText,
-                                className: S.wm,
-                            }),
-                            (0, i.jsx)(d.Text, {
-                                color: "text-strong",
-                                variant: "text-sm/normal",
-                                className: S.yu,
-                                children: N.intl.string(N.t.OJjFi5),
-                            }),
-                        ],
-                    }),
-                    title: t.labelTitle,
-                    subtitle: t.labelSubtitle,
-                    buttons: (0, i.jsxs)(d.ButtonGroup, {
-                        children: [
+    l.useEffect(() => {
+        r(T.i.INDIRECT_ACTION);
+    }, [r]);
+    let h = l.useMemo(() => t.ctaQuests.find((e) => a.has(e.questId)), [a, t.ctaQuests]);
+    return (0, i.jsx)(g.R, {
+        adContentId: t.campaignId,
+        adCreativeType: s.p.QUEST_HOME_HERO,
+        questContent: A.uF.QUEST_HOME_HERO,
+        sourceQuestContent: A.uF.QUEST_HOME_HERO,
+        children: (e) =>
+            (0, i.jsx)(I.A, {
+                ref: e,
+                topContent: (0, i.jsxs)(d.BJc, {
+                    direction: "horizontal",
+                    justify: "space-between",
+                    align: "start",
+                    children: [
+                        (0, i.jsx)("img", {
+                            src: t.assetSponsorImage.url,
+                            alt: t.assetSponsorImage.altText,
+                            className: S.wm,
+                        }),
+                        (0, i.jsx)(d.Text, {
+                            color: "text-strong",
+                            variant: "text-sm/normal",
+                            className: S.yu,
+                            children: N.intl.string(N.t.OJjFi5),
+                        }),
+                    ],
+                }),
+                title: t.labelTitle,
+                subtitle: t.labelSubtitle,
+                buttons: (0, i.jsxs)(d.ButtonGroup, {
+                    children: [
+                        (0, i.jsx)(d.Button, {
+                            onClick: () => {
+                                (0, c.A)(t.ctaSponsorUrl.url),
+                                    (0, _.vK)({
+                                        adContentId: t.campaignId,
+                                        adCreativeType: s.p.QUEST_HOME_HERO,
+                                        questContent: A.uF.QUEST_HOME_HERO,
+                                        questContentCTA: m.Cy.OPEN_GAME_LINK,
+                                        sourceQuestContent: A.uF.QUEST_HOME_HERO,
+                                    });
+                            },
+                            size: "md",
+                            text: t.ctaSponsorUrl.title,
+                            variant: "overlay-primary",
+                        }),
+                        null != h &&
                             (0, i.jsx)(d.Button, {
                                 onClick: () => {
-                                    (0, c.A)(t.ctaSponsorUrl.url),
+                                    n?.(h.questId),
                                         (0, _.vK)({
                                             adContentId: t.campaignId,
                                             adCreativeType: s.p.QUEST_HOME_HERO,
                                             questContent: A.uF.QUEST_HOME_HERO,
-                                            questContentCTA: m.Cy.OPEN_GAME_LINK,
+                                            questContentCTA: m.Cy.VIEW_QUESTS,
                                             sourceQuestContent: A.uF.QUEST_HOME_HERO,
                                         });
                                 },
                                 size: "md",
-                                text: t.ctaSponsorUrl.title,
-                                variant: "overlay-primary",
+                                text: h.title,
+                                variant: "color-mix",
                             }),
-                            t.ctaQuests.map((e, a) =>
-                                l.has(e.questId)
-                                    ? (0, i.jsx)(
-                                          d.Button,
-                                          {
-                                              onClick: () => {
-                                                  n?.(e.questId),
-                                                      (0, _.vK)({
-                                                          adContentId: t.campaignId,
-                                                          adCreativeType: s.p.QUEST_HOME_HERO,
-                                                          questContent: A.uF.QUEST_HOME_HERO,
-                                                          questContentCTA: m.Cy.VIEW_QUESTS,
-                                                          sourceQuestContent: A.uF.QUEST_HOME_HERO,
-                                                      });
-                                              },
-                                              size: "md",
-                                              text: e.title,
-                                              variant: "color-mix",
-                                          },
-                                          a,
-                                      )
-                                    : null,
-                            ),
-                        ],
-                    }),
-                    background: (0, i.jsx)(y, { assetImage: t.assetHeroImage, assetVideo: t.assetHeroVideo }),
+                    ],
                 }),
-        })
-    );
+                background: (0, i.jsx)(b, { assetImage: t.assetHeroImage, assetVideo: t.assetHeroVideo }),
+            }),
+    });
 }
 function O() {
     return (0, i.jsx)("div", { className: r()(x.iE, x.FG, x.B3), children: (0, i.jsx)(d.y$y, {}) });
