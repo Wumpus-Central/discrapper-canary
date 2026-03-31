f3d592e07d397fbdc77dc428d09947c61be68b3d
DiscrapperManager <admin@minerpl.xyz>
2026-03-31 23:35:16 +0200

    canaryRelease: 520613
    Main scripts: 3
    Lazy Loaded Scripts: 2218
    Chunks:
     * Main Files: 13321
     * Scripts: 12844
    Strings: 24372
    Routes: 804
    Experiments Count: 65
    Apex Experiments Count: 231

--- chunks/115703.js
+++ chunks/115703.js
@@ -1,31 +1,33 @@
 "use strict";
-n.d(t, { A: () => u });
+n.d(t, { A: () => c });
 var r = n(64700),
     i = n(429913),
+    s = n(885151),
     a = n(403362);
-let s = 5;
-function o(e) {
+let o = 5;
+function l(e) {
     return (t, n) => {
         let r = e[t.id]?.score ?? 0,
             i = e[n.id]?.score ?? 0;
         return r !== i ? i - r : 0;
     };
 }
-function l(e) {
+function u(e) {
+    (0, s.W)(e.gameApplicationIds);
     let t = (0, i.A)(e.gameApplicationIds);
     return r.useMemo(() => t.filter(a.Vq), [t]);
 }
-function u(e) {
+function c(e) {
     let t = e.gameActivity,
-        n = l(e),
+        n = u(e),
         i = r.useMemo(() => {
-            let e = o(t);
+            let e = l(t);
             return n.sort(e), n;
         }, [n, t]),
-        a = r.useMemo(() => i.slice(0, s), [i]);
+        s = r.useMemo(() => i.slice(0, o), [i]);
     return {
-        gamesToDisplay: a,
-        lastGameToDisplay: r.useMemo(() => i[s] ?? null, [i]),
-        remainingGames: r.useMemo(() => i.slice(s), [i]),
+        gamesToDisplay: s,
+        lastGameToDisplay: r.useMemo(() => i[o] ?? null, [i]),
+        remainingGames: r.useMemo(() => i.slice(o), [i]),
     };
 }
--- chunks/438842.js
+++ chunks/438842.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { A: () => E });
+n.d(t, { A: () => A });
 var r = n(627968),
     i = n(64700),
     s = n(503698),
@@ -8,37 +8,41 @@ var r = n(627968),
     l = n(990078),
     u = n(397927),
     c = n(573435),
-    d = n(355345);
-let _ = 10,
-    f = 2;
-function p(e) {
+    d = n(885151),
+    _ = n(486020),
+    f = n(439570);
+let p = 10,
+    h = 2;
+function m(e) {
     let { game: t, activityLevel: n, hideTooltip: i } = e,
-        s = t.getIconURL(24);
-    if (null == s) return null;
-    let a = !i,
-        p = n?.level === o._.HIGH,
-        h = (0, r.jsx)(u.Text, { variant: "text-sm/medium", color: "interactive-text-active", children: t.name });
+        { data: s } = (0, d.k)(t.id),
+        a = s?.supplementalData?.iconHash,
+        m = null != a ? _.Ay.getGameAssetURL({ id: t.id, hash: a, size: 24 }) : t.getIconURL(24);
+    if (null == m) return null;
+    let E = !i,
+        g = n?.level === o._.HIGH,
+        A = (0, r.jsx)(u.Text, { variant: "text-sm/medium", color: "interactive-text-active", children: t.name });
     return (0, r.jsx)(l.m, {
-        __unsupportedReactNodeAsText: h,
+        __unsupportedReactNodeAsText: A,
         position: "bottom",
         "aria-label": t.name,
-        shouldShow: a,
+        shouldShow: E,
         children: (0, r.jsxs)("div", {
-            className: d.Gt,
+            className: f.Gt,
             children: [
-                (0, r.jsx)("img", { className: d.T_, src: s, alt: t.name }),
-                p &&
+                (0, r.jsx)("img", { className: f.T_, src: m, alt: t.name }),
+                g &&
                     (0, r.jsx)(c.Ay, {
                         mask: c.hW.GAMEPLAY_HIGH_ACTIVITY_ICON,
-                        width: _ + 2 * f,
-                        height: _ + 2 * f,
-                        className: d.Sy,
+                        width: p + 2 * h,
+                        height: p + 2 * h,
+                        className: f.Sy,
                         children: (0, r.jsx)("div", {
-                            className: d._8,
+                            className: f._8,
                             children: (0, r.jsx)(u.Y3C, {
                                 color: u.LU0.unsafe_rawColors.ORANGE_330.css,
-                                width: _,
-                                height: _,
+                                width: p,
+                                height: p,
                                 size: "custom",
                             }),
                         }),
@@ -47,17 +51,17 @@ function p(e) {
         }),
     });
 }
-function h(e) {
+function E(e) {
     let { games: t, activity: n } = e;
     return (0, r.jsx)("div", {
-        className: d.Fz,
+        className: f.Fz,
         children: t.map((e) =>
             (0, r.jsxs)(
                 "div",
                 {
-                    className: d.ZS,
+                    className: f.ZS,
                     children: [
-                        (0, r.jsx)(p, { game: e, activityLevel: n[e.id] }),
+                        (0, r.jsx)(m, { game: e, activityLevel: n[e.id] }),
                         (0, r.jsx)(u.Text, {
                             variant: "text-xs/medium",
                             color: "interactive-text-active",
@@ -70,35 +74,38 @@ function h(e) {
         ),
     });
 }
-function m(e) {
+function g(e) {
     let { game: t, activityLevel: n } = e;
     return (0, r.jsxs)("div", {
-        className: d.E,
+        className: f.E,
         children: [
-            (0, r.jsx)(p, { game: t, activityLevel: n, hideTooltip: !0 }),
+            (0, r.jsx)(m, { game: t, activityLevel: n, hideTooltip: !0 }),
             (0, r.jsx)(u.Text, { variant: "text-sm/medium", color: "text-default", children: t.name }),
         ],
     });
 }
-function E(e) {
+function A(e) {
     let { gamesToDisplay: t, lastGameToDisplay: n, remainingGames: s, activity: o } = e,
-        c = i.useMemo(() => {
+        { data: c } = (0, d.k)(n?.id),
+        p = c?.supplementalData?.iconHash,
+        h = null != p ? _.Ay.getGameAssetURL({ id: n.id, hash: p, size: 24 }) : n?.getIconURL(24),
+        A = i.useMemo(() => {
             if (null == n) return null;
-            let e = n.getIconURL(24);
+            let e = h;
             if (null == e) return null;
-            if (0 === s.length) return (0, r.jsx)(p, { game: n, activityLevel: o[n.id] });
+            if (0 === s.length) return (0, r.jsx)(m, { game: n, activityLevel: o[n.id] });
             let t = s;
             return (0, r.jsx)(l.m, {
                 "aria-label": "",
                 position: "bottom",
-                __unsupportedReactNodeAsText: (0, r.jsx)(h, { games: t, activity: o }),
+                __unsupportedReactNodeAsText: (0, r.jsx)(E, { games: t, activity: o }),
                 children: (0, r.jsxs)("div", {
-                    className: a()(d.Gt, d.oL),
+                    className: a()(f.Gt, f.oL),
                     children: [
-                        (0, r.jsx)("img", { className: a()(d.T_, d.tJ), src: e, alt: n.name }),
-                        (0, r.jsx)("div", { className: d.Fg }),
+                        (0, r.jsx)("img", { className: a()(f.T_, f.tJ), src: e, alt: n.name }),
+                        (0, r.jsx)("div", { className: f.Fg }),
                         (0, r.jsx)("div", {
-                            className: d.AZ,
+                            className: f.AZ,
                             children: (0, r.jsx)(u.Text, {
                                 variant: "text-xs/normal",
                                 color: "always-white",
@@ -108,13 +115,13 @@ function E(e) {
                     ],
                 }),
             });
-        }, [n, s, o]);
+        }, [n, h, s, o]);
     return null == t || 0 === t.length
         ? null
         : 1 === t.length
-          ? (0, r.jsx)("div", { className: d.kL, children: (0, r.jsx)(m, { game: t[0], activityLevel: o[t[0].id] }) })
+          ? (0, r.jsx)("div", { className: f.kL, children: (0, r.jsx)(g, { game: t[0], activityLevel: o[t[0].id] }) })
           : (0, r.jsxs)("div", {
-                className: d.kL,
-                children: [t.map((e) => (0, r.jsx)(p, { game: e, activityLevel: o[e.id] }, e.id)), c],
+                className: f.kL,
+                children: [t.map((e) => (0, r.jsx)(m, { game: e, activityLevel: o[e.id] }, e.id)), A],
             });
 }
