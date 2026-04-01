9ecee5b7dbce7ea9245a135fb97842fb5c728ddd
DiscrapperManager <admin@minerpl.xyz>
2026-04-01 21:51:07 +0200

    canaryRelease: 521326
    Main scripts: 3
    Lazy Loaded Scripts: 2193
    Chunks:
     * Main Files: 13498
     * Scripts: 12811
    Strings: 24384
    Routes: 807
    Experiments Count: 54
    Apex Experiments Count: 229

--- chunks/610223.js
+++ chunks/610223.js
@@ -1,41 +1,48 @@
 "use strict";
-n.d(t, { T: () => d, W: () => c });
+n.d(t, { T: () => _, W: () => d });
 var r = n(64700),
     i = n(311907),
     s = n(602674),
     a = n(133636);
-let o = (0, s.v)();
-async function l(e) {
-    let t = await fetch(e),
-        n = await o?.decodeAudioData(await t.arrayBuffer()),
-        r = o?.createBufferSource();
-    return null != r && null != n && (r.buffer = n), r;
+let o = new Map(),
+    l = (0, s.v)();
+async function u(e) {
+    let t = o.get(e);
+    if (null != t) {
+        let e = l?.createBufferSource();
+        return null != e && (e.buffer = t), e;
+    }
+    let n = await fetch(e),
+        r = await l?.decodeAudioData(await n.arrayBuffer());
+    null != r && o.set(e, r);
+    let i = l?.createBufferSource();
+    return null != i && null != r && (i.buffer = r), i;
 }
-async function u(e, t) {
-    let n = await l(e);
-    return null == n || null == o ? null : (n.connect(t).connect(o.destination), n.start(), n);
-}
-function c(e) {
-    let t = (0, i.bG)([a.A], () => a.A.soundVolume);
-    return (0, r.useCallback)(() => {
-        let n = o?.createGain();
-        if (null != n) return (n.gain.value = t), u(e, n);
-    }, [e, t]);
+async function c(e, t) {
+    let n = await u(e);
+    return null == n || null == l ? null : (n.connect(t).connect(l.destination), n.start(), n);
 }
 function d(e) {
+    let t = (0, i.bG)([a.A], () => a.A.soundVolume);
+    return (0, r.useCallback)(() => {
+        let n = l?.createGain();
+        if (null != n) return (n.gain.value = t), c(e, n);
+    }, [e, t]);
+}
+function _(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1],
         n = (0, r.useRef)(null),
-        s = (0, r.useRef)(o?.createGain()),
-        l = (0, i.bG)([a.A], () => a.A.musicVolume);
+        s = (0, r.useRef)(l?.createGain()),
+        o = (0, i.bG)([a.A], () => a.A.musicVolume);
     (0, r.useEffect)(() => {
         (async () => {
             null != n.current && (n.current.stop(), (n.current = null)),
                 null != s.current &&
-                    (t || ((n.current = await u(e, s.current)), null != n.current && (n.current.loop = !0)));
+                    (t || ((n.current = await c(e, s.current)), null != n.current && (n.current.loop = !0)));
         })();
     }, [t, e]),
         (0, r.useEffect)(() => {
-            null != s.current && (s.current.gain.value = l);
-        }, [l]),
+            null != s.current && (s.current.gain.value = o);
+        }, [o]),
         (0, r.useEffect)(() => () => n.current?.stop(), []);
 }
