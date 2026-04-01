3fa476bac0fad65ec0cb1788ba0b2c8b41babd28
DiscrapperManager <admin@minerpl.xyz>
2026-04-01 17:57:57 +0200

    canaryRelease: 521084
    Main scripts: 3
    Lazy Loaded Scripts: 2229
    Chunks:
     * Main Files: 13388
     * Scripts: 12853
    Strings: 24407
    Routes: 804
    Experiments Count: 59
    Apex Experiments Count: 230

--- chunks/424728.js
+++ chunks/424728.js
@@ -6,5 +6,5 @@ var r = n(99066),
     a = n(500094),
     o = n(840251),
     l = n(688151);
-let u = [i.m, r.t4, a.A, r.ds, s.A],
+let u = [i.m, r.t4, a.A, s.A],
     c = new o.E(u, l.$G.CONNECTION_OPEN, { location: "app open" });
--- chunks/99066.js
+++ chunks/99066.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { VS: () => c, ds: () => l, sE: () => d, t4: () => u });
+n.d(t, { VS: () => u, sE: () => c, t4: () => l });
 var r = n(600975),
     i = n(945810),
     s = n(335934),
@@ -12,17 +12,6 @@ let o = (0, i.mj)({
     }),
     l = (0, r.C)({
         kind: "user",
-        id: "2025-04_hotwheels_holdout_the_sequel",
-        label: "Tracking a holdout hashed to member list experiment for data readouts",
-        commonTriggerPoint: a.$G.CONNECTION_OPEN,
-        defaultConfig: {},
-        treatments: [
-            { id: 0, label: "Holdout", config: {} },
-            { id: 1, label: "Shadow Treatment", config: {} },
-        ],
-    }),
-    u = (0, r.C)({
-        kind: "user",
         id: "2025-09_hotwheels_nvidia_boost",
         label: "Next iteration of the activity feed ranking model.",
         commonTriggerPoint: a.$G.CONNECTION_OPEN,
@@ -32,12 +21,12 @@ let o = (0, i.mj)({
             { id: 17, label: "ML model V3 - Nvidia big boost", config: {} },
         ],
     });
-function c(e) {
+function u(e) {
     let { enabled: t } = o.getConfig({ location: e }),
         n = (0, s.HF)(e, !1);
     return t || n;
 }
-function d(e) {
+function c(e) {
     let { enabled: t, impressionCappingEnabled: n } = o.getConfig({ location: e });
     return t && !0 === n;
 }
