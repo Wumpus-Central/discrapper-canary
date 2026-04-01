45361f20603a6a02f5c49b0beda9c42bee7b1248
DiscrapperManager <admin@minerpl.xyz>
2026-04-01 17:54:36 +0200

    canaryRelease: 521083
    Main scripts: 3
    Lazy Loaded Scripts: 2229
    Chunks:
     * Main Files: 13388
     * Scripts: 12853
    Strings: 24407
    Routes: 804
    Experiments Count: 60
    Apex Experiments Count: 230

--- chunks/531913.js
+++ chunks/531913.js
@@ -1,28 +1,29 @@
-n.d(t, { A: () => m });
-var i = n(64700),
-    a = n(485584),
-    l = n(311907),
-    s = n(201718),
-    r = n(339580),
-    o = n(773669),
-    c = n(188951);
-let d = [],
-    u = [];
-function m(e, t) {
-    (0, s.P)(e);
-    let n = (0, l.bG)([r.A], () => r.A.getUserIdentityByApplication(e, t)),
-        m = (0, l.bG)([o.default], () => o.default.locale),
-        g = i.useMemo(() => [t], [t]),
-        [p] = (0, c.A)(g),
-        x = i.useMemo(() => (0, a.VG)(n?.profile ?? void 0), [n?.profile]),
-        f = (0, l.bG)([r.A], () => r.A.getFetchState(e) !== r.e.FETCHED);
+"use strict";
+n.d(t, { A: () => _ });
+var r = n(64700),
+    i = n(485584),
+    s = n(311907),
+    a = n(201718),
+    o = n(339580),
+    l = n(773669),
+    u = n(188951);
+let c = [],
+    d = [];
+function _(e, t) {
+    (0, a.P)(e);
+    let n = (0, s.bG)([o.A], () => o.A.getUserIdentityByApplication(e, t)),
+        _ = (0, s.bG)([l.default], () => l.default.locale),
+        f = r.useMemo(() => [t], [t]),
+        [p] = (0, u.A)(f),
+        h = r.useMemo(() => (0, i.VG)(n?.profile ?? void 0), [n?.profile]),
+        m = (0, s.bG)([o.A], () => o.A.getFetchState(e) !== o.e.FETCHED);
     return {
-        data: x,
-        locale: m,
+        data: h,
+        locale: _,
         surfaceConfigs: p?.surfaces ?? {},
-        applicationAssets: d,
-        localizedStrings: u,
-        isLoading: f,
+        applicationAssets: c,
+        localizedStrings: d,
+        isLoading: m,
         hasIdentity: null != n,
     };
 }
--- chunks/964195.js
+++ chunks/964195.js
@@ -1,51 +1,66 @@
 "use strict";
-n.d(t, { A: () => g, F: () => A });
+n.d(t, { A: () => R, F: () => v });
 var r = n(627968),
     i = n(64700),
     s = n(503698),
     a = n.n(s),
-    o = n(311907),
-    l = n(397927),
-    u = n(975460),
-    c = n(850046),
-    d = n(619517),
-    _ = n(633075),
-    f = n(622543),
-    p = n(576622),
-    h = n(985018),
-    m = n(754434);
-function E(e, t) {
-    let n = (0, c.A)(
+    o = n(485584),
+    l = n(598748),
+    u = n(311907),
+    c = n(397927),
+    d = n(975460),
+    _ = n(369374),
+    f = n(531913),
+    p = n(850046),
+    h = n(619517),
+    m = n(633075),
+    E = n(622543),
+    g = n(576622),
+    A = n(985018),
+    I = n(754434);
+function T(e, t) {
+    let n = (0, u.bG)([E.A], () => E.A.getUserProfile(e));
+    return (
+        i.useEffect(() => {
+            let t = new AbortController();
+            return (0, g.A)(e, void 0, { abortSignal: t.signal }), () => t.abort();
+        }, [e]),
+        i.useMemo(
+            () => ({
+                hasWidget:
+                    null != n &&
+                    null != n.widgets &&
+                    n.widgets.some((e) => e instanceof m.R && e.applicationId === t?.id),
+                isLoadingProfile: null == n,
+            }),
+            [n, t],
+        )
+    );
+}
+function S(e, t) {
+    let n = (0, p.A)(
             e,
             i.useMemo(() => (null != t ? [t.id] : []), [t]),
         ),
-        r = (0, o.bG)([f.A], () => f.A.getUserProfile(e));
-    i.useEffect(() => {
-        let t = new AbortController();
-        return (0, p.A)(e, void 0, { abortSignal: t.signal }), () => t.abort();
-    }, [e]);
-    let s = i.useMemo(
-        () => null != r && null != r.widgets && r.widgets.some((e) => e instanceof _.R && e.applicationId === t?.id),
-        [r, t],
-    );
-    return null == t || null == n[t.id] || null == r ? null : { previewData: n[t.id], hasWidget: s };
+        { hasWidget: r, isLoadingProfile: s } = T(e, t);
+    return null == t || null == n[t.id] || s ? null : { previewData: n[t.id], hasWidget: r };
 }
-function g(e) {
-    let t = (0, u.g)(e.activityApplication),
-        n = E(e.userId, t);
-    return null == n ? null : (0, r.jsx)(A, { ...e, ...n });
+function y(e) {
+    let t = (0, d.g)(e.activityApplication),
+        n = S(e.userId, t);
+    return null == n ? null : (0, r.jsx)(v, { ...e, ...n });
 }
-function A(e) {
+function v(e) {
     let {
         previewData: { previewIcon: t, previewText: n },
         hasWidget: i,
     } = e;
     return (0, r.jsxs)("div", {
-        className: a()(m.kL, e.className),
+        className: a()(I.kL, e.className),
         children: [
             (0, r.jsx)("div", {
-                className: m.Qs,
-                children: (0, r.jsxs)(l.Text, {
+                className: I.Qs,
+                children: (0, r.jsxs)(c.Text, {
                     variant: "text-xs/semibold",
                     lineClamp: 1,
                     children: [
@@ -53,8 +68,8 @@ function A(e) {
                             (0, r.jsxs)(r.Fragment, {
                                 children: [
                                     (0, r.jsx)("div", {
-                                        className: m.Kk,
-                                        children: (0, r.jsx)(d.Ay, {
+                                        className: I.Kk,
+                                        children: (0, r.jsx)(h.Ay, {
                                             src: t.proxyUrl,
                                             width: t.width,
                                             height: t.height,
@@ -69,14 +84,52 @@ function A(e) {
                 }),
             }),
             i &&
-                (0, r.jsx)(l.DUT, {
-                    "aria-label": h.intl.string(h.t["OBCR+p"]),
-                    className: m.NO,
+                (0, r.jsx)(c.DUT, {
+                    "aria-label": A.intl.string(A.t["OBCR+p"]),
+                    className: I.NO,
                     onClick: (t) => {
                         e.onClickViewMore(t);
                     },
-                    children: (0, r.jsx)(l.KS6, { size: "xxs" }),
+                    children: (0, r.jsx)(c.KS6, { size: "xxs" }),
                 }),
         ],
     });
+}
+function N(e) {
+    let t = (0, d.g)(e.activityApplication);
+    return null == t ? null : (0, r.jsx)(C, { ...e, widgetApplication: t });
+}
+function C(e) {
+    let t = (0, f.A)(e.userId, e.widgetApplication.id),
+        n = t.surfaceConfigs[l.m.ACTIVITY_ACCESSORY],
+        { hasWidget: i, isLoadingProfile: s } = T(e.userId, e.widgetApplication);
+    return null == n || s
+        ? null
+        : (0, r.jsxs)("div", {
+              className: a()(I.kL, e.className),
+              children: [
+                  (0, r.jsx)("div", {
+                      className: I.Qs,
+                      children: (0, r.jsx)(o.kH, {
+                          surface: l.m.ACTIVITY_ACCESSORY,
+                          surfaceConfig: n,
+                          data: t.data,
+                          locale: t.locale,
+                          applicationAssets: t.applicationAssets,
+                          localizedStrings: t.localizedStrings,
+                      }),
+                  }),
+                  i &&
+                      (0, r.jsx)(c.DUT, {
+                          "aria-label": A.intl.string(A.t["OBCR+p"]),
+                          className: I.NO,
+                          onClick: e.onClickViewMore,
+                          children: (0, r.jsx)(c.KS6, { size: "xxs" }),
+                      }),
+              ],
+          });
+}
+function R(e) {
+    let { enabled: t } = _.A.useConfig({ location: "ActivityApplicationWidgetPreview" });
+    return t ? (0, r.jsx)(N, { ...e }) : (0, r.jsx)(y, { ...e });
 }
