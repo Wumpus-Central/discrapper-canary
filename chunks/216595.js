ff28f5e41c1b7e4a631ec8559d73937e7032926a
DiscrapperManager <admin@minerpl.xyz>
2026-04-01 07:26:27 +0200

    canaryRelease: 520942
    Main scripts: 3
    Lazy Loaded Scripts: 2219
    Chunks:
     * Main Files: 13318
     * Scripts: 12910
    Strings: 24405
    Routes: 804
    Experiments Count: 61
    Apex Experiments Count: 231

--- chunks/216595.js
+++ chunks/216595.js
@@ -1,56 +1,56 @@
-r.d(t, { A: () => A });
-var s = r(627968),
-    n = r(503698),
-    l = r.n(n),
-    a = r(934551),
-    i = r(158954),
-    o = r(311907),
-    u = r(397927),
-    c = r(47167),
-    d = r(713654),
-    h = r(598104),
-    _ = r(263063),
-    E = r(71393),
-    S = r(985018),
-    f = r(298573);
-let g = { top: 8, bottom: 8, left: -4, right: -4 };
-function A(e) {
-    let { channel: t, children: r } = e;
-    return (0, s.jsx)(i.vN3, {
-        offset: g,
-        children: (0, s.jsxs)("div", {
-            className: f.ZO,
+n.d(t, { A: () => f });
+var i = n(627968),
+    l = n(503698),
+    s = n.n(l),
+    a = n(934551),
+    r = n(158954),
+    o = n(311907),
+    d = n(397927),
+    c = n(47167),
+    u = n(713654),
+    h = n(598104),
+    A = n(263063),
+    _ = n(71393),
+    m = n(985018),
+    g = n(784445);
+let p = { top: 8, bottom: 8, left: -4, right: -4 };
+function f(e) {
+    let { channel: t, children: n } = e;
+    return (0, i.jsx)(r.vN3, {
+        offset: p,
+        children: (0, i.jsxs)("div", {
+            className: g.ZO,
             "data-recents-channel": t.id,
             children: [
-                (0, s.jsx)(p, { channel: t }),
-                t.isPrivate() ? null : (0, s.jsx)(a.ChevronSmallRightIcon, { size: "xxs" }),
-                (0, s.jsx)(R, { channel: t }),
-                r,
+                (0, i.jsx)(x, { channel: t }),
+                t.isPrivate() ? null : (0, i.jsx)(a.ChevronSmallRightIcon, { size: "xxs" }),
+                (0, i.jsx)(E, { channel: t }),
+                n,
             ],
         }),
     });
 }
-function p(e) {
+function x(e) {
     let { channel: t } = e,
-        r = (0, o.bG)([E.A], () => E.A.getGuild(t.guild_id));
-    return null != r
-        ? (0, s.jsx)(_.Ay, { className: f.Kk, guild: r, size: _.Ay.Sizes.SMALLER, "aria-hidden": !0 })
+        n = (0, o.bG)([_.A], () => _.A.getGuild(t.guild_id));
+    return null != n
+        ? (0, i.jsx)(A.Ay, { className: g.Kk, guild: n, size: A.Ay.Sizes.SMALLER, "aria-hidden": !0 })
         : t.isGroupDM()
-          ? (0, s.jsx)(h.A, { channel: t, size: u._3J.SIZE_24 })
-          : (0, s.jsx)("div", { className: l()(f.Kk, f.ro), children: (0, s.jsx)(a.ChatIcon, { size: "xxs" }) });
+          ? (0, i.jsx)(h.A, { channel: t, size: d._3J.SIZE_24, "aria-hidden": !0 })
+          : (0, i.jsx)("div", { className: s()(g.Kk, g.ro), children: (0, i.jsx)(a.ChatIcon, { size: "xxs" }) });
 }
-function R(e) {
+function E(e) {
     let { channel: t } = e,
-        r = (0, o.bG)([E.A], () => E.A.getGuild(t.guild_id)),
-        n = (0, c.Ay)(t, !1),
-        l = (0, d.gU)(t, r),
-        a = t.isPrivate() || null == l,
-        i = t.isDM() ? S.intl.formatToPlainString(S.t.smD7XV, { username: n }) : n;
-    return (0, s.jsxs)("div", {
-        className: f.yP,
+        n = (0, o.bG)([_.A], () => _.A.getGuild(t.guild_id)),
+        l = (0, c.Ay)(t, !1),
+        s = (0, u.gU)(t, n),
+        a = t.isPrivate() || null == s,
+        r = t.isDM() ? m.intl.formatToPlainString(m.t.smD7XV, { username: l }) : l;
+    return (0, i.jsxs)("div", {
+        className: g.yP,
         children: [
-            a ? null : (0, s.jsx)(l, { className: f.j1, size: "xxs" }),
-            (0, s.jsx)(u.Heading, { variant: "text-md/semibold", color: "text-default", children: i }),
+            a ? null : (0, i.jsx)(s, { className: g.j1, size: "xxs" }),
+            (0, i.jsx)(d.Heading, { variant: "text-md/semibold", color: "text-default", children: r }),
         ],
     });
 }
