958b34abfc9539f1de79228010627c46b2dff075
DiscrapperManager <admin@minerpl.xyz>
2026-04-02 23:54:24 +0200

    canaryRelease: 522437
    Main scripts: 3
    Lazy Loaded Scripts: 2195
    Chunks:
     * Main Files: 13504
     * Scripts: 12828
    Strings: 24419
    Routes: 809
    Experiments Count: 53
    Apex Experiments Count: 231

--- chunks/352017.js
+++ chunks/352017.js
@@ -2,6 +2,7 @@
 e.exports = {
     hH: "profileFrame__7d80f",
     S_: "profileFrameLayer__7d80f",
+    qG: "fadeIn__7d80f",
     Wm: "front__7d80f",
     aX: "back__7d80f",
     im: "staple__7d80f",
