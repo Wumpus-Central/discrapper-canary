d3154a597ebb71db00603f72116604b07615fc0f
DiscrapperManager <admin@minerpl.xyz>
2026-04-02 23:46:22 +0200

    canaryRelease: 522404
    Main scripts: 3
    Lazy Loaded Scripts: 2195
    Chunks:
     * Main Files: 13504
     * Scripts: 12828
    Strings: 24419
    Routes: 809
    Experiments Count: 53
    Apex Experiments Count: 231

--- chunks/851109.js
+++ chunks/851109.js
@@ -1,10 +1,9 @@
 "use strict";
-n.d(t, { GE: () => u, RK: () => s, U5: () => i, X8: () => l, yF: () => a });
-var r = n(600975),
-    i = (function (e) {
+n.d(t, { GE: () => o, RK: () => i, U5: () => r, X8: () => a, yF: () => s });
+var r = (function (e) {
         return (e.POPOUT = "popout"), (e.SIDEBAR = "sidebar"), (e.LEGACY = "legacy"), e;
     })({}),
-    s = (function (e) {
+    i = (function (e) {
         return (
             (e.SERVER_RAIL_TOP = "server-rail-top"),
             (e.SERVER_RAIL_BOTTOM = "server-rail-bottom"),
@@ -12,45 +11,8 @@ var r = n(600975),
             e
         );
     })({}),
-    a = (function (e) {
+    s = (function (e) {
         return (e.TABS = "tabs"), (e.DROPDOWN = "dropdown"), e;
     })({});
-let o = (0, r.C)({
-        kind: "user",
-        id: "2025-04_desktop_notification_center",
-        label: "New Notification Center (Desktop)",
-        defaultConfig: { notificationCenterVariant: null, entrypoint: null, filterStyle: null },
-        treatments: [
-            {
-                id: 1,
-                label: "Server Rail Entrypoint + Legacy Inbox",
-                config: { notificationCenterVariant: "legacy", entrypoint: "server-rail-top", filterStyle: null },
-            },
-            {
-                id: 2,
-                label: "Title Bar Entrypoint + Popout",
-                config: { notificationCenterVariant: "popout", entrypoint: "title-bar-left", filterStyle: "dropdown" },
-            },
-            {
-                id: 3,
-                label: "Server Rail Entrypoint + Sidebar",
-                config: {
-                    notificationCenterVariant: "sidebar",
-                    entrypoint: "server-rail-top",
-                    filterStyle: "dropdown",
-                },
-            },
-            {
-                id: 4,
-                label: "Server Rail Entrypoint + Popout",
-                config: { notificationCenterVariant: "popout", entrypoint: "server-rail-top", filterStyle: "dropdown" },
-            },
-        ],
-    }),
-    l = (e) =>
-        o.useExperiment({ location: e.location }, { autoTrackExposure: e.autoTrackExposure ?? !0, disable: e.disable }),
-    u = (e) =>
-        o.getCurrentConfig(
-            { location: e.location },
-            { autoTrackExposure: e.autoTrackExposure ?? !0, disable: e.disable },
-        );
+let a = (e) => ({ notificationCenterVariant: null, entrypoint: null, filterStyle: null }),
+    o = (e) => ({ notificationCenterVariant: null, entrypoint: null, filterStyle: null });
