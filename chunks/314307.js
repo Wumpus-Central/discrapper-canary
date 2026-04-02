cd620ed1f722f42720414a2bf06701e2b4c060cd
DiscrapperManager <admin@minerpl.xyz>
2026-04-02 23:52:29 +0200

    canaryRelease: 522420
    Main scripts: 3
    Lazy Loaded Scripts: 2195
    Chunks:
     * Main Files: 13504
     * Scripts: 12828
    Strings: 24419
    Routes: 809
    Experiments Count: 53
    Apex Experiments Count: 231

--- chunks/314307.js
+++ chunks/314307.js
@@ -2,33 +2,33 @@ n.d(t, { Ay: () => m, WK: () => d, cr: () => u, j1: () => h });
 var i = n(627968);
 n(64700);
 var l = n(503698),
-    a = n.n(l),
-    s = n(397927),
+    s = n.n(l),
+    a = n(397927),
     r = n(375901),
     o = n(652215),
-    c = n(423059);
+    c = n(982692);
 function d(e) {
     let { locked: t = !1, channelType: n } = e,
-        l = n === o.rbe.GUILD_VOICE || n === o.rbe.GUILD_STAGE_VOICE ? s.oyn : t ? s.I$d : s.N$i;
+        l = n === o.rbe.GUILD_VOICE || n === o.rbe.GUILD_STAGE_VOICE ? a.oyn : t ? a.I$d : a.N$i;
     return (0, i.jsx)("div", {
-        className: a()(c.qk, c.tP),
-        children: (0, i.jsx)(l, { color: s.LU0.colors.WHITE, size: "custom", width: 42, height: 42 }),
+        className: s()(c.qk, c.tP),
+        children: (0, i.jsx)(l, { color: a.LU0.colors.ICON_STRONG, size: "custom", width: 42, height: 42 }),
     });
 }
 function u(e) {
     let { children: t, className: n } = e;
-    return (0, i.jsx)(s.Heading, {
+    return (0, i.jsx)(a.Heading, {
         "aria-hidden": "true",
-        className: a()(n, c.wx),
+        className: s()(n, c.wx),
         variant: "heading-xxl/extrabold",
         children: t,
     });
 }
 function h(e) {
     let { children: t, className: n } = e;
-    return (0, i.jsx)(s.Text, { variant: "text-md/normal", color: "none", className: a()(c.h_, n), children: t });
+    return (0, i.jsx)(a.Text, { variant: "text-md/normal", color: "none", className: s()(c.h_, n), children: t });
 }
 let m = function (e) {
-    let { className: t, channelId: n, children: l, ...s } = e;
-    return (0, i.jsx)("div", { className: a()(t, c.kL), id: (0, r.j)(n, n), ...s, children: l });
+    let { className: t, channelId: n, children: l, ...a } = e;
+    return (0, i.jsx)("div", { className: s()(t, c.kL), id: (0, r.j)(n, n), ...a, children: l });
 };
