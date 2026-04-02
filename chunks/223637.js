608c2f7e3539b413ce5cd011ae938e6054b8dd0c
DiscrapperManager <admin@minerpl.xyz>
2026-04-02 20:33:54 +0200

    canaryRelease: 522130
    Main scripts: 3
    Lazy Loaded Scripts: 2194
    Chunks:
     * Main Files: 13508
     * Scripts: 12830
    Strings: 24417
    Routes: 809
    Experiments Count: 53
    Apex Experiments Count: 231

--- chunks/223637.js
+++ chunks/223637.js
@@ -1,13 +1,13 @@
 "use strict";
-n.d(t, { A: () => s });
+n.d(t, { A: () => a });
 var r = n(791332),
     i = n.n(r);
-let a = /\n$/,
-    s = {
+let s = /\n$/,
+    a = {
         ...i().defaultRules.heading,
         requiredFirstCharacters: [" ", "#"],
         match: (e, t, n) =>
-            t.allowHeading && (null == n || "" === n || null != n.match(a))
-                ? (0, r.anyScopeRegex)(/^ *(#{1,3})(?:\s+)((?![#]+)[^\n]+?)#*\s*(?:\n|$)/)(e, t, n)
+            t.allowHeading && (null == n || "" === n || null != n.match(s))
+                ? (0, r.anyScopeRegex)(/^ *(#{1,3})(?:\s+)([^\n]+?)#*\s*(?:\n|$)/)(e, t, n)
                 : null,
     };
