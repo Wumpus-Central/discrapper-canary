c81311a8268eb98716044c220a4c9678846923ac
DiscrapperManager <admin@minerpl.xyz>
2026-04-02 21:16:16 +0200

    canaryRelease: 522190
    Main scripts: 3
    Lazy Loaded Scripts: 2195
    Chunks:
     * Main Files: 13508
     * Scripts: 12833
    Strings: 24417
    Routes: 809
    Experiments Count: 53
    Apex Experiments Count: 232

--- chunks/117945.js
+++ chunks/117945.js
@@ -1,7 +1,7 @@
 t.d(n, { T: () => s });
-var l = t(607399),
-    i = t(795791),
+var i = t(607399),
+    l = t(795791),
     r = t(788868);
 function s(e, n) {
-    return !e && n === r.pe.TIER_2 && !l.Ct && !l.KY && !(0, i.u6)();
+    return !e && n === r.pe.TIER_2 && !i.Ct && !i.KY && null == (0, l.uM)();
 }
--- chunks/795791.js
+++ chunks/795791.js
@@ -1,17 +1,23 @@
 "use strict";
-n.d(t, { b7: () => o, u6: () => l });
+n.d(t, { $w: () => l, uM: () => u });
 var r = n(492462),
     i = n(873263);
 n(809733), n(728458);
-var s = n(652215);
-let a = (e, t) => !e.startsWith(s.BVt.BILLING_MANAGE_SUBSCRIPTION) && t === s.W6J.MOBILE_WEB_REDIRECT_CHECKOUT,
-    o = () => {
-        let { search: e, pathname: t } = (0, i.zy)(),
-            { deep_link_type: n } = (0, r.parse)(e);
-        return a(t, n);
+var s = n(26279),
+    a = n(652215);
+let o = (e, t, n) => {
+        if (!e.startsWith(a.BVt.BILLING_MANAGE_SUBSCRIPTION)) {
+            if (t === a.W6J.MOBILE_WEB_REDIRECT_CHECKOUT) return s.uH.MOBILE_WEB_REDIRECT_CHECKOUT;
+            if (n === s.uH.META_QUEST_WEB_REDIRECT_CHECKOUT) return s.uH.META_QUEST_WEB_REDIRECT_CHECKOUT;
+        }
     },
     l = () => {
+        let { search: e, pathname: t } = (0, i.zy)(),
+            { deep_link_type: n, flow_type: s } = (0, r.parse)(e);
+        return o(t, n, s);
+    },
+    u = () => {
         let e = window.location.pathname,
-            { deep_link_type: t } = (0, r.parse)(window.location.search);
-        return a(e, t);
+            { deep_link_type: t, flow_type: n } = (0, r.parse)(window.location.search);
+        return o(e, t, n);
     };
