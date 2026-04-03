0d31c34a5ef9a976a5bd204760dd7601d2290d61
DiscrapperManager <admin@minerpl.xyz>
2026-04-03 18:38:54 +0200

    canaryRelease: 522883
    Main scripts: 3
    Lazy Loaded Scripts: 2243
    Chunks:
     * Main Files: 13513
     * Scripts: 12896
    Strings: 24434
    Routes: 809
    Experiments Count: 52
    Apex Experiments Count: 233

--- chunks/250105.js
+++ chunks/250105.js
@@ -2,8 +2,8 @@
 n.d(t, { Ay: () => l, cT: () => u });
 var r = n(720757),
     i = n(311907),
-    a = n(961350),
-    s = n(403362),
+    s = n(961350),
+    a = n(403362),
     o = n(217222);
 function l(e) {
     return (0, r.A)(e, o.A, u, c);
@@ -13,23 +13,23 @@ function u(e, t) {
         case "guild":
             return t.guildId;
         case "user":
-            return a.default.getId();
+            return s.default.getId();
         case "installation":
-            return a.default.getInstallationForTracking() ?? "";
+            return s.default.getInstallationForTracking()?.split(".")[0] ?? "";
         default:
-            (0, s.xb)(e);
+            (0, a.xb)(e);
     }
 }
 function c(e, t) {
-    let [n, r] = (0, i.yK)([a.default], () => [a.default.getId(), a.default.getInstallationForTracking()]);
+    let [n, r] = (0, i.yK)([s.default], () => [s.default.getId(), s.default.getInstallationForTracking()]);
     switch (e) {
         case "guild":
             return t.guildId;
         case "user":
             return n;
         case "installation":
-            return r ?? "";
+            return r?.split(".")[0] ?? "";
         default:
-            (0, s.xb)(e);
+            (0, a.xb)(e);
     }
 }
