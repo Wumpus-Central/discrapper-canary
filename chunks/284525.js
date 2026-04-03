811fb7e8f467c86799af4530a3c7c7db9d6c6d06
DiscrapperManager <admin@minerpl.xyz>
2026-04-03 16:21:51 +0200

    canaryRelease: 522798
    Main scripts: 3
    Lazy Loaded Scripts: 2244
    Chunks:
     * Main Files: 13512
     * Scripts: 12880
    Strings: 24434
    Routes: 809
    Experiments Count: 53
    Apex Experiments Count: 233

--- chunks/284525.js
+++ chunks/284525.js
@@ -3,8 +3,8 @@ n.d(t, { A: () => o });
 var r = n(627968);
 n(64700);
 var i = n(990078),
-    a = n(397927),
-    s = n(594262);
+    s = n(397927),
+    a = n(594262);
 function o(e) {
     let {
             activity: t,
@@ -16,19 +16,20 @@ function o(e) {
             size: d = "sm",
             ..._
         } = e,
-        f = (0, s.l)({ activity: t ?? void 0, embeddedActivity: n, user: o, onGameJoin: l, location: u });
+        f = (0, a.l)({ activity: t ?? void 0, embeddedActivity: n, user: o, onGameJoin: l, location: u });
     if (null == f) return null;
-    let { isJoining: p, handleJoinRequest: h, buttonCTA: m, tooltip: g, isEnabled: E } = f;
+    let { isJoining: p, handleJoinRequest: h, buttonCTA: m, tooltip: E, isEnabled: g } = f;
     return (0, r.jsx)(
         i.m,
         {
-            text: g,
-            children: (0, r.jsx)(a.Button, {
+            text: E,
+            asContainer: !g,
+            children: (0, r.jsx)(s.Button, {
                 variant: c,
                 size: d,
                 text: m,
                 onClick: h,
-                disabled: !E,
+                disabled: !g,
                 loading: p,
                 fullWidth: !0,
                 ..._,
