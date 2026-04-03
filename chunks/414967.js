4f676f7da3c31b65cf9ea39b95f48fbde7a78de8
DiscrapperManager <admin@minerpl.xyz>
2026-04-03 04:21:27 +0200

    canaryRelease: 522661
    Main scripts: 3
    Lazy Loaded Scripts: 2195
    Chunks:
     * Main Files: 13512
     * Scripts: 12829
    Strings: 24432
    Routes: 809
    Experiments Count: 53
    Apex Experiments Count: 233

--- chunks/414967.js
+++ chunks/414967.js
@@ -1,4 +1,4 @@
-n.d(t, { A: () => f });
+n.d(t, { A: () => C });
 var i = n(627968);
 n(64700);
 var a = n(877624),
@@ -13,21 +13,24 @@ var a = n(877624),
     h = n(881489),
     _ = n(412260),
     m = n(852218),
-    g = n(231265),
-    p = n(979080);
-let E = "PremiumServerDriveAnnouncementModal";
-class I extends s.A {
+    p = n(231265),
+    g = n(979080),
+    E = n(652215);
+let I = "PremiumServerDriveAnnouncementModal";
+class f extends s.A {
     _initialize() {
         r.h.subscribe("PREMIUM_MARKETING_DATA_READY", this.mayShowAnnouncementModal),
             r.h.subscribe("PREMIUM_MARKETING_PREVIEW", this.handlePreview),
             r.h.subscribe("ACTIVE_PROMOTIONS_FETCH_SUCCESS", this.handleActivePromotionsFetchSuccess),
-            r.h.subscribe("BILLING_USER_OFFER_FETCH_SUCCESS", this.handleUserOfferFetchSuccess);
+            r.h.subscribe("BILLING_USER_OFFER_FETCH_SUCCESS", this.handleUserOfferFetchSuccess),
+            r.h.subscribe("ENTITLEMENT_CREATE", this.handleEntitlementCreate);
     }
     _terminate() {
         r.h.unsubscribe("PREMIUM_MARKETING_DATA_READY", this.mayShowAnnouncementModal),
             r.h.unsubscribe("PREMIUM_MARKETING_PREVIEW", this.handlePreview),
             r.h.unsubscribe("ACTIVE_PROMOTIONS_FETCH_SUCCESS", this.handleActivePromotionsFetchSuccess),
-            r.h.unsubscribe("BILLING_USER_OFFER_FETCH_SUCCESS", this.handleUserOfferFetchSuccess);
+            r.h.unsubscribe("BILLING_USER_OFFER_FETCH_SUCCESS", this.handleUserOfferFetchSuccess),
+            r.h.unsubscribe("ENTITLEMENT_CREATE", this.handleEntitlementCreate);
     }
     maybeShowAnnouncementModalFromPromotions = (e) => {
         for (let t of e)
@@ -50,19 +53,23 @@ class I extends s.A {
         null != t &&
             this.maybeShowAnnouncementModalFromPromotions(Object.values(_.A.promotionsByType[m.pt.MARKETING_MOMENT]));
     };
+    handleEntitlementCreate = (e) => {
+        let { entitlement: t } = e;
+        u.P.isDisallowPopupsSet() || t.source_type !== E.GD.REVERSE_TRIAL || (0, h.al)();
+    };
     maybeOpenServerDriveAnnouncementModal = async (e, t, a, r) => {
-        if ((0, l.kBI)(E)) return !1;
-        let s = (0, p.H)({ promotionId: t, content: a, isPreview: r });
+        if ((0, l.kBI)(I)) return !1;
+        let s = (0, g.H)({ promotionId: t, content: a, isPreview: r });
         if (null != s)
             return (
-                (!1 !== r || s?.contentIdentifier !== "summer_bogo_content" || !!(await (0, g.C)())) &&
+                (!1 !== r || s?.contentIdentifier !== "summer_bogo_content" || !!(await (0, p.C)())) &&
                 ((0, l.mMO)(
                     async () => {
                         let { default: a } = await Promise.resolve().then(n.bind(n, 979080));
                         return (n) =>
                             (0, i.jsx)(a, { renderModalProps: n, componentId: e, promotionId: t, properties: s });
                     },
-                    { modalKey: E },
+                    { modalKey: I },
                 ),
                 !0)
             );
@@ -75,7 +82,7 @@ class I extends s.A {
     mayShowAnnouncementModal = async () => {
         if ((await (0, A.hb)(), u.P.isDisallowPopupsSet())) return;
         let e = d.default.getCurrentUser();
-        (null == e || e.verified) && (0, h.wK)();
+        (null == e || e.verified) && ((0, h.al)(), (0, h.QM)());
     };
 }
-let f = new I();
+let C = new f();
--- chunks/881489.js
+++ chunks/881489.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { Zb: () => g, ds: () => E, wK: () => I });
+n.d(t, { QM: () => S, Zb: () => g, al: () => T, ds: () => E });
 var r = n(627968);
 n(64700);
 var i = n(311907),
@@ -26,34 +26,33 @@ function g(e) {
     return Math.max(1, (0, l.Vb)(t).days);
 }
 let A = "ReverseTrialUpsellModal";
-function I() {
-    let e = null,
-        t = null,
-        i = "",
-        o = d.default.getCurrentUser();
-    if (null == o || (0, a.ueM)()) return;
-    if (o.isOnReverseTrial()) {
-        let n = f.A.getFractionalPremium({ excludeReverseTrial: !1 }).find((e) => e.sourceType === h.GD.REVERSE_TRIAL);
-        null != n && ((e = "initial"), (t = s.M.ML_REVERSE_TRIAL_UPSELL_MODAL), (i = n.id));
-    } else {
-        let n = _.A.getUserTrialOffer(p.Tt);
-        null != n && ((e = "followup"), (t = s.M.ML_REVERSE_TRIAL_FOLLOWUP_UPSELL_MODAL), (i = n.id));
-    }
-    if (null == t) return;
-    let { isDismissed: l } = (0, u.u$)(t, i);
-    if (l) return;
-    let m = (e) => {
+function I(e, t, i) {
+    if ((0, a.ueM)()) return;
+    let { isDismissed: s } = (0, u.u$)(t, i);
+    if (s) return;
+    let o = (e) => {
         (0, c.qr)(t, i, { dismissAction: e });
     };
     (0, a.mMO)(
         async () => {
             if ("followup" === e) {
                 let { default: e } = await n.e("24580").then(n.bind(n, 34255));
-                return (t) => (0, r.jsx)(e, { renderModalProps: t, markAsDismissed: m });
+                return (t) => (0, r.jsx)(e, { renderModalProps: t, markAsDismissed: o });
             }
             let { default: t } = await n.e("47948").then(n.bind(n, 166247));
-            return (e) => (0, r.jsx)(t, { renderModalProps: e, markAsDismissed: m });
+            return (e) => (0, r.jsx)(t, { renderModalProps: e, markAsDismissed: o });
         },
         { modalKey: A },
     );
+}
+function T() {
+    let e = d.default.getCurrentUser();
+    if (null == e || !e.isOnReverseTrial()) return;
+    let t = f.A.getFractionalPremium({ excludeReverseTrial: !1 }).find((e) => e.sourceType === h.GD.REVERSE_TRIAL);
+    null != t && I("initial", s.M.ML_REVERSE_TRIAL_UPSELL_MODAL, t.id);
+}
+function S() {
+    if (null == d.default.getCurrentUser()) return;
+    let e = _.A.getUserTrialOffer(p.Tt);
+    null != e && I("followup", s.M.ML_REVERSE_TRIAL_FOLLOWUP_UPSELL_MODAL, e.id);
 }
