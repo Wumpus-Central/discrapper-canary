c08cfb33315ffd0868a67af89b771469c1a22b8e
DiscrapperManager <admin@minerpl.xyz>
2026-04-03 04:12:59 +0200

    canaryRelease: 522658
    Main scripts: 3
    Lazy Loaded Scripts: 2195
    Chunks:
     * Main Files: 13512
     * Scripts: 12829
    Strings: 24432
    Routes: 809
    Experiments Count: 53
    Apex Experiments Count: 233

--- chunks/563087.js
+++ chunks/563087.js
@@ -1,2 +1,2 @@
 "use strict";
-e.exports = { Dr: "tidaDescription__9a831", bV: "sectionHeader__9a831", TF: "tidaBulletList__9a831" };
+e.exports = { T: "tidaBulletList__9a831" };
--- chunks/759788.js
+++ chunks/759788.js
@@ -6,7 +6,7 @@ var s = n(397927),
     r = n(17372),
     l = n(204173),
     a = n(985018),
-    o = n(618264);
+    o = n(563087);
 let d = (e) => {
     let { showBackButton: t, onBack: n, renderFooter: d, onReopen: c } = e,
         u = (0, l.i)(r.tY.MEDIA_TAKEDOWN, c),
@@ -20,27 +20,28 @@ let d = (e) => {
             ],
         });
     return (0, i.jsxs)(s.BJc, {
-        gap: 8,
+        gap: 24,
         children: [
-            (0, i.jsx)(s.Heading, { variant: "heading-xl/semibold", children: a.intl.string(a.t.YignUm) }),
-            (0, i.jsxs)("div", {
-                className: o.Dr,
+            (0, i.jsxs)(s.BJc, {
+                gap: 8,
                 children: [
+                    (0, i.jsx)(s.Heading, { variant: "heading-xl/semibold", children: a.intl.string(a.t.YignUm) }),
                     (0, i.jsx)(s.Text, {
                         variant: "text-md/normal",
                         color: "text-subtle",
                         children: a.intl.string(a.t["3zG2Y9"]),
                     }),
-                    (0, i.jsxs)("div", {
+                ],
+            }),
+            (0, i.jsxs)(s.BJc, {
+                gap: 16,
+                children: [
+                    (0, i.jsxs)(s.BJc, {
+                        gap: 8,
                         children: [
-                            (0, i.jsx)(s.Text, {
-                                variant: "text-md/semibold",
-                                color: "text-subtle",
-                                className: o.bV,
-                                children: a.intl.string(a.t.CfBo0z),
-                            }),
+                            (0, i.jsx)(s.Text, { variant: "text-md/semibold", children: a.intl.string(a.t.CfBo0z) }),
                             (0, i.jsxs)("ul", {
-                                className: o.TF,
+                                className: o.T,
                                 children: [
                                     (0, i.jsx)("li", {
                                         children: (0, i.jsx)(s.Text, {
@@ -79,24 +80,20 @@ let d = (e) => {
                             policyURL: "https://discord.com/safety/non-consensual-intimate-media-policy-explainer",
                         }),
                     }),
-                    (0, i.jsxs)("div", {
-                        children: [
-                            (0, i.jsx)(s.Text, {
-                                variant: "text-md/semibold",
-                                color: "text-subtle",
-                                className: o.bV,
-                                children: a.intl.string(a.t.FJh2zi),
-                            }),
-                            (0, i.jsx)(s.Text, {
-                                variant: "text-md/normal",
-                                color: "text-subtle",
-                                children: a.intl.format(a.t.R2Q57u, {
-                                    supportOrgURL: "https://stopncii.org/partners/global-network-of-partners/",
-                                    wellbeingURL: "https://discord.com/safety-wellbeing",
-                                    helpCenterURL: "https://support.discord.com/hc/articles/38675715591831",
-                                }),
-                            }),
-                        ],
+                ],
+            }),
+            (0, i.jsxs)(s.BJc, {
+                gap: 8,
+                children: [
+                    (0, i.jsx)(s.Text, { variant: "text-md/semibold", children: a.intl.string(a.t.FJh2zi) }),
+                    (0, i.jsx)(s.Text, {
+                        variant: "text-md/normal",
+                        color: "text-subtle",
+                        children: a.intl.format(a.t.R2Q57u, {
+                            supportOrgURL: "https://stopncii.org/partners/global-network-of-partners/",
+                            wellbeingURL: "https://discord.com/safety-wellbeing",
+                            helpCenterURL: "https://support.discord.com/hc/articles/38675715591831",
+                        }),
                     }),
                 ],
             }),
