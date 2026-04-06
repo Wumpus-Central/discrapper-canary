35d93499bbf095647e64b390d31db3d061a8c4c3
DiscrapperManager <admin@minerpl.xyz>
2026-04-06 17:29:39 +0200

    canaryRelease: 523543
    Main scripts: 3
    Lazy Loaded Scripts: 2243
    Chunks:
     * Main Files: 13513
     * Scripts: 12905
    Strings: 24447
    Routes: 809
    Experiments Count: 51
    Apex Experiments Count: 237

--- chunks/876587.js
+++ chunks/876587.js
@@ -1,28 +1,43 @@
-n.d(t, { O: () => c });
+n.d(t, { O: () => g });
 var i = n(627968);
 n(64700);
 var s = n(554146),
     l = n(342494),
-    a = n(932001),
-    r = n(49999),
-    o = n(985018),
-    d = n(201682);
-let c = (e) => {
+    a = n(397927),
+    r = n(932001),
+    o = n(808247),
+    d = n(788868),
+    c = n(49999),
+    u = n(985018),
+    m = n(201682);
+let g = (e) => {
     let { targetElementRef: t } = e,
-        [n, c] = (0, a.kn)([s.M.PREMIUM_WISHLIST_COACHMARK]);
+        [n, g] = (0, r.kn)([s.M.PREMIUM_WISHLIST_COACHMARK]);
     return n !== s.M.PREMIUM_WISHLIST_COACHMARK
         ? null
         : (0, i.jsx)(l.AM, {
               targetElementRef: t,
-              title: o.intl.string(o.t["1YLUuN"]),
-              body: o.intl.string(o.t.IqPvOn),
-              graphic: { type: "image", src: d.A },
+              title: u.intl.string(u.t["1YLUuN"]),
+              body: u.intl.string(u.t.IqPvOn),
+              graphic: { type: "image", src: m.A },
               size: "video",
               alignmentStrategy: "edge",
               align: "right",
               position: "bottom",
               caretConfig: { align: "end" },
-              actions: [{ text: o.intl.string(o.t.moIhI0), onClick: () => c(r.i.TAKE_ACTION) }],
-              onRequestClose: () => c(r.i.USER_DISMISS),
+              actions: [
+                  {
+                      text: u.intl.string(u.t["8DkMEQ"]),
+                      onClick: async () => {
+                          try {
+                              await o.A.addSkuToWishlist(d.pe.TIER_2), g(c.i.TAKE_ACTION);
+                          } catch {
+                              (0, a.showToast)((0, a.createToast)(u.intl.string(u.t.F8FvUy), a.ToastType.FAILURE)),
+                                  a.ORC.announce(u.intl.string(u.t.F8FvUy));
+                          }
+                      },
+                  },
+              ],
+              onRequestClose: () => g(c.i.USER_DISMISS),
           });
 };
