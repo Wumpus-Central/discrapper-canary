4b03ccc158f8a9c6c68d2c9bf5b9c2ec82e7115e
DiscrapperManager <admin@minerpl.xyz>
2026-04-06 16:47:11 +0200

    canaryRelease: 523531
    Main scripts: 3
    Lazy Loaded Scripts: 2243
    Chunks:
     * Main Files: 13513
     * Scripts: 12905
    Strings: 24447
    Routes: 809
    Experiments Count: 51
    Apex Experiments Count: 237

--- chunks/475889.js
+++ chunks/475889.js
@@ -1,24 +1,30 @@
 "use strict";
 n.d(t, { H: () => d });
-var i = n(64700),
-    s = n(574381),
-    l = n(311907),
+var l = n(64700),
+    i = n(574381),
+    s = n(311907),
+    a = n(919638),
     r = n(858466),
-    a = n(725613),
-    o = n(661470),
+    o = n(869180),
+    u = n(725613),
     c = n(652215);
 function d(e) {
-    let { hasRequestedStartTimes: t, startTime: n } = (0, l.cf)([a.A], () => ({
-            hasRequestedStartTimes: a.A.hasRequestedStartTimes(e.guild_id),
-            startTime: a.A.getStartTime(e),
+    let {
+            hasRequestedStartTimes: t,
+            startTime: n,
+            isGuildUnavailable: d,
+        } = (0, s.cf)([u.A, a.A], () => ({
+            hasRequestedStartTimes: u.A.hasRequestedStartTimes(e.guild_id),
+            startTime: u.A.getStartTime(e),
+            isGuildUnavailable: a.A.isUnavailable(e.guild_id),
         })),
-        d = (0, o.a)(e.guild_id, "useStartTime"),
-        u = ((0, s.un)() || (0, s.m0)()) && !d,
+        m = (0, o.a)(e.guild_id, "useStartTime"),
+        g = ((0, i.un)() || (0, i.m0)()) && !m,
         h = e.type === c.rbe.GUILD_VOICE;
     return (
-        i.useEffect(() => {
-            t || !h || u || (0, r.U)(e.guild_id);
-        }, [h, e.guild_id, t, u]),
+        l.useEffect(() => {
+            t || !h || g || d || (0, r.U)(e.guild_id);
+        }, [h, e.guild_id, t, g, d]),
         n
     );
 }
