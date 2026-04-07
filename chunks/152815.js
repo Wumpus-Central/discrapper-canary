937642d01f906f0b0c7f3dcf4bfa79c456467c97
DiscrapperManager <admin@minerpl.xyz>
2026-04-07 19:19:29 +0200

    canaryRelease: 524312
    Main scripts: 3
    Lazy Loaded Scripts: 2243
    Chunks:
     * Main Files: 13513
     * Scripts: 12949
    Strings: 24532
    Routes: 812
    Experiments Count: 49
    Apex Experiments Count: 239

--- chunks/152815.js
+++ chunks/152815.js
@@ -1,85 +1,90 @@
-n.d(t, { P: () => f, ThirdPartyPromotionsModal: () => T });
+n.d(t, { P: () => b, ThirdPartyPromotionsModal: () => S });
 var i = n(627968);
 n(64700);
 var s = n(158954),
     l = n(311907),
     a = n(397927),
-    r = n(597758),
-    o = n(35587),
-    d = n(412260),
-    c = n(116011),
-    u = n(881373),
-    m = n(125877),
-    g = n(284498),
-    _ = n(985018),
-    x = n(616825);
-let A = {
-        Logitech30P: { title: g.default.jkdhZq, titleParams: { discountPercent: u.aW }, body: g.default.Yl5ww1 },
+    r = n(975571),
+    o = n(597758),
+    d = n(35587),
+    c = n(412260),
+    u = n(116011),
+    m = n(881373),
+    g = n(125877),
+    _ = n(652215),
+    x = n(653624),
+    A = n(985018),
+    h = n(276478);
+let p = {
+        Logitech30P: { title: x.default.jkdhZq, titleParams: { discountPercent: m.aW }, body: x.default.Yl5ww1 },
         Logitech5PNI: {
-            title: g.default.zeBjoX,
-            titleParams: { discountPercent: u.y$ },
-            body: g.default.fC4abC,
+            title: x.default.zeBjoX,
+            titleParams: { discountPercent: m.y$ },
+            body: x.default.fC4abC,
             bodyParams: { months: 6 },
         },
-        Logitech5PGS: { title: g.default.ogcfac, titleParams: { discountPercent: u.K2 } },
+        Logitech5PGS: { title: x.default.ogcfac, titleParams: { discountPercent: m.K2 } },
     },
-    h = {
+    T = {
         logitech: {
-            modalTitle: g.default["2I7nK+"],
-            modalSubtitle: { message: g.default.W8jOD0, values: { termsUrl: "https://www.logitech.com/terms" } },
+            modalTitle: x.default["2I7nK+"],
+            modalSubtitle: {
+                message: x.default.W8jOD0,
+                values: { termsUrl: r.A.getArticleURL(_.MVz.LOGITECH_PROMOTION) },
+            },
             landingUrl: "https://www.logitech.com/",
         },
     },
-    p = (e, t) => {
-        let n = A[e.outboundTitle];
+    f = (e, t) => {
+        let n = p[e.outboundTitle];
         return {
             id: e.id,
             partnerId: e.partnerId ?? "",
-            title: null != n ? _.intl.formatToPlainString(n.title, n.titleParams ?? {}) : "",
-            body: null != n && null != n.body ? _.intl.formatToPlainString(n.body, n.bodyParams ?? {}) : "",
+            title: null != n ? A.intl.formatToPlainString(n.title, n.titleParams ?? {}) : "",
+            body: null != n && null != n.body ? A.intl.formatToPlainString(n.body, n.bodyParams ?? {}) : "",
             startDate: e.startDate,
             endDate: e.endDate,
             redemptionURL: e.outboundRedemptionPageLink,
             code: t,
         };
     },
-    T = (e) => {
+    S = (e) => {
         let { partnerId: t, transitionState: n, onClose: a } = e,
-            r = h[t],
-            [u, A] = (0, l.yK)([d.A], () => {
-                let { recurring: e, oneTime: n } = d.A.getPromotionsByPartner(t);
+            r = T[t],
+            [o, m] = (0, l.yK)([c.A], () => {
+                let { recurring: e, oneTime: n } = c.A.getPromotionsByPartner(t);
                 return [e, n];
             }),
-            { promotionsLoaded: T, claimedOutboundPromotionCodeMap: f } = (0, o.y7)();
-        if (!T) return (0, i.jsx)(s.y$y, {});
-        let S = r?.modalSubtitle != null ? _.intl.format(r.modalSubtitle.message, r.modalSubtitle.values) : void 0,
-            E = null == r ? t : _.intl.string(r.modalTitle);
+            { promotionsLoaded: _, claimedOutboundPromotionCodeMap: p } = (0, d.y7)();
+        if (!_) return (0, i.jsx)(s.y$y, {});
+        let S = r?.modalSubtitle != null ? A.intl.format(r.modalSubtitle.message, r.modalSubtitle.values) : void 0,
+            b = null == r ? t : A.intl.string(r.modalTitle);
         return (0, i.jsx)(s.Modal, {
-            title: E,
+            title: b,
             subtitle: S,
             actions: [],
             transitionState: n,
             onClose: a,
             children: (0, i.jsxs)("div", {
-                className: x.kL,
+                className: h.kL,
                 children: [
-                    u.length > 0 &&
+                    o.length > 0 &&
                         (0, i.jsxs)(i.Fragment, {
                             children: [
                                 (0, i.jsx)(s.EYj, {
                                     variant: "text-md/medium",
                                     color: "text-subtle",
-                                    children: _.intl.string(g.default["9Y2p6p"]),
+                                    children: A.intl.string(x.default["9Y2p6p"]),
                                 }),
                                 (0, i.jsx)("div", {
-                                    className: x.kR,
-                                    children: u.map((e) =>
+                                    className: h.kR,
+                                    children: o.map((e) =>
                                         (0, i.jsx)(
-                                            c.wx,
+                                            u.wx,
                                             {
                                                 recurrence: {
-                                                    ...p(e, f[e.id] ?? null),
-                                                    asset: c.oL[e.partnerId ?? ""]?.asset ?? "",
+                                                    ...f(e, p[e.id] ?? null),
+                                                    asset: u.oL[e.partnerId ?? ""]?.asset ?? "",
                                                 },
                                                 showPartnerImage: !0,
                                                 displayCTAInDedicatedFooter: !0,
@@ -90,18 +95,18 @@ let A = {
                                 }),
                             ],
                         }),
-                    A.length > 0 &&
+                    m.length > 0 &&
                         (0, i.jsxs)("div", {
-                            className: x.E7,
+                            className: h.E7,
                             children: [
                                 (0, i.jsx)(s.EYj, {
                                     variant: "text-md/medium",
                                     color: "text-subtle",
-                                    children: _.intl.string(g.default.Wm58LR),
+                                    children: A.intl.string(x.default.Wm58LR),
                                 }),
                                 (0, i.jsx)("div", {
-                                    className: x.kR,
-                                    children: A.map((e) => (0, i.jsx)(m.$, { promotion: p(e, f[e.id] ?? null) }, e.id)),
+                                    className: h.kR,
+                                    children: m.map((e) => (0, i.jsx)(g.$, { promotion: f(e, p[e.id] ?? null) }, e.id)),
                                 }),
                             ],
                         }),
@@ -109,9 +114,9 @@ let A = {
             }),
         });
     },
-    f = (e) => {
+    b = (e) => {
         let { partnerId: t } = e;
-        r.Ay.fetchActivePromotions(),
+        o.Ay.fetchActivePromotions(),
             (0, a.mMO)(async () => {
                 let { ThirdPartyPromotionsModal: e } = await Promise.resolve().then(n.bind(n, 152815));
                 return (n) => (0, i.jsx)(e, { ...n, partnerId: t });
