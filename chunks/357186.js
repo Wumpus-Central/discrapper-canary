b3dd0999fd7fd570a7e5304545e3826430b507b8
DiscrapperManager <admin@minerpl.xyz>
2026-04-07 17:08:44 +0200

    canaryRelease: 524198
    Main scripts: 3
    Lazy Loaded Scripts: 2243
    Chunks:
     * Main Files: 13513
     * Scripts: 12948
    Strings: 24532
    Routes: 812
    Experiments Count: 50
    Apex Experiments Count: 239

--- chunks/357186.js
+++ chunks/357186.js
@@ -14,7 +14,6 @@ function f(e) {
     switch (e) {
         case i.M.COLLECTIBLES_SHOP_ENTRY_MARKETING:
             return u.A.getMarketingBySurface(r.R.DESKTOP_SHOP_BUTTON)?.version ?? 0;
-        case i.M.GIFTING_PROMOTION_MOBILE_FIRST_TIME_HALFSHEET:
         case i.M.GIFTING_PROMOTION_REMINDER:
             return (0, d.p)();
         case i.M.ACTIVITIES_VOICE_LAUNCHER_BADGE:
