1ffd1c8505d8076eebdfd363f03d77450c6ee003
DiscrapperManager <admin@minerpl.xyz>
2026-04-07 18:33:52 +0200

    canaryRelease: 524262
    Main scripts: 3
    Lazy Loaded Scripts: 2243
    Chunks:
     * Main Files: 13514
     * Scripts: 12948
    Strings: 24531
    Routes: 812
    Experiments Count: 50
    Apex Experiments Count: 239

--- chunks/431040.js
+++ chunks/431040.js
@@ -2,178 +2,175 @@ n.d(t, { A: () => _ });
 var a = n(627968),
     l = n(64700),
     i = n(397927),
-    r = n(541830),
-    s = n(240248),
+    r = n(405269),
+    s = n(541830),
+    o = n(240248),
     c = n(899273),
-    o = n(504186),
-    d = n(814698),
-    u = n(839671),
-    m = n(936636),
-    x = n(424994),
-    h = n(985018),
-    g = n(706445);
-function f() {
-    return (0, a.jsx)(i.Text, { variant: "text-sm/normal", color: "text-subtle", children: h.intl.string(h.t.GruYxV) });
+    d = n(504186),
+    u = n(814698),
+    m = n(839671),
+    x = n(936636),
+    h = n(424994),
+    f = n(985018),
+    p = n(706445);
+function g() {
+    return (0, a.jsx)(i.Text, { variant: "text-sm/normal", color: "text-subtle", children: f.intl.string(f.t.GruYxV) });
 }
 let _ = function (e) {
     let { application: t, detectedGame: n, trackAction: _ } = e,
-        p = l.useMemo(() => n.genres.map(r.du).join(", "), [n]),
-        v = n.publishers.join(", "),
-        A = n.developers.join(", "),
-        j = n.firstReleaseDate,
-        E = (0, o.A)(n),
+        v = l.useMemo(() => n.genres.map(s.du).join(", "), [n]),
+        A = n.publishers.join(", "),
+        j = n.developers.join(", "),
+        E = n.firstReleaseDate,
+        b = (0, d.A)(n),
         C = (0, c.Y)(n),
-        I = !(0, s.uJ)(p),
-        b = !(0, s.uJ)(v),
-        N = !(0, s.uJ)(A),
-        S = !(0, s.uJ)(j),
-        T = E.length > 0,
-        k = C.length > 0 && !C.every((e) => (0, s.uJ)(e.url));
+        I = !(0, o.uJ)(v),
+        N = !(0, o.uJ)(A),
+        S = !(0, o.uJ)(j),
+        T = !(0, o.uJ)(E),
+        y = b.length > 0,
+        L = C.length > 0 && !C.every((e) => (0, o.uJ)(e.url));
     return (0, a.jsxs)("div", {
-        className: g.uW,
+        className: p.uW,
         children: [
             (0, a.jsx)("div", {
-                className: g.Gf,
+                className: p.Gf,
                 children: (0, a.jsx)(i.Heading, {
                     variant: "heading-sm/semibold",
                     color: "text-strong",
-                    children: h.intl.string(h.t["7OjmmH"]),
+                    children: f.intl.string(f.t["7OjmmH"]),
                 }),
             }),
             (0, a.jsxs)("div", {
-                className: g.kL,
+                className: p.kL,
                 children: [
                     (0, a.jsxs)("div", {
-                        className: g.J1,
+                        className: p.J1,
                         children: [
                             (0, a.jsx)(i.Text, {
                                 variant: "text-sm/normal",
                                 color: "text-subtle",
-                                children: 1 !== n.genres.length ? h.intl.string(h.t.pDgwYB) : h.intl.string(h.t.mjFKqn),
+                                children: 1 !== n.genres.length ? f.intl.string(f.t.pDgwYB) : f.intl.string(f.t.mjFKqn),
                             }),
                             I
                                 ? (0, a.jsx)(i.Text, {
                                       variant: "text-sm/normal",
                                       color: "text-subtle",
-                                      className: g.Gu,
-                                      children: p,
+                                      className: p.Gu,
+                                      children: v,
                                   })
-                                : (0, a.jsx)(f, {}),
+                                : (0, a.jsx)(g, {}),
                         ],
                     }),
                     (0, a.jsxs)("div", {
-                        className: g.J1,
+                        className: p.J1,
                         children: [
                             (0, a.jsx)(i.Text, {
                                 variant: "text-sm/normal",
                                 color: "text-subtle",
                                 children:
                                     1 !== n.publishers.length
-                                        ? h.intl.string(h.t.Hc7Enk)
-                                        : h.intl.string(h.t["4Byy/G"]),
-                            }),
-                            b
-                                ? (0, a.jsx)(i.Text, {
-                                      variant: "text-sm/normal",
-                                      color: "text-subtle",
-                                      className: g.Gu,
-                                      children: v,
-                                  })
-                                : (0, a.jsx)(f, {}),
-                        ],
-                    }),
-                    (0, a.jsxs)("div", {
-                        className: g.J1,
-                        children: [
-                            (0, a.jsx)(i.Text, {
-                                variant: "text-sm/normal",
-                                color: "text-subtle",
-                                children:
-                                    1 !== n.developers.length ? h.intl.string(h.t.KATEJB) : h.intl.string(h.t.na3PT0),
+                                        ? f.intl.string(f.t.Hc7Enk)
+                                        : f.intl.string(f.t["4Byy/G"]),
                             }),
                             N
                                 ? (0, a.jsx)(i.Text, {
                                       variant: "text-sm/normal",
                                       color: "text-subtle",
-                                      className: g.Gu,
+                                      className: p.Gu,
                                       children: A,
                                   })
-                                : (0, a.jsx)(f, {}),
+                                : (0, a.jsx)(g, {}),
                         ],
                     }),
                     (0, a.jsxs)("div", {
-                        className: g.J1,
+                        className: p.J1,
                         children: [
                             (0, a.jsx)(i.Text, {
                                 variant: "text-sm/normal",
                                 color: "text-subtle",
-                                children: h.intl.string(h.t.H3mPDT),
+                                children:
+                                    1 !== n.developers.length ? f.intl.string(f.t.KATEJB) : f.intl.string(f.t.na3PT0),
                             }),
                             S
                                 ? (0, a.jsx)(i.Text, {
                                       variant: "text-sm/normal",
                                       color: "text-subtle",
-                                      className: g.Gu,
-                                      children: new Date(j).toLocaleDateString(h.intl.currentLocale, {
-                                          year: "numeric",
-                                          month: "long",
-                                          day: "numeric",
-                                      }),
+                                      className: p.Gu,
+                                      children: j,
                                   })
-                                : (0, a.jsx)(f, {}),
+                                : (0, a.jsx)(g, {}),
                         ],
                     }),
                     (0, a.jsxs)("div", {
-                        className: g.J1,
+                        className: p.J1,
                         children: [
                             (0, a.jsx)(i.Text, {
                                 variant: "text-sm/normal",
                                 color: "text-subtle",
-                                children: E.length > 1 ? h.intl.string(h.t.PNqxNe) : h.intl.string(h.t["UxAag+"]),
+                                children: f.intl.string(f.t.H3mPDT),
                             }),
                             T
-                                ? (0, a.jsx)("div", {
-                                      className: g.Gu,
-                                      children: E.map((e) => (0, a.jsx)(m.s, { platform: e }, e)),
+                                ? (0, a.jsx)(i.Text, {
+                                      variant: "text-sm/normal",
+                                      color: "text-subtle",
+                                      className: p.Gu,
+                                      children: r.i$(new Date(E), "LL"),
                                   })
-                                : (0, a.jsx)(f, {}),
+                                : (0, a.jsx)(g, {}),
                         ],
                     }),
                     (0, a.jsxs)("div", {
-                        className: g.J1,
+                        className: p.J1,
                         children: [
                             (0, a.jsx)(i.Text, {
                                 variant: "text-sm/normal",
                                 color: "text-subtle",
-                                children: h.intl.string(h.t["Oj3o1/"]),
+                                children: b.length > 1 ? f.intl.string(f.t.PNqxNe) : f.intl.string(f.t["UxAag+"]),
                             }),
-                            k
+                            y
                                 ? (0, a.jsx)("div", {
-                                      className: g.Gu,
-                                      children: C.map((e) => (0, a.jsx)(u.K, { website: e, trackAction: _ }, e.url)),
+                                      className: p.Gu,
+                                      children: b.map((e) => (0, a.jsx)(x.s, { platform: e }, e)),
                                   })
-                                : (0, a.jsx)(f, {}),
+                                : (0, a.jsx)(g, {}),
                         ],
                     }),
                     (0, a.jsxs)("div", {
-                        className: g.J1,
+                        className: p.J1,
                         children: [
                             (0, a.jsx)(i.Text, {
                                 variant: "text-sm/normal",
                                 color: "text-subtle",
-                                children: h.intl.string(h.t["BwQ+9e"]),
+                                children: f.intl.string(f.t["Oj3o1/"]),
+                            }),
+                            L
+                                ? (0, a.jsx)("div", {
+                                      className: p.Gu,
+                                      children: C.map((e) => (0, a.jsx)(m.K, { website: e, trackAction: _ }, e.url)),
+                                  })
+                                : (0, a.jsx)(g, {}),
+                        ],
+                    }),
+                    (0, a.jsxs)("div", {
+                        className: p.J1,
+                        children: [
+                            (0, a.jsx)(i.Text, {
+                                variant: "text-sm/normal",
+                                color: "text-subtle",
+                                children: f.intl.string(f.t["BwQ+9e"]),
                             }),
                             (0, a.jsx)(i.Text, {
                                 variant: "text-sm/normal",
                                 color: "text-subtle",
-                                className: g.Gu,
-                                children: h.intl.format(h.t.XPFZVl, { igdbLink: x.s8 }),
+                                className: p.Gu,
+                                children: f.intl.format(f.t.XPFZVl, { igdbLink: h.s8 }),
                             }),
                         ],
                     }),
                 ],
             }),
-            (0, a.jsx)("div", { className: g.OQ, children: (0, a.jsx)(d.A, { application: t, trackAction: _ }) }),
+            (0, a.jsx)("div", { className: p.OQ, children: (0, a.jsx)(u.A, { application: t, trackAction: _ }) }),
         ],
     });
 };
