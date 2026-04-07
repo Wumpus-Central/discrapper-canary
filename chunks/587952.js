ace9f4a1392bfb5c3929746e4a715875cc9678c3
DiscrapperManager <admin@minerpl.xyz>
2026-04-07 19:21:21 +0200

    canaryRelease: 524313
    Main scripts: 3
    Lazy Loaded Scripts: 2243
    Chunks:
     * Main Files: 13514
     * Scripts: 12949
    Strings: 24532
    Routes: 812
    Experiments Count: 50
    Apex Experiments Count: 239

--- chunks/587952.js
+++ chunks/587952.js
@@ -1,11 +1,12 @@
 "use strict";
-n.d(t, { $: () => d });
+n.d(t, { $: () => _ });
 var r = n(186369),
     i = n(140547),
     s = n(581007),
     a = n(962392),
     o = n(164891),
     l = n(164696),
-    u = n(840251),
-    c = n(688151);
-let d = new u.E([o.A, i.Eh, r.S, s.aC, a.O, l.Ay], c.$G.VOICE_CALL, { location: "voice call initiated" });
+    u = n(596418),
+    c = n(840251),
+    d = n(688151);
+let _ = new c.E([o.A, i.Eh, r.S, s.aC, a.O, l.Ay, u.A], d.$G.VOICE_CALL, { location: "voice call initiated" });
