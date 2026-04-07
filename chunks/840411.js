73edf527fff1e2fac60e3d60797c50adf4c4824f
DiscrapperManager <admin@minerpl.xyz>
2026-04-07 20:39:47 +0200

    canaryRelease: 524434
    Main scripts: 3
    Lazy Loaded Scripts: 2244
    Chunks:
     * Main Files: 13515
     * Scripts: 12958
    Strings: 24538
    Routes: 812
    Experiments Count: 49
    Apex Experiments Count: 239

--- chunks/840411.js
+++ chunks/840411.js
@@ -1,11 +1,11 @@
-n.d(t, { A: () => i });
 function i(e, t, n, i) {
     let l = null,
         a = null;
     n < i
         ? ((l = t[i]?.skuId ?? null), (a = t[i + 1]?.skuId ?? null))
         : ((l = t[i - 1]?.skuId ?? null), (a = t[i]?.skuId ?? null));
-    let r = [...t],
-        [s] = r.splice(n, 1);
-    return r.splice(i, 0, s), { newWishlistData: e.set("items", r), previousSkuId: l, nextSkuId: a };
+    let s = [...t],
+        [r] = s.splice(n, 1);
+    return s.splice(i, 0, r), { newWishlistData: e.set("items", s), previousSkuId: l, nextSkuId: a };
 }
+n.d(t, { A: () => i }), n(872472), n(394300);
