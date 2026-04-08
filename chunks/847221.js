391876bee0be8824366a988e4a9fb4deb7d47d9e
DiscrapperManager <admin@minerpl.xyz>
2026-04-09 00:05:17 +0200

    canaryRelease: 525497
    Main scripts: 3
    Lazy Loaded Scripts: 2272
    Chunks:
     * Main Files: 13526
     * Scripts: 12985
    Strings: 24549
    Routes: 811
    Experiments Count: 48
    Apex Experiments Count: 239

--- chunks/847221.js
+++ chunks/847221.js
@@ -1,89 +1,104 @@
-n.d(t, { A: () => p });
+n.d(t, { A: () => f });
 var i = n(627968),
     l = n(64700),
     s = n(503698),
     a = n.n(s),
     r = n(311907),
     o = n(397927),
-    d = n(609425),
-    c = n(73392),
-    u = n(769022),
-    h = n(287809),
-    A = n(562153),
-    _ = n(850457);
+    d = n(442433),
+    c = n(609425),
+    u = n(73392),
+    h = n(769022),
+    A = n(287809),
+    _ = n(562153),
+    m = n(850457);
 n(281405);
-var m = n(985018),
-    g = n(77676);
-function p(e) {
+var g = n(985018),
+    p = n(77676);
+function f(e) {
     let { channel: t } = e,
-        [n, s] = l.useState(!1),
-        p = l.useRef(null),
-        f = l.useRef(null);
+        [s, f] = l.useState(!1),
+        x = l.useRef(null),
+        E = l.useRef(null);
     l.useEffect(
         () => () => {
-            clearTimeout(f.current);
+            clearTimeout(E.current);
         },
         [],
     );
-    let x = (0, r.bG)([u.A, h.default], () => {
-            let e = u.A.getLastLeftUserId(t.id);
-            return null != e ? h.default.getUser(e) : null;
+    let I = (0, r.bG)([h.A, A.default], () => {
+            let e = h.A.getLastLeftUserId(t.id);
+            return null != e ? A.default.getUser(e) : null;
         }, [t.id]),
-        E = A.Ay.useName(t.guild_id, t.id, x),
-        I = (0, d.A)({ userId: x?.id, guildId: t.guild_id }),
-        C = (0, c.a)({ displayNameStyles: I }),
-        N = () => {
-            clearTimeout(f.current), (f.current = setTimeout(() => s(!0), 100));
+        C = _.Ay.useName(t.guild_id, t.id, I),
+        N = (0, c.A)({ userId: I?.id, guildId: t.guild_id }),
+        T = (0, u.a)({ displayNameStyles: N }),
+        S = (e) => {
+            e.preventDefault(),
+                null != I &&
+                    (0, d.L3)(e, async () => {
+                        let { default: e } = await Promise.all([
+                            n.e("97262"),
+                            n.e("42128"),
+                            n.e("84841"),
+                            n.e("50970"),
+                        ]).then(n.bind(n, 107632));
+                        return (n) => (0, i.jsx)(e, { ...n, user: I, guildId: t.guild_id, channel: t });
+                    });
         },
-        T = () => {
-            clearTimeout(f.current), (f.current = setTimeout(() => s(!1), 100));
+        b = () => {
+            clearTimeout(E.current), (E.current = setTimeout(() => f(!0), 100));
+        },
+        v = () => {
+            clearTimeout(E.current), (E.current = setTimeout(() => f(!1), 100));
         };
-    return null == x
+    return null == I
         ? null
         : (0, i.jsx)(o.YNO, {
-              targetElementRef: p,
+              targetElementRef: x,
               position: "right",
-              shouldShow: n,
-              onRequestOpen: N,
-              onRequestClose: T,
+              shouldShow: s,
+              onRequestOpen: b,
+              onRequestClose: v,
               useMouseEnter: !0,
               spacing: 17,
               renderPopout: () =>
-                  (0, i.jsx)(_.A, { channel: t, source: "ghost_user", onMouseEnter: N, onMouseLeave: T }),
+                  (0, i.jsx)(m.A, { channel: t, source: "ghost_user", onMouseEnter: b, onMouseLeave: v }),
               children: (e) => {
                   let { onClick: n, ...l } = e;
                   return (0, i.jsxs)(o.DUT, {
-                      innerRef: p,
-                      className: g.nM,
+                      innerRef: x,
+                      className: p.nM,
                       "aria-haspopup": "dialog",
-                      onClick: N,
+                      onClick: b,
+                      onContextMenu: S,
                       ...l,
-                      onMouseLeave: T,
+                      onMouseLeave: v,
                       children: [
                           (0, i.jsxs)("div", {
-                              className: g.R3,
+                              className: p.R3,
                               children: [
                                   (0, i.jsx)(o.euF, {
-                                      src: x.getAvatarURL(t.guild_id, 24),
+                                      src: I.getAvatarURL(t.guild_id, 24),
                                       size: o._3J.SIZE_24,
                                       "aria-hidden": !0,
                                       status: o.clD.INVISIBLE,
                                       statusColor: "currentColor",
                                   }),
-                                  (0, i.jsx)(o.O4, { className: g.SM }),
+                                  (0, i.jsx)(o.O4, { className: p.SM }),
                               ],
                           }),
                           (0, i.jsx)(o.Text, {
                               variant: "text-sm/normal",
-                              className: g.Pf,
-                              children: m.intl.format(m.t.TlVwqn, {
-                                  user: E,
+                              className: p.Pf,
+                              children: g.intl.format(g.t.TlVwqn, {
+                                  user: C,
                                   userHook: (e, t) =>
                                       (0, i.jsx)(
                                           o.Text,
                                           {
                                               variant: "text-sm/semibold",
-                                              className: a()(g.Xh, C),
+                                              className: a()(p.Xh, T),
                                               color: "text-muted",
                                               children: e,
                                           },
@@ -94,7 +109,7 @@ function p(e) {
                                           o.Text,
                                           {
                                               variant: "text-sm/normal",
-                                              className: g.cp,
+                                              className: p.cp,
                                               color: "text-muted",
                                               children: e,
                                           },
