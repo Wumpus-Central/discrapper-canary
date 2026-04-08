3381ebafaa6b3b77eb18ef01be2e784ab6887ffb
DiscrapperManager <admin@minerpl.xyz>
2026-04-08 19:58:15 +0200

    canaryRelease: 525168
    Main scripts: 3
    Lazy Loaded Scripts: 2273
    Chunks:
     * Main Files: 13524
     * Scripts: 12988
    Strings: 24549
    Routes: 812
    Experiments Count: 49
    Apex Experiments Count: 238

--- chunks/201438.js
+++ chunks/201438.js
@@ -1,11 +1,11 @@
 n.d(t, { A: () => r });
 var i = n(417597),
-    l = n(760751),
-    a = n(885151),
+    a = n(760751),
+    l = n(454908),
     s = n(985018);
 function r(e, t) {
-    let { data: n, isLoading: r } = (0, a.k)(e),
-        o = (0, i.bG)([l.A], () => l.A.getDetectableGame(e));
+    let { data: n, isLoading: r } = (0, l.I)(e),
+        o = (0, i.bG)([a.A], () => a.A.getDetectableGame(e));
     return {
         coverImageUrl: n?.getCoverURL(),
         gameName: n?.name ?? o?.name ?? t ?? s.intl.string(s.t.GIWFlF),
