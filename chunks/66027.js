25e3e2e38731f9c12ede5a8cce55c4f807f8aeb6
DiscrapperManager <admin@minerpl.xyz>
2026-04-08 15:29:10 +0200

    canaryRelease: 524943
    Main scripts: 3
    Lazy Loaded Scripts: 2273
    Chunks:
     * Main Files: 13523
     * Scripts: 12985
    Strings: 24544
    Routes: 812
    Experiments Count: 49
    Apex Experiments Count: 239

--- chunks/66027.js
+++ chunks/66027.js
@@ -2,7 +2,7 @@
 n.d(t, { n: () => i });
 let i = (0, n(945810).mj)({
     name: "2026-03-mobile-web-invite-server-profile",
-    kind: "user",
+    kind: "installation",
     defaultConfig: { enabled: !1 },
     variations: { 1: { enabled: !0 } },
 });
