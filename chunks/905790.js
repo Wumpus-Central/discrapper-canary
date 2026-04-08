98f2bb45d40edd92def0ffeccff86909cab0816c
DiscrapperManager <admin@minerpl.xyz>
2026-04-09 01:31:25 +0200

    canaryRelease: 525623
    Main scripts: 3
    Lazy Loaded Scripts: 2272
    Chunks:
     * Main Files: 13526
     * Scripts: 12984
    Strings: 24549
    Routes: 811
    Experiments Count: 49
    Apex Experiments Count: 238

--- chunks/905790.js
+++ chunks/905790.js
@@ -1,56 +1,58 @@
 "use strict";
-n.d(t, { A: () => A }), n(321073);
+n.d(t, { A: () => I }), n(321073);
 var r = n(627968),
     i = n(311907),
-    a = n(554146),
-    s = n(102119),
+    s = n(554146),
+    a = n(102119),
     o = n(563380),
     l = n(47671),
     u = n(346734),
     c = n(332408),
     d = n(233380),
     _ = n(826673),
-    f = n(379848),
-    p = n(287809),
-    h = n(927578),
-    m = n(793943),
+    f = n(932001),
+    p = n(186111),
+    h = n(287809),
+    m = n(927578),
+    E = n(793943),
     g = n(49999),
-    E = n(907896);
-let A = function (e) {
+    A = n(487422);
+let I = function (e) {
     let { mobile: t } = e,
-        n = (0, i.bG)([p.default], () => h.Ay.canUseClientThemes(p.default.getCurrentUser())),
-        A = (0, i.bG)([l.A], () => l.A.isCoachmark),
-        I = (0, u.a)(),
-        { activePanel: T, metadata: y } = (0, m.fy)(),
-        S = [];
-    null == T && (A && !n && S.push(a.M.CLIENT_THEMES_COACHMARK), I && n && S.push(a.M.CUSTOM_THEME_COACHMARK));
-    let [v, C] = (0, f.kn)(S, g.m.SIDEBAR, !0),
-        b = v === a.M.CLIENT_THEMES_COACHMARK,
-        N = v === a.M.CUSTOM_THEME_COACHMARK,
-        R = T === m.HP.CLIENT_THEMES || b,
-        O = T === m.HP.APP_ICON,
-        D = T === m.HP.CUSTOM_THEME || N,
-        L = T === m.HP.APPLICATION_TEST_MODE_DEBUG;
-    if (!(R || O || D || L)) return null;
-    let w = () =>
+        n = (0, i.bG)([h.default], () => m.Ay.canUseClientThemes(h.default.getCurrentUser())),
+        I = (0, i.bG)([l.A], () => l.A.isCoachmark),
+        T = (0, u.a)(),
+        { activePanel: S, metadata: y } = (0, E.fy)(),
+        v = (0, i.bG)([p.A], () => p.A.hasLayers()),
+        N = [];
+    null == S && !v && (I && !n && N.push(s.M.CLIENT_THEMES_COACHMARK), T && n && N.push(s.M.CUSTOM_THEME_COACHMARK));
+    let [C, R] = (0, f.kn)(N, g.m.SIDEBAR, !0),
+        O = C === s.M.CLIENT_THEMES_COACHMARK,
+        b = C === s.M.CUSTOM_THEME_COACHMARK,
+        D = S === E.HP.CLIENT_THEMES || O,
+        L = S === E.HP.APP_ICON,
+        w = S === E.HP.CUSTOM_THEME || b,
+        M = S === E.HP.APPLICATION_TEST_MODE_DEBUG;
+    if (!(D || L || w || M)) return null;
+    let x = () =>
         (0, r.jsxs)(r.Fragment, {
             children: [
-                R &&
+                D &&
                     (0, r.jsx)(c.A, {
-                        markAsDismissed: b
-                            ? C
+                        markAsDismissed: O
+                            ? R
                             : () => {
-                                  (0, _.Dr)(a.M.CLIENT_THEMES_COACHMARK, {
+                                  (0, _.Dr)(s.M.CLIENT_THEMES_COACHMARK, {
                                       dismissAction: g.i.INDIRECT_ACTION,
                                       forceTrack: !0,
                                   });
                               },
-                        showClientThemesCoachmark: b,
+                        showClientThemesCoachmark: O,
                     }),
-                O && (0, r.jsx)(s.A, { isCoachmark: !1, markAsDismissed: C }),
-                D && (0, r.jsx)(d.A, { metadata: y ?? {}, markAsDismissed: C, isCoachmark: N, isMobile: t }),
-                L && (0, r.jsx)(o.A, {}),
+                L && (0, r.jsx)(a.A, { isCoachmark: !1, markAsDismissed: R }),
+                w && (0, r.jsx)(d.A, { metadata: y ?? {}, markAsDismissed: R, isCoachmark: b, isMobile: t }),
+                M && (0, r.jsx)(o.A, {}),
             ],
         });
-    return t ? (0, r.jsx)("div", { className: E.A, children: w() }) : w();
+    return t ? (0, r.jsx)("div", { className: A.A, children: x() }) : x();
 };
