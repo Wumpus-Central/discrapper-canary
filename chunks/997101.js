d1d1bbbb842bd812266fe7029f42c318c5c69e62
DiscrapperManager <admin@minerpl.xyz>
2026-04-08 20:05:31 +0200

    canaryRelease: 525178
    Main scripts: 3
    Lazy Loaded Scripts: 2273
    Chunks:
     * Main Files: 13524
     * Scripts: 12988
    Strings: 24549
    Routes: 812
    Experiments Count: 49
    Apex Experiments Count: 238

--- chunks/997101.js
+++ chunks/997101.js
@@ -844,6 +844,7 @@ let i = {
             "WY",
         ]),
     },
+    EMBARGOED_BILLING_ADDRESS_COUNTRIES: new Set(["KP", "SY"]),
     ALLOWED_BILLING_ADDRESS_COUNTRIES: new Set([
         "AC",
         "AD",
