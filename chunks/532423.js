ff6c4778571719c005b894f64a24ac7a50740a3b
DiscrapperManager <admin@minerpl.xyz>
2026-04-08 22:41:05 +0200

    canaryRelease: 525373
    Main scripts: 3
    Lazy Loaded Scripts: 2273
    Chunks:
     * Main Files: 13523
     * Scripts: 12985
    Strings: 24549
    Routes: 811
    Experiments Count: 48
    Apex Experiments Count: 239

--- chunks/532423.js
+++ chunks/532423.js
@@ -3,30 +3,39 @@ var i = n(627968),
     l = n(64700),
     s = n(877624),
     a = n(554146),
-    r = n(397927),
-    o = n(367727),
+    r = n(367727),
+    o = n(954571),
     d = n(549996),
     c = n(498470),
-    u = n(652215),
-    h = n(49999),
-    A = n(258527);
+    u = n(635995),
+    h = n(652215),
+    A = n(49999);
 function _() {
     let e = (0, d.c)(s.C.NAGBAR),
         t = e?.properties.properties.oneofKind === "nagbar" ? e.properties.properties.nagbar : null,
         n = e?.promotionId,
         _ = l.useCallback(() => {
-            null != n && (0, o.qr)(a.M.PREMIUM_MARKETING_MOMENT_NAGBAR_UPSELL, n, { dismissAction: h.i.USER_DISMISS });
-        }, [n]);
+            null != n && (0, r.qr)(a.M.PREMIUM_MARKETING_MOMENT_NAGBAR_UPSELL, n, { dismissAction: A.i.USER_DISMISS });
+        }, [n]),
+        m = l.useCallback(() => {
+            o.default.track(h.HAw.APP_NOTICE_CLOSED, { notice_type: h.kqX.PREMIUM_MARKETING_NAGBAR }), _();
+        }, [_]);
     if (null == t) return null;
-    let m = (0, c.h)({ buttonAction: t.ctaAction, onClose: _ });
-    return (0, i.jsxs)(r.$Td, {
-        color: r.Hv$.PREMIUM_TIER_2,
+    let g = (0, c.h)({ buttonAction: t.ctaAction, onClose: _ });
+    return (0, i.jsxs)(u.T0, {
+        onClick: m,
         children: [
-            (0, i.jsx)(r.PMB, { noticeType: u.kqX.PREMIUM_MARKETING_NAGBAR, onClick: _ }),
-            (0, i.jsx)(r.tvc, { size: "md", color: "currentColor", className: A.P }),
-            t.body,
+            (0, i.jsx)(u.In, { children: t.body }),
             "" !== t.ctaLabel &&
-                (0, i.jsx)(r.Z_L, { noticeType: u.kqX.PREMIUM_MARKETING_NAGBAR, onClick: m, children: t.ctaLabel }),
+                (0, i.jsx)(u.fY, {
+                    text: t.ctaLabel,
+                    onClick: () => {
+                        o.default.track(h.HAw.APP_NOTICE_PRIMARY_CTA_OPENED, {
+                            notice_type: h.kqX.PREMIUM_MARKETING_NAGBAR,
+                        }),
+                            g();
+                    },
+                }),
         ],
     });
 }
