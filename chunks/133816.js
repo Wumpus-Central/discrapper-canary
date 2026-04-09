bb9a408111b262dd7b6f42ac1cd5b9e304e404aa
DiscrapperManager <admin@minerpl.xyz>
2026-04-09 20:07:18 +0200

    canaryRelease: 526007
    Main scripts: 3
    Lazy Loaded Scripts: 2271
    Chunks:
     * Main Files: 13524
     * Scripts: 12981
    Strings: 24559
    Routes: 811
    Experiments Count: 49
    Apex Experiments Count: 235

--- chunks/133816.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-n.d(t, { O: () => r });
-let r = (0, n(945810).mj)({
-    kind: "user",
-    name: "2025-02-adaptive-message-limit",
-    defaultConfig: { strategy: "off" },
-    variations: { 0: { strategy: "off" }, 1: { strategy: "conservative" }, 2: { strategy: "aggressive" } },
-});
--- chunks/326337.js
+++ chunks/326337.js
@@ -1,41 +1,35 @@
 "use strict";
-n.d(t, { I: () => y, h: () => S });
+n.d(t, { I: () => g, h: () => E });
 var r = n(311907),
     i = n(775602),
     s = n(253932),
     a = n(531685),
-    o = n(133816),
-    l = n(652215);
-let u = 145,
-    c = 400,
-    d = 37,
-    _ = 29,
-    f = 1.3,
-    p = 15,
-    h = 91,
-    m = 65,
-    E = 1.05,
-    g = 10;
-function A(e, t, n) {
+    o = n(652215);
+let l = 145,
+    u = 400,
+    c = 91,
+    d = 65,
+    _ = 1.05,
+    f = 10;
+function p(e, t, n) {
     return Math.max(t, Math.min(n, e));
 }
-function I() {
-    return Math.max(a.A.windowSize().height - u, c);
+function h() {
+    return Math.max(a.A.windowSize().height - l, u);
 }
-function T(e, t, n, r, i) {
-    return A(Math.ceil(Math.ceil(e / (t * (i / l.hH7.FONT_SIZE_DEFAULT))) * n), r, l.EMb);
+function m(e, t, n) {
+    return p(Math.ceil(Math.ceil(e / ((t ? d : c) * (n / o.hH7.FONT_SIZE_DEFAULT))) * _), f, o.EMb);
 }
-function S() {
-    let e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "getMessageLimit",
-        { strategy: t } = o.O.getConfig({ location: e });
-    if ("off" === t) return l.EMb;
-    let n = I(),
-        r = i.A.fontSize,
-        a = s.hH.getSetting();
-    return "conservative" === t ? T(n, a ? _ : d, f, p, r) : T(n, a ? m : h, E, g, r);
+function E() {
+    arguments.length > 0 && void 0 !== arguments[0] && arguments[0];
+    let e = h(),
+        t = i.A.fontSize;
+    return m(e, s.hH.getSetting(), t);
 }
-function y(e, t) {
-    let n = o.O.useConfig({ location: "useMessageLimit" }).strategy,
-        i = (0, r.bG)([a.A], () => ("off" === n ? 0 : Math.max(a.A.windowSize().height - u, c)), [n]);
-    return "off" === n ? l.EMb : "conservative" === n ? T(i, e ? _ : d, f, p, t) : T(i, e ? m : h, E, g, t);
+function g(e, t) {
+    return m(
+        (0, r.bG)([a.A], () => Math.max(a.A.windowSize().height - l, u)),
+        e,
+        t,
+    );
 }
