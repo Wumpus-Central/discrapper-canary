c74a51c67dbac1c9bc9bb1a5ef989917b4a71fe4
DiscrapperManager <admin@minerpl.xyz>
2026-04-09 03:12:55 +0200

    canaryRelease: 525693
    Main scripts: 3
    Lazy Loaded Scripts: 2271
    Chunks:
     * Main Files: 13528
     * Scripts: 12979
    Strings: 24555
    Routes: 811
    Experiments Count: 49
    Apex Experiments Count: 237

--- chunks/729168.js
+++ chunks/729168.js
@@ -1,60 +1,66 @@
-n.d(t, { A: () => c });
+n.d(t, { A: () => u });
 var i = n(627968),
-    a = n(64700),
-    l = n(503698),
-    r = n.n(l),
+    r = n(64700),
+    a = n(503698),
+    l = n.n(a),
     s = n(397927),
     o = n(770178),
-    d = n(311401);
-let c = a.forwardRef(function (e, t) {
-    let { topContent: n, title: l, subtitle: c, buttons: u, background: A, className: h } = e,
-        [_, m] = a.useState("display-lg"),
-        g = a.useCallback((e) => {
-            m(e.contentRect.width < 500 ? "display-md" : "display-lg");
+    d = n(652215),
+    c = n(192164);
+let u = r.forwardRef(function (e, t) {
+    let { topContent: n, title: a, subtitle: u, buttons: A, background: h, className: _ } = e,
+        [m, p] = r.useState("display-lg"),
+        g = r.useCallback((e) => {
+            p(e.contentRect.width < 500 ? "display-md" : "display-lg");
         }, []),
-        p = (0, o.w)(g, [], { fireOnMount: !0 });
-    return (0, i.jsx)("div", {
-        className: r()(d.iE, "theme-midnight", h),
-        ref: (e) => {
-            (p.current = e), "function" == typeof t ? t(e) : null != t && (t.current = e);
-        },
-        children: (0, i.jsxs)("div", {
-            className: d.FG,
-            children: [
-                (0, i.jsxs)(s.BJc, {
-                    className: d.Qs,
-                    direction: "vertical",
-                    justify: null != n ? "space-between" : "end",
+        E = (0, o.w)(g, [], { fireOnMount: !0 });
+    return (0, i.jsx)(s.NPJ, {
+        theme: d.NJ8.MIDNIGHT,
+        disableAdaptiveTheme: !0,
+        children: (e) =>
+            (0, i.jsx)("div", {
+                className: l()(c.iE, e, _),
+                ref: (e) => {
+                    (E.current = e), "function" == typeof t ? t(e) : null != t && (t.current = e);
+                },
+                children: (0, i.jsxs)("div", {
+                    className: c.FG,
                     children: [
-                        n,
                         (0, i.jsxs)(s.BJc, {
-                            className: d.yp,
-                            gap: 16,
+                            className: c.Qs,
+                            direction: "vertical",
+                            justify: null != n ? "space-between" : "end",
                             children: [
+                                n,
                                 (0, i.jsxs)(s.BJc, {
-                                    gap: 8,
+                                    className: c.yp,
+                                    gap: 16,
                                     children: [
-                                        (0, i.jsx)(s.Heading, {
-                                            color: "text-strong",
-                                            variant: _,
-                                            className: d.DD,
-                                            children: l,
+                                        (0, i.jsxs)(s.BJc, {
+                                            gap: 8,
+                                            children: [
+                                                (0, i.jsx)(s.Heading, {
+                                                    color: "text-strong",
+                                                    variant: m,
+                                                    className: c.DD,
+                                                    children: a,
+                                                }),
+                                                (0, i.jsx)(s.Text, {
+                                                    color: "text-strong",
+                                                    variant: "text-md/normal",
+                                                    className: c.VA,
+                                                    children: u,
+                                                }),
+                                            ],
                                         }),
-                                        (0, i.jsx)(s.Text, {
-                                            color: "text-strong",
-                                            variant: "text-md/normal",
-                                            className: d.VA,
-                                            children: c,
-                                        }),
+                                        A,
                                     ],
                                 }),
-                                u,
                             ],
                         }),
+                        h,
                     ],
                 }),
-                A,
-            ],
-        }),
+            }),
     });
 });
