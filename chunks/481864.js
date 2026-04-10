63f2a874ddfe7bc71ee9810ec1edd76d7af172d0
DiscrapperManager <admin@minerpl.xyz>
2026-04-10 16:39:43 +0200

    canaryRelease: 526692
    Main scripts: 3
    Lazy Loaded Scripts: 2277
    Chunks:
     * Main Files: 13530
     * Scripts: 13004
    Strings: 24605
    Routes: 816
    Experiments Count: 48
    Apex Experiments Count: 236

--- chunks/481864.js
+++ chunks/481864.js
@@ -1,7 +1,7 @@
 "use strict";
 n.d(t, { $u: () => s, ab: () => i, uw: () => r });
 let r = [
-    ["laughter_screaming_2s", "onnx", "6e83b727810ed54f64b5d06e3fc835e2d4769e18597b6aecb6f69be25de3d53c"],
+    ["laughter_screaming_v3_scaled", "onnx", "93d22e6923c9674b2c5d3339456796e96b17022335238ec3f93d81ae82c01cf0"],
     ["ggml-base", "bin", "60ed5bc3dd14eea856493d334349b405782ddcaf0028d4b5df4088345fba2efe"],
 ];
 function i(e, t) {
