bbceae5ab21fc782f18c9fd580428ed9a477ac42
DiscrapperManager <admin@minerpl.xyz>
2026-04-10 20:10:07 +0200

    canaryRelease: 526867
    Main scripts: 3
    Lazy Loaded Scripts: 2280
    Chunks:
     * Main Files: 13532
     * Scripts: 13010
    Strings: 24602
    Routes: 817
    Experiments Count: 48
    Apex Experiments Count: 238

--- chunks/387945.js
+++ chunks/387945.js
@@ -1,18 +1,23 @@
-r.d(t, { A: () => a });
+r.d(t, { A: () => d });
 var n = r(627968);
 r(64700);
-var l = r(397927),
-    i = r(536572),
-    s = r(561769);
-let a = (e) => {
+var l = r(575593),
+    i = r(397927),
+    s = r(320447),
+    a = r(536572),
+    u = r(561769),
+    o = r(985018);
+let d = (e) => {
     let { skuId: t, className: r } = e,
-        a = (0, s.Vm)(t),
-        u = (0, i.VG)(a);
-    return (0, n.jsx)(l.Heading, {
+        d = (0, u.Vm)(t),
+        c = (0, a.VG)(d),
+        { enabled: p } = (0, s.P)("ProductCardName"),
+        m = p && null != d && d.type === l.R.BUNDLE && d.items.length > 0;
+    return (0, n.jsx)(i.Heading, {
         variant: "heading-md/medium",
         color: "text-strong",
         className: r,
         lineClamp: 1,
-        children: u,
+        children: m ? o.intl.format(o.t.UTc0ny, { count: d.items.length, productName: c }) : c,
     });
 };
