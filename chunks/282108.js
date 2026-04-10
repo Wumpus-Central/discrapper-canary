86923768d11b65f1c6dd4e6b4fd735dbd3b633ba
DiscrapperManager <admin@minerpl.xyz>
2026-04-10 21:21:53 +0200

    canaryRelease: 526952
    Main scripts: 3
    Lazy Loaded Scripts: 2280
    Chunks:
     * Main Files: 13532
     * Scripts: 13011
    Strings: 24604
    Routes: 817
    Experiments Count: 47
    Apex Experiments Count: 240

--- chunks/282108.js
+++ chunks/282108.js
@@ -83,7 +83,6 @@ function v(e) {
                     return e.file;
                 case s.I5.SECTION:
                 case s.I5.ACTION_ROW:
-                case s.I5.CONTAINER:
                     return e.components.flatMap(v);
                 default:
                     return [];
