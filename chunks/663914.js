26c33f066259d4395b3a80e438285d9a3420dfac
DiscrapperManager <admin@minerpl.xyz>
2026-04-10 18:12:12 +0200

    canaryRelease: 526756
    Main scripts: 3
    Lazy Loaded Scripts: 2277
    Chunks:
     * Main Files: 13531
     * Scripts: 13004
    Strings: 24605
    Routes: 816
    Experiments Count: 48
    Apex Experiments Count: 237

--- chunks/663914.js
+++ chunks/663914.js
@@ -6,6 +6,7 @@ var r = (function (e) {
         (e[(e.DESKTOP_ACCOUNT_PANEL_AREA = 1)] = "DESKTOP_ACCOUNT_PANEL_AREA"),
         (e[(e.MOBILE_HOME_DOCK_AREA = 2)] = "MOBILE_HOME_DOCK_AREA"),
         (e[(e.QUEST_HOME_BANNER_DESKTOP = 3)] = "QUEST_HOME_BANNER_DESKTOP"),
+        (e[(e.QUEST_HOME_MOBILE_CAROUSEL = 4)] = "QUEST_HOME_MOBILE_CAROUSEL"),
         e
     );
 })({});
