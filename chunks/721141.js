7635cfb389de983a82215cd39b7d0f7081b70c16
DiscrapperManager <admin@minerpl.xyz>
2026-04-10 21:18:18 +0200

    canaryRelease: 526951
    Main scripts: 3
    Lazy Loaded Scripts: 2280
    Chunks:
     * Main Files: 13532
     * Scripts: 13011
    Strings: 24604
    Routes: 817
    Experiments Count: 47
    Apex Experiments Count: 240

--- chunks/721141.js
+++ chunks/721141.js
@@ -1,16 +1,21 @@
 "use strict";
-n.d(t, { y: () => s });
+n.d(t, { y: () => u });
 var r = n(652896),
-    i = n(709710),
+    i = n(723702),
+    s = n(487329),
     a = n(601900);
-let s = {
+let o = BigInt(-3821);
+function l(e) {
+    return (0, i.isMac)() && e === o ? `${e} - your Mac may be low on disk space` : e.toString();
+}
+let u = {
     getActiveErrors: (e) => {
         let { activeStreams: t } = e;
         return t
             .filter((e) => null != e.errorCode)
             .map((e) => ({
-                type: i.iy.SCREENSHARE_OS_ERROR,
-                errorMessage: e.errorCode.toString(),
+                type: s.iy.SCREENSHARE_OS_ERROR,
+                errorMessage: l(e.errorCode),
                 ...(0, a.id)((0, r._z)(e)),
             }));
     },
