13cb0e28b37b929acae6d4b0e2e239bb07e7e119
DiscrapperManager <admin@minerpl.xyz>
2026-04-11 02:18:14 +0200

    canaryRelease: 527256
    Main scripts: 3
    Lazy Loaded Scripts: 2272
    Chunks:
     * Main Files: 13530
     * Scripts: 13018
    Strings: 24611
    Routes: 817
    Experiments Count: 46
    Apex Experiments Count: 243

--- chunks/416092.js
+++ chunks/416092.js
@@ -1,30 +1,31 @@
-n.d(t, { U: () => A });
+n.d(t, { U: () => E });
 var i = n(478437),
-    s = n(367513),
-    r = n(604681),
-    a = n(313961),
-    l = n(672385),
-    o = n(950191),
-    c = n(761640),
-    d = n(734057),
+    r = n(367513),
+    l = n(604681),
+    o = n(313961),
+    s = n(672385),
+    a = n(950191),
+    d = n(761640),
+    c = n(734057),
     u = n(309010),
     _ = n(967198),
-    m = n(652215);
-let A = {
+    A = n(652215);
+let E = {
     binds: ["mod+u"],
     comboKeysBindGlobal: !0,
     action() {
         let e = _.A.getGuildId(),
             t = u.A.getChannelId(e),
-            n = d.A.getChannel(t);
-        if (null != t && null != n && n.type === i.r.GUILD_VOICE)
-            return s.A.updateChatOpen(t, !a.A.getChatOpen(t), "toggle users keybind"), !1;
+            n = c.A.getChannel(t),
+            E = null != t && n?.isVocalThread() === !0 && o.A.getUserParticipantCount(t) > 0;
+        if (null != t && null != n && (n.type === i.r.GUILD_VOICE || E))
+            return r.A.updateChatOpen(t, !o.A.getChatOpen(t), "toggle users keybind"), !1;
         if (null != t && null != n && n.type === i.r.DM) {
-            let e = c.Ay.getSection(t, n?.isDM()),
-                i = (0, o.AP)(n.getRecipientId()),
-                s = e === m.YvQ.PROFILE;
-            return (0, l.am)({ displayProfile: i, isProfileOpen: !s }), r.A.toggleUserProfileSidebarSection(), !1;
+            let e = d.Ay.getSection(t, n?.isDM()),
+                i = (0, a.AP)(n.getRecipientId()),
+                r = e === A.YvQ.PROFILE;
+            return (0, s.am)({ displayProfile: i, isProfileOpen: !r }), l.A.toggleUserProfileSidebarSection(), !1;
         }
-        return r.A.toggleMembersSection(), !1;
+        return l.A.toggleMembersSection(), !1;
     },
 };
