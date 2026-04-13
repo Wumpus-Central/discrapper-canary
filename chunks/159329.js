9dc29c23bf13c7e7809404b1c0ba5a1fc3711c13
DiscrapperManager <admin@minerpl.xyz>
2026-04-13 23:51:28 +0200

    canaryRelease: 527865
    Main scripts: 3
    Lazy Loaded Scripts: 2273
    Chunks:
     * Main Files: 13540
     * Scripts: 13027
    Strings: 24632
    Routes: 818
    Experiments Count: 47
    Apex Experiments Count: 246

--- chunks/159329.js
+++ chunks/159329.js
@@ -1,12 +1,22 @@
-n.d(t, { P3: () => r });
-let i = (0, n(600975).C)({
-    id: "2025-05_safety_flows",
-    label: "Safety Flows",
-    kind: "user",
-    defaultConfig: { enabled: !1 },
-    treatments: [{ id: 1, label: "Enabled", config: { enabled: !0 } }],
-});
-function r(e) {
-    let { location: t } = e;
-    return i.getCurrentConfig({ location: t }, { autoTrackExposure: !1 }).enabled;
+n.d(t, { P3: () => s });
+var i = n(600975),
+    l = n(250105);
+let r = (0, i.C)({
+        id: "2025-05_safety_flows",
+        label: "Safety Flows",
+        kind: "user",
+        defaultConfig: { enabled: !1 },
+        treatments: [{ id: 1, label: "Enabled", config: { enabled: !0 } }],
+    }),
+    a = (0, l.Ay)({
+        name: "2026-04-safety-flows",
+        kind: "user",
+        defaultConfig: { enabled: !1 },
+        variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
+    });
+function s(e) {
+    let { location: t } = e,
+        n = r.getCurrentConfig({ location: t }, { autoTrackExposure: !1 }).enabled,
+        i = a.getConfig({ location: t }).enabled;
+    return n || i;
 }
