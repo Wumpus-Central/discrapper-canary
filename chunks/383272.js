632f256ca5ca972ffb76eeaede1efa49432e1060
DiscrapperManager <admin@minerpl.xyz>
2026-04-13 17:25:52 +0200

    canaryRelease: 527462
    Main scripts: 3
    Lazy Loaded Scripts: 2272
    Chunks:
     * Main Files: 13532
     * Scripts: 13018
    Strings: 24624
    Routes: 817
    Experiments Count: 47
    Apex Experiments Count: 243

---
+++ chunks/383272.js
@@ -0,0 +1,9 @@
+"use strict";
+n.d(t, { g$: () => r });
+let r = (0, n(600975).C)({
+    kind: "guild",
+    id: "2026-04_server_theme",
+    label: "Server Theme",
+    defaultConfig: { enabled: !1 },
+    treatments: [{ id: 1, label: "Enable Server Theme", config: { enabled: !0 } }],
+});
