014c91de40e54def4ac2737e11e349d95deee2d7
DiscrapperManager <admin@minerpl.xyz>
2026-04-13 23:02:08 +0200

    canaryRelease: 527793
    Main scripts: 3
    Lazy Loaded Scripts: 2273
    Chunks:
     * Main Files: 13538
     * Scripts: 13029
    Strings: 24638
    Routes: 818
    Experiments Count: 47
    Apex Experiments Count: 246

---
+++ chunks/563119.js
@@ -0,0 +1,40 @@
+"use strict";
+n.d(t, { $: () => o });
+var r = n(627968);
+n(64700);
+var i = n(827734),
+    s = n(996682),
+    a = n(27989);
+let o = (e) => {
+    let {
+            size: t = "md",
+            width: n,
+            height: o,
+            color: l = i.A.colors.INTERACTIVE_ICON_DEFAULT,
+            colorClass: u = "",
+            ...c
+        } = e,
+        d = (0, a.J)(t),
+        _ = d?.width ?? n,
+        f = d?.height ?? o;
+    return (0, r.jsxs)("svg", {
+        ...(0, s.A)(c),
+        xmlns: "http://www.w3.org/2000/svg",
+        width: _,
+        height: f,
+        fill: "none",
+        viewBox: "0 0 24 24",
+        children: [
+            (0, r.jsx)("path", {
+                fill: "string" == typeof l ? l : l.css,
+                d: "M21.3 15.3a1 1 0 1 1 1.4 1.4L20.42 19l2.3 2.3a1 1 0 1 1-1.42 1.4L19 20.42l-2.3 2.3a1 1 0 1 1-1.4-1.42L17.58 19l-2.3-2.3a1 1 0 1 1 1.42-1.4L19 17.58l2.3-2.3ZM13.88 19.88A2.99 2.99 0 0 0 13 22H5a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1h9.76l-.88.88Z",
+                className: u,
+            }),
+            (0, r.jsx)("path", {
+                fill: "string" == typeof l ? l : l.css,
+                d: "M12 2a4 4 0 0 1 3.1 6.52c-.76.95-1.6 1.99-1.6 3.21V13H19c.55 0 1.06.15 1.5.4a3 3 0 0 0-.51.37l-.11.1-.88.89-.88-.88A3 3 0 0 0 13.76 18H3a1 1 0 0 1-1-1v-1a3 3 0 0 1 3-3h5.5v-1.27c0-1.22-.84-2.26-1.6-3.21A4 4 0 0 1 12 2Z",
+                className: u,
+            }),
+        ],
+    });
+};
