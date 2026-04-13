1a2a57e8d78943a23e4346df5301719d30b373d7
DiscrapperManager <admin@minerpl.xyz>
2026-04-13 20:45:12 +0200

    canaryRelease: 527629
    Main scripts: 3
    Lazy Loaded Scripts: 2273
    Chunks:
     * Main Files: 13534
     * Scripts: 13027
    Strings: 24635
    Routes: 818
    Experiments Count: 47
    Apex Experiments Count: 244

--- chunks/859587.js
+++ chunks/859587.js
@@ -1,45 +1,41 @@
-r.d(t, { x: () => o });
+r.d(t, { x: () => d });
 var n = r(64700),
-    l = r(954571),
-    i = r(440938),
-    s = r(298072),
-    a = r(623373),
-    u = r(652215);
-function o(e, t) {
-    let r = (0, i.uM)(),
-        o = (0, s.Q)(t),
-        c = n.useRef(!1),
-        d = n.useRef(r),
-        m = n.useRef(t),
-        h = n.useRef(o);
+    l = r(66455),
+    i = r(688810),
+    s = r(954571),
+    a = r(440938),
+    o = r(298072),
+    u = r(623373),
+    c = r(652215);
+function d(e, t) {
+    let r = (0, a.uM)(),
+        { analyticsLocations: d } = (0, i.Ay)(),
+        p = (0, o.Q)(t),
+        k = n.useRef(!1),
+        C = (0, l.A)(r),
+        m = (0, l.A)(t),
+        x = (0, l.A)(p),
+        g = (0, l.A)(d);
     n.useEffect(() => {
-        d.current = r;
-    }, [r]),
-        n.useEffect(() => {
-            m.current = t;
-        }, [t]),
-        n.useEffect(() => {
-            h.current = o;
-        }, [o]),
-        n.useEffect(() => {
-            if (!e) {
-                c.current = !1;
-                return;
-            }
-            if (c.current) return;
-            let t = setTimeout(() => {
-                let e = d.current,
-                    t = m.current,
-                    r = (0, a.B1)(t);
-                l.default.track(u.HAw.SHOP_CARD_HOVERED, {
-                    shop_session_id: e?.sessionId,
-                    sku_id: r ? (t.variants[h.current]?.skuId ?? t.skuId) : t.skuId,
-                    position_in_section: e?.tilePosition,
-                    card_id: e?.cardId,
-                    card_contents: (0, a.v8)(t),
-                }),
-                    (c.current = !0);
-            }, 250);
-            return () => clearTimeout(t);
-        }, [e]);
+        if (!e) {
+            k.current = !1;
+            return;
+        }
+        if (k.current) return;
+        let t = setTimeout(() => {
+            let e = C.current,
+                t = m.current,
+                r = (0, u.B1)(t);
+            s.default.track(c.HAw.SHOP_CARD_HOVERED, {
+                shop_session_id: e?.sessionId,
+                sku_id: r ? (t.variants[x.current]?.skuId ?? t.skuId) : t.skuId,
+                position_in_section: e?.tilePosition,
+                card_id: e?.cardId,
+                product_sku_ids: (0, u.v8)(t),
+                location_stack: g.current,
+            }),
+                (k.current = !0);
+        }, 250);
+        return () => clearTimeout(t);
+    }, [e, C, m, x, g]);
 }
