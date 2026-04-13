9faffc548730399e2421752ac052fc15e4a97a1b
DiscrapperManager <admin@minerpl.xyz>
2026-04-13 23:06:57 +0200

    canaryRelease: 527806
    Main scripts: 3
    Lazy Loaded Scripts: 2273
    Chunks:
     * Main Files: 13538
     * Scripts: 13028
    Strings: 24632
    Routes: 818
    Experiments Count: 47
    Apex Experiments Count: 246

--- chunks/49229.js
+++ chunks/49229.js
@@ -1,9 +1,9 @@
 "use strict";
-n.d(t, { A: () => S });
+n.d(t, { A: () => y });
 var r = n(562465),
     i = n(582754),
-    a = n(73153),
-    s = n(315982),
+    s = n(73153),
+    a = n(315982),
     o = n(395484),
     l = n(700241),
     u = n(662502),
@@ -14,8 +14,8 @@ var r = n(562465),
     p = n(157559),
     h = n(442433),
     m = n(652215),
-    g = n(235627),
-    E = n(985018);
+    E = n(235627),
+    g = n(985018);
 function A(e) {
     (0, h.Z_)(), p.A.show(e);
 }
@@ -24,46 +24,46 @@ function I() {
 }
 function T(e, t, n) {
     let { status: r, body: i } = e,
-        a = i && i.code;
+        s = i && i.code;
     switch (r) {
         case 429:
             0 === t &&
                 A({
-                    title: E.intl.string(E.t["3D5eox"]),
-                    body: E.intl.string(E.t.TuJriJ),
-                    confirmText: E.intl.string(E.t.DppXIx),
+                    title: g.intl.string(g.t["3D5eox"]),
+                    body: g.intl.string(g.t.TuJriJ),
+                    confirmText: g.intl.string(g.t.DppXIx),
                 });
             break;
         case 403:
-            if (a === m.t02.EMAIL_VERIFICATION_REQUIRED) {
+            if (s === m.t02.EMAIL_VERIFICATION_REQUIRED) {
                 A({
-                    title: E.intl.string(E.t.Gqf33E),
-                    body: E.intl.string(E.t.GHOBdx),
-                    confirmText: E.intl.string(E.t.HbTSE6),
+                    title: g.intl.string(g.t.Gqf33E),
+                    body: g.intl.string(g.t.GHOBdx),
+                    confirmText: g.intl.string(g.t.HbTSE6),
                     onConfirm: () => {
-                        s.R();
+                        a.R();
                     },
                 });
                 break;
             }
         default:
-            if (a === m.t02.USER_QUARANTINED) I();
-            else if ((0, c.O)(r, a)) break;
-            else if (a === m.t02.RELATIONSHIP_INVALID_NO_CONFIRMATION) break;
+            if (s === m.t02.USER_QUARANTINED) I();
+            else if ((0, c.O)(r, s)) break;
+            else if (s === m.t02.RELATIONSHIP_INVALID_NO_CONFIRMATION) break;
             else if (0 === t) {
-                let e = null != n ? (0, _.vU)(a || 0, n) : E.intl.string(E.t.paDJBM);
-                A({ title: E.intl.string(E.t["6moJ8s"]), body: e, confirmText: E.intl.string(E.t.BddRzS) });
+                let e = null != n ? (0, _.vU)(s || 0, n) : g.intl.string(g.t.paDJBM);
+                A({ title: g.intl.string(g.t["6moJ8s"]), body: e, confirmText: g.intl.string(g.t.BddRzS) });
             }
     }
     throw e;
 }
-let y = {
+let S = {
         sendRequest(e) {
-            let { discordTag: t, context: n, captchaPayload: i, errorUxConfig: a = 0 } = e,
-                [s, o] = t.split("#");
+            let { discordTag: t, context: n, captchaPayload: i, note: s, errorUxConfig: a = 0 } = e,
+                [o, l] = t.split("#");
             return r.Bo.post({
                 url: m.Rsh.USER_RELATIONSHIPS(),
-                body: { username: s, discriminator: parseInt(o), ...i },
+                body: { username: o, discriminator: parseInt(l), note: s, ...i },
                 context: n,
                 oldFormErrors: !0,
                 rejectWithError: !1,
@@ -75,8 +75,8 @@ let y = {
             let {
                     userId: n,
                     context: i,
-                    type: a,
-                    fromFriendSuggestion: s,
+                    type: s,
+                    fromFriendSuggestion: a,
                     confirmStrangerRequest: o,
                     captchaPayload: l,
                 } = e,
@@ -84,7 +84,7 @@ let y = {
                 c = d.default.getUser(n);
             return r.Bo.put({
                 url: m.Rsh.USER_RELATIONSHIP(n),
-                body: { type: a, from_friend_suggestion: s, confirm_stranger_request: o, ...l },
+                body: { type: s, from_friend_suggestion: a, confirm_stranger_request: o, ...l },
                 context: i,
                 oldFormErrors: !0,
                 rejectWithError: !1,
@@ -96,33 +96,33 @@ let y = {
         },
         acceptFriendRequest(e) {
             function t() {
-                i.OR.announce(E.intl.string(E.t["3goNa5"]));
+                i.OR.announce(g.intl.string(g.t["3goNa5"]));
             }
-            return y.addRelationship(e, t);
+            return S.addRelationship(e, t);
         },
         cancelFriendRequest(e, t) {
             function n() {
-                i.OR.announce(E.intl.string(E.t.pLUaxR));
+                i.OR.announce(g.intl.string(g.t.pLUaxR));
             }
-            return y.removeRelationship(e, t, n);
+            return S.removeRelationship(e, t, n);
         },
         removeFriend(e, t) {
             function n() {
-                i.OR.announce(E.intl.string(E.t.vGSLa2));
+                i.OR.announce(g.intl.string(g.t.vGSLa2));
             }
-            y.removeRelationship(e, t, n);
+            S.removeRelationship(e, t, n);
         },
         blockUser(e, t) {
             function n() {
-                i.OR.announce(E.intl.string(E.t.mU0Vrp));
+                i.OR.announce(g.intl.string(g.t.mU0Vrp));
             }
-            return y.addRelationship({ userId: e, context: t, type: m.eA$.BLOCKED }, n);
+            return S.addRelationship({ userId: e, context: t, type: m.eA$.BLOCKED }, n);
         },
         unblockUser(e, t) {
             function n() {
-                i.OR.announce(E.intl.string(E.t["9t1au7"]));
+                i.OR.announce(g.intl.string(g.t["9t1au7"]));
             }
-            return y.removeRelationship(e, t, n);
+            return S.removeRelationship(e, t, n);
         },
         removeRelationship: (e, t, n) =>
             r.Bo.del({ url: m.Rsh.USER_RELATIONSHIP(e), context: t, oldFormErrors: !0, rejectWithError: !1 })
@@ -130,14 +130,14 @@ let y = {
                     n?.();
                 })
                 .catch(() => {
-                    i.OR.announce(E.intl.string(E.t.n6Jo3E));
+                    i.OR.announce(g.intl.string(g.t.n6Jo3E));
                 }),
         updateRelationship: (e, t) =>
             r.Bo.patch({ url: m.Rsh.USER_RELATIONSHIP(e), body: { nickname: t }, rejectWithError: !1 }),
         fetchRelationships() {
             r.Bo.get({ url: m.Rsh.USER_RELATIONSHIPS(), oldFormErrors: !0, rejectWithError: !0 }).then(
-                (e) => a.h.dispatch({ type: "LOAD_RELATIONSHIPS_SUCCESS", relationships: e.body }),
-                () => a.h.dispatch({ type: "LOAD_RELATIONSHIPS_FAILURE" }),
+                (e) => s.h.dispatch({ type: "LOAD_RELATIONSHIPS_SUCCESS", relationships: e.body }),
+                () => s.h.dispatch({ type: "LOAD_RELATIONSHIPS_FAILURE" }),
             );
         },
         confirmClearPendingRelationships(e) {
@@ -150,41 +150,41 @@ let y = {
                 rejectWithError: !1,
             })
                 .then(() => {
-                    a.h.dispatch({ type: "RELATIONSHIP_PENDING_INCOMING_REMOVED" });
+                    s.h.dispatch({ type: "RELATIONSHIP_PENDING_INCOMING_REMOVED" });
                 })
                 .catch(() => {
-                    i.OR.announce(E.intl.string(E.t.n6Jo3E));
+                    i.OR.announce(g.intl.string(g.t.n6Jo3E));
                 }),
         clearPendingSpamAndIgnored: () =>
             r.Bo.del({
                 url: m.Rsh.USER_RELATIONSHIPS(),
                 query: { relationship_type: m.eA$.PENDING_INCOMING },
-                body: { filters: [g.w.SPAM, g.w.IGNORED] },
+                body: { filters: [E.w.SPAM, E.w.IGNORED] },
                 rejectWithError: !1,
             })
                 .then(() => {
-                    a.h.dispatch({ type: "RELATIONSHIP_PENDING_INCOMING_REMOVED" });
+                    s.h.dispatch({ type: "RELATIONSHIP_PENDING_INCOMING_REMOVED" });
                 })
                 .catch(() => {
-                    i.OR.announce(E.intl.string(E.t.n6Jo3E));
+                    i.OR.announce(g.intl.string(g.t.n6Jo3E));
                 }),
         ignoreUser: (e, t, n) =>
             r.Bo.put({ url: m.Rsh.IGNORE_USER(e), context: { location: t }, rejectWithError: !1 })
                 .then(() => {
                     u.A.showIgnoreSuccessToast(e, n),
-                        i.OR.announce(E.intl.string(E.t.Us93Ca)),
-                        a.h.dispatch({ type: "RELATIONSHIP_IGNORE_USER_SUCCESS", userId: e, timestamp: Date.now() });
+                        i.OR.announce(g.intl.string(g.t.Us93Ca)),
+                        s.h.dispatch({ type: "RELATIONSHIP_IGNORE_USER_SUCCESS", userId: e, timestamp: Date.now() });
                 })
                 .catch(() => {
-                    u.A.showFailedToast(), i.OR.announce(E.intl.string(E.t.n6Jo3E));
+                    u.A.showFailedToast(), i.OR.announce(g.intl.string(g.t.n6Jo3E));
                 }),
         unignoreUser: (e, t, n) =>
             r.Bo.del({ url: m.Rsh.IGNORE_USER(e), context: { location: t }, rejectWithError: !1 })
                 .then(() => {
-                    u.A.showUnignoreSuccessToast(e, n), i.OR.announce(E.intl.string(E.t.QlH5w6));
+                    u.A.showUnignoreSuccessToast(e, n), i.OR.announce(g.intl.string(g.t.QlH5w6));
                 })
                 .catch(() => {
-                    u.A.showFailedToast(), i.OR.announce(E.intl.string(E.t.n6Jo3E));
+                    u.A.showFailedToast(), i.OR.announce(g.intl.string(g.t.n6Jo3E));
                 }),
     },
-    S = y;
+    y = S;
