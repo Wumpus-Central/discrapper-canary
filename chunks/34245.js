99afcdfaf9a5ad2a42d24af2f45d20f19d47d0b7
DiscrapperManager <admin@minerpl.xyz>
2026-04-14 17:06:07 +0200

    canaryRelease: 528229
    Main scripts: 3
    Lazy Loaded Scripts: 2264
    Chunks:
     * Main Files: 13573
     * Scripts: 13003
    Strings: 24649
    Routes: 817
    Experiments Count: 47
    Apex Experiments Count: 247

--- chunks/34245.js
+++ chunks/34245.js
@@ -11,20 +11,20 @@ var r = n(627968),
     d = n(253932),
     _ = n(486020),
     f = n(957565),
-    p = n(652215),
-    h = n(985018);
+    h = n(652215),
+    p = n(985018);
 function m(e) {
     let { user: t, guildId: n, onSuccess: s } = e,
         m = (0, c.Ay)(t.id, n ?? void 0),
         E = d.Q_.useSetting(),
         { tidaWebformEnabled: g } = u.A.useExperiment({ location: "useCopyUserInfoItem" }, { autoTrackExposure: !1 }),
-        A = (0, l.A)({ id: t.id, label: h.intl.string(h.t["/AXYnE"]), onSuccess: s }),
-        I = i.useMemo(() => (null == m ? null : m.getBannerURL({ canAnimate: !0, size: p.XAf })), [m]),
+        A = (0, l.A)({ id: t.id, label: p.intl.string(p.t["/AXYnE"]), onSuccess: s }),
+        I = i.useMemo(() => (null == m ? null : m.getBannerURL({ canAnimate: !0, size: h.XAf })), [m]),
         T = i.useCallback(() => {
             (0, f.C)(t.id), s?.();
         }, [t.id, s]),
         S = i.useCallback(() => {
-            let e = _.Ay.getUserAvatarURL(t, !0, p.XAf);
+            let e = _.Ay.getUserAvatarURL(t, !0, h.XAf);
             a()(null != e, "cannot copy null avatar URL"), (0, f.C)(e), s?.();
         }, [t, s]),
         y = i.useCallback(() => {
@@ -34,24 +34,19 @@ function m(e) {
         ? g
             ? (0, r.jsxs)(o.Drp, {
                   id: "copy-user-info",
-                  label: h.intl.string(h.t.QvQeLv),
+                  label: p.intl.string(p.t.QvQeLv),
                   children: [
-                      (0, r.jsx)(o.Drp, {
-                          id: "copy-user-id",
-                          label: h.intl.string(h.t["/AXYnE"]),
-                          action: T,
-                          leadingAccessory: { type: "icon", icon: o.L9S },
-                      }),
+                      (0, r.jsx)(o.Drp, { id: "copy-user-id", label: p.intl.string(p.t["/AXYnE"]), action: T }),
                       null != t.avatar &&
                           (0, r.jsx)(o.Drp, {
                               id: "copy-user-avatar-link",
-                              label: h.intl.string(h.t.gERDvM),
+                              label: p.intl.string(p.t.gERDvM),
                               action: S,
                           }),
                       null != I &&
                           (0, r.jsx)(o.Drp, {
                               id: "copy-user-banner-link",
-                              label: h.intl.string(h.t.hsNv0R),
+                              label: p.intl.string(p.t.hsNv0R),
                               action: y,
                           }),
                   ],
