ffe8bab869380726011ab7c8bef9b30600edbf3f
DiscrapperManager <admin@minerpl.xyz>
2026-04-14 20:57:49 +0200

    canaryRelease: 528442
    Main scripts: 3
    Lazy Loaded Scripts: 2268
    Chunks:
     * Main Files: 13529
     * Scripts: 13048
    Strings: 24662
    Routes: 817
    Experiments Count: 45
    Apex Experiments Count: 246

--- chunks/370968.js
+++ chunks/370968.js
@@ -3,58 +3,59 @@ n.d(t, { A: () => I });
 var r = n(627968);
 n(64700);
 var i = n(503698),
-    a = n.n(i),
-    s = n(311907),
+    s = n.n(i),
+    a = n(311907),
     o = n(397927),
     l = n(775602),
     u = n(607470),
     c = n(422936),
     d = n(234419),
-    _ = n(465794),
+    _ = n(725807),
     f = n(811611),
     p = n(652215),
     h = n(788868),
     m = n(985018),
-    g = n(694230);
-let E = "https://cdn.discordapp.com/assets/22_211_SuperReactions_GTM_Hero_v09.mp4",
+    E = n(380875);
+let g = "https://cdn.discordapp.com/assets/22_211_SuperReactions_GTM_Hero_v09.mp4",
     A = 32;
 function I(e) {
     let { onDismiss: t } = e,
-        n = (0, s.bG)([l.A], () => l.A.useReducedMotion),
+        n = (0, a.bG)([l.A], () => l.A.useReducedMotion),
         i = m.intl.string(m.t.eikz43),
         I = (0, d.V)(),
         T = (0, c.O)(),
-        y = I?.subscription_trial != null || null != T,
-        S = { object: p.ZSU.BUTTON_CTA, section: p.JJy.SUPER_REACTION_PICKER };
+        S = I?.subscription_trial != null || null != T,
+        y = { object: p.ZSU.BUTTON_CTA, section: p.JJy.SUPER_REACTION_PICKER };
     return (0, r.jsxs)("div", {
-        className: g.iE,
+        className: E.iE,
         children: [
             (0, r.jsx)(o.DUT, {
                 onClick: t,
-                className: g.b,
+                className: E.b,
                 "aria-label": m.intl.string(m.t.WAI6xu),
                 children: (0, r.jsx)(o.PGe, { size: "md", color: "currentColor" }),
             }),
             (0, r.jsxs)("div", {
-                className: g.Qs,
+                className: E.Qs,
                 children: [
                     (0, r.jsx)("div", {
-                        className: g.jo,
+                        className: E.jo,
                         children: (0, r.jsx)(u.A, {
-                            className: a()(g.vK, { [g.Vk]: y }),
-                            src: E,
+                            className: s()(E.vK, { [E.Vk]: S }),
+                            src: g,
                             loop: !0,
                             autoPlay: !n,
                             muted: !0,
                             controls: !0,
+                            disablePictureInPicture: !0,
                         }),
                     }),
-                    y
+                    S
                         ? (0, r.jsx)(f.Ay, {
                               type: h.e.BURST_REACTION_QUICK_ACTION_UPSELL,
                               subscriptionTier: I?.subscription_trial?.sku_id ?? h.pe.TIER_2,
                               headingText: m.intl.string(m.t.Wfl5zp),
-                              analyticsLocationObject: S,
+                              analyticsLocationObject: y,
                               discountOffer: T,
                               trialOffer: I,
                               children: i,
@@ -62,25 +63,25 @@ function I(e) {
                         : (0, r.jsxs)(r.Fragment, {
                               children: [
                                   (0, r.jsxs)("div", {
-                                      className: g.wx,
+                                      className: E.wx,
                                       children: [
                                           (0, r.jsx)(o.tvc, {
                                               size: "custom",
                                               color: "currentColor",
-                                              className: g.ax,
+                                              className: E.ax,
                                               width: A,
                                               height: A,
                                           }),
                                           (0, r.jsx)(o.Heading, {
-                                              className: g.TK,
+                                              className: E.TK,
                                               variant: "heading-xl/bold",
                                               children: m.intl.string(m.t.Wfl5zp),
                                           }),
                                       ],
                                   }),
-                                  (0, r.jsx)(o.Text, { className: g.Fb, variant: "text-md/normal", children: i }),
+                                  (0, r.jsx)(o.Text, { className: E.Fb, variant: "text-md/normal", children: i }),
                                   (0, r.jsx)("div", {
-                                      className: g.sk,
+                                      className: E.sk,
                                       children: (0, r.jsx)(_.A, {
                                           subscriptionTier: h.pe.TIER_2,
                                           textOptions: { textOverride: m.intl.string(m.t.sEAnVH) },
