f3f39b742e172eb78401c53214355b33ce427709
DiscrapperManager <admin@minerpl.xyz>
2026-04-14 17:12:57 +0200

    canaryRelease: 528235
    Main scripts: 3
    Lazy Loaded Scripts: 2264
    Chunks:
     * Main Files: 13572
     * Scripts: 13003
    Strings: 24649
    Routes: 817
    Experiments Count: 47
    Apex Experiments Count: 246

--- chunks/717995.js
+++ chunks/717995.js
@@ -4,15 +4,15 @@ var r = n(627968),
     i = n(64700),
     s = n(503698),
     a = n.n(s),
-    o = n(687498),
+    o = n(445887),
     l = n(508382),
     u = n(158954),
     c = n(502939),
     d = n(397927),
     _ = n(129837),
     f = n(112317),
-    p = n(348275),
-    h = n(958263);
+    h = n(348275),
+    p = n(958263);
 function m(e, t, n) {
     return e?.ownerDocument ?? t?.ownerDocument ?? n?.ownerDocument;
 }
@@ -29,11 +29,11 @@ function I(e) {
             id: S,
             content: y,
             position: v = "top",
-            align: N = "center",
-            spacing: C = E,
+            align: C = "center",
+            spacing: N = E,
             caretConfig: R,
-            layerContext: O,
-            animationStyle: b,
+            layerContext: b,
+            animationStyle: O,
             positionKey: D,
         } = e,
         { isRichTooltip: L } = (0, f.w6)(),
@@ -42,41 +42,42 @@ function I(e) {
     i.useLayoutEffect(() => {
         x(m(T?.current, I, s.current));
     }, [T, I, s]);
-    let P = i.useMemo(() => (("left" === v || "right" === v ? A : g).includes(N) ? N : "center"), [v, N]);
+    let P = i.useMemo(() => (("left" === v || "right" === v ? A : g).includes(C) ? C : "center"), [v, C]);
     if (!n) return null;
     let k = (e) => {
         let { position: t, nudge: n } = e,
             i = t ?? v,
             s = R?.align ?? (null != n && 0 !== n ? "custom" : "center"),
             l = R?.customOffset ?? n ?? 0,
-            u = { position: R?.position ?? (0, p.l8)(i), align: s, customOffset: "custom" === s ? l : void 0 },
+            u = { position: R?.position ?? (0, h.l8)(i), align: s, customOffset: "custom" === s ? l : void 0 },
             c = (0, r.jsxs)("div", {
                 id: S,
-                className: a()(h.YL, { [h.mj]: L }),
+                className: a()(p.YL, { [p.mj]: L }),
                 role: "tooltip",
                 "data-position": i,
                 "data-mana-component": "tooltip",
                 children: [
                     (0, r.jsx)(_.z, { caretConfig: u }),
                     (0, r.jsx)("div", {
-                        className: h.rv,
+                        className: p.rv,
                         children:
                             "string" == typeof y ? (0, r.jsx)(d.Text, { variant: "text-sm/medium", children: y }) : y,
                     }),
                 ],
             });
-        return b ? (0, r.jsx)(o.animated.div, { style: b, children: c }) : c;
+        return O ? (0, r.jsx)(o.animated.div, { style: O, children: c }) : c;
     };
     if (w) {
-        let e = (0, l.Pv)(v, N),
+        let e = (0, l.Pv)(v, C),
             t = I ?? s.current;
         return (0, r.jsx)(l.Ow, {
-            className: h.BM,
+            className: p.BM,
             open: n,
-            spacing: C,
+            spacing: N,
             placement: e,
             reference: t,
             ownerDocument: M,
+            autoUpdate: null != D,
             renderLayer: (e) => {
                 let { placement: t, shift: n } = e,
                     r = -(n?.x ?? 0);
@@ -89,11 +90,11 @@ function I(e) {
         targetRef: T ?? s,
         position: v,
         align: P,
-        spacing: C,
+        spacing: N,
         positionKey: D,
         autoInvert: !0,
         nudgeAlignIntoViewport: !0,
         children: k,
     });
-    return null != O ? (0, r.jsx)(u.Wdr, { layerContext: O, children: U }) : U;
+    return null != b ? (0, r.jsx)(u.Wdr, { layerContext: b, children: U }) : U;
 }
