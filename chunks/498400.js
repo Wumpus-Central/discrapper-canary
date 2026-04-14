6e14cc95c55946afc6547f73e6888cdb6b41200c
DiscrapperManager <admin@minerpl.xyz>
2026-04-15 00:31:30 +0200

    canaryRelease: 528702
    Main scripts: 3
    Lazy Loaded Scripts: 2268
    Chunks:
     * Main Files: 13516
     * Scripts: 13037
    Strings: 24639
    Routes: 818
    Experiments Count: 44
    Apex Experiments Count: 247

--- chunks/498400.js
+++ chunks/498400.js
@@ -1,102 +1,86 @@
-n.d(t, { A: () => w }), n(323874), n(14289), n(35956);
+n.d(t, { A: () => M });
 var i = n(627968),
     s = n(64700),
     l = n(503698),
     r = n.n(l),
-    a = n(873263),
-    o = n(311907),
-    d = n(397927),
-    c = n(73153),
-    u = n(73825),
-    m = n(974544),
-    g = n(107834),
-    _ = n(793574),
-    x = n(688810),
-    h = n(323082),
-    A = n(86379),
-    p = n(160946),
-    T = n(459793),
-    f = n(726649),
-    S = n(611924),
-    E = n(532794),
-    b = n(976860),
-    C = n(351906),
-    v = n(287809),
-    N = n(295405),
-    I = n(166403),
-    j = n(954571),
-    y = n(474090),
-    O = n(560138),
-    R = n(694080),
-    L = n(553081),
-    P = n(422936),
-    D = n(234419),
-    G = n(558533),
-    M = n(788868),
-    U = n(652215),
-    k = n(285959);
-let w = function (e) {
-    let { entrypoint: t = M.Mf.UserSettings } = e;
-    (0, S.P)(f.b);
-    let n = (0, A.Hp)(),
-        { sourceAnalyticsLocations: l, analyticsLocations: w } = (0, x.Ay)(_.A.PREMIUM_MARKETING),
-        V = (0, o.bG)([I.A], () => I.A.hasFetchedSubscriptions()),
-        B = (0, o.bG)([N.A], () => N.A.hasFetchedPaymentSources),
-        F = (0, o.bG)([v.default], () => v.default.getCurrentUser()),
-        H = F?.id,
-        z = (0, D.V)(),
-        Y = (0, P.O)(),
-        X = (0, p.Y)(M.T7),
-        [K, W] = s.useState(!0),
-        Z = s.useRef(0),
-        q = (0, y.YE)(F, M.PremiumTypes.TIER_2),
-        Q = (0, o.bG)([O.A], () => O.A.hasFetched);
+    a = n(311907),
+    o = n(397927),
+    d = n(73153),
+    c = n(73825),
+    u = n(974544),
+    m = n(107834),
+    g = n(793574),
+    _ = n(688810),
+    x = n(323082),
+    h = n(86379),
+    A = n(160946),
+    p = n(459793),
+    T = n(726649),
+    f = n(611924),
+    S = n(351906),
+    E = n(287809),
+    b = n(295405),
+    C = n(166403),
+    v = n(954571),
+    N = n(474090),
+    I = n(560138),
+    j = n(694080),
+    y = n(553081),
+    O = n(422936),
+    R = n(234419),
+    L = n(558533),
+    P = n(788868),
+    D = n(652215),
+    G = n(285959);
+let M = function (e) {
+    let { entrypoint: t = P.Mf.UserSettings } = e;
+    (0, f.P)(T.b);
+    let n = (0, h.Hp)(),
+        { sourceAnalyticsLocations: l, analyticsLocations: M } = (0, _.Ay)(g.A.PREMIUM_MARKETING),
+        U = (0, a.bG)([C.A], () => C.A.hasFetchedSubscriptions()),
+        k = (0, a.bG)([b.A], () => b.A.hasFetchedPaymentSources),
+        w = (0, a.bG)([E.default], () => E.default.getCurrentUser()),
+        V = w?.id,
+        B = (0, R.V)(),
+        F = (0, O.O)(),
+        H = (0, A.Y)(P.T7),
+        [z, Y] = s.useState(!0),
+        X = s.useRef(0),
+        K = (0, N.YE)(w, P.PremiumTypes.TIER_2),
+        W = (0, a.bG)([I.A], () => I.A.hasFetched);
     s.useEffect(() => {
-        Q || h.xG();
-    }, [Q]),
+        W || x.xG();
+    }, [W]),
         s.useEffect(() => {
-            c.h.wait(async () => {
+            d.h.wait(async () => {
                 let e = Date.now();
-                await Promise.all([h.hP(), h.$o(), (0, u.zS)(null, null, U.tF5.DISCOVERY)]),
-                    (Z.current = Date.now() - e),
-                    W(!1);
+                await Promise.all([x.hP(), x.$o(), (0, c.zS)(null, null, D.tF5.DISCOVERY)]),
+                    (X.current = Date.now() - e),
+                    Y(!1);
             });
         }, []),
         s.useEffect(() => {
-            K ||
-                j.default.track(U.HAw.PREMIUM_MARKETING_PAGE_VIEWED, {
+            z ||
+                v.default.track(D.HAw.PREMIUM_MARKETING_PAGE_VIEWED, {
                     location_stack: l,
-                    load_duration_ms: Z.current,
+                    load_duration_ms: X.current,
                 });
-        }, [l, K]),
+        }, [l, z]),
         s.useEffect(() => {
-            n && (null != z || null != Y) && (0, R.u1)(z, Y);
-        }, [n, z, Y]);
-    let J = (0, a.zy)(),
-        $ = s.useRef(!1),
-        ee = V && B && X;
-    s.useEffect(() => {
-        if ($.current || !ee) return;
-        let e = new URLSearchParams(J.search).get("checkout");
-        if (null == e) return;
-        let t = { nitro_basic: M.pe.TIER_0, nitro: M.pe.TIER_2 }[e];
-        null != t &&
-            (($.current = !0),
-            (0, b.bG)(U.BVt.APPLICATION_STORE),
-            (0, E.A)({ subscriptionTier: t, analyticsLocations: w }));
-    }, [ee, J.search, w]);
-    let et = (0, o.bG)([C.A], () => C.A.enabled),
-        en = t === M.Mf.ApplicationStoreHome,
-        ei = et
-            ? (0, i.jsx)(m.A, {})
+            n && (null != B || null != F) && (0, j.u1)(B, F);
+        }, [n, B, F]);
+    let Z = (0, a.bG)([S.A], () => S.A.enabled),
+        q = t === P.Mf.ApplicationStoreHome,
+        Q = Z
+            ? (0, i.jsx)(u.A, {})
             : n
-              ? (0, i.jsx)(T.uK, {})
-              : en && q
-                ? (0, i.jsx)(x.f5, { value: w, children: (0, i.jsx)(L.A, { userId: H }) })
-                : V && X && B
+              ? (0, i.jsx)(p.uK, {})
+              : q && K
+                ? (0, i.jsx)(_.f5, { value: M, children: (0, i.jsx)(y.A, { userId: V }) })
+                : U && H && k
                   ? null
-                  : (0, i.jsx)("div", { className: r()(k.kL, k.Lq), children: (0, i.jsx)(d.y$y, {}) });
-    return null != ei
-        ? (0, i.jsxs)(i.Fragment, { children: [en && (0, i.jsx)(g.A, {}), ei] })
-        : (0, i.jsx)(x.f5, { value: w, children: (0, i.jsx)(G.A, { entrypoint: t }) });
+                  : (0, i.jsx)("div", { className: r()(G.kL, G.Lq), children: (0, i.jsx)(o.y$y, {}) });
+    return null != Q
+        ? (0, i.jsxs)(i.Fragment, { children: [q && (0, i.jsx)(m.A, {}), Q] })
+        : (0, i.jsx)(_.f5, { value: M, children: (0, i.jsx)(L.A, { entrypoint: t }) });
 };
