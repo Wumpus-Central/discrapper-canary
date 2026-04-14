b77ea16151ca0d0d4248d992d7c0fa7991475005
DiscrapperManager <admin@minerpl.xyz>
2026-04-14 20:12:52 +0200

    canaryRelease: 528376
    Main scripts: 3
    Lazy Loaded Scripts: 2264
    Chunks:
     * Main Files: 13573
     * Scripts: 13002
    Strings: 24662
    Routes: 817
    Experiments Count: 46
    Apex Experiments Count: 245

--- chunks/187559.js
+++ chunks/187559.js
@@ -1,17 +1,14 @@
-n.d(t, { MH: () => d, f1: () => r, j6: () => a });
+n.d(t, { MH: () => o, j6: () => s });
 var i = n(954571),
-    s = n(324593),
+    r = n(324593),
     l = n(652215);
-function r() {
-    i.default.track(l.HAw.USER_SETTINGS_SEARCH_PRESS);
+function s() {
+    i.default.track(l.HAw.USER_SETTINGS_SEARCH_QUERY_ENTERED, { search_session_id: r.A.getSearchSessionId() });
 }
-function a() {
-    i.default.track(l.HAw.USER_SETTINGS_SEARCH_QUERY_ENTERED, { search_session_id: s.A.getSearchSessionId() });
-}
-function d(e) {
+function o(e) {
     let { searchSessionDuration: t } = e;
     i.default.track(l.HAw.USER_SETTINGS_SEARCH_CLOSED, {
-        search_session_id: s.A.getSearchSessionId(),
+        search_session_id: r.A.getSearchSessionId(),
         search_session_duration_ms: t,
     });
 }
