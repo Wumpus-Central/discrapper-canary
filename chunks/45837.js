5a10994b1fa0dbd31e4bb676cd8029918f4a2eac
DiscrapperManager <admin@minerpl.xyz>
2026-04-14 07:47:48 +0200

    canaryRelease: 528139
    Main scripts: 3
    Lazy Loaded Scripts: 2264
    Chunks:
     * Main Files: 13573
     * Scripts: 12998
    Strings: 24640
    Routes: 817
    Experiments Count: 47
    Apex Experiments Count: 247

--- chunks/45837.js
+++ chunks/45837.js
@@ -1,18 +1,20 @@
 "use strict";
-n.d(t, { A: () => o });
+n.d(t, { A: () => l });
 var r = n(64700),
     i = n(311907),
-    a = n(775602),
-    s = n(531685);
-function o() {
+    s = n(775602),
+    a = n(253932),
+    o = n(531685);
+function l() {
     let e = arguments.length > 0 && void 0 !== arguments[0] && arguments[0],
         [t, n] = r.useState(!1),
-        o = (0, i.bG)([a.A], () => a.A.useReducedMotion),
-        l = (0, i.bG)([s.A], () => s.A.isFocused()),
-        u = !o && !e,
-        c = l && (t || u);
+        l = (0, i.bG)([s.A], () => s.A.useReducedMotion),
+        u = (0, i.bG)([o.A], () => o.A.isFocused()),
+        c = a.kt.useSetting(),
+        d = !l && !e && c,
+        _ = u && (t || d);
     return {
-        shouldAnimate: c,
+        shouldAnimate: _,
         onMouseEnter: r.useCallback(() => n(!0), []),
         onMouseLeave: r.useCallback(() => n(!1), []),
     };
