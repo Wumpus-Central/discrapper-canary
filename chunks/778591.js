97ff3d6eb7c09d0c54653cc144e45d61db8f23b1
DiscrapperManager <admin@minerpl.xyz>
2026-04-15 22:37:03 +0200

    canaryRelease: 529374
    Main scripts: 3
    Lazy Loaded Scripts: 2236
    Chunks:
     * Main Files: 13331
     * Scripts: 12988
    Strings: 24451
    Routes: 814
    Experiments Count: 45
    Apex Experiments Count: 246

--- chunks/778591.js
+++ chunks/778591.js
@@ -14,14 +14,11 @@ function d(e) {
     return (0, s.bG)([a.A], () => {
         if (null == e) return null;
         let t = a.A.getGame(e);
-        if (t?.steamReleaseStatus === r.Y.RETIRED_ABANDONED) return null;
-        let n = t?.thirdPartySkus?.find((e) => e.distributor === l.d3x.STEAM),
-            s = n?.id;
-        if (!(0, o.uJ)(s)) return c(s);
-        if (t?.websites != null) {
-            let e = t.websites.find((e) => e.category === i.V.STEAM);
-            if (e?.url != null) return e.url;
-        }
-        return null;
+        if (null == t || t.steamReleaseStatus === r.Y.RETIRED_ABANDONED) return null;
+        let n = t.websites.find((e) => e.category === i.V.STEAM)?.url,
+            s = t.thirdPartySkus.filter((e) => e.distributor === l.d3x.STEAM && !(0, o.uJ)(e.id)),
+            u = s[0]?.id,
+            d = (0, o.uJ)(u) ? null : c(u);
+        return s.length > 1 && null != n ? n : null != d ? d : null != n ? n : null;
     }, [e]);
 }
