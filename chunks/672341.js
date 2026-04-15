1a47447e7b66e265ff68a9d869dfc3d13659c73c
DiscrapperManager <admin@minerpl.xyz>
2026-04-15 08:11:37 +0200

    canaryRelease: 528957
    Main scripts: 3
    Lazy Loaded Scripts: 2268
    Chunks:
     * Main Files: 13522
     * Scripts: 13027
    Strings: 24644
    Routes: 818
    Experiments Count: 44
    Apex Experiments Count: 247

--- chunks/672341.js
+++ chunks/672341.js
@@ -1,8 +1,8 @@
 n.d(t, { A: () => en }), n(321073), n(667532);
 var i = n(627968),
     l = n(64700),
-    s = n(311907),
-    a = n(397927),
+    a = n(311907),
+    s = n(397927),
     r = n(843472),
     o = n(432371),
     c = n(775602),
@@ -13,11 +13,11 @@ var i = n(627968),
     A = n(655377),
     g = n(816144),
     p = n(928764),
-    f = n(640708),
-    _ = n(265869),
+    _ = n(640708),
+    f = n(265869),
     E = n(715757),
-    x = n(378570),
-    C = n(349435),
+    C = n(378570),
+    x = n(349435),
     S = n(452082),
     I = n(357179),
     T = n(74114),
@@ -28,24 +28,24 @@ var i = n(627968),
     j = n(383233),
     R = n(222823),
     M = n(309010),
-    D = n(287809),
+    L = n(287809),
     O = n(405269),
-    L = n(927813),
+    D = n(927813),
     P = n(661191),
-    w = n(662433),
-    k = n(375901),
+    k = n(326337),
+    w = n(375901),
     U = n(857069),
     G = n(675171),
     F = n(806621),
     H = n(636922),
     B = n(371741),
     V = n(66149),
-    K = n(922529),
-    W = n(652176),
+    W = n(922529),
+    K = n(652176),
     z = n(540385),
     Y = n(613907),
-    q = n(829444),
-    J = n(626360),
+    J = n(829444),
+    q = n(626360),
     $ = n(381941),
     Z = n(652215),
     X = n(327337),
@@ -54,10 +54,10 @@ function ee(e) {
     return null != e && e.type === Z.TZK.MESSAGE && e.content.id === e.groupId;
 }
 let et = l.memo(function (e) {
-    let { file: t, channel: n, user: l, isGroupStart: s, compact: a } = e;
+    let { file: t, channel: n, user: l, isGroupStart: a, compact: s } = e;
     return (0, i.jsx)(H.A, {
-        compact: a,
-        isGroupStart: s,
+        compact: s,
+        isGroupStart: a,
         channel: n,
         message: new j.Ay({
             id: t.id,
@@ -83,37 +83,37 @@ function en(e) {
             showNewMessagesBar: en,
             messageDisplayCompact: ei,
             channelStream: el,
-            uploads: es,
-            scrollManager: ea,
+            uploads: ea,
+            scrollManager: es,
             specs: er,
             filterAfterTimestamp: eo,
             showingQuarantineBanner: ec,
             hideSummaries: ed,
             jumpBarClassName: eu,
         } = e,
-        eh = D.default.getCurrentUser(),
-        em = () => ea.isInitialized() || m.ready,
+        eh = L.default.getCurrentUser(),
+        em = () => es.isInitialized() || m.ready,
         eA = (0, F.r)(l),
         eg = m.length > 0 && m.first()?.isFirstMessageInForumPost(l),
         ep = (0, o.cI)(l),
-        ef = (0, s.bG)([N.A], () => N.A.shouldShowTopicsBar() && !ed),
-        e_ = (0, T.l)(l.id),
+        e_ = (0, a.bG)([N.A], () => N.A.shouldShowTopicsBar() && !ed),
+        ef = (0, T.l)(l.id),
         eE = (0, S.j)(l.id, X.Rx),
-        ex = (0, u.E)(l.id),
-        eC = (0, G.A)(),
+        eC = (0, u.E)(l.id),
+        ex = (0, G.A)(),
         eS = (function (e, t) {
             if (e.isDM() && null != t)
-                if (t.type === C._j.STRANGER_DANGER)
+                if (t.type === x._j.STRANGER_DANGER)
                     return (0, i.jsx)(v.e, { channelId: e.id, warningId: t.id, senderId: e.getRecipientId() });
-                else if (t.type === C._j.LIKELY_ATO)
+                else if (t.type === x._j.LIKELY_ATO)
                     return (0, i.jsx)(h.L, { channelId: e.id, warningId: t.id, senderId: e.getRecipientId() });
                 else return (0, i.jsx)(I._, { channelId: e.id, warningId: t.id, senderId: e.getRecipientId() });
             return null;
-        })(l, e_ ?? eE ?? ex),
+        })(l, ef ?? eE ?? eC),
         eI = l.isForumPost() && !eg ? (0, i.jsx)(p.A, { postId: l.id }) : null,
-        eT = (0, _.A)(l.id),
+        eT = (0, f.A)(l.id),
         ev = (0, E.W1)(l);
-    (0, q.A)();
+    (0, J.A)();
     let eN = null,
         ey = [],
         eb = el.map((e, t) => {
@@ -131,11 +131,12 @@ function en(e) {
                               isBeforeGroup: null == e.content && ee(el[t + 1]),
                           })
                         : (0, i.jsx)(
-                              K.A,
+                              W.A,
                               {
                                   isUnread: n,
                                   isBeforeGroup: null == e.content && ee(el[t + 1]),
                                   id: n ? $.q4 : void 0,
+                                  itemId: null != e.content ? `divider-${e.contentKey ?? t}` : void 0,
                                   children: e.content,
                               },
                               `divider-${e.contentKey ?? e.unreadId ?? t}`,
@@ -170,22 +171,22 @@ function en(e) {
                     )
                 );
             }
-            if (null != eo && eo > e.content.timestamp.getTime() * L.A.Millis.SECOND) return;
+            if (null != eo && eo > e.content.timestamp.getTime() * D.A.Millis.SECOND) return;
             e.type === Z.TZK.MESSAGE && null == eN && (eN = e);
             let n = e.groupId === eN?.groupId ? eN.content.id : e.groupId,
-                s = ev && e.content.isFirstMessageInForumPost(l),
-                a = e.type === Z.TZK.THREAD_STARTER_MESSAGE ? B.VO : B.Ay;
+                a = ev && e.content.isFirstMessageInForumPost(l),
+                s = e.type === Z.TZK.THREAD_STARTER_MESSAGE ? B.VO : B.Ay;
             return (0, i.jsx)(
-                a,
+                s,
                 {
-                    compact: ei && !s,
+                    compact: ei && !a,
                     channel: l,
                     message: e.content,
                     groupId: n,
                     flashKey: e.flashKey,
-                    id: (0, k.j)(l.id, e.content.id),
+                    id: (0, w.j)(l.id, e.content.id),
                     isLastItem: t >= el.length - 1,
-                    renderContentOnly: eT || s,
+                    renderContentOnly: eT || a,
                 },
                 e.content.id,
             );
@@ -194,7 +195,7 @@ function en(e) {
     let ej = el[el.length - 1];
     if (
         (null != eh &&
-            es.forEach((e, t) => {
+            ea.forEach((e, t) => {
                 let n = 0 === t && (0, U.l)(l, ej, new j.Ay({ type: Z.lAJ.DEFAULT, author: eh }));
                 ey.push(
                     (0, i.jsx)(et, { file: e, channel: l, user: eh, isGroupStart: n, compact: ei }, `upload-${e.id}`),
@@ -215,52 +216,52 @@ function en(e) {
     ) {
         let e,
             n,
-            s = R.Ay.getOldestUnreadTimestamp(l.id),
-            r = 0 !== s ? s : P.default.extractTimestamp(l.id),
+            a = R.Ay.getOldestUnreadTimestamp(l.id),
+            r = 0 !== a ? a : P.default.extractTimestamp(l.id),
             c = (0, O.ro)(new Date(), new Date(r));
         if (
             (R.Ay.isEstimated(l.id)
                 ? ((e = c ? Q.t.wvtbbG : Q.t.tHqbtg), (n = Q.t.vaPWFe))
                 : ((e = c ? Q.t["BctFH/"] : Q.t["3wXb9P"]), (n = Q.t["4H8ldG"])),
-            ep && (0, o.Kc)(l) && eC.includes(J.i.SUMMARIES))
+            ep && (0, o.Kc)(l) && ex.includes(q.i.SUMMARIES))
         ) {
-            let s = R.Ay.ackMessageId(l.id),
+            let a = R.Ay.ackMessageId(l.id),
                 c = (0, y.L)(l.id, R.Ay.getOldestUnreadMessageId(l.id));
             if (
                 ((0, d.zV)(Z.HAw.SUMMARIES_UNREAD_BAR_VIEWED, {
                     num_unread_summaries: c,
                     num_unread_messages: H,
-                    last_ack_message_id: s,
-                    summaries_enabled_by_user: ef,
+                    last_ack_message_id: a,
+                    summaries_enabled_by_user: e_,
                     summaries_enabled_for_channel: (0, o.pk)(l),
                 }),
                 (0, o.pk)(l))
             ) {
-                let s = ef ? Q.intl.format(n, { count: H }) : Q.intl.format(e, { count: H, timestamp: r });
-                if (ef) {
+                let a = e_ ? Q.intl.format(n, { count: H }) : Q.intl.format(e, { count: H, timestamp: r });
+                if (e_) {
                     let e =
                         c > 0
                             ? (0, i.jsxs)(i.Fragment, {
                                   children: [
-                                      (0, i.jsx)(a.Text, {
+                                      (0, i.jsx)(s.Text, {
                                           variant: "text-sm/medium",
                                           color: "currentColor",
                                           children: Q.intl.format(n, { count: H }),
                                       }),
-                                      (0, i.jsx)(f.A, {
+                                      (0, i.jsx)(_.A, {
                                           style: { paddingLeft: 8, paddingRight: 8 },
                                           height: 4,
                                           width: 4,
                                       }),
-                                      (0, i.jsx)(a.Text, {
+                                      (0, i.jsx)(s.Text, {
                                           variant: "text-sm/medium",
                                           color: "currentColor",
                                           children: Q.intl.format(Q.t.CBftDc, { count: c }),
                                       }),
                                   ],
                               })
-                            : (0, i.jsx)(a.Text, { variant: "text-sm/medium", color: "currentColor", children: s });
-                    t = (0, i.jsx)(W.OZ, { scrollManager: ea, content: e, channel: l });
+                            : (0, i.jsx)(s.Text, { variant: "text-sm/medium", color: "currentColor", children: a });
+                    t = (0, i.jsx)(K.OZ, { scrollManager: es, content: e, channel: l });
                 } else {
                     let e = (0, i.jsx)("div", {
                         style: { display: "flex", textTransform: "none", alignItems: "center" },
@@ -268,47 +269,47 @@ function en(e) {
                             c > 0
                                 ? (0, i.jsxs)(i.Fragment, {
                                       children: [
-                                          (0, i.jsx)(a.Text, {
+                                          (0, i.jsx)(s.Text, {
                                               variant: "text-sm/medium",
                                               color: "currentColor",
                                               children: Q.intl.format(n, { count: H }),
                                           }),
-                                          (0, i.jsx)(f.A, {
+                                          (0, i.jsx)(_.A, {
                                               style: { paddingLeft: 8, paddingRight: 8 },
                                               height: 4,
                                               width: 4,
                                           }),
-                                          (0, i.jsx)(a.Text, {
+                                          (0, i.jsx)(s.Text, {
                                               variant: "text-sm/medium",
                                               color: "currentColor",
                                               children: Q.intl.format(Q.t.CBftDc, { count: c }),
                                           }),
                                       ],
                                   })
-                                : (0, i.jsx)(a.Text, { variant: "text-sm/medium", color: "currentColor", children: s }),
+                                : (0, i.jsx)(s.Text, { variant: "text-sm/medium", color: "currentColor", children: a }),
                     });
-                    t = (0, i.jsx)(W.GN, { content: e, channelId: l.id });
+                    t = (0, i.jsx)(K.GN, { content: e, channelId: l.id });
                 }
             }
         } else
-            eC.includes(J.i.NEW_MESSAGES) &&
-                (t = (0, i.jsx)(W.GN, { content: Q.intl.format(e, { count: H, timestamp: r }), channelId: l.id }));
+            ex.includes(q.i.NEW_MESSAGES) &&
+                (t = (0, i.jsx)(K.GN, { content: Q.intl.format(e, { count: H, timestamp: r }), channelId: l.id }));
     }
     if (
         (null == t &&
             (0, o.pk)(l) &&
-            ef &&
-            eC.includes(J.i.SUMMARIES) &&
-            (t = (0, i.jsx)(W.UK, { channel: l, scrollManager: ea })),
+            e_ &&
+            ex.includes(q.i.SUMMARIES) &&
+            (t = (0, i.jsx)(K.UK, { channel: l, scrollManager: es })),
         m.error)
     )
-        n = (0, i.jsx)(W.Ez, {
+        n = (0, i.jsx)(K.Ez, {
             loading: m.loadingMore,
             onClick: () => {
                 var e;
                 return (
                     (e = l.id),
-                    void r.A.fetchMessages({ channelId: e, limit: (0, w.h)("renderStream.reload"), truncate: !0 })
+                    void r.A.fetchMessages({ channelId: e, limit: (0, k.h)("renderStream.reload"), truncate: !0 })
                 );
             },
             className: eu,
@@ -317,22 +318,22 @@ function en(e) {
         let { jumpReturnTargetId: e } = m;
         n =
             m.loadingMore && m.jumpedToPresent
-                ? (0, i.jsx)(W.Ab, { className: eu })
+                ? (0, i.jsx)(K.Ab, { className: eu })
                 : null != e
-                  ? (0, i.jsx)(W.Ab, {
-                        type: W.ks.REPLY,
+                  ? (0, i.jsx)(K.Ab, {
+                        type: K.ks.REPLY,
                         onClick: () => {
                             r.A.jumpToMessage({ channelId: l.id, messageId: e, flash: !0 });
                         },
                         className: eu,
                     })
-                  : (0, i.jsx)(W.Ab, {
+                  : (0, i.jsx)(K.Ab, {
                         onClick: () => {
                             let e;
                             return (
-                                r.A.jumpToPresent(l.id, (0, w.h)("renderStream.jumpToPresent")),
+                                r.A.jumpToPresent(l.id, (0, k.h)("renderStream.jumpToPresent")),
                                 (e = M.A.getChannelId()),
-                                void (l.id === e && (0, x.iN)(l.id))
+                                void (l.id === e && (0, C.iN)(l.id))
                             );
                         },
                         className: eu,
--- chunks/922529.js
+++ chunks/922529.js
@@ -1,19 +1,22 @@
-n.d(e, { A: () => d });
+n.d(t, { A: () => u });
 var i = n(627968),
     l = n(64700),
-    r = n(503698),
-    s = n.n(r),
-    a = n(813516),
-    o = n(480972);
-let u = l.forwardRef(function (t, e) {
-        let { className: n, isBeforeGroup: l = !1, ...r } = t,
-            { children: u } = r;
-        return (0, i.jsx)(a.A, {
-            ...r,
-            ref: e,
+    a = n(503698),
+    s = n.n(a),
+    r = n(837381),
+    o = n(813516),
+    c = n(480972);
+let d = l.forwardRef(function (e, t) {
+        let { className: n, isBeforeGroup: l = !1, itemId: a, ...d } = e,
+            { children: u } = d,
+            { role: h, onFocus: m, ...A } = (0, r.rm)(a ?? "");
+        return (0, i.jsx)(o.A, {
+            ...d,
+            ...(null != a ? { ...A, onFocus: m } : void 0),
+            ref: t,
             role: "separator",
             "aria-label": "string" == typeof u ? u : void 0,
-            className: s()(n, { [o.yF]: !0, [o.ov]: null != u, [o.rd]: l }),
+            className: s()(n, { [c.yF]: !0, [c.ov]: null != u, [c.rd]: l }),
         });
     }),
-    d = l.memo(u);
+    u = l.memo(d);
