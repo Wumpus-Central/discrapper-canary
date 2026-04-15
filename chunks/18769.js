3742e7fe6385dfe1e1b3cc316648bddc0c27ad4e
DiscrapperManager <admin@minerpl.xyz>
2026-04-15 18:08:06 +0200

    canaryRelease: 529071
    Main scripts: 3
    Lazy Loaded Scripts: 2270
    Chunks:
     * Main Files: 13522
     * Scripts: 13035
    Strings: 24659
    Routes: 818
    Experiments Count: 45
    Apex Experiments Count: 247

--- chunks/18769.js
+++ chunks/18769.js
@@ -1,36 +1,40 @@
-r.d(t, { A: () => d });
+r.d(t, { A: () => u });
 var l = r(627968),
-    n = r(427941),
-    a = r(653793),
-    s = r(219561),
-    c = r(190503),
-    i = r(91964);
-let d = (e) => {
-    let { backgroundImage: t, logoSrc: r, supplementalImage: d, href: o, ctaText: u } = e;
-    return (0, l.jsxs)("div", {
-        className: i.rJ,
+    n = r(397927),
+    a = r(976860),
+    s = r(427941),
+    c = r(653793),
+    i = r(219561),
+    d = r(190503),
+    o = r(91964);
+let u = (e) => {
+    let { backgroundImage: t, logoSrc: r, supplementalImage: u, href: m, ctaText: h } = e,
+        p = null == h && null != m;
+    return (0, l.jsxs)(n.DUT, {
+        className: o.rJ,
+        onClick: p ? () => (0, a.pX)(m) : void 0,
         children: [
-            (0, l.jsx)(a.A, {
+            (0, l.jsx)(c.A, {
                 overflowable: !0,
-                children: (0, l.jsx)(n.A, {
+                children: (0, l.jsx)(s.A, {
                     backgroundImage: t,
                     backgroundPosition: "left",
                     padding: 24,
                     children: (0, l.jsx)("div", {
-                        className: i.rf,
+                        className: o.rf,
                         children: (0, l.jsxs)("div", {
-                            className: i.kb,
+                            className: o.kb,
                             children: [
                                 (0, l.jsx)("div", {}),
                                 null != r &&
-                                    (0, l.jsx)("div", { className: i.wm, children: (0, l.jsx)(c.A, { src: r }) }),
-                                null != u && (0, l.jsx)(s.ar, { ctaText: u, variant: "overlay-primary", href: o }),
+                                    (0, l.jsx)("div", { className: o.wm, children: (0, l.jsx)(d.A, { src: r }) }),
+                                null != h && (0, l.jsx)(i.ar, { ctaText: h, variant: "overlay-primary", href: m }),
                             ],
                         }),
                     }),
                 }),
             }),
-            null != d && (0, l.jsx)("div", { className: i.hP, children: (0, l.jsx)(c.A, { src: d }) }),
+            null != u && (0, l.jsx)("div", { className: o.hP, children: (0, l.jsx)(d.A, { src: u }) }),
         ],
     });
 };
