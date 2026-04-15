e1d4cd2a2fa66543a2efe4d396482e10eb093d7b
DiscrapperManager <admin@minerpl.xyz>
2026-04-15 22:59:06 +0200

    canaryRelease: 529403
    Main scripts: 3
    Lazy Loaded Scripts: 2236
    Chunks:
     * Main Files: 13331
     * Scripts: 12988
    Strings: 24451
    Routes: 814
    Experiments Count: 45
    Apex Experiments Count: 246

--- chunks/696292.js
+++ chunks/696292.js
@@ -57,6 +57,7 @@ var r = (function (e) {
         (e[(e.USER_PROFILE_HEADER = 52)] = "USER_PROFILE_HEADER"),
         (e[(e.USER_SETTINGS = 53)] = "USER_SETTINGS"),
         (e[(e.NITRO_HOME_PERK_CARD = 54)] = "NITRO_HOME_PERK_CARD"),
+        (e[(e.QUEST_HOME_MOBILE_CAROUSEL = 55)] = "QUEST_HOME_MOBILE_CAROUSEL"),
         e
     );
 })({});
