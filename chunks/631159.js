bbb99ade50926977e7b7b52446c0e50b1d31d603
DiscrapperManager <admin@minerpl.xyz>
2026-04-16 00:43:30 +0200

    noisesSuppression 529531

--- chunks/631159.js
+++ chunks/631159.js
@@ -1,5 +1,1 @@
-"use strict";
-function r(e) {
-    for (var t = arguments.length, n = Array(t > 1 ? t - 1 : 0), r = 1; r < t; r++) n[r - 1] = arguments[r];
-}
-n.d(t, { G: () => r });
+r.d(e,{G:()=>n});function n(t){for(var e=arguments.length,r=Array(e>1?e-1:0),n=1;n<e;n++)r[n-1]=arguments[n]}t,{G:()=>r})
