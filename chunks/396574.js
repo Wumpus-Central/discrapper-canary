a7912ff9cb520a8fd4907dcbc55aaf3dbb683be2
DiscrapperManager <admin@minerpl.xyz>
2026-04-16 08:49:16 +0200

    canaryRelease: 529841
    Main scripts: 3
    Lazy Loaded Scripts: 2236
    Chunks:
     * Main Files: 13334
     * Scripts: 12993
    Strings: 24449
    Routes: 814
    Experiments Count: 45
    Apex Experiments Count: 246

--- chunks/396574.js
+++ chunks/396574.js
@@ -1,27 +1,44 @@
-var n = t(214958),
-    a = t.n(n);
-let o = parseInt(a().version ?? "0", 10),
-    i = null != a().ua && a().ua.indexOf("OculusBrowser") > -1;
-(() => {
-    if (!("u" > typeof window) || null != window.WebSocket) return a().name;
-})(),
-    i ||
-        ("Firefox" === a().name && o >= 80) ||
-        ("Chrome" === a().name && o >= 37) ||
-        ("Opera" === a().name && o >= 66) ||
-        ("Node.js" === a().name && o >= 6) ||
-        ("Electron" === a().name && o >= 1) ||
-        ("Safari" === a().name && o >= 13) ||
-        a().name,
-    "Chrome" === a().name ||
-        "Safari" === a().name ||
-        ("Firefox" === a().name && o >= 80) ||
-        "Opera" === a().name ||
-        a().name,
-    "u" > typeof RTCPeerConnection && RTCPeerConnection.prototype.addTransceiver,
-    "u" > typeof RTCRtpSender && ("transform" in RTCRtpSender.prototype || RTCRtpSender.prototype),
-    ("Chrome" === a().name && o >= 58) || ("Safari" === a().name && o >= 15) || a().name,
-    ("Chrome" === a().name && o >= 72) ||
-        ("Safari" === a().name && o >= 11) ||
-        ("Opera" === a().name && o >= 60) ||
-        a().name;
+"use strict";
+n.d(t, { Hz: () => l, It: () => s, LU: () => f, PF: () => c, VP: () => o, fA: () => _, g7: () => u, zU: () => d });
+var r = n(214958),
+    i = n.n(r);
+let s = parseInt(i().version ?? "0", 10),
+    a = null != i().ua && i().ua.indexOf("OculusBrowser") > -1,
+    o = (() => {
+        if ("u" > typeof window && null == window.WebSocket) return !1;
+        switch (i().name) {
+            case "IE":
+            case "Microsoft Edge":
+                return s >= 15;
+            default:
+                return !0;
+        }
+    })(),
+    l =
+        a ||
+        ("Firefox" === i().name && s >= 80) ||
+        ("Chrome" === i().name && s >= 37) ||
+        ("Opera" === i().name && s >= 66) ||
+        ("Node.js" === i().name && s >= 6) ||
+        ("Electron" === i().name && s >= 1) ||
+        ("Safari" === i().name && s >= 13) ||
+        ("Microsoft Edge" === i().name && s >= 37),
+    u =
+        "Chrome" === i().name ||
+        "Safari" === i().name ||
+        ("Firefox" === i().name && s >= 80) ||
+        "Opera" === i().name ||
+        "Microsoft Edge" === i().name,
+    c = "u" > typeof RTCPeerConnection && "function" == typeof RTCPeerConnection.prototype.addTransceiver,
+    d =
+        "u" > typeof RTCRtpSender &&
+        ("transform" in RTCRtpSender.prototype || "createEncodedStreams" in RTCRtpSender.prototype),
+    _ =
+        ("Chrome" === i().name && s >= 58) ||
+        ("Safari" === i().name && s >= 15) ||
+        ("Firefox" === i().name && s >= 108),
+    f =
+        ("Chrome" === i().name && s >= 72) ||
+        ("Safari" === i().name && s >= 11) ||
+        ("Opera" === i().name && s >= 60) ||
+        ("Microsoft Edge" === i().name && s >= 79);
