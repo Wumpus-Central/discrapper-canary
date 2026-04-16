b98d0eaeb7e45f7231de3f54e67d62df7d598142
DiscrapperManager <admin@minerpl.xyz>
2026-04-16 02:22:02 +0200

    canaryRelease: 529677
    Main scripts: 3
    Lazy Loaded Scripts: 2236
    Chunks:
     * Main Files: 13332
     * Scripts: 12992
    Strings: 24459
    Routes: 814
    Experiments Count: 45
    Apex Experiments Count: 245

--- chunks/685321.js
+++ chunks/685321.js
@@ -1,4 +1,4 @@
-i.r(e), i.d(e, { default: () => o });
-let o = JSON.parse(
-    '{"ihBfyA":["Add to Favorites"],"PACPOV":["Game"],"snNYS+":["Clip Options"],"xkY5xS":["Try searching for something else"],"+M2iLf":["No clips found"],"JbyR1U":["Filters: ",[1,"count"]],"IZsalP":["Remove from Favorites"]}',
+s.r(i), s.d(i, { default: () => a });
+let a = JSON.parse(
+    '{"ihBfyA":["Add to Favorites"],"PACPOV":["Game"],"snNYS+":["Clip Options"],"GPfOas":["Change your shortcut"],"DLzdl7":["Edit and share your clips anytime from here."],"4K56sP":["Discord Clips works with any PC game."],"BIwQis":["Press ",[1,"keybind"]," to capture a clip while gaming."],"HOUDUm":["Setup a shortcut to capture a clip while gaming."],"m2GEpP":["All your gaming moments in one place"],"xkY5xS":["Try searching for something else"],"+M2iLf":["No clips found"],"JbyR1U":["Filters: ",[1,"count"]],"IZsalP":["Remove from Favorites"]}',
 );
