5116b09849cd144c0a7f380a6d17737648461f91
DiscrapperManager <admin@minerpl.xyz>
2026-04-17 20:13:08 +0200

    canaryRelease: 531083
    Main scripts: 3
    Lazy Loaded Scripts: 2232
    Chunks:
     * Main Files: 13364
     * Scripts: 12979
    Strings: 24473
    Routes: 815
    Experiments Count: 42
    Apex Experiments Count: 253

--- chunks/566908.js
+++ chunks/566908.js
@@ -1,30 +1,33 @@
 "use strict";
-n.d(t, { Lg: () => _, Sc: () => d, UF: () => c, dx: () => u }), n(938796);
+n.d(t, { Lg: () => f, Sc: () => _, UF: () => d, dx: () => c }), n(938796);
 var r = n(665260),
     i = n(311907),
     s = n(626584),
     a = n(677413),
     o = n(287809),
-    l = n(652215);
-let u = new s.A("Scheduled Messages");
-function c(e) {
+    l = n(336089),
+    u = n(652215);
+let c = new s.A("Scheduled Messages");
+function d(e) {
     let { content: t, flags: n } = e,
         [i, s] = (0, a.A)(t);
-    return i ? [s, (0, r.UI)(n ?? 0, l.pr7.SUPPRESS_NOTIFICATIONS)] : [t, n ?? 0];
+    return i ? [s, (0, r.UI)(n ?? 0, u.pr7.SUPPRESS_NOTIFICATIONS)] : [t, n ?? 0];
 }
-function d() {
-    return (0, i.bG)([o.default], () => o.default.getCurrentUser()?.isStaff() ?? !1);
+function _() {
+    let e = (0, i.bG)([o.default], () => o.default.getCurrentUser()?.isStaff() ?? !1),
+        t = l.A.useConfig({ location: "useCanUseScheduledMessages" });
+    return e || t;
 }
-function _(e) {
+function f(e) {
     return {
         userId: e.user_id,
         scheduledMessageId: e.scheduled_message_id,
         sendAtTimestamp: e.send_at_timestamp,
-        scheduledMessage: f(e.scheduled_message),
+        scheduledMessage: p(e.scheduled_message),
         state: e.state,
     };
 }
-function f(e) {
+function p(e) {
     return {
         channelId: e.channel_id,
         content: e.content,
