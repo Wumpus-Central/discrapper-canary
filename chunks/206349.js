9eb7414179304df028f40c974dc6e7cf353b4ecd
DiscrapperManager <admin@minerpl.xyz>
2026-04-17 22:08:56 +0200

    canaryRelease: 531227
    Main scripts: 3
    Lazy Loaded Scripts: 2231
    Chunks:
     * Main Files: 13367
     * Scripts: 12971
    Strings: 24473
    Routes: 815
    Experiments Count: 42
    Apex Experiments Count: 251

--- chunks/206349.js
+++ chunks/206349.js
@@ -1,9 +1,9 @@
-n.d(t, { A: () => b });
+n.d(t, { A: () => E });
 var i = n(627968),
     s = n(64700),
     l = n(503698),
-    a = n.n(l),
-    r = n(311907),
+    r = n.n(l),
+    a = n(311907),
     o = n(397927),
     d = n(73153),
     c = n(923408),
@@ -12,28 +12,31 @@ var i = n(627968),
     g = n(160946),
     _ = n(178929),
     x = n(859241),
-    A = n(295405),
-    h = n(166403),
+    h = n(295405),
+    A = n(166403),
     p = n(767692),
     T = n(196199),
     f = n(652215),
-    S = n(724227);
-function b() {
+    S = n(752606);
+function E() {
     let e = (0, _.j)("UserSettingsPremiumGuildSubscriptions");
     s.useEffect(() => {
         d.h.wait(() => {
             m.hP(), m.$o(), (0, c.CD)(), (0, u.zS)(null, null, f.tF5.DISCOVERY), (0, c.tO)(!0);
         });
     }, []);
-    let { hasFetchedSubscriptions: t, premiumSubscription: n } = (0, r.cf)([h.A], () => ({
-            hasFetchedSubscriptions: h.A.hasFetchedSubscriptions(),
-            premiumSubscription: h.A.getPremiumTypeSubscription(),
+    let { hasFetchedSubscriptions: t, premiumSubscription: n } = (0, a.cf)([A.A], () => ({
+            hasFetchedSubscriptions: A.A.hasFetchedSubscriptions(),
+            premiumSubscription: A.A.getPremiumTypeSubscription(),
         })),
         l = (0, g.Y)(),
-        b = (0, r.bG)([A.A], () => A.A.hasFetchedPaymentSources),
-        E = (0, r.bG)([x.A], () => x.A.isFetchingCurrentUserAppliedBoosts);
-    return t && l && b && !E
-        ? (0, i.jsxs)("div", {
+        E = (0, a.bG)([h.A], () => h.A.hasFetchedPaymentSources),
+        b = (0, a.bG)([x.A], () => x.A.isFetchingCurrentUserAppliedBoosts),
+        C = !t || !l || !E || b,
+        [v, N] = s.useState(!1);
+    return (C || v || N(!0), C && !v)
+        ? (0, i.jsx)("div", { className: r()(S.kL, S.Lq), children: (0, i.jsx)(o.y$y, {}) })
+        : (0, i.jsxs)("div", {
               className: S.kL,
               children: [
                   (0, i.jsx)("div", { className: S.Tp }),
@@ -44,6 +47,5 @@ function b() {
                           : (0, i.jsx)(p.A, { premiumSubscription: n }),
                   }),
               ],
-          })
-        : (0, i.jsx)("div", { className: a()(S.kL, S.Lq), children: (0, i.jsx)(o.y$y, {}) });
+          });
 }
