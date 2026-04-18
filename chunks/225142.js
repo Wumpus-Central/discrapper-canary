ff620d307af035088472f2419947784c5a16eb78
DiscrapperManager <admin@minerpl.xyz>
2026-04-18 09:50:05 +0200

    canaryRelease: 531672
    Main scripts: 3
    Lazy Loaded Scripts: 2232
    Chunks:
     * Main Files: 13383
     * Scripts: 12977
    Strings: 24488
    Routes: 815
    Experiments Count: 42
    Apex Experiments Count: 250

--- chunks/225142.js
+++ chunks/225142.js
@@ -39,13 +39,13 @@ let m = async (e) => {
     },
     g = function (e, t) {
         let n = !(arguments.length > 2) || void 0 === arguments[2] || arguments[2];
-        if (null == t) return;
+        if ((i.h.dispatch({ type: "SELECT_HOME_RESOURCE_CHANNEL", guildId: e, channelId: t }), null == t)) return;
         let r = u.A.getChannel(t),
-            i = _.h.getResourceForChannel(e, t);
+            f = _.h.getResourceForChannel(e, t);
         null == e ||
             o.A.isFullServerPreview(e) ||
             null == r ||
-            null == i ||
+            null == f ||
             c.default.track(h.HAw.SERVER_GUIDE_CHANNEL_SELECTED, {
                 guild_id: e,
                 channel_id: r.id,
@@ -61,6 +61,7 @@ let m = async (e) => {
             });
     },
     A = (e, t) => {
+        i.h.dispatch({ type: "SELECT_NEW_MEMBER_ACTION_CHANNEL", guildId: e, channelId: t });
         let n = u.A.getChannel(t),
             r = _.h.getActionForChannel(e, t);
         null == e ||
--- chunks/265869.js
+++ chunks/265869.js
@@ -1,19 +1,21 @@
-n.d(t, { A: () => h }), n(938796);
+n.d(t, { A: () => d }), n(938796);
 var i = n(665260),
     l = n(417597),
-    a = n(761640),
     s = n(734057),
-    r = n(309010),
-    o = n(473529),
-    c = n(813503),
-    d = n(652215),
-    u = n(746080);
-function h(e) {
-    let t = (0, l.bG)([s.A, r.A, a.Ay], () => {
+    a = n(220706),
+    r = n(473529),
+    o = n(652215),
+    c = n(746080);
+function d(e) {
+    let t = (0, l.bG)([s.A, a.A], () => {
             let t = s.A.getChannel(e);
-            if (null != t && (0, i.Lt)(t.flags, u.lx.IS_GUILD_RESOURCE_CHANNEL) && (0, c.A)(t, r.A, a.Ay))
+            if (
+                null != t &&
+                (0, i.Lt)(t.flags, c.lx.IS_GUILD_RESOURCE_CHANNEL) &&
+                a.A.getSelectedResourceChannelId(t?.guild_id) === t.id
+            )
                 return t.guild_id;
         }),
-        n = (0, o.d)(t ?? d.dJq);
+        n = (0, r.d)(t ?? o.dJq);
     return null != t && n;
 }
