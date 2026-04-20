5f1d586b4ea14f63f9a50668c4e112e0a4b23b5e
DiscrapperManager <admin@minerpl.xyz>
2026-04-20 18:49:12 +0200

    canaryRelease: 531841
    Main scripts: 3
    Lazy Loaded Scripts: 2237
    Chunks:
     * Main Files: 13364
     * Scripts: 12982
    Strings: 24490
    Routes: 815
    Experiments Count: 42
    Apex Experiments Count: 250

--- chunks/516664.js
+++ chunks/516664.js
@@ -2,15 +2,15 @@ n.d(t, { m: () => d });
 var i = n(311907),
     s = n(419954),
     l = n(287809),
-    a = n(780964),
-    r = n(417628),
+    r = n(780964),
+    a = n(417628),
     o = n(985018);
-let d = (0, s.Tf)(a.X.ACCOUNT_DISABLE_SETTING, {
+let d = (0, s.Tf)(r.X.ACCOUNT_DISABLE_SETTING, {
     useTitle: () => o.intl.string(o.t["p/Tjtp"]),
     useSubtitle: () => o.intl.string(o.t.YvDmKb),
     useLabel: () => o.intl.string(o.t.jf5GGb),
-    useVariant: () => "secondary",
-    onClick: () => (0, r.c)(!1),
+    useVariant: () => "critical-secondary",
+    onClick: () => (0, a.c)(!1),
     usePredicate: () => {
         let e = (0, i.bG)([l.default], () => l.default.getCurrentUser());
         return e?.isClaimed() ?? !1;
--- chunks/935781.js
+++ chunks/935781.js
@@ -1,12 +1,12 @@
-n.d(t, { N: () => r });
+n.d(t, { N: () => a });
 var i = n(419954),
     s = n(780964),
     l = n(417628),
-    a = n(985018);
-let r = (0, i.Tf)(s.X.ACCOUNT_DELETE_SETTING, {
-    useTitle: () => a.intl.string(a.t["gIpzR+"]),
-    useSubtitle: () => a.intl.string(a.t.Bd6dOf),
-    useLabel: () => a.intl.string(a.t["8lQ2rR"]),
-    useVariant: () => "critical-secondary",
+    r = n(985018);
+let a = (0, i.Tf)(s.X.ACCOUNT_DELETE_SETTING, {
+    useTitle: () => r.intl.string(r.t["gIpzR+"]),
+    useSubtitle: () => r.intl.string(r.t.Bd6dOf),
+    useLabel: () => r.intl.string(r.t["8lQ2rR"]),
+    useVariant: () => "critical-primary",
     onClick: () => (0, l.c)(!0),
 });
