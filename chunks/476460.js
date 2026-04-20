fafc0b7c9f7552eab3f97cde13c7829ae8995092
DiscrapperManager <admin@minerpl.xyz>
2026-04-20 23:27:07 +0200

    canaryRelease: 532151
    Main scripts: 3
    Lazy Loaded Scripts: 2237
    Chunks:
     * Main Files: 13369
     * Scripts: 12980
    Strings: 24504
    Routes: 815
    Experiments Count: 42
    Apex Experiments Count: 247

--- chunks/476460.js
+++ chunks/476460.js
@@ -1,5 +1,14 @@
-n.d(t, { Q: () => a });
+n.d(t, { Q: () => d });
 var i = n(419954),
-    s = n(780964),
-    l = n(511902);
-let a = (0, i.eA)(s.X.ACCOUNT_MFA_NESTED_PANEL, { buildLayout: () => [l.n] });
+    s = n(933297),
+    l = n(780964),
+    r = n(272338),
+    a = n(511902),
+    o = n(985018);
+let d = (0, i.eA)(l.X.ACCOUNT_MFA_NESTED_PANEL, {
+    useTrailingDecoration: () => ({
+        type: s.sw.TEXT,
+        useText: () => ((0, r.f)() ? o.intl.string(o.t.lQsY7B) : o.intl.string(o.t.WsUuTt)),
+    }),
+    buildLayout: () => [a.n],
+});
