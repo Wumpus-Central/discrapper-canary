1784021586711325294f97498f17a629e5053a24
DiscrapperManager <admin@minerpl.xyz>
2026-04-20 20:52:59 +0200

    canaryRelease: 531998
    Main scripts: 3
    Lazy Loaded Scripts: 2237
    Chunks:
     * Main Files: 13368
     * Scripts: 12980
    Strings: 24492
    Routes: 815
    Experiments Count: 42
    Apex Experiments Count: 248

--- chunks/814698.js
+++ chunks/814698.js
@@ -1,28 +1,26 @@
-n.d(t, { A: () => x });
-var a = n(627968),
-    l = n(64700),
+n.d(t, { A: () => m });
+var l = n(627968),
+    a = n(64700),
     i = n(397927),
     r = n(155718),
     s = n(975807),
-    c = n(602859),
     o = n(194362),
-    d = n(409626),
-    u = n(652215),
-    m = n(985018);
-function x(e) {
+    c = n(409626),
+    d = n(652215),
+    u = n(985018);
+function m(e) {
     let { application: t, trackAction: n } = e,
-        x = (0, c.s)("GameProfile"),
-        h = l.useCallback(async () => {
-            n(d.Ws.ClaimGame);
-            let e = await (0, o.a)(u.dSh.DEVELOPER_PORTAL_APPLICATIONS_GAME_IDENTITY);
+        m = a.useCallback(async () => {
+            n(c.Ws.ClaimGame);
+            let e = await (0, o.a)(d.dSh.DEVELOPER_PORTAL_APPLICATIONS_GAME_IDENTITY);
             (0, s.A)(e);
         }, [n]),
-        f = l.useCallback((e) => (0, a.jsx)(i.MzZ, { onClick: h, children: e }), [h]);
-    return !x || t.linkedGames?.some((e) => e.type === r.Mh.OFFICIAL)
+        f = a.useCallback((e) => (0, l.jsx)(i.MzZ, { onClick: m, children: e }), [m]);
+    return t.linkedGames?.some((e) => e.type === r.Mh.OFFICIAL)
         ? null
-        : (0, a.jsx)(i.Text, {
+        : (0, l.jsx)(i.Text, {
               variant: "text-xs/normal",
               color: "text-muted",
-              children: m.intl.format(m.t.KAjfKl, { claimLink: f }),
+              children: u.intl.format(u.t.KAjfKl, { claimLink: f }),
           });
 }
