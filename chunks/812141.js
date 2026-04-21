0217a0fa8668face4436f4448a98f5d59ae46ab8
DiscrapperManager <admin@minerpl.xyz>
2026-04-21 22:04:33 +0200

    canaryRelease: 532758
    Main scripts: 3
    Lazy Loaded Scripts: 2241
    Chunks:
     * Main Files: 13379
     * Scripts: 12989
    Strings: 24568
    Routes: 816
    Experiments Count: 42
    Apex Experiments Count: 252

--- chunks/812141.js
+++ chunks/812141.js
@@ -1,8 +1,7 @@
-"use strict";
-n.d(t, { p: () => r });
-var a = n(989349),
-    i = n.n(a);
-let r = (e) => {
+n.d(t, { p: () => l });
+var r = n(989349),
+    i = n.n(r);
+let l = (e) => {
     let t = e > 0 ? i()().add(e, "second").toISOString() : null;
     return { muted: !0, mute_config: { selected_time_window: e, end_time: t } };
 };
