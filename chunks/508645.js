3bb7e43bddfc3d877e2b378ea3c283fe500d43bd
DiscrapperManager <admin@minerpl.xyz>
2026-04-22 00:50:50 +0200

    canaryRelease: 532981
    Main scripts: 3
    Lazy Loaded Scripts: 2241
    Chunks:
     * Main Files: 13380
     * Scripts: 12991
    Strings: 24589
    Routes: 816
    Experiments Count: 41
    Apex Experiments Count: 252

--- chunks/508645.js
+++ chunks/508645.js
@@ -1,4 +1,4 @@
-t.r(r), t.d(r, { default: () => h });
-let h = JSON.parse(
-    '{"odtr7f":["Pick a preset theme or build your own. Every member sees this theme when they open the server, and it adapts to their light or dark mode. ",[8,"$link",["Learn more"],[[1,"articleUrl"]]]],"/6NbRv":["Server Theme"]}',
+h.r(r), h.d(r, { default: () => t });
+let t = JSON.parse(
+    '{"odtr7f":["Pick a preset theme or build your own. Every member sees this theme when they open the server, and it adapts to their light or dark mode. ",[8,"$link",["Learn more"],[[1,"articleUrl"]]]],"XhiIAu":["This changes how your server looks for all members."],"aZpFFn":["Enable Server Theme"],"jSrqR2":["Presets"],"HkvkHP":["Each member\'s light or dark mode preference will be shown for them."],"zry4l5":["Preview Theme"],"Ck5zxP":["Select your theme"],"/6NbRv":["Server Theme"]}',
 );
