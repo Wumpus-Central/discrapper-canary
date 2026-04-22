9e45716504f00a9b65c5a123b8891e1f71d0373f
DiscrapperManager <admin@minerpl.xyz>
2026-04-23 01:02:39 +0200

    canaryRelease: 533688
    Main scripts: 3
    Lazy Loaded Scripts: 2478
    Chunks:
     * Main Files: 9326
     * Scripts: 16611
    Strings: 24569
    Routes: 819
    Experiments Count: 42
    Apex Experiments Count: 251

--- chunks/383329.js
+++ chunks/383329.js
@@ -1,20 +1,21 @@
-n.d(t, { R: () => u });
+"use strict";
+n.d(t, { R: () => _ });
 var i = n(64700),
-    l = n(735438),
-    a = n.n(l),
-    s = n(311907),
-    r = n(735321),
+    r = n(735438),
+    a = n.n(r),
+    l = n(311907),
+    s = n(735321),
     o = n(760751),
     c = n(282435);
 function d(e) {
     return c.Bf.get(e) ?? 0;
 }
-function u() {
-    let e = (0, s.bG)([o.A], () => o.A.games),
+function _() {
+    let e = (0, l.bG)([o.A], () => o.A.games),
         t = i.useMemo(
             () =>
                 a()(e)
-                    .filter((e) => (0, r.XX)(e))
+                    .filter((e) => (0, s.XX)(e))
                     .map((e) => ({ id: e.id, value: e.id, label: e.name }))
                     .sortBy((e) => {
                         let { value: t } = e;
