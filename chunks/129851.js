f480a8d77f5f7c7b982b3d4ea804e73c44c8470b
DiscrapperManager <admin@minerpl.xyz>
2026-04-22 22:51:31 +0200

    canaryRelease: 533539
    Main scripts: 3
    Lazy Loaded Scripts: 2478
    Chunks:
     * Main Files: 9326
     * Scripts: 16610
    Strings: 24569
    Routes: 817
    Experiments Count: 42
    Apex Experiments Count: 252

--- chunks/129851.js
+++ chunks/129851.js
@@ -1,272 +1,251 @@
 "use strict";
-n.d(t, { A: () => B });
+n.d(t, { A: () => M });
 var i = n(627968),
     s = n(64700),
     r = n(503698),
     l = n.n(r),
-    a = n(311907),
-    o = n(289873),
-    c = n(830215),
-    d = n(6981),
-    u = n(29093),
-    _ = n(8e5),
-    h = n(934842),
-    m = n(664053),
-    p = n(944300),
-    g = n(854378),
-    A = n(952116),
-    f = n(491509),
-    x = n(895600),
-    E = n(559568),
-    I = n(274303),
-    v = n(13437),
-    N = n(148864),
-    j = n(976860),
-    C = n(913612),
-    T = n(210714),
-    b = n(933924),
-    S = n(961350),
-    y = n(115036),
-    R = n(954571),
-    w = n(538796),
-    O = n(652215),
-    L = n(401755),
-    k = n(985018),
-    G = n(379154),
-    U = n(818050);
-class D extends s.PureComponent {
-    componentDidMount() {
-        let { handoffAvailable: e, authenticated: t, giftCodeSKU: n, invite: i, location: s } = this.props;
-        e && !t ? (0, d.ST)() : t && this.props.loginOrSSO(t, s, !0),
-            R.default.track(
-                O.HAw.LOGIN_VIEWED,
+    a = n(935399),
+    o = n(311907),
+    c = n(289873),
+    d = n(830215),
+    u = n(6981),
+    _ = n(475743),
+    h = n(29093),
+    m = n(8e5),
+    g = n(934842),
+    p = n(664053),
+    A = n(944300),
+    f = n(854378),
+    x = n(952116),
+    E = n(491509),
+    I = n(895600),
+    v = n(559568),
+    N = n(274303),
+    j = n(13437),
+    C = n(148864),
+    T = n(976860),
+    b = n(913612),
+    y = n(210714),
+    S = n(933924),
+    R = n(961350),
+    w = n(115036),
+    O = n(954571),
+    L = n(538796),
+    k = n(652215),
+    G = n(401755),
+    U = n(985018),
+    P = n(379154),
+    D = n(818050);
+let B = (e) => n.g.location.assign(e);
+function M(e) {
+    let {
+        invite: t,
+        guildTemplate: n,
+        giftCodeSKU: r,
+        giftCode: M,
+        location: V,
+        redirectTo: F,
+        transitionTo: W,
+        replaceWith: H,
+        authBoxClassName: z,
+        showMobileWebHandoff: K,
+        loginSource: Q,
+        disableAutofocusOnDefaultForm: q,
+    } = e;
+    (0, b.K)();
+    let Y = (0, o.bG)([R.default], () => R.default.isAuthenticated()),
+        X = (0, o.bG)([w.A], () => w.A.isHandoffAvailable()),
+        $ = (0, o.bG)([R.default], () => R.default.getLoginStatus()),
+        Z = (0, o.bG)([C.A], () => C.A.getCountryCode()),
+        J = (0, o.bG)([N.A], () => N.A.getHasLoggedInAccounts()),
+        {
+            checkingHandoff: ee,
+            redirecting: et,
+            login: en,
+            password: ei,
+            phoneVerifyError: es,
+            dismissedChooseAccount: er,
+            setDismissedChooseAccount: el,
+            errors: ea,
+            conditionalMediationAbortController: eo,
+            loginSource: ec,
+            giftCodeSKUId: ed,
+            loginOrSSO: eu,
+            loginRef: e_,
+            passwordRef: eh,
+            codeRef: em,
+            handleLogin: eg,
+            handleIPAuthorize: ep,
+            handlePasswordReset: eA,
+            handleForgotPassword: ef,
+            handleResendCode: ex,
+            handleReset: eE,
+            handleCancelAccountDeletion: eI,
+            handleGotoRegister: ev,
+            handleAuthSuccess: eN,
+            loginReset: ej,
+            onLoginChange: eC,
+            onPasswordChange: eT,
+            setLoginRef: eb,
+            setPasswordRef: ey,
+        } = (0, A.A)({
+            invite: t,
+            guildTemplate: n,
+            giftCode: M,
+            handoffAvailable: X,
+            authenticated: Y,
+            transitionTo: W ?? B,
+            replaceWith: H,
+            redirectTo: F,
+            location: V,
+            loginSource: Q,
+        });
+    (0, a.Ay)(() => {
+        X && !Y ? (0, u.ST)() : Y && eu(Y, V, !0),
+            O.default.track(
+                k.HAw.LOGIN_VIEWED,
                 {
-                    location: null != i ? "Invite Login Page" : "Non-Invite Login Page",
-                    login_source: this.props.loginSource,
-                    authenticated: t,
-                    ...(null != n ? (0, f.A)(n, !1, !1) : {}),
-                    source: (0, j.PR)(),
+                    location: null != t ? "Invite Login Page" : "Non-Invite Login Page",
+                    login_source: ec,
+                    authenticated: Y,
+                    ...(null != r ? (0, E.A)(r, !1, !1) : {}),
+                    source: (0, T.PR)(),
                 },
                 { flush: !0 },
             ),
-            t ||
-                (0, b.a)({
-                    abortController: this.props.conditionalMediationAbortController,
-                    loginSource: this.props.loginSource,
-                    giftCodeSKUId: this.props.giftCodeSKUId,
-                }),
-            c.A.getLocationMetadata(),
-            (0, T.d)("login");
-    }
-    componentDidUpdate(e) {
-        let { authenticated: t, location: n, handoffAvailable: i } = this.props,
-            { checkingHandoff: s, redirecting: r } = this.props;
-        !t ||
-            e.authenticated ||
-            (s && (i || r)) ||
-            (this.props.conditionalMediationAbortController.abort("Transitioning to authenticated state"),
-            this.props.loginOrSSO(t, n)),
-            e.errors !== this.props.errors &&
-                (this.hasError("password")
-                    ? this.props.passwordRef.current?.focus()
-                    : this.hasError("email") || this.hasError("login")
-                      ? this.props.loginRef.current?.focus()
-                      : this.hasError("code") && this.props.codeRef.current?.focus());
-    }
-    hasError(e) {
-        return null != this.props.errors[e];
-    }
-    renderResolving() {
-        let { authBoxClassName: e, country: t } = this.props;
-        return (0, i.jsx)(m.A, {
-            authBoxClassName: e,
-            country: t,
-            login: this.props.login,
-            password: this.props.password,
-            onLoginChange: this.props.onLoginChange,
-            onPasswordChange: this.props.onPasswordChange,
-            setLoginRef: this.props.setLoginRef,
-            setPasswordRef: this.props.setPasswordRef,
-        });
-    }
-    renderDefaultForm(e) {
-        let {
-                invite: t,
-                giftCode: n,
-                loginStatus: s,
-                country: r,
-                showMobileWebHandoff: l,
-                disableAutofocusOnDefaultForm: a,
-            } = this.props,
-            {
-                dismissedChooseAccount: o,
-                login: c,
-                password: d,
-                errors: u,
-                conditionalMediationAbortController: h,
-            } = this.props;
-        return (0, i.jsx)(_.A, {
+            Y || (0, S.a)({ abortController: eo, loginSource: ec, giftCodeSKUId: ed }),
+            d.A.getLocationMetadata(),
+            (0, y.d)("login");
+    });
+    let eS = (0, _.A)(Y);
+    s.useEffect(() => {
+        let e = ee && (X || et);
+        Y && !1 === eS && !e && (eo.abort("Transitioning to authenticated state"), eu(Y, V));
+    }, [Y, X, eS, et, ee, eo, eu, V]);
+    let eR = (0, _.A)(ea);
+    s.useEffect(() => {
+        let e = (e) => null != ea[e];
+        null != eR &&
+            eR !== ea &&
+            (e("password")
+                ? eh.current?.focus()
+                : e("email") || e("login")
+                  ? e_.current?.focus()
+                  : e("code") && em.current?.focus());
+    }, [ea, e_, eh, em, eR]);
+    let ew = (e) =>
+        (0, i.jsx)(m.A, {
             invite: t,
-            giftCode: n,
-            loginStatus: s,
-            country: r,
-            showMobileWebHandoff: l,
-            disableAutofocusOnDefaultForm: a,
+            giftCode: M,
+            loginStatus: $,
+            country: Z,
+            showMobileWebHandoff: K,
+            disableAutofocusOnDefaultForm: q,
             withQR: e,
-            dismissedChooseAccount: o,
-            login: c,
-            password: d,
-            errors: u,
-            conditionalMediationAbortController: h,
-            onLoginChange: this.props.onLoginChange,
-            onPasswordChange: this.props.onPasswordChange,
-            setLoginRef: this.props.setLoginRef,
-            setPasswordRef: this.props.setPasswordRef,
+            dismissedChooseAccount: er,
+            login: en,
+            password: ei,
+            errors: ea,
+            conditionalMediationAbortController: eo,
+            onLoginChange: eC,
+            onPasswordChange: eT,
+            setLoginRef: eb,
+            setPasswordRef: ey,
             onReturnToChooseAccount: () => {
-                this.props.setDismissedChooseAccount(!1);
+                el(!1);
             },
-            handleForgotPassword: this.props.handleForgotPassword,
-            handleGotoRegister: this.props.handleGotoRegister,
-            handleAuthSuccess: this.props.handleAuthSuccess,
+            handleForgotPassword: ef,
+            handleGotoRegister: ev,
+            handleAuthSuccess: eN,
         });
-    }
-    render() {
-        let {
-                invite: e,
-                guildTemplate: t,
-                loginStatus: n,
-                handoffAvailable: s,
-                authBoxClassName: r,
-                showMobileWebHandoff: a,
-            } = this.props,
-            { checkingHandoff: c, redirecting: d, phoneVerifyError: _ } = this.props;
-        if (d || c) return (0, i.jsx)(g.Ay, { children: (0, i.jsx)(o.y, {}) });
-        if (s)
-            return (0, i.jsxs)(g.Ay, {
-                className: r,
+    if (et || ee) return (0, i.jsx)(f.Ay, { children: (0, i.jsx)(c.y, {}) });
+    if (X)
+        return (0, i.jsxs)(f.Ay, {
+            className: z,
+            children: [
+                (0, i.jsx)(f.CK, {}),
+                (0, i.jsx)(f.hE, { className: D.QB, children: U.intl.string(U.t.S6RMNA) }),
+                (0, i.jsx)(f.tK, { children: U.intl.string(U.t.YZiJbh) }),
+            ],
+        });
+    switch ($) {
+        case k.aUe.LOGGING_IN_MFA_SMS:
+        case k.aUe.MFA_SMS_STEP:
+        case k.aUe.LOGGING_IN_MFA:
+        case k.aUe.MFA_STEP:
+            return (0, i.jsx)(g.A, { loginSource: ec, giftCodeSKUId: ed });
+        case k.aUe.ACCOUNT_SCHEDULED_FOR_DELETION:
+        case k.aUe.ACCOUNT_DISABLED:
+            return (0, i.jsx)(h.A, {
+                loginStatus: $,
+                authBoxClassName: z ?? "",
+                onSubmit: eE,
+                onCancelAccountDeletion: eI,
+            });
+        case k.aUe.PHONE_IP_AUTHORIZATION:
+            return (0, i.jsx)(f.Ay, {
+                tag: "form",
+                className: z,
+                children: (0, i.jsx)(x.A, {
+                    title: U.intl.string(U.t.w55Oco),
+                    subtitle: U.intl.format(U.t.CfRZBj, { onResendClick: ex }),
+                    error: es,
+                    onSubmit: ep,
+                    onCancel: ej,
+                }),
+            });
+        case k.aUe.PASSWORD_RECOVERY_PHONE_VERIFICATION:
+            return (0, i.jsx)(f.Ay, {
+                tag: "form",
+                className: z,
+                children: (0, i.jsx)(x.A, {
+                    title: U.intl.string(U.t["+xqy3d"]),
+                    subtitle: U.intl.format(U.t.ef4uZ7, { onResendClick: ex }),
+                    error: es,
+                    onSubmit: eA,
+                    onCancel: ej,
+                }),
+            });
+        case k.aUe.LOGGING_IN:
+        case k.aUe.NONE:
+        default:
+            if (null != t && t.state === k.elq.RESOLVING)
+                return (0, i.jsx)(p.A, {
+                    authBoxClassName: z,
+                    country: Z,
+                    login: en,
+                    password: ei,
+                    onLoginChange: eC,
+                    onPasswordChange: eT,
+                    setLoginRef: eb,
+                    setPasswordRef: ey,
+                });
+            if (null != n)
+                if (n.state === G.QB.RESOLVING)
+                    return (0, i.jsx)(p.A, {
+                        authBoxClassName: z,
+                        country: Z,
+                        login: en,
+                        password: ei,
+                        onLoginChange: eC,
+                        onPasswordChange: eT,
+                        setLoginRef: eb,
+                        setPasswordRef: ey,
+                    });
+                else
+                    return (0, i.jsx)(L.A, {
+                        onSubmit: eg,
+                        tag: "form",
+                        className: l()(z, P.Sy),
+                        children: () => [(0, i.jsx)(I.A, { guildTemplate: n }, "template"), ew(!1)],
+                    });
+            if (J && !er) return (0, i.jsx)(j.A, { onDismiss: () => el(!0) });
+            return (0, i.jsxs)("div", {
+                className: P.__invalid_pageContainer,
                 children: [
-                    (0, i.jsx)(g.CK, {}),
-                    (0, i.jsx)(g.hE, { className: U.QB, children: k.intl.string(k.t.S6RMNA) }),
-                    (0, i.jsx)(g.tK, { children: k.intl.string(k.t.YZiJbh) }),
+                    (0, i.jsx)(f.Ay, { onSubmit: eg, tag: "form", className: z, expanded: !0, children: ew(!0) }),
+                    K && (0, i.jsx)(v.A, {}),
                 ],
             });
-        switch (n) {
-            case O.aUe.LOGGING_IN_MFA_SMS:
-            case O.aUe.MFA_SMS_STEP:
-            case O.aUe.LOGGING_IN_MFA:
-            case O.aUe.MFA_STEP:
-                return (0, i.jsx)(h.A, {
-                    loginSource: this.props.loginSource,
-                    giftCodeSKUId: this.props.giftCodeSKUId,
-                });
-            case O.aUe.ACCOUNT_SCHEDULED_FOR_DELETION:
-            case O.aUe.ACCOUNT_DISABLED:
-                return (0, i.jsx)(u.A, {
-                    loginStatus: this.props.loginStatus,
-                    authBoxClassName: this.props.authBoxClassName ?? "",
-                    onSubmit: this.props.handleReset,
-                    onCancelAccountDeletion: this.props.handleCancelAccountDeletion,
-                });
-            case O.aUe.PHONE_IP_AUTHORIZATION:
-                return (0, i.jsx)(g.Ay, {
-                    tag: "form",
-                    className: r,
-                    children: (0, i.jsx)(A.A, {
-                        title: k.intl.string(k.t.w55Oco),
-                        subtitle: k.intl.format(k.t.CfRZBj, { onResendClick: this.props.handleResendCode }),
-                        error: _,
-                        onSubmit: this.props.handleIPAuthorize,
-                        onCancel: this.props.loginReset,
-                    }),
-                });
-            case O.aUe.PASSWORD_RECOVERY_PHONE_VERIFICATION:
-                return (0, i.jsx)(g.Ay, {
-                    tag: "form",
-                    className: r,
-                    children: (0, i.jsx)(A.A, {
-                        title: k.intl.string(k.t["+xqy3d"]),
-                        subtitle: k.intl.format(k.t.ef4uZ7, { onResendClick: this.props.handleResendCode }),
-                        error: _,
-                        onSubmit: this.props.handlePasswordReset,
-                        onCancel: this.props.loginReset,
-                    }),
-                });
-            case O.aUe.LOGGING_IN:
-            case O.aUe.NONE:
-            default:
-                if (null != e && e.state === O.elq.RESOLVING) return this.renderResolving();
-                if (null != t)
-                    if (t.state === L.QB.RESOLVING) return this.renderResolving();
-                    else
-                        return (0, i.jsx)(w.A, {
-                            onSubmit: this.props.handleLogin,
-                            tag: "form",
-                            className: l()(this.props.authBoxClassName, G.Sy),
-                            children: () => [
-                                (0, i.jsx)(x.A, { guildTemplate: t }, "template"),
-                                this.renderDefaultForm(!1),
-                            ],
-                        });
-                if (this.props.hasLoggedInAccounts && !this.props.dismissedChooseAccount)
-                    return (0, i.jsx)(v.A, { onDismiss: () => this.props.setDismissedChooseAccount(!0) });
-                return (0, i.jsxs)("div", {
-                    className: G.__invalid_pageContainer,
-                    children: [
-                        (0, i.jsx)(g.Ay, {
-                            onSubmit: this.props.handleLogin,
-                            tag: "form",
-                            className: r,
-                            expanded: !0,
-                            children: this.renderDefaultForm(!0),
-                        }),
-                        a && (0, i.jsx)(E.A, {}),
-                    ],
-                });
-        }
     }
 }
-let P = (e) => n.g.location.assign(e),
-    B = function (e) {
-        (0, C.K)();
-        let {
-                authenticated: t,
-                handoffAvailable: n,
-                loginStatus: s,
-                country: r,
-                hasLoggedInAccounts: l,
-            } = (0, a.cf)([y.A, S.default, I.A, N.A], () => ({
-                authenticated: S.default.isAuthenticated(),
-                handoffAvailable: y.A.isHandoffAvailable(),
-                loginStatus: S.default.getLoginStatus(),
-                country: N.A.getCountryCode(),
-                hasLoggedInAccounts: I.A.getHasLoggedInAccounts(),
-            })),
-            o = (0, p.A)({
-                invite: e.invite,
-                guildTemplate: e.guildTemplate,
-                giftCode: e.giftCode,
-                handoffAvailable: n,
-                authenticated: t,
-                transitionTo: e.transitionTo ?? P,
-                replaceWith: e.replaceWith,
-                redirectTo: e.redirectTo,
-                location: e.location,
-                loginSource: e.loginSource,
-            });
-        return (0, i.jsx)(D, {
-            invite: e.invite,
-            guildTemplate: e.guildTemplate,
-            giftCodeSKU: e.giftCodeSKU,
-            giftCode: e.giftCode,
-            loginStatus: s,
-            handoffAvailable: n,
-            authenticated: t,
-            location: e.location,
-            authBoxClassName: e.authBoxClassName,
-            country: r,
-            hasLoggedInAccounts: l,
-            showMobileWebHandoff: e.showMobileWebHandoff,
-            disableAutofocusOnDefaultForm: e.disableAutofocusOnDefaultForm,
-            ...o,
-        });
-    };
