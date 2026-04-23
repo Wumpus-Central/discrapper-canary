e7f2f8024e36cd2e06f7b1bfb6c53ab21fe45aea
DiscrapperManager <admin@minerpl.xyz>
2026-04-23 03:45:00 +0200

    canaryRelease: 533833
    Main scripts: 3
    Lazy Loaded Scripts: 2479
    Chunks:
     * Main Files: 9327
     * Scripts: 16608
    Strings: 24593
    Routes: 819
    Experiments Count: 42
    Apex Experiments Count: 250

--- chunks/520467.js
+++ chunks/520467.js
@@ -1,25 +1,28 @@
-n.d(t, { A: () => o });
-var l = n(627968);
-n(64700);
-var i = n(503698),
-    s = n.n(i),
-    r = n(939249),
-    a = n(834730),
-    d = n(252316);
+l.d(t, { A: () => o });
+var n = l(627968);
+l(64700);
+var i = l(503698),
+    s = l.n(i),
+    a = l(939249),
+    r = l(834730),
+    d = l(505244);
 function o(e) {
-    let { steps: t, stepIndex: n, onClick: i } = e;
-    return (0, l.jsx)("div", {
+    let { steps: t, stepIndex: l, onClick: i } = e;
+    return (0, n.jsx)("div", {
         className: d.kL,
+        role: "tablist",
         children: t.map((e, t) => {
-            let o = n === t;
-            return (0, l.jsxs)(
-                r.D,
+            let o = l === t;
+            return (0, n.jsxs)(
+                a.D,
                 {
                     onClick: () => i(t),
                     className: d._h,
+                    role: "tab",
+                    "aria-selected": o,
                     children: [
-                        (0, l.jsx)("div", { className: s()(d.hr, { [d.YD]: o }) }),
-                        (0, l.jsx)(a.E, {
+                        (0, n.jsx)("div", { className: s()(d.hr, { [d.YD]: o }) }),
+                        (0, n.jsx)(r.E, {
                             color: o ? "text-brand" : "text-muted",
                             variant: "text-xs/normal",
                             children: e,
