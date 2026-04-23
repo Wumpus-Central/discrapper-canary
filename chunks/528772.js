ccfb901caf354c1208ad80d2a3a70cdba6cb167c
DiscrapperManager <admin@minerpl.xyz>
2026-04-23 02:01:02 +0200

    canaryRelease: 533744
    Main scripts: 3
    Lazy Loaded Scripts: 2478
    Chunks:
     * Main Files: 9326
     * Scripts: 16610
    Strings: 24560
    Routes: 819
    Experiments Count: 42
    Apex Experiments Count: 250

--- chunks/528772.js
+++ chunks/528772.js
@@ -1,20 +1,19 @@
-"use strict";
 n.d(t, { A: () => o });
-var r = n(311907),
-    i = n(73153);
-let l = !1,
-    a = !1;
-class s extends r.Ay.Store {
+var l = n(311907),
+    s = n(73153);
+let a = !1,
+    r = !1;
+class i extends l.Ay.Store {
     static displayName = "SoundboardOverlayStore";
     get keepOpen() {
-        return a;
+        return r;
     }
     get enabled() {
-        return l;
+        return a;
     }
 }
-let o = new s(i.h, {
+let o = new i(s.h, {
     SOUNDBOARD_SET_OVERLAY_ENABLED: function (e) {
-        (l = e.enabled), e.enabled && (a = e.keepOpen ?? !1);
+        (a = e.enabled), e.enabled && (r = e.keepOpen ?? !1);
     },
 });
