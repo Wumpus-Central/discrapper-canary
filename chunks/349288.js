a0516df2cbe087734ff51522edcd0143f53821a1
DiscrapperManager <admin@minerpl.xyz>
2026-04-24 17:54:45 +0200

    canaryRelease: 534750
    Main scripts: 30
    Lazy Loaded Scripts: 2464
    Chunks:
     * Main Files: 9835
     * Scripts: 7566
    Strings: 24637
    Routes: 820
    Experiments Count: 38
    Apex Experiments Count: 237

--- chunks/349288.js
+++ chunks/349288.js
@@ -1,44 +1,45 @@
 "use strict";
-n.r(t), n.d(t, { Anchor: () => f });
-var r = n(627968),
-    i = n(64700),
+n.r(t), n.d(t, { Anchor: () => E });
+var i = n(627968),
+    r = n(64700),
     s = n(503698),
     a = n.n(s),
     o = n(470562),
     l = n(531142),
-    u = n(939249),
-    d = n(187322),
-    c = n(752239),
-    _ = n(983763);
-function f(e) {
+    d = n(939249),
+    _ = n(187322),
+    u = n(752239),
+    c = n(983763);
+function E(e) {
     let {
         href: t,
         onClick: n,
         className: s,
-        children: f,
-        rel: E,
-        target: h,
-        useDefaultUnderlineStyles: p = !0,
-        title: m,
-        style: g,
+        children: E,
+        rel: h,
+        target: m,
+        useDefaultUnderlineStyles: f = !0,
+        title: g,
+        style: p,
         focusProps: A,
         ref: I,
-        ...T
+        role: T,
+        ...S
     } = e;
-    null != t && null == n && (n = c.V.getDefaultLinkInterceptor(t));
-    let S = i.useContext(l.g),
-        y = {
-            className: a()(_.Y, { [_.D]: p }, s),
+    null != t && null == n && (n = u.V.getDefaultLinkInterceptor(t));
+    let N = r.useContext(l.g),
+        C = {
+            className: a()(c.Y, { [c.D]: f }, s),
             href: t,
             onClick: n,
-            rel: null != E ? E : void 0,
-            target: null != h ? h : void 0,
-            title: null != m ? m : void 0,
-            style: null != g ? g : void 0,
+            rel: null != h ? h : void 0,
+            target: null != m ? m : void 0,
+            title: null != g ? g : void 0,
+            style: null != p ? p : void 0,
         };
-    return (null == t || (0, o.e)(t) || ((y.rel = "noreferrer noopener"), (y.target = "_blank")),
-    S && delete y.href,
+    return (null == t || (0, o.e)(t) || ((C.rel = "noreferrer noopener"), (C.target = "_blank")),
+    N && delete C.href,
     null != n)
-        ? (0, r.jsx)(u.D, { tag: "a", ...T, ...y, onClick: n, focusProps: A, innerRef: I, children: f })
-        : (0, r.jsx)(d.vN, { ...A, children: (0, r.jsx)("a", { ...T, ...y, ref: I, children: f }) });
+        ? (0, i.jsx)(d.D, { tag: "a", ...S, ...C, onClick: n, focusProps: A, innerRef: I, role: "link", children: E })
+        : (0, i.jsx)(_.vN, { ...A, children: (0, i.jsx)("a", { ...S, ...C, ref: I, children: E }) });
 }
