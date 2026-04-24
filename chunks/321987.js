f46f7380dd19f0401e26301e2dfbc6e446fc3331
DiscrapperManager <admin@minerpl.xyz>
2026-04-24 19:09:29 +0200

    canaryRelease: 534823
    Main scripts: 30
    Lazy Loaded Scripts: 2464
    Chunks:
     * Main Files: 9836
     * Scripts: 7567
    Strings: 24641
    Routes: 822
    Experiments Count: 38
    Apex Experiments Count: 237

--- chunks/321987.js
+++ chunks/321987.js
@@ -1,45 +1,46 @@
 "use strict";
-n.d(t, { $: () => p, f: () => A });
-var i = n(627968);
-n(64700);
-var r = n(503698),
-    s = n.n(r),
-    a = n(224640),
-    o = n(20742),
-    l = n(430993),
-    d = n(364840),
-    _ = n(696208),
-    u = n(17928),
-    c = n(43990),
-    E = n(726249),
-    h = n(540999),
-    m = n(652215),
-    f = n(985018),
-    g = n(966258);
-function p(e) {
+n.d(t, { $: () => A, f: () => I });
+var i = n(627968),
+    r = n(64700),
+    s = n(503698),
+    a = n.n(s),
+    o = n(224640),
+    l = n(20742),
+    d = n(430993),
+    _ = n(364840),
+    u = n(696208),
+    c = n(17928),
+    E = n(43990),
+    h = n(726249),
+    m = n(540999),
+    f = n(652215),
+    g = n(985018),
+    p = n(966258);
+function A(e) {
     let { children: t, removeChildWrapper: n, wrapperClassName: r } = e;
-    (0, E.HU)({ location: f.intl.string(f.t["3S2494"]) });
-    let a = (0, u.bG)([h.A], () => h.A.isDeveloper) ? "92px" : void 0;
-    return (0, i.jsx)(c.N, {
-        theme: m.NJ8.DARK,
+    (0, h.HU)({ location: g.intl.string(g.t["3S2494"]) });
+    let s = (0, c.bG)([m.A], () => m.A.isDeveloper) ? "92px" : void 0;
+    return (0, i.jsx)(E.N, {
+        theme: f.NJ8.DARK,
         children: (e) =>
             (0, i.jsx)("div", {
-                className: s()(e, g.Y, r),
-                style: { marginBottom: a },
-                children: !0 === n ? t : (0, i.jsx)("div", { className: g.Q, children: t }),
+                className: a()(e, p.Y, r),
+                style: { marginBottom: s },
+                children: !0 === n ? t : (0, i.jsx)("div", { className: p.Q, children: t }),
             }),
     });
 }
-function A(e) {
-    let { hideHeader: t, ...n } = e;
-    return (0, i.jsxs)(a.d, {
+function I(e) {
+    let { hideHeader: t, ...n } = e,
+        s = (0, i.jsx)(r.Fragment, {}, "controls");
+    return (0, i.jsxs)(o.d, {
         ...n,
         onClose: () => Promise.resolve(n.onClose?.()),
         children: [
-            t ? null : (0, i.jsx)(o.rQ, { title: n.title ?? "", subtitle: n.subtitle }),
-            (0, i.jsx)(l.c, { controls: n.input, children: n.children }),
-            (0, i.jsx)(d.j, { children: n.preview }),
-            (0, i.jsx)(_.H, {
+            t ? null : (0, i.jsx)(l.rQ, { title: n.title ?? "", subtitle: n.subtitle }),
+            (0, i.jsx)(d.c, { controls: s, children: n.children }),
+            (0, i.jsx)(_.j, { children: n.preview }),
+            (0, i.jsx)(u.H, {
                 leading: n.actionBarInput,
                 actions: n.actions ?? [],
                 actionsFullWidth: null == n.actionBarInput,
