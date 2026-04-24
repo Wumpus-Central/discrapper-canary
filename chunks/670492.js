c9337c7b84e6e97d2d981bb61d21324b43a1ad3c
DiscrapperManager <admin@minerpl.xyz>
2026-04-24 12:31:00 +0200

    canaryRelease: 534686
    Main scripts: 30
    Lazy Loaded Scripts: 2464
    Chunks:
     * Main Files: 9835
     * Scripts: 7566
    Strings: 24637
    Routes: 820
    Experiments Count: 38
    Apex Experiments Count: 237

--- chunks/670492.js
+++ chunks/670492.js
@@ -1,57 +1,58 @@
-n.d(t, { A: () => R });
-var i = n(735438),
-    E = n.n(i),
-    l = n(247775),
+"use strict";
+n.d(e, { A: () => h });
+var r = n(735438),
+    i = n.n(r),
+    o = n(247775),
     a = n(17928),
     s = n(228366);
-let r = !1,
-    o = [],
-    c = "",
+let l = !1,
+    c = [],
+    d = "",
     u = !1,
-    _ = { viewNonce: "", regenerateNonce: "" };
-class I extends a.Ay.Store {
+    f = { viewNonce: "", regenerateNonce: "" };
+class p extends a.Ay.Store {
     static displayName = "MFAStore";
     getVerificationKey() {
-        return c;
+        return d;
     }
     getBackupCodes() {
-        return o;
+        return c;
     }
     get togglingSMS() {
-        return r;
+        return l;
     }
     getNonces() {
-        return _;
+        return f;
     }
     get hasSeenBackupPrompt() {
         return u;
     }
 }
-let R = new I(s.h, {
-    MFA_ENABLE_SUCCESS: function (e) {
-        let { token: t, codes: n } = e;
-        void 0 !== t && l.setToken(t), (o = n);
+let h = new p(s.h, {
+    MFA_ENABLE_SUCCESS: function (t) {
+        let { token: e, codes: n } = t;
+        void 0 !== e && o.setToken(e), (c = n);
     },
-    MFA_DISABLE_SUCCESS: function (e) {
-        let { token: t } = e;
-        l.setToken(t);
+    MFA_DISABLE_SUCCESS: function (t) {
+        let { token: e } = t;
+        o.setToken(e);
     },
     MFA_SMS_TOGGLE: function () {
-        r = !0;
+        l = !0;
     },
     MFA_SMS_TOGGLE_COMPLETE: function () {
-        r = !1;
+        l = !1;
     },
     MFA_CLEAR_BACKUP_CODES: function () {
-        o = [];
+        c = [];
     },
-    MFA_VIEW_BACKUP_CODES: function (e) {
-        let { codes: t, key: n } = e;
-        (o = E().sortBy(t, "code")), (c = n);
+    MFA_VIEW_BACKUP_CODES: function (t) {
+        let { codes: e, key: n } = t;
+        (c = i().sortBy(e, "code")), (d = n);
     },
-    MFA_SEND_VERIFICATION_KEY: function (e) {
-        let { nonces: t } = e;
-        _ = t;
+    MFA_SEND_VERIFICATION_KEY: function (t) {
+        let { nonces: e } = t;
+        f = e;
     },
     MFA_SEEN_BACKUP_CODE_PROMPT: function () {
         u = !0;
