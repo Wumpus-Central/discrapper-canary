57d9075d7963d2a88d100d8093d312e4fbf7d150
DiscrapperManager <admin@minerpl.xyz>
2026-04-24 21:01:53 +0200

    canaryRelease: 534941
    Main scripts: 30
    Lazy Loaded Scripts: 2464
    Chunks:
     * Main Files: 9836
     * Scripts: 7568
    Strings: 24644
    Routes: 822
    Experiments Count: 38
    Apex Experiments Count: 237

--- chunks/489254.js
+++ chunks/489254.js
@@ -1,10 +1,10 @@
-n.d(t, { n: () => s });
-let i = (0, n(250105).Ay)({
-    name: "2026-04-boosting-pre-checkout-modal-refresh",
+r.d(n, { n: () => _ });
+let t = (0, r(250105).Ay)({
+    name: "2026-04-boosting-pre-checkout-modal-refresh-v2",
     kind: "user",
     defaultConfig: { enabled: !1 },
     variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
 });
-function s(e) {
-    return i.useConfig({ location: e }).enabled;
+function _(e) {
+    return t.useConfig({ location: e }).enabled;
 }
