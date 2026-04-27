18fbfb1369f2b5ba4c42f1a0e48096779be0f60d
DiscrapperManager <admin@minerpl.xyz>
2026-04-27 19:22:09 +0200

    canaryRelease: 535482
    Main scripts: 31
    Lazy Loaded Scripts: 2579
    Chunks:
     * Main Files: 8240
     * Scripts: 10279
    Strings: 24695
    Routes: 823
    Experiments Count: 42
    Apex Experiments Count: 260

--- chunks/940622.js
+++ chunks/940622.js
@@ -1,13 +1,12 @@
-"use strict";
-n.d(t, { C: () => p, JE: () => d, NE: () => u, VU: () => m, mb: () => _ }), n(323874), n(14289), n(35956);
-var a = n(64700),
-    r = n(735438),
-    i = n(353640),
-    l = n(121894),
-    s = n(816866),
-    o = n(87558),
-    c = n(559474);
-let d = (0, i.v)((e) => ({
+a.d(t, { C: () => p, JE: () => d, NE: () => u, VU: () => m, mb: () => h }), a(323874), a(14289), a(35956);
+var r = a(64700),
+    n = a(735438),
+    l = a(353640),
+    i = a(121894),
+    s = a(816866),
+    o = a(87558),
+    c = a(559474);
+let d = (0, l.v)((e) => ({
         previewEnabled: !1,
         collectionAssets: {},
         avatarDecorationAssets: {},
@@ -18,55 +17,55 @@ let d = (0, i.v)((e) => ({
         setPreviewEnabled: (t) => e({ previewEnabled: t }),
         setHeroLogoMaxHeight: (t) => e({ heroLogoMaxHeight: t }),
         setHeroResponsive: (t) => e({ heroResponsive: t }),
-        upsertCollectionAsset: (t, n) =>
-            (0, l.r)(() => {
+        upsertCollectionAsset: (t, a) =>
+            (0, i.r)(() => {
                 e((e) => {
-                    let a = e.collectionAssets[t];
-                    null != a && URL.revokeObjectURL(a.src);
-                    let r = URL.createObjectURL(n),
-                        i = { ...e.collectionAssets };
+                    let r = e.collectionAssets[t];
+                    null != r && URL.revokeObjectURL(r.src);
+                    let n = URL.createObjectURL(a),
+                        l = { ...e.collectionAssets };
                     return (
-                        (i[t] = { type: t, name: n.name, src: r }), { ...e, collectionAssets: i, previewEnabled: !0 }
+                        (l[t] = { type: t, name: a.name, src: n }), { ...e, collectionAssets: l, previewEnabled: !0 }
                     );
                 });
             }),
         deleteCollectionAsset: (t) =>
-            (0, l.r)(() => {
+            (0, i.r)(() => {
                 e((e) => {
-                    let n = e.collectionAssets[t];
-                    if (null == n) return e;
-                    URL.revokeObjectURL(n.src);
-                    let { [t]: a, ...r } = e.collectionAssets,
-                        i = Object.keys(r).length > 0 || Object.keys(e.avatarDecorationAssets).length > 0;
-                    return { ...e, collectionAssets: r, previewEnabled: i };
+                    let a = e.collectionAssets[t];
+                    if (null == a) return e;
+                    URL.revokeObjectURL(a.src);
+                    let { [t]: r, ...n } = e.collectionAssets,
+                        l = Object.keys(n).length > 0 || Object.keys(e.avatarDecorationAssets).length > 0;
+                    return { ...e, collectionAssets: n, previewEnabled: l };
                 });
             }),
         upsertAvatarDecorationAsset: (t) =>
-            (0, l.r)(() => {
+            (0, i.r)(() => {
                 e((e) => {
-                    let n = e.avatarDecorationAssets[t.name];
-                    null != n && URL.revokeObjectURL(n.src);
-                    let a = URL.createObjectURL(t),
-                        r = { ...e.avatarDecorationAssets };
+                    let a = e.avatarDecorationAssets[t.name];
+                    null != a && URL.revokeObjectURL(a.src);
+                    let r = URL.createObjectURL(t),
+                        n = { ...e.avatarDecorationAssets };
                     return (
-                        (r[t.name] = { type: c.Jn.AVATAR_DECORATION, name: t.name, src: a }),
-                        { ...e, avatarDecorationAssets: r, previewEnabled: !0 }
+                        (n[t.name] = { type: c.Jn.AVATAR_DECORATION, name: t.name, src: r }),
+                        { ...e, avatarDecorationAssets: n, previewEnabled: !0 }
                     );
                 });
             }),
         deleteAvatarDecorationAsset: (t) =>
-            (0, l.r)(() => {
+            (0, i.r)(() => {
                 e((e) => {
-                    let n = e.avatarDecorationAssets[t];
-                    if (null == n) return e;
-                    URL.revokeObjectURL(n.src);
-                    let { [t]: a, ...r } = e.avatarDecorationAssets,
-                        i = Object.keys(r).length > 0 || Object.keys(e.collectionAssets).length > 0;
-                    return { ...e, avatarDecorationAssets: r, previewEnabled: i };
+                    let a = e.avatarDecorationAssets[t];
+                    if (null == a) return e;
+                    URL.revokeObjectURL(a.src);
+                    let { [t]: r, ...n } = e.avatarDecorationAssets,
+                        l = Object.keys(n).length > 0 || Object.keys(e.collectionAssets).length > 0;
+                    return { ...e, avatarDecorationAssets: n, previewEnabled: l };
                 });
             }),
         clearAssets: () =>
-            (0, l.r)(() => {
+            (0, i.r)(() => {
                 e(
                     (e) => (
                         Object.values(e.collectionAssets).forEach((e) => URL.revokeObjectURL(e.src)),
@@ -84,14 +83,14 @@ let d = (0, i.v)((e) => ({
                 );
             }),
         setPreviewProfileEffectSkuId: (t) =>
-            (0, l.r)(() => e({ previewProfileEffectSkuId: t, previewEnabled: null != t })),
+            (0, i.r)(() => e({ previewProfileEffectSkuId: t, previewEnabled: null != t })),
         setPreviewAvatarDecorationKey: (t) =>
-            (0, l.r)(() => e({ previewAvatarDecorationKey: t, previewEnabled: null != t })),
+            (0, i.r)(() => e({ previewAvatarDecorationKey: t, previewEnabled: null != t })),
     })),
     u = () => {
         let e = d((e) => e.collectionAssets),
             t = d((e) => e.avatarDecorationAssets);
-        return a.useMemo(
+        return r.useMemo(
             () => ({
                 collectionAssets: Object.values(e).sort((e, t) => e.name.localeCompare(t.name)),
                 avatarDecorationAssets: Object.values(t).sort((e, t) => e.name.localeCompare(t.name)),
@@ -99,35 +98,35 @@ let d = (0, i.v)((e) => ({
             [e, t],
         );
     },
-    _ = (e) => d((t) => (t.previewEnabled ? t.collectionAssets[e]?.src : null)),
+    h = (e) => d((t) => (t.previewEnabled ? t.collectionAssets[e]?.src : null)),
     p = (e) => {
-        let { previewEnabled: t, previewProfileEffectSkuId: n } = d(),
-            i = (0, s.ZK)(t ? n : null);
-        return a.useMemo(() => {
-            if (null == i || null == e) return null;
-            let { effects: t, stillFrames: n } = i,
-                a = null != n && Object.keys(n).length > 0;
-            if (0 === t.length && !a) return null;
-            let l = (0, r.cloneDeep)(e);
+        let { previewEnabled: t, previewProfileEffectSkuId: a } = d(),
+            l = (0, s.ZK)(t ? a : null);
+        return r.useMemo(() => {
+            if (null == l || null == e) return null;
+            let { effects: t, stillFrames: a } = l,
+                r = null != a && Object.keys(a).length > 0;
+            if (0 === t.length && !r) return null;
+            let i = (0, n.cloneDeep)(e);
             return (
-                (l.title = i.name),
-                (l.effects = t.map((e) => {
-                    let { base64: t, ...n } = e;
-                    return n;
+                (i.title = l.name),
+                (i.effects = t.map((e) => {
+                    let { base64: t, ...a } = e;
+                    return a;
                 })),
-                a &&
-                    ((l.reducedMotionSrc = n[o.qH.REDUCED_MOTION]?.src ?? ""),
-                    (l.staticFrameSrc = n[o.qH.STATIC]?.src ?? ""),
-                    (l.thumbnailPreviewSrc = n[o.qH.THUMBNAIL]?.src ?? "")),
-                l
+                r &&
+                    ((i.reducedMotionSrc = a[o.qH.REDUCED_MOTION]?.src ?? ""),
+                    (i.staticFrameSrc = a[o.qH.STATIC]?.src ?? ""),
+                    (i.thumbnailPreviewSrc = a[o.qH.THUMBNAIL]?.src ?? "")),
+                i
             );
-        }, [i, e]);
+        }, [l, e]);
     },
     m = () =>
         d((e) => {
             if (!e.previewEnabled) return null;
             let t = e.previewAvatarDecorationKey;
             if (null != t) return e.avatarDecorationAssets[t]?.src ?? null;
-            let n = Object.values(e.avatarDecorationAssets);
-            return n[0]?.src;
+            let a = Object.values(e.avatarDecorationAssets);
+            return a[0]?.src;
         });
