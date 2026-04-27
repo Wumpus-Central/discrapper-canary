584127f70e414ed77bf45804db4d6836ce7f9366
DiscrapperManager <admin@minerpl.xyz>
2026-04-27 22:14:25 +0200

    canaryRelease: 535637
    Main scripts: 31
    Lazy Loaded Scripts: 2579
    Chunks:
     * Main Files: 8241
     * Scripts: 10279
    Strings: 24703
    Routes: 823
    Experiments Count: 42
    Apex Experiments Count: 260

--- chunks/469993.js
+++ chunks/469993.js
@@ -1,23 +1,26 @@
-n.d(t, { ME: () => d, Z1: () => E, oS: () => o });
+n.d(t, { ME: () => c, Z1: () => d, oS: () => E });
 var i = n(17928),
+    a = n(546605),
     r = n(287809),
-    a = n(615405),
+    _ = n(615405),
     s = n(652215);
-let _ = new Set(["US"]);
-function l() {
-    return r.default.getCurrentUser()?.storeCountry?.country ?? a.A.ipCountryCode;
-}
+let l = new Set(["US"]);
 function o() {
-    return (0, i.bG)([r.default, a.A], () => {
-        let e = l();
-        return null != e && _.has(e);
-    });
+    return (0, a.wy)("CreatorMonetizationEligibility")
+        ? (r.default.getCurrentUser()?.storeCountry?.country ?? _.A.ipCountryCode)
+        : _.A.ipCountryCode;
 }
 function E() {
-    let e = l();
-    return null != e && _.has(e);
+    return (0, i.bG)([r.default, _.A], () => {
+        let e = o();
+        return null != e && l.has(e);
+    });
 }
-function d(e) {
+function d() {
+    let e = o();
+    return null != e && l.has(e);
+}
+function c(e) {
     let t = null != e?.id,
         n = null != e && e.features.has(s.GuildFeatures.CREATOR_MONETIZABLE_WHITEGLOVE);
     return t || n;
--- chunks/546605.js
+++ chunks/546605.js
@@ -1,8 +1,10 @@
-n.d(t, { vg: () => r });
-let a = (0, n(945810).mj)({
+"use strict";
+n.d(t, { vg: () => r, wy: () => s });
+let i = (0, n(945810).mj)({
         name: "2026-03-store-country",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
     }),
-    r = (e) => a.useConfig({ location: e }).enabled;
+    r = (e) => i.useConfig({ location: e }).enabled,
+    s = (e) => i.getConfig({ location: e }).enabled;
