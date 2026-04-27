e9ac20bde9ec57084fdb75afd9ab034834b34c21
DiscrapperManager <admin@minerpl.xyz>
2026-04-27 22:21:57 +0200

    noisesSuppression 535646

--- chunks/66933.js
+++ chunks/66933.js
@@ -1,9 +1,7 @@
-"use strict";
 n.d(t, { A: () => i });
-let r = (0, n(945810).mj)({
-        name: "2026-03-app-guild-validation",
-        kind: "guild",
-        defaultConfig: { enabled: !1 },
-        variations: { 0: { enabled: !0 }, 1: { enabled: !0 } },
-    }),
-    i = 21552 == n.j ? r : null;
+let i = (0, n(945810).mj)({
+    name: "2026-03-app-guild-validation",
+    kind: "guild",
+    defaultConfig: { enabled: !1 },
+    variations: { 0: { enabled: !0 }, 1: { enabled: !0 } },
+});
