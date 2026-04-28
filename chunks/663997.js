9cdf1d7fee4c1b0dd2830c0bc38285d2912ddfce
DiscrapperManager <admin@minerpl.xyz>
2026-04-28 23:04:08 +0200

    canaryRelease: 536366
    Main scripts: 92
    Lazy Loaded Scripts: 3908
    Chunks:
     * Main Files: 8639
     * Scripts: 9835
    Strings: 24672
    Routes: 823
    Experiments Count: 40
    Apex Experiments Count: 254

--- chunks/663997.js
+++ chunks/663997.js
@@ -1,55 +1,56 @@
-n.d(t, { K: () => u, L: () => d });
-var i = n(64700),
-    l = n(989349),
-    s = n.n(l),
-    a = n(198982),
-    r = n(661191),
-    o = n(624458),
-    c = n(513461);
-let d = 100;
-function u(e) {
-    let { guildId: t, guildJoinRequests: n } = e,
-        l = i.useRef(!1),
-        [u, h] = i.useState(null),
-        A = i.useRef(null),
-        _ = i.useRef(!1);
+r.d(t, { K: () => o, L: () => f });
+var n = r(64700),
+    u = r(989349),
+    l = r.n(u),
+    a = r(845584),
+    i = r(935208),
+    s = r(624458),
+    c = r(513461);
+let f = 100;
+function o(e) {
+    let { guildId: t, guildJoinRequests: r } = e,
+        u = n.useRef(!1),
+        [o, d] = n.useState(null),
+        h = n.useRef(null),
+        g = n.useRef(!1);
     return {
-        fetchNextPage: i.useCallback(
-            async (e, i) => {
-                if (l.current) return;
-                let m = `${e}-${i}`,
-                    g = !1;
-                if ((m !== A.current && ((A.current = m), (_.current = !1), (g = !0)), _.current)) return;
-                null != u && h(null);
-                let p = (function (e, t, n, i) {
-                    let l = n === c.B5.SUBMITTED;
+        fetchNextPage: n.useCallback(
+            async (e, n) => {
+                if (u.current) return;
+                let p = `${e}-${n}`,
+                    m = !1;
+                if ((p !== h.current && ((h.current = p), (g.current = !1), (m = !0)), g.current)) return;
+                null != o && d(null);
+                let b = (function (e, t, r, n) {
+                    let u = r === c.B5.SUBMITTED;
                     if (t === c._e.TIMESTAMP_DESC)
-                        if (i) return { before: r.default.fromTimestamp(new Date().getTime()) };
+                        if (n || 0 === e.length) return { before: i.default.fromTimestamp(new Date().getTime()) };
                         else {
                             let t = e[e.length - 1];
-                            return { before: l ? t.joinRequestId : t.actionedAt };
+                            return { before: u ? t.joinRequestId : t.actionedAt };
                         }
-                    if (i) return { after: r.default.fromTimestamp(s()().subtract(180, "days").valueOf()) };
+                    if (n || 0 === e.length)
+                        return { after: i.default.fromTimestamp(l()().subtract(180, "days").valueOf()) };
                     {
                         let t = e[e.length - 1];
-                        return { after: l ? t.joinRequestId : t.actionedAt };
+                        return { after: u ? t.joinRequestId : t.actionedAt };
                     }
-                })(n, e, i, g);
+                })(r, e, n, m);
                 try {
-                    l.current = !0;
-                    let e = await o.A.fetchGuildJoinRequests({ guildId: t, status: i, limit: d, force: !0, ...p });
+                    u.current = !0;
+                    let e = await s.A.fetchGuildJoinRequests({ guildId: t, status: n, limit: f, force: !0, ...b });
                     if (null != e) {
                         let { guild_join_requests: t } = e.body;
-                        t.length < d && (_.current = !0);
+                        t.length < f && (g.current = !0);
                     }
                 } catch (e) {
-                    h(new a.LG(e).getAnyErrorMessage());
+                    d(new a.LG(e).getAnyErrorMessage());
                 } finally {
-                    l.current = !1;
+                    u.current = !1;
                 }
             },
-            [u, t, n],
+            [o, t, r],
         ),
-        error: u,
+        error: o,
     };
 }
