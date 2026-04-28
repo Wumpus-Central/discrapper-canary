6ce056deadd85eb5fce33dd0116058199e089a49
DiscrapperManager <admin@minerpl.xyz>
2026-04-28 20:44:23 +0200

    canaryRelease: 536232
    Main scripts: 94
    Lazy Loaded Scripts: 3905
    Chunks:
     * Main Files: 8696
     * Scripts: 9810
    Strings: 24672
    Routes: 823
    Experiments Count: 40
    Apex Experiments Count: 255

--- chunks/50292.js
+++ chunks/50292.js
@@ -1,58 +1,60 @@
-n.d(t, { default: () => h }), n(323874), n(14289), n(35956);
-var r = n(627968),
-    i = n(645758),
-    a = n(772707),
-    l = n(403581),
-    s = n(793574),
-    c = n(688810),
-    o = n(532794),
-    d = n(920050),
-    u = n(553081),
-    p = n(976860),
-    _ = n(652215),
-    E = n(788868),
-    f = n(985018);
-function h(e) {
-    let { onClose: t, multiplier: n, eligibleToReceivePremiumRewards: h, ...T } = e,
-        { analyticsLocations: A } = (0, c.Ay)(s.A.QUEST_ORB_MULTIPLIER_INFO_MODAL),
-        b = h
-            ? f.intl.format(f.t.NpUfej, { bonusOrbMultiplier: n })
-            : f.intl.format(f.t["G5k+lZ"], { bonusOrbMultiplier: n });
-    return (0, r.jsx)(a.k, {
-        ...T,
-        onClose: t,
-        badge: { type: "beta", variant: "expressive" },
+r.d(i, { default: () => k }), r(323874), r(14289), r(35956);
+var n = r(627968),
+    e = r(122720),
+    a = r(772707),
+    s = r(403581),
+    l = r(793574),
+    o = r(688810),
+    p = r(31502),
+    c = r(532794),
+    u = r(920050),
+    d = r(553081),
+    _ = r(976860),
+    b = r(652215),
+    v = r(788868),
+    O = r(985018);
+function k(t) {
+    let { onClose: i, multiplier: r, eligibleToReceivePremiumRewards: k, ...C } = t,
+        { analyticsLocations: M } = (0, o.Ay)(l.A.QUEST_ORB_MULTIPLIER_INFO_MODAL),
+        T = (0, p.l)("QuestOrbMultiplierInfoModal"),
+        g = k
+            ? O.intl.format(O.t.NpUfej, { bonusOrbMultiplier: r })
+            : O.intl.format(O.t["G5k+lZ"], { bonusOrbMultiplier: r });
+    return (0, n.jsx)(a.k, {
+        ...C,
+        onClose: i,
+        badge: T ? void 0 : { type: "beta", variant: "expressive" },
         gradientColor: "nitro-pink",
-        title: f.intl.string(f.t.F2MShO),
-        subtitle: b,
-        graphic: { type: "rive", rive: i.E, props: { artboard: "Torbs_Multiply" } },
-        actions: h
+        title: O.intl.string(O.t.F2MShO),
+        subtitle: g,
+        graphic: { type: "rive", rive: e.E, props: { artboard: "Torbs_Multiply" } },
+        actions: k
             ? [
-                  { text: f.intl.string(f.t.cpT0Cq), variant: "secondary", onClick: t },
+                  { text: O.intl.string(O.t.cpT0Cq), variant: "secondary", onClick: i },
                   {
-                      text: f.intl.string(f.t.hvVgAZ),
+                      text: O.intl.string(O.t.hvVgAZ),
                       variant: "primary",
                       onClick: () => {
-                          let e = _.BVt.NITRO_HOME,
-                              n = new URLSearchParams({ [u.WU]: d.QUEST_ORB_MULTIPLIER_CARD_ID }).toString();
-                          t(), (0, p.pX)(e.includes("?") ? `${e}&${n}` : `${e}?${n}`);
+                          let t = b.BVt.NITRO_HOME,
+                              r = new URLSearchParams({ [d.WU]: u.QUEST_ORB_MULTIPLIER_CARD_ID }).toString();
+                          i(), (0, _.pX)(t.includes("?") ? `${t}&${r}` : `${t}?${r}`);
                       },
                   },
               ]
             : [
                   {
-                      text: f.intl.string(f.t.PcTCB7),
+                      text: O.intl.string(O.t.PcTCB7),
                       variant: "secondary",
                       onClick: () => {
-                          t(), (0, p.pX)(_.BVt.NITRO_HOME);
+                          i(), (0, _.pX)(b.BVt.NITRO_HOME);
                       },
                   },
                   {
-                      text: f.intl.string(f.t.pj0XBN),
+                      text: O.intl.string(O.t.pj0XBN),
                       variant: "expressive",
-                      icon: l.t,
+                      icon: s.t,
                       onClick: () => {
-                          t(), (0, o.A)({ subscriptionTier: E.pe.TIER_2, analyticsLocations: A });
+                          i(), (0, c.A)({ subscriptionTier: v.pe.TIER_2, analyticsLocations: M });
                       },
                   },
               ],
