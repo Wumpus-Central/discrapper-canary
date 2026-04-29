335950f90d8203970a72d106e516d10f8c070af4
DiscrapperManager <admin@minerpl.xyz>
2026-04-29 22:21:40 +0200

    canaryRelease: 537016
    Main scripts: 94
    Lazy Loaded Scripts: 3917
    Chunks:
     * Main Files: 8659
     * Scripts: 9844
    Strings: 24683
    Routes: 824
    Experiments Count: 41
    Apex Experiments Count: 257

--- chunks/635980.js
+++ chunks/635980.js
@@ -1,4 +1,4 @@
-n.d(t, { default: () => Z, H: () => X });
+n.d(t, { default: () => Q, H: () => Z });
 var i = n(627968),
     r = n(64700),
     a = n(65412),
@@ -8,30 +8,30 @@ var i = n(627968),
     d = n(212245),
     c = n(736653),
     u = n(793574),
-    _ = n(688810),
-    A = n(826673),
-    h = n(38405),
+    A = n(688810),
+    h = n(826673),
+    _ = n(38405),
     E = n(403581),
     p = n(232582),
     m = n(821609),
     I = n(349288),
     g = n(954571),
     C = n(637706),
-    f = n(498470),
-    T = n(652215),
+    T = n(498470),
+    f = n(652215),
     N = n(985018),
     S = n(870433);
 n(323874), n(14289), n(35956);
 var L = n(503698),
-    y = n.n(L),
-    O = n(110259),
+    O = n.n(L),
+    y = n(110259),
     b = n(877624),
     v = n(17928),
-    R = n(815021),
-    P = n(777666),
-    D = n(534514),
-    x = n(834730),
-    w = n(935462),
+    P = n(815021),
+    D = n(777666),
+    R = n(534514),
+    w = n(834730),
+    x = n(935462),
     M = n(315629),
     U = n(775602),
     G = n(139286),
@@ -44,29 +44,29 @@ var L = n(503698),
     Y = n(774774),
     $ = n(49999),
     z = n(695594);
-let W = [],
-    q = (e) => {
-        let { tagText: t, className: n } = e;
-        return (0, i.jsx)(P.Lp, { className: y()(z.Tc, n), text: t });
-    },
+let q = [],
     K = (e) => {
+        let { tagText: t, className: n } = e;
+        return (0, i.jsx)(D.Lp, { className: O()(z.Tc, n), text: t });
+    },
+    W = (e) => {
         let { header: t, subHeader: n, imageSrc: r, tagText: a, wideStyle: l } = e;
         return (0, i.jsxs)("div", {
-            className: y()(z.Vr, { [z.tF]: l }),
+            className: O()(z.Vr, { [z.tF]: l }),
             children: [
-                null != a ? (0, i.jsx)(q, { tagText: a, className: z.bX }) : null,
-                (0, i.jsx)("img", { alt: "", className: y()(z._n, { [z.tF]: l }), src: r }),
+                null != a ? (0, i.jsx)(K, { tagText: a, className: z.bX }) : null,
+                (0, i.jsx)("img", { alt: "", className: O()(z._n, { [z.tF]: l }), src: r }),
                 (0, i.jsxs)("div", {
-                    className: y()(z.Zz, { [z.tF]: l }),
+                    className: O()(z.Zz, { [z.tF]: l }),
                     children: [
-                        (0, i.jsx)(D.D, { variant: "heading-sm/semibold", className: z.Hf, children: t }),
-                        (0, i.jsx)(x.E, { variant: "text-xs/normal", color: "text-subtle", children: n }),
+                        (0, i.jsx)(R.D, { variant: "heading-md/bold", className: z.Hf, children: t }),
+                        (0, i.jsx)(w.E, { variant: "text-md/medium", color: "text-subtle", children: n }),
                     ],
                 }),
             ],
         });
     };
-function Q(e) {
+function X(e) {
     let {
             componentId: t,
             promotionId: n,
@@ -76,43 +76,43 @@ function Q(e) {
             heroArtClassName: d,
             header: c,
             headerClassName: u,
-            modalTopPill: _,
-            subHeader: A,
+            modalTopPill: A,
+            subHeader: h,
             subHeaderExtra: E,
             featureCards: p,
             changeLogId: m,
             button: I,
             body: C,
-            disclaimer: f,
+            disclaimer: T,
             disclaimerExtra: S,
         } = e,
-        { onClose: L, transitionState: P } = s,
-        q = (0, V.GV)(),
-        Q = p.length % 2 == 0,
-        X = (0, v.bG)([U.A], () => U.A.useReducedMotion),
-        [Z, J] = r.useState(Date.now()),
+        { onClose: L, transitionState: D } = s,
+        K = (0, V.GV)(),
+        X = p.length % 2 == 0,
+        Z = (0, v.bG)([U.A], () => U.A.useReducedMotion),
+        [Q, J] = r.useState(Date.now()),
         [ee, et] = r.useState(0),
         [en, ei] = r.useState(0),
         [er, ea] = r.useState(!1),
         [el, es] = r.useState(!0),
-        eo = r.useRef(Z),
+        eo = r.useRef(Q),
         ed = r.useRef(ee),
         ec = r.useRef(en),
         eu = r.useRef(er),
-        e_ = r.useRef(el),
-        [eA, eh] = r.useState(W),
+        eA = r.useRef(el),
+        [eh, e_] = r.useState(q),
         eE = r.useRef(!1);
     function ep() {
         let e = Date.now(),
             t = e - eo.current,
             n = ed.current,
             i = ec.current;
-        return eu.current && (et((n += t)), e_.current || ei((i += t))), J(e), [n, i];
+        return eu.current && (et((n += t)), eA.current || ei((i += t))), J(e), [n, i];
     }
     return (
         (0, G.A)({
-            type: O.ImpressionTypes.MODAL,
-            name: O.ImpressionNames.PREMIUM_MARKETING_COMPONENT,
+            type: y.ImpressionTypes.MODAL,
+            name: y.ImpressionNames.PREMIUM_MARKETING_COMPONENT,
             properties: { component_id: t, component_type: b.C.ANNOUNCEMENT_MODAL },
         }),
         r.useEffect(() => {
@@ -127,34 +127,34 @@ function Q(e) {
                             r = URL.createObjectURL(i);
                         return { ...e, src: r };
                     });
-                    eh(await Promise.all(e));
+                    e_(await Promise.all(e));
                 } catch (e) {
-                    h.A.captureException(e), eh(W);
+                    _.A.captureException(e), e_(q);
                 }
             };
             !0 !== eE.current && e(), (eE.current = !0);
-        }, [o, eA]),
+        }, [o, eh]),
         r.useEffect(
             () => () => {
-                null != eA &&
-                    eA.forEach((e) => {
+                null != eh &&
+                    eh.forEach((e) => {
                         URL.revokeObjectURL(e.src);
                     });
             },
-            [eA],
+            [eh],
         ),
         r.useEffect(() => {
-            (eo.current = Z), (ed.current = ee), (ec.current = en), (eu.current = er), (e_.current = el);
-        }, [Z, ee, en, er, el]),
+            (eo.current = Q), (ed.current = ee), (ec.current = en), (eu.current = er), (eA.current = el);
+        }, [Q, ee, en, er, el]),
         r.useEffect(
             () => () => {
                 if ("video" === o.type || "embed" === o.type) {
                     let [e, t] = ep();
-                    g.default.track(T.HAw.CHANGE_LOG_VIDEO_PLAYED, {
+                    g.default.track(f.HAw.CHANGE_LOG_VIDEO_PLAYED, {
                         change_log_id: m,
                         seconds_played: Math.round(e / 1e3),
                     }),
-                        g.default.track(T.HAw.CHANGE_LOG_VIDEO_UNMUTE, {
+                        g.default.track(f.HAw.CHANGE_LOG_VIDEO_UNMUTE, {
                             change_log_id: m,
                             seconds_unmuted: Math.round(t / 1e3),
                         });
@@ -166,9 +166,9 @@ function Q(e) {
             (0, H.Vh)(l.M.PREMIUM_MARKETING_MOMENT_ANNOUNCEMENT_UPSELL);
             let e = Date.now();
             return (
-                g.default.track(T.HAw.CHANGE_LOG_OPENED, { change_log_id: m }),
+                g.default.track(f.HAw.CHANGE_LOG_OPENED, { change_log_id: m }),
                 () => {
-                    g.default.track(T.HAw.CHANGE_LOG_CLOSED, {
+                    g.default.track(f.HAw.CHANGE_LOG_CLOSED, {
                         change_log_id: m,
                         seconds_open: Math.round((Date.now() - e) / 1e3),
                     }),
@@ -176,27 +176,27 @@ function Q(e) {
                 }
             );
         }, [m, n]),
-        (0, i.jsx)(w.EO, {
+        (0, i.jsx)(x.EO, {
             "data-migration-pending": !0,
-            className: y()(z.zr, z.kv, a),
-            transitionState: P,
-            "aria-labelledby": q,
+            className: O()(z.zr, z.kv, a),
+            transitionState: D,
+            "aria-labelledby": K,
             parentComponent: "PremiumAnnouncementModalVariant1",
             children: (0, i.jsxs)(M.h, {
                 color: "nitro-pink",
                 children: [
                     (0, i.jsx)("div", {
                         className: z.cG,
-                        children: (0, i.jsx)(R.J, { "data-migration-pending": !0, onClick: async () => await L() }),
+                        children: (0, i.jsx)(P.J, { "data-migration-pending": !0, onClick: async () => await L() }),
                     }),
-                    (0, i.jsxs)(w.$m, {
+                    (0, i.jsxs)(x.$m, {
                         "data-migration-pending": !0,
                         className: z.Qs,
                         children: [
                             "video" === o.type
                                 ? (0, i.jsx)(F.A, {
-                                      className: y()(z.Ki, d),
-                                      autoPlay: !X,
+                                      className: O()(z.Ki, d),
+                                      autoPlay: !Z,
                                       loop: !0,
                                       muted: !0,
                                       controls: !0,
@@ -204,7 +204,7 @@ function Q(e) {
                                       src: o.src,
                                       poster: o.poster,
                                       onPlay: (e) => {
-                                          g.default.track(T.HAw.CHANGE_LOG_VIDEO_INTERACTED, { change_log_id: m }),
+                                          g.default.track(f.HAw.CHANGE_LOG_VIDEO_INTERACTED, { change_log_id: m }),
                                               J(Date.now()),
                                               ea(!0),
                                               es(e.currentTarget.muted);
@@ -219,7 +219,7 @@ function Q(e) {
                                           ep(), es(e.currentTarget.muted), ea(!1);
                                       },
                                       disablePictureInPicture: !0,
-                                      children: eA?.map((e) => {
+                                      children: eh?.map((e) => {
                                           let t = (0, N.getLanguages)().find((t) => t.code === e.locale);
                                           return null == t
                                               ? null
@@ -238,7 +238,7 @@ function Q(e) {
                                   })
                                 : "embed" === o.type
                                   ? (0, i.jsx)(j.rr, {
-                                        className: y()(z.Ki, d),
+                                        className: O()(z.Ki, d),
                                         allowFullScreen: !1,
                                         href: o.href,
                                         thumbnail: o.thumbnail,
@@ -250,15 +250,15 @@ function Q(e) {
                                         renderImageComponent: B.LL,
                                         renderLinkComponent: B.bU,
                                         onPlay: () => {
-                                            g.default.track(T.HAw.CHANGE_LOG_VIDEO_INTERACTED, { change_log_id: m });
+                                            g.default.track(f.HAw.CHANGE_LOG_VIDEO_INTERACTED, { change_log_id: m });
                                         },
                                     })
                                   : "image" === o.type
-                                    ? (0, i.jsx)("img", { alt: "", className: y()(z.Ki, d), src: o.src })
+                                    ? (0, i.jsx)("img", { alt: "", className: O()(z.Ki, d), src: o.src })
                                     : null,
-                            null != _ && "" !== _
+                            null != A && "" !== A
                                 ? (0, i.jsx)(Y.e4, {
-                                      text: _,
+                                      text: A,
                                       colorOptions: Y.at.PREMIUM_TIER_2_WHITE_FILL,
                                       className: z.ON,
                                   })
@@ -266,32 +266,32 @@ function Q(e) {
                             (0, i.jsxs)("div", {
                                 className: z.Z,
                                 children: [
-                                    (0, i.jsx)(D.D, { variant: "display-md", className: y()(z.TK, u), children: c }),
-                                    (0, i.jsxs)(x.E, {
+                                    (0, i.jsx)(R.D, { variant: "display-md", className: O()(z.TK, u), children: c }),
+                                    (0, i.jsxs)(w.E, {
                                         variant: "text-md/medium",
                                         color: "text-subtle",
                                         className: z.uI,
-                                        children: [A, E?.()],
+                                        children: [h, E?.()],
                                     }),
                                 ],
                             }),
                             null != C &&
                                 "" !== C &&
-                                (0, i.jsx)(D.D, { variant: "heading-lg/semibold", className: z.G3, children: C }),
+                                (0, i.jsx)(R.D, { variant: "heading-lg/semibold", className: z.G3, children: C }),
                             p.length > 0 &&
                                 (0, i.jsx)("div", {
-                                    className: y()(z.IS, { [z.tF]: Q }),
+                                    className: O()(z.IS, { [z.tF]: X }),
                                     children: p.map((e, t) =>
-                                        (0, i.jsx)(K, { ...e, wideStyle: Q }, `${e.header}_${t}`),
+                                        (0, i.jsx)(W, { ...e, wideStyle: X }, `${e.header}_${t}`),
                                     ),
                                 }),
-                            null != f &&
-                                "" !== f &&
-                                (0, i.jsxs)(x.E, {
+                            null != T &&
+                                "" !== T &&
+                                (0, i.jsxs)(w.E, {
                                     variant: "text-xs/medium",
                                     color: "text-subtle",
                                     className: z.ed,
-                                    children: [f, S?.()],
+                                    children: [T, S?.()],
                                 }),
                             I(),
                         ],
@@ -301,21 +301,21 @@ function Q(e) {
         })
     );
 }
-function X(e) {
+function Z(e) {
     let { promotionId: t, content: n, isPreview: i } = e,
         r = s.m.fromBinary((0, a.A)(n));
     if ("announcementModalVariant1" !== r.properties.oneofKind) return null;
     {
-        let { isDismissed: e } = (0, A.u$)(l.M.PREMIUM_MARKETING_MOMENT_ANNOUNCEMENT_UPSELL, t);
+        let { isDismissed: e } = (0, h.u$)(l.M.PREMIUM_MARKETING_MOMENT_ANNOUNCEMENT_UPSELL, t);
         return !i && e ? null : (i && (r.properties.announcementModalVariant1.dismissKey = ""), r);
     }
 }
-function Z(e) {
+function Q(e) {
     let { componentId: t, promotionId: n, renderModalProps: r, properties: a } = e,
         { location: l } = (0, d.p)(),
-        { analyticsLocations: s } = (0, _.Ay)(u.A.PREMIUM_MARKETING_ANNOUNCEMENT_MODAL),
-        A = (0, c.Ay)(),
-        L = (0, o.q)(A);
+        { analyticsLocations: s } = (0, A.Ay)(u.A.PREMIUM_MARKETING_ANNOUNCEMENT_MODAL),
+        h = (0, c.Ay)(),
+        L = (0, o.q)(h);
     if ("announcementModalVariant1" === a.properties.oneofKind) {
         let e = (function (e) {
             let {
@@ -330,33 +330,33 @@ function Z(e) {
                 d = `AnnouncementModalVariant1_${t}`,
                 { onClose: c } = a,
                 u = r.button?.copy ?? N.intl.string(N.t.YScQSF),
-                _ = r.button?.buttonAction === p.dz.OPEN_MARKETING_PAGE ? "jump_to_mkt_button" : "get_nitro_button",
-                A = (0, f.h)({
+                A = r.button?.buttonAction === p.dz.OPEN_MARKETING_PAGE ? "jump_to_mkt_button" : "get_nitro_button",
+                h = (0, T.h)({
                     buttonAction: r.button?.buttonAction,
                     deeplinkSection: r.button?.deeplinkSection,
                     analyticsLocations: l,
                     analyticsLocation: s,
                     onClose: c,
                 }),
-                h = (0, C.C)(r.helpArticle, r.helpArticleId),
+                _ = (0, C.C)(r.helpArticle, r.helpArticleId),
                 L =
-                    null != h
+                    null != _
                         ? () =>
                               (0, i.jsxs)(i.Fragment, {
                                   children: [
                                       "\xa0",
-                                      (0, i.jsx)(I.Anchor, { className: S.$T, href: h.url, children: h.linkText }),
+                                      (0, i.jsx)(I.Anchor, { className: S.$T, href: _.url, children: _.linkText }),
                                   ],
                               })
                         : void 0,
-                y = (0, C.C)(r.disclaimer?.disclaimerHelpArticle, ""),
-                O =
-                    null != y
+                O = (0, C.C)(r.disclaimer?.disclaimerHelpArticle, ""),
+                y =
+                    null != O
                         ? () =>
                               (0, i.jsxs)(i.Fragment, {
                                   children: [
                                       "\xa0",
-                                      (0, i.jsx)(I.Anchor, { className: S.$T, href: y.url, children: y.linkText }),
+                                      (0, i.jsx)(I.Anchor, { className: S.$T, href: O.url, children: O.linkText }),
                                   ],
                               })
                         : void 0,
@@ -393,20 +393,20 @@ function Z(e) {
                             variant: "expressive",
                             size: "md",
                             onClick: () => {
-                                g.default.track(T.HAw.CHANGE_LOG_CTA_CLICKED, {
+                                g.default.track(f.HAw.CHANGE_LOG_CTA_CLICKED, {
                                     change_log_id: d,
-                                    cta_type: _,
+                                    cta_type: A,
                                     seconds_open: Math.round((Date.now() - e) / 1e3),
                                     target: d,
                                 }),
-                                    A();
+                                    h();
                             },
                             text: u,
                             icon: E.t,
                         });
                     },
                     disclaimer: r.disclaimer?.disclaimerText !== "" ? r.disclaimer?.disclaimerText : void 0,
-                    disclaimerExtra: O,
+                    disclaimerExtra: y,
                 }
             );
         })({
@@ -418,10 +418,10 @@ function Z(e) {
             analyticsLocation: l,
             isLightTheme: L,
         });
-        return (0, i.jsx)(Q, { ...e });
+        return (0, i.jsx)(X, { ...e });
     }
     return (
-        h.A.captureMessage(
+        _.A.captureMessage(
             `Property type ${a.properties.oneofKind} is not supported to render ServerDriveAnnouncementModal`,
         ),
         null
