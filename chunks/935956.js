902c80d6a4ce0a75a6e0622a60119a83a33ded2b
DiscrapperManager <admin@minerpl.xyz>
2026-04-29 21:37:43 +0200

    canaryRelease: 536966
    Main scripts: 94
    Lazy Loaded Scripts: 3917
    Chunks:
     * Main Files: 8658
     * Scripts: 9834
    Strings: 24683
    Routes: 824
    Experiments Count: 41
    Apex Experiments Count: 258

--- chunks/935956.js
+++ chunks/935956.js
@@ -1,41 +1,41 @@
-e.d(r, { A: () => g, u: () => x });
-var n = e(627968);
-e(64700);
-var l = e(503698),
-    a = e.n(l),
-    i = e(534514),
-    t = e(31502),
-    c = e(256599),
-    u = e(385337),
-    d = e(744064),
-    m = e(254748),
-    o = e(256358),
-    h = e(985018),
-    N = e(367791),
-    p = e(102338),
-    j = e(672193);
-let x = "croissant";
+r.d(e, { A: () => g, u: () => j });
+var l = r(627968);
+r(64700);
+var n = r(503698),
+    a = r.n(n),
+    i = r(534514),
+    t = r(31502),
+    u = r(256599),
+    c = r(385337),
+    d = r(744064),
+    m = r(254748),
+    p = r(256358),
+    h = r(985018),
+    o = r(367791),
+    N = r(102338),
+    x = r(672193);
+let j = "xgpp";
 function g(s) {
-    let { glowingSectionId: r } = s,
-        e = (0, t.l)("premium_subscriber_home_rewards"),
-        { logitechCard: l, steelseriesCard: g } = (0, o.A)();
-    if (!e) return null;
-    let C = null != l || null != g;
-    return (0, n.jsx)(m.A, {
-        id: x,
-        sectionClassName: a()(p.uW, p.Uv, p.qr),
-        heading: (0, n.jsx)(i.D, {
+    let { glowingSectionId: e } = s,
+        r = (0, t.l)("premium_subscriber_home_rewards"),
+        { logitechCard: n, steelseriesCard: g } = (0, p.A)();
+    if (!r) return null;
+    let C = null != n || null != g;
+    return (0, l.jsx)(m.A, {
+        id: j,
+        sectionClassName: a()(N.uW, N.Uv, N.qr),
+        heading: (0, l.jsx)(i.D, {
             variant: "display-sm",
-            className: a()(j.R_, N.U6),
+            className: a()(x.R_, o.U6),
             children: h.intl.string(h.t.NG1e6l),
         }),
-        beforeGrid: (0, n.jsx)("div", { className: N.JE, children: (0, n.jsx)(c.A, { glowing: r === x }) }),
+        beforeGrid: (0, l.jsx)("div", { className: o.JE, children: (0, l.jsx)(u.A, { glowing: e === j }) }),
         grid: C
-            ? (0, n.jsxs)(n.Fragment, {
+            ? (0, l.jsxs)(l.Fragment, {
                   children: [
-                      (0, n.jsx)(u.A, { containerClassName: j.Nr }),
-                      null != l && (0, n.jsx)(d.S, { ...l, containerClassName: j.Nr }),
-                      null != g && (0, n.jsx)(d.S, { ...g, containerClassName: j.Nr }),
+                      (0, l.jsx)(c.A, { containerClassName: x.Nr }),
+                      null != n && (0, l.jsx)(d.S, { ...n, containerClassName: x.Nr }),
+                      null != g && (0, l.jsx)(d.S, { ...g, containerClassName: x.Nr }),
                   ],
               })
             : null,
