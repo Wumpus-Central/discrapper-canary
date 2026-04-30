db1f3436c1bf36bfdc5418cbc3eaa8551c6d17cb
DiscrapperManager <admin@minerpl.xyz>
2026-04-30 21:04:01 +0200

    canaryRelease: 537661
    Main scripts: 670
    Lazy Loaded Scripts: 3284
    Chunks:
     * Main Files: 11799
     * Scripts: 6861
    Strings: 24733
    Routes: 820
    Experiments Count: 43
    Apex Experiments Count: 269

--- chunks/891197.js
+++ chunks/891197.js
@@ -1,35 +1,13 @@
-r.d(n, { N: () => u, p: () => p });
-var l = r(627968),
-    t = r(284009),
-    s = r.n(t),
-    i = r(534514),
-    a = r(156312),
-    d = r(615310),
-    c = r(632638),
-    o = r(985018);
-let u = (e) => {
-    let { className: n } = e;
-    return (0, l.jsx)("div", {
+l.d(n, { N: () => a });
+var e = l(627968);
+l(284009);
+var i = l(534514);
+l(156312), l(615310), l(632638);
+var t = l(985018);
+let a = (r) => {
+    let { className: n } = r;
+    return (0, e.jsx)("div", {
         className: n,
-        children: (0, l.jsx)(i.D, { variant: "heading-md/semibold", children: o.intl.string(o.t.eALkIR) }),
+        children: (0, e.jsx)(i.D, { variant: "heading-md/semibold", children: t.intl.string(t.t.eALkIR) }),
     });
 };
-function p(e) {
-    let { steps: n, currentStep: r, paymentError: t, className: i } = e,
-        { steps: o, step: p } = (0, d.Ay)(),
-        { paymentError: x } = (0, a.P5)();
-    return (
-        (n = n ?? o),
-        null == r && null != p && (r = p),
-        s()(r, "step should be set"),
-        s()(n, "step should be set"),
-        (t = t ?? x),
-        (0, l.jsx)(c.A, {
-            steps: n,
-            currentStep: r,
-            paymentError: t,
-            body: (0, l.jsx)(u, { className: i }),
-            footer: null,
-        })
-    );
-}
