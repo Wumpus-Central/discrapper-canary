85c58bfdeeca1ce18163ea7681d251cbc52a1031
DiscrapperManager <admin@minerpl.xyz>
2026-05-01 10:44:17 +0200

    canaryRelease: 538034
    Main scripts: 152
    Lazy Loaded Scripts: 3642
    Chunks:
     * Main Files: 11373
     * Scripts: 8604
    Strings: 24762
    Routes: 820
    Experiments Count: 43
    Apex Experiments Count: 323

--- chunks/120386.js
+++ chunks/120386.js
@@ -16,23 +16,22 @@ var i = n(627968),
     m = n(84540),
     k = n(854627),
     C = n(930349),
-    x = n(735438),
-    A = n.n(x),
+    A = n(735438),
+    x = n.n(A),
     p = n(993408),
     y = n(841702),
     j = n(486020),
     I = n(515718),
-    N = n(985018),
+    N = n(375708),
     L = n(209150);
 let w = d._3.SIZE_72,
-    B = f.Ay.getEnableHardwareAcceleration() ? u.Js : u.eu;
-function z(e) {
-    let { user: l, guildId: n, avatarDecorationOverride: a, decorationStyle: t, isHovered: r, disabled: d } = e,
+    z = f.Ay.getEnableHardwareAcceleration() ? u.Js : u.eu;
+function B(e) {
+    let { user: l, guildId: n, avatarDecorationOverride: a, decorationStyle: t, shouldAnimate: r } = e,
         {
-            avatarDecorationSrc: u,
-            avatarSrc: c,
-            avatarPlaceholderSrc: o,
-            eventHandlers: v,
+            avatarDecorationSrc: d,
+            avatarSrc: u,
+            avatarPlaceholderSrc: c,
         } = (0, k.A)({
             userId: l.id,
             guildId: n,
@@ -41,15 +40,14 @@ function z(e) {
             avatarDecorationOverride: a,
             animateOnHover: !r,
         }),
-        g = "default" === t && null != u && r;
-    return (0, i.jsx)(B, {
-        className: s()({ [L.yT]: null == u, [L.m_]: "placeholder" === t }),
-        src: g ? c : o,
-        imageClassName: g && !d ? L.WG : void 0,
-        avatarDecoration: u,
+        o = "default" === t && null != d && r;
+    return (0, i.jsx)(z, {
+        className: s()({ [L.yT]: null == d, [L.m_]: "placeholder" === t }),
+        src: o ? u : c,
+        imageClassName: o ? L.WG : void 0,
+        avatarDecoration: d,
         size: w,
         "aria-hidden": !0,
-        ...v,
     });
 }
 function O(e) {
@@ -59,90 +57,92 @@ function O(e) {
         u = null != n,
         f = a.useRef(null),
         { isHoveringOrFocusing: k } = (0, g.A)(f),
-        x = (0, b.a4)({ user: l }),
-        L = (0, b.a4)({ user: l, guildId: n ?? void 0 }),
-        { pendingAvatarDecoration: B } = (0, b.CP)(n ?? void 0),
-        O = void 0 !== B,
-        R = null === B || (!O && null == L),
-        S = u && null != x,
-        _ = R && S,
-        H = (0, o.A)((0, b.lw)({ userValue: x, guildValue: L, pendingValue: B, guildId: n })),
-        { sampleDecoration: V, refreshSample: D } = (function (e) {
-            let { categories: l, purchases: n } = (0, y.Ay)({ stalePurchasesOK: !0 }),
-                i = a.useMemo(() => (0, p.ps)(n, l).filter((e) => null != e.asset && "" !== e.asset), [n, l]),
-                [t, s] = a.useState(null),
-                r = a.useRef([]),
-                d = a.useRef(0),
-                u = a.useCallback(
-                    (l) => {
+        A = k && !t,
+        L = (0, b.a4)({ user: l }),
+        z = (0, b.a4)({ user: l, guildId: n ?? void 0 }),
+        { pendingAvatarDecoration: O } = (0, b.CP)(n ?? void 0),
+        R = void 0 !== O,
+        S = null === O || (!R && null == z),
+        _ = u && null != L,
+        V = S && _,
+        D = (0, o.A)((0, b.lw)({ userValue: L, guildValue: z, pendingValue: O, guildId: n })),
+        { sampleDecoration: P, refreshSample: H } = (function (e) {
+            let { size: l, disabled: n = !1 } = e,
+                { categories: i, purchases: t } = (0, y.Ay)({ stalePurchasesOK: !0 }),
+                s = a.useMemo(() => (0, p.ps)(t, i).filter((e) => null != e.asset && "" !== e.asset), [t, i]),
+                [r, d] = a.useState(null),
+                u = a.useRef([]),
+                c = a.useRef(0),
+                o = a.useCallback(
+                    (e) => {
                         let n = (0, j.F_)({
-                            avatarDecoration: { asset: l.asset, skuId: l.skuId },
+                            avatarDecoration: { asset: e.asset, skuId: e.skuId },
                             canAnimate: !0,
-                            size: (0, v.Te)(e),
+                            size: (0, v.Te)(l),
                         });
                         null != n && (0, I.NN)(n).catch(() => {});
                     },
-                    [e],
+                    [l],
                 );
             return (
                 a.useEffect(() => {
-                    if (0 === i.length) return;
-                    let e = A().shuffle(i);
-                    (r.current = e), (d.current = 0), u(e[0]);
-                }, [i, u]),
+                    if (n || 0 === s.length || u.current.length > 0) return;
+                    let e = x().shuffle(s);
+                    (u.current = e), (c.current = 0), o(e[0]);
+                }, [n, s, o]),
                 {
-                    sampleDecoration: t,
+                    sampleDecoration: r,
                     refreshSample: a.useCallback(() => {
-                        let e = r.current;
+                        if (n) return;
+                        let e = u.current;
                         if (0 === e.length) return;
-                        let l = d.current % e.length,
-                            n = (l + 1) % e.length,
-                            i = e[l],
-                            a = e[n];
-                        s({ asset: i.asset, skuId: i.skuId }), (d.current = n), u(a);
-                    }, [u]),
+                        let l = c.current % e.length,
+                            i = (l + 1) % e.length,
+                            a = e[l],
+                            t = e[i];
+                        d({ asset: a.asset, skuId: a.skuId }), (c.current = i), o(t);
+                    }, [n, o]),
                 }
             );
-        })(w),
-        P = R && k && null != V && !_;
+        })({ size: w, disabled: t || !S }),
+        W = S && !V && A && null != P;
     a.useEffect(() => {
-        R && ((null != V && k) || D());
+        k && H();
     }, [k]);
-    let W =
-            null != H && (O ? null != B : null != L)
+    let q =
+            null != D && (R ? null != O : null != z)
                 ? {
                       onClick: () => (0, m.p)({ guildId: n ?? void 0, avatarDecoration: null }),
-                      type: S ? "reset" : "remove",
-                      accessibleLabel: N.intl.string(S ? N.t.pJsnPf : N.t["2U4Bga"]),
+                      type: _ ? "reset" : "remove",
+                      accessibleLabel: N.intl.string(_ ? N.t.pJsnPf : N.t["2U4Bga"]),
                   }
                 : void 0,
-        q = () => (0, v.L)({ analyticsLocations: s, guild: d ?? void 0, stackingBehavior: "stack" }),
-        E = (0, i.jsx)(z, {
+        E = () => (0, v.L)({ analyticsLocations: s, guild: d ?? void 0, stackingBehavior: "stack" }),
+        G = (0, i.jsx)(B, {
             user: l,
             guildId: n,
-            avatarDecorationOverride: P ? V : O ? H : void 0,
-            decorationStyle: P ? "placeholder" : _ ? "fallback" : "default",
-            isHovered: k,
-            disabled: t,
+            avatarDecorationOverride: W ? P : R ? D : void 0,
+            decorationStyle: W ? "placeholder" : V ? "fallback" : "default",
+            shouldAnimate: A,
         });
     return (0, i.jsx)("div", {
         ref: f,
-        children: R
+        children: S
             ? (0, i.jsx)(C.kL, {
                   variant: "square",
-                  onClick: q,
+                  onClick: E,
                   accessibleLabel: N.intl.string(N.t.HykynS),
                   disabled: t,
-                  dimContent: _,
-                  children: E,
+                  dimContent: V,
+                  children: G,
               })
             : (0, i.jsx)(C.NW, {
                   variant: "square",
-                  onClick: q,
+                  onClick: E,
                   accessibleLabel: N.intl.string(N.t.HykynS),
-                  deleteButtonConfig: W,
+                  deleteButtonConfig: q,
                   disabled: t,
-                  children: E,
+                  children: G,
               }),
     });
 }
