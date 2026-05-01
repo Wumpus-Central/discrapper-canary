0a77c66c5200b6bb44961d7bd7543c0cbbc68666
DiscrapperManager <admin@minerpl.xyz>
2026-05-01 22:56:01 +0200

    canaryRelease: 538285
    Main scripts: 150
    Lazy Loaded Scripts: 3636
    Chunks:
     * Main Files: 11388
     * Scripts: 8455
    Strings: 24772
    Routes: 820
    Experiments Count: 43
    Apex Experiments Count: 323

--- chunks/28647.js
+++ chunks/28647.js
@@ -1,1 +1,14 @@
-a.d(s,{z:()=>e});var o=a(192308),t=a(228366),i=a(946974);let e={binds:["mod+/","mod+shift+/"],comboKeysBindGlobal:!0,action:()=>((0,o.hasModalOpen)(i.P)?t.h.dispatch({type:"HIDE_KEYBOARD_SHORTCUTS"}):t.h.dispatch({type:"SHOW_KEYBOARD_SHORTCUTS"}),!1)}TCUTS"}),!1)}
+a.d(s, { z: () => e });
+var o = a(192308),
+    t = a(228366),
+    i = a(946974);
+let e = {
+    binds: ["mod+/", "mod+shift+/"],
+    comboKeysBindGlobal: !0,
+    action: () => (
+        (0, o.hasModalOpen)(i.P)
+            ? t.h.dispatch({ type: "HIDE_KEYBOARD_SHORTCUTS" })
+            : t.h.dispatch({ type: "SHOW_KEYBOARD_SHORTCUTS" }),
+        !1
+    ),
+};
