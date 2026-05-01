4586d7c71f33f7700c49b3b9fb5c4be38b00bafd
DiscrapperManager <admin@minerpl.xyz>
2026-05-01 18:22:41 +0200

    canaryRelease: 538084
    Main scripts: 152
    Lazy Loaded Scripts: 3642
    Chunks:
     * Main Files: 11373
     * Scripts: 8604
    Strings: 24762
    Routes: 820
    Experiments Count: 43
    Apex Experiments Count: 323

--- chunks/734925.js
+++ chunks/734925.js
@@ -1,32 +1,33 @@
-n.d(t, { A: () => d });
+n.d(t, { A: () => c });
 var l = n(627968);
 n(64700);
-var r = n(783878),
-    i = n(778712),
-    a = n(966327),
-    s = n(937008),
+var i = n(783878),
+    r = n(778712),
+    s = n(966327),
+    a = n(937008),
     o = n(427262),
-    u = n(985018);
-let d = (e) => {
-    let { selectedSkuId: t, validateSelectedGift: n, className: d, recipients: c } = e,
-        { giftRecipient: C, setGiftRecipient: p } = (0, s.Pv)();
+    u = n(375708);
+let c = (e) => {
+    let { selectedSkuId: t, validateSelectedGift: n, className: c, recipients: d } = e,
+        { giftRecipient: C, setGiftRecipient: p } = (0, a.Pv)();
     return null == t
         ? null
         : (0, l.jsx)("div", {
-              className: d,
-              children: (0, l.jsx)(r.Z, {
+              className: c,
+              children: (0, l.jsx)(i.Z, {
                   selectionMode: "single",
                   label: u.intl.string(u.t.xFn72s),
                   placeholder: u.intl.string(u.t.R0vK0N),
-                  value: C,
+                  value: C?.id,
                   onSelectionChange: (e) => {
-                      n(e, t), p(e);
+                      let l = d.find((t) => t.id === e);
+                      null != l && (n(l, t), p(l));
                   },
-                  options: c.map((e) => ({
+                  options: d.map((e) => ({
                       id: e.id,
-                      value: e,
+                      value: e.id,
                       label: `${o.Ay.getUserTag(e)}`,
-                      leading: (0, l.jsx)(a.A, { user: e, size: i._3.SIZE_20 }),
+                      leading: (0, l.jsx)(s.A, { user: e, size: r._3.SIZE_20 }),
                   })),
               }),
           });
