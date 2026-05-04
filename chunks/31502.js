9e39095410f9ecc32d3b2ec0465665b510296113
DiscrapperManager <admin@minerpl.xyz>
2026-05-05 01:40:13 +0200

    noisesSuppression 538961

--- chunks/31502.js
+++ chunks/31502.js
@@ -1,16 +1,15 @@
-"use strict";
-n.d(t, { Q: () => s, l: () => r });
-let i = (0, n(945810).mj)({
+s.d(t, { Q: () => r, l: () => i });
+let l = (0, s(945810).mj)({
     name: "2026-03-croissant",
     kind: "user",
     defaultConfig: { enabled: !1 },
     variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
 });
-function r(e) {
-    let { enabled: t } = i.useConfig({ location: e });
+function i(e) {
+    let { enabled: t } = l.useConfig({ location: e });
     return t;
 }
-function s(e) {
-    let { enabled: t } = i.getConfig({ location: e });
+function r(e) {
+    let { enabled: t } = l.getConfig({ location: e });
     return t;
 }
