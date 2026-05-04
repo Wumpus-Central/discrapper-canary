f27093312d9f3de485c4e333753983e335b420dc
DiscrapperManager <admin@minerpl.xyz>
2026-05-05 01:42:47 +0200

    canaryRelease: 538964
    Main scripts: 150
    Lazy Loaded Scripts: 3636
    Chunks:
     * Main Files: 11390
     * Scripts: 8453
    Strings: 24790
    Routes: 820
    Experiments Count: 41
    Apex Experiments Count: 323

--- chunks/31502.js
+++ chunks/31502.js
@@ -1,15 +1,16 @@
-s.d(t, { Q: () => r, l: () => i });
-let l = (0, s(945810).mj)({
+"use strict";
+n.d(t, { Q: () => s, l: () => r });
+let i = (0, n(945810).mj)({
     name: "2026-03-croissant",
     kind: "user",
     defaultConfig: { enabled: !1 },
     variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
 });
-function i(e) {
-    let { enabled: t } = l.useConfig({ location: e });
+function r(e) {
+    let { enabled: t } = i.useConfig({ location: e });
     return t;
 }
-function r(e) {
-    let { enabled: t } = l.getConfig({ location: e });
+function s(e) {
+    let { enabled: t } = i.getConfig({ location: e });
     return t;
 }
