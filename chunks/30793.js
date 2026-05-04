abaef6d91f00440c32a501196c63a4c9910d2983
DiscrapperManager <admin@minerpl.xyz>
2026-05-04 15:54:59 +0200

    canaryRelease: 538501
    Main scripts: 150
    Lazy Loaded Scripts: 3636
    Chunks:
     * Main Files: 11386
     * Scripts: 8454
    Strings: 24778
    Routes: 820
    Experiments Count: 43
    Apex Experiments Count: 323

--- chunks/30793.js
+++ chunks/30793.js
@@ -1,191 +1,190 @@
-"use strict";
-n.d(t, { A: () => b });
-var i = n(989349),
-    r = n.n(i),
-    s = n(17928),
-    a = n(451988),
-    o = n(228366),
-    l = n(869038),
-    u = n(7133),
-    c = n(45938),
-    d = n(652215);
-let _ = {},
-    f = new Map(),
+E.d(t, { A: () => g });
+var n = E(989349),
+    l = E.n(n),
+    r = E(17928),
+    i = E(451988),
+    _ = E(228366),
+    o = E(869038),
+    c = E(7133),
+    s = E(45938),
+    a = E(652215);
+let d = {},
+    u = new Map(),
+    S = [],
+    C = [],
     h = [],
-    p = [],
-    E = [],
-    m = new Set(),
-    g = {},
-    A = {},
-    I = new Set();
-function T(e) {
-    let t = u.A.createFromServer(e),
-        n = t.code;
-    if (f.has(n)) f.set(n, f.get(n).merge(t));
-    else if ((f.set(n, t), null != t.expiresAt)) {
-        let e = new a.Ep();
-        (_[n] = e),
+    p = new Set(),
+    f = {},
+    O = {},
+    A = new Set();
+function F(e) {
+    let t = c.A.createFromServer(e),
+        E = t.code;
+    if (u.has(E)) u.set(E, u.get(E).merge(t));
+    else if ((u.set(E, t), null != t.expiresAt)) {
+        let e = new i.Ep();
+        (d[E] = e),
             (function e(t) {
-                let n = f.get(t);
-                if (null == n || null == n.expiresAt) return;
-                let i = n.expiresAt.valueOf() - r()().valueOf();
-                if (i <= 0) f.delete(t), delete _[t], R.emitChange();
+                let E = u.get(t);
+                if (null == E || null == E.expiresAt) return;
+                let n = E.expiresAt.valueOf() - l()().valueOf();
+                if (n <= 0) u.delete(t), delete d[t], y.emitChange();
                 else {
-                    let n = _[t];
-                    if (null == n) return;
-                    n.start(Math.min(d.mnr, i), () => e(t));
+                    let E = d[t];
+                    if (null == E) return;
+                    E.start(Math.min(a.mnr, n), () => e(t));
                 }
-            })(n);
+            })(E);
     }
 }
-function S(e) {
+function D(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
-    if (t && !I.has(e.channel_id)) return !1;
-    let n = (0, c.pF)(e) ? (0, c.e7)(e?.embeds != null ? e?.embeds[0].url : void 0) : (0, c.e7)(e.content);
+    if (t && !A.has(e.channel_id)) return !1;
+    let E = (0, s.pF)(e) ? (0, s.e7)(e?.embeds != null ? e?.embeds[0].url : void 0) : (0, s.e7)(e.content);
     return (
-        0 !== n.length &&
-        (n.forEach((e) => {
-            h.includes(e) ||
-                E.includes(e) ||
-                (N({ code: e }), o.h.wait(() => l.A.resolveGiftCode(e, !1, !0).catch(d.FXj)));
+        0 !== E.length &&
+        (E.forEach((e) => {
+            S.includes(e) ||
+                h.includes(e) ||
+                (T({ code: e }), _.h.wait(() => o.A.resolveGiftCode(e, !1, !0).catch(a.FXj)));
         }),
         !1)
     );
 }
-function N(e) {
+function T(e) {
     let { code: t } = e;
-    h.includes(t) || (h = [...h, t]);
+    S.includes(t) || (S = [...S, t]);
 }
-function y(e) {
+function I(e) {
     let { message: t } = e;
-    return S(t, !0);
+    return D(t, !0);
 }
-function C(e) {
-    let { channelId: t, messages: n } = e;
-    I.add(t), n.forEach((e) => S(e, !0));
+function R(e) {
+    let { channelId: t, messages: E } = e;
+    A.add(t), E.forEach((e) => D(e, !0));
 }
-function v(e) {
+function G(e) {
     let { firstMessages: t } = e;
     if (null == t) return !1;
-    t?.forEach((e) => S(e));
+    t?.forEach((e) => D(e));
 }
-class O extends s.Ay.Store {
+class U extends r.Ay.Store {
     static displayName = "GiftCodeStore";
     get(e) {
-        let t = f.get(e);
+        let t = u.get(e);
         return null == t || t.isExpired() ? null : t;
     }
     getError(e) {
-        return null != e ? A[e] : null;
+        return null != e ? O[e] : null;
     }
-    getForGifterSKUAndPlan(e, t, n) {
-        return Array.from(f.values()).filter(
-            (i) => i.userId === e && i.skuId === t && (null == n || i.subscriptionPlanId === n) && !i.isExpired(),
+    getForGifterSKUAndPlan(e, t, E) {
+        return Array.from(u.values()).filter(
+            (n) => n.userId === e && n.skuId === t && (null == E || n.subscriptionPlanId === E) && !n.isExpired(),
         );
     }
     getIsResolving(e) {
-        return h.includes(e);
+        return S.includes(e);
     }
     getIsResolved(e) {
-        return E.includes(e);
+        return h.includes(e);
     }
     getIsAccepting(e) {
-        return p.includes(e);
+        return C.includes(e);
     }
     getUserGiftCodesFetchingForSKUAndPlan(e, t) {
-        return m.has((0, c.Kx)(e, t));
+        return p.has((0, s.Kx)(e, t));
     }
     getUserGiftCodesLoadedAtForSKUAndPlan(e, t) {
-        return g[(0, c.Kx)(e, t)];
+        return f[(0, s.Kx)(e, t)];
     }
     getResolvingCodes() {
-        return h;
+        return S;
     }
     getResolvedCodes() {
-        return E;
+        return h;
     }
     getAcceptingCodes() {
-        return p;
+        return C;
     }
 }
-let R = new O(o.h, {
+let y = new U(_.h, {
         CONNECTION_OPEN: function () {
-            return I.clear(), !1;
+            return A.clear(), !1;
         },
         CHANNEL_SELECT: function (e) {
             let { channelId: t } = e;
-            return null != t && I.add(t), !1;
+            return null != t && A.add(t), !1;
         },
-        GIFT_CODE_RESOLVE: N,
+        GIFT_CODE_RESOLVE: T,
         GIFT_CODE_RESOLVE_SUCCESS: function (e) {
             let { giftCode: t } = e;
-            return (h = h.filter((e) => e !== t.code)), E.includes(t.code) || (E = [...E, t.code]), T(t);
+            return (S = S.filter((e) => e !== t.code)), h.includes(t.code) || (h = [...h, t.code]), F(t);
         },
         GIFT_CODE_RESOLVE_FAILURE: function (e) {
-            let { code: t, error: n } = e;
-            (h = h.filter((e) => e !== t)), E.includes(t) || (E = [...E, t]), null != n && (A[t] = n);
+            let { code: t, error: E } = e;
+            (S = S.filter((e) => e !== t)), h.includes(t) || (h = [...h, t]), null != E && (O[t] = E);
         },
         GIFT_CODE_REDEEM: function (e) {
             let { code: t } = e;
-            p.includes(t) || (p = [...p, t]);
+            C.includes(t) || (C = [...C, t]);
         },
         GIFT_CODE_REDEEM_SUCCESS: function (e) {
             let { code: t } = e;
-            p = p.filter((e) => e !== t);
-            let n = f.get(t);
-            null != n && f.set(t, n.merge({ redeemed: !0, uses: n.uses + 1 }));
+            C = C.filter((e) => e !== t);
+            let E = u.get(t);
+            null != E && u.set(t, E.merge({ redeemed: !0, uses: E.uses + 1 }));
         },
         GIFT_CODE_REDEEM_FAILURE: function (e) {
-            let { code: t, error: n } = e;
-            p = p.filter((e) => e !== t);
-            let i = f.get(t);
-            if (((A[t] = n), null != i))
-                switch (n.code) {
-                    case d.t02.UNKNOWN_GIFT_CODE:
-                        f.set(t, i.set("revoked", !0));
+            let { code: t, error: E } = e;
+            C = C.filter((e) => e !== t);
+            let n = u.get(t);
+            if (((O[t] = E), null != n))
+                switch (E.code) {
+                    case a.t02.UNKNOWN_GIFT_CODE:
+                        u.set(t, n.set("revoked", !0));
                         break;
-                    case d.t02.INVALID_GIFT_REDEMPTION_EXHAUSTED:
-                        f.set(t, i.set("uses", i.maxUses));
+                    case a.t02.INVALID_GIFT_REDEMPTION_EXHAUSTED:
+                        u.set(t, n.set("uses", n.maxUses));
                 }
         },
         GIFT_CODE_REVOKE_SUCCESS: function (e) {
             let { code: t } = e;
-            f.delete(t);
-            let n = _[t];
-            null != n && (n.stop(), delete _[t]), E.includes(t) || (E = [...E, t]);
+            u.delete(t);
+            let E = d[t];
+            null != E && (E.stop(), delete d[t]), h.includes(t) || (h = [...h, t]);
         },
         GIFT_CODE_CREATE_SUCCESS: function (e) {
             let { giftCode: t } = e;
-            T(t);
+            F(t);
         },
         GIFT_CODES_FETCH: function (e) {
-            let { skuId: t, subscriptionPlanId: n } = e;
-            m.add((0, c.Kx)(t, n));
+            let { skuId: t, subscriptionPlanId: E } = e;
+            p.add((0, s.Kx)(t, E));
         },
         GIFT_CODES_FETCH_SUCCESS: function (e) {
-            let { giftCodes: t, skuId: n, subscriptionPlanId: i } = e;
-            t.forEach(T);
-            let r = (0, c.Kx)(n, i);
-            (g[r] = Date.now()), m.delete(r);
+            let { giftCodes: t, skuId: E, subscriptionPlanId: n } = e;
+            t.forEach(F);
+            let l = (0, s.Kx)(E, n);
+            (f[l] = Date.now()), p.delete(l);
         },
         GIFT_CODES_FETCH_FAILURE: function (e) {
-            let { skuId: t, subscriptionPlanId: n } = e;
-            m.delete((0, c.Kx)(t, n));
+            let { skuId: t, subscriptionPlanId: E } = e;
+            p.delete((0, s.Kx)(t, E));
         },
-        MESSAGE_CREATE: y,
-        MESSAGE_UPDATE: y,
-        LOCAL_MESSAGES_LOADED: C,
-        LOAD_MESSAGES_SUCCESS: C,
-        LOAD_MESSAGES_AROUND_SUCCESS: C,
+        MESSAGE_CREATE: I,
+        MESSAGE_UPDATE: I,
+        LOCAL_MESSAGES_LOADED: R,
+        LOAD_MESSAGES_SUCCESS: R,
+        LOAD_MESSAGES_AROUND_SUCCESS: R,
         LOAD_RECENT_MENTIONS_SUCCESS: function (e) {
             let { messages: t } = e;
-            t.forEach((e) => S(e));
+            t.forEach((e) => D(e));
         },
         LOAD_PINNED_MESSAGES_SUCCESS: function (e) {
             let { pins: t } = e;
             t.forEach((e) => {
                 let { message: t } = e;
-                return S(t);
+                return D(t);
             });
         },
         SEARCH_MESSAGES_SUCCESS: function (e) {
@@ -193,23 +192,23 @@ let R = new O(o.h, {
             t.forEach((e) => {
                 let { messages: t } = e;
                 t.forEach((e) => {
-                    e.forEach((e) => S(e));
+                    e.forEach((e) => D(e));
                 });
             });
         },
         GIFT_CODE_UPDATE: function (e) {
-            let { uses: t, code: n } = e,
-                i = f.get(n);
-            null != i && f.set(n, i.set("uses", Math.max(i.uses, t)));
+            let { uses: t, code: E } = e,
+                n = u.get(E);
+            null != n && u.set(E, n.set("uses", Math.max(n.uses, t)));
         },
-        LOAD_THREADS_SUCCESS: v,
-        LOAD_ARCHIVED_THREADS_SUCCESS: v,
+        LOAD_THREADS_SUCCESS: G,
+        LOAD_ARCHIVED_THREADS_SUCCESS: G,
         LOAD_FORUM_POSTS: function (e) {
             let { threads: t } = e;
             Object.values(t).map((e) => {
                 let { first_message: t } = e;
-                return null != t && S(t);
+                return null != t && D(t);
             });
         },
     }),
-    b = R;
+    g = y;
--- chunks/425116.js
+++ chunks/425116.js
@@ -1,23 +1,23 @@
 "use strict";
-n.d(t, { $: () => c, A: () => S });
+n.d(t, { $: () => _, A: () => S });
 var i = n(228366),
     r = n(439372),
     s = n(287809),
     a = n(219893),
     o = n(695515),
     l = n(936926),
-    d = n(580296),
-    _ = n(602339),
-    u = n(985018);
-function c() {
+    u = n(580296),
+    c = n(602339),
+    d = n(375708);
+function _() {
     return o.A.isCurrentUserInRestrictedHours();
 }
-let E = !1,
+let f = !1,
     h = null,
-    m = null,
-    f = null;
-function g() {
-    null != m && (clearTimeout(m), (m = null));
+    p = null,
+    E = null;
+function m() {
+    null != p && (clearTimeout(p), (p = null));
     let e = (function () {
         let e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : new Date();
         if (!(0, l.Eq)({ location: "RestrictedHoursManager" })) return null;
@@ -36,47 +36,59 @@ function g() {
         if (e.minutesUntil <= 16) {
             var t;
             let n = `${e.rule.ruleId}:${e.startAtMs}`;
-            if (n !== f) {
+            if (n !== E) {
                 let r, s;
                 (t = e.rule),
-                    (r = u.intl.string(_.default["0JlDg0"])),
+                    (r = d.intl.string(c.default["0JlDg0"])),
                     (s = `${(0, a.n1)(t.days)} ${(0, a.li)(t)}`.trim()),
                     i.h.dispatch({ type: "RESTRICTED_HOURS_WARNING", title: r, subtitle: s }),
-                    (f = n);
+                    (E = n);
             }
-            m = setTimeout(() => {
-                (m = null), g();
+            p = setTimeout(() => {
+                (p = null), m();
             }, 6e4);
         } else
-            m = setTimeout(
+            p = setTimeout(
                 () => {
-                    (m = null), g();
+                    (p = null), m();
                 },
                 Math.max(0, (e.minutesUntil - 16) * 6e4),
             );
 }
-function p() {
-    let e = c();
-    e !== E && ((E = e) ? (0, d.openRestrictedHoursModal)() : (0, d.k)(), g());
+function g() {
+    let e = _();
+    e !== f &&
+        ((f = e),
+        i.h.dispatch({ type: "RESTRICTED_HOURS_STATE_CHANGE", isInRestrictedHours: f }),
+        f ? (0, u.openRestrictedHoursModal)() : (0, u.k)(),
+        m());
 }
 function A() {
-    (E = !1), (f = null), (0, d.k)(), null != m && (clearTimeout(m), (m = null));
+    (f = !1),
+        (E = null),
+        i.h.dispatch({ type: "RESTRICTED_HOURS_STATE_CHANGE", isInRestrictedHours: !1 }),
+        (0, u.k)(),
+        null != p && (clearTimeout(p), (p = null));
 }
 function I() {
-    p(), g();
+    g(), m();
 }
 class T extends r.A {
     stores = new Map().set(s.default, I).set(o.A, I);
     actions = { POST_CONNECTION_OPEN: I, CURRENT_USER_UPDATE: I, LOGOUT: A };
     _initialize() {
-        (E = c()) && (0, d.openRestrictedHoursModal)(), (h = setInterval(p, 6e4)), g();
+        (f = _()),
+            i.h.dispatch({ type: "RESTRICTED_HOURS_STATE_CHANGE", isInRestrictedHours: f }),
+            f && (0, u.openRestrictedHoursModal)(),
+            (h = setInterval(g, 6e4)),
+            m();
     }
     _terminate() {
         null != h && (clearInterval(h), (h = null)),
-            null != m && (clearTimeout(m), (m = null)),
-            (f = null),
-            (0, d.k)(),
-            (E = !1);
+            null != p && (clearTimeout(p), (p = null)),
+            (E = null),
+            (0, u.k)(),
+            (f = !1);
     }
 }
 let S = new T();
