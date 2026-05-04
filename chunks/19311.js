6e1e4f4be08a8f76e59c925586411563d8e10c26
DiscrapperManager <admin@minerpl.xyz>
2026-05-04 20:31:31 +0200

    noisesSuppression 538623

--- chunks/19311.js
+++ chunks/19311.js
@@ -1,61 +1,61 @@
-n.d(t, { Ay: () => N, ti: () => E });
+n.d(t, { Ay: () => N, CY: () => h, ti: () => m });
 var l,
     r = n(627968);
 n(64700);
-var i = n(503698),
-    a = n.n(i),
-    u = n(331322),
-    o = n(990078),
-    s = n(821609),
+var u = n(503698),
+    i = n.n(u),
+    a = n(331322),
+    s = n(990078),
+    o = n(821609),
     d = n(123292),
     c = n(364840),
     A = n(999784),
-    f = n(375708),
-    m = n(613676),
-    E =
+    E = n(375708),
+    f = n(613676),
+    m =
         (((l = {})[(l.CONTINUE = 0)] = "CONTINUE"),
         (l[(l.UPGRADE = 1)] = "UPGRADE"),
         (l[(l.PURCHASE = 2)] = "PURCHASE"),
         l);
 let h = (e) => (2 === e ? "expressive" : 0 === e ? "primary" : "active"),
-    C = (e) => {
+    T = (e) => {
         let {
                 primaryIcon: t,
                 primaryCTA: n,
                 primaryType: l,
-                primaryText: i,
-                primaryDisabled: a,
-                primarySubmitting: u,
-                onPrimary: o,
+                primaryText: u,
+                primaryDisabled: i,
+                primarySubmitting: a,
+                onPrimary: s,
             } = e,
             d = {
                 type: l,
-                disabled: a,
-                onClick: o,
-                icon: null == t ? void 0 : () => (0, r.jsx)(t, { color: "currentColor", className: m.C4 }),
-                loading: u,
-                text: i,
+                disabled: i,
+                onClick: s,
+                icon: null == t ? void 0 : () => (0, r.jsx)(t, { color: "currentColor", className: f.C4 }),
+                loading: a,
+                text: u,
             };
-        return 2 === n ? (0, r.jsx)(s.$, { variant: h(n), ...d }) : (0, r.jsx)(s.$, { ...d, variant: h(n) });
+        return 2 === n ? (0, r.jsx)(o.$, { variant: h(n), ...d }) : (0, r.jsx)(o.$, { ...d, variant: h(n) });
     },
-    T = (e) => {
+    p = (e) => {
         let {
                 onBack: t,
                 backText: n,
                 primaryIcon: l,
-                primaryCTA: i,
-                primaryType: s,
-                primaryText: E,
+                primaryCTA: u,
+                primaryType: o,
+                primaryText: m,
                 primaryTooltip: h,
-                primaryDisabled: T,
+                primaryDisabled: p,
                 primarySubmitting: N,
-                onPrimary: g,
+                onPrimary: C,
             } = e,
-            p =
+            I =
                 null == t
                     ? null
-                    : (0, r.jsx)(d.Q, { text: n ?? f.intl.string(f.t["13/7kX"]), onClick: t, variant: "secondary" }),
-            I = (0, r.jsxs)(u.B, {
+                    : (0, r.jsx)(d.Q, { text: n ?? E.intl.string(E.t["13/7kX"]), onClick: t, variant: "secondary" }),
+            S = (0, r.jsxs)(a.B, {
                 direction: "horizontal",
                 align: "center",
                 gap: 0,
@@ -64,25 +64,25 @@ let h = (e) => (2 === e ? "expressive" : 0 === e ? "primary" : "active"),
                 children: [
                     (0, r.jsx)(A.A, {}),
                     (() => {
-                        if (null == i || null == E) return null;
+                        if (null == u || null == m) return null;
                         let e = {
                             primaryIcon: l,
-                            primaryCTA: i,
-                            primaryType: s,
-                            primaryText: E,
-                            primaryDisabled: T,
+                            primaryCTA: u,
+                            primaryType: o,
+                            primaryText: m,
+                            primaryDisabled: p,
                             primarySubmitting: N,
-                            onPrimary: g,
+                            onPrimary: C,
                         };
                         return null != h
-                            ? (0, r.jsx)(o.m, { text: h, asContainer: !0, children: (0, r.jsx)(C, { ...e }) })
-                            : (0, r.jsx)(C, { ...e, onPrimary: g });
+                            ? (0, r.jsx)(s.m, { text: h, asContainer: !0, children: (0, r.jsx)(T, { ...e }) })
+                            : (0, r.jsx)(T, { ...e, onPrimary: C });
                     })(),
                 ],
             });
         return (0, r.jsx)(c.j, {
-            children: (0, r.jsxs)("div", { className: a()(m.eT, { [m.T$]: null == p }), children: [p, I] }),
+            children: (0, r.jsxs)("div", { className: i()(f.eT, { [f.T$]: null == I }), children: [I, S] }),
         });
     };
-T.CTAType = E;
-let N = T;
+p.CTAType = m;
+let N = p;
--- chunks/891197.js
+++ chunks/891197.js
@@ -1,13 +1,35 @@
-n.d(t, { N: () => a });
-var l = n(627968);
-n(284009);
-var r = n(534514);
-n(902958), n(615310), n(632638);
-var i = n(375708);
-let a = (e) => {
+n.d(t, { N: () => c, p: () => A });
+var l = n(627968),
+    r = n(284009),
+    u = n.n(r),
+    i = n(534514),
+    a = n(902958),
+    s = n(615310),
+    o = n(632638),
+    d = n(375708);
+let c = (e) => {
     let { className: t } = e;
     return (0, l.jsx)("div", {
         className: t,
-        children: (0, l.jsx)(r.D, { variant: "heading-md/semibold", children: i.intl.string(i.t.eALkIR) }),
+        children: (0, l.jsx)(i.D, { variant: "heading-md/semibold", children: d.intl.string(d.t.eALkIR) }),
     });
 };
+function A(e) {
+    let { steps: t, currentStep: n, paymentError: r, className: i } = e,
+        { steps: d, step: A } = (0, s.Ay)(),
+        { paymentError: E } = (0, a.P5)();
+    return (
+        (t = t ?? d),
+        null == n && null != A && (n = A),
+        u()(n, "step should be set"),
+        u()(t, "step should be set"),
+        (r = r ?? E),
+        (0, l.jsx)(o.A, {
+            steps: t,
+            currentStep: n,
+            paymentError: r,
+            body: (0, l.jsx)(c, { className: i }),
+            footer: null,
+        })
+    );
+}
