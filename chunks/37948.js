15f134a546bce9571839268d683e1d41533528ce
DiscrapperManager <admin@minerpl.xyz>
2026-05-04 18:25:37 +0200

    canaryRelease: 538541
    Main scripts: 150
    Lazy Loaded Scripts: 3636
    Chunks:
     * Main Files: 11387
     * Scripts: 8454
    Strings: 24778
    Routes: 820
    Experiments Count: 43
    Apex Experiments Count: 323

--- chunks/37948.js
+++ chunks/37948.js
@@ -1,32 +1,33 @@
-l.d(t, { A: () => r }), l(323874), l(14289), l(35956);
-var n = l(64700),
-    i = l(738533);
+"use strict";
+n.d(t, { A: () => o }), n(323874), n(14289), n(35956);
+var i = n(64700),
+    r = n(738533);
 let s = "steam",
     a = /^\/app\/(\d+)(?:\/)?/;
-function r(e) {
+function o(e) {
     let t = (function (e) {
-        let [t, l] = n.useState(!1);
+        let [t, n] = i.useState(!1);
         return (
-            n.useEffect(() => {
-                i.A.isProtocolRegistered(e).then(l);
+            i.useEffect(() => {
+                r.A.isProtocolRegistered(e).then(n);
             }, [e]),
             t
         );
     })(s);
-    return n.useCallback(
-        (l) => {
-            let n;
-            if (null != l) {
+    return i.useCallback(
+        (n) => {
+            let i;
+            if (null != n) {
                 try {
-                    n = new URL(l);
+                    i = new URL(n);
                 } catch {
                     return;
                 }
-                if ("store.steampowered.com" === n.hostname && t) {
-                    let e = n.pathname.match(a)?.[1];
-                    null != e && (l = `${s}://store/${e}`);
-                } else n.searchParams.set("utm_source", "discord"), (l = n.toString());
-                null != e ? e(l) : window.open(l, "_blank", "noreferrer noopener");
+                if ("store.steampowered.com" === i.hostname && t) {
+                    let e = i.pathname.match(a)?.[1];
+                    null != e && (n = `${s}://store/${e}`);
+                } else i.searchParams.set("utm_source", "discord"), (n = i.toString());
+                null != e ? e(n) : window.open(n, "_blank", "noreferrer noopener");
             }
         },
         [e, t],
--- chunks/913948.js
+++ chunks/913948.js
@@ -1,56 +1,59 @@
 "use strict";
 n.d(t, { A: () => h }), n(321073);
-var l = n(627968),
-    i = n(64700),
-    s = n(821609),
-    a = n(477782),
-    r = n(922016),
-    o = n(550079),
+var i = n(627968),
+    r = n(64700),
+    s = n(989441),
+    a = n(821609),
+    o = n(477782),
+    l = n(922016),
+    u = n(980707),
     c = n(900797),
-    u = n(847374),
-    d = n(985018);
+    d = n(847374),
+    _ = n(37948),
+    f = n(375708);
 function h(e) {
     let {
             distributorCTAConfigs: t,
             buttonVariant: n = "secondary",
             fullWidth: h = !0,
-            stopPropagation: m = !1,
-            onAction: p,
-            onClose: f,
+            stopPropagation: p = !1,
+            onAction: E,
+            onClose: m,
         } = e,
-        g = i.useRef(null),
-        [_, x] = i.useState(!1);
+        g = (0, _.A)(),
+        A = r.useRef(null),
+        [I, T] = r.useState(!1);
     if (0 === t.length) return null;
-    let C = (e, t) => {
-        p?.({ action: t }), f?.(), window.open(e, "_blank", "noopener,noreferrer");
+    let S = (e, t, n) => {
+        E?.({ action: n }), m?.(), t === s.D.STEAM ? g(e) : window.open(e, "_blank", "noreferrer noopener");
     };
     if (1 === t.length) {
-        let { ctaConfig: e, skuId: i } = t[0];
-        return (0, l.jsx)(s.$, {
+        let { ctaConfig: e, skuId: r } = t[0];
+        return (0, i.jsx)(a.$, {
             variant: n,
             size: "sm",
             icon: e.icon,
             text: e.getLabel(),
             fullWidth: h,
             onClick: (t) => {
-                m && t.stopPropagation(), C(e.getStoreUrl(i), e.analyticsAction);
+                p && t.stopPropagation(), S(e.getStoreUrl(r), e.distributor, e.analyticsAction);
             },
         });
     }
-    let A = t.flatMap((e, t) => {
-        let { ctaConfig: n, skuId: i } = e,
+    let N = t.flatMap((e, t) => {
+        let { ctaConfig: n, skuId: r } = e,
             s = [];
         return (
-            t > 0 && s.push((0, l.jsx)(a.bX, {}, `sep-${n.distributor}`)),
+            t > 0 && s.push((0, i.jsx)(o.bX, {}, `sep-${n.distributor}`)),
             s.push(
-                (0, l.jsx)(
-                    a.Dr,
+                (0, i.jsx)(
+                    o.Dr,
                     {
                         id: `distributor-${n.distributor}`,
                         label: n.getStoreName(),
                         iconLeft: n.icon,
                         leadingAccessory: { type: "icon", icon: n.icon },
-                        action: () => C(n.getStoreUrl(i), n.analyticsAction),
+                        action: () => S(n.getStoreUrl(r), n.distributor, n.analyticsAction),
                     },
                     n.distributor,
                 ),
@@ -58,38 +61,38 @@ function h(e) {
             s
         );
     });
-    return (0, l.jsx)(r.Y, {
-        targetElementRef: g,
+    return (0, i.jsx)(l.Y, {
+        targetElementRef: A,
         position: "bottom",
-        onRequestOpen: () => x(!0),
-        onRequestClose: () => x(!1),
+        onRequestOpen: () => T(!0),
+        onRequestClose: () => T(!1),
         renderPopout: (e) => {
             let { closePopout: t } = e;
-            return (0, l.jsx)("div", {
+            return (0, i.jsx)("div", {
                 onClick: (e) => e.stopPropagation(),
-                style: { width: "fit-content", minWidth: g.current?.offsetWidth },
-                children: (0, l.jsx)(o.W, {
+                style: { width: "fit-content", minWidth: A.current?.offsetWidth },
+                children: (0, i.jsx)(u.W, {
                     "data-menu-migrated": !0,
                     navId: "play-on-distributor-menu",
                     onClose: t,
                     onSelect: void 0,
-                    "aria-label": d.intl.string(d.t["3XhYOS"]),
-                    children: (0, l.jsx)(a.rX, { children: A }),
+                    "aria-label": f.intl.string(f.t["3XhYOS"]),
+                    children: (0, i.jsx)(o.rX, { children: N }),
                 }),
             });
         },
         children: (e) =>
-            (0, l.jsx)(s.$, {
-                buttonRef: g,
+            (0, i.jsx)(a.$, {
+                buttonRef: A,
                 variant: n,
                 size: "sm",
-                icon: _ ? c.t : u.a,
+                icon: I ? c.t : d.a,
                 iconPosition: "end",
-                text: d.intl.string(d.t.nSHoxC),
+                text: f.intl.string(f.t.nSHoxC),
                 fullWidth: h,
                 ...e,
                 onClick: (t) => {
-                    m && t.stopPropagation(), e.onClick?.(t);
+                    p && t.stopPropagation(), e.onClick?.(t);
                 },
             }),
     });
