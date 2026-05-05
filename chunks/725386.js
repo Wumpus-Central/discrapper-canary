7eee6570d3dc519196478663dc0857fb9cd72fbd
DiscrapperManager <admin@minerpl.xyz>
2026-05-05 23:12:04 +0200

    canaryRelease: 539526
    Main scripts: 142
    Lazy Loaded Scripts: 3673
    Chunks:
     * Main Files: 11384
     * Scripts: 8656
    Strings: 24802
    Routes: 820
    Experiments Count: 40
    Apex Experiments Count: 324

--- chunks/725386.js
+++ chunks/725386.js
@@ -1,51 +1,58 @@
-n.d(t, { A: () => p });
+n.d(t, { A: () => A });
 var i = n(64700),
-    l = n(540185),
-    s = n(702841),
-    a = n(382483),
-    r = n(385113),
-    c = n(369374),
-    o = n(457965),
-    u = n(885386),
-    d = n(287809),
-    g = n(403362),
-    m = n(633075),
-    f = n(289173),
-    x = n(667049),
-    I = n(229231);
-function p(e) {
-    let { enabled: t } = c.A.useConfig({ location: e.location }),
-        n = u.Q_.useSetting(),
-        p = (0, s.bG)([d.default], () => d.default.getCurrentUser()),
-        A = (0, x.A)(p?.id),
-        h = (0, o.w$)(e),
-        E = (0, s.bG)([r.A], () => r.A.getAllConfigsByApplication());
+    l = n(598748),
+    s = n(540185),
+    a = n(702841),
+    r = n(382483),
+    c = n(385113),
+    o = n(369374),
+    u = n(457965),
+    d = n(885386),
+    g = n(287809),
+    m = n(403362),
+    f = n(633075),
+    x = n(289173),
+    I = n(667049),
+    p = n(229231);
+function A(e) {
+    let { enabled: t } = o.A.useConfig({ location: e.location }),
+        n = d.Q_.useSetting(),
+        A = (0, a.bG)([g.default], () => g.default.getCurrentUser()),
+        h = (0, I.A)(A?.id),
+        E = (0, u.w$)(e),
+        j = (0, a.bG)([c.A], () => c.A.getAllConfigsByApplication());
     return (
         i.useEffect(() => {
-            t && ((0, a.Wq)(), n && (0, a.i$)());
+            t && ((0, r.Wq)(), n && (0, r.i$)());
         }, [t, n]),
         i.useMemo(() => {
-            let e = new Set(A.filter((e) => e instanceof m.R).map((e) => e.applicationId)),
-                n = new Set(A.map((e) => e.type));
-            return I.Zc.flatMap((i) =>
-                i === l.x.APPLICATION
+            let e = new Set(h.filter((e) => e instanceof f.R).map((e) => e.applicationId)),
+                n = new Set(h.map((e) => e.type));
+            return p.Zc.flatMap((i) =>
+                i === s.x.APPLICATION
                     ? t
-                        ? Object.values(E)
+                        ? Object.values(j)
                               .map((e) => e[0])
-                              .filter(g.Vq)
+                              .filter(m.Vq)
                               .filter((t) => !e.has(t.application_id))
-                              .map((e) => new m.R({ applicationId: e.application_id }))
-                        : null == h
+                              .filter(
+                                  (e) =>
+                                      null != e.surfaces[l.m.WIDGET_TOP] &&
+                                      null != e.surfaces[l.m.WIDGET_BOTTOM] &&
+                                      null != e.surfaces[l.m.ADD_WIDGET_PREVIEW],
+                              )
+                              .map((e) => new f.R({ applicationId: e.application_id }))
+                        : null == E
                           ? []
-                          : h
-                                .filter((t) => !e.has(t.applicationId))
-                                .map((e) => new m.R({ applicationId: e.applicationId }))
-                    : (0, f.hL)(i)
+                          : E.filter((t) => !e.has(t.applicationId)).map(
+                                (e) => new f.R({ applicationId: e.applicationId }),
+                            )
+                    : (0, x.hL)(i)
                       ? n.has(i)
                           ? []
-                          : new f.Yy({ type: i, games: [] })
-                      : void (0, g.xb)(i),
+                          : new x.Yy({ type: i, games: [] })
+                      : void (0, m.xb)(i),
             );
-        }, [t, E, h, A])
+        }, [t, j, E, h])
     );
 }
