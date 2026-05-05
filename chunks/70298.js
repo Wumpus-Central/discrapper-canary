b9a2eaf06bcbadcd7be48335e7792f661055c99e
DiscrapperManager <admin@minerpl.xyz>
2026-05-05 21:38:51 +0200

    canaryRelease: 539433
    Main scripts: 150
    Lazy Loaded Scripts: 3667
    Chunks:
     * Main Files: 11392
     * Scripts: 8483
    Strings: 24794
    Routes: 820
    Experiments Count: 40
    Apex Experiments Count: 325

--- chunks/70298.js
+++ chunks/70298.js
@@ -1,6 +1,14 @@
 "use strict";
 function i() {
     let e = window;
-    return null != e.jQuery || null != e.$ || null != e.BetterDiscord || null != e.BdApi || null != e.rambox;
+    return (
+        null != e.jQuery ||
+        null != e.$ ||
+        null != e.BetterDiscord ||
+        null != e.BdApi ||
+        null != e.rambox ||
+        null != e.Vencord ||
+        null != e.VencordNative
+    );
 }
 n.d(t, { b: () => i });
