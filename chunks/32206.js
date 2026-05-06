d03a5670ecf16c8f0e6d526d73c6c50e6b603566
DiscrapperManager <admin@minerpl.xyz>
2026-05-06 22:29:38 +0200

    noisesSuppression 540303

--- chunks/32206.js
+++ chunks/32206.js
@@ -1,10 +1,9 @@
-"use strict";
-n.d(t, { A: () => s, c: () => r });
-let i = (0, n(945810).mj)({
+a.d(l, { A: () => n, c: () => r });
+let i = (0, a(945810).mj)({
         name: "2026-03-collectibles-profile-frames",
         kind: "user",
         defaultConfig: { enableProfileFrames: !1 },
         variations: { 0: { enableProfileFrames: !1 }, 1: { enableProfileFrames: !0 } },
     }),
     r = (e) => i.useConfig({ location: e }).enableProfileFrames,
-    s = i;
+    n = 21552 == a.j ? i : null;
