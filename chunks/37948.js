cc067bb4a17c952c2ac32a61e7c4b8f2a9588f58
DiscrapperManager <admin@minerpl.xyz>
2026-05-07 00:25:22 +0200

    canaryRelease: 540424
    Main scripts: 152
    Lazy Loaded Scripts: 3653
    Chunks:
     * Main Files: 11406
     * Scripts: 8625
    Strings: 24810
    Routes: 818
    Experiments Count: 40
    Apex Experiments Count: 322

--- chunks/37948.js
+++ chunks/37948.js
@@ -1,19 +1,20 @@
 "use strict";
-n.d(t, { A: () => o }), n(323874), n(14289), n(35956);
+n.d(t, { A: () => l }), n(323874), n(14289), n(35956);
 var i = n(64700),
-    r = n(738533);
-let s = "steam",
-    a = /^\/app\/(\d+)(?:\/)?/;
-function o(e) {
+    r = n(975807),
+    s = n(738533);
+let a = "steam",
+    o = /^\/app\/(\d+)(?:\/)?/;
+function l(e) {
     let t = (function (e) {
         let [t, n] = i.useState(!1);
         return (
             i.useEffect(() => {
-                r.A.isProtocolRegistered(e).then(n);
+                s.A.isProtocolRegistered(e).then(n);
             }, [e]),
             t
         );
-    })(s);
+    })(a);
     return i.useCallback(
         (n) => {
             let i;
@@ -24,10 +25,22 @@ function o(e) {
                     return;
                 }
                 if ("store.steampowered.com" === i.hostname && t) {
-                    let e = i.pathname.match(a)?.[1];
-                    null != e && (n = `${s}://store/${e}`);
-                } else i.searchParams.set("utm_source", "discord"), (n = i.toString());
-                null != e ? e(n) : window.open(n, "_blank", "noreferrer noopener");
+                    let t = i.pathname.match(o)?.[1];
+                    if (null != t) {
+                        let n = `${a}://store/${t}`;
+                        if (null != e) return void e(n);
+                        i.searchParams.set("utm_source", "discord");
+                        let s = i.toString(),
+                            o = setTimeout(() => {
+                                (0, r.A)(s);
+                            }, 2500);
+                        return (
+                            window.addEventListener("blur", () => clearTimeout(o), { once: !0 }),
+                            void (window.location.href = n)
+                        );
+                    }
+                }
+                i.searchParams.set("utm_source", "discord"), (n = i.toString()), null != e ? e(n) : (0, r.A)(n);
             }
         },
         [e, t],
