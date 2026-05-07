d1b193500a4605da1d2116696434da66342d6d37
DiscrapperManager <admin@minerpl.xyz>
2026-05-07 22:35:44 +0200

    canaryRelease: 540941
    Main scripts: 150
    Lazy Loaded Scripts: 3651
    Chunks:
     * Main Files: 11421
     * Scripts: 8592
    Strings: 24826
    Routes: 817
    Experiments Count: 40
    Apex Experiments Count: 323

--- chunks/299619.js
+++ chunks/299619.js
@@ -1,7 +1,7 @@
 "use strict";
 n.d(t, { A: () => l });
-var r = n(627968),
-    i = n(64700),
+var i = n(627968),
+    r = n(64700),
     s = n(663675),
     a = n(621466);
 let o = (e) => {
@@ -11,67 +11,67 @@ let o = (e) => {
                 autoPlay: o,
                 playOnHover: l,
                 onPlayError: u,
-                responsive: d,
-                mediaLayoutType: c,
+                responsive: c,
+                mediaLayoutType: d,
                 useReducedMotion: _,
                 ...f
             } = e,
-            E = !_ && !l && o,
-            h = i.useRef(null);
-        function p() {
+            h = !_ && !l && o,
+            p = r.useRef(null);
+        function E() {
             l &&
-                h?.current?.play()?.catch((e) => {
+                p?.current?.play()?.catch((e) => {
                     if (null != u) u(e);
                     else throw e;
                 });
         }
         function m() {
-            l && h?.current?.pause();
+            l && p?.current?.pause();
         }
-        return (
-            i.useLayoutEffect(
-                () => () => {
-                    let { current: e } = h;
-                    null != e &&
-                        (function (e) {
-                            e.removeAttribute("src"),
-                                Array.from(e.children).forEach((e) => {
-                                    (0, a.vq)(e, HTMLSourceElement) &&
-                                        (e.removeAttribute("src"), e.removeAttribute("type")),
-                                        (0, a.vq)(e, HTMLImageElement) && e.removeAttribute("src");
-                                });
-                            try {
-                                e.load();
-                            } catch (e) {}
-                        })(e);
-                },
-                [],
-            ),
-            i.useLayoutEffect(
+        r.useLayoutEffect(
+            () => () => {
+                let { current: e } = p;
+                null != e &&
+                    (function (e) {
+                        e.removeAttribute("src"),
+                            Array.from(e.children).forEach((e) => {
+                                (0, a.vq)(e, HTMLSourceElement) &&
+                                    (e.removeAttribute("src"), e.removeAttribute("type")),
+                                    (0, a.vq)(e, HTMLImageElement) && e.removeAttribute("src");
+                            });
+                        try {
+                            e.load();
+                        } catch (e) {}
+                    })(e);
+            },
+            [],
+        ),
+            r.useLayoutEffect(
                 () => (
-                    "function" == typeof n ? (n(null), n(h.current)) : null != n && (n.current = h.current),
+                    "function" == typeof n ? (n(null), n(p.current)) : null != n && (n.current = p.current),
                     () => {
                         "function" == typeof n ? n(null) : null != n && (n.current = null);
                     }
                 ),
-                [n, h],
-            ),
-            (0, r.jsx)("video", {
-                "aria-label": t,
-                ref: h,
-                autoPlay: E,
-                onMouseEnter: p,
-                onMouseLeave: m,
-                onFocus: p,
-                onBlur: m,
-                style:
-                    c === s.d.MOSAIC
-                        ? { width: "100%", height: "100%", maxHeight: "inherit", objectFit: "contain" }
-                        : d
-                          ? { maxWidth: f.width, maxHeight: f.height, width: "100%", height: "100%" }
-                          : {},
-                ...f,
-            })
-        );
+                [n, p],
+            );
+        let g = f.tabIndex ?? (!0 === f.controls ? 0 : -1);
+        return (0, i.jsx)("video", {
+            "aria-label": t,
+            ref: p,
+            autoPlay: h,
+            onMouseEnter: E,
+            onMouseLeave: m,
+            onFocus: E,
+            onBlur: m,
+            style:
+                d === s.d.MOSAIC
+                    ? { width: "100%", height: "100%", maxHeight: "inherit", objectFit: "contain" }
+                    : c
+                      ? { maxWidth: f.width, maxHeight: f.height, width: "100%", height: "100%" }
+                      : {},
+            tabIndex: g,
+            ...f,
+        });
     },
-    l = i.forwardRef((e, t) => (0, r.jsx)(o, { ...e, externalRef: t }));
+    l = r.forwardRef((e, t) => (0, i.jsx)(o, { ...e, externalRef: t }));
