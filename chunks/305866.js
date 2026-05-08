49aaca163c6d6a8540219d18c6e2f5bcc2049f6c
DiscrapperManager <admin@minerpl.xyz>
2026-05-08 18:23:13 +0200

    canaryRelease: 541355
    Main scripts: 154
    Lazy Loaded Scripts: 3655
    Chunks:
     * Main Files: 11540
     * Scripts: 8468
    Strings: 24833
    Routes: 818
    Experiments Count: 40
    Apex Experiments Count: 326

--- chunks/305866.js
+++ chunks/305866.js
@@ -1,41 +1,46 @@
 "use strict";
-n.d(t, { M: () => u, l: () => d });
-var r = n(627968),
-    i = n(64700),
+n.d(t, { M: () => u, l: () => c });
+var i = n(627968),
+    r = n(64700),
     s = n(315710),
     a = n(707554),
     o = n(187322),
     l = n(989395);
-let u = i.createContext({ inDialog: void 0 }),
-    d = i.forwardRef(function (e, t) {
-        let { children: n, impressionType: d, impression: c, disableTrack: _, returnRef: f, ...E } = e,
-            h = i.useRef(null),
-            p = i.useRef(null),
-            [m, g] = i.useState(!1),
-            A = m ? p : h;
+let u = r.createContext({ inDialog: void 0 }),
+    c = r.forwardRef(function (e, t) {
+        let { children: n, impressionType: c, impression: d, disableTrack: _, returnRef: f, ...h } = e,
+            p = r.useRef(null),
+            E = r.useRef(null),
+            [m, g] = r.useState(!1),
+            A = m ? E : p;
         (0, s.t)(A, { returnRef: f }),
-            i.useContext(l.A)(
-                { type: d, name: c?.impressionName, properties: c?.impressionProperties },
+            r.useContext(l.A)(
+                { type: c, name: d?.impressionName, properties: d?.impressionProperties },
                 { disableTrack: _ },
+            );
+        let I = r.useCallback(
+                (e) => {
+                    (p.current = e), "function" == typeof t ? t(e) : null != t && (t.current = e);
+                },
+                [t],
             ),
-            i.useImperativeHandle(t, () => h.current);
-        let I = i.useCallback(
-            (e) => {
-                m !== e && g(e);
-            },
-            [m],
-        );
-        return (0, r.jsx)(u.Provider, {
-            value: { inDialog: !0, setFocusLockDisabled: I },
-            children: (0, r.jsx)("div", {
-                ...E,
-                ref: h,
+            T = r.useCallback(
+                (e) => {
+                    m !== e && g(e);
+                },
+                [m],
+            );
+        return (0, i.jsx)(u.Provider, {
+            value: { inDialog: !0, setFocusLockDisabled: T },
+            children: (0, i.jsx)("div", {
+                ...h,
+                ref: I,
                 role: "dialog",
                 tabIndex: -1,
                 "aria-modal": !0,
-                children: (0, r.jsx)(a.F, {
+                children: (0, i.jsx)(a.F, {
                     forceLevel: 1,
-                    children: (0, r.jsx)(o.xp, { containerRef: h, children: n }),
+                    children: (0, i.jsx)(o.xp, { containerRef: p, children: n }),
                 }),
             }),
         });
