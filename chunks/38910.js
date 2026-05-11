8f28117745f1c4b18ce6de5b4db2c88cbf86c880
DiscrapperManager <admin@minerpl.xyz>
2026-05-11 16:17:39 +0200

    noisesSuppression 541908

--- chunks/38910.js
+++ chunks/38910.js
@@ -1,1 +1,17 @@
-n.d(t,{A:()=>r});var i=n(636537),l=n(228366),s=n(652215);let r={async fetch(){try{let e=await i.Bo.get({url:s.Rsh.FRIEND_SUGGESTIONS,rejectWithError:!0});l.h.dispatch({type:"LOAD_FRIEND_SUGGESTIONS_SUCCESS",suggestions:e.body})}catch(e){l.h.dispatch({type:"LOAD_FRIEND_SUGGESTIONS_FAILURE"})}},ignore(e){i.Bo.del({url:s.Rsh.FRIEND_SUGGESTION(e),rejectWithError:!0})}}hError:!0})}}
+n.d(t, { A: () => r });
+var i = n(636537),
+    l = n(228366),
+    s = n(652215);
+let r = {
+    async fetch() {
+        try {
+            let e = await i.Bo.get({ url: s.Rsh.FRIEND_SUGGESTIONS, rejectWithError: !0 });
+            l.h.dispatch({ type: "LOAD_FRIEND_SUGGESTIONS_SUCCESS", suggestions: e.body });
+        } catch (e) {
+            l.h.dispatch({ type: "LOAD_FRIEND_SUGGESTIONS_FAILURE" });
+        }
+    },
+    ignore(e) {
+        i.Bo.del({ url: s.Rsh.FRIEND_SUGGESTION(e), rejectWithError: !0 });
+    },
+};
