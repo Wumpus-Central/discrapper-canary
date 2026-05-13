8c79df40d37a5d1139f888b828e58dba0183b97c
DiscrapperManager <admin@minerpl.xyz>
2026-05-14 01:26:59 +0200

    canaryRelease: 543204
    Main scripts: 148
    Lazy Loaded Scripts: 3677
    Chunks:
     * Main Files: 11361
     * Scripts: 8708
    Strings: 24872
    Routes: 818
    Experiments Count: 39
    Apex Experiments Count: 329

--- chunks/45837.js
+++ chunks/45837.js
@@ -1,18 +1,1 @@
-n.d(t, { A: () => s });
-var i = n(64700),
-    r = n(17928),
-    a = n(775602),
-    u = n(885386),
-    l = n(531685);
-function s() {
-    let e = arguments.length > 0 && void 0 !== arguments[0] && arguments[0],
-        [t, n] = i.useState(!1),
-        s = (0, r.bG)([a.A], () => a.A.useReducedMotion),
-        o = (0, r.bG)([l.A], () => l.A.isFocused()),
-        c = u.kt.useSetting();
-    return {
-        shouldAnimate: o && (t || (!s && !e && c)),
-        onMouseEnter: i.useCallback(() => n(!0), []),
-        onMouseLeave: i.useCallback(() => n(!1), []),
-    };
-}
+n.d(t,{A:()=>s});var i=n(64700),r=n(17928),a=n(775602),u=n(885386),l=n(531685);function s(){let e=arguments.length>0&&void 0!==arguments[0]&&arguments[0],[t,n]=i.useState(!1),s=(0,r.bG)([a.A],()=>a.A.useReducedMotion),o=(0,r.bG)([l.A],()=>l.A.isFocused()),c=u.kt.useSetting();return{shouldAnimate:o&&(t||!s&&!e&&c),onMouseEnter:i.useCallback(()=>n(!0),[]),onMouseLeave:i.useCallback(()=>n(!1),[])}}=>n(!1),[])}}
