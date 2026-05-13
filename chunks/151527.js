326748db4c017ff57c268c96668e1f31faa8f190
DiscrapperManager <admin@minerpl.xyz>
2026-05-13 22:23:44 +0200

    canaryRelease: 542284
    Main scripts: 152
    Lazy Loaded Scripts: 3655
    Chunks:
     * Main Files: 11534
     * Scripts: 8490
    Strings: 24851
    Routes: 818
    Experiments Count: 40
    Apex Experiments Count: 324

---
+++ chunks/151527.js
@@ -0,0 +1,4 @@
+s.r(i), s.d(i, { default: () => t });
+let t = JSON.parse(
+    '{"q5M7HV":["Other"],"zRKbpz":["Server"],"UzRF+8":["Social"],"NGdNZb":["Direct messages"],"zJlwvV":["Discord system messages"],"40TIqW":["Event notifications"],"HibKoy":["Forum notifications"],"vcrTV5":["Friend Activity"],"NxgGZA":["Friend requests"],"9EDo+/":["Gaming"],"IUH/Oe":["Incoming calls"],"4qWUAO":["Messages"],"n0Wp6j":["Missed Messages"],"T+79Eo":["Other"],"kIrLfg":["Other server notifications"],"MOjygY":["Polls"],"wv4QHR":["Reaction Notifications"],"S5cB9e":["Real-time"],"4n388K":["Stage notifications"],"Iy9grw":["Voice & Live Activity"],"VeBD1N":["Voice connected"]}',
+);
---
+++ chunks/483110.js
@@ -0,0 +1,5 @@
+d.r(s), d.d(s, { default: () => h, messagesLoader: () => p });
+let { createLoader: a } = d(919523),
+    p = a({ "en-US": () => d.e("92780").then(d.bind(d, 151527)) }, "en-US"),
+    { makeMessagesProxy: t } = d(919523),
+    h = t(p);
