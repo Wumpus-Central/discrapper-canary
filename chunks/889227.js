6e70806afcb9584c975fc55c1466a115adc2b122
DiscrapperManager <admin@minerpl.xyz>
2026-05-13 21:25:46 +0200

    canaryRelease: 542026
    Main scripts: 152
    Lazy Loaded Scripts: 3651
    Chunks:
     * Main Files: 11529
     * Scripts: 8485
    Strings: 24840
    Routes: 818
    Experiments Count: 40
    Apex Experiments Count: 325

--- chunks/889227.js
+++ chunks/889227.js
@@ -6,16 +6,16 @@ var i = n(136722),
     a = n(315069),
     o = n(821956),
     l = n(780898),
-    d = n(945096),
-    _ = n(47537),
-    u = n(244284),
-    c = n(817857),
-    E = n(392737),
+    u = n(945096),
+    c = n(47537),
+    d = n(244284),
+    _ = n(349871),
+    f = n(392737),
     h = n(486020),
-    m = n(474090),
-    f = n(935208),
-    g = n(652215),
-    p = n(788868);
+    p = n(474090),
+    E = n(935208),
+    m = n(652215),
+    g = n(788868);
 class A extends a.A {
     id;
     username;
@@ -59,7 +59,7 @@ class A extends a.A {
         const t = e.premium_type ?? e.premiumType;
         (this.id = e.id),
             (this.username = e.username ?? ""),
-            (this.discriminator = e.discriminator ?? g.h3J),
+            (this.discriminator = e.discriminator ?? m.h3J),
             (this.avatar = e.avatar ?? null),
             (this.avatarDecoration = e.avatar_decoration_data ?? e.avatarDecorationData),
             (this.email = e.email ?? null),
@@ -69,7 +69,7 @@ class A extends a.A {
             (this.mfaEnabled = e.mfa_enabled ?? e.mfaEnabled ?? !1),
             (this.mobile = e.mobile ?? !1),
             (this.desktop = e.desktop ?? !1),
-            (this.premiumType = t === g.oA2 ? null : t),
+            (this.premiumType = t === m.oA2 ? null : t),
             (this.flags = e.flags ?? 0),
             (this.publicFlags = e.public_flags ?? e.publicFlags ?? 0),
             (this.purchasedFlags = e.purchased_flags ?? e.purchasedFlags ?? 0),
@@ -82,12 +82,12 @@ class A extends a.A {
             (this.personalConnectionId = e.personal_connection_id ?? e.personalConnectionId ?? null),
             (this.globalName = e.global_name ?? e.globalName),
             (this.banner = e.banner),
-            (this.primaryGuild = (0, _.j)(e.primary_guild ?? e.primaryGuild ?? null)),
+            (this.primaryGuild = (0, c.j)(e.primary_guild ?? e.primaryGuild ?? null)),
             (this.collectibles = e.collectibles),
-            (this.displayNameStyles = e.displayNameStyles ?? (0, d.mT)(e.display_name_styles)),
-            (this.premiumState = e.premiumState ?? (0, E.f)(e.premium_state)),
-            (this.perks = e.perks?.activePerksBitmask != null ? e.perks : (0, c.c$)(e.perks)),
-            (this.restrictedSchedule = (0, u.QU)(e.restricted_schedule ?? e.restrictedSchedule)),
+            (this.displayNameStyles = e.displayNameStyles ?? (0, u.mT)(e.display_name_styles)),
+            (this.premiumState = e.premiumState ?? (0, f.f)(e.premium_state)),
+            (this.perks = e.perks?.activePerksBitmask != null ? e.perks : (0, _.c$)(e.perks)),
+            (this.restrictedSchedule = (0, d.QU)(e.restricted_schedule ?? e.restrictedSchedule)),
             (this.appTransactionIds = e.appTransactionIds ?? e.app_transaction_ids ?? null),
             (this.storeCountry = (function (e) {
                 return null == e
@@ -111,27 +111,27 @@ class A extends a.A {
                         return i.zy(i.kg(t, n), r);
                     },
                 },
-                isStaff: { writable: !1, configurable: !1, enumerable: !1, value: () => this.hasFlag(g.nhx.STAFF) },
+                isStaff: { writable: !1, configurable: !1, enumerable: !1, value: () => this.hasFlag(m.nhx.STAFF) },
                 isStaffPersonal: {
                     writable: !1,
                     configurable: !1,
                     enumerable: !1,
-                    value: () => !this.hasFlag(g.nhx.STAFF) && null != this.personalConnectionId,
+                    value: () => !this.hasFlag(m.nhx.STAFF) && null != this.personalConnectionId,
                 },
                 hasAnyStaffLevel: {
                     writable: !1,
                     configurable: !1,
                     enumerable: !1,
                     value: () =>
-                        this.hasFlag(g.nhx.STAFF) ||
-                        this.hasFlag(g.nhx.COLLABORATOR) ||
-                        this.hasFlag(g.nhx.RESTRICTED_COLLABORATOR),
+                        this.hasFlag(m.nhx.STAFF) ||
+                        this.hasFlag(m.nhx.COLLABORATOR) ||
+                        this.hasFlag(m.nhx.RESTRICTED_COLLABORATOR),
                 },
             }),
             this.globalName?.length === 0 && (this.globalName = null);
     }
     get createdAt() {
-        return new Date(f.default.extractTimestamp(this.id));
+        return new Date(E.default.extractTimestamp(this.id));
     }
     hasVerifiedEmailOrPhone() {
         return !0 === this.verified || null != this.phone;
@@ -200,20 +200,20 @@ class A extends a.A {
         return (0, r.Lt)(this.premiumUsageFlags, e);
     }
     hasHadSKU(e) {
-        let t = p.OO[e];
+        let t = g.OO[e];
         return null != t && this.hasPurchasedFlag(t);
     }
     hasHadPremium() {
         let e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : null,
-            t = this.hasPurchasedFlag(p.lA.PREMIUM_TIER_0),
-            n = this.hasPurchasedFlag(p.lA.PREMIUM_TIER_1),
-            i = this.hasPurchasedFlag(p.lA.PREMIUM_TIER_2);
+            t = this.hasPurchasedFlag(g.lA.PREMIUM_TIER_0),
+            n = this.hasPurchasedFlag(g.lA.PREMIUM_TIER_1),
+            i = this.hasPurchasedFlag(g.lA.PREMIUM_TIER_2);
         switch (e) {
-            case p.PremiumTypes.TIER_0:
+            case g.PremiumTypes.TIER_0:
                 return t;
-            case p.PremiumTypes.TIER_1:
+            case g.PremiumTypes.TIER_1:
                 return n;
-            case p.PremiumTypes.TIER_2:
+            case g.PremiumTypes.TIER_2:
                 return i;
             default:
                 return t || n || i;
@@ -221,20 +221,20 @@ class A extends a.A {
     }
     hadPremiumSubscription() {
         let e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : null;
-        return !(0, m.ki)(this) && this.hasHadPremium(e);
+        return !(0, p.ki)(this) && this.hasHadPremium(e);
     }
     hasFreePremium() {
-        return this.isStaff() || this.hasFlag(g.nhx.PARTNER) || this.isStaffPersonal();
+        return this.isStaff() || this.hasFlag(m.nhx.PARTNER) || this.isStaffPersonal();
     }
     isOnReverseTrial() {
-        return (0, m.ki)(this) && this.premiumState?.premiumSource === s.fE.REVERSE_TRIAL;
+        return (0, p.ki)(this) && this.premiumState?.premiumSource === s.fE.REVERSE_TRIAL;
     }
     isPremiumWithPremiumGroup() {
-        return (0, m.ki)(this, p.PremiumTypes.TIER_2) && this.premiumState?.premiumSource === s.fE.SUBSCRIPTION_GROUP;
+        return (0, p.ki)(this, g.PremiumTypes.TIER_2) && this.premiumState?.premiumSource === s.fE.SUBSCRIPTION_GROUP;
     }
     isPremiumWithFractionalPremiumOnly() {
         return (
-            (0, m.ki)(this, p.PremiumTypes.TIER_2) &&
+            (0, p.ki)(this, g.PremiumTypes.TIER_2) &&
             (this.premiumState?.premiumSubscriptionType === s.jK.NONE_UNSPECIFIED ||
                 this.premiumState?.premiumSubscriptionType === s.jK.BOOST_ONLY) &&
             this.premiumState?.premiumSource === s.fE.FRACTIONAL_NITRO
@@ -242,22 +242,25 @@ class A extends a.A {
     }
     isFractionalPremiumWithNoStandardSub() {
         return (
-            (0, m.ki)(this, p.PremiumTypes.TIER_2) &&
+            (0, p.ki)(this, g.PremiumTypes.TIER_2) &&
             this.premiumState?.premiumSource === s.fE.FRACTIONAL_NITRO &&
             this.premiumState.premiumSubscriptionType !== s.jK.TIER_2
         );
     }
+    isFractionalPremium() {
+        return (0, p.ki)(this, g.PremiumTypes.TIER_2) && this.premiumState?.premiumSource === s.fE.FRACTIONAL_NITRO;
+    }
     hasUrgentMessages() {
-        return this.hasFlag(g.nhx.HAS_UNREAD_URGENT_MESSAGES);
+        return this.hasFlag(m.nhx.HAS_UNREAD_URGENT_MESSAGES);
     }
     isNonUserBot() {
-        return this.isSystemUser() || (this.bot && this.discriminator === g.h3J);
+        return this.isSystemUser() || (this.bot && this.discriminator === m.h3J);
     }
     isLocalBot() {
-        return this.bot && this.id === g.oIV;
+        return this.bot && this.id === m.oIV;
     }
     isVerifiedBot() {
-        return this.isSystemUser() || this.isLocalBot() || this.hasFlag(g.nhx.VERIFIED_BOT);
+        return this.isSystemUser() || this.isLocalBot() || this.hasFlag(m.nhx.VERIFIED_BOT);
     }
     isSystemUser() {
         return !0 === this.system;
@@ -275,7 +278,7 @@ class A extends a.A {
         return this.isPremiumWithPremiumGroup() && this.premiumGroupRole === s.qT.PRIMARY;
     }
     get isProvisional() {
-        return this.hasFlag(g.nhx.PROVISIONAL_ACCOUNT);
+        return this.hasFlag(m.nhx.PROVISIONAL_ACCOUNT);
     }
     get avatarDecoration() {
         return this.avatarDecorationData;
