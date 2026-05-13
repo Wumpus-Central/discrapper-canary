3d8be379fc658d091f8d666afe3852cf101d6458
DiscrapperManager <admin@minerpl.xyz>
2026-05-14 00:01:20 +0200

    canaryRelease: 542809
    Main scripts: 151
    Lazy Loaded Scripts: 3668
    Chunks:
     * Main Files: 11523
     * Scripts: 8531
    Strings: 24865
    Routes: 818
    Experiments Count: 40
    Apex Experiments Count: 326

--- chunks/580296.js
+++ chunks/580296.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { openRestrictedHoursModal: () => N, k: () => y });
+n.d(t, { openRestrictedHoursModal: () => y, k: () => C });
 var i = n(627968),
     r = n(64700),
     s = n(663066),
@@ -10,76 +10,79 @@ var i = n(627968),
     c = n(17928),
     d = n(349288),
     _ = n(830215),
-    f = n(287809),
-    h = n(375708),
-    p = n(695515),
-    E = n(425116),
-    m = n(602339),
-    g = n(88438);
-function A() {
-    _.A.logout("restricted_hours");
-}
-function I(e) {
+    f = n(976860),
+    h = n(287809),
+    p = n(375708),
+    E = n(695515),
+    m = n(425116),
+    g = n(652215),
+    A = n(602339),
+    I = n(88438);
+function T(e) {
     let { onClose: t } = e,
-        n = (0, c.bG)([f.default, p.A], E.$),
-        s = (0, c.bG)([f.default], () => {
-            let e = f.default.getCurrentUser()?.restrictedSchedule?.getNextEndTime();
+        n = (0, c.bG)([h.default, E.A], m.$),
+        s = (0, c.bG)([h.default], () => {
+            let e = h.default.getCurrentUser()?.restrictedSchedule?.getNextEndTime();
             return null == e
                 ? null
-                : new Intl.DateTimeFormat(h.intl.currentLocale, {
+                : new Intl.DateTimeFormat(p.intl.currentLocale, {
                       hour: "numeric",
                       minute: "2-digit",
                       weekday: "long",
                   }).format(e);
         }),
-        a = (0, c.bG)([f.default], () => f.default.getCurrentUser()?.username ?? "");
+        a = (0, c.bG)([h.default], () => h.default.getCurrentUser()?.username ?? ""),
+        T = r.useRef(!1),
+        S = r.useCallback(() => {
+            (T.current = !0), (0, f.pX)(g.BVt.DEFAULT_LOGGED_OUT), _.A.logout("restricted_hours");
+        }, []);
     r.useEffect(() => {
-        n || t();
+        n || T.current || t();
     }, [n, t]);
-    let _ = null != s ? h.intl.format(m.default.VfqJvY, { endTime: s }) : h.intl.string(m.default.abikhN);
+    let N = null != s ? p.intl.format(A.default.VfqJvY, { endTime: s }) : p.intl.string(A.default.abikhN);
     return (0, i.jsxs)("div", {
-        className: g.Tp,
+        className: I.Tp,
         children: [
-            (0, i.jsx)("div", { className: g.gh, "aria-hidden": !0 }),
-            (0, i.jsx)("div", { className: g.zX, "aria-hidden": !0, children: (0, i.jsx)("div", { className: g.cU }) }),
+            (0, i.jsx)("div", { className: I.gh, "aria-hidden": !0 }),
+            (0, i.jsx)("div", { className: I.zX, "aria-hidden": !0, children: (0, i.jsx)("div", { className: I.cU }) }),
             (0, i.jsxs)(l.B, {
                 direction: "vertical",
                 align: "center",
                 gap: 16,
-                className: `${g.kL} ${g.vx}`,
+                className: `${I.kL} ${I.vx}`,
                 children: [
                     (0, i.jsx)(o.w, {
                         artboard: "Teen Screen Time Illo",
                         stateMachine: "State Machine 1",
-                        className: g.jw,
+                        className: I.jw,
                     }),
                     (0, i.jsx)(u.E, {
                         variant: "text-lg/medium",
                         color: "text-overlay-light",
-                        className: g.h_,
-                        children: _,
+                        className: I.h_,
+                        children: N,
                     }),
                 ],
             }),
             (0, i.jsx)("div", {
-                className: `${g.qr} ${g.vx}`,
+                className: `${I.qr} ${I.vx}`,
                 children: (0, i.jsx)(u.E, {
                     variant: "text-sm/medium",
                     color: "text-subtle",
-                    children: h.intl.format(m.default.iqeKDz, {
+                    children: p.intl.format(A.default.iqeKDz, {
                         username: a,
-                        loginHook: (e, t) => (0, i.jsx)(d.Anchor, { onClick: A, children: e }, t),
+                        loginHook: (e, t) => (0, i.jsx)(d.Anchor, { onClick: S, children: e }, t),
                     }),
                 }),
             }),
         ],
     });
 }
-var T = n(191627);
-let S = !1;
-function N() {
-    S || ((S = !0), (0, s.B8)(() => (0, i.jsx)(I, { onClose: () => y() }), { layerKey: T.Uy, Layer: a.Ay }));
-}
+var S = n(191627);
+let N = !1;
 function y() {
-    (S = !1), (0, s.dF)(T.Uy);
+    N || ((N = !0), (0, s.B8)(() => (0, i.jsx)(T, { onClose: () => C() }), { layerKey: S.Uy, Layer: a.Ay }));
+}
+function C() {
+    (N = !1), (0, s.dF)(S.Uy);
 }
