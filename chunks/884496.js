55e50ac04b7083ed295820a0bb35a584e3911727
DiscrapperManager <admin@minerpl.xyz>
2026-05-14 13:47:39 +0200

    canaryRelease: 544057
    Main scripts: 164
    Lazy Loaded Scripts: 3681
    Chunks:
     * Main Files: 11879
     * Scripts: 8436
    Strings: 24892
    Routes: 818
    Experiments Count: 40
    Apex Experiments Count: 329

--- chunks/884496.js
+++ chunks/884496.js
@@ -1,18 +1,18 @@
 e.d(s, { B: () => h });
 var r = e(627968);
 e(64700);
-var t = e(503698),
-    l = e.n(t),
-    n = e(939249),
-    i = e(289873),
+var i = e(503698),
+    n = e.n(i),
+    t = e(939249),
+    l = e(289873),
     c = e(7689),
     o = e(789645),
-    p = e(985018),
-    d = e(374473);
+    d = e(375708),
+    p = e(374473);
 let h = (a) => {
-    let { hasContent: s, onClear: e, className: t, isLoading: h, size: u = "sm" } = a;
-    return (0, r.jsx)(n.D, {
-        className: l()(t, d.Qm, { [d.Wn]: s }),
+    let { hasContent: s, onClear: e, className: i, isLoading: h, size: u = "sm" } = a;
+    return (0, r.jsx)(t.D, {
+        className: n()(i, p.Qm, { [p.Wn]: s }),
         "data-size": u,
         onClick: (a) => {
             a.stopPropagation(), null != e && e(a);
@@ -23,20 +23,22 @@ let h = (a) => {
             a.preventDefault(), a.stopPropagation();
         },
         children: (0, r.jsx)("div", {
-            className: d.zc,
+            className: p.zc,
             children: h
-                ? (0, r.jsx)(i.y, { type: i.y.Type.SPINNING_CIRCLE, className: l()(d.Kk, d.RK) })
+                ? (0, r.jsx)(l.y, { type: l.y.Type.SPINNING_CIRCLE, className: n()(p.Kk, p.RK) })
                 : (0, r.jsxs)(r.Fragment, {
                       children: [
                           (0, r.jsx)(c.$, {
                               color: "currentColor",
-                              className: l()(d.Kk, { [d.RK]: !s }),
-                              "aria-label": p.intl.string(p.t["5h0QOP"]),
+                              className: n()(p.Kk, { [p.RK]: !s }),
+                              "aria-label": d.intl.string(d.t["5h0QOP"]),
+                              "aria-hidden": s,
                           }),
                           (0, r.jsx)(o.P, {
                               color: "currentColor",
-                              className: l()(d.IU, { [d.RK]: s }),
-                              "aria-label": p.intl.string(p.t.VkKicb),
+                              className: n()(p.IU, { [p.RK]: s }),
+                              "aria-label": d.intl.string(d.t.VkKicb),
+                              "aria-hidden": !s,
                           }),
                       ],
                   }),
