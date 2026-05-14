4373387f4b6706a037107b5ee7bcb80cd2c23d92
DiscrapperManager <admin@minerpl.xyz>
2026-05-14 14:07:42 +0200

    canaryRelease: 544117
    Main scripts: 164
    Lazy Loaded Scripts: 3681
    Chunks:
     * Main Files: 11879
     * Scripts: 8436
    Strings: 24901
    Routes: 818
    Experiments Count: 40
    Apex Experiments Count: 329

--- chunks/283012.js
+++ chunks/283012.js
@@ -1,8 +1,8 @@
-n.d(l, { A: () => U, v: () => y }), n(938796);
+n.d(l, { A: () => R, v: () => U }), n(938796);
 var s = n(627968),
-    a = n(64700),
-    t = n(503698),
-    r = n.n(t),
+    t = n(64700),
+    a = n(503698),
+    r = n.n(a),
     i = n(665260),
     c = n(17928),
     d = n(359778),
@@ -11,12 +11,12 @@ var s = n(627968),
 if (21552 == n.j) var m = n(276293);
 if (21552 == n.j) var x = n(534514);
 if (21552 == n.j) var h = n(622629);
-var j = n(47167),
-    A = n(713654),
-    g = n(734057),
+var g = n(47167),
+    j = n(713654),
+    A = n(734057),
     N = n(696451),
-    v = n(71393),
-    f = n(576705),
+    f = n(71393),
+    v = n(576705),
     I = n(44234),
     b = n(486020),
     p = n(225142),
@@ -24,23 +24,27 @@ var j = n(47167),
     E = n(65995),
     G = n(111487),
     M = n(652215),
-    _ = n(340837),
-    L = n(985018),
+    L = n(340837),
+    _ = n(375708),
     k = n(664019);
-function y(e) {
-    let { title: l, emojiId: n, emojiName: a, icon: t, completed: i, Icon: c, onClick: m, ...x } = e,
+function U(e) {
+    let { title: l, emojiId: n, emojiName: t, icon: a, completed: i, Icon: c, onClick: m, ...x } = e,
         h = "channel" === x.variant ? x.channelId : null,
-        j = "static" === x.variant ? x.subtitle : x.channelName,
-        A = null != h ? b.Ay.getNewMemberActionIconURL({ channelId: h, icon: t }) : null;
+        g = "static" === x.variant ? x.subtitle : x.channelName,
+        j = null != h ? b.Ay.getNewMemberActionIconURL({ channelId: h, icon: a }) : null;
     return (0, s.jsxs)(d.Z, {
         className: r()(k.XI, { [k.xU]: null != m }),
         onClick: m,
+        "aria-label": _.intl.formatToPlainString(_.t.FXFkPg, {
+            title: l,
+            status: i ? _.intl.string(_.t.g62IJl) : _.intl.string(_.t.DaELnU),
+        }),
         children: [
-            null != A
-                ? (0, s.jsx)("img", { src: A, className: k.Kk, width: 32, height: 32, alt: "", "aria-hidden": !0 })
+            null != j
+                ? (0, s.jsx)("img", { src: j, className: k.Kk, width: 32, height: 32, alt: "", "aria-hidden": !0 })
                 : (0, s.jsx)(G.A, {
                       emojiId: n,
-                      emojiName: a,
+                      emojiName: t,
                       size: G.g.LARGE,
                       defaultComponent: (0, s.jsx)("div", {
                           className: k.uM,
@@ -51,7 +55,7 @@ function y(e) {
                 className: k.$I,
                 children: [
                     (0, s.jsx)(u.E, { variant: "text-md/semibold", color: "text-strong", children: l }),
-                    null != j ? (0, s.jsx)(u.E, { variant: "text-xs/normal", color: "text-muted", children: j }) : null,
+                    null != g ? (0, s.jsx)(u.E, { variant: "text-xs/normal", color: "text-muted", children: g }) : null,
                 ],
             }),
             i
@@ -60,21 +64,21 @@ function y(e) {
         ],
     });
 }
-function R(e) {
+function y(e) {
     let { action: l, completed: n } = e,
-        { channelId: t, title: r, emoji: i, icon: d } = l,
+        { channelId: a, title: r, emoji: i, icon: d } = l,
         { id: u, name: o } = i ?? {},
-        x = (0, c.bG)([g.A], () => g.A.getChannel(t)),
-        h = (0, j.Ay)(x, !0),
-        N = (0, c.bG)([f.A], () => f.A.can(M.xBc.VIEW_CHANNEL, x)),
-        v = a.useMemo(() => {
+        x = (0, c.bG)([A.A], () => A.A.getChannel(a)),
+        h = (0, g.Ay)(x, !0),
+        N = (0, c.bG)([v.A], () => v.A.can(M.xBc.VIEW_CHANNEL, x)),
+        f = t.useMemo(() => {
             if (null != x) return () => (0, p.qo)(x.guild_id, x.id);
         }, [x]);
     if (null == x || !N) return null;
-    let I = (0, A.gU)(x) ?? m.N;
-    return (0, s.jsx)(y, {
+    let I = (0, j.gU)(x) ?? m.N;
+    return (0, s.jsx)(U, {
         variant: "channel",
-        channelId: t,
+        channelId: a,
         title: r,
         channelName: h,
         emojiId: u,
@@ -82,19 +86,19 @@ function R(e) {
         icon: d,
         completed: n,
         Icon: I,
-        onClick: v,
+        onClick: f,
     });
 }
-function U(e) {
+function R(e) {
     let { guildId: l } = e,
         n = (0, c.bG)([C.h], () => C.h.getNewMemberActions(l), [l]),
-        t = (0, c.bG)([E.A], () => E.A.getCompletedActions(l)),
+        a = (0, c.bG)([E.A], () => E.A.getCompletedActions(l)),
         o = (0, c.bG)([N.Ay], () => N.Ay.getSelfMember(l)),
-        m = (0, c.bG)([v.A], () => v.A.getGuild(l));
-    a.useEffect(() => {
-        null == t && o?.flags != null && (0, i.Lt)(o.flags ?? 0, _.D.STARTED_HOME_ACTIONS) && (0, p.aW)(l);
-    }, [t, l, o?.flags]);
-    let j = a.useCallback(() => {
+        m = (0, c.bG)([f.A], () => f.A.getGuild(l));
+    t.useEffect(() => {
+        null == a && o?.flags != null && (0, i.Lt)(o.flags ?? 0, L.D.STARTED_HOME_ACTIONS) && (0, p.aW)(l);
+    }, [a, l, o?.flags]);
+    let g = t.useCallback(() => {
         null != m && null != m.rulesChannelId && (0, p.qo)(m.id, m.rulesChannelId);
     }, [m]);
     return null == o || null == n || 0 === n.length
@@ -105,15 +109,15 @@ function U(e) {
                   (0, s.jsx)(x.D, {
                       variant: "heading-lg/bold",
                       color: "text-strong",
-                      children: L.intl.string(L.t.LhlgY9),
+                      children: _.intl.string(_.t.LhlgY9),
                   }),
                   n.map((e) =>
-                      (0, s.jsx)(R, { action: e, completed: t?.[e.channelId] === !0, guildId: l }, e.channelId),
+                      (0, s.jsx)(y, { action: e, completed: a?.[e.channelId] === !0, guildId: l }, e.channelId),
                   ),
                   m?.rulesChannelId != null &&
                       (0, s.jsxs)(d.Z, {
                           className: r()(k.XI, k.xU),
-                          onClick: j,
+                          onClick: g,
                           children: [
                               (0, s.jsx)("div", {
                                   className: k.KK,
@@ -124,7 +128,7 @@ function U(e) {
                                   children: (0, s.jsx)(u.E, {
                                       variant: "text-md/semibold",
                                       color: "text-strong",
-                                      children: L.intl.string(L.t["K/i3iQ"]),
+                                      children: _.intl.string(_.t["K/i3iQ"]),
                                   }),
                               }),
                           ],
