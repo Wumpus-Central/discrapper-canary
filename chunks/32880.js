56c89dbd391f97aae77c9d269aec43ec83e9583a
DiscrapperManager <admin@minerpl.xyz>
2026-05-16 01:21:18 +0200

    canaryRelease: 545669
    Main scripts: 160
    Lazy Loaded Scripts: 3676
    Chunks:
     * Main Files: 11773
     * Scripts: 8527
    Strings: 24941
    Routes: 819
    Experiments Count: 40
    Apex Experiments Count: 328

--- chunks/32880.js
+++ chunks/32880.js
@@ -1,32 +1,33 @@
-t.d(a, { s: () => r });
-var h = t(627968);
-t(64700);
-var i = t(661531),
-    l = t(996682),
-    e = t(27989);
-let r = (s) => {
+"use strict";
+n.d(t, { s: () => o });
+var i = n(627968);
+n(64700);
+var r = n(661531),
+    s = n(996682),
+    a = n(27989);
+let o = (e) => {
     let {
-            size: a = "md",
-            width: t,
-            height: r,
-            color: d = i.A.colors.INTERACTIVE_ICON_DEFAULT,
-            colorClass: p = "",
+            size: t = "md",
+            width: n,
+            height: o,
+            color: l = r.A.colors.INTERACTIVE_ICON_DEFAULT,
+            colorClass: u = "",
             ...c
-        } = s,
-        w = (0, e.J)(a),
-        n = w?.width ?? t,
-        o = w?.height ?? r;
-    return (0, h.jsx)("svg", {
-        ...(0, l.A)(c),
+        } = e,
+        d = (0, a.J)(t),
+        _ = d?.width ?? n,
+        f = d?.height ?? o;
+    return (0, i.jsx)("svg", {
+        ...(0, s.A)(c),
         xmlns: "http://www.w3.org/2000/svg",
-        width: n,
-        height: o,
+        width: _,
+        height: f,
         fill: "none",
         viewBox: "0 0 24 24",
-        children: (0, h.jsx)("path", {
-            fill: "string" == typeof d ? d : d.css,
+        children: (0, i.jsx)("path", {
+            fill: "string" == typeof l ? l : l.css,
             d: "M12 2a1 1 0 0 1 1 1v10.59l3.3-3.3a1 1 0 1 1 1.4 1.42l-5 5a1 1 0 0 1-1.4 0l-5-5a1 1 0 1 1 1.4-1.42l3.3 3.3V3a1 1 0 0 1 1-1ZM3 20a1 1 0 1 0 0 2h18a1 1 0 1 0 0-2H3Z",
-            className: p,
+            className: u,
         }),
     });
 };
--- chunks/34880.js
+++ chunks/34880.js
@@ -1,1 +1,2 @@
-e.exports = s.p + "b25d99901c2d06a2.wasm";
+"use strict";
+e.exports = n.p + "b25d99901c2d06a2.wasm";
