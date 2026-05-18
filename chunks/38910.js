1809c7f2e83db25600dded53b344cd16dc562b3b
DiscrapperManager <admin@minerpl.xyz>
2026-05-19 00:03:19 +0200

    canaryRelease: 546367
    Main scripts: 151
    Lazy Loaded Scripts: 3699
    Chunks:
     * Main Files: 11733
     * Scripts: 8632
    Strings: 24995
    Routes: 819
    Experiments Count: 39
    Apex Experiments Count: 328

--- chunks/38910.js
+++ chunks/38910.js
@@ -1,18 +1,1 @@
-"use strict";
-n.d(t, { A: () => a });
-var i = n(636537),
-    r = n(228366),
-    s = n(652215);
-let a = {
-    async fetch() {
-        try {
-            let e = await i.Bo.get({ url: s.Rsh.FRIEND_SUGGESTIONS, rejectWithError: !0 });
-            r.h.dispatch({ type: "LOAD_FRIEND_SUGGESTIONS_SUCCESS", suggestions: e.body });
-        } catch (e) {
-            r.h.dispatch({ type: "LOAD_FRIEND_SUGGESTIONS_FAILURE" });
-        }
-    },
-    ignore(e) {
-        i.Bo.del({ url: s.Rsh.FRIEND_SUGGESTION(e), rejectWithError: !0 });
-    },
-};
+n.d(t,{A:()=>r});var i=n(636537),l=n(228366),s=n(652215);let r={async fetch(){try{let e=await i.Bo.get({url:s.Rsh.FRIEND_SUGGESTIONS,rejectWithError:!0});l.h.dispatch({type:"LOAD_FRIEND_SUGGESTIONS_SUCCESS",suggestions:e.body})}catch(e){l.h.dispatch({type:"LOAD_FRIEND_SUGGESTIONS_FAILURE"})}},ignore(e){i.Bo.del({url:s.Rsh.FRIEND_SUGGESTION(e),rejectWithError:!0})}}hError:!0})}}
