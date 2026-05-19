d8dd6973af20d26721fe4d8b9b5c19523893d4b0
DiscrapperManager <admin@minerpl.xyz>
2026-05-19 21:34:43 +0200

    canaryRelease: 546936
    Main scripts: 163
    Lazy Loaded Scripts: 3729
    Chunks:
     * Main Files: 11769
     * Scripts: 8610
    Strings: 25014
    Routes: 819
    Experiments Count: 39
    Apex Experiments Count: 330

--- chunks/192444.js
+++ chunks/192444.js
@@ -1,20 +1,19 @@
 "use strict";
 n.d(t, {
-    n6: () => N,
-    Iq: () => _,
-    Ig: () => g,
-    jS: () => f,
-    Ym: () => h,
-    rI: () => A,
-    m8: () => E,
-    A8: () => C,
-    OO: () => p,
-    r4: () => I,
-    ev: () => y,
-    aD: () => v,
-    uK: () => d,
-    lk: () => S,
-    sy: () => m,
+    m8: () => h,
+    OO: () => f,
+    n6: () => I,
+    Iq: () => c,
+    Ig: () => E,
+    jS: () => d,
+    ev: () => T,
+    Ym: () => _,
+    aD: () => N,
+    rI: () => m,
+    uK: () => u,
+    lk: () => A,
+    sy: () => p,
+    A8: () => S,
 });
 var i,
     r,
@@ -22,129 +21,114 @@ var i,
         (((i = {})[(i.PREMIUM_TIER_2_MULTIPLIER_PERCENTAGE_POINTS = 120)] =
             "PREMIUM_TIER_2_MULTIPLIER_PERCENTAGE_POINTS"),
         i),
-    a = n(17928),
-    o = n(945810),
-    l = n(287809),
-    u = n(646917),
-    c = n(576761);
+    a = n(945810),
+    o = n(646917),
+    l = n(576761);
 n(190107);
-let d = (0, o.mj)({
+let u = (0, a.mj)({
         name: "2025-11-video-end-card-v2",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 1: { enabled: !0 } },
     }),
-    _ = (0, o.mj)({
+    c = (0, a.mj)({
         name: "2026-02-fetch-quest-home-takeover-on-connection-open",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
     }),
-    f = (0, o.mj)({
+    d = (0, a.mj)({
         name: "2026-02-new-video-quest-progress-bar-styling",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
     }),
-    h = (0, o.mj)({
+    _ = (0, a.mj)({
         name: "2026-03-desktop-activity-quest-header",
         kind: "user",
         defaultConfig: { enabled: !1, showSkipButton: !1 },
         variations: { 1: { enabled: !0, showSkipButton: !0 }, 2: { enabled: !0, showSkipButton: !1 } },
     }),
-    p = (0, o.mj)({
+    f = (0, a.mj)({
         name: "2026-05-app-store-overlay-feature-gate",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 1: { enabled: !0 } },
     }),
-    E = (0, o.mj)({
+    h = (0, a.mj)({
         name: "2026-03-quest-reload-video-on-obscured",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 1: { enabled: !0 } },
     });
-var m =
+var p =
     (((r = {})[(r.DEFAULT = 0)] = "DEFAULT"),
     (r[(r.AUTO_ENABLE_CAPTIONS = 1)] = "AUTO_ENABLE_CAPTIONS"),
     (r[(r.AUTO_UNMUTE = 2)] = "AUTO_UNMUTE"),
     r);
-let g = (0, o.mj)({
+let E = (0, a.mj)({
         name: "2026-03-muted-video-quest-new-defaults",
         kind: "user",
         defaultConfig: { enabled: !1, variant: 0 },
         variations: { 0: { enabled: !1, variant: 0 }, 1: { enabled: !0, variant: 1 }, 2: { enabled: !0, variant: 2 } },
     }),
-    A = (0, o.mj)({
+    m = (0, a.mj)({
         name: "2026-03-get-decision-hero-ads",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 1: { enabled: !0 } },
     }),
-    I = (0, o.mj)({
-        name: "2025-03-quests-premium-rewards-holdout",
-        kind: "user",
-        defaultConfig: { enabled: !1 },
-        variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
-    }),
-    T = (0, o.mj)({
+    g = (0, a.mj)({
         name: "2026-04-quests-premium-orb-multiplier-marketing",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
     });
-function S(e) {
-    let { enabled: t } = T.useConfig({ location: e }),
-        n = (function (e) {
-            let { location: t } = e,
-                n = (0, a.bG)([l.default], () => l.default.getCurrentUser()),
-                i = null != n && n.isStaff(),
-                r = I.useConfig({ location: t });
-            return !i && !1 === r.enabled;
-        })({ location: e }),
-        i = (0, u.z)();
+function A(e) {
+    let { enabled: t } = g.useConfig({ location: e }),
+        n = (0, o.z)();
     return {
-        shouldShowBonusOrbsUX: i !== c.MA.INELIGIBLE && i !== c.MA.CREPE && !n && t,
+        shouldShowBonusOrbsUX: n !== l.MA.INELIGIBLE && n !== l.MA.CREPE && t,
         multiplier: s.PREMIUM_TIER_2_MULTIPLIER_PERCENTAGE_POINTS / 100,
     };
 }
-(0, o.mj)({
+(0, a.mj)({
     name: "2026-03-video-quest-player-refactor",
     kind: "user",
     defaultConfig: { enabled: !1 },
     variations: { 1: { enabled: !0 } },
 });
-let N = (0, o.mj)({
+let I = (0, a.mj)({
     name: "2026-04-composed-quest-player",
     kind: "user",
     defaultConfig: { enabled: !1 },
     variations: { 1: { enabled: !0 } },
 });
-(0, o.mj)({
+(0, a.mj)({
     name: "2026-04-mobile-quest-orb-reward-modal-cta-priority",
     kind: "user",
     defaultConfig: { enabled: !1, variant: 0 },
     variations: { 0: { enabled: !1, variant: 0 }, 1: { enabled: !0, variant: 1 }, 2: { enabled: !0, variant: 2 } },
 }),
-    (0, o.mj)({
+    (0, a.mj)({
         name: "2026-03-mobile-quest-home-red-dot-notification",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
     });
-let y = (0, o.mj)({
+let T = (0, a.mj)({
         name: "2026-04-quest-heartbeat-executable-fingerprint",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
     }),
-    C = (0, o.mj)({
+    S = (0, a.mj)({
         name: "2026-05-quest-home-new-layout",
         kind: "user",
         defaultConfig: { enabled: !1 },
         variations: { 0: { enabled: !1 }, 1: { enabled: !0 } },
     }),
-    v = (0, o.mj)({
+    N = (0, a.mj)({
         name: "2026-05-quest-home-tile-redesign",
         kind: "user",
         defaultConfig: { enabled: !1 },
--- chunks/885973.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-n.d(t, { C: () => s });
-var r = n(245853),
-    i = n(287809);
-let s = (e) => {
-    let t = i.default.getCurrentUser();
-    return !(null != t && t.isStaff()) && !r.r4.getConfig({ location: e.location }).enabled;
-};
