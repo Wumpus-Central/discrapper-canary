fddd23ae210ac48830f1d2d71f3944b36ece6955
DiscrapperManager <admin@minerpl.xyz>
2026-05-20 20:31:08 +0200

    canaryRelease: 547699
    Main scripts: 150
    Lazy Loaded Scripts: 3758
    Chunks:
     * Main Files: 11773
     * Scripts: 8637
    Strings: 25044
    Routes: 819
    Experiments Count: 39
    Apex Experiments Count: 328

--- chunks/876587.js
+++ chunks/876587.js
@@ -1,28 +1,28 @@
 s.d(t, { O: () => g });
-var a = s(627968);
+var i = s(627968);
 s(64700);
-var r = s(554146),
-    i = s(43105),
-    n = s(691540),
-    l = s(857250),
-    o = s(97483),
-    c = s(765178),
-    d = s(932001),
-    m = s(808247),
-    u = s(788868),
-    _ = s(49999),
-    x = s(985018),
-    p = s(201682);
+var l = s(554146),
+    n = s(43105),
+    a = s(691540),
+    r = s(857250),
+    c = s(97483),
+    o = s(765178),
+    d = s(131607),
+    u = s(808247),
+    m = s(788868),
+    x = s(49999),
+    p = s(375708),
+    h = s(201682);
 let g = (e) => {
-    let { targetElementRef: t } = e,
-        [s, g] = (0, d.kn)([r.M.PREMIUM_WISHLIST_COACHMARK]);
-    return s !== r.M.PREMIUM_WISHLIST_COACHMARK
+    let { targetElementRef: t, body: s = p.intl.string(p.t.IqPvOn) } = e,
+        [g, j] = (0, d.kn)([l.M.PREMIUM_WISHLIST_COACHMARK]);
+    return g !== l.M.PREMIUM_WISHLIST_COACHMARK
         ? null
-        : (0, a.jsx)(i.A, {
+        : (0, i.jsx)(n.A, {
               targetElementRef: t,
-              title: x.intl.string(x.t["1YLUuN"]),
-              body: x.intl.string(x.t.IqPvOn),
-              graphic: { type: "image", src: p.A },
+              title: p.intl.string(p.t["1YLUuN"]),
+              body: s,
+              graphic: { type: "image", src: h.A },
               size: "video",
               alignmentStrategy: "edge",
               align: "right",
@@ -30,17 +30,17 @@ let g = (e) => {
               caretConfig: { align: "end" },
               actions: [
                   {
-                      text: x.intl.string(x.t["8DkMEQ"]),
+                      text: p.intl.string(p.t["8DkMEQ"]),
                       onClick: async () => {
                           try {
-                              await m.A.addSkuToWishlist(u.pe.TIER_2), g(_.i.TAKE_ACTION);
+                              await u.A.addSkuToWishlist(m.pe.TIER_2), j(x.i.TAKE_ACTION);
                           } catch {
-                              (0, n.P0)((0, l.o)(x.intl.string(x.t.F8FvUy), o.Ck.FAILURE)),
-                                  c.O.announce(x.intl.string(x.t.F8FvUy));
+                              (0, a.P0)((0, r.o)(p.intl.string(p.t.F8FvUy), c.Ck.FAILURE)),
+                                  o.O.announce(p.intl.string(p.t.F8FvUy));
                           }
                       },
                   },
               ],
-              onRequestClose: () => g(_.i.USER_DISMISS),
+              onRequestClose: () => j(x.i.USER_DISMISS),
           });
 };
