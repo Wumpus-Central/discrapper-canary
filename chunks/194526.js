8dee0c102a214dc7bf4883ec75f28d655cd46c7e
DiscrapperManager <admin@minerpl.xyz>
2026-05-22 01:48:04 +0200

    canaryRelease: 549044
    Main scripts: 166
    Lazy Loaded Scripts: 3783
    Chunks:
     * Main Files: 11720
     * Scripts: 8755
    Strings: 25098
    Routes: 818
    Experiments Count: 39
    Apex Experiments Count: 326

--- chunks/194526.js
+++ chunks/194526.js
@@ -1,94 +1,99 @@
 "use strict";
-n.d(t, { I: () => g });
+n.d(t, { I: () => A }), n(323874), n(14289), n(35956);
 var i = n(64700),
-    r = n(323889),
-    s = n(17928),
-    a = n(228366),
-    o = n(859703),
-    l = n(710969);
-let u = new Map();
-function c(e) {
-    let t = u.get(e);
-    return null == t && ((t = new Set()), u.set(e, t)), t;
+    r = n(873263),
+    s = n(323889),
+    a = n(17928),
+    o = n(228366),
+    l = n(859703),
+    u = n(710969);
+let c = new Map();
+function d(e) {
+    let t = c.get(e);
+    return null == t && ((t = new Set()), c.set(e, t)), t;
 }
-function d() {
-    let e = o.A.quests,
-        t = c(r.p.QUEST),
+function _() {
+    let e = l.A.quests,
+        t = d(s.p.QUEST),
         n = !1;
     for (let [r, s] of e) {
         var i;
         t.has(r) ||
-            (0, l.Ic)(s) ||
+            (0, u.Ic)(s) ||
             null == (i = s.userStatus) ||
             (null == i.enrolledAt && null == i.completedAt && null == i.claimedAt && 0 === i.dismissedQuestContent) ||
             (t.add(r), (n = !0));
     }
-    if (0 !== o.A.lastFetchedCurrentQuests && e.size > 0)
+    if (0 !== l.A.lastFetchedCurrentQuests && e.size > 0)
         for (let i of t) {
             let r = e.get(i);
-            (null == r || (0, l.Ic)(r)) && (t.delete(i), (n = !0));
+            (null == r || (0, u.Ic)(r)) && (t.delete(i), (n = !0));
         }
-    let s = c(r.p.QUEST_HOME_HERO);
-    if (null != o.A.getLastFetchedQuestHomeHero() && s.size > 0) {
-        let e = o.A.getQuestHomeHero();
-        for (let t of s) (null == e || t !== e.id) && (s.delete(t), (n = !0));
+    let r = d(s.p.QUEST_HOME_HERO);
+    if (null != l.A.getLastFetchedQuestHomeHero() && r.size > 0) {
+        let e = l.A.getQuestHomeHero();
+        for (let t of r) (null == e || t !== e.id) && (r.delete(t), (n = !0));
     }
     return n;
 }
-class _ extends s.Ay.PersistedStore {
+class f extends a.Ay.PersistedStore {
     static displayName = "AdContentSeenStore";
     static persistKey = "AdContentSeenStore";
     initialize(e) {
-        if ((this.waitFor(o.A), (u = new Map()), null != e))
-            for (let [t, n] of Object.entries(e.seenContentIds)) u.set(Number(t), new Set(n));
-        this.syncWith([o.A], d);
+        if ((this.waitFor(l.A), (c = new Map()), null != e))
+            for (let [t, n] of Object.entries(e.seenContentIds)) c.set(Number(t), new Set(n));
+        this.syncWith([l.A], _);
     }
     getState() {
         let e = {};
-        for (let [t, n] of u) e[t] = Array.from(n);
+        for (let [t, n] of c) e[t] = Array.from(n);
         return { seenContentIds: e };
     }
     hasSeen(e, t) {
-        return u.get(e)?.has(t) ?? !1;
+        return c.get(e)?.has(t) ?? !1;
     }
 }
-let f = new _(a.h, {
+let h = new f(o.h, {
     AD_CONTENT_MARK_SEEN: function (e) {
-        let t = c(e.adCreativeType),
+        let t = d(e.adCreativeType),
             n = !1;
         for (let i of e.contentIds) t.has(i) || (t.add(i), (n = !0));
         return n;
     },
     AD_CONTENT_MARK_UNSEEN: function (e) {
-        let t = u.get(e.adCreativeType);
+        let t = c.get(e.adCreativeType);
         if (null == t) return !1;
         let n = !1;
         for (let i of e.contentIds) t.has(i) && (t.delete(i), (n = !0));
         return n;
     },
 });
-var h = n(340124),
-    p = n(31587);
-function E(e) {
+var p = n(340124),
+    E = n(31587);
+function m(e) {
     return null != e && "" !== e;
 }
-function m(e) {
-    return E(e?.start) && E(e?.end);
+function g(e) {
+    return m(e?.start) && m(e?.end);
 }
-function g() {
-    let e = (0, p.T2)(),
+function A() {
+    let e = (0, E.T2)(),
         t = e?.id,
-        n = (0, s.bG)([f], () => null != t && f.hasSeen(r.p.QUEST_HOME_HERO, t), [t]);
+        { search: n } = (0, r.zy)(),
+        o = i.useMemo(() => new URLSearchParams(n).get(E.L1.AD_CREATIVE_IDS), [n]),
+        l = i.useMemo(() => null != o && null != t && o.includes(t), [o, t]),
+        u = (0, a.bG)([h], () => null != t && h.hasSeen(s.p.QUEST_HOME_HERO, t), [t]);
     return {
         shouldShowQuestHomeHeroContent:
             (function (e) {
                 if (null == e) return !1;
-                let t = E(e.tooltipTitle) && E(e.tooltipSubtitle);
-                return E(e.image) || m(e.linearGradient) || m(e.radialGradient) || t;
-            })(e?.questHomeEntrypoint) && !n,
+                let t = m(e.tooltipTitle) && m(e.tooltipSubtitle);
+                return m(e.image) || g(e.linearGradient) || g(e.radialGradient) || t;
+            })(e?.questHomeEntrypoint) &&
+            (l || !u),
         dismissQuestHomeHeroContent: i.useCallback(() => {
-            null != t && (0, h.zh)(r.p.QUEST_HOME_HERO, [t]);
-        }, [t]),
+            l || (null != t && (0, p.zh)(s.p.QUEST_HOME_HERO, [t]));
+        }, [t, l]),
         questHomeHero: e,
     };
 }
