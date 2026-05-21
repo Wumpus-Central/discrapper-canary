5acb01b894bc4046d2a0905fe2844cdea945f53f
DiscrapperManager <admin@minerpl.xyz>
2026-05-22 00:02:26 +0200

    canaryRelease: 548894
    Main scripts: 164
    Lazy Loaded Scripts: 3782
    Chunks:
     * Main Files: 11720
     * Scripts: 8752
    Strings: 25089
    Routes: 818
    Experiments Count: 39
    Apex Experiments Count: 326

--- chunks/441574.js
+++ chunks/441574.js
@@ -1944,11 +1944,10 @@ class eT extends S.G {
         super("discord_protos.users.v1.AprilFools2026Badge", [
             { no: 1, name: "common", kind: "message", T: () => eI },
             { no: 2, name: "level", kind: "scalar", T: 5 },
-            { no: 3, name: "combat_class", kind: "scalar", T: 9 },
         ]);
     }
     create(e) {
-        let t = { level: 0, combatClass: "" };
+        let t = { level: 0 };
         return (
             globalThis.Object.defineProperty(t, T.$, { enumerable: !1, value: this }),
             void 0 !== e && (0, I.x)(this, t, e),
@@ -1967,9 +1966,6 @@ class eT extends S.G {
                 case 2:
                     r.level = e.int32();
                     break;
-                case 3:
-                    r.combatClass = e.string();
-                    break;
                 default:
                     let s = n.readUnknownField;
                     if ("throw" === s)
@@ -1982,8 +1978,7 @@ class eT extends S.G {
     }
     internalBinaryWrite(e, t, n) {
         e.common && eI.internalBinaryWrite(e.common, t.tag(1, A.O0.LengthDelimited).fork(), n).join(),
-            0 !== e.level && t.tag(2, A.O0.Varint).int32(e.level),
-            "" !== e.combatClass && t.tag(3, A.O0.LengthDelimited).string(e.combatClass);
+            0 !== e.level && t.tag(2, A.O0.Varint).int32(e.level);
         let i = n.writeUnknownFields;
         return !1 !== i && (!0 == i ? A.f$.onWrite : i)(this.typeName, e, t), t;
     }
