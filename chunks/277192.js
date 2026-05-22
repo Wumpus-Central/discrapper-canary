20243bf8ef9f5eff538d06362613f2d92a3d6852
DiscrapperManager <admin@minerpl.xyz>
2026-05-22 19:05:15 +0200

    canaryRelease: 549424
    Main scripts: 162
    Lazy Loaded Scripts: 3760
    Chunks:
     * Main Files: 11828
     * Scripts: 8638
    Strings: 25079
    Routes: 818
    Experiments Count: 39
    Apex Experiments Count: 333

--- chunks/277192.js
+++ chunks/277192.js
@@ -623,10 +623,10 @@ function e6(e) {
     e3.A.open(e, eR.BEX.SOUNDBOARD),
         (0, eF.openModalLazy)(async () => {
             let { default: t } = await Promise.all([
-                n.e("54827"),
-                n.e("66655"),
+                n.e("53680"),
+                n.e("50671"),
                 n.e("60909"),
-                n.e("23484"),
+                n.e("60193"),
                 n.e("76846"),
                 n.e("42888"),
                 n.e("8555"),
@@ -636,9 +636,9 @@ function e6(e) {
                 n.e("8979"),
                 n.e("20643"),
                 n.e("80559"),
+                n.e("69006"),
                 n.e("98913"),
                 n.e("74049"),
-                n.e("69006"),
                 n.e("12811"),
             ]).then(n.bind(n, 191110));
             return (n) => (0, i.jsx)(t, { ...n, guildId: e });
@@ -738,6 +738,7 @@ function ti(e) {
                 children: (0, i.jsx)(T.D, {
                     className: e9.bV,
                     onClick: r,
+                    onKeyDown: (e) => e.stopPropagation(),
                     "aria-expanded": !n,
                     children: (0, i.jsxs)("div", {
                         className: e9.M2,
