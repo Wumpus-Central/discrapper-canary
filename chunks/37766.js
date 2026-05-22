7dad6d09aaddd6bc288f5af182cd73214e942258
DiscrapperManager <admin@minerpl.xyz>
2026-05-22 18:43:25 +0200

    canaryRelease: 549396
    Main scripts: 162
    Lazy Loaded Scripts: 3760
    Chunks:
     * Main Files: 11828
     * Scripts: 8638
    Strings: 25079
    Routes: 818
    Experiments Count: 39
    Apex Experiments Count: 332

--- chunks/37766.js
+++ chunks/37766.js
@@ -1,19 +1,18 @@
-"use strict";
-n.d(t, { _: () => a });
-var i = n(627968);
-n(64700);
-var r = n(996682),
-    s = n(27989);
-let a = (e) => {
-    let { size: t = "md", width: n, height: a, ...o } = e,
-        l = (0, s.J)(t),
-        u = l?.width ?? n,
-        c = l?.height ?? a;
+l.d(t, { _: () => n });
+var i = l(627968);
+l(64700);
+var r = l(996682),
+    c = l(27989);
+let n = (e) => {
+    let { size: t = "md", width: l, height: n, ...a } = e,
+        s = (0, c.J)(t),
+        h = s?.width ?? l,
+        d = s?.height ?? n;
     return (0, i.jsxs)("svg", {
-        ...(0, r.A)(o),
+        ...(0, r.A)(a),
         xmlns: "http://www.w3.org/2000/svg",
-        width: u,
-        height: c,
+        width: h,
+        height: d,
         fill: "none",
         viewBox: "0 0 24 24",
         children: [
