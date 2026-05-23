ebf79d126d17590239bce1cd13dfcab82bc0bc7b
DiscrapperManager <admin@minerpl.xyz>
2026-05-23 03:28:17 +0200

    canaryRelease: 549980
    Main scripts: 136
    Lazy Loaded Scripts: 3737
    Chunks:
     * Main Files: 11730
     * Scripts: 8783
    Strings: 25119
    Routes: 819
    Experiments Count: 39
    Apex Experiments Count: 335

--- chunks/872363.js
+++ chunks/872363.js
@@ -7,11 +7,11 @@ var s = a(627968),
     c = a(23339),
     o = a(17928),
     p = a(661531),
-    d = a(990078),
-    u = a(319060),
-    _ = a(192308),
-    m = a(403581),
-    E = a(939249),
+    u = a(990078),
+    _ = a(319060),
+    d = a(192308),
+    m = a(939249),
+    E = a(403581),
     A = a(777666),
     y = a(844222),
     I = a(866323),
@@ -20,110 +20,107 @@ var s = a(627968),
     T = a(688810),
     L = a(402216),
     O = a(287809),
-    h = a(174459),
-    S = a(428262),
+    S = a(174459),
+    h = a(428262),
     N = a(650338),
     f = a(821715),
     D = a(652215),
     U = a(788868),
     C = a(375708),
     b = a(18238);
-let j = (0, c.xI)(u.A.LIVE_INDICATOR_BORDER_RADIUS),
-    P = { opacity: 0, transform: "translate3d(100%, 0, 0)" },
-    k = { opacity: 1, transform: "translate3d(0%, 0, 0)" },
-    M = { opacity: 0 },
+let P = (0, c.xI)(_.A.LIVE_INDICATOR_BORDER_RADIUS),
+    k = { opacity: 0, transform: "translate3d(100%, 0, 0)" },
+    M = { opacity: 1, transform: "translate3d(0%, 0, 0)" },
+    j = { opacity: 0 },
     v = { opacity: 1 },
-    V = { borderRadius: `${j}px ${j}px ${j}px ${j}px` },
-    g = { borderRadius: `0px ${j}px ${j}px 0px` },
-    w = { mass: 1, tension: 500, friction: 18, clamp: !0 },
+    V = { borderRadius: `${P}px ${P}px ${P}px ${P}px` },
+    w = { borderRadius: `0px ${P}px ${P}px 0px` },
+    g = { mass: 1, tension: 500, friction: 18, clamp: !0 },
     H = (e) => {
         let t,
             r,
             {
                 participant: n,
                 isUpsellEnabled: c,
-                shape: u,
+                shape: _,
                 size: y,
                 didTrackUpsellViewed: I,
                 setDidTrackUpsellViewed: R,
-                className: j,
-                premiumIndicator: P,
-                quality: k,
+                className: P,
+                premiumIndicator: k,
+                quality: M,
             } = e,
-            { analyticsLocations: M } = (0, T.Ay)(),
+            { analyticsLocations: j } = (0, T.Ay)(),
             v = null != (0, f.A)(n);
         try {
-            t = (0, N.HB)(k);
+            t = (0, N.HB)(M);
         } catch (e) {
             t = !1;
         }
         try {
-            r = (0, N.OI)(k);
+            r = (0, N.OI)(M);
         } catch (e) {
             r = !1;
         }
         let V = t || r,
-            { location: g } = (0, x.p)(),
-            w = (0, o.bG)([O.default], () => O.default.getCurrentUser()),
-            H = c && !S.Ay.isPremium(w, U.PremiumTypes.TIER_1) && !S.Ay.canStreamQuality(S.Ay.StreamQuality.MID, w),
+            { location: w } = (0, x.p)(),
+            g = (0, o.bG)([O.default], () => O.default.getCurrentUser()),
+            H = c && !h.Ay.isPremium(g, U.PremiumTypes.TIER_1) && !h.Ay.canStreamQuality(h.Ay.StreamQuality.MID, g),
             G = l.useCallback(() => {
                 H &&
                     V &&
-                    (0, _.openModalLazy)(async () => {
+                    (0, d.openModalLazy)(async () => {
                         let { default: e } = await Promise.all([a.e("93475"), a.e("18630")]).then(a.bind(a, 826789));
-                        return (t) => (0, s.jsx)(e, { ...t, analyticsSource: g });
+                        return (t) => (0, s.jsx)(e, { ...t, analyticsSource: w });
                     });
-            }, [H, V, g]);
+            }, [H, V, w]);
         if (
             (l.useEffect(() => {
                 !I &&
                     V &&
-                    (h.default.track(D.HAw.PREMIUM_UPSELL_VIEWED, {
+                    (S.default.track(D.HAw.PREMIUM_UPSELL_VIEWED, {
                         type: U.e.STREAM_QUALITY_INDICATOR,
                         has_premium_stream_fps: t,
                         has_premium_stream_resolution: r,
-                        location_stack: M,
+                        location_stack: j,
                     }),
                     R(!0));
-            }, [t, r, V, I, R, M]),
-            null == k)
+            }, [t, r, V, I, R, j]),
+            null == M)
         )
             return null;
-        let W = H && V,
-            Y = i()(b.t5, y, L.u1[u], v ? b.Y5 : b.Lc, { [b.vk]: W }),
-            F = (0, s.jsxs)(s.Fragment, {
+        let W = (0, s.jsx)(u.m, {
+            text: v ? C.intl.string(C.t.q8TiVt) : V ? C.intl.string(C.t.IHgpEn) : C.intl.string(C.t.vLb0VW),
+            position: "bottom",
+            children: (0, s.jsxs)(m.D, {
+                onClick: G,
+                className: i()(b.t5, y, L.u1[_], v ? b.Y5 : b.Lc, { [b.vk]: H && V }),
                 children: [
-                    V ? (0, s.jsx)(m.t, { size: "md", color: "currentColor", className: b.fY }) : null,
-                    (0, s.jsx)("span", { className: b.UD, children: (0, N.ME)(k.maxResolution) }),
-                    (0, s.jsx)("span", { children: (0, N.Bs)(k.maxFrameRate) }),
+                    V ? (0, s.jsx)(E.t, { size: "md", color: "currentColor", className: b.fY }) : null,
+                    (0, s.jsx)("span", { className: b.UD, children: (0, N.ME)(M.maxResolution) }),
+                    (0, s.jsx)("span", { children: (0, N.Bs)(M.maxFrameRate) }),
                 ],
             }),
-            $ = (0, s.jsx)(d.m, {
-                text: v ? C.intl.string(C.t.q8TiVt) : V ? C.intl.string(C.t.IHgpEn) : C.intl.string(C.t.vLb0VW),
-                position: "bottom",
-                children: W
-                    ? (0, s.jsx)(E.D, { onClick: G, className: Y, children: F })
-                    : (0, s.jsx)("span", { className: Y, children: F }),
-            });
+        });
         return (0, s.jsx)(A.Lp, {
-            text: $,
-            className: i()(j, b.AA, { [b.Dc]: V && P }),
+            text: W,
+            className: i()(P, b.AA, { [b.Dc]: V && k }),
             color: p.A.unsafe_rawColors.PRIMARY_500.css,
-            shape: L.u1[u],
+            shape: L.u1[_],
         });
     },
     G = (e) => {
         let { participant: t, showQuality: a, isUpsellEnabled: r = !0, size: c, className: o, premiumIndicator: p } = e,
-            [d, u] = l.useState(!1),
-            _ = (0, N.N5)(t),
+            [u, _] = l.useState(!1),
+            d = (0, N.N5)(t),
             { reducedMotion: m } = l.useContext(y.C),
-            E = a && null != _,
+            E = a && null != d,
             x = (0, I.p)(
                 E,
-                { enter: { from: m.enabled ? M : P, to: m.enabled ? v : k }, leave: m.enabled ? M : P, config: w },
+                { enter: { from: m.enabled ? j : k, to: m.enabled ? v : M }, leave: m.enabled ? j : k, config: g },
                 "animate-always",
             ),
-            T = (0, R.z)({ to: E ? g : V, config: w }, "animate-always");
+            T = (0, R.z)({ to: E ? w : V, config: g }, "animate-always");
         return ((e) => {
             let { className: a, popoutProps: l } = e;
             return (0, s.jsxs)("div", {
@@ -140,10 +137,10 @@ let j = (0, c.xI)(u.A.LIVE_INDICATOR_BORDER_RADIUS),
                                       size: c,
                                       shape: A.EG.ROUND_LEFT,
                                       isUpsellEnabled: r,
-                                      didTrackUpsellViewed: d,
-                                      setDidTrackUpsellViewed: u,
+                                      didTrackUpsellViewed: u,
+                                      setDidTrackUpsellViewed: _,
                                       premiumIndicator: p,
-                                      quality: _,
+                                      quality: d,
                                   }),
                               })
                             : null,
