1380172e1242042e3e2b2f2f84767b45c6658385
DiscrapperManager <admin@minerpl.xyz>
2026-05-27 00:14:03 +0200

    canaryRelease: 550825
    Main scripts: 136
    Lazy Loaded Scripts: 3727
    Chunks:
     * Main Files: 11763
     * Scripts: 8747
    Strings: 25128
    Routes: 820
    Experiments Count: 38
    Apex Experiments Count: 345

--- chunks/6494.js
+++ chunks/6494.js
@@ -1,6 +1,6 @@
 "use strict";
-n.d(t, { A: () => r });
-let r = new (class {
+n.d(t, { A: () => i });
+let i = new (class {
     setActive(e) {
         return Promise.resolve();
     }
@@ -9,6 +9,7 @@ let r = new (class {
     setFocusedStream(e) {}
     setMirrored(e) {}
     setPipAspectRatio(e, t) {}
+    refreshPipUi() {}
     updateSourceTrackingView() {}
     addOnPipModeChangedListener(e) {}
     addOnPipModeWillChangeListener(e) {}
