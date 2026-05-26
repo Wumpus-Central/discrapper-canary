67708c1e251bd1229c63a6a734782f518880c711
DiscrapperManager <admin@minerpl.xyz>
2026-05-26 20:27:38 +0200

    canaryRelease: 550516
    Main scripts: 136
    Lazy Loaded Scripts: 3739
    Chunks:
     * Main Files: 11735
     * Scripts: 8642
    Strings: 25120
    Routes: 819
    Experiments Count: 38
    Apex Experiments Count: 339

--- chunks/423562.js
+++ chunks/423562.js
@@ -1,21 +1,19 @@
 "use strict";
-n.d(t, { A: () => h });
-var l = n(627968);
+n.d(t, { A: () => _ });
+var i = n(627968);
 n(64700);
-var i = n(17928),
+var r = n(17928),
     s = n(86147),
     a = n(729475),
-    r = n(869146),
-    o = n(531685),
-    c = n(475815),
-    u = n(709562),
-    d = n(985018);
-let h = i.Ay.connectStores([o.A, r.A], (e) => {
-    let { node: t, guestWindow: n } = e;
-    return { enabled: (0, c._U)(t, n?.document) };
-})(function (e) {
-    let { enabled: t, ...n } = e,
-        i = t ? d.intl.string(d.t.Z7MyNB) : d.intl.string(d.t.OIDkcp),
-        r = t ? s.z : a.T;
-    return (0, l.jsx)(u.A, { label: i, iconComponent: r, ...n });
-});
+    o = n(869146),
+    l = n(531685),
+    u = n(475815),
+    c = n(204651),
+    d = n(375708);
+function _(e) {
+    let { node: t, guestWindow: n, ..._ } = e,
+        f = (0, r.bG)([l.A, o.A], () => (0, u._U)(t, n?.document), [t, n]),
+        h = f ? d.intl.string(d.t.Z7MyNB) : d.intl.string(d.t.OIDkcp),
+        p = f ? s.z : a.T;
+    return (0, i.jsx)(c.A, { label: h, iconComponent: p, ..._ });
+}
