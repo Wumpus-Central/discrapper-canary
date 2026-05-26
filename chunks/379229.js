4eedcb201fff90314bab113e92997f28f43055f9
DiscrapperManager <admin@minerpl.xyz>
2026-05-26 22:34:26 +0200

    canaryRelease: 550682
    Main scripts: 136
    Lazy Loaded Scripts: 3739
    Chunks:
     * Main Files: 11735
     * Scripts: 8642
    Strings: 25123
    Routes: 819
    Experiments Count: 38
    Apex Experiments Count: 341

--- chunks/379229.js
+++ chunks/379229.js
@@ -1,20 +1,20 @@
-n.d(t, { bW: () => l, cD: () => o, lE: () => i, mD: () => s, o: () => c });
-let i = 216e5,
-    l = 864e6,
-    s = 2592e5;
-var a,
-    r,
-    o = (((a = {}).WARNING = "warning"), (a.UNREAD = "unread"), a),
-    c =
-        (((r = {})[(r.LEVEL_REACHED = 0)] = "LEVEL_REACHED"),
-        (r[(r.PERKS_PURCHASABLE = 1)] = "PERKS_PURCHASABLE"),
-        (r[(r.PERKS_AVAILABLE = 2)] = "PERKS_AVAILABLE"),
-        (r[(r.NEW_PERK_AVAILABLE = 3)] = "NEW_PERK_AVAILABLE"),
-        (r[(r.BOOST_TO_UNLOCK = 4)] = "BOOST_TO_UNLOCK"),
-        (r[(r.EXPIRING_PERK = 5)] = "EXPIRING_PERK"),
-        (r[(r.GAME_SERVER_HOSTING_AVAILABLE = 6)] = "GAME_SERVER_HOSTING_AVAILABLE"),
-        (r[(r.GAME_SERVER_HOSTING_GUILD_ELIGIBLE = 7)] = "GAME_SERVER_HOSTING_GUILD_ELIGIBLE"),
-        (r[(r.GAME_SERVER_HOSTING_ENABLED = 8)] = "GAME_SERVER_HOSTING_ENABLED"),
-        (r[(r.GAME_SERVER_NEW_GAMES = 9)] = "GAME_SERVER_NEW_GAMES"),
-        (r[(r.GAME_SERVER_PRICING_CHANGE = 10)] = "GAME_SERVER_PRICING_CHANGE"),
-        r);
+n.d(t, { bW: () => l, cD: () => s, lE: () => u, mD: () => r, o: () => E });
+let u = 216e5,
+    l = 2592e6,
+    r = 2592e5;
+var i,
+    o,
+    s = (((i = {}).WARNING = "warning"), (i.UNREAD = "unread"), i),
+    E =
+        (((o = {})[(o.LEVEL_REACHED = 0)] = "LEVEL_REACHED"),
+        (o[(o.PERKS_PURCHASABLE = 1)] = "PERKS_PURCHASABLE"),
+        (o[(o.PERKS_AVAILABLE = 2)] = "PERKS_AVAILABLE"),
+        (o[(o.NEW_PERK_AVAILABLE = 3)] = "NEW_PERK_AVAILABLE"),
+        (o[(o.BOOST_TO_UNLOCK = 4)] = "BOOST_TO_UNLOCK"),
+        (o[(o.EXPIRING_PERK = 5)] = "EXPIRING_PERK"),
+        (o[(o.GAME_SERVER_HOSTING_AVAILABLE = 6)] = "GAME_SERVER_HOSTING_AVAILABLE"),
+        (o[(o.GAME_SERVER_HOSTING_GUILD_ELIGIBLE = 7)] = "GAME_SERVER_HOSTING_GUILD_ELIGIBLE"),
+        (o[(o.GAME_SERVER_HOSTING_ENABLED = 8)] = "GAME_SERVER_HOSTING_ENABLED"),
+        (o[(o.GAME_SERVER_NEW_GAMES = 9)] = "GAME_SERVER_NEW_GAMES"),
+        (o[(o.GAME_SERVER_PRICING_CHANGE = 10)] = "GAME_SERVER_PRICING_CHANGE"),
+        o);
