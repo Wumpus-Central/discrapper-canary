be3011da13f1addd178426efdc38db18feec7dfe
DiscrapperManager <admin@minerpl.xyz>
2026-05-28 20:42:11 +0200

    canaryRelease: 552433
    Main scripts: 149
    Lazy Loaded Scripts: 3763
    Chunks:
     * Main Files: 11523
     * Scripts: 9048
    Strings: 25168
    Routes: 821
    Experiments Count: 38
    Apex Experiments Count: 349

--- chunks/50268.js
+++ chunks/50268.js
@@ -1,29 +1,30 @@
-n.d(e, { A: () => s });
+"use strict";
+n.d(t, { A: () => c });
 var i = n(627968);
 n(64700);
 var r = n(284009),
-    l = n.n(r),
-    o = n(477782),
-    u = n(473935),
-    a = n(885386),
-    c = n(957565);
-function s(t) {
-    let { id: e, label: n, onSuccess: r, shiftId: s, showIconFirst: d, showWithoutDeveloperMode: A } = t,
-        p = a.Q_.useSetting();
-    if (__OVERLAY__ || !(A || p) || !c.p5 || null == e) return null;
-    let f = `devmode-copy-id-${e}`;
+    s = n.n(r),
+    a = n(477782),
+    o = n(473935),
+    l = n(885386),
+    u = n(957565);
+function c(e) {
+    let { id: t, label: n, onSuccess: r, shiftId: c, showIconFirst: d, showWithoutDeveloperMode: _ } = e,
+        h = l.Q_.useSetting();
+    if (__OVERLAY__ || !(_ || h) || !u.p5 || null == t) return null;
+    let f = `devmode-copy-id-${t}`;
     return (0, i.jsx)(
-        o.Dr,
+        a.Dr,
         {
             id: f,
             label: n,
-            action: function (t) {
-                let n = null != s && t.shiftKey ? s : e;
-                l()(null != n, "cannot copy null text"), (0, c.C)(n, r);
+            action: function (e) {
+                let n = null != c && e.shiftKey ? c : t;
+                s()(null != n, "cannot copy null text"), (0, u.C)(n, r);
             },
-            icon: d ? void 0 : u.L,
-            iconLeft: d ? u.L : void 0,
-            leadingAccessory: { type: "icon", icon: u.L },
+            icon: d ? void 0 : o.L,
+            iconLeft: d ? o.L : void 0,
+            leadingAccessory: { type: "icon", icon: o.L },
         },
         f,
     );
