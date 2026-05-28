bae4569cc021313910548b158c13179df1940da9
DiscrapperManager <admin@minerpl.xyz>
2026-05-28 20:06:23 +0200

    canaryRelease: 552363
    Main scripts: 146
    Lazy Loaded Scripts: 3764
    Chunks:
     * Main Files: 11522
     * Scripts: 9050
    Strings: 25166
    Routes: 821
    Experiments Count: 38
    Apex Experiments Count: 348

--- chunks/55619.js
+++ chunks/55619.js
@@ -1,1 +1,10 @@
-p.d(t,{A:()=>a});var s=p(228366);let a={setEnabled(e){this.update({enabled:e})},update(e){for(let t of Object.keys(e))s.h.dispatch({type:"STREAMER_MODE_UPDATE",key:t,value:e[t]})}}alue:e[t]})}}
+p.d(t, { A: () => a });
+var s = p(228366);
+let a = {
+    setEnabled(e) {
+        this.update({ enabled: e });
+    },
+    update(e) {
+        for (let t of Object.keys(e)) s.h.dispatch({ type: "STREAMER_MODE_UPDATE", key: t, value: e[t] });
+    },
+};
