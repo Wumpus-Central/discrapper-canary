e7daba1fa3dbf61350b181a5aaade8024f2afa22
DiscrapperManager <admin@minerpl.xyz>
2026-05-28 19:29:46 +0200

    canaryRelease: 552320
    Main scripts: 168
    Lazy Loaded Scripts: 3757
    Chunks:
     * Main Files: 11628
     * Scripts: 8927
    Strings: 25164
    Routes: 821
    Experiments Count: 38
    Apex Experiments Count: 347

--- chunks/81009.js
+++ chunks/81009.js
@@ -5,16 +5,16 @@ var s = n(691540),
     a = n(857250),
     o = n(97483),
     l = n(192308),
-    d = n(976860),
-    _ = n(247775),
-    u = n(228366),
-    c = n(439372),
-    E = n(287809),
-    h = n(954571),
-    m = n(274303),
-    f = n(652215);
-let g = !1;
-class p extends c.A {
+    u = n(976860),
+    c = n(247775),
+    d = n(228366),
+    _ = n(439372),
+    h = n(287809),
+    f = n(174459),
+    p = n(274303),
+    E = n(652215);
+let m = !1;
+class g extends _.A {
     onSwitchStart;
     onSwitchSuccess;
     onSwitchError;
@@ -24,42 +24,42 @@ class p extends c.A {
     }
     actions = { LOGOUT: (e) => this.handleLogout(e) };
     _initialize() {
-        u.h.subscribe("CONNECTION_OPEN", () => this.handleConnectionOpen()), this.handleConnectionOpen();
+        d.h.subscribe("CONNECTION_OPEN", () => this.handleConnectionOpen()), this.handleConnectionOpen();
     }
     _terminate() {
-        u.h.unsubscribe("CONNECTION_OPEN", () => this.handleConnectionOpen());
+        d.h.unsubscribe("CONNECTION_OPEN", () => this.handleConnectionOpen());
     }
     handleConnectionOpen = () => {
-        let e = E.default.getCurrentUser();
+        let e = h.default.getCurrentUser();
         if (null == e) return;
         if (null != r) {
-            if (r !== i) {
-                let t = m.A.getUsers().map((e) => {
+            if (r !== e.id) {
+                let t = p.A.getUsers().map((e) => {
                     let { id: t } = e;
                     return t;
                 });
-                h.default.track(f.HAw.MULTI_ACCOUNT_SWITCH_SUCCESS, { from_user_id: r, linked_user_ids: t }),
-                    this.onSwitchSuccess?.(e, g);
-            } else h.default.track(f.HAw.MULTI_ACCOUNT_SWITCH_FAILURE), this.onSwitchError?.(e);
+                f.default.track(E.HAw.MULTI_ACCOUNT_SWITCH_SUCCESS, { from_user_id: r, linked_user_ids: t }),
+                    this.onSwitchSuccess?.(e, m);
+            } else f.default.track(E.HAw.MULTI_ACCOUNT_SWITCH_FAILURE), this.onSwitchError?.(e);
             r = null;
         }
         i = e.id;
-        let t = _.getToken();
-        null != t && "" !== t && _.setToken(t, e.id), this.onTokenSet?.(e);
+        let t = c.getToken();
+        null != t && "" !== t && c.setToken(t, e.id), this.onTokenSet?.(e);
     };
     handleLogout = (e) => {
         e.isSwitchingAccount
-            ? ((r = i), this.onSwitchStart?.(), (g = !!e.goHomeAfterSwitching))
-            : ((g = !1), _.removeToken(i)),
+            ? ((r = i), this.onSwitchStart?.(), (m = !!e.goHomeAfterSwitching))
+            : ((m = !1), c.removeToken(i)),
             (i = null);
     };
 }
 var A = n(573879),
-    I = n(985018);
-let T = new p({
+    I = n(375708);
+let T = new g({
     onSwitchSuccess(e, t) {
         setTimeout(() => {
-            t && (0, d.pX)(f.BVt.ME),
+            t && (0, u.pX)(E.BVt.ME),
                 (0, s.P0)((0, a.o)(I.intl.formatToPlainString(I.t.wx7O3L, { username: e.username }), o.Ck.SUCCESS));
         }, 100);
     },
