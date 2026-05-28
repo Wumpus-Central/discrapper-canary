a021894f2a7739a5f18c7fb4c26693fd2876725b
DiscrapperManager <admin@minerpl.xyz>
2026-05-28 19:45:50 +0200

    canaryRelease: 552341
    Main scripts: 168
    Lazy Loaded Scripts: 3757
    Chunks:
     * Main Files: 11628
     * Scripts: 8927
    Strings: 25164
    Routes: 821
    Experiments Count: 38
    Apex Experiments Count: 347

--- chunks/156652.js
+++ chunks/156652.js
@@ -1,27 +1,27 @@
-t.d(n, { A: () => m, x: () => h });
+t.d(n, { A: () => C, x: () => h });
 var l,
     i = t(627968),
-    a = t(64700),
-    s = t(503698),
-    r = t.n(s),
+    s = t(64700),
+    a = t(503698),
+    r = t.n(a),
     o = t(187322),
     c = t(765671),
     u = t(401685),
     d = t(919706),
     A = t(499997),
     h = (((l = {})[(l.TOP = 0)] = "TOP"), (l[(l.BOTTOM = 1)] = "BOTTOM"), l);
-function p(e) {
+function m(e) {
     let {
             disableGradients: n = !1,
             renderHeader: t,
             renderBottomLeft: l,
-            renderBottomCenter: s,
+            renderBottomCenter: a,
             renderBottomRight: r,
             renderCenter: d,
         } = e,
-        h = a.useRef(null),
-        { width: p, ref: m } = (0, c.Ay)(),
-        g = (0, u.bf)(p);
+        h = s.useRef(null),
+        { width: m, ref: C } = (0, c.Ay)(),
+        E = (0, u.bf)(m);
     return (0, i.jsx)(o.xp, {
         containerRef: h,
         children: (0, i.jsxs)("div", {
@@ -33,13 +33,13 @@ function p(e) {
                 (0, i.jsx)("div", { className: A.K1, children: t?.() }),
                 d?.(),
                 (0, i.jsx)(u.Ay.Provider, {
-                    value: g,
+                    value: E,
                     children: (0, i.jsxs)("div", {
                         className: A.q6,
-                        ref: m,
+                        ref: C,
                         children: [
                             (0, i.jsx)("div", { className: A.i$, children: l?.() }),
-                            (0, i.jsx)("div", { className: A.Kz, children: s?.() }),
+                            (0, i.jsx)("div", { className: A.Kz, children: a?.() }),
                             (0, i.jsx)("div", { className: A.i$, children: r?.() }),
                         ],
                     }),
@@ -48,30 +48,30 @@ function p(e) {
         }),
     });
 }
-function m(e) {
+function C(e) {
     let {
         screenMessage: n,
         onDoubleClick: t,
         onActive: l,
-        onForceIdle: a,
-        idle: s,
+        onForceIdle: s,
+        idle: a,
         children: o,
         renderChatToasts: c,
         renderVoiceChannelEffects: u,
         style: h,
-        ...m
+        ...C
     } = e;
     return (0, i.jsxs)("div", {
-        className: r()(A.zr, { [A.N7]: s }),
+        className: r()(A.zr, { [A.N7]: a }),
         style: h,
         onMouseMove: l,
         onMouseDown: l,
-        onMouseLeave: a,
+        onMouseLeave: s,
         onDoubleClick: t,
         children: [
+            (0, i.jsx)(m, { ...C }),
             o,
             null != n ? (0, i.jsx)(d.A, { size: "large", ...n }) : null,
-            (0, i.jsx)(p, { ...m }),
             c?.(),
             u?.(),
         ],
