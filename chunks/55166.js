132c1035e119756b07952b80455473b668a3d3fe
DiscrapperManager <admin@minerpl.xyz>
2026-05-29 16:58:32 +0200

    noisesSuppression 553235

--- chunks/55166.js
+++ chunks/55166.js
@@ -1,1 +1,39 @@
-r.d(t,{I:()=>o});var n=r(64700);function i(e,t){(null==t||t>e.length)&&(t=e.length);for(var r=0,n=Array(t);r<t;r++)n[r]=e[r];return n}function o(e,t){var r,o=function(e){if(Array.isArray(e))return i(e)}(r=t||[])||function(e){if("u">typeof Symbol&&null!=e[Symbol.iterator]||null!=e["@@iterator"])return Array.from(e)}(r)||function(e){if(e){if("string"==typeof e)return i(e,void 0);var t=Object.prototype.toString.call(e).slice(8,-1);if("Object"===t&&e.constructor&&(t=e.constructor.name),"Map"===t||"Set"===t)return Array.from(e);if("Arguments"===t||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t))return i(e,void 0)}}(r)||function(){throw TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}();return null==t&&"function"!=typeof e&&o.push(e),(0,n.useMemo)(function(){return"function"==typeof e?e():e},o)} e?e():e},s)}
+"use strict";
+n.d(t, { I: () => s });
+var i = n(64700);
+function r(e, t) {
+    (null == t || t > e.length) && (t = e.length);
+    for (var n = 0, i = Array(t); n < t; n++) i[n] = e[n];
+    return i;
+}
+function s(e, t) {
+    var n,
+        s =
+            (function (e) {
+                if (Array.isArray(e)) return r(e);
+            })((n = t || [])) ||
+            (function (e) {
+                if (("u" > typeof Symbol && null != e[Symbol.iterator]) || null != e["@@iterator"])
+                    return Array.from(e);
+            })(n) ||
+            (function (e) {
+                if (e) {
+                    if ("string" == typeof e) return r(e, void 0);
+                    var t = Object.prototype.toString.call(e).slice(8, -1);
+                    if (("Object" === t && e.constructor && (t = e.constructor.name), "Map" === t || "Set" === t))
+                        return Array.from(e);
+                    if ("Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t)) return r(e, void 0);
+                }
+            })(n) ||
+            (function () {
+                throw TypeError(
+                    "Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.",
+                );
+            })();
+    return (
+        null == t && "function" != typeof e && s.push(e),
+        (0, i.useMemo)(function () {
+            return "function" == typeof e ? e() : e;
+        }, s)
+    );
+}
