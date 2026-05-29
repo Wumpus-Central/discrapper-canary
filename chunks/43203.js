e6f15025750674d568a2bb65a65a4dc0cbdeb584
DiscrapperManager <admin@minerpl.xyz>
2026-05-29 21:44:08 +0200

    canaryRelease: 553512
    Main scripts: 167
    Lazy Loaded Scripts: 3758
    Chunks:
     * Main Files: 11618
     * Scripts: 8890
    Strings: 25199
    Routes: 821
    Experiments Count: 38
    Apex Experiments Count: 350

--- chunks/43203.js
+++ chunks/43203.js
@@ -1,1 +1,46 @@
-n.d(t,{$:()=>h,A:()=>p});var l=n(228366),s=n(376728),i=n(976860),a=n(495544),r=n(174459),o=n(723702),u=n(19575),d=n(636401),c=n(613057),m=n(652215);async function h(e,t){let n=arguments.length>2&&void 0!==arguments[2]?arguments[2]:{},{invite:c}=await s.Ay.resolveInvite(e,t);if(null==c)throw new d.A({errorCode:m.Lw6.INVALID_INVITE},`Invalid invite id: ${e}`);return n?.installationId!=null&&r.default.track(m.HAw.EXTERNAL_INSTALLATION_ID_RECEIVED,{received_installation_id:n.installationId,location:"openInviteFromRPC"}),a.default.isAuthenticated()?l.h.dispatch({type:"INVITE_MODAL_OPEN",invite:c,code:e,context:m.BRT.APP,received_installation_id:null!=n.installationId?String(n.installationId):void 0}):(n?.installationId!=null&&s.Ay.setReceivedInstallationIdForInviteCode(c.code,String(n.installationId)),(0,i.bG)(m.BVt.INVITE(e))),o.isPlatformEmbedded&&u.Ay.focus(),{invite:c,code:e}}let p={[m.e$_.OPEN_INVITE]:{scope:c.hj,async handler(e){let{args:t}=e,{code:n,...l}=t;await h(n,"RPC OPEN_INVITE Handler",l)}}}andler",i)}}}
+n.d(t, { $: () => h, A: () => p });
+var l = n(228366),
+    s = n(376728),
+    i = n(976860),
+    a = n(495544),
+    r = n(174459),
+    o = n(723702),
+    u = n(19575),
+    d = n(636401),
+    c = n(613057),
+    m = n(652215);
+async function h(e, t) {
+    let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : {},
+        { invite: c } = await s.Ay.resolveInvite(e, t);
+    if (null == c) throw new d.A({ errorCode: m.Lw6.INVALID_INVITE }, `Invalid invite id: ${e}`);
+    return (
+        n?.installationId != null &&
+            r.default.track(m.HAw.EXTERNAL_INSTALLATION_ID_RECEIVED, {
+                received_installation_id: n.installationId,
+                location: "openInviteFromRPC",
+            }),
+        a.default.isAuthenticated()
+            ? l.h.dispatch({
+                  type: "INVITE_MODAL_OPEN",
+                  invite: c,
+                  code: e,
+                  context: m.BRT.APP,
+                  received_installation_id: null != n.installationId ? String(n.installationId) : void 0,
+              })
+            : (n?.installationId != null &&
+                  s.Ay.setReceivedInstallationIdForInviteCode(c.code, String(n.installationId)),
+              (0, i.bG)(m.BVt.INVITE(e))),
+        o.isPlatformEmbedded && u.Ay.focus(),
+        { invite: c, code: e }
+    );
+}
+let p = {
+    [m.e$_.OPEN_INVITE]: {
+        scope: c.hj,
+        async handler(e) {
+            let { args: t } = e,
+                { code: n, ...l } = t;
+            await h(n, "RPC OPEN_INVITE Handler", l);
+        },
+    },
+};
