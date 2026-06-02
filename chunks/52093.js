69c05fa7aa3e391d0873f6b071ecd7492be1672a
DiscrapperManager <admin@minerpl.xyz>
2026-06-02 20:30:18 +0200

    canaryRelease: 555244
    Main scripts: 173
    Lazy Loaded Scripts: 3840
    Chunks:
     * Main Files: 11850
     * Scripts: 8865
    Strings: 25315
    Routes: 822
    Experiments Count: 38
    Apex Experiments Count: 349

--- chunks/52093.js
+++ chunks/52093.js
@@ -1,1 +1,77 @@
-s.d(t,{Ay:()=>j,L5:()=>h});var l=s(627968),i=s(64700),n=s(503698),r=s.n(n);s(17928);var a=s(947641),c=s(661531),o=s(834730),d=s(534514);s(287809),s(629455),s(801365),s(375708);var m=s(629436);let x=i.createContext({isCurrent:!1});function u(e){let{children:t,isComplete:s,isCurrent:i,hasNextStep:n}=e;return(0,l.jsxs)("li",{className:r()(m.gY,{[m.HP]:s},{[m.fS]:i},{[m.h4]:n}),children:[(0,l.jsxs)("div",{className:m.cJ,children:[(0,l.jsx)("div",{className:m.$P,children:s&&(0,l.jsx)(a.r,{className:m.ap,color:c.A.colors.WHITE})}),n&&(0,l.jsx)("div",{className:m.UK})]}),(0,l.jsx)("div",{className:m.M7,children:(0,l.jsx)(x.Provider,{value:{isCurrent:i},children:t})})]})}function h(e){let{children:t}=e,{isCurrent:s}=i.useContext(x);return(0,l.jsx)(o.E,{color:s?"text-default":"text-muted",variant:"text-xs/medium",children:t})}let j=function(e){let{children:t,heading:s,steps:i}=e,n=i.findIndex(e=>!e.isComplete);return(0,l.jsxs)("div",{className:m.iE,children:[(0,l.jsx)("div",{className:m.Mj,children:(0,l.jsx)(d.D,{className:m.R_,color:"text-strong",variant:"text-xs/semibold",children:s})}),(0,l.jsx)("div",{className:m.lp,children:(0,l.jsx)("ul",{children:i.map((e,t)=>(0,l.jsx)(u,{isComplete:e.isComplete,isCurrent:t===n,hasNextStep:t<i.length-1,children:e.renderContent()},t))})}),t]})}t))})}),t]})}
+"use strict";
+n.d(t, { Ay: () => p, L5: () => f });
+var i = n(627968),
+    r = n(64700),
+    s = n(503698),
+    a = n.n(s);
+n(17928);
+var o = n(947641),
+    l = n(661531),
+    u = n(834730),
+    c = n(534514);
+n(287809), n(629455), n(801365), n(375708);
+var d = n(629436);
+let _ = r.createContext({ isCurrent: !1 });
+function h(e) {
+    let { children: t, isComplete: n, isCurrent: r, hasNextStep: s } = e;
+    return (0, i.jsxs)("li", {
+        className: a()(d.gY, { [d.HP]: n }, { [d.fS]: r }, { [d.h4]: s }),
+        children: [
+            (0, i.jsxs)("div", {
+                className: d.cJ,
+                children: [
+                    (0, i.jsx)("div", {
+                        className: d.$P,
+                        children: n && (0, i.jsx)(o.r, { className: d.ap, color: l.A.colors.WHITE }),
+                    }),
+                    s && (0, i.jsx)("div", { className: d.UK }),
+                ],
+            }),
+            (0, i.jsx)("div", {
+                className: d.M7,
+                children: (0, i.jsx)(_.Provider, { value: { isCurrent: r }, children: t }),
+            }),
+        ],
+    });
+}
+function f(e) {
+    let { children: t } = e,
+        { isCurrent: n } = r.useContext(_);
+    return (0, i.jsx)(u.E, { color: n ? "text-default" : "text-muted", variant: "text-xs/medium", children: t });
+}
+let p = function (e) {
+    let { children: t, heading: n, steps: r } = e,
+        s = r.findIndex((e) => !e.isComplete);
+    return (0, i.jsxs)("div", {
+        className: d.iE,
+        children: [
+            (0, i.jsx)("div", {
+                className: d.Mj,
+                children: (0, i.jsx)(c.D, {
+                    className: d.R_,
+                    color: "text-strong",
+                    variant: "text-xs/semibold",
+                    children: n,
+                }),
+            }),
+            (0, i.jsx)("div", {
+                className: d.lp,
+                children: (0, i.jsx)("ul", {
+                    children: r.map((e, t) =>
+                        (0, i.jsx)(
+                            h,
+                            {
+                                isComplete: e.isComplete,
+                                isCurrent: t === s,
+                                hasNextStep: t < r.length - 1,
+                                children: e.renderContent(),
+                            },
+                            t,
+                        ),
+                    ),
+                }),
+            }),
+            t,
+        ],
+    });
+};
