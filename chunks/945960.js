d28f8c2e66fd8e633c69785bd7ef67fd6f094cda
DiscrapperManager <admin@minerpl.xyz>
2026-06-03 18:59:40 +0200

    canaryRelease: 556011
    Main scripts: 151
    Lazy Loaded Scripts: 3846
    Chunks:
     * Main Files: 11746
     * Scripts: 8913
    Strings: 25327
    Routes: 822
    Experiments Count: 38
    Apex Experiments Count: 352

--- chunks/945960.js
+++ chunks/945960.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { Vn: () => h, _D: () => f, qz: () => p, u1: () => E });
+n.d(t, { Vn: () => f, u1: () => E, qz: () => p, _D: () => h });
 var i = n(636537),
     r = n(554146),
     s = n(228366),
@@ -8,16 +8,23 @@ var i = n(636537),
     l = n(594061),
     u = n(174459),
     c = n(723702),
-    d = n(38405),
-    _ = n(652215);
-async function f(e) {
+    d = n(38405);
+(0, n(945810).mj)({
+    name: "2026-06-android-two-week-trials",
+    kind: "user",
+    defaultConfig: { enabled: !1, discountCTAEnabled: !1 },
+    variations: { 1: { enabled: !1, discountCTAEnabled: !0 }, 2: { enabled: !0, discountCTAEnabled: !0 } },
+}),
+    n(788868);
+var _ = n(652215);
+async function h(e) {
     let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
         n =
             arguments.length > 2 && void 0 !== arguments[2]
                 ? arguments[2]
                 : { offerId: void 0, paymentGatewayOverride: void 0 },
-        f = arguments.length > 3 ? arguments[3] : void 0,
-        h = arguments.length > 4 ? arguments[4] : void 0;
+        h = arguments.length > 3 ? arguments[3] : void 0,
+        f = arguments.length > 4 ? arguments[4] : void 0;
     if (t) {
         s.h.dispatch({ type: "BILLING_USER_OFFER_FETCH_START" });
         try {
@@ -34,13 +41,13 @@ async function f(e) {
                     url: _.Rsh.USER_OFFER,
                     body: null != m || null != p ? { payment_gateway: m, offer_id: p } : {},
                     rejectWithError: !0,
-                    retries: null != f ? f.retries : void 0,
+                    retries: null != h ? h.retries : void 0,
                 }),
                 A = g.body.user_trial_offer ?? null,
                 I = g.body.user_discount_offer ?? null;
             if (null != p && null != I && I.discount_id !== p) {
                 let e = Error("Returned user discount offer does not match offer ID request parameter");
-                throw (d.A.captureException(e, { extra: { offer_id: p, user_discount_offer: I }, ...h }), e);
+                throw (d.A.captureException(e, { extra: { offer_id: p, user_discount_offer: I }, ...f }), e);
             }
             return (
                 null == A &&
@@ -59,7 +66,7 @@ async function f(e) {
     }
     return !1;
 }
-async function h() {
+async function f() {
     s.h.dispatch({ type: "BILLING_USER_OFFER_FETCH_START" });
     try {
         let e = (await i.Bo.get({ url: _.Rsh.CHURN_USER_OFFER, rejectWithError: !0 })).body.offer ?? null,
