6660baeea2901f7f466cc4c36112b9637ce7da7a
DiscrapperManager <admin@minerpl.xyz>
2026-06-05 00:52:14 +0200

    canaryRelease: 557523
    Main scripts: 161
    Lazy Loaded Scripts: 3829
    Chunks:
     * Main Files: 11905
     * Scripts: 8856
    Strings: 25350
    Routes: 823
    Experiments Count: 38
    Apex Experiments Count: 355

--- chunks/65154.js
+++ chunks/65154.js
@@ -1,38 +1,39 @@
-r.d(t, { S: () => o });
-var n = r(627968);
-r(64700);
-var a = r(661531),
-    i = r(996682),
-    l = r(27989);
+"use strict";
+n.d(t, { S: () => o });
+var i = n(627968);
+n(64700);
+var r = n(661531),
+    s = n(996682),
+    a = n(27989);
 let o = (e) => {
     let {
             size: t = "md",
-            width: r,
+            width: n,
             height: o,
-            secondaryColor: s = "transparent",
-            secondaryColorClass: d = "",
-            color: c = a.A.colors.INTERACTIVE_ICON_DEFAULT,
-            colorClass: u = "",
-            ...m
+            secondaryColor: l = "transparent",
+            secondaryColorClass: u = "",
+            color: c = r.A.colors.INTERACTIVE_ICON_DEFAULT,
+            colorClass: d = "",
+            ..._
         } = e,
-        p = (0, l.J)(t),
-        E = p?.width ?? r,
-        _ = p?.height ?? o;
-    return (0, n.jsxs)("svg", {
-        ...(0, i.A)(m),
+        h = (0, a.J)(t),
+        f = h?.width ?? n,
+        p = h?.height ?? o;
+    return (0, i.jsxs)("svg", {
+        ...(0, s.A)(_),
         xmlns: "http://www.w3.org/2000/svg",
-        width: E,
-        height: _,
+        width: f,
+        height: p,
         fill: "none",
         viewBox: "0 0 24 24",
         children: [
-            (0, n.jsx)("circle", { cx: "12", cy: "12", r: "10", fill: "string" == typeof s ? s : s.css, className: d }),
-            (0, n.jsx)("path", {
+            (0, i.jsx)("circle", { cx: "12", cy: "12", r: "10", fill: "string" == typeof l ? l : l.css, className: u }),
+            (0, i.jsx)("path", {
                 fill: "string" == typeof c ? c : c.css,
                 fillRule: "evenodd",
                 d: "M12 23a11 11 0 1 0 0-22 11 11 0 0 0 0 22ZM9 8.09c0-.88 1-1.4 1.73-.9l5.8 3.91c.63.44.63 1.36 0 1.8l-5.8 3.9C10 17.3 9 16.8 9 15.92V8.1Z",
                 clipRule: "evenodd",
-                className: u,
+                className: d,
             }),
         ],
     });
