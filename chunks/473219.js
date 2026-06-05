8a857ca832443d54cf51593653bf99919693afec
DiscrapperManager <admin@minerpl.xyz>
2026-06-05 23:24:27 +0200

    canaryRelease: 558492
    Main scripts: 189
    Lazy Loaded Scripts: 3844
    Chunks:
     * Main Files: 11888
     * Scripts: 8872
    Strings: 25378
    Routes: 822
    Experiments Count: 38
    Apex Experiments Count: 357

--- chunks/473219.js
+++ chunks/473219.js
@@ -1,52 +1,64 @@
-a.d(l, { A: () => g });
-var i = a(627968),
-    n = a(64700),
-    r = a(688810),
-    t = a(344346),
-    s = a(637193),
-    d = a(919395),
-    u = a(84540),
-    c = a(930349),
-    o = a(375708),
-    v = a(535592);
-function g(e) {
-    let { user: l, guildId: a, disabled: g } = e,
-        { analyticsLocations: f } = (0, r.Ay)(),
-        h = null != a,
-        { guildNameplate: b, pendingNameplate: p } = (0, d.rv)(l, a ?? void 0),
-        m = l.collectibles?.nameplate,
-        k = h ? b : m,
-        A = void 0 !== p,
-        I = null === p || (!A && null == k),
-        C = h && null != m,
-        x = (0, d.lw)({ pendingValue: p, userValue: m, guildValue: b, guildId: a ?? void 0 }),
-        y =
-            null != x && (A ? null != p : null != k)
+l.d(n, { A: () => f });
+var t = l(627968),
+    a = l(64700),
+    r = l(688810),
+    i = l(674658),
+    s = l(344346),
+    o = l(637193),
+    u = l(536572),
+    d = l(919395),
+    c = l(84540),
+    g = l(930349),
+    v = l(375708),
+    h = l(535592);
+function f(e) {
+    let { user: n, guildId: l, disabled: f } = e,
+        { analyticsLocations: p } = (0, r.Ay)(),
+        m = null != l,
+        { guildNameplate: b, pendingNameplate: x } = (0, d.rv)(n, l ?? void 0),
+        C = n.collectibles?.nameplate,
+        I = m ? b : C,
+        A = void 0 !== x,
+        k = null === x || (!A && null == I),
+        y = m && null != C,
+        j = (0, d.lw)({ pendingValue: x, userValue: C, guildValue: b, guildId: l ?? void 0 }),
+        { product: N } = (0, i.q)(j?.skuId),
+        E = A ? null != x : null != I,
+        w =
+            null != j && E
                 ? {
-                      onClick: () => (0, u.p)({ guildId: a ?? void 0, nameplate: null }),
-                      type: C ? "reset" : "remove",
-                      accessibleLabel: o.intl.string(C ? o.t.neYqhR : o.t["9zwziY"]),
+                      onClick: () => (0, c.p)({ guildId: l ?? void 0, nameplate: null }),
+                      type: y ? "reset" : "remove",
+                      accessibleLabel: v.intl.string(y ? v.t.neYqhR : v.t["9zwziY"]),
                   }
                 : void 0,
-        j = n.useCallback(() => {
-            (0, s.p)({ analyticsLocations: f, guildId: a ?? void 0, stackingBehavior: "stack" });
-        }, [f, a]);
-    return (0, i.jsx)(c.V, {
-        affordance: I && !C ? "add" : y,
+        S = a.useCallback(() => {
+            (0, o.p)({ analyticsLocations: p, guildId: l ?? void 0, stackingBehavior: "stack" });
+        }, [p, l]);
+    return (0, t.jsx)(g.V, {
+        affordance: k && !y ? "add" : w,
         variant: "bar",
-        onClick: j,
-        accessibleLabel: o.intl.string(o.t.BwdeM1),
+        onClick: S,
+        accessibleLabel: v.intl.string(v.t.x5CoXR),
+        accessibleValue: (function (e) {
+            let { nameplatePreview: n, productName: l, hasPendingSelection: t } = e;
+            return null == n
+                ? v.intl.string(v.t["3Xph0/"])
+                : null != l && "" !== l
+                  ? l
+                  : v.intl.string(t ? v.t.mo1Huz : v.t.s9kNKK);
+        })({ nameplatePreview: j, productName: (0, u.VG)(N), hasPendingSelection: null != x }),
         "aria-haspopup": "dialog",
-        disabled: g,
+        disabled: f,
         renderPreview: (e) =>
-            (0, i.jsx)("div", {
-                className: v.M,
+            (0, t.jsx)("div", {
+                className: h.M,
                 "aria-hidden": !0,
-                children: (0, i.jsx)(t.A, {
-                    user: l,
-                    guildId: a ?? void 0,
-                    nameplate: x,
-                    showPlaceholderUser: I || !e,
+                children: (0, t.jsx)(s.A, {
+                    user: n,
+                    guildId: l ?? void 0,
+                    nameplate: j,
+                    showPlaceholderUser: k || !e,
                     isHighlighted: e,
                 }),
             }),
