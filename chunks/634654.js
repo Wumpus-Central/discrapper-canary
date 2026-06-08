89871fb8e260727c732f2c5b194548c7418b790f
DiscrapperManager <admin@minerpl.xyz>
2026-06-08 23:06:40 +0200

    canaryRelease: 559320
    Main scripts: 167
    Lazy Loaded Scripts: 3878
    Chunks:
     * Main Files: 11785
     * Scripts: 9001
    Strings: 25385
    Routes: 822
    Experiments Count: 37
    Apex Experiments Count: 358

--- chunks/634654.js
+++ chunks/634654.js
@@ -1,16 +1,18 @@
-e.d(s, { Db: () => t, bJ: () => c, hN: () => h, lJ: () => r, rv: () => a });
-var i,
-    d,
-    h =
-        (((i = {}).GET_STARTED_CLICK = "get_started_click"),
-        (i.DISMISS_FULL_PREVIEW = "dismiss_full_preview"),
-        (i.DISMISS_TAB_PREVIEW = "dismiss_tab_preview"),
-        (i.DISMISS_CHANNEL_ROW = "dismiss_channel_row"),
-        i),
+i.d(e, { Db: () => u, bJ: () => a, hN: () => r, lJ: () => s, rv: () => o });
+var l,
+    n,
     r =
-        (((d = {}).CHANNEL_ROW_CONTEXT_MENU = "channel_row_context_menu"),
-        (d.SHOP_HEADER_BUTTON = "shop_header_button"),
-        d);
-let t = "hasClickedGuildShopProductPreviewTab",
-    c = "hasClickedGuildShopChannelRow",
-    a = "https://discord.com/servershop";
+        (((l = {}).GET_STARTED_CLICK = "get_started_click"),
+        (l.DISMISS_FULL_PREVIEW = "dismiss_full_preview"),
+        (l.DISMISS_TAB_PREVIEW = "dismiss_tab_preview"),
+        (l.DISMISS_CHANNEL_ROW = "dismiss_channel_row"),
+        l),
+    s =
+        21552 == i.j
+            ? (((n = {}).CHANNEL_ROW_CONTEXT_MENU = "channel_row_context_menu"),
+              (n.SHOP_HEADER_BUTTON = "shop_header_button"),
+              n)
+            : null;
+let u = "hasClickedGuildShopProductPreviewTab",
+    a = "hasClickedGuildShopChannelRow",
+    o = "https://discord.com/servershop";
