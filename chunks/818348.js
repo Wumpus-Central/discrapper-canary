99bb394c7bc4a10c1e3f0c97ad10365f4b59e315
DiscrapperManager <admin@minerpl.xyz>
2026-06-09 00:21:37 +0200

    canaryRelease: 559405
    Main scripts: 175
    Lazy Loaded Scripts: 3875
    Chunks:
     * Main Files: 11785
     * Scripts: 9002
    Strings: 25389
    Routes: 823
    Experiments Count: 37
    Apex Experiments Count: 360

--- chunks/818348.js
+++ chunks/818348.js
@@ -676,6 +676,7 @@ var ex =
         (T.MKTG_APPLICATION_STEP = "mktg_application_step"),
         (T.MKTG_WARFRAME_CTA_CLICKED = "mktg_warframe_cta_clicked"),
         (T.MKTG_PAGE_CTA_CLICKED = "mktg_page_cta_clicked"),
+        (T.MKTG_PAGE_SCROLLED = "mktg_page_scrolled"),
         (T.MKTG_VIDEO_PLAYED = "mktg_video_played"),
         (T.DEEP_LINK_CLICKED = "deep_link_clicked"),
         (T.SEO_PAGE_VIEWED = "seo_server_landing_page_viewed"),
