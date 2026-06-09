729f0dec1a3ed4a1beda4a4d4b418e4ea0a96d44
DiscrapperManager <admin@minerpl.xyz>
2026-06-09 21:08:29 +0200

    canaryRelease: 559913
    Main scripts: 149
    Lazy Loaded Scripts: 3848
    Chunks:
     * Main Files: 11822
     * Scripts: 8958
    Strings: 25395
    Routes: 823
    Experiments Count: 37
    Apex Experiments Count: 360

--- chunks/235665.js
+++ chunks/235665.js
@@ -1,9 +1,10 @@
-r.d(t, { A: () => a });
-var i = r(627968);
-if ((r(64700), 21552 == r.j)) var n = r(192308);
-function a(e) {
-    (0, n.openModalLazy)(async () => {
-        let { default: t } = await Promise.all([r.e("95681"), r.e("60222")]).then(r.bind(r, 13197));
-        return (r) => (0, i.jsx)(t, { ...e, ...r });
+_.d(t, { A: () => S });
+var n = _(627968);
+_(64700);
+var i = _(192308);
+function S(e) {
+    (0, i.openModalLazy)(async () => {
+        let { default: t } = await Promise.all([_.e("95681"), _.e("60222")]).then(_.bind(_, 13197));
+        return (_) => (0, n.jsx)(t, { ...e, ..._ });
     });
 }
