b5b3e70e836bf700fc898343ef500e94a23de170
DiscrapperManager <admin@minerpl.xyz>
2026-06-09 23:51:18 +0200

    canaryRelease: 560160
    Main scripts: 161
    Lazy Loaded Scripts: 3837
    Chunks:
     * Main Files: 11938
     * Scripts: 8802
    Strings: 25398
    Routes: 823
    Experiments Count: 37
    Apex Experiments Count: 358

--- chunks/200254.js
+++ chunks/200254.js
@@ -1,8 +1,8 @@
 e.exports = {
     wx: "header_feae96",
     P9: "questInfo_feae96",
-    Qf: "questIcon_feae96",
     v0: "questIconContainer_feae96",
+    Qf: "questIcon_feae96",
     L$: "progressContainer_feae96",
     hr: "progressBar_feae96",
     SX: "progressBarFill_feae96",
