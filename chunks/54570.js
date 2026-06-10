4e140968f2dea2da1d211d144fa76335e11e1566
DiscrapperManager <admin@minerpl.xyz>
2026-06-10 19:22:11 +0200

    canaryRelease: 560642
    Main scripts: 155
    Lazy Loaded Scripts: 3851
    Chunks:
     * Main Files: 11654
     * Scripts: 9151
    Strings: 25416
    Routes: 823
    Experiments Count: 37
    Apex Experiments Count: 357

--- chunks/54570.js
+++ chunks/54570.js
@@ -1,17 +1,18 @@
-a.d(t, { AU: () => l, kC: () => h, kP: () => i, pr: () => c, zU: () => n });
-var s = a(228366);
-function l(e, t, a, l, i) {
-    s.h.dispatch({ type: "SPEAK_TEXT", text: e, interrupt: t, maxLength: a, onStart: l, onEnd: i });
+"use strict";
+n.d(t, { AU: () => r, kC: () => a, kP: () => s, pr: () => o, zU: () => l });
+var i = n(228366);
+function r(e, t, n, r, s) {
+    i.h.dispatch({ type: "SPEAK_TEXT", text: e, interrupt: t, maxLength: n, onStart: r, onEnd: s });
 }
-function i(e, t) {
-    s.h.dispatch({ type: "SPEAK_MESSAGE", channel: e, message: t });
+function s(e, t) {
+    i.h.dispatch({ type: "SPEAK_MESSAGE", channel: e, message: t });
 }
-function h(e, t) {
-    s.h.dispatch({ type: "SPEAKING_MESSAGE", channelId: e, messageId: t });
+function a(e, t) {
+    i.h.dispatch({ type: "SPEAKING_MESSAGE", channelId: e, messageId: t });
 }
-function c() {
-    s.h.dispatch({ type: "STOP_SPEAKING" });
+function o() {
+    i.h.dispatch({ type: "STOP_SPEAKING" });
 }
-function n(e) {
-    s.h.dispatch({ type: "SET_TTS_SPEECH_RATE", speechRate: e });
+function l(e) {
+    i.h.dispatch({ type: "SET_TTS_SPEECH_RATE", speechRate: e });
 }
