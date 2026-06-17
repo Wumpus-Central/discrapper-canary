99242557d2cc813b4af822875f24464c75039306
DiscrapperManager <admin@minerpl.xyz>
2026-06-17 00:15:40 +0000

    canaryRelease: 564301
    Main scripts: 691
    Lazy Loaded Scripts: 4054
    Chunks:
     * Main Files: 11489
     * Scripts: 9696
    Strings: 25525
    Routes: 825
    Experiments Count: 36
    Apex Experiments Count: 372

--- chunks/301958.js
+++ chunks/301958.js
@@ -3,8 +3,8 @@ _.exports = {
     VU: "withGradient__553bf",
     XO: "withQuestHomeHeroContent__553bf",
     jR: "withOnboardingContent__553bf",
-    Nd: "questHomeHeroBackgroundImage__553bf",
     qS: "badge__553bf",
     q2: "shine__553bf",
     ys: "glow__553bf",
+    Nd: "questHomeHeroBackgroundImage__553bf",
 };
