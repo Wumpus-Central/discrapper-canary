38a73a50aab96ed7d72ed9bff616ac33182b2c51
DiscrapperManager <admin@minerpl.xyz>
2026-06-18 09:50:29 +0000

    canaryRelease: 565322
    Main scripts: 663
    Lazy Loaded Scripts: 4048
    Chunks:
     * Main Files: 11490
     * Scripts: 9601
    Strings: 25554
    Routes: 825
    Experiments Count: 36
    Apex Experiments Count: 371

--- chunks/77157.js
+++ chunks/77157.js
@@ -1,15 +1,14 @@
-"use strict";
-n.d(t, { A: () => o });
-var i = n(17928),
-    r = n(590180),
-    s = n(4227),
-    a = n(892118);
+a.d(r, { A: () => o });
+var n = a(17928),
+    t = a(590180),
+    l = a(4227),
+    i = a(892118);
 function o(e) {
-    return (0, i.bG)([r.A, s.A], () => {
+    return (0, n.bG)([t.A, l.A], () => {
         if (null == e) return;
-        let t = r.A.getProduct(e);
-        if ((0, a.s)(t?.items[0])) return t.items[0];
-        let n = s.A.getPurchase(e);
-        if ((0, a.s)(n?.items[0])) return n.items[0];
+        let r = t.A.getProduct(e);
+        if ((0, i.s)(r?.items[0])) return r.items[0];
+        let a = l.A.getPurchase(e);
+        if ((0, i.s)(a?.items[0])) return a.items[0];
     });
 }
