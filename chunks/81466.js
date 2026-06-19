ceb04ea993e9440b079d814382ff6859b403e3db
DiscrapperManager <admin@minerpl.xyz>
2026-06-19 00:35:44 +0000

    noisesSuppression 566065

--- chunks/81466.js
+++ chunks/81466.js
@@ -1,41 +1,40 @@
-"use strict";
-n.d(t, { C: () => o });
-var i = n(627968);
-n(64700);
-var r = n(661531),
-    s = n(996682),
-    a = n(27989);
-let o = (e) => {
+e.d(a, { C: () => c });
+var h = e(627968);
+e(64700);
+var t = e(661531),
+    i = e(996682),
+    l = e(27989);
+let c = (s) => {
     let {
-            size: t = "md",
-            width: n,
-            height: o,
-            color: l = r.A.colors.INTERACTIVE_ICON_DEFAULT,
-            colorClass: u = "",
-            ...c
-        } = e,
-        d = (0, a.J)(t),
-        _ = d?.width ?? n,
-        h = d?.height ?? o;
-    return (0, i.jsxs)("svg", {
-        ...(0, s.A)(c),
+            size: a = "md",
+            width: e,
+            height: c,
+            color: d = t.A.colors.INTERACTIVE_ICON_DEFAULT,
+            colorClass: p = "",
+            ...v
+        } = s,
+        r = (0, l.J)(a),
+        n = r?.width ?? e,
+        o = r?.height ?? c;
+    return (0, h.jsxs)("svg", {
+        ...(0, i.A)(v),
         xmlns: "http://www.w3.org/2000/svg",
-        width: _,
-        height: h,
+        width: n,
+        height: o,
         fill: "none",
         viewBox: "0 0 24 24",
         children: [
-            (0, i.jsx)("path", {
-                fill: "string" == typeof l ? l : l.css,
+            (0, h.jsx)("path", {
+                fill: "string" == typeof d ? d : d.css,
                 d: "M7 1a1 1 0 0 1 1 1v.75c0 .14.11.25.25.25h7.5c.14 0 .25-.11.25-.25V2a1 1 0 1 1 2 0v.75c0 .14.11.25.25.25H19a3 3 0 0 1 3 3 1 1 0 0 1-1 1H3a1 1 0 0 1-1-1 3 3 0 0 1 3-3h.75c.14 0 .25-.11.25-.25V2a1 1 0 0 1 1-1Z",
-                className: u,
+                className: p,
             }),
-            (0, i.jsx)("path", {
-                fill: "string" == typeof l ? l : l.css,
+            (0, h.jsx)("path", {
+                fill: "string" == typeof d ? d : d.css,
                 fillRule: "evenodd",
                 d: "M2 10a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v9a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-9Zm3.5 2a.5.5 0 0 0-.5.5v3c0 .28.22.5.5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3Z",
                 clipRule: "evenodd",
-                className: u,
+                className: p,
             }),
         ],
     });
