252ba8983381e85ac48026d000d6d078f6621503
DiscrapperManager <admin@minerpl.xyz>
2026-06-22 17:00:47 +0000

    canaryRelease: 566299
    Main scripts: 663
    Lazy Loaded Scripts: 4063
    Chunks:
     * Main Files: 11500
     * Scripts: 9683
    Strings: 25577
    Routes: 827
    Experiments Count: 36
    Apex Experiments Count: 306

--- chunks/86147.js
+++ chunks/86147.js
@@ -1,33 +1,32 @@
-"use strict";
-n.d(t, { z: () => o });
-var i = n(627968);
+n.d(t, { z: () => s });
+var r = n(627968);
 n(64700);
-var r = n(661531),
-    s = n(996682),
-    a = n(27989);
-let o = (e) => {
+var l = n(661531),
+    a = n(996682),
+    i = n(27989);
+let s = (e) => {
     let {
             size: t = "md",
             width: n,
-            height: o,
-            color: l = r.A.colors.INTERACTIVE_ICON_DEFAULT,
-            colorClass: u = "",
+            height: s,
+            color: u = l.A.colors.INTERACTIVE_ICON_DEFAULT,
+            colorClass: o = "",
             ...c
         } = e,
-        d = (0, a.J)(t),
-        _ = d?.width ?? n,
-        h = d?.height ?? o;
-    return (0, i.jsx)("svg", {
-        ...(0, s.A)(c),
+        d = (0, i.J)(t),
+        m = d?.width ?? n,
+        f = d?.height ?? s;
+    return (0, r.jsx)("svg", {
+        ...(0, a.A)(c),
         xmlns: "http://www.w3.org/2000/svg",
-        width: _,
-        height: h,
+        width: m,
+        height: f,
         fill: "none",
         viewBox: "0 0 24 24",
-        children: (0, i.jsx)("path", {
-            fill: "string" == typeof l ? l : l.css,
+        children: (0, r.jsx)("path", {
+            fill: "string" == typeof u ? u : u.css,
             d: "M8 6a2 2 0 0 1-2 2H3a1 1 0 0 0 0 2h3a4 4 0 0 0 4-4V3a1 1 0 0 0-2 0v3ZM8 18a2 2 0 0 0-2-2H3a1 1 0 1 1 0-2h3a4 4 0 0 1 4 4v3a1 1 0 1 1-2 0v-3ZM18 8a2 2 0 0 1-2-2V3a1 1 0 1 0-2 0v3a4 4 0 0 0 4 4h3a1 1 0 1 0 0-2h-3ZM16 18c0-1.1.9-2 2-2h3a1 1 0 1 0 0-2h-3a4 4 0 0 0-4 4v3a1 1 0 1 0 2 0v-3Z",
-            className: u,
+            className: o,
         }),
     });
 };
