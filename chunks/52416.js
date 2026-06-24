3157cad5fad41afe59ea7259d2cf2cd03ee0d0bc
DiscrapperManager <admin@minerpl.xyz>
2026-06-24 00:02:53 +0000

    canaryRelease: 567751
    Main scripts: 656
    Lazy Loaded Scripts: 4059
    Chunks:
     * Main Files: 11516
     * Scripts: 9683
    Strings: 25623
    Routes: 831
    Experiments Count: 37
    Apex Experiments Count: 311

--- chunks/52416.js
+++ chunks/52416.js
@@ -1,58 +1,64 @@
-n.d(t, { A: () => u });
-var a = n(627968);
-n(64700);
-var o = n(877624),
-    l = n(43105),
-    r = n(549996),
-    i = n(498470),
-    d = n(807098),
-    s = n(637706),
-    p = n(49999);
-function u(e) {
-    let { children: t, targetElementRef: n, dismissPopover: u } = e,
-        c = (0, r.c)(o.C.PREMIUM_TAB_POPOVER),
-        b =
-            null != c && "premiumTabPopover" === c.properties.properties.oneofKind
-                ? c.properties.properties.premiumTabPopover
+n.d(t, { A: () => b });
+var o = n(627968),
+    a = n(64700),
+    l = n(877624),
+    r = n(338854),
+    i = n(43105),
+    d = n(59318),
+    s = n(549996),
+    u = n(498470),
+    p = n(807098),
+    c = n(637706),
+    m = n(49999);
+function b(e) {
+    let { children: t, targetElementRef: n, dismissPopover: b } = e,
+        _ = (0, s.c)(l.C.PREMIUM_TAB_POPOVER),
+        f =
+            null != _ && "premiumTabPopover" === _.properties.properties.oneofKind
+                ? _.properties.properties.premiumTabPopover
                 : null,
-        m = (0, d.T)(b?.asset);
-    if (null == b) return t;
-    let { icon: f } = (0, i.x)({ buttonAction: b.button?.buttonAction }),
-        g =
-            b.button?.copy != null
-                ? [
-                      {
-                          text: b.button.copy,
-                          variant: "expressive",
-                          onClick: (0, i.h)({
-                              buttonAction: b.button.buttonAction,
-                              deeplinkSection: b.button.deeplinkSection,
-                              applicationId: b.button.navigableStorefrontApplicationId?.value,
-                              onClose: () => u(p.i.USER_DISMISS),
-                          }),
-                          icon: f,
-                      },
-                  ]
+        A = (0, p.T)(f?.asset),
+        T = a.useMemo(() => null != A && (0, d.r1)(A), [A]);
+    if (null == f) return t;
+    let { icon: g } = (0, u.x)({ buttonAction: f.button?.buttonAction }),
+        I = () => b(m.i.USER_DISMISS),
+        N =
+            f.button?.copy != null
+                ? {
+                      text: f.button.copy,
+                      variant: "expressive",
+                      onClick: (0, u.h)({
+                          buttonAction: f.button.buttonAction,
+                          deeplinkSection: f.button.deeplinkSection,
+                          applicationId: f.button.navigableStorefrontApplicationId?.value,
+                          onClose: I,
+                      }),
+                      icon: g,
+                  }
                 : void 0,
-        _ = (0, s.C)(b.helpArticle, b.helpArticleId);
-    return (0, a.jsxs)(a.Fragment, {
-        children: [
-            (0, a.jsx)("div", { children: t }),
-            (0, a.jsx)(l.A, {
-                targetElementRef: n,
-                title: b.header,
-                body: b.body,
-                textLink: null != _ ? { text: _.linkText, link: _.url } : void 0,
-                graphic: null != m ? { type: "image", src: m } : void 0,
-                size: "md",
-                align: "top",
-                alignmentStrategy: "edge",
-                position: "right",
-                caretConfig: { align: "start" },
-                actions: g,
-                onRequestClose: () => u(p.i.USER_DISMISS),
-                gradientColor: "nitro-pink",
-            }),
-        ],
-    });
+        E = (0, c.C)(f.helpArticle, f.helpArticleId),
+        y = null != E ? { text: E.linkText, link: E.url } : void 0,
+        S = {
+            targetElementRef: n,
+            title: f.header,
+            body: f.body,
+            textLink: y,
+            position: "right",
+            alignmentStrategy: "edge",
+            align: "top",
+            caretConfig: { align: "start" },
+            onRequestClose: I,
+        };
+    if (T) {
+        let e = { ...S, assetUrl: A ?? "", action: N, disableMediaViewer: !0 };
+        return (0, o.jsxs)(o.Fragment, { children: [(0, o.jsx)("div", { children: t }), (0, o.jsx)(r.H, { ...e })] });
+    }
+    let C = {
+        ...S,
+        graphic: null != A ? { type: "image", src: A, aspectRatio: "16/9" } : void 0,
+        size: "md",
+        actions: null != N ? [N] : void 0,
+        gradientColor: "nitro-pink",
+    };
+    return (0, o.jsxs)(o.Fragment, { children: [(0, o.jsx)("div", { children: t }), (0, o.jsx)(i.A, { ...C })] });
 }
