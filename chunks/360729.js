2e2e364e30330fb856b40a694ab046851a0d7ee1
DiscrapperManager <admin@minerpl.xyz>
2026-06-24 18:04:40 +0000

    canaryRelease: 568155
    Main scripts: 658
    Lazy Loaded Scripts: 4066
    Chunks:
     * Main Files: 11548
     * Scripts: 9693
    Strings: 25645
    Routes: 832
    Experiments Count: 37
    Apex Experiments Count: 313

--- chunks/360729.js
+++ chunks/360729.js
@@ -4,6 +4,9 @@ let i = (0, n(600975).C)({
     kind: "guild",
     id: "2026-06_guild_rooms",
     label: "Guild Rooms",
-    defaultConfig: { enabled: !1 },
-    treatments: [{ id: 1, label: "Enable Guild Rooms in this guild", config: { enabled: !0 } }],
+    defaultConfig: { enabled: !1, experimental: !1 },
+    treatments: [
+        { id: 1, label: "Enable Guild Rooms in this guild", config: { enabled: !0, experimental: !1 } },
+        { id: 2, label: "Enable Guild Rooms in this guild", config: { enabled: !0, experimental: !0 } },
+    ],
 });
