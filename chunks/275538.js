373ffd28f509cda674650907aebd6d6ba6f2dde6
DiscrapperManager <admin@minerpl.xyz>
2026-06-24 19:10:15 +0000

    canaryRelease: 568267
    Main scripts: 658
    Lazy Loaded Scripts: 4066
    Chunks:
     * Main Files: 11550
     * Scripts: 9696
    Strings: 25655
    Routes: 832
    Experiments Count: 37
    Apex Experiments Count: 312

--- chunks/275538.js
+++ chunks/275538.js
@@ -1,45 +1,45 @@
-s.d(t, { A: () => q });
-var n,
-    r = s(627968),
-    a = s(64700),
-    i = s(702841),
-    l = s(765178),
-    o = s(289873),
-    c = s(834730),
-    d = s(331322),
-    u = s(123292),
-    h = s(778712),
-    p = s(707554),
-    m = s(685094),
-    N = s(830215),
-    E = s(730134);
+n.d(t, { A: () => q });
+var s,
+    r = n(627968),
+    a = n(64700),
+    i = n(702841),
+    l = n(765178),
+    o = n(289873),
+    c = n(834730),
+    d = n(331322),
+    u = n(123292),
+    h = n(778712),
+    p = n(707554),
+    m = n(685094),
+    N = n(830215),
+    E = n(730134);
 function y() {
     return "u" < typeof document || "visible" === document.visibilityState;
 }
-var A = s(854378),
-    f = s(414121),
+var A = n(854378),
+    f = n(414121),
     g =
-        (((n = {})[(n.INITIALIZING = 0)] = "INITIALIZING"),
-        (n[(n.PENDING_REMOTE_INIT = 1)] = "PENDING_REMOTE_INIT"),
-        (n[(n.PENDING_TICKET = 2)] = "PENDING_TICKET"),
-        (n[(n.PENDING_LOGIN = 3)] = "PENDING_LOGIN"),
-        (n[(n.FINISH = 4)] = "FINISH"),
-        n),
-    w = s(158390),
-    I = s(636537),
-    x = s(765548),
-    C = s(626584),
-    v = s(751124),
-    S = s(625494);
-s(393431), s(532706), s(42231), s(232424), s(949626), s(767709), s(65162), s(508300);
-var _ = s(284009),
-    j = s.n(_),
-    b = s(889227);
+        (((s = {})[(s.INITIALIZING = 0)] = "INITIALIZING"),
+        (s[(s.PENDING_REMOTE_INIT = 1)] = "PENDING_REMOTE_INIT"),
+        (s[(s.PENDING_TICKET = 2)] = "PENDING_TICKET"),
+        (s[(s.PENDING_LOGIN = 3)] = "PENDING_LOGIN"),
+        (s[(s.FINISH = 4)] = "FINISH"),
+        s),
+    w = n(158390),
+    I = n(636537),
+    x = n(765548),
+    C = n(626584),
+    v = n(751124),
+    S = n(625494);
+n(393431), n(532706), n(42231), n(232424), n(949626), n(767709), n(65162), n(508300);
+var _ = n(284009),
+    j = n.n(_),
+    b = n(889227);
 async function T(e, t) {
-    let s = (t = await G.decryptEncodedCiphertext(e, t)).match(/^(\d+):(\d{1,4}):([a-zA-Z0-9_]+):(.*)$/);
-    if (null == s) throw Error("Invalid encoded user record.");
-    let [, n, r, a, i] = s;
-    return new b.A({ id: n, discriminator: r, avatar: "0" === a ? null : a, username: i });
+    let n = (t = await G.decryptEncodedCiphertext(e, t)).match(/^(\d+):(\d{1,4}):([a-zA-Z0-9_]+):(.*)$/);
+    if (null == n) throw Error("Invalid encoded user record.");
+    let [, s, r, a, i] = n;
+    return new b.A({ id: s, discriminator: r, avatar: "0" === a ? null : a, username: i });
 }
 function k(e) {
     return btoa(String.fromCharCode(...new Uint8Array(e)))
@@ -80,38 +80,38 @@ let G = new (class {
         );
     }
     async decryptEncodedCiphertext(e, t) {
-        let s = new TextDecoder(),
-            n = await O(e, P(t));
-        return s.decode(n);
+        let n = new TextDecoder(),
+            s = await O(e, P(t));
+        return n.decode(s);
     }
     async decryptNonce(e, t) {
         return k(await O(e, P(t)));
     }
     release() {}
 })();
-var R = s(652215);
+var R = n(652215);
 let D = new C.A("useAuthWebsocket");
-var M = s(293731),
-    F = s(495544),
-    K = s(723702),
-    W = s(427262),
-    U = s(19575),
-    $ = s(818348),
-    B = s(375708),
-    H = s(611008),
-    z = s(818050);
-let V = s(906118);
+var M = n(293731),
+    F = n(495544),
+    K = n(723702),
+    W = n(427262),
+    U = n(19575),
+    $ = n(818348),
+    B = n(375708),
+    H = n(611008),
+    z = n(818050);
+let V = n(906118);
 function Z(e) {
     let { text: t = "" } = e,
-        [s, n] = a.useState(!1);
+        [n, s] = a.useState(!1);
     a.useEffect(() => {
         let e = new Image();
-        (e.src = V), (e.onload = () => n(!0)), (e.onerror = () => n(!0));
+        (e.src = V), (e.onload = () => s(!0)), (e.onerror = () => s(!0));
     }, [V]),
         a.useEffect(() => {
-            s && l.O.announce(B.intl.string(B.t.j2p125));
-        }, [s]);
-    let i = "" !== t && s;
+            n && l.O.announce(B.intl.string(B.t.j2p125));
+        }, [n]);
+    let i = "" !== t && n;
     return (0, r.jsx)("div", {
         className: H.Ac,
         role: i ? "img" : void 0,
@@ -137,14 +137,14 @@ function Z(e) {
     });
 }
 function Q(e) {
-    let { className: t, children: s } = e;
-    return (0, r.jsx)(c.E, { variant: "text-md/normal", color: "text-feedback-critical", className: t, children: s });
+    let { className: t, children: n } = e;
+    return (0, r.jsx)(c.E, { variant: "text-md/normal", color: "text-feedback-critical", className: t, children: n });
 }
 function J(e) {
     let {
         state: t,
-        cancel: s,
-        errorMessage: n,
+        cancel: n,
+        errorMessage: s,
         conditionalMediationAbortController: a,
         isPasswordlessActive: i,
         isMultiAccount: l,
@@ -158,8 +158,8 @@ function J(e) {
                         text: t.step === g.PENDING_REMOTE_INIT ? `https://discord.com/ra/${t.fingerprint}` : "",
                     }),
                     (0, r.jsx)(A.hE, { className: z.QB, children: B.intl.string(B.t.UPiHaL) }),
-                    null != n
-                        ? (0, r.jsx)(Q, { children: n })
+                    null != s
+                        ? (0, r.jsx)(Q, { children: s })
                         : (0, r.jsx)(A.tK, { children: B.intl.format(B.t["Qq+A6i"], {}) }),
                     (0, r.jsx)(d.B, {
                         padding: 8,
@@ -170,12 +170,12 @@ function J(e) {
                             onClick: () =>
                                 (function (e) {
                                     let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1],
-                                        s =
+                                        n =
                                             K.isPlatformEmbedded && U.Ay.supportsFeature(R.BYE.WEBAUTHN)
                                                 ? U.Ay.webAuthnAuthenticate
                                                 : M.J;
                                     N.A.authenticatePasswordless({
-                                        authenticateFunc: s,
+                                        authenticateFunc: n,
                                         conditionalMediationAbortController: e,
                                         isMultiAccount: t,
                                     }).catch(() => {});
@@ -203,7 +203,7 @@ function J(e) {
                         children: (0, r.jsx)(u.Q, {
                             text: B.intl.string(B.t.nOOhwo),
                             textVariant: "text-sm/normal",
-                            onClick: s,
+                            onClick: n,
                         }),
                     }),
                 ],
@@ -215,11 +215,11 @@ function J(e) {
     }
 }
 function q(e) {
-    let { onAuthenticateSuccess: t, conditionalMediationAbortController: s, isMultiAccount: n = !1 } = e,
+    let { onAuthenticateSuccess: t, conditionalMediationAbortController: n, isMultiAccount: s = !1 } = e,
         l = (0, i.bG)([F.default], () => F.default.getIsPasswordlessActive()),
         { state: o, cancel: c } = (function (e, t) {
-            let s = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
-                [n, r] = a.useState(0),
+            let n = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
+                [s, r] = a.useState(0),
                 [i, l] = a.useState(!1),
                 [o, c] = a.useState({ step: g.INITIALIZING }),
                 d = a.useRef(null),
@@ -246,10 +246,14 @@ function q(e) {
                         (D.info("reconnecting, now that document is visible"), l(!1), r((e) => e + 1));
                 }, [o, t, i, l]),
                 a.useEffect(() => {
-                    let t = Date.now(),
-                        n = (e) => `[${Date.now() - t}ms] ${e}`,
-                        r = (e) => D.info(n(e)),
-                        a = `${window.GLOBAL_ENV.REMOTE_AUTH_ENDPOINT}/?v=2`;
+                    let t = Date.now();
+                    function s(e) {
+                        return `[${Date.now() - t}ms] ${e}`;
+                    }
+                    function r(e) {
+                        return D.info(s(e));
+                    }
+                    let a = `${window.GLOBAL_ENV.REMOTE_AUTH_ENDPOINT}/?v=2`;
                     a.startsWith("//") && (a = `wss:${a}`);
                     let i = (0, v.A)(a);
                     D.info(`[0ms] connecting to ${a}`);
@@ -262,93 +266,90 @@ function q(e) {
                         if (null != l) return l;
                         throw Error("No key pair set");
                     }
-                    let f = () => {
-                            y
-                                ? ((y = !1), i.send(JSON.stringify({ op: "heartbeat" })))
-                                : (r("heartbeat timeout, reconnecting."), i.close(), p());
-                        },
-                        w = async (t) => {
-                            let { data: a } = t,
-                                l = JSON.parse(a);
-                            switch (l.op) {
-                                case "nonce_proof": {
-                                    let e = l.encrypted_nonce,
-                                        t = await G.decryptNonce(A(), e);
-                                    r("computed nonce proof"), i.send(JSON.stringify({ op: "nonce_proof", nonce: t }));
-                                    return;
-                                }
-                                case "pending_remote_init": {
-                                    u.succeed(), S._.dispatch(R.jej.WAVE_EMPHASIZE);
-                                    let e = await G.publicKeyFingerprint(A());
-                                    if (e !== l.fingerprint) throw Error(`bad fingerprint ${e} !== ${l.fingerprint}`);
-                                    r("handshake complete awaiting remote auth."),
-                                        c({ step: g.PENDING_REMOTE_INIT, fingerprint: e });
-                                    return;
-                                }
-                                case "pending_login": {
-                                    let t = l.ticket;
-                                    if (null == t) return void p();
-                                    c({ step: g.PENDING_LOGIN, ticket: t }),
-                                        I.Bo.post({
-                                            url: R.Rsh.REMOTE_AUTH_LOGIN,
-                                            body: { ticket: t },
-                                            oldFormErrors: !0,
-                                            rejectWithError: !0,
-                                        })
-                                            .then(async (t) => {
-                                                if (null == d.current) return void p();
-                                                let n = await G.decryptEncodedCiphertext(
-                                                        d.current,
-                                                        t.body.encrypted_token,
-                                                    ),
-                                                    r = await G.publicKeyFingerprint(d.current);
-                                                s ? await N.A.switchAccountToken(n) : await N.A.loginToken(n, !1), e(r);
-                                            })
-                                            .catch(() => p());
-                                    return;
-                                }
-                                case "pending_ticket": {
-                                    S._.dispatch(R.jej.WAVE_EMPHASIZE),
-                                        r("remote auth handshake started, awaiting ticket/cancel.");
-                                    let e = l.encrypted_user_payload,
-                                        t = await T(A(), e);
-                                    c({ step: g.PENDING_TICKET, user: t });
-                                    return;
-                                }
-                                case "cancel":
-                                    r("remote auth handshake cancelled."), h();
-                                    return;
-                                case "hello": {
-                                    r(`got hello, auth timeout=${l.timeout_ms}ms`);
-                                    let e = l.heartbeat_interval;
-                                    E = setTimeout(
-                                        () => {
-                                            (E = null), f(), (m = setInterval(f, e));
-                                        },
-                                        Math.floor(e * Math.random()),
-                                    );
-                                    return;
-                                }
-                                case "heartbeat_ack":
-                                    y = !0;
-                                    return;
-                                default:
-                                    D.warn(n("received unsupported message"));
+                    function f() {
+                        y
+                            ? ((y = !1), i.send(JSON.stringify({ op: "heartbeat" })))
+                            : (r("heartbeat timeout, reconnecting."), i.close(), p());
+                    }
+                    async function w(t) {
+                        let { data: a } = t,
+                            l = JSON.parse(a);
+                        switch (l.op) {
+                            case "nonce_proof": {
+                                let e = l.encrypted_nonce,
+                                    t = await G.decryptNonce(A(), e);
+                                r("computed nonce proof"), i.send(JSON.stringify({ op: "nonce_proof", nonce: t }));
+                                return;
                             }
-                        },
-                        x = async () => {
-                            (l = await G.generateRsaKeyPair()), (o = await G.serializePublicKey(l));
-                            let e = await G.publicKeyFingerprint(l);
-                            r(`connected, handshaking with fingerprint: ${e}`),
-                                i.send(JSON.stringify({ op: "init", encoded_public_key: o })),
-                                (d.current = l);
-                        },
-                        C = (e) => {
-                            r(`disconnected, code: ${e.code} ${e.reason}`), p();
-                        },
-                        _ = (e) => {
-                            r(`disconnected, error: ${JSON.stringify(e)}`), p();
-                        };
+                            case "pending_remote_init": {
+                                u.succeed(), S._.dispatch(R.jej.WAVE_EMPHASIZE);
+                                let e = await G.publicKeyFingerprint(A());
+                                if (e !== l.fingerprint) throw Error(`bad fingerprint ${e} !== ${l.fingerprint}`);
+                                r("handshake complete awaiting remote auth."),
+                                    c({ step: g.PENDING_REMOTE_INIT, fingerprint: e });
+                                return;
+                            }
+                            case "pending_login": {
+                                let t = l.ticket;
+                                if (null == t) return void p();
+                                c({ step: g.PENDING_LOGIN, ticket: t }),
+                                    I.Bo.post({
+                                        url: R.Rsh.REMOTE_AUTH_LOGIN,
+                                        body: { ticket: t },
+                                        oldFormErrors: !0,
+                                        rejectWithError: !0,
+                                    })
+                                        .then(async (t) => {
+                                            if (null == d.current) return void p();
+                                            let s = await G.decryptEncodedCiphertext(d.current, t.body.encrypted_token),
+                                                r = await G.publicKeyFingerprint(d.current);
+                                            n ? await N.A.switchAccountToken(s) : await N.A.loginToken(s, !1), e(r);
+                                        })
+                                        .catch(() => p());
+                                return;
+                            }
+                            case "pending_ticket": {
+                                S._.dispatch(R.jej.WAVE_EMPHASIZE),
+                                    r("remote auth handshake started, awaiting ticket/cancel.");
+                                let e = l.encrypted_user_payload,
+                                    t = await T(A(), e);
+                                c({ step: g.PENDING_TICKET, user: t });
+                                return;
+                            }
+                            case "cancel":
+                                r("remote auth handshake cancelled."), h();
+                                return;
+                            case "hello": {
+                                r(`got hello, auth timeout=${l.timeout_ms}ms`);
+                                let e = l.heartbeat_interval;
+                                E = setTimeout(
+                                    () => {
+                                        (E = null), f(), (m = setInterval(f, e));
+                                    },
+                                    Math.floor(e * Math.random()),
+                                );
+                                return;
+                            }
+                            case "heartbeat_ack":
+                                y = !0;
+                                return;
+                            default:
+                                D.warn(s("received unsupported message"));
+                        }
+                    }
+                    let x = async () => {
+                        (l = await G.generateRsaKeyPair()), (o = await G.serializePublicKey(l));
+                        let e = await G.publicKeyFingerprint(l);
+                        r(`connected, handshaking with fingerprint: ${e}`),
+                            i.send(JSON.stringify({ op: "init", encoded_public_key: o })),
+                            (d.current = l);
+                    };
+                    function C(e) {
+                        r(`disconnected, code: ${e.code} ${e.reason}`), p();
+                    }
+                    function _(e) {
+                        r(`disconnected, error: ${JSON.stringify(e)}`), p();
+                    }
                     return (
                         i.addEventListener("open", x),
                         i.addEventListener("message", w),
@@ -367,7 +368,7 @@ function q(e) {
                                 null != m && clearInterval(m);
                         }
                     );
-                }, [h, e, n, u, p, s]),
+                }, [h, e, s, u, p, n]),
                 { state: o, cancel: h }
             );
         })(
@@ -389,7 +390,7 @@ function q(e) {
                     e
                 );
             })(),
-            n,
+            s,
         ),
         d = (function (e) {
             switch (e) {
@@ -413,9 +414,9 @@ function q(e) {
                 children: (0, r.jsx)(J, {
                     state: o,
                     cancel: c,
-                    conditionalMediationAbortController: s,
+                    conditionalMediationAbortController: n,
                     isPasswordlessActive: l,
-                    isMultiAccount: n,
+                    isMultiAccount: s,
                 }),
             }),
         }),
