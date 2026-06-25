5ba507ae8b5fc8804e44915950840953d261a3d4
DiscrapperManager <admin@minerpl.xyz>
2026-06-25 17:44:16 +0000

    noisesSuppression 569074

--- chunks/70191.js
+++ chunks/70191.js
@@ -1,6 +1,5 @@
-"use strict";
-n.d(t, { A: () => r });
+n.d(t, { A: () => l });
 var i = n(652215);
-function r(e) {
+function l(e) {
     return (e.type === i.$pd.PLAYING || e.type === i.$pd.COMPETING) && null != e.application_id;
 }
