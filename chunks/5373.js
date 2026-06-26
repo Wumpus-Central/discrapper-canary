4614ae9f61b0e60fac2ed34f0323b41911e3967c
DiscrapperManager <admin@minerpl.xyz>
2026-06-26 20:29:14 +0000

    canaryRelease: 570265
    Main scripts: 307
    Lazy Loaded Scripts: 3913
    Chunks:
     * Main Files: 11488
     * Scripts: 8134
    Strings: 25685
    Routes: 832
    Experiments Count: 37
    Apex Experiments Count: 314

--- chunks/5373.js
+++ chunks/5373.js
@@ -5,7 +5,7 @@ var l,
     i = e(503698),
     t = e.n(i),
     u = e(460890),
-    m = e(563852),
+    m = e(214028),
     o = (((l = o || {}).XXSMALL = "xxsmall"), (l.XSMALL = "xsmall"), (l.SMALL = "small"), (l.LARGE = "large"), l);
 let d = "indeterminate";
 function c(a) {
@@ -17,48 +17,47 @@ function c(a) {
             foregroundColor: o = "var(--progressbar-indicator-background)",
             backgroundColor: c = "var(--progressbar-track-background)",
             foregroundGradientColor: g,
-            animate: b = !0,
-            stepNumber: p,
-            stepCount: v,
+            animate: p = !0,
+            stepNumber: v,
+            stepCount: b,
         } = a,
-        { i18n: h } = (0, u.G9)(),
-        x = null != l ? l : null != p && null != v ? (p / (v + 1)) * 100 : 0;
+        { i18n: x } = (0, u.G9)(),
+        h = null != l ? l : null != v && null != b ? (v / (b + 1)) * 100 : 0;
     r = null != g ? { background: `linear-gradient(to right, ${g[0]}, ${g[1]})` } : { backgroundColor: o };
     let k = s.useMemo(() => {
         let a = { role: "progressbar", "aria-label": "Progress Bar" };
-        return (
-            null != p && null != v
-                ? ((a["aria-valuenow"] = p),
-                  (a["aria-valuemin"] = 1),
-                  (a["aria-valuemax"] = v),
-                  (a["aria-label"] = h.STEP_INDICATOR(p, v)))
-                : null != l &&
-                  "number" == typeof l &&
-                  ((a["aria-valuenow"] = l),
-                  (a["aria-valuemin"] = 0),
-                  (a["aria-valuemax"] = 100),
-                  (a["aria-label"] = h.PERCENT_COMPLETE(Math.round(l)))),
-            a
-        );
-    }, [p, v, l, h]);
+        if (null != v && null != b)
+            (a["aria-valuenow"] = v),
+                (a["aria-valuemin"] = 1),
+                (a["aria-valuemax"] = b),
+                (a["aria-label"] = x.STEP_INDICATOR(v, b));
+        else if (null != l && "number" == typeof l) {
+            let r = Math.round(l);
+            (a["aria-valuenow"] = r),
+                (a["aria-valuemin"] = 0),
+                (a["aria-valuemax"] = 100),
+                (a["aria-valuetext"] = x.PERCENT_COMPLETE(r));
+        }
+        return a;
+    }, [v, b, l, x]);
     return (0, n.jsx)("div", {
         className: t()(e, m.progress, m[i]),
         style: { backgroundColor: c },
         ...k,
         children:
-            x !== d && "number" == typeof x
+            h !== d && "number" == typeof h
                 ? (0, n.jsx)("div", {
-                      className: t()(m.progressBar, m[i], { [m.animating]: b }),
-                      style: { transform: `translate3d(${Math.min(Math.max(0, x), 100) - 100}%, 0, 0)`, ...r },
+                      className: t()(m.progressBar, m[i], { [m.animating]: p }),
+                      style: { transform: `translate3d(${Math.min(Math.max(0, h), 100) - 100}%, 0, 0)`, ...r },
                   })
                 : (0, n.jsxs)(s.Fragment, {
                       children: [
                           (0, n.jsx)("div", {
-                              className: t()(m.indeterminateBar1, m[i], { [m.animating]: b }),
+                              className: t()(m.indeterminateBar1, m[i], { [m.animating]: p }),
                               style: r,
                           }),
                           (0, n.jsx)("div", {
-                              className: t()(m.indeterminateBar2, m[i], { [m.animating]: b }),
+                              className: t()(m.indeterminateBar2, m[i], { [m.animating]: p }),
                               style: r,
                           }),
                       ],
