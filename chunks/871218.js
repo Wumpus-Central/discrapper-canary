4de5bfda145d1c23a0bfb846f408a768d0010c17
DiscrapperManager <admin@minerpl.xyz>
2026-06-30 23:27:28 +0000

    canaryRelease: 571816
    Main scripts: 309
    Lazy Loaded Scripts: 3917
    Chunks:
     * Main Files: 11496
     * Scripts: 8172
    Strings: 25729
    Routes: 832
    Experiments Count: 36
    Apex Experiments Count: 318

--- chunks/871218.js
+++ chunks/871218.js
@@ -1,30 +1,29 @@
 "use strict";
-n.d(t, { A: () => N });
+n.d(t, { A: () => y });
 var i = n(439372),
-    r = n(695515),
-    s = n(158390),
-    a = n(636537),
-    o = n(941426),
-    l = n(228366),
-    u = n(927813),
-    c = n(38405),
-    d = n(577517);
-let _ = new o.Vy("ParentalConsentWarning"),
-    h = new s.A(5 * u.A.Millis.SECOND, 5 * u.A.Millis.MINUTE, !0),
+    r = n(158390),
+    s = n(636537),
+    a = n(941426),
+    o = n(228366),
+    l = n(927813),
+    u = n(38405),
+    c = n(577517);
+let d = new a.Vy("ParentalConsentWarning"),
+    _ = new r.A(5 * l.A.Millis.SECOND, 5 * l.A.Millis.MINUTE, !0),
+    h = null,
     f = null,
-    p = null,
-    E = 0;
-function m() {
-    null != p && (clearTimeout(p), (p = null));
+    p = 0;
+function E() {
+    null != f && (clearTimeout(f), (f = null));
 }
-function g() {
-    if (null != f) return f;
-    let e = E;
-    return (f = (async () => {
+function m() {
+    if (null != h) return h;
+    let e = p;
+    return (h = (async () => {
         try {
             var t;
-            let n = await a.Bo.get({ url: "/users/@me/parental-consent/warning", rejectWithError: !0 });
-            if (e !== E) return;
+            let n = await s.Bo.get({ url: "/users/@me/parental-consent/warning", rejectWithError: !0 });
+            if (e !== p) return;
             let i =
                 ((t = n.body),
                 {
@@ -32,53 +31,50 @@ function g() {
                     daysRemaining: "number" == typeof t.days_remaining ? t.days_remaining : null,
                     surfaces: Array.isArray(t.surfaces) ? t.surfaces : [],
                 });
-            h.succeed(), m(), l.h.dispatch({ type: "PARENTAL_CONSENT_WARNING_FETCH_SUCCESS", warning: i });
+            _.succeed(), E(), o.h.dispatch({ type: "PARENTAL_CONSENT_WARNING_FETCH_SUCCESS", warning: i });
         } catch (t) {
-            if (e !== E) return;
-            _.error("Failed to fetch parental-consent warning", t),
-                c.A.captureException(t, { tags: { source: "parental_consent_warning", step: "fetch_warning" } }),
-                null == p &&
-                    (p = setTimeout(() => {
-                        (p = null), A();
-                    }, h.fail()));
+            if (e !== p) return;
+            d.error("Failed to fetch parental-consent warning", t),
+                u.A.captureException(t, { tags: { source: "parental_consent_warning", step: "fetch_warning" } }),
+                null == f &&
+                    (f = setTimeout(() => {
+                        (f = null), g();
+                    }, _.fail()));
         } finally {
-            e === E && (f = null);
+            e === p && (h = null);
         }
     })());
 }
+async function g() {
+    null == h && null == f && c.A.shouldFetchToday() && (await m());
+}
 async function A() {
-    null == f && null == p && d.A.shouldFetchToday() && (await g());
+    E(), null != h && (p++, (h = null)), await m();
 }
-async function I() {
-    m(), null != f && (E++, (f = null)), await g();
-}
-var T = n(748209),
-    S = n(191627);
-function y(e) {
-    return e.some((e) => e.link_status === S.Ef.ACTIVE && e.link_type === S.QM.PARENT);
-}
-class C extends i.A {
+var I = n(748209),
+    T = n(191627);
+class S extends i.A {
     actions = {
         POST_CONNECTION_OPEN: () => {
-            d.A.getWarning()?.surfaces?.includes(T.x.BANNER) === !0 && y(Object.values(r.A.getLinkedUsers()))
-                ? I()
-                : A();
+            g();
         },
         WINDOW_FOCUS: (e) => {
             let { focused: t } = e;
-            t && A();
+            t && g();
         },
         WINDOW_VISIBILITY_CHANGE: (e) => {
             let { visible: t } = e;
-            t && A();
+            t && g();
         },
         FAMILY_CENTER_REQUEST_LINK_UPDATE_SUCCESS: (e) => {
             let { linkedUsers: t } = e;
-            y(t) && I();
+            c.A.getWarning()?.surfaces?.includes(I.x.BANNER) === !0 &&
+                t.some((e) => e.link_status === T.Ef.ACTIVE && e.link_type === T.QM.PARENT) &&
+                A();
         },
         LOGOUT: () => {
-            E++, m(), (f = null), h.succeed();
+            p++, E(), (h = null), _.succeed();
         },
     };
 }
-let N = new C();
+let y = new S();
