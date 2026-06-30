879fb81b715bacbc15da8abd55ca32c3344dc355
DiscrapperManager <admin@minerpl.xyz>
2026-06-30 22:24:16 +0000

    canaryRelease: 571766
    Main scripts: 309
    Lazy Loaded Scripts: 3917
    Chunks:
     * Main Files: 11495
     * Scripts: 8172
    Strings: 25727
    Routes: 832
    Experiments Count: 36
    Apex Experiments Count: 318

--- chunks/754998.js
+++ chunks/754998.js
@@ -1,16 +1,24 @@
 "use strict";
-n.d(t, { $: () => r });
-let i = (0, n(945810).mj)({
+n.d(t, { $: () => l });
+var i = n(17928),
+    r = n(462887),
+    s = n(945810),
+    a = n(363195);
+let o = (0, s.mj)({
     name: "2026-06-expressive-button-test",
     kind: "user",
-    defaultConfig: { buttonVariant: null },
-    variations: {
-        0: { buttonVariant: null },
-        1: { buttonVariant: "overlay-primary" },
-        2: { buttonVariant: "primary" },
-    },
+    defaultConfig: { buttonStyle: 0 },
+    variations: { 0: { buttonStyle: 0 }, 1: { buttonStyle: 1 }, 2: { buttonStyle: 2 } },
 });
-function r(e) {
-    let { buttonVariant: t } = i.useConfig({ location: e });
-    return t ?? void 0;
+function l(e) {
+    let { buttonStyle: t } = o.useConfig({ location: e }),
+        n = (0, i.bG)([a.A], () => (0, r.q)(a.A.theme));
+    switch (t) {
+        case 1:
+            return n ? "primary" : "overlay-primary";
+        case 2:
+            return "primary";
+        default:
+            return;
+    }
 }
