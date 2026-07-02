887144e99379db7bc8e007957caecdb78c9425cc
DiscrapperManager <admin@minerpl.xyz>
2026-07-02 19:02:08 +0000

    canaryRelease: 573052
    Main scripts: 310
    Lazy Loaded Scripts: 3921
    Chunks:
     * Main Files: 11531
     * Scripts: 8177
    Strings: 25751
    Routes: 832
    Experiments Count: 36
    Apex Experiments Count: 318

--- chunks/468403.js
+++ chunks/468403.js
@@ -1,44 +1,23 @@
-i.d(l, { default: () => h });
-var s = i(627968),
-    t = i(64700),
-    a = i(862482),
-    e = i(192308),
-    r = i(935462),
-    c = i(834730),
-    o = i(821609),
-    d = i(375708);
-function h(n) {
-    let { onConfirm: l, ...i } = n,
-        h = t.useCallback(() => {
-            l(), (0, e.closeAllModals)();
-        }, [l]);
-    return (0, s.jsxs)(r.EO, {
-        size: r.rI.SMALL,
-        ...i,
-        parentComponent: "DisableLandingPageWarningModal",
-        children: [
-            (0, s.jsx)(r.$m, {
-                children: (0, s.jsx)(c.E, {
-                    variant: "text-md/normal",
-                    style: { marginTop: 24 },
-                    children: d.intl.string(d.t.nmBUv7),
-                }),
-            }),
-            (0, s.jsxs)(r.jl, {
-                children: [
-                    (0, s.jsx)(o.$, {
-                        variant: "secondary",
-                        text: d.intl.string(d.t["ETE/oC"]),
-                        onClick: e.closeAllModals,
-                    }),
-                    (0, s.jsx)(a.$n, {
-                        color: a.XD.RED,
-                        onClick: h,
-                        style: { marginRight: 8 },
-                        children: d.intl.string(d.t.R9GHya),
-                    }),
-                ],
-            }),
+n.d(i, { default: () => c });
+var a = n(627968),
+    r = n(64700),
+    s = n(189213),
+    e = n(834730),
+    l = n(375708);
+function c(t) {
+    let { onConfirm: i, onClose: n, transitionState: c } = t,
+        o = r.useCallback(() => {
+            i(), n();
+        }, [i, n]);
+    return (0, a.jsx)(s.Modal, {
+        title: l.intl.string(l.t["5yF7e1"]),
+        size: "sm",
+        onClose: n,
+        transitionState: c,
+        actions: [
+            { variant: "secondary", text: l.intl.string(l.t["ETE/oC"]), onClick: n },
+            { variant: "critical-primary", text: l.intl.string(l.t.R9GHya), onClick: o },
         ],
+        children: (0, a.jsx)(e.E, { variant: "text-md/normal", children: l.intl.string(l.t.nmBUv7) }),
     });
 }
