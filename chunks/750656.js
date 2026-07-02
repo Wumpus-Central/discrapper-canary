ede82def5618aeecac223da9bbb5f2e0a5c55468
DiscrapperManager <admin@minerpl.xyz>
2026-07-02 19:57:51 +0000

    canaryRelease: 573105
    Main scripts: 310
    Lazy Loaded Scripts: 3921
    Chunks:
     * Main Files: 11536
     * Scripts: 8173
    Strings: 25751
    Routes: 832
    Experiments Count: 36
    Apex Experiments Count: 318

--- chunks/750656.js
+++ chunks/750656.js
@@ -1,13 +1,15 @@
 "use strict";
 n.d(t, {
     Jl: () => l,
-    LP: () => d,
-    NU: () => _,
+    LP: () => h,
+    NU: () => f,
+    PS: () => _,
     Wf: () => u,
     _k: () => a,
     gz: () => o,
-    ii: () => h,
-    lU: () => c,
+    ii: () => E,
+    kp: () => c,
+    lU: () => d,
     re: () => s,
 });
 var i = n(508425),
@@ -28,28 +30,30 @@ let s = [i.z.SOLID, i.z.GRADIENT, i.z.NEON, i.z.TOON, i.z.POP],
         [0xf5983c, 0xe55ae7],
         [0xe4541e, 6674404],
     ].map((e) => ({ colors: e, name: "" })),
-    c = [
+    d = [
         [0xf4a7d8, 9626600, 0xf8f59a, 0xc9f7d8],
         [0xff6b9a, 0xffd85a, 5629578, 7064575],
         [0xff8a5c, 0xffd15a, 0xf06aa6, 0x9b6bff],
         [0xd83a5f, 0xc94bcb, 9133311, 0xb06cff],
         [5227519, 7268312, 9075967, 0xb7f5ff],
     ],
-    d = 100,
-    _ = [
+    c = 0.78,
+    _ = 0.72,
+    h = 100,
+    f = [
         [0xfe9242, 3534206, 0xffe047, 0xff4cd2, 5793266],
-        [0xff3d3d, 0xff8a2a, 0xffd447, 0xff4fa3, 8086015],
+        [0xff3d3d, 0xff8a2a, 0xffd447, 0xff4fa3, 5621759],
         [7997702, 0xc81e12, 0xf45a18, 0xff9500, 0xffd447],
         [8316888, 8178687, 8359167, 0xb56dff, 0xf38ad8],
         [3528287, 1497266, 2147829, 2850047, 7032319],
         [0xa7e8a1, 0xd9f08c, 0xffe08a, 0xffc4a8, 0xf3b6d9],
     ];
-function h(e) {
+function E(e) {
     switch (e) {
         case i.z.GUMMY:
-            return c;
+            return d;
         case i.z.PRISM:
-            return _;
+            return f;
         case i.z.GRADIENT:
         case i.z.GLOW:
             return u.map((e) => e.colors);
