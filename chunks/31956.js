6a73bf9c3be806c21538f57affd60d6ad641ab2c
DiscrapperManager <admin@minerpl.xyz>
2026-07-03 00:53:28 +0000

    canaryRelease: 573385
    Main scripts: 310
    Lazy Loaded Scripts: 3921
    Chunks:
     * Main Files: 11537
     * Scripts: 8176
    Strings: 25756
    Routes: 832
    Experiments Count: 36
    Apex Experiments Count: 319

--- chunks/31956.js
+++ chunks/31956.js
@@ -1,34 +1,33 @@
-"use strict";
-n.d(t, { A: () => o });
+n.d(t, { A: () => c });
 var i = n(64700),
     r = n(702841),
-    s = n(47675),
-    a = n(590180);
-function o(e) {
-    let { skuId: t, openedAt: n, context: o, analyticsLocations: l } = e,
-        u = (0, r.bG)([a.A], () => a.A.getProductFetch(t)),
+    l = n(47675),
+    s = n(590180);
+function c(e) {
+    let { skuId: t, openedAt: n, context: c, analyticsLocations: a } = e,
+        o = (0, r.bG)([s.A], () => s.A.getProductFetch(t)),
+        u = (0, i.useRef)(void 0),
         d = (0, i.useRef)(void 0),
-        c = (0, i.useRef)(void 0),
         _ = (0, i.useRef)(void 0),
-        h = (0, i.useRef)(!1);
+        I = (0, i.useRef)(!1);
     (0, i.useEffect)(() => {
         if (null == t || null == n) return;
-        (c.current !== t || _.current !== n) &&
-            ((c.current = t), (_.current = n), (h.current = !1), (d.current = void 0));
+        (d.current !== t || _.current !== n) &&
+            ((d.current = t), (_.current = n), (I.current = !1), (u.current = void 0));
         let e = Date.now();
-        (d.current ??= e - n),
-            u?.state !== "success" ||
-                h.current ||
-                ((h.current = !0),
-                (0, s.wd)({
+        (u.current ??= e - n),
+            o?.state !== "success" ||
+                I.current ||
+                ((I.current = !0),
+                (0, l.wd)({
                     profileUi: "PROFILE_FRAME",
-                    timeToInteractiveMs: d.current,
+                    timeToInteractiveMs: u.current,
                     timeToLoadMs: e - n,
-                    timeToFetchMs: null != u.startedAt && null != u.endedAt ? u.endedAt - u.startedAt : void 0,
+                    timeToFetchMs: null != o.startedAt && null != o.endedAt ? o.endedAt - o.startedAt : void 0,
                     viewStartedAt: n,
-                    fetchStartedAt: u.startedAt,
-                    analyticsLocations: l,
-                    ...o,
+                    fetchStartedAt: o.startedAt,
+                    analyticsLocations: a,
+                    ...c,
                 }));
-    }, [t, n, u]);
+    }, [t, n, o]);
 }
