550a94784eef7f42fe279a1d037819fcfc68edff
DiscrapperManager <admin@minerpl.xyz>
2026-07-07 00:44:16 +0000

    canaryRelease: 574003
    Main scripts: 300
    Lazy Loaded Scripts: 4213
    Chunks:
     * Main Files: 10659
     * Scripts: 9052
    Strings: 25775
    Routes: 834
    Experiments Count: 36
    Apex Experiments Count: 321

--- chunks/533772.js
+++ chunks/533772.js
@@ -1,51 +1,38 @@
 l.d(t, { A: () => m, O: () => d });
-var a = l(627968),
-    n = l(503698),
-    r = l.n(n),
+var n = l(627968),
+    a = l(503698),
+    r = l.n(a),
     i = l(834730),
-    s = l(318254),
-    o = l(430825),
+    o = l(318254),
+    s = l(430825),
     c = l(375708),
     u = l(979393);
 function d(e) {
-    let { variant: t, color: l = "text-default", className: n, orbPrice: r } = e;
-    return (0, a.jsx)(i.E, {
-        className: n,
+    let { variant: t, color: l = "text-default", className: a, orbPrice: r } = e;
+    return (0, n.jsx)(i.E, {
+        className: a,
         variant: t,
         color: l,
         lineClamp: 1,
-        children: c.intl.format(o.default["0oZGvW"], {
+        children: c.intl.format(s.default["0oZGvW"], {
             orbPrice: r,
-            orbIconHook: () => (0, a.jsx)(s.C, { className: u.f, size: "xs", color: "currentColor" }, "orbs-icon"),
+            orbIconHook: () => (0, n.jsx)(o.C, { className: u.f, size: "xs", color: "currentColor" }, "orbs-icon"),
         }),
     });
 }
 function m(e) {
-    let { orbsGate: t, className: l, orbPrice: n, fiatPrice: s, textDefaultColor: m = "text-default" } = e;
+    let { orbsGate: t, className: l, orbPrice: a, fiatPrice: o, textDefaultColor: m = "text-default" } = e;
     return "HIDDEN" === t
         ? null
-        : "CAN_CHECKOUT" === t
-          ? (0, a.jsx)(i.E, {
-                className: r()(u.Q, l),
-                variant: "text-md/medium",
-                color: "text-muted",
-                lineClamp: 1,
-                children: c.intl.format(o.default.Kkah9a, {
-                    price1Hook: () => (0, a.jsx)(d, { variant: "text-md/bold", color: m, orbPrice: n }, "orbs-price"),
-                    price2Hook: () =>
-                        (0, a.jsx)(i.E, { variant: "text-md/bold", color: m, lineClamp: 1, children: s }, "fiat-price"),
-                }),
-            })
-          : (0, a.jsx)(i.E, {
-                className: r()(u.Q, l),
-                variant: "text-md/medium",
-                color: "text-muted",
-                lineClamp: 1,
-                children: c.intl.format(o.default.Kkah9a, {
-                    price1Hook: () =>
-                        (0, a.jsx)(i.E, { variant: "text-md/bold", color: m, lineClamp: 1, children: s }, "fiat-price"),
-                    price2Hook: () =>
-                        (0, a.jsx)(d, { variant: "text-md/semibold", color: "text-muted", orbPrice: n }, "orbs-price"),
-                }),
-            });
+        : (0, n.jsx)(i.E, {
+              className: r()(u.Q, l),
+              variant: "text-md/medium",
+              color: "text-muted",
+              lineClamp: 1,
+              children: c.intl.format(s.default.Kkah9a, {
+                  price1Hook: () => (0, n.jsx)(d, { variant: "text-md/bold", color: m, orbPrice: a }, "orbs-price"),
+                  price2Hook: () =>
+                      (0, n.jsx)(i.E, { variant: "text-md/bold", color: m, lineClamp: 1, children: o }, "fiat-price"),
+              }),
+          });
 }
