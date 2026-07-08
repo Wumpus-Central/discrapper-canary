f2909e1e211aa195862679705020113517a7fe5b
DiscrapperManager <admin@minerpl.xyz>
2026-07-08 20:32:02 +0000

    canaryRelease: 575168
    Main scripts: 328
    Lazy Loaded Scripts: 4315
    Chunks:
     * Main Files: 10158
     * Scripts: 9219
    Strings: 25795
    Routes: 838
    Experiments Count: 36
    Apex Experiments Count: 322

--- chunks/99462.js
+++ chunks/99462.js
@@ -1,12 +1,11 @@
-"use strict";
-n.d(t, { Fu: () => _, GZ: () => u, __: () => A, rn: () => E });
+n.d(t, { Fu: () => _, GZ: () => u, __: () => d, rn: () => A });
 var i = n(496431),
-    r = n(773669),
-    a = n(975571),
+    l = n(773669),
+    r = n(975571),
     s = n(428262),
-    l = n(410516),
+    a = n(410516),
     o = n(202541),
-    d = n(652215),
+    E = n(652215),
     c = n(375708);
 function u(e, t, n, i) {
     switch (e) {
@@ -33,21 +32,21 @@ function u(e, t, n, i) {
 let _ = (e) => {
     let t = e.expires_at,
         n = (0, i.A)(null != t ? Date.parse(t) : 0, 1e3),
-        r = e?.subscription_trial?.sku_id;
-    return null == t || null == r
+        l = e?.subscription_trial?.sku_id;
+    return null == t || null == l
         ? null
         : u(
-              r,
+              l,
               n,
               (0, s.re)({
                   intervalType: e.subscription_trial?.interval,
                   intervalCount: e.subscription_trial?.interval_count,
               }),
-              a.A.getArticleURL(e.trial_id === o.yo ? d.MVz.NITRO_TRIAL_FOR_ALL : d.MVz.PREMIUM_TRIAL),
+              r.A.getArticleURL(e.trial_id === o.yo ? E.MVz.NITRO_TRIAL_FOR_ALL : E.MVz.PREMIUM_TRIAL),
           );
 };
-function E(e, t, n) {
-    let i = new Intl.NumberFormat(r.default.locale, {
+function A(e, t, n) {
+    let i = new Intl.NumberFormat(l.default.locale, {
         style: "percent",
         minimumFractionDigits: 0,
         maximumFractionDigits: 0,
@@ -61,8 +60,10 @@ function E(e, t, n) {
                 discountPercentage: i,
             });
 }
-let A = (e) => {
+let d = (e) => {
     let t = e.expiresAt,
         n = (0, i.A)(null != t ? t.getTime() : 0, 1e3);
-    return null == t ? null : E(n, Number(e.discount.amount), (0, l.hm)(e));
+    return null == t ? null : A(n, Number(e.discount.amount), (0, a.hm)(e));
 };
+(0, l.hm)(e);
+)}
