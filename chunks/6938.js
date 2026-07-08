b71bca954cde0ed6881dbf3a80052a8580c2073a
DiscrapperManager <admin@minerpl.xyz>
2026-07-08 17:50:26 +0000

    canaryRelease: 574984
    Main scripts: 328
    Lazy Loaded Scripts: 4314
    Chunks:
     * Main Files: 10156
     * Scripts: 9218
    Strings: 25790
    Routes: 837
    Experiments Count: 36
    Apex Experiments Count: 320

--- chunks/6938.js
+++ chunks/6938.js
@@ -150,7 +150,6 @@ class _ extends a.A {
     revision;
     orderLineItems;
     billingFacetRecord;
-    externalGatewayFacet;
     giftingFacet;
     subscriptionFacet;
     checkoutContextRecord;
@@ -163,7 +162,6 @@ class _ extends a.A {
             revision: e.revision,
             orderLineItems: e.order_line_items,
             billingFacetRecord: u.createFromOrder(e),
-            externalGatewayFacet: e.external_gateway_facet ?? null,
             giftingFacet: e.gifting_facet ?? null,
             checkoutContextRecord: d.createFromOrder(e),
             createdAt: e.created_at,
@@ -178,7 +176,6 @@ class _ extends a.A {
             (this.revision = e.revision),
             (this.orderLineItems = e.orderLineItems ?? []),
             (this.billingFacetRecord = e.billingFacetRecord ?? null),
-            (this.externalGatewayFacet = e.externalGatewayFacet ?? null),
             (this.giftingFacet = e.giftingFacet ?? null),
             (this.subscriptionFacet = e.subscriptionFacet ?? null),
             (this.checkoutContextRecord = e.checkoutContextRecord ?? null),
