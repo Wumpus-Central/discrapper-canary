23baa33a58cd3a893c3e860ebacce21f55490fb8
DiscrapperManager <admin@minerpl.xyz>
2026-07-08 11:33:48 +0000

    noisesSuppression 574793

--- chunks/82038.js
+++ chunks/82038.js
@@ -118,5 +118,3 @@ let A = new Set(["PageDown", "PageUp", "Home", "End", "ArrowUp", "ArrowDown", "A
             d._.dispatchToLastSubscribed(u.jej.TEXTAREA_FOCUS, { event: e, wasEnterPressed: _ });
         },
     };
-Pressed: m;
-})}}
