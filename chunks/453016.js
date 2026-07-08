3d2e0f4425fbb2362ca3b069fe84a3ce98669260
DiscrapperManager <admin@minerpl.xyz>
2026-07-08 10:33:49 +0000

    canaryRelease: 574542
    Main scripts: 302
    Lazy Loaded Scripts: 4212
    Chunks:
     * Main Files: 10661
     * Scripts: 9052
    Strings: 25787
    Routes: 836
    Experiments Count: 36
    Apex Experiments Count: 319

--- chunks/453016.js
+++ chunks/453016.js
@@ -1,28 +1,28 @@
-i.d(t, { _V: () => u, aS: () => d, y4: () => c });
-var n = i(627968);
-i(64700);
-var l = i(292666);
-if (21552 == i.j) var s = i(260598);
-var r = i(31457),
-    a = i(260197),
-    o = i(375708);
-function u(e) {
-    let { value: t, placeholder: i, onChange: s, disabled: r, autofocus: o } = e;
-    return (0, n.jsx)(l.k, { maxLength: a.cs, value: t ?? "", placeholder: i, onChange: s, disabled: r, autoFocus: o });
-}
-function d(e) {
-    let { formField: t } = e;
-    return (0, n.jsx)(r._H, { title: t.label, children: (0, n.jsx)(s.f, { value: t.response, disabled: !0 }) });
-}
+l.d(t, { _V: () => c, aS: () => o, y4: () => u });
+var s = l(627968);
+l(64700);
+var i = l(292666),
+    n = l(260598),
+    r = l(31457),
+    a = l(260197),
+    d = l(375708);
 function c(e) {
-    let { formField: t, autofocus: i, onChange: l } = e;
-    return (0, n.jsx)(r.cS, {
+    let { value: t, placeholder: l, onChange: n, disabled: r, autofocus: d } = e;
+    return (0, s.jsx)(i.k, { maxLength: a.cs, value: t ?? "", placeholder: l, onChange: n, disabled: r, autoFocus: d });
+}
+function o(e) {
+    let { formField: t } = e;
+    return (0, s.jsx)(r._H, { title: t.label, children: (0, s.jsx)(n.f, { value: t.response, disabled: !0 }) });
+}
+function u(e) {
+    let { formField: t, autofocus: l, onChange: i } = e;
+    return (0, s.jsx)(r.cS, {
         title: t.label,
-        children: (0, n.jsx)(u, {
-            onChange: l,
+        children: (0, s.jsx)(c, {
+            onChange: i,
             value: t.response,
-            autofocus: i,
-            placeholder: o.intl.string(o.t["Sqn+Wh"]),
+            autofocus: l,
+            placeholder: d.intl.string(d.t["Sqn+Wh"]),
         }),
     });
 }
