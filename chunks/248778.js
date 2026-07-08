83082645b943fe15be70c7892b1a3127a37e4509
DiscrapperManager <admin@minerpl.xyz>
2026-07-08 10:36:11 +0000

    canaryRelease: 574560
    Main scripts: 302
    Lazy Loaded Scripts: 4212
    Chunks:
     * Main Files: 10661
     * Scripts: 9052
    Strings: 25787
    Routes: 836
    Experiments Count: 36
    Apex Experiments Count: 319

--- chunks/248778.js
+++ chunks/248778.js
@@ -1,28 +1,21 @@
 "use strict";
-n.d(t, { DW: () => u, ux: () => c });
-var i = n(17928),
-    r = n(945810),
-    s = n(287809);
-let a = (0, r.mj)({
+n.d(t, { DW: () => a, ux: () => o });
+var i = n(945810);
+let r = (0, i.mj)({
         kind: "user",
         name: "2026-06-gummy-bears",
         defaultConfig: { enabled: !1 },
         variations: { 1: { enabled: !0 } },
     }),
-    o = (0, r.mj)({
+    s = (0, i.mj)({
         kind: "user",
         name: "2026-06-gummy-viewers",
         defaultConfig: { enabled: !1 },
         variations: { 1: { enabled: !0 } },
     });
-function l() {
-    return (0, i.bG)([s.default], () => s.default.getCurrentUser()?.isStaff() ?? !1);
+function a(e) {
+    return s.useConfig({ location: e }).enabled;
 }
-function u(e) {
-    let t = o.useConfig({ location: e });
-    return l() && t.enabled;
-}
-function c(e) {
-    let t = a.useConfig({ location: e });
-    return l() && t.enabled;
+function o(e) {
+    return r.useConfig({ location: e }).enabled;
 }
