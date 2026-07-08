6f79e749179f8efaa02d6b08cb9073e1e25a1e4f
DiscrapperManager <admin@minerpl.xyz>
2026-07-08 19:14:31 +0000

    canaryRelease: 575094
    Main scripts: 328
    Lazy Loaded Scripts: 4315
    Chunks:
     * Main Files: 10158
     * Scripts: 9219
    Strings: 25795
    Routes: 837
    Experiments Count: 36
    Apex Experiments Count: 322

--- chunks/573879.js
+++ chunks/573879.js
@@ -1,15 +1,16 @@
 "use strict";
-n.d(t, { Gl: () => r, JE: () => s, WX: () => o, ov: () => i });
+n.d(t, { Gl: () => r, JE: () => a, WX: () => l, ov: () => i });
 let i = "switch-accounts-modal",
     r = "multiaccount-login-modal",
-    s = "multiaccount_cta_tooltip_seen";
-var a,
-    o =
-        (((a = {}).YOU_ACCOUNT_ACTION_SHEET = "you_account_action_sheet"),
-        (a.CHOOSE_ACCOUNT = "choose_account"),
-        (a.MANAGE_ACCOUNTS_MODAL = "manage_accounts_modal"),
-        (a.MULTI_ACCOUNTS_LIST = "multi_accounts_list"),
-        (a.MULTI_ACCOUNT_MENU = "multi_account_menu"),
-        (a.PUSH_NOTIFICATION = "push_notification"),
-        (a.SHARE_EXTENSION = "share_extension"),
-        a);
+    a = "multiaccount_cta_tooltip_seen";
+var s,
+    l =
+        (((s = {}).YOU_ACCOUNT_ACTION_SHEET = "you_account_action_sheet"),
+        (s.CHOOSE_ACCOUNT = "choose_account"),
+        (s.MANAGE_ACCOUNTS_MODAL = "manage_accounts_modal"),
+        (s.MULTI_ACCOUNTS_LIST = "multi_accounts_list"),
+        (s.MULTI_ACCOUNT_MENU = "multi_account_menu"),
+        (s.PUSH_NOTIFICATION_INITIAL = "push_notification_initial"),
+        (s.PUSH_NOTIFICATION = "push_notification"),
+        (s.SHARE_EXTENSION = "share_extension"),
+        s);
