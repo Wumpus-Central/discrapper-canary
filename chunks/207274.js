294973672230b335d60fa397774107b396754a35
DiscrapperManager <admin@minerpl.xyz>
2026-07-09 18:07:55 +0000

    canaryRelease: 575680
    Main scripts: 328
    Lazy Loaded Scripts: 4301
    Chunks:
     * Main Files: 10164
     * Scripts: 9183
    Strings: 25810
    Routes: 838
    Experiments Count: 36
    Apex Experiments Count: 321

--- chunks/207274.js
+++ chunks/207274.js
@@ -11,42 +11,39 @@ var i = n(627968),
     p = n(792348),
     h = n(734057),
     m = n(451409),
-    A = n(287809),
-    x = n(360729);
+    A = n(309010),
+    x = n(287809),
+    g = n(360729);
 n(980504);
-var g = n(662731),
-    f = n(375708),
-    C = n(364103),
-    y = n(452933);
+var f = n(189090),
+    C = n(375708),
+    y = n(617031),
+    j = n(452933);
 function E(e) {
     let { channelId: t } = e,
         n = (0, o.bG)([h.A], () => h.A.getChannel(t)?.guild_id),
-        { experimental: a } = x.A.useExperiment({ guildId: n, location: "GuildRoom" }, { autoTrackExposure: !1 });
+        { experimental: a } = g.A.useExperiment({ guildId: n, location: "GuildRoom" }, { autoTrackExposure: !1 });
     l.useEffect(() => {
         a && (0, c.E7)({ disableAnalytics: !0 });
     }, [a]);
-    let r = (0, o.bG)([d.A], () => d.A.getSound("0", "1")),
-        u = (0, o.bG)([d.A], () => d.A.isPlayingSound("1")),
-        p = (0, i.jsx)("img", {
-            className: s()(C.wZ, { [C.mW]: u && a }),
-            src: y.A,
-            alt: f.intl.string(g.default["93KE7U"]),
-        });
-    return a && null != r
-        ? (0, i.jsx)(j, { channelId: t, sound: r, isQuackPlaying: u, children: p })
-        : (0, i.jsx)("div", { className: C.p9, children: p });
+    let s = (0, o.bG)([d.A], () => d.A.getSound("0", "1")),
+        r = (0, i.jsx)("img", { className: y.wZ, src: j.A, alt: C.intl.string(f.default["93KE7U"]) });
+    return a && null != s
+        ? (0, i.jsx)(v, { channelId: t, sound: s, children: r })
+        : (0, i.jsx)("div", { className: y.p9, children: r });
 }
-function j(e) {
+function v(e) {
     let { channelId: t, sound: n, children: l } = e,
-        a = (0, o.bG)([A.default], () => A.default.getCurrentUser()),
+        a = (0, o.bG)([x.default], () => x.default.getCurrentUser()),
         c = (0, o.bG)([h.A], () => h.A.getChannel(t)),
         d = (0, o.bG)([m.Ay], () => m.Ay.isDeaf()),
-        { playSoundboardSound: x } = (0, p.A)(n, t),
-        g = (0, u.Ir)(a, n, c) && (0, u.Au)(c) && !d;
+        g = (0, o.bG)([A.A], () => A.A.getVoiceChannelId() === t),
+        { playSoundboardSound: f } = (0, p.A)(n, t),
+        C = g && (0, u.Ir)(a, n, c) && (0, u.Au)(c) && !d;
     return (0, i.jsx)(r.D, {
-        className: s()(C.uA, g ? void 0 : C.r9),
-        "aria-disabled": !g,
-        onClick: g ? () => x() : void 0,
+        className: s()(y.uA, C ? void 0 : y.r9),
+        "aria-disabled": !C,
+        onClick: C ? () => f() : void 0,
         children: l,
     });
 }
--- chunks/617031.js
+++ chunks/617031.js
@@ -1,7 +1,1 @@
-e.exports = {
-    wZ: "duck__2dee0",
-    uA: "quackButton__2dee0",
-    mW: "quacking__2dee0",
-    p9: "duckContainer__2dee0",
-    r9: "disabled__2dee0",
-};
+e.exports = { wZ: "duck__2dee0", uA: "quackButton__2dee0", r9: "disabled__2dee0", p9: "duckContainer__2dee0" };
