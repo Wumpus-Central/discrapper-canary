77c2314a4d9261db14b38e600317a1c83a0a5331
DiscrapperManager <admin@minerpl.xyz>
2026-07-09 22:03:49 +0000

    canaryRelease: 575975
    Main scripts: 328
    Lazy Loaded Scripts: 4301
    Chunks:
     * Main Files: 10164
     * Scripts: 9183
    Strings: 25811
    Routes: 839
    Experiments Count: 36
    Apex Experiments Count: 321

--- chunks/885338.js
+++ chunks/885338.js
@@ -1,41 +1,41 @@
-e.r(l), e.d(l, { default: () => O });
-var t = e(627968),
-    i = e(64700),
-    r = e(503698),
-    s = e.n(r),
-    a = e(873263),
-    c = e(607399),
-    u = e(862482),
-    d = e(349288),
-    o = e(458518),
-    h = e(573648),
-    m = e(950198),
-    N = e(652215),
-    f = e(375708),
-    p = e(844733);
-let C = "https://dis.gd/Amazon-Music-Connection",
-    g = null != window.opener;
+l.r(t), l.d(t, { default: () => O });
+var e = l(627968),
+    i = l(64700),
+    r = l(503698),
+    s = l.n(r),
+    a = l(873263),
+    c = l(607399),
+    u = l(862482),
+    o = l(349288),
+    d = l(458518),
+    m = l(573648),
+    h = l(950198),
+    N = l(652215),
+    f = l(375708),
+    p = l(373821);
+let g = "https://dis.gd/Amazon-Music-Connection",
+    C = null != window.opener;
 function O() {
     let { type: n } = (0, a.g)(),
-        l = (0, m.mw)(n),
-        e = (0, o.o)().get("error-code"),
+        t = (0, h.mw)(n),
+        l = (0, d.o)().get("error-code"),
         r = i.useCallback(() => {
             window.close();
         }, []),
         O = i.useMemo(() => {
-            switch (l) {
+            switch (t) {
                 case N.fg2.AMAZON_MUSIC:
-                    return (0, t.jsx)("div", {
+                    return (0, e.jsx)("div", {
                         className: s()(p.iU, p.zH),
                         children: c.Fr
                             ? f.intl.format(f.t["0EikXi"], {
-                                  link: () => (0, t.jsx)(d.Anchor, { href: C, children: C }),
+                                  link: () => (0, e.jsx)(o.Anchor, { href: g, children: g }),
                               })
-                            : f.intl.format(f.t.hKHZhO, { link: () => (0, t.jsx)(d.Anchor, { href: C, children: C }) }),
+                            : f.intl.format(f.t.hKHZhO, { link: () => (0, e.jsx)(o.Anchor, { href: g, children: g }) }),
                     });
                 case N.fg2.YOUTUBE:
-                    if (e === N.t02.UNKNOWN_ACCOUNT.toString())
-                        return (0, t.jsx)("div", {
+                    if (l === N.t02.UNKNOWN_ACCOUNT.toString())
+                        return (0, e.jsx)("div", {
                             className: s()(p.iU, p.zH),
                             children: f.intl.format(f.t.nQPJdL, {
                                 link: "https://support.google.com/youtube/answer/1646861",
@@ -45,39 +45,44 @@ function O() {
                 default:
                     return null;
             }
-        }, [e, l]),
-        j = null != e ? Number(e) : null,
-        A = i.useMemo(() => {
-            if (null == l) return null;
-            let n = h.A.get(l);
-            switch (j) {
+        }, [l, t]),
+        U = null != l ? Number(l) : null,
+        j = i.useMemo(() => {
+            if (null == t) return null;
+            let n = m.A.get(t);
+            switch (U) {
                 case N.t02.INVALID_CONNECTION_WHITESPACE_NAME:
-                    return (0, t.jsx)("div", {
+                    return (0, e.jsx)("div", {
                         className: s()(p.iU, p.zH),
-                        children: f.intl.format(f.t["O7/vPw"], { name: n.name }),
+                        children:
+                            t === N.fg2.YOUTUBE
+                                ? f.intl.format(f.t.SXH74p, {
+                                      link: "https://support.google.com/youtube/answer/2657964",
+                                  })
+                                : f.intl.format(f.t["O7/vPw"], { name: n.name }),
                     });
                 case N.t02.CONNECTION_RATE_LIMITED:
-                    return (0, t.jsx)("div", { className: s()(p.iU, p.zH), children: f.intl.string(f.t["Y6Jo+p"]) });
+                    return (0, e.jsx)("div", { className: s()(p.iU, p.zH), children: f.intl.string(f.t["Y6Jo+p"]) });
                 case N.t02.TOO_MANY_CONNECTIONS:
-                    return (0, t.jsx)("div", { className: s()(p.iU, p.zH), children: f.intl.string(f.t.M92PQ3) });
+                    return (0, e.jsx)("div", { className: s()(p.iU, p.zH), children: f.intl.string(f.t.M92PQ3) });
                 default:
                     return null;
             }
-        }, [j, l]);
-    if (null == l) return null;
-    let U = h.A.get(l);
-    return (0, t.jsxs)(m.mP, {
-        platformType: l,
+        }, [U, t]);
+    if (null == t) return null;
+    let A = m.A.get(t);
+    return (0, e.jsxs)(h.mP, {
+        platformType: t,
         children: [
-            (0, t.jsx)("div", { className: s()(p.iU, p.z3), children: f.intl.format(f.t.E62dXo, { name: U.name }) }),
-            (0, t.jsx)("div", { className: s()(p.iU, p.zH), children: f.intl.string(f.t.Ra6lcp) }),
+            (0, e.jsx)("div", { className: s()(p.iU, p.z3), children: f.intl.format(f.t.E62dXo, { name: A.name }) }),
+            (0, e.jsx)("div", { className: s()(p.iU, p.zH), children: f.intl.string(f.t.Ra6lcp) }),
             O,
-            A,
-            g ? (0, t.jsx)(u.$n, { className: p.HM, onClick: r, children: f.intl.string(f.t.i4jeWR) }) : null,
-            null != e
-                ? (0, t.jsx)("div", {
+            j,
+            C ? (0, e.jsx)(u.$n, { className: p.HM, onClick: r, children: f.intl.string(f.t.i4jeWR) }) : null,
+            null != l
+                ? (0, e.jsx)("div", {
                       className: s()(p.iU, p.nf),
-                      children: f.intl.format(f.t.LElyIL, { errorCode: e }),
+                      children: f.intl.format(f.t.LElyIL, { errorCode: l }),
                   })
                 : null,
         ],
