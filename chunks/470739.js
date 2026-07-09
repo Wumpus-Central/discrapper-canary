97b45b393d5096b829b2353f8088b7f926624f9a
DiscrapperManager <admin@minerpl.xyz>
2026-07-09 20:51:35 +0000

    canaryRelease: 575885
    Main scripts: 328
    Lazy Loaded Scripts: 4301
    Chunks:
     * Main Files: 10164
     * Scripts: 9183
    Strings: 25810
    Routes: 839
    Experiments Count: 36
    Apex Experiments Count: 321

--- chunks/470739.js
+++ chunks/470739.js
@@ -1,27 +1,41 @@
 "use strict";
-n.d(t, { _: () => a });
+n.d(t, { _: () => o });
 var i = n(627968),
-    r = n(192308);
-function a() {
+    r = n(296489),
+    a = n.n(r),
+    s = n(192308);
+let l = null;
+function o() {
     let {
         initialBadgeId: e,
         targetUserId: t,
-        targetUsername: a,
-        viewingCurrentUserBadges: s,
-        stackingBehavior: l,
+        targetUsername: r,
+        viewingCurrentUserBadges: o,
+        stackingBehavior: d,
     } = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : {};
-    return (0, r.openModalLazy)(
-        async () => {
-            let { default: r } = await Promise.all([n.e("58877"), n.e("28971")]).then(n.bind(n, 546214));
-            return (n) =>
-                (0, i.jsx)(r, {
-                    ...n,
-                    initialBadgeId: e,
-                    targetUserId: t,
-                    targetUsername: a,
-                    viewingCurrentUserBadges: s,
-                });
-        },
-        { stackingBehavior: l ?? "stack" },
+    null != l && (0, s.closeModalInAllContexts)(l);
+    let c = a()("badge-directory-modal");
+    return (
+        (l = c),
+        (0, s.openModalLazy)(
+            async () => {
+                let { default: a } = await Promise.all([n.e("17277"), n.e("28971")]).then(n.bind(n, 546214));
+                return (n) =>
+                    (0, i.jsx)(a, {
+                        ...n,
+                        initialBadgeId: e,
+                        targetUserId: t,
+                        targetUsername: r,
+                        viewingCurrentUserBadges: o,
+                    });
+            },
+            {
+                modalKey: c,
+                stackingBehavior: d ?? "stack",
+                onCloseCallback: () => {
+                    l === c && (l = null);
+                },
+            },
+        )
     );
 }
