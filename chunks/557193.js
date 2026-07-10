adaca58fdd805c69f90a8a4d5fa11711fe97a6f6
DiscrapperManager <admin@minerpl.xyz>
2026-07-10 20:42:09 +0000

    canaryRelease: 576689
    Main scripts: 328
    Lazy Loaded Scripts: 4308
    Chunks:
     * Main Files: 10171
     * Scripts: 9232
    Strings: 25827
    Routes: 839
    Experiments Count: 36
    Apex Experiments Count: 325

---
+++ chunks/557193.js
@@ -0,0 +1,22 @@
+"use strict";
+function i(e, t) {
+    return (
+        null != e &&
+        null != t &&
+        (e.tag !== t.tag ||
+            e.badge !== t.badge ||
+            e.badgeColorPrimary !== t.badgeColorPrimary ||
+            e.badgeColorSecondary !== t.badgeColorSecondary)
+    );
+}
+n.d(t, { HU: () => i }),
+    n(685073),
+    n(71393),
+    n(576705),
+    (0, n(945810).mj)({
+        name: "2026-06-mobile-server-tag",
+        kind: "user",
+        defaultConfig: { enabled: !1 },
+        variations: { 1: { enabled: !0 } },
+    }),
+    n(652215);
--- chunks/786173.js
+++ chunks/786173.js
@@ -1,33 +1,37 @@
-n.d(t, { A: () => a });
+n.d(t, { A: () => d });
 var u = n(64700),
     l = n(512750),
-    r = n(17928),
-    i = n(71393),
-    o = n(488803),
-    s = n(383272),
-    A = n(414133),
-    _ = n(568065),
-    E = n(652215);
-function a(e, t) {
-    let n = (0, o.C$)(e, "useGuildPowerupNewPerkMarketingVersion"),
-        a = (0, r.bG)([i.A], () => i.A.getGuild(e)?.features.has(E.GuildFeatures.GAME_SERVERS)),
-        d = (0, s.DD)(e, "useGuildPowerupNewPerkMarketingVersion"),
-        G = (0, A.OS)("useGuildPowerupNewPerkMarketingVersion"),
-        R = d && G;
+    r = n(17928);
+n(557193);
+var i = n(71393),
+    o = n(576705),
+    s = n(488803),
+    A = n(383272),
+    _ = n(414133),
+    E = n(568065),
+    a = n(652215);
+function d(e, t) {
+    let n = (0, s.C$)(e, "useGuildPowerupNewPerkMarketingVersion"),
+        d = (0, r.bG)([i.A], () => i.A.getGuild(e)?.features.has(a.GuildFeatures.GAME_SERVERS)),
+        G = (0, A.DD)(e, "useGuildPowerupNewPerkMarketingVersion"),
+        R = (0, _.OS)("useGuildPowerupNewPerkMarketingVersion"),
+        S = G && R,
+        P = (0, r.bG)([o.A, i.A], () => o.A.can(a.xBc.MANAGE_GUILD, i.A.getGuild(e)));
     return u.useMemo(() => {
+        t?.allPowerups?.[l.SL], t?.unlockedPowerups?.[l.SL];
         let e = t?.allPowerups?.[l.d0] != null,
             u = t?.unlockedPowerups?.[l.d0] != null;
-        if (R && e && !u) return _.QS.GUILD_THEME;
+        if (S && e && !u) return E.QS.GUILD_THEME;
         let r = t?.allPowerups?.[l.zY] != null,
             i = t?.unlockedPowerups?.[l.zY] != null;
         return r && !i
-            ? _.QS.FILE_UPLOAD_250_MB
-            : Array.from(_.Q0[_.QS.GUILD_TAG_BADGE_PACKS_WAVE_TWO]).some((e) => t?.unlockedPowerups?.[e] != null)
-              ? n && !a
-                  ? _.QS.GAME_SERVER_HOSTING
-                  : Array.from(_.Q0[_.QS.GUILD_TAG_BADGE_PACKS_WAVE_ONE]).some((e) => t?.unlockedPowerups?.[e] != null)
+            ? E.QS.FILE_UPLOAD_250_MB
+            : Array.from(E.Q0[E.QS.GUILD_TAG_BADGE_PACKS_WAVE_TWO]).some((e) => t?.unlockedPowerups?.[e] != null)
+              ? n && !d
+                  ? E.QS.GAME_SERVER_HOSTING
+                  : Array.from(E.Q0[E.QS.GUILD_TAG_BADGE_PACKS_WAVE_ONE]).some((e) => t?.unlockedPowerups?.[e] != null)
                     ? 0
-                    : _.QS.GUILD_TAG_BADGE_PACKS_WAVE_ONE
-              : _.QS.GUILD_TAG_BADGE_PACKS_WAVE_TWO;
-    }, [t, n, a, R]);
+                    : E.QS.GUILD_TAG_BADGE_PACKS_WAVE_ONE
+              : E.QS.GUILD_TAG_BADGE_PACKS_WAVE_TWO;
+    }, [t, n, d, S, e, P]);
 }
