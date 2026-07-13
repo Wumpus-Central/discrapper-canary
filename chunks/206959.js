0fb9c0df72f0fa06b27149cd7995fbb4ce31b982
DiscrapperManager <admin@minerpl.xyz>
2026-07-13 18:10:38 +0000

    canaryRelease: 577223
    Main scripts: 359
    Lazy Loaded Scripts: 4329
    Chunks:
     * Main Files: 10190
     * Scripts: 9309
    Strings: 25843
    Routes: 839
    Experiments Count: 36
    Apex Experiments Count: 325

--- chunks/206959.js
+++ chunks/206959.js
@@ -27,7 +27,7 @@ let C = [
     { name: "H264", encode: !0, decode: !0 },
     { name: "VP8", encode: !0, decode: !0 },
 ];
-function R(e, t) {
+function O(e, t) {
     let n = arguments.length > 2 && void 0 !== arguments[2] && arguments[2],
         i = t.concat(C),
         r = [];
@@ -45,7 +45,7 @@ function R(e, t) {
     }
     return r;
 }
-function O(e) {
+function R(e) {
     let t = [],
         n = e.has(N.fd.SIGNAL_AV1_ENCODE),
         i = e.has(N.fd.SIGNAL_AV1_DECODE);
@@ -552,12 +552,12 @@ class H extends g.A {
                 n.on(c.yq.Stats, n.handleStats),
                     n.conn.setOnVideoCallback(n.handleVideo),
                     i.getCodecCapabilities((t) => {
-                        let i = O(n.experimentFlags);
+                        let i = R(n.experimentFlags);
                         (n.codecs = [
                             { type: "audio", name: N.UK.OPUS, priority: 1, payloadType: 120 },
                             ...(function (e, t) {
                                 let n = arguments.length > 2 && void 0 !== arguments[2] && arguments[2];
-                                return R(L(e), t, n);
+                                return O(L(e), t, n);
                             })(t, i).map((e, t) => {
                                 let n = 101 + 2 * t;
                                 return {
@@ -617,13 +617,13 @@ class H extends g.A {
                     n.getCodecCapabilities((i) => {
                         (this.onVideoCodecsCallbackAt = performance.now()),
                             this.logger.info(`Available engine codecs: ${JSON.stringify(i)}`);
-                        let r = O(this.experimentFlags);
+                        let r = R(this.experimentFlags);
                         this.logger.info(`Experimental codecs: ${JSON.stringify(r)}`);
                         let o = L(i),
                             d = this.lastOverrideCodecDenylist.length > 0;
                         this.codecs = [
                             { type: "audio", name: N.UK.OPUS, priority: 1, payloadType: 120 },
-                            ...R(o, r, d).map((e, t) => {
+                            ...O(o, r, d).map((e, t) => {
                                 let n = 101 + 2 * t;
                                 return {
                                     type: "video",
@@ -1684,6 +1684,7 @@ function j(e, t) {
                 effects: _,
                 macosTransportType: E,
                 windowsEndpointFormFactor: A,
+                windowsDeviceService: h,
             } = t;
             return (
                 /^default/.test(s)
@@ -1703,6 +1704,7 @@ function j(e, t) {
                     effects: _,
                     macosTransportType: E,
                     windowsEndpointFormFactor: A,
+                    windowsDeviceService: h,
                 }
             );
         });
