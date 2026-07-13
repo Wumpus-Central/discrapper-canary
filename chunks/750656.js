c7ce6b49dd657f305a83a1a013b00f4efff74462
DiscrapperManager <admin@minerpl.xyz>
2026-07-13 22:50:01 +0000

    canaryRelease: 577554
    Main scripts: 363
    Lazy Loaded Scripts: 4303
    Chunks:
     * Main Files: 10188
     * Scripts: 9281
    Strings: 25840
    Routes: 839
    Experiments Count: 36
    Apex Experiments Count: 329

--- chunks/750656.js
+++ chunks/750656.js
@@ -1,26 +1,26 @@
 "use strict";
 n.d(t, {
-    Jl: () => l,
-    LP: () => h,
-    NU: () => f,
+    Jl: () => o,
+    LP: () => E,
+    NU: () => A,
     PS: () => _,
-    Wf: () => u,
-    _k: () => a,
-    gz: () => o,
-    ii: () => E,
-    kp: () => d,
+    Wf: () => d,
+    _k: () => s,
+    gz: () => l,
+    ii: () => h,
+    kp: () => u,
     lU: () => c,
-    re: () => s,
+    re: () => a,
 });
 var i = n(508425),
     r = n(559949);
-let s = [i.z.SOLID, i.z.GRADIENT, i.z.NEON, i.z.TOON, i.z.POP],
-    a = [r.x.PLAYPEN_SANS, r.x.ORBITRON, r.x.NEW_ROCKER, r.x.KALAM],
-    o = [i.z.GUMMY, i.z.PRISM],
-    l = [
+let a = [i.z.SOLID, i.z.GRADIENT, i.z.NEON, i.z.TOON, i.z.POP],
+    s = [r.x.PLAYPEN_SANS, r.x.ORBITRON, r.x.NEW_ROCKER, r.x.KALAM],
+    l = [i.z.GUMMY, i.z.PRISM],
+    o = [
         1628845, 2417517, 1874155, 0xc32aff, 0xfc1965, 0xc6a615, 695675, 1027403, 747943, 0xa913e5, 0xda0149, 0xf93722,
     ],
-    u = [
+    d = [
         [2797222, 16762e3],
         [2535780, 9497343],
         [0xe45eff, 2522592],
@@ -31,16 +31,17 @@ let s = [i.z.SOLID, i.z.GRADIENT, i.z.NEON, i.z.TOON, i.z.POP],
         [0xe4541e, 6674404],
     ].map((e) => ({ colors: e, name: "" })),
     c = [
-        [0xf4a7d8, 9626600, 0xf8f59a, 0xc9f7d8],
-        [0xff6b9a, 0xffd85a, 5629578, 7064575],
-        [5227519, 7268312, 9075967, 0xb7f5ff],
-        [0xff8a5c, 0xffd15a, 0xf06aa6, 5621759],
-        [0xd83a5f, 0xc94bcb, 9133311, 0xb06cff],
+        [0xe9a9d5, 0xa9ddf0, 0xb9a7ee, 0xb9eacb],
+        [0xff6fc2, 0xf54fa8, 0xff9bd6, 0xd96ab7],
+        [0xffb57a, 0xff936f, 0xff7c78, 0xf06b8a],
+        [0xc4ed65, 0x98de60, 7130467, 4570214],
+        [8173823, 9363664, 0x9d94f5, 6737904],
+        [0xd77bff, 0xb56bff, 0xe45ce5, 9137407],
     ],
-    d = 0.78,
+    u = 0.78,
     _ = 0.72,
-    h = 38,
-    f = [
+    E = 38,
+    A = [
         [0xfe9242, 3534206, 0xffe047, 0xff4cd2, 5793266],
         [0xff3d3d, 0xff8a2a, 0xffd447, 0xff4fa3, 8086015],
         [7997702, 0xc81e12, 0xf45a18, 0xff9500, 0xffd447],
@@ -48,16 +49,16 @@ let s = [i.z.SOLID, i.z.GRADIENT, i.z.NEON, i.z.TOON, i.z.POP],
         [3528287, 1497266, 2147829, 2850047, 7032319],
         [0xa7e8a1, 0xd9f08c, 0xffe08a, 0xffc4a8, 0xf3b6d9],
     ];
-function E(e) {
+function h(e) {
     switch (e) {
         case i.z.GUMMY:
             return c;
         case i.z.PRISM:
-            return f;
+            return A;
         case i.z.GRADIENT:
         case i.z.GLOW:
-            return u.map((e) => e.colors);
+            return d.map((e) => e.colors);
         default:
-            return l.map((e) => [e]);
+            return o.map((e) => [e]);
     }
 }
--- chunks/945096.js
+++ chunks/945096.js
@@ -1,28 +1,29 @@
 "use strict";
 n.d(t, {
     $0: () => c,
-    UZ: () => f,
-    Xr: () => g,
-    as: () => u,
-    cf: () => h,
+    UZ: () => I,
+    Xr: () => m,
+    as: () => d,
+    cf: () => A,
     gN: () => p,
-    mT: () => E,
-    sx: () => m,
-    xo: () => d,
+    mT: () => f,
+    nO: () => h,
+    sx: () => T,
+    xo: () => u,
 });
 var i = n(310784),
     r = n.n(i),
-    s = n(508425),
-    a = n(559949),
-    o = n(317097),
-    l = n(750656);
-function u(e) {
+    a = n(508425),
+    s = n(559949),
+    l = n(317097),
+    o = n(750656);
+function d(e) {
     switch (e) {
-        case s.z.GRADIENT:
+        case a.z.GRADIENT:
             return 2;
-        case s.z.GUMMY:
+        case a.z.GUMMY:
             return 4;
-        case s.z.PRISM:
+        case a.z.PRISM:
             return 5;
         default:
             return 1;
@@ -32,7 +33,7 @@ function c(e) {
     let t = r()(e).alpha(1),
         n = t.get("hsl.l"),
         i = Math.min(1, 1.2 * t.get("hsl.s")),
-        s = Math.min(0.6, n + 0.1);
+        a = Math.min(0.6, n + 0.1);
     return {
         main: e,
         light1: t.set("hsl.l", Math.min(1, 1.2 * n)).hex(),
@@ -40,43 +41,56 @@ function c(e) {
         dark1: t.set("hsl.l", Math.max(0, 0.6 * n)).hex(),
         dark2: t.set("hsl.l", Math.max(0, 0.2 * n)).hex(),
         toonStroke: t.set("hsl.l", Math.max(0.12, 0.4 * n)).hex(),
-        neonStroke: t.set("hsl.s", i).set("hsl.l", s).hex(),
+        neonStroke: t.set("hsl.s", i).set("hsl.l", a).hex(),
     };
 }
-function d(e) {
+function u(e) {
     return ((e % 360) + 360) % 360;
 }
-let _ = [0, 60, 110, 30];
+let _ = [
+    { hueShift: -18, saturation: 0.54, lightness: 0.72 },
+    { hueShift: -5, saturation: 0.66, lightness: 0.6 },
+    { hueShift: 9, saturation: 0.56, lightness: 0.68 },
+    { hueShift: 22, saturation: 0.6, lightness: 0.63 },
+];
+function E(e) {
+    let [t, n, i] = r()((0, l.Hl)(e)).hsl();
+    return [Number.isNaN(t) ? 0 : t, n, i];
+}
+function A(e) {
+    let [t] = E(e);
+    return _.map((e) => {
+        let { hueShift: n, saturation: i, lightness: a } = e;
+        return r()
+            .hsl(u(t + n), i, a)
+            .num();
+    });
+}
 function h(e) {
-    let [t, n, i] = (function (e) {
-        let [t, n, i] = r()((0, o.Hl)(e)).hsl();
-        return [Number.isNaN(t) ? 0 : t, n, i];
-    })(e);
-    return _.map((e) =>
-        r()
-            .hsl(d(t + e), n, i)
-            .num(),
-    );
+    let t = e[0];
+    if (null == t) return I(0);
+    let [n] = E(t);
+    return I(u(n - _[0].hueShift));
+}
+function I(e) {
+    return r().hsl(e, o.kp, o.PS).num();
 }
 function f(e) {
-    return r().hsl(e, l.kp, l.PS).num();
-}
-function E(e) {
     return null == e ? null : { fontId: e.font_id, effectId: e.effect_id, colors: e.colors };
 }
 function p(e, t) {
     let n = t[Math.floor(Math.random() * t.length)],
         i = e[Math.floor(Math.random() * e.length)],
-        r = (0, l.ii)(n);
+        r = (0, o.ii)(n);
     return { fontId: i, effectId: n, colors: [...r[Math.floor(Math.random() * r.length)]] };
 }
-function m(e, t) {
+function T(e, t) {
     if (t || null == e) return e;
-    let n = l._k.includes(e.fontId) ? a.x.DEFAULT : e.fontId,
-        i = l.gz.includes(e.effectId) ? s.z.SOLID : e.effectId;
+    let n = o._k.includes(e.fontId) ? s.x.DEFAULT : e.fontId,
+        i = o.gz.includes(e.effectId) ? a.z.SOLID : e.effectId;
     return n === e.fontId && i === e.effectId ? e : { ...e, fontId: n, effectId: i };
 }
-function g(e) {
+function m(e) {
     if (null == e) return !1;
     let t = e.replace(/[^\p{L}]/gu, "");
     return /\P{Script=Latin}/u.test(t);
