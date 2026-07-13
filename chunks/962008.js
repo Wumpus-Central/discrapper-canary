1054caf84eb503a7585dfe6a1a0e0930c0eeb3a5
DiscrapperManager <admin@minerpl.xyz>
2026-07-13 23:55:30 +0000

    canaryRelease: 577627
    Main scripts: 363
    Lazy Loaded Scripts: 4303
    Chunks:
     * Main Files: 10188
     * Scripts: 9281
    Strings: 25840
    Routes: 839
    Experiments Count: 36
    Apex Experiments Count: 329

--- chunks/962008.js
+++ chunks/962008.js
@@ -1,5 +1,5 @@
 "use strict";
-n.d(t, { q: () => g });
+n.d(t, { q: () => S });
 var i,
     r = n(64700),
     a = n(989349),
@@ -29,43 +29,54 @@ var p = n(202541),
     T = n(652215),
     m = n(375708);
 function g(e) {
+    let t = e.premiumSince;
+    return null == t ? null : s()().diff(s()(t).add(1, "day"), "months");
+}
+function S(e) {
     var t;
-    let n = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
-        i = (0, o.bG)([c.default], () => c.default.locale),
-        a = (function (e) {
+    let n,
+        i = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
+        a = (0, o.bG)([c.default], () => c.default.locale),
+        s = (function (e) {
             let { location: t } = e;
             return f.useConfig({ location: t });
         })({ location: "useMonthlyToYearlyUpsellContent" }),
         h =
-            !n || null == e || (t = e).status !== T.Dmq.ACTIVE || t.isPurchasedExternally || (0, _.Nc)(t)
-                ? null
-                : (function (e) {
+            i &&
+            null != e &&
+            (t = e).status === T.Dmq.ACTIVE &&
+            !t.isPurchasedExternally &&
+            !(0, _.Nc)(t) &&
+            null != (n = g(t)) &&
+            n <= 4
+                ? (function (e) {
                       let t = (0, _.EL)(e);
                       if (null == t || !p.BL.has(t.planId)) return null;
                       let n = p.hd[t.planId]?.skuId;
                       if (null == n) return null;
                       let i = p.zE[n];
                       return null == i ? null : { yearlyPlanId: i, skuId: (0, _.mH)(n) };
-                  })(e),
-        g = (0, A.zz)(h?.yearlyPlanId ?? null),
-        S = e?.paymentSourceId ?? void 0,
-        { priceOptions: N } = (0, d.A)({
+                  })(e)
+                : null,
+        S = (0, A.zz)(h?.yearlyPlanId ?? null),
+        N = e?.paymentSourceId ?? void 0,
+        { priceOptions: C } = (0, d.A)({
             activeSubscription: e ?? null,
             skuIDs: null != h ? [h.skuId] : [],
-            paymentSourceId: S,
+            paymentSourceId: N,
             isGift: !1,
         });
     return (0, r.useMemo)(() => {
-        if (null == g || null == e || !N.loaded || N.paymentSourceId !== S) return null;
-        let t = (0, _.z_)(g, !1, N);
+        if (null == S || null == e || !C.loaded || C.paymentSourceId !== N) return null;
+        let t = (0, _.z_)(S, !1, C);
         if (null == t) return null;
         let n = (0, l.LQ)(t.amount, t.currency);
         if (null == n || n <= 0) return null;
-        let r = (0, E.$g)(n, t.currency, { maximumFractionDigits: 0 }),
-            o = (0, _.L_)({ planId: g.id, priceOptions: N, subscriptionPlan: g }),
-            d = null != o ? (0, u.l9)(i, o / 100) : null,
-            c = null != d ? m.intl.formatToPlainString(m.t.IAybsG, { discount: d }) : void 0,
-            A = (function (e, t, n) {
+        let i = (0, E.$g)(n, t.currency, { maximumFractionDigits: 0 }),
+            r = (0, _.L_)({ planId: S.id, priceOptions: C, subscriptionPlan: S }),
+            o = null != r ? (0, u.l9)(a, r / 100) : null,
+            d = null != o ? m.intl.formatToPlainString(m.t.IAybsG, { discount: o }) : void 0,
+            c = (function (e, t, n) {
                 switch (e) {
                     case I.CONTROL:
                         return {
@@ -83,15 +94,10 @@ function g(e) {
                             body: m.intl.string(m.t.deiBu8),
                         };
                     case I.LOYALTY: {
-                        let e = (function (e) {
-                            let t = e.premiumSince;
-                            if (null == t) return null;
-                            let n = s()().diff(s()(t).add(1, "day"), "months");
-                            return n > 0 ? n : null;
-                        })(n);
+                        let e = g(n) ?? 0;
                         return {
                             title:
-                                null != e
+                                e > 0
                                     ? m.intl.formatToPlainString(m.t.n0NjuD, { amount: t, months: e })
                                     : m.intl.formatToPlainString(m.t.WBEYyz, { amount: t }),
                             body: m.intl.string(m.t.UZWxcy),
@@ -100,9 +106,9 @@ function g(e) {
                     default:
                         return null;
                 }
-            })(a, r, e);
-        if (null == A) return null;
-        let { title: h, body: f } = A;
-        return { title: h, body: f, cta: m.intl.string(m.t.xmQfYw), badge: c, yearlyPlanId: g.id };
-    }, [i, a, e, g, N, S]);
+            })(s, i, e);
+        if (null == c) return null;
+        let { title: A, body: h } = c;
+        return { title: A, body: h, cta: m.intl.string(m.t.xmQfYw), badge: d, yearlyPlanId: S.id };
+    }, [a, s, e, S, C, N]);
 }
