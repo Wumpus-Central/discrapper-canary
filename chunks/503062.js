24b578bbcad5374559778b11c22cd37a979992ee
DiscrapperManager <admin@minerpl.xyz>
2026-07-15 17:21:36 +0000

    canaryRelease: 578676
    Main scripts: 362
    Lazy Loaded Scripts: 4304
    Chunks:
     * Main Files: 10185
     * Scripts: 9256
    Strings: 25852
    Routes: 839
    Experiments Count: 35
    Apex Experiments Count: 328

--- chunks/503062.js
+++ chunks/503062.js
@@ -1,8 +1,8 @@
-i.d(s, { A: () => F });
+i.d(s, { A: () => w });
 var n = i(627968),
     a = i(64700),
-    t = i(503698),
-    l = i.n(t),
+    l = i(503698),
+    t = i.n(l),
     r = i(17928),
     o = i(778712),
     u = i(97808),
@@ -15,8 +15,8 @@ var n = i(627968),
     v = i(178418),
     x = i(87664),
     g = i(449582),
-    C = i(88686),
-    M = i(174755),
+    M = i(88686),
+    C = i(174755),
     b = i(290863),
     j = i(19575),
     p = i(956705),
@@ -26,35 +26,36 @@ var n = i(627968),
     _ = i(996988),
     k = i(921519);
 let S = o._3.SIZE_40,
-    y = j.Ay.getEnableHardwareAcceleration();
-function F(e) {
-    let { user: s, status: t, guildId: o, channelId: j, onSelect: F } = e,
-        { theme: L, themeType: w } = (0, I.E)(),
+    F = j.Ay.getEnableHardwareAcceleration();
+function w(e) {
+    let { user: s, status: l, guildId: o, channelId: j, onSelect: w } = e,
+        { theme: y, themeType: L } = (0, I.E)(),
         U = a.useMemo(() => s.isNonUserBot() || (0, v.c)(s, j), [s, j]),
         { activities: D, isMobileOnline: R } = (0, r.cf)([b.A], () => ({
             activities: b.A.getActivities(s.id),
             isMobileOnline: b.A.isMobileOnline(s.id),
         })),
-        T = (0, x.A)(s.id),
-        { voiceChannel: P } = (0, h.Ay)({ userId: s.id, guildId: o }),
+        P = (0, x.A)(s.id),
+        { voiceChannel: T } = (0, h.Ay)({ userId: s.id, guildId: o }),
         z = (0, p.r)("UserProfileMutualFriendRow"),
-        [H, O] = a.useState(!1),
+        H = (0, p.G)("UserProfileMutualFriendRow"),
+        [O, K] = a.useState(!1),
         {
-            avatarSrc: K,
-            avatarDecorationSrc: W,
-            eventHandlers: B,
-        } = (0, E.A)({ userId: s.id, guildId: o, size: S, animateOnHover: !H }),
-        V = y && z ? u.Js : u.eu,
+            avatarSrc: W,
+            avatarDecorationSrc: B,
+            eventHandlers: G,
+        } = (0, E.A)({ userId: s.id, guildId: o, size: S, animateOnHover: !O }),
+        V = F && z ? u.Js : u.eu,
         Y = a.useRef(null),
         Z = (0, g.r)({ user: s, guildId: o }),
-        G = z ? Z : null,
-        Q = a.useCallback(() => O(!0), []),
-        q = a.useCallback(() => O(!1), []);
+        Q = H ? Z : null,
+        q = a.useCallback(() => K(!0), []),
+        J = a.useCallback(() => K(!1), []);
     return (0, n.jsxs)(d.D, {
-        ...(z ? { onMouseEnter: Q, onMouseLeave: q } : {}),
-        focusProps: w === _.d.MODAL_V2 ? { offset: { top: 4, right: 4, left: 4 } } : { offset: { right: 8 } },
-        className: l()(k.nM, { [k.EY]: null != G }),
-        onClick: F,
+        ...(z ? { onMouseEnter: q, onMouseLeave: J } : {}),
+        focusProps: L === _.d.MODAL_V2 ? { offset: { top: 4, right: 4, left: 4 } } : { offset: { right: 8 } },
+        className: t()(k.nM, { [k.EY]: null != Q }),
+        onClick: w,
         onContextMenu: function (e) {
             (0, f.L3)(e, async () => {
                 let { default: e } = await Promise.all([
@@ -86,19 +87,19 @@ function F(e) {
             });
         },
         children: [
-            (0, n.jsx)(M.A, { nameplate: H ? G : null, hovered: H, content: Y, placement: C.u.MUTUAL_FRIENDS_LIST }),
+            (0, n.jsx)(C.A, { nameplate: O ? Q : null, hovered: O, content: Y, placement: M.u.MUTUAL_FRIENDS_LIST }),
             (0, n.jsxs)("div", {
                 ref: Y,
                 className: k.Qs,
                 children: [
                     (0, n.jsx)(V, {
-                        ...(z ? B : {}),
-                        src: K,
-                        avatarDecoration: z ? W : void 0,
+                        ...(z ? G : {}),
+                        src: W,
+                        avatarDecoration: z ? B : void 0,
                         "aria-label": s.username,
                         size: S,
-                        status: U ? N.clD.UNKNOWN : t,
-                        statusBackdropColor: U ? void 0 : (0, c.C$)(L),
+                        status: U ? N.clD.UNKNOWN : l,
+                        statusBackdropColor: U ? void 0 : (0, c.C$)(y),
                         isMobile: R,
                         className: k.my,
                     }),
@@ -112,7 +113,7 @@ function F(e) {
                                 discriminatorClass: k.D2,
                                 showGuildTag: z,
                             }),
-                            (0, n.jsx)(m.A, { user: s, activities: D, applicationStream: T, voiceChannel: P }),
+                            (0, n.jsx)(m.A, { user: s, activities: D, applicationStream: P, voiceChannel: T }),
                         ],
                     }),
                 ],
--- chunks/956705.js
+++ chunks/956705.js
@@ -1,12 +1,19 @@
 "use strict";
-n.d(t, { r: () => r });
+n.d(t, { G: () => a, r: () => r });
 let i = (0, n(945810).mj)({
     name: "2026-04-consistent-profiles",
     kind: "user",
-    defaultConfig: { enabled: !1 },
-    variations: { 1: { enabled: !0 } },
+    defaultConfig: { enabled: !1, showMutualFriendsNameplate: !1 },
+    variations: {
+        1: { enabled: !0, showMutualFriendsNameplate: !1 },
+        2: { enabled: !0, showMutualFriendsNameplate: !0 },
+    },
 });
 function r(e) {
     let { enabled: t } = i.useConfig({ location: e });
+    return t;
+}
+function a(e) {
+    let { showMutualFriendsNameplate: t } = i.useConfig({ location: e });
     return t;
 }
