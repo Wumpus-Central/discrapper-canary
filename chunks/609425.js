45fe066d4307220d480b1e3bc1e9ff8ad26c03f6
DiscrapperManager <admin@minerpl.xyz>
2026-07-16 19:57:48 +0000

    canaryRelease: 579767
    Main scripts: 371
    Lazy Loaded Scripts: 4325
    Chunks:
     * Main Files: 10189
     * Scripts: 9303
    Strings: 25879
    Routes: 839
    Experiments Count: 35
    Apex Experiments Count: 334

--- chunks/609425.js
+++ chunks/609425.js
@@ -1,27 +1,28 @@
 "use strict";
-n.d(t, { A: () => u });
+n.d(t, { A: () => d });
 var i = n(64700),
     r = n(17928),
-    s = n(386467),
-    a = n(696451),
-    o = n(287809),
-    l = n(824994);
-function u() {
+    a = n(386467),
+    s = n(696451),
+    l = n(287809),
+    o = n(824994);
+function d() {
     let {
             userId: e,
             guildId: t,
             pendingDisplayNameStyles: n,
+            ignoreDisabledStylesSetting: d = !1,
         } = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : {},
-        u = (0, l.W)({ location: "useDisplayNameStyles" }),
-        c = (0, r.bG)([o.default], () => (null != e ? o.default.getUser(e) : o.default.getCurrentUser())),
-        d = (0, i.useContext)(s.A);
-    null == t && (t = d);
-    let _ = (0, r.bG)([a.Ay], () => (null != t && null != c ? a.Ay.getMember(t, c.id) : null));
-    return u
+        c = (0, o.W)({ location: "useDisplayNameStyles" }),
+        u = (0, r.bG)([l.default], () => (null != e ? l.default.getUser(e) : l.default.getCurrentUser())),
+        _ = (0, i.useContext)(a.A);
+    null == t && (t = _);
+    let E = (0, r.bG)([s.Ay], () => (null != t && null != u ? s.Ay.getMember(t, u.id) : null));
+    return c || d
         ? void 0 !== n
             ? null === n && null != t
-                ? c?.displayNameStyles
+                ? u?.displayNameStyles
                 : n
-            : (_?.displayNameStyles ?? c?.displayNameStyles)
+            : (E?.displayNameStyles ?? u?.displayNameStyles)
         : null;
 }
