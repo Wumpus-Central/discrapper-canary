96d8d5276613610c11b9b2f1be0acf6cd4e83fd5
DiscrapperManager <admin@minerpl.xyz>
2026-07-17 17:26:21 +0000

    noisesSuppression 580331

--- chunks/93474.js
+++ chunks/93474.js
@@ -1,105 +1,106 @@
-n.d(t, { A: () => C });
+"use strict";
+n.d(t, { A: () => g });
 var i = n(17928),
-    l = n(228366),
-    r = n(779185),
+    r = n(228366),
+    a = n(779185),
     s = n(320095),
-    a = n(734057),
+    l = n(734057),
     o = n(232835),
-    E = n(935208),
+    d = n(935208),
     c = n(753738),
     u = n(877133),
     _ = n(652215);
-let A = {},
-    d = 0,
-    T = {},
+let E = {},
+    A = 0,
+    h = {},
     I = {};
-function N(e) {
-    null != A[e] && delete A[e], d++;
+function f(e) {
+    null != E[e] && delete E[e], A++;
 }
-function R(e) {
+function p(e) {
     let t,
         n,
-        { messageData: i, errorResponseBody: l } = e;
+        { messageData: i, errorResponseBody: r } = e;
     return (
-        (n = { id: (t = (0, r.cR)(i)), isBlockedEdit: (0, r.Qn)(i), messageData: i, errorMessage: (0, c.PD)(i, l) }),
-        (A[t] = n),
-        d++,
+        (n = { id: (t = (0, a.cR)(i)), isBlockedEdit: (0, a.Qn)(i), messageData: i, errorMessage: (0, c.PD)(i, r) }),
+        (E[t] = n),
+        A++,
         !0
     );
 }
-function S(e) {
+function T(e) {
     let { channelId: t, messages: n } = e,
-        i = a.A.getChannel(t)?.getGuildId();
+        i = l.A.getChannel(t)?.getGuildId();
     if (null == i) return !1;
-    let l = I[i],
-        r = n.reduce(
+    let r = I[i],
+        a = n.reduce(
             (e, t) =>
                 t.type === _.lAJ.AUTO_MODERATION_ACTION &&
                 t.embeds?.some((e) => {
                     let { type: t } = e;
                     return t === _.Auw.AUTO_MODERATION_NOTIFICATION;
                 })
-                    ? null == e || -1 === E.default.compare(e, t.id)
+                    ? null == e || -1 === d.default.compare(e, t.id)
                         ? t.id
                         : void 0
                     : e,
-            l,
+            r,
         );
-    return null != r && I[i] !== r && ((I[i] = r), !0);
+    return null != a && I[i] !== a && ((I[i] = a), !0);
 }
-class O extends i.Ay.PersistedStore {
+class m extends i.Ay.PersistedStore {
     static displayName = "GuildAutomodMessageStore";
     static persistKey = "GuildAutomodMessages";
     initialize(e) {
-        this.waitFor(a.A, o.A), null != e && ((A = e.automodFailedMessages), (T = e.mentionRaidDetectionByGuild));
+        this.waitFor(l.A, o.A), null != e && ((E = e.automodFailedMessages), (h = e.mentionRaidDetectionByGuild));
     }
     getState() {
-        return { automodFailedMessages: A, mentionRaidDetectionByGuild: T, lastIncidentAlertMessage: I };
+        return { automodFailedMessages: E, mentionRaidDetectionByGuild: h, lastIncidentAlertMessage: I };
     }
     getMessage(e) {
-        return null == e ? null : (A[e] ?? null);
+        return null == e ? null : (E[e] ?? null);
     }
     getMessagesVersion() {
-        return d;
+        return A;
     }
     getMentionRaidDetected(e) {
-        return T[e] ?? null;
+        return h[e] ?? null;
     }
     getLastIncidentAlertMessage(e) {
         return I[e] ?? null;
     }
 }
-let C = new O(l.h, {
+let g = new m(r.h, {
     CONNECTION_OPEN: function (e) {
-        return 0 !== Object.keys(A).length && ((A = {}), d++, !0);
+        return 0 !== Object.keys(E).length && ((E = {}), A++, !0);
     },
-    LOAD_MESSAGES_SUCCESS: S,
-    LOCAL_MESSAGES_LOADED: S,
+    LOAD_MESSAGES_SUCCESS: T,
+    LOCAL_MESSAGES_LOADED: T,
     MESSAGE_CREATE: function (e) {
         let { guildId: t, message: n } = e;
         if (null == t || n.type !== _.lAJ.AUTO_MODERATION_ACTION) return !1;
         let i = (0, s.rh)(n);
         return !!(0, u.ER)(i) && !!(0, u.de)(i) && ((I[t] = i.id), !0);
     },
-    MESSAGE_SEND_FAILED_AUTOMOD: R,
-    MESSAGE_EDIT_FAILED_AUTOMOD: R,
+    MESSAGE_SEND_FAILED_AUTOMOD: p,
+    MESSAGE_EDIT_FAILED_AUTOMOD: p,
     REMOVE_AUTOMOD_MESSAGE_NOTICE: function (e) {
         let { messageId: t } = e;
-        return N(t), !0;
+        return f(t), !0;
     },
     MESSAGE_END_EDIT: function (e) {
         let { response: t } = e;
         if (t?.body == null || t.body.code === _.t02.AUTOMOD_MESSAGE_BLOCKED) return !1;
         let n = t.body.id;
         if (null == n) return !1;
-        N(n);
+        f(n);
     },
     AUTO_MODERATION_MENTION_RAID_DETECTION: function (e) {
         let { guildId: t, decisionId: n, suspiciousMentionActivityUntil: i } = e;
-        return (T[t] = { guildId: t, decisionId: n, suspiciousMentionActivityUntil: i }), !0;
+        return (h[t] = { guildId: t, decisionId: n, suspiciousMentionActivityUntil: i }), !0;
     },
     AUTO_MODERATION_MENTION_RAID_NOTICE_DISMISS: function (e) {
         let { guildId: t } = e;
-        return delete T[t], !0;
+        return delete h[t], !0;
     },
 });
--- chunks/94264.js
+++ chunks/94264.js
@@ -1,40 +1,41 @@
-a.d(r, { A: () => E });
-var t = a(627968);
-a(64700);
-var s = a(503698),
-    n = a.n(s),
-    o = a(892227),
-    c = a(661531),
-    _ = a(403581),
-    l = a(318254),
-    u = a(914410),
-    i = a(440005),
-    d = a(366505),
-    p = a(992501);
+"use strict";
+n.d(t, { A: () => h });
+var i = n(627968);
+n(64700);
+var r = n(503698),
+    a = n.n(r),
+    s = n(892227),
+    l = n(661531),
+    o = n(403581),
+    d = n(318254),
+    c = n(914410),
+    u = n(440005),
+    _ = n(366505),
+    E = n(992501);
 let A = {
-        churning: { start: c.A.unsafe_rawColors.OPACITY_RED_80.css, end: c.A.unsafe_rawColors.RED_NEW_30.css },
-        active: { start: c.A.unsafe_rawColors.OPACITY_GREEN_80.css, end: c.A.unsafe_rawColors.GREEN_NEW_30.css },
+        churning: { start: l.A.unsafe_rawColors.OPACITY_RED_80.css, end: l.A.unsafe_rawColors.RED_NEW_30.css },
+        active: { start: l.A.unsafe_rawColors.OPACITY_GREEN_80.css, end: l.A.unsafe_rawColors.GREEN_NEW_30.css },
     },
-    E = function (e) {
-        let { className: r } = e,
-            { passesProgressBarInvariant: a, programReward: s, totalDays: c } = (0, d.F)();
-        if (!a || null == s || null == c) return null;
-        let E = (0, o.default)(new Date(s.next_reward_date), new Date()),
-            R = s.program_current_state === i.L.PREMIUM_CHURNING,
-            v = R ? A.churning.start : A.active.start,
-            C = R ? A.churning.end : A.active.end;
-        return (0, t.jsxs)("div", {
-            className: n()(p.k, r),
+    h = function (e) {
+        let { className: t } = e,
+            { passesProgressBarInvariant: n, programReward: r, totalDays: l } = (0, _.F)();
+        if (!n || null == r || null == l) return null;
+        let h = (0, s.default)(new Date(r.next_reward_date), new Date()),
+            I = r.program_current_state === u.L.PREMIUM_CHURNING,
+            f = I ? A.churning.start : A.active.start,
+            p = I ? A.churning.end : A.active.end;
+        return (0, i.jsxs)("div", {
+            className: a()(E.k, t),
             children: [
-                (0, t.jsx)(u.Ay, {
-                    variant: u.qP.UNSET,
-                    progress: c - E,
-                    maximum: c,
-                    override: { default: { gradientStart: v, gradientEnd: C } },
+                (0, i.jsx)(c.Ay, {
+                    variant: c.qP.UNSET,
+                    progress: l - h,
+                    maximum: l,
+                    override: { default: { gradientStart: f, gradientEnd: p } },
                 }),
-                R
-                    ? (0, t.jsx)(_.t, { size: "sm", color: "currentColor", className: p.K })
-                    : (0, t.jsx)(l.C, { size: "sm", color: "currentColor", className: p.K }),
+                I
+                    ? (0, i.jsx)(o.t, { size: "sm", color: "currentColor", className: E.K })
+                    : (0, i.jsx)(d.C, { size: "sm", color: "currentColor", className: E.K }),
             ],
         });
     };
