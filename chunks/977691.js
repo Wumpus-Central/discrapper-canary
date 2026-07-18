43156852ba8e1038d87ad5502044ee7fb0d9dd7c
DiscrapperManager <admin@minerpl.xyz>
2026-07-18 01:52:39 +0000

    canaryRelease: 580816
    Main scripts: 371
    Lazy Loaded Scripts: 4324
    Chunks:
     * Main Files: 10192
     * Scripts: 9310
    Strings: 25891
    Routes: 839
    Experiments Count: 35
    Apex Experiments Count: 335

--- chunks/977691.js
+++ chunks/977691.js
@@ -1,34 +1,33 @@
-n.d(e, { startComposedHighlight: () => s });
+n.d(e, { startComposedHighlight: () => i });
 let l = "mana-composed-highlight",
     a = "mana-type-consolidation",
     o = /\.((?:\\.|[\w-])+)/g;
-function s() {
+function i() {
     let t = document.getElementById(l);
     if (null != t) return () => t.remove();
     let e = (function () {
-        let t = new Set(),
-            e = (n) => {
-                for (let l of n) {
-                    let n = l.selectorText;
-                    if (null != n && n.includes(a)) for (let e of n.matchAll(o)) t.add(e[1].replace(/\\/g, ""));
-                    let s = l.cssRules;
-                    null != s && e(s);
-                }
-            };
-        for (let t of document.styleSheets)
+        let t = new Set();
+        for (let e of document.styleSheets)
             try {
-                e(t.cssRules);
+                !(function e(n) {
+                    for (let l of n) {
+                        let n = l.selectorText;
+                        if (null != n && n.includes(a)) for (let e of n.matchAll(o)) t.add(e[1].replace(/\\/g, ""));
+                        let i = l.cssRules;
+                        null != i && e(i);
+                    }
+                })(e.cssRules);
             } catch {}
         return t.delete(a), [...t];
     })();
     if (0 === e.length) return () => {};
     let n = e.map((t) => `[class~="${t}"]`).join(","),
-        s = `.highlight-mana-text :is(${n}):not([data-text-variant])`,
-        i = "box-shadow:inset 0 0 0 1px var(--yellow-300)",
+        i = `.highlight-mana-text :is(${n}):not([data-text-variant])`,
+        s = "box-shadow:inset 0 0 0 1px var(--yellow-300)",
         d = document.createElement("style");
     return (
         (d.id = l),
-        (d.textContent = `${s}{${i}}${s} *:not(:has(*)):not(:empty):not([data-text-variant]):not([data-text-variant] *){${i}}`),
+        (d.textContent = `${i}{${s}}${i} *:not(:has(*)):not(:empty):not([data-text-variant]):not([data-text-variant] *){${s}}`),
         document.head.appendChild(d),
         () => d.remove()
     );
