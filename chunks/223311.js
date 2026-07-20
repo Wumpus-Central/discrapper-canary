98e4f800eade30ea0d375e26edd4da3c8899b989
DiscrapperManager <admin@minerpl.xyz>
2026-07-20 23:17:10 +0000

    canaryRelease: 581454
    Main scripts: 367
    Lazy Loaded Scripts: 4326
    Chunks:
     * Main Files: 10171
     * Scripts: 9305
    Strings: 25909
    Routes: 839
    Experiments Count: 35
    Apex Experiments Count: 331

---
+++ chunks/223311.js
@@ -0,0 +1,7 @@
+l.d(e, { A: () => r });
+var n = l(17928),
+    u = l(918467);
+function r(t) {
+    let e = (0, n.bG)([u.A], () => u.A.getPromotionsForApplication(t));
+    return e?.[0] ?? null;
+}
