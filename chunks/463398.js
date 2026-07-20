c7e99fcc0bee38afd760190651afa014b7156e29
DiscrapperManager <admin@minerpl.xyz>
2026-07-20 17:10:36 +0000

    canaryRelease: 581048
    Main scripts: 371
    Lazy Loaded Scripts: 4328
    Chunks:
     * Main Files: 10192
     * Scripts: 9295
    Strings: 25903
    Routes: 839
    Experiments Count: 35
    Apex Experiments Count: 335

--- chunks/463398.js
+++ chunks/463398.js
@@ -3,18 +3,17 @@ n.d(t, { A: () => E });
 var i = n(439372),
     r = n(773669),
     a = n(287809),
-    s = n(428262);
+    s = n(166403),
+    l = n(428262);
 n(960851);
-var l = n(962644),
-    o = n(412260),
-    d = n(202541),
-    c = n(652215),
-    u = n(818348);
+var o = n(962644),
+    d = n(412260),
+    c = n(202541),
+    u = n(652215);
 class _ extends i.A {
     actions = {
         POST_CONNECTION_OPEN: this.onPostConnectionOpen.bind(this),
         EXPERIMENTS_FETCH_SUCCESS: this.onPostConnectionOpen.bind(this),
-        BILLING_SUBSCRIPTION_UPDATE_SUCCESS: this.onSubscriptionUpdated.bind(this),
         IAP_PURCHASE_PRODUCT_SUCCESS: this.onMobilePurchaseSuccess.bind(this),
         GPLAY_PURCHASE_VERIFIED: this.onMobilePurchaseSuccess.bind(this),
         BILLING_USER_OFFER_ACKNOWLEDGED_SUCCESS: this.onOfferUpdated.bind(this),
@@ -22,31 +21,66 @@ class _ extends i.A {
         BILLING_REFERRAL_RESOLVE_SUCCESS: this.onOfferUpdated.bind(this),
         VIRTUAL_CURRENCY_REDEEM_SUCCESS: this.onVCRedeemed.bind(this),
     };
-    stores = new Map().set(r.default, this.onLocaleChanged.bind(this));
+    stores = new Map()
+        .set(r.default, this.onLocaleChanged.bind(this))
+        .set(s.A, this.onSubscriptionStateChanged.bind(this))
+        .set(d.A, this.onPromotionsFetchSettled.bind(this));
+    lastSubscriptionStateSignature = null;
+    hasPendingSubscriptionRefetch = !1;
+    _terminate() {
+        (this.hasPendingSubscriptionRefetch = !1), (this.lastSubscriptionStateSignature = null);
+    }
     onLocaleChanged() {
-        null == o.A.lastFetchedActivePromotions ||
-            (o.A.lastFetchedActivePromotionsLocale !== r.default.locale && (0, l.BE)(!1));
+        null == d.A.lastFetchedActivePromotions ||
+            (d.A.lastFetchedActivePromotionsLocale !== r.default.locale && (0, o.BE)(!1));
     }
     async onPostConnectionOpen() {
-        (0, l.BE)(), await this.maybeFetchBogoPromotion();
+        (0, o.BE)(), await this.maybeFetchBogoPromotion();
     }
     maybeFetchBogoPromotion = async () => {
         let e = a.default.getCurrentUser();
-        s.Ay.isPremiumExactly(e, d.PremiumTypes.TIER_2);
+        l.Ay.isPremiumExactly(e, c.PremiumTypes.TIER_2);
     };
-    onSubscriptionUpdated(e) {
-        let { subscription: t } = e;
-        t.type === u.rz.PREMIUM && (0, l.BE)(!1);
+    onSubscriptionStateChanged() {
+        let e,
+            t =
+                null == (e = s.A.getSubscriptions(!1))
+                    ? ""
+                    : Object.values(e)
+                          .map((e) => {
+                              let t = e.items
+                                  .map((e) => e.planId)
+                                  .sort()
+                                  .join("|");
+                              return `${e.id}:${e.type}:${e.status}:${t}`;
+                          })
+                          .sort()
+                          .join(",");
+        if (
+            t !== this.lastSubscriptionStateSignature &&
+            ((this.lastSubscriptionStateSignature = t), null != d.A.lastFetchedActivePromotions)
+        ) {
+            if (d.A.isFetchingActivePromotions) {
+                this.hasPendingSubscriptionRefetch = !0;
+                return;
+            }
+            (0, o.BE)(!1);
+        }
+    }
+    onPromotionsFetchSettled() {
+        !this.hasPendingSubscriptionRefetch ||
+            d.A.isFetchingActivePromotions ||
+            ((this.hasPendingSubscriptionRefetch = !1), (0, o.BE)(!1));
     }
     onMobilePurchaseSuccess() {
-        (0, l.BE)(!1);
+        (0, o.BE)(!1);
     }
     onOfferUpdated() {
-        (0, l.BE)(!1);
+        (0, o.BE)(!1);
     }
     onVCRedeemed(e) {
         let { entitlements: t } = e;
-        t.some((e) => e.type === c.zF_.FRACTIONAL_REDEMPTION) && (0, l.BE)(!1);
+        t.some((e) => e.type === u.zF_.FRACTIONAL_REDEMPTION) && (0, o.BE)(!1);
     }
 }
 let E = new _();
