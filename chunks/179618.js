485cf517d91ec9ce945b371489b926fdc5f11767
DiscrapperManager <admin@minerpl.xyz>
2026-07-21 03:10:59 +0000

    canaryRelease: 581619
    Main scripts: 367
    Lazy Loaded Scripts: 4324
    Chunks:
     * Main Files: 10170
     * Scripts: 9331
    Strings: 25916
    Routes: 839
    Experiments Count: 35
    Apex Experiments Count: 330

--- chunks/179618.js
+++ chunks/179618.js
@@ -1,1 +1,8 @@
-_.exports = { iE: "wrapper__58105", Lw: "overlay__58105", AS: "item__58105" };
+_.exports = {
+    iE: "wrapper__58105",
+    Lw: "overlay__58105",
+    AS: "item__58105",
+    RK: "visible__58105",
+    yo: "hovered__58105",
+    wH: "selected__58105",
+};
--- chunks/941971.js
+++ chunks/941971.js
@@ -1,34 +1,17 @@
 "use strict";
-n.d(t, { A: () => c });
+n.d(t, { A: () => l });
 var i = n(627968);
 n(64700);
 var r = n(503698),
-    s = n.n(r),
-    a = n(580929),
-    o = n(866323),
-    l = n(568858);
-let u = Object.freeze({ tension: 400, friction: 30 }),
-    d = Object.freeze({ opacity: 0, height: 8, transform: "translateX(-4px) translateZ(0)" });
-function c(e) {
-    let { selected: t = !1, hovered: n = !1, unread: r = !1, disabled: c = !1, className: _, overlay: h = !1 } = e;
-    (t = !c && t), (n = !c && n), (r = !c && r);
-    let f = { opacity: 1, height: t ? 40 : n ? 20 : 8, transform: "translateX(0) translateZ(0)" },
-        E = (0, o.p)(
-            t || n || r,
-            {
-                config: u,
-                keys: (e) => (e ? "pill" : "empty"),
-                initial: t || n || r ? f : null,
-                from: d,
-                leave: d,
-                enter: f,
-                update: f,
-            },
-            n || document.hasFocus() ? "animate-always" : "animate-never",
-        );
+    a = n.n(r),
+    s = n(179618);
+function l(e) {
+    let { selected: t = !1, hovered: n = !1, unread: r = !1, disabled: l = !1, className: o, overlay: d = !1 } = e;
+    (t = !l && t), (n = !l && n), (r = !l && r);
+    let c = t || n || r;
     return (0, i.jsx)("div", {
-        className: s()(_, l.iE, { [l.Lw]: h }),
+        className: a()(o, s.iE, { [s.Lw]: d }),
         "aria-hidden": !0,
-        children: E((e, t) => t && (0, i.jsx)(a.animated.span, { className: l.AS, style: e })),
+        children: (0, i.jsx)("span", { className: a()(s.AS, { [s.RK]: c, [s.yo]: n, [s.wH]: t }) }),
     });
 }
