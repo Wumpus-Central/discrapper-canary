3b136dd27bca7516246ac83209b7b4049f707664
DiscrapperManager <admin@minerpl.xyz>
2026-07-23 18:19:59 +0000

    canaryRelease: 583192
    Main scripts: 363
    Lazy Loaded Scripts: 4351
    Chunks:
     * Main Files: 10143
     * Scripts: 9224
    Strings: 26060
    Routes: 845
    Experiments Count: 36
    Apex Experiments Count: 332

---
+++ chunks/101464.js
@@ -0,0 +1,9 @@
+_.exports = {
+    Up: "view__881e1",
+    p_: "list__881e1",
+    nM: "row__881e1",
+    Tm: "rank__881e1",
+    UU: "name__881e1",
+    hc: "edit__881e1",
+    KA: "editActions__881e1",
+};
