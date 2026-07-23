81ea0e897007666fb00f457314a629b2ce606c89
DiscrapperManager <admin@minerpl.xyz>
2026-07-23 18:22:21 +0000

    canaryRelease: 583195
    Main scripts: 363
    Lazy Loaded Scripts: 4351
    Chunks:
     * Main Files: 10143
     * Scripts: 9224
    Strings: 26060
    Routes: 845
    Experiments Count: 36
    Apex Experiments Count: 332

--- chunks/66933.js
+++ chunks/66933.js
@@ -1,8 +1,7 @@
-n.d(t, { A: () => r });
-let l = (0, n(945810).mj)({
-        name: "2026-03-app-guild-validation",
-        kind: "guild",
-        defaultConfig: { enabled: !1 },
-        variations: { 0: { enabled: !0 }, 1: { enabled: !0 } },
-    }),
-    r = 21552 == n.j ? l : null;
+n.d(e, { A: () => i });
+let i = (0, n(945810).mj)({
+    name: "2026-03-app-guild-validation",
+    kind: "guild",
+    defaultConfig: { enabled: !1 },
+    variations: { 0: { enabled: !0 }, 1: { enabled: !0 } },
+});
