7fe24195ed6ece3890457d76c6adf7550271d8de
DiscrapperManager <admin@minerpl.xyz>
2026-07-23 20:17:39 +0000

    canaryRelease: 583315
    Main scripts: 363
    Lazy Loaded Scripts: 4350
    Chunks:
     * Main Files: 10142
     * Scripts: 9212
    Strings: 26066
    Routes: 845
    Experiments Count: 36
    Apex Experiments Count: 330

--- chunks/972786.js
+++ chunks/972786.js
@@ -1,54 +1,55 @@
-n.d(t, { A: () => u });
-var r = n(17928),
-    l = n(228366);
-let i = new Map(),
+n.d(t, { A: () => T });
+var l = n(17928),
+    i = n(228366);
+let r = new Map(),
     s = null,
-    o = [],
-    a = new Map();
-class c extends r.Ay.Store {
+    a = [],
+    o = new Map();
+class u extends l.Ay.Store {
     getAllProjects() {
-        return Array.from(i.values());
+        return Array.from(r.values());
     }
     getProject(e) {
-        return i.get(e) ?? null;
+        return r.get(e) ?? null;
     }
     getLogs(e) {
-        return a.get(e) ?? o;
+        return o.get(e) ?? a;
     }
     getProjectsFetchState() {
         return s;
     }
 }
-function E(e) {
+function c(e) {
     let { project: t } = e;
-    i.set(t.id, t);
+    r.set(t.id, t);
 }
-let u = new c(l.h, {
-    LOGOUT: function () {
-        if (0 === i.size && 0 === a.size && null == s) return !1;
-        i.clear(), a.clear(), (s = null);
-    },
-    VIBEGRATIONS_PROJECTS_FETCH_START: function (e) {
-        s = { type: "loading" };
-    },
-    VIBEGRATIONS_PROJECTS_FETCH_SUCCESS: function (e) {
-        let { projects: t } = e;
-        for (let e of (i.clear(), t)) i.set(e.id, e);
-        s = { type: "success", fetchedAt: Date.now() };
-    },
-    VIBEGRATIONS_PROJECTS_FETCH_FAIL: function (e) {
-        s = { type: "error", fetchedAt: Date.now() };
-    },
-    VIBEGRATIONS_PROJECT_CREATE_SUCCESS: E,
-    VIBEGRATIONS_PROJECT_UPDATE_SUCCESS: E,
-    VIBEGRATIONS_PROJECT_DELETE_SUCCESS: function (e) {
-        let { projectId: t } = e;
-        i.delete(t), a.delete(t);
-    },
-    VIBEGRATIONS_LOG_APPEND: function (e) {
-        let { projectId: t, log: n } = e,
-            r = a.get(t),
-            l = null == r ? [n] : r.concat(n);
-        a.set(t, l.length > 500 ? l.slice(-500) : l);
-    },
-});
+let E = new u(i.h, {
+        LOGOUT: function () {
+            if (0 === r.size && 0 === o.size && null == s) return !1;
+            r.clear(), o.clear(), (s = null);
+        },
+        VIBEGRATIONS_PROJECTS_FETCH_START: function (e) {
+            s = { type: "loading" };
+        },
+        VIBEGRATIONS_PROJECTS_FETCH_SUCCESS: function (e) {
+            let { projects: t } = e;
+            for (let e of (r.clear(), t)) r.set(e.id, e);
+            s = { type: "success", fetchedAt: Date.now() };
+        },
+        VIBEGRATIONS_PROJECTS_FETCH_FAIL: function (e) {
+            s = { type: "error", fetchedAt: Date.now() };
+        },
+        VIBEGRATIONS_PROJECT_CREATE_SUCCESS: c,
+        VIBEGRATIONS_PROJECT_UPDATE_SUCCESS: c,
+        VIBEGRATIONS_PROJECT_DELETE_SUCCESS: function (e) {
+            let { projectId: t } = e;
+            r.delete(t), o.delete(t);
+        },
+        VIBEGRATIONS_LOG_APPEND: function (e) {
+            let { projectId: t, log: n } = e,
+                l = o.get(t),
+                i = null == l ? [n] : l.concat(n);
+            o.set(t, i.length > 500 ? i.slice(-500) : i);
+        },
+    }),
+    T = 21552 == n.j ? E : null;
