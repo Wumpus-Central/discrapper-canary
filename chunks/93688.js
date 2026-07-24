68448a4291f15136243ac1f3ea82c72e238971d2
DiscrapperManager <admin@minerpl.xyz>
2026-07-24 17:20:37 +0000

    canaryRelease: 583720
    Main scripts: 363
    Lazy Loaded Scripts: 4351
    Chunks:
     * Main Files: 10139
     * Scripts: 9213
    Strings: 26087
    Routes: 846
    Experiments Count: 36
    Apex Experiments Count: 327

--- chunks/93688.js
+++ chunks/93688.js
@@ -1,41 +1,40 @@
-"use strict";
-n.d(t, { W: () => l });
-var i = n(627968);
-n(64700);
-var r = n(661531),
-    a = n(996682),
-    s = n(27989);
-let l = (e) => {
+e.d(s, { W: () => d });
+var t = e(627968);
+e(64700);
+var i = e(661531),
+    l = e(996682),
+    h = e(27989);
+let d = (a) => {
     let {
-            size: t = "md",
-            width: n,
-            height: l,
-            color: o = r.A.colors.INTERACTIVE_ICON_DEFAULT,
-            colorClass: d = "",
-            ...c
-        } = e,
-        u = (0, s.J)(t),
-        _ = u?.width ?? n,
-        E = u?.height ?? l;
-    return (0, i.jsxs)("svg", {
-        ...(0, a.A)(c),
+            size: s = "md",
+            width: e,
+            height: d,
+            color: p = i.A.colors.INTERACTIVE_ICON_DEFAULT,
+            colorClass: c = "",
+            ...r
+        } = a,
+        n = (0, h.J)(s),
+        o = n?.width ?? e,
+        v = n?.height ?? d;
+    return (0, t.jsxs)("svg", {
+        ...(0, l.A)(r),
         xmlns: "http://www.w3.org/2000/svg",
-        width: _,
-        height: E,
+        width: o,
+        height: v,
         fill: "none",
         viewBox: "0 0 24 24",
         children: [
-            (0, i.jsx)("path", {
-                fill: "string" == typeof o ? o : o.css,
+            (0, t.jsx)("path", {
+                fill: "string" == typeof p ? p : p.css,
                 d: "M20 3a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H4a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h16ZM4 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm3 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm3 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2Z",
-                className: d,
+                className: c,
             }),
-            (0, i.jsx)("path", {
-                fill: "string" == typeof o ? o : o.css,
+            (0, t.jsx)("path", {
+                fill: "string" == typeof p ? p : p.css,
                 fillRule: "evenodd",
                 d: "M20 3a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H4a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h16ZM4 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm3 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm3 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2Z",
                 clipRule: "evenodd",
-                className: d,
+                className: c,
             }),
         ],
     });
