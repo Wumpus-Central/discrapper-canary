9c3dbce5f485dbf1cc541092b75e704a3d5ee50c
DiscrapperManager <admin@minerpl.xyz>
2026-07-25 22:18:01 +0000

    canaryRelease: 584106
    Main scripts: 361
    Lazy Loaded Scripts: 4358
    Chunks:
     * Main Files: 10147
     * Scripts: 9226
    Strings: 26104
    Routes: 846
    Experiments Count: 36
    Apex Experiments Count: 328

--- chunks/94343.js
+++ chunks/94343.js
@@ -1,18 +1,17 @@
-n.d(t, { A: () => a });
-var l = n(64700),
+"use strict";
+n.d(t, { A: () => s });
+var i = n(64700),
     r = n(17928),
-    i = n(775602);
-function a(e, t) {
-    let n = (0, r.bG)([i.Ay], () => i.Ay.useReducedMotion);
-    l.useEffect(() => {
+    a = n(775602);
+function s(e, t) {
+    let n = (0, r.bG)([a.Ay], () => a.Ay.useReducedMotion);
+    i.useEffect(() => {
         if (!t) return;
-        let l = e.current;
-        if (null == l) return;
+        let i = e.current;
+        if (null == i) return;
         let r = requestAnimationFrame(() => {
-            l.scrollIntoView({ behavior: n ? "auto" : "smooth" });
+            i.scrollIntoView({ behavior: n ? "auto" : "smooth" });
         });
         return () => cancelAnimationFrame(r);
     }, [e, t, n]);
 }
-r;
-)},[e,t,n])}
