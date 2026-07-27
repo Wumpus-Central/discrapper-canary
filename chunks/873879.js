28fe236bb1e73dc8ee14474922437adc6c30f0a6
DiscrapperManager <admin@minerpl.xyz>
2026-07-27 22:46:26 +0000

    canaryRelease: 584504
    Main scripts: 361
    Lazy Loaded Scripts: 4362
    Chunks:
     * Main Files: 10149
     * Scripts: 9228
    Strings: 26132
    Routes: 852
    Experiments Count: 36
    Apex Experiments Count: 328

--- chunks/873879.js
+++ chunks/873879.js
@@ -1,62 +1,70 @@
 "use strict";
-n.d(t, { A: () => E });
+n.d(t, { A: () => m });
 var i = n(284009),
     r = n.n(i),
-    s = n(791332),
-    a = n.n(s);
-let o = /\n{2,}$/,
-    l = /(?:^|\n)( *)$/,
-    u = "(?:[*-]|\\d+\\.)",
-    c = "(%INDENT_CAPTURE_PATTERN%)(" + u + ") +",
-    d = RegExp("^" + c.replace("%INDENT_CAPTURE_PATTERN%", " *")),
-    _ = c + "[^\\n]*(?:\\n(?!%INDENT_CAPTURE_PATTERN%" + u + " )[^\\n]*)*(\n|$)",
-    h = / *\n$/,
-    f = RegExp("^( *)(" + u + ") [\\s\\S]+?(?:\\n(?! )(?!\\1" + u + " )|$)"),
-    p = /^[ \t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]+$/,
-    E = {
-        ...a().defaultRules.list,
-        requiredFirstCharacters: " *-0123456789".split(""),
+    a = n(791332),
+    s = n.n(a);
+let l = /\n{2,}$/,
+    o = /(?:^|\n)( *)$/,
+    d = "(?:[*-]|\\d+\\.)",
+    c = "(%INDENT_CAPTURE_PATTERN%)(" + d + ") +",
+    u = RegExp("^" + c.replace("%INDENT_CAPTURE_PATTERN%", " *")),
+    _ = c + "[^\\n]*(?:\\n(?!%INDENT_CAPTURE_PATTERN%" + d + " )[^\\n]*)*(\n|$)",
+    E = / *\n$/,
+    A = "^( *)(" + d + ") [\\s\\S]+?(?:\\n(?! )(?!\\1" + d + " )|$)",
+    h = new RegExp(A),
+    I = RegExp("^\\n" + A.slice(1)),
+    f = /^\n/,
+    p = /\n *$/,
+    T = /^[ \t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]+$/,
+    m = {
+        ...s().defaultRules.list,
+        requiredFirstCharacters: "\n *-0123456789".split(""),
         match: (e, t) => {
             if (!t.allowList || t._listLevel >= 11) return null;
+            if (10 === e.charCodeAt(0)) {
+                let n = null == t.prevCapture ? "" : t.prevCapture[0];
+                return "" === n || p.test(n) ? null : I.exec(e);
+            }
             let n = null == t.prevCapture ? "" : t.prevCapture[0],
-                i = l.exec(n);
-            return null == i || p.test(i[0]) ? null : f.exec(e);
+                i = o.exec(n);
+            return null == i || T.test(i[0]) ? null : h.exec(e);
         },
         parse: (e, t, n) => {
             let i = e[2],
-                s = i.length > 1,
-                a = s ? Math.min(1e9, Math.max(1, +i)) : void 0,
-                l = e[0].replace(o, "\n"),
-                u = d.exec(l),
-                c = null != u ? u[0].length : 0,
-                f = null != u ? u[1].length : 0,
-                p = ` {${f},${f + 1}}`,
-                E = RegExp(_.replaceAll("%INDENT_CAPTURE_PATTERN%", p), "gm"),
-                m = RegExp("^ {1," + c + "}", "gm"),
-                g = l.match(E);
-            r()(null != g, "markup list items can not be parsed.");
-            let A = !1;
+                a = i.length > 1,
+                s = a ? Math.min(1e9, Math.max(1, +i)) : void 0,
+                o = e[0].replace(f, "").replace(l, "\n"),
+                d = u.exec(o),
+                c = null != d ? d[0].length : 0,
+                A = null != d ? d[1].length : 0,
+                h = ` {${A},${A + 1}}`,
+                I = RegExp(_.replaceAll("%INDENT_CAPTURE_PATTERN%", h), "gm"),
+                p = RegExp("^ {1," + c + "}", "gm"),
+                T = o.match(I);
+            r()(null != T, "markup list items can not be parsed.");
+            let m = !1;
             return {
-                ordered: s,
-                start: a,
-                items: g.map((e, i) => {
+                ordered: a,
+                start: s,
+                items: T.map((e, i) => {
                     let r,
-                        s = e.replace(d, "").replace(m, ""),
-                        a = i === g.length - 1,
-                        o = -1 !== s.indexOf("\n\n") || (a && A);
-                    A = o;
-                    let l = n.inline,
-                        u = n._list,
+                        a = e.replace(u, "").replace(p, ""),
+                        s = i === T.length - 1,
+                        l = -1 !== a.indexOf("\n\n") || (s && m);
+                    m = l;
+                    let o = n.inline,
+                        d = n._list,
                         c = n._listLevel;
                     (n._list = !0),
                         (n._listLevel = (c ?? 0) + 1),
-                        o ? ((n.inline = !1), (r = s.replace(h, "\n\n"))) : ((n.inline = !0), (r = s.replace(h, "")));
+                        l ? ((n.inline = !1), (r = a.replace(E, "\n\n"))) : ((n.inline = !0), (r = a.replace(E, "")));
                     let _ = t(r, { ...n, allowHeading: !1 }).map(
                         (e) => (
                             "text" === e.type && null != e.content && (e.content = e.content.replace(/\n+\s*$/, "")), e
                         ),
                     );
-                    return (n.inline = l), (n._list = u), (n._listLevel = c), _;
+                    return (n.inline = o), (n._list = d), (n._listLevel = c), _;
                 }),
             };
         },
