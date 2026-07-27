3cbf25695a38c8bf1a4fb66b57c803ab653b967b
DiscrapperManager <admin@minerpl.xyz>
2026-07-27 23:03:58 +0000

    canaryRelease: 584521
    Main scripts: 361
    Lazy Loaded Scripts: 4362
    Chunks:
     * Main Files: 10149
     * Scripts: 9230
    Strings: 26132
    Routes: 852
    Experiments Count: 36
    Apex Experiments Count: 328

--- chunks/318785.js
+++ chunks/318785.js
@@ -1,12 +1,13 @@
-l.d(i, { b: () => d });
-var n = l(17928),
-    t = l(696451),
-    r = l(71393),
-    u = l(685073);
-function d() {
-    return (0, n.yK)([r.A, t.Ay], () =>
-        r.A.getGuildsArray().filter(
-            (e) => (0, u.Rg)(e) && t.Ay.getSelfMember(e.id)?.joinedAt != null && e.profile?.tag != null,
-        ),
+d.d(t, { b: () => o });
+var a = d(17928),
+    c = d(696451),
+    n = d(71393),
+    s = d(685073);
+function o() {
+    return (0, a.yK)([n.A, c.Ay], () =>
+        n.A.getGuildsArray().filter((e) => {
+            let t = c.Ay.getSelfMember(e.id);
+            return (0, s.Rg)(e) && t?.joinedAt != null && !0 !== t.isPending && e.profile?.tag != null;
+        }),
     );
 }
