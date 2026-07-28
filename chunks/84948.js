9df2a115bbbe142b8f19f1d71692bf55bbd06901
DiscrapperManager <admin@minerpl.xyz>
2026-07-28 16:53:26 +0000

    canaryRelease: 584842
    Main scripts: 361
    Lazy Loaded Scripts: 4398
    Chunks:
     * Main Files: 10151
     * Scripts: 9267
    Strings: 26166
    Routes: 853
    Experiments Count: 36
    Apex Experiments Count: 327

--- chunks/84948.js
+++ chunks/84948.js
@@ -1,9 +1,9 @@
 "use strict";
-n.d(t, { A: () => d });
+n.d(t, { A: () => c });
 var i = n(574381),
     r = n(626584),
-    s = n(464477);
-function a(e) {
+    a = n(464477);
+function s(e) {
     let t = new Uint8Array(16),
         n = new DataView(t.buffer);
     return (
@@ -18,50 +18,55 @@ function a(e) {
     );
 }
 n(393431), n(532706), n(42231), n(232424), n(949626), n(767709), n(65162), n(508300);
-let o = new r.A("SignalHelpers.web"),
-    l = window.GLOBAL_ENV.WEBAUTHN_ORIGIN,
-    u = window.PublicKeyCredential;
-async function c(e, t, n) {
-    if (!s.d4) return null;
+let l = new r.A("SignalHelpers.web"),
+    o = window.GLOBAL_ENV.WEBAUTHN_ORIGIN;
+async function d(e, t, n) {
+    if (!a.d4) return null;
     if (i.Av && (0, i.cX)() && window.DiscordNative?.webAuthn?.[e] != null)
         return await window.DiscordNative.nativeModules.ensureModule("discord_webauthn"), t;
     let r = await PublicKeyCredential.getClientCapabilities?.();
     return void 0 !== r && r[e] ? n : null;
 }
-let d = class {
+let c = class {
     static async signalAllAcceptedCredentials(e, t) {
-        let n = await c(
+        let n = await d(
             "signalAllAcceptedCredentials",
             (e, t, n) => window.DiscordNative.webAuthn.signalAllAcceptedCredentials(e, t, n),
-            (e, t, n) => u.signalAllAcceptedCredentials({ rpId: e, userId: t, allAcceptedCredentialIds: n }),
+            (e, t, n) =>
+                window.PublicKeyCredential.signalAllAcceptedCredentials({
+                    rpId: e,
+                    userId: t,
+                    allAcceptedCredentialIds: n,
+                }),
         );
         if (null == n) return;
-        let i = a(t),
+        let i = s(t),
             r = e.map((e) => e.cred_id).filter((e) => e);
-        o.info("signalAllAcceptedCredentials", { rpId: l, encodedUserId: i, allAcceptedCredentialIds: r }),
-            await n(l, i, r).catch(o.warn);
+        l.info("signalAllAcceptedCredentials", { rpId: o, encodedUserId: i, allAcceptedCredentialIds: r }),
+            await n(o, i, r).catch(l.warn);
     }
     static async signalCurrentUserDetails(e) {
-        let t = await c(
+        let t = await d(
             "signalCurrentUserDetails",
             (e, t, n, i) => window.DiscordNative.webAuthn.signalCurrentUserDetails(e, t, n, i),
-            (e, t, n, i) => u.signalCurrentUserDetails({ rpId: e, userId: t, name: n, displayName: i }),
+            (e, t, n, i) =>
+                window.PublicKeyCredential.signalCurrentUserDetails({ rpId: e, userId: t, name: n, displayName: i }),
         );
         if (null == t) return;
         let n = e.email ?? e.global_name ?? e.username,
             i = e.username,
-            r = a(e.id);
-        o.info("signalCurrentUserDetails", { rpId: l, userId: r, name: n, displayName: i }),
-            await t(l, r, n, i).catch(o.warn);
+            r = s(e.id);
+        l.info("signalCurrentUserDetails", { rpId: o, userId: r, name: n, displayName: i }),
+            await t(o, r, n, i).catch(l.warn);
     }
     static async signalUnknownCredential(e) {
-        let t = await c(
+        let t = await d(
             "signalUnknownCredential",
             (e, t) => window.DiscordNative.webAuthn.signalUnknownCredential(e, t),
-            (e, t) => u.signalUnknownCredential({ rpId: e, credentialId: t }),
+            (e, t) => window.PublicKeyCredential.signalUnknownCredential({ rpId: e, credentialId: t }),
         );
         if (null == t) return;
         let n = "string" == typeof e ? JSON.parse(e).id : e.cred_id;
-        o.info("signalUnknownCredential", { rpId: l, credentialId: n }), await t(l, n).catch(o.warn);
+        l.info("signalUnknownCredential", { rpId: o, credentialId: n }), await t(o, n).catch(l.warn);
     }
 };
