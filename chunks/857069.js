fc4cf331521a378c4cfbdc67a00ac31f4e96ff3b
DiscrapperManager <admin@minerpl.xyz>
2026-07-28 22:50:42 +0000

    canaryRelease: 585233
    Main scripts: 361
    Lazy Loaded Scripts: 4412
    Chunks:
     * Main Files: 10158
     * Scripts: 9336
    Strings: 26196
    Routes: 854
    Experiments Count: 36
    Apex Experiments Count: 325

--- chunks/857069.js
+++ chunks/857069.js
@@ -1,19 +1,22 @@
-n.d(t, { A: () => u, l: () => o });
+n.d(t, { A: () => c, l: () => u });
 var a = n(58703),
     i = n(927813),
     s = n(935208),
     l = n(143413),
     r = n(652215);
-let d = 7 * i.A.Millis.MINUTE;
-function o(e, t, n) {
+let o = 7 * i.A.Millis.MINUTE;
+function d(e) {
+    return null == e || "" === e ? null : e;
+}
+function u(e, t, n) {
     return (
         null == t ||
         (t.type === r.TZK.MESSAGE && t.content.id === t.content.channel_id) ||
         (t.type !== r.TZK.MESSAGE && t.type !== r.TZK.THREAD_STARTER_MESSAGE) ||
-        u(e, t.content, n)
+        c(e, t.content, n)
     );
 }
-function u(e, t, n) {
+function c(e, t, n) {
     return (
         !!(n.hasFlag(r.pr7.HAS_THREAD) || n.isCommandType()) ||
         t.blocked !== n.blocked ||
@@ -27,11 +30,13 @@ function u(e, t, n) {
                   (null != n.webhookId && t.author.username !== n.author.username) ||
                   (e?.isForumPost() && t.id === s.default.castChannelIdAsMessageId(e.id)) ||
                   !(0, a.ro)(t.timestamp, n.timestamp) ||
-                  !(0, a.v0)(t.timestamp, n.timestamp, d) ||
+                  !(0, a.v0)(t.timestamp, n.timestamp, o) ||
                   (n.hasFlag(r.pr7.SUPPRESS_NOTIFICATIONS) && !t.hasFlag(r.pr7.SUPPRESS_NOTIFICATIONS)) ||
                   (t.hasFlag(r.pr7.SUPPRESS_NOTIFICATIONS) &&
                       !n.hasFlag(r.pr7.SUPPRESS_NOTIFICATIONS) &&
                       (n.mentions.length > 0 || n.mentionRoles.length > 0 || n.mentionEveryone))
-              ) || n.applicationId !== t.applicationId)
+              ) ||
+              n.applicationId !== t.applicationId ||
+              d(n.additionalName) !== d(t.additionalName))
     );
 }
