2cc47b12ad3878bc4b642e87967427c79f96c501
DiscrapperManager <admin@minerpl.xyz>
2026-07-28 23:11:11 +0000

    canaryRelease: 585255
    Main scripts: 361
    Lazy Loaded Scripts: 4412
    Chunks:
     * Main Files: 10160
     * Scripts: 9335
    Strings: 26196
    Routes: 854
    Experiments Count: 36
    Apex Experiments Count: 324

--- chunks/167579.js
+++ chunks/167579.js
@@ -1,20 +1,16 @@
 "use strict";
-n.d(t, { A: () => l });
+n.d(t, { A: () => o, N: () => l });
 var i = n(17928),
     r = n(280450),
-    a = n(210144),
+    a = n(293246),
     s = n(977997);
 function l(e) {
-    return (0, i.cf)(
-        [s.A, a.Ay, r.default],
-        () =>
-            (function (e) {
-                let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : s.A,
-                    n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : a.Ay,
-                    i = arguments.length > 3 && void 0 !== arguments[3] ? arguments[3] : r.default,
-                    l = null != e ? t.getVoiceState(e.getGuildId(), i.getId()) : null;
-                return { selfDeaf: n.isSelfDeaf(), deaf: l?.deaf ?? !1 };
-            })(e, s.A, a.Ay, r.default),
-        [e],
-    );
+    let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : s.A,
+        n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : a.Ay,
+        i = arguments.length > 3 && void 0 !== arguments[3] ? arguments[3] : r.default,
+        l = null != e ? t.getVoiceState(e.getGuildId(), i.getId()) : null;
+    return { selfDeaf: n.isSelfDeaf(), deaf: l?.deaf ?? !1 };
+}
+function o(e) {
+    return (0, i.cf)([s.A, a.Ay, r.default], () => l(e, s.A, a.Ay, r.default), [e]);
 }
