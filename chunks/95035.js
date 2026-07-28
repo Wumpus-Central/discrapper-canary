59f7f724f6ee2ecd3c22aab1477eea6c9ab1df25
DiscrapperManager <admin@minerpl.xyz>
2026-07-28 16:27:13 +0000

    canaryRelease: 584809
    Main scripts: 361
    Lazy Loaded Scripts: 4398
    Chunks:
     * Main Files: 10151
     * Scripts: 9267
    Strings: 26166
    Routes: 853
    Experiments Count: 36
    Apex Experiments Count: 327

--- chunks/95035.js
+++ chunks/95035.js
@@ -1,29 +1,30 @@
-a.d(t, { A: () => u });
-var r = a(627968),
-    l = a(64700),
-    n = a(187322),
-    s = a(976860),
-    p = a(650583);
-let u = l.forwardRef(function (e, t) {
-    let { href: a, children: u, onClick: c, onKeyPress: h, focusProps: i, transitionExtras: d, ...o } = e,
-        k = l.useCallback(
+"use strict";
+n.d(t, { A: () => o });
+var i = n(627968),
+    r = n(64700),
+    a = n(187322),
+    s = n(976860),
+    l = n(650583);
+let o = r.forwardRef(function (e, t) {
+    let { href: n, children: o, onClick: d, onKeyPress: c, focusProps: u, transitionExtras: _, ...E } = e,
+        A = r.useCallback(
             (e) => {
                 e.repeat ||
-                    ((e.key === p.dh.SPACE || e.key === p.dh.ENTER) &&
-                        (e.preventDefault(), null != a && (0, s.pX)(a, d), c?.()),
-                    h?.(e));
+                    ((e.key === l.dh.SPACE || e.key === l.dh.ENTER) &&
+                        (e.preventDefault(), null != n && (0, s.pX)(n, _), d?.()),
+                    c?.(e));
             },
-            [a, h, c, d],
+            [n, c, d, _],
         ),
-        f = l.useCallback(
+        h = r.useCallback(
             (e) => {
                 e.metaKey ||
                     e.shiftKey ||
                     0 !== e.button ||
-                    (e.preventDefault(), e.stopPropagation(), null != a && (0, s.pX)(a, d), c?.());
+                    (e.preventDefault(), e.stopPropagation(), null != n && (0, s.pX)(n, _), d?.());
             },
-            [a, c, d],
+            [n, d, _],
         ),
-        C = (0, r.jsx)("a", { ref: t, href: a, onClick: f, onKeyPress: k, ...o, children: u });
-    return (0, r.jsx)(n.vN, { ...i, children: C });
+        I = (0, i.jsx)("a", { ref: t, href: n, onClick: h, onKeyPress: A, ...E, children: o });
+    return (0, i.jsx)(a.vN, { ...u, children: I });
 });
