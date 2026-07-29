4d4cb3bbcc8ec37f9ca98847739c1342de03211d
DiscrapperManager <admin@minerpl.xyz>
2026-07-29 15:36:51 +0000

    canaryRelease: 585489
    Main scripts: 361
    Lazy Loaded Scripts: 4412
    Chunks:
     * Main Files: 10167
     * Scripts: 9346
    Strings: 26200
    Routes: 854
    Experiments Count: 36
    Apex Experiments Count: 324

--- chunks/595529.js
+++ chunks/595529.js
@@ -1,232 +1,231 @@
 "use strict";
-let i;
-n.d(t, { b5: () => M, Bo: () => v, AA: () => U, eE: () => D, PP: () => w, Vu: () => b, Tr: () => P, DP: () => R });
-var r = n(582128),
-    a = n(17928),
-    s = n(554146),
-    l = n(206885),
-    o = n(626584),
-    d = n(773371),
-    c = n(184809),
-    u = n(41984),
-    _ = n(296027);
-new o.A("OverlayUtils");
-var E = n(617617),
-    A = n(967198),
-    h = n(174459),
-    I = n(935208),
-    f = n(460288),
-    p = n(199773),
-    T = n(558845),
-    m = n(826673),
-    g = n(367727),
-    S = n(757792),
-    N = n(652215),
-    C = n(355097);
-function O(e, t, n, l, o) {
-    let A = (0, T.Ay)((t) => null != e && t.currentlyShown.has(e)),
-        I = (0, a.bG)([_.default, d.default, c.default], () => {
-            if (__OVERLAY__) return c.default.isInstanceLocked();
-            let e = _.default.getOverlayRenderingTrackedGames();
-            return 0 !== e.length && e.some((e) => e.overlayMethod !== u.Ue.Disabled && d.default.isInputLocked(e.pid));
+n.d(t, { b5: () => b, Bo: () => D, AA: () => P, eE: () => y, PP: () => U, Vu: () => v, Tr: () => M, DP: () => O });
+var i = n(582128),
+    r = n(17928),
+    a = n(554146),
+    s = n(206885),
+    l = n(626584),
+    o = n(773371),
+    d = n(184809),
+    c = n(41984),
+    u = n(296027);
+new l.A("OverlayUtils");
+var _ = n(617617),
+    E = n(967198),
+    A = n(174459),
+    h = n(935208),
+    I = n(460288),
+    f = n(199773),
+    p = n(558845),
+    T = n(826673),
+    m = n(367727),
+    g = n(757792),
+    S = n(652215),
+    N = n(355097);
+function C(e, t, l, E, h) {
+    let I = (0, p.Ay)((t) => null != e && t.currentlyShown.has(e)),
+        T = (0, r.bG)([u.default, o.default, d.default], () => {
+            if (__OVERLAY__) return d.default.isInstanceLocked();
+            let e = u.default.getOverlayRenderingTrackedGames();
+            return 0 !== e.length && e.some((e) => e.overlayMethod !== c.Ue.Disabled && o.default.isInputLocked(e.pid));
         }),
-        f = (0, a.bG)(null != i ? [i] : [], () => i?.getFocusedPID()),
-        m = (0, a.bG)([p.A], () => null != e && p.A.hasUserHitDCCap(e, t)),
-        S = r.useRef(t);
+        g = s.O ? n(773371).default : null,
+        C = (0, r.bG)(null != g ? [g] : [], () => g?.getFocusedPID()),
+        O = (0, r.bG)([f.A], () => null != e && f.A.hasUserHitDCCap(e, t)),
+        R = i.useRef(t);
     return (
-        (S.current = t),
-        r.useEffect(() => {
+        (R.current = t),
+        i.useEffect(() => {
             if (null != e) {
                 var t;
                 return (
-                    E.A.hasLoaded(C.oD.PRELOADED_USER_SETTINGS) ||
-                        ((t = S.current),
-                        h.default.track(N.HAw.DISMISSIBLE_CONTENT_SHOWN_BEFORE_CONNECTION_OPEN, {
-                            content_type: s.M[e],
-                            group_name: n ?? null,
-                            latest_version: l ?? null,
+                    _.A.hasLoaded(N.oD.PRELOADED_USER_SETTINGS) ||
+                        ((t = R.current),
+                        A.default.track(S.HAw.DISMISSIBLE_CONTENT_SHOWN_BEFORE_CONNECTION_OPEN, {
+                            content_type: a.M[e],
+                            group_name: l ?? null,
+                            latest_version: E ?? null,
                             guild_id: t ?? null,
-                            snowflake_id: o ?? null,
+                            snowflake_id: h ?? null,
                         })),
-                    (0, g.Vh)(e, { groupName: n, guildId: S.current, version: l, snowflakeId: o }, I, f),
+                    (0, m.Vh)(e, { groupName: l, guildId: R.current, version: E, snowflakeId: h }, T, C),
                     () => {
                         if (null == e) return;
-                        let t = !p.A.hasUserHitDCCap();
-                        (0, T.pd)({ content: e, groupName: n }, t);
+                        let t = !f.A.hasUserHitDCCap();
+                        (0, p.pd)({ content: e, groupName: l }, t);
                     }
                 );
             }
-        }, [e, n, m, I, l, f, o]),
-        A && null != e ? e : null
+        }, [e, l, O, T, E, C, h]),
+        I && null != e ? e : null
     );
 }
-function R(e, t) {
-    let n = (0, a.bG)([E.A], () => E.A.settings.userContent?.dismissedContents),
-        i = (0, a.bG)([A.A], () => A.A.getGuildId()),
-        s = (0, S.Sg)(e),
+function O(e, t) {
+    let n = (0, r.bG)([_.A], () => _.A.settings.userContent?.dismissedContents),
+        a = (0, r.bG)([E.A], () => E.A.getGuildId()),
+        s = (0, g.Sg)(e),
         l = null;
     return (
-        E.A.hasLoaded(C.oD.PRELOADED_USER_SETTINGS)
-            ? (l = s.find((e) => null == n || !(0, f.c0)(n, e)))
-            : null != n && (l = s.find((e) => !(0, f.c0)(n, e))),
+        _.A.hasLoaded(N.oD.PRELOADED_USER_SETTINGS)
+            ? (l = s.find((e) => null == n || !(0, I.c0)(n, e)))
+            : null != n && (l = s.find((e) => !(0, I.c0)(n, e))),
         [
-            O(l, i, t),
-            r.useCallback(
+            C(l, a, t),
+            i.useCallback(
                 (e, n) => {
-                    null != l && (0, m.Dr)(l, { dismissAction: e, groupName: t, guildId: i, forceTrack: n });
+                    null != l && (0, T.Dr)(l, { dismissAction: e, groupName: t, guildId: a, forceTrack: n });
                 },
-                [l, t, i],
+                [l, t, a],
             ),
         ]
     );
 }
-i = l.O ? n(773371).default : null;
-let L = {};
-function y(e) {
-    return (0, a.bG)(
-        [E.A],
-        () => (null !== e ? E.A.settings.userContent?.recurringDismissibleContentStates?.[e] : null) ?? L,
+let R = {};
+function L(e) {
+    return (0, r.bG)(
+        [_.A],
+        () => (null !== e ? _.A.settings.userContent?.recurringDismissibleContentStates?.[e] : null) ?? R,
     );
 }
-function D(e, t, n) {
-    let { lastDismissedVersion: i } = y(e),
-        s = (0, a.bG)([A.A], () => A.A.getGuildId()),
+function y(e, t, n) {
+    let { lastDismissedVersion: a } = L(e),
+        s = (0, r.bG)([E.A], () => E.A.getGuildId()),
         l = null;
     if (null != e) {
-        let n = !(0, S.P3)(e);
-        E.A.hasLoaded(C.oD.PRELOADED_USER_SETTINGS)
-            ? (l = n && (null == i || i < t) ? e : null)
-            : null != i && (l = n && i < t ? e : null);
+        let n = !(0, g.P3)(e);
+        _.A.hasLoaded(N.oD.PRELOADED_USER_SETTINGS)
+            ? (l = n && (null == a || a < t) ? e : null)
+            : null != a && (l = n && a < t ? e : null);
     }
     return [
-        O(l, s, n, t),
-        r.useCallback(
+        C(l, s, n, t),
+        i.useCallback(
             (e, i) => {
-                null != l && (0, g.$l)(l, t, { dismissAction: e, groupName: n, guildId: s, forceTrack: i, version: t });
+                null != l && (0, m.$l)(l, t, { dismissAction: e, groupName: n, guildId: s, forceTrack: i, version: t });
             },
             [l, n, s, t],
         ),
     ];
 }
-function v(e, t, n) {
-    let { lastDismissedAtMs: i, numTimesDismissed: s } = y(e),
-        l = (0, a.bG)([A.A], () => A.A.getGuildId()),
+function D(e, t, n) {
+    let { lastDismissedAtMs: a, numTimesDismissed: s } = L(e),
+        l = (0, r.bG)([E.A], () => E.A.getGuildId()),
         o = null;
     return (
-        null != e && (o = G(!(0, S.P3)(e), i, s, t) ? e : null),
+        null != e && (o = w(!(0, g.P3)(e), a, s, t) ? e : null),
         [
-            O(o, l, n),
-            r.useCallback(
+            C(o, l, n),
+            i.useCallback(
                 (e, t) => {
-                    null != o && (0, g.uh)(o, { dismissAction: e, groupName: n, guildId: l, forceTrack: t });
+                    null != o && (0, m.uh)(o, { dismissAction: e, groupName: n, guildId: l, forceTrack: t });
                 },
                 [o, n, l],
             ),
         ]
     );
 }
-function b(e, t, n) {
-    let { lastDismissedObjectId: i } = y(e),
-        s = (0, a.bG)([A.A], () => A.A.getGuildId()),
+function v(e, t, n) {
+    let { lastDismissedObjectId: a } = L(e),
+        s = (0, r.bG)([E.A], () => E.A.getGuildId()),
         l = null;
     if (null != e) {
-        let n = !(0, S.P3)(e);
-        E.A.hasLoaded(C.oD.PRELOADED_USER_SETTINGS)
-            ? (l = n && (null == i || 1 === I.default.compare(t, i)) ? e : null)
-            : null != i && (l = n && 1 === I.default.compare(t, i) ? e : null);
+        let n = !(0, g.P3)(e);
+        _.A.hasLoaded(N.oD.PRELOADED_USER_SETTINGS)
+            ? (l = n && (null == a || 1 === h.default.compare(t, a)) ? e : null)
+            : null != a && (l = n && 1 === h.default.compare(t, a) ? e : null);
     }
     return [
-        O(l, s, n, void 0, t),
-        r.useCallback(
+        C(l, s, n, void 0, t),
+        i.useCallback(
             (e, i) => {
-                null != l && (0, g.qr)(l, t, { dismissAction: e, groupName: n, guildId: s, forceTrack: i });
+                null != l && (0, m.qr)(l, t, { dismissAction: e, groupName: n, guildId: s, forceTrack: i });
             },
             [l, n, s, t],
         ),
     ];
 }
-function M(e, t, n, i) {
-    let s = (0, a.bG)([E.A], () => E.A.getGuildDismissedContentState(n)),
+function b(e, t, n, a) {
+    let s = (0, r.bG)([_.A], () => _.A.getGuildDismissedContentState(n)),
         l = null != e ? (null == s ? null : s?.[e]) : null,
         o = l?.lastDismissedObjectId,
         d = null;
     if (null != e) {
-        let n = !(0, S.P3)(e);
-        E.A.hasLoaded(C.oD.PRELOADED_USER_SETTINGS)
-            ? (d = n && (null == o || 1 === I.default.compare(t, o)) ? e : null)
-            : null != o && (d = n && 1 === I.default.compare(t, o) ? e : null);
+        let n = !(0, g.P3)(e);
+        _.A.hasLoaded(N.oD.PRELOADED_USER_SETTINGS)
+            ? (d = n && (null == o || 1 === h.default.compare(t, o)) ? e : null)
+            : null != o && (d = n && 1 === h.default.compare(t, o) ? e : null);
     }
     return [
-        O(d, n, i, void 0, t),
-        r.useCallback(
-            (e, r) => {
-                null != d && (0, m.in)(d, t, n, { dismissAction: e, groupName: i, guildId: n, forceTrack: r });
+        C(d, n, a, void 0, t),
+        i.useCallback(
+            (e, i) => {
+                null != d && (0, T.in)(d, t, n, { dismissAction: e, groupName: a, guildId: n, forceTrack: i });
             },
-            [d, i, n, t],
+            [d, a, n, t],
         ),
     ];
 }
-function P(e, t, n, i) {
-    let s = (0, a.bG)([A.A], () => A.A.getGuildId()),
+function M(e, t, n, a) {
+    let s = (0, r.bG)([E.A], () => E.A.getGuildId()),
         l = null;
     return (
-        null == e || (0, g.j6)(e, n, t) || (l = e),
+        null == e || (0, m.j6)(e, n, t) || (l = e),
         [
-            O(l, s, i, void 0, n),
-            r.useCallback(
+            C(l, s, a, void 0, n),
+            i.useCallback(
                 (e, t) => {
-                    null != l && (0, g.qr)(l, n, { dismissAction: e, groupName: i, guildId: s, forceTrack: t });
+                    null != l && (0, m.qr)(l, n, { dismissAction: e, groupName: a, guildId: s, forceTrack: t });
                 },
-                [l, i, s, n],
+                [l, a, s, n],
             ),
         ]
     );
 }
-function U(e, t, n) {
-    let i = (0, a.bG)([E.A], () => E.A.getGuildDismissedContentState(t)),
-        s = (0, S.Sg)(e),
+function P(e, t, n) {
+    let a = (0, r.bG)([_.A], () => _.A.getGuildDismissedContentState(t)),
+        s = (0, g.Sg)(e),
         l = null;
     return (
-        E.A.hasLoaded(C.oD.PRELOADED_USER_SETTINGS)
-            ? (l = s.find((e) => null == i || null == i[e] || !1 === i[e].dismissed))
-            : null != i && (l = s.find((e) => null == i[e] || !1 === i[e].dismissed)),
+        _.A.hasLoaded(N.oD.PRELOADED_USER_SETTINGS)
+            ? (l = s.find((e) => null == a || null == a[e] || !1 === a[e].dismissed))
+            : null != a && (l = s.find((e) => null == a[e] || !1 === a[e].dismissed)),
         [
-            O(l, t, n),
-            r.useCallback(
+            C(l, t, n),
+            i.useCallback(
                 (e, i) => {
-                    null != l && (0, m.dX)(l, t, { dismissAction: e, groupName: n, guildId: t, forceTrack: i });
+                    null != l && (0, T.dX)(l, t, { dismissAction: e, groupName: n, guildId: t, forceTrack: i });
                 },
                 [l, n, t],
             ),
         ]
     );
 }
-function w(e, t, n, i) {
-    let s = (0, a.bG)([E.A], () => E.A.getGuildDismissedContentState(t)),
+function U(e, t, n, a) {
+    let s = (0, r.bG)([_.A], () => _.A.getGuildDismissedContentState(t)),
         l = null != e ? (null == s ? null : s?.[e]) : null,
         o = l?.numTimesDismissed,
         d = null;
     return (
-        null != e && (d = G(!(0, S.P3)(e), l?.lastDismissedAtMs, o, n) ? e : null),
+        null != e && (d = w(!(0, g.P3)(e), l?.lastDismissedAtMs, o, n) ? e : null),
         [
-            O(d, t, i),
-            r.useCallback(
+            C(d, t, a),
+            i.useCallback(
                 (e, n) => {
-                    null != d && (0, m.KO)(d, t, { dismissAction: e, groupName: i, guildId: t, forceTrack: n });
+                    null != d && (0, T.KO)(d, t, { dismissAction: e, groupName: a, guildId: t, forceTrack: n });
                 },
-                [d, i, t],
+                [d, a, t],
             ),
         ]
     );
 }
-function G(e, t, n, i) {
+function w(e, t, n, i) {
     let r = null != t ? (Number.isNaN(Number(t)) ? void 0 : Number(t)) : void 0,
         a = void 0 === r ? 0 : r + i.cooldownDurationMs,
         s = Date.now(),
         l = null == i.showAfterTimestamp || (s >= i.showAfterTimestamp && (r ?? 0) <= i.showAfterTimestamp),
         o = null == i.numTimesToRecur || 0 === i.numTimesToRecur || null == n || n < i.numTimesToRecur;
     return (
-        (E.A.hasLoaded(C.oD.PRELOADED_USER_SETTINGS) || (null != r && null != n)) &&
+        (_.A.hasLoaded(N.oD.PRELOADED_USER_SETTINGS) || (null != r && null != n)) &&
         e &&
         l &&
         (null == r || s >= a) &&
