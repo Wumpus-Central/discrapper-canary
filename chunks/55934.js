b64f950e2d41e68d5200e797c9829ee2467d860b
DiscrapperManager <admin@minerpl.xyz>
2026-08-04 17:06:22 +0000

    canaryRelease: 588183
    Main scripts: 363
    Lazy Loaded Scripts: 4504
    Chunks:
     * Main Files: 10129
     * Scripts: 9505
    Strings: 26288
    Routes: 859
    Experiments Count: 36
    Apex Experiments Count: 336

--- chunks/55934.js
+++ chunks/55934.js
@@ -1,7 +1,7 @@
 e.d(a, { default: () => k });
 var i = e(477900),
-    s = e(582128),
-    n = e(192308),
+    n = e(582128),
+    s = e(192308),
     l = e(772707),
     c = e(34188),
     o = e(793574),
@@ -9,14 +9,14 @@ var i = e(477900),
     d = e(149256),
     p = e(758836),
     u = e(375708);
-let b = new Date(2026, 8, 9);
+let b = new Date(2026, 8, 11);
 function k(t) {
     let { transitionState: a, onClose: e } = t,
-        k = s.useCallback(() => {
-            (0, n.closeAllModals)(), (0, d.I)();
+        k = n.useCallback(() => {
+            (0, s.closeAllModals)(), (0, d.I)();
         }, []),
-        C = s.useCallback(() => {
-            (0, n.closeAllModals)(),
+        C = n.useCallback(() => {
+            (0, s.closeAllModals)(),
                 (0, r.Cz)({ tab: p.G2.HOME, analyticsLocations: [], analyticsSource: o.A.PREMIUM_MARKETING });
         }, []);
     return (0, i.jsx)(l.k, {
@@ -25,7 +25,7 @@ function k(t) {
             src: "https://cdn.discordapp.com/assets/content/d4a35e70f4690b501fc8daa265f8b10d78b4fd705e6e48477e30e03ab92a2e19.png",
             aspectRatio: "16/9",
         },
-        gradientColor: "purple",
+        gradientColor: "nitro-pink",
         title: u.intl.string(u.t.qIVU1h),
         subtitle: u.intl.format(u.t.SlS71X, { date: b }),
         onClose: e,
