9a0f7a84fec74188d07ba8ab7b982e6b412d02f6
DiscrapperManager <admin@minerpl.xyz>
2026-08-05 19:16:01 +0000

    canaryRelease: 588776
    Main scripts: 367
    Lazy Loaded Scripts: 4478
    Chunks:
     * Main Files: 10137
     * Scripts: 9483
    Strings: 26301
    Routes: 859
    Experiments Count: 36
    Apex Experiments Count: 335

---
+++ chunks/326009.js
@@ -0,0 +1,101 @@
+"use strict";
+n.d(t, { A: () => N, N: () => m });
+var i = n(477900),
+    r = n(582128),
+    a = n(503698),
+    s = n.n(a),
+    l = n(750943),
+    o = n(691540),
+    d = n(857250),
+    c = n(97483),
+    u = n(259678),
+    _ = n(289873),
+    E = n(946274),
+    A = n(38405),
+    h = n(958805),
+    I = n(652215);
+function f(e, t) {
+    let n = window.GLOBAL_ENV.CDN_HOST ?? I.f34;
+    return `https://${n}/widget-assets/${e}/${t}`;
+}
+var p = n(375708),
+    T = n(902391);
+function m(e) {
+    let { className: t, userId: n, image: r } = e;
+    if ("localDataUri" in r) return (0, i.jsx)("img", { className: t, alt: "", src: r.localDataUri });
+    let a = f(n, r.fileId);
+    return (0, i.jsx)("img", { className: t, alt: "", src: a, width: r.width, height: r.height });
+}
+function g(e) {
+    let { canEdit: t } = e;
+    return (0, i.jsx)("div", {
+        className: T.qf,
+        children: t ? (0, i.jsx)(l.X, { className: T.Dm, size: "md" }) : null,
+    });
+}
+function S(e) {
+    let { className: t, userId: n, image: a, previewUri: l, onImageUploadStarted: I, onImageUploadCompleted: m } = e,
+        S = r.useRef(0),
+        N = r.useCallback(
+            async (e, t) => {
+                S.current = S.current + 1;
+                let n = S.current;
+                I?.(e);
+                try {
+                    let i = await h.A.uploadWidgetAsset(t);
+                    if (S.current !== n) return;
+                    m({ filename: i, localDataUri: e });
+                } catch (e) {
+                    if (S.current !== n) return;
+                    (0, o.P0)((0, d.o)(p.intl.string(p.t.F4Neqh), c.Ck.FAILURE)), A.A.captureException(e), m(null);
+                }
+            },
+            [I, m],
+        ),
+        C = l ?? (null != a ? ("localDataUri" in a ? a.localDataUri : f(n, a.fileId)) : null);
+    return (0, i.jsx)(u.vN, {
+        within: !0,
+        children: (0, i.jsxs)("div", {
+            className: s()(T.kL, t),
+            children: [
+                null != C ? (0, i.jsx)("img", { alt: "", src: C, className: T.Sl }) : (0, i.jsx)(g, { canEdit: !0 }),
+                null != l
+                    ? (0, i.jsx)("div", {
+                          className: T.ob,
+                          children: (0, i.jsx)(_.y, { type: _.t.SPINNING_CIRCLE_SIMPLE }),
+                      })
+                    : null,
+                (0, i.jsx)(E.Ay, {
+                    tabIndex: 0,
+                    onChange: N,
+                    maxFileSizeBytes: 0xa00000,
+                    multiple: !1,
+                    "aria-label": p.intl.string(p.t["MsUY/S"]),
+                }),
+            ],
+        }),
+    });
+}
+function N(e) {
+    let {
+        className: t,
+        canEdit: n,
+        userId: r,
+        image: a,
+        previewUri: l,
+        onImageUploadStarted: o,
+        onImageUploadCompleted: d,
+    } = e;
+    return n
+        ? (0, i.jsx)(S, {
+              className: t,
+              userId: r,
+              image: a,
+              previewUri: l,
+              onImageUploadStarted: o,
+              onImageUploadCompleted: d,
+          })
+        : null == a
+          ? (0, i.jsx)("div", { className: t, children: (0, i.jsx)(g, { canEdit: !1 }) })
+          : (0, i.jsx)(m, { className: s()(T.Sl, t), userId: r, image: a });
+}
