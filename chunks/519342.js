6acbd547e58713b1ce91b4ef512641384b113511
DiscrapperManager <admin@minerpl.xyz>
2026-08-06 17:02:50 +0000

    canaryRelease: 589151
    Main scripts: 367
    Lazy Loaded Scripts: 4499
    Chunks:
     * Main Files: 10141
     * Scripts: 9508
    Strings: 26311
    Routes: 859
    Experiments Count: 36
    Apex Experiments Count: 336

--- chunks/519342.js
+++ chunks/519342.js
@@ -3,7 +3,7 @@ var i = n(477900),
     l = n(582128),
     a = n(503698),
     s = n.n(a),
-    r = n(560952),
+    r = n(534740),
     o = n(821578),
     c = n(17928),
     d = n(717421),
@@ -24,19 +24,19 @@ var i = n(477900),
     I = n(996752),
     b = n(610274),
     T = n(655413),
-    R = n(884863),
-    _ = n(4264),
+    _ = n(884863),
+    R = n(4264),
     S = n(662731),
     P = n(375708),
     L = n(820447);
-function w(e) {
+function O(e) {
     let {
             userId: t,
             channelId: a,
             x: N,
             y: I,
-            statusId: w,
-            numericAvatarSize: O,
+            statusId: O,
+            numericAvatarSize: w,
             avatarSize: M,
             posturesEnabled: D,
         } = e,
@@ -48,9 +48,9 @@ function w(e) {
         F = (0, m.A)({ userId: t }),
         z = (0, c.bG)([C.A], () => C.A.isFocused()),
         W = (0, y.tx)(B, a, U),
-        Y = (0, c.bG)([E.A], () => E.A.getRoom(a)?.background ?? o.I.DEFAULT),
-        { seats: K } = _.iX[Y],
-        X = K.find((e) => e.position.x === N && e.position.y === I),
+        K = (0, c.bG)([E.A], () => E.A.getRoom(a)?.background ?? o.I.DEFAULT),
+        { seats: Y } = R.iX[K],
+        X = Y.find((e) => e.position.x === N && e.position.y === I),
         $ = (0, d.z)({ x: N, y: I, config: { ...r.config.default, duration: 250 } }),
         Z = l.useCallback(() => {
             null != B && (0, v.EB)({ guildId: B, channelId: a, interactionType: "clicked" });
@@ -107,14 +107,15 @@ function w(e) {
             null != B && (0, v.EB)({ guildId: B, channelId: a, interactionType: "hovered" });
         }, [B, a]);
     if (null == U || null == B) return null;
-    let J = H?.isVoiceDeafened()
-            ? P.intl.formatToPlainString(P.t["9hDjai"], { username: W })
+    let J = null != X ? P.intl.formatToPlainString(S.default["4bL+KW"], { username: W, seatLabel: X.getLabel() }) : W,
+        ee = H?.isVoiceDeafened()
+            ? P.intl.formatToPlainString(P.t["9hDjai"], { username: J })
             : H?.isVoiceMuted()
-              ? P.intl.formatToPlainString(P.t.Hd1oVG, { username: W })
-              : W,
-        ee = (0, i.jsx)(A.A, {
+              ? P.intl.formatToPlainString(P.t.Hd1oVG, { username: J })
+              : J,
+        et = (0, i.jsx)(A.A, {
             userId: U.id,
-            src: U.getAvatarURL(B, O, F && z),
+            src: U.getAvatarURL(B, w, F && z),
             size: M,
             muted: H?.isVoiceMuted() ?? !1,
             deafen: H?.isVoiceDeafened() ?? !1,
@@ -122,49 +123,49 @@ function w(e) {
             ringing: !1,
             className: X?.dim ? L.r3 : void 0,
         }),
-        et = {
-            left: $.x.to((e) => `calc(${e}% - ${O / 2}px)`),
-            top: $.y.to((e) => `calc(${e}% - ${O / 2}px)`),
+        en = {
+            left: $.x.to((e) => `calc(${e}% - ${w / 2}px)`),
+            top: $.y.to((e) => `calc(${e}% - ${w / 2}px)`),
             zIndex: V ? 1e6 : 1e3 * Math.round(I) + Math.round(N),
         };
     if (!D)
         return (0, i.jsx)(r.animated.div, {
             role: "listitem",
             className: L.f1,
-            style: et,
+            style: en,
             children: (0, i.jsx)(u.D, {
-                "aria-label": J,
+                "aria-label": ee,
                 className: L.KI,
                 onMouseEnter: Q,
                 onClick: Z,
                 onContextMenu: q,
-                children: ee,
+                children: et,
             }),
         });
-    let en = b.x.find((e) => e.id === w),
-        ei = en?.icon,
-        el = (0, i.jsx)("div", { className: L.R3, children: ee });
+    let ei = b.x.find((e) => e.id === O),
+        el = ei?.icon,
+        ea = (0, i.jsx)("div", { className: L.R3, children: et });
     return (0, i.jsxs)(i.Fragment, {
         children: [
             (0, i.jsx)(r.animated.div, {
                 role: "listitem",
                 className: L.f1,
-                style: et,
+                style: en,
                 children: k
-                    ? (0, i.jsxs)(R.T, {
+                    ? (0, i.jsxs)(_.T, {
                           isOpen: V,
                           onOpenChange: G,
                           children: [
-                              (0, i.jsx)(R.T.Trigger, {
+                              (0, i.jsx)(_.T.Trigger, {
                                   children: (e) => {
                                       let { ref: t, onClick: n, ...l } = e;
                                       return (0, i.jsx)(T.A, {
                                           name: W,
-                                          status: en,
+                                          status: ei,
                                           shouldShow: !V,
                                           children: (0, i.jsx)(u.D, {
                                               ...l,
-                                              "aria-label": J,
+                                              "aria-label": ee,
                                               className: s()(L.KI, L.hZ, { [L.Zu]: V }),
                                               innerRef: (e) => {
                                                   t.current = e;
@@ -174,23 +175,23 @@ function w(e) {
                                                   n(), Z();
                                               },
                                               onContextMenu: q,
-                                              children: el,
+                                              children: ea,
                                           }),
                                       });
                                   },
                               }),
-                              (0, i.jsx)(R.T.Popup, {
+                              (0, i.jsx)(_.T.Popup, {
                                   "aria-label": P.intl.string(S.default.LTAf2V),
                                   children: b.x.map((e) => {
                                       let t = e.icon;
                                       return (0, i.jsx)(
-                                          R.T.Item,
+                                          _.T.Item,
                                           {
                                               text: P.intl.string(e.label),
                                               icon: t,
-                                              selected: w === e.id,
+                                              selected: O === e.id,
                                               onClick: () => {
-                                                  let t = w === e.id;
+                                                  let t = O === e.id;
                                                   (0, j.AQ)(B, a, {
                                                       user_status_id: t ? 0 : e.id,
                                                       user_status_text: t ? "" : P.intl.string(e.label),
@@ -205,28 +206,28 @@ function w(e) {
                       })
                     : (0, i.jsx)(T.A, {
                           name: W,
-                          status: en,
+                          status: ei,
                           children: (0, i.jsx)(u.D, {
-                              "aria-label": J,
+                              "aria-label": ee,
                               className: L.KI,
                               onMouseEnter: Q,
                               onClick: Z,
                               onContextMenu: q,
-                              children: el,
+                              children: ea,
                           }),
                       }),
             }),
-            (k || (null != en && null != ei)) &&
+            (k || (null != ei && null != el)) &&
                 (0, i.jsxs)(r.animated.div, {
                     className: s()(L.v7, { [L.Zu]: V }),
-                    style: { ...et, width: O, height: O },
+                    style: { ...en, width: w, height: w },
                     "aria-hidden": !0,
                     children: [
-                        null != en &&
-                            null != ei &&
+                        null != ei &&
+                            null != el &&
                             (0, i.jsx)("div", {
                                 className: s()(L.qS, L.Od),
-                                children: (0, i.jsx)(ei, { size: "xxs", color: "currentColor" }),
+                                children: (0, i.jsx)(el, { size: "xxs", color: "currentColor" }),
                             }),
                         k &&
                             (0, i.jsx)("div", {
@@ -238,7 +239,7 @@ function w(e) {
         ],
     });
 }
-function O(e) {
+function w(e) {
     let { channelId: t, guildId: n, users: l, numericAvatarSize: a, avatarSize: s } = e,
         { posturesEnabled: r } = N.A.useExperiment({ guildId: n, location: "GuildRoomUserList" });
     return (0, i.jsx)("div", {
@@ -248,7 +249,7 @@ function O(e) {
         children: l.entries().map((e) => {
             let [n, l] = e;
             return (0, i.jsx)(
-                w,
+                O,
                 {
                     userId: n,
                     channelId: t,
@@ -271,5 +272,5 @@ function M(e) {
         { numericAvatarSize: s, avatarSize: r } = (0, I.F)(n);
     return 0 === a.size || null == l
         ? null
-        : (0, i.jsx)(O, { channelId: t, guildId: l, users: a, numericAvatarSize: s, avatarSize: r });
+        : (0, i.jsx)(w, { channelId: t, guildId: l, users: a, numericAvatarSize: s, avatarSize: r });
 }
