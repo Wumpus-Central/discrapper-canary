9b4d4425d8f88d9fa4a30d60baf76e21caef3084
DiscrapperManager <admin@minerpl.xyz>
2026-08-10 18:07:07 +0000

    canaryRelease: 590226
    Main scripts: 369
    Lazy Loaded Scripts: 4510
    Chunks:
     * Main Files: 10152
     * Scripts: 9536
    Strings: 26342
    Routes: 859
    Experiments Count: 36
    Apex Experiments Count: 332

--- chunks/683412.js
+++ chunks/683412.js
@@ -1,14 +1,19 @@
 "use strict";
-n.d(t, { A: () => o });
-var i = n(791332),
+n.d(t, { A: () => l });
+var i = n(478676),
     r = n.n(i);
-let s = /\n$/,
-    a = /^ *-# +((?!(-#)+)[^\n]+?) *(?:\n|$)/,
-    o = {
+let a = /\n$/,
+    s = /^ *-# +((?!-#)[^\n]+)(?:\n|$)/,
+    l = {
         order: r().defaultRules.heading.order,
         requiredFirstCharacters: ["-"],
-        match: (e, t, n) => (null == n || "" === n || null != n.match(s) ? (0, i.anyScopeRegex)(a)(e, t, n) : null),
+        match: (e, t, n) =>
+            !1 === t.allowSubtext
+                ? null
+                : null == n || "" === n || null != n.match(a)
+                  ? (0, i.anyScopeRegex)(s)(e, t, n)
+                  : null,
         parse: function (e, t, n) {
-            return { content: (0, i.parseInline)(t, e[1].trim(), n) };
+            return { content: (0, i.parseInline)(t, e[1].trim(), { ...n, allowSubtext: !1 }) };
         },
     };
