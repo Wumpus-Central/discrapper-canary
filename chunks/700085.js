c2f558cfae857f00b8eab4f9fab0cc3cf38f9bf9
DiscrapperManager <admin@minerpl.xyz>
2026-08-11 17:58:17 +0000

    canaryRelease: 590937
    Main scripts: 363
    Lazy Loaded Scripts: 4507
    Chunks:
     * Main Files: 10143
     * Scripts: 9501
    Strings: 26379
    Routes: 858
    Experiments Count: 36
    Apex Experiments Count: 331

--- chunks/700085.js
+++ chunks/700085.js
@@ -1,16 +1,26 @@
-n.d(i, { default: () => l });
-var r = n(477900),
-    s = n(189213),
-    a = n(834730),
-    e = n(375708);
-function l(t) {
+n.d(i, { default: () => d });
+var s = n(477900),
+    r = n(189213),
+    e = n(834730),
+    a = n(780964),
+    l = n(766075),
+    o = n(375708);
+function d(t) {
     let { transitionState: i, onClose: n } = t;
-    return (0, r.jsx)(s.Modal, {
+    return (0, s.jsx)(r.Modal, {
         size: "sm",
         transitionState: i,
         onClose: n,
-        title: e.intl.string(e.t.WfwodX),
-        actions: [{ variant: "primary", onClick: () => n(), text: e.intl.string(e.t.hvVgAZ) }],
-        children: (0, r.jsx)(a.E, { variant: "text-md/normal", children: e.intl.string(e.t.I27WXW) }),
+        title: o.intl.string(o.t.WfwodX),
+        actions: [
+            {
+                variant: "primary",
+                onClick: () => {
+                    n(), (0, l.openUserSettings)(a.X.ACCOUNT_STANDING_PANEL);
+                },
+                text: o.intl.string(o.t.hvVgAZ),
+            },
+        ],
+        children: (0, s.jsx)(e.E, { variant: "text-md/normal", children: o.intl.string(o.t.I27WXW) }),
     });
 }
