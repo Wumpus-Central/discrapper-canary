e9e1243fd7ab590eba6a3793a057d4839a4a3354
DiscrapperManager <admin@minerpl.xyz>
2026-08-11 03:12:37 +0000

    canaryRelease: 590704
    Main scripts: 363
    Lazy Loaded Scripts: 4507
    Chunks:
     * Main Files: 10170
     * Scripts: 9514
    Strings: 26388
    Routes: 862
    Experiments Count: 36
    Apex Experiments Count: 332

--- chunks/681582.js
+++ chunks/681582.js
@@ -12,31 +12,70 @@ function u(e) {
     let { children: t } = e,
         [n, u] = r.useState(null),
         [_, E] = r.useState(null),
-        A = r.useRef(new Set()),
-        [h, I] = r.useState(!1),
-        f = (0, s.bG)([l.Ay], () => l.Ay.getCurrentlySelectedChannelId()),
-        p = r.useCallback((e) => {
-            A.current.delete(e), I(A.current.size > 0);
+        A = r.useRef(void 0),
+        h = r.useRef(new Set()),
+        [I, f] = r.useState(!1),
+        p = (0, s.bG)([l.Ay], () => l.Ay.getCurrentlySelectedChannelId()),
+        T = r.useCallback((e) => {
+            h.current.delete(e), f(h.current.size > 0);
         }, []),
-        T = r.useCallback((e) => (A.current.add(e), I(!0), () => p(e)), [p]),
-        m = r.useCallback((e, t) => {
-            for (let n of A.current) n(e, t);
+        m = r.useCallback((e) => (h.current.add(e), f(!0), () => T(e)), [T]),
+        g = r.useCallback((e, t) => {
+            for (let n of h.current) n(e, t);
         }, []);
+    r.useEffect(() => {
+        _?.clearConfetti();
+    }, [_, p]);
+    let S = r.useCallback((e) => {
+            window.clearTimeout(A.current),
+                (A.current = window.setTimeout(() => {
+                    (e.width = 0), (e.height = 0);
+                }, 1e4));
+        }, []),
+        N = r.useCallback((e) => {
+            window.clearTimeout(A.current);
+            let t = e.canvas;
+            if (0 === t.width && 0 === t.height) {
+                let { width: e, height: n } = t.getBoundingClientRect();
+                (t.width = e * window.devicePixelRatio), (t.height = n * window.devicePixelRatio);
+            }
+        }, []),
+        C = r.useCallback(
+            (e) => {
+                S(e.canvas);
+            },
+            [S],
+        );
     return (
         r.useEffect(() => {
-            _?.clearConfetti();
-        }, [_, f]),
+            let e = _?.getCanvas();
+            if (null == e) return;
+            let t = new ResizeObserver(() => S(e));
+            return (
+                t.observe(e),
+                () => {
+                    t.disconnect(), window.clearTimeout(A.current);
+                }
+            );
+        }, [_, S]),
         (0, i.jsxs)(i.Fragment, {
             children: [
                 (0, i.jsx)(o.k, {
                     confettiCanvas: _,
                     spriteCanvas: n,
                     baseConfig: d.Mw,
-                    addClickListener: T,
-                    removeClickListener: p,
+                    addClickListener: m,
+                    removeClickListener: T,
                     children: t,
                 }),
-                (0, i.jsx)(a.Fk, { ref: E, className: c.J, environment: d.XA, onClick: h ? m : void 0 }),
+                (0, i.jsx)(a.Fk, {
+                    ref: E,
+                    className: c.J,
+                    environment: d.XA,
+                    onClick: I ? g : void 0,
+                    onBeforeRender: N,
+                    onAfterRender: C,
+                }),
                 (0, i.jsx)(a.K_, { ref: u, colors: d._t, sprites: d.uI, spriteWidth: d.wn, spriteHeight: d.wn }),
             ],
         })
