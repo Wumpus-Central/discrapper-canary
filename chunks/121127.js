53513903b976087e8d980bbc684b0689b34965c9
DiscrapperManager <admin@minerpl.xyz>
2026-08-12 19:56:20 +0000

    canaryRelease: 591953
    Main scripts: 363
    Lazy Loaded Scripts: 4533
    Chunks:
     * Main Files: 10143
     * Scripts: 9500
    Strings: 26373
    Routes: 858
    Experiments Count: 36
    Apex Experiments Count: 325

--- chunks/121127.js
+++ chunks/121127.js
@@ -1,43 +1,44 @@
-n.d(t, { A: () => y });
+n.d(t, { A: () => I });
 var i = n(477900),
     a = n(582128),
     d = n(17928),
-    r = n(477782),
-    l = n(228366),
-    s = n(901472),
-    p = n(985925),
-    c = n(267102),
+    l = n(477782),
+    r = n(192308),
+    s = n(228366),
+    o = n(901472),
+    c = n(985925),
+    p = n(267102),
     h = n(474397),
-    o = n(378570),
-    u = n(761640),
-    C = n(734057),
-    A = n(309010),
-    _ = n(174459),
-    b = n(652215),
-    E = n(746080),
-    g = n(486974),
-    k = n(375708);
-function y(e) {
-    let { userId: t, guildId: n, channelId: y, location: I, onAction: O, appContext: S } = e,
-        j = a.useContext(_.AnalyticsContext),
-        w = (0, c.aL)(),
-        L = (0, d.bG)([A.Ay], () => y ?? A.Ay.getChannelId(n, !0), [y, n]),
-        x = (0, p.q)(n);
-    return null != n && x
-        ? (0, i.jsx)(r.Dr, {
+    u = n(378570),
+    C = n(761640),
+    A = n(734057),
+    _ = n(309010),
+    b = n(174459),
+    E = n(652215),
+    g = n(746080),
+    k = n(486974),
+    y = n(375708);
+function I(e) {
+    let { userId: t, guildId: n, channelId: I, location: O, onAction: S, appContext: j } = e,
+        w = a.useContext(b.AnalyticsContext),
+        L = (0, p.aL)(),
+        x = (0, d.bG)([_.Ay], () => I ?? _.Ay.getChannelId(n, !0), [I, n]),
+        z = (0, c.q)(n);
+    return null != n && z
+        ? (0, i.jsx)(l.Dr, {
               id: "mod-view",
-              label: k.intl.string(k.t.kj3tz2),
+              label: y.intl.string(y.t.kj3tz2),
               action: () => {
-                  O?.(), (0, h.A)(S), w.dispatch(b.jej.POPOUT_CLOSE);
-                  let e = C.A.getChannel(L),
-                      i = { modViewPanel: g.g.INFO, sourceLocation: I ?? j.location };
+                  S?.(), (0, h.A)(j), L.dispatch(E.jej.POPOUT_CLOSE), (0, r.closeAllModals)();
+                  let e = A.A.getChannel(x),
+                      i = { modViewPanel: k.g.INFO, sourceLocation: O ?? w.location };
                   if (e?.isThread() && null != e.parent_id) {
-                      u.Ay.getCurrentSidebarChannelId(e.parent_id) === e.id &&
-                          (l.h.dispatch({ type: "SIDEBAR_CLOSE", baseChannelId: e.parent_id }), (0, o.iN)(e.id)),
-                          (0, s.z)(n, t, e.parent_id, i);
+                      C.Ay.getCurrentSidebarChannelId(e.parent_id) === e.id &&
+                          (s.h.dispatch({ type: "SIDEBAR_CLOSE", baseChannelId: e.parent_id }), (0, u.iN)(e.id)),
+                          (0, o.z)(n, t, e.parent_id, i);
                       return;
                   }
-                  (0, s.z)(n, t, L ?? E.VV.MEMBER_SAFETY, i);
+                  (0, o.z)(n, t, x ?? g.VV.MEMBER_SAFETY, i);
               },
           })
         : null;
