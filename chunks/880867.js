35914407350cf7729cea00654623b37323eda97c
DiscrapperManager <admin@minerpl.xyz>
2026-08-14 22:30:23 +0000

    canaryRelease: 593796
    Main scripts: 369
    Lazy Loaded Scripts: 4541
    Chunks:
     * Main Files: 10210
     * Scripts: 9523
    Strings: 26441
    Routes: 859
    Experiments Count: 35
    Apex Experiments Count: 324

--- chunks/880867.js
+++ chunks/880867.js
@@ -1,90 +1,107 @@
 l.d(t, { default: () => I });
-var a = l(477900),
-    i = l(582128),
-    s = l(691540),
-    n = l(857250),
-    r = l(97483),
-    o = l(192308),
-    c = l(148494),
-    d = l(953832),
-    u = l(451909),
-    m = l(378570),
+var i = l(477900),
+    a = l(582128),
+    n = l(691540),
+    s = l(857250),
+    o = l(97483),
+    r = l(192308),
+    c = l(173936),
+    d = l(148494),
+    u = l(953832),
+    m = l(451909),
+    C = l(378570),
     h = l(223863),
     p = l(734057),
-    x = l(403362),
-    C = l(97808),
-    f = l(778712),
-    k = l(834730),
-    v = l(427262),
+    x = l(957565),
+    f = l(403362),
+    k = l(97808),
+    S = l(778712),
+    v = l(834730),
+    g = l(427262),
     A = l(19575),
     E = l(999291),
-    S = l(718019),
-    g = l(918709);
-let w = A.Ay.getEnableHardwareAcceleration() ? C.Js : C.eu,
-    j = f._3.SIZE_40;
-function y(e) {
+    w = l(718019),
+    y = l(918709);
+let j = A.Ay.getEnableHardwareAcceleration() ? k.Js : k.eu,
+    P = S._3.SIZE_40;
+function b(e) {
     let { user: t } = e,
         l = (0, E._c)(t.id),
-        { avatarProps: i, eventHandlers: s } = (0, S.V)({ user: t, displayProfile: l, avatarSize: j }),
-        n = v.Ay.useName(t),
-        r = v.Ay.useUserTag(t);
-    return (0, a.jsxs)("div", {
-        className: g.V,
+        { avatarProps: a, eventHandlers: n } = (0, w.V)({ user: t, displayProfile: l, avatarSize: P }),
+        s = g.Ay.useName(t),
+        o = g.Ay.useUserTag(t);
+    return (0, i.jsxs)("div", {
+        className: y.V,
         children: [
-            (0, a.jsx)(w, { ...i, ...s }),
-            (0, a.jsxs)("div", {
-                className: g.D,
+            (0, i.jsx)(j, { ...a, ...n }),
+            (0, i.jsxs)("div", {
+                className: y.D,
                 children: [
-                    (0, a.jsx)(k.E, { variant: "text-md/medium", color: "text-default", lineClamp: 1, children: n }),
-                    (0, a.jsx)(k.E, { variant: "text-sm/medium", color: "text-muted", lineClamp: 1, children: r }),
+                    (0, i.jsx)(v.E, { variant: "text-md/medium", color: "text-default", lineClamp: 1, children: s }),
+                    (0, i.jsx)(v.E, { variant: "text-sm/medium", color: "text-muted", lineClamp: 1, children: o }),
                 ],
             }),
         ],
     });
 }
-var V = l(652215),
-    _ = l(381941),
-    b = l(375708);
+var U = l(652215),
+    V = l(381941),
+    _ = l(375708);
 function I(e) {
-    let { user: t, source: l, onClose: C, ...f } = e,
-        k = `${location.protocol}//${location.host}${V.BVt.USERS(t.id)}`,
-        v = i.useCallback(
+    let { user: t, source: l, onClose: k, ...S } = e,
+        v = `${location.protocol}//${location.host}${U.BVt.USERS(t.id)}`,
+        g = a.useCallback(
             async (e, t, l) => {
-                let { withMessage: a, transitionToDestination: i, closeAfterSend: d } = t;
+                let { withMessage: i, transitionToDestination: a, closeAfterSend: c } = t;
                 l(!0);
                 try {
-                    let t = (await Promise.all(e.map(h.pk))).filter(x.Vq);
+                    let t = (await Promise.all(e.map(h.pk))).filter(f.Vq);
                     if (0 === t.length) return void l(!1);
-                    d && C();
-                    let f =
-                        null != a && "" !== a
-                            ? `${k}
+                    c && k();
+                    let u =
+                        null != i && "" !== i
+                            ? `${v}
 
-${a}`
-                            : k;
+${i}`
+                            : v;
                     for (let e of t) {
                         let t = p.A.getChannel(e);
                         null != t &&
-                            (await c.A.sendMessage(t.id, u.Ay.parse(t, f), !1, { location: _.Hx.USER_PROFILE }));
+                            (await d.A.sendMessage(t.id, m.Ay.parse(t, u), !1, { location: V.Hx.USER_PROFILE }));
                     }
-                    (0, s.P0)((0, n.o)(b.intl.string(b.t.kwmYkt), r.Ck.SUCCESS)),
-                        i &&
+                    (0, n.P0)((0, s.o)(_.intl.string(_.t.kwmYkt), o.Ck.SUCCESS)),
+                        a &&
                             null != t[0] &&
-                            ((0, m.iN)(t[0], { openTextInVoiceIfVoiceChannel: !0 }), (0, o.closeAllModals)());
+                            ((0, C.iN)(t[0], { openTextInVoiceIfVoiceChannel: !0 }), (0, r.closeAllModals)());
                 } catch (e) {
-                    (0, s.P0)((0, n.o)(b.intl.string(b.t.iufib1), r.Ck.FAILURE));
+                    (0, n.P0)((0, s.o)(_.intl.string(_.t.iufib1), o.Ck.FAILURE));
                 } finally {
                     l(!1);
                 }
             },
-            [C, k],
+            [k, v],
+        ),
+        A = a.useMemo(
+            () => [
+                {
+                    variant: "secondary",
+                    text: void 0,
+                    "aria-label": _.intl.string(_.t.WqhZss),
+                    onClick: () => {
+                        (0, x.C)(v, () => (0, n.P0)((0, s.o)(_.intl.string(_.t["L/PwZf"]), o.Ck.SUCCESS)));
+                    },
+                    icon: c.q,
+                },
+            ],
+            [v],
         );
-    return (0, a.jsx)(d.ForwardModal, {
-        ...f,
-        onClose: C,
+    return (0, i.jsx)(u.ForwardModal, {
+        ...S,
+        onClose: k,
         source: l,
-        customTitle: b.intl.string(b.t["sFN1/M"]),
-        customPreview: (0, a.jsx)(y, { user: t }),
-        customSendHandler: v,
+        customTitle: _.intl.string(_.t["sFN1/M"]),
+        customPreview: (0, i.jsx)(b, { user: t }),
+        customSendHandler: g,
+        additionalActions: A,
     });
 }
