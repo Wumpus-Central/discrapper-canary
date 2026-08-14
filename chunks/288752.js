d642b78572605de5143d407ecaf178e0a3e67a71
DiscrapperManager <admin@minerpl.xyz>
2026-08-14 00:17:44 +0000

    canaryRelease: 593147
    Main scripts: 369
    Lazy Loaded Scripts: 4534
    Chunks:
     * Main Files: 10195
     * Scripts: 9500
    Strings: 26402
    Routes: 858
    Experiments Count: 35
    Apex Experiments Count: 327

--- chunks/288752.js
+++ chunks/288752.js
@@ -1,4 +1,4 @@
-o.r(t), o.d(t, { default: () => i });
-let i = JSON.parse(
-    '{"WnnzG7":["Select number of gifts"],"P+DDLh":["Get 5,000 Orbs"],"dOZh6X":["When you complete this gift purchase"],"+utqaz":["Gift Background Option: ",[1,"giftStyle"]],"B9XqQk":["Box"],"AJ4iir":["Cake"],"P5keo3":["Chest"],"w84vET":["Coffee"],"6dCq/u":["Cup"],"+HMF8k":["Nitroween"],"vd1fu/":["Holiday Box"],"aubYGR":["Holiday Cake"],"vjxYqU":["Holiday Chest"],"bHuJLa":["Holiday Coffee"],"M6cPwB":["Snowglobe"],"GzPel1":["Box"],"bGKjmg":["Got It!"]}',
+t.r(o), t.d(o, { default: () => a });
+let a = JSON.parse(
+    '{"WnnzG7":["Select number of gifts"],"P+DDLh":["Get 5,000 Orbs"],"aMeOMj":["You can spend them in the Shop!"],"dOZh6X":["When you complete this gift purchase"],"ZME/HA":["You unlocked 5,000 Orbs."],"+utqaz":["Gift Background Option: ",[1,"giftStyle"]],"B9XqQk":["Box"],"AJ4iir":["Cake"],"P5keo3":["Chest"],"w84vET":["Coffee"],"6dCq/u":["Cup"],"+HMF8k":["Nitroween"],"vd1fu/":["Holiday Box"],"aubYGR":["Holiday Cake"],"vjxYqU":["Holiday Chest"],"bHuJLa":["Holiday Coffee"],"M6cPwB":["Snowglobe"],"GzPel1":["Box"],"bGKjmg":["Got It!"]}',
 );
