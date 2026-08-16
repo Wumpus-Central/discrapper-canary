b3322fafc701ecf1bcceed6bfe9058c01efc90f7
DiscrapperManager <admin@minerpl.xyz>
2026-08-16 03:06:47 +0000

    canaryRelease: 594086
    Main scripts: 557
    Lazy Loaded Scripts: 4741
    Chunks:
     * Main Files: 10260
     * Scripts: 10674
    Strings: 26445
    Routes: 859
    Experiments Count: 35
    Apex Experiments Count: 322

--- chunks/135109.js
+++ chunks/135109.js
@@ -1,99 +1,158 @@
-a.r(i), a.d(i, { default: () => T });
-var l = a(477900),
-    d = a(582128),
-    n = a(17928),
-    t = a(834730),
-    s = a(47167),
-    c = a(726249),
-    r = a(742589),
-    o = a(71393),
-    u = a(683180),
-    h = a(866665),
-    p = a(408278),
-    x = a(811893),
-    m = a(47294),
-    I = a(16961),
-    v = a(672929),
-    g = a(228366),
-    A = a(494126),
-    _ = a(91242),
-    f = a(5867);
-async function j(e) {
-    null == _.A.getFrame(e) ||
-        ((await (0, A.refreshProxyTicket)(e)) &&
-            ((0, A.promoteFrame)(e),
-            (0, A.updateFramePanelMode)(e, f.Gd.ACTIVITY_POPOUT_WINDOW),
-            g.h.dispatch({ type: "ACTIVITY_POPOUT_WINDOW_OPEN" })));
+l.r(i), l.d(i, { default: () => M });
+var a = l(477900),
+    n = l(582128),
+    t = l(17928),
+    d = l(834730),
+    r = l(47167),
+    s = l(726249),
+    c = l(742589),
+    u = l(71393),
+    o = l(683180),
+    h = l(866665),
+    x = l(408278),
+    p = l(663417),
+    A = l(811893),
+    m = l(789645),
+    I = l(47294),
+    g = l(16961),
+    f = l(625180),
+    N = l(91242),
+    C = l(580954),
+    v = l(672929),
+    j = l(228366),
+    y = l(494126),
+    T = l(5867);
+async function _(e) {
+    null == N.A.getFrame(e) ||
+        ((await (0, y.refreshProxyTicket)(e)) &&
+            ((0, y.promoteFrame)(e),
+            (0, y.updateFramePanelMode)(e, T.Gd.ACTIVITY_POPOUT_WINDOW),
+            j.h.dispatch({ type: "ACTIVITY_POPOUT_WINDOW_OPEN" })));
 }
-var y = a(165610),
-    N = a(818085),
-    b = a(375708);
-function P(e) {
-    let { applicationId: i, surface: a } = e,
-        n = (0, v.A)(i, a),
-        t = (0, I.G)(),
-        s = d.useCallback(() => {
-            null != n && (0, m.A)({ onConfirm: () => j(n.id) });
-        }, [n]);
-    return t && (0, y.x1)(n)
-        ? (0, l.jsx)(h.m, {
-              text: b.intl.string(N.default["6g22y7"]),
-              ariaHidden: !0,
-              children: (0, l.jsx)(p.K, {
-                  "aria-label": b.intl.string(N.default["6g22y7"]),
-                  icon: x.t,
-                  variant: "icon-only",
-                  size: "sm",
-                  onClick: s,
-              }),
-          })
-        : null;
+var P = l(574172),
+    b = l(869146),
+    k = l(976860),
+    O = l(808728),
+    E = l(576705),
+    F = l(652215),
+    V = l(165610),
+    H = l(818085),
+    B = l(375708);
+function L(e) {
+    let { applicationId: i, surface: l, guildId: t } = e,
+        d = (0, v.A)(i, l),
+        r = (0, g.G)(),
+        s = (0, V.x1)(d) && d.data.proxyTicketRefreshing,
+        c = n.useCallback(() => {
+            null == d || s || f.A.refreshProxyTicket(d.id);
+        }, [d, s]),
+        u = n.useCallback(() => {
+            null != d && (0, I.A)({ onConfirm: () => _(d.id) });
+        }, [d]),
+        o = n.useCallback(() => {
+            let e = d?.id;
+            null != e &&
+                b.A.getWindowOpen(F.MLl.ACTIVITY_POPOUT) &&
+                N.A.getMainFrame()?.id === e &&
+                (0, P.close)(F.MLl.ACTIVITY_POPOUT),
+                (0, C.A)().leaveFrame(e),
+                (0, k.pX)(
+                    (function (e, i) {
+                        if (null == e) return F.BVt.FRIENDS;
+                        let l = O.Ay.getDefaultChannel(e);
+                        if (null != l && l.id !== i) return F.BVt.CHANNEL(e, l.id);
+                        let a = O.Ay.getFirstChannel(e, (e) => {
+                            let { channel: l } = e;
+                            return l.id !== i && E.A.can(F.xBc.VIEW_CHANNEL, l);
+                        });
+                        return null != a ? F.BVt.CHANNEL(e, a.id) : F.BVt.FRIENDS;
+                    })(t, (0, V.h)(l)),
+                );
+        }, [d?.id, t, l]);
+    if (!(0, V.x1)(d)) return null;
+    let j = B.intl.string(H.default.xKexN1),
+        y = B.intl.string(H.default["6g22y7"]),
+        T = B.intl.string(H.default.Ea0Wrr);
+    return (0, a.jsxs)(a.Fragment, {
+        children: [
+            (0, a.jsx)(h.m, {
+                text: j,
+                ariaHidden: !0,
+                children: (0, a.jsx)(x.K, {
+                    "aria-label": j,
+                    icon: p.f,
+                    variant: "icon-only",
+                    size: "sm",
+                    loading: s,
+                    onClick: c,
+                }),
+            }),
+            r
+                ? (0, a.jsx)(h.m, {
+                      text: y,
+                      ariaHidden: !0,
+                      children: (0, a.jsx)(x.K, {
+                          "aria-label": y,
+                          icon: A.t,
+                          variant: "icon-only",
+                          size: "sm",
+                          onClick: u,
+                      }),
+                  })
+                : null,
+            (0, a.jsx)(h.m, {
+                text: T,
+                ariaHidden: !0,
+                children: (0, a.jsx)(x.K, { "aria-label": T, icon: m.P, variant: "icon-only", size: "sm", onClick: o }),
+            }),
+        ],
+    });
 }
-var k = a(750896),
-    C = a(957907),
-    O = a(448922);
-function T(e) {
+var W = l(142882),
+    w = l(957907),
+    D = l(448922);
+function M(e) {
     let { channel: i } = e,
-        a = (0, u.B9)(i.topic),
-        h = (0, C.A)(i),
-        p = (0, s.Ay)(i),
-        x = (0, n.bG)([o.A], () => o.A.getGuild(i.guild_id), [i.guild_id]),
-        m = d.useMemo(() => ({ type: y.U4.APP_CHANNEL, channelId: i.id, guildId: i.guild_id ?? void 0 }), [i]);
-    return null == a
+        l = (0, o.B9)(i.topic),
+        h = (0, w.A)(i),
+        x = (0, r.Ay)(i),
+        p = (0, t.bG)([u.A], () => u.A.getGuild(i.guild_id), [i.guild_id]),
+        A = n.useMemo(() => ({ type: V.U4.APP_CHANNEL, channelId: i.id, guildId: i.guild_id ?? void 0 }), [i]);
+    return null == l
         ? null
-        : (0, l.jsxs)("div", {
-              className: O.kL,
+        : (0, a.jsxs)("div", {
+              className: D.kL,
               children: [
-                  (0, l.jsx)(c.HI, { location: x?.name, subsection: p ?? void 0 }),
-                  (0, l.jsx)("div", {
-                      className: O.N1,
-                      children: (0, l.jsx)(
-                          r.A,
+                  (0, a.jsx)(s.HI, { location: p?.name, subsection: x ?? void 0 }),
+                  (0, a.jsx)("div", {
+                      className: D.N1,
+                      children: (0, a.jsx)(
+                          c.A,
                           {
                               guildId: i.guild_id,
                               channelId: i.id,
                               channelType: i.type,
                               hideSearch: !0,
-                              toolbar: (0, l.jsx)(P, { applicationId: a, surface: m }),
-                              className: O.wx,
-                              "aria-label": p ?? void 0,
-                              children: (0, l.jsx)(t.E, {
+                              toolbar: (0, a.jsx)(L, { applicationId: l, surface: A, guildId: i.guild_id }),
+                              className: D.wx,
+                              "aria-label": x ?? void 0,
+                              children: (0, a.jsx)(d.E, {
                                   variant: "text-md/semibold",
                                   color: "text-default",
-                                  className: O.Bv,
-                                  children: p,
+                                  className: D.Bv,
+                                  children: x,
                               }),
                           },
                           `header-${i.id}`,
                       ),
                   }),
-                  (0, l.jsx)("div", {
-                      className: O.Ix,
+                  (0, a.jsx)("div", {
+                      className: D.Ix,
                       "data-vibegrations-channel": !0,
-                      children: (0, l.jsx)(k.A, {
+                      children: (0, a.jsx)(W.A, {
                           projectId: h?.id ?? null,
-                          applicationId: a,
-                          surface: m,
+                          applicationId: l,
+                          surface: A,
                           chatOpen: !1,
                           previewReady: !0,
                       }),
