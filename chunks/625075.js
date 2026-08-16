68c9bfa4a5a2e71744c34101d6c57526f15bfcac
DiscrapperManager <admin@minerpl.xyz>
2026-08-16 01:15:13 +0000

    canaryRelease: 594065
    Main scripts: 557
    Lazy Loaded Scripts: 4741
    Chunks:
     * Main Files: 10260
     * Scripts: 10674
    Strings: 26443
    Routes: 859
    Experiments Count: 35
    Apex Experiments Count: 322

--- chunks/625075.js
+++ chunks/625075.js
@@ -1,7 +1,7 @@
 "use strict";
-n.d(t, { B: () => r, k: () => a });
+n.d(t, { M: () => r, k: () => a });
 var i = n(945810);
-let r = n(975571).A.getArticleURL("42704051358359"),
+let r = "https://discord.com/blog/a-letter-to-the-discord-community-in-brazil",
     a = (0, i.mj)({
         name: "2026-08-video-guard",
         kind: "user",
