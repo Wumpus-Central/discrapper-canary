f3e7692ae967065128cfd70666d101762864f5ef
DiscrapperManager <admin@minerpl.xyz>
2026-09-01 12:51:26 +0200

    canaryRelease: 605162
    Main scripts: 571
    Lazy Loaded Scripts: 4903
    Chunks:
     * Main Files: 10366
     * Scripts: 10899
    Strings: 26993
    Routes: 873
    Experiments Count: 35
    Apex Experiments Count: 333

--- chunks/271995.js
+++ chunks/271995.js
@@ -1,95 +1,85 @@
-s.d(e, { $b: () => E, QB: () => g, _k: () => _, bh: () => x }), s(582128);
+s.d(e, { QB: () => x, _k: () => _, aO: () => g, bh: () => E }), s(582128);
 var n = s(661531),
     i = s(628284),
     r = s(695366),
     a = s(285796),
     l = s(738188),
-    c = s(290136),
-    o = s(975807),
+    o = s(290136),
+    c = s(975807),
     d = s(739010),
     u = s(335891),
-    N = s(259960),
-    m = s(239093),
-    h = s(375708);
-function g() {
+    m = s(259960),
+    h = s(239093),
+    N = s(375708);
+function x() {
     return {
         [d.PN.ALL_GOOD]: {
-            title: h.t.uaKrRi,
-            description: h.intl.format(h.t.pEdBD4, {
-                termsOfService: m.d$.TOS_LINK,
-                communityGuidelines: m.d$.COMMUNITY_GUIDELINES,
+            title: N.t.uaKrRi,
+            description: N.intl.format(N.t.pEdBD4, {
+                termsOfService: h.d$.TOS_LINK,
+                communityGuidelines: h.d$.COMMUNITY_GUIDELINES,
             }),
-            status: h.t["/Idfao"],
+            status: N.t["/Idfao"],
             Icon: i.y,
             color: n.A.colors.STATUS_POSITIVE,
         },
         [d.PN.LIMITED]: {
-            title: h.t.epkcmS,
-            description: h.intl.string(h.t["774juc"]),
-            status: h.t.umleq4,
+            title: N.t.epkcmS,
+            description: N.intl.string(N.t["774juc"]),
+            status: N.t.umleq4,
             Icon: r.E,
             color: n.A.colors.STATUS_WARNING,
         },
         [d.PN.VERY_LIMITED]: {
-            title: h.t.crzE2X,
-            description: h.intl.string(h.t["T/Ufh9"]),
-            status: h.t.WBtMHf,
+            title: N.t.crzE2X,
+            description: N.intl.string(N.t["T/Ufh9"]),
+            status: N.t.WBtMHf,
             Icon: r.E,
             color: n.A.unsafe_rawColors.ORANGE_345,
         },
         [d.PN.AT_RISK]: {
-            title: h.t.XRNVzO,
-            description: h.intl.string(h.t["hbH+9S"]),
-            status: h.t["7f+4Lg"],
+            title: N.t.XRNVzO,
+            description: N.intl.string(N.t["hbH+9S"]),
+            status: N.t["7f+4Lg"],
             Icon: r.E,
             color: n.A.colors.ICON_FEEDBACK_CRITICAL,
         },
         [d.PN.SUSPENDED]: {
-            title: h.t.MExFkz,
-            description: h.intl.string(h.t["2liUvt"]),
-            status: h.t["0OONGB"],
+            title: N.t.MExFkz,
+            description: N.intl.string(N.t["2liUvt"]),
+            status: N.t["0OONGB"],
             Icon: a.a,
             color: n.A.colors.ICON_MUTED,
         },
     };
 }
+function g() {
+    let t = (0, u.K)();
+    if (!(0, m.v)()) return null;
+    let { status: e } = x()[t.state];
+    return N.intl.formatToPlainString(e, { hook: (t) => t });
+}
 function E() {
     let t = (0, u.K)();
-    if (!(0, N.v)()) return h.intl.string(h.t.ZTNur7);
+    if (!(0, m.v)()) return N.intl.string(N.t["9m2Bkk"]);
     switch (t.state) {
         case d.PN.ALL_GOOD:
-            return h.intl.string(h.t["0Djcou"]);
-        case d.PN.LIMITED:
-            return h.intl.string(h.t["4AV3TM"]);
-        case d.PN.VERY_LIMITED:
-            return h.intl.string(h.t["aUNu/o"]);
-        case d.PN.AT_RISK:
-            return h.intl.string(h.t.j1eOqz);
-        case d.PN.SUSPENDED:
-            return h.intl.string(h.t.r77e2V);
-    }
-}
-function x() {
-    let t = (0, u.K)();
-    if (!(0, N.v)()) return h.intl.string(h.t["9m2Bkk"]);
-    switch (t.state) {
-        case d.PN.ALL_GOOD:
-            return h.intl.format(h.t.pEdBD4, {
+            return N.intl.format(N.t.pEdBD4, {
                 termsOfService: (t) => {
-                    t.stopPropagation(), (0, o.A)(m.d$.TOS_LINK);
+                    t.stopPropagation(), (0, c.A)(h.d$.TOS_LINK);
                 },
                 communityGuidelines: (t) => {
-                    t.stopPropagation(), (0, o.A)(m.d$.COMMUNITY_GUIDELINES);
+                    t.stopPropagation(), (0, c.A)(h.d$.COMMUNITY_GUIDELINES);
                 },
             });
         case d.PN.LIMITED:
-            return h.intl.string(h.t["774juc"]);
+            return N.intl.string(N.t["774juc"]);
         case d.PN.VERY_LIMITED:
-            return h.intl.string(h.t["T/Ufh9"]);
+            return N.intl.string(N.t["T/Ufh9"]);
         case d.PN.AT_RISK:
-            return h.intl.string(h.t["hbH+9S"]);
+            return N.intl.string(N.t["hbH+9S"]);
         case d.PN.SUSPENDED:
-            return h.intl.string(h.t["2liUvt"]);
+            return N.intl.string(N.t["2liUvt"]);
     }
 }
 let I = {
@@ -115,8 +105,8 @@ let I = {
         },
         [d.PN.SUSPENDED]: { color: n.A.colors.ICON_MUTED, backgroundColor: n.A.colors.BACKGROUND_MOD_MUTED, Icon: a.a },
     },
-    A = { color: n.A.colors.ICON_MUTED, backgroundColor: n.A.colors.BACKGROUND_MOD_MUTED, Icon: c.CircleQuestionIcon };
+    A = { color: n.A.colors.ICON_MUTED, backgroundColor: n.A.colors.BACKGROUND_MOD_MUTED, Icon: o.CircleQuestionIcon };
 function _() {
     let t = (0, u.K)();
-    return (0, N.v)() ? I[t.state] : A;
+    return (0, m.v)() ? I[t.state] : A;
 }
